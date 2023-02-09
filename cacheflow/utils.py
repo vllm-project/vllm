5e644b912ec10ec0fac02ccdc0f9a4d8342d8f4c
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2023-02-09 11:26:50 +0000

    Add utils

---
+++ cacheflow/utils.py
@@ -0,0 +1,20 @@
+import enum
+
+
+class Device(enum.Enum):
+    GPU = enum.auto()
+    CPU = enum.auto()
+
+
+class Counter:
+
+    def __init__(self, start: int = 0) -> None:
+        self.counter = start
+
+    def next(self) -> int:
+        id = self.counter
+        self.counter += 1
+        return id
+
+    def reset(self) -> None:
+        self.counter = 0
