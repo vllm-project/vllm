5a64bab0ebc4040f2c8387c2b2903fab2b7ec6b6
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2023-02-16 09:24:42 +0000

    Remove redundant fn
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- csrc/cache.cpp
+++ csrc/cache.cpp
@@ -5,16 +5,9 @@ void copy_blocks(
   torch::Tensor& dst,
   const std::map<int64_t, int64_t>& block_mapping);
 
-void copy_cache_blocks(
-  torch::Tensor& src,
-  torch::Tensor& dst,
-  const std::map<int64_t, int64_t>& block_mapping) {
-  copy_blocks(src, dst, block_mapping);
-}
-
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def(
     "copy_cache_blocks",
-    &copy_cache_blocks,
+    &copy_blocks,
     "Copy the cache blocks from src to dst");
 }
