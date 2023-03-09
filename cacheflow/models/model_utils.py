90970e1b25d7c1451869d240d585748aa64465c3
Zhuohan Li <zhuohan123@gmail.com>
2023-03-09 07:50:45 +0000

    support multi-gpu tensor parallelism

--- cacheflow/models/model_utils.py
+++ cacheflow/models/model_utils.py
@@ -1,14 +1,9 @@
-import os
-import glob
-import shutil
 from typing import Union
-from tqdm import tqdm
 
 import numpy as np
 import torch
 import torch.nn as nn
 from transformers import AutoConfig
-from huggingface_hub import snapshot_download
 
 from cacheflow.models.opt import OPTForCausalLM
 
@@ -34,39 +29,12 @@ def get_model(
         torch_dtype = dtype
     torch.set_default_dtype(torch_dtype)
     config = AutoConfig.from_pretrained(model_name)
-    weights_dir = download_opt_weights(model_name)
     for model_class_name, model_class in MODEL_CLASSES.items():
         if model_class_name in model_name:
             model = model_class(config)
+            weights_dir = model_class.download_weights(model_name)
             model.load_weights(weights_dir)
             return model.eval(), torch_dtype
     raise ValueError(f'Invalid model name: {model_name}')
 
 
-def download_opt_weights(model_name: str, path: str = "/tmp/transformers"):
-    path = os.path.join(path, f"{model_name}-np")
-    path = os.path.abspath(os.path.expanduser(path))
-    test_weight_path = os.path.join(path, "model.decoder.embed_positions.weight")
-    if os.path.exists(test_weight_path):
-        return path
-
-    folder = snapshot_download(model_name, allow_patterns="*.bin")
-    bin_files = glob.glob(os.path.join(folder, "*.bin"))
-
-    if "/" in model_name:
-        model_name = model_name.split("/")[1].lower()
-    os.makedirs(path, exist_ok=True)
-
-    for bin_file in tqdm(bin_files, desc="Convert format"):
-        state = torch.load(bin_file)
-        for name, param in tqdm(state.items(), leave=False):
-            param_path = os.path.join(path, name)
-            with open(param_path, "wb") as f:
-                np.save(f, param.cpu().detach().numpy())
-
-            # shared embedding
-            if "model.decoder.embed_tokens.weight" in name:
-                shutil.copy(param_path, param_path.replace(
-                    "model.decoder.embed_tokens.weight", "lm_head.weight"))
-
-    return path
