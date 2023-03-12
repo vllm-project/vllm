96b216cf20a85ddc487c0be54c8b1f2fbea98d42
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2023-03-12 07:11:56 +0000

    Fix __init__

--- cacheflow/models/__init__.py
+++ cacheflow/models/__init__.py
@@ -1,14 +1,12 @@
 from cacheflow.models.input_metadata import InputMetadata
-from cacheflow.models.memory_analyzer import compute_max_num_cpu_blocks
-from cacheflow.models.memory_analyzer import compute_max_num_gpu_blocks
+from cacheflow.models.model_utils import get_memory_analyzer
 from cacheflow.models.model_utils import get_model
-from cacheflow.models.model_utils import set_seed
+from cacheflow.models.utils import set_seed
 
 
 __all__ = [
     'InputMetadata',
-    'compute_max_num_cpu_blocks',
-    'compute_max_num_gpu_blocks',
+    'get_memory_analyzer',
     'get_model',
     'set_seed',
 ]
