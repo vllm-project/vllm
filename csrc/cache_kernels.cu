82fc4f42d48726eb7aafc86d918d2b00cc6a3b9a
Siyuan <suquark@gmail.com>
2023-04-09 01:49:30 -0700

    update

--- csrc/cache_kernels.cu
+++ csrc/cache_kernels.cu
@@ -272,84 +272,44 @@ __global__ void gather_cached_kv_kernel(
 // Grid: (num_blocks, block_size).
 template<typename scalar_t>
 __global__ void gather_cached_kv_kernel_2(
-  scalar_t* __restrict__ out,             // [cu_seqlens_k[-1], 3(QKV), num_heads, head_size]
+  scalar_t* __restrict__ key,             // [num_tokens, [stride], num_heads, head_size]
+  scalar_t* __restrict__ value,           // [num_tokens, [stride], num_heads, head_size]
   const scalar_t* __restrict__ k_cache,   // [num_blocks, num_heads, head_size/x, block_size, x]
   const scalar_t* __restrict__ v_cache,   // [num_blocks, num_heads, head_size, block_size]
-  const int* __restrict__ block_tables,   // [num_seqs, max_num_blocks_per_seq]
-  const int* __restrict__ cu_seqlens_k,   // aka 'cu_seqlens_k' in '_flash_attn_forward', or 'context_lens' in cacheflow
-  const int num_seqs,
-  const int max_num_blocks_per_seq,
+  const int* __restrict__ slot_mapping,   // [num_tokens]
+  const int key_stride,
+  const int value_stride,
   const int num_heads,
-  const int head_size) {
-    // Each CUDA gird is mapped to (num_blocks, num_heads).
-    const int block_idx = blockIdx.x;
-    const int num_blocks = gridDim.x;
-    const int block_offset = blockIdx.y;
-    const int block_size = gridDim.y;
-    // Each CUDA block is responsible for (head_size, block_size).
-    const int thread_idx = threadIdx.x;
-    const int num_threads = blockDim.x;
-    // in the original attention kernel, each thread group fetch x elements at a time.
-    constexpr int x = 16 / sizeof(scalar_t);
+  const int head_size,
+  const int block_size,
+  const int x) {
+    const int token_idx = blockIdx.x;
+    const int slot_idx = slot_mapping[token_idx];
+    const int block_idx = slot_idx / block_size;
+    const int block_offset = slot_idx % block_size;
 
-    // the index of the sequence this thread is working on.
-    int seq_idx;
-    // the index of the block in the sequence this thread is working on.
-    int local_block_idx;
-    // calculate the sequence index and block index in the sequence.
-    int num_total_blocks = 0;
-#pragma unroll
-    for (int i = 0; i < num_seqs; ++i) {
-      int context_len = cu_seqlens_k[i + 1] - cu_seqlens_k[i];
-      int num_blocks = (context_len + block_size - 1) / block_size;
-      num_total_blocks += num_blocks;
-      if (num_total_blocks > block_idx) {
-        seq_idx = i;
-        local_block_idx = block_idx - (num_total_blocks - num_blocks);
-        break;
-      }
-    }
-
-    // const int context_len = cu_seqlens_k[seq_idx];
-    // const int num_blocks = (context_len + block_size - 1) / block_size;
-    const int* block_table = block_tables + seq_idx * max_num_blocks_per_seq;
-    const int physical_block_number = block_table[local_block_idx];
-    const int physical_cache_offset = physical_block_number * num_heads * head_size * block_size;
-
-    // The common output pointer base used by both key and value:
-    scalar_t* common_out = out + (block_idx * block_size + block_offset) * 3 * num_heads * head_size;
-    // key is the second tensor in QKV, so qkv_offset = 1
-    scalar_t* key_out = common_out + 1 * num_heads * head_size;
-    // value is the third tensor in QKV, so qkv_offset = 2
-    scalar_t* value_out = common_out + 2 * num_heads * head_size;
-
-    // process key in chunks
-#pragma unroll
-    for (int i = threadIdx.x; i < num_heads * head_size; i += blockDim.x) {
-      // calculate offsets in [num_heads, head_size/x, x]
-      const int head_idx = i / x / (head_size / x);
-      const int head_offset = i / x % (head_size / x);
-      const int x_offset = i % x;
-
-      const scalar_t* k_ptr = k_cache + physical_cache_offset
-                              + head_idx * (head_size/x) * block_size * x
-                              + head_offset * block_size * x
-                              + block_offset * x
-                              + x_offset;
-      key_out[head_idx * head_size + head_offset * x + x_offset] = __ldg(k_ptr);
-    }
-
-    // process value in chunks
-#pragma unroll
-    for (int i = threadIdx.x; i < num_heads * head_size; i += blockDim.x) {
-      // calculate offsets in [num_heads, head_size]
+    const int num_tokens = num_heads * head_size;
+    for (int i = threadIdx.x; i < num_tokens; i += blockDim.x) {
+      const int tgt_key_idx = token_idx * key_stride + i;
+      const int tgt_value_idx = token_idx * value_stride + i;
+  
       const int head_idx = i / head_size;
       const int head_offset = i % head_size;
+      const int x_idx = head_offset / x;  // the offset of the [head_size/x] dimension
+      const int x_offset = head_offset % x;
+  
+      const int src_key_idx = block_idx * num_heads * (head_size / x) * block_size * x
+                              + head_idx * (head_size / x) * block_size * x
+                              + x_idx * block_size * x
+                              + block_offset * x
+                              + x_offset;
+      const int src_value_idx = block_idx * num_heads * head_size * block_size
+                                + head_idx * head_size * block_size
+                                + head_offset * block_size
+                                + block_offset;
 
-      const scalar_t* v_ptr = v_cache + physical_cache_offset
-                              + i * block_size   // equal to (head_idx * head_size + head_offset) * block_size
-                              + block_offset;
-      value_out[i] = __ldg(v_ptr);
+      key[tgt_key_idx] = __ldg(&key_cache[src_key_idx]);
+      value[tgt_value_idx] = __ldg(&value_cache[src_value_idx]);
     }
 }
 
@@ -379,6 +339,45 @@ void reshape_and_cache(
     "reshape_and_cache_kernel",
     [&] {
       cacheflow::reshape_and_cache_kernel<scalar_t><<<grid, block, 0, stream>>>(
+        key.data_ptr<scalar_t>(),
+        value.data_ptr<scalar_t>(),
+        key_cache.data_ptr<scalar_t>(),
+        value_cache.data_ptr<scalar_t>(),
+        slot_mapping.data_ptr<int>(),
+        key_stride,
+        value_stride,
+        num_heads,
+        head_size,
+        block_size,
+        x);
+    });
+}
+
+
+void gather_cached_kv(
+  torch::Tensor& key,           // [out] [num_tokens, num_heads, head_size]
+  torch::Tensor& value,         // [out] [num_tokens, num_heads, head_size]
+  torch::Tensor& key_cache,     // [in]  [num_blocks, num_heads, head_size/x, block_size, x]
+  torch::Tensor& value_cache,   // [in]  [num_blocks, num_heads, head_size, block_size]
+  torch::Tensor& slot_mapping)  // [in]  [num_tokens]
+{
+  int num_tokens = key.size(0);
+  int num_heads = key.size(1);
+  int head_size = key.size(2);
+  int block_size = key_cache.size(3);
+  int x = key_cache.size(4);
+
+  int key_stride = key.stride(0);
+  int value_stride = value.stride(0);
+
+  dim3 grid(num_tokens);
+  dim3 block(std::min(num_heads * head_size, 512));
+  const cudaStream_t stream = at::cuda::getCurrentCUDAStream();
+  AT_DISPATCH_FLOATING_TYPES_AND_HALF(
+    key.scalar_type(),
+    "gather_cached_kv_kernel_2",
+    [&] {
+      cacheflow::gather_cached_kv_kernel_2<scalar_t><<<grid, block, 0, stream>>>(
         key.data_ptr<scalar_t>(),
         value.data_ptr<scalar_t>(),
         key_cache.data_ptr<scalar_t>(),
@@ -441,47 +440,48 @@ void gather_cached_kv(
 }
 */
 
-// same group of threads will be working on the same block
-void gather_cached_kv(
-  torch::Tensor& qkv_out,         // [cu_seqlens_k[-1], 3(QKV), num_heads, head_size]
-  torch::Tensor& key_cache,       // [num_blocks, num_heads, head_size/x, block_size, x]
-  torch::Tensor& value_cache,     // [num_blocks, num_heads, head_size, block_size]
-  torch::Tensor& cu_seqlens_k,    // aka 'cu_seqlens_k' in '_flash_attn_forward', or 'context_lens' in cacheflow
-  torch::Tensor& seqlens_k,       // CPU version of 'cu_seqlens_k'
-  torch::Tensor& block_tables) {  // [num_seqs, max_num_blocks_per_seq]
-    const int num_seqs = cu_seqlens_k.size(0) - 1;
-    const int num_heads = value_cache.size(1);
-    const int head_size = value_cache.size(2);
-    const int block_size = value_cache.size(3);
-    // const int x = key_cache.size(4);
-    const int max_num_blocks_per_seq = block_tables.size(1);
-    const int* context_lens_ptr = cu_seqlens_k.data_ptr<int>();
-    const int* cpu_context_lens_ptr = seqlens_k.data_ptr<int>();
 
-    // calculate the total amount of blocks
-    int num_total_blocks = 0;
-    for (int i = 0; i < num_seqs; ++i) {
-      int context_len = cpu_context_lens_ptr[i + 1] - cpu_context_lens_ptr[i];
-      int num_blocks = (context_len + block_size - 1) / block_size;
-      num_total_blocks += num_blocks;
-    }
+// // same group of threads will be working on the same block
+// void gather_cached_kv(
+//   torch::Tensor& qkv_out,         // [cu_seqlens_k[-1], 3(QKV), num_heads, head_size]
+//   torch::Tensor& key_cache,       // [num_blocks, num_heads, head_size/x, block_size, x]
+//   torch::Tensor& value_cache,     // [num_blocks, num_heads, head_size, block_size]
+//   torch::Tensor& cu_seqlens_k,    // aka 'cu_seqlens_k' in '_flash_attn_forward', or 'context_lens' in cacheflow
+//   torch::Tensor& seqlens_k,       // CPU version of 'cu_seqlens_k'
+//   torch::Tensor& block_tables) {  // [num_seqs, max_num_blocks_per_seq]
+//     const int num_seqs = cu_seqlens_k.size(0) - 1;
+//     const int num_heads = value_cache.size(1);
+//     const int head_size = value_cache.size(2);
+//     const int block_size = value_cache.size(3);
+//     // const int x = key_cache.size(4);
+//     const int max_num_blocks_per_seq = block_tables.size(1);
+//     const int* context_lens_ptr = cu_seqlens_k.data_ptr<int>();
+//     const int* cpu_context_lens_ptr = seqlens_k.data_ptr<int>();
 
-    dim3 grid(num_total_blocks, block_size);
-    dim3 block(std::min(num_heads * head_size, 512));
-    const cudaStream_t stream = at::cuda::getCurrentCUDAStream();
-    AT_DISPATCH_FLOATING_TYPES_AND_HALF(
-      key_cache.scalar_type(),
-      "gather_cached_kv_kernel_2",
-      [&] {
-        cacheflow::gather_cached_kv_kernel_2<scalar_t><<<grid, block, 0, stream>>>(
-          qkv_out.data_ptr<scalar_t>(),
-          key_cache.data_ptr<scalar_t>(),
-          value_cache.data_ptr<scalar_t>(),
-          block_tables.data_ptr<int>(),
-          cu_seqlens_k.data_ptr<int>(),
-          num_seqs,
-          max_num_blocks_per_seq,
-          num_heads,
-          head_size);
-      });
-}
+//     // calculate the total amount of blocks
+//     int num_total_blocks = 0;
+//     for (int i = 0; i < num_seqs; ++i) {
+//       int context_len = cpu_context_lens_ptr[i + 1] - cpu_context_lens_ptr[i];
+//       int num_blocks = (context_len + block_size - 1) / block_size;
+//       num_total_blocks += num_blocks;
+//     }
+
+//     dim3 grid(num_total_blocks, block_size);
+//     dim3 block(std::min(num_heads * head_size, 512));
+//     const cudaStream_t stream = at::cuda::getCurrentCUDAStream();
+//     AT_DISPATCH_FLOATING_TYPES_AND_HALF(
+//       key_cache.scalar_type(),
+//       "gather_cached_kv_kernel_2",
+//       [&] {
+//         cacheflow::gather_cached_kv_kernel_2<scalar_t><<<grid, block, 0, stream>>>(
+//           qkv_out.data_ptr<scalar_t>(),
+//           key_cache.data_ptr<scalar_t>(),
+//           value_cache.data_ptr<scalar_t>(),
+//           block_tables.data_ptr<int>(),
+//           cu_seqlens_k.data_ptr<int>(),
+//           num_seqs,
+//           max_num_blocks_per_seq,
+//           num_heads,
+//           head_size);
+//       });
+// }
