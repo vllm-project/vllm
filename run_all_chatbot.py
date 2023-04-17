989d0195f3a75cf977b33338d1fb14fd0d9a62e7
Lianmin Zheng <lianminzheng@gmail.com>
2023-04-17 02:47:54 +0000

    update rates

--- run_all_chatbot.py
+++ run_all_chatbot.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
     parser.add_argument("--duration", type=int, required=True)
     args = parser.parse_args()
 
-    rates = [0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 1.00, 1.10]
-    new_prob = 0.1
+    rates = [0.10, 0.20, 0.30, 0.40, 0.50, 0.55, 0.60, 0.70, 0.80, 0.85, 0.90, 0.95, 1.00, 1.10]
+    new_prob = 0.5
     duration = args.duration
     for rate in rates:
         cmd = f"python benchmark/benchmark_chatbot.py --dataset sharegpt_clean_lang_10k_opt_tokenized.pkl --model facebook/opt-13b --request-rate {rate} --duration {duration} --n1 1.0 --new-prob {new_prob} --use-dummy"
