87766dda430b1e7f43cabe9c52c8e2e58b9480d6
Siyuan <suquark@gmail.com>
2023-04-17 15:26:08 -0400

    update benchmark script

--- benchmark_prefix_translation_orca.sh
+++ benchmark_prefix_translation_orca.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 # This script is used to test the performance of prefix translation for Orca.
 
-for num_prefix in 1 8 2 4; do
-    for request_rate in 1 8 16 24 32 40 48; do
-        python benchmark/benchmark_prefix_translation_orca.py \
-            --model ~/hf-llama/llama-13b/ \
-            --num-prefix-examples "$num_prefix" \
-            --request-rate "$request_rate" \
-            --duration 1200 \
-            --n1 1.0 \
-            --len-estimator oracle \
-            &>> log.txt
-    done
+#!/bin/bash
+num_prefix=1
+for request_rate in 1 24 26 28 29 30 31 32; do
+    python benchmark/benchmark_prefix_translation_orca.py \
+        --model ~/hf-llama/llama-13b/ \
+        --num-prefix-examples "$num_prefix" \
+        --request-rate "$request_rate" \
+        --duration 1200 \
+        --n1 1.0 \
+        --len-estimator oracle \
+        &>> log.txt
+done
+
+num_prefix=5
+for request_rate in 1 4 8 12 13 14 15 16; do
+    python benchmark/benchmark_prefix_translation_orca.py \
+        --model ~/hf-llama/llama-13b/ \
+        --num-prefix-examples "$num_prefix" \
+        --request-rate "$request_rate" \
+        --duration 1200 \
+        --n1 1.0 \
+        --len-estimator oracle \
+        &>> log.txt
 done
