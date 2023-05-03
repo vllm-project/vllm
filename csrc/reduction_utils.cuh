ddc4d811fb4335d0eb07b79787c1bc4d228a5498
woWoosuk Kwon <woosuk.kwon@berkeley.edu>
2023-05-03 05:53:55 +0000

    [Minor] Code formatting

--- csrc/reduction_utils.cuh
+++ csrc/reduction_utils.cuh
@@ -1,41 +1,34 @@
 #pragma once
 
-#define FINAL_MASK 0xffffffff
-
 namespace cacheflow {
 
 template<typename T>
-__inline__ __device__ T warpReduceSum(T val)
-{
+__inline__ __device__ T warpReduceSum(T val) {
 #pragma unroll
-    for (int mask = 16; mask > 0; mask >>= 1)
-        val += __shfl_xor_sync(FINAL_MASK, val, mask, 32);
-    return val;
+  for (int mask = 16; mask > 0; mask >>= 1)
+    val += __shfl_xor_sync(0xffffffff, val, mask, 32);
+  return val;
 }
 
 /* Calculate the sum of all elements in a block */
 template<typename T>
-__inline__ __device__ T blockReduceSum(T val)
-{
-    static __shared__ T shared[32];
-    int lane = threadIdx.x & 0x1f;
-    int wid = threadIdx.x >> 5;
+__inline__ __device__ T blockReduceSum(T val) {
+  static __shared__ T shared[32];
+  int lane = threadIdx.x & 0x1f;
+  int wid = threadIdx.x >> 5;
 
-    val = warpReduceSum<T>(val);
+  val = warpReduceSum<T>(val);
 
-    if (lane == 0)
-        shared[wid] = val;
+  if (lane == 0)
+    shared[wid] = val;
 
-    __syncthreads();
+  __syncthreads();
 
-    // Modify from blockDim.x << 5 to blockDim.x / 32. to prevent
-    // blockDim.x is not divided by 32
-    val = (threadIdx.x < (blockDim.x / 32.f)) ? shared[lane] : (T)(0.0f);
-    val = warpReduceSum<T>(val);
-
-    return val;
+  // Modify from blockDim.x << 5 to blockDim.x / 32. to prevent
+  // blockDim.x is not divided by 32
+  val = (threadIdx.x < (blockDim.x / 32.f)) ? shared[lane] : (T)(0.0f);
+  val = warpReduceSum<T>(val);
+  return val;
 }
 
 } // namespace cacheflow
-
-#undef FINAL_MASK
