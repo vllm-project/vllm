729e14b20d03c3ec58d35a90429f7feb320f9c3c
woWoosuk Kwon <woosuk.kwon@berkeley.edu>
2023-05-09 22:56:45 +0000

    Add a tracking issue in comment

--- cacheflow/frontend/utils.py
+++ cacheflow/frontend/utils.py
@@ -12,6 +12,7 @@ def get_tokenizer(
     config = AutoConfig.from_pretrained(model_name)
     if config.model_type == "llama":
         # LLaMA fast tokenizer has a bug related to protobuf.
+        # See https://github.com/WoosukKwon/cacheflow/issues/80#issue-1698550554
         if "use_fast" in kwargs:
             kwargs.pop("use_fast")
         return AutoTokenizer.from_pretrained(
