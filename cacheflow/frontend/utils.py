9fe9fbfb5599a2035d2a30a48fce490e42a0f519
woWoosuk Kwon <woosuk.kwon@berkeley.edu>
2023-05-09 23:02:24 +0000

    Minor refactoring

--- cacheflow/frontend/utils.py
+++ cacheflow/frontend/utils.py
@@ -4,17 +4,19 @@ from transformers import (AutoConfig, AutoTokenizer, PreTrainedTokenizer,
                           PreTrainedTokenizerFast)
 
 
+_MODEL_TYPES_WITH_SLOW_TOKENIZER = [
+    # LLaMA fast tokenizer has a bug related to protobuf.
+    # See https://github.com/WoosukKwon/cacheflow/issues/80#issue-1698550554
+    "llama",
+]
+
+
 def get_tokenizer(
     model_name: str,
     *args,
     **kwargs,
 ) -> Union[PreTrainedTokenizer, PreTrainedTokenizerFast]:
     config = AutoConfig.from_pretrained(model_name)
-    if config.model_type == "llama":
-        # LLaMA fast tokenizer has a bug related to protobuf.
-        # See https://github.com/WoosukKwon/cacheflow/issues/80#issue-1698550554
-        if "use_fast" in kwargs:
-            kwargs.pop("use_fast")
-        return AutoTokenizer.from_pretrained(
-            model_name, use_fast=False, *args, **kwargs)
+    if config.model_type in _MODEL_TYPES_WITH_SLOW_TOKENIZER:
+        kwargs["use_fast"] = False
     return AutoTokenizer.from_pretrained(model_name, *args, **kwargs)
