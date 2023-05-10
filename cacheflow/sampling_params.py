6c4422d3589f726f956fe820816e547cb010cc99
woWoosuk Kwon <woosuk.kwon@berkeley.edu>
2023-05-10 19:46:12 +0000

    Add top-k to sampling params

--- cacheflow/sampling_params.py
+++ cacheflow/sampling_params.py
@@ -8,6 +8,7 @@ class SamplingParams:
         n: int,
         temperature: float,
         top_p: float,
+        top_k: int,
         use_beam_search: bool,
         stop_token_ids: Set[int],
         max_num_steps: int,
@@ -20,6 +21,9 @@ class SamplingParams:
                 f'temperature must be non-negative, got {temperature}.')
         if not 0.0 < top_p <= 1.0:
             raise ValueError(f'top_p must be in (0, 1], got {top_p}.')
+        if top_k < -1 or top_k == 0:
+            raise ValueError(f'top_k must be -1 (disable), or at least 1, '
+                             f'got {top_k}.')
         if max_num_steps < 1:
             raise ValueError(
                 f'max_num_steps must be at least 1, got {max_num_steps}.')
@@ -49,6 +53,7 @@ class SamplingParams:
         self.n = n
         self.temperature = temperature
         self.top_p = top_p
+        self.top_k = top_k
         self.use_beam_search = use_beam_search
         self.stop_token_ids = stop_token_ids
         self.max_num_steps = max_num_steps
@@ -58,6 +63,7 @@ class SamplingParams:
         return (f'SamplingParams(n={self.n}, '
                 f'temperature={self.temperature}, '
                 f'top_p={self.top_p}, '
+                f'top_k={self.top_k},'
                 f'use_beam_search={self.use_beam_search}, '
                 f'stop_token_ids={self.stop_token_ids}, '
                 f'max_num_steps={self.max_num_steps}, '
@@ -69,6 +75,7 @@ class SamplingParams:
             n=d.get('n', 1),
             temperature=d.get('temperature', 1.0),
             top_p=d.get('top_p', 1.0),
+            top_k=d.get('top_k', -1),
             use_beam_search=d.get('use_beam_search', False),
             stop_token_ids=set(d.get('stop_token_ids', set())),
             max_num_steps=d.get('max_num_steps', 16),
