707ec647bb3a5018e5d8cdded409d6244bbb9ea5
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2023-05-14 21:54:32 -0700

    Add copyright headers for HF models (#103)

--- cacheflow/model_executor/models/gpt2.py
+++ cacheflow/model_executor/models/gpt2.py
@@ -1,3 +1,21 @@
+# coding=utf-8
+# Copyright 2023 The CacheFlow team.
+# Adapted from https://github.com/huggingface/transformers/blob/v4.28.0/src/transformers/models/gpt2/modeling_gpt2.py
+#
+# Copyright 2018 The OpenAI Team Authors and HuggingFace Inc. team.
+# Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """1D GPT-2 model compatible with HuggingFace weights."""
 from typing import Dict, List, Optional, Tuple
 
--- cacheflow/model_executor/models/llama.py
+++ cacheflow/model_executor/models/llama.py
@@ -1,3 +1,25 @@
+# coding=utf-8
+# Copyright 2023 The CacheFlow team.
+# Adapted from https://github.com/huggingface/transformers/blob/v4.28.0/src/transformers/models/llama/modeling_llama.py
+#
+# Copyright 2022 EleutherAI and the HuggingFace Inc. team. All rights reserved.
+#
+# This code is based on EleutherAI's GPT-NeoX library and the GPT-NeoX
+# and OPT implementations in this library. It has been modified from its
+# original forms to accommodate minor architectural differences compared
+# to GPT-NeoX and OPT used by the Meta AI team that trained the model.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """1D LLaMA model compatible with HuggingFace weights."""
 from typing import Dict, List, Optional, Tuple
 
