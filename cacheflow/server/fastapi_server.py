8886fa57189e4e25f87f59a7e6b240b26895f9ed
woWoosuk Kwon <woosuk.kwon@berkeley.edu>
2023-05-20 18:49:50 +0000

    Fix FastAPI server

--- cacheflow/server/fastapi_server.py
+++ cacheflow/server/fastapi_server.py
@@ -3,18 +3,19 @@ import asyncio
 import json
 import time
 from typing import List, Dict, Optional
+import uuid
 
 from fastapi import FastAPI, Request
 from fastapi.responses import StreamingResponse
 import ray
 import uvicorn
 
+from cacheflow.outputs import RequestOutput
 from cacheflow.sampling_params import SamplingParams
 from cacheflow.server.arg_utils import (
     add_server_arguments, create_server_configs_from_args)
 from cacheflow.server.llm_server import LLMServer
 from cacheflow.server.ray_utils import initialize_cluster
-from cacheflow.sequence import Sequence, SequenceGroup
 
 TIMEOUT_TO_PREVENT_DEADLOCK = 1 # seconds
 app = FastAPI()
@@ -29,36 +30,37 @@ class FastAPIServer:
             remote_server_class = ray.remote(num_gpus=1)(LLMServer)
         self.server = remote_server_class.remote(*args, **kwargs)
 
-        self.running_seq_groups: Dict[str, SequenceGroup] = {}
-        self.sequence_group_events: Dict[str, asyncio.Event] = {}
+        # Request id -> request output.
+        self.request_outputs: Dict[str, RequestOutput] = {}
+        # Request id -> event to notify that there is new output.
+        self.request_events: Dict[str, asyncio.Event] = {}
         self.is_server_running = False
 
     async def server_step(self):
         self.is_server_running = True
-        streaming_outputs, _ = await self.server.step.remote()
+        request_outputs = await self.server.step.remote()
         self.is_server_running = False
         # Notify the waiting coroutines that there are new outputs ready.
-        for stream_output in streaming_outputs:
-            group_id = seq_group.group_id
-            self.running_seq_groups[group_id] = seq_group
-            self.sequence_group_events[group_id].set()
+        for request_output in request_outputs:
+            request_id = request_output.request_id
+            self.request_outputs[request_id] = request_output
+            self.request_events[request_id].set()
 
     async def generate(self, request_dict: Dict):
         # Preprocess the request.
         arrival_time = time.time()
         prompt = request_dict.pop("prompt")
-        request_dict["stream"] = True
         sampling_params = SamplingParams(**request_dict)
 
         # Create an event to notify us that there is new output from the
         # cacheflow server.
-        group_event = asyncio.Event()
-        self.running_seq_groups[group_id] = seq_group
-        self.sequence_group_events[group_id] = group_event
+        request_id = str(uuid.uuid4().hex[:8])
+        request_event = asyncio.Event()
+        self.request_events[request_id] = request_event
 
         # Add the request into the cacheflow server's waiting queue.
-        await self.server.add_request(
-            prompt, sampling_params, arrival_time=arrival_time)
+        await self.server.add_request.remote(
+            request_id, prompt, sampling_params, arrival_time=arrival_time)
 
         # The cacheflow server does not have a background loop that keeps
         # processing incoming requests. Therefore, we need to keep kicking
@@ -72,29 +74,30 @@ class FastAPIServer:
             # when there is new output available for the sequence group.
             # Added a timeout to prevent deadlock.
             try:
-                await asyncio.wait_for(group_event.wait(), timeout=TIMEOUT_TO_PREVENT_DEADLOCK)
+                await asyncio.wait_for(request_event.wait(),
+                                       timeout=TIMEOUT_TO_PREVENT_DEADLOCK)
             except asyncio.TimeoutError:
                 continue
             # Reset the event to wait for the next output.
-            group_event.clear()
+            request_event.clear()
 
-            # Decode and return new outputs
-            seq_group = self.running_seq_groups[group_id]
-            all_outputs = []
-            for seq in seq_group.seqs:
-                token_ids = seq.get_token_ids()
-                output = self.tokenizer.decode(token_ids, skip_special_tokens=True)
-                all_outputs.append(output)
+            # Decode and return new outputs.
+            request_output = self.request_outputs[request_id]
+            prompt = request_output.prompt
+            text_outputs = [
+                prompt + output.text
+                for output in request_output.outputs
+            ]
             ret = {
-                "text": all_outputs,
+                "text": text_outputs,
                 "error": 0,
             }
             yield (json.dumps(ret) + "\0").encode("utf-8")
 
             # Once finished, release the resources of the sequence group.
-            if seq_group.is_finished():
-                del self.running_seq_groups[group_id]
-                del self.sequence_group_events[group_id]
+            if request_output.done:
+                del self.request_outputs[request_id]
+                del self.request_events[request_id]
                 # Kick the server if the server is not running. This is to
                 # prevent that there are still requests in server's waiting
                 # queue to be executed.
