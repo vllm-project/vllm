96ceda01a1282f6e3f8c3471dcfb0bb1306a3b32
woWoosuk Kwon <woosuk.kwon@berkeley.edu>
2023-05-21 07:05:24 +0000

    Add offline inference example

---
+++ examples/offline_inference.py
@@ -0,0 +1,17 @@
+from cacheflow import LLM, SamplingParams
+
+
+prompts = [
+    "Hello, my name is",
+    "The president of the United States is",
+    "The capital of France is",
+    "The future of AI is",
+]
+sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+
+llm = LLM(model="gpt2")
+outputs = llm.generate(prompts, sampling_params)
+for output in outputs:
+    prompt = output.prompt
+    generated_text = output.outputs[0].text
+    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
