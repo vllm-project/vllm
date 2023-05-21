1ea9fee697113e5ef53c9956d3aba71d53dbd839
woWoosuk Kwon <woosuk.kwon@berkeley.edu>
2023-05-21 07:34:36 +0000

    Add comments

--- examples/offline_inference.py
+++ examples/offline_inference.py
@@ -1,15 +1,20 @@
 from cacheflow import LLM, SamplingParams
 
 
+# Sample prompts.
 prompts = [
     "Hello, my name is",
     "The president of the United States is",
     "The capital of France is",
     "The future of AI is",
 ]
+# Create a sampling params object.
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
+# Create an LLM.
 llm = LLM(model="facebook/opt-125m")
+# Generate text. The output is a list of RequestOutput objects
+# that contain the prompt, generated text, and other information.
 outputs = llm.generate(prompts, sampling_params)
 for output in outputs:
     prompt = output.prompt
