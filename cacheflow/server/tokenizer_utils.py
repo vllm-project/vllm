35ff77391d506219bd03a5bb60e188747a76d8e2
woWoosuk Kwon <woosuk.kwon@berkeley.edu>
2023-05-23 05:50:43 +0000

    Allow unoptimized path

--- cacheflow/server/tokenizer_utils.py
+++ cacheflow/server/tokenizer_utils.py
@@ -40,9 +40,31 @@ def detokenize_incrementally(
     output_tokens = prev_output_tokens + [new_token]
 
     # Convert the tokens to a string.
-    # We optimize tokenizer._decode() by assuming that the tokenizer does not
-    # have added_tokens_encoder.
-    if hasattr(tokenizer, "added_tokens_encoder"):
-        assert not tokenizer.added_tokens_encoder
-    output_text = tokenizer.convert_tokens_to_string(output_tokens)
+    # Optimization: If the tokenizer does not have `added_tokens_encoder`,
+    # then we can just use `convert_tokens_to_string`.
+    if not (hasattr(tokenizer, "added_tokens_encoder") and 
+            tokenizer.added_tokens_encoder):
+        output_text = tokenizer.convert_tokens_to_string(output_tokens)
+        return new_token, output_text
+
+    # Adapted from https://github.com/huggingface/transformers/blob/v4.28.0/src/transformers/tokenization_utils.py#L921
+    # NOTE(woosuk): The following code is slow.
+    sub_texts = []
+    current_sub_text = []
+    for token in output_tokens:
+        if skip_special_tokens and token in tokenizer.all_special_ids:
+            continue
+        if (hasattr(tokenizer, "added_tokens_encoder") and
+            token in tokenizer.added_tokens_encoder):
+            if current_sub_text:
+                sub_text = tokenizer.convert_tokens_to_string(current_sub_text)
+                sub_texts.append(sub_text)
+                current_sub_text = []
+            sub_texts.append(token)
+        else:
+            current_sub_text.append(token)
+    if current_sub_text:
+        sub_text = tokenizer.convert_tokens_to_string(current_sub_text)
+        sub_texts.append(sub_text)
+    output_text = " ".join(sub_texts)
     return new_token, output_text
