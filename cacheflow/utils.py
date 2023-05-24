eb92ebd0b3553864e3ad042e979ac4c7aba6091a
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2023-05-23 18:22:26 -0700

    Print warnings/errors for large swap space (#123)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- cacheflow/utils.py
+++ cacheflow/utils.py
@@ -24,8 +24,10 @@ class Counter:
 
 
 def get_gpu_memory(gpu: int = 0) -> int:
+    """Returns the total memory of the GPU in bytes."""
     return torch.cuda.get_device_properties(gpu).total_memory
 
 
 def get_cpu_memory() -> int:
+    """Returns the total CPU memory of the node in bytes."""
     return psutil.virtual_memory().total
