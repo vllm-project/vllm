14b8fdde78d39a0d6bb938c69d47ad3ca2fd94d4
woWoosuk Kwon <woosuk.kwon@berkeley.edu>
2023-05-27 06:24:31 +0000

    Fix setup script & bfloat16 guard

--- setup.py
+++ setup.py
@@ -1,28 +1,55 @@
+import subprocess
 from typing import List
 
+from packaging.version import parse, Version
 import setuptools
 import torch
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 from torch.utils.cpp_extension import CUDA_HOME
 
-
-# Build custom operators.
-CXX_FLAGS = ["-g"]
+# Compiler flags.
+CXX_FLAGS = ["-g", "-O2"]
 # TODO(woosuk): Should we use -O3?
 NVCC_FLAGS = ["-O2"]
+
 
 if not torch.cuda.is_available():
     raise RuntimeError(
         f"Cannot find CUDA at CUDA_HOME: {CUDA_HOME}. "
         "CUDA must be available in order to build the package.")
 
-# FIXME(woosuk): Consider the case where the machine has multiple GPUs with
-# different compute capabilities.
-compute_capability = torch.cuda.get_device_capability()
-major, minor = compute_capability
-# Enable bfloat16 support if the compute capability is >= 8.0.
-if major >= 8:
-    NVCC_FLAGS.append("-DENABLE_BF16")
+
+def get_nvcc_cuda_version(cuda_dir: str) -> Version:
+    """Get the CUDA version from nvcc.
+
+    Adapted from https://github.com/NVIDIA/apex/blob/8b7a1ff183741dd8f9b87e7bafd04cfde99cea28/setup.py
+    """
+    nvcc_output = subprocess.check_output([cuda_dir + "/bin/nvcc", "-V"],
+                                          universal_newlines=True)
+    output = nvcc_output.split()
+    release_idx = output.index("release") + 1
+    bare_metal_version = parse(output[release_idx].split(",")[0])
+    return bare_metal_version
+
+
+# Check CUDA version.
+nvcc_cuda_version = get_nvcc_cuda_version(CUDA_HOME)
+if nvcc_cuda_version < Version("11.0"):
+    raise RuntimeError("CUDA 11.0 or higher is required to build the package.")
+
+# Select architectures to compile for based on the NVCC CUDA version.
+# NOTE(woosuk): This will increase the build time as we compile for multiple
+# architectures, regardless of whether they are used or not.
+# CUDA 11.0 supports compute capability up to 8.0.
+NVCC_FLAGS += ["-gencode", "arch=compute_70,code=sm_70"]
+NVCC_FLAGS += ["-gencode", "arch=compute_75,code=sm_75"]
+NVCC_FLAGS += ["-gencode", "arch=compute_80,code=sm_80"]
+# Compute capability 8.6 is supported since CUDA 11.1.
+if nvcc_cuda_version >= Version("11.1"):
+    NVCC_FLAGS += ["-gencode", "arch=compute_86,code=sm_86"]
+# Compute capability 9.0 is supported since CUDA 11.8.
+if nvcc_cuda_version >= Version("11.8"):
+    NVCC_FLAGS += ["-gencode", "arch=compute_90,code=sm_90"]
 
 ext_modules = []
 
