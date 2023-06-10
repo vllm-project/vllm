51a53324a33d2c447513332a2f8c3eda51a7b706
woWoosuk Kwon <woosuk.kwon@berkeley.edu>
2023-06-10 17:47:30 +0000

    Minor fix

--- benchmarks/launch_hf_server.sh
+++ benchmarks/launch_hf_server.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 PORT=8001
-MODEL=$1  # huggyllama/llama-13b
+MODEL=$1        # huggyllama/llama-13b
+TOKENS=$2       # 8192
 
 docker run --gpus all --shm-size 1g -p $PORT:80 \
            -v $PWD/data:/data \
            ghcr.io/huggingface/text-generation-inference:0.8 \
            --model-id $MODEL \
            --sharded false  \
-           --max-input-length 2047 \
+           --max-input-length 1024 \
            --max-total-tokens 2048 \
-           --max-batch-total-tokens 100
+           --max-concurrent-requests 1000 \
+           --max-batch-total-tokens $TOKENS
