298695b76691867ecd320ea6a2c6d0c6a843d5ae
Michael Feil <63565275+michaelfeil@users.noreply.github.com>
2023-06-22 19:49:27 +0200

    GPTBigCode (StarCoder, SantaCoder Support) (#209)

--- vllm/model_executor/model_loader.py
+++ vllm/model_executor/model_loader.py
@@ -6,13 +6,14 @@ import torch.nn as nn
 from transformers import PretrainedConfig
 
 from vllm.config import ModelConfig
-from vllm.model_executor.models import (GPT2LMHeadModel, GPTNeoXForCausalLM,
+from vllm.model_executor.models import (GPT2LMHeadModel, GPTBigCodeForCausalLM, GPTNeoXForCausalLM,
                                         LlamaForCausalLM, OPTForCausalLM)
 from vllm.model_executor.weight_utils import initialize_dummy_weights
 
 # TODO(woosuk): Lazy-load the model classes.
 _MODEL_REGISTRY = {
     "GPT2LMHeadModel": GPT2LMHeadModel,
+    "GPTBigCodeForCausalLM": GPTBigCodeForCausalLM,
     "GPTNeoXForCausalLM": GPTNeoXForCausalLM,
     "LlamaForCausalLM": LlamaForCausalLM,
     "OPTForCausalLM": OPTForCausalLM,
