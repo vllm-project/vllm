e0400f4971650463a3b20b5b5f0aeeb121c4e0ec
michaelfeil <me@michaelfeil.eu>
2023-06-22 19:32:34 +0200

    reorder imports

--- vllm/model_executor/models/__init__.py
+++ vllm/model_executor/models/__init__.py
@@ -1,8 +1,9 @@
-from vllm.model_executor.models.gpt_neox import GPTNeoXForCausalLM
 from vllm.model_executor.models.gpt2 import GPT2LMHeadModel
+from vllm.model_executor.models.gpt_bigcode import GPTBigCodeForCausalLM
+from vllm.model_executor.models.gpt_neox import GPTNeoXForCausalLM
 from vllm.model_executor.models.llama import LlamaForCausalLM
 from vllm.model_executor.models.opt import OPTForCausalLM
-from vllm.model_executor.models.gpt_bigcode import GPTBigCodeForCausalLM
+
 
 
 __all__ = [
