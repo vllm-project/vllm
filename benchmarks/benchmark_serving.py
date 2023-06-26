f1d314b5e014dc73ffbf528144459863511aeb68
Zhuohan Li <zhuohan123@gmail.com>
2023-06-26 11:56:08 -0700

    [Fix] Fix default port number in benchmark scripts

--- benchmarks/benchmark_serving.py
+++ benchmarks/benchmark_serving.py
@@ -217,7 +217,7 @@ if __name__ == "__main__":
     parser.add_argument("--backend", type=str, default="vllm",
                         choices=["vllm", "tgi"])
     parser.add_argument("--host", type=str, default="localhost")
-    parser.add_argument("--port", type=int, default=8001)
+    parser.add_argument("--port", type=int, default=8000)
     parser.add_argument("--dataset", type=str, required=True,
                         help="Path to the dataset.")
     parser.add_argument("--tokenizer", type=str, required=True,
