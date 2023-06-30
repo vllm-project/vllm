572d9183342ccd82b36b79f6126883fb2115e927
woWoosuk Kwon <woosuk.kwon@berkeley.edu>
2023-06-30 07:51:48 +0000

    Fix get_config

--- vllm/transformers_utils/config.py
+++ vllm/transformers_utils/config.py
@@ -4,6 +4,6 @@ from vllm.transformers_utils.configs import *
 
 def get_config(model: str) -> PretrainedConfig:
     config = AutoConfig.from_pretrained(model, trust_remote_code=True)
-    if isinstance(config, RWConfig):
+    if config.model_type == "RefinedWeb":
         config = RWConfig.from_pretrained(model)
     return config
