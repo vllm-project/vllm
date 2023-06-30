d26f0fe2acc2e99fca1e7bfc1c4c1c01467d0659
woWoosuk Kwon <woosuk.kwon@berkeley.edu>
2023-06-30 06:41:19 +0000

    Fix weight loading

--- vllm/model_executor/models/falcon.py
+++ vllm/model_executor/models/falcon.py
@@ -6,7 +6,6 @@ InputMetadata to extract the original 2D shape of the input.
 """
 from typing import Dict, List, Optional, Tuple
 
-import numpy as np
 import torch
 from torch import nn
 
@@ -30,13 +29,35 @@ class Attention(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.hidden_size = config.hidden_size
-        self.num_heads = config.n_head
-        self.head_dim = self.hidden_size // self.num_heads
-        assert self.head_dim * self.num_heads == self.hidden_size
+        assert config.multi_query
+        self.total_num_query_heads = config.n_head
+        self.head_dim = self.hidden_size // self.total_num_query_heads
+        assert self.head_dim * self.total_num_query_heads == self.hidden_size
 
-        self.query_key_value = ColumnParallelLinear(
+        tensor_model_parallel_world_size = get_tensor_model_parallel_world_size()
+        assert self.total_num_query_heads % tensor_model_parallel_world_size == 0
+        self.num_query_heads = (self.total_num_query_heads //
+                                tensor_model_parallel_world_size)
+
+        self.total_num_kv_heads = getattr(config, "n_head_kv", 1)
+        assert self.total_num_kv_heads % tensor_model_parallel_world_size == 0
+        self.num_kv_heads = (self.total_num_kv_heads //
+                             tensor_model_parallel_world_size)
+
+        # Grouped MQA.
+        assert self.num_query_heads % self.num_kv_heads == 0
+        self.query_per_kv = self.num_query_heads // self.num_kv_heads
+
+        self.query = ColumnParallelLinear(
             self.hidden_size,
-            3 * self.hidden_size,  # FIXME(woosuk)
+            self.hidden_size,
+            bias=config.bias,
+            gather_output=False,
+            perform_initialization=False,
+        )
+        self.key_value = ColumnParallelLinear(
+            self.hidden_size,
+            2 * self.total_num_kv_heads * self.head_dim,
             bias=config.bias,
             gather_output=False,
             perform_initialization=False,
@@ -53,7 +74,7 @@ class Attention(nn.Module):
         rotary_dim = int(self.head_dim * 0.5)
         assert rotary_dim % 2 == 0
         scaling = self.head_dim ** -0.5
-        self.attn = PagedAttentionWithRoPE(self.num_heads, self.head_dim,
+        self.attn = PagedAttentionWithRoPE(self.num_query_heads, self.head_dim,
                                            scaling, rotary_dim)
 
     def forward(
@@ -64,12 +85,20 @@ class Attention(nn.Module):
         input_metadata: InputMetadata,
         cache_event: Optional[torch.cuda.Event],
     ) -> torch.Tensor:
-        qkv = self.query_key_value(hidden_states)
-        q, k, v = qkv.chunk(chunks=3, dim=-1)
+        q, _ = self.query(hidden_states)
+        kv, _ = self.key_value(hidden_states)
+        k, v = kv.chunk(chunks=2, dim=-1)
+
+        # FIXME(woosuk): For now, we regard MQA as MHA and replicate the keys
+        # and values for each head. This is not efficient and should be fixed
+        # in the future.
+        k = k.repeat(1, self.query_per_kv)
+        v = v.repeat(1, self.query_per_kv)
+
         k_cache, v_cache = kv_cache
         attn_output = self.attn(
             position_ids, q, k, v, k_cache, v_cache, input_metadata, cache_event)
-        output = self.dense(attn_output)
+        output, _ = self.dense(attn_output)
         return output
 
 
@@ -90,9 +119,9 @@ class MLP(nn.Module):
                                                perform_initialization=False)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.dense_h_to_4h(x)
+        x, _ = self.dense_h_to_4h(x)
         x = self.act(x)
-        x = self.dense_4h_to_h(x)
+        x, _ = self.dense_4h_to_h(x)
         return x
 
 
@@ -104,11 +133,13 @@ class DecoderLayer(nn.Module):
         hidden_size = config.hidden_size
         self.num_heads = config.n_head
 
-        self.input_layernorm = nn.LayerNorm(hidden_size, eps=config.layer_norm_epsilon)
+        self.input_layernorm = nn.LayerNorm(hidden_size,
+                                            eps=config.layer_norm_epsilon)
         self.self_attention = Attention(config)
         if not config.parallel_attn:
             # unused if parallel attn
-            self.post_attention_layernorm = nn.LayerNorm(hidden_size, eps=config.layer_norm_epsilon)
+            self.post_attention_layernorm = nn.LayerNorm(
+                hidden_size, eps=config.layer_norm_epsilon)
         self.mlp = MLP(config)
 
     def forward(
@@ -151,10 +182,12 @@ class RWModel(nn.Module):
         assert not config.alibi
 
         # Embedding + LN Embedding
-        self.word_embeddings = nn.Embedding(config.vocab_size, self.embed_dim)
+        self.word_embeddings = VocabParallelEmbedding(
+            config.vocab_size, self.embed_dim, perform_initialization=False)
 
         # Transformer blocks
-        self.h = nn.ModuleList([DecoderLayer(config) for _ in range(config.num_hidden_layers)])
+        self.h = nn.ModuleList(
+            [DecoderLayer(config) for _ in range(config.num_hidden_layers)])
 
         # Final Layer Norm
         self.ln_f = nn.LayerNorm(self.embed_dim, eps=config.layer_norm_epsilon)
@@ -222,62 +255,53 @@ class RWForCausalLM(nn.Module):
 
         for name, loaded_weight in hf_model_weights_iterator(
             model_name_or_path, cache_dir, use_np_cache):
-            param = state_dict[name]
 
-            def _expand_mqa_mha(qkv_array, n_head, head_dim):
-                """manipulates along axis=0 from MQA to MHA
-                inputs: qkv_array.shape=((n_heads + 2) * head_dim, hidden_dim)
-                    with n_heads for q, then 1 for k, 1 for 1 v, times head dim
-                return: qkv_array.shape=(3 * n_heads * head_dim, hidden_dim)
-
-                TODO: this function is no longer needed once vllm supports MQA.
-                """
-                qkv_array = qkv_array.numpy()
-                
-                dims_q = n_head * head_dim
-                q, k, v = np.split(qkv_array, (dims_q, dims_q + head_dim), axis=0)
-                # q is fine, but k & v have not replicated shape along the first axis
-                # as long as MQA is not nativly supported, increase memory and replicated
-                # (head_dim, hidden_dim) to (n_heads * head_dim, hidden_dim)
-                if k.ndim == 2 and v.ndim == 2:
-                    replication = (n_head, 1)  # weights
-                else:
-                    replication = n_head  # biases
-                # replicate n_head times for q, v
-                k, v = np.tile(k, replication), np.tile(v, replication)
-                # concat q, k, v along the first axis (n_heads * head_dim, hidden_dim)
-                # to (3 * n_heads * head_dim, hidden_dim)
-                qkv_array = np.concatenate((q, k, v), axis=0)
-                return torch.from_numpy(qkv_array)
-
-            # For the fused QKV linear layer, manually shard the weights.
+            # For the fused QKV linear layer, we split the weight into query and key_value
+            # and shard them along the head dimension.
             if "query_key_value" in name:
-                # The fused QKV weight has the shape of [3 * num_heads * head_size, hidden_size].
-                # When tensor parallelism is used, we shard the weights along the head dimension.
-                total_num_heads = self.config.num_attention_heads
-                hidden_size = self.config.hidden_size
-                head_size = hidden_size // total_num_heads
-                num_heads = total_num_heads // tensor_model_parallel_world_size
-                head_start = tensor_model_parallel_rank * num_heads
-                head_end = (tensor_model_parallel_rank + 1) * num_heads
-
-                if name.endswith(".weight"):
-                    # FIXME
-                    orig_dtype = loaded_weight.dtype
-                    loaded_weight = loaded_weight.float()
-                    loaded_weight = _expand_mqa_mha(loaded_weight, n_head=total_num_heads, head_dim=head_size)
-                    loaded_weight = loaded_weight.view(3, total_num_heads, head_size, hidden_size)
-                    loaded_weight = loaded_weight[:, head_start:head_end, :, :]
-                    loaded_weight = loaded_weight.reshape(-1, hidden_size)
-                    loaded_weight = loaded_weight.to(orig_dtype)
-                elif name.endswith(".bias"):
-                    loaded_weight = _expand_mqa_mha(loaded_weight, n_head=total_num_heads, head_dim=head_size)
-                    loaded_weight = loaded_weight.view(3, total_num_heads, head_size)
-                    loaded_weight = loaded_weight[:, head_start:head_end, :]
-                    loaded_weight = loaded_weight.reshape(-1)
-                else:
+                if not name.endswith(".weight"):
+                    # Falcon does not use bias for linear layers.
                     raise ValueError(f"Unexpected parameter name {name}")
-            load_tensor_parallel_weights(param, loaded_weight, name,
-                                         self._column_parallel_weights,
-                                         self._row_parallel_weights,
-                                         tensor_model_parallel_rank)
+
+                # The fused QKV weight has the shape of
+                # [(num_q_heads + 2 * num_kv_heads) * head_size, hidden_size].
+                total_num_query_heads = self.config.num_attention_heads
+                hidden_size = self.config.hidden_size
+                head_size = hidden_size // total_num_query_heads
+
+                num_query_heads = (total_num_query_heads //
+                                   tensor_model_parallel_world_size)
+                query_head_start = tensor_model_parallel_rank * num_query_heads
+                query_head_end = (tensor_model_parallel_rank + 1) * num_query_heads
+
+                total_num_kv_heads = getattr(self.config, "n_head_kv", 1)
+                num_kv_heads = (total_num_kv_heads //
+                                tensor_model_parallel_world_size)
+                kv_head_start = tensor_model_parallel_rank * num_kv_heads
+                kv_head_end = (tensor_model_parallel_rank + 1) * num_kv_heads
+                loaded_weight = loaded_weight.view(
+                    num_query_heads + 2 * num_kv_heads, head_size, hidden_size)
+
+                # Load query weights.
+                query_weight = loaded_weight[:num_query_heads, :, :]
+                query_weight = loaded_weight[query_head_start:query_head_end, :, :]
+                query_weight = query_weight.reshape(-1, hidden_size)
+                param = state_dict[name.replace("query_key_value", "query")]
+                assert param.shape == query_weight.shape
+                param.data.copy_(query_weight)
+
+                # Load key and value weights.
+                key_value_weight = loaded_weight[num_query_heads:, :, :]
+                key_value_weight = key_value_weight.reshape(
+                    2, num_kv_heads, head_size, hidden_size)
+                key_value_weight = key_value_weight[:, kv_head_start:kv_head_end, :, :]
+                key_value_weight = key_value_weight.reshape(-1, hidden_size)
+                param = state_dict[name.replace("query_key_value", "key_value")]
+                assert param.shape == key_value_weight.shape
+                param.data.copy_(key_value_weight)
+            else:
+                param = state_dict[name]
+                load_tensor_parallel_weights(param, loaded_weight, name,
+                                             self._column_parallel_weights,
+                                             self._row_parallel_weights,
+                                             tensor_model_parallel_rank)
