c11357a2bd0aef0991174637d71f8d43a997ce19
Michael Feil <michael.feil@michaelfeil.eu>
2023-07-01 13:12:10 +0200

    update helper func

--- vllm/model_executor/models/codegen_template.py
+++ vllm/model_executor/models/codegen_template.py
@@ -35,16 +35,6 @@ try:
             self.set_linear(spec.decoder.projection, model.lm_head)
             return spec
 
-        def get_vocabulary(self, model, tokenizer):
-            tokens = super().get_vocabulary(model, tokenizer)
-
-            extra_ids = model.config.vocab_size - len(tokens)
-            for i in range(extra_ids):
-                # fix for additional vocab, see GPTNeoX Converter
-                tokens.append("<extra_id_%d>" % i)
-
-            return tokens
-
         def set_vocabulary(self, spec, tokens):
             spec.register_vocabulary(tokens)
 
