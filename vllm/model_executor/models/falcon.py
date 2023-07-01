6bd7513c6e5188d011af07a3291497d91fed3b1a
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2023-07-01 00:03:27 +0000

    Fix KV handling

--- vllm/model_executor/models/falcon.py
+++ vllm/model_executor/models/falcon.py
@@ -93,8 +93,13 @@ class Attention(nn.Module):
         # FIXME(woosuk): For now, we regard MQA as MHA and replicate the keys
         # and values for each head. This is not efficient and should be fixed
         # in the future.
-        k = k.repeat(1, self.query_per_kv)
-        v = v.repeat(1, self.query_per_kv)
+        k = k.view(-1, self.num_kv_heads, self.head_dim)
+        k = k.repeat(1, 1, self.query_per_kv)
+        k = k.reshape(-1, self.num_kv_heads * self.query_per_kv * self.head_dim)
+
+        v = v.view(-1, self.num_kv_heads, self.head_dim)
+        v = v.repeat(1, 1, self.query_per_kv)
+        v = v.reshape(-1, self.num_kv_heads * self.query_per_kv * self.head_dim)
 
         k_cache, v_cache = kv_cache
         attn_output = self.attn(
@@ -136,19 +141,18 @@ class DecoderLayer(nn.Module):
 
         # NOTE(woosuk): Falcon-7B uses the same layernorm for attention and
         # MLP. However, Falcon-40B uses separate layernorms for them.
-        # FIXME(woosuk): This is a hacky way to distinguish the two models.
-        if hidden_size == 4544:
+        if _is_falcon_7b(config):
             # Falcon-7B
             self.input_layernorm = nn.LayerNorm(hidden_size,
                                                 eps=config.layer_norm_epsilon)
-        elif hidden_size == 8192:
+        elif _is_falcon_40b(config):
             # Falcon-40B
             self.ln_attn = nn.LayerNorm(hidden_size,
                                         eps=config.layer_norm_epsilon)
             self.ln_mlp = nn.LayerNorm(hidden_size,
                                        eps=config.layer_norm_epsilon)
         else:
-            raise ValueError(f"Only Falcon-7B and Falcon-40B are supported.")
+            raise ValueError("Only 7B and 40B models are supported for now.")
         assert config.parallel_attn
         self.self_attention = Attention(config)
         self.mlp = MLP(config)
@@ -162,14 +166,16 @@ class DecoderLayer(nn.Module):
         cache_event: Optional[torch.cuda.Event],
     ) -> torch.Tensor:
         residual = hidden_states
-        if hasattr(self, "input_layernorm"):
+        if _is_falcon_7b(self.config):
             # Falcon-7B
             ln_attn = self.input_layernorm(hidden_states)
             ln_mlp = ln_attn
-        else:
+        elif _is_falcon_40b(self.config):
             # Falcon-40B
             ln_attn = self.ln_attn(hidden_states)
             ln_mlp = self.ln_mlp(hidden_states)
+        else:
+            assert False
 
         # Self attention.
         attention_output = self.self_attention(
@@ -268,7 +274,7 @@ class RWForCausalLM(nn.Module):
 
         for name, loaded_weight in hf_model_weights_iterator(
             model_name_or_path, cache_dir, use_np_cache):
-            if not "query_key_value" in name:
+            if "query_key_value" not in name:
                 param = state_dict[name]
                 load_tensor_parallel_weights(param, loaded_weight, name,
                                              self._column_parallel_weights,
@@ -282,37 +288,76 @@ class RWForCausalLM(nn.Module):
                 # Falcon does not use bias for linear layers.
                 raise ValueError(f"Unexpected parameter name {name}")
 
-            # The fused QKV weight has the shape of
-            # [(num_q_heads + 2 * num_kv_heads) * head_size, hidden_size].
             total_num_query_heads = self.config.num_attention_heads
             hidden_size = self.config.hidden_size
             head_size = hidden_size // total_num_query_heads
-            loaded_weight = loaded_weight.view(-1, head_size, hidden_size)
 
-            # Load query weights.
+            assert total_num_query_heads % tensor_model_parallel_world_size == 0
             num_query_heads = (total_num_query_heads //
                                tensor_model_parallel_world_size)
             query_head_start = tensor_model_parallel_rank * num_query_heads
             query_head_end = (tensor_model_parallel_rank + 1) * num_query_heads
 
-            query_weight = loaded_weight[query_head_start:query_head_end, :, :]
-            query_weight = query_weight.reshape(-1, hidden_size)
-            param = state_dict[name.replace("query_key_value", "query")]
-            assert param.shape == query_weight.shape
-            param.data.copy_(query_weight)
-
-            # Load key and value weights.
             total_num_kv_heads = self.config.n_head_kv
+            assert total_num_kv_heads % tensor_model_parallel_world_size == 0
             num_kv_heads = (total_num_kv_heads //
                             tensor_model_parallel_world_size)
             kv_head_start = tensor_model_parallel_rank * num_kv_heads
             kv_head_end = (tensor_model_parallel_rank + 1) * num_kv_heads
 
-            key_value_weight = loaded_weight[total_num_query_heads:, :, :]
-            key_value_weight = key_value_weight.reshape(
-                2, total_num_kv_heads, head_size, hidden_size)
-            key_value_weight = key_value_weight[:, kv_head_start:kv_head_end, :, :]
-            key_value_weight = key_value_weight.reshape(-1, hidden_size)
-            param = state_dict[name.replace("query_key_value", "key_value")]
-            assert param.shape == key_value_weight.shape
-            param.data.copy_(key_value_weight)
+            if _is_falcon_7b(self.config):
+                # Falcon-7B (MQA)
+                # The fused QKV weight has the shape of
+                # [(num_q_heads + 2 * num_kv_heads) * head_size, hidden_size].
+                loaded_weight = loaded_weight.view(-1, head_size, hidden_size)
+
+                # Load query weights.
+                query_weight = loaded_weight[query_head_start:query_head_end, :, :]
+                query_weight = query_weight.reshape(-1, hidden_size)
+                param = state_dict[name.replace("query_key_value", "query")]
+                assert param.shape == query_weight.shape
+                param.data.copy_(query_weight)
+
+                # Load key_value weights.
+                key_value_weight = loaded_weight[total_num_query_heads:, :, :]
+                key_value_weight = key_value_weight.reshape(
+                    2, total_num_kv_heads, head_size, hidden_size)
+                key_value_weight = key_value_weight[:, kv_head_start:kv_head_end, :, :]
+                key_value_weight = key_value_weight.reshape(-1, hidden_size)
+                param = state_dict[name.replace("query_key_value", "key_value")]
+                assert param.shape == key_value_weight.shape
+                param.data.copy_(key_value_weight)
+            elif _is_falcon_40b(self.config):
+                # Falcon-40B (Grouped MQA)
+                # The fused QKV weight has the shape of
+                # [total_num_kv_heads, num_query_per_kv + 2, head_size, hidden_size].
+                num_query_per_kv = total_num_query_heads // total_num_kv_heads
+                loaded_weight = loaded_weight.view(
+                    total_num_kv_heads, num_query_per_kv + 2, head_size, hidden_size)
+
+                # Load query weights.
+                query_weight = loaded_weight[kv_head_start:kv_head_end, :-2, :, :]
+                query_weight = query_weight.reshape(-1, hidden_size)
+                param = state_dict[name.replace("query_key_value", "query")]
+                assert param.shape == query_weight.shape
+                param.data.copy_(query_weight)
+
+                # Load key_value weights.
+                key_value_weight = loaded_weight[kv_head_start:kv_head_end, -2:, :, :]
+                key_value_weight = key_value_weight.permute(1, 0, 2, 3)
+                key_value_weight = key_value_weight.reshape(-1, hidden_size)
+                param = state_dict[name.replace("query_key_value", "key_value")]
+                assert param.shape == key_value_weight.shape
+                param.data.copy_(key_value_weight)
+            else:
+                assert False
+
+
+def _is_falcon_7b(config: RWConfig) -> bool:
+    # FIXME(woosuk): This is hacky.
+    return config.hidden_size == 4544
+
+
+def _is_falcon_40b(config: RWConfig) -> bool:
+    # FIXME(woosuk): This is hacky.
+    return config.hidden_size == 8192
