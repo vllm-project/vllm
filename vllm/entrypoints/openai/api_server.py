5cec0107bb84b2977ccc78760e89683f03659848
Michele Catalano <michele.catalano@mayflower.de>
2023-07-03 09:34:37 +0200

    Update vllm/entrypoints/openai/api_server.py
    Co-authored-by: Zhuohan Li <zhuohan123@gmail.com>

--- vllm/entrypoints/openai/api_server.py
+++ vllm/entrypoints/openai/api_server.py
@@ -124,7 +124,11 @@ async def create_completion(raw_request: Request):
 
     model_name = request.model
     request_id = f"cmpl-{random_uuid()}"
-    prompt = request.prompt if type(request.prompt) != list else ' '.join(request.prompt)
+    if isinstance(request.prompt, list):
+        assert len(request.prompt) == 1
+        prompt = request.prompt[0]
+    else:
+        prompt = request.prompt
     created_time = int(time.time())
     try:
         sampling_params = SamplingParams(
