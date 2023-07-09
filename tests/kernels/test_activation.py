35c04f5ea6a12264c89d69481cb75618c87d3b58
Andre Slavescu <andre.slavescu@gmail.com>
2023-07-09 03:45:50 -0400

    fast_gelu test

--- tests/kernels/test_activation.py
+++ tests/kernels/test_activation.py
@@ -8,6 +8,10 @@ def ref_silu_and_mul(x: torch.Tensor) -> torch.Tensor:
     x1, x2 = x.chunk(chunks=2, dim=1)
     return F.silu(x1) * x2
 
+def ref_gelu(x: torch.Tensor) -> torch.Tensor:
+    x1 = x.chunk(chunks=2, dim=1)
+    return F.gelu(x1, approximate='tanh')
+
 
 @torch.inference_mode()
 def run_silu_and_mul(
@@ -22,9 +26,30 @@ def run_silu_and_mul(
     assert torch.allclose(out, ref_out, atol=1e-5, rtol=1e-5)
 
 
+@torch.inference_mode()
+def run_fast_gelu(
+    num_tokens: int,
+    d: int,
+    dtype: torch.dtype,
+) -> None:
+    x = torch.randn(num_tokens, 2 * d, dtype=dtype, device='cuda')
+    out = torch.empty(num_tokens, d, dtype=dtype, device='cuda')
+    activation_ops.fast_gelu(out, x)
+    ref_out = ref_gelu(x)
+    assert torch.allclose(out, ref_out, atol=1e-5, rtol=1e-5)
+
+
 def test_silu_and_mul() -> None:
     for dtype in [torch.half, torch.bfloat16, torch.float]:
         for num_tokens in [7, 83, 2048]:
             for d in [512, 4096, 5120, 13824]:
                 print(f'Testing dtype={dtype}, num_tokens={num_tokens}, d={d}')
                 run_silu_and_mul(num_tokens, d, dtype)
+
+
+def test_fast_gelu() -> None:
+    for dtype in [torch.half, torch.bfloat16, torch.float]:
+        for num_tokens in [7, 83, 2048]:
+            for d in [512, 4096, 5120, 13824]:
+                print(f'Testing dtype={dtype}, num_tokens={num_tokens}, d={d}')
+                run_fast_gelu(num_tokens, d, dtype)
