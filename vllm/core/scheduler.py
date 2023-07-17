3b5267857653d689acc44dbe3c3d73a898f59367
Zhuohan Li <zhuohan123@gmail.com>
2023-07-17 00:33:48 -0400

    [Fix] Fix the condition of max_seq_len (#477)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/core/scheduler.py
+++ vllm/core/scheduler.py
@@ -190,7 +190,7 @@ class Scheduler:
                     break
 
                 num_prompt_tokens = seq_group.get_seqs()[0].get_len()
-                if num_prompt_tokens >= self.scheduler_config.max_seq_len:
+                if num_prompt_tokens > self.scheduler_config.max_seq_len:
                     logger.warning(
                         f"Input prompt ({num_prompt_tokens} tokens) is too long"
                         " and exceeds limit of "
