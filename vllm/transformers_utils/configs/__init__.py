2f9d4553ec7b3db445ac8b727e209a43af869539
BoringDoggie <57415741+ericzhou571@users.noreply.github.com>
2023-07-19 14:01:13 +0800

    add support for baichuan_13b

--- vllm/transformers_utils/configs/__init__.py
+++ vllm/transformers_utils/configs/__init__.py
@@ -1,7 +1,9 @@
 from vllm.transformers_utils.configs.mpt import MPTConfig
 from vllm.transformers_utils.configs.baichuan import BaiChuanConfig
+from vllm.transformers_utils.configs.baichuan_13b import BaichuanConfig
 
 __all__ = [
     "MPTConfig",
-    "BaiChuanConfig",
+    "BaiChuanConfig", # 7b
+    "BaichuanConfig", # 13b
 ]
