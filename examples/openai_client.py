c33680c4eea2085e0c8e6113d4d564e84164227b
Zhuohan Li <zhuohan123@gmail.com>
2023-07-25 21:16:08 +0000

    [Fix] Add chat completion Example and make fastchat an optional dependency

--- examples/openai_client.py
+++ examples/openai_client.py
@@ -3,26 +3,40 @@ import openai
 # Modify OpenAI's API key and API base to use vLLM's API server.
 openai.api_key = "EMPTY"
 openai.api_base = "http://localhost:8000/v1"
-model = "facebook/opt-125m"
 
-# Test list models API
+# List models API
 models = openai.Model.list()
 print("Models:", models)
 
-# Test completion API
-stream = True
+model = models["data"][0]["id"]
+
+# Completion API
+stream = False
 completion = openai.Completion.create(
     model=model,
     prompt="A robot may not injure a human being",
     echo=False,
     n=2,
-    best_of=3,
     stream=stream,
     logprobs=3)
 
-# print the completion
+print("Completion results:", )
 if stream:
     for c in completion:
         print(c)
 else:
-    print("Completion result:", completion)
+    print(completion)
+
+# Chat completion API
+chat_completion = openai.ChatCompletion.create(
+  model=model,
+  messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+    ]
+)
+
+print("Chat completion results:")
+print(chat_completion)
