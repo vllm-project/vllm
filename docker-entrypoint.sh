eeb3951a059b3e40c34326995c7e0b129ab5e45a
John Sabath <john@stability.ai>
2023-08-04 18:22:50 +0000

    tokenizer

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 MODEL=${MODEL:-stabilityai/StableBeluga-7B}
+TOKENIZER=${TOKENIZER:-$MODEL}
 NUM_SHARD=${NUM_SHARD:-1}
 HOST=${HOST:-0.0.0.0}
 PORT=${PORT:-80}
 
 python -u -m vllm.entrypoints.api_server \
     --model $MODEL \
+    --tokenizer $TOKENIZER \
     --host $HOST \
     --port $PORT \
     --tensor-parallel-size $NUM_SHARD
