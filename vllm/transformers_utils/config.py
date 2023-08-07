124b43d7c9279915647323b872e031d9c734ff2e
wq.chu <wq.chu@tianrang-inc.com>
2023-08-07 10:23:27 +0800

    add QWen-7b

--- vllm/transformers_utils/config.py
+++ vllm/transformers_utils/config.py
@@ -5,6 +5,7 @@ from vllm.transformers_utils.configs import *  # pylint: disable=wildcard-import
 _CONFIG_REGISTRY = {
     "mpt": MPTConfig,
     "baichuan": BaiChuanConfig,
+    "qwen": QWenConfig,
     "RefinedWeb": RWConfig,  # For tiiuae/falcon-40b(-instruct)
     "RefinedWebModel": RWConfig,  # For tiiuae/falcon-7b(-instruct)
 }
--- vllm/transformers_utils/configs/__init__.py
+++ vllm/transformers_utils/configs/__init__.py
@@ -1,5 +1,6 @@
 from vllm.transformers_utils.configs.mpt import MPTConfig
 from vllm.transformers_utils.configs.baichuan import BaiChuanConfig
+from vllm.transformers_utils.configs.qwen import QWenConfig
 # RWConfig is for the original tiiuae/falcon-40b(-instruct) and
 # tiiuae/falcon-7b(-instruct) models. Newer Falcon models will use the
 # `FalconConfig` class from the official HuggingFace transformers library.
@@ -8,5 +9,6 @@ from vllm.transformers_utils.configs.falcon import RWConfig
 __all__ = [
     "MPTConfig",
     "BaiChuanConfig",
+    "QWenConfig",
     "RWConfig",
 ]
