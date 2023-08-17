402105e847188ad5ebcc9294824805961b51be5d
Daniel <Danielphone221299@gmail.com>
2023-08-17 21:43:53 +0300

    Replaced create release with a js script

---
+++ .github/workflows/scripts/create_release.js
@@ -0,0 +1,22 @@
+async function run(github, context) {
+	try {
+		const response = await github.rest.repos.createRelease({
+			draft: false,
+			generate_release_notes: true,
+			name: process.env.RELEASE_TAG,
+			owner: context.repo.owner,
+			prerelease: false,
+			repo: context.repo.repo,
+			tag_name: process.env.RELEASE_TAG,
+		});
+	
+		core.exportVariable('release_id', response.data.id);
+		core.exportVariable('upload_url', response.data.upload_url);
+	} catch (error) {
+		core.setFailed(error.message);
+	}
+}
+
+module.exports = (github, context) => {
+	run(github, context)
+}
