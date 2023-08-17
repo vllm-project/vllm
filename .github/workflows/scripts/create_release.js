3a5f28d591f5c11000d8f630943d87bbddc3f84c
Daniel <Danielphone221299@gmail.com>
2023-08-17 22:12:16 +0300

    added pypi upload

--- .github/workflows/scripts/create_release.js
+++ .github/workflows/scripts/create_release.js
@@ -10,8 +10,7 @@ async function run(github, context) {
 			tag_name: process.env.RELEASE_TAG,
 		});
 	
-		core.exportVariable('release_id', response.data.id);
-		core.exportVariable('upload_url', response.data.upload_url);
+		core.setOutput('upload_url', response.data.upload_url);
 	} catch (error) {
 		core.setFailed(error.message);
 	}
