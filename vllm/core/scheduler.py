dc89552eaaf9d95a130794db662d29ada49627a3
wenjun93 <gwj1235@yeah.net>
2023-08-24 07:44:15 +0800

    Clean up code (#844)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/core/scheduler.py
+++ vllm/core/scheduler.py
@@ -379,9 +379,6 @@ class Scheduler:
         seq_group: SequenceGroup,
         blocks_to_swap_out: Dict[int, int],
     ) -> None:
-        seqs = seq_group.get_seqs(status=SequenceStatus.RUNNING)
-        for seq in seqs:
-            seq.status = SequenceStatus.SWAPPED
         self._swap_out(seq_group, blocks_to_swap_out)
         self.swapped.append(seq_group)
 
