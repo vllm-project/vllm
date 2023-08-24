eed18888859bde4050883110d51cb7607b1ac42e
root <rirv938@gmail.com>
2023-08-24 09:56:25 +0100

    update

--- vllm/awq_quantization/kernels/csrc/position_embedding/pos_encoding.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <torch/extension.h>
-
-void rotary_embedding_neox(
-  torch::Tensor& positions,
-  torch::Tensor& query,
-  torch::Tensor& key,
-  int head_size,
-  torch::Tensor& cos_sin_cache);
