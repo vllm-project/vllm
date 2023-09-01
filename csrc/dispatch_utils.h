46d9982399348cf4c1c4f9d33011ec370d8865cf
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2023-09-01 14:36:21 +0000

    Minor

--- csrc/dispatch_utils.h
+++ csrc/dispatch_utils.h
@@ -4,11 +4,11 @@
  */
 #include <torch/extension.h>
 
-#define VLLM_DISPATCH_CASE_FLOATING_TYPES(...)  \
-  AT_DISPATCH_CASE(at::ScalarType::Float, __VA_ARGS__)  \
-  AT_DISPATCH_CASE(at::ScalarType::Half, __VA_ARGS__) \
+#define VLLM_DISPATCH_CASE_FLOATING_TYPES(...)              \
+  AT_DISPATCH_CASE(at::ScalarType::Float, __VA_ARGS__)      \
+  AT_DISPATCH_CASE(at::ScalarType::Half, __VA_ARGS__)       \
   AT_DISPATCH_CASE(at::ScalarType::BFloat16, __VA_ARGS__)
 
-#define VLLM_DISPATCH_FLOATING_TYPES(TYPE, NAME, ...) \
-  AT_DISPATCH_SWITCH(                                       \
+#define VLLM_DISPATCH_FLOATING_TYPES(TYPE, NAME, ...)             \
+  AT_DISPATCH_SWITCH(                                             \
     TYPE, NAME, VLLM_DISPATCH_CASE_FLOATING_TYPES(__VA_ARGS__))
