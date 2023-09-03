d651b6682256ae6aaa744884257f53280f740b3f
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2023-09-03 13:18:41 +0000

    Minor

--- tests/kernels/test_layernorm.py
+++ tests/kernels/test_layernorm.py
@@ -1,5 +1,3 @@
-import math
-
 import pytest
 import torch
 import torch.nn as nn
@@ -8,7 +6,7 @@ from vllm import layernorm_ops
 
 DTYPES = [torch.half, torch.bfloat16, torch.float]
 HIDDEN_SIZES = [67, 768, 2048, 5120, 8192]  # Arbitrary values for testing
-NUM_TOKENS = [7, 83, 2048]  # Arbitrary values for testing
+NUM_TOKENS = [7, 83, 4096]  # Arbitrary values for testing
 SEEDS = [0]
 
 
@@ -17,18 +15,16 @@ class RefRMSNorm(nn.Module):
     def __init__(self, hidden_size, eps=1e-6):
         super().__init__()
         weight = torch.empty(hidden_size)
-        weight.uniform_(-math.sqrt(hidden_size), math.sqrt(hidden_size))
+        weight.normal_(mean=1.0, std=0.1)
         self.weight = nn.Parameter(weight)
         self.variance_epsilon = eps
 
     def forward(self, hidden_states):
-        variance = hidden_states.to(torch.float32).pow(2).mean(-1,
-                                                               keepdim=True)
-        hidden_states = hidden_states * torch.rsqrt(variance +
-                                                    self.variance_epsilon)
-        if self.weight.dtype in [torch.half, torch.bfloat16]:
-            hidden_states = hidden_states.to(self.weight.dtype)
-        return self.weight * hidden_states
+        input_dtype = hidden_states.dtype
+        hidden_states = hidden_states.to(torch.float32)
+        variance = hidden_states.pow(2).mean(-1, keepdim=True)
+        hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
+        return self.weight * hidden_states.to(input_dtype)
 
 
 @pytest.mark.parametrize("num_tokens", NUM_TOKENS)
@@ -44,8 +40,11 @@ def test_rms_norm(
 ) -> None:
     torch.random.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    x = torch.randn(num_tokens, hidden_size, dtype=dtype, device="cuda")
-    ref = RefRMSNorm(hidden_size).cuda().to(dtype)
+
+    scale = float(hidden_size**-0.5)
+    x = torch.empty(num_tokens, hidden_size, dtype=dtype, device="cuda")
+    x.uniform_(-scale, scale)
+    ref = RefRMSNorm(hidden_size).to(dtype).cuda()
 
     out = torch.empty_like(x)
     layernorm_ops.rms_norm(
@@ -55,4 +54,4 @@ def test_rms_norm(
         ref.variance_epsilon,
     )
     ref_out = ref(x)
-    assert torch.allclose(out, ref_out, atol=1e-3, rtol=1e-5)
+    assert torch.allclose(out, ref_out, atol=1e-2, rtol=1e-5)
