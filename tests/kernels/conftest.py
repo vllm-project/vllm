02d6331fdc0323a87c6b9fe8feb5cb179a45b584
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2023-09-03 09:36:05 +0000

    Normalize KV cache

--- tests/kernels/conftest.py
+++ tests/kernels/conftest.py
@@ -16,21 +16,24 @@ def create_kv_caches(
     torch.random.manual_seed(seed)
     torch.cuda.manual_seed(seed)
 
+    scale = head_size**-0.5
     x = 16 // torch.tensor([], dtype=dtype).element_size()
     key_cache_shape = (num_blocks, num_heads, head_size // x, block_size, x)
     key_caches = []
     for _ in range(num_layers):
-        key_cache = torch.randn(size=key_cache_shape,
+        key_cache = torch.empty(size=key_cache_shape,
                                 dtype=dtype,
                                 device='cuda')
+        key_cache.uniform_(-scale, scale)
         key_caches.append(key_cache)
 
     value_cache_shape = (num_blocks, num_heads, head_size, block_size)
     value_caches = []
     for _ in range(num_layers):
-        value_cache = torch.randn(size=value_cache_shape,
+        value_cache = torch.empty(size=value_cache_shape,
                                   dtype=dtype,
                                   device='cuda')
+        value_cache.uniform_(-scale, scale)
         value_caches.append(value_cache)
     return key_caches, value_caches
 
