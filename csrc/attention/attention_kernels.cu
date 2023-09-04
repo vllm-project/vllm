ea6782a16dc96a2cc0d02a89d80b886470969ea9
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2023-09-04 00:05:41 +0000

    Only consider block_idx == num_blocks - 1

--- csrc/attention/attention_kernels.cu
+++ csrc/attention/attention_kernels.cu
@@ -263,13 +263,15 @@ __global__ void single_query_cached_kv_attention_kernel(
       if (row_idx < HEAD_SIZE) {
         const int offset = row_idx * BLOCK_SIZE + physical_block_offset;
         V_vec v_vec = *reinterpret_cast<const V_vec*>(v_ptr + offset);
-        // NOTE(woosuk): When v_vec contains the tokens that are out of the context,
-        // we should explicitly zero out the values since they may contain NaNs.
-        // See https://github.com/vllm-project/vllm/issues/641#issuecomment-1682544472
-        scalar_t* v_vec_ptr = reinterpret_cast<scalar_t*>(&v_vec);
+        if (block_idx == num_blocks - 1) {
+          // NOTE(woosuk): When v_vec contains the tokens that are out of the context,
+          // we should explicitly zero out the values since they may contain NaNs.
+          // See https://github.com/vllm-project/vllm/issues/641#issuecomment-1682544472
+          scalar_t* v_vec_ptr = reinterpret_cast<scalar_t*>(&v_vec);
 #pragma unroll
-        for (int j = 0; j <= V_VEC_SIZE; j++) {
-          v_vec_ptr[j] = token_idx + j < context_len ? v_vec_ptr[j] : zero_value;
+          for (int j = 0; j <= V_VEC_SIZE; j++) {
+            v_vec_ptr[j] = token_idx + j < context_len ? v_vec_ptr[j] : zero_value;
+          }
         }
         accs[i] += dot(logits_vec, v_vec);
       }
