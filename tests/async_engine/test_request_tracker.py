9b0fc428193411b80c520e70f1fc339403376ac1
Antoni Baum <antoni.baum@protonmail.com>
2023-09-06 12:14:59 -0700

    Improve test
    Signed-off-by: Antoni Baum <antoni.baum@protonmail.com>

--- tests/async_engine/test_request_tracker.py
+++ tests/async_engine/test_request_tracker.py
@@ -6,19 +6,22 @@ from vllm.outputs import RequestOutput
 
 def test_request_tracker():
     tracker = RequestTracker()
-    tracker.add_request("1")
+    stream_1 = tracker.add_request("1")
     new, finished = tracker.get_new_and_finished_requests()
     assert len(new) == 1
     assert new[0]["request_id"] == "1"
     assert not finished
+    assert not stream_1.finished
 
-    tracker.add_request("2")
-    tracker.add_request("3")
+    stream_2 = tracker.add_request("2")
+    stream_3 = tracker.add_request("3")
     new, finished = tracker.get_new_and_finished_requests()
     assert len(new) == 2
     assert new[0]["request_id"] == "2"
     assert new[1]["request_id"] == "3"
     assert not finished
+    assert not stream_2.finished
+    assert not stream_3.finished
 
     # request_ids must be unique
     with pytest.raises(KeyError):
@@ -29,15 +32,17 @@ def test_request_tracker():
     assert len(finished) == 1
     assert "1" in finished
     assert not new
+    assert stream_1.finished
 
-    tracker.add_request("4")
+    stream_4 = tracker.add_request("4")
     tracker.abort_request("4")
     new, finished = tracker.get_new_and_finished_requests()
     assert len(finished) == 1
     assert "4" in finished
     assert not new
+    assert stream_4.finished
 
-    tracker.add_request("5")
+    stream_5 = tracker.add_request("5")
     tracker.process_request_output(
         RequestOutput("2", "output", [], [], finished=True))
     new, finished = tracker.get_new_and_finished_requests()
@@ -45,3 +50,5 @@ def test_request_tracker():
     assert "2" in finished
     assert len(new) == 1
     assert new[0]["request_id"] == "5"
+    assert stream_2.finished
+    assert not stream_5.finished
