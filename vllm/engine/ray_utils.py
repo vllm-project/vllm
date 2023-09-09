39d5691724320535c958555717274e1cec931ca2
Jingru <niejingru@hotmail.com>
2023-09-09 08:21:30 +0800

    fix "tansformers_module" ModuleNotFoundError when load model with `trust_remote_code=True` (#871)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/engine/ray_utils.py
+++ vllm/engine/ray_utils.py
@@ -11,7 +11,11 @@ try:
         """Ray wrapper for vllm.worker.Worker, allowing Worker to be
         lazliy initialized after Ray sets CUDA_VISIBLE_DEVICES."""
 
-        def __init__(self) -> None:
+        def __init__(self, init_cached_hf_modules=False) -> None:
+            if init_cached_hf_modules:
+                # pylint: disable=import-outside-toplevel
+                from transformers.dynamic_module_utils import init_hf_modules
+                init_hf_modules()
             self.worker = None
 
         def init_worker(self, worker_init_fn):
