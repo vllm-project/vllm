e67b4f2c2a216ff12d4f607caa3ba3409ae3f572
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2023-09-11 00:26:35 -0700

    Use FP32 in RoPE initialization (#1004)
    Co-authored-by: One <imone@tuta.io>

--- vllm/model_executor/layers/attention.py
+++ vllm/model_executor/layers/attention.py
@@ -264,10 +264,10 @@ class PagedAttentionWithRoPE(PagedAttention):
         self.is_neox_style = is_neox_style
 
         # Create the cos and sin cache.
-        inv_freq = 1.0 / (base**(
-            torch.arange(0, rotary_dim, 2, device="cuda") / rotary_dim))
-        t = torch.arange(max_position, device="cuda").float()
-        freqs = torch.einsum("i,j -> ij", t, inv_freq.float())
+        inv_freq = 1.0 / (base**(torch.arange(
+            0, rotary_dim, 2, dtype=torch.float, device="cuda") / rotary_dim))
+        t = torch.arange(max_position, dtype=torch.float, device="cuda")
+        freqs = torch.einsum("i,j -> ij", t, inv_freq)
         cos = freqs.cos()
         sin = freqs.sin()
         cache = torch.cat((cos, sin), dim=-1)
