4b7fd8e4a378a3ae4788e756b5e697e847cc504c
wanmok <chanwanmok@gmail.com>
2023-09-22 06:37:37 -0400

    Reformatted sampling_params.py

--- vllm/sampling_params.py
+++ vllm/sampling_params.py
@@ -55,25 +55,23 @@ class SamplingParams:
         get_prompt_logprobs： Whether to output logprobs for the prompt.
     """
 
-    def __init__(
-        self,
-        n: int = 1,
-        best_of: Optional[int] = None,
-        presence_penalty: float = 0.0,
-        frequency_penalty: float = 0.0,
-        temperature: float = 1.0,
-        top_p: float = 1.0,
-        top_k: int = -1,
-        use_beam_search: bool = False,
-        length_penalty: float = 1.0,
-        early_stopping: Union[bool, str] = False,
-        stop: Union[None, str, List[str]] = None,
-        stop_token_ids: List[int] = None,
-        ignore_eos: bool = False,
-        max_tokens: int = 16,
-        logprobs: Optional[int] = None,
-        get_prompt_logprobs: bool = False
-    ) -> None:
+    def __init__(self,
+                 n: int = 1,
+                 best_of: Optional[int] = None,
+                 presence_penalty: float = 0.0,
+                 frequency_penalty: float = 0.0,
+                 temperature: float = 1.0,
+                 top_p: float = 1.0,
+                 top_k: int = -1,
+                 use_beam_search: bool = False,
+                 length_penalty: float = 1.0,
+                 early_stopping: Union[bool, str] = False,
+                 stop: Union[None, str, List[str]] = None,
+                 stop_token_ids: List[int] = None,
+                 ignore_eos: bool = False,
+                 max_tokens: int = 16,
+                 logprobs: Optional[int] = None,
+                 get_prompt_logprobs: bool = False) -> None:
         self.n = n
         self.best_of = best_of if best_of is not None else n
         self.presence_penalty = presence_penalty
