3e7c3181c70efa3e3ab63263c2306679eed279ff
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2023-09-27 06:03:50 +0000

    Minor

--- vllm/transformers_utils/config.py
+++ vllm/transformers_utils/config.py
@@ -1,5 +1,6 @@
-from transformers import AutoConfig, PretrainedConfig
 from typing import Optional
+
+from transformers import AutoConfig, PretrainedConfig
 
 from vllm.transformers_utils.configs import *  # pylint: disable=wildcard-import
 
