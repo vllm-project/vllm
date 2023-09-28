36bc8b1b14a8c05110c2f8a85558a2830c2eda4c
Zhuohan Li <zhuohan123@gmail.com>
2023-09-28 05:18:47 +0000

    fix comment & variable name

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -103,21 +103,13 @@ class LlamaAttention(nn.Module):
         assert self.total_num_heads % tp_size == 0
         self.num_heads = self.total_num_heads // tp_size
         self.total_num_kv_heads = num_kv_heads
-        #it pertains to the execution of the Llama2-70b model on 16 or more GPUs.
-        # According to the configuration,
-        # we have a total of 8 kv-heads and 64 heads for the Llama2-70b model.
-        # Each kv-head is shared among 8 query-heads.
-        #When we shard the model based on the heads dimension to avoid
-        # duplicate computations, it’s natural to replicate kv-heads.
-        # This allows us to support up to 64 GPUs.
-        # Explicit replication isn’t necessary;
-        # we only need to remap the GPU rank to read the appropriate weights.
-        #For instance,
-        # GPUs 0 and 1 are remapped to 0,
-        # GPUs 2 and 3 are remapped to 1, and so on.
         if self.total_num_kv_heads >= tp_size:
+            # Number of KV heads is greater than TP size, so we partition
+            # the KV heads across multiple tensor parallel GPUs.
             assert self.total_num_kv_heads % tp_size == 0
         else:
+            # Number of KV heads is less than TP size, so we replicate
+            # the KV heads across multiple tensor parallel GPUs.
             assert tp_size % self.total_num_kv_heads == 0
         self.num_kv_heads = max(1, self.total_num_kv_heads // tp_size)
         num_kv_heads_replicas = max(1, tp_size // self.total_num_kv_heads)
@@ -131,8 +123,7 @@ class LlamaAttention(nn.Module):
         self.qkv_proj = ParallelLinear.column(
             hidden_size,
             (self.total_num_heads +
-             2 * self.total_num_kv_heads * num_kv_heads_replicas)
-            *  #### to make (2 * self.total_num_kv_heads * num_kv_heads_replicas)%world_size == 0
+             2 * self.total_num_kv_heads * num_kv_heads_replicas) *
             self.head_dim,
             bias=False,
             gather_output=False,
@@ -341,13 +332,15 @@ class LlamaForCausalLM(nn.Module):
                 row_parallel_weights.append(f"{layer}.{suffix}")
 
         tp_size = get_tensor_model_parallel_world_size()
-        tensor_model_parallel_rank = get_tensor_model_parallel_rank()
+        tp_rank = get_tensor_model_parallel_rank()
         q_proj_shard_size = (self.config.hidden_size // tp_size)
         num_kv_heads_replicas = max(1,
                                     tp_size // self.config.num_key_value_heads)
-        kv_proj_shard_size = (
-            self.config.hidden_size // self.config.num_attention_heads *
-            max(1, self.config.num_key_value_heads // tp_size))
+        num_kv_heads_per_gpu = max(1,
+                                   self.config.num_key_value_heads // tp_size)
+        kv_proj_shard_size = (self.config.hidden_size //
+                              self.config.num_attention_heads *
+                              num_kv_heads_per_gpu)
         attention_weight_specs = [
             # (weight_name, shard_size, offset)
             ("q_proj", q_proj_shard_size, 0),
@@ -384,12 +377,12 @@ class LlamaForCausalLM(nn.Module):
                     offset //= self.quant_config.pack_factor
 
                 if weight_name != "q_proj":
-                    tensor_model_parallel_rank_remap = tensor_model_parallel_rank // num_kv_heads_replicas
+                    shard_id = tp_rank // num_kv_heads_replicas
                 else:
-                    tensor_model_parallel_rank_remap = tensor_model_parallel_rank
-                loaded_weight = loaded_weight[
-                    shard_size * tensor_model_parallel_rank_remap:shard_size *
-                    (tensor_model_parallel_rank_remap + 1)]
+                    shard_id = tp_rank
+                loaded_weight = loaded_weight[shard_size *
+                                              shard_id:shard_size *
+                                              (shard_id + 1)]
                 param_slice = param.data[offset:offset + shard_size]
                 assert param_slice.shape == loaded_weight.shape
 
@@ -408,9 +401,8 @@ class LlamaForCausalLM(nn.Module):
                     param = param.T
 
                 shard_size = param.shape[0] // 2
-                loaded_weight = loaded_weight[
-                    shard_size * tensor_model_parallel_rank:shard_size *
-                    (tensor_model_parallel_rank + 1)]
+                loaded_weight = loaded_weight[shard_size * tp_rank:shard_size *
+                                              (tp_rank + 1)]
                 param_slice = param.data[shard_size * stride_id:shard_size *
                                          (stride_id + 1)]
                 assert param_slice.shape == loaded_weight.shape
@@ -426,10 +418,9 @@ class LlamaForCausalLM(nn.Module):
 
             if "embed_tokens" in name or "lm_head" in name:
                 load_padded_tensor_parallel_vocab(param, loaded_weight,
-                                                  tensor_model_parallel_rank)
+                                                  tp_rank)
                 continue
 
             load_tensor_parallel_weights(param, loaded_weight, name,
                                          column_parallel_weights,
-                                         row_parallel_weights,
-                                         tensor_model_parallel_rank)
+                                         row_parallel_weights, tp_rank)
