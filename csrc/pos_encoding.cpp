d5d4fcda1f9641f52dbd392b69d2ca6088575f94
zhangying169 <zhangying169@meituan.com>
2023-09-28 10:56:49 +0800

    fuse dequant and pos_encoding

--- csrc/pos_encoding.cpp
+++ csrc/pos_encoding.cpp
@@ -1,16 +1,21 @@
 #include <torch/extension.h>
 
-void rotary_embedding(
-  torch::Tensor& positions,
-  torch::Tensor& query,
-  torch::Tensor& key,
-  int head_size,
-  torch::Tensor& cos_sin_cache,
-  bool is_neox);
+void rotary_embedding(torch::Tensor &positions, torch::Tensor &query,
+                      torch::Tensor &key, int head_size,
+                      torch::Tensor &cos_sin_cache, bool is_neox);
+void invoke_dequant_rotary_embedding(
+    torch::Tensor &positions, // [num_tokens]
+    torch::Tensor &query,     // [num_tokens, num_heads * head_size]
+    torch::Tensor &query_out, // [num_tokens, num_heads * head_size]
+    torch::Tensor &key,       // [num_tokens, num_kv_heads * head_size]
+    torch::Tensor &key_out,   // [num_tokens, num_kv_heads * head_size]
+    int head_size,
+    torch::Tensor &cos_sin_cache, // [max_position, rot_dim]
+    const float query_scale, const float key_scale, bool is_neox);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  m.def(
-    "rotary_embedding",
-    &rotary_embedding,
-    "Apply GPT-NeoX or GPT-J style rotary embedding to query and key");
+  m.def("rotary_embedding", &rotary_embedding,
+        "Apply GPT-NeoX or GPT-J style rotary embedding to query and key");
+  m.def("invoke_dequant_rotary_embedding", &invoke_dequant_rotary_embedding,
+        "Dequant the input and apply rotary embedding.");
 }
