bb268c93f33b9fa1c7a19e8c048382309ad1d844
amaleshvemula <vemulaamalesh1997@gmail.com>
2023-10-06 17:05:08 +0200

    Update model_loader.py
    Added MistralForCausalLM awq quantization solution

--- vllm/model_executor/model_loader.py
+++ vllm/model_executor/model_loader.py
@@ -34,7 +34,7 @@ _MODEL_REGISTRY = {
 
 # FIXME(woosuk): Remove this once all models support quantization.
 _MODEL_CLASSES_SUPPORT_QUANTIZATION = [
-    LlamaForCausalLM,
+    LlamaForCausalLM, MistralForCausalLM
 ]
 
 
