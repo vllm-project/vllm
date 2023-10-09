b7fa377cbe38009d146d17f2b01557eb1c1f044e
amazeqiu <amazeqiu@tencent.com>
2023-10-09 15:57:11 +0800

    修改RMSNorm实现方式于hf对齐

--- vllm/model_executor/layers/layernorm.py
+++ vllm/model_executor/layers/layernorm.py
@@ -2,31 +2,47 @@
 import torch
 import torch.nn as nn
 
-from vllm import layernorm_ops
+# from vllm import layernorm_ops
 
 
-class RMSNorm(nn.Module):
-    """Root mean square normalization.
+# class RMSNorm(nn.Module):
+#     """Root mean square normalization.
 
-    Computes x -> w * x / sqrt(E[x^2] + eps) where w is the learned weight.
-    Refer to https://arxiv.org/abs/1910.07467
-    """
+#     Computes x -> w * x / sqrt(E[x^2] + eps) where w is the learned weight.
+#     Refer to https://arxiv.org/abs/1910.07467
+#     """
 
-    def __init__(
-        self,
-        hidden_size: int,
-        eps: float = 1e-6,
-    ) -> None:
+#     def __init__(
+#         self,
+#         hidden_size: int,
+#         eps: float = 1e-6,
+#     ) -> None:
+#         super().__init__()
+#         self.weight = nn.Parameter(torch.ones(hidden_size))
+#         self.variance_epsilon = eps
+
+#     def forward(self, x: torch.Tensor) -> torch.Tensor:
+#         out = torch.empty_like(x)
+#         layernorm_ops.rms_norm(
+#             out,
+#             x,
+#             self.weight.data,
+#             self.variance_epsilon,
+#         )
+#         return out
+
+class RMSNorm(torch.nn.Module):
+    def __init__(self, hidden_size, epsilon=1e-6):
         super().__init__()
-        self.weight = nn.Parameter(torch.ones(hidden_size))
-        self.variance_epsilon = eps
+        self.weight = torch.nn.Parameter(torch.empty(hidden_size))
+        self.epsilon = epsilon
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        out = torch.empty_like(x)
-        layernorm_ops.rms_norm(
-            out,
-            x,
-            self.weight.data,
-            self.variance_epsilon,
-        )
-        return out
+    def forward(self, hidden_states):
+        variance = hidden_states.to(torch.float32).pow(2).mean(-1, keepdim=True)
+        hidden_states = hidden_states * torch.rsqrt(variance + self.epsilon)
+
+        # convert into half-precision
+        if self.weight.dtype in [torch.float16, torch.bfloat16]:
+            hidden_states = hidden_states.to(self.weight.dtype)
+
+        return self.weight * hidden_states
