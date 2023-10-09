388a2154293112fb12882cac6c35558b7ad52ead
zhangying169 <zhangying169@meituan.com>
2023-10-09 19:54:58 +0800

    add tests for fusion kernels

--- tests/kernels/test_activation.py
+++ tests/kernels/test_activation.py
@@ -9,6 +9,9 @@ DTYPES = [torch.half, torch.bfloat16, torch.float]
 NUM_TOKENS = [7, 83, 2048]  # Arbitrary values for testing
 D = [512, 4096, 5120, 13824]  # Arbitrary values for testing
 SEEDS = [0]
+SCALE_UP = [0.09, 1.2, 1.9]
+SCALE_GATE = [2.17, 1.2, 1.9]
+SCALE_OUT = [1.2, 1.9, 0.17]
 
 
 def ref_silu_and_mul(x: torch.Tensor) -> torch.Tensor:
@@ -34,6 +37,40 @@ def test_silu_and_mul(
     activation_ops.silu_and_mul(out, x)
     ref_out = ref_silu_and_mul(x)
     assert torch.allclose(out, ref_out, atol=1e-5, rtol=1e-5)
+
+
+@pytest.mark.parametrize("num_tokens", NUM_TOKENS)
+@pytest.mark.parametrize("d", D)
+@pytest.mark.parametrize("dtype", DTYPES)
+@pytest.mark.parametrize("seed", SEEDS)
+@pytest.mark.parametrize("scale_gate", SCALE_GATE)
+@pytest.mark.parametrize("scale_up", SCALE_UP)
+@pytest.mark.parametrize("scale_out", SCALE_OUT)
+@torch.inference_mode()
+def test_dequant_silu_and_mul_quant(
+    num_tokens: int,
+    d: int,
+    dtype: torch.dtype,
+    seed: int,
+    scale_gate: float,
+    scale_up: float,
+    scale_out: float
+) -> None:
+    torch.random.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    # x = torch.randn(num_tokens, 2 * d, dtype=dtype, device='cuda')
+    x = torch.randint(-1000, 1000, (num_tokens, 2 * d), dtype=torch.int32, device='cuda')
+    x_ = torch.empty_like(x, dtype=dtype)
+    x_[:, :d] = x[:, :d] * scale_gate
+    x_[:, d:] = x[:, d:] * scale_up
+    out1 = torch.empty(num_tokens, d, dtype=dtype, device='cuda')
+    activation_ops.silu_and_mul(out1, x_)
+    out1 = (out1 / scale_out).round().clamp(-128, 127).to(torch.int8)
+    # ref_out = ref_silu_and_mul(x)
+
+    out2 = torch.empty(num_tokens, d, dtype=torch.int8, device='cuda')
+    activation_ops.invoke_dequant_silu_and_mul_quant(out2, x, scale_gate, scale_up, scale_out)
+    assert torch.allclose(out1, out2, atol=2)
 
 
 @pytest.mark.parametrize("num_tokens", NUM_TOKENS)
--- tests/kernels/test_layernorm.py
+++ tests/kernels/test_layernorm.py
@@ -2,12 +2,13 @@ import pytest
 import torch
 import torch.nn as nn
 
-from vllm import layernorm_ops
+from vllm import layernorm_ops, fused_kernels
 
 DTYPES = [torch.half, torch.bfloat16, torch.float]
 HIDDEN_SIZES = [67, 768, 2048, 5120, 8192]  # Arbitrary values for testing
 NUM_TOKENS = [7, 83, 4096]  # Arbitrary values for testing
 SEEDS = [0]
+SCALE = [0.1, 0.5, 0.8, 1.2, 2.1]
 
 
 class RefRMSNorm(nn.Module):
@@ -56,3 +57,98 @@ def test_rms_norm(
     )
     ref_out = ref(x)
     assert torch.allclose(out, ref_out, atol=1e-2, rtol=1e-5)
+
+@pytest.mark.parametrize("num_tokens", NUM_TOKENS)
+@pytest.mark.parametrize("hidden_size", HIDDEN_SIZES)
+@pytest.mark.parametrize("dtype", DTYPES)
+@pytest.mark.parametrize("seed", SEEDS)
+@torch.inference_mode()
+def test_rms_norm_quant(
+    num_tokens: int,
+    hidden_size: int,
+    dtype: torch.dtype,
+    seed: int,
+) -> None:
+    torch.random.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+
+    scale = float(hidden_size**-0.5)
+    x = torch.empty(num_tokens, hidden_size, dtype=dtype, device="cuda")
+    x.uniform_(-scale, scale)
+    ref = RefRMSNorm(hidden_size).to(dtype).cuda()
+
+    out1 = torch.empty_like(x)
+    layernorm_ops.rms_norm(
+        out1,
+        x,
+        ref.weight.data,
+        ref.variance_epsilon,
+    )
+    out1 = out1.clamp(-128, 127).round().to(torch.int8)
+    out2 = torch.empty_like(x, dtype=torch.int8)
+    layernorm_ops.invoke_rms_norm_quant(out2, x, ref.weight.data, ref.variance_epsilon)
+    assert torch.allclose(out1, out2, atol=1.0)
+
+@pytest.mark.parametrize("num_tokens", NUM_TOKENS)
+@pytest.mark.parametrize("hidden_size", HIDDEN_SIZES)
+@pytest.mark.parametrize("dtype", DTYPES)
+@pytest.mark.parametrize("seed", SEEDS)
+@pytest.mark.parametrize("scale", SCALE)
+@torch.inference_mode()
+def test_dequant_add_residual_rms_norm_quant(
+    num_tokens: int,
+    hidden_size: int,
+    dtype: torch.dtype,
+    seed: int,
+    scale: float
+) -> None:
+    torch.random.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+
+    s = float(hidden_size**-0.5)
+    residual = torch.empty(num_tokens, hidden_size, dtype=dtype, device="cuda")
+    # x = torch.randint(torch.iinfo(torch.int32).min, torch.iinfo(torch.int32).max, (num_tokens, hidden_size), dtype=torch.int32, device="cuda")
+    x = torch.randint(-1000, 1000, (num_tokens, hidden_size), dtype=torch.int32, device="cuda")
+    residual.uniform_(-s, s)
+    ref = RefRMSNorm(hidden_size).to(dtype).cuda()
+    x_ = (x * scale + residual).to(dtype)
+
+    out1 = torch.empty_like(x_)
+    layernorm_ops.rms_norm(
+        out1,
+        x_,
+        ref.weight.data,
+        ref.variance_epsilon,
+    )
+    out1 = out1.round().clamp(-128, 127).to(torch.int8)
+    out2 = torch.empty_like(x, dtype=torch.int8)
+    layernorm_ops.invoke_dequant_add_residual_rms_norm_quant(out2, x, residual, ref.weight.data, ref.variance_epsilon, scale)
+
+    assert torch.allclose(out1, out2, atol=1.0)
+
+@pytest.mark.parametrize("num_tokens", NUM_TOKENS)
+@pytest.mark.parametrize("hidden_size", HIDDEN_SIZES)
+@pytest.mark.parametrize("dtype", DTYPES)
+@pytest.mark.parametrize("seed", SEEDS)
+@pytest.mark.parametrize("scale", SCALE)
+@torch.inference_mode()
+def test_dequant_add_residual(
+    num_tokens: int,
+    hidden_size: int,
+    dtype: torch.dtype,
+    seed: int,
+    scale: float
+) -> None:
+    torch.random.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+
+    s = float(hidden_size**-0.5)
+    residual = torch.empty(num_tokens, hidden_size, dtype=dtype, device="cuda")
+    x = torch.randint(torch.iinfo(torch.int32).min, torch.iinfo(torch.int32).max, (num_tokens, hidden_size), dtype=torch.int32, device="cuda")
+    residual.uniform_(-s, s)
+    out1 = (x * scale + residual).to(dtype)
+
+    out2 = torch.empty_like(x, dtype=dtype)
+    fused_kernels.invoke_dequant_add_residual(out2, x, residual, scale)
+
+    assert torch.allclose(out1, out2, atol=0.001)
