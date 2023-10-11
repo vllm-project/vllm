e428f2498441cb6606859ec61a76bc4ab60e1b22
amazeqiu <amazeqiu@tencent.com>
2023-10-11 12:20:53 +0800

    修改attention测试指标为1e-4

--- tests/kernels/test_attention.py
+++ tests/kernels/test_attention.py
@@ -193,7 +193,7 @@ def test_single_query_cached_kv_attention(
     # NOTE(woosuk): Due to the kernel-level differences in the two
     # implementations, there is a small numerical difference in the two
     # outputs. Thus, we use a relaxed tolerance for the test.
-    assert torch.allclose(output, ref_output, atol=1e-5, rtol=1e-5)
+    assert torch.allclose(output, ref_output, atol=1e-4, rtol=1e-5)
 
 
 def ref_multi_query_kv_attention(
@@ -292,4 +292,4 @@ def test_multi_query_kv_attention(
         scale,
         dtype,
     )
-    assert torch.allclose(output, ref_output, atol=1e-5, rtol=1e-5)
+    assert torch.allclose(output, ref_output, atol=1e-4, rtol=1e-5)
