565d1dfc97e7339262edf9ef2cc150d97db00765
amazeqiu <amazeqiu@tencent.com>
2023-10-14 15:49:01 +0800

    debug lora

--- vllm/model_executor/models/baichuan.py
+++ vllm/model_executor/models/baichuan.py
@@ -192,7 +192,6 @@ class BaiChuanAttention(nn.Module):
         input_metadata: InputMetadata,
         cache_event: Optional[torch.cuda.Event],
     ) -> torch.Tensor:
-        print("Compute qkv")
         qkv, _ = self.W_pack(hidden_states)
         q, k, v = qkv.chunk(chunks=3, dim=-1)
         k_cache, v_cache = kv_cache
@@ -287,6 +286,7 @@ class BaiChuanModel(nn.Module):
         cache_events: Optional[List[torch.cuda.Event]],
     ) -> torch.Tensor:
         hidden_states = self.embed_tokens(input_ids)
+        print(f"Compute input ids: {input_ids}")
         for i in range(len(self.layers)):
             if cache_events is None:
                 cache_event = None
