9e6d8cc30647becff76de3ba16ab49b1a758ee40
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2023-10-16 17:56:47 +0000

    Fix PyTorch version to 2.0.1 in workflow

--- .github/workflows/scripts/pytorch-install.sh
+++ .github/workflows/scripts/pytorch-install.sh
@@ -5,7 +5,8 @@ cuda_version=$2
 
 # Install torch
 $python_executable -m pip install numpy pyyaml scipy ipython mkl mkl-include ninja cython typing pandas typing-extensions dataclasses setuptools && conda clean -ya
-$python_executable -m pip install torch -f https://download.pytorch.org/whl/cu${cuda_version//./}/torch_stable.html
+# Temporarily fix the PyTorch version to v2.0.1
+$python_executable -m pip install torch==2.0.1+cu${cuda_version//./} --index-url https://download.pytorch.org/whl/cu${cuda_version//./}
 
 # Print version information
 $python_executable --version
