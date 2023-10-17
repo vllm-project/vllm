4ee29a9e1d05ec9eaa8f9610b4e264e7f9fdb1c0
zhangying169 <zhangying169@meituan.com>
2023-10-17 19:37:55 +0800

    add quant, dequant kernel

--- csrc/fused.cpp
+++ csrc/fused.cpp
@@ -6,7 +6,21 @@ void invoke_dequant_add_residual(
     torch::Tensor &residual, // [num_tokens, hidden_size]
     float scale);
 
+void invoke_dequant(
+    torch::Tensor &out,      // [num_tokens, hidden_size]
+    torch::Tensor &input,    // [num_tokens, hidden_size]
+    float scale);
+
+void invoke_quant(
+    torch::Tensor &out,      // [num_tokens, hidden_size]
+    torch::Tensor &input,    // [num_tokens, hidden_size]
+    float scale);
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("invoke_dequant_add_residual", &invoke_dequant_add_residual,
         "Add the dequanted result and residual.");
+  m.def("invoke_dequant", &invoke_dequant,
+        "Dequant.");
+  m.def("invoke_quant", &invoke_quant,
+        "Quant.");
 }
