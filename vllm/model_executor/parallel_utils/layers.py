89f383721fe154f9df7a7f08503df2b594778c36
amazeqiu <amazeqiu@tencent.com>
2023-10-19 16:11:53 +0800

    修复计算lora函数参数类型不兼容py3.8错误

--- vllm/model_executor/parallel_utils/layers.py
+++ vllm/model_executor/parallel_utils/layers.py
@@ -304,7 +304,7 @@ class RowParallelLinear(torch.nn.Module):
         return output, output_bias
 
 
-def compulate_lora(obj: LoraLayer,x: torch.Tensor, output:torch.Tensor, batch_token_lengths: list[int], batch_lora_ids: list[str]) -> torch.Tensor:
+def compulate_lora(obj: LoraLayer,x: torch.Tensor, output:torch.Tensor, batch_token_lengths: list, batch_lora_ids: list) -> torch.Tensor:
     # print(f"batch_token_lengths: {batch_token_lengths}; batch_lora_ids: {batch_lora_ids}")
     assert len(batch_token_lengths) == len(batch_lora_ids), (batch_token_lengths, batch_lora_ids)
     start = 0
