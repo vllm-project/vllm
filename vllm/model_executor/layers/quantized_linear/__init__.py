1f24755bf802a2061bd46f3dd1191b7898f13f45
chooper1 <54963573+chooper1@users.noreply.github.com>
2023-10-22 03:14:59 -0300

    Support SqueezeLLM (#1326)
    Co-authored-by: squeeze-ai-lab <squeezeailab.bair@gmail.com>
    Co-authored-by: Woosuk Kwon <woosuk.kwon@berkeley.edu>

--- vllm/model_executor/layers/quantized_linear/__init__.py
+++ vllm/model_executor/layers/quantized_linear/__init__.py
@@ -1,10 +1,14 @@
 from vllm.model_executor.layers.quantized_linear.awq import (
     AWQColumnParallelLinear, AWQRowParallelLinear)
+from vllm.model_executor.layers.quantized_linear.squeezellm import (
+    SqueezeLLMColumnParallelLinear, SqueezeLLMRowParallelLinear)
 from vllm.model_executor.parallel_utils.layers import (ColumnParallelLinear,
                                                        RowParallelLinear)
 
 _QUANTIZED_LINEAR_REGISTRY = {
     "awq": (AWQColumnParallelLinear, AWQRowParallelLinear),
+    "squeezellm":
+    (SqueezeLLMColumnParallelLinear, SqueezeLLMRowParallelLinear),
 }
 
 
