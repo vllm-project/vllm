68017c36e52de4ea32f9f020ab789c29e6279910
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2023-11-07 05:35:58 +0000

    Remove GPTQ kernels

--- csrc/quantization.cpp
+++ csrc/quantization.cpp
@@ -8,23 +8,6 @@ torch::Tensor awq_gemm(
   torch::Tensor _zeros,
   int split_k_iters);
 
-uintptr_t make_q_matrix(
-    torch::Tensor q_weight,
-    torch::Tensor q_perm,
-    torch::Tensor q_invperm,
-    torch::Tensor gptq_qzeros,
-    torch::Tensor gptq_scales,
-    torch::Tensor gptq_g_idx,
-    torch::Tensor temp_dq
-);
-
-void gemm_half_q_half(
-    torch::Tensor a,
-    uintptr_t b,
-    torch::Tensor c,
-    bool force_cuda
-);
-
 void gptq_descact_matmul(
   torch::Tensor vec,
   torch::Tensor mat,
@@ -44,14 +27,6 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     "awq_gemm",
     &awq_gemm,
     "Quantized GEMM for AWQ");
-  m.def(
-    "make_q_matrix",
-    &make_q_matrix,
-    "make_q_matrix");
-  m.def(
-    "gemm_half_q_half",
-    &gemm_half_q_half,
-    "gemm_half_q_half");
   m.def(
     "gptq_descact_matmul",
     &gptq_descact_matmul,
