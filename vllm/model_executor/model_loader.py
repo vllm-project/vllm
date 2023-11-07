b21e0c4fbb453a0757bfbe0bbdc8902f422ab641
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2023-11-07 06:57:00 +0000

    Roll back

--- vllm/model_executor/model_loader.py
+++ vllm/model_executor/model_loader.py
@@ -10,7 +10,6 @@ from vllm.config import ModelConfig
 from vllm.model_executor.models import *  # pylint: disable=wildcard-import
 from vllm.model_executor.weight_utils import (get_quant_config,
                                               initialize_dummy_weights)
-from vllm.model_executor.layers.quantized_linear.utils import quant_post_init
 
 # TODO(woosuk): Lazy-load the model classes.
 _MODEL_REGISTRY = {
@@ -28,34 +27,19 @@ _MODEL_REGISTRY = {
     "LlamaForCausalLM": LlamaForCausalLM,
     "LLaMAForCausalLM": LlamaForCausalLM,  # For decapoda-research/llama-*
     "MistralForCausalLM": MistralForCausalLM,
-    "MPTForCausalLM": MPTForCausalLM,
+    # transformers's mpt class has lower case
+    "MptForCausalLM": MptForCausalLM,
+    "MPTForCausalLM": MptForCausalLM,
     "OPTForCausalLM": OPTForCausalLM,
     "QWenLMHeadModel": QWenLMHeadModel,
     "RWForCausalLM": FalconForCausalLM,
 }
 
 # FIXME(woosuk): Remove this once all models support quantization.
-_MODEL_CLASSES_SUPPORT_QUANTIZATION = {
-    "awq": [LlamaForCausalLM, MistralForCausalLM],
-    "squeezellm": [LlamaForCausalLM, MistralForCausalLM],
-    "gptq": [
-        LlamaForCausalLM,
-        QWenLMHeadModel,
-        BaiChuanForCausalLM,
-        BaichuanForCausalLM,
-        BloomForCausalLM,
-        GPT2LMHeadModel,
-        GPTJForCausalLM,
-        GPTNeoXForCausalLM,
-        GPTBigCodeForCausalLM,
-        InternLMForCausalLM,
-        FalconForCausalLM,
-        AquilaForCausalLM,
-        OPTForCausalLM,
-        MPTForCausalLM,
-        MistralForCausalLM,
-    ],
-}
+_MODEL_CLASSES_SUPPORT_QUANTIZATION = [
+    LlamaForCausalLM,
+    MistralForCausalLM,
+]
 
 
 @contextlib.contextmanager
@@ -77,19 +61,17 @@ def _get_model_architecture(config: PretrainedConfig) -> Type[nn.Module]:
         f"Supported architectures: {list(_MODEL_REGISTRY.keys())}")
 
 
-def get_model(model_config: ModelConfig, max_tokens: int) -> nn.Module:
+def get_model(model_config: ModelConfig) -> nn.Module:
     model_class = _get_model_architecture(model_config.hf_config)
 
     # Get the quantization config.
     quant_config = None
     if model_config.quantization is not None:
-        if model_class not in _MODEL_CLASSES_SUPPORT_QUANTIZATION[
-                model_config.quantization]:
+        if model_class not in _MODEL_CLASSES_SUPPORT_QUANTIZATION:
             raise ValueError(
                 f"Quantization is not supported for {model_class}.")
         quant_config = get_quant_config(model_config.quantization,
                                         model_config.model,
-                                        model_config.hf_config,
                                         model_config.download_dir)
         capability = torch.cuda.get_device_capability()
         capability = capability[0] * 10 + capability[1]
@@ -109,9 +91,7 @@ def get_model(model_config: ModelConfig, max_tokens: int) -> nn.Module:
     with _set_default_torch_dtype(model_config.dtype):
         # Create a model instance.
         # The weights will be initialized as empty tensors.
-        if model_config.quantization is not None and (
-                model_class in _MODEL_CLASSES_SUPPORT_QUANTIZATION[
-                    model_config.quantization]):
+        if model_class in _MODEL_CLASSES_SUPPORT_QUANTIZATION:
             model = model_class(model_config.hf_config, quant_config)
         else:
             model = model_class(model_config.hf_config)
@@ -125,6 +105,4 @@ def get_model(model_config: ModelConfig, max_tokens: int) -> nn.Module:
             model.load_weights(model_config.model, model_config.download_dir,
                                model_config.load_format, model_config.revision)
             model = model.cuda()
-        if model_config.quantization is not None:
-            quant_post_init(model, max_tokens)
     return model.eval()
