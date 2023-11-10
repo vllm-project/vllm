d09b8caa60224dd5f114eb3120053c079c592270
ljss <450993438@qq.com>
2023-11-10 10:46:15 +0800

    Format

--- vllm/model_executor/layers/attention.py
+++ vllm/model_executor/layers/attention.py
@@ -98,18 +98,15 @@ class PagedAttention(nn.Module):
         """
         if self.num_kv_heads != self.num_heads:
             # Project the key and value tensors to the desired number of heads.
-            query = query.view(query.shape[0],
-                               self.num_kv_heads,
-                               self.num_queries_per_kv,
-                               query.shape[-1])
-            key = key[:, :, None, :].expand(key.shape[0],
-                                            self.num_kv_heads,
-                                            self.num_queries_per_kv,
-                                            key.shape[-1])
-            value = value[:, :, None, :].expand(value.shape[0],
-                                                self.num_kv_heads,
-                                                self.num_queries_per_kv,
-                                                value.shape[-1])
+            query = query.view(query.shape[0], self.num_kv_heads,
+                               self.num_queries_per_kv, query.shape[-1])
+            key = key[:, :,
+                      None, :].expand(key.shape[0], self.num_kv_heads,
+                                      self.num_queries_per_kv, key.shape[-1])
+            value = value[:, :,
+                          None, :].expand(value.shape[0], self.num_kv_heads,
+                                          self.num_queries_per_kv,
+                                          value.shape[-1])
 
         # TODO(woosuk): The unsqueeze op may incur some CPU overhead. Optimize.
         out = xops.memory_efficient_attention_forward(
@@ -453,18 +450,15 @@ class PagedAttentionWithALiBi(PagedAttention):
         """
         if self.num_kv_heads != self.num_heads:
             # Project the key and value tensors to the desired number of heads.
-            query = query.view(query.shape[0],
-                               self.num_kv_heads,
-                               self.num_queries_per_kv,
-                               query.shape[-1])
-            key = key[:, :, None, :].expand(key.shape[0],
-                                            self.num_kv_heads,
-                                            self.num_queries_per_kv,
-                                            key.shape[-1])
-            value = value[:, :, None, :].expand(value.shape[0],
-                                                self.num_kv_heads,
-                                                self.num_queries_per_kv,
-                                                value.shape[-1])
+            query = query.view(query.shape[0], self.num_kv_heads,
+                               self.num_queries_per_kv, query.shape[-1])
+            key = key[:, :,
+                      None, :].expand(key.shape[0], self.num_kv_heads,
+                                      self.num_queries_per_kv, key.shape[-1])
+            value = value[:, :,
+                          None, :].expand(value.shape[0], self.num_kv_heads,
+                                          self.num_queries_per_kv,
+                                          value.shape[-1])
         batch_size = input_metadata.num_prompts
         seq_len = input_metadata.max_prompt_len
 
