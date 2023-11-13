35d0a903b761a3b643f8adc0a68b91edc6387dac
roy <jasonailu87@gmail.com>
2023-11-13 20:02:06 +0800

    add doc

--- vllm/sampling_params.py
+++ vllm/sampling_params.py
@@ -52,6 +52,9 @@ class SamplingParams:
             to consider. Must be in (0, 1]. Set to 1 to consider all tokens.
         top_k: Integer that controls the number of top tokens to consider. Set
             to -1 to consider all tokens.
+        min_p: Float that represents the minimum probability for a token to be 
+            considered, relative to the probability of the most likely token.
+            Must be in [0, 1]. Set to 0 to disable this.
         use_beam_search: Whether to use beam search instead of sampling.
         length_penalty: Float that penalizes sequences based on their length.
             Used in beam search.
