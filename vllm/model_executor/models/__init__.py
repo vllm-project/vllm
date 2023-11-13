6f3b8e93070b7829ac5cd9efd1b6dcd303ad8138
maximzubkov <maksim.zubkov@epfl.ch>
2023-11-13 19:54:14 +0100

    Minor update in model configuration

--- vllm/model_executor/models/__init__.py
+++ vllm/model_executor/models/__init__.py
@@ -13,7 +13,7 @@ from vllm.model_executor.models.mistral import MistralForCausalLM
 from vllm.model_executor.models.mpt import MptForCausalLM
 from vllm.model_executor.models.opt import OPTForCausalLM
 from vllm.model_executor.models.qwen import QWenLMHeadModel
-from vllm.model_executor.models.phi import PhiForCausalLM
+from vllm.model_executor.models.phi_1_5 import PhiForCausalLM
 from vllm.model_executor.models.chatglm import ChatGLMForCausalLM
 from vllm.model_executor.models.yi import YiForCausalLM
 
