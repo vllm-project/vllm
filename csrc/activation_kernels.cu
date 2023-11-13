1c65ed0db86b8a4b2f763bc466957f073537492f
ljss <31004720+beginlner@users.noreply.github.com>
2023-11-13 16:44:53 +0800

    BugFix

--- csrc/activation_kernels.cu
+++ csrc/activation_kernels.cu
@@ -18,8 +18,8 @@ __global__ void silu_and_mul_kernel(
   const int d) {
   const int64_t token_idx = blockIdx.x;
   for (int64_t idx = threadIdx.x; idx < d; idx += blockDim.x) {
-    const scalar_t x = __ldg(&input[token_idx * 2 * d + idx]);
-    const scalar_t y = __ldg(&input[token_idx * 2 * d + d + idx]);
+    const scalar_t x = __ldg(&input[(int64_t) token_idx * 2 * d + idx]);
+    const scalar_t y = __ldg(&input[(int64_t) token_idx * 2 * d + d + idx]);
     out[token_idx * d + idx] = silu(x) * y;
   }
 }
@@ -57,7 +57,7 @@ __global__ void activation_kernel(
   const int d) {
   const int64_t token_idx = blockIdx.x;
   for (int64_t idx = threadIdx.x; idx < d; idx += blockDim.x) {
-    const scalar_t x = __ldg(&input[token_idx * d + idx]);
+    const scalar_t x = __ldg(&input[(int64_t) token_idx * d + idx]);
     out[token_idx * d + idx] = ACT_FN(x);
   }
 }
