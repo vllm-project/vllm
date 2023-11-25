fa60236ebd3c89c831fe4c956592a33ec6ee7314
mspronesti <massimiliano.pronesti@gmail.com>
2023-11-25 20:16:00 +0100

    chore(examples): upgrade to OpenAI v1

--- examples/openai_completion_client.py
+++ examples/openai_completion_client.py
@@ -1,24 +1,28 @@
-import openai
+from openai import OpenAI
 
 # Modify OpenAI's API key and API base to use vLLM's API server.
-openai.api_key = "EMPTY"
-openai.api_base = "http://localhost:8000/v1"
+openai_api_key = "EMPTY"
+openai_api_base = "http://localhost:8000/v1"
 
-# List models API
-models = openai.Model.list()
-print("Models:", models)
+client = OpenAI(
+    # defaults to os.environ.get("OPENAI_API_KEY")
+    api_key=openai_api_key,
+    base_url=openai_api_base,
+)
 
-model = models["data"][0]["id"]
+models = client.models.list()
+model = models.data[0].id
 
 # Completion API
 stream = False
-completion = openai.Completion.create(
+completion = client.completions.create(
     model=model,
     prompt="A robot may not injure a human being",
     echo=False,
     n=2,
     stream=stream,
-    logprobs=3)
+    logprobs=3
+)
 
 print("Completion results:")
 if stream:
