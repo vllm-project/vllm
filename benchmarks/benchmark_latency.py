2af2396f2d6310e3aa33c301653be572522ac866
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2023-11-29 19:57:45 +0000

    Add CPU profile

--- benchmarks/benchmark_latency.py
+++ benchmarks/benchmark_latency.py
@@ -37,8 +37,10 @@ def main(args: argparse.Namespace):
 
     def run_to_completion(profile: bool = False):
         if profile:
-            with torch.profiler.profile(
-                    activities=[torch.profiler.ProfilerActivity.CUDA]) as p:
+            with torch.profiler.profile(activities=[
+                    torch.profiler.ProfilerActivity.CPU,
+                    torch.profiler.ProfilerActivity.CUDA
+            ]) as p:
                 llm.generate(prompt_token_ids=dummy_prompt_token_ids,
                              sampling_params=sampling_params,
                              use_tqdm=False)
