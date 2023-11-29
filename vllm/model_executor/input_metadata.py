95f6dace72f679be45e5fe26abf7ddc1c321864b
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2023-11-29 23:41:34 +0000

    Add docstrings to InputMetadata

--- vllm/model_executor/input_metadata.py
+++ vllm/model_executor/input_metadata.py
@@ -4,6 +4,15 @@ import torch
 
 
 class InputMetadata:
+    """Metadata for input sequences. Used in PagedAttention.
+
+    Args:
+        prompt_lens: Lengths of prompts.
+        slot_mapping: The address to write the new KV to of each token.
+        max_context_len: The maximum context length.
+        context_lens: the length of attention context for each sequence.
+        block_tables: The block tables. (Seq id -> list of physical block)
+    """
 
     def __init__(
         self,
