ab9085e553064dc346d72ba62337c462afbeed76
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2023-11-29 20:02:30 +0000

    Fix Yi

--- vllm/model_executor/models/yi.py
+++ vllm/model_executor/models/yi.py
@@ -134,11 +134,10 @@ class YiAttention(nn.Module):
             base=self.rope_theta,
             rope_scaling=rope_scaling,
         )
-        self.attn = PagedAttention(
-            self.num_heads,
-            self.head_dim,
-            self.scaling
-            num_kv_heads=self.num_kv_heads)
+        self.attn = PagedAttention(self.num_heads,
+                                   self.head_dim,
+                                   self.scaling,
+                                   num_kv_heads=self.num_kv_heads)
 
     def forward(
         self,
@@ -152,8 +151,8 @@ class YiAttention(nn.Module):
         q, k, v = qkv.split([self.q_size, self.kv_size, self.kv_size], dim=-1)
         q, k = self.rotary_emb(positions, q, k)
         k_cache, v_cache = kv_cache
-        attn_output = self.attn(q, k, v, k_cache, v_cache,
-                                input_metadata, cache_event)
+        attn_output = self.attn(q, k, v, k_cache, v_cache, input_metadata,
+                                cache_event)
         output, _ = self.o_proj(attn_output)
         return output
 
