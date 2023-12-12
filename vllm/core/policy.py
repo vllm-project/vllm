b2db32b76bcd0e75fbf909b2f02be2f508adb0d6
xxw1995 <385952345@qq.com>
2023-12-12 21:24:06 +0800

    add policy

--- vllm/core/policy.py
+++ vllm/core/policy.py
@@ -34,6 +34,64 @@ class FCFS(Policy):
         return now - seq_group.arrival_time
 
 
+class RoundRobin(Policy):
+    def __init__(self, time_slice: float):
+        self.time_slice = time_slice
+        self.current_time = 0
+        self.last_exec_time = {}
+
+    def get_priority(
+            self,
+            now: float,
+            seq_group: SequenceGroup,
+    ) -> float:
+        self.current_time = now
+
+        if seq_group not in self.last_exec_time:
+            self.last_exec_time[seq_group] = now
+
+        time_since_last_exec = now - self.last_exec_time[seq_group]
+
+        # If the time slice has been exhausted or if the job is new, then update its last execution time to the current time and set its priority to high (returning a smaller value).
+        if time_since_last_exec >= self.time_slice or seq_group.is_new:
+            return seq_group.arrival_time
+
+        return now
+
+class PriorityQueuesScheduler(Policy):
+    def __init__(self):
+        super().__init__()
+        self.queues = {
+            'high': [],
+            'medium': [],
+            'low': [],
+        }
+
+    def add_job(self, seq_group: SequenceGroup, priority: str):
+        self.queues[priority].append(seq_group)
+
+    def get_priority(
+            self,
+            now: float,
+            seq_group: SequenceGroup,
+    ) -> float:
+        # In this strategy, the arrival_time can be combined with priority to determine the return value.
+        # A simple method is to represent different priorities with fixed numerical values and then add the arrival time as the priority value.
+        # Assume ‘high’ = 1, ‘medium’ = 2, ‘low’ = 3.
+        priority_weights = {'high': 1, 'medium': 2, 'low': 3}
+        seq_priority = None
+        for priority, queue in self.queues.items():
+            if seq_group in queue:
+                seq_priority = priority
+                break
+
+        if seq_priority:
+            return priority_weights[seq_priority] + seq_group.arrival_time
+
+        #If a job is not in any queue, a method may be needed to handle it
+        return float('inf')
+
+
 class PolicyFactory:
 
     _POLICY_REGISTRY = {
