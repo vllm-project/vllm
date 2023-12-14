1b744e239b4bf8e986c59de3d4f8892c01a0de2e
Antoni Baum <antoni.baum@protonmail.com>
2023-12-13 21:54:32 -0800

    Fix top_k

--- vllm/model_executor/models/mixtral.py
+++ vllm/model_executor/models/mixtral.py
@@ -130,6 +130,7 @@ class MixtralMoE(nn.Module):
         self.rank = get_tensor_model_parallel_rank()
         self.tp_size = get_tensor_model_parallel_world_size()
         self.num_total_experts = config.num_local_experts
+        self.top_k = config.num_experts_per_tok
         if self.tp_size > self.num_total_experts:
             raise ValueError(
                 f"Tensor parallel size {self.tp_size} is greater than "
@@ -162,7 +163,7 @@ class MixtralMoE(nn.Module):
 
         routing_weights = F.softmax(router_logits, dim=1, dtype=torch.float)
         routing_weights, selected_experts = torch.topk(routing_weights,
-                                                       self.num_total_experts,
+                                                       self.top_k,
                                                        dim=-1)
         routing_weights /= routing_weights.sum(dim=-1, keepdim=True)
 
