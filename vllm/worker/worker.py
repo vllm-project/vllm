32c7cf3645aebf7190d38039fb7417cdc3ca73cf
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2023-12-15 03:19:49 +0000

    Remove __del__

--- vllm/worker/worker.py
+++ vllm/worker/worker.py
@@ -155,15 +155,6 @@ class Worker:
                                                  self.gpu_cache)
         return output
 
-    def __del__(self):
-        # Clean up the distributed environment.
-        # This is needed to avoid hanging on exit, since the cupy states are
-        # not cleaned up properly.
-        # NOTE(woosuk): The __del__ method is called by the Python garbage
-        # collector, so the timing of the call is not deterministic.
-        if model_parallel_is_initialized():
-            destroy_model_parallel()
-
 
 def _init_distributed_environment(
     parallel_config: ParallelConfig,
