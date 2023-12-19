648c676a523c88c68bc6f85ae9ee60e8c6fc22bc
Andrew Lapp <andrew@rew.la>
2023-12-19 15:51:00 -0600

    commit grammar work before I delete it so its in my history

--- vllm/grammar.py
+++ vllm/grammar.py
@@ -1,4 +1,10 @@
 import collections
+from parsimonious.grammar import Grammar
+
+import parsimonious
+
+from lark import Lark, Transformer, v_args
+from parsley import makeGrammar
 
 
 class TokenIndex:
@@ -129,6 +135,7 @@ class EpsilonNFA:
 
         return visited
 
+
 class TokenConstraintLogitProcessor:
     def __init__(self, tokenizer, nfa):
         self.tokenizer = tokenizer
@@ -205,10 +212,197 @@ class TokenConstraintLogitProcessor:
         return legal_tokens
 
 
+class LarkPDA:
+    """
+    Traverses a Lark Parser's PushDown Automata character by character
+    """
+    def __init__(self, lark_grammar: str, start: str = "value"):
+        self.parser = Lark(
+            lark_grammar,
+            start=start,
+            lexer="basic"
+        )
+
+    def step_seq(self, seq):
+        for char in seq:
+            self.step(char)
+
+    def step(self, char):
+        """
+        Updates the canonical state
+        """
+        next_states = self.next_char_state_map()[char]
+        if not next_states:
+            raise ValueError(f"Illegal transition from '{self.current_str}', no next state for '{char}'")
+        self.current_states = next_states
+        self.current_str += char
+
+    def simulate_step(self, chars, state_set=None):
+        """
+        Return map of chars and their resulting next state-set given a current state-set and new chars
+        """
+        if state_set is None:
+            state_set = self.current_states
+        state_map = self.next_char_state_map(state_set)
+        return {tok: state_map[tok] for tok in chars if tok in state_map}
+
+    def next_char_state_map(self, current_states=None):
+        """
+        Creates a mapping of possible next chars to a set of valid states for each char
+        """
+        if current_states is None:
+            current_states = self.current_states
+
+        char_to_states = collections.defaultdict(set)
+
+        if bool(current_states & set(self.nfa["final_states"])):
+            char_to_states[None] = None
+
+        for state in self._resolve_epsilon_closure(current_states):
+            for char, next_states in self.nfa["transition_function"][state].items():
+                if next_states and char != "$":
+                    char_to_states[char].update(next_states)
+
+        return char_to_states
+
+
+def lark_to_pushdown_automata_spec(lark_grammar: str, start: str = "value") -> dict:
+    parser = Lark(lark_grammar, start=start, lexer="basic")
+    import pdb;pdb.set_trace()
+
+
+def handle_regex(pattern):
+    """
+    Handle a regex pattern and convert it into a state representation for PDA.
+
+    Args:
+    pattern (str): The pattern of the regular expression.
+
+    Returns:
+    str: A unique representation of the regex for the PDA.
+    """
+    # Convert the regex pattern into a suitable representation for the PDA.
+    # This is a placeholder for the actual conversion logic.
+    return f"REGEX({pattern})"
+
+
+from parsimonious.nodes import NodeVisitor
+from parsimonious.expressions import Literal, Sequence, Regex
+
+def convert_grammar_to_pda(grammar):
+    pda = {
+        'states': set(),
+        'initial_state': 'q0',
+        'final_states': {'q_accept'},
+        'stack_alphabet': set(),
+        'transition_function': collections.defaultdict(lambda: collections.defaultdict(list))
+    }
+
+    def process_expression(state, expr, is_terminal=False):
+        if isinstance(expr, Literal):
+            pda['stack_alphabet'].add(expr.literal)
+            next_state = 'q_accept' if is_terminal else 'q' + str(len(pda['states']) + 1)
+            pda['transition_function'][state][('epsilon', expr.literal)].append((next_state, []))
+            if not is_terminal:
+                pda['states'].add(next_state)
+        elif isinstance(expr, Sequence):
+            current_state = state
+            for i, member in enumerate(expr.members):
+                is_last_member = i == len(expr.members) - 1
+                process_expression(current_state, member, is_terminal=is_last_member)
+                if not is_last_member:
+                    current_state = 'q' + str(len(pda['states']))
+        elif isinstance(expr, Regex):
+            raise NotImplementedError("Regex handling not implemented")
+
+    for non_terminal, production in grammar.items():
+        pda['states'].add(non_terminal)
+        pda['stack_alphabet'].add(non_terminal)
+        process_expression(non_terminal, production)
+
+    return dict(pda)
+
+
+# Test Case
+def test_convert_simple_grammar_to_pda():
+    grammar = Grammar("""
+    expression = term "+" term
+    term       = "number"
+    """)
+
+    expected_pda = {
+        'states': {'expression', 'term', 'q0', 'q_accept'},
+        'initial_state': 'q0',
+        'final_states': {'q_accept'},
+        'stack_alphabet': {'expression', 'term', '+', 'number'},
+        'transition_function': {
+            'q0': {('epsilon', 'epsilon'): [('expression', ['term', '+', 'term'])]},
+            'expression': {('epsilon', 'term'): [('term', [])]},
+            'term': {('epsilon', 'number'): [('q_accept', [])]}
+        }
+    }
+
+    actual_pda = convert_grammar_to_pda(grammar)
+    import pprint
+    pprint.pprint(actual_pda)
+    assert actual_pda == expected_pda, "PDA configuration does not match expected output"
+
+
 if __name__ == "__main__":
-    from automata_toolkit import regex_to_nfa
     import transformers
     import numpy as np
+
+    test_convert_simple_grammar_to_pda()
+    import pdb;pdb.set_trace()
+
+
+    grammar = r"""
+    expr        = (entry / emptyline)*
+    entry       = section pair*
+
+    section     = lpar word rpar ws
+    pair        = key equal value ws?
+
+    key         = word+
+    value       = (word / quoted)+
+    word        = ~r"[-\w]+"
+    quoted      = ~'"[^\"]+"'
+    equal       = ws? "=" ws?
+    lpar        = "["
+    rpar        = "]"
+    ws          = ~"\s*"
+    emptyline   = ws+
+    """
+
+    pda_config = convert_grammar_to_pda(Grammar(grammar))
+    import pdb;pdb.set_trace()
+
+    try_parsley()
+
+    json_grammar = r"""
+        ?start: value
+
+        ?value: object
+              | array
+              | string
+              | SIGNED_NUMBER      -> number
+              | "true"             -> true
+              | "false"            -> false
+              | "null"             -> null
+
+        array  : "[" [value ("," value)*] "]"
+        object : "{" [pair ("," pair)*] "}"
+        pair   : string ":" value
+
+        string : ESCAPED_STRING
+
+        %import common.ESCAPED_STRING
+        %import common.SIGNED_NUMBER
+        %import common.WS
+
+        %ignore WS
+    """
+    #lark_to_pushdown_automata_spec(json_grammar)
 
     tokenizer = transformers.AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
 
