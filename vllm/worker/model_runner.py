f967c54b721e1eea19cfdf4cda2b91f2bf916534
Hanzhi Zhou <hanzhi713@163.com>
2023-12-19 16:40:29 -0800

    Update vllm/worker/model_runner.py
    Co-authored-by: Woosuk Kwon <woosuk.kwon@berkeley.edu>

--- vllm/worker/model_runner.py
+++ vllm/worker/model_runner.py
@@ -559,6 +559,6 @@ def _get_graph_batch_size(batch_size: int) -> int:
         return (batch_size + 7) // 8 * 8
 
 
-def async_h2d(data: list, dtype, pin_memory):
+def _async_h2d(data: list, dtype, pin_memory):
     t = torch.tensor(data, dtype=dtype, pin_memory=pin_memory)
     return t.to(device="cuda", non_blocking=True)
