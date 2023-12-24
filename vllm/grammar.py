db143ba7d42151ddecda664bdb29a9e685b13cdd
Andrew Lapp <andrew@rew.la>
2023-12-24 09:54:04 -0600

    do lookup rather than recursive cache check to prevent recursion limit excess

--- vllm/grammar.py
+++ vllm/grammar.py
@@ -116,6 +116,42 @@ def memoize_by_instance(method):
     return wrapper
 
 
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+        self.value = None
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def __setitem__(self, key, value):
+        node = self.root
+        for char in key:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end_of_word = True
+        node.value = value
+
+    def get_best(self, word):
+        node = self.root
+        prefix = ""
+        best_value = node.value
+        for char in word:
+            if char in node.children:
+                prefix += char
+                node = node.children[char]
+                if node.is_end_of_word:
+                    best_value = node.value
+            else:
+                break  # break if char not in trie
+        remainder = word[len(prefix):]
+        assert best_value is not None
+        return prefix, best_value, remainder
+
+
 @dataclass
 class IncrementalParserState:
     """
@@ -136,6 +172,9 @@ class IncrementalParserState:
     prior_terminal_ids: tuple[str]
     partial_token: str
 
+    # orthogonal unique state key
+    full_seq: str
+
     # function of key
     interactive_parser: FastInteractiveParser
     terminal_candidates: list
@@ -144,6 +183,7 @@ class IncrementalParserState:
     _ignored_terms: set
     _seq_validator: dict
     _memo: dict
+    _full_seq_trie: Trie
 
 
     @classmethod
@@ -176,29 +216,47 @@ class IncrementalParserState:
         _seq_validator[("$END", "complete")] = lambda seq: seq is None
 
 
-        return cls(
+        parser = cls(
             interactive_parser=interactive_parser,
             prior_terminal_ids=tuple(),
+            full_seq="",
             partial_token="",
             terminal_candidates=None,
             _ignored_terms=set(lark_parser.lexer_conf.ignore),
             _seq_validator=_seq_validator,
-            _memo={}
+            _memo={},
+            _full_seq_trie=Trie()
         )
+        parser._full_seq_trie[""] = parser
+        return parser
 
-    def new(self, prior_terminal_ids, partial_token, **kwargs):
-        # cache
-        key = (prior_terminal_ids, partial_token)
-        if key in self._memo:
-            return self._memo[key]
+    def new(self, prior_terminal_ids, partial_token, full_seq, **kwargs):
+        # check cache
+        term_key = (prior_terminal_ids, partial_token)
+        if term_key in self._memo:
+            return self._memo[term_key]
+
+        # create
         instance_dict = {
             f.name: getattr(self, f.name)
             for f in fields(self)
         }
         instance_dict.update(kwargs)
         inst = self.__class__(**instance_dict)
-        self._memo[key] = inst
+
+        # update cache
+        self._memo[term_key] = inst
+        self._full_seq_trie[full_seq] = inst
         return inst
+
+    def __getitem__(self, full_seq):
+        """
+        Get the parser of a full sequence
+        """
+        match_seq, parser, remainder_seq = self._full_seq_trie.get_best(full_seq)
+        if remainder_seq:
+            parser = parser.step_seq(remainder_seq)
+        return parser
 
     @memoize_by_instance
     def step_seq(self, new_seq: str):
@@ -222,18 +280,21 @@ class IncrementalParserState:
                 new_interactive_parser = self.interactive_parser
             else:
                 new_interactive_parser = self.get_stepped_parser_state(terminal_name)
+
+            ml = complete_terminal["match_length"]
+            remainder_seq = new_seq[ml:]
+            processed_seq = new_seq[:ml]
+
             new_parser = self.new(
+                full_seq=self.full_seq + processed_seq,
                 interactive_parser=new_interactive_parser,
                 prior_terminal_ids=tuple(list(self.prior_terminal_ids) + [terminal_name]),
                 partial_token="",
                 terminal_candidates=None,
             )
 
-            leftover = len(new_seq) - complete_terminal["match_length"]
-            if leftover:
-                return new_parser.step_seq(
-                    new_maybe_partial_token[-leftover:]
-                )
+            if remainder_seq:
+                return new_parser.step_seq(remainder_seq)
             else:
                 return new_parser
 
@@ -243,6 +304,7 @@ class IncrementalParserState:
         )
         if partial_terminal_ids:
             return self.new(
+                full_seq=self.full_seq + new_seq,
                 prior_terminal_ids=self.prior_terminal_ids,
                 partial_token=new_maybe_partial_token,
                 terminal_candidates=partial_terminal_ids,
@@ -350,7 +412,7 @@ class NextTokenValidator:
         """
         Generate valid token strings given the full sequence
         """
-        parser = self.root_parser.step_seq(full_seq)
+        parser = self.root_parser[full_seq]
         for tok_str in self.vocab:
             if parser.is_valid_next_seq(tok_str):
                 yield tok_str
@@ -372,12 +434,12 @@ class GrammarLogitsProcessor(NextTokenValidator):
                  logits: torch.Tensor) -> torch.Tensor:
         # get valid token IDs given prior tokens
         sequence = self.tokenizer.decode(token_ids)
-        valid_token_ids = self.get_valid_next_token_id_set(sequence)
+        valid_token_ids = self.get_valid_next_token_ids(sequence)
+        valid = torch.tensor(list(valid_token_ids), dtype=torch.long)
 
         # modify logits given valid token IDs
         N = len(logits)
         mask = torch.zeros(N, dtype=torch.bool)
-        valid = torch.tensor(valid_token_ids, dtype=torch.long)
         mask[valid] = True
         logits[~mask] = float('-inf')
         return logits
