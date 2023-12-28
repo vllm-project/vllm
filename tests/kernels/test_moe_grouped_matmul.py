90e223ac1b883912c151bf16a7646326b6d1ee64
chenshen <scv119@gmail.com>
2023-12-27 18:48:57 -0800

    update

--- tests/kernels/test_moe_grouped_matmul.py
+++ tests/kernels/test_moe_grouped_matmul.py
@@ -5,21 +5,25 @@ import torch
 
 from vllm.model_executor.layers.moe import grouped_matmul
 
+
 def ref_grouped_matmul(
     fused_input: torch.Tensor,
     cum_group_range: torch.Tensor,
     fused_group_b: torch.Tensor,
-    activation: str="",
+    activation: str = "",
 ) -> torch.Tensor:
     num_groups = cum_group_range.shape[0] - 1
-    output = torch.zeros(fused_input.shape[0], fused_group_b.shape[2], device=fused_input.device, dtype=fused_input.dtype)
+    output = torch.zeros(fused_input.shape[0],
+                         fused_group_b.shape[2],
+                         device=fused_input.device,
+                         dtype=fused_input.dtype)
     for i in range(num_groups):
         group_i = fused_input[cum_group_range[i]:cum_group_range[i + 1]]
         group_i_b = fused_group_b[i]
         output[cum_group_range[i]:cum_group_range[i + 1]] = group_i @ group_i_b
 
     if activation == "silu":
-        output = torch.nn.functional.silu(output) 
+        output = torch.nn.functional.silu(output)
     return output
 
 
@@ -28,7 +32,8 @@ def ref_grouped_matmul(
 @pytest.mark.parametrize("n", [128, 1024, 2000])
 @pytest.mark.parametrize("k", [128, 1024, 2000])
 @pytest.mark.parametrize("activation", ["", "silu"])
-@pytest.mark.parametrize("dtype", [torch.float16, torch.float32, torch.bfloat16])
+@pytest.mark.parametrize("dtype",
+                         [torch.float16, torch.float32, torch.bfloat16])
 def test_moe_grouped_matmul(
     group_size: int,
     m: int,
@@ -40,11 +45,15 @@ def test_moe_grouped_matmul(
     groups = [random.randint(1, m) for _ in range(group_size)]
     batch_size = sum(groups)
     fused_input = torch.randn(batch_size, k, dtype=dtype, device="cuda")
-    cum_group_range = torch.tensor([0] + list(itertools.accumulate(groups)), dtype=torch.int32, device="cuda")
+    cum_group_range = torch.tensor([0] + list(itertools.accumulate(groups)),
+                                   dtype=torch.int32,
+                                   device="cuda")
     fused_group_b = torch.randn(group_size, k, n, dtype=dtype, device="cuda")
 
-    ref_output = ref_grouped_matmul(fused_input, cum_group_range, fused_group_b, activation)
-    output = grouped_matmul(fused_input, cum_group_range, fused_group_b, activation)
+    ref_output = ref_grouped_matmul(fused_input, cum_group_range,
+                                    fused_group_b, activation)
+    output = grouped_matmul(fused_input, cum_group_range, fused_group_b,
+                            activation)
     diff = torch.abs(ref_output - output)
     mean = torch.mean(diff)
     max = torch.max(diff)
