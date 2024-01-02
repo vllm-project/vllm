1c579dcaf975e5a1ee76688a7a32725e26e73ac4
JasonZhu1313 <jasonchu13@outlook.com>
2024-01-02 14:13:48 -0800

    update the code in squeezellm to be compliant with gptq 3-bit change

--- vllm/model_executor/layers/quantization/squeezellm.py
+++ vllm/model_executor/layers/quantization/squeezellm.py
@@ -9,6 +9,8 @@ from vllm.model_executor.layers.linear import (LinearMethodBase,
 from vllm.model_executor.layers.quantization.base_config import QuantizationConfig
 from vllm.utils import is_hip
 
+STORAGE_BITS_SIZE = 32
+
 
 class SqueezeLLMConfig(QuantizationConfig):
     """Config class for SqueezeLLM.
@@ -27,7 +29,7 @@ class SqueezeLLMConfig(QuantizationConfig):
                 "Currently, only 4-bit weight quantization is supported for "
                 f"SqueezeLLM, but got {self.weight_bits} bits.")
 
-        self.pack_factor = 32 // self.weight_bits
+        self.pack_factor = STORAGE_BITS_SIZE // self.weight_bits
 
     def __repr__(self) -> str:
         return f"SqueezeLLMConfig(weight_bits={self.weight_bits})"
@@ -71,14 +73,15 @@ class SqueezeLLMLinearMethod(LinearMethodBase):
                        output_size_per_partition: int, input_size: int,
                        output_size: int,
                        params_dtype: torch.dtype) -> Dict[str, Any]:
-        if input_size_per_partition % self.quant_config.pack_factor != 0:
+        if input_size_per_partition % STORAGE_BITS_SIZE != 0:
             raise ValueError(
                 "The input size is not aligned with the quantized "
                 "weight shape. This can be caused by too large "
                 "tensor parallel size.")
         qweight = Parameter(
             torch.empty(
-                input_size_per_partition // self.quant_config.pack_factor,
+                input_size_per_partition // STORAGE_BITS_SIZE *
+                self.quant_config.weight_bits,
                 output_size_per_partition,
                 device="cuda",
                 dtype=torch.int32,
@@ -90,7 +93,8 @@ class SqueezeLLMLinearMethod(LinearMethodBase):
                 "input_dim": 0,
                 "output_dim": 1,
                 "packed_dim": 0,
-                "pack_factor": self.quant_config.pack_factor,
+                "weight_bits": self.quant_config.weight_bits,
+                "storage_bits_size": STORAGE_BITS_SIZE,
             })
         lookup_table = Parameter(
             torch.empty(
