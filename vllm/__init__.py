67d05df10544134d07489d54b2731730c1f0eded
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2024-01-03 17:35:56 -0800

    Bump up to v0.2.7 (#2337)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/__init__.py
+++ vllm/__init__.py
@@ -8,7 +8,7 @@ from vllm.entrypoints.llm import LLM
 from vllm.outputs import CompletionOutput, RequestOutput
 from vllm.sampling_params import SamplingParams
 
-__version__ = "0.2.6"
+__version__ = "0.2.7"
 
 __all__ = [
     "LLM",
