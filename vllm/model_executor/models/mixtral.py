0fe75f31865cff5579d06491985468ab9aea4a5e
chen shen <scv119@gmail.com>
2024-01-03 23:37:26 -0800

    update

--- vllm/model_executor/models/mixtral.py
+++ vllm/model_executor/models/mixtral.py
@@ -146,11 +146,10 @@ class MixtralDecoderLayer(nn.Module):
             rope_theta=rope_theta,
             sliding_window=config.sliding_window,
             linear_method=linear_method)
-        self.block_sparse_moe = MoE(
-            num_experts=config.num_local_experts,
-            top_k=config.num_experts_per_tok,
-            hidden_size=config.hidden_size,
-            intermediate_size=config.intermediate_size)
+        self.block_sparse_moe = MoE(num_experts=config.num_local_experts,
+                                    top_k=config.num_experts_per_tok,
+                                    hidden_size=config.hidden_size,
+                                    intermediate_size=config.intermediate_size)
         self.input_layernorm = RMSNorm(config.hidden_size,
                                        eps=config.rms_norm_eps)
         self.post_attention_layernorm = RMSNorm(config.hidden_size,
