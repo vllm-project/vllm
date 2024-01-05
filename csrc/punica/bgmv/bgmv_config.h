891070257c145b506a20666a3cb70afcf674d4ca
Antoni Baum <antoni.baum@protonmail.com>
2024-01-04 16:34:11 -0800

    Add 5632 to kernel sizes

--- csrc/punica/bgmv/bgmv_config.h
+++ csrc/punica/bgmv/bgmv_config.h
@@ -27,6 +27,7 @@ void bgmv_kernel(out_T *__restrict__ Y, const in_T *__restrict__ X,
     f(in_T, out_T, W_T, narrow, 4096) \
     f(in_T, out_T, W_T, narrow, 5120) \
     f(in_T, out_T, W_T, narrow, 5504) \
+    f(in_T, out_T, W_T, narrow, 5632) \
     f(in_T, out_T, W_T, narrow, 6912) \
     f(in_T, out_T, W_T, narrow, 7168) \
     f(in_T, out_T, W_T, narrow, 8192) \
