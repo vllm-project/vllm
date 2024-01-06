a42c3ca107f8cc95f0b58158a21a9eb9f388ebf9
rsnm2 <rshaw@neuralmagic.com>
2024-01-06 16:48:40 +0000

    stash to move to other machine

--- vllm/engine/metrics.py
+++ vllm/engine/metrics.py
@@ -1,3 +1,5 @@
+import time
+import numpy as np
 from aioprometheus import Counter, Gauge, Histogram
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
@@ -8,66 +10,6 @@ labels = {}
 
 def add_global_metrics_labels(**kwargs):
     labels.update(kwargs)
-
-
-# Stats defintions.
-# These are the interface between the LLMEngine and the Logger/Metrics.
-@dataclass
-class Stats:
-    # System stats representing snapshot of state.
-    total_gpu_blocks: int
-    total_cpu_blocks: int
-    free_gpu_blocks: int
-    free_cpu_blocks: int
-    num_running: int
-    num_waiting: int
-    num_swapped: int
-
-    # Raw stats from most recent model iteration.
-    prompt_run: bool
-    num_batched_tokens: int
-    iter_timings: List[float]
-    e2e_timings: List[float]
-
-
-class PrometheusMetric(ABC):
-    """Log Stats to a Prometheus Metric."""
-
-    @abstractmethod
-    def log(self, stats: Stats, labels: Dict[str, str]) -> None:
-        raise NotImplementedError
-
-
-@dataclass
-class CounterMetric(PrometheusMetric):
-    """Compute and log Counter"""
-    counter: Counter
-    fn: Callable[[Stats], Union[int, float]]
-
-    def log(self, stats: Stats, labels: Dict[str, str]) -> None:
-        self.counter.add(labels, self.fn(stats))
-
-
-@dataclass
-class GaugeMetric(PrometheusMetric):
-    """Compute and log Gauge"""
-    gauge: Gauge
-    fn: Callable[[Stats], Union[int, float]]
-
-    def log(self, stats: Stats, labels: Dict[str, str]) -> None:
-        self.gauge.set(labels, self.fn(stats))
-
-
-@dataclass
-class HistogramMetric(PrometheusMetric):
-    """Compute and log Histogram"""
-    histogram: Histogram
-    fn: Callable[[Stats], Union[List[int], List[float]]]
-
-    def log(self, stats: Stats, labels: Dict[str, str]) -> None:
-        for metric in self.fn(stats):
-            self.histogram.observe(labels, metric)
-
 
 # The begin-* and end* here are used by the documentation generator
 # to extract the metrics definitions.
@@ -97,9 +39,9 @@ histogram_time_to_first_token = Histogram(
     "vllm_time_to_first_token_seconds",
     "Histogram of time to first token in seconds.",
     buckets=[0.05, 0.1, 0.25, 0.5, 0.75, 1.0, 2.5, 5.0, 7.5, 10.0])
-histogram_inter_token_latency = Histogram(
-    "vllm_inter_token_latency_seconds",
-    "Histogram of inter token latency in seconds.",
+histogram_time_per_output_tokens = Histogram(
+    "vllm_time_per_output_tokens_seconds",
+    "Histogram of time per output token in seconds.",
     buckets=[0.01, 0.02, 0.04, 0.06, 0.08, 0.1, 0.25, 0.5, 1.0, 5.0])
 histogram_e2e_request_latency = Histogram(
     "vllm_e2e_request_latency_seconds",
@@ -108,46 +50,139 @@ histogram_e2e_request_latency = Histogram(
 
 # end-metrics-definitions
 
+# Stats defintions.
+# These are the interface between the LLMEngine and the Logger/Metrics.
+@dataclass
+class Stats:
+    # System stats representing snapshot of state.
+    num_running: int
+    num_waiting: int
+    num_swapped: int
+    gpu_cache_usage: float
+    cpu_cache_usage: float
+    # Raw stats from most recent model iteration.
+    num_prompt_tokens: int
+    num_generation_tokens: int
+    time_to_first_tokens: List[float]
+    time_per_output_tokens: List[float]
+    time_e2e_requests: List[float]
+
+class TrackedStats:
+    self.stats: Union[List[int], List[float]] = []
+    self.times: List[float] = []
+
+    def append(self, new_stat: Union[int, float], now: float) -> None:
+        self.stats.append(new_item)
+        self.times.append(now)
+
+    def reset(self) -> None:
+        self.stats = []
+        self.times = []
+
+    def get_throughput(self) -> float:
+        if self.stats.len() > 1:
+            return np.sum(self.stats) / self.times[-1]
+        return 0.0
+        
+class PrometheusMetric(ABC):
+    """Log Stats to a Prometheus Metric and Tracks Metrics For Local Logging"""
+    def __init__(self, stats_attr: str, template: Template) -> None:
+        # Attribute to extract correct data from Stats in self.log.
+        self.stats_attr = stats_attr
+        # String template with $metric for local logging.
+        self.template = template
+
+    @abstractmethod
+    # Extract data from stats, log to prometheus, and track data.
+    def log(self, stats: Stats, labels: Dict[str, str], now: float) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    # Compute metric from tracked data log.
+    def compute_metric(self) -> str:
+        raise NotImplementedError
+
+class CounterMetric(PrometheusMetric):
+    """Implementation of PrometheusMetric for Counters"""
+    def __init__(self, counter: Counter, stats_attr: str, template: Template) -> None:
+        self.counter = counter
+        self.tracked_stats = TrackedStats()
+        super().__init__(stats_attr, template)
+
+    def log(self, stats: Stats, labels: Dict[str, str], now: float) -> None:
+        stat = getattr(stats, self.stats_attr, 0)
+        self.counter.add(labels, stat)
+        self.tracked_stats.append(stat, now)
+
+    def compute_metric(self):
+        metric = self.tracked_data.get_throughput()
+        self.tracked_stats.reset()
+        return self.template.safe_substitute(metric=metric)
+
+class GaugeMetric(PrometheusMetric):
+    """Implementation of PrometheusMetric for Gauges"""
+    def __init__(self, gauge: Gauge, stats_attr: str, template: Template) -> None:
+        self.gauge = gauge
+        self.metric = 0
+        super().__init__(stats_attr, template)
+
+    def log(self, stats: Stats, labels: Dict[str, str], now: float) -> None:
+        stat = getattr(stats, self.stats_attr, 0)
+        self.gauge.set(labels, stat)
+        self.metric = stat
+
+    def compute_metric(self):
+        return self.template.safe_substitute(metric=self.metric)
+
+class HistogramMetric(PrometheusMetric):
+    """Implementation of PrometheusMetric for Histograms"""
+    def __init__(self, histogram: Histogram, stats_attr: str, template: Template) -> None:
+        self.histogram = histogram
+        self.tracked_stats = TrackedStats()
+        super().__init__(stats_attr, template)
+
+    def log(self, stats: Stats, labels: Dict[str, str], now: float) -> None:
+        stat = getattr(stats, self.stats_attr, 0)
+        self.gauge.set(labels, stat)
+        self.last_stat = stat
+
+    def compute_metric(self):
+        metric = self.tracked_data.get_throughput()
+        return self.template.safe_substitute(metric=self.last_stat)
+
+
+
+    def log(self, stats: Stats, labels: Dict[str, str]) -> None:
+        self.counter.add(labels, getattr(stats, self.attr, 0))
+    def log(self, stats: Stats, labels: Dict[str, str]) -> None:
+        self.gauge.set(labels, getattr(stats, self.attr, 0))
+    def log(self, stats: Stats, labels: Dict[str, str]) -> None:
+        for metric in getattr(stats, self.attr, []):
+            self.histogram.observe(labels, metric)
+
 
 # Functions to extract Metric from Stats.
 def _cache_usage(num_total: int, num_free: int) -> float:
     return 1.0 - num_free / num_total if num_total > 0 else 0.
-
-
-prompt_tokens_fn = lambda stats: stats.num_batched_tokens if stats.prompt_run else 0
-generation_tokens_fn = lambda stats: stats.num_batched_tokens if not stats.prompt_run else 0
-scheduler_running_fn = lambda stats: stats.num_running
-scheduler_swapped_fn = lambda stats: stats.num_swapped
-scheduler_waiting_fn = lambda stats: stats.num_waiting
-gpu_cache_usage_fn = lambda stats: _cache_usage(stats.total_gpu_blocks, stats.
-                                                free_gpu_blocks)
-cpu_cache_usage_fn = lambda stats: _cache_usage(stats.total_cpu_blocks, stats.
-                                                free_cpu_blocks)
-time_to_first_token_fn = lambda stats: stats.iter_timings if stats.prompt_run else [
-]
-inter_token_latency_fn = lambda stats: stats.iter_timings if not stats.prompt_run else [
-]
-e2e_request_latency_fn = lambda stats: stats.e2e_timings
-
 
 class PrometheusLogger:
     """PrometheusLogger used by LLMEngine to log stats to Prom."""
 
     def __init__(self) -> None:
         self.metrics: List[PrometheusMetric] = [
-            GaugeMetric(gauge_scheduler_running, scheduler_running_fn),
-            GaugeMetric(gauge_scheduler_swapped, scheduler_swapped_fn),
-            GaugeMetric(gauge_scheduler_waiting, scheduler_waiting_fn),
-            GaugeMetric(gauge_gpu_cache_usage, gpu_cache_usage_fn),
-            GaugeMetric(gauge_cpu_cache_usage, cpu_cache_usage_fn),
-            CounterMetric(counter_prompt_tokens, prompt_tokens_fn),
-            CounterMetric(counter_generation_tokens, generation_tokens_fn),
+            GaugeMetric(gauge_scheduler_running, "num_running"),
+            GaugeMetric(gauge_scheduler_swapped, "num_swapped"),
+            GaugeMetric(gauge_scheduler_waiting, "num_waiting"),
+            GaugeMetric(gauge_gpu_cache_usage, "gpu_cache_usage"),
+            GaugeMetric(gauge_cpu_cache_usage, "cpu_cache_usage"),
+            CounterMetric(counter_prompt_tokens, "num_prompt_tokens"),
+            CounterMetric(counter_generation_tokens, "num_generation_tokens"),
             HistogramMetric(histogram_time_to_first_token,
-                            time_to_first_token_fn),
+                            "time_to_first_tokens"),
             HistogramMetric(histogram_inter_token_latency,
-                            inter_token_latency_fn),
+                            "time_per_output_tokens"),
             HistogramMetric(histogram_e2e_request_latency,
-                            e2e_request_latency_fn),
+                            "time_e2e_requests"),
         ]
 
     def log(self, stats: Stats) -> None:
