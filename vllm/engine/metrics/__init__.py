a90d4478159fc1b9bd16f2af9279e01dc8958f9b
rsnm2 <rshaw@neuralmagic.com>
2024-01-06 20:30:50 +0000

    readded files

---
+++ vllm/engine/metrics/__init__.py
@@ -0,0 +1,2 @@
+from vllm.engine.metrics.metrics import MetricsLogger, Stats, add_global_metrics_labels
+from vllm.engine.metrics.metrics_registry import METRICS_REGISTRY
---
+++ vllm/engine/metrics/metrics.py
@@ -0,0 +1,152 @@
+import time
+import logging
+import numpy as np
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+from typing import Union, List, Dict, Callable
+
+from aioprometheus import Counter, Gauge, Histogram
+
+labels = {}
+
+def add_global_metrics_labels(**kwargs):
+    labels.update(kwargs)
+
+@dataclass
+class Stats:
+    # System stats.
+    num_running: int
+    num_waiting: int
+    num_swapped: int
+    gpu_cache_usage: float
+    cpu_cache_usage: float
+
+    # Raw stats from last model iteration.
+    num_prompt_tokens: int
+    num_generation_tokens: int
+    time_to_first_tokens: List[float]
+    time_per_output_tokens: List[float]
+    time_e2e_requests: List[float]
+
+class TrackedStats:
+    """Data to compute metrics for logging to StdOut"""
+    def __init__(self):
+        self.stats: Union[List[int], List[float]] = []
+        self.last_reset_time: float = time.now()
+
+    def append(self, new_stat: Union[int, float]) -> None:
+        self.stats.append(new_item)
+
+    def reset(self, now: float) -> None:
+        self.stats = []
+        self.last_reset_time = now
+
+    def get_throughput(self, now: float) -> float:
+        return np.sum(self.stats) / (now - self.last_reset_time)
+
+    def get_mean(self) -> float:
+        return np.mean(self.stats) if len(self.stats) > 0 else 0
+    
+    def get_last(self) -> Union[int, float]:
+        return self.stats[-1] if len(self.stats) > 0 else 0
+        
+class PrometheusMetric(ABC):
+    """Log Stats to Prometheus and TrackStats"""
+    def __init__(self, attr: str, template: str, log_local: bool=True) -> None:
+        # Attribute to get correct attibute from Stats.
+        self.attr = attr
+        # String template with $metric for local logging.
+        self.template = template
+        # Tracked Stats for Logging Locally
+        self.tracked_stats = TrackedStats()
+        # Whether this metric should log locally.
+        self.log_local = log_local
+
+    @abstractmethod
+    # Log to Prometheus and local TrackedStats.
+    def log(self, stats: Stats, labels: Dict[str, str], now: float) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    # Compute metric from local TrackedStats.
+    def _compute_metric(self, now: float) -> Union[int, float]:
+        raise NotImplementedError
+    
+    # Compute metric and convert to string.
+    def get_str(self, now: float) -> str:
+        return self.template.format(self._compute_metric(now))
+
+    # Track stat for local logging.
+    def track_stat(self, stat: Union[int. float]) -> None:
+        if self.log_local:
+            self.tracked_stats.append(now)
+    
+    # Reset tracked stats for next local logging interval.
+    def reset_stats(self, now: float) -> None:
+        self.tracked_stats.reset(now)
+
+class CounterMetric(PrometheusMetric):
+    def __init__(self, counter: Counter, *args, **kwargs) -> None:
+        self.counter = counter
+        super().__init__(*args, **kwargs)
+
+    def log(self, stats: Stats, labels: Dict[str, str], now: float) -> None:
+        stat = getattr(stats, self.stats_attr, 0)
+        self.counter.add(labels, stat)
+        self.track_stat(stat)
+
+    def _compute_metric(self, now: float) -> Union[int, float]:
+        return self.tracked_data.get_throughput(now)
+
+class GaugeMetric(PrometheusMetric):
+    def __init__(self, gauge: Gauge, *args, **kwargs) -> None:
+        self.gauge = gauge
+        super().__init__(*args, **kwargs)
+
+    def log(self, stats: Stats, labels: Dict[str, str], now: float) -> None:
+        stat = getattr(stats, self.stats_attr, 0)
+        self.gauge.set(labels, stat)
+        self.track_stat(stat)
+
+    def _compute_metric(self, now: float) -> Union[int, float]:
+        return self.tracked_stats.get_last()
+
+class HistogramMetric(PrometheusMetric):
+    def __init__(self, histogram: Histogram, *args, **kwargs) -> None:
+        self.histogram = histogram
+        super().__init__(*args, **kwargs)
+
+    def log(self, stats: Stats, labels: Dict[str, str], now: float) -> None:
+        for stat in getattr(stats, self.attr, []):
+            self.histogram.observe(labels, stat)
+            self.track_stat(stat)
+
+    def _compute_metric(self, now: float) -> Union[int, float]:
+        return self.tracked_data.get_mean()
+        
+class MetricsLogger:
+    """Used LLMEngine to log metrics to Promethus and Stdout."""
+    def __init__(self, metrics: List[PrometheusMetric], local_logger: logging.Logger, local_interval: float) -> None:
+        self.metrics = metrics
+        self.local_logger = local_logger
+        self.last_local_log = time.now()
+        self.local_interval = local_interval
+    
+    def _interval_elasped(self, now: float):
+        return now - self.last_local_log > self.local_interval
+
+    def log(self, now: float, stats: Stats) -> None:
+        # Log locally if local_interval sec elapsed. 
+        log_local = self._interval_elasped(now)
+        
+        for metric in self.metrics:
+            # Log to Prometheus.
+            metric.log(labels=labels, stats=stats, now=now)
+
+            # Log to StdOut and reset local tracked stats.
+            if log_local and metric.log_local:
+                self.local_logger.info(metric.get_str(now=now))
+                metric.reset_stats()
+        
+        if log_local:
+            self.last_local_log = now
