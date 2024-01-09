99617a35124f32224009f9374916fe96b2c1ab61
simon-mo <simon.mo@hey.com>
2024-01-09 19:19:46 +0000

    fix workspace

--- tests/async_engine/test_api_server.py
+++ tests/async_engine/test_api_server.py
@@ -30,7 +30,7 @@ def api_server():
         "api_server_async_engine.py").absolute()
     uvicorn_process = subprocess.Popen([
         sys.executable, "-u",
-        str(script_path), "--model", "facebook/opt-125m"
+        str(script_path), "--model", "facebook/opt-125m", "--host", "127.0.0.1",
     ])
     yield
     uvicorn_process.terminate()
