853180f8bc5e335b07f0ef7be8079b3e1b7fe0d3
Cade Daniel <edacih@gmail.com>
2024-01-09 13:47:34 -0800

    Removing test bash script

--- .github/workflows/scripts/test_scripts/run_spec_decode_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Run speculative decoding tests.
-# Requires aws.g5.2xlarge or larger.
-
-set -eou pipefail
-
-timeout $(( 5 * 60 )) pytest -vvs tests/samplers/test_rejection_sampler.py
