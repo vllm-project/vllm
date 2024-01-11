7c766777f614f0b6c49d24595ad6fa80bb73c554
simon-mo <simon.mo@hey.com>
2024-01-11 19:45:48 +0000

    try fix tests

--- .buildkite/run-benchmarks.sh
+++ .buildkite/run-benchmarks.sh
@@ -9,13 +9,11 @@ python3 benchmarks/benchmark_latency.py 2>&1 | tee benchmark_latency.txt
 python3 benchmarks/benchmark_throughput.py --input-len 256 --output-len 256 2>&1 | tee benchmark_throughput.txt
 
 # write the results into a markdown file
-cat << EOF > benchmark_results.md
-# Latency
-${cat benchmark_latency.txt}
-
-# Throughput
-${cat benchmark_throughput.txt}
-EOF
+cat "# Benchmark" >> benchmark_results.md
+cat "## Latency" >> benchmark_results.md
+cat benchmark_latency.txt >> benchmark_results.md
+cat "## Throughput" >> benchmark_results.md
+cat benchmark_throughput.txt >> benchmark_results.md
 
 # upload the results to buildkite
 /workspace/buildkite-agent annotate --style "info" --context "benchmark-results" < benchmark_results.md
--- tests/kernels/test_cache.py
+++ tests/kernels/test_cache.py
@@ -11,7 +11,7 @@ NUM_LAYERS = [1]  # Arbitrary values for testing
 NUM_HEADS = [8]  # Arbitrary values for testing
 HEAD_SIZES = [64, 80, 96, 112, 128, 256]
 BLOCK_SIZES = [8, 16, 32]
-NUM_BLOCKS = [1024, 24000]  # Arbitrary values for testing
+NUM_BLOCKS = [1024, 12000]  # Arbitrary values for testing
 NUM_MAPPINGS = [256]  # Arbitrary values for testing
 SEEDS = [0]
 DEVICES = [i for i in range(1 if torch.cuda.device_count() == 1 else 2)]
