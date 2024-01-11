a697e9b821b61825cc138694733054c7032d98eb
simon-mo <simon.mo@hey.com>
2024-01-11 21:24:12 +0000

    another round

--- .buildkite/run-benchmarks.sh
+++ .buildkite/run-benchmarks.sh
@@ -9,11 +9,14 @@ python3 benchmarks/benchmark_latency.py 2>&1 | tee benchmark_latency.txt
 python3 benchmarks/benchmark_throughput.py --input-len 256 --output-len 256 2>&1 | tee benchmark_throughput.txt
 
 # write the results into a markdown file
-echo "# Benchmark" >> benchmark_results.md
-echo "## Latency" >> benchmark_results.md
-sed -n '1p;$p' benchmark_latency.txt >> benchmark_results.md
-echo "## Throughput" >> benchmark_results.md
-sed -n '1p;$p' benchmark_throughput.txt >> benchmark_results.md
+echo "### Latency Benchmarks" >> benchmark_results.md
+sed -n '1p' benchmark_latency.txt >> benchmark_results.md
+echo "" >> benchmark_results.md
+sed -n '$p' benchmark_latency.txt >> benchmark_results.md
+echo "### Throughput Benchmarks" >> benchmark_results.md
+sed -n '1p' benchmark_throughput.txt >> benchmark_results.md
+echo "" >> benchmark_results.md
+sed -n '$p' benchmark_throughput.txt >> benchmark_results.md
 
 # upload the results to buildkite
 /workspace/buildkite-agent annotate --style "info" --context "benchmark-results" < benchmark_results.md
