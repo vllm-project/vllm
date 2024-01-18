075939b288fbd66157f6666374c315a62e6300f8
Jadiel de Armas <jadielam@gmail.com>
2024-01-18 09:15:49 -0500

    Added max_capacity to prefix pool, together with tests

---
+++ tests/prefix_caching/test_prefix_pool.py
@@ -0,0 +1,61 @@
+from vllm.prefix import PrefixPool
+
+import pytest
+
+@pytest.fixture
+def no_max_capacity_prefix_pool() -> PrefixPool:
+    return PrefixPool(block_size=32)
+
+
+def test_prefix_length_behaviours(no_max_capacity_prefix_pool: PrefixPool):
+    """
+    This test checks that prefixes of length less than pool.block_size are not created and are not added to the pool.
+    It also checks that prefixes of length equal to or greater to pool.block_size are created and added to the pool.
+    """
+    prefix_1 = no_max_capacity_prefix_pool.add_or_get_prefix(list(range(no_max_capacity_prefix_pool.block_size - 1)))
+    prefix_2 = no_max_capacity_prefix_pool.add_or_get_prefix(list(range(no_max_capacity_prefix_pool.block_size)))
+    prefix_3 = no_max_capacity_prefix_pool.add_or_get_prefix(list(range(no_max_capacity_prefix_pool.block_size * 2)))
+    assert prefix_1 is None
+    assert prefix_2 is not None
+    assert prefix_3 is not None
+    assert len(no_max_capacity_prefix_pool) == 2
+
+def test_same_prefix_added_twice(no_max_capacity_prefix_pool: PrefixPool):
+    """
+    Tests that when a prefix is added more than once to the pool, all subsequent additions
+    return the same prefix object that was created the first time.
+    """
+    prefix_1 = no_max_capacity_prefix_pool.add_or_get_prefix(list(range(no_max_capacity_prefix_pool.block_size)))
+    prefix_2 = no_max_capacity_prefix_pool.add_or_get_prefix(list(range(no_max_capacity_prefix_pool.block_size)))
+    assert prefix_1 is prefix_2
+    assert len(no_max_capacity_prefix_pool) == 1
+
+def test_prefix_pool_max_capacity():
+    max_capacity = 1
+    max_capacity_prefix_pool = PrefixPool(block_size=32, max_capacity=max_capacity)
+
+    # Tests that on the third insertion, new object is created because capacity limits reached,
+    # but that the newly created object is equal to the old object
+    prefix_1 = max_capacity_prefix_pool.add_or_get_prefix(list(range(max_capacity_prefix_pool.block_size)))
+    _ = max_capacity_prefix_pool.add_or_get_prefix(list(range(max_capacity_prefix_pool.block_size * 2)))
+    prefix_3 = max_capacity_prefix_pool.add_or_get_prefix(list(range(max_capacity_prefix_pool.block_size)))
+    assert prefix_1 is not prefix_3
+    assert prefix_1 == prefix_3
+
+    # Tests that the max capacity remains the same
+    for i in range(10):
+        _ = max_capacity_prefix_pool.add_or_get_prefix(list(range(max_capacity_prefix_pool.block_size + i)))
+        assert len(max_capacity_prefix_pool) == max_capacity
+
+
+def test_assertion_raised_with_invalid_max_capacity():
+    with pytest.raises(AssertionError):
+        _ = PrefixPool(32, max_capacity=-1)
+
+    with pytest.raises(AssertionError):
+        _ = PrefixPool(32, max_capacity=0)
+
+
+if __name__ == "__main__":
+    import pytest
+    pytest.main([__file__])
--- vllm/prefix.py
+++ vllm/prefix.py
@@ -1,4 +1,5 @@
-from typing import Dict, List, Sequence, Tuple, Optional
+from typing import Any, Dict, List, Sequence, Tuple, Optional
+from collections import OrderedDict
 
 from vllm.block import BlockTable
 
@@ -18,7 +19,7 @@ class Prefix:
     def __init__(
         self,
         token_ids: Sequence[int],
-        block_size: int,
+        block_size: int
     ) -> None:
         self.token_ids = tuple(token_ids)
         self.block_size = block_size
@@ -43,16 +44,20 @@ class Prefix:
 
     def __hash__(self) -> int:
         return self.hash
+    
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, Prefix):
+            return False
+        return self.hash == other.hash
 
     def set_block_table(self, block_table: BlockTable) -> None:
         self.block_table = block_table.copy()
 
 
 class PrefixPool:
-    """Manages all the prompt prefixes.
-
-    NOTE: This feature is experimental and may be replaced with automatic
-        prefix caching in the future.
+    """Manages all the prompt prefixes. If the max_capacity argument is not None,
+    the pool will act as a LRU cache and remove the least recently used prefix once
+    the capacity is reached.
 
     Args:
         block_size: The block size of the executed model.
@@ -60,28 +65,64 @@ class PrefixPool:
     Attributes:
         prefixes: A list of all the prefixes.
         block_size: The block size of the executed model.
+        max_capacity: The maximum number of prefixes allowed in the pool at any given time. 
+            The default value is None, which means there is no limit. It can only take positive
+            values.
     """
 
     def __init__(
         self,
         block_size: int,
+        max_capacity: Optional[int] = None
     ) -> None:
-        # TODO(zhuohan): Add a capacity limit to the prefix pool.
-        self.prefixes: Dict[int, Prefix] = {}
+        self.prefixes: OrderedDict[int, Prefix] = OrderedDict()
         self.block_size = block_size
+        
+        if max_capacity is not None:
+            # NOTE(to remove after consultation): I have also been thinking if we need to 
+            # assert that max_capacity must be greater than or equal to the max allowed 
+            # batch size. My own analysis has led me to believe that this is not necessary
+            # because even if a prefix is removed from the pool before it even gets computed,
+            # this will not stop the prefix from being computed. It only means that the prefix
+            # will not stay allocated for next cycles of computation and future requests
+            # with the prefix will have to recompute it.
+            assert max_capacity > 0, "max_capacity must be positive."
+        
+        self.max_capacity = max_capacity
 
+    def __len__(self) -> int:
+        return len(self.prefixes)
+    
     def _truncate_token_ids(self, token_ids: Sequence[int]) -> Tuple[int]:
         new_length = len(token_ids) // self.block_size * self.block_size
         return tuple(token_ids[:new_length])
 
-    def add_or_get_prefix(self, token_ids: Sequence[int],
-                          lora_int_id: int) -> Optional[Prefix]:
+    def add_or_get_prefix(self, token_ids: Sequence[int]) -> Optional[Prefix]:
+        """
+        Adds a prefix to the pool if it does not already exist. If it does exist,
+        it returns the existing prefix. If the pool is at max capacity, it removes
+        the least recently used prefix before adding the new prefix.
+
+        Notice that if the length of token_ids is less than the block_size, no 
+        prefix is created and None is returned.
+        """
         token_ids = self._truncate_token_ids(token_ids)
         if len(token_ids) == 0:
             # Prefix is empty.
             return None
+        
+        # Check first if prefix exists, moving it to the end of the OrderedDict.
+        # so that the LRU policy is maintained. Return the existing prefix.
         prefix = Prefix(token_ids, self.block_size)
-        prefix_hash = hash((prefix, lora_int_id))
-        if prefix_hash not in self.prefixes:
-            self.prefixes[prefix_hash] = prefix
-        return self.prefixes[prefix_hash]
+        prefix_hash = hash(prefix)
+        if prefix_hash in self.prefixes:
+            prefix = self.prefixes[prefix_hash]
+            self.prefixes.move_to_end(prefix_hash)
+            return prefix
+        
+        # Prefix does not exist. Add created prefix to the pool and return it.
+        # Always, before adding anything to the pool, checking the capacity constraints.
+        if len(self.prefixes) == self.max_capacity:
+            _ = self.prefixes.popitem(last=False)
+        self.prefixes[prefix_hash] = prefix
+        return prefix
