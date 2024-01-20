c52e7815815686c39b892517817b586cf138fec3
Nick Hill <nickhill@us.ibm.com>
2024-01-20 15:55:20 -0800

    [Fix] Keep `scheduler.running` as deque
    #2290 changed the scheduler seq group lists to be deques for more efficient updates, but missed one place where the `running` deque gets converted back to a list.

--- vllm/core/scheduler.py
+++ vllm/core/scheduler.py
@@ -332,10 +332,8 @@ class Scheduler:
         self.block_manager.free(seq)
 
     def free_finished_seq_groups(self) -> None:
-        self.running = [
-            seq_group for seq_group in self.running
-            if not seq_group.is_finished()
-        ]
+        self.running = deque(seq_group for seq_group in self.running
+                             if not seq_group.is_finished())
 
     def _allocate(self, seq_group: SequenceGroup) -> None:
         self.block_manager.allocate(seq_group)
