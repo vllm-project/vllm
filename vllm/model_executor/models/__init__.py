7639ab73ab7088ec498a20e03e6148cbc2f097bb
roy <jasonailu87@gmail.com>
2024-01-21 21:55:10 +0800

    init support

--- vllm/model_executor/models/__init__.py
+++ vllm/model_executor/models/__init__.py
@@ -24,6 +24,7 @@ _MODELS = {
     "GPTJForCausalLM": ("gpt_j", "GPTJForCausalLM"),
     "GPTNeoXForCausalLM": ("gpt_neox", "GPTNeoXForCausalLM"),
     "InternLMForCausalLM": ("internlm", "InternLMForCausalLM"),
+    "InternLM2ForCausalLM": ("internlm2", "InternLMForCausalLM"),
     "LlamaForCausalLM": ("llama", "LlamaForCausalLM"),
     # For decapoda-research/llama-*
     "LLaMAForCausalLM": ("llama", "LlamaForCausalLM"),
