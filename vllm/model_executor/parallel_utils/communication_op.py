618c96478aa05d577b26213a6732b51c0973787d
Ginda Chen <32371474+GindaChen@users.noreply.github.com>
2024-01-21 11:03:17 -0800

    Fix formatting err

--- vllm/model_executor/parallel_utils/communication_op.py
+++ vllm/model_executor/parallel_utils/communication_op.py
@@ -8,7 +8,8 @@ import torch
 from vllm.model_executor.parallel_utils.parallel_state import (
     get_tensor_model_parallel_rank,
     get_tensor_model_parallel_world_size,
-    get_tensor_model_parallel_group, get_tensor_model_parallel_src_rank,
+    get_tensor_model_parallel_group,
+    get_tensor_model_parallel_src_rank,
 )
 
 
