da6a1d734f5c0f44041be6d664b76281128d816f
Nikola Borisov <nikola.borisof@gmail.com>
2024-01-23 22:38:55 -0800

    [Bugfix] fix crash if max_tokens=None (#2570)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

---
+++ tests/test_sampling_params.py
@@ -0,0 +1,13 @@
+"""Tests for the SamplingParams class.
+"""
+from vllm import SamplingParams
+
+
+def test_max_tokens_none():
+    """max_tokens=None should be allowed"""
+    SamplingParams(temperature=0.01, top_p=0.1, max_tokens=None)
+
+
+if __name__ == "__main__":
+    import pytest
+    pytest.main([__file__])
