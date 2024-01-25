ecd4c8f26bbaeffe1d7c234c22510475d04b48e1
Simon Mo <simon.mo@hey.com>
2024-01-24 21:19:08 -0800

    Apply suggestions from code review

--- vllm/model_executor/models/qwen2.py
+++ vllm/model_executor/models/qwen2.py
@@ -21,7 +21,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Inference-only Qwen2 model compatible with HuggingFace weights."""
 from typing import List, Optional, Tuple
 
