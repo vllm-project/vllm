f4737a8023ac62cdd71d83424122f67b333fb674
dakotamahan-stability <139925645+dakotamahan-stability@users.noreply.github.com>
2024-01-26 14:45:19 -0600

    Support for Stable LM 2 (#2598)
    Co-authored-by: Zhuohan Li <zhuohan123@gmail.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/model_executor/models/stablelm.py
+++ vllm/model_executor/models/stablelm.py
@@ -98,7 +98,7 @@ class StablelmAttention(nn.Module):
         self.scaling = self.head_dim**-0.5
         self.q_size = self.num_heads * self.head_dim
         self.kv_size = self.num_key_value_heads * self.head_dim
-
+        self.qkv_bias = getattr(config, "use_qkv_bias", False)
         if (self.head_dim * self.num_heads * tp_size) != self.hidden_size:
             raise ValueError(
                 f"hidden_size must be divisible by num_heads (got `hidden_size`: {self.hidden_size}"
@@ -108,7 +108,7 @@ class StablelmAttention(nn.Module):
                                           self.head_dim,
                                           self.total_num_heads,
                                           self.total_num_key_value_heads,
-                                          bias=False,
+                                          self.qkv_bias,
                                           linear_method=linear_method)
         self.o_proj = RowParallelLinear(self.total_num_heads * self.head_dim,
                                         self.hidden_size,
