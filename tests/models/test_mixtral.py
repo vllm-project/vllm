84224804c041c38011c03a6b6712db3a78bf36e6
Philipp Moritz <pcmoritz@gmail.com>
2024-01-30 20:22:24 -0800

    update

--- tests/models/test_mixtral.py
+++ tests/models/test_mixtral.py
@@ -1,3 +1,10 @@
+"""Compare the outputs of HF and vLLM for Mistral models using greedy sampling.
+
+
+Run `pytest tests/models/test_mixtral.py`
+"""
+
+import pytest
 import torch
 from transformers import MixtralConfig
 from transformers.models.mixtral.modeling_mixtral import MixtralSparseMoeBlock
@@ -35,3 +42,36 @@ def test_mixtral_moe():
     vllm_states = vllm_moe.forward(inputs)
 
     assert torch.allclose(hf_states, vllm_states, rtol=1e-3, atol=1e-3)
+
+
+MODELS = [
+    "mistralai/Mixtral-8x7B-Instruct-v0.1",
+]
+
+
+@pytest.mark.parametrize("model", MODELS)
+@pytest.mark.parametrize("dtype", ["bfloat16"])
+@pytest.mark.parametrize("max_tokens", [128])
+def test_models(
+    hf_runner,
+    vllm_runner,
+    example_long_prompts,
+    model: str,
+    dtype: str,
+    max_tokens: int,
+) -> None:
+    hf_model = hf_runner(model, dtype=dtype)
+    hf_outputs = hf_model.generate_greedy(example_long_prompts, max_tokens)
+    del hf_model
+
+    vllm_model = vllm_runner(model, dtype=dtype)
+    vllm_outputs = vllm_model.generate_greedy(example_long_prompts, max_tokens)
+    del vllm_model
+
+    for i in range(len(example_long_prompts)):
+        hf_output_ids, hf_output_str = hf_outputs[i]
+        vllm_output_ids, vllm_output_str = vllm_outputs[i]
+        assert hf_output_str == vllm_output_str, (
+            f"Test{i}:\nHF: {hf_output_str!r}\nvLLM: {vllm_output_str!r}")
+        assert hf_output_ids == vllm_output_ids, (
+            f"Test{i}:\nHF: {hf_output_ids}\nvLLM: {vllm_output_ids}")
