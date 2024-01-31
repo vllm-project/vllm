5441b91daa7c99e805431036435bceb003acccf4
Philipp Moritz <pcmoritz@gmail.com>
2024-01-30 18:11:53 -0800

    update

--- tests/models/test_mixtral.py
+++ tests/models/test_mixtral.py
@@ -9,31 +9,33 @@ from vllm.model_executor.models.mixtral import MixtralMoE
 def test_mixtral_moe():
     "Make sure our Mixtral MoE implementation agrees with the one from huggingface."
 
-    # Instantiate our and huggingface's MoE blocks
-    config = MixtralConfig()
-    hf_moe = MixtralSparseMoeBlock(config).to("cuda")
-    vllm_moe = MixtralMoE(
-        num_experts=config.num_local_experts,
-        top_k=config.num_experts_per_tok,
-        hidden_size=config.hidden_size,
-        intermediate_size=config.intermediate_size,
-        tp_size=1,
-    )
+    with torch.inference_mode():
 
-    # Load the weights
-    vllm_moe.gate.linear_weights["weight"][:] = hf_moe.gate.weight.data
-    for i in range(config.num_local_experts):
-        vllm_moe.ws[i][:] = torch.cat((
-            hf_moe.experts[i].w1.weight.data,
-            hf_moe.experts[i].w3.weight.data,
-        ), dim=0).to("cuda")
-        vllm_moe.w2s[i][:] = hf_moe.experts[i].w2.weight.data
+        # Instantiate our and huggingface's MoE blocks
+        config = MixtralConfig()
+        hf_moe = MixtralSparseMoeBlock(config).to("cuda")
+        vllm_moe = MixtralMoE(
+            num_experts=config.num_local_experts,
+            top_k=config.num_experts_per_tok,
+            hidden_size=config.hidden_size,
+            intermediate_size=config.intermediate_size,
+            tp_size=1,
+        )
 
-    # Generate input batch of dimensions [batch_size, seq_len, hidden_dim]
-    inputs = torch.randn((1, 64, config.hidden_size)).to("cuda")
+        # Load the weights
+        vllm_moe.gate.linear_weights["weight"][:] = hf_moe.gate.weight.data
+        for i in range(config.num_local_experts):
+            vllm_moe.ws[i][:] = torch.cat((
+                hf_moe.experts[i].w1.weight.data,
+                hf_moe.experts[i].w3.weight.data,
+            ), dim=0).to("cuda")
+            vllm_moe.w2s[i][:] = hf_moe.experts[i].w2.weight.data
 
-    # Run forward passes for both MoE blocks
-    hf_states, _ = hf_moe.forward(inputs)
-    vllm_states = vllm_moe.forward(inputs)
+        # Generate input batch of dimensions [batch_size, seq_len, hidden_dim]
+        inputs = torch.randn((1, 64, config.hidden_size)).to("cuda")
 
-    assert torch.allclose(hf_states,vllm_states, rtol=1e-3, atol=1e-3)
+        # Run forward passes for both MoE blocks
+        hf_states, _ = hf_moe.forward(inputs)
+        vllm_states = vllm_moe.forward(inputs)
+
+        assert torch.allclose(hf_states,vllm_states, rtol=1e-3, atol=1e-3)
