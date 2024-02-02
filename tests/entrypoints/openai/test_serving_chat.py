3ea2d5edf52128ee3dcb6d689cc6e7fce3840c1e
Sebastian Schönnenbeck <sebastian.schoennenbeck@comma-soft.com>
2024-02-02 13:02:17 +0000

    Use mock-engine in test

--- tests/entrypoints/openai/test_serving_chat.py
+++ tests/entrypoints/openai/test_serving_chat.py
@@ -1,17 +1,30 @@
 import asyncio
 
+from dataclasses import dataclass
+
 from vllm.entrypoints.openai.serving_chat import OpenAIServingChat
-from vllm import AsyncEngineArgs
-from vllm.engine.async_llm_engine import AsyncLLMEngine
 
 MODEL_NAME = "openai-community/gpt2"
 CHAT_TEMPLATE = "Dummy chat template for testing"
 
 
+@dataclass
+class MockModelConfig:
+    tokenizer = MODEL_NAME
+    trust_remote_code = False
+    tokenizer_mode = "auto"
+    max_model_len = 100
+
+
+@dataclass
+class MockEngine:
+
+    async def get_model_config(self):
+        return MockModelConfig
+
+
 async def _async_serving_chat_init():
-    engine_args = AsyncEngineArgs(model=MODEL_NAME)
-    engine = AsyncLLMEngine.from_engine_args(engine_args)
-    serving_completion = OpenAIServingChat(engine,
+    serving_completion = OpenAIServingChat(MockEngine(),
                                            served_model=MODEL_NAME,
                                            response_role="assistant",
                                            chat_template=CHAT_TEMPLATE)
