2c6ff2675bf88497b428be6c8212ab8520124873
Andrew Feldman <afeldman@neuralmagic.com>
2024-02-02 04:34:02 -0500

    wip semi_structured_sparse_w16a16

--- examples/offline_inference_semi_structured_sparse.py
+++ examples/offline_inference_semi_structured_sparse.py
@@ -5,7 +5,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0" # Use only cuda:0
 
 model = LLM(
     "nm-testing/zephyr-50sparse-24", 
-    sparsity="sparse_w16a16",   # If left off, model will be loaded as dense
+    sparsity="semi_structured_sparse_w16a16",   # If left off, model will be loaded as dense
     enforce_eager=True,         # Does not work with cudagraphs yet
     dtype="float16",
     tensor_parallel_size=1,
