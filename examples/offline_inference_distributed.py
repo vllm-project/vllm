3596d35e318c31cbc9968b6b801104655ae3a44b
Cheng Su <scnju13@gmail.com>
2024-02-01 16:01:20 -0800

    Address comments

--- examples/offline_inference_distributed.py
+++ examples/offline_inference_distributed.py
@@ -2,24 +2,13 @@
 This example shows how to use Ray Data for running offline batch inference
 distributively on a multi-nodes cluster.
 
-Learn more of Ray Data in https://docs.ray.io/en/latest/data/data.html
+Learn more about Ray Data in https://docs.ray.io/en/latest/data/data.html
 """
 
 from vllm import LLM, SamplingParams
 from typing import Dict
 import numpy as np
 import ray
-
-# Initialize Ray cluster with required dependencies.
-ray.init(
-    runtime_env={
-        "pip": [
-            "accelerate>=0.16.0",
-            "transformers>=4.26.0",
-            "numpy<1.24",  # remove when mlflow updates beyond 2.2
-            "torch",
-        ]
-    })
 
 # Create a sampling params object.
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
@@ -58,12 +47,24 @@ ds = ds.map_batches(
     # Set the concurrency to the number of LLM instances.
     concurrency=10,
     # Specify the number of GPUs required per LLM instance.
+    # NOTE: Do NOT set `num_gpus` when using vLLM with tensor-parallelism
+    # (i.e., `tensor_parallel_size`).
     num_gpus=1,
     # Specify the batch size for inference.
     batch_size=32,
 )
 
+# Peek first 10 results.
+# NOTE: This is for local testing and debugging. For production use case,
+# one should write full result out as shown below.
+outputs = ds.take(limit=10)
+for output in outputs:
+    prompt = output["prompt"]
+    generated_text = output["generated_text"]
+    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+
 # Write inference output data out as Parquet files to S3.
 # Multiple files would be written to the output destination,
 # and each task would write one or more files separately.
-ds.write_parquet("s3://<your-output-bucket>")
+#
+# ds.write_parquet("s3://<your-output-bucket>")
