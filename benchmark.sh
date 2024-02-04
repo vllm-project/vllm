55191f52afb6b885d89175f8bf3aa290c6fea1af
Artem Yatsenko <yatsenkoa2020@gmail.com>
2024-02-04 18:59:26 +0000

    working decoding

--- benchmark.sh
+++ benchmark.sh
@@ -2,5 +2,5 @@ python3 benchmarks/benchmark_throughput.py \
         --input-len 550 \
         --output-len 150 \
         --model mistralai/Mistral-7B-v0.1 \
-        --num-prompts 10 \
+        --num-prompts 100 \
         --enforce-eager
