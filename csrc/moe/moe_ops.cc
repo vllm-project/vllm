92ac8dd3fc1757e87e85dd28a05ec7eedfefefcc
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2024-02-05 18:00:13 +0000

    Remove CUTLASS

--- csrc/moe/moe_ops.cc
+++ csrc/moe/moe_ops.cc
@@ -4,7 +4,4 @@
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("topk_softmax", &topk_softmax, "Apply topk softmax to the gating outputs.");
-  m.def("expand_and_permute", &expand_and_permute, "Expand and permute the input tokens.");
-  m.def("moe_mlp", &moe_mlp, "Apply the MoE MLP.");
-  m.def("unpermute_and_reduce", &unpermute_and_reduce, "Unpermute and reduce the MoE outputs.");
 }
--- csrc/moe/moe_ops.h
+++ csrc/moe/moe_ops.h
@@ -7,28 +7,3 @@ void topk_softmax(
   torch::Tensor& topk_indices,
   torch::Tensor& token_expert_indices,
   torch::Tensor& gating_output);
-
-void expand_and_permute(
-  torch::Tensor& permuted_tokens,
-  torch::Tensor& cum_num_tokens_per_expert,
-  torch::Tensor& reverse_permutation_map,
-  torch::Tensor& input_tokens,
-  torch::Tensor& topk_indices,
-  torch::Tensor& token_expert_indices);
-
-void moe_mlp(
-  torch::Tensor& moe_output,
-  torch::Tensor& input_tokens,
-  torch::Tensor& cum_num_tokens_per_expert,
-  torch::Tensor& fc1_expert_weights,
-  const c10::optional<torch::Tensor>& fc1_expert_biases,
-  int fc1_activation_type,
-  torch::Tensor& fc2_expert_weights);
-
-void unpermute_and_reduce(
-  torch::Tensor& output_tokens,
-  torch::Tensor& experts_output,
-  torch::Tensor& topk_weights,
-  torch::Tensor& topk_indices,
-  torch::Tensor& reverse_permutation_map,
-  bool renormalize);
