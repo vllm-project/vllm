8145f47f2893f9a2ca26fe3c13847d64f3311c16
Philipp Moritz <pcmoritz@gmail.com>
2024-02-05 03:46:55 -0800

    update

--- csrc/moe_kernels.cu
+++ csrc/moe_kernels.cu
@@ -255,11 +255,11 @@ typename Gemm::Arguments MakeArguments(torch::Tensor a,
   typename Gemm::Arguments arguments{
     cutlass::gemm::GemmUniversalMode::kGrouped,
     {static_cast<int>(num_experts), reinterpret_cast<typename ProblemShape::UnderlyingProblemShape*>(problem_sizes.data_ptr()), problem_sizes_host.data()},
-    {reinterpret_cast<ElementA**>(ptr_a.mutable_data_ptr()), (StrideA*)stride_a.data_ptr(),
-     reinterpret_cast<ElementB**>(ptr_b.mutable_data_ptr()), (StrideB*)stride_b.data_ptr()},
+    {ptr_a.mutable_data_ptr<ElementA*>(), (StrideA*)stride_a.data_ptr(),
+     ptr_b.mutable_data_ptr<ElementB*>(), (StrideB*)stride_b.data_ptr()},
     {{/*alpha=*/1.0f, /*beta=*/0.0f},
-     reinterpret_cast<ElementC**>(ptr_c.mutable_data_ptr()), (StrideC*)stride_c.data_ptr(),
-     reinterpret_cast<ElementC**>(ptr_c.mutable_data_ptr()), (StrideC*)stride_c.data_ptr()},
+     ptr_c.mutable_data_ptr<ElementC*>(), (StrideC*)stride_c.data_ptr(),
+     ptr_c.mutable_data_ptr<ElementC*>(), (StrideC*)stride_c.data_ptr()},
     hw_info
   };
 
