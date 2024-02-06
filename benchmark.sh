3922712022033c131893f9ddf846e6c37bfebb1e
Artem Yatsenko <yatsenkoa2020@gmail.com>
2024-02-06 03:22:49 +0000

    rm unnecessary files, add flashinfer as requirement

--- benchmark.sh
+++
@@ -1,6 +0,0 @@
-python3 benchmarks/benchmark_throughput.py \
-        --input-len 1000 \
-        --output-len 1000 \
-        --model mistralai/Mistral-7B-v0.1 \
-        --num-prompts 10 \
-        --enforce-eager
