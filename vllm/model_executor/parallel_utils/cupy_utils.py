bd5557d726c50b290aa779fe5784d3fab112e541
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2024-02-08 21:19:30 +0000

    Compatible with cupy 13.0

--- vllm/model_executor/parallel_utils/cupy_utils.py
+++ vllm/model_executor/parallel_utils/cupy_utils.py
@@ -7,14 +7,15 @@ CUDA graphs.
 TODO: Remove this file when torch.distributed.all_reduce is fixed.
 """
 import contextlib
+from unittest.mock import patch
 
 import torch
 from torch.distributed import ReduceOp
 
 try:
     import cupy
-    from cupyx.distributed import NCCLBackend
     from cupy.cuda import nccl
+    from cupyx.distributed._nccl_comm import NCCLBackend, _get_nccl_dtype_and_count
 except ImportError as e:
     cupy = e
     nccl = None
@@ -30,21 +31,22 @@ _OP_MAPPING = {
     ReduceOp.MAX: "max",
 }
 
-
-class NCCLBackendWithBFloat16(NCCLBackend):
-    # This is enough to add bfloat16 support for most operations,
-    # but broadcast will fail (will require changes in compiled
-    # cupy code).
-    def _get_nccl_dtype_and_count(self, array, count=None):
-        nccl_dtype, count = super()._get_nccl_dtype_and_count(array, count)
-        torch_dtype = getattr(array, "_torch_dtype", None)
-        if torch_dtype is torch.bfloat16:
-            nccl_dtype = nccl.NCCL_BFLOAT16
-        return nccl_dtype, count
-
-
 _NCCL_BACKEND = None
 _WORLD_SIZE = 0
+
+_get_nccl_dtype_and_count_orginal = _get_nccl_dtype_and_count
+
+
+def _get_nccl_dtype_and_count_bf16(*args, **kwargs):
+    """Patch/hack to force bf16 dtype in cupy NCCL.
+
+    cupy doesn't support bf16 by default, but the underlying NCCL
+    kernels do. We can just force the dtype to be bf16 and it will
+    work fine."""
+    dtype, count = _get_nccl_dtype_and_count_orginal(*args, **kwargs)
+    # Hardcoded to always return bf16 dtype
+    dtype = nccl.NCCL_BFLOAT16
+    return dtype, count
 
 
 def is_initialized() -> bool:
@@ -71,7 +73,7 @@ def init_process_group(world_size: int, rank: int, host: str,
 
     if isinstance(cupy, Exception):
         raise ImportError(
-            "NCCLBackend is not available. Please install cupy.") from cupy
+            "NCCLBackend is not available. Please install cupy==13.0.0.") from cupy
 
     # TODO(woosuk): Create TP and PP process groups for CuPy.
     global _NCCL_BACKEND
@@ -81,7 +83,7 @@ def init_process_group(world_size: int, rank: int, host: str,
         f"{rank=} should be a integer between [0, {world_size})")
 
     cupy.cuda.runtime.setDevice(torch.cuda.current_device())
-    _NCCL_BACKEND = NCCLBackendWithBFloat16(world_size, rank, host, port)
+    _NCCL_BACKEND = NCCLBackend(world_size, rank, host, port)
     _WORLD_SIZE = world_size
 
 
@@ -95,11 +97,16 @@ def all_reduce(input_: torch.Tensor, op=ReduceOp.SUM) -> None:
         # cupy will fail. This will not change
         # the underlying data.
         input_ = input_.view(torch.float16)
+        maybe_patch_cupy_bf16 = patch(
+            "cupyx.distributed._nccl_comm._get_nccl_dtype_and_count",
+            _get_nccl_dtype_and_count_bf16)
+    else:
+        maybe_patch_cupy_bf16 = contextlib.nullcontext()
     cupy_input = cupy.asarray(input_)
-    cupy_input._torch_dtype = torch_dtype  # pylint: disable=protected-access
-    _NCCL_BACKEND.all_reduce(in_array=cupy_input,
-                             out_array=cupy_input,
-                             op=_OP_MAPPING[op])
+    with maybe_patch_cupy_bf16:
+        _NCCL_BACKEND.all_reduce(in_array=cupy_input,
+                                 out_array=cupy_input,
+                                 op=_OP_MAPPING[op])
 
 
 def destroy_process_group() -> None:
