31c2cdd6fb90e528f43ed9f7e82828f3d368a736
Jonathan <jonchen0219@gmail.com>
2024-02-09 00:51:04 -0800

    added OpenAI embedded model API call

--- vllm/entrypoints/openai/api_server.py
+++ vllm/entrypoints/openai/api_server.py
@@ -19,7 +19,7 @@ from fastapi.responses import JSONResponse, StreamingResponse, Response
 from vllm.engine.arg_utils import AsyncEngineArgs
 from vllm.engine.async_llm_engine import AsyncLLMEngine
 from vllm.engine.metrics import add_global_metrics_labels
-from vllm.entrypoints.openai.protocol import CompletionRequest, ChatCompletionRequest, ErrorResponse
+from vllm.entrypoints.openai.protocol import CompletionRequest, ChatCompletionRequest, ErrorResponse, EmbeddingRequest
 from vllm.logger import init_logger
 from vllm.entrypoints.openai.serving_chat import OpenAIServingChat
 from vllm.entrypoints.openai.serving_completion import OpenAIServingCompletion
@@ -170,6 +170,17 @@ async def create_completion(request: CompletionRequest, raw_request: Request):
                                  media_type="text/event-stream")
     else:
         return JSONResponse(content=generator.model_dump())
+
+@app.post("/v1/embeddings")
+async def create_embeddings(request: EmbeddingRequest):
+
+    ## need to implement 
+    generator = await openai_serving_completion.create_completion()
+    if isinstance(generator, ErrorResponse):
+        return JSONResponse(content=generator.model_dump(),status_code=generator.code)
+    else:
+        return  JSONResponse(content=generator.model_dump())
+
 
 
 if __name__ == "__main__":
