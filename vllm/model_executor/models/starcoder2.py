0f492f55bc46df8a7bf80227fe39359c1fe9cee2
Matthew Wong <Matthew.Wong2@amd.com>
2024-02-12 22:31:09 +0000

    Initial skeleton; scaling factors in CacheEngine and PagedAttention

--- vllm/model_executor/models/starcoder2.py
+++ vllm/model_executor/models/starcoder2.py
@@ -47,7 +47,7 @@ except ImportError:
     # NOTE: Please install transformers from source or use transformers>=4.39.0
     from transformers import PretrainedConfig as Starcoder2Config
 
-KVCache = Tuple[torch.Tensor, torch.Tensor]
+KVCache = Tuple[torch.Tensor, torch.Tensor, Optional[torch.Tensor]]
 
 
 class Starcoder2Attention(nn.Module):
@@ -121,8 +121,9 @@ class Starcoder2Attention(nn.Module):
         qkv, _ = self.qkv_proj(hidden_states)
         q, k, v = qkv.split([self.q_size, self.kv_size, self.kv_size], dim=-1)
         q, k = self.rotary_emb(positions, q, k)
-        k_cache, v_cache = kv_cache
-        attn_output = self.attn(q, k, v, k_cache, v_cache, input_metadata)
+        k_cache, v_cache, kv_cache_scaling_factor = kv_cache
+        attn_output = self.attn(q, k, v, k_cache, v_cache,
+                                kv_cache_scaling_factor, input_metadata)
         output, _ = self.o_proj(attn_output)
         return output
 
--- vllm/model_executor/weight_utils.py
+++ vllm/model_executor/weight_utils.py
@@ -261,6 +261,22 @@ def hf_model_weights_iterator(
             torch.cuda.empty_cache()
 
 
+def kv_cache_scales_iterator(filename: str) -> Iterator[Tuple[int, torch.Tensor]]:
+    """
+    A simple utility to read in KV cache scaling factors that have been
+    previously serialized to disk. Used by the CacheEngine to populate its
+    caches with the appropriate scaling factors. The first object of the pair
+    is the cache (and model) layer corresponding to the scaling factor, and the 
+    second is the scaling factor itself. Keep this function in sync with the output 
+    of 3rdparty/quantization/extract_scales.py and with the scaling factor structure
+    assumed to hold in worker/cache_engine.py
+    """
+    with open(filename) as f:
+        layer_scale_factor_map = json.load(f, parse_int=int, parse_constant=float)
+        for layer_idx, scale_factor in layer_scale_factor_map.items():
+            yield int(layer_idx), float(scale_factor)
+
+
 def convert_pyslice_to_tensor(x: Any) -> torch.Tensor:
     """convert PySafeSlice object from safetensors to torch.Tensor
 
