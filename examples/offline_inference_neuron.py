a9f094be793105260fc3dbd584cf393abbfc4da9
Liangfu Chen <liangfc@amazon.com>
2024-02-12 17:46:30 +0000

    specify containts of neuron backend support

--- examples/offline_inference_neuron.py
+++ examples/offline_inference_neuron.py
@@ -13,6 +13,10 @@ sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 # Create an LLM.
 llm = LLM(model="openlm-research/open_llama_3b",
           max_num_seqs=8,
+          # The max_model_len and block_size arguments are required to be same as max sequence length,
+          # when targeting neuron device. Currently, this is a known limitation in continuous batching
+          # support in transformers-neuronx.
+          # TODO(liangfu): Support paged-attention in transformers-neuronx.
           max_model_len=128,
           block_size=128)
 # Generate texts from the prompts. The output is a list of RequestOutput objects
