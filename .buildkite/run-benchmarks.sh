e42eb3ea1af070a1c1e901ce4e4e1e08b907c48f
Roger Wang <ywang@roblox.com>
2024-02-11 20:25:02 -0800

    fix CI

--- .buildkite/run-benchmarks.sh
+++ .buildkite/run-benchmarks.sh
@@ -6,7 +6,7 @@ set -o pipefail
 # cd into parent directory of this file
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
-(wget && curl) || (apt-get update && apt-get install -y wget curl)
+(which wget && which curl) || (apt-get update && apt-get install -y wget curl)
 
 # run python benchmarks and upload the result to buildkite
 python3 benchmarks/benchmark_latency.py 2>&1 | tee benchmark_latency.txt
