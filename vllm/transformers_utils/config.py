5c976a7e1a1bec875bf6474824b7dff39e38de18
Roy <jasonailu87@gmail.com>
2024-02-13 16:09:23 +0800

    Refactor llama family models (#2637)

--- vllm/transformers_utils/config.py
+++ vllm/transformers_utils/config.py
@@ -5,14 +5,10 @@ from transformers import AutoConfig, PretrainedConfig
 from vllm.transformers_utils.configs import *
 
 _CONFIG_REGISTRY = {
-    "aquila": AquilaConfig,
-    "baichuan": BaiChuanConfig,
     "chatglm": ChatGLMConfig,
     "mpt": MPTConfig,
-    "qwen": QWenConfig,
     "RefinedWeb": RWConfig,  # For tiiuae/falcon-40b(-instruct)
     "RefinedWebModel": RWConfig,  # For tiiuae/falcon-7b(-instruct)
-    "yi": YiConfig,
 }
 
 
