98c230b37554eb918d4658d8ecb65dfca1b8400e
simon-mo <simon.mo@hey.com>
2024-02-13 02:47:15 +0000

    add initial code for generating snapshots

---
+++ tests/models/snapshot-tests/generate-snapshot.py
@@ -0,0 +1,51 @@
+import json
+import os
+
+import pytest
+
+from test_models_snapshot import MODELS, DTYPE, MAX_TOKENS
+
+snapshot_path = "./snapshot.json"
+
+
+def update_snapshot(model, dtype, max_tokens, prompt, hf_output_str):
+    key = f"{model}-{dtype}-{max_tokens}"
+
+    if not os.path.exists(snapshot_path):
+        with open(snapshot_path, "w") as f:
+            f.write("{}")
+    with open(snapshot_path) as f:
+        snapshot = json.load(f)
+
+    snapshot.setdefault(key, {})
+    snapshot[key].setdefault(prompt, [])
+    snapshot[key][prompt].append(hf_output_str)
+
+    with open(snapshot_path, "w") as f:
+        json.dump(snapshot, f, indent=2)
+
+
+@pytest.mark.parametrize("model", MODELS)
+@pytest.mark.parametrize("dtype", [DTYPE])
+@pytest.mark.parametrize("max_tokens", [MAX_TOKENS])
+def test_models(
+    hf_runner,
+    example_prompts,
+    model: str,
+    dtype: str,
+    max_tokens: int,
+) -> None:
+    print("Running", model, dtype, max_tokens)
+    hf_model = hf_runner(model, dtype=dtype)
+    hf_outputs = hf_model.generate_greedy(example_prompts, max_tokens)
+    del hf_model
+
+    for i in range(len(example_prompts)):
+        hf_output_ids, hf_output_str = hf_outputs[i]
+
+        update_snapshot(model, dtype, max_tokens, example_prompts[i],
+                        hf_output_ids, hf_output_str)
+
+
+if __name__ == "__main__":
+    pytest.main(["-v", "-s", __file__])
---
+++ tests/models/snapshot-tests/test_models_snapshot.py
@@ -0,0 +1,44 @@
+"""Compare the outputs of HF and vLLM when using greedy sampling.
+
+The data is from HF implementation is generated ahead of time on A100 or H100 by using
+`python generate-snapshot.py` or manually update the `snapshot.json` file.
+"""
+import pytest
+
+MODELS = [
+    "facebook/opt-125m", "meta-llama/Llama-2-7b-hf",
+    "mistralai/Mistral-7B-v0.1", "Deci/DeciLM-7b", "tiiuae/falcon-7b", "gpt2",
+    "bigcode/tiny_starcoder_py", "EleutherAI/gpt-j-6b",
+    "EleutherAI/pythia-70m", "bigscience/bloom-560m", "mosaicml/mpt-7b",
+    "microsoft/phi-2", "stabilityai/stablelm-3b-4e1t"
+]
+DTYPE = "float"
+MAX_TOKENS = 128
+
+
+@pytest.mark.parametrize("model", MODELS)
+@pytest.mark.parametrize("dtype", [DTYPE])
+@pytest.mark.parametrize("max_tokens", [MAX_TOKENS])
+def test_models(
+    hf_runner,
+    vllm_runner,
+    example_prompts,
+    model: str,
+    dtype: str,
+    max_tokens: int,
+) -> None:
+    hf_model = hf_runner(model, dtype=dtype)
+    hf_outputs = hf_model.generate_greedy(example_prompts, max_tokens)
+    del hf_model
+
+    vllm_model = vllm_runner(model, dtype=dtype)
+    vllm_outputs = vllm_model.generate_greedy(example_prompts, max_tokens)
+    del vllm_model
+
+    for i in range(len(example_prompts)):
+        hf_output_ids, hf_output_str = hf_outputs[i]
+        vllm_output_ids, vllm_output_str = vllm_outputs[i]
+        assert hf_output_str == vllm_output_str, (
+            f"Test{i}:\nHF: {hf_output_str!r}\nvLLM: {vllm_output_str!r}")
+        assert hf_output_ids == vllm_output_ids, (
+            f"Test{i}:\nHF: {hf_output_ids}\nvLLM: {vllm_output_ids}")
