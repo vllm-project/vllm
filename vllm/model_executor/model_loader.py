d937b136e1d8f4532a183e85c9bf4742bafe6006
Terry <149540247+tterrysun@users.noreply.github.com>
2024-02-13 15:55:45 -0800

    Add LoRA support for Mixtral (#2831)
    * add mixtral lora support
    
    * formatting
    
    * fix incorrectly ported logic
    
    * polish tests
    
    * minor fixes and refactoring
    
    * minor fixes
    
    * formatting
    
    * rename and remove redundant logic
    
    * refactoring
    
    * refactoring
    
    * minor fix
    
    * minor refactoring
    
    * fix code smell
    
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/model_executor/model_loader.py
+++ vllm/model_executor/model_loader.py
@@ -66,7 +66,7 @@ def get_model(model_config: ModelConfig,
         # Create a model instance.
         # The weights will be initialized as empty tensors.
         with torch.device(device_config.device):
-            if getattr(model_class, "supports_lora", False):
+            if hasattr(model_class, "supported_lora_modules"):
                 model = model_class(model_config.hf_config, linear_method,
                                     lora_config)
             elif lora_config:
