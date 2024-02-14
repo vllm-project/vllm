22766b48ecb18c037dc49004717d66a9027fcee2
Doug Lehr <Doug.Lehr@amd.com>
2024-02-14 23:51:08 +0000

    Allow benchmark_latency to take a list of input/output/batches for faster execution
    Also add reporting functionality for easy display

--- benchmarks/benchmark_latency.py
+++ benchmarks/benchmark_latency.py
@@ -3,17 +3,23 @@ import argparse
 import time
 from pathlib import Path
 from typing import Optional
-
+import pandas as pd
 import numpy as np
 import torch
 from tqdm import tqdm
 
 from vllm import LLM, SamplingParams
+from torch.profiler import profile, record_function, ProfilerActivity
 
+def list_of_ints(arg):
+    return list(map(int, arg.split(',')))
 
 def main(args: argparse.Namespace):
     print(args)
 
+    print(f'>>>Loading LLM')
+    if args.report:
+        results_df = pd.DataFrame(columns=['model', 'batch', 'tp', 'input', 'output', 'latency'])
     # NOTE(woosuk): If the request cannot be processed in a single batch,
     # the engine will automatically process the request in multiple batches.
     llm = LLM(
@@ -26,57 +32,62 @@ def main(args: argparse.Namespace):
         enforce_eager=args.enforce_eager,
     )
 
-    sampling_params = SamplingParams(
-        n=args.n,
-        temperature=0.0 if args.use_beam_search else 1.0,
-        top_p=1.0,
-        use_beam_search=args.use_beam_search,
-        ignore_eos=True,
-        max_tokens=args.output_len,
-    )
-    print(sampling_params)
-    dummy_prompt_token_ids = [[0] * args.input_len] * args.batch_size
+    for batch_size in args.batch_size:
+        for output_len in args.output_len:
+            for input_len in args.input_len:
+                print(f'>>>RUNNING {args.model} Batch_size:{batch_size} Input_len:{input_len} Output_len:{output_len}') 
+                sampling_params = SamplingParams(
+                    n=args.n,
+                    temperature=0.0 if args.use_beam_search else 1.0,
+                    top_p=1.0,
+                    use_beam_search=args.use_beam_search,
+                    ignore_eos=True,
+                    max_tokens=output_len,
+                )
+                print(sampling_params)
+                dummy_prompt_token_ids = [[0] * input_len] * batch_size
 
-    def run_to_completion(profile_dir: Optional[str] = None):
-        if profile_dir:
-            with torch.profiler.profile(
-                    activities=[
-                        torch.profiler.ProfilerActivity.CPU,
-                        torch.profiler.ProfilerActivity.CUDA,
-                    ],
-                    on_trace_ready=torch.profiler.tensorboard_trace_handler(
-                        str(profile_dir))) as p:
-                llm.generate(prompt_token_ids=dummy_prompt_token_ids,
-                             sampling_params=sampling_params,
-                             use_tqdm=False)
-            print(p.key_averages())
-        else:
-            start_time = time.perf_counter()
-            llm.generate(prompt_token_ids=dummy_prompt_token_ids,
-                         sampling_params=sampling_params,
-                         use_tqdm=False)
-            end_time = time.perf_counter()
-            latency = end_time - start_time
-            return latency
+                def run_to_completion(profile_dir: Optional[str] = None):
+                    if profile_dir:
+                        with torch.profiler.profile(
+                                activities=[
+                                    torch.profiler.ProfilerActivity.CPU,
+                                    torch.profiler.ProfilerActivity.CUDA,
+                                ],
+                                on_trace_ready=torch.profiler.tensorboard_trace_handler(
+                                    str(profile_dir))) as p:
+                            llm.generate(prompt_token_ids=dummy_prompt_token_ids,
+                                        sampling_params=sampling_params,
+                                        use_tqdm=False)
+                        print(p.key_averages())
+                    else:
+                        start_time = time.perf_counter()
+                        llm.generate(prompt_token_ids=dummy_prompt_token_ids,
+                                    sampling_params=sampling_params,
+                                    use_tqdm=False)
+                        end_time = time.perf_counter()
+                        latency = end_time - start_time
+                        return latency
 
-    print("Warming up...")
-    run_to_completion(profile_dir=None)
+                print("Warming up...")
+                run_to_completion(profile_dir=None)
 
-    if args.profile:
-        profile_dir = args.profile_result_dir
-        if not profile_dir:
-            profile_dir = Path(
-                "."
-            ) / "vllm_benchmark_result" / f"latency_result_{time.time()}"
-        print(f"Profiling (results will be saved to '{profile_dir}')...")
-        run_to_completion(profile_dir=args.profile_result_dir)
-        return
+                if args.profile:
+                    profile_dir = args.profile_result_dir
+                    if not profile_dir:
+                        profile_dir = Path(
+                            "."
+                        ) / "vllm_benchmark_result" / f"latency_result_{time.time()}"
+                    print(f"Profiling (results will be saved to '{profile_dir}')...")
+                    run_to_completion(profile_dir=args.profile_result_dir)
+                    return
 
-    # Benchmark.
-    latencies = []
-    for _ in tqdm(range(args.num_iters), desc="Profiling iterations"):
-        latencies.append(run_to_completion(profile_dir=None))
-    print(f'Avg latency: {np.mean(latencies)} seconds')
+                # Benchmark.
+                latencies = []
+                for _ in tqdm(range(args.num_iters), desc="Profiling iterations"):
+                    latencies.append(run_to_completion(profile_dir=None))
+                print(f'Avg latency: {np.mean(latencies)} seconds')
+
 
 
 if __name__ == '__main__':
@@ -90,9 +101,9 @@ if __name__ == '__main__':
                         choices=['awq', 'gptq', 'squeezellm', None],
                         default=None)
     parser.add_argument('--tensor-parallel-size', '-tp', type=int, default=1)
-    parser.add_argument('--input-len', type=int, default=32)
-    parser.add_argument('--output-len', type=int, default=128)
-    parser.add_argument('--batch-size', type=int, default=8)
+    parser.add_argument('--input-len', type=list_of_ints, default=32)
+    parser.add_argument('--output-len', type=list_of_ints, default=128)
+    parser.add_argument('--batch-size', type=list_of_ints, default=8)
     parser.add_argument('--n',
                         type=int,
                         default=1,
@@ -127,5 +138,10 @@ if __name__ == '__main__':
         default=None,
         help=('path to save the pytorch profiler output. Can be visualized '
               'with ui.perfetto.dev or Tensorboard.'))
+    parser.add_argument('--warmup-only', action='store_true',
+                        help='only run warmup, useful for tuning')
+    parser.add_argument('--report', action='store_true',
+                        help='turn on dataframe reporting')
+    parser.add_argument('--report-file', type=str, default=None)
     args = parser.parse_args()
     main(args)
