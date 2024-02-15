7fd056fb80596331bce6b498948f2a3e9697e9f1
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2024-02-14 22:17:44 -0800

    [BugFix] Fix GC bug for `LLM` class (#2882)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -4,6 +4,10 @@ It should include tests that are reported by users and making sure they
 will never happen again.
 
 """
+import gc
+
+import torch
+
 from vllm import LLM, SamplingParams
 
 
@@ -33,6 +37,20 @@ def test_max_tokens_none():
     outputs = llm.generate(prompts, sampling_params=sampling_params)
 
     assert len(prompts) == len(outputs)
+
+
+def test_gc():
+    llm = LLM("facebook/opt-125m", enforce_eager=True)
+    del llm
+
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    # The memory allocated for model and KV cache should be released.
+    # The memory allocated for PyTorch and others should be less than 50MB.
+    # Usually, it's around 10MB.
+    allocated = torch.cuda.memory_allocated()
+    assert allocated < 50 * 1024 * 1024
 
 
 if __name__ == "__main__":
