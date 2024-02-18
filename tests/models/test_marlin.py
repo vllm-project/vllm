9b1bc5fde4140199f7db2ee116f336e909a570e5
rsnm2 <rshaw@neuralmagic.com>
2024-02-18 19:22:50 +0000

    merged into upstream main

--- tests/models/test_marlin.py
+++ tests/models/test_marlin.py
@@ -32,6 +32,8 @@ class ModelPair:
 model_pairs = [
     ModelPair(model_marlin="nm-testing/zephyr-beta-7b-marlin-g128",
               model_gptq="nm-testing/zephyr-beta-7b-gptq-g128"),
+    ModelPair(model_marlin="nm-testing/zephyr-beta-7b-marlin-channelwise",
+              model_gptq="nm-testing/zephyr-beta-7b-gptq-channelwise"),
     ModelPair(model_marlin="robertgshaw2/TinyLlama-1.1B-Chat-v1.0-g128-marlin",
               model_gptq="robertgshaw2/TinyLlama-1.1B-Chat-v1.0-g128-gptq")
 ]
