97e6d0349441b5e763c8caeca138cef6848cf01b
Simon Mo <simon.mo@hey.com>
2024-02-18 21:36:19 -0800

    Add warning to prevent changes to benchmark api server (#2858)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/entrypoints/api_server.py
+++ vllm/entrypoints/api_server.py
@@ -1,3 +1,9 @@
+"""
+NOTE: This API server is used only for demonstrating usage of AsyncEngine and simple performance benchmarks.
+It is not intended for production use. For production use, we recommend using our OpenAI compatible server.
+We are also not going to accept PRs modifying this file, please change `vllm/entrypoints/openai/api_server.py` instead.
+"""
+
 import argparse
 import json
 from typing import AsyncGenerator
