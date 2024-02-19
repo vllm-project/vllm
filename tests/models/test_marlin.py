7a9b828c44654604e464fc2fac7ac2b12fdc646a
rsnm2 <rshaw@neuralmagic.com>
2024-02-19 15:20:55 +0000

    updated retry testing to use pytest-flaky rather than implementing this logic myself.

--- tests/models/test_marlin.py
+++ tests/models/test_marlin.py
@@ -1,9 +1,8 @@
 """Compare the outputs of a GPTQ model to a Marlin model.
 
 Note: GPTQ and Marlin do not have bitwise correctness. 
-
-As a result, in this test, we just confirm that the top 5 selected tokens of the 
-Marlin model are in the top 5 selected tokens of the GPTQ model. 
+As a result, in this test, we just confirm that the top selected tokens of the 
+Marlin/GPTQ models are in the top 3 selections of eachother.
 
 Note: Marlin internally uses locks to synchronize the threads. This can
 result in very slight nondeterminism for Marlin. As a result, we re-run the test
@@ -39,13 +38,13 @@ model_pairs = [
 ]
 
 
+@pytest.mark.flaky(reruns=2)
 @pytest.mark.skipif(marlin_not_supported,
                     reason="Marlin is not supported on this GPU type.")
 @pytest.mark.parametrize("model_pair", model_pairs)
 @pytest.mark.parametrize("dtype", ["half"])
 @pytest.mark.parametrize("max_tokens", [32])
 @pytest.mark.parametrize("num_logprobs", [3])
-@pytest.mark.parametrize("failure_tolerance", [3])
 def test_models(
     vllm_runner,
     example_prompts,
@@ -53,65 +52,50 @@ def test_models(
     dtype: str,
     max_tokens: int,
     num_logprobs: int,
-    failure_tolerance: int,
 ) -> None:
+    marlin_model = vllm_runner(model_pair.model_marlin, dtype=dtype)
+    marlin_outputs = marlin_model.generate_greedy_logprobs(
+        example_prompts, max_tokens, num_logprobs)
 
-    # Run the experiment failure_tolerance times
-    for retry_idx in range(failure_tolerance):
-        marlin_model = vllm_runner(model_pair.model_marlin, dtype=dtype)
-        marlin_outputs = marlin_model.generate_greedy_logprobs(
-            example_prompts, max_tokens, num_logprobs)
+    # Note: not sure why, but deleting just the model on Ada Lovelace
+    #   does not free the GPU memory. On Ampere, deleting the just model
+    #   frees the memory.
+    del marlin_model.model.llm_engine.driver_worker
+    del marlin_model
 
-        # Note: not sure why, but deleting just the model on Ada Lovelace
-        #   does not free the GPU memory. On Ampere, deleting the just model
-        #   frees the memory.
-        del marlin_model.model.llm_engine.driver_worker
-        del marlin_model
+    gptq_model = vllm_runner(model_pair.model_gptq, dtype=dtype)
+    gptq_outputs = gptq_model.generate_greedy_logprobs(
+        example_prompts, max_tokens, num_logprobs)
 
-        gptq_model = vllm_runner(model_pair.model_gptq, dtype=dtype)
-        gptq_outputs = gptq_model.generate_greedy_logprobs(
-            example_prompts, max_tokens, num_logprobs)
+    # Note: not sure why, but deleting just the model on Ada Lovelace
+    #   does not free the GPU memory. On Ampere, deleting the just model
+    #   frees the memory.
+    del gptq_model.model.llm_engine.driver_worker
+    del gptq_model
 
-        # Note: not sure why, but deleting just the model on Ada Lovelace
-        #   does not free the GPU memory. On Ampere, deleting the just model
-        #   frees the memory.
-        del gptq_model.model.llm_engine.driver_worker
-        del gptq_model
+    # loop through the prompts
+    for prompt_idx in range(len(example_prompts)):
+        gptq_output_ids, gptq_output_str, gptq_logprobs = gptq_outputs[
+            prompt_idx]
+        marlin_output_ids, marlin_output_str, marlin_logprobs = marlin_outputs[
+            prompt_idx]
 
-        # index of the failed_prompt
-        failed_prompt_idx = -1
-        failed_input_idx = -1
+        for idx, (gptq_output_id, marlin_output_id) in enumerate(
+                zip(gptq_output_ids, marlin_output_ids)):
+            # If sequence is not an exact match,
+            if marlin_output_id != gptq_output_id:
+                if gptq_output_id not in marlin_logprobs[idx]:
+                    print("\n\n\nOMG IM HERE\n\n\n")
+                if marlin_output_id not in gptq_logprobs[idx]:
+                    print("\n\n\nOMG IM HERE\n\n\n")
 
-        # loop through the prompts
-        for prompt_idx in range(len(example_prompts)):
-            gptq_output_ids, gptq_output_str, gptq_logprobs = gptq_outputs[
-                prompt_idx]
-            marlin_output_ids, marlin_output_str, marlin_logprobs = marlin_outputs[
-                prompt_idx]
+                # Each predicted token must be in top 5 of the other's
+                assert gptq_output_id in marlin_logprobs[idx], (
+                    f"Test{prompt_idx}:\nGPTQ:\t{gptq_output_str!r}\nMarlin:\t{marlin_output_str!r}"
+                )
+                assert marlin_output_id in gptq_logprobs[idx], (
+                    f"Test{prompt_idx}:\nGPTQ:\t{gptq_output_str!r}\nMarlin:\t{marlin_output_str!r}"
+                )
 
-            for idx, (gptq_output_id, marlin_output_id) in enumerate(
-                    zip(gptq_output_ids, marlin_output_ids)):
-                # If sequence is not an exact match,
-                if marlin_output_id != gptq_output_id:
-                    # Each predicted token must be in top 3 of the other's or iteration is a failure
-                    if (gptq_output_id not in marlin_logprobs[idx]
-                            or marlin_output_id not in gptq_logprobs[idx]):
-                        failed_prompt_idx = prompt_idx
-                        failed_input_idx = idx
-                    break
-
-            # Break out of this retry
-            if failed_prompt_idx != -1:
-                print(f"Found failure on retry idx {retry_idx}")
+                # Break out since sequences will now diverge.
                 break
-
-        # Return if we
-        if failed_prompt_idx == -1:
-            return
-
-    assert gptq_output_id in marlin_logprobs[failed_input_idx], (
-        f"Test{failed_prompt_idx}:\nGPTQ:\t{gptq_output_str!r}\nMarlin:\t{marlin_output_str!r}"
-    )
-    assert marlin_output_id in gptq_logprobs[failed_input_idx], (
-        f"Test{failed_prompt_idx}:\nGPTQ:\t{gptq_output_str!r}\nMarlin:\t{marlin_output_str!r}"
-    )
