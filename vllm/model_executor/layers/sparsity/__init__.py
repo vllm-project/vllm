2827dfe2d900b1c1ea3855c6f109eb067ea5f226
Michael Goin <michael@neuralmagic.com>
2024-02-21 15:50:44 -0800

    Only import magic_wand if sparsity is enabled (#37)
    Tested by making sure magic_wand was uninstalled and this code for a
    dense model runs fine:
    ```python
    from vllm import LLM, SamplingParams
    model = LLM("nm-testing/opt-125m-pruned2.4", enforce_eager=True)
    ```
    
    Then testing with a sparse model run:
    ```python
    from vllm import LLM, SamplingParams
    model = LLM("nm-testing/opt-125m-pruned2.4", sparsity="sparse_w16a16", enforce_eager=True)
    ```
    output:
    ```
    ...
      File "/home/michael/code/neuralmagic-vllm/vllm/model_executor/weight_utils.py", line 93, in get_sparse_config
        from vllm.model_executor.layers.sparsity import get_sparsity_config
      File "/home/michael/code/neuralmagic-vllm/vllm/model_executor/layers/sparsity/__init__.py", line 6, in <module>
        raise ValueError(
    ValueError: magic_wand is not available and required for sparsity support. Please install it with `pip install magic_wand`
    ```

--- vllm/model_executor/layers/sparsity/__init__.py
+++ vllm/model_executor/layers/sparsity/__init__.py
@@ -1,8 +1,15 @@
 from typing import Type
+import importlib.util
 
-from vllm.model_executor.layers.sparsity.base_config import SparsityConfig
-from vllm.model_executor.layers.sparsity.sparse_w16a16 import SparseW16A16Config
-from vllm.model_executor.layers.sparsity.semi_structured_sparse_w16a16 import SemiStructuredSparseW16A16Config
+is_magic_wand_available = importlib.util.find_spec("magic_wand") is not None
+if not is_magic_wand_available:
+    raise ValueError(
+        "magic_wand is not available and required for sparsity "
+        "support. Please install it with `pip install magic_wand`")
+
+from vllm.model_executor.layers.sparsity.base_config import SparsityConfig  # noqa: E402
+from vllm.model_executor.layers.sparsity.sparse_w16a16 import SparseW16A16Config  # noqa: E402
+from vllm.model_executor.layers.sparsity.semi_structured_sparse_w16a16 import SemiStructuredSparseW16A16Config  # noqa: E402
 
 _SPARSITY_CONFIG_REGISTRY = {
     "sparse_w16a16": SparseW16A16Config,
