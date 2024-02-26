079cba53b7fc41d646b951feee845d59fdb363e4
James Fleming <james@neuralmagic.com>
2024-02-26 14:47:05 -0500

    actual add kernel

--- csrc/ops.h
+++ csrc/ops.h
@@ -100,6 +100,15 @@ torch::Tensor gptq_gemm(
   torch::Tensor b_g_idx,
   bool use_exllama);
 
+torch::Tensor aqlm_gemm(
+  torch::Tensor a,
+  torch::Tensor b_q_weight,
+  torch::Tensor b_gptq_qzeros,
+  torch::Tensor b_gptq_scales,
+  torch::Tensor b_g_idx,
+  bool use_exllama);
+
+
 void gptq_shuffle(
   torch::Tensor q_weight,
   torch::Tensor q_perm);
