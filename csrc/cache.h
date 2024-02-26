a627903d314d64c3d54c347788dc096e0ab0958f
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2024-02-26 15:00:54 -0800

    [Minor] Remove gather_cached_kv kernel (#3043)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- csrc/cache.h
+++ csrc/cache.h
@@ -23,13 +23,6 @@ void reshape_and_cache(
   torch::Tensor& slot_mapping,
   const std::string& kv_cache_dtype);
 
-void gather_cached_kv(
-  torch::Tensor& key,
-  torch::Tensor& value,
-  torch::Tensor& key_cache,
-  torch::Tensor& value_cache,
-  torch::Tensor& slot_mapping);
-
 // Just for unittest
 void convert_fp8_e5m2(
   torch::Tensor& src_cache,
