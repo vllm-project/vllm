7c0bf6e783d7ea1610c30dee32367f1c9653df90
Matthew Wong <Matthew.Wong2@amd.com>
2024-02-26 20:28:33 +0000

    Change convention: Initialize scaling factors always if KV cache is FP8 and exceptions are handled, else terminate program execution

--- vllm/worker/model_runner.py
+++ vllm/worker/model_runner.py
@@ -110,21 +110,25 @@ class ModelRunner:
                 self.model.embedding_padding_modules)
             self.model = self.lora_manager.create_lora_manager(self.model)
         
-        if self.model_config.kv_cache_scales_path is not None:
-            if self.kv_cache_dtype == "fp8":
+        if self.kv_cache_dtype == "fp8":
+            if self.model_config.kv_cache_scales_path is not None:
                 if callable(getattr(self.model, "load_kv_cache_scales", None)):
                     self.model.load_kv_cache_scales(self.model_config.kv_cache_scales_path)
                 else:
-                    logger.warn("Using FP8 KV cache and scaling factors provided but "
-                                f"model {self.model.__class__} does not support loading "
-                                "scaling factors. Defaulting to scaling factors of 1.0, "
-                                "This may lead to less accurate results!")
+                    raise RuntimeError("Using FP8 KV cache and scaling factors provided but "
+                                       f"model {self.model.__class__} does not support loading "
+                                       "scaling factors.")
+            elif callable(getattr(self.model, "load_dummy_kv_cache_scales", None)):
+                logger.warn(f"Using FP8 KV cache but no scaling factors provided. Defaulting to "
+                            "scaling factors of 1.0, This may lead to less accurate results!")
+                self.model.load_dummy_kv_cache_scales()
             else:
-                logger.warn("KV cache scaling factors provided, but the KV cache data type is not FP8. "
-                            "KV cache scaling factors will not be used.")
-        elif self.kv_cache_dtype == "fp8":
-            logger.warn(f"Using FP8 KV cache but no scaling factors provided. Defaulting to "
-                         "scaling factors of 1.0, This may lead to less accurate results!")
+                raise RuntimeError(f"Using FP8 KV cache but no scaling factors provided and model "
+                                    "does not support loading dummy KV cache scaling factors.")
+        elif self.model_config.kv_cache_scales_path is not None:
+            logger.warn("KV cache scaling factors provided, but the KV cache data type is not FP8. "
+                        "KV cache scaling factors will not be used.")
+
 
     def set_block_size(self, block_size: int) -> None:
         self.block_size = block_size
