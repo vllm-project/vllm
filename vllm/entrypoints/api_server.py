51662599a91be0ab1f3f32b18c74a77de8116393
Ganesh Jagadeesan <cjagadeesan@dataminr.com>
2024-02-27 21:45:57 +0000

    fix: multi-lora with sample api-server

--- vllm/entrypoints/api_server.py
+++ vllm/entrypoints/api_server.py
@@ -17,9 +17,13 @@ from vllm.engine.async_llm_engine import AsyncLLMEngine
 from vllm.sampling_params import SamplingParams
 from vllm.utils import random_uuid
 
+from vllm.lora.request import LoRARequest
+from vllm.entrypoints.openai.api_server import LoRAParserAction
+
 TIMEOUT_KEEP_ALIVE = 5  # seconds.
 app = FastAPI()
 engine = None
+adapters = {}
 
 
 @app.get("/health")
@@ -34,19 +38,29 @@ async def generate(request: Request) -> Response:
 
     The request should be a JSON object with the following fields:
     - prompt: the prompt to use for the generation.
+    - adapter: name of the LoRA adapter to be used.
     - stream: whether to stream the results or not.
     - other fields: the sampling parameters (See `SamplingParams` for details).
     """
     request_dict = await request.json()
     prompt = request_dict.pop("prompt")
+    adapter = request_dict.pop("adapter", None)
     prefix_pos = request_dict.pop("prefix_pos", None)
     stream = request_dict.pop("stream", False)
     sampling_params = SamplingParams(**request_dict)
     request_id = random_uuid()
 
+    if not adapter:
+        lora_request = None
+    elif adapter not in adapters:
+        raise ValueError(f"{adapter} not a valid adapter in this service")
+    else:
+        lora_request = adapters[adapter]
+
     results_generator = engine.generate(prompt,
                                         sampling_params,
                                         request_id,
+                                        lora_request=lora_request,
                                         prefix_pos=prefix_pos)
 
     # Streaming case
@@ -89,11 +103,27 @@ if __name__ == "__main__":
         type=str,
         default=None,
         help="FastAPI root_path when app is behind a path based routing proxy")
+    parser.add_argument(
+        "--lora-modules",
+        type=str,
+        default=None,
+        nargs='+',
+        action=LoRAParserAction,
+        help=
+        "LoRA module configurations in the format name=path. Multiple modules can be specified."
+    )
     parser = AsyncEngineArgs.add_cli_args(parser)
     args = parser.parse_args()
 
     engine_args = AsyncEngineArgs.from_cli_args(args)
     engine = AsyncLLMEngine.from_engine_args(engine_args)
+    adapters = {
+        lora.name: LoRARequest(
+            lora_name=lora.name,
+            lora_int_id=i,
+            lora_local_path=lora.local_path,
+        ) for i, lora in enumerate(args.lora_modules, start=1)
+    } if args.enable_lora else {}
 
     app.root_path = args.root_path
     uvicorn.run(app,
