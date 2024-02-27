de2b481e6dff47192ce01cc3cbb0c7ac19e6e0ab
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2024-02-27 23:15:32 +0000

    Explicitly call GC for memory reuse

--- tests/lora/test_llama.py
+++ tests/lora/test_llama.py
@@ -1,3 +1,5 @@
+import gc
+
 import pytest
 import ray
 
@@ -127,12 +129,16 @@ def test_llama_lora_warmup(sql_lora_files):
     def get_num_gpu_blocks_lora():
         llm = vllm.LLM(MODEL_PATH, enable_lora=True, max_num_seqs=16)
         num_gpu_blocks_lora_warmup = llm.llm_engine.cache_config.num_gpu_blocks
+        del llm
+        gc.collect()
         return num_gpu_blocks_lora_warmup
 
     @ray.remote(num_gpus=1)
     def get_num_gpu_blocks_no_lora():
         llm = vllm.LLM(MODEL_PATH, max_num_seqs=16)
         num_gpu_blocks_no_lora_warmup = llm.llm_engine.cache_config.num_gpu_blocks
+        del llm
+        gc.collect()
         return num_gpu_blocks_no_lora_warmup
 
     num_gpu_blocks_lora_warmup = ray.get(get_num_gpu_blocks_lora.remote())
