d956844419c9aed4c47b99cdd8a558118bca2441
楚天翔 <tianxiang.ctx@alibaba-inc.com>
2024-02-27 13:49:57 +0800

    fix format

--- vllm/model_executor/layers/fused_moe/__init__.py
+++ vllm/model_executor/layers/fused_moe/__init__.py
@@ -1,8 +1,4 @@
 from vllm.model_executor.layers.fused_moe.fused_moe import (
     fused_moe, moe_align_block_size, fused_topk)
 
-__all__ = [
-    "fused_moe",
-    "moe_align_block_size",
-    "fused_topk"
-]
+__all__ = ["fused_moe", "moe_align_block_size", "fused_topk"]
