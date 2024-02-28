0ff8825a28136daba6cc4731a91e7182c18099b7
Bill Nell <bill@neuralmagic.com>
2024-02-28 18:34:27 +0000

    refactor setup.py so cmake configuration is separate from cmake build

--- setup.py
+++ setup.py
@@ -47,8 +47,93 @@ class CMakeExtension(Extension):
 
 
 class cmake_build_ext(build_ext):
-    # A flag to ensure that the cmake config step only runs once.
-    did_config = False
+    # A dict of extension directories that have been configured.
+    did_config = {}
+
+    #
+    # Determine number of compilation jobs and optionally nvcc compile threads.
+    #
+    def compute_num_jobs(self):
+        try:
+            num_jobs = len(os.sched_getaffinity(0))
+        except AttributeError:
+            num_jobs = os.cpu_count()
+
+        nvcc_cuda_version = get_nvcc_cuda_version()
+        if nvcc_cuda_version >= Version("11.2"):
+            nvcc_threads = int(os.getenv("NVCC_THREADS", 8))
+            num_jobs = max(1, round(num_jobs / (nvcc_threads / 4)))
+        else:
+            nvcc_threads = None
+
+        return num_jobs, nvcc_threads
+
+    #
+    # Perform cmake configuration for a single extension.
+    #
+    def configure(self, ext):
+        # If we've already configured using the CMakeLists.txt for
+        # this extension, exit early.
+        if ext.cmake_lists_dir in cmake_build_ext.did_config:
+            return
+
+        cmake_build_ext.did_config[ext.cmake_lists_dir] = True
+
+        # Select the build type.
+        # Note: optimization level + debug info are set by the build type
+        cfg = os.getenv("CMAKE_BUILD_TYPE", "RelWithDebInfo")
+
+        # where .so files will be written, should be the same for all extensions
+        # that use the same CMakeLists.txt.
+        outdir = os.path.abspath(
+            os.path.dirname(self.get_ext_fullpath(ext.name)))
+
+        cmake_args = [
+            '-DCMAKE_BUILD_TYPE={}'.format(cfg),
+            '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={}'.format(outdir),
+            '-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY={}'.format(self.build_temp),
+        ]
+
+        verbose = bool(int(os.getenv('VERBOSE', '0')))
+        if verbose:
+            cmake_args += ['-DCMAKE_VERBOSE_MAKEFILE=ON']
+
+        if is_sccache_available():
+            cmake_args += [
+                '-DCMAKE_CXX_COMPILER_LAUNCHER=sccache',
+                '-DCMAKE_CUDA_COMPILER_LAUNCHER=sccache',
+            ]
+        elif is_ccache_available():
+            cmake_args += [
+                '-DCMAKE_CXX_COMPILER_LAUNCHER=ccache',
+                '-DCMAKE_CUDA_COMPILER_LAUNCHER=ccache',
+            ]
+
+        # Pass the python executable to cmake so it can find an exact
+        # match.
+        cmake_args += ['-DVLLM_PYTHON_EXECUTABLE={}'.format(sys.executable)]
+
+        #
+        # Setup parallelism and build tool
+        #
+        num_jobs, nvcc_threads = self.compute_num_jobs()
+
+        if nvcc_threads:
+            cmake_args += ['-DNVCC_THREADS={}'.format(nvcc_threads)]
+
+        if is_ninja_available():
+            build_tool = ['-G', 'Ninja']
+            cmake_args += [
+                '-DCMAKE_JOB_POOL_COMPILE:STRING=compile',
+                '-DCMAKE_JOB_POOLS:STRING=compile={}'.format(num_jobs),
+            ]
+        else:
+            # Default build tool to whatever cmake picks.
+            build_tool = []
+
+        subprocess.check_call(
+            ['cmake', ext.cmake_lists_dir, *build_tool, *cmake_args],
+            cwd=self.build_temp)
 
     def build_extensions(self):
         # Ensure that CMake is present and working
@@ -57,90 +142,23 @@ class cmake_build_ext(build_ext):
         except OSError as e:
             raise RuntimeError('Cannot find CMake executable') from e
 
+        # Create build directory if it does not exist.
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+
+        # Build all the extensions
         for ext in self.extensions:
-
-            extdir = os.path.abspath(
-                os.path.dirname(self.get_ext_fullpath(ext.name)))
-
-            # Note: optimization level + debug info set by the build type
-            cfg = os.getenv("VLLM_BUILD_TYPE", "RelWithDebInfo")
-
-            cmake_args = [
-                '-DCMAKE_BUILD_TYPE=%s' % cfg,
-                # Ask CMake to place the resulting library in the directory
-                # containing the extension
-                '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(
-                    cfg.upper(), extdir),
-                # Other intermediate static libraries are placed in a
-                # temporary build directory instead
-                '-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_{}={}'.format(
-                    cfg.upper(), self.build_temp),
-            ]
-
-            verbose = bool(int(os.getenv('VERBOSE', '0')))
-            if verbose:
-                cmake_args += ['-DCMAKE_VERBOSE_MAKEFILE=ON']
-
-            if is_sccache_available():
-                cmake_args += [
-                    '-DCMAKE_CXX_COMPILER_LAUNCHER=sccache',
-                    '-DCMAKE_CUDA_COMPILER_LAUNCHER=sccache',
-                ]
-            elif is_ccache_available():
-                cmake_args += [
-                    '-DCMAKE_CXX_COMPILER_LAUNCHER=ccache',
-                    '-DCMAKE_CUDA_COMPILER_LAUNCHER=ccache',
-                ]
-
-            #
-            # Setup parallelism
-            #
-            try:
-                num_jobs = len(os.sched_getaffinity(0))
-            except AttributeError:
-                num_jobs = os.cpu_count()
-
-            nvcc_cuda_version = get_nvcc_cuda_version()
-            if nvcc_cuda_version >= Version("11.2"):
-                nvcc_threads = int(os.getenv("NVCC_THREADS", 8))
-                num_jobs = max(1, round(num_jobs / (nvcc_threads / 4)))
-                cmake_args += ['-DNVCC_THREADS={}'.format(nvcc_threads)]
-
-            if not os.path.exists(self.build_temp):
-                os.makedirs(self.build_temp)
+            self.configure(ext)
 
             ext_target_name = remove_prefix(ext.name, "vllm.")
+            num_jobs, _ = self.compute_num_jobs()
 
-            if is_ninja_available():
-                build_tool = ['-G', 'Ninja']
-                cmake_args += [
-                    '-DCMAKE_JOB_POOL_COMPILE:STRING=compile',
-                    '-DCMAKE_JOB_POOLS:STRING=compile={}'.format(num_jobs),
-                ]
-                build_jobs = []
-            else:
-                build_tool = ['-G', 'Unix Makefiles']
-                build_jobs = ['-j', str(num_jobs)]
-
-            # Pass the python executable to cmake so it can find an exact
-            # match.
-            cmake_args += [
-                '-DVLLM_PYTHON_EXECUTABLE={}'.format(sys.executable)
-            ]
-
-            # Config
-            if not cmake_build_ext.did_config:
-                cmake_build_ext.did_config = True
-                subprocess.check_call(
-                    ['cmake', ext.cmake_lists_dir, *build_tool, *cmake_args],
-                    cwd=self.build_temp)
-
-            # Build
             build_args = [
-                '--build', '.', '--config', cfg, '--target', ext_target_name
+                '--build', '.', '--target', ext_target_name, '-j',
+                str(num_jobs)
             ]
-            subprocess.check_call(['cmake', *build_args, *build_jobs],
-                                  cwd=self.build_temp)
+
+            subprocess.check_call(['cmake', *build_args], cwd=self.build_temp)
 
 
 def _is_cuda() -> bool:
