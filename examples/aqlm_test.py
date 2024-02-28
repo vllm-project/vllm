024b54ca48c5bf41dba258f0984a74d254289eea
James Fleming <james@neuralmagic.com>
2024-02-28 21:51:50 +0000

    change

--- examples/aqlm_test.py
+++ examples/aqlm_test.py
@@ -2,7 +2,6 @@ from vllm import LLM, SamplingParams
 
 #model = LLM("nm-testing/llama2.c-stories110M-pruned2.4")
 #model = LLM("BlackSamorez/Llama-2-7b-AQLM-2Bit-1x16-hf", enforce_eager=True)
-
 model = LLM("/home/jaemz/.cache/huggingface/hub/models--BlackSamorez--Llama-2-7b-AQLM-2Bit-1x16-hf/snapshots/1756949fed7fe691c00f015e2ebd18503a975f3b", enforce_eager=True)
 
 sampling_params = SamplingParams(max_tokens=200, temperature=0)
