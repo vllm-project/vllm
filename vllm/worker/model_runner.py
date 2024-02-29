6032c9c24bf49c39511189c012bcf53f157f9c6f
mgoin <michael@neuralmagic.com>
2024-02-29 19:26:51 +0000

    Format

--- vllm/worker/model_runner.py
+++ vllm/worker/model_runner.py
@@ -87,10 +87,10 @@ class ModelRunner:
     def load_model(self) -> None:
         with measure_cuda_memory() as m:
             self.model = get_model(self.model_config,
-                               self.device_config,
-                               lora_config=self.lora_config,
-                               parallel_config=self.parallel_config,
-                               scheduler_config=self.scheduler_config)
+                                   self.device_config,
+                                   lora_config=self.lora_config,
+                                   parallel_config=self.parallel_config,
+                                   scheduler_config=self.scheduler_config)
 
         self.model_memory_usage = m.consumed_memory
         logger.info(
