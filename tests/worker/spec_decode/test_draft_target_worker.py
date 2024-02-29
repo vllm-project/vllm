bec5cbabda06a8c4bfc61f941b6bd0c950e56282
Cade Daniel <edacih@gmail.com>
2024-02-28 23:00:21 -0800

    test

--- tests/worker/spec_decode/test_draft_target_worker.py
+++ tests/worker/spec_decode/test_draft_target_worker.py
@@ -1,14 +1,3 @@
-# This library may only be used in the Anyscale Platform.
-# Notwithstanding the terms of any license or notice within this container,
-# you may not modify, copy or remove this file.
-# Your right to use this library is subject to the
-# Anyscale Terms of Service (anyscale.com/terms)
-# or other written agreement between you and Anyscale.
-
-# Copyright (2023 and onwards) Anyscale, Inc.
-# This Software includes software developed at Anyscale (anyscale.com/)
-# and its use is subject to the included LICENSE file.
-
 import torch
 import random
 import pytest
