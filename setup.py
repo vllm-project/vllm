17f6685170928bcca7944c4463915746e9ba2861
Billy Cao <aliencaocao@gmail.com>
2024-03-01 03:13:58 +0800

    Fix building from source on WSL (#3112)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def _is_neuron() -> bool:
     torch_neuronx_installed = True
     try:
         subprocess.run(["neuron-ls"], capture_output=True, check=True)
-    except FileNotFoundError:
+    except (FileNotFoundError, PermissionError):
         torch_neuronx_installed = False
     return torch_neuronx_installed
 
