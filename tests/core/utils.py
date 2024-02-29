6273f7471d91ca918a1c9a476d94ed797ea21dea
sang <rkooo567@gmail.com>
2024-02-29 01:40:54 -0800

    ip

---
+++ tests/core/utils.py
@@ -0,0 +1,39 @@
+# This library may only be used in the Anyscale Platform.
+# Notwithstanding the terms of any license or notice within this container,
+# you may not modify, copy or remove this file.
+# Your right to use this library is subject to the
+# Anyscale Terms of Service (anyscale.com/terms)
+# or other written agreement between you and Anyscale.
+
+# Copyright (2023 and onwards) Anyscale, Inc.
+# This Software includes software developed at Anyscale (anyscale.com/)
+# and its use is subject to the included LICENSE file.
+
+import time
+from typing import Tuple
+
+from vllm import SamplingParams
+from vllm.sequence import Sequence, SequenceGroup
+
+
+def create_dummy_prompt(
+        request_id: str,
+        prompt_length: int,
+        block_size: int = None,
+        num_processed_token_ids: int = 0) -> Tuple[Sequence, SequenceGroup]:
+    if not block_size:
+        block_size = prompt_length
+
+    # Create dummy prompt sequence with tokens 0...block_size-1
+    # and prompt "0 ... block_size".
+    prompt_tokens = list(range(prompt_length))
+    prompt_str = " ".join([str(t) for t in prompt_tokens])
+    prompt = Sequence(int(request_id),
+                      prompt_str,
+                      prompt_tokens,
+                      block_size,
+                      num_processed_token_ids=num_processed_token_ids)
+    seq_group = SequenceGroup(request_id, [prompt], SamplingParams(),
+                              time.time(), time.perf_counter())
+
+    return prompt, seq_group
