e17a29d6dfa9f8550bcc21ba894428568154a34e
sang <rkooo567@gmail.com>
2024-02-29 02:20:32 -0800

    upstream tests.

--- tests/core/test_block_manager.py
+++ tests/core/test_block_manager.py
@@ -1,14 +1,3 @@
-# This library may only be used in the Anyscale Platform.
-# Notwithstanding the terms of any license or notice within this container,
-# you may not modify, copy or remove this file.
-# Your right to use this library is subject to the
-# Anyscale Terms of Service (anyscale.com/terms)
-# or other written agreement between you and Anyscale.
-
-# Copyright (2023 and onwards) Anyscale, Inc.
-# This Software includes software developed at Anyscale (anyscale.com/)
-# and its use is subject to the included LICENSE file.
-
 import pytest
 import time
 from typing import List
@@ -17,8 +6,7 @@ from vllm import SamplingParams
 from vllm.block import PhysicalTokenBlock
 from vllm.core.block_manager import BlockAllocator, BlockSpaceManager, AllocStatus
 from vllm.utils import Device
-from vllm.sequence import SampleLogprob, Sequence, SequenceGroup, SequenceStatus
-from tests.utils import round_up_to_next_block
+from vllm.sequence import Sequence, SequenceGroup, SequenceStatus
 
 from .utils import create_dummy_prompt
 
@@ -109,70 +97,22 @@ def test_append_slot_single_seq():
     block_manager.allocate(seq_group)
 
     # Nothing to append. Sequence has no new logical blocks.
-    assert block_manager.can_append_slots(seq_group)
+    assert block_manager.can_append_slot(seq_group)
     before_blocks = block_manager.get_num_free_gpu_blocks()
-    assert not block_manager.append_slots(prompt)
+    assert not block_manager.append_slot(prompt)
     after_blocks = block_manager.get_num_free_gpu_blocks()
     assert before_blocks == after_blocks
 
     # Add block_size number of new tokens and append slot.
     for i in range(block_size):
         token_id = i + 5
-        prompt.append_token_id(token_id, {token_id: SampleLogprob(0.0)})
+        prompt.append_token_id(token_id, {token_id: 0.0})
 
-    assert block_manager.can_append_slots(seq_group)
+    assert block_manager.can_append_slot(seq_group)
     before_blocks = block_manager.get_num_free_gpu_blocks()
-    assert not block_manager.append_slots(prompt)
+    assert not block_manager.append_slot(prompt)
     after_blocks = block_manager.get_num_free_gpu_blocks()
     assert before_blocks - after_blocks == 1
-
-
-@pytest.mark.parametrize("prompt_len", [1, 10, 100])
-@pytest.mark.parametrize("num_unprocessed_tokens", [1, 10, 100])
-@pytest.mark.parametrize("block_size", [1, 8, 16, 32])
-def test_append_multiple_slot_single_seq(prompt_len: int,
-                                         num_unprocessed_tokens: int,
-                                         block_size: int):
-    """Verify correct allocation when multiple tokens need to be processed.
-    """
-    num_cpu_blocks = 0
-    num_gpu_blocks = 8192 // block_size
-    block_manager = BlockSpaceManager(block_size=block_size,
-                                      num_gpu_blocks=num_gpu_blocks,
-                                      num_cpu_blocks=num_cpu_blocks,
-                                      watermark=0)
-
-    # Allocate single seq to gpu block.
-    prompt, seq_group = create_dummy_prompt(request_id="1",
-                                            prompt_length=prompt_len,
-                                            block_size=block_size)
-    block_manager.allocate(seq_group)
-
-    # Nothing to append. Sequence has no new logical blocks.
-    assert block_manager.can_append_slots(seq_group)
-    before_blocks = block_manager.get_num_free_gpu_blocks()
-    assert not block_manager.append_slots(prompt)
-    after_blocks = block_manager.get_num_free_gpu_blocks()
-    assert before_blocks == after_blocks
-
-    # Append new tokens, expect correct number of new blocks
-    new_token_ids = list(range(num_unprocessed_tokens))
-
-    prompt.append_token_ids(
-        new_token_ids,
-        [{token_id: SampleLogprob(logprob=0.0)
-          for token_id in new_token_ids}])
-
-    old_seq_len_in_blocks = round_up_to_next_block(prompt_len, block_size)
-    new_seq_len_in_blocks = round_up_to_next_block(
-        prompt_len + num_unprocessed_tokens, block_size)
-    num_expected_new_blocks = new_seq_len_in_blocks - old_seq_len_in_blocks
-
-    assert block_manager.can_append_slots(seq_group)
-    before_blocks = block_manager.get_num_free_gpu_blocks()
-    assert not block_manager.append_slots(prompt)
-    after_blocks = block_manager.get_num_free_gpu_blocks()
-    assert before_blocks - after_blocks == num_expected_new_blocks
 
 
 def test_append_slot_cow():
@@ -188,20 +128,17 @@ def test_append_slot_cow():
     prompt = Sequence(1, "one two three", [1, 2, 3], block_size)
     child = prompt.fork(2)
     token_id = 4
-    child.append_token_id(token_id, {token_id: SampleLogprob(0.0)})
+    child.append_token_id(token_id, {token_id: 0.0})
     seq_group = SequenceGroup("1", [prompt, child], SamplingParams(),
                               time.time(), time.perf_counter)
     block_manager.allocate(seq_group)
 
     # Append slot for child token.
     # Last block being modified is shared. Copy on write occurs.
-    assert block_manager.can_append_slots(seq_group)
+    assert block_manager.can_append_slot(seq_group)
     before_blocks = block_manager.get_num_free_gpu_blocks()
-    cow_src_dst = block_manager.append_slots(child)
-
-    assert len(cow_src_dst) > 0
-    for src_block, dst_block in cow_src_dst.items():
-        assert src_block != dst_block
+    src_block, dst_block = block_manager.append_slot(child)
+    assert src_block != dst_block
 
     after_blocks = block_manager.get_num_free_gpu_blocks()
     assert before_blocks - after_blocks == 1
@@ -228,8 +165,8 @@ def test_fork():
         prompt) == block_manager.get_block_table(child)
     token_id = 4
     # Append token to child. Block is shared so copy on write occurs.
-    child.append_token_id(token_id, {token_id: SampleLogprob(0.0)})
-    block_manager.append_slots(child)
+    child.append_token_id(token_id, {token_id: 0.0})
+    block_manager.append_slot(child)
     assert block_manager.get_block_table(
         prompt) != block_manager.get_block_table(child)
 
@@ -244,14 +181,15 @@ def test_swap():
                                       watermark=0)
 
     prompt, seq_group = create_dummy_prompt("1", prompt_length=block_size - 1)
-    prompt.status = SequenceStatus.RUNNING
+    prompt.status = SequenceStatus.WAITING
     block_manager.allocate(seq_group)
 
     # Emulate a forward pass by appending a single token.
     # The block manager then knows how many unprocessed
     # tokens will be written in the next forward pass.
     token_id = 0
-    prompt.append_token_id(token_id, {token_id: SampleLogprob(0.0)})
+    prompt.status = SequenceStatus.RUNNING
+    prompt.append_token_id(token_id, {token_id: 0.0})
 
     # Swap seq group from GPU -> CPU.
     gpu_blocks = block_manager.get_block_table(prompt)
