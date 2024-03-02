57753e4225bee3353fed2d194920a435520074f8
roy <jasonailu87@gmail.com>
2024-03-02 11:47:53 +0800

    fix ci

--- tests/models/test_models.py
+++ tests/models/test_models.py
@@ -18,13 +18,12 @@ MODELS = [
     "mosaicml/mpt-7b",
     "microsoft/phi-2",
     "stabilityai/stablelm-3b-4e1t",
-    "allenai/OLMo-1B",
     "bigcode/starcoder2-3b",
 ]
 
 
 @pytest.mark.parametrize("model", MODELS)
-@pytest.mark.parametrize("dtype", ["float"])
+@pytest.mark.parametrize("dtype", ["half"])
 @pytest.mark.parametrize("max_tokens", [128])
 def test_models(
     hf_runner,
