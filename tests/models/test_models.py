fce3ebefd6e472b93d68b86f6ba6d8984a49b1db
roy <jasonailu87@gmail.com>
2024-03-02 12:39:26 +0800

    set model length

--- tests/models/test_models.py
+++ tests/models/test_models.py
@@ -37,7 +37,7 @@ def test_models(
     hf_outputs = hf_model.generate_greedy(example_prompts, max_tokens)
     del hf_model
 
-    vllm_model = vllm_runner(model, dtype=dtype)
+    vllm_model = vllm_runner(model, dtype=dtype, max_model_len=2048)
     vllm_outputs = vllm_model.generate_greedy(example_prompts, max_tokens)
     del vllm_model
 
