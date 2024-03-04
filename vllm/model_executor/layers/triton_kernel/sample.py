f215c3e9bfe5eb8eca5c0467aecc1b46f9a1cc3f
Antoni Baum <antoni.baum@protonmail.com>
2024-03-04 13:26:23 -0800

    Faster kernel

--- vllm/model_executor/layers/triton_kernel/sample.py
+++ vllm/model_executor/layers/triton_kernel/sample.py
@@ -1,8 +1,11 @@
+import math
+from typing import Tuple, Optional
+
 import torch
 import triton
 import triton.language as tl
-import math
-from typing import Tuple, Optional
+
+from vllm.model_executor.layers.triton_kernel.rand import seeded_uniform
 
 _EPS = 1e-6
 
@@ -53,20 +56,28 @@ def _multi_split_sample(
                     device=probs.device) for _ in range(n_splits)
     ]
     for i in range(n_splits):
+        n_samples = sample_indices.shape[0]
+        n_cols = split_probs[i].shape[1]
+        n_best = sampled_tokens_tmp[i].shape[1]
+        uniform_noise = seeded_uniform(n_samples,
+                                       n_best,
+                                       n_cols,
+                                       seeds=seeds[i].flatten(),
+                                       device=split_probs[i].device,
+                                       dtype=split_probs[i].dtype)
         # TODO(yard1): See if we can remove the contiguous() calls.
         # Will need kernel support.
         _sample(
-            split_probs[i],
-            split_logprobs[i],
+            split_probs[i].contiguous(),
+            split_logprobs[i].contiguous(),
             sample_indices,
             sampled_tokens_tmp[i],
             sampled_logprobs_tmp[i],
             sampled_modified_probs_tmp[i],
             seeds[i],
-            modify_greedy_probs=modify_greedy_probs,
-            # Don't save logprobs in kernel, we need to gather them
-            # below
-            save_logprobs=False,
+            uniform_noise,
+            modify_greedy_probs=False,
+            save_logprobs=save_logprobs,
             save_modified_probs=True,
         )
         if i > 0:
@@ -85,12 +96,14 @@ def _multi_split_sample(
     else:
         sampled_logprobs = None
     sampled_modified_probs = sampled_modified_probs.squeeze(0)
+
     if modify_greedy_probs:
         # We need to modify the greedy probs for the sampled tokens.
         # We can't do this in the kernel as we need to know the
         # sampled tokens.
         probs.fill_(0.0)
         probs.scatter_(1, sampled_tokens, 1.0)
+
     return (sampled_tokens, sampled_logprobs, sampled_modified_probs)
 
 
@@ -186,17 +199,28 @@ def sample(
         sampled_modified_probs = torch.empty(sampled_modified_probs_size,
                                              dtype=probs.dtype,
                                              device=probs.device)
+        n_samples = sample_indices.shape[0]
+        n_cols = probs.shape[1]
+        uniform_noise = seeded_uniform(n_samples,
+                                       max_best_of,
+                                       n_cols,
+                                       seeds=seeds.flatten(),
+                                       device=probs.device,
+                                       dtype=probs.dtype)
 
-        _sample(probs,
-                logprobs,
-                sample_indices,
-                sampled_tokens,
-                sampled_logprobs,
-                sampled_modified_probs,
-                seeds,
-                modify_greedy_probs=modify_greedy_probs,
-                save_logprobs=save_logprobs,
-                save_modified_probs=_save_modified_probs)
+        _sample(
+            probs,
+            logprobs,
+            sample_indices,
+            sampled_tokens,
+            sampled_logprobs,
+            sampled_modified_probs,
+            seeds,
+            uniform_noise,
+            modify_greedy_probs=modify_greedy_probs,
+            save_logprobs=save_logprobs,
+            save_modified_probs=_save_modified_probs,
+        )
     return (sampled_tokens, sampled_logprobs if save_logprobs else None,
             sampled_modified_probs if _save_modified_probs else None)
 
@@ -208,14 +232,44 @@ def _sample(probs: torch.Tensor,
             output_logprobs: torch.Tensor,
             output_modified_probs: torch.Tensor,
             seeds: torch.Tensor,
+            uniform_noise: torch.Tensor,
             *,
             modify_greedy_probs: bool = False,
             save_logprobs: bool = True,
-            save_modified_probs: bool = False) -> None:
-    # Operates in place.
+            save_modified_probs: bool = False) -> torch.Tensor:
+    """Sample tokens from probs.
+
+    Args:
+        probs [batch_size, vocab_size]: probs to sample from.
+        logprobs [batch_size, vocab_size]: logprobs (used when
+            save_logprobsis True).
+        sample_indices [n]: Indices of the samples to use for each row of probs.
+        output_samples [n, n_best]: Output tensor to store samples in.
+        output_logprobs [n, n_best]: Output tensor to store logprobs in.
+        output_modified_probs [n, n_best]: Output tensor to store
+            probs of chosen tokens in (modified with noise).
+        seeds [n]: Seeds to use for sampling. If the seed is 0, we use
+            greedy sampling. Note this is ONLY used for determining
+            whether to use random sampling or not. The actual random
+            noise should be passed as uniform_noise.
+        uniform_noise [batch_size, n_best, vocab_size]: Uniform
+            noise to use for random sampling (will be converted
+            to exponential gumbel noise by the kernel).
+        modify_greedy_probs: If True, we modify the probs tensor in-place
+            to encode the sampling method used for each row. This is used
+            in speculative decoding. Only applies in greedy decoding.
+        save_logprobs: If True, we save the logprobs of the sampled tokens
+            in the output_logprobs tensor.
+        save_modified_probs: If True, we save the modified probs (with noise)
+            of the sampled tokens in the output_modified_probs tensor.
+            DOES NOT include the modification done by modify_greedy_probs
+            (because we want to use the unmodified probs to pick the best
+            split in case of multi-split sampling).
+    """
     n_samples = sample_indices.shape[0]
     n_cols = probs.shape[1]
     n_best = output_samples.shape[1] if len(output_samples.shape) > 1 else 1
+
     # The block size is the smallest power of two greater than the number of
     # columns in probs
     block_size = triton.next_power_of_2(n_cols)
@@ -228,9 +282,10 @@ def _sample(probs: torch.Tensor,
         num_warps = 16
     elif block_size >= 2048:
         num_warps = 8
+
     # Enqueue kernel. The 1D launch grid is simple: we have one kernel
     # instance per row of the probs matrix
-    _sample_triton_kernel[(n_samples, n_best)](
+    _sample_triton[(n_samples, n_best)](
         sample_indices,
         output_samples,
         output_logprobs,
@@ -238,8 +293,11 @@ def _sample(probs: torch.Tensor,
         probs,
         logprobs,
         seeds,
+        uniform_noise,
         output_samples.stride(0),
         probs.stride(0),
+        uniform_noise.stride(0),
+        uniform_noise.stride(1) if n_best > 1 else 1,
         n_samples,
         n_cols,
         n_best,
@@ -249,16 +307,19 @@ def _sample(probs: torch.Tensor,
         save_logprobs=save_logprobs,
         save_modified_probs=save_modified_probs,
     )
+    return output_samples, output_logprobs, output_modified_probs
 
 
 @triton.jit
-def _sample_triton_kernel(
+def _sample_triton(
         sample_indices_ptr: torch.Tensor, output_ptr: torch.Tensor,
         output_logprobs_ptr: torch.Tensor,
         output_modified_probs_ptr: torch.Tensor, probs_ptr: torch.Tensor,
-        logprobs_ptr: torch.Tensor, seed_ptr: torch.Tensor,
-        output_row_stride: int, probs_row_stride: int, n_samples: int,
-        n_cols: int, n_best: int, block_size: tl.constexpr,
+        logprobs_ptr: torch.Tensor, seeds_ptr: torch.Tensor,
+        uniform_noise_ptr: torch.Tensor, output_row_stride: int,
+        probs_row_stride: int, uniform_noise_row_stride: int,
+        uniform_noise_best_stride: int, n_samples: int, n_cols: int,
+        n_best: int, block_size: tl.constexpr,
         modify_greedy_probs: tl.constexpr, save_logprobs: tl.constexpr,
         save_modified_probs: tl.constexpr):
     # The rows are independent, so we parallelize across those
@@ -267,6 +328,8 @@ def _sample_triton_kernel(
 
     # Load the row index from DRAM
     row_idx = tl.load(sample_indices_ptr + sample_idx)
+    seed = tl.load(seeds_ptr + sample_idx)
+    uses_random_sampling = seed != 0
 
     # The stride represents how much we need to increase the
     # pointer to advance 1 row
@@ -275,25 +338,28 @@ def _sample_triton_kernel(
     # The block size is the next power of two greater than n_cols,
     # so we can fit each row in a single block
     col_offsets = tl.arange(0, block_size)
-    probs_ptrs = row_start_ptr + col_offsets
 
     # Load the row into SRAM, using a mask since block_size may be > than n_cols
-    row = tl.load(probs_ptrs, mask=col_offsets < n_cols, other=float("-inf"))
-    seed = tl.load(seed_ptr + sample_idx)
-    uses_random_sampling = seed != 0
+    row = tl.load(row_start_ptr + col_offsets,
+                  mask=col_offsets < n_cols,
+                  other=float("-inf"))
 
     if uses_random_sampling:
-        random_uniform = tl.rand(seed + best_idx, col_offsets)
+        uniform_noise_start_ptr = uniform_noise_ptr + sample_idx * uniform_noise_row_stride + best_idx * uniform_noise_best_stride
+        uniform_noise = tl.load(uniform_noise_start_ptr + col_offsets,
+                                mask=col_offsets < n_cols,
+                                other=0.5)
 
+        # NEEDS TO BE MANUALLY KEPT IN SYNC WITH vllm/tests/ops/test_sampler.py
         # tl.rand returns values in [0, 1), so we clamp lower bound
         # to _EPS to avoid log(0) and thus division by nan later
-        lb = tl.full(random_uniform.shape, _EPS, random_uniform.dtype)
-        random_uniform = tl.maximum(random_uniform, lb)
+        lb = tl.full(uniform_noise.shape, _EPS, uniform_noise.dtype)
+        uniform_noise = tl.maximum(uniform_noise, lb)
         # Use the inversion method to turn uniform samples
         # into exponential samples
-        random_exponential = -tl.log(random_uniform)
+        exponential_noise = -tl.log(uniform_noise)
 
-        row /= random_exponential
+        row /= exponential_noise
 
     sampled_value, sampled_token = tl.max(row, axis=0, return_indices=True)
     # clamp sampled token to n_cols - 1
@@ -313,7 +379,9 @@ def _sample_triton_kernel(
             # the sampling method must be encoded within the sampled
             # probability distributions.
             row = tl.where(col_offsets == sampled_token, 1.0, 0.0)
-            tl.store(probs_ptrs, row, mask=col_offsets < n_cols)
+            tl.store(row_start_ptr + col_offsets,
+                     row,
+                     mask=col_offsets < n_cols)
 
     if save_modified_probs:
         output_row_start_ptr = (output_modified_probs_ptr +
