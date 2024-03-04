ffda652002b5efd7841ab3a1c921d27e8c85e077
Ubuntu <ubuntu@ip-172-31-63-18.ec2.internal>
2024-03-04 17:48:28 +0000

    add_mistral_support

--- vllm/model_executor/models/neuron/mistral.py
+++ vllm/model_executor/models/neuron/mistral.py
@@ -30,8 +30,6 @@ from transformers import MistralConfig
 from vllm.model_executor.input_metadata import InputMetadata
 from vllm.model_executor.layers.sampler import Sampler
 from vllm.model_executor.sampling_metadata import SamplingMetadata
-from vllm.model_executor.parallel_utils.parallel_state import (
-    get_tensor_model_parallel_world_size)
 from vllm.sequence import SamplerOutput
 import os
 
