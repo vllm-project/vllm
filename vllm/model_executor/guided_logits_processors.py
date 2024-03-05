e039f433cde905570b4c1657d65b19990f324852
simon-mo <simon.mo@hey.com>
2024-03-05 23:00:14 +0000

    don't del last seq

--- vllm/model_executor/guided_logits_processors.py
+++ vllm/model_executor/guided_logits_processors.py
@@ -87,7 +87,6 @@ class BaseLogitsProcessor:
             last_seq_id = hash(tuple(input_ids[:-1]))
             self.fsm_state[seq_id] = self.fsm.next_state(
                 self.fsm_state[last_seq_id], last_token)
-            del self.fsm_state[last_seq_id]
 
         allowed_tokens = self.fsm.allowed_token_ids(self.fsm_state[seq_id])
 
