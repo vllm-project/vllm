763f90eb9b8c7cb679134ed6fe89c80d92135a2f
Tao He <sighingnow@gmail.com>
2024-03-06 11:38:02 +0800

    Fixes the misuse of time.time()/time.monotonic()
    Signed-off-by: Tao He <sighingnow@gmail.com>

--- vllm/entrypoints/openai/serving_chat.py
+++ vllm/entrypoints/openai/serving_chat.py
@@ -101,7 +101,7 @@ class OpenAIServingChat(OpenAIServing):
     ) -> Union[ErrorResponse, AsyncGenerator[str, None]]:
 
         model_name = request.model
-        created_time = int(time.monotonic())
+        created_time = int(time.time())
         chunk_object_type = "chat.completion.chunk"
         first_iteration = True
 
@@ -239,7 +239,7 @@ class OpenAIServingChat(OpenAIServing):
             request_id: str) -> Union[ErrorResponse, ChatCompletionResponse]:
 
         model_name = request.model
-        created_time = int(time.monotonic())
+        created_time = int(time.time())
         final_res: RequestOutput = None
 
         async for res in result_generator:
