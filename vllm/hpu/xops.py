e170c3aaf4608169a8161f4422020ab780371702
Mikhail Dvoretckii <mikhail.dvoretckii@intel.com>
2024-03-06 10:05:33 +0000

    Implement FSDPA-based prompt attention (#8)
    * Implement FSDPA-based prompt attention
    
    * Restore prior mask initialization in non-fused path
    
    * Update batch size inference
    
    * Remove debug code
    
    Co-authored-by: Tomasz Krupa <tomasz.krupa@intel.com>
    
    * Realign API with default implementation
    
    Also adds fallback in case of non-diagonal masking
    
    ---------
    
    Co-authored-by: Mikhail Dvoretckii <mdvoretckii@habana.ai>
    Co-authored-by: Tomasz Krupa <tomasz.krupa@intel.com>

--- vllm/hpu/xops.py
+++ vllm/hpu/xops.py
@@ -9,7 +9,13 @@ import habana_frameworks.torch as htorch
 import torch
 import torch.nn.functional as F
 from typing import List, Optional, Tuple, Union
-from .attn_bias import AttentionBias
+from .attn_bias import AttentionBias, BlockDiagonalCausalMask
+
+try:
+    from habana_frameworks.torch.hpex.kernels import FusedSDPA
+except ImportError:
+    print("Not using HPU fused scaled dot-product attention kernel.")
+    FusedSDPA = None
 
 
 def block_masked_attention(
@@ -38,30 +44,48 @@ def memory_efficient_attention_forward(
     p: float = 0.0,
     scale: Optional[float] = None,
 ) -> torch.Tensor:
+    assert attn_bias is not None, "Attention mask is required for prompt processing"
     dim = query.dim()
-    if dim == 4:
-        query, key, value = query.squeeze(0), key.squeeze(0), value.squeeze(0)
-    num_seqs = len(cu_seq_lens) - 1
-    outputs = []
-    for i in range(num_seqs):
-        start_idx = cu_seq_lens[i]
-        end_idx = cu_seq_lens[i + 1]
-        seq_len = end_idx - start_idx
-        mask_start_idx = i * seq_len
-        mask_end_idx = (i + 1) * seq_len
+    if FusedSDPA and isinstance(attn_bias, BlockDiagonalCausalMask):
+        _, _, heads, head_dim = query.shape
+        bs = len(cu_seq_lens) - 1
+        seq_len_q = query.shape[1] // bs
+        seq_len_kv = key.shape[1] // bs
+        query = query.reshape(bs, seq_len_q, heads, head_dim).permute(0, 2, 1, 3)
+        key = key.reshape(bs, seq_len_kv, heads, head_dim).permute(0, 2, 1, 3)
+        value = value.reshape(bs, seq_len_kv, heads, head_dim).permute(0, 2, 1, 3)
+        attn_mask = torch.ones((bs, heads, seq_len_q, seq_len_kv), device=query.device, dtype=torch.bool).tril()
+        import habana_frameworks.torch.hpu as ht
+        with ht.sdp_kernel(enable_recompute=False):  # (flash_attention_recompute and q_len == 1)):
+            out = FusedSDPA.apply(
+                query, key, value, attn_mask, 0.0, False, None
+            )
+        htorch.core.mark_step()
+        out = out.permute(0, 2, 1, 3).reshape(bs * seq_len_q, heads, head_dim)
+    else:
+        if dim == 4:
+            query, key, value = query.squeeze(0), key.squeeze(0), value.squeeze(0)
+        num_seqs = len(cu_seq_lens) - 1
+        outputs = []
+        for i in range(num_seqs):
+            start_idx = cu_seq_lens[i]
+            end_idx = cu_seq_lens[i + 1]
+            seq_len = end_idx - start_idx
+            mask_start_idx = i * seq_len
+            mask_end_idx = (i + 1) * seq_len
 
-        # Create attention mask.
-        attn_mask = attn_bias.materialize(device=query.device)
-        output = block_masked_attention(
-            query[start_idx:end_idx],
-            key[start_idx:end_idx],
-            value[start_idx:end_idx],
-            scale,
-            attn_mask=attn_mask[mask_start_idx:mask_end_idx,
-                                mask_start_idx:mask_end_idx],
-        )
-        outputs.append(output)
-    out = torch.cat(outputs, dim=0)
+            # Create attention mask.
+            attn_mask = attn_bias.materialize(device=query.device)
+            output = block_masked_attention(
+                query[start_idx:end_idx],
+                key[start_idx:end_idx],
+                value[start_idx:end_idx],
+                scale,
+                attn_mask=attn_mask[mask_start_idx:mask_end_idx,
+                                    mask_start_idx:mask_end_idx],
+            )
+            outputs.append(output)
+        out = torch.cat(outputs, dim=0)
     if dim == 4:
         out = out.unsqueeze(0)
     return out
