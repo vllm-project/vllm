20297f2c009fe778b1f3de85a89d0c77a0ad141b
Cade Daniel <edacih@gmail.com>
2024-03-05 18:45:22 -0800

    clean

--- vllm/worker/spec_decode/scoring.py
+++ vllm/worker/spec_decode/scoring.py
@@ -5,27 +5,17 @@ import logging
 import time
 from dataclasses import dataclass
 
-#import msgspec
 import torch
 import traceback
 
 from vllm.worker.spec_decode.metrics import SpecDecodeWorkerMetrics, AsyncMetricsCollector
-#from vllm.anyscale.shm.msgspec_shm import SharedMsgspecBufferWithEvent
-#from vllm.sequence import (SampleLogprob, SamplerOutput, SequenceGroupMetadata,
-#                           ExecuteModelData, SequenceOutputs, SequenceData,
-#                           SequenceGroupOutputs, SpecDecodeWorkerMetrics)
 from vllm.sequence import (SamplerOutput, SequenceGroupMetadata, SequenceData,
                            SequenceGroupOutput, SequenceOutput)
 from vllm.worker.worker import Worker
 from vllm.worker.spec_decode.multi_step_worker import MultiStepWorker
-#from vllm.worker.prompt_lookup_worker import PromptLookupWorker
-#from vllm.worker.single_tp_worker import SingleTpWorker
-#from vllm.model_executor.layers.sampler import sampler_output_to_torch
 from vllm.model_executor.layers.rejection_sampler import RejectionSampler
 from vllm.model_executor.parallel_utils.parallel_state import get_tensor_model_parallel_group
 from vllm.config import CacheConfig
-#from vllm.worker.base_worker import BaseWorker
-#from vllm.model_executor.layers.sampler import RawSamplerOutput
 from vllm.utils import in_wsl
 from vllm.worker.spec_decode.util import nvtx_range, sampler_output_to_torch, SpeculativeProposals, get_all_seq_ids
 from vllm.worker.spec_decode.interfaces import SpeculativeScorer
@@ -33,11 +23,6 @@ from vllm.worker.spec_decode.interfaces import SpeculativeScorer
 SeqId = int
 TargetSeqId = int
 TokenId = int
-
-logger = logging.getLogger(__name__)
-
-from abc import ABC, abstractmethod
-
 
 class BatchExpansionTop1Scorer(SpeculativeScorer):
     def __init__(self, scorer_worker: Worker, device: str, vocab_size: int):
@@ -213,7 +198,6 @@ class BatchExpansionTop1Scorer(SpeculativeScorer):
         return SequenceGroupMetadata(
             request_id=seq_group_metadata.request_id,
             is_prompt=seq_group_metadata.is_prompt,
-            #is_chunked_prefill=seq_group_metadata.is_chunked_prefill,
             seq_data={
                 target_seq_id:
                 SequenceData(
