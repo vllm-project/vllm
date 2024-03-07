a3039dd1c86cc66d0340684cb1a934682431cddb
James Fleming <james@neuralmagic.com>
2024-03-07 23:00:07 +0000

    remove comments

--- examples/aqlm_example.py
+++ examples/aqlm_example.py
@@ -20,27 +20,17 @@ def main():
 
     args = parser.parse_args()
 
-    # These are the verified working models.
     models = [
         "ISTA-DASLab/Llama-2-7b-AQLM-2Bit-1x16-hf",
         "ISTA-DASLab/Llama-2-7b-AQLM-2Bit-2x8-hf",
+        "ISTA-DASLab/Llama-2-13b-AQLM-2Bit-1x16-hf",
+        "ISTA-DASLab/Mixtral-8x7b-AQLM-2Bit-1x16-hf",
         "BlackSamorez/TinyLlama-1_1B-Chat-v1_0-AQLM-2Bit-1x16-hf",
-        "BlackSamorez/Llama-2-7b-AQLM-2Bit-1x16-hf",
-        "BlackSamorez/Llama-2-7b-AQLM-2Bit-2x8-hf"
     ]
 
     model = LLM(models[args.model],
                 enforce_eager=True,
                 tensor_parallel_size=args.tensor_parallel_size)
-
-    # this has the dimensions 0 and 1 transposed for the codes, and we don't currently support 8x8 anyway.
-    #model = LLM("BlackSamorez/Llama-2-7b-AQLM-2Bit-8x8-hf", enforce_eager=True)
-    # this model hangs, need to investigate.
-    #model = LLM("BlackSamorez/Mixtral-8x7B-Instruct-v0_1-AQLM-2Bit-1x16-hf", enforce_eager=True)
-
-    # These have custom code and no quantization_config block.
-    #model = LLM("BlackSamorez/Llama-2-13b-AQLM-2Bit-1x16-hf", enforce_eager=True, trust_remote_code=True)
-    #model = LLM("BlackSamorez/Mixtral-8x7b-AQLM-2Bit-1x16-hf", enforce_eager=True)
 
     sampling_params = SamplingParams(max_tokens=100, temperature=0)
     outputs = model.generate("Hello my name is",
