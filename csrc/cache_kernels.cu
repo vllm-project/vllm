4ffe256fd3f15e04a2de5c4aeb596c086d634d04
skrider <skrider@berkeley.edu>
2024-03-08 03:55:48 +0000

    refactor reshape_and_cache_flash

--- csrc/cache_kernels.cu
+++ csrc/cache_kernels.cu
@@ -213,8 +213,10 @@ template<typename scalar_t>
 __global__ void reshape_and_cache_flash_kernel(
   const scalar_t* __restrict__ key,           // [num_tokens, num_heads, head_size]
   const scalar_t* __restrict__ value,         // [num_tokens, num_heads, head_size]
-  scalar_t* __restrict__ kv_cache,            // [num_blocks, 2, block_size, num_heads, head_size]
+  scalar_t* __restrict__ k_cache,            // [num_blocks, block_size, num_heads, head_size]
+  scalar_t* __restrict__ v_cache,            // [num_blocks, block_size, num_heads, head_size]
   const int64_t* __restrict__ slot_mapping,   // [num_tokens]
+  const int block_stride,
   const int key_stride,
   const int value_stride,
   const int num_heads,
@@ -234,12 +236,12 @@ __global__ void reshape_and_cache_flash_kernel(
     const int64_t src_value_idx = token_idx * value_stride + i;
     const int head_idx = i / head_size;
     const int head_offset = i % head_size;
-    const int64_t tgt_value_idx = block_idx * block_size * num_heads * head_size
+    const int64_t tgt_value_idx = block_idx * block_stride
                               + block_offset * num_heads * head_size
                               + head_idx * head_size
                               + head_offset;
-    kv_cache[tgt_value_idx] = __ldg(&key[src_key_idx]);
-    kv_cache[tgt_value_idx + block_size * num_heads * head_size] = __ldg(&value[src_value_idx]);
+    k_cache[tgt_value_idx] = __ldg(&key[src_key_idx]);
+    v_cache[tgt_value_idx] = __ldg(&value[src_value_idx]);
   }
 }
 
@@ -304,7 +306,8 @@ void reshape_and_cache(
 void reshape_and_cache_flash(
   torch::Tensor& key,           // [num_tokens, num_heads, head_size]
   torch::Tensor& value,         // [num_tokens, num_heads, head_size]
-  torch::Tensor& kv_cache,      // [num_blocks, 2, block_size, num_heads, head_size]
+  torch::Tensor& k_cache,      // [num_blocks, block_size, num_heads, head_size]
+  torch::Tensor& v_cache,      // [num_blocks, block_size, num_heads, head_size]
   torch::Tensor& slot_mapping,  // [num_tokens]
   const std::string& kv_cache_dtype)
 {
@@ -314,10 +317,12 @@ void reshape_and_cache_flash(
   int num_tokens = key.size(0);
   int num_heads = key.size(1);
   int head_size = key.size(2);
-  int block_size = kv_cache.size(2);
+  int block_size = kv_cache.size(1);
 
   int key_stride = key.stride(0);
   int value_stride = value.stride(0);
+  int block_stride = k_cache.stride(0);
+  TORCH_CHECK(k_cache.stride(0) == v_cache.stride(0));
 
   dim3 grid(num_tokens);
   dim3 block(std::min(num_heads * head_size, 512));
@@ -330,8 +335,10 @@ void reshape_and_cache_flash(
       vllm::reshape_and_cache_flash_kernel<scalar_t><<<grid, block, 0, stream>>>(
         key.data_ptr<scalar_t>(),
         value.data_ptr<scalar_t>(),
-        kv_cache.data_ptr<scalar_t>(),
+        k_cache.data_ptr<scalar_t>(),
+        v_cache.data_ptr<scalar_t>(),
         slot_mapping.data_ptr<int64_t>(),
+        block_stride,
         key_stride,
         value_stride,
         num_heads,
