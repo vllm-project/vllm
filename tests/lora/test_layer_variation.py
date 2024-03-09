4543e8fb43994562f16be8cc177890d87d840303
tterrysun <terry@anyscale.com>
2024-03-08 16:14:59 -0800

    use smaller model

--- tests/lora/test_layer_variation.py
+++ tests/lora/test_layer_variation.py
@@ -10,7 +10,7 @@ import vllm
 from vllm.lora.request import LoRARequest
 from .conftest import cleanup
 
-MODEL_PATH = "meta-llama/Llama-2-7b-hf"
+MODEL_PATH = "Felladrin/Llama-68M-Chat-v1"
 PROMPTS = [
     "[system] Given a target sentence construct the underlying meaning representation\nof the input sentence as a single function with attributes and attribute\nvalues. This function should describe the target string accurately and the\nfunction must be one of the following ['inform', 'request', 'give_opinion',\n'confirm', 'verify_attribute', 'suggest', 'request_explanation',\n'recommend', 'request_attribute'].\n\nThe attributes must be one of the following:\n['name', 'exp_release_date', 'release_year', 'developer', 'esrb', 'rating',\n'genres', 'player_perspective', 'has_multiplayer', 'platforms',\n'available_on_steam', 'has_linux_release', 'has_mac_release', 'specifier'] [/system] [user] Here is the target sentence:\nSpellForce 3 is a pretty bad game. The developer Grimlore Games is clearly a bunch of no-talent hacks, and 2017 was a terrible year for games anyway. [/user] [assistant]",
     "[system] Given a target sentence construct the underlying meaning representation\nof the input sentence as a single function with attributes and attribute\nvalues. This function should describe the target string accurately and the\nfunction must be one of the following ['inform', 'request', 'give_opinion',\n'confirm', 'verify_attribute', 'suggest', 'request_explanation',\n'recommend', 'request_attribute'].\n\nThe attributes must be one of the following:\n['name', 'exp_release_date', 'release_year', 'developer', 'esrb', 'rating',\n'genres', 'player_perspective', 'has_multiplayer', 'platforms',\n'available_on_steam', 'has_linux_release', 'has_mac_release', 'specifier'] [/system] [user] Here is the target sentence:\nI wanted to like Grimlore Games' 2017 entry, but in SpellForce 3 they just didn't get anything right. [/user] [assistant]",
@@ -67,10 +67,8 @@ for length in range(2, 6):
 
 # Test the correctness when layer and rank are varied
 @pytest.mark.parametrize("tp_size", [1])
-# @pytest.mark.parametrize("target_modules", TARGET_MODULES_LIST)
-# @pytest.mark.parametrize("rank", [8, 16, 32, 64])
-@pytest.mark.parametrize("target_modules", TARGET_MODULES_LIST[0])
-@pytest.mark.parametrize("rank", [8])
+@pytest.mark.parametrize("target_modules", TARGET_MODULES_LIST)
+@pytest.mark.parametrize("rank", [8, 16, 32, 64])
 def test_layer_variation_correctness(tp_size, target_modules, rank):
     if torch.cuda.device_count() < tp_size:
         pytest.skip(f"Not enough GPUs for tensor parallelism {tp_size}")
