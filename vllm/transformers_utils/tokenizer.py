3eae0421440f7f9181f371d2bf1b95cfbaf740aa
roy <jasonailu87@gmail.com>
2024-03-10 11:13:43 +0800

    fix

--- vllm/transformers_utils/tokenizer.py
+++ vllm/transformers_utils/tokenizer.py
@@ -120,7 +120,7 @@ class TokenizerGroup:
 
     def get_lora_tokenizer(
             self,
-            lora_request: Optional[LoRARequest]) -> "PreTrainedTokenizer":
+            lora_request: Optional[LoRARequest] = None) -> "PreTrainedTokenizer":
         if not lora_request or not self.enable_lora:
             return self.tokenizer
         if lora_request.lora_int_id not in self.lora_tokenizers:
