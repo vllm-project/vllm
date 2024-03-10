c70a05984edfe6dee51a2bd7fd2f09ff02224245
roy <jasonailu87@gmail.com>
2024-03-10 11:01:32 +0800

    fix test

--- vllm/entrypoints/openai/serving_completion.py
+++ vllm/entrypoints/openai/serving_completion.py
@@ -126,7 +126,7 @@ class OpenAIServingCompletion(OpenAIServing):
             lora_request = self._maybe_get_lora(request)
             guided_decode_logit_processor = (
                 await get_guided_decoding_logits_processor(
-                    request, self.engine.get_tokenizer()))
+                    request, await self.engine.get_tokenizer()))
             if guided_decode_logit_processor is not None:
                 if sampling_params.logits_processors is None:
                     sampling_params.logits_processors = []
