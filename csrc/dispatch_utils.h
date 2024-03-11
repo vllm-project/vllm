ba87bdbc7a5d149f54f4e9016e7123401708ce07
Philipp Moritz <pcmoritz@gmail.com>
2024-03-11 15:16:03 -0700

    fix

--- csrc/dispatch_utils.h
+++ csrc/dispatch_utils.h
@@ -15,7 +15,7 @@
   AT_DISPATCH_SWITCH(                                             \
     TYPE, NAME, VLLM_DISPATCH_CASE_FLOATING_TYPES(__VA_ARGS__))
 
-#define VLLM_DISPATCH_CASE_FLOATING_TYPES_FP8(TYPE, NAME, ...)   \
+#define VLLM_DISPATCH_CASE_FLOATING_TYPES_FP8(...)               \
   AT_DISPATCH_CASE(at::ScalarType::Float, __VA_ARGS__)           \
   AT_DISPATCH_CASE(at::ScalarType::Half, __VA_ARGS__)            \
   AT_DISPATCH_CASE(at::ScalarType::BFloat16, __VA_ARGS__)        \
