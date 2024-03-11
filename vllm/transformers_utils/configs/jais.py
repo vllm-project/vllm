b4012a2135e727cbb5ae104585aebdd1417197fe
Lalit Pradhan <lalit.pradhan@core42.ai>
2024-03-11 07:27:43 +0000

    applied ruff

--- vllm/transformers_utils/configs/jais.py
+++ vllm/transformers_utils/configs/jais.py
@@ -24,32 +24,40 @@ logger = logging.get_logger(__name__)
 
 class JAISConfig(PretrainedConfig):
     """
-    This is the configuration class to store the configuration of a [`JAISModel`]. It is used to instantiate a JAIS
-    model according to the specified arguments, defining the model architecture.
+    This is the configuration class to store the configuration of a
+    [`JAISModel`]. It is used to instantiate a JAIS model according to the
+    specified arguments, defining the model architecture.
 
-    Configuration objects inherit from [`PretrainedConfig`] and can be used to control the model outputs. Read the
-    documentation from [`PretrainedConfig`] for more information.
+    Configuration objects inherit from [`PretrainedConfig`] and can be used
+    to control the model outputs. Read the documentation from
+    [`PretrainedConfig`] for more information.
 
 
     Args:
         vocab_size (`int`, *optional*, defaults to 50257):
-            Vocabulary size of the JAIS model. Defines the number of different tokens that can be represented by the
+            Vocabulary size of the JAIS model. Defines the number of different
+            tokens that can be represented by the
             `inputs_ids` passed when calling [`JAISModel`].
         n_positions (`int`, *optional*, defaults to 1024):
-            The maximum sequence length that this model might ever be used with. Typically set this to something large
-            just in case (e.g., 512 or 1024 or 2048).
+            The maximum sequence length that this model might ever be used
+            with. Typically set this to something large just in case
+            (e.g., 512 or 1024 or 2048).
         n_embd (`int`, *optional*, defaults to 768):
             Dimensionality of the embeddings and hidden states.
         n_layer (`int`, *optional*, defaults to 12):
             Number of hidden layers in the Transformer encoder.
         n_head (`int`, *optional*, defaults to 12):
-            Number of attention heads for each attention layer in the Transformer encoder.
+            Number of attention heads for each attention layer in the
+            Transformer encoder.
         n_inner (`int`, *optional*, defaults to None):
-            Dimensionality of the inner feed-forward layers. `None` will set it to 4 times n_embd
+            Dimensionality of the inner feed-forward layers. `None` will set
+            it to 4 times n_embd
         activation_function (`str`, *optional*, defaults to `"gelu"`):
-            Activation function, to be selected in the list `["relu", "silu", "gelu", "tanh", "gelu_new", "swiglu"]`.
+            Activation function, to be selected in the list
+            `["relu", "silu", "gelu", "tanh", "gelu_new", "swiglu"]`.
         resid_pdrop (`float`, *optional*, defaults to 0.1):
-            The dropout probability for all fully connected layers in the embeddings, encoder, and pooler.
+            The dropout probability for all fully connected layers in
+            the embeddings, encoder, and pooler.
         embd_pdrop (`float`, *optional*, defaults to 0.1):
             The dropout ratio for the embeddings.
         attn_pdrop (`float`, *optional*, defaults to 0.1):
@@ -57,34 +65,48 @@ class JAISConfig(PretrainedConfig):
         layer_norm_epsilon (`float`, *optional*, defaults to 1e-5):
             The epsilon to use in the layer normalization layers.
         initializer_range (`float`, *optional*, defaults to 0.02):
-            The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
+            The standard deviation of the truncated_normal_initializer for
+            initializing all weight matrices.
         scale_attn_weights (`bool`, *optional*, defaults to `True`):
             Scale attention weights by dividing by sqrt(hidden_size)..
         use_cache (`bool`, *optional*, defaults to `True`):
-            Whether or not the model should return the last key/values attentions (not used by all models).
-        scale_attn_by_inverse_layer_idx (`bool`, *optional*, defaults to `False`):
-            Whether to additionally scale attention weights by `1 / layer_idx + 1`.
+            Whether or not the model should return the last key/values
+            attentions (not used by all models).
+        scale_attn_by_inverse_layer_idx (`bool`, *optional*,
+            defaults to `False`):
+            Whether to additionally scale attention weights by
+            `1 / layer_idx + 1`.
         reorder_and_upcast_attn (`bool`, *optional*, defaults to `False`):
-            Whether to scale keys (K) prior to computing attention (dot-product) and upcast attention
-            dot-product/softmax to float() when training with mixed precision.
+            Whether to scale keys (K) prior to computing attention
+            (dot-product)
+            and upcast attention dot-product/softmax to float() when training
+            with mixed precision.
         position_embedding_type (`str`, *optional*, defaults to `"learned"`):
             Positional embedding can be either `"alibi"` or `"learned"`.
         mup_width_scale (`float`, *optional*, defaults to 1.0):
-            muP parameter to scale learning rate and initializers. Calculated as (`d_model,0 / d_model`), where
-            `d_model` is the model's width and `d_model,0` is the proxy model's width.
+            muP parameter to scale learning rate and initializers. Calculated
+            as (`d_model,0 / d_model`), where
+            `d_model` is the model's width and `d_model,0` is the proxy
+            model's width.
         mup_embeddings_scale (`float`, *optional*, defaults to 1.0):
             muP parameter to scale token and position embeddings.
         mup_output_alpha (`float`, *optional*, defaults to 1.0):
-            muP parameter to scale output logits (`output_logits_scale = mup_output_alpha * mup_width_scale`).
+            muP parameter to scale output logits
+            (`output_logits_scale = mup_output_alpha * mup_width_scale`).
         mup_scale_qk_dot_by_d (`bool`, *optional*, defaults to `False`):
-            Scale attention weights by dividing by hidden_size instead of sqrt(hidden_size). Need to set
-            scale_attn_weights to `True` as well.
+            Scale attention weights by dividing by hidden_size instead of
+            sqrt(hidden_size). Need to set scale_attn_weights to `True` as
+            well.
         alibi_scaling (`Dict`, *optional*):
-            Dictionary containing the scaling configuration for ALiBi embeddings. Currently only supports linear
-            scaling strategy. Can specify either the scaling `factor` (must be a float greater than 1) for fixed scaling
-            or `train_seq_len` for dynamic scaling on input samples with sequence length > `train_seq_len`. The expected
+            Dictionary containing the scaling configuration for ALiBi
+            embeddings. Currently only supports linear
+            scaling strategy. Can specify either the scaling `factor` (must be
+            a float greater than 1) for fixed scaling
+            or `train_seq_len` for dynamic scaling on input samples with
+            sequence length > `train_seq_len`. The expected
             formats are `{"type": strategy name, "factor": scaling factor}` or
-            `{"type": strategy name, "train_seq_len": training sequence length}`.
+            `{"type": strategy name,
+            "train_seq_len": training sequence length}`.
         architectures (`List`, *optional*, defaults to ['JAISLMHeadModel']):
             architecture names for Jais.
 
@@ -191,7 +213,8 @@ class JAISConfig(PretrainedConfig):
             or len(self.alibi_scaling) != 2
         ):
             raise ValueError(
-                "`alibi_scaling` must be a dictionary with two fields, `type` and `factor` or `type` and `train_seq_len`, "
+                "`alibi_scaling` must be a dictionary with two fields,"
+                "`type` and `factor` or `type` and `train_seq_len`, "
                 f"got {self.alibi_scaling}"
             )
         alibi_scaling_type = self.alibi_scaling.get("type", None)
@@ -199,7 +222,8 @@ class JAISConfig(PretrainedConfig):
         alibi_dynamic_scaling = self.alibi_scaling.get("train_seq_len", None)
         if alibi_scaling_type is None or alibi_scaling_type != "linear":
             raise ValueError(
-                f"`alibi_scaling`'s type field must be 'linear', got {alibi_scaling_type}"
+                f"`alibi_scaling`'s type field must be 'linear',"
+                f"got {alibi_scaling_type}"
             )
         if (
             alibi_scaling_factor is not None
@@ -207,7 +231,8 @@ class JAISConfig(PretrainedConfig):
             or alibi_scaling_factor <= 1.0
         ):
             raise ValueError(
-                f"`alibi_scaling`'s factor field must be a float > 1.0, got {alibi_scaling_factor}"
+                f"`alibi_scaling`'s factor field must be a float > 1.0,"
+                f"got {alibi_scaling_factor}"
             )
         if (
             alibi_dynamic_scaling is not None
@@ -215,5 +240,6 @@ class JAISConfig(PretrainedConfig):
             or alibi_dynamic_scaling <= 1
         ):
             raise ValueError(
-                f"`alibi_scaling`'s `train_seq_len` field must be an integer > 1, got {alibi_dynamic_scaling}"
+                f"`alibi_scaling`'s `train_seq_len` field must be an"
+                f"integer > 1, got {alibi_dynamic_scaling}"
             )
