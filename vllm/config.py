cd7db7a8503910974f86b4bcea83c31d37ba4b07
Sanger Steel <sangersteel@gmail.com>
2024-03-12 17:43:54 -0400

    fix: Fix incorrect `TensorizerArgs` import in `config.py`
    Also enforces a default float16 precision for models when serializing

--- vllm/config.py
+++ vllm/config.py
@@ -14,7 +14,7 @@ from vllm.utils import get_cpu_memory, get_nvcc_cuda_version, is_hip, is_neuron
 
 if TYPE_CHECKING:
     from ray.util.placement_group import PlacementGroup
-    from vllm.model_executor.tensorizer_loading import TensorizerArgs
+    from vllm.model_executor.tensorizer_loader import TensorizerArgs
 
 logger = init_logger(__name__)
 
@@ -85,7 +85,7 @@ class ModelConfig:
         quantization: Optional[str] = None,
         enforce_eager: bool = False,
         max_context_len_to_capture: Optional[int] = None,
-        tensorizer_args: Optional[TensorizerArgs] = None,
+        tensorizer_args: Optional["TensorizerArgs"] = None,
         max_logprobs: int = 5,
     ) -> None:
         self.model = model
