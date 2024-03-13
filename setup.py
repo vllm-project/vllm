a487ecfeebe10870de1903cf6a6e33eb2e0ec795
陈序 <chenxu2048@gmail.com>
2024-03-14 00:43:24 +0800

    [Minor Fix] Use cupy-cuda11x in CUDA 11.8 build (#3256)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- setup.py
+++ setup.py
@@ -431,6 +431,12 @@ def get_requirements() -> List[str]:
     else:
         with open(get_path("requirements.txt")) as f:
             requirements = f.read().strip().split("\n")
+        if nvcc_cuda_version <= Version("11.8"):
+            # replace cupy-cuda12x with cupy-cuda11x for cuda 11.x
+            for i in range(len(requirements)):
+                if requirements[i].startswith("cupy-cuda12x"):
+                    requirements[i] = "cupy-cuda11x"
+                    break
     return requirements
 
 
