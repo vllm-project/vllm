db8f29c636e3ad290001f1ab1c296f1ee1ee980d
Matthew Wong <Matthew.Wong2@amd.com>
2024-03-15 00:05:54 +0000

    Fix scales_path location

--- vllm/engine/arg_utils.py
+++ vllm/engine/arg_utils.py
@@ -321,8 +321,8 @@ class EngineArgs:
             self.load_format, self.dtype, self.seed,
             self.revision, self.code_revision,
             self.tokenizer_revision, self.max_model_len, self.quantization,
-            self.enforce_eager, self.max_context_len_to_capture,
-            self.scales_path, self.max_logprobs)
+            self.scales_path, self.enforce_eager,
+            self.max_context_len_to_capture, self.max_logprobs)
         cache_config = CacheConfig(self.block_size,
                                    self.gpu_memory_utilization,
                                    self.swap_space, self.kv_cache_dtype,
