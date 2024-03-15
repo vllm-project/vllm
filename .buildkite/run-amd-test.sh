cb7989a340ea9ae0cab561e12d2c48108438eb70
simon-mo <simon.mo@hey.com>
2024-03-15 16:07:35 -0700

    fix docker command

--- .buildkite/run-amd-test.sh
+++ .buildkite/run-amd-test.sh
@@ -8,6 +8,6 @@ trap remove_docker_container EXIT
 
 remove_docker_container
 
-docker run -d --network host -n rocm rocm
+docker run --detached --network host --name rocm rocm
 while [ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8000)" != "200" ]; do sleep 1; done
 python examples/openai_completion_client.py
