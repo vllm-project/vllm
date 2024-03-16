fbc07a9bff92533638e1f51ae7dbf5972bc92ea1
simon-mo <simon.mo@hey.com>
2024-03-15 17:28:44 -0700

    use rocm docker

--- .buildkite/run-amd-test.sh
+++ .buildkite/run-amd-test.sh
@@ -10,6 +10,18 @@ trap remove_docker_container EXIT
 
 remove_docker_container
 
-docker run --gpus all --network host --name rocm rocm python3 -m vllm.entrypoints.api_server &
-while [ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8000)" != "200" ]; do sleep 1; done
+docker run --device /dev/kfd --device /dev/dri --network host --name rocm rocm python3 -m vllm.entrypoints.api_server &
+
+timeout=300
+counter=0
+
+while [ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8000)" != "200" ]; do
+    sleep 1
+    counter=$((counter+1))
+    if [ $counter -ge $timeout ]; then
+        echo "Timeout after $timeout seconds"
+        break
+    fi
+done
+
 python examples/api_client.py
