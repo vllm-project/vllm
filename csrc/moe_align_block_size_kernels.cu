6099b128de14bb406a366a25f44dc4b9999f4dc7
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2024-03-18 11:26:24 -0700

    [Bugfix] Make moe_align_block_size AMD-compatible (#3470)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- csrc/moe_align_block_size_kernels.cu
+++ csrc/moe_align_block_size_kernels.cu
@@ -111,7 +111,8 @@ void moe_align_block_size(
 
         // set dynamic shared mem
         auto kernel = vllm::moe_align_block_size_kernel<scalar_t>;
-        AT_CUDA_CHECK(cudaFuncSetAttribute(kernel, cudaFuncAttributeMaxDynamicSharedMemorySize, shared_mem));
+        AT_CUDA_CHECK(
+            VLLM_DevFuncAttribute_SET_MaxDynamicSharedMemorySize((void *)kernel, shared_mem));
         kernel<<<1, num_experts, shared_mem, stream>>>(
             topk_ids.data_ptr<scalar_t>(),
             sorted_token_ids.data_ptr<int32_t>(), 
