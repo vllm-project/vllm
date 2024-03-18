ad68ff574a4d90ed1961fffd03ee947da8532eac
Sanger Steel <sangersteel@gmail.com>
2024-03-18 14:41:20 -0400

    chore: Allow env var or CLI arg specification for S3 credentials

--- vllm/model_executor/tensorizer_loader.py
+++ vllm/model_executor/tensorizer_loader.py
@@ -91,12 +91,13 @@ class TensorizerArgs:
 
     def __post_init__(self):
         self.file_obj = self.tensorizer_uri
-        self.s3_access_key_id = self.s3_access_key_id or os.environ.get(
-            "S3_ACCESS_KEY_ID")
-        self.s3_secret_access_key = self.s3_secret_access_key or os.environ.get(
-            "S3_SECRET_ACCESS_KEY")
-        self.s3_endpoint = self.s3_endpoint or os.environ.get(
-            "S3_ENDPOINT_URL")
+        self.s3_access_key_id = (self.s3_access_key_id or os.environ.get(
+            "S3_ACCESS_KEY_ID")) or None
+        self.s3_secret_access_key = (self.s3_secret_access_key or
+                                     os.environ.get(
+            "S3_SECRET_ACCESS_KEY")) or None
+        self.s3_endpoint = (self.s3_endpoint or os.environ.get(
+            "S3_ENDPOINT_URL")) or None
         self.stream_params = {
             "s3_access_key_id": self.s3_access_key_id,
             "s3_secret_access_key": self.s3_secret_access_key,
