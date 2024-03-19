1fe4cbb2ef2320e69bd7a010b2ff436340ed2ba8
Cade Daniel <edacih@gmail.com>
2024-03-19 00:48:14 -0700

    wip

--- tests/test_block2.py
+++ tests/test_block2.py
@@ -2,9 +2,13 @@ import random
 import pytest
 from typing import Optional, List
 import random
+from unittest.mock import MagicMock
+import math
 
 from vllm.block2 import NaiveBlockAllocator, NaiveBlock, BlockAllocator, Block
 from vllm.block2 import RefCounter
+from vllm.block2 import PrefixCachingBlock
+
 
 class TestRefCounter:
 
@@ -97,3 +101,135 @@ class TestNaiveBlockAllocator:
                 oom_block = allocate_block()
 
             block_to_free = new_block
+
+class TestPrefixCachingBlock:
+
+    @staticmethod
+    @pytest.mark.parametrize("seed", list(range(10)))
+    @pytest.mark.parametrize("block_size", [1, 16])
+    @pytest.mark.parametrize("is_curr_block_full", [True, False])
+    def test_first_block_has_correct_content_hash(seed: int, block_size: int,
+                                                  is_curr_block_full: bool):
+        """Verify a block which is first in the sequence has the correct hash.
+        """
+        random.seed(seed)
+        num_to_fill = block_size if is_curr_block_full else random.randint(
+            0, block_size - 1)
+        token_ids = list(range(num_to_fill))
+    
+        block_with_prev = PrefixCachingBlock(prev_block=None, token_ids=token_ids, block_size=block_size)
+    
+        if is_curr_block_full:
+            # Expect hash since block is full.
+            assert block_with_prev.content_hash == PrefixCachingBlock.hash_block_tokens(is_first_block=True,
+                                                    prev_block_hash=None,
+                                                    cur_block_token_ids=token_ids)
+        else:
+            # Do not expect hash since block is not full.
+            assert block_with_prev.content_hash is None
+
+    @staticmethod
+    @pytest.mark.parametrize("seed", list(range(10)))
+    @pytest.mark.parametrize("block_size", [1, 16])
+    @pytest.mark.parametrize("is_curr_block_full", [True, False])
+    @pytest.mark.parametrize("prev_block_has_hash", [True, False])
+    def test_nth_block_has_correct_content_hash(seed: int, block_size: int,
+                                                is_curr_block_full: bool,
+                                                prev_block_has_hash: bool):
+        """Verify a block which is not first in the sequence has the correct hash.
+        """
+
+        random.seed(seed)
+    
+        previous_block = MagicMock(spec=PrefixCachingBlock)
+        prev_block_hash = random.randint(0, 1000)
+        previous_block.content_hash = (
+            prev_block_hash if prev_block_has_hash else None)
+
+        num_to_fill = block_size if is_curr_block_full else random.randint(
+            0, block_size - 1)
+        token_ids = list(range(num_to_fill))
+    
+        block_with_prev = PrefixCachingBlock(prev_block=previous_block,
+                                            token_ids=token_ids,
+                                            block_size=block_size,
+                                            )
+    
+    
+        if is_curr_block_full and prev_block_has_hash:
+            # Expect hash since block is full and previous block has hash.
+            assert block_with_prev.content_hash == PrefixCachingBlock.hash_block_tokens(
+                is_first_block=False,
+                prev_block_hash=prev_block_hash,
+                cur_block_token_ids=token_ids)
+        else:
+            # Do not expect hash since block is not full or the previous block
+            # does not have a hash.
+            assert block_with_prev.content_hash is None
+
+    @staticmethod
+    @pytest.mark.parametrize("block_size", [1, 2, 16])
+    @pytest.mark.parametrize("num_tokens", list(range(3)))
+    @pytest.mark.parametrize("num_empty_trailing_blocks", [0, 1, 10])
+    def test_blocks_have_correct_hash_in_chain(block_size: int, num_tokens: int,
+                                               num_empty_trailing_blocks: int):
+        """Create two chains of logical blocks with the same contents.
+        Assert the hashes are equal.
+        """
+        random.seed(0)
+    
+        token_ids = [random.randint(0, 50_000) for _ in range(num_tokens)]
+    
+        first_chain, second_chain = [
+            TestPrefixCachingBlock.create_chain(block_size=block_size,
+                         token_ids=token_ids,
+                         num_empty_trailing_blocks=num_empty_trailing_blocks)
+            for _ in range(2)
+        ]
+    
+        for first_chain_block, second_chain_block in zip(first_chain,
+                                                         second_chain):
+            assert first_chain_block.content_hash == second_chain_block.content_hash
+    
+        if not first_chain or not second_chain:
+            assert first_chain == second_chain
+            assert num_tokens == 0
+
+    @staticmethod
+    def create_chain(block_size: int,
+                     token_ids: List[int],
+                     num_empty_trailing_blocks=0) -> List[PrefixCachingBlock]:
+        """Helper method which creates a chain of blocks.
+        """
+        blocks = []
+        num_blocks = math.ceil(
+            len(token_ids) / block_size) + num_empty_trailing_blocks
+    
+        if num_blocks == 0:
+            return []
+    
+        prev_block = None
+        for block_number in range(0, num_blocks):
+            prev_block = PrefixCachingBlock(
+                                           prev_block=prev_block,
+                                           token_ids=[],
+                                           block_size=block_size,
+                                           )
+    
+            tokens_to_append = token_ids[block_number *
+                                         block_size:(block_number + 1) *
+                                         block_size]
+            if tokens_to_append:
+                prev_block.append_token_ids(tokens_to_append)
+    
+            blocks.append(prev_block)
+    
+        return blocks
+
+#class TestPrefixCachingBlockAllocator:
+#    
+#    @staticmethod
+#    def test_allocate_with_cache():
+#        allocator = PrefixCachingBlockAllocator()
+#
+#
--- vllm/block2.py
+++ vllm/block2.py
@@ -160,7 +160,6 @@ class NaiveBlockAllocator(BlockAllocator):
         refcount = self._refcounter.decr(block_index)
         if refcount == 0:
             self._free_block_indices.add(block_index)
-            
 
     def _allocate_new_block(self):
         if not self._free_block_indices:
@@ -171,60 +170,141 @@ class NaiveBlockAllocator(BlockAllocator):
         self._free_block_indices.remove(block_index)
         return block_index
 
+    @property
+    def refcounter(self):
+        return self._refcounter
     
 
-#class PrefixCachingBlock(Block):
-#    def __init__(self, prev_block: Block, token_ids: List[int]):
-#        self._token_ids = token_ids[:]
-#        self._prev_block = prev_block
-#
-#    def append_token_ids(self, token_ids: List[int]) -> None:
-#        pass
-#
-#    @property
-#    def physical_block_index(self) -> Optional[int]:
-#        pass
-#
-#    @physical_block_index.setter
-#    def physical_block_index(self) -> None:
-#        pass
-#
-#    @property
-#    def content_hash(self) -> Optional[int]:
-#        pass
-#
-#
-#class PrefixCachingBlockAllocator(BlockAllocator):
-#    PrefixHash = int
-#    BlockIndex = int
-#    
-#    def __init__(self):
-#        #self._mutable_block_allocator = NaiveBlockAllocator()
-#        #self._cached_blocks: Dict[int, Block]
-#        self._cached_blocks: Dict[PrefixHash, BlockIndex] = {}
-#        self._refcounter: Dict[int, int] = {}
-#    
-#    def allocate_mutable(self, prev_block: Block) -> Block:
-#        """Look in freelist. If found, return.
-#        Else, look in cachelist (refcount==0). If found, return.
-#
-#        Otherwise, raise :(
-#        """
-#        pass
-#
-#    def allocate_immutable(self, prev_block: Block, token_ids: List[int]) -> Block:
-#        assert isinstance(prev_block, PrefixCachingBlock)
-#
-#        block = PrefixCachingBlock(prev_block=prev_block, token_ids=token_ids)
-#        assert block.content_hash is not None
-#
-#        cached_block_index = self._cached_blocks.get(block.content_hash, default=None)
-#        if cached_block_index is not None:
-#            block.physical_block_index = cached_block_index
-#            self._refcounter[block.physical_block_index] += 1
-#            return block
-#        
-#        # Do same logic as allocate_mutable; look in freelist, else look in weakref freelist.
-# 
-#    def free(self, block: Block) -> None:
-#        pass
+class PrefixCachingBlock(Block):
+    def __init__(
+        self,
+        prev_block: Optional["PrefixCachingBlock"],
+        token_ids: List[int],
+        block_size: int,
+    ):
+        self._prev_block = prev_block
+        self._token_ids = token_ids[:]
+        self._block_size = block_size
+        self._cached_content_hash: Optional[int] = None
+
+        if self._prev_block is not None:
+            assert isinstance(self._prev_block, PrefixCachingBlock)
+
+    def append_token_ids(self, token_ids: List[int]) -> None:
+        assert len(self._token_ids) + len(token_ids) <= self._block_size
+        self._token_ids.extend(token_ids)
+
+    @property
+    def physical_block_index(self) -> Optional[int]:
+        pass
+
+    @physical_block_index.setter
+    def physical_block_index(self) -> None:
+        pass
+
+    def is_full(self) -> bool:
+        return len(self._token_ids) == self._block_size
+
+    @property
+    def content_hash(self) -> Optional[int]:
+        """Return the content-based hash of the current block, or None if it is
+        not yet defined.
+
+        For the content-based hash to be defined, the current block must be
+        full.
+        """
+
+        # If the hash is already computed, return it.
+        if self._cached_content_hash is not None:
+            return self._cached_content_hash
+
+        # We cannot compute a hash for the current block because it is not full.
+        if not self.is_full():
+            return None
+
+        is_first_block = self._prev_block is None
+        prev_block_hash = (None if is_first_block else self._prev_block.content_hash)
+
+        # Previous block exists but does not yet have a hash.
+        # Return no hash in this case.
+        if prev_block_hash is None and not is_first_block:
+            return None
+
+        self._cached_content_hash = PrefixCachingBlock.hash_block_tokens(
+            is_first_block,
+            prev_block_hash,
+            cur_block_token_ids=self._token_ids)
+        return self._cached_content_hash
+
+    @staticmethod
+    def hash_block_tokens(is_first_block: bool, prev_block_hash: Optional[int], cur_block_token_ids) -> int:
+        """Computes a hash value corresponding to the contents of a block and
+        the contents of the preceding block(s). The hash value is used for
+        prefix caching.
+
+        NOTE: Content-based hashing does not yet support LoRA.
+
+        Parameters:
+        - is_first_block (bool): A flag indicating if the block is the first in
+            the sequence.
+        - prev_block_hash (Optional[int]): The hash of the previous block. None
+            if this is the first block.
+        - cur_block_token_ids (List[int]): A list of token ids in the current
+            block. The current block is assumed to be full.
+
+        Returns:
+        - int: The computed hash value for the block.
+        """
+        assert (prev_block_hash is None) == is_first_block
+        return hash((is_first_block, prev_block_hash, *cur_block_token_ids)) 
+
+
+class PrefixCachingBlockAllocator(BlockAllocator):
+    PrefixHash = int
+    BlockIndex = int
+    
+    def __init__(self, num_blocks: int, block_size: int):
+        #self._mutable_block_allocator = NaiveBlockAllocator()
+        #self._cached_blocks: Dict[int, Block]
+        self._cached_blocks: Dict[PrefixHash, BlockIndex] = {}
+
+        self._hashless_allocator = NaiveBlockAllocator(
+            block_cls=PrefixCachingBlock,
+            num_blocks=num_blocks,
+            block_size=block_size,
+        )
+
+        self._refcounter = self._hashless_allocator.refcounter
+    
+    def allocate_mutable(self, prev_block: Block) -> Block:
+        """Look in freelist. If found, return.
+        Else, look in cachelist (refcount==0). If found, return.
+
+        Otherwise, raise :(
+        """
+        pass
+
+    def allocate_immutable(self, prev_block: Block, token_ids: List[int]) -> Block:
+        assert isinstance(prev_block, PrefixCachingBlock)
+
+        block = PrefixCachingBlock(prev_block=prev_block, token_ids=token_ids)
+        assert block.content_hash is not None
+
+        cached_block_index = self._cached_blocks.get(block.content_hash, default=None)
+        if cached_block_index is not None:
+            block.physical_block_index = cached_block_index
+            self._refcounter.incr(block.physical_block_index)
+            return block
+
+        try:
+            mutable_block = self._hashless_allocator.allocate_mutable(prev_block=prev_block)
+            block.physical_block_index = mutable_block.physical_block_index
+            return block
+        except BlockAllocator.NoFreeBlocksError:
+            pass
+
+        # TODO: weakref
+        raise NotImplementedError
+ 
+    def free(self, block: Block) -> None:
+        pass
