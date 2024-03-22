239c0a94dbf887a34c30e79bd0afd1104d534f87
Xiaowei Jiang <xwjiang2010@gmail.com>
2024-03-22 13:52:37 -0700

    fix

--- .buildkite/download-images.sh
+++ .buildkite/download-images.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 set -e
-mkdir -p ../tests/images/
+
 # Missing AWS credentials
+# mkdir -p ../tests/images/
 # aws s3 sync s3://air-example-data-2/vllm_opensource_llava/ ../tests/images/
+mkdir -p images
+cd images
 wget https://air-example-data-2.s3.us-west-2.amazonaws.com/vllm_opensource_llava/stop_sign_pixel_values.pt
 wget https://air-example-data-2.s3.us-west-2.amazonaws.com/vllm_opensource_llava/stop_sign_image_features.pt
 wget https://air-example-data-2.s3.us-west-2.amazonaws.com/vllm_opensource_llava/cherry_blossom_pixel_values.pt
