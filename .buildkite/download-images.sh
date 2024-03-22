908798aabadd61049ecd34bb0c14027b96cd4c29
Xiaowei Jiang <xwjiang2010@gmail.com>
2024-03-22 14:30:55 -0700

    fix

--- .buildkite/download-images.sh
+++ .buildkite/download-images.sh
@@ -5,6 +5,7 @@ set -e
 # Missing AWS credentials
 # mkdir -p ../tests/images/
 # aws s3 sync s3://air-example-data-2/vllm_opensource_llava/ ../tests/images/
+cd ../tests
 mkdir -p images
 cd images
 wget https://air-example-data-2.s3.us-west-2.amazonaws.com/vllm_opensource_llava/stop_sign_pixel_values.pt
