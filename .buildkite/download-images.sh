32a6e3fff294413276bc8b3521e1d4ceb90fecbc
Xiaowei Jiang <xwjiang2010@gmail.com>
2024-03-22 13:13:47 -0700

    working dir

--- .buildkite/download-images.sh
+++ .buildkite/download-images.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 set -e
-mkdir -p images/
-aws s3 sync s3://air-example-data-2/vllm_opensource_llava/ images/
+mkdir -p ../tests/images/
+aws s3 sync s3://air-example-data-2/vllm_opensource_llava/ ../tests/images/
