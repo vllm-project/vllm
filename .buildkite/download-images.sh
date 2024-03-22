c3ca81036d32bcf76ba23be92d4f4352a7eaa30a
Xiaowei Jiang <xwjiang2010@gmail.com>
2024-03-22 15:15:56 -0700

    install wget

--- .buildkite/download-images.sh
+++ .buildkite/download-images.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-set -e
+set -ex
+set -o pipefail
+
+# cd into parent directory of this file
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
+
+(which wget && which curl) || (apt-get update && apt-get install -y wget curl)
 
 # Missing AWS credentials
 # mkdir -p ../tests/images/
 # aws s3 sync s3://air-example-data-2/vllm_opensource_llava/ ../tests/images/
-cd ../tests
+cd tests
 mkdir -p images
 cd images
 wget https://air-example-data-2.s3.us-west-2.amazonaws.com/vllm_opensource_llava/stop_sign_pixel_values.pt
