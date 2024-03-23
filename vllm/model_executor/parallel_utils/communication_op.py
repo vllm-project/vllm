68e4792739b418dbaa00117f8b04a17b379e66cd
youkaichao <youkaichao@126.com>
2024-03-22 22:42:45 -0700

    rename cupy --> pynccl

--- vllm/model_executor/parallel_utils/communication_op.py
+++ vllm/model_executor/parallel_utils/communication_op.py
@@ -9,7 +9,7 @@ from vllm.model_executor.parallel_utils.parallel_state import (
     get_tensor_model_parallel_rank,
     get_tensor_model_parallel_world_size,
     get_tensor_model_parallel_group,
-    is_cupy_nccl_enabled_for_all_reduce,
+    is_pynccl_enabled_for_all_reduce,
 )
 from vllm.model_executor.parallel_utils.custom_all_reduce import (
     custom_all_reduce)
@@ -33,7 +33,7 @@ def tensor_model_parallel_all_reduce(input_: torch.Tensor) -> torch.Tensor:
     out = custom_all_reduce(input_)
     if out is not None:
         return out
-    if is_cupy_nccl_enabled_for_all_reduce():
+    if is_pynccl_enabled_for_all_reduce():
         # TODO: support multiple parallel groups.
         pynccl_utils.all_reduce(input_)
     else:
