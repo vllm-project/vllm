1fb1eac953f2ad593fb3219303903be1fdb96d9f
Xiaowei Jiang <xwjiang2010@gmail.com>
2024-03-22 17:44:13 -0700

    up
    Signed-off-by: Xiaowei Jiang <xwjiang2010@gmail.com>

--- .buildkite/download-images.sh
+++ .buildkite/download-images.sh
@@ -3,15 +3,9 @@
 set -ex
 set -o pipefail
 
-# cd into parent directory of this file
-cd "$(dirname "${BASH_SOURCE[0]}")/.."
-
 (which wget && which curl) || (apt-get update && apt-get install -y wget curl)
 
-# Missing AWS credentials
-# mkdir -p ../tests/images/
-# aws s3 sync s3://air-example-data-2/vllm_opensource_llava/ ../tests/images/
-cd tests
+# aws s3 sync s3://air-example-data-2/vllm_opensource_llava/ images/
 mkdir -p images
 cd images
 wget https://air-example-data-2.s3.us-west-2.amazonaws.com/vllm_opensource_llava/stop_sign_pixel_values.pt
@@ -20,3 +14,5 @@ wget https://air-example-data-2.s3.us-west-2.amazonaws.com/vllm_opensource_llava
 wget https://air-example-data-2.s3.us-west-2.amazonaws.com/vllm_opensource_llava/cherry_blossom_image_features.pt
 wget https://air-example-data-2.s3.us-west-2.amazonaws.com/vllm_opensource_llava/stop_sign.jpg
 wget https://air-example-data-2.s3.us-west-2.amazonaws.com/vllm_opensource_llava/cherry_blossom.jpg
+
+cd -
