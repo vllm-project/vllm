843cb1676c5034f81982add6bb8e719aa853e536
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2024-03-24 07:30:10 +0000

    use enforce_eager for test_logprob

--- tests/samplers/test_logprobs.py
+++ tests/samplers/test_logprobs.py
@@ -24,9 +24,11 @@ def test_get_prompt_logprobs(
         max_tokens=max_tokens,
     )
     del hf_model
-    torch.cuda.empty_cache()
 
-    vllm_model = vllm_runner(model, dtype=dtype, max_logprobs=num_top_logprobs)
+    vllm_model = vllm_runner(model,
+                             dtype=dtype,
+                             max_logprobs=num_top_logprobs,
+                             enforce_eager=True)
     vllm_sampling_params = SamplingParams(max_tokens=max_tokens,
                                           logprobs=num_top_logprobs,
                                           prompt_logprobs=5,
