0ab246be5b5eb3941ff135df3440151cb81ad09a
Alexei-V-Ivanov-AMD <156011006+Alexei-V-Ivanov-AMD@users.noreply.github.com>
2024-03-28 18:17:38 -0500

    Update run-benchmarks.sh
    Insert the minimal perplexity computation benchmark.

--- .buildkite/run-benchmarks.sh
+++ .buildkite/run-benchmarks.sh
@@ -15,6 +15,9 @@ bench_latency_exit_code=$?
 python3 benchmarks/benchmark_throughput.py --input-len 256 --output-len 256 2>&1 | tee benchmark_throughput.txt
 bench_throughput_exit_code=$?
 
+# run python-based benchmark perplexity
+python3 benchmarks/measure_pplv2_MC.py --model=llama-2-7b-chat-hf --data=/tests/prompts/wiki.test.raw --context-size=1024 --sample-size=512 --patch-size=1 2>&1 | tee benchmark_measure_pplv2.txt
+
 # run server-based benchmarks and upload the result to buildkite
 python3 -m vllm.entrypoints.openai.api_server --model meta-llama/Llama-2-7b-chat-hf &
 server_pid=$!
