28c74e0f79d4a1e99f28f9179c22677d918c7b7a
youkaichao <youkaichao@126.com>
2024-03-31 14:29:25 -0700

    fix lint

--- vllm/implementations/distributed_tasks/__init__.py
+++ vllm/implementations/distributed_tasks/__init__.py
@@ -1,2 +1,8 @@
-from vllm.implementations.distributed_tasks.global_coordinator_task import GlobalCoordinatorDistributedTask
-from vllm.implementations.distributed_tasks.group_coordinator_task import GroupCoordinatorDistributedTask
+from vllm.implementations.distributed_tasks.global_coordinator_task import (
+    GlobalCoordinatorDistributedTask)
+from vllm.implementations.distributed_tasks.group_coordinator_task import (
+    GroupCoordinatorDistributedTask)
+
+__all__ = [
+    'GlobalCoordinatorDistributedTask', 'GroupCoordinatorDistributedTask'
+]
