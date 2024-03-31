2593b1084130e4ea0f4ef80e6b5f46cf76cdeefb
youkaichao <youkaichao@126.com>
2024-03-31 13:33:10 -0700

    add test for pynccl w and w/o cudagraph

--- tests/distributed/test_pynccl.py
+++ tests/distributed/test_pynccl.py
@@ -1,92 +1,52 @@
-import multiprocessing
-import os
-
 import pytest
 import torch
 
-from vllm.model_executor.parallel_utils.pynccl import (NCCLCommunicator,
-                                                       ncclGetUniqueId)
+from vllm.implementations.launcher.mp_launcher import MPLauncher
+from vllm.implementations.coordinator import CoordinatorType
+from vllm.implementations.communicator import CommunicatorType
+from vllm.implementations.distributed_tasks.global_coordinator_task import GlobalCoordinatorDistributedTask
 
-
-def distributed_run(fn, world_size):
-    number_of_processes = world_size
-    processes = []
-    for i in range(number_of_processes):
-        env = os.environ.copy()
-        env['RANK'] = str(i)
-        env['LOCAL_RANK'] = str(i)
-        env['WORLD_SIZE'] = str(number_of_processes)
-        env['LOCAL_WORLD_SIZE'] = str(number_of_processes)
-        env['MASTER_ADDR'] = 'localhost'
-        env['MASTER_PORT'] = '12345'
-        p = multiprocessing.Process(target=fn, args=(env, ))
-        processes.append(p)
-        p.start()
-
-    for p in processes:
-        p.join()
-
-
-def update_env(fn):
-    # `multiprocessing.Process` cannot accept environment variables directly
-    # so we need to pass the environment variables as arguments
-    # and update the environment variables in the function
-    def wrapper(env):
-        import os
-        os.environ.update(env)
-        fn()
-
-    return wrapper
-
-
-@update_env
-def worker_fn():
-    comm = NCCLCommunicator()
-    tensor = torch.ones(16, 1024, 1024, dtype=torch.float32).cuda(comm.rank)
-    comm.all_reduce(tensor)
-    result = tensor.mean().cpu().item()
-    assert result == comm.world_size
-
+class AllReduceDistributedTask(GlobalCoordinatorDistributedTask):
+    def post_init_distributed(self, **kwargs):
+        tensor = torch.ones(16, 1024, 1024, dtype=torch.float32).cuda(self.coordinator.get_local_rank())
+        self.communicator.all_reduce(tensor_in=tensor)
+        result = tensor.mean().cpu().item()
+        assert result == self.coordinator.get_local_world_size()    
 
 @pytest.mark.skipif(torch.cuda.device_count() < 2,
                     reason="Need at least 2 GPUs to run the test.")
 def test_pynccl():
-    distributed_run(worker_fn, 2)
+    MPLauncher(n_tasks=2).launch(
+        task_type=AllReduceDistributedTask,
+        coordinator_type=CoordinatorType.TORCH_DISTRIBUTED,
+        communicator_type=CommunicatorType.PYNCCL,
+    )
 
 
-@update_env
-def worker_fn_with_cudagraph():
-    with torch.no_grad():
+class CUDAGraphAllReduceDistributedTask(GlobalCoordinatorDistributedTask):
+    def post_init_distributed(self, **kwargs):
         graph = torch.cuda.CUDAGraph()
-        comm = NCCLCommunicator()
+        device=f'cuda:{self.coordinator.get_rank()}'
+        stream = torch.cuda.Stream(device=device)
+
         # run something in the default stream to initialize torch engine
-        a = torch.ones((4, 4), device=f'cuda:{comm.rank}')
+        a = torch.ones((4, 4), device=device)
         torch.cuda.synchronize()
-        with torch.cuda.graph(graph, stream=comm.stream):
+        with torch.cuda.graph(graph, stream=stream):
             # operation during the graph capture is recorded but not executed
             # see https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#creating-a-graph-using-stream-capture # noqa
-            comm.all_reduce(a)
-        comm.stream.synchronize()
-        assert a.mean().cpu().item() == comm.world_size**0
+            self.communicator.all_reduce(a, stream=stream)
+        stream.synchronize()
+        assert a.mean().cpu().item() == self.coordinator.get_world_size() ** 0
         graph.replay()
-        comm.stream.synchronize()
-        assert a.mean().cpu().item() == comm.world_size**1
-
+        stream.synchronize()
+        assert a.mean().cpu().item() == self.coordinator.get_world_size() ** 1
 
 @pytest.mark.skipif(torch.cuda.device_count() < 2,
                     reason="Need at least 2 GPUs to run the test.")
 def test_pynccl_with_cudagraph():
-    distributed_run(worker_fn_with_cudagraph, 2)
-
-
-def test_ncclGetUniqueId():
-    unique_id = ncclGetUniqueId()
-    # `list(unique_id.internal)` is something like this:
-    # [34, -16, 23, 83, 109, -19, 59, 95, 2, 0, -86, 55, 10, -128, 0, 29, 0,
-    # 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    # 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    # 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    # 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    # 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    # as long as the function doesn't raise an exception, we're good
-    assert unique_id is not None
+    MPLauncher(n_tasks=2).launch(
+        task_type=CUDAGraphAllReduceDistributedTask,
+        coordinator_type=CoordinatorType.TORCH_DISTRIBUTED,
+        communicator_type=CommunicatorType.PYNCCL,
+    )
