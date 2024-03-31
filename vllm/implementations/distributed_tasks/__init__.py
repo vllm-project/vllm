8ac282d961a7c73ee9637b70b684238078b0b87b
youkaichao <youkaichao@126.com>
2024-03-31 14:28:37 -0700

    fix __init__.py

---
+++ vllm/implementations/distributed_tasks/__init__.py
@@ -0,0 +1,2 @@
+from vllm.implementations.distributed_tasks.global_coordinator_task import GlobalCoordinatorDistributedTask
+from vllm.implementations.distributed_tasks.group_coordinator_task import GroupCoordinatorDistributedTask
