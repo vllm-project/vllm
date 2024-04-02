f8d452e74cece32f5c57c006b03c92862fa28be0
Cade Daniel <edacih@gmail.com>
2024-04-02 14:01:47 -0700

    lint

--- vllm/config.py
+++ vllm/config.py
@@ -1022,4 +1022,5 @@ class EngineConfig:
     def to_dict(self):
         """Return the configs as a dictionary, for use in **kwargs.
         """
-        return dict((field.name, getattr(self, field.name)) for field in fields(self))
+        return dict(
+            (field.name, getattr(self, field.name)) for field in fields(self))
