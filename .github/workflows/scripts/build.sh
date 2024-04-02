3f81a84454d3e683fec6a2adcf395049daf372b9
youkaichao <youkaichao@gmail.com>
2024-04-02 11:54:33 -0700

    [CI/Build] fix TORCH_CUDA_ARCH_LIST in wheel build (#3801)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -15,6 +15,7 @@ $python_executable -m pip install -r requirements.txt
 export MAX_JOBS=1
 # Make sure punica is built for the release (for LoRA)
 export VLLM_INSTALL_PUNICA_KERNELS=1
-
+# Make sure release wheels are built for the following architectures
+export TORCH_CUDA_ARCH_LIST="7.0 7.5 8.0 8.6 8.9 9.0+PTX"
 # Build
 $python_executable setup.py bdist_wheel --dist-dir=dist
