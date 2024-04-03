d277679aab8ec3716ac4da70d04e94d35b4fa38d
okalldal <oscar@kalldal.se>
2024-04-03 08:53:02 +0000

    run yapf and ruff

--- vllm/engine/arg_utils.py
+++ vllm/engine/arg_utils.py
@@ -398,10 +398,10 @@ class EngineArgs:
         model_config = ModelConfig(
             self.model, self.tokenizer, self.tokenizer_mode,
             self.trust_remote_code, self.download_dir, self.load_format,
-            self.dtype, self.seed, self.model_class, self.revision, self.code_revision,
-            self.tokenizer_revision, self.max_model_len, self.quantization,
-            self.enforce_eager, self.max_context_len_to_capture,
-            self.max_logprobs)
+            self.dtype, self.seed, self.model_class, self.revision,
+            self.code_revision, self.tokenizer_revision, self.max_model_len,
+            self.quantization, self.enforce_eager,
+            self.max_context_len_to_capture, self.max_logprobs)
         cache_config = CacheConfig(self.block_size,
                                    self.gpu_memory_utilization,
                                    self.swap_space, self.kv_cache_dtype,
