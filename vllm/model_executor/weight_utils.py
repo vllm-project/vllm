21bca06f5dcd59f24f63051d154723547c7c4d57
Sanger Steel <sangersteel@gmail.com>
2024-04-04 10:08:33 -0400

    chore: Replace `PerformanceWarning` after rebase

--- vllm/model_executor/weight_utils.py
+++ vllm/model_executor/weight_utils.py
@@ -23,6 +23,7 @@ from vllm.logger import init_logger
 from vllm.model_executor.layers.quantization import (QuantizationConfig,
                                                      get_quantization_config)
 from vllm.model_executor.layers.quantization.schema import QuantParamSchema
+from vllm.model_executor.tensorizer_loader import PerformanceWarning
 
 logger = init_logger(__name__)
 
