78d40919d60ed82f82b5a57f50eafe06b7f91bad
Yun Ding <yunding@nvidia.com>
2024-04-04 10:17:20 -0700

    [tokenizer] fix formatting

--- vllm/engine/arg_utils.py
+++ vllm/engine/arg_utils.py
@@ -426,7 +426,8 @@ class EngineArgs:
             self.dtype, self.seed, self.revision, self.code_revision,
             self.tokenizer_revision, self.max_model_len, self.quantization,
             self.quantization_param_path, self.enforce_eager,
-            self.max_context_len_to_capture, self.max_logprobs, self.disable_tokenizer)
+            self.max_context_len_to_capture, self.max_logprobs,
+            self.disable_tokenizer)
         cache_config = CacheConfig(self.block_size,
                                    self.gpu_memory_utilization,
                                    self.swap_space, self.kv_cache_dtype,
