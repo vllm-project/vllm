ae210469ece5130c3f013918d90f921a47633844
Noam Gat <noamgat@gmail.com>
2024-04-05 19:28:08 +0300

    Fixing test_guided_logits_processor_black_box()

--- tests/entrypoints/test_guided_processors.py
+++ tests/entrypoints/test_guided_processors.py
@@ -79,33 +79,33 @@ def test_guided_logits_processors():
 
 
 @pytest.mark.asyncio
-async def test_lm_format_enforcer_guided_logits_processor():
+@pytest.mark.parametrize("backend", ["outlines", "lm-format-enforcer"])
+async def test_guided_logits_processor_black_box(backend: str):
     tokenizer = AutoTokenizer.from_pretrained('HuggingFaceH4/zephyr-7b-beta')
-    for backend in ['outlines', 'lm-format-enforcer']:
-        token_ids = tokenizer.encode(
-            f"Give an example IPv4 address with this regex: {TEST_REGEX}")
-        regex_request = CompletionRequest(model='test',
-                                          prompt=token_ids,
-                                          guided_regex=TEST_REGEX)
-        regex_lp = await get_guided_decoding_logits_processor(
-            backend, regex_request, tokenizer)
-        assert regex_lp is not None
-        tensor = torch.rand(32000)
-        original_tensor = torch.clone(tensor)
-        regex_lp(token_ids, tensor)
-        assert tensor.shape == original_tensor.shape
-        assert not torch.allclose(tensor, original_tensor)
+    token_ids = tokenizer.encode(
+        f"Give an example IPv4 address with this regex: {TEST_REGEX}")
+    regex_request = CompletionRequest(model='test',
+                                      prompt=token_ids,
+                                      guided_regex=TEST_REGEX)
+    regex_lp = await get_guided_decoding_logits_processor(
+        backend, regex_request, tokenizer)
+    assert regex_lp is not None
+    tensor = torch.rand(32000)
+    original_tensor = torch.clone(tensor)
+    tensor = regex_lp(token_ids, tensor)
+    assert tensor.shape == original_tensor.shape
+    assert not torch.allclose(tensor, original_tensor)
 
-        token_ids = tokenizer.encode(
-            f"Give an employee profile that fits this schema: {TEST_SCHEMA}")
-        json_request = CompletionRequest(model='test',
-                                         prompt=token_ids,
-                                         guided_json=TEST_SCHEMA)
-        json_lp = await get_guided_decoding_logits_processor(
-            backend, json_request, tokenizer)
-        assert json_lp is not None
-        tensor = torch.rand(32000)
-        original_tensor = torch.clone(tensor)
-        json_lp(token_ids, tensor)
-        assert tensor.shape == original_tensor.shape
-        assert not torch.allclose(tensor, original_tensor)
+    token_ids = tokenizer.encode(
+        f"Give an employee profile that fits this schema: {TEST_SCHEMA}")
+    json_request = CompletionRequest(model='test',
+                                     prompt=token_ids,
+                                     guided_json=TEST_SCHEMA)
+    json_lp = await get_guided_decoding_logits_processor(
+        backend, json_request, tokenizer)
+    assert json_lp is not None
+    tensor = torch.rand(32000)
+    original_tensor = torch.clone(tensor)
+    tensor = json_lp(token_ids, tensor)
+    assert tensor.shape == original_tensor.shape
+    assert not torch.allclose(tensor, original_tensor)
