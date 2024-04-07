d62e5221fd914dc4192108a5a0f889137ae563ee
Maksim Zubkov <maksim.d.zubkov@gmail.com>
2024-04-07 19:20:21 +0200

    Update logits processor apply

--- vllm/model_executor/layers/logits_processor.py
+++ vllm/model_executor/layers/logits_processor.py
@@ -87,18 +87,18 @@ def _apply_logits_processors(
     logits_row_idx = 0
     found_logits_processors = False
     for seq_ids, sampling_params in sampling_metadata.seq_groups:
-        logits_processors = sampling_params.logits_processors
-        if logits_processors:
-            found_logits_processors = True
-            for seq_id in seq_ids:
+        for i, seq_id in enumerate(seq_ids):
+            logits_processors = sampling_params.logits_processors[i]
+            if logits_processors:
+                found_logits_processors = True
                 logits_row = logits[logits_row_idx]
                 token_ids = sampling_metadata.seq_data[seq_id].output_token_ids
                 for logits_processor in logits_processors:
                     logits_row = logits_processor(token_ids, logits_row)
                 logits[logits_row_idx] = logits_row
                 logits_row_idx += 1
-        else:
-            logits_row_idx += len(seq_ids)
+            else:
+                logits_row_idx += len(seq_ids)
     if found_logits_processors:
         assert logits_row_idx == logits.shape[0]
     return logits
