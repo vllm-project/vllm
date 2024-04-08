f46864d68dfb46ff88f574e6844f10fdb14cd3b5
egortolmachev <150433814+egortolmachev@users.noreply.github.com>
2024-04-08 17:59:38 +0300

    [Bugfix] Added Command-R GPTQ support (#3849)
    Co-authored-by: Egor Tolmachev <t333ga@gmail.com>

--- vllm/model_executor/models/commandr.py
+++ vllm/model_executor/models/commandr.py
@@ -349,11 +349,21 @@ class CohereForCausalLM(nn.Module):
                 if shard_name not in name:
                     continue
                 name = name.replace(shard_name, param_name)
+                # Skip loading extra bias for GPTQ models.
+                if name.endswith(".bias") and name not in params_dict:
+                    continue
                 param = params_dict[name]
                 weight_loader = param.weight_loader
                 weight_loader(param, loaded_weight, shard_id)
                 break
             else:
+                # lm_head is not used in vllm as it is tied with embed_token.
+                # To prevent errors, skip loading lm_head.weight.
+                if "lm_head.weight" in name:
+                    continue
+                # Skip loading extra bias for GPTQ models.
+                if name.endswith(".bias") and name not in params_dict:
+                    continue
                 param = params_dict[name]
                 weight_loader = getattr(param, "weight_loader",
                                         default_weight_loader)
