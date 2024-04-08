680fcde1bad0fb96f1f69a9b002e3a146555291a
andy-neuma <andy@neuralmagic.com>
2024-04-08 15:53:21 +0000

    still need skip list, but shards seem okay

--- .github/scripts/run-tests.py
+++ .github/scripts/run-tests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import re
 import sys
 import yaml
 
@@ -9,14 +10,37 @@ import yaml
 #     print("Usage: ./run-tests.py <filename>")
 #     sys.exit(1)
 
-def run_cmd(cmd):
-    print(f"{cmd}")
+def cc_flags(cmd, src_dir, test_dir, cc_report):
+    flags = f" --cov={src_dir} --cov={test_dir} --cov-report=html:{cc_report} --cov-append "
+    pytest_cmd = "pytest "
+    insert_flag_pos = cmd.find(pytest_cmd) + len(pytest_cmd)
+    return cmd[:insert_flag_pos] + flags + cmd[insert_flag_pos:]
 
 
-def run_cmds(cmds):
+def use_CUDA_VISIBLE_DEVICES(cmd):
+    shard_substring = ' --shard-id=$$BUILDKITE_PARALLEL_JOB --num-shards=$$BUILDKITE_PARALLEL_JOB_COUNT'
+    shard_pos = cmd.find(shard_substring)
+    cmd_to_run = cmd
+    if shard_pos > 0:
+        cmd_to_run = cmd.replace(shard_substring, "")
+        cmd_to_run = " CUDA_VISIBLE_DEVICES=0,1 " + cmd_to_run
+    return cmd_to_run
+
+
+def run_cmd(cmd, test_dir):
+    print(f"original: {cmd}")
+    nm_cmd = cc_flags(use_CUDA_VISIBLE_DEVICES(cmd), "vllm", "tests", "cc-vllm-html")
+    print(f"nm: {nm_cmd}")
+    cmd_status = os.system(f"cd {test_dir} && {nm_cmd}")
+    return int(cmd_status)
+
+
+def run_cmds(cmds, test_dir):
     print("running commands ...")
+    cmds_status = 0
     for cmd in cmds:
-        run_cmd(cmd)
+        cmds_status += run_cmd(cmd, test_dir)
+    return cmds_status
 
 
 def run_test_pipeline():
@@ -32,21 +56,31 @@ def run_test_pipeline():
             print(ee)
             raise ee
 
+    test_status = 0
+    test_dir = "tests"
     for entry in steps:
         label = entry['label']
         print(f"STARTING LABEL: {label}")
+
+        dont_skip = set(["Regression Test", "Basic Correctness Test", "Kernels Test %N"])
+        if label not in dont_skip:
+            continue
+
         if 'command' in entry:
             cmd = entry['command']
-            run_cmd(cmd)
+            test_status += run_cmd(cmd, test_dir)
             print(f"completed ... {label}")
         elif 'commands' in entry:
             cmds = entry['commands']
-            run_cmds(cmds)
+            test_status += run_cmds(cmds, test_dir)
         else:
             print(f"step doesn't have 'command' or 'commands' ... '{entry}'")
         print(f"FINISHED LABEL: {label}")
         print()
 
+    return test_status
+
 
 if __name__ == "__main__":
-    run_test_pipeline()
+    test_status = run_test_pipeline()
+    print(f"test_status: {test_status}")
