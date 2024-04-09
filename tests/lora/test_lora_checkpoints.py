ccf5e7b31afef026b937e70f6fe5868506a916f0
jeejeeli <pandaleefree@gmail.com>
2024-04-09 18:32:59 +0800

    code format

--- tests/lora/test_lora_checkpoints.py
+++ tests/lora/test_lora_checkpoints.py
@@ -1,5 +1,5 @@
 import pytest
-from vllm.lora.request import LoRARequest
+
 from vllm.lora.models import LoRAModel
 from vllm.model_executor.models.baichuan import BaiChuanBaseForCausalLM
 
@@ -9,7 +9,7 @@ def test_load_checkpoints(lora_name, chatglm3_lora_files, baichuan_lora_files):
     supported_lora_modules = BaiChuanBaseForCausalLM.supported_lora_modules
     packed_modules_mapping = BaiChuanBaseForCausalLM.packed_modules_mapping
     embedding_modules = BaiChuanBaseForCausalLM.embedding_modules
-    embedding_padding_modules = BaiChuanBaseForCausalLM.embedding_padding_modules
+    embed_padding_modules = BaiChuanBaseForCausalLM.embedding_padding_modules
     expected_lora_modules = []
     for module in supported_lora_modules:
         if module in packed_modules_mapping:
@@ -17,7 +17,7 @@ def test_load_checkpoints(lora_name, chatglm3_lora_files, baichuan_lora_files):
         else:
             expected_lora_modules.append(module)
     if lora_name == "baichuan7B":
-        # For the baichuan7B model, load it's LoRA, 
+        # For the baichuan7B model, load it's LoRA,
         # and the test should pass.
         LoRAModel.from_local_checkpoint(
             baichuan_lora_files,
@@ -25,9 +25,9 @@ def test_load_checkpoints(lora_name, chatglm3_lora_files, baichuan_lora_files):
             lora_model_id=1,
             device="cpu",
             embedding_modules=embedding_modules,
-            embedding_padding_modules=embedding_padding_modules)
+            embedding_padding_modules=embed_padding_modules)
     else:
-        # For the baichuan7B model, load chatglm3-6b's LoRA, 
+        # For the baichuan7B model, load chatglm3-6b's LoRA,
         # and the test should raise the following error
         expected_error = "Please verify that the loaded LoRA module is correct"  # noqa: E501
         with pytest.raises(ValueError, match=expected_error):
@@ -37,5 +37,4 @@ def test_load_checkpoints(lora_name, chatglm3_lora_files, baichuan_lora_files):
                 lora_model_id=1,
                 device="cpu",
                 embedding_modules=embedding_modules,
-                embedding_padding_modules=embedding_padding_modules)
-
+                embedding_padding_modules=embed_padding_modules)
