c7400b70ede51b8d79312c3ec5fbd4582592e688
youkaichao <youkaichao@126.com>
2024-04-08 22:52:12 -0700

    fix backend for ensure_model_parallel_initialized

--- vllm/model_executor/parallel_utils/parallel_state.py
+++ vllm/model_executor/parallel_utils/parallel_state.py
@@ -132,12 +132,13 @@ def initialize_model_parallel(
 def ensure_model_parallel_initialized(
     tensor_model_parallel_size: int,
     pipeline_model_parallel_size: int,
-    backend: str = "nccl",
+    backend: Optional[str] = None,
 ) -> None:
     """Helper to initialize model parallel groups if they are not initialized,
     or ensure tensor-parallel and pipeline-parallel sizes are equal to expected
     values if the model parallel groups are initialized.
     """
+    backend = backend or torch.distributed.get_backend(_DEVICE_WORLD_GROUP)
     if not model_parallel_is_initialized():
         initialize_model_parallel(tensor_model_parallel_size,
                                   pipeline_model_parallel_size, backend)
