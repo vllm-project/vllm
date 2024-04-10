92a0d80b2b1c2ff1b2764be329f25438a8579b6a
youkaichao <youkaichao@126.com>
2024-04-09 19:06:07 -0700

    fix divide

--- vllm/model_executor/layers/activation.py
+++ vllm/model_executor/layers/activation.py
@@ -10,7 +10,7 @@ from vllm._C import ops
 from vllm.model_executor.layers.quantization import QuantizationConfig
 from vllm.distributed import (
     get_tensor_model_parallel_rank, get_tensor_model_parallel_world_size)
-from vllm.utils import divide
+from vllm.distributed import divide
 from vllm.model_executor.utils import set_weight_attrs
 
 
