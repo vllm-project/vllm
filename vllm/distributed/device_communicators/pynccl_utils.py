5a699130d821867a5c56dfbdf33131e27cd4dce7
youkaichao <youkaichao@126.com>
2024-04-10 13:38:57 -0700

    fix isor

--- vllm/distributed/device_communicators/pynccl_utils.py
+++ vllm/distributed/device_communicators/pynccl_utils.py
@@ -9,9 +9,8 @@ from vllm.logger import init_logger
 logger = init_logger(__name__)
 
 try:
-    from vllm.distributed.device_communicators.pynccl import (
-        NCCLCommunicator, ncclGetVersion,
-    )
+    from vllm.distributed.device_communicators.pynccl import (NCCLCommunicator,
+                                                              ncclGetVersion)
 except Exception as e:
     # in non-NVIDIA environments, we can't import the nccl module
     # e.g. when running on machines with AMD GPUs
