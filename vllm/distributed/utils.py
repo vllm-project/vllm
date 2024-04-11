c81a0e136d688d3f61aab1b03f117b5a8831db47
youkaichao <youkaichao@126.com>
2024-04-11 13:58:55 -0700

    use for both distributed and non-distributed

--- vllm/distributed/utils.py
+++ vllm/distributed/utils.py
@@ -7,6 +7,9 @@ import os
 from typing import Sequence
 
 import torch
+import torch.distributed as dist
+
+from .parallel_state import get_cpu_world_group
 
 
 def ensure_divisibility(numerator, denominator):
@@ -50,22 +53,41 @@ def split_tensor_along_last_dim(
     return tensor_list
 
 
+_gpu_p2p_access_cache = None
+
+
 def gpu_p2p_access_check(i: int, j: int) -> bool:
     """Check if GPU i can access GPU j."""
+
+    # if the cache variable is already calculated,
+    # read from the cache instead of checking it again
+    global _gpu_p2p_access_cache
+    if _gpu_p2p_access_cache is not None:
+        return _gpu_p2p_access_cache[f"{i}->{j}"]
+
+    is_distributed = dist.is_initialized()
+    cpu_world_group = get_cpu_world_group()
+
     num_dev = torch.cuda.device_count()
     cuda_visible_devices = os.environ.get("CUDA_VISIBLE_DEVICES", None)
     if cuda_visible_devices is None:
         cuda_visible_devices = ",".join(str(i) for i in range(num_dev))
     path = os.path.expanduser(
         f"~/.config/vllm/gpu_p2p_access_cache_for_{cuda_visible_devices}.json")
-    if os.path.exists(path):
-        with open(path, "r") as f:
-            cache = json.load(f)
-    else:
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    if (not is_distributed or dist.get_rank(cpu_world_group)== 0) \
+        and (not os.path.exists(path)):
+        # only the master process can enter this block to calculate the cache
         cache = {}
         for _i in range(num_dev):
             for _j in range(num_dev):
-                cache[f"{_i}->{_j}"] = torch.cuda.can_device_access_peer(_i, _j)
+                cache[f"{_i}->{_j}"] = torch.cuda.can_device_access_peer(
+                    _i, _j)
         with open(path, "w") as f:
             json.dump(cache, f, indent=4)
-    return cache[f"{i}->{j}"]
+    if is_distributed and cpu_world_group is not None:
+        dist.barrier(cpu_world_group)
+    with open(path, "r") as f:
+        cache = json.load(f)
+    _gpu_p2p_access_cache = cache
+    return _gpu_p2p_access_cache[f"{i}->{j}"]
