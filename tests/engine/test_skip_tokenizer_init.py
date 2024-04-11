4b3c5e3c5c511e667cd9921ce6e8786ab66d963c
Yun Ding <yunding@nvidia.com>
2024-04-10 18:11:01 -0700

    [tokenizer] test generate based on prompt token ids

--- tests/engine/test_skip_tokenizer_init.py
+++ tests/engine/test_skip_tokenizer_init.py
@@ -9,16 +9,14 @@ def test_skip_tokenizer_initialization(model: str):
     # This test checks if the flag skip_tokenizer_init skips the initialization
     # of tokenizer and detokenizer. The generated output is expected to contain
     # token ids.
-    prompt = (
-        "You are a helpful assistant. How do I build a car from cardboard and "
-        "paper clips? Is there an easy to follow video tutorial available "
-        "online for free?")
-
     llm = LLM(model=model, skip_tokenizer_init=True)
-    sampling_params = SamplingParams(max_tokens=10,
-                                     temperature=0.0,
-                                     detokenize=False)
-
+    sampling_params = SamplingParams()
     with pytest.raises(ValueError) as err:
-        llm.generate(prompt, sampling_params)
+        llm.generate("abc", sampling_params)
     assert "prompts must be None if" in str(err.value)
+    outputs = llm.generate(prompt_token_ids=[1, 2, 3],
+                           sampling_params=sampling_params)
+    assert len(outputs) > 0
+    completions = outputs[0].outputs
+    assert len(completions) > 0
+    assert completions[0].text == ""
