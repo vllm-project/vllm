9d568fc32fda4c449f51578cd5a538f9a4b45404
Sanger Steel <sangersteel@gmail.com>
2024-04-11 12:27:47 -0400

    fix: Revert change to `examples/multilora_inference.py`

--- examples/multilora_inference.py
+++ examples/multilora_inference.py
@@ -104,21 +104,12 @@ def initialize_engine() -> LLMEngine:
     #   use the same rank, it is recommended to set this as low as possible.
     # max_cpu_loras: controls the size of the CPU LoRA cache.
     from vllm.model_executor.tensorizer_loader import TensorizerArgs
-    engine_args = EngineArgs(
-        model="meta-llama/Llama-2-7b-hf",
-        load_format="tensorizer",
-        enable_lora=True,
-        tensorizer_args=TensorizerArgs(
-            tensorizer_uri=
-            "s3://tensorized-ssteel/Llama-2-7b-hf-vllm.tensors",  ## To serialized model from HF model hub
-            num_readers=1,
-            vllm_tensorized=True),
-        max_loras=1,
-        max_lora_rank=8,
-        max_cpu_loras=2,
-        max_num_seqs=50,
-        max_model_len=1000,
-    )
+    engine_args = EngineArgs(model="meta-llama/Llama-2-7b-hf",
+                             enable_lora=True,
+                             max_loras=1,
+                             max_lora_rank=8,
+                             max_cpu_loras=2,
+                             max_num_seqs=256)
     return LLMEngine.from_engine_args(engine_args)
 
 
