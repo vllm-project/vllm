4fb87628cc24684e1726ca734ee5509157069a02
Alexei V. Ivanov <alexei.ivanov@amd.com>
2024-04-12 05:52:57 -0500

    Test.

---
+++ .buildkite/run-amd-test_async_engine.sh
@@ -0,0 +1,18 @@
+# This script build the ROCm docker image and run the API server inside the container.
+# It serves a sanity check for compilation and basic model usage.
+set -ex
+
+# Print ROCm version
+rocminfo
+
+# Try building the docker image
+docker build -t rocm -f Dockerfile.rocm .
+
+# Setup cleanup
+remove_docker_container() { docker rm -f rocm_test_async_engine || true; }
+trap remove_docker_container EXIT
+remove_docker_container
+
+# Run the image
+docker run --device /dev/kfd --device /dev/dri --network host --name rocm_test_async_engine rocm python3 -m pytest -v -s vllm/tests/async_engine
+
