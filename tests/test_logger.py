c087f9d509fd903759b25b5e81b4a999a89a6ed3
Danny Guinther <dguinther@neuralmagic.com>
2024-04-12 15:02:24 -0400

    Move logger.py to logger/__init__.py to prepare for rework

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,8 +1,10 @@
+import logging
 import os
 import sys
 import tempfile
 
-from vllm.logger import enable_trace_function_call
+from vllm.logger import (_DATE_FORMAT, _FORMAT, NewLineFormatter,
+                         enable_trace_function_call, init_logger)
 
 
 def f1(x):
@@ -25,3 +27,29 @@ def test_trace_function_call():
     assert "f2" in content
     sys.settrace(None)
     os.remove(path)
+
+
+def test_vllm_root_logger_configuration():
+    logger = logging.getLogger("vllm")
+    assert logger.level == logging.DEBUG
+    assert not logger.propagate
+
+    handler = logger.handlers[0]
+    assert handler.stream == sys.stdout
+    assert handler.level == logging.INFO
+
+    formatter = handler.formatter
+    assert formatter is not None
+    assert isinstance(formatter, NewLineFormatter)
+    assert formatter._fmt == _FORMAT
+    assert formatter.datefmt == _DATE_FORMAT
+
+
+def test_init_logger_configures_the_logger_like_the_root_logger():
+    root_logger = logging.getLogger("vllm")
+    logger = init_logger(__name__)
+
+    assert logger.name == __name__
+    assert logger.level == logging.DEBUG
+    assert logger.handlers == root_logger.handlers
+    assert logger.propagate == root_logger.propagate
