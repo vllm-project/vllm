5d3ba9fb3945b6bc788b144c9fbdcd4e6a0453a1
Alexei V. Ivanov <alexei.ivanov@amd.com>
2024-04-12 07:28:46 -0500

    Test.

---
+++ .buildkite/run-amd-test_distributed.sh
@@ -0,0 +1,27 @@
+# This script build the ROCm docker image and run the API server inside the container.
+# It serves a sanity check for compilation and basic model usage.
+set -ex
+
+# Print ROCm version
+rocminfo
+
+# Try building the docker image
+docker build -t rocm -f Dockerfile.rocm .
+
+# Setup cleanup
+remove_docker_container() { docker rm -f rocm_test_distributed || true; }
+trap remove_docker_container EXIT
+remove_docker_container
+
+# Run the image
+docker run --device /dev/kfd --device /dev/dri --network host --name rocm_test_distributed \
+       	rocm  python3 -m pytest -v-s vllm/tests/test_pynccl.py
+
+docker run --device /dev/kfd --device /dev/dri --network host --name rocm_test_distributed \
+	        rocm  /bin/bash -c "TEST_DIST_MODEL=facebook/opt-125m python3 -m pytest \
+		-v-s vllm/tests/test_basic_distributed_correctness.py"
+
+docker run --device /dev/kfd --device /dev/dri --network host --name rocm_test_distributed \
+	                rocm  /bin/bash -c "TEST_DIST_MODEL=meta-llama/Llama-2-7b-hf python3 -m pytest \
+			                -v-s vllm/tests/test_basic_distributed_correctness.py"
+
