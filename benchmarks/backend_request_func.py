ffd9ca857a299a07c020b5e11a4480116aa29717
Elinx <xizzuli@163.com>
2024-04-17 19:07:23 +0800

    [Bugfix] fix output parsing error for trtllm backend (#4137)
    Co-authored-by: Roger Wang <ywang@roblox.com>

--- benchmarks/backend_request_func.py
+++ benchmarks/backend_request_func.py
@@ -139,6 +139,7 @@ async def async_request_trt_llm(
                                               "data:")
 
                         data = json.loads(chunk)
+                        output.generated_text += data["text_output"]
                         timestamp = time.perf_counter()
                         # First token
                         if ttft == 0.0:
@@ -153,7 +154,6 @@ async def async_request_trt_llm(
                         most_recent_timestamp = timestamp
 
                     output.latency = most_recent_timestamp - st
-                    output.generated_text = json.loads(data)["text_output"]
                     output.success = True
 
                 else:
