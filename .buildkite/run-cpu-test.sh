2520dbff725be43fe78dea68e189920cde86b6e3
Yuan Zhou <yuan.zhou@intel.com>
2024-04-18 13:34:25 +0800

    adding big model test
    Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

--- .buildkite/run-cpu-test.sh
+++ .buildkite/run-cpu-test.sh
@@ -30,3 +30,7 @@ docker exec cpu-test bash -c "cd tests;
   bash ../.buildkite/download-images.sh
   pytest -v -s models --ignore=models/test_llava.py --ignore=models/test_mistral.py --ignore=models/test_marlin.py --ignore=models/test_big_models.py"
 
+# Run big model test
+docker exec cpu-test bash -c "cd tests;
+  sed -i 's/half/float/g' models/test_big_models.py
+  pytest -v -s models/test_big_models.py"
