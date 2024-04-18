5a565a4e5453d998c618c4e7bdf3ed0d4d3e3000
youkaichao <youkaichao@126.com>
2024-04-18 11:46:13 -0700

    fix ruff

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,13 +1,17 @@
-from vllm.logger import enable_trace_function_call
-import tempfile
 import os
 import sys
+import tempfile
+
+from vllm.logger import enable_trace_function_call
+
 
 def f1(x):
     return f2(x)
 
+
 def f2(x):
     return x
+
 
 def test_trace_function_call():
     fd, path = tempfile.mkstemp()
@@ -16,7 +20,7 @@ def test_trace_function_call():
     f1(1)
     with open(path, 'r') as f:
         content = f.read()
-    
+
     assert "f1" in content
     assert "f2" in content
     sys.settrace(None)
