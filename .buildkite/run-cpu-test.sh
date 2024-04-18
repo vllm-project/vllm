b04c6b1c1b63abaf40f506a58e189bebe1b1bff6
Yuan Zhou <yuan.zhou@intel.com>
2024-04-18 08:10:58 +0800

    ignore models
    Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

--- .buildkite/run-cpu-test.sh
+++ .buildkite/run-cpu-test.sh
@@ -11,9 +11,7 @@ trap remove_docker_container EXIT
 remove_docker_container
 
 # Run the image and launch offline inference
-<<<<<<< HEAD
-docker run --network host --env VLLM_CPU_KVCACHE_SPACE=1 --name cpu-test cpu-test python3 vllm/examples/offline_inference.py
-=======
+
 docker run -itd --network host --env VLLM_CPU_KVCACHE_SPACE=4 --name cpu-test cpu-test
 
 # offline inference
@@ -26,9 +24,9 @@ docker exec cpu-test bash -c "python3 examples/offline_inference.py"
 docker exec cpu-test bash -c "cd tests;
   pip install pytest Pillow
   rm -f __init__.py
-  sed -i '/*stablelm-3b-4e1t/d' models/test_models.py
+  sed -i '/\"stabilityai/d' models/test_models.py
   sed -i '/torch.cuda.empty_cache/d' conftest.py
   sed -i 's/cuda/cpu/g' conftest.py
   bash ../.buildkite/download-images.sh
-  pytest -v -s models --ignore=models/test_llava.py --ignore=models/test_mistral.py --ignore=models/test_marlin.py"
->>>>>>> ca0870d3... enable more tests
+  pytest -v -s models --ignore=models/test_llava.py --ignore=models/test_mistral.py --ignore=models/test_marlin.py --ignore=models/test_big_models.py"
+
