bb35129d4c45e407adf3c0814fba46f912a4f7ec
youkaichao <youkaichao@126.com>
2024-04-18 11:39:58 -0700

    add tests

---
+++ tests/test_logger.py
@@ -0,0 +1,23 @@
+from vllm.logger import enable_trace_function_call
+import tempfile
+import os
+import sys
+
+def f1(x):
+    return f2(x)
+
+def f2(x):
+    return x
+
+def test_trace_function_call():
+    fd, path = tempfile.mkstemp()
+    cur_dir = os.path.dirname(__file__)
+    enable_trace_function_call(path, cur_dir)
+    f1(1)
+    with open(path, 'r') as f:
+        content = f.read()
+    
+    assert "f1" in content
+    assert "f2" in content
+    sys.settrace(None)
+    os.remove(path)
