cedee50a52ed131871c0d60c63feac7301e18411
Nick Hill <nickhill@us.ibm.com>
2024-04-19 07:49:01 -0700

    Revert @staticmethod

--- vllm/worker/worker_base.py
+++ vllm/worker/worker_base.py
@@ -108,8 +108,7 @@ class WorkerWrapperBase:
         self.worker_class_name = worker_class_name
         self.worker = None
 
-    @staticmethod
-    def update_environment_variables(envs: Dict[str, str]) -> None:
+    def update_environment_variables(self, envs: Dict[str, str]) -> None:
         key = 'CUDA_VISIBLE_DEVICES'
         if key in envs and key in os.environ:
             # overwriting CUDA_VISIBLE_DEVICES is desired behavior
