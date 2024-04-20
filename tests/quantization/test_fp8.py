5b70cd1c7c9cf7cc2e60fb5454d460b2c3d3de92
Philipp Moritz <pcmoritz@gmail.com>
2024-04-19 17:00:29 -0700

    fix test

--- tests/quantization/test_fp8.py
+++ tests/quantization/test_fp8.py
@@ -18,7 +18,7 @@ capability = capability[0] * 10 + capability[1]
 def test_load_fp16_model(vllm_runner) -> None:
     llm = vllm_runner("facebook/opt-125m", quantization="fp8")
 
-    model = llm.llm_engine.model_executor.driver_worker.model_runner.model
+    model = llm.model.llm_engine.model_executor.driver_worker.model_runner.model
     fc1 = model.model.decoder.layers[0].fc1
-    assert isinstance(fc1.linaer_method, Fp8LinearMethod)
+    assert isinstance(fc1.linear_method, Fp8LinearMethod)
     assert fc1.weight.dtype == torch.float8_e4m3fn
