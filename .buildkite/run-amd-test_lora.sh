38f72af40448962389fe52f3021f27b3b13323a7
Alexei V. Ivanov <alexei.ivanov@amd.com>
2024-04-21 10:59:03 -0500

    Test.

--- .buildkite/run-amd-test_lora.sh
+++ .buildkite/run-amd-test_lora.sh
@@ -34,21 +34,21 @@ remove_docker_container
 
 export HIP_VISIBLE_DEVICES=1
 docker run --device /dev/kfd --device /dev/dri --shm-size=10.24gb --network host -e HIP_VISIBLE_DEVICES \
-        --name rocm_test_lora0 rocm /bin/bash -c "pip install peft; \
+        --name rocm_test_lora0 rocm /bin/bash -c "pip install peft; export VLLM_INSTALL_PUNICA_KERNELS=1; \
 	python3 -m pytest -v -s vllm/tests/lora" --shard-id=0 --num-shards=4 &
 
 export HIP_VISIBLE_DEVICES=2
 docker run --device /dev/kfd --device /dev/dri --shm-size=10.24gb --network host -e HIP_VISIBLE_DEVICES \
-        --name rocm_test_lora1 rocm /bin/bash -c "pip install peft; \
+        --name rocm_test_lora1 rocm /bin/bash -c "pip install peft; export VLLM_INSTALL_PUNICA_KERNELS=1; \
 	python3 -m pytest -v -s vllm/tests/lora" --shard-id=1 --num-shards=4 &
 
 export HIP_VISIBLE_DEVICES=3
 docker run --device /dev/kfd --device /dev/dri --shm-size=10.24gb --network host -e HIP_VISIBLE_DEVICES \
-        --name rocm_test_lora2 rocm /bin/bash -c "pip install peft; \
+        --name rocm_test_lora2 rocm /bin/bash -c "pip install peft; export VLLM_INSTALL_PUNICA_KERNELS=1; \
 	python3 -m pytest -v -s vllm/tests/lora" --shard-id=2 --num-shards=4 &
 
 export HIP_VISIBLE_DEVICES=4
 docker run --device /dev/kfd --device /dev/dri --shm-size=10.24gb --network host -e HIP_VISIBLE_DEVICES \
-        --name rocm_test_lora3 rocm /bin/bash -c "pip install peft; \
+        --name rocm_test_lora3 rocm /bin/bash -c "pip install peft; export VLLM_INSTALL_PUNICA_KERNELS=1; \
 	python3 -m pytest -v -s vllm/tests/lora" --shard-id=3 --num-shards=4
 
