8fbc18c6c8d5e1a643e106b06bcd4c33eff86c6a
simon-mo <simon.mo@hey.com>
2024-04-23 10:29:54 -0700

    fix syntax

---
+++ .buildkite/build-artifacts.sh
@@ -0,0 +1,19 @@
+#!/usr/env/bin bash
+
+# This script is used to build Python wheels and Docker Images for vLLM.
+# We will use container to build wheels to ensure reproducibility and isolation
+
+set -ex
+
+export DOCKER_BUILDKIT=1
+docker build --build-arg max_jobs=16 \
+    --build-arg python_package_version=python3.8 \
+    --target build \
+    -f Dockerfile \
+    -t vllm-build \
+    .
+
+# copy the wheels to the host, in the container it is located in /workspace/dist
+docker run --rm -v $(pwd):/host vllm-build cp -r /workspace/dist /host
+
+ls dist
