2b7949c1c2e34de41d9cfc84dd0e377cc6bd58c2
James Fleming <jaemz@alum.mit.edu>
2024-04-23 13:59:33 -0400

    AQLM CUDA support (#3287)
    Co-authored-by: mgoin <michael@neuralmagic.com>

--- csrc/ops.h
+++ csrc/ops.h
@@ -86,6 +86,21 @@ void gelu_fast(
   torch::Tensor& input);
 
 #ifndef USE_ROCM
+torch::Tensor aqlm_gemm(
+  const torch::Tensor& input,
+  const torch::Tensor& codes,
+  const torch::Tensor& codebooks,
+  const torch::Tensor& scales,
+  const torch::Tensor& codebook_partition_sizes,
+  const std::optional<torch::Tensor>& bias
+);
+
+torch::Tensor aqlm_dequant(
+  const torch::Tensor& codes,
+  const torch::Tensor& codebooks,
+  const torch::Tensor& codebook_partition_sizes
+);
+
 torch::Tensor awq_gemm(
   torch::Tensor _in_feats,
   torch::Tensor _kernel,
