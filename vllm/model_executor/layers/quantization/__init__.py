e6dd46f18a1184277742572268156e9721e71f85
Robert Shaw <rshaw@neuralmagic.com>
2024-04-24 13:16:07 +0000

    updated names

--- vllm/model_executor/layers/quantization/__init__.py
+++ vllm/model_executor/layers/quantization/__init__.py
@@ -5,7 +5,7 @@ from vllm.model_executor.layers.quantization.awq import AWQConfig
 from vllm.model_executor.layers.quantization.base_config import (
     QuantizationConfig)
 from vllm.model_executor.layers.quantization.fp8 import FP8Config
-from vllm.model_executor.layers.quantization.fp8_static import FP8StaticConfig
+from vllm.model_executor.layers.quantization.fp8_serialized import FP8SerializedConfig
 from vllm.model_executor.layers.quantization.gptq import GPTQConfig
 from vllm.model_executor.layers.quantization.marlin import MarlinConfig
 from vllm.model_executor.layers.quantization.squeezellm import SqueezeLLMConfig
@@ -14,7 +14,7 @@ QUANTIZATION_METHODS = {
     "aqlm": AQLMConfig,
     "awq": AWQConfig,
     "fp8": FP8Config,
-    "fp8_static": FP8StaticConfig,
+    "fp8_serialized": FP8SerializedConfig,
     "gptq": GPTQConfig,
     "squeezellm": SqueezeLLMConfig,
     "marlin": MarlinConfig,
