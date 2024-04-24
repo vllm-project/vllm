f96428e692d2bccefa4799b7815ff63faf49442b
Robert Shaw <rshaw@neuralmagic.com>
2024-04-24 01:58:36 +0000

    stash

--- tests/quantization/test_fp8.py
+++ tests/quantization/test_fp8.py
@@ -5,19 +5,22 @@ Run `pytest tests/quantization/test_fp8.py --forked`.
 import pytest
 import torch
 
+from vllm import SamplingParams
 from vllm.model_executor.layers.quantization import QUANTIZATION_METHODS
 from vllm.model_executor.layers.quantization.fp8 import Fp8LinearMethod
 
 capability = torch.cuda.get_device_capability()
 capability = capability[0] * 10 + capability[1]
-print(capability)
 
 @pytest.mark.skipif(
     capability < QUANTIZATION_METHODS["fp8_static"].get_min_capability(),
     reason="FP8 is not supported on this GPU type.")
 def test_load_static_model(vllm_runner) -> None:
-    llm = vllm_runner("FriendliAI/Llama-2-7b-chat-hf-fp8", quantization="fp8_static")
-    print(llm)
+    llm = vllm_runner("FriendliAI/Llama-2-7b-chat-hf-fp8", quantization="fp8_static", enforce_eager=True)
+    model = llm.model.llm_engine.model_executor.driver_worker.model_runner.model
+    fc1 = model.model.decoder.layers[0].fc1
+    assert isinstance(fc1.linear_method, Fp8LinearMethod)
+    print(llm.generate("Hello my name is", SamplingParams(max_tokens=20)))
 
 @pytest.mark.skipif(
     capability < QUANTIZATION_METHODS["fp8"].get_min_capability(),
