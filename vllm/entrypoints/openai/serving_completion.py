b3759afabea1e6943db436bf7515b5fbf0c57c6d
Roy <jasonailu87@gmail.com>
2024-04-27 19:30:08 +0800

    [Bugfix][Core] Fix get decoding config from ray (#4335)

--- vllm/entrypoints/openai/serving_completion.py
+++ vllm/entrypoints/openai/serving_completion.py
@@ -89,7 +89,7 @@ class OpenAIServingCompletion(OpenAIServing):
         try:
             sampling_params = request.to_sampling_params()
             lora_request = self._maybe_get_lora(request)
-            decoding_config = self.engine.engine.decoding_config
+            decoding_config = await self.engine.get_decoding_config()
             guided_decoding_backend = request.guided_decoding_backend \
                 or decoding_config.guided_decoding_backend
             guided_decode_logit_processor = (
