1e03ad8be0565605679eb2162151babc573821f2
Qubitium <417764+Qubitium@users.noreply.github.com>
2024-04-29 12:00:42 +0000

    standardize lm_head for ParallelLMHead

--- vllm/model_executor/models/internlm2.py
+++ vllm/model_executor/models/internlm2.py
@@ -246,7 +246,7 @@ class InternLM2ForCausalLM(nn.Module):
         self.config = config
         self.quant_config = quant_config
         self.model = InternLM2Model(config, quant_config)
-        self.output = ParallelLMHead(config.vocab_size, config.hidden_size)
+        self.lm_head = ParallelLMHead(config.vocab_size, config.hidden_size)
         self.logits_processor = LogitsProcessor(config.vocab_size)
         self.sampler = Sampler()
 
@@ -263,7 +263,7 @@ class InternLM2ForCausalLM(nn.Module):
 
     def compute_logits(self, hidden_states: torch.Tensor,
                        sampling_metadata: SamplingMetadata) -> torch.Tensor:
-        logits = self.logits_processor(self.output, hidden_states,
+        logits = self.logits_processor(self.lm_head, hidden_states,
                                        sampling_metadata)
         return logits
 
