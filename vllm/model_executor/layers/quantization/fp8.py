41fbde92035f7cab6369f09081132da40d0c9bf5
Robert Shaw <rshaw@neuralmagic.com>
2024-04-29 02:08:59 +0000

    ./format.sh

--- vllm/model_executor/layers/quantization/fp8.py
+++ vllm/model_executor/layers/quantization/fp8.py
@@ -29,7 +29,8 @@ class Fp8Config(QuantizationConfig):
             logger.warning("Detected fp8 checkpoint. Please note that the "
                            "format is experimental and subject to change.")
         if activation_scheme not in ACTIVATION_SCHEMES:
-            raise ValueError(f"Unsupported activation scheme {activation_scheme}")
+            raise ValueError(
+                f"Unsupported activation scheme {activation_scheme}")
         self.activation_scheme = activation_scheme
 
     @classmethod
