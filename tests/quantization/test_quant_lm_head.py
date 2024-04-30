18e699652bc3bff0e5dc40e502bb0aba1e2912d4
Qubitium <417764+Qubitium@users.noreply.github.com>
2024-04-30 02:30:37 +0000

    format

--- tests/quantization/test_quant_lm_head.py
+++ tests/quantization/test_quant_lm_head.py
@@ -8,9 +8,8 @@ import pytest
 import torch
 
 from vllm import CompletionOutput, SamplingParams
-from vllm.model_executor.layers.quantization.gptq import GPTQLinearMethod
-
 from vllm.model_executor.layers.linear import UnquantizedLinearMethod
+from vllm.model_executor.layers.quantization.gptq import GPTQLinearMethod
 
 LM_HEAD_TRUE_MODEL = ("LnL-AI/TinyLlama-1.1B-intermediate-step-1341k-3T-"
                       "autoround-lm_head-symFalse")
@@ -32,18 +31,21 @@ MAX_TOKENS = 20
 
 @pytest.mark.parametrize("model_quant_type", MODELS_QUANT_TYPE)
 def test_lm_head(
-        vllm_runner,
-        model_quant_type: str,
+    vllm_runner,
+    model_quant_type: str,
 ) -> None:
     model, expected_output = model_quant_type
-    vllm_model = vllm_runner(model, dtype=torch.float16, enforce_eager=True
-                             , enable_prefix_caching=True
-                             , gpu_memory_utilization=0.5
-                             , max_model_len=2048
-                             , seed=898
-                             , tensor_parallel_size=1)
-    print("vllm_model", type(vllm_model),
-          vllm_model.model.llm_engine.model_executor.driver_worker.model_runner.model.lm_head.linear_method)
+    vllm_model = vllm_runner(model,
+                             dtype=torch.float16,
+                             enforce_eager=True,
+                             enable_prefix_caching=True,
+                             gpu_memory_utilization=0.5,
+                             max_model_len=2048,
+                             seed=898,
+                             tensor_parallel_size=1)
+    print(
+        "vllm_model", type(vllm_model), vllm_model.model.llm_engine.
+        model_executor.driver_worker.model_runner.model.lm_head.linear_method)
     llm_engine = vllm_model.model.llm_engine
 
     quantization_config = llm_engine.model_config.hf_config.quantization_config
@@ -51,8 +53,8 @@ def test_lm_head(
     if quantization_config.get("lm_head"):
         quant_lm_head = True
 
-    lm_head_layer = (vllm_model.model.llm_engine.model_executor.driver_worker
-                     .model_runner.model.lm_head)
+    lm_head_layer = (vllm_model.model.llm_engine.model_executor.driver_worker.
+                     model_runner.model.lm_head)
 
     if quant_lm_head:
         assert isinstance(lm_head_layer.linear_method, GPTQLinearMethod)
@@ -69,8 +71,8 @@ def test_lm_head(
     output: Optional[CompletionOutput] = None
     output_text = ""
     while llm_engine.has_unfinished_requests():
-        (request_output,) = llm_engine.step()
-        (output,) = request_output.outputs
+        (request_output, ) = llm_engine.step()
+        (output, ) = request_output.outputs
 
         # Ensure we don't backtrack
         assert output.text.startswith(output_text)
