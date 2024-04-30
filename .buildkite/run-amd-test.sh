5f6b49a5cca5cfe8d6867270fc142583dcf0df2b
simon-mo <simon.mo@hey.com>
2024-04-30 16:51:48 -0700

    add back container cleanup

--- .buildkite/run-amd-test.sh
+++ .buildkite/run-amd-test.sh
@@ -2,7 +2,10 @@
 set -ex
 
 # Print ROCm version
+echo "--- ROCm info"
 rocminfo
+
+echo "--- Resetting GPUs"
 
 echo "reset" > /opt/amdgpu/etc/gpu_state
 
@@ -14,14 +17,27 @@ while true; do
         fi
 done
 
+echo "--- Building container"
 sha=$(git rev-parse --short HEAD)
 container_name=rocm_${sha}
-docker build -t ${container_name} -f Dockerfile.rocm .
+docker build \
+        -t ${container_name} \
+        -f Dockerfile.rocm \
+        --progress plain \
+        .
 
+remove_docker_container() {
+   docker rm -f ${container_name} || docker image rm -f ${container_name} || true
+}
+trap remove_docker_container EXIT
+
+echo "--- Running container"
 docker run \
         --device /dev/kfd --device /dev/dri \
         --network host \
         --rm \
         -e HF_TOKEN \
+        --name ${container_name} \
         ${container_name} \
         /bin/bash -c "$1"
+
