2ac0ec708bb6f44d8d83a1f07074655473929559
zhaoyang-star <zhaoyangstar@foxmail.com>
2024-05-02 14:56:45 +0000

    fix isort error

--- vllm/config.py
+++ vllm/config.py
@@ -11,7 +11,7 @@ from vllm.logger import init_logger
 from vllm.model_executor.layers.quantization import (QUANTIZATION_METHODS,
                                                      get_quantization_config)
 from vllm.transformers_utils.config import get_config, get_hf_text_config
-from vllm.utils import (get_cpu_memory, is_cpu, is_hip, is_neuron)
+from vllm.utils import get_cpu_memory, is_cpu, is_hip, is_neuron
 
 GPTQMarlinConfig = get_quantization_config("gptq_marlin")
 
