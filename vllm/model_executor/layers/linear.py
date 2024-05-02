75757d54c7143964c0bf2400bd774295331b8f71
Dipika Sikka <dipikasikka1@gmail.com>
2024-05-02 01:12:14 +0000

    update shape change

--- vllm/model_executor/layers/linear.py
+++ vllm/model_executor/layers/linear.py
@@ -460,11 +460,12 @@ class MergedColumnParallelLinear(ColumnParallelLinear):
                     "MergedColumnParallelLinear, assume the weight is "
                     "the same for all partitions.")
 
-        if len(param_data.shape) == 0:
-            param_data = param_data.reshape(1)
+        if fp8_scales_shard_indexer is None:
+            if len(param_data.shape) == 0:
+                param_data = param_data.reshape(1)
 
-        if len(loaded_weight.shape) == 0:
-            loaded_weight = loaded_weight.reshape(1)
+            if len(loaded_weight.shape) == 0:
+                loaded_weight = loaded_weight.reshape(1)
 
         assert param_data.shape == loaded_weight.shape
         param_data.copy_(loaded_weight)
@@ -758,7 +759,8 @@ class RowParallelLinear(LinearBase):
             param_data, loaded_weight = fp8_scales_shard_indexer(param_data,
                                                                  loaded_weight,
                                                                  shard_id=0)
-        if len(loaded_weight.shape) == 0:
+
+        if fp8_scales_shard_indexer is None and len(loaded_weight.shape) == 0:
             loaded_weight = loaded_weight.reshape(1)
 
         assert param_data.shape == loaded_weight.shape
