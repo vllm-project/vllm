c7a2f0cbb7ba7ffbfd9db93d224520db0b84ccd0
youkaichao <youkaichao@126.com>
2024-05-03 16:43:13 -0700

    fix import

--- vllm/distributed/communication_op.py
+++ vllm/distributed/communication_op.py
@@ -24,8 +24,7 @@ def tensor_model_parallel_all_reduce(input_: torch.Tensor) -> torch.Tensor:
     """
     from vllm.distributed.device_communicators.custom_all_reduce import (
         custom_all_reduce)
-    from vllm.distributed.device_communicators.parallel_state import (
-        _TP_PYNCCL_COMMUNICATOR)
+    from vllm.distributed.parallel_state import _TP_PYNCCL_COMMUNICATOR
 
     # Bypass the function if we are using only 1 GPU.
     if get_tensor_model_parallel_world_size() == 1:
