22e9bb8d8bcee1d099293dba3dad2d5235ba0377
Michal Moskal <michal@moskal.me>
2024-05-03 20:49:53 +0000

    simplify test

--- tests/core/block/e2e/test_correctness_sliding_window.py
+++ tests/core/block/e2e/test_correctness_sliding_window.py
@@ -1,5 +1,5 @@
 import random
-from typing import Iterable
+from typing import Iterable, List
 
 import pytest
 
@@ -7,6 +7,42 @@ from vllm import LLM, SamplingParams
 
 # relatively small model with 4k sliding window
 MODEL = "bigcode/starcoder2-3b"
+
+
+# the prompt is just under 10k tokens; sliding window is 4k
+# so the answer is outside sliding window, but should still be correct
+def prep_prompts(batch_size: int):
+    prompts: List[str] = []
+    answer: List[int] = []
+    indices: List[int] = []
+    random.seed(1)
+    for _ in range(batch_size):
+        idx = random.randint(30, 90)
+        indices.append(idx)
+        prompt = "```python\n# We set a number of variables, " + \
+                 f"x{idx} will be important later\n"
+        ln = random.randint(800, 1100)
+        for k in range(30, ln):
+            v = random.randint(10, 99)
+            if k == idx:
+                answer.append(v)
+            prompt += f"x{k} = {v}\n"
+        prompt += f"# Now, we check the value of x{idx}:\n"
+        prompt += f"assert x{idx} == "
+        prompts.append(prompt)
+    return prompts, answer, indices
+
+
+def check_answers(indices: List[int], answer: List[int], outputs: List[str]):
+    answer2 = [int(text[0:2].strip()) for text in outputs]
+    print(list(zip(indices, zip(answer, answer2))))
+    numok = 0
+    for a1, a2 in zip(answer, answer2):
+        if a1 == a2:
+            numok += 1
+    frac_ok = numok / len(answer)
+    print(f"Numok: {numok}/{len(answer)} {frac_ok}")
+    assert frac_ok > 0.7
 
 
 @pytest.mark.parametrize(
@@ -28,58 +64,24 @@ MODEL = "bigcode/starcoder2-3b"
 @pytest.mark.parametrize("seed", [1])
 def test_sliding_window_retrival(baseline_llm_generator, test_llm_generator,
                                  batch_size, seed):
-    output_len = 128
-    temperature = 0.0
-
-    # the prompt is just under 10k tokens; sliding window is 4k
-    # so the answer is outside sliding window, but should still be correct
-
-    prompts = []
-    answer = []
-    indices = []
-    random.seed(seed)
-    for _ in range(batch_size):
-        idx = random.randint(30, 90)
-        indices.append(idx)
-        prompt = "```python\n# We set a number of variables, " + \
-                 f"x{idx} will be important later\n"
-        ln = random.randint(800, 1100)
-        for k in range(30, ln):
-            v = random.randint(10, 99)
-            if k == idx:
-                answer.append(v)
-            prompt += f"x{k} = {v}\n"
-        prompt += f"# Now, we check the value of x{idx}:\n"
-        prompt += f"assert x{idx} == "
-        prompts.append(prompt)
-
     sampling_params = SamplingParams(
-        max_tokens=output_len,
+        max_tokens=128,
         ignore_eos=True,
-        temperature=temperature,
+        temperature=0.0,
     )
+
+    prompts, answer, indices = prep_prompts(batch_size)
 
     print('Getting token ids from block manager v1')
     baseline_texts = get_text_from_llm_generator(baseline_llm_generator,
                                                  prompts, sampling_params)
 
-    answer2 = [int(text[0:2].strip()) for text in baseline_texts]
-    print(list(zip(indices, zip(answer, answer2))))
-    numok = 0
-    for a1, a2 in zip(answer, answer2):
-        if a1 == a2:
-            numok += 1
-    frac_ok = numok / len(answer)
-    print(f"Numok: {numok}/{len(answer)} {frac_ok}")
-    assert frac_ok > 0.7
-
-    answer3 = [int(text[0:2].strip()) for text in baseline_texts]
+    check_answers(indices, answer, baseline_texts)
 
     print('Getting token ids from block manager v2')
     test_texts = get_text_from_llm_generator(test_llm_generator, prompts,
                                              sampling_params)
-
-    print(list(zip(indices, zip(answer2, answer3))))
+    check_answers(indices, answer, test_texts)
 
     for expected_text, actual_text in zip(baseline_texts, test_texts):
         assert expected_text == actual_text
@@ -103,51 +105,19 @@ def test_sliding_window_retrival(baseline_llm_generator, test_llm_generator,
 @pytest.mark.parametrize("batch_size", [5])
 @pytest.mark.parametrize("seed", [1])
 def test_sliding_window_chunked_prefill(test_llm_generator, batch_size, seed):
-    output_len = 128
-    temperature = 0.0
-
-    # the prompt is just under 10k tokens; sliding window is 4k
-    # so the answer is outside sliding window, but should still be correct
-
-    prompts = []
-    answer = []
-    indices = []
-    random.seed(seed)
-    for _ in range(batch_size):
-        idx = random.randint(30, 90)
-        indices.append(idx)
-        prompt = "```python\n# We set a number of variables, " + \
-                 f"x{idx} will be important later\n"
-        ln = random.randint(800, 1100)
-        for k in range(30, ln):
-            v = random.randint(10, 99)
-            if k == idx:
-                answer.append(v)
-            prompt += f"x{k} = {v}\n"
-        prompt += f"# Now, we check the value of x{idx}:\n"
-        prompt += f"assert x{idx} == "
-        prompts.append(prompt)
-
     sampling_params = SamplingParams(
-        max_tokens=output_len,
+        max_tokens=10,
         ignore_eos=True,
-        temperature=temperature,
+        temperature=0.0,
     )
+
+    prompts, answer, indices = prep_prompts(batch_size)
 
     # We don't compare with the baseline model here, since the results
     # slightly different due to different tailing in attention.
     test_texts = get_text_from_llm_generator(test_llm_generator, prompts,
                                              sampling_params)
-
-    answer2 = [int(text[0:2].strip()) for text in test_texts]
-    print(list(zip(indices, zip(answer, answer2))))
-    numok = 0
-    for a1, a2 in zip(answer, answer2):
-        if a1 == a2:
-            numok += 1
-    frac_ok = numok / len(answer)
-    print(f"Numok: {numok}/{len(answer)} {frac_ok}")
-    assert frac_ok > 0.7
+    check_answers(indices, answer, test_texts)
 
 
 def get_text_from_llm_generator(llm_generator: Iterable[LLM], prompts,
