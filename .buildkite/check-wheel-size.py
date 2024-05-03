9210e417449e527b9ae3a861180634230deaa031
simon-mo <simon.mo@hey.com>
2024-05-03 10:24:51 -0700

    address comments

--- .buildkite/check-wheel-size.py
+++ .buildkite/check-wheel-size.py
@@ -1,6 +1,6 @@
 import os
 
-MAX_SIZE = 100 * 1024 * 1024  # 100 MB
+MAX_SIZE_MB = 100
 
 
 def check_wheel_size(directory):
@@ -9,10 +9,11 @@ def check_wheel_size(directory):
             if f.endswith(".whl"):
                 wheel_path = os.path.join(root, f)
                 wheel_size = os.path.getsize(wheel_path)
-                if wheel_size > MAX_SIZE:
+                wheel_size_mb = wheel_size / 1024 * 1024
+                if wheel_size > MAX_SIZE_MB:
                     print(
-                        f"Wheel {wheel_path} is too large ({wheel_size} bytes) "
-                        f"compare to the allowed size ({MAX_SIZE} bytes).")
+                        f"Wheel {wheel_path} is too large ({wheel_size_mb} MB) "
+                        f"compare to the allowed size ({MAX_SIZE_MB} MB).")
                     return 1
     return 0
 
