2bb33609d804055fe295da7fc3a146940e4f0901
Mor Zusman <morz@ai21.com>
2024-05-05 18:42:42 +0300

    Align with master

--- vllm/model_executor/__init__.py
+++ vllm/model_executor/__init__.py
@@ -1,7 +1,7 @@
-from vllm.model_executor.mamba_metadata import MambaCacheParams, RequestInfo
 from vllm.model_executor.sampling_metadata import SamplingMetadata
 from vllm.model_executor.utils import set_random_seed
 
 __all__ = [
-    "SamplingMetadata", "set_random_seed", "MambaCacheParams", "RequestInfo"
+    "SamplingMetadata",
+    "set_random_seed",
 ]
