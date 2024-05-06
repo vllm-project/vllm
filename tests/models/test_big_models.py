a85a14533543b49758ac6c8c5b0cd913b0a7ad30
sang <rkooo567@gmail.com>
2024-05-06 15:55:57 -0700

    add  llama 3 test

--- tests/models/test_big_models.py
+++ tests/models/test_big_models.py
@@ -8,6 +8,7 @@ import pytest
 
 MODELS = [
     "meta-llama/Llama-2-7b-hf",
+    "meta-llama/Meta-Llama-3-8B-Instruct",
     # "mistralai/Mistral-7B-v0.1",  # Broken
     # "Deci/DeciLM-7b",  # Broken
     # "tiiuae/falcon-7b",  # Broken
