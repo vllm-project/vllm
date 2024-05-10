8a8bb1cd7ea2b0dd4e8882f61294435aa9c2f96a
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2024-05-10 22:04:24 +0000

    Fix CPU

--- vllm/worker/cpu_model_runner.py
+++ vllm/worker/cpu_model_runner.py
@@ -53,7 +53,15 @@ class CPUModelRunner:
         self.kv_cache_dtype = kv_cache_dtype
         self.sliding_window = model_config.get_sliding_window()
         self.block_size = cache_config.block_size
-        self.attn_backend = get_attn_backend(self.model_config.dtype)
+        self.attn_backend = get_attn_backend(
+            self.model_config.get_num_attention_heads(self.parallel_config),
+            self.model_config.get_head_size(),
+            self.model_config.get_num_kv_heads(self.parallel_config),
+            self.model_config.get_sliding_window(),
+            self.model_config.dtype,
+            self.kv_cache_dtype,
+            self.block_size,
+        )
 
         # Lazy initialization.
         self.model: nn.Module  # Set after init_Model
