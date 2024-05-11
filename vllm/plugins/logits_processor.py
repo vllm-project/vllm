e6431cc9a58f66ca5097311da8c07694b4079ca9
NadavShmayo <nadavs2310123@gmail.com>
2024-05-11 18:48:08 +0300

    Add plugin contracts package with logits processor file

---
+++ vllm/plugins/logits_processor.py
@@ -0,0 +1,14 @@
+from typing import Callable, Type
+
+from pydantic import BaseModel
+from transformers import PreTrainedTokenizer
+
+from vllm.sampling_params import LogitsProcessor
+
+
+class LogitsProcessorPlugin:
+    def __init__(self,
+                 logits_processor_class: Callable[[PreTrainedTokenizer, BaseModel], LogitsProcessor],
+                 parameters_model: Type[BaseModel]):
+        self.logits_processor_class = logits_processor_class
+        self.parameters_model = parameters_model
