ff2f1c8386930930b70888206b246c94572fa22e
hoshi-hiyouga <hiyouga@buaa.edu.cn>
2024-05-15 19:09:37 +0800

    Update loader.py

--- vllm/model_executor/model_loader/loader.py
+++ vllm/model_executor/model_loader/loader.py
@@ -25,10 +25,9 @@ from vllm.model_executor.model_loader.weight_utils import (
     download_weights_from_hf, filter_files_not_needed_for_inference,
     get_quant_config, initialize_dummy_weights, np_cache_weights_iterator,
     pt_weights_iterator, safetensors_weights_iterator)
-from vllm.model_executor.models.llava import LlavaForConditionalGeneration
 
 _VISION_MODEL_CLASSES = [
-    LlavaForConditionalGeneration,
+    "LlavaForConditionalGeneration",
 ]
 
 logger = init_logger(__name__)
@@ -72,7 +71,7 @@ def _get_model_initialization_kwargs(
             "but LoRA is enabled. Support for this model may "
             "be added in the future. If this is important to you, "
             "please open an issue on github.")
-    elif model_class in _VISION_MODEL_CLASSES:
+    elif model_class.__name__ in _VISION_MODEL_CLASSES:
         extra_kwargs["vision_language_config"] = vision_language_config
     return extra_kwargs
 
