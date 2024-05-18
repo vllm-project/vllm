f6de88ca91fad9bde2f58ac0b5c3e37c2feda18d
Alexei V. Ivanov <alexei.ivanov@amd.com>
2024-05-18 00:36:27 +0000

    .

--- tests/spec_decode/e2e/conftest.py
+++ tests/spec_decode/e2e/conftest.py
@@ -9,7 +9,8 @@ import torch
 from vllm.utils import is_hip
 
 if (not is_hip()):
-    from pynvml import nvmlDeviceGetHandleByIndex, nvmlDeviceGetMemoryInfo, nvmlInit
+    from pynvml import nvmlDeviceGetHandleByIndex
+    from pynvml import nvmlDeviceGetMemoryInfo, nvmlInit
 
 from vllm import LLM
 from vllm.engine.arg_utils import AsyncEngineArgs
