b4bb69e5db647a90323e5de4c250b054cf3a53ae
Cyrus Leung <tlleungac@connect.ust.hk>
2024-05-19 15:13:33 +0800

    [Bugfix][Model] Add base class for vision-language models (#4809)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

---
+++ tests/models/test_registry.py
@@ -0,0 +1,9 @@
+import pytest
+
+from vllm.model_executor.models import _MODELS, ModelRegistry
+
+
+@pytest.mark.parametrize("model_cls", _MODELS)
+def test_registry_imports(model_cls):
+    # Ensure all model classes can be imported successfully
+    ModelRegistry.load_model_cls(model_cls)
