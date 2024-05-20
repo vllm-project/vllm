efb0599738262bab84bfb98f927c95343c8cb439
Joshua Rosenkranz <joshua.rosenkranz@gmail.com>
2024-05-20 15:27:05 -0400

    initial commit of mlp_speculator and hidden_states_worker to support MLPSpeculator from fms

---
+++ vllm/model_executor/models/mlp_speculator.py
@@ -0,0 +1,137 @@
+from typing import Optional, List, Iterable, Tuple
+
+import torch.nn as nn
+import torch
+import math
+from fms_extras.models.hf.modeling_mlp_speculator import MLPSpeculatorConfig
+from vllm.attention import AttentionMetadata
+from vllm.model_executor import SamplingMetadata
+from vllm.model_executor.layers.sampler import Sampler
+from vllm.model_executor.layers.vocab_parallel_embedding import VocabParallelEmbedding
+from vllm.model_executor.model_loader.weight_utils import default_weight_loader
+from vllm.sampling_params import LogitsProcessor
+from vllm.sequence import SamplerOutput
+
+
+class MLPSpeculatorLayerNorm(nn.Module):
+    """
+    A L2 normalization implementation
+    ...
+    Args
+    ----
+    normalized_shape : int
+        Dimensionality of input data (size of final tensor axis)
+    elementwise_scale_weight : torch.Tensor
+        learned scaling term after normalization?
+    elementwise_shift_bias : torch.Tensor
+        learned bias term after normalization?
+    eps : float
+        Safety term to prevent division by zero. Make sure the chosen value fits in the range of your encoding scheme (i.e. fp16 requires eps >= 6e-8).
+    """
+
+    def __init__(
+        self,
+        normalized_shape,
+        eps=1e-06,
+    ):
+        super(MLPSpeculatorLayerNorm, self).__init__()
+        self.weight = nn.Parameter(torch.empty(normalized_shape))
+        self.bias = nn.Parameter(torch.empty(normalized_shape))
+        self.eps = eps
+
+    def forward(self, x):
+        xf = x
+        xf = xf * torch.rsqrt(xf.pow(2).mean(-1, keepdim=True) + self.eps)
+        x = xf.type_as(x)
+        x = self.weight * x
+        x = x + self.bias
+        return x
+
+class MLPSpeculator(nn.Module):
+    def __init__(
+        self,
+        config: MLPSpeculatorConfig,
+    ) -> None:
+        super().__init__()
+        self.n_predict = config.n_predict
+        self.vocab_size = config.vocab_size
+        self.emb_dim = config.emb_dim
+        self.inner_dim = config.inner_dim if config.inner_dim != 0 else config.emb_dim
+        self.emb = nn.ModuleList([
+                VocabParallelEmbedding(config.vocab_size, self.inner_dim, org_num_embeddings=config.vocab_size)
+                for _ in range(config.n_predict)
+        ])
+
+        self.proj = nn.ModuleList([
+            nn.Linear((self.emb_dim if i == 0 else self.inner_dim), self.inner_dim, bias=False)
+            for i in range(config.n_predict)
+        ])
+
+        self.head = nn.ModuleList([nn.Linear(self.inner_dim, self.vocab_size, bias=False) for _ in range(config.n_predict)])
+        self.ln = nn.ModuleList([MLPSpeculatorLayerNorm(self.inner_dim) for _ in range(config.n_predict)])
+
+        self.state_weight = 0.5 ** (0.5 / config.n_predict)
+        self.emb_weight = math.sqrt((1 - self.state_weight ** 2) * (self.inner_dim / 2))
+        self.activation = nn.GELU()
+        self.config = config
+        self.logits_processor = LogitsProcessor(config.vocab_size, config.vocab_size, 1.0)
+        self.sampler = Sampler()
+
+    def forward(
+        self,
+        input_ids: torch.Tensor,
+        positions: torch.Tensor,
+        kv_caches: List[torch.Tensor],
+        attn_metadata: AttentionMetadata,
+    ) -> torch.Tensor:
+        # prune the hidden states
+        if self.current_head_index == 0:
+            accepted_tokens_len = positions.max(dim=1)[0] + 1  # add 1 for length
+            if self.first_decode_step:
+                self.prev_context_lengths = accepted_tokens_len
+                self.first_decode_step = False
+            else:
+                accepted_tokens_len -= self.prev_context_lengths
+
+            self.previous_hidden_state = self.previous_hidden_state.gather(
+                1,
+                (accepted_tokens_len - 1)[:, None, None].expand(-1, 1, self.previous_hidden_state.size(2))
+            ) # b x 1 x d
+
+        # Project and predict
+        z = self.emb[self.current_head_index](input_ids[:, -1])  # b k d
+        state = self.proj[self.current_head_index](self.previous_hidden_state)
+        # Weighted add of state_weight*state and emb_weight*z
+        # Let subsequent LN take care of denominator
+        # state_weight is close to 1, so shouldn't be any precision issues
+        state = torch.add(state, z, alpha=self.emb_weight / self.state_weight)
+        state = self.activation(self.ln[self.current_head_index](state))  # b k d
+
+        # todo: not yet supporting top_k_tokens_per_head
+
+        self.previous_hidden_state = state
+        self.current_head_index += 1
+        return state
+
+    def compute_logits(self, hidden_states: torch.Tensor,
+                       sampling_metadata: SamplingMetadata) -> torch.Tensor:
+        current_head_index = self.current_head_index - 1
+        logits = self.logits_processor(self.head[current_head_index].weight, hidden_states,
+                                       sampling_metadata)
+        return logits
+
+    def sample(
+        self,
+        logits: torch.Tensor,
+        sampling_metadata: SamplingMetadata,
+    ) -> Optional[SamplerOutput]:
+        next_tokens = self.sampler(logits, sampling_metadata)
+        return next_tokens
+
+    def load_weights(self, weights: Iterable[Tuple[str, torch.Tensor]]):
+        params_dict = dict(self.named_parameters())
+        for name, loaded_weight in weights:
+            param = params_dict[name]
+            weight_loader = getattr(param, "weight_loader",
+                                    default_weight_loader)
+            weight_loader(param, loaded_weight)
---
+++ vllm/spec_decode/hidden_states_worker.py
@@ -0,0 +1,79 @@
+from typing import List, Optional
+
+from vllm.model_executor.models.mlp_speculator import MLPSpeculator
+from vllm.sequence import SequenceGroupMetadata, ExecuteModelRequest, SamplerOutput
+from vllm.worker.worker import Worker
+import torch
+
+class HiddenStatesWorker(Worker):
+
+    def __init__(self, speculator: MLPSpeculator, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.speculator = speculator
+
+    def _get_hidden_states(
+        self,
+        seq_group_metadata_list: List[SequenceGroupMetadata],
+        kv_caches: List[torch.Tensor],
+    ):
+
+        (input_tokens, input_positions, attn_metadata, sampling_metadata,
+         lora_requests, lora_mapping, multi_modal_input
+         ) = self.model_runner.prepare_input_tensors(seq_group_metadata_list)
+
+        if self.model_runner.lora_config:
+            self.model_runner.set_active_loras(lora_requests, lora_mapping)
+
+        # Currently cuda graph is only supported by the decode phase.
+        prefill_meta = attn_metadata.prefill_metadata
+        decode_meta = attn_metadata.decode_metadata
+        if prefill_meta is None and decode_meta.use_cuda_graph:
+            graph_batch_size = input_tokens.shape[0]
+            model_executable = self.model_runner.graph_runners[graph_batch_size]
+        else:
+            model_executable = self.model_runner.model
+        execute_model_kwargs = {
+            "input_ids": input_tokens,
+            "positions": input_positions,
+            "kv_caches": kv_caches,
+            "attn_metadata": attn_metadata,
+        }
+        if self.vision_language_config:
+            execute_model_kwargs.update({"image_input": multi_modal_input})
+
+        # save the previous hidden states for later use
+        hidden_states = model_executable(**execute_model_kwargs)
+
+        # Compute the logits.
+        logits = self.model_runner.model.compute_logits(hidden_states, sampling_metadata)
+
+        # Only perform sampling in the driver worker.
+        if not self.model_runner.is_driver_worker:
+            return None
+
+        # Sample the next token.
+        output = self.model_runner.model.sample(
+            logits=logits,
+            sampling_metadata=sampling_metadata,
+        )
+
+        return output, hidden_states
+
+
+    @torch.inference_mode()
+    def execute_model(
+            self,
+            execute_model_req: Optional[ExecuteModelRequest] = None
+    ) -> List[SamplerOutput]:
+        # if we are executing the prompt, we need to flag the first decode step since pruning is handled differently
+        if execute_model_req.seq_group_metadata_list[0].is_prompt:
+            self.speculator.first_decode_step = True
+
+        # reset the head to call in speculator
+        self.speculator.current_head_index = 0
+
+        sampler_output, hidden_states = self._get_hidden_states(execute_model_req.seq_group_metadata_list, self.gpu_cache)
+
+        # set the previous hidden states based on the output hidden states of the base model
+        self.speculator.previous_hidden_state = hidden_states
+        return sampler_output
