6c293610cb625659eb54d946c2ee5d8f129043fc
Tyler Michael Smith <tyler@neuralmagic.com>
2024-05-21 16:10:46 +0000

    Pass cuda streams into CUTLASS

--- csrc/quantization/cutlass_w8a8/scaled_mm_dq_c3x.cu
+++ csrc/quantization/cutlass_w8a8/scaled_mm_dq_c3x.cu
@@ -1,5 +1,7 @@
 #include <torch/extension.h>
 
+#include <ATen/cuda/CUDAContext.h>
+
 #include <iostream>
 #include <sstream>
 #include <vector>
@@ -170,6 +172,8 @@ void cutlass_scaled_mm_dq_dispatcher(torch::Tensor &out, torch::Tensor const &a,
 
   args.epilogue.thread = {a_args, {b_args}};
 
+  auto stream = at::cuda::getCurrentCUDAStream(a.get_device());
+
   // Launch the CUTLASS GEMM kernel.
   using GemmOp = cutlass::gemm::device::GemmUniversalAdapter<GemmKernel>;
   GemmOp gemm_op;
@@ -178,7 +182,7 @@ void cutlass_scaled_mm_dq_dispatcher(torch::Tensor &out, torch::Tensor const &a,
   size_t workspace_size = gemm_op.get_workspace_size(args);
   TORCH_CHECK(workspace_size == 0);
 
-  cutlass::Status status = gemm_op.run(args);
+  cutlass::Status status = gemm_op.run(args, stream);
   CUTLASS_CHECK(status);
 }
 }  // namespace
