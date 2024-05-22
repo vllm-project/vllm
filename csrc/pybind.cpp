5c5dc84f2fc9a340d1bd34ff66ee018a25bea6e1
Dipika Sikka <dipikasikka1@gmail.com>
2024-05-22 14:42:07 +0000

    clang format again

--- csrc/pybind.cpp
+++ csrc/pybind.cpp
@@ -67,9 +67,8 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
           "Aligning the number of tokens to be processed by each expert such "
           "that it is divisible by the block size.");
 
-  ops.def(
-    "static_scaled_int8_quant", &static_scaled_int8_quant, "Compute int8 quantized tensor for given scaling factor");
-
+  ops.def("static_scaled_int8_quant", &static_scaled_int8_quant,
+          "Compute int8 quantized tensor for given scaling factor");
 
   // Cache ops
   pybind11::module cache_ops = m.def_submodule("cache_ops", "vLLM cache ops");
