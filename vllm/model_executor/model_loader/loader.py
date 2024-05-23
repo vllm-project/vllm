c76ae569edc2226700dbfab01c53a89741250ce1
Robert Shaw <114415538+robertgshaw2-neuralmagic@users.noreply.github.com>
2024-05-24 00:26:58 +0200

    Update loader.py

--- vllm/model_executor/model_loader/loader.py
+++ vllm/model_executor/model_loader/loader.py
@@ -194,7 +194,7 @@ class DefaultModelLoader(BaseModelLoader):
             # there are both sharded safetensors files and a consolidated
             # safetensors file. Using both in the weight_loader breaks.
             # Here, we download the `model.safetensors.index.json` and filter
-            # any safetensors files not found.
+            # any safetensors files not found in the index.
             if not is_local:
                 download_safetensors_index_file_from_hf(
                     model_name_or_path, self.load_config.download_dir,
