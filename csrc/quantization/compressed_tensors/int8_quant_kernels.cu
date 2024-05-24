7a83601cfe57c3d7d7a7b3f49fbb4a8231f6832e
Varun Sundar Rabindranath <varun@neuralmagic.com>
2024-05-24 20:52:58 +0000

    clang-format

--- csrc/quantization/compressed_tensors/int8_quant_kernels.cu
+++ csrc/quantization/compressed_tensors/int8_quant_kernels.cu
@@ -5,7 +5,6 @@
 #include "../../dispatch_utils.h"
 #include "../../reduction_utils.cuh"
 
-
 static inline __device__ int8_t float_to_int8_rn(float x) {
 #ifdef USE_ROCM
   static const float i8_min =
@@ -40,14 +39,10 @@ __global__ void static_scaled_int8_quant_kernel(
   }
 }
 
-
 template <typename scalar_t, typename scale_type>
 __global__ void dynamic_scaled_int8_quant_kernel(
-  const scalar_t* __restrict__ input,
-  int8_t* __restrict__ out,
-  scale_type scale,
-  const int hidden_size) {
-
+    const scalar_t* __restrict__ input, int8_t* __restrict__ out,
+    scale_type scale, const int hidden_size) {
   const int tid = threadIdx.x;
   const int token_idx = blockIdx.x;
 
@@ -57,8 +52,7 @@ __global__ void dynamic_scaled_int8_quant_kernel(
   for (int i = tid; i < hidden_size; i += blockDim.x) {
     float val = (float)input[token_idx * hidden_size + i];
     val = val > zero ? val : -val;
-    if (val > amax_val)
-      amax_val = val;
+    if (val > amax_val) amax_val = val;
   }
 
   __shared__ float s_amax;
@@ -71,12 +65,10 @@ __global__ void dynamic_scaled_int8_quant_kernel(
 
   float tmp_scale = 127.0f / s_amax;
   for (int i = tid; i < hidden_size; i += blockDim.x) {
-    out[token_idx * hidden_size + i] =
-        float_to_int8_rn(((float)input[token_idx * hidden_size + i]) * tmp_scale);
+    out[token_idx * hidden_size + i] = float_to_int8_rn(
+        ((float)input[token_idx * hidden_size + i]) * tmp_scale);
   }
 }
-
-
 
 void static_scaled_int8_quant(torch::Tensor& out,    // [..., hidden_size]
                               torch::Tensor& input,  // [..., hidden_size]
@@ -97,11 +89,9 @@ void static_scaled_int8_quant(torch::Tensor& out,    // [..., hidden_size]
       });
 }
 
-
-void dynamic_scaled_int8_quant(
-  torch::Tensor& out,   // [..., hidden_size]
-  torch::Tensor& input, // [..., hidden_size]
-  torch::Tensor& scales) {
+void dynamic_scaled_int8_quant(torch::Tensor& out,    // [..., hidden_size]
+                               torch::Tensor& input,  // [..., hidden_size]
+                               torch::Tensor& scales) {
   TORCH_CHECK(input.is_contiguous());
   TORCH_CHECK(out.is_contiguous());
   int hidden_size = input.size(-1);
@@ -109,12 +99,11 @@ void dynamic_scaled_int8_quant(
   dim3 grid(num_tokens);
   dim3 block(std::min(hidden_size, 1024));
   const cudaStream_t stream = at::cuda::getCurrentCUDAStream();
-  VLLM_DISPATCH_FLOATING_TYPES(input.scalar_type(), "dynamic_scaled_int8_quant_kernel", [&] {
-    vllm::dynamic_scaled_int8_quant_kernel<scalar_t, float*><<<grid, block, 0, stream>>>(
-      input.data_ptr<scalar_t>(),
-      out.data_ptr<int8_t>(),
-      scales.data_ptr<float>(),
-      hidden_size);
-  });
+  VLLM_DISPATCH_FLOATING_TYPES(
+      input.scalar_type(), "dynamic_scaled_int8_quant_kernel", [&] {
+        vllm::dynamic_scaled_int8_quant_kernel<scalar_t, float*>
+            <<<grid, block, 0, stream>>>(input.data_ptr<scalar_t>(),
+                                         out.data_ptr<int8_t>(),
+                                         scales.data_ptr<float>(), hidden_size);
+      });
 }
-
