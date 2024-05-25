d4357329583efb20c0aca42f77a649c1f5de3000
Bill Nell <bill@neuralmagic.com>
2024-05-25 20:31:43 +0000

    fixes

--- csrc/moe/moe_ops.cpp
+++ csrc/moe/moe_ops.cpp
@@ -6,5 +6,11 @@
 
 TORCH_LIBRARY_EXPAND(TORCH_EXTENSION_NAME, m) {
   // Apply topk softmax to the gating outputs.
+  m.def("topk_softmax(Tensor topk_weights, Tensor topk_indices, Tensor token_expert_indices, Tensor gating_output) -> ()");
   m.impl("topk_softmax", torch::kCUDA, &topk_softmax);
+}
+
+// TODO: get rid of this
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
+{
 }
