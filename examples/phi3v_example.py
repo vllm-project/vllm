591a9b89d155679ea3bac0f67cd8a932d1e478be
Isotr0py <2037008807@qq.com>
2024-05-26 14:46:28 +0800

    format phi3v_example

--- examples/phi3v_example.py
+++ examples/phi3v_example.py
@@ -1,28 +1,27 @@
-import requests
+import os
+import subprocess
+
 from PIL import Image
 from transformers import AutoProcessor
 
-from vllm import LLM
+from vllm import LLM, SamplingParams
 from vllm.sequence import MultiModalData
 
 
 def run_phi3v():
-    model_path = "/data/LLM-model/Phi-3-vision-128k-instruct"
+    model_path = "microsoft/Phi-3-vision-128k-instruct"
     processor = AutoProcessor.from_pretrained(model_path,
                                               trust_remote_code=True)
     llm = LLM(
         model=model_path,
         trust_remote_code=True,
-        dtype='bfloat16',
-        max_model_len=4096,
         image_input_type="pixel_values",
         image_token_id=-1,
         image_input_shape="1008, 1344",
         image_feature_size=1024,
     )
 
-    url = "https://www.ilankelman.org/stopsigns/australia.jpg"
-    image = Image.open(requests.get(url, stream=True).raw)
+    image = Image.open("images/stop_sign.jpg")
     user_prompt = '<|user|>\n'
     assistant_prompt = '<|assistant|>\n'
     suffix = "<|end|>\n"
@@ -33,14 +32,29 @@ def run_phi3v():
     inputs = processor(prompt, image, return_tensors="pt")
     multi_modal_data = MultiModalData(type=MultiModalData.Type.IMAGE,
                                       data=inputs["pixel_values"])
-
+    sampling_params = SamplingParams(temperature=0, max_tokens=64)
     outputs = llm.generate(prompt_token_ids=inputs["input_ids"].tolist(),
+                           sampling_params=sampling_params,
                            multi_modal_data=multi_modal_data)
-    # outputs = llm.generate(prompt)
     for o in outputs:
         generated_text = o.outputs[0].text
         print(generated_text)
 
 
 if __name__ == "__main__":
+    s3_bucket_path = "s3://air-example-data-2/vllm_opensource_llava/"
+    local_directory = "images"
+
+    # Make sure the local directory exists or create it
+    os.makedirs(local_directory, exist_ok=True)
+
+    # Use AWS CLI to sync the directory, assume anonymous access
+    subprocess.check_call([
+        "aws",
+        "s3",
+        "sync",
+        s3_bucket_path,
+        local_directory,
+        "--no-sign-request",
+    ])
     run_phi3v()
