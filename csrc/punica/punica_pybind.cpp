5ddf7c061695e9d0d930b0a08ba441f6835af074
Bill Nell <bill@neuralmagic.com>
2024-05-26 03:33:15 +0000

    fix punica_pybind signature

--- csrc/punica/punica_pybind.cpp
+++ csrc/punica/punica_pybind.cpp
@@ -8,7 +8,7 @@ TORCH_LIBRARY_EXPAND(TORCH_EXTENSION_NAME, m) {
   m.def("dispatch_bgmv(Tensor y, Tensor x, Tensor w, Tensor indicies, int layer_idx, float scale) -> ()");
   m.impl("dispatch_bgmv", torch::kCUDA, &dispatch_bgmv);
 
-  m.def("dispatch_bgmv_low_level(Tensor y, Tensor x, Tensor w, Tensor indicies, int64_t layer_idx, float scale, int h_in, int h_out, int y_offset) -> ()");
+  m.def("dispatch_bgmv_low_level(Tensor y, Tensor x, Tensor w, Tensor indicies, int layer_idx, float scale, int h_in, int h_out, int y_offset) -> ()");
   m.impl("dispatch_bgmv_low_level", torch::kCUDA, &dispatch_bgmv_low_level);
 }
 
