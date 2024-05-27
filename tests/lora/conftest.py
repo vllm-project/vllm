3000604ae8cb837cd7811e42a0ff48d6329a3edc
Isotr0py <2037008807@qq.com>
2024-05-27 12:45:40 +0800

    add internlm2 lora test

--- tests/lora/conftest.py
+++ tests/lora/conftest.py
@@ -171,6 +171,11 @@ def phi2_lora_files():
 
 
 @pytest.fixture(scope="session")
+def phi2_lora_files():
+    return snapshot_download(repo_id="isotr0py/InternLM2-1_8B-test-sql-lora")
+
+
+@pytest.fixture(scope="session")
 def long_context_lora_files_16k_1():
     return snapshot_download(repo_id="SangBinCho/long_context_16k_testing_1")
 
