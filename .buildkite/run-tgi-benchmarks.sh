0f522ae02cbc5ba12612175f02a977db6ca9fe07
KuntaiDu <kuntai@uchicago.edu>
2024-05-27 21:55:19 +0000

    Kuntai: format the bash script using shfmt

--- .buildkite/run-tgi-benchmarks.sh
+++ .buildkite/run-tgi-benchmarks.sh
@@ -17,7 +17,6 @@ add-apt-repository -y "deb https://developer.download.nvidia.com/compute/cuda/re
 apt-get update
 apt-get install -y cuda-toolkit-12-1
 
-
 # install conda
 (which wget && which curl) || (apt-get update && apt-get install -y wget curl)
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
@@ -26,7 +25,6 @@ bash Miniconda3-latest-Linux-x86_64.sh -b -u -p ~/miniconda3
 # equivalent to `source ~/.bashrc` but is runnable in non-interactive shell
 eval "$(cat ~/.bashrc | tail -n +15)"
 
-
 # create conda environment for tgi
 conda create -n text-generation-inference python=3.11 -y
 eval "$(conda shell.bash hook)"
@@ -34,7 +32,7 @@ conda activate text-generation-inference
 
 # Install tgi
 (which cargo) || (curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && source "$HOME/.cargo/env")
-source "$HOME/.cargo/env" 
+source "$HOME/.cargo/env"
 (which protoc) || (apt install protobuf-compiler -y)
 (which pkg-config) || (apt install pkg-config -y)
 apt install libssl-dev gcc -y
@@ -51,7 +49,7 @@ make -f Makefile-flash-att-v2 install-flash-attention-v2-cuda
 make -f Makefile-vllm build-vllm-cuda
 cd vllm
 pip install . -vvv
-text-generation-launcher --port 8000 --model-id meta-llama/Llama-2-7b-chat-hf  &
+text-generation-launcher --port 8000 --model-id meta-llama/Llama-2-7b-chat-hf &
 tgi_pid=$!
 echo $tgi_pid
 
@@ -65,43 +63,41 @@ cd /vllm-workspace
 git clone https://github.com/vllm-project/vllm.git
 wget https://huggingface.co/datasets/anon8231489123/ShareGPT_Vicuna_unfiltered/resolve/main/ShareGPT_V3_unfiltered_cleaned_split.json
 
-
 # gradually reduce the request rate from 20, untill all request successed
 request_rate=20
 get_successful_requests() {
-    grep "Successful requests:" benchmark_serving.txt | awk '{print $3}'
+  grep "Successful requests:" benchmark_serving.txt | awk '{print $3}'
 }
 # Loop until the successful requests are 1000
 while true; do
-    echo "Running benchmark with request rate $request_rate..."
-    python3 vllm/benchmarks/benchmark_serving.py --backend tgi --model meta-llama/Llama-2-7b-chat-hf --dataset-name sharegpt --dataset-path ./ShareGPT_V3_unfiltered_cleaned_split.json --num-prompts 1000 --endpoint /generate_stream --request-rate $request_rate --port 8000 --save-result 2>&1 | tee benchmark_serving.txt
-    bench_serving_exit_code=$?
-    successful_requests=$(get_successful_requests)
-    echo "Successful requests: $successful_requests"
-    if [ "$successful_requests" -eq 1000 ]; then
-        echo "Reached 1000 successful requests with request rate $request_rate"
-        break
-    fi
-    request_rate=$((request_rate - 1))
-    if [ "$request_rate" -lt 1 ]; then
-        echo "Request rate went below 1. Exiting."
-        break
-    fi
+  echo "Running benchmark with request rate $request_rate..."
+  python3 vllm/benchmarks/benchmark_serving.py --backend tgi --model meta-llama/Llama-2-7b-chat-hf --dataset-name sharegpt --dataset-path ./ShareGPT_V3_unfiltered_cleaned_split.json --num-prompts 1000 --endpoint /generate_stream --request-rate $request_rate --port 8000 --save-result 2>&1 | tee benchmark_serving.txt
+  bench_serving_exit_code=$?
+  successful_requests=$(get_successful_requests)
+  echo "Successful requests: $successful_requests"
+  if [ "$successful_requests" -eq 1000 ]; then
+    echo "Reached 1000 successful requests with request rate $request_rate"
+    break
+  fi
+  request_rate=$((request_rate - 1))
+  if [ "$request_rate" -lt 1 ]; then
+    echo "Request rate went below 1. Exiting."
+    break
+  fi
 done
 kill $tgi_pid
 
-
-echo "### TGI Serving Benchmarks" >> benchmark_results.md
-sed -n '1p' benchmark_serving.txt >> benchmark_results.md # first line
-echo "" >> benchmark_results.md
-echo '```' >> benchmark_results.md
-tail -n 20 benchmark_serving.txt >> benchmark_results.md # last 20 lines
-echo '```' >> benchmark_results.md
+echo "### TGI Serving Benchmarks" >>benchmark_results.md
+sed -n '1p' benchmark_serving.txt >>benchmark_results.md # first line
+echo "" >>benchmark_results.md
+echo '```' >>benchmark_results.md
+tail -n 20 benchmark_serving.txt >>benchmark_results.md # last 20 lines
+echo '```' >>benchmark_results.md
 
 # if the agent binary is not found, skip uploading the results, exit 0
 if [ ! -f /workspace/buildkite-agent ]; then
-    exit 0
+  exit 0
 fi
 
 # upload the results to buildkite
-/workspace/buildkite-agent annotate --style "info" --context "benchmark-results" < benchmark_results.md
+/workspace/buildkite-agent annotate --style "info" --context "benchmark-results" <benchmark_results.md
