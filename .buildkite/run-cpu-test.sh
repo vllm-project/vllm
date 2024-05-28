22d5228b739f097acb389e4d84380102e061242c
Yuan Zhou <yuan.zhou@intel.com>
2024-05-28 15:43:31 +0800

    soft link tests folder
    Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

--- .buildkite/run-cpu-test.sh
+++ .buildkite/run-cpu-test.sh
@@ -14,19 +14,20 @@ remove_docker_container
 docker run -itd -v ~/.cache/huggingface:/root/.cache/huggingface --network host --env VLLM_CPU_KVCACHE_SPACE=4 --name cpu-test cpu-test
 
 # offline inference
-docker exec cpu-test bash -c "python3 vllm/examples/offline_inference.py"
+docker exec cpu-test bash -c "python3 examples/offline_inference.py"
 
 # async engine test, not passing due to distributed inference support missing
 #docker exec cpu-test bash -c "cd tests; pytest -v -s async_engine"
 
 # Run basic model test
-docker exec cpu-test bash -c "cd vllm/tests;
+docker exec cpu-test bash -c "cd tests;
   pip install pytest Pillow
   bash ../.buildkite/download-images.sh
-  pytest -v -s models --ignore=models/test_llava.py --ignore=models/test_mistral.py \
-    --ignore=models/test_big_models.py --ignore=models/test_embedding.py"
+  cd ../
+  pytest -v -s tests/models --ignore=tests/models/test_llava.py --ignore=tests/models/test_mistral.py \
+    --ignore=tests/models/test_big_models.py --ignore=tests/models/test_embedding.py"
 
 # Run big model test
 #docker exec cpu-test bash -c "cd tests;
-#  sed -i 's/half/float/g' models/test_big_models.py
-#  pytest -v -s models/test_big_models.py"
+#  sed -i 's/half/float/g' tests/models/test_big_tests/models.py
+#  pytest -v -s tests/models/test_big_tests/models.py"
