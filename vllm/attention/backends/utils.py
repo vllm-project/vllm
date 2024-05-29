afcb42e125b16a8afa5bb05740783e684b53f6fd
Andrew Feldman <afeldman@neuralmagic.com>
2024-05-29 15:46:39 -0400

    explanatory comment

--- vllm/attention/backends/utils.py
+++ vllm/attention/backends/utils.py
@@ -1,5 +1,8 @@
 """Attention utils"""
 
+# Error string(s) for encoder/decoder
+# unsupported attention scenarios
+
 STR_NOT_IMPL_ENC_DEC_CHUNKED_PREFILL = \
 "Encoder/decoder models " + \
 "currently do not support chunked prefill."
