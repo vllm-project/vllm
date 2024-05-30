ab8eced95a2a379024f95263c132b0be85b25af1
charlifu <charlifu@amd.com>
2024-05-30 19:11:58 +0000

    make tunedgemm using custom kernels for BS=1

--- vllm/model_executor/layers/tuned_gemm.py
+++ vllm/model_executor/layers/tuned_gemm.py
@@ -33,23 +33,6 @@ class TunedGemm:
         if self.tune_path is not None and Path(self.tune_path).is_file():
             self.bestsols = pd.read_csv(self.tune_path)
 
-    def apply_custom(self, ds):
-        M, N, K = ds['M'], ds['N'], ds['K']
-        #apply custom matvec (only for f16 dtype)
-        if N == 1:
-            ds1 = ds.copy()
-            ds1['libtype'] = 'custom'
-            if K == 8192 and (M == 1280 or M == 7168):  #NOQA: SIM114
-                ds1['solidx'] = 8
-                return ds1
-            elif K == 3584 and M == 8192:
-                ds1['solidx'] = 8
-                return ds1
-            elif K <= 8192 and K % 8 == 0 and M % 4 == 0:
-                ds1['solidx'] = 1
-                return ds1
-        return ds
-
     def create_ds(self):
         df = self.bestsols
         solds = {}
@@ -60,8 +43,6 @@ class TunedGemm:
                 soltype = 1
             elif ds['libtype'] == 'rocblas':
                 soltype = 2
-            elif ds['libtype'] == 'custom':
-                soltype = 3
             solds[key] = (soltype, int(ds['solidx']))
         self.solids = solds
         #print('>>>',solds)
@@ -86,27 +67,23 @@ class TunedGemm:
         m = weights.shape[0]
         n = inp_view.shape[0]
         k = inp_view.shape[1]
+        if n == 1 and inp_view.dtype == torch.float16:
+            out = torch.empty(inp_view.shape[0],
+                              weights.shape[0],
+                              dtype=inp.dtype,
+                              device='cuda')
+            if (k == 8192 and
+                (m == 1280 or m == 7168)) or (k == 3584 and m == 8192):
+                _custom_C.LLMM1(weights, inp_view, out, 8)
+            elif k <= 8192 and k % 8 == 0 and m % 4 == 0:
+                _custom_C.LLMM1(weights, inp_view, out, 4)
+            else:
+                out = F.linear(inp_view, weights)
+            return out
         soltype, solidx = self.query_sol(m=m, n=n, k=k)
         if soltype == 1:
             #print(">>> found hipblas")
             out = hipb_mm(inp_view, weights.t(), solidx)
-        elif soltype == 3:
-            ##only matvec is supported currently
-            out = torch.empty(inp.shape[0],
-                              weights.shape[0],
-                              dtype=torch.float16,
-                              device='cuda')
-            #print('>>>Matvec',inp.shape,weights.shape,soltype,solidx)
-            if solidx <= 1:
-                _custom_C.LLMM1(weights, inp, out, 4)
-            elif solidx == 2:
-                _custom_C.LLMM1(weights, inp, out, 2)
-            elif solidx == 8:
-                _custom_C.LLMM1(weights, inp, out, 8)
-            elif solidx == 20:
-                _custom_C.LLZZ(weights, inp, out, 0)
-            elif solidx == 21:
-                _custom_C.LLZZ(weights, inp, out, 1)
         elif soltype == 2:
             #print(">>> found rocblas")
             out = rocb_mm(inp_view, weights.t(), solidx)
