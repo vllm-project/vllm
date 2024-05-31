838b90eba4d267822084e64c722b24ef44077c12
jianan-gu <jianan.gu@intel.com>
2024-05-31 02:39:41 -0700

    add ipex awq cpu support

--- vllm/model_executor/layers/quantization/awq.py
+++ vllm/model_executor/layers/quantization/awq.py
@@ -2,7 +2,7 @@ from typing import Any, Dict, List, Optional
 
 import torch
 from torch.nn.parameter import Parameter
-
+import intel_extension_for_pytorch
 from vllm import _custom_ops as ops
 from vllm.model_executor.layers.linear import LinearBase, LinearMethodBase
 from vllm.model_executor.layers.quantization.base_config import (
@@ -150,6 +150,78 @@ class AWQLinearMethod(LinearMethodBase):
         layer.register_parameter("scales", scales)
         set_weight_attrs(scales, extra_weight_attrs)
 
+    def awq_reverse_reorder_int_tensor(self,int_tensor, bits: int):
+        assert bits == 4
+
+        int_tensor = int_tensor.T.contiguous()
+        compress_ratio = (32 // bits)
+        assert int_tensor.shape[-1] % compress_ratio == 0
+
+        order_map = [0, 2, 4, 6, 1, 3, 5, 7]
+        order_tensor = torch.tensor(
+            order_map, dtype=torch.int32, device=int_tensor.device).reshape(1, -1)
+        order_tensor = order_tensor.repeat(
+            int_tensor.shape[1]//compress_ratio, 1)
+        order_tensor = order_tensor + torch.arange(0, int_tensor.shape[1],
+                                                    compress_ratio, dtype=torch.int32, device=int_tensor.device).reshape(-1, 1)
+        order_tensor = order_tensor.reshape(-1)
+
+        reverse_order_tensor = torch.arange(order_tensor.shape[0])[order_tensor]
+        reverse_order_tensor = reverse_order_tensor[order_tensor]
+        int_tensor = int_tensor[:, reverse_order_tensor]
+        return int_tensor
+    def unpack_awq(self, awq_qweight: torch.Tensor, awq_qzeros: torch.Tensor, awq_scales: torch.Tensor, bits: int, group_size: int):
+        """
+        Args:
+            awq_qweight (`torch.LongTensor`):
+                Expected shape: (in_features, out_features // (32 // bits))
+            awq_qzeros (`torch.LongTensor`):
+                Expected shape: (in_features // group_size, out_features // (32 // bits))
+            awq_scales (`torch.LongTensor`):
+                Expected shape: (in_features // group_size, out_features)
+
+        Returns:
+            fp16_weight (`torch.LongTensor`):
+                With shape (in_features, out_features).
+            zeros (`torch.LongTensor`):
+                With shape (in_features // group_size, out_features).
+        """
+        assert bits == 4
+
+        qzeros = awq_qzeros
+        qweight = awq_qweight
+        qweight = qweight.T.contiguous()
+
+        scales = awq_scales
+        scales = scales.reshape(-1, 1, scales.shape[-1])
+
+        infeatures = awq_qweight.shape[0]
+
+        wf = torch.tensor(list(range(0, 32, bits)), dtype=torch.int32, device=qzeros.device).unsqueeze(0)
+        zeros = torch.bitwise_right_shift(torch.unsqueeze(qzeros, 2), wf.unsqueeze(0)).to(
+            torch.int16 if bits == 8 else torch.int8)
+
+        #zeros = zeros + 1
+
+        torch.bitwise_and(zeros, (2 ** bits) - 1, out=zeros)
+
+        zeros = zeros.reshape(-1, 1, zeros.shape[1] * zeros.shape[2])
+
+        weight = torch.bitwise_right_shift(torch.unsqueeze(
+            qweight, 1), wf.unsqueeze(-1)).to(torch.int16 if bits == 8 else torch.int8)
+        torch.bitwise_and(weight, (2 ** bits) - 1, out=weight)
+        weight = weight.reshape(-1, group_size, weight.shape[2])
+
+        weight = weight.view(-1, weight.shape[-1])
+        zeros = zeros.view(-1, zeros.shape[-1])
+
+        zeros = zeros.T.contiguous()
+        zeros = self.awq_reverse_reorder_int_tensor(zeros, bits)
+        weight = self.awq_reverse_reorder_int_tensor(weight, bits)
+
+        return weight.contiguous(), zeros.contiguous()
+
+
     def apply(self,
               layer: torch.nn.Module,
               x: torch.Tensor,
@@ -161,15 +233,26 @@ class AWQLinearMethod(LinearMethodBase):
         out_shape = (x.shape[:-1] + (qweight.shape[-1] * pack_factor, ))
         reshaped_x = x.reshape(-1, x.shape[-1])
 
-        # num_tokens >= threshold
-        FP16_MATMUL_HEURISTIC_CONDITION = x.shape[:-1].numel() >= 256
+        if not hasattr(self,"_op_context") :
+            t, zp_x = self.unpack_awq(qweight, qzeros, scales, 4, 128)
+            # # transpose -> [N, K]
+            t = t.T.contiguous()
+            qweight_ = t[:, 1::2].bitwise_left_shift(4).bitwise_or_(t[:, ::2]).to(torch.uint8)
+            scales_ = scales.t().contiguous()
+            self._op_context = torch.ops.ipex_prepack.weight_only_qlinear_prepack_int4(
+                qweight_,
+                scales_,
+                zp_x.t_().contiguous(),
+                bias,
+                None,
+                None,
+                128,
+                2, # 2 for bf16 compute, 3 for int8 compute
+                1,
+            )
 
-        if FP16_MATMUL_HEURISTIC_CONDITION:
-            out = ops.awq_dequantize(qweight, scales, qzeros, 0, 0, 0)
-            out = torch.matmul(reshaped_x, out)
-        else:
-            out = ops.awq_gemm(reshaped_x, qweight, scales, qzeros,
-                               pack_factor)
+        out = torch.ops.torch_ipex.ipex_woq_linear(reshaped_x, self._op_context.get_data_handle())
+
         if bias is not None:
             out.add_(bias)
         return out.reshape(out_shape)
--- vllm/model_executor/model_loader/loader.py
+++ vllm/model_executor/model_loader/loader.py
@@ -34,7 +34,7 @@ from vllm.model_executor.model_loader.weight_utils import (
     pt_weights_iterator, safetensors_weights_iterator)
 from vllm.model_executor.models.vlm_base import VisionLanguageModelBase
 from vllm.model_executor.utils import set_weight_attrs
-from vllm.utils import is_tpu
+from vllm.utils import is_tpu, is_cpu
 
 logger = init_logger(__name__)
 
@@ -45,20 +45,23 @@ def _get_quantization_config(
     """Get the quantization config."""
     if model_config.quantization is not None:
         quant_config = get_quant_config(model_config, load_config)
-        capability = torch.cuda.get_device_capability()
-        capability = capability[0] * 10 + capability[1]
-        if capability < quant_config.get_min_capability():
-            raise ValueError(
-                f"The quantization method {model_config.quantization} is not "
-                "supported for the current GPU. "
-                f"Minimum capability: {quant_config.get_min_capability()}. "
-                f"Current capability: {capability}.")
-        supported_dtypes = quant_config.get_supported_act_dtypes()
-        if model_config.dtype not in supported_dtypes:
-            raise ValueError(
-                f"{model_config.dtype} is not supported for quantization "
-                f"method {model_config.quantization}. Supported dtypes: "
-                f"{supported_dtypes}")
+
+        if not is_cpu():
+            capability = torch.cuda.get_device_capability()
+            capability = capability[0] * 10 + capability[1]
+            if capability < quant_config.get_min_capability():
+                raise ValueError(
+                    f"The quantization method {model_config.quantization} is not "
+                    "supported for the current GPU. "
+                    f"Minimum capability: {quant_config.get_min_capability()}. "
+                    f"Current capability: {capability}.")
+            supported_dtypes = quant_config.get_supported_act_dtypes()
+            if model_config.dtype not in supported_dtypes:
+                raise ValueError(
+                    f"{model_config.dtype} is not supported for quantization "
+                    f"method {model_config.quantization}. Supported dtypes: "
+                    f"{supported_dtypes}")
+
         return quant_config
     return None
 
