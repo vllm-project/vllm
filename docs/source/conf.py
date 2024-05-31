a996b34cca09cdb6579b37aa42f321582a51920a
Cyrus Leung <tlleungac@connect.ust.hk>
2024-05-31 01:49:58 +0000

    Fix mock imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -83,11 +83,14 @@ def setup(app):
 # Mock out external dependencies here, otherwise the autodoc pages may be blank.
 autodoc_mock_imports = [
     "cpuinfo",
+    "torch",
     "transformers",
+    "psutil",
     "prometheus_client",
     "sentencepiece",
     "vllm.cuda_utils",
     "vllm._C",
+    "numpy",
     "tqdm",
     "tensorizer",
 ]
