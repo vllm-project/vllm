dcde110a2d27b741241040f6c63d8dbb9bc65929
Bla_ckB <50193121+BlackBird-Coding@users.noreply.github.com>
2024-06-01 02:51:05 +0700

    chore: fix Line too long (112 > 80)

--- vllm/core/scheduler.py
+++ vllm/core/scheduler.py
@@ -421,7 +421,11 @@ class Scheduler:
                 num_running_seqs = seq_group.get_max_num_running_seqs()
                 budget.subtract_num_seqs(seq_group.request_id,
                                          num_running_seqs)
-                if curr_loras is not None and seq_group.lora_int_id > 0 and seq_group.lora_int_id in curr_loras:
+                if (
+                    curr_loras is not None
+                    and seq_group.lora_int_id > 0
+                    and seq_group.lora_int_id in curr_loras
+                ):
                         curr_loras.remove(seq_group.lora_int_id)
 
                 if running_queue:
