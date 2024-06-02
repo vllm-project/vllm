a3ef9199b99c706bd7567b76534d32d61b0135b7
Danial Javady <danialjavady96@gmail.com>
2024-06-02 18:50:57 -0400

    revert float4 for fp32 and implement for halfs

--- csrc/quantization/compressed_tensors/int8_quant_kernels.cu
+++ csrc/quantization/compressed_tensors/int8_quant_kernels.cu
@@ -23,39 +23,79 @@ static inline __device__ int8_t float_to_int8_rn(float x) {
 #endif
 }
 
+static inline __device__ int8_t half_to_int8_rn(__half x) {
+  uint32_t dst;
+  unsigned short conv = *(reinterpret_cast<unsigned short*>(&(x)));
+  asm("cvt.rni.sat.s8.f16 %0, %1;" : "=r"(dst) : "h"(conv));
+  return dst;
+}
+
 namespace vllm {
+
+typedef struct __align__(8) {
+  half x;
+  half y;
+  half z;
+  half w;
+}
+half4;
 
 template <typename scalar_t, typename scale_type>
 __global__ void static_scaled_int8_quant_kernel(
     const scalar_t* __restrict__ input, int8_t* __restrict__ out,
-    scale_type inverted_scale, const int hidden_size) {
+    scale_type inverted_scale, const int hidden_size);
+
+template <>
+__global__ void static_scaled_int8_quant_kernel<at::Half, float>(
+    const at::Half* __restrict__ input, int8_t* __restrict__ out,
+    float inverted_scale, const int hidden_size) {
   const int tid = threadIdx.x;
   const int token_idx = blockIdx.x;
-  const float4* vectorized = reinterpret_cast<const float4*>(input);
+  const half4* vectorized = reinterpret_cast<const half4*>(input);
   const int traverse_space = hidden_size >> 2;
+  // float2half is inexpensive plus multiplying two halves leads to a larger
+  // loss of precision
+  const __half h_inverted_scale = __float2half(inverted_scale);
 
 #pragma unroll 4
   for (int i = tid; i < traverse_space; i += blockDim.x) {
     int index = token_idx * traverse_space + i;
 
-    float4 data = vectorized[index];
+    half4 data_half4 = vectorized[index];
 
-    data.x *= inverted_scale;
-    data.y *= inverted_scale;
-    data.z *= inverted_scale;
-    data.w *= inverted_scale;
+    float4 data_float4;
+    data_float4.x = __half2float(data_half4.x) * inverted_scale;
+    data_float4.y = __half2float(data_half4.y) * inverted_scale;
+    data_float4.z = __half2float(data_half4.z) * inverted_scale;
+    data_float4.w = __half2float(data_half4.w) * inverted_scale;
 
-    // use char4 for vectorized stores
+    int8_t quantized0 = half_to_int8_rn(__float2half(data_float4.x));
+    int8_t quantized1 = half_to_int8_rn(__float2half(data_float4.y));
+    int8_t quantized2 = half_to_int8_rn(__float2half(data_float4.z));
+    int8_t quantized3 = half_to_int8_rn(__float2half(data_float4.w));
+
     char4 store_value;
-    store_value.x = static_cast<char>(float_to_int8_rn(data.x));
-    store_value.y = static_cast<char>(float_to_int8_rn(data.y));
-    store_value.z = static_cast<char>(float_to_int8_rn(data.z));
-    store_value.w = static_cast<char>(float_to_int8_rn(data.w));
+    store_value.x = static_cast<char>(quantized0);
+    store_value.y = static_cast<char>(quantized1);
+    store_value.z = static_cast<char>(quantized2);
+    store_value.w = static_cast<char>(quantized3);
 
     reinterpret_cast<char4*>(out)[index] = store_value;
   }
 }
 
+template <typename scalar_t, typename scale_type>
+__global__ void static_scaled_int8_quant_kernel(
+    const scalar_t* __restrict__ input, int8_t* __restrict__ out,
+    scale_type scale, const int hidden_size) {
+  const int tid = threadIdx.x;
+  const int token_idx = blockIdx.x;
+
+  for (int i = tid; i < hidden_size; i += blockDim.x) {
+    out[token_idx * hidden_size + i] =
+        float_to_int8_rn(((float)input[token_idx * hidden_size + i]) * scale);
+  }
+}
 }  // namespace vllm
 
 void static_scaled_int8_quant(torch::Tensor& out,    // [..., hidden_size]
