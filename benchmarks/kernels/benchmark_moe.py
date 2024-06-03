005b07cd58ceb821fccf29b4cbe95e9f3acaad66
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2024-06-03 18:19:37 +0000

    Minor

--- benchmarks/kernels/benchmark_moe.py
+++ benchmarks/kernels/benchmark_moe.py
@@ -298,7 +298,7 @@ if __name__ == "__main__":
     parser.add_argument("--model",
                         type=str,
                         default="mistralai/Mixtral-8x7B-Instruct-v0.1")
-    parser.add_argument("--tp-size", type=int, default=2)
+    parser.add_argument("--tp-size", "-tp", type=int, default=2)
     parser.add_argument("--dtype",
                         type=str,
                         choices=["auto", "fp8"],
