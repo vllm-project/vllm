b1fc9236a98a4245f162492cef54ad6222f069f6
Abhinav Goyal <abhinav.goyal@flipkart.com>
2024-06-03 17:44:13 +0530

    Removing medusa line from config.py

--- vllm/transformers_utils/configs/__init__.py
+++ vllm/transformers_utils/configs/__init__.py
@@ -5,7 +5,6 @@ from vllm.transformers_utils.configs.dbrx import DbrxConfig
 # `FalconConfig` class from the official HuggingFace transformers library.
 from vllm.transformers_utils.configs.falcon import RWConfig
 from vllm.transformers_utils.configs.jais import JAISConfig
-from vllm.transformers_utils.configs.medusa import MedusaConfig
 from vllm.transformers_utils.configs.mpt import MPTConfig
 
 __all__ = [
@@ -14,5 +13,4 @@ __all__ = [
     "MPTConfig",
     "RWConfig",
     "JAISConfig",
-    "MedusaConfig",
 ]
