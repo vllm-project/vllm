ed8f8b3aa6eba958e0e527510f50aa3cc94f66c4
Andrew Feldman <afeldman@neuralmagic.com>
2024-06-03 12:40:06 -0400

    Comments & type hints

--- tests/kernels/utils.py
+++ tests/kernels/utils.py
@@ -1,9 +1,21 @@
 """Kernel test utils"""
 
-STR_BACKEND_ENV_VAR = "VLLM_ATTENTION_BACKEND"
-STR_FLASH_ATTN_VAL = "FLASH_ATTN"
-STR_INVALID_VAL = "INVALID"
+import pytest
+
+STR_BACKEND_ENV_VAR: str = "VLLM_ATTENTION_BACKEND"
+STR_FLASH_ATTN_VAL: str = "FLASH_ATTN"
+STR_INVALID_VAL: str = "INVALID"
 
 
-def override_backend(mpatch, backend_name):
+def override_backend(mpatch: pytest.MonkeyPatch, backend_name: str) -> None:
+    '''
+    Override vLLM attention backend temporarily,
+    using pytest monkeypatch to ensure that the env vars get
+    reset once the test context exits.
+
+    Arguments:
+
+    * mpatch: pytest monkeypatch instance
+    * backend_name: attention backend name to force
+    '''
     mpatch.setenv(STR_BACKEND_ENV_VAR, backend_name)
