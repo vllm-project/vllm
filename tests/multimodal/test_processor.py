766eab0860ccd16d23acf90727c26e063014eb0c
Cyrus Leung <tlleungac@connect.ust.hk>
2024-06-04 12:01:46 +0800

    [CI/Build] Add inputs tests (#5215)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- tests/multimodal/test_processor.py
+++ tests/multimodal/test_processor.py
@@ -6,8 +6,10 @@ from vllm.config import ModelConfig, VisionLanguageConfig
 from vllm.multimodal import MULTIMODAL_REGISTRY
 from vllm.multimodal.image import ImagePixelData
 
+from ..conftest import _STR_DTYPE_TO_TORCH_DTYPE
 
-@pytest.mark.parametrize("dtype", ["half", "bfloat16", "float"])
+
+@pytest.mark.parametrize("dtype", ["half", "float"])
 def test_clip_image_processor(hf_images, dtype):
     MODEL_NAME = "llava-hf/llava-1.5-7b-hf"
     IMAGE_HEIGHT = IMAGE_WIDTH = 33
@@ -36,8 +38,8 @@ def test_clip_image_processor(hf_images, dtype):
     for image in hf_images:
         hf_result = hf_processor.preprocess(
             image,
-            return_tensors="np",
-        )
+            return_tensors="pt",
+        ).to(dtype=_STR_DTYPE_TO_TORCH_DTYPE[dtype])
         vllm_result = MULTIMODAL_REGISTRY.process_input(
             ImagePixelData(image),
             model_config=model_config,
@@ -45,7 +47,8 @@ def test_clip_image_processor(hf_images, dtype):
         )
 
         assert hf_result.keys() == vllm_result.keys()
-        for key, hf_arr in hf_result.items():
+        for key, hf_tensor in hf_result.items():
+            hf_arr: np.ndarray = hf_tensor.numpy()
             vllm_arr: np.ndarray = vllm_result[key].numpy()
 
             assert hf_arr.shape == vllm_arr.shape, f"Failed for key={key}"
