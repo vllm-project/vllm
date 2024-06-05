6fe557cca3f51d43573156761129b7a86864f4a9
mgoin <michael@neuralmagic.com>
2024-06-05 17:30:04 +0000

    Fix cache_dtype

--- vllm/engine/arg_utils.py
+++ vllm/engine/arg_utils.py
@@ -645,7 +645,7 @@ class EngineArgs:
             block_size=self.block_size,
             gpu_memory_utilization=self.gpu_memory_utilization,
             swap_space=self.swap_space,
-            kv_cache_dtype=self.kv_cache_dtype,
+            cache_dtype=self.kv_cache_dtype,
             num_gpu_blocks_override=self.num_gpu_blocks_override,
             sliding_window=model_config.get_sliding_window(),
             enable_prefix_caching=self.enable_prefix_caching)
