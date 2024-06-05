9621a58a04075c6cb063b06908a3dd7e4f7e51da
mgoin <michael@neuralmagic.com>
2024-06-05 16:33:10 +0000

    Newline

--- tests/quantization/utils.py
+++ tests/quantization/utils.py
@@ -3,7 +3,6 @@ import torch
 from vllm.model_executor.layers.quantization import QUANTIZATION_METHODS
 
 
-
 def is_quant_method_supported(quant_method: str) -> bool:
     # Currently, all quantization methods require Nvidia or AMD GPUs
     if not torch.cuda.is_available():
