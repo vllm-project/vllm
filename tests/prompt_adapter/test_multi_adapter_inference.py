638795a57dd0ee2f84d71b080e73b78d7bda42c8
Swapnil Parekh <swapnilp@ibm.com>
2024-06-05 12:36:38 -0400

    adding test

--- tests/prompt_adapter/test_multi_adapter_inference.py
+++ tests/prompt_adapter/test_multi_adapter_inference.py
@@ -1,5 +1,4 @@
-import vllm
-from vllm import EngineArgs, LLMEngine, RequestOutput, SamplingParams
+from vllm import EngineArgs, LLMEngine, SamplingParams
 from vllm.prompt_adapter.request import PromptAdapterRequest
 
 MODEL_PATH = "bigscience/bloomz-560m"
@@ -10,23 +9,17 @@ pa_path2 = 'swapnilbp/angry_tweet_ptune'
 def do_sample(engine):
 
     prompts = [
-        (
-            "Tweet text: I have complaints! Label: "   ,
-            SamplingParams(temperature=0.0, max_tokens=3),
-            PromptAdapterRequest("hate_speech", 1, pa_path2 ,8)),
-        (
-            "Tweet text: I have no problems Label: "   ,
-            SamplingParams(temperature=0.0, max_tokens=3),
-            PromptAdapterRequest("hate_speech2", 2, pa_path2,8)),
-        (
-            "Tweet text: I have complaints! Label: "   ,
-            SamplingParams(temperature=0.0, max_tokens=3),
-            None),
-    (
-            "Tweet text: I have no problems Label: "   ,
-            SamplingParams(temperature=0.0, max_tokens=3),
-            PromptAdapterRequest("complain", 3, pa_path,8)),
-        
+        ("Tweet text: I have complaints! Label: ",
+         SamplingParams(temperature=0.0, max_tokens=3),
+         PromptAdapterRequest("hate_speech", 1, pa_path2, 8)),
+        ("Tweet text: I have no problems Label: ",
+         SamplingParams(temperature=0.0, max_tokens=3),
+         PromptAdapterRequest("hate_speech2", 2, pa_path2, 8)),
+        ("Tweet text: I have complaints! Label: ",
+         SamplingParams(temperature=0.0, max_tokens=3), None),
+        ("Tweet text: I have no problems Label: ",
+         SamplingParams(temperature=0.0, max_tokens=3),
+         PromptAdapterRequest("complain", 3, pa_path, 8)),
     ]
 
     request_id = 0
@@ -39,9 +32,9 @@ def do_sample(engine):
                                sampling_params,
                                prompt_adapter_request=pa_request)
             request_id += 1
-    
+
         request_outputs = engine.step()
-    
+
         for request_output in request_outputs:
             if request_output.finished:
                 results.add(request_output.outputs[0].text)
@@ -50,8 +43,10 @@ def do_sample(engine):
 
 def test_twitter_prompt_adapter():
     engine_args = EngineArgs(model="bigscience/bloomz-560m",
-                                 max_prompt_adapters=3,
-                                 enable_prompt_adapter=True)
+                             max_prompt_adapters=3,
+                             enable_prompt_adapter=True)
     engine = LLMEngine.from_engine_args(engine_args)
-    expected_output = {' quot;I', 'hate speech', 'no complaint', 'not hate speech'}
+    expected_output = {
+        ' quot;I', 'hate speech', 'no complaint', 'not hate speech'
+    }
     assert do_sample(engine) == expected_output
