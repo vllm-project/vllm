d0587ebc86f39e0bcfd563da7747f350904241a6
sfc-gh-zhwang <flex.wang@snowflake.com>
2024-06-04 22:31:26 -0700

    commit

--- tests/lora/test_mixtral.py
+++ tests/lora/test_mixtral.py
@@ -29,7 +29,7 @@ def do_sample(llm, lora_path: str, lora_id: int):
     return generated_texts
 
 
-@pytest.mark.parametrize("tp_size", [4, 8])
+@pytest.mark.parametrize("tp_size", [4])
 def test_mixtral_lora(mixtral_lora_files, tp_size):
     if torch.cuda.device_count() < tp_size:
         pytest.skip(f"Not enough GPUs for tensor parallelism {tp_size}")
@@ -38,7 +38,8 @@ def test_mixtral_lora(mixtral_lora_files, tp_size):
                    enable_lora=True,
                    max_num_seqs=16,
                    max_loras=4,
-                   tensor_parallel_size=tp_size)
+                   tensor_parallel_size=tp_size,
+                   worker_use_ray=True)
 
     expected_lora_output = [
         "give_opinion(name[SpellForce 3], release_year[2017], developer[Grimlore Games], rating[poor])",  # noqa: E501
