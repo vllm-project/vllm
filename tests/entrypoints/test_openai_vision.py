1b32e2f0e50d2415fd96e93b9dc18feaf23dfa5c
Roger Wang <ywang@roblox.com>
2024-06-04 22:29:13 -0700

    revert and update token count

--- tests/entrypoints/test_openai_vision.py
+++ tests/entrypoints/test_openai_vision.py
@@ -90,7 +90,7 @@ async def test_single_chat_session_image(server, client: openai.AsyncOpenAI,
     choice = chat_completion.choices[0]
     assert choice.finish_reason == "length"
     assert chat_completion.usage == openai.types.CompletionUsage(
-        completion_tokens=10, prompt_tokens=594, total_tokens=604)
+        completion_tokens=10, prompt_tokens=606, total_tokens=616)
 
     message = choice.message
     message = chat_completion.choices[0].message
