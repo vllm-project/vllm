04a542dfd20a9da6b401b330f4a7d7d87ce4c221
maor-ps <154728172+maor-ps@users.noreply.github.com>
2024-06-06 15:05:21 +0300

    Update protocol.py

--- vllm/entrypoints/openai/protocol.py
+++ vllm/entrypoints/openai/protocol.py
@@ -462,8 +462,8 @@ class CompletionRequest(OpenAIBaseModel):
     @model_validator(mode="before")
     @classmethod
     def check_logprobs(cls, data):
-        if ("logprobs" in data and
-            ((not isinstance(data["logprobs"], int)) or data["logprobs"] < 0)):
+        if "logprobs" in data and data[
+                "logprobs"] is not None and not data["logprobs"] >= 0:
             raise ValueError("if passed, `logprobs` must be a positive value.")
         return data
 
