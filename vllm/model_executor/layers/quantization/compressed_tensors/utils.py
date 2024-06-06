2c6e5804f2267a5bc3dc1198f2703f7c37cd71ee
Dipika Sikka <dipikasikka1@gmail.com>
2024-06-06 14:56:43 +0000

    format fix

--- vllm/model_executor/layers/quantization/compressed_tensors/utils.py
+++ vllm/model_executor/layers/quantization/compressed_tensors/utils.py
@@ -1,9 +1,10 @@
 import re
-from torch.nn import Module
 from enum import Enum
 from typing import Any, Dict, Iterable, Optional
 
 from pydantic import BaseModel, Field
+from torch.nn import Module
+
 
 class QuantizationType(str, Enum):
     """
@@ -12,6 +13,7 @@ class QuantizationType(str, Enum):
 
     INT = "int"
     FLOAT = "float"
+
 
 class QuantizationStrategy(str, Enum):
     """
@@ -24,23 +26,25 @@ class QuantizationStrategy(str, Enum):
     BLOCK = "block"
     TOKEN = "token"
 
+
 class QuantizationArgs(BaseModel):
     """
-    User facing arguments used to define a quantization config for weights or
-    activations
+    User facing arguments used to define a quantization config 
+    for weights or activations
 
     :param num_bits: quantization bit depth
     :param type: dtype to quantized to, either int or float
-    :param symmetric: whether or not quantization scale is symmetric about zero-point
-    :param strategy: string id determining the scope of scale/zero-point to apply
+    :param symmetric: whether or not quantization scale is symmetric
+    :param strategy: string determining the scope of scale/zero-point to apply
     :param group_size: group length to use for the group strategy
-    :param block_structure: 2d block structure to use for the block strategy, must be
-    of the format "2x4", "8x16", etc.
-    :param dynamic: set True to perform dynamic quantization - values will not be
-        calibrated during calibration phase, instead during inference new quantization
-        ranges will be observed with every sample. Defaults to False for static
-        quantization. Note that enabling dynamic quantization will change the default
-        observer to a memoryless one
+    :param block_structure: 2d block structure to use for the block 
+    strategy, must be of the format "2x4", "8x16", etc.
+    :param dynamic: set True to perform dynamic quantization -
+        values will not be calibrated during calibration phase, 
+        instead during inference new quantization ranges will be 
+        observed with every sample. Defaults to False for static
+        quantization. Note that enabling dynamic quantization 
+        will change the default observer to a memoryless one
     """
 
     num_bits: int = 8
@@ -52,33 +56,32 @@ class QuantizationArgs(BaseModel):
     dynamic: bool = False
     observer: str = Field(
         default="minmax",
-        description=(
-            "The class to use to compute the quantization param - "
-            "scale and zero-point'"
-        ),
+        description=("The class to use to compute the quantization param - "
+                     "scale and zero-point'"),
     )
     observer_kwargs: Dict[str, Any] = Field(
         default_factory=dict,
-        description=(
-            "optional dict of kwargs to be passed directly to torch quantization "
-            "Observers constructor excluding quantization range or symmetry"
-        ),
+        description=
+        ("optional dict of kwargs to be passed directly to torch quantization "
+         "Observers constructor excluding quantization range or symmetry"),
     )
 
 
 def find_first_name_or_class_match(
-    name: str, module: Module, targets: Iterable[str], check_contains: bool = False
-) -> Optional[str]:
+        name: str,
+        module: Module,
+        targets: Iterable[str],
+        check_contains: bool = False) -> Optional[str]:
     # first element of targets that matches the given name
     # if no name matches returns first target that matches the class name
     # returns None otherwise
     return _find_first_match(name, targets) or _find_first_match(
-        module.__class__.__name__, targets, check_contains
-    )
+        module.__class__.__name__, targets, check_contains)
 
-def _find_first_match(
-    value: str, targets: Iterable[str], check_contains: bool = False
-) -> Optional[str]:
+
+def _find_first_match(value: str,
+                      targets: Iterable[str],
+                      check_contains: bool = False) -> Optional[str]:
     # returns first element of target that matches value either
     # exactly or as a regex after 're:'. if check_contains is set to True,
     # additionally checks if the target string is contained with value.
