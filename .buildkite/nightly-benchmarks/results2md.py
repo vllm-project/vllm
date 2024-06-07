43deac5be2c4e6ea71f8474e719dcff47c8102d1
KuntaiDu <kuntai@uchicago.edu>
2024-06-06 17:20:17 -0700

    Kuntai: update markdown generation script for

--- .buildkite/nightly-benchmarks/results2md.py
+++ .buildkite/nightly-benchmarks/results2md.py
@@ -6,18 +6,29 @@ from tabulate import tabulate
 
 results_folder = Path("results/")
 
+# latency results and the keys that will be printed into markdown
+latency_results = []
+latency_column_mapping = {
+    "test_name": "Test name",
+    "avg_latency": "Average latency (s)",
+    "P10": "P10 (s)",
+    "P25": "P25 (s)",
+    "P50": "P50 (s)",
+    "P75": "P75 (s)",
+    "P90": "P90 (s)",
+}
+
+
 # serving results and the keys that will be printed into markdown
 serving_results = []
 serving_column_mapping = {
     "test_name": "Test name",
     "completed": "# of req.",
     "request_throughput": "Tput (req/s)",
+    "input_throughput": "Input Tput (tok/s)",
+    "output_throughput": "Output Tput (tok/s)",
     "mean_ttft_ms": "Mean TTFT (ms)",
-    "median_ttft_ms": "Median (ms)",
-    "p99_ttft_ms": "P99 (ms)",
     "mean_tpot_ms": "Mean TPOT (ms)",
-    "median_tpot_ms": "Median (ms)",
-    "p99_tpot_ms": "P99 (ms)",
 }
 
 for test_file in results_folder.glob(f"*.json"):
@@ -41,18 +52,46 @@ for test_file in results_folder.glob(f"*.json"):
         continue
     
     elif "latency" in f.name:
+        # this result is generated via `benchmark_latency.py`
+        
+        # attach the benchmarking command to raw_result
+        with open(test_file.with_suffix(".commands"), "r") as f:
+            command = json.loads(f.read())
+        raw_result.update(command)
+        
+        # update the test name of this result
+        raw_result.update({"test_name": test_file.stem})
+        
+        # get different percentiles
+        for perc in [10, 25, 50, 75, 90]:
+            raw_result.update({
+                f"P{perc}": raw_result["percentiles"][perc]
+            })
+        
+        # add the result to raw_result
+        latency_results.append(raw_result)
         continue
     
     print(f"Skipping {test_file}")
 
+
+latency_results = pd.DataFrame.from_dict(latency_results)
 serving_results = pd.DataFrame.from_dict(serving_results)
 
-# Remapping the keys according to serving_column_mapping
+# remapping the key, for visualization purpose
+latency_results = latency_results[
+    list(latency_column_mapping.keys())
+].rename(columns=latency_column_mapping)
 serving_results = serving_results[
     list(serving_column_mapping.keys())
 ].rename(columns=serving_column_mapping)
 
-# get markdown table for serving
+# get markdown tables
+latency_md_table = tabulate(
+    latency_results, 
+    headers='keys', 
+    tablefmt='pipe',
+    showindex=False)
 serving_md_table = tabulate(
     serving_results, 
     headers='keys', 
@@ -61,6 +100,9 @@ serving_md_table = tabulate(
 
 # document the result
 with open(results_folder / "benchmark_results.md", "w") as f: 
+    f.write("## Latency tests\n")
+    f.write(latency_md_table)
+    f.write("\n")
     f.write("## Online serving tests\n")
     f.write(serving_md_table)
     f.write("\n")
