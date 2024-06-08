41e4ccceb5ef7135c80169e78c0f6fc01f10ccb7
Dipika Sikka <dipikasikka1@gmail.com>
2024-06-08 16:06:53 +0000

    run with default settings

--- tests/quantization/test_compressed_tensors.py
+++ tests/quantization/test_compressed_tensors.py
@@ -5,6 +5,7 @@ Run `pytest tests/quantization/test_compressed_tensors.py`.
 
 import torch
 
+from vllm import SamplingParams
 from vllm.model_executor.layers.quantization.compressed_tensors.compressed_tensors import (  # noqa: E501
     CompressedTensorsLinearMethod, CompressedTensorsW8A8DynamicToken,
     CompressedTensorsW8A8StaticTensor)
@@ -37,6 +38,14 @@ def test_compressed_tensors_w8a8_static_setup(vllm_runner):
         assert qkv_proj.weight_scale.shard_splitter is not None
         assert qkv_proj.weight_scale.logical_widths is not None
         assert qkv_proj.input_scale.dtype is torch.float32
+
+
+def test_compressed_tensors_no_enforce_eager(vllm_runner):
+    model_path = "nm-testing/tinyllama-oneshot-w8a8-static-v2"
+    with vllm_runner(model_path) as llm:
+        sampling_params = SamplingParams()
+        output = llm.generate("Hello world!", sampling_params=sampling_params)
+        assert output
 
 
 def test_compressed_tensors_w8a8_dynanmic_per_token(vllm_runner):
