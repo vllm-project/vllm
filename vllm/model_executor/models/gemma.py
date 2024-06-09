2f65bf7eed9f1cc7be70373ca2194f758b92f8a5
Roger Wang <ywang@roblox.com>
2024-06-08 22:38:56 -0700

    add embedding method to gemma

--- vllm/model_executor/models/gemma.py
+++ vllm/model_executor/models/gemma.py
@@ -264,6 +264,9 @@ class GemmaModel(nn.Module):
         normalizer = self.config.hidden_size**0.5
         self.register_buffer("normalizer", torch.tensor(normalizer))
 
+    def get_input_embeddings(self, input_ids: torch.Tensor) -> torch.Tensor:
+        return self.embed_tokens(input_ids)
+
     def forward(
         self,
         input_ids: torch.Tensor,
