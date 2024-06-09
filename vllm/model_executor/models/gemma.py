4a9551dc9b1c3116cda91e65d325f0d6637f0b06
Roger Wang <ywang@roblox.com>
2024-06-08 23:48:14 -0700

    update gemma forward

--- vllm/model_executor/models/gemma.py
+++ vllm/model_executor/models/gemma.py
@@ -273,10 +273,13 @@ class GemmaModel(nn.Module):
         positions: torch.Tensor,
         kv_caches: List[torch.Tensor],
         attn_metadata: AttentionMetadata,
+        inputs_embeds: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
-        hidden_states = self.embed_tokens(input_ids)
+        if inputs_embeds is not None:
+            hidden_states = inputs_embeds
+        else:
+            hidden_states = self.get_input_embeddings(input_ids)
         hidden_states *= self.normalizer
-
         residual = None
         for i in range(len(self.layers)):
             layer = self.layers[i]
