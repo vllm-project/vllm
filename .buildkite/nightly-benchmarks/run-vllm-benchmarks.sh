5bd23e95f8debee7534674ee513b36f262df8c91
Kuntai Du <kuntai@uchicago.edu>
2024-06-09 15:28:22 -0700

    Kuntai: add GPU memory usage check after killing vllm server

--- .buildkite/nightly-benchmarks/run-vllm-benchmarks.sh
+++ .buildkite/nightly-benchmarks/run-vllm-benchmarks.sh
@@ -61,7 +61,7 @@ wait_for_server() {
   timeout 600 bash -c 'until curl localhost:8000/v1/completions; do sleep 1; done' || exit 1
 }
 
-kill_vllm() {
+kill_gpu_processes() {
   # kill vllm instances
   # List all the processes using the GPU and get their PIDs
   pids=$(nvidia-smi --query-compute-apps=pid --format=csv,noheader)
@@ -79,7 +79,14 @@ kill_vllm() {
       echo "All GPU processes have been killed."
   fi
 
+  # waiting for GPU processes to be fully killed
   sleep 10
+
+  # Print the GPU memory usage
+  # so that we know if all GPU processes are killed.
+  gpu_memory_usage=$(nvidia-smi --query-gpu=memory.used --format=csv,noheader,nounits -i 0)
+  echo "GPU 0 Memory Usage: $gpu_memory_usage MB"
+
 }
 
 
@@ -143,7 +150,7 @@ jq -c '.[]' $LATENCY_TESTS | while read -r params; do
   # run the benchmark
   eval $latency_command
 
-  kill_vllm
+  kill_gpu_processes
 
 done
 
@@ -213,7 +220,7 @@ jq -c '.[]' $SERVING_TESTS | while read -r params; do
   eval $client_command
 
   # clean up
-  kill_vllm
+  kill_gpu_processes
 
   # debugging memory leaking
   used_memory=$(nvidia-smi --query-gpu=memory.used --format=csv,noheader,nounits -i 0)
