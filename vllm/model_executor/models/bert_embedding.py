c2fecf3a7db233157fc4d3c41fe0198f5032f0eb
laishzh <laishengzhang@gmail.com>
2024-06-10 00:40:38 +0800

    feat: implements bert

--- vllm/model_executor/models/bert_embedding.py
+++ vllm/model_executor/models/bert_embedding.py
@@ -2,23 +2,17 @@ from typing import Iterable, List, Optional, Tuple
 
 import torch
 from torch import nn
-from torch.nn import LayerNorm
-
-from transformers import BertModel
 from transformers import BertConfig
 
 from vllm.attention import Attention, AttentionMetadata
 from vllm.config import CacheConfig
-from vllm.distributed import (get_tensor_model_parallel_rank,
-                              get_tensor_model_parallel_world_size,
-                              tensor_model_parallel_all_reduce)
-from vllm.model_executor.layers.linear import (ColumnParallelLinear,
-                                               QKVParallelLinear,
+from vllm.distributed import get_tensor_model_parallel_world_size
+from vllm.model_executor.layers.activation import get_act_fn
+from vllm.model_executor.layers.linear import (QKVParallelLinear,
                                                RowParallelLinear)
 from vllm.model_executor.layers.pooler import Pooler, PoolingType
 from vllm.model_executor.layers.quantization.base_config import (
     QuantizationConfig)
-from vllm.model_executor.model_loader.weight_utils import default_weight_loader
 from vllm.model_executor.pooling_metadata import PoolingMetadata
 from vllm.sequence import PoolerOutput
 
@@ -39,7 +33,9 @@ class BertEmbeddingModel(nn.Module):
         **kwargs,
     ) -> None:
         super().__init__()
-        self.model = BertModel(kwargs["config"], add_pooling_layer=False)
+        self.model = BertModel(config=kwargs["config"],
+                               cache_config=kwargs.get("cache_config", None),
+                               quant_config=kwargs.get("quant_config", None))
         self._pooler = Pooler(pooling_type=PoolingType.LAST, normalize=True)
 
     def forward(
@@ -50,13 +46,11 @@ class BertEmbeddingModel(nn.Module):
         attn_metadata: AttentionMetadata,
         inputs_embeds: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
-        ts = self.model.forward(input_ids=input_ids.unsqueeze(0),
-                                position_ids=positions,
-                                past_key_values=None,
-                                inputs_embeds=inputs_embeds,
-                                return_dict=False,
-                                )
-        return ts[0].squeeze(0)
+        return self.model.forward(input_ids=input_ids,
+                                  position_ids=positions,
+                                  kv_caches=kv_caches,
+                                  inputs_embeds=inputs_embeds,
+                                  attn_metadata=attn_metadata)
 
     def pooler(
         self,
@@ -70,27 +64,58 @@ class BertEmbeddingModel(nn.Module):
         for name, ts in weights:
             print("Parameter: ", name)
 
-class BertEmbedding(nn.Module):
-    def __init__(self, config: BertConfig):
-        super().__init__()
-        
-        self.size = config.hidden_size
 
-        self.word_embeddings = nn.Embedding(config.vocab_size, config.hidden_size, padding_idx=config.pad_token_id)
-        self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)
-        self.token_type_embeddings = nn.Embedding(config.type_vocab_size, config.hidden_size)
-        self.norm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
-        self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        
-        self.position_embedding_type = config.position_embedding_type
-        if self.position_embedding_type != "absolute":
-            raise ValueError("Only 'absolute' position_embedding_type is supported")
-        
+class BertModel(nn.Module):
+    def __init__(
+        self,
+        config: BertConfig,
+        cache_config: Optional[CacheConfig] = None,
+        quant_config: Optional[QuantizationConfig] = None,
+    ):
+        super().__init__()
+        self.embedding = BertEmbedding(config)
+        self.encoder = BertEncoder(config, cache_config, quant_config)
+
     def forward(
         self,
         input_ids: torch.Tensor,
-        token_type_ids: torch.Tensor,
         position_ids: torch.Tensor,
+        kv_caches: List[torch.Tensor],
+        attn_metadata: AttentionMetadata,
+        inputs_embeds: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
+        hidden_states = self.embedding(input_ids=input_ids,
+                                       position_ids=position_ids,
+                                       inputs_embeds=inputs_embeds)
+        output = self.encoder(hidden_states, kv_caches, attn_metadata)
+        return output
+
+
+class BertEmbedding(nn.Module):
+    def __init__(self, config: BertConfig):
+        super().__init__()
+        self.size = config.hidden_size
+
+        self.word_embeddings = nn.Embedding(config.vocab_size,
+                                            config.hidden_size,
+                                            padding_idx=config.pad_token_id)
+        self.position_embeddings = nn.Embedding(config.max_position_embeddings,
+                                                config.hidden_size)
+        self.token_type_embeddings = nn.Embedding(config.type_vocab_size,
+                                                  config.hidden_size)
+        self.norm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+
+        self.position_embedding_type = config.position_embedding_type
+        if self.position_embedding_type != "absolute":
+            raise ValueError("Only 'absolute' position_embedding_type" +
+                             " is supported")
+
+    def forward(
+        self,
+        input_ids: torch.Tensor,
+        position_ids: Optional[torch.Tensor] = None,
+        token_type_ids: Optional[torch.Tensor] = None,
         inputs_embeds: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
         if input_ids is not None:
@@ -107,23 +132,54 @@ class BertEmbedding(nn.Module):
 
         # position embeddings
         if position_ids is None:
-            position_ids = torch.arange(seq_length, dtype=torch.long, device=device)
+            position_ids = torch.arange(seq_length, dtype=torch.long,
+                                        device=device)
             position_ids = position_ids.unsqueeze(0).expand_as(input_ids)
         position_embeddings = self.position_embeddings(position_ids)
 
         # token type embeddings
         if token_type_ids is None:
-            token_type_ids = torch.zeros(input_shape, dtype=torch.long, device=device)
+            token_type_ids = torch.zeros(input_shape, dtype=torch.long,
+                                         device=device)
         token_type_embeddings = self.token_type_embeddings(token_type_ids)
 
-        embeddings = inputs_embeds + token_type_embeddings + position_embeddings
+        embeddings = inputs_embeds + token_type_embeddings
+        embeddings += position_embeddings
         embeddings = self.norm(embeddings)
         embeddings = self.dropout(embeddings)
         return embeddings
-        
+
 
 class BertEncoder(nn.Module):
-    pass
+    def __init__(
+        self,
+        config: BertConfig,
+        cache_config: Optional[CacheConfig] = None,
+        quant_config: Optional[QuantizationConfig] = None,
+    ):
+        super().__init__()
+        self.layers = nn.ModuleList([
+            BertLayer(config=config,
+                      cache_config=cache_config,
+                      quant_config=quant_config)
+            for _ in range(config.num_hidden_layers)
+        ])
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        kv_caches: List[torch.Tensor],
+        attn_metadata: AttentionMetadata,
+    ) -> torch.Tensor:
+        for i in range(len(self.layers)):
+            layer = self.layers[i]
+            hidden_states = layer(
+                hidden_states,
+                kv_caches[i],
+                attn_metadata,
+            )
+        return hidden_states
+
 
 class BertLayer(nn.Module):
     def __init__(
@@ -133,10 +189,58 @@ class BertLayer(nn.Module):
         quant_config: Optional[QuantizationConfig] = None,
     ):
         super(BertLayer, self).__init__()
-        self.attention = BertAttention(config=config, cache_config=cache_config, quant_config=quant_config)
-        TODO:
+        self.attention = BertAttention(config=config,
+                                       cache_config=cache_config,
+                                       quant_config=quant_config)
+        self.intermediate = BertIntermediate(config)
+        self.output = BertOutput(config)
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        kv_cache: Optional[torch.Tensor],
+        attn_metadata: AttentionMetadata,
+    ):
+        self_attention_outputs = self.attention(
+            hidden_states,
+            kv_cache,
+            attn_metadata,
+        )
+
+        output = self.feed_forward(self_attention_outputs)
+        return output
+
+    def feed_forward(self, attention_output):
+        intermediate_output = self.intermediate(attention_output)
+        layer_output = self.output(intermediate_output, attention_output)
+        return layer_output
+
 
 class BertAttention(nn.Module):
+    def __init__(
+        self,
+        config: BertConfig,
+        cache_config: Optional[CacheConfig] = None,
+        quant_config: Optional[QuantizationConfig] = None,
+    ):
+        super().__init__()
+        self.self_attn = BertSelfAttention(config=config,
+                                           cache_config=cache_config,
+                                           quant_config=quant_config)
+        self.output = BertSelfOutput(config)
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        kv_cache: torch.Tensor,
+        attn_metadata: AttentionMetadata,
+    ) -> torch.Tensor:
+        self_outputs = self.self_attn(hidden_states, kv_cache, attn_metadata)
+        attn_output = self.output(self_outputs[0], hidden_states)
+        return attn_output
+
+
+class BertSelfAttention(nn.Module):
 
     def __init__(
         self,
@@ -147,19 +251,19 @@ class BertAttention(nn.Module):
         super().__init__()
         self.hidden_size = config.hidden_size
         tp_size = get_tensor_model_parallel_world_size()
-        
+
         self.total_num_heads = config.num_attention_heads
         assert self.total_num_heads % tp_size == 0
         self.num_heads = self.total_num_heads // tp_size
         self.total_num_kv_heads = self.total_num_heads
         self.head_dim = self.hidden_size // self.total_num_heads
         assert self.head_dim * self.total_num_heads == self.hidden_size
-        
+
         self.num_kv_heads = max(1, self.total_num_kv_heads // tp_size)
-        
+
         self.postition_embedding = config.position_embedding_type
         self.max_position_embeddings = config.max_position_embeddings
-        
+
         self.q_size = self.num_heads * self.head_dim
         self.kv_size = self.num_kv_heads * self.head_dim
 
@@ -180,7 +284,7 @@ class BertAttention(nn.Module):
             bias=False,
             quant_config=quant_config,
         )
-        
+
         self.attn = Attention(
             num_heads=self.num_heads,
             head_size=self.head_dim,
@@ -189,42 +293,59 @@ class BertAttention(nn.Module):
             cache_config=cache_config,
             quant_config=quant_config,
         )
-        
 
     def forward(
         self,
-        positions: torch.Tensor,
         hidden_states: torch.Tensor,
         kv_cache: torch.Tensor,
         attn_metadata: AttentionMetadata,
     ) -> torch.Tensor:
-        qkv, bias = self.query_key_value(hidden_states)
+        qkv, _ = self.query_key_value(hidden_states)
         q, k, v = qkv.split([self.q_size, self.kv_size, self.kv_size], dim=-1)
         atten_output = self.atten(q, k, v, kv_cache, attn_metadata)
         output, _ = self.o_proj(atten_output)
         return output
 
-    
+
+class BertSelfOutput(nn.Module):
+    def __init__(self, config: BertConfig):
+        super(BertSelfOutput, self).__init__()
+        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        self.norm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+
+    def forward(self, hidden_states, input_tensor):
+        hidden_states = self.dense(hidden_states)
+        hidden_states = self.dropout(hidden_states)
+        hidden_states = self.norm(hidden_states + input_tensor)
+        return hidden_states
+
+
+class BertIntermediate(nn.Module):
+    def __init__(self, config: BertConfig):
+        super().__init__()
+        self.dense = nn.Linear(config.hidden_size, config.intermediate_size)
+        self.intermediate_act_fn = get_act_fn(config.hidden_act)
+
+    def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
+        hidden_states = self.dense(hidden_states)
+        hidden_states = self.intermediate_act_fn(hidden_states)
+        return hidden_states
+
+
 class BertOutput(nn.Module):
     def __init__(self, config: BertConfig):
         super().__init__()
         self.dense = nn.Linear(config.intermediate_size, config.hidden_size)
-        self.LayerNorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+        self.norm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
-    def forward(self, hidden_states: torch.Tensor, input_tensor: torch.Tensor) -> torch.Tensor:
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        input_tensor: torch.Tensor,
+    ) -> torch.Tensor:
         hidden_states = self.dense(hidden_states)
         hidden_states = self.dropout(hidden_states)
-        hidden_states = self.LayerNorm(hidden_states + input_tensor)
+        hidden_states = self.norm(hidden_states + input_tensor)
         return hidden_states
-
-class BertModel(nn.Module):
-    def __init__(self, **kwargs) -> None:
-        super(BertModel, self).__init__(**kwargs)
-        
-        self.embedding = BertEmbedding()
-        self.encoder = BertEncoder()
-        self.softmax = 
-        
-    
-    def forward(self, input_ids, attention_mask, token_type_ids, position_ids, head_mask, inputs_embeds, encoder_hidden_states, encoder_attention_mask):
