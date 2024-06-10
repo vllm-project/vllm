383e7347cd2aaf9d51c9842eef77f06658a6d35b
Simon Mo <simon.mo@hey.com>
2024-06-10 17:56:06 -0500

    Bump version to v0.5.0 (#5384)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/__init__.py
+++ vllm/__init__.py
@@ -12,7 +12,7 @@ from vllm.outputs import (CompletionOutput, EmbeddingOutput,
 from vllm.pooling_params import PoolingParams
 from vllm.sampling_params import SamplingParams
 
-__version__ = "0.4.3"
+__version__ = "0.5.0"
 
 __all__ = [
     "LLM",
