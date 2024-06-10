abd7473cab817d62f344f7411b017b1641634eca
youkaichao <youkaichao@126.com>
2024-06-09 20:36:03 -0700

    fix var name

--- tests/distributed/test_same_node.py
+++ tests/distributed/test_same_node.py
@@ -1,8 +1,8 @@
 from vllm.distributed.parallel_state import is_in_the_same_node
 
 torch.distributed.init_process_group(backend="gloo")
-ans = is_in_the_same_node(torch.distributed.group.WORLD)
-if ans:
+test_result = is_in_the_same_node(torch.distributed.group.WORLD)
+if test_result:
     exit(0)
 else:
     exit(1)
