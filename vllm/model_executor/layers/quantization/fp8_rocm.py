d254de7f0b27b2c6513ac6f8e472a01de6c49a46
Charlie Fu <Charlie.Fu@amd.com>
2024-06-10 11:40:08 -0500

    Adding fp8 to gradlib (#44)
    * adding fp8 gemm tunner to gradlib
    
    * formatting
    
    * add instructions
    
    * Linting
    
    * adding fp8 gemm tunner to gradlib
    
    formatting
    
    add instructions
    
    * Linting fp8 gradlib
    
    * fix merging issue of ROCm_performance.md
    
    * delete fp8_gemm_tuner.py
    
    * Fix linting for triton: unmeld if with constexpr
    
    * update tutorial
    
    * Fix linting again
    
    * fix typo
    
    ---------
    
    Co-authored-by: Matthew Wong <Matthew.Wong2@amd.com>

--- vllm/model_executor/layers/quantization/fp8_rocm.py
+++ vllm/model_executor/layers/quantization/fp8_rocm.py
@@ -30,10 +30,10 @@ class Fp8RocmConfig(QuantizationConfig):
         #print(f"Integral Cross factor = {self.factor}")
         if gemm_type == "fp8_8":
             self.gemm_method = Fp8RocmLinearMethod.apply_fp8_8
-            tuned_filename = "/projects/tuned_fp8_8.csv"
+            tuned_filename = "/tmp/tuned_fp8_8.csv"
         elif gemm_type == "fp8_16":
             self.gemm_method = Fp8RocmLinearMethod.apply_fp8_16
-            tuned_filename = "/projects/tuned_fp8_16.csv"
+            tuned_filename = "/tmp/tuned_fp8_16.csv"
         else:
             raise ValueError(f"Unknown fp8 gemm type: {gemm_type}")
         try:
@@ -50,7 +50,7 @@ class Fp8RocmConfig(QuantizationConfig):
             m = shape["M"]
             n = shape["N"]
             k = shape["K"]
-            algo = shape["algo"]
+            algo = shape["solidx"]
             self._tuned[(m, n, k)] = algo
 
     @classmethod
@@ -224,7 +224,7 @@ class Fp8RocmLinearMethod(LinearMethodBase):
 
             if os.getenv("TUNE_FP8") == "1":
                 try:
-                    df = pd.read_csv("/projects/fp8_shapes.csv")
+                    df = pd.read_csv("/tmp/fp8_shapes.csv")
                 except (IOError, pd.errors.EmptyDataError,
                         pd.errors.ParserError):
                     df = pd.DataFrame(columns=["M", "N", "K"])
@@ -234,7 +234,7 @@ class Fp8RocmLinearMethod(LinearMethodBase):
                         "N": [n],
                         "K": [k]
                     })]).drop_duplicates()
-                df.to_csv("/projects/fp8_shapes.csv", index=False)
+                df.to_csv("/tmp/fp8_shapes.csv", index=False)
             algo = 0
         res = vllm_ops.fp8_gemm_16(x8, weight.t(), asf, wsf, int(algo))
         return res
@@ -271,7 +271,7 @@ class Fp8RocmLinearMethod(LinearMethodBase):
                         "N": [n],
                         "K": [k]
                     })]).drop_duplicates()
-                df.to_csv("/projects/fp8_shapes.csv", index=False)
+                df.to_csv("/tmp/fp8_shapes.csv", index=False)
             algo = 0
 
         res = vllm_ops.fp8_gemm(x8, weight.t(), asf, wsf, osf, int(algo))
