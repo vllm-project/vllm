4eefa0e7bf43ee035c847118ce287889f24d5f73
youkaichao <youkaichao@126.com>
2024-06-09 20:35:13 -0700

    fix lint

--- tests/distributed/test_same_node.py
+++ tests/distributed/test_same_node.py
@@ -1,4 +1,5 @@
 from vllm.distributed.parallel_state import is_in_the_same_node
+
 torch.distributed.init_process_group(backend="gloo")
 ans = is_in_the_same_node(torch.distributed.group.WORLD)
 if ans:
