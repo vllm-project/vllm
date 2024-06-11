6c705cb159c5734e3c57ced747722ab1fdfaff1a
Cody Yu <hao.yu.cody@gmail.com>
2024-06-11 13:11:12 -0700

    fix

--- tests/quantization/test_fp8.py
+++ tests/quantization/test_fp8.py
@@ -28,32 +28,46 @@ def test_load_fp16_model(vllm_runner) -> None:
 @pytest.mark.skipif(
     capability < QUANTIZATION_METHODS["fp8"].get_min_capability(),
     reason="FP8 is not supported on this GPU type.")
-@pytest.mark.parameterize("dtype", [torch.float16, torch.bfloat16])
+@pytest.mark.parametrize("dtype", [torch.float16, torch.bfloat16])
 def test_scaled_fp8_quant(dtype) -> None:
 
-    def fp8_quant_ref(tensor) -> torch.Tensor:
+    def quantize_ref(tensor, inv_scale):
+        # The reference implementation that fully aligns to
+        # the kernel being tested.
         finfo = torch.finfo(torch.float8_e4m3fn)
-        scale = finfo.max / tensor.abs().max()
-        qweight = (tensor * scale).clamp(min=finfo.min, max=finfo.max)
-        return qweight.to(torch.float8_e4m3fn), scale
+        scale = inv_scale.reciprocal()
+        qweight = (tensor.to(torch.float32) * scale).clamp(min=finfo.min,
+                                                           max=finfo.max)
+        qweight = qweight.to(torch.float8_e4m3fn)
+        return qweight
+
+    def per_tensor_dequantize(tensor, inv_scale, dtype):
+        fake_qweight = tensor.to(dtype)
+        dq_weight = fake_qweight * inv_scale
+        return dq_weight
 
     # Note that we use a shape % 4 != 0 to cover edge cases,
     # because scaled_fp8_quant is vectorized by 4.
-    x = torch.randn(size=(10, 10), dtype=torch.float16,
-                    device="cuda").to(dtype)
-    ref_y, ref_scale = fp8_quant_ref(x)
-    ref_y = ref_y.to(dtype)  # Convert back for comparison
+    x = (torch.randn(size=(11, 11), device="cuda") * 13).to(dtype)
 
     # Dynamic quantization
-    y, scale = scaled_fp8_quant(x, None)
-    assert ref_scale.cpu().item() == scale.cpu().item()
-    assert torch.allclose(ref_y, y.to(dtype))
+    ref_y, inv_scale = scaled_fp8_quant(x, None)
+    ref_y = per_tensor_dequantize(ref_y, inv_scale, dtype)
+
+    # Reference dynamic quantizaton
+    y = quantize_ref(x, inv_scale)
+    assert torch.allclose(ref_y,
+                          per_tensor_dequantize(y, inv_scale, dtype))
 
     # Static quantization
-    y, _ = scaled_fp8_quant(x, scale)
-    assert torch.allclose(ref_y, y.to(dtype))
+    y, _ = scaled_fp8_quant(x, inv_scale)
+    assert torch.allclose(ref_y,
+                          per_tensor_dequantize(y, inv_scale, dtype))
 
     # Padding
-    y, _ = scaled_fp8_quant(x, scale, batch_dim_padding=17)
+    y, _ = scaled_fp8_quant(x, inv_scale, batch_dim_padding=17)
     assert y.shape[0] == 17
-    assert torch.allclose(ref_y, torch.narrow(y, 0, 0, x.shape[0]).to(dtype))
+    assert torch.allclose(ref_y,
+                          per_tensor_dequantize(
+                              torch.narrow(y, 0, 0, x.shape[0]), inv_scale,
+                              dtype))
