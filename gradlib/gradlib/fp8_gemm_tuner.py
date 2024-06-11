ff241027abbfdef8a1e9d0c431cf9bc10520884d
Li Li <li.li3@amd.com>
2024-06-12 00:07:30 +0800

    Update fp8_gemm_tuner.py exchange import torch and hipbsolidxgemm (#46)
    * Update fp8_gemm_tuner.py exchange import torch and hipbsolidxgemm
    
    ImportError: libc10.so: cannot open shared object file: No such file or directory
    
    https://stackoverflow.com/a/65710714
    
    * run isort on fp9_gemm_tuner.py
    
    * add # isort: split
    
    * fix yapf
    
    ---------
    
    Co-authored-by: charlifu <charlifu@amd.com>

--- gradlib/gradlib/fp8_gemm_tuner.py
+++ gradlib/gradlib/fp8_gemm_tuner.py
@@ -4,9 +4,9 @@ import os
 import random
 from pathlib import Path
 
+import torch  # isort: split
 import hipbsolidxgemm
 import pandas as pd
-import torch
 import torch.nn.functional as F
 
 hipbsolidxgemm.hipb_create_extension()
