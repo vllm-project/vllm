a29c9c524771a709bd7cc00cb2f3eb161257c020
Wooyeon Lee <wooyeon0.lee@samsung.com>
2024-06-12 17:04:43 +0900

    verify arg

--- vllm/config.py
+++ vllm/config.py
@@ -946,14 +946,21 @@ class SpeculativeConfig:
         This is mostly a copy of the target parallel config.
         """
 
-        _speculative_tensor_parallel_size = (
+        speculative_tensor_parallel_size = (
             speculative_tensor_parallel_size
             or target_parallel_config.tensor_parallel_size
         )
 
+        if speculative_tensor_parallel_size > \
+            target_parallel_config.tensor_parallel_size:
+            raise ValueError(
+                f"{speculative_tensor_parallel_size=} cannot be"
+                f"larger than {target_parallel_config.tensor_parallel_size}"
+            )
+
         draft_parallel_config = ParallelConfig(
             pipeline_parallel_size=target_parallel_config.pipeline_parallel_size,
-            tensor_parallel_size=_speculative_tensor_parallel_size,
+            tensor_parallel_size=speculative_tensor_parallel_size,
             distributed_executor_backend=target_parallel_config.
             distributed_executor_backend,
             max_parallel_loading_workers=target_parallel_config.
