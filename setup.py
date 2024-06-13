ebbc13e9e95db8b87559af75d71d815eb06f7a1c
Kunshang Ji <kunshang.ji@intel.com>
2024-06-13 22:09:36 +0800

    format

--- setup.py
+++ setup.py
@@ -240,6 +240,7 @@ def _is_xpu() -> bool:
 def _build_custom_ops() -> bool:
     return _is_cuda() or _is_hip() or _is_cpu()
 
+
 def _install_punica() -> bool:
     return envs.VLLM_INSTALL_PUNICA_KERNELS
 
