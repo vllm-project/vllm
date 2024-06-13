73dd63ebacd337f3f80b3e3d68ffae84476a9606
simon-mo <simon.mo@hey.com>
2024-06-13 15:20:51 -0700

    don't verbose

--- .buildkite/nightly-benchmarks/wait-for-image.sh
+++ .buildkite/nightly-benchmarks/wait-for-image.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-set -x
-
 TOKEN=$(curl -s -L "https://public.ecr.aws/token?service=public.ecr.aws&scope=repository:q9t5s3a7/vllm-ci-test-repo:pull" | jq -r .token)
 URL="https://public.ecr.aws/v2/q9t5s3a7/vllm-ci-test-repo/manifests/$BUILDKITE_COMMIT"
 
@@ -9,6 +7,8 @@ while [ $retries -lt 1000 ]; do
     if [ $(curl -s -L -H "Authorization: Bearer $TOKEN" -o /dev/null -w "%{http_code}" $URL) -eq 200 ]; then
         exit 0
     fi
+
+    echo "Waiting for image to be available..."
 
     retries=$((retries + 1))
     sleep 5
