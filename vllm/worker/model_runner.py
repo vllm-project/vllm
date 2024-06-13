d7163dedd79e6f3c282872aea47f785acd40f9eb
Yanming Wang <yanmwang@amazon.com>
2024-06-13 21:14:56 +0000

    [Logging] Log cudagraph memory usage

--- vllm/worker/model_runner.py
+++ vllm/worker/model_runner.py
@@ -896,6 +896,7 @@ class ModelRunner:
                     "You can also reduce the `max_num_seqs` as needed "
                     "to decrease memory usage.")
         start_time = time.perf_counter()
+        start_free_memory = torch.cuda.mem_get_info()[0]
 
         # Prepare dummy inputs. These will be reused for all batch sizes.
         max_batch_size = max(_BATCH_SIZES_TO_CAPTURE)
@@ -961,8 +962,11 @@ class ModelRunner:
 
         end_time = time.perf_counter()
         elapsed_time = end_time - start_time
+        end_free_memory = torch.cuda.mem_get_info()[0]
+        consumed_memory = (start_free_memory - end_free_memory) / float(2**30)
         # This usually takes < 10 seconds.
-        logger.info("Graph capturing finished in %.0f secs.", elapsed_time)
+        logger.info("Graph capturing finished in %.0f secs, and took %.4f GB",
+                    elapsed_time, consumed_memory)
 
     @property
     def vocab_size(self) -> int:
