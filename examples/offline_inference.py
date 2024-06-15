e7742e75953702f9f302c3da714ec93fa03fc831
Nick Hill <nickhill@us.ibm.com>
2024-06-14 20:26:36 -0700

    Move offline_inference example to separate file

--- examples/offline_inference.py
+++ examples/offline_inference.py
@@ -1,10 +1,4 @@
-import time
-
 from vllm import LLM, SamplingParams
-
-template = ("Below is an instruction that describes a task. Write a response "
-            "that appropriately completes the request.\n\n### Instruction:\n{}"
-            "\n\n### Response:")
 
 # Sample prompts.
 prompts = [
@@ -13,23 +7,14 @@ prompts = [
     "The capital of France is",
     "The future of AI is",
 ]
-prompts = [template.format(prompt) for prompt in prompts]
 # Create a sampling params object.
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
 # Create an LLM.
-llm = LLM(model="ibm-granite/granite-7b-instruct",
-          use_v2_block_manager=True,
-          enforce_eager=True,
-          speculative_model="ibm-granite/granite-7b-instruct-accelerator",
-          num_speculative_tokens=5)
+llm = LLM(model="facebook/opt-125m")
 # Generate texts from the prompts. The output is a list of RequestOutput objects
 # that contain the prompt, generated text, and other information.
 outputs = llm.generate(prompts, sampling_params)
-start = time.time()
-outputs = llm.generate(prompts, sampling_params)
-end = time.time()
-print((end - start) / sum([len(o.outputs[0].token_ids) for o in outputs]))
 # Print the outputs.
 for output in outputs:
     prompt = output.prompt
