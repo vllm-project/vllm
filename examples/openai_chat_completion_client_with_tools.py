606ec64bd1732fa0a1ffb13ec33d0c56f2714aab
Kyle Mistele <kyle@constellate.ai>
2024-06-16 16:39:01 -0500

    feat(example): add example chat completion with tool usage

---
+++ examples/openai_chat_completion_client_with_tools.py
@@ -0,0 +1,61 @@
+from openai import OpenAI
+
+# Modify OpenAI's API key and API base to use vLLM's API server.
+openai_api_key = "EMPTY"
+openai_api_base = "http://localhost:8000/v1"
+
+client = OpenAI(
+    # defaults to os.environ.get("OPENAI_API_KEY")
+    api_key=openai_api_key,
+    base_url=openai_api_base,
+)
+
+models = client.models.list()
+model = models.data[0].id
+tools = [{
+    "type": "function",
+    "function": {
+        "name": "get_current_weather",
+        "description": "Get the current weather in a given location",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "city": {
+                    "type": "string",
+                    "description": "The city to find the weather for, e.g. 'San Francisco'"
+                },
+                "state": {
+                    "type": "string",
+                    "description": "the two-letter abbreviation for the state that the city is in, e.g. 'CA' which would mean 'California'"
+                },
+                "unit": {
+                    "type": "string",
+                    "description": "The unit to fetch the temperature in",
+                    "enum": ["celsius", "fahrenheit"]
+                }
+            }
+        }
+    }
+}]
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Hi! How are you doing today?"
+        },
+        {
+            "role": "assistant",
+            "content": "I'm doing well! How can I help you?"
+        },
+        {
+            "role": "user",
+            "content": "Can you tell me what the weather will be in Dallas Texas?"
+        }
+    ],
+    model=model,
+    tools=tools
+)
+
+print("Chat completion results:")
+print(chat_completion)
