a648f5df9943c7019f0b5366a1effe53ecedf98f
Wooyeon Lee <wooyeon0.lee@samsung.com>
2024-06-17 11:16:59 +0900

    yapf fix

--- tests/spec_decode/e2e/test_integration_dist.py
+++ tests/spec_decode/e2e/test_integration_dist.py
@@ -98,8 +98,8 @@ def test_target_model_tp_gt_1(baseline_llm_generator, test_llm_generator,
 @pytest.mark.parametrize("batch_size", [2])
 @pytest.mark.parametrize("seed", [1])
 def test_draft_model_tp_lt_target_model(test_llm_generator,
-                                              baseline_llm_generator,
-                                              batch_size: int):
+                                        baseline_llm_generator,
+                                        batch_size: int):
     """Verify spec decode works well with smaller tp for draft models.
     """
     run_greedy_equality_correctness_test(baseline_llm_generator,
