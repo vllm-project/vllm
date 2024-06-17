cb3ac133c86b5cd75155638e2a21c1e25b3d8a01
Roger Wang <ywang@roblox.com>
2024-06-16 23:54:07 -0700

    format

--- vllm/model_executor/models/clip.py
+++ vllm/model_executor/models/clip.py
@@ -1,9 +1,10 @@
-from typing import Iterable, List, Literal, Optional, Tuple, TypedDict, Union
+from typing import Optional, Tuple
 
 import torch
 import torch.nn as nn
 from transformers import CLIPConfig, CLIPVisionConfig
 from transformers.models.clip.modeling_clip import CLIPAttention
+
 from vllm.model_executor.layers.activation import get_act_fn
 from vllm.model_executor.layers.linear import (ColumnParallelLinear,
                                                RowParallelLinear)
@@ -12,6 +13,7 @@ from vllm.model_executor.layers.quantization.base_config import (
 
 
 class CLIPVisionEmbeddings(nn.Module):
+
     def __init__(self, config: CLIPVisionConfig):
         super().__init__()
         self.config = config
@@ -29,15 +31,19 @@ class CLIPVisionEmbeddings(nn.Module):
             bias=False,
         )
 
-        self.num_patches = (self.image_size // self.patch_size) ** 2
+        self.num_patches = (self.image_size // self.patch_size)**2
         self.num_positions = self.num_patches + 1
-        self.position_embedding = nn.Embedding(self.num_positions, self.embed_dim)
-        self.register_buffer("position_ids", torch.arange(self.num_positions).expand((1, -1)), persistent=False)
+        self.position_embedding = nn.Embedding(self.num_positions,
+                                               self.embed_dim)
+        self.register_buffer("position_ids",
+                             torch.arange(self.num_positions).expand((1, -1)),
+                             persistent=False)
 
     def forward(self, pixel_values: torch.FloatTensor) -> torch.Tensor:
         batch_size = pixel_values.shape[0]
         target_dtype = self.patch_embedding.weight.dtype
-        patch_embeds = self.patch_embedding(pixel_values.to(dtype=target_dtype))  # shape = [*, width, grid, grid]
+        patch_embeds = self.patch_embedding(pixel_values.to(
+            dtype=target_dtype))  # shape = [*, width, grid, grid]
         patch_embeds = patch_embeds.flatten(2).transpose(1, 2)
 
         class_embeds = self.class_embedding.expand(batch_size, 1, -1)
@@ -47,12 +53,16 @@ class CLIPVisionEmbeddings(nn.Module):
 
 
 class CLIPMLP(nn.Module):
-    def __init__(self, config: CLIPConfig, quant_config: Optional[QuantizationConfig]):
+
+    def __init__(self, config: CLIPConfig,
+                 quant_config: Optional[QuantizationConfig]):
         super().__init__()
         self.config = config
         self.activation_fn = get_act_fn(config.hidden_act)
-        self.fc1 = ColumnParallelLinear(config.hidden_size, config.intermediate_size, quant_config)
-        self.fc2 = RowParallelLinear(config.intermediate_size, config.hidden_size, quant_config)
+        self.fc1 = ColumnParallelLinear(config.hidden_size,
+                                        config.intermediate_size, quant_config)
+        self.fc2 = RowParallelLinear(config.intermediate_size,
+                                     config.hidden_size, quant_config)
 
     def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
         hidden_states = self.fc1(hidden_states)
@@ -60,27 +70,25 @@ class CLIPMLP(nn.Module):
         hidden_states = self.fc2(hidden_states)
         return hidden_states
 
+
 class CLIPEncoderLayer(nn.Module):
-    def __init__(self, 
-                 config: CLIPConfig):
+
+    def __init__(self, config: CLIPConfig):
         super().__init__()
 
         self.self_attn = CLIPAttention(config)
-        self.layer_norm1 = nn.LayerNorm(config.embed_dim, eps=config.layer_norm_eps)
+        self.layer_norm1 = nn.LayerNorm(config.embed_dim,
+                                        eps=config.layer_norm_eps)
         self.mlp = CLIPMLP(config)
-        self.layer_norm2 = nn.LayerNorm(config.embed_dim, eps=config.layer_norm_eps)
+        self.layer_norm2 = nn.LayerNorm(config.embed_dim,
+                                        eps=config.layer_norm_eps)
 
-    def forward(
-        self,
-        hidden_states: torch.Tensor
-    ) -> Tuple[torch.FloatTensor]:
+    def forward(self, hidden_states: torch.Tensor) -> Tuple[torch.FloatTensor]:
 
         residual = hidden_states
 
         hidden_states = self.layer_norm1(hidden_states)
-        hidden_states = self.self_attn(
-            hidden_states=hidden_states
-        )
+        hidden_states = self.self_attn(hidden_states=hidden_states)
         hidden_states = residual + hidden_states
 
         residual = hidden_states
@@ -93,8 +101,8 @@ class CLIPEncoderLayer(nn.Module):
 
 class CLIPEncoder(nn.Module):
     """
-    Transformer encoder consisting of `config.num_hidden_layers` self attention layers. Each layer is a
-    [`CLIPEncoderLayer`].
+    Transformer encoder consisting of `config.num_hidden_layers` self 
+    attention layers. Each layer is a [`CLIPEncoderLayer`].
 
     Args:
         config: CLIPConfig
@@ -103,21 +111,21 @@ class CLIPEncoder(nn.Module):
     def __init__(self, config: CLIPConfig):
         super().__init__()
         self.config = config
-        self.layers = nn.ModuleList([CLIPEncoderLayer(config) for _ in range(config.num_hidden_layers)])
+        self.layers = nn.ModuleList([
+            CLIPEncoderLayer(config) for _ in range(config.num_hidden_layers)
+        ])
         self.gradient_checkpointing = False
 
-    def forward(
-        self,
-        inputs_embeds: torch.Tensor
-    ):
+    def forward(self, inputs_embeds: torch.Tensor):
 
         hidden_states = inputs_embeds
         for encoder_layer in self.layers:
-            hidden_states = encoder_layer(hidden_states,
-            )
+            hidden_states = encoder_layer(hidden_states, )
         return hidden_states
 
+
 class CLIPVisionTransformer(nn.Module):
+
     def __init__(self, config: CLIPVisionConfig):
         super().__init__()
         self.config = config
@@ -135,11 +143,10 @@ class CLIPVisionTransformer(nn.Module):
         hidden_states = self.embeddings(pixel_values)
         hidden_states = self.pre_layrnorm(hidden_states)
 
-        encoder_outputs = self.encoder(
-            inputs_embeds=hidden_states
-        )
+        encoder_outputs = self.encoder(inputs_embeds=hidden_states)
 
         return encoder_outputs.hidden_states
+
 
 class CLIPVisionModel(nn.Module):
     config_class = CLIPVisionConfig
@@ -151,12 +158,7 @@ class CLIPVisionModel(nn.Module):
 
     def get_input_embeddings(self) -> nn.Module:
         return self.vision_model.embeddings.patch_embedding
-    
-    def forward(
-        self,
-        pixel_values: Optional[torch.FloatTensor] = None
-    ):
 
-        return self.vision_model(
-            pixel_values=pixel_values
-        )
+    def forward(self, pixel_values: Optional[torch.FloatTensor] = None):
+
+        return self.vision_model(pixel_values=pixel_values)
