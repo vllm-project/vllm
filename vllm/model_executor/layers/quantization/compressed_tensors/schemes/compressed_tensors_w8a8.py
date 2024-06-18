1066ef52bc439288b2f32777421f12659af36ab4
Dipika Sikka <dipikasikka1@gmail.com>
2024-06-18 20:55:30 +0000

    format fix

--- vllm/model_executor/layers/quantization/compressed_tensors/schemes/compressed_tensors_w8a8.py
+++ vllm/model_executor/layers/quantization/compressed_tensors/schemes/compressed_tensors_w8a8.py
@@ -1,14 +1,17 @@
+from typing import Callable, List, Tuple, Union
+
+import torch
+from torch.nn import Parameter
+
 from vllm.model_executor.layers.quantization.compressed_tensors.schemes import (
     CompressedTensorsScheme)
 from vllm.model_executor.layers.quantization.compressed_tensors.utils import (
     QuantizationStrategy)
 from vllm.model_executor.utils import set_weight_attrs
-from typing import Union, Tuple, Callable, List
-import torch
-from torch.nn import Parameter
-from vllm.model_executor.utils import set_weight_attrs
+
 
 class CompressedTensorsW8A8(CompressedTensorsScheme):
+
     def __init__(self, strategy: str):
         self.strategy = strategy
 
@@ -66,8 +69,6 @@ class CompressedTensorsW8A8(CompressedTensorsScheme):
                 "weight_loader": weight_loader,
                 "logical_widths": output_partition_sizes
             })
-
-
 
         # Don't need a shard_splitter for channel-wise quantization
         # Use the default loading method
