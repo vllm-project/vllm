3e462538ce8436e0e97f02c0f86c7d7653eb9bf1
Stephanie Wang <swang@cs.berkeley.edu>
2024-06-19 10:57:45 -0700

    lint
    Signed-off-by: Stephanie Wang <swang@cs.berkeley.edu>

--- vllm/worker/worker_base.py
+++ vllm/worker/worker_base.py
@@ -67,7 +67,8 @@ class WorkerBase(ABC):
 
     @abstractmethod
     def execute_model(
-        self, execute_model_req: Optional[ExecuteModelRequest] = None
+        self,
+        execute_model_req: Optional[ExecuteModelRequest] = None
     ) -> Optional[List[SamplerOutput]]:
         raise NotImplementedError
 
@@ -176,7 +177,8 @@ class LocalOrDistributedWorkerBase(WorkerBase):
         raise NotImplementedError
 
     def execute_model(
-        self, execute_model_req: Optional[ExecuteModelRequest] = None
+        self,
+        execute_model_req: Optional[ExecuteModelRequest] = None
     ) -> Optional[List[SamplerOutput]]:
         """Executes at least one model step on the given sequences, unless no
         sequences are provided."""
