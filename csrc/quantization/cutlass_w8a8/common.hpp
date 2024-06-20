9624ef29e5c6d43955dc0fef8082d5322e23848a
Varun Sundar Rabindranath <varunsundar08@gmail.com>
2024-06-20 19:03:21 +0530

    [Kernel] Update Cutlass int8 kernel configs for SM80 (#5275)
    Co-authored-by: Varun Sundar Rabindranath <varun@neuralmagic.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- csrc/quantization/cutlass_w8a8/common.hpp
+++ csrc/quantization/cutlass_w8a8/common.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "cutlass/cutlass.h"
+#include <climits>
 
 /**
  * Helper function for checking CUTLASS errors
@@ -10,3 +11,9 @@
     TORCH_CHECK(status == cutlass::Status::kSuccess, \
                 cutlassGetStatusString(status))      \
   }
+
+inline uint32_t next_pow_2(uint32_t const num) {
+  if (num <= 1) return num;
+  return 1 << (CHAR_BIT * sizeof(num) - __builtin_clz(num - 1));
+}
+
