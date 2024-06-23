28bbc67c5940942238655a37bf3fa3ae21df3101
Varun Sundar Rabindranath <varunsundar08@gmail.com>
2024-06-24 02:37:11 +0530

    [BugFix] [Kernel] Add Cutlass2x fallback kernels (#5744)
    Co-authored-by: Varun Sundar Rabindranath <varun@neuralmagic.com>
    Signed-off-by: Alvant <alvasian@yandex.ru>

--- csrc/quantization/cutlass_w8a8/common.hpp
+++ csrc/quantization/cutlass_w8a8/common.hpp
@@ -17,3 +17,11 @@ inline uint32_t next_pow_2(uint32_t const num) {
   return 1 << (CHAR_BIT * sizeof(num) - __builtin_clz(num - 1));
 }
 
+inline int get_cuda_max_shared_memory_per_block_opt_in(int const device) {
+  int max_shared_mem_per_block_opt_in = 0;
+  cudaDeviceGetAttribute(&max_shared_mem_per_block_opt_in,
+                        cudaDevAttrMaxSharedMemoryPerBlockOptin,
+                        device);
+  return max_shared_mem_per_block_opt_in;
+}
+
