307a8ec5216d8dc494d158d8fa00546edf881f00
Murali Andoorveedu <37849411+andoorve@users.noreply.github.com>
2024-06-23 17:42:28 -0400

    [Distributed] Add send and recv helpers (#5719)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- tests/distributed/test_custom_all_reduce.py
+++ tests/distributed/test_custom_all_reduce.py
@@ -12,8 +12,7 @@ from vllm.distributed.parallel_state import (get_tensor_model_parallel_group,
                                              get_tp_group, graph_capture)
 
 from ..utils import (ensure_model_parallel_initialized,
-                     init_test_distributed_environment,
-                     multi_process_tensor_parallel)
+                     init_test_distributed_environment, multi_process_parallel)
 
 random.seed(42)
 test_sizes = [random.randint(1024, 2048 * 1024) for _ in range(8)]
@@ -113,4 +112,4 @@ def test_custom_allreduce(tp_size, pipeline_parallel_size, test_target):
     world_size = tp_size * pipeline_parallel_size
     if world_size > torch.cuda.device_count():
         pytest.skip("Not enough GPUs to run the test.")
-    multi_process_tensor_parallel(tp_size, pipeline_parallel_size, test_target)
+    multi_process_parallel(tp_size, pipeline_parallel_size, test_target)
