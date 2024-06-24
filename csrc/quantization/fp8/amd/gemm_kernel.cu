c678326e703f1fff06cafc0036c7fc9a1ec21c1a
HaiShaw <hixiao@gmail.com>
2024-06-23 22:59:39 -0700

    Remove unused definition: max_workspace_size

--- csrc/quantization/fp8/amd/gemm_kernel.cu
+++ csrc/quantization/fp8/amd/gemm_kernel.cu
@@ -9,8 +9,6 @@
 #include <hipblaslt/hipblaslt.h>
 #include <hipblaslt/hipblaslt-ext.hpp>
 
-#define max_workspace_size 2 * 128 * 1024 * 1024
-
 #define CHECK_CUDA(x) TORCH_CHECK(x.is_cuda(), #x " must be a CUDA tensor")
 #define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
 #define CHECK_INPUT(x)                                                                                                 \
