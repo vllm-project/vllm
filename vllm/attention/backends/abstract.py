a5d065a052efe8d61f4613b857d336d6786faef8
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2024-06-26 06:45:07 +0000

    Minor typo

--- vllm/attention/backends/abstract.py
+++ vllm/attention/backends/abstract.py
@@ -47,7 +47,7 @@ class AttentionBackend(ABC):
     @abstractmethod
     def copy_blocks(
         kv_caches: List[torch.Tensor],
-        src_to_dists: torch.Tensor,
+        src_to_dst: torch.Tensor,
     ) -> None:
         raise NotImplementedError
 
