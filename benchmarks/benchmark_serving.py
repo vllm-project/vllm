66710c61bca859c2bc43ebc2853fb68efe338d23
haichuan1221 <1778876540@qq.com>
2024-06-26 15:19:20 +0800

    fix format issue

--- benchmarks/benchmark_serving.py
+++ benchmarks/benchmark_serving.py
@@ -530,8 +530,8 @@ def main(args: argparse.Namespace):
                     )
 
         # Traffic
-        result_json["request_rate"] = (args.request_rate if args.request_rate
-                                       < float("inf") else "inf")
+        result_json["request_rate"] = (  # noqa
+            args.request_rate if args.request_rate < float("inf") else "inf")
 
         # Merge with benchmark result
         result_json = {**result_json, **benchmark_result}
