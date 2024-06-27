cdcbfa26f45d85a03b04fc7d6e066de0cd791e0b
Robert Shaw <rshaw@neuralmagic>
2024-06-27 15:22:51 +0000

    stash

--- .buildkite/lm-eval-harness/test_lm_eval_correctness.py
+++ .buildkite/lm-eval-harness/test_lm_eval_correctness.py
@@ -22,7 +22,7 @@ lm_eval: "lm_eval_t" = pytest.importorskip("lm_eval",
 RTOL = 0.02
 TEST_DATA_FILE = os.environ.get(
     "LM_EVAL_TEST_DATA_FILE",
-    ".buildkite/lm-eval-harness/configs/small-models.txt")
+    ".buildkite/lm-eval-harness/small-models.yaml")
 
 
 def wait_for_server(timeout=900) -> bool:
@@ -46,18 +46,12 @@ def wait_for_server(timeout=900) -> bool:
 
 
 def launch_lm_eval(eval_config):
-    os.environ["OPENAI_API_KEY"] = "dummy"
-    openai_args = ",".join([
-        f"model={eval_config['model_name']}",
-        "tokenizer_backend=huggingface",
-        "base_url=http://localhost:8000/v1",
-    ])
+    model_args=f"pretrained={eval_config['model_name']},tensor_parallel_size={cuda_device_count_stateless()}"
 
     results = lm_eval.simple_evaluate(
-        model="local-completions",
-        model_args=openai_args,
+        model="vllm",
+        model_args=model_args,
         tasks=[task["name"] for task in eval_config["tasks"]],
-        batch_size=32,
         num_fewshot=eval_config["num_fewshot"],
         limit=eval_config["limit"],
     )
@@ -69,43 +63,16 @@ def test_lm_eval_correctness():
     eval_config = yaml.safe_load(
         Path(TEST_DATA_FILE).read_text(encoding="utf-8"))
 
-    # Setup server launch.
-    server_args = {
-        "model": eval_config["model_name"],
-        "max-model-len": 4096,
-        "tensor-parallel-size": cuda_device_count_stateless(),
-        # TODO (@robertgshaw2): understand why default (mp) does not
-        # shut down cleanly (it works, but not clean).
-        "distributed-executor-backend": "ray",
-        "disable-log-requests": "",
-    }
+    # Launch eval requests.
+    results = launch_lm_eval(eval_config)
 
-    server_cmd = "python3 -m vllm.entrypoints.openai.api_server " + \
-                    " ".join([f"--{k} {v}"
-                                for k, v in server_args.items()])
-
-    try:
-        # Launch server.
-        server_process = subprocess.Popen("exec " + server_cmd, shell=True)
-        assert wait_for_server(), "Server did not start up in time."
-
-        # Launch eval requests.
-        results = launch_lm_eval(eval_config)
-
-        # Confirm scores match ground truth.
-        for task in eval_config["tasks"]:
-            for metric in task["metrics"]:
-                ground_truth = metric["value"]
-                measured_value = results["results"][task["name"]][
-                    metric["name"]]
-                print(
-                    f'{task["name"]} | {metric["name"]}: '
-                    f'ground_truth={ground_truth} | measured={measured_value}')
-                assert numpy.isclose(ground_truth, measured_value, rtol=RTOL)
-
-    finally:
-        assert server_process is not None
-        server_process.terminate()
-
-        # Make sure the server finishes tearing down.
-        time.sleep(10.)
+    # Confirm scores match ground truth.
+    for task in eval_config["tasks"]:
+        for metric in task["metrics"]:
+            ground_truth = metric["value"]
+            measured_value = results["results"][task["name"]][
+                metric["name"]]
+            print(
+                f'{task["name"]} | {metric["name"]}: '
+                f'ground_truth={ground_truth} | measured={measured_value}')
+            assert numpy.isclose(ground_truth, measured_value, rtol=RTOL)
