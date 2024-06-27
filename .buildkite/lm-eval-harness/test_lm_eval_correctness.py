5e1190cd031a4f72147f01b2514d0d8d0725b763
Robert Shaw <rshaw@neuralmagic>
2024-06-27 15:57:21 +0000

    updated files

--- .buildkite/lm-eval-harness/test_lm_eval_correctness.py
+++ .buildkite/lm-eval-harness/test_lm_eval_correctness.py
@@ -7,7 +7,8 @@ import yaml
 
 RTOL = 0.02
 TEST_DATA_FILE = os.environ.get(
-    "LM_EVAL_TEST_DATA_FILE", ".buildkite/lm-eval-harness/small-models.yaml")
+    "LM_EVAL_TEST_DATA_FILE",
+    ".buildkite/lm-eval-harness/configs/Meta-Llama-3-8B-Instruct.yaml")
 
 TP_SIZE = os.environ.get("LM_EVAL_TP_SIZE", 1)
 
@@ -28,18 +29,20 @@ def launch_lm_eval(eval_config):
 
 
 def test_lm_eval_correctness():
-    eval_config = yaml.safe_load(
+    eval_configs = yaml.safe_load(
         Path(TEST_DATA_FILE).read_text(encoding="utf-8"))
 
-    breakpoint()
-    # Launch eval requests.
-    results = launch_lm_eval(eval_config)
+    for eval_config in eval_configs:
+        # Launch eval requests.
+        results = launch_lm_eval(eval_config)
 
-    # Confirm scores match ground truth.
-    for task in eval_config["tasks"]:
-        for metric in task["metrics"]:
-            ground_truth = metric["value"]
-            measured_value = results["results"][task["name"]][metric["name"]]
-            print(f'{task["name"]} | {metric["name"]}: '
-                  f'ground_truth={ground_truth} | measured={measured_value}')
-            assert numpy.isclose(ground_truth, measured_value, rtol=RTOL)
+        # Confirm scores match ground truth.
+        for task in eval_config["tasks"]:
+            for metric in task["metrics"]:
+                ground_truth = metric["value"]
+                measured_value = results["results"][task["name"]][
+                    metric["name"]]
+                print(
+                    f'{task["name"]} | {metric["name"]}: '
+                    f'ground_truth={ground_truth} | measured={measured_value}')
+                assert numpy.isclose(ground_truth, measured_value, rtol=RTOL)
