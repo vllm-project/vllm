4fed894587bf8f2f258b0339bb2f7171261733ef
rshaw@neuralmagic.com <rshaw@neuralmagic>
2024-06-30 16:26:52 +0000

    fix spurious change in qwen2

--- vllm/model_executor/models/qwen2_moe.py
+++ vllm/model_executor/models/qwen2_moe.py
@@ -435,8 +435,8 @@ class Qwen2MoeForCausalLM(nn.Module):
             for (param_name, weight_name, shard_id) in stacked_params_mapping:
                 if weight_name not in name:
                     continue
-                # Skip loading extra bias for GPTQ models.
                 name = name.replace(weight_name, param_name)
+                # Skip loading extra bias for GPTQ models.
                 if name.endswith(".bias") and name not in params_dict:
                     continue
                 # Skip experts that are not assigned to this worker.
