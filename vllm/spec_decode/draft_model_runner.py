68e27de192b95cd949bbc020633d643de709455c
Mor Zusman <morz@ai21.com>
2024-06-30 13:11:42 +0300

    Format

--- vllm/spec_decode/draft_model_runner.py
+++ vllm/spec_decode/draft_model_runner.py
@@ -83,10 +83,8 @@ class TP1DraftModelRunner(ModelRunner):
         TODO: In-place update model_input and remove this function.
         """
         self.cached_seq_group_metadata_list = seq_group_metadata_list
-        return super().prepare_model_input(
-            seq_group_metadata_list,
-            finished_requests_ids
-        )
+        return super().prepare_model_input(seq_group_metadata_list,
+                                           finished_requests_ids)
 
     def update_model_input(
             self, model_input: ModelInputForGPUWithSamplingMetadata,
