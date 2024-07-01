3ba37dc2d27ee9e0e82a7c0112ce5834c41fe31f
kevin <kevin@anyscale.com>
2024-07-01 22:36:26 +0000

    p
    Signed-off-by: kevin <kevin@anyscale.com>

--- .buildkite/run-multi-node-test.sh
+++ .buildkite/run-multi-node-test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 set -euo pipefail
-set -x
 
 if [[ $# -lt 3 ]]; then
     echo "Please provide the number of nodes and GPU per node."
@@ -24,7 +23,6 @@ fi
 echo "List of commands"
 for command in "${COMMANDS[@]}"; do
     echo $command
-    echo "Z"
 done
 
 start_network() {
@@ -33,7 +31,6 @@ start_network() {
 
 start_nodes() {
     for node in $(seq 0 $(($NUM_NODES-1))); do
-        GPU_DEVICES="'"
         GPU_DEVICES+='"device='
         for node_gpu in $(seq 0 $(($NUM_GPUS - 1))); do
             DEVICE_NUM=$(($node * $NUM_GPUS + $node_gpu))
@@ -43,16 +40,14 @@ start_nodes() {
             fi
         done
         GPU_DEVICES+='"'
-        GPU_DEVICES+="'"
-        echo "Starting node$node with GPU devices: $GPU_DEVICES"
+        # echo "Starting node$node with GPU devices: $GPU_DEVICES"
         docker run -d --gpus "$GPU_DEVICES" --name node$node --network docker-net --ip 192.168.10.$((10 + $node)) --rm $DOCKER_IMAGE tail -f /dev/null
     done
 }
 
 run_nodes() {
     for node in $(seq 0 $(($NUM_NODES-1))); do
-        GPU_DEVICES="'"
-        GPU_DEVICES+='device="'
+        GPU_DEVICES+='"device='
         for node_gpu in $(seq 0 $(($NUM_GPUS - 1))); do
             DEVICE_NUM=$(($node * $NUM_GPUS + $node_gpu))
             GPU_DEVICES+=$(($DEVICE_NUM))
@@ -61,7 +56,6 @@ run_nodes() {
             fi
         done
         GPU_DEVICES+='"'
-        GPU_DEVICES+="'"
         echo "Running node$node with GPU devices: $GPU_DEVICES"
         if [ $node -lt $(($NUM_NODES - 1)) ]; then
             docker run -d --gpus "$GPU_DEVICES" --name node$node --network docker-net --ip 192.168.10.$((10 + $node)) --rm $DOCKER_IMAGE /bin/bash -c "${COMMANDS[$node]}"
@@ -73,9 +67,8 @@ run_nodes() {
 cleanup() {
     docker network rm docker-net
 }
-
-start_network
-start_nodes
-run_nodes
-
 trap cleanup EXIT
+#start_network
+#start_nodes
+#run_nodes
+
