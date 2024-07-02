c92257c63384b8f36c4d28d8641f0cb9eba5efce
Muralidhar Andoorveedu <muralidhar.andoorveedu@centml.ai>
2024-07-02 04:52:05 +0000

    Address Nick nits and fix CUDAGraph correctness
    Signed-off-by: Muralidhar Andoorveedu <muralidhar.andoorveedu@centml.ai>

--- vllm/worker/worker_base.py
+++ vllm/worker/worker_base.py
@@ -141,7 +141,7 @@ class WorkerInput:
             blocks_to_swap_in=tensor_dict.pop("blocks_to_swap_in"),
             blocks_to_swap_out=tensor_dict.pop("blocks_to_swap_out"),
             blocks_to_copy=tensor_dict.pop("blocks_to_copy"),
-            virtual_engine=tensor_dict.pop("virtual_engine"),
+            virtual_engine=tensor_dict["virtual_engine"],
         )
 
     def as_broadcastable_tensor_dict(
