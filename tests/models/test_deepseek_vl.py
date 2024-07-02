80533ea6b16e6114b6be44aae6cef1cf8084af5d
刘衍聪_Yancong <sp.yancong.liu@enflame.cn>
2024-07-02 16:17:35 +0800

    uodate test case and fix test bugs

--- tests/models/test_deepseek_vl.py
+++ tests/models/test_deepseek_vl.py
@@ -1,36 +1,33 @@
 from typing import List, Optional, Tuple, Type
 
 import pytest
-
 import torch
-from transformers import AutoTokenizer
-from transformers import AutoModelForCausalLM
-
-from transformers import LlamaForCausalLM
-from transformers import AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, LlamaForCausalLM
 
 from vllm.config import VisionLanguageConfig
-
-from ..conftest import IMAGE_ASSETS, HfRunner, VllmRunner, _ImageAssets
-
 from vllm.model_executor.models.deepseek_vl import (
-    model_name_to_cls,
-    MultiModalityPreTrainedModel,
-    VLMImageProcessor,
-)
+    MultiModalityPreTrainedModel, VLMImageProcessor, model_name_to_cls)
 from vllm.transformers_utils.config import DeepSeekMultiModalityConfig
 
+from ..conftest import HfRunner, VllmRunner, _ImageAssets
 
 pytestmark = pytest.mark.vlm
 
 # The image token is placed before "user" on purpose so that the test can pass
 HF_IMAGE_PROMPTS = [
-    "You are a helpful language and vision assistant. You are able to understand the visual content that the user provides, and assist the user with a variety of tasks using natural language.\n User: <image_placeholder>What's the content of the image?\nAssistant:",
-    "You are a helpful language and vision assistant. You are able to understand the visual content that the user provides, and assist the user with a variety of tasks using natural language.\n User: <image_placeholder>What is the season?\nAssistant:",
+    "You are a helpful language and vision assistant. You are able" \
+      "to understand the visual content that the user provides, and assist " \
+      "the user with a variety of tasks using natural language.\n User:" \
+      " <image_placeholder>What's the content of the image?\nAssistant:",
+    "You are a helpful language and vision assistant. You are able to "\
+    "understand the visual content that the user provides, and assist the "\
+    "user with a variety of tasks using natural language.\n User: "\
+    "<image_placeholder>What is the season?\nAssistant:",
 ]
 
 
 class MultiModalityCausalLM(MultiModalityPreTrainedModel):
+
     def __init__(self, config: DeepSeekMultiModalityConfig):
         super().__init__(config)
 
@@ -77,13 +74,11 @@ class MultiModalityCausalLM(MultiModalityPreTrainedModel):
         # [b, T, D]
         input_ids[input_ids < 0] = 0  # ignore the image embeddings
         inputs_embeds = self.language_model.get_input_embeddings()(
-            input_ids
-        ).reshape(1, -1, 4096)
+            input_ids).reshape(1, -1, 4096)
 
         # replace with the image embeddings
         images_embeds = images_embeds.reshape(
-            1, -1, self.config.aligner_config.params["n_embed"]
-        )
+            1, -1, self.config.aligner_config.params["n_embed"])
         inputs_embeds[images_seq_mask] = images_embeds
 
         return inputs_embeds
@@ -99,15 +94,21 @@ def get_input(tokenizer, prompt, image):
     images_outputs = vl_image(image, return_tensors="pt")
     images_emb_mask = torch.ones(1, 1, 576) == 1
     prepare = {
-        "sft_format": prompt,
-        "input_ids": input_ids.to("cuda"),
-        "pixel_values": images_outputs.pixel_values.to(torch.bfloat16)
-        .to("cuda")
-        .reshape(1, -1, 3, 1024, 1024),
-        "num_image_tokens": 576,
-        "images_seq_mask": image_token_mask.to("cuda").reshape(1, -1),
-        "images_emb_mask": images_emb_mask.to("cuda"),
-        "attention_mask": torch.ones(1, len(input_ids)).to("cuda"),
+        "sft_format":
+        prompt,
+        "input_ids":
+        input_ids.to("cuda"),
+        "pixel_values":
+        images_outputs.pixel_values.to(torch.bfloat16).to("cuda").reshape(
+            1, -1, 3, 1024, 1024),
+        "num_image_tokens":
+        576,
+        "images_seq_mask":
+        image_token_mask.to("cuda").reshape(1, -1),
+        "images_emb_mask":
+        images_emb_mask.to("cuda"),
+        "attention_mask":
+        torch.ones(1, len(input_ids)).to("cuda"),
     }
     return prepare
 
@@ -148,7 +149,8 @@ def vllm_to_hf_output(
     """Sanitize vllm output to be comparable with hf output.
     The function reduces `input_ids` from 1, 32000, 32000, ..., 32000,
     x1, x2, x3 ... to 1, 32000, x1, x2, x3 ...
-    It also reduces `output_str` from "<image_placeholder><image_placeholder>bla" to "bla".
+    It also reduces `output_str` from 
+    "<image_placeholder><image_placeholder>bla" to "bla".
     """
     input_ids, output_str = vllm_output
     image_token_id = vlm_config.image_token_id
@@ -157,13 +159,11 @@ def vllm_to_hf_output(
     image_token_str = tokenizer.decode(image_token_id)
 
     hf_input_ids = [
-        input_id
-        for idx, input_id in enumerate(input_ids)
+        input_id for idx, input_id in enumerate(input_ids)
         if input_id != image_token_id or input_ids[idx - 1] != image_token_id
     ]
     hf_output_str = output_str.replace(
-        image_token_str * vlm_config.image_feature_size, ""
-    )
+        image_token_str * vlm_config.image_feature_size, "")
 
     return hf_input_ids, hf_output_str
 
@@ -199,40 +199,37 @@ def run_test(
         p.replace(
             "<image_placeholder>",
             "<image_placeholder>" * vlm_config.image_feature_size,
-        )
-        for p in HF_IMAGE_PROMPTS
+        ) for p in HF_IMAGE_PROMPTS
     ]
 
-    with vllm_runner(model_id,
-                     dtype=dtype,
-                     tensor_parallel_size=tensor_parallel_size,
-                     distributed_executor_backend=distributed_executor_backend,
-                     enforce_eager=True,
-                     **vlm_config.as_cli_args_dict()) as vllm_model:
+    with vllm_runner(
+            model_id,
+            dtype=dtype,
+            tensor_parallel_size=tensor_parallel_size,
+            distributed_executor_backend=distributed_executor_backend,
+            enforce_eager=True,
+            **vlm_config.as_cli_args_dict(),
+    ) as vllm_model:
         vllm_images = [asset.for_vllm(vlm_config) for asset in image_assets]
-        vllm_outputs = vllm_model.generate_greedy(
-            vllm_image_prompts, max_tokens, images=vllm_images
-        )
-    AutoModelForCausalLM.register(
-        DeepSeekMultiModalityConfig, MultiModalityCausalLM
-    )
+        vllm_outputs = vllm_model.generate_greedy(vllm_image_prompts,
+                                                  max_tokens,
+                                                  images=vllm_images)
+    AutoModelForCausalLM.register(DeepSeekMultiModalityConfig,
+                                  MultiModalityCausalLM)
     tokenizer = AutoTokenizer.from_pretrained(model_id)
-    hf_model = AutoModelForCausalLM.from_pretrained(
-        model_id, trust_remote_code=True
-    )
+    hf_model = AutoModelForCausalLM.from_pretrained(model_id,
+                                                    trust_remote_code=True)
     hf_model = hf_model.to("cuda").eval()
     prepare_input_one = get_input(
         tokenizer,
-        HF_IMAGE_PROMPTS[0].replace(
-            "<image_placeholder>", "<image_placeholder>" * 576
-        ),
+        HF_IMAGE_PROMPTS[0].replace("<image_placeholder>",
+                                    "<image_placeholder>" * 576),
         hf_images,
     )
     prepare_input_two = get_input(
         tokenizer,
-        HF_IMAGE_PROMPTS[1].replace(
-            "<image_placeholder>", "<image_placeholder>" * 576
-        ),
+        HF_IMAGE_PROMPTS[1].replace("<image_placeholder>",
+                                    "<image_placeholder>" * 576),
         hf_images,
     )
     prepare_input_one = hf_model.prepare_inputs_embeds(**prepare_input_one)
@@ -252,23 +249,20 @@ def run_test(
         do_sample=False,
         use_cache=True,
     )
-    hf_outputs = []
+    hf_outputs: List = []
     for o in outputs:
         hf_outputs.append(
-            o, tokenizer.decode(o.cpu().tolist(), skip_special_tokens=True)
-        )
+            (o, tokenizer.decode(o.cpu().tolist(), skip_special_tokens=True)))
 
     for i in range(len(HF_IMAGE_PROMPTS)):
         hf_output_ids, hf_output_str = hf_outputs[i]
         vllm_output_ids, vllm_output_str = vllm_to_hf_output(
-            vllm_outputs[i], vlm_config, model_id
-        )
+            vllm_outputs[i], vlm_config, model_id)
         assert (
             hf_output_str == vllm_output_str
         ), f"Test{i}:\nHF: {hf_output_str!r}\nvLLM: {vllm_output_str!r}"
-        assert (
-            hf_output_ids == vllm_output_ids
-        ), f"Test{i}:\nHF: {hf_output_ids}\nvLLM: {vllm_output_ids}"
+        assert (hf_output_ids == vllm_output_ids
+                ), f"Test{i}:\nHF: {hf_output_ids}\nvLLM: {vllm_output_ids}"
 
 
 @pytest.mark.parametrize("model_and_config", model_and_vl_config)
