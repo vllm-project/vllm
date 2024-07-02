a1207f2ae9579cdddf6c5f169f46f16ee274eb92
Kyle Mistele <kyle@constellate.ai>
2024-07-02 17:23:43 -0500

    unfinished: work on hermes 2 tool call parser

--- vllm/entrypoints/openai/tool_parsers.py
+++ vllm/entrypoints/openai/tool_parsers.py
@@ -5,6 +5,7 @@ from transformers import (AutoTokenizer, PreTrainedTokenizer,
                           PreTrainedTokenizerFast)
 import json
 from pydantic import BaseModel
+import re
 
 logger = init_logger(__name__)
 
@@ -14,11 +15,25 @@ class ToolParser:
     def __init__(self):
         pass
 
+    tool_call_start: str = '<tool_call>'
+    tool_call_end: str = '</tool_call>'
+
+    # regex to match between <tool_call> and </tool_call> OR between <tool_call> and EOS (happens sometimes :))
+    tool_call_regex = re.compile(r'<tool_call>(.*?)</tool_call>|<tool_call>(.*)', re.DOTALL)
+
     def extract_tool_calls(self, model_response: ChatCompletionResponse) -> List[ToolCall]:
         """
         Abstract method intended to be used for extracting tool calls for use in a NON-STREAMING response
         """
-        raise NotImplementedError('AbstractToolParser.extract_tool_calls has not been implemented!')
+
+        # sanity check; avoid unnecessary processing
+        if self.tool_call_start not in model_response.choices[0].message.content:
+            return []
+
+        tool_call_tuples = self.tool_call_regex.findall(model_response.choices[0].message.content)
+        tool_calls = [match[0] if match[0] else match[1] for match in tool_call_tuples]
+        print('got tool calls for hermes 2 pro!', tool_calls)
+        return []
 
     def extract_tool_calls_streaming(self, generator):
         raise NotImplementedError('AbstractToolParser.extract_tool_calls_streaming has not been implemented!')
