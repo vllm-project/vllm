0128ff00b8e61950915ba5cf48425da8c4122a7f
youkaichao <youkaichao@gmail.com>
2024-07-03 16:40:31 -0700

    [core][distributed] support n layers % pp size != 0 (#6115)
    Signed-off-by: Alvant <alvasian@yandex.ru>

--- vllm/distributed/utils.py
+++ vllm/distributed/utils.py
@@ -50,8 +50,15 @@ def split_tensor_along_last_dim(
 
 def get_pp_indices(num_hidden_layers: int, pp_rank: int,
                    pp_size: int) -> Tuple[int, int]:
-    layers_per_partition = divide(num_hidden_layers, pp_size)
+    """Try to evenly distribute layers across partitions.
+    If the number of layers is not divisible by the number of partitions,
+    the last partition will have the remaining layers.
+    """
+    layers_per_partition = num_hidden_layers // pp_size
     start_layer = pp_rank * layers_per_partition
     end_layer = start_layer + layers_per_partition
+
+    if pp_rank == pp_size - 1:
+        end_layer = num_hidden_layers
 
     return (start_layer, end_layer)
--- vllm/worker/openvino_worker.py
+++ vllm/worker/openvino_worker.py
@@ -154,6 +154,7 @@ class OpenVINOWorker(LoraNotSupportedWorkerBase):
     ) -> None:
         self.model_config = model_config
         self.parallel_config = parallel_config
+        self.parallel_config.rank = rank
         self.scheduler_config = scheduler_config
         self.device_config = device_config
         self.cache_config = cache_config
