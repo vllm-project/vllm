79f59fec5cb8269431b62df4c38d1c66330ea9fb
Robert Shaw <114415538+robertgshaw2-neuralmagic@users.noreply.github.com>
2024-07-02 22:54:29 -0400

    Update fp8.py

--- vllm/model_executor/layers/quantization/fp8.py
+++ vllm/model_executor/layers/quantization/fp8.py
@@ -240,6 +240,7 @@ class Fp8LinearMethod(LinearMethodBase):
         # ops.scaled_fp8_quant supports both dynamic and static quant.
         #   If dynamic, layer.input_scale is None and x_scale computed from x.
         #   If static, layer.input_scale is scalar and x_scale is input_scale.
+
         if bias is None and self.cutlass_fp8_supported:
             qinput, x_scale = ops.scaled_fp8_quant(x, layer.input_scale)
 
