e613a1dc892a16775046ff8eed0add21bb63e9f3
Abhinav Goyal <abhinav.goyal@flipkart.com>
2024-07-03 14:03:28 +0530

    minor fixes after merge

--- vllm/model_executor/models/medusa.py
+++ vllm/model_executor/models/medusa.py
@@ -68,8 +68,7 @@ class Medusa(nn.Module):
         logits = []
 
         for hs, lm_head in zip(hidden_states, self.lm_heads):
-            _logits = self.logits_processor(lm_head.weight, hs,
-                                            sampling_metadata)
+            _logits = self.logits_processor(lm_head, hs, sampling_metadata)
 
             if self.token_map is None:
                 logits.append(_logits)
@@ -129,24 +128,32 @@ class Medusa(nn.Module):
 
     def load_weights(self, weights: Iterable[Tuple[str, torch.Tensor]]):
         params_dict = dict(self.named_parameters())
+
+        weights_map = {}
+
         for name, loaded_weight in weights:
             name = name.replace("medusa_heads.", "")
 
-            if name == "token_map" and\
-                self.truncated_vocab_size < self.orig_vocab_size:
-                self.token_map = nn.Parameter(
-                    loaded_weight, requires_grad=False).to(
-                        device=self.lm_heads[0].weight.device)
-                continue
+            if name == "token_map":
+                if self.truncated_vocab_size < self.orig_vocab_size:
+                    self.token_map = nn.Parameter(loaded_weight,
+                                                  requires_grad=False)
+            elif name in params_dict:
+                weights_map[name] = loaded_weight
 
-            # Skip loading extra heads
-            if name not in params_dict:
-                continue
+        for name, loaded_weight in weights_map.items():
+            if "lm_head" in name and self.token_map is not None and\
+                loaded_weight.shape[0] > self.token_map.shape[0]:
+
+                loaded_weight = loaded_weight[self.token_map]
 
             param = params_dict[name]
             weight_loader = getattr(param, "weight_loader",
                                     default_weight_loader)
             weight_loader(param, loaded_weight)
+
+        if self.token_map is not None:
+            self.token_map.to(device=self.lm_heads[0].weight.device)
 
         assert (self.truncated_vocab_size
                 == self.orig_vocab_size) or (self.token_map is not None)
