487f87aee1b5f20b5c52944deedc747c0880a24d
youkaichao <youkaichao@126.com>
2024-07-02 17:00:18 -0700

    fix variable

--- vllm/platforms/__init__.py
+++ vllm/platforms/__init__.py
@@ -3,10 +3,12 @@ import torch
 from .interface import Platform, PlatformEnum
 
 if torch.version.cuda is not None:
-    from .cuda import CudaPlatform as CurrentPlatform
+    from .cuda import CudaPlatform
+    current_platform = CudaPlatform()
 elif torch.version.hip is not None:
-    from .rocm import RocmPlatform as CurrentPlatform
+    from .rocm import RocmPlatform
+    current_platform = RocmPlatform()
 else:
-    CurrentPlatform = None
+    current_platform = None
 
-__all__ = ['Platform', 'PlatformEnum', 'CurrentPlatform']
+__all__ = ['Platform', 'PlatformEnum', 'current_platform']
