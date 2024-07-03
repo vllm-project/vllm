3b56f133000dde81d33f3d4e1e118de0d70a7b9c
jiang1.li <jiang1.li@intel.com>
2024-07-03 16:15:36 +0000

    update

--- .buildkite/run-cpu-test.sh
+++ .buildkite/run-cpu-test.sh
@@ -2,6 +2,8 @@
 # It serves a sanity check for compilation and basic model usage.
 set -ex
 
+docker image prune -f
+
 # Try building the docker image
 docker build -t cpu-test -f Dockerfile.cpu .
 docker build --build-arg VLLM_CPU_DISABLE_AVX512="true" -t cpu-test-avx2 -f Dockerfile.cpu .
@@ -28,4 +30,18 @@ docker exec cpu-test bash -c "cd tests;
   pytest -v -s tests/models -m \"not vlm\" --ignore=tests/models/test_embedding.py --ignore=tests/models/test_registry.py --ignore=tests/models/test_jamba.py" # Mamba on CPU is not supported
 
 # online inference
-docker exec cpu-test bash -c "VLLM_CPU_KVCACHE_SPACE=10 VLLM_CPU_OMP_THREADS_BIND=all python3 -m vllm.entrypoints.openai.api_server --model facebook/opt-125m & server_pid=$! && wget https://huggingface.co/datasets/anon8231489123/ShareGPT_Vicuna_unfiltered/resolve/main/ShareGPT_V3_unfiltered_cleaned_split.json && timeout 600 bash -c 'until curl localhost:8000/v1/models; do sleep 1; done' || exit 1 && python3 benchmarks/benchmark_serving.py --backend vllm --dataset-name sharegpt --dataset ./ShareGPT_V3_unfiltered_cleaned_split.json --model facebook/opt-125m --num-prompts 20 --endpoint /v1/completions --tokenizer facebook/opt-125m && echo $server_pid && kill $server_pid"
+docker exec cpu-test bash -c "
+  export VLLM_CPU_KVCACHE_SPACE=10 
+  export VLLM_CPU_OMP_THREADS_BIND=96-142 
+  python3 -m vllm.entrypoints.openai.api_server --model facebook/opt-125m & server_pid=$! 
+  wget https://huggingface.co/datasets/anon8231489123/ShareGPT_Vicuna_unfiltered/resolve/main/ShareGPT_V3_unfiltered_cleaned_split.json 
+  timeout 600 bash -c 'until curl localhost:8000/v1/models; do sleep 1; done' || exit 1
+  python3 benchmarks/benchmark_serving.py \
+    --backend vllm \
+    --dataset-name sharegpt \
+    --dataset ./ShareGPT_V3_unfiltered_cleaned_split.json \
+    --model facebook/opt-125m \
+    --num-prompts 20 \
+    --endpoint /v1/completions \
+    --tokenizer facebook/opt-125m
+  echo $server_pid && kill $server_pid"
