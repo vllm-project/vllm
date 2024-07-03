a0fd035a8f189a30536f7b90bf763d56679389bb
Robert Shaw <rshaw@neuralmagic.com>
2024-07-03 22:42:20 +0000

    fix nit

--- vllm/model_executor/layers/quantization/compressed_tensors/schemes/__init__.py
+++ vllm/model_executor/layers/quantization/compressed_tensors/schemes/__init__.py
@@ -1,17 +1,18 @@
 from .compressed_tensors_scheme import CompressedTensorsScheme
 from .compressed_tensors_unquantized import CompressedTensorsUnquantized
-from .compressed_tensors_w4a16_24 import CompressedTensorsW4A16Sparse24, W4A16SPARSE24_SUPPORTED_BITS, 
+from .compressed_tensors_w4a16_24 import (CompressedTensorsW4A16Sparse24,
+                                          W4A16SPARSE24_SUPPORTED_BITS)
 from .compressed_tensors_w8a8 import CompressedTensorsW8A8
 from .compressed_tensors_w8a8_fp8 import CompressedTensorsW8A8Fp8
-from .compressed_tensors_wNa16 import CompressedTensorsWNA16
+from .compressed_tensors_wNa16 import (CompressedTensorsWNA16,
+                                       WNA16_SUPPORTED_BITS)
 
 __all__ = [
     "CompressedTensorsScheme",
     "CompressedTensorsUnquantized",
     "CompressedTensorsWNA16",
     "CompressedTensorsW4A16Sparse24",
-    "CompressedTensorsW8A8DynamicToken",
-    "CompressedTensorsW8A8StaticTensor",
+    "CompressedTensorsW8A8",
     "CompressedTensorsW8A8Fp8",
     "WNA16_SUPPORTED_BITS",
     "W4A16SPARSE24_SUPPORTED_BITS",
