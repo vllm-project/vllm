bdce9fbdcde57e93605d8ab8303318034d1a2282
Robert Shaw <114415538+robertgshaw2-neuralmagic@users.noreply.github.com>
2024-07-03 18:50:08 -0400

    [ Misc ] Clean Up `CompressedTensorsW8A8` (#6113)
    Signed-off-by: Alvant <alvasian@yandex.ru>

--- vllm/model_executor/layers/quantization/compressed_tensors/schemes/__init__.py
+++ vllm/model_executor/layers/quantization/compressed_tensors/schemes/__init__.py
@@ -3,9 +3,6 @@ from .compressed_tensors_unquantized import (  # noqa: F401
     CompressedTensorsUnquantized)
 from .compressed_tensors_w4a16_24 import (  # noqa: F401
     W4A16SPARSE24_SUPPORTED_BITS, CompressedTensorsW4A16Sparse24)
-from .compressed_tensors_w8a8_dynamictoken import (  # noqa: F401, E501
-    CompressedTensorsW8A8DynamicToken)
-from .compressed_tensors_w8a8_statictensor import (  # noqa: F401, E501
-    CompressedTensorsW8A8StaticTensor)
+from .compressed_tensors_w8a8 import CompressedTensorsW8A8  # noqa: F401
 from .compressed_tensors_wNa16 import WNA16_SUPPORTED_BITS  # noqa: F401
 from .compressed_tensors_wNa16 import CompressedTensorsWNA16  # noqa: F401
