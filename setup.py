def464e3d70a5554c12758995ae7c57dc02065f6
Konrad Zawora <kzawora@habana.ai>
2024-07-04 14:43:01 +0300

    fix hpu autodetection (again)

--- setup.py
+++ setup.py
@@ -206,6 +206,7 @@ class cmake_build_ext(build_ext):
 
 
 def _is_hpu() -> bool:
+    is_hpu_available = True
     try:
         subprocess.run(["hl-smi"], capture_output=True, check=True)
     except (FileNotFoundError, PermissionError, subprocess.CalledProcessError):
