a2971dcb42ade261584f19868894860347ccc218
Alexander Matveev <alexm@neuralmagic.com>
2024-07-09 19:04:32 +0000

    Rob's comment

--- csrc/quantization/gptq_marlin/gptq_marlin.cu
+++ csrc/quantization/gptq_marlin/gptq_marlin.cu
@@ -1506,8 +1506,11 @@ bool is_valid_config(thread_config_t const& th_config, int max_m_blocks,
   // For act_order with is_k_full == False, we load a chunk of scales.
   // If GPU L1 cache is small, then reduce the N thread size to ensure
   // we load less data
-  if (has_act_order && !is_k_full && max_shared_mem < 1024 * 110) {
-    if (th_config.thread_n > 64) {
+  if (has_act_order && !is_k_full &&
+      max_shared_mem <
+          1024 * 110 /* Small GPUs have around 100K of L1 cache */) {
+    if (th_config.thread_n >
+        64 /* thread_n == 64 is the minimal one that we want to have */) {
       return false;
     }
   }
