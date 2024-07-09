13d8c0479f218528dd149362d393deae869fb0bd
Kuntai Du <kuntai@uchicago.edu>
2024-07-09 16:31:27 -0700

    allow running performance benchmark & nightly benchmark simultaneously

--- .buildkite/nightly-benchmarks/kickoff-pipeline.sh
+++ .buildkite/nightly-benchmarks/kickoff-pipeline.sh
@@ -10,7 +10,6 @@ apt install -y curl jq
 # Install minijinja for templating
 curl -sSfL https://github.com/mitsuhiko/minijinja/releases/latest/download/minijinja-cli-installer.sh | sh
 source $HOME/.cargo/env
-target_yaml_file=""
 
 # If BUILDKITE_PULL_REQUEST != "false", then we check the PR labels using curl and jq
 if [ "$BUILDKITE_PULL_REQUEST" != "false" ]; then
@@ -18,16 +17,11 @@ if [ "$BUILDKITE_PULL_REQUEST" != "false" ]; then
 
   if [[ $PR_LABELS == *"perf-benchmarks"* ]]; then
     echo "This PR has the 'perf-benchmarks' label. Proceeding with the performance benchmarks."
-    target_yaml_file=".buildkite/nightly-benchmarks/benchmark-pipeline.yaml"
+    buildkite-agent pipeline upload .buildkite/nightly-benchmarks/benchmark-pipeline.yaml
   fi
 
   if [[ $PR_LABELS == *"nightly-benchmarks"* ]]; then
     echo "This PR has the 'nightly-benchmark' label. Proceeding with the nightly benchmarks."
-    target_yaml_file=".buildkite/nightly-benchmarks/nightly-pipeline.yaml"
+    buildkite-agent pipeline upload .buildkite/nightly-benchmarks/nightly-pipeline.yaml
   fi
-fi
-
-if [ -n "$target_yaml_file" ]; then
-  # Upload sample.yaml
-  buildkite-agent pipeline upload $target_yaml_file
 fi
