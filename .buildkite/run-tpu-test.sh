1ffd872ca785182f506b8645a926a09a2125a271
kevin <kevin@anyscale.com>
2024-07-10 07:39:06 +0000

    p
    Signed-off-by: kevin <kevin@anyscale.com>

--- .buildkite/run-tpu-test.sh
+++ .buildkite/run-tpu-test.sh
@@ -2,10 +2,7 @@ set -ex
 
 echo $USER
 groups $USER
-sudo usermod -aG docker buildkite-agent
-sudo systemctl restart docker
-
-
+newgrp docker
 
 # Build the docker image.
 docker build -f Dockerfile.tpu -t vllm-tpu .
