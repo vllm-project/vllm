28751c85b246e96134b01b584d0d5d95e4ebc126
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2024-07-10 16:45:18 +0000

    Reflect comment

--- .buildkite/run-tpu-test.sh
+++ .buildkite/run-tpu-test.sh
@@ -1,4 +1,4 @@
-set -ex
+set -e
 
 # Build the docker image.
 docker build -f Dockerfile.tpu -t vllm-tpu .
@@ -8,5 +8,5 @@ remove_docker_container() { docker rm -f tpu-test || true; }
 trap remove_docker_container EXIT
 
 # Run a simple end-to-end example.
-docker run --privileged --net host --shm-size=16G -it -e HF_TOKEN --name tpu-test vllm-tpu \
+docker run --privileged --net host --shm-size=16G -it -e HF_TOKEN=$HF_TOKEN --name tpu-test vllm-tpu \
     python3 /workspace/vllm/examples/offline_inference_tpu.py
