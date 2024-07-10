db0e64fd1717e22c699ba6383326f98cd973ee79
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2024-07-10 19:31:39 +0000

    Clean up prev container

--- .buildkite/run-tpu-test.sh
+++ .buildkite/run-tpu-test.sh
@@ -6,7 +6,10 @@ docker build -f Dockerfile.tpu -t vllm-tpu .
 # Set up cleanup.
 remove_docker_container() { docker rm -f tpu-test || true; }
 trap remove_docker_container EXIT
+# Remove the container that might not be cleaned up in the previous run.
+remove_docker_container
 
+# For HF_TOKEN.
 source /etc/environment
 # Run a simple end-to-end example.
 docker run --privileged --net host --shm-size=16G -it -e HF_TOKEN=$HF_TOKEN --name tpu-test vllm-tpu \
