ee4097aa61cc4d0624a80db4f5a8d7e8ef514843
kevin <kevin@anyscale.com>
2024-07-10 09:27:45 +0000

    p
    Signed-off-by: kevin <kevin@anyscale.com>

--- .buildkite/run-tpu-test.sh
+++ .buildkite/run-tpu-test.sh
@@ -10,5 +10,5 @@ trap remove_docker_container EXIT
 source /etc/environment
 echo $HF_TOKEN
 # Run a simple end-to-end example.
-docker run --privileged --net host --shm-size=16G -it -e HF_TOKEN --name tpu-test vllm-tpu \
+docker run --privileged --net host --shm-size=16G -it -e HF_TOKEN=$HF_TOKEN --name tpu-test vllm-tpu \
     python3 /workspace/vllm/examples/offline_inference_tpu.py
