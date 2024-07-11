d568203ee1f569267bc69d4ac460bc2dd7c6b265
aniaan <hi@aniaan.dev>
2024-07-11 11:15:29 +0800

    fix(xpu): fix max_context_len_to_capture

--- vllm/worker/xpu_model_runner.py
+++ vllm/worker/xpu_model_runner.py
@@ -109,9 +109,8 @@ class XPUModelRunner(ModelRunnerBase[ModelInputForXPU]):
 
         self.kv_cache_dtype = kv_cache_dtype
         self.block_size = cache_config.block_size
-        self.max_context_len_to_capture = (
-            self.model_config.max_context_len_to_capture
-            if self.model_config is not None else 0)
+        self.max_context_len_to_capture = (None if self.model_config
+                                           is not None else 0)
 
         self.attn_backend = get_attn_backend(
             self.model_config.get_num_attention_heads(self.parallel_config),
