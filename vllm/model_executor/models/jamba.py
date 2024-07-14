57559b3d64f6645f32bdcaf4f79e7c2cd0f64ffe
Mor Zusman <morz@ai21.com>
2024-07-14 17:38:27 +0300

    Format

--- vllm/model_executor/models/jamba.py
+++ vllm/model_executor/models/jamba.py
@@ -36,10 +36,8 @@ from vllm.model_executor.models.interfaces import HasInnerState
 from vllm.model_executor.sampling_metadata import SamplingMetadata
 from vllm.model_executor.utils import set_weight_attrs
 from vllm.sequence import IntermediateTensors, SamplerOutput
-from vllm.worker.model_runner import (
-    _BATCH_SIZES_TO_CAPTURE,
-    _get_graph_batch_size,
-)
+from vllm.worker.model_runner import (_BATCH_SIZES_TO_CAPTURE,
+                                      _get_graph_batch_size)
 
 KVCache = Tuple[torch.Tensor, torch.Tensor]
 
