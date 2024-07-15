92d9906a5a26afc22c5cc2a829eec8098b6e188f
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2024-07-15 19:19:04 +0000

    Fix

--- vllm/model_executor/layers/fused_moe/moe_tpu.py
+++ vllm/model_executor/layers/fused_moe/moe_tpu.py
@@ -1,10 +1,10 @@
 import torch
 import torch.nn.functional as F
 import torch_xla.core.xla_model as xm
-from torch_xla.experimental.custom_kernel import _histogram, gmm
+from torch_xla.experimental.custom_kernel import _histogram
 
 
-# @torch.compile(backend="openxla")
+@torch.compile(backend="openxla")
 def fused_moe(
     hidden_states: torch.Tensor,
     w1: torch.Tensor,
@@ -15,10 +15,10 @@ def fused_moe(
 ) -> torch.Tensor:
     """
     Args:
-        hidden_states: [batch_size, seq_len, hidden_size]
+        hidden_states: [*, hidden_size]
         w1: [num_experts, hidden_size, intermediate_size * 2]
         w2: [num_experts, intermediate_size, hidden_size]
-        gating_output: [batch_size, seq_len, num_experts]
+        gating_output: [*, num_experts]
     """
     orig_shape = hidden_states.shape
     hidden_size = hidden_states.shape[-1]
@@ -45,53 +45,12 @@ def fused_moe(
     group_sizes = _histogram(topk_indices.to(torch.int32), 0, num_experts - 1)
 
     x = hidden_states[token_indices]
-    x = gmm(x, w1, group_sizes)
+    x = torch.ops.xla.gmm(x, w1, group_sizes)
     x = F.silu(x[..., :intermediate_size]) * x[..., intermediate_size:]
-    x = gmm(x, w2, group_sizes)
+    x = torch.ops.xla.gmm(x, w2, group_sizes)
     x = x[topk_argsort_revert_indices].reshape(-1, topk, hidden_size)
 
     x = x * topk_weights.unsqueeze_(dim=-1)
     x = x.sum(dim=-2)
     x = x.reshape(orig_shape)
     return x
-
-
-if __name__ == "__main__":
-    BATCH_SIZE = 1
-    SEQ_LEN = 1024
-    HIDDEN_SIZE = 1024
-    INTERMEDIATE_SIZE = 1536
-    NUM_EXPERTS = 8
-    TOPK = 2
-    DTYPE = torch.bfloat16
-    device = xm.xla_device()
-
-    x = torch.randn(BATCH_SIZE,
-                    SEQ_LEN,
-                    HIDDEN_SIZE,
-                    dtype=DTYPE,
-                    device=device)
-    gating_output = torch.randn(BATCH_SIZE,
-                                SEQ_LEN,
-                                NUM_EXPERTS,
-                                dtype=torch.float,
-                                device=device)
-    w1 = torch.randn(NUM_EXPERTS,
-                     HIDDEN_SIZE,
-                     INTERMEDIATE_SIZE * 2,
-                     dtype=DTYPE,
-                     device=device)
-    w2 = torch.randn(NUM_EXPERTS,
-                     INTERMEDIATE_SIZE,
-                     HIDDEN_SIZE,
-                     dtype=DTYPE,
-                     device=device)
-    output = fused_moe(
-        x,
-        w1,
-        w2,
-        gating_output,
-        TOPK,
-        renormalize=False,
-    )
-    output = output.cpu()
