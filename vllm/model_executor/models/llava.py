7804ec48ea54baa039a7e80066977f6b984c8bae
Roger Wang <ywang@roblox.com>
2024-07-14 22:03:53 -0700

    format

--- vllm/model_executor/models/llava.py
+++ vllm/model_executor/models/llava.py
@@ -339,9 +339,8 @@ class LlavaForConditionalGeneration(nn.Module, SupportsVision):
                     break
                 else:
                     use_default_weight_loading = True
-            if use_default_weight_loading:
-                if name in params_dict:
-                    param = params_dict[name]
-                    weight_loader = getattr(param, "weight_loader",
-                                            default_weight_loader)
-                    weight_loader(param, loaded_weight)
+            if use_default_weight_loading and name in params_dict:
+                param = params_dict[name]
+                weight_loader = getattr(param, "weight_loader",
+                                        default_weight_loader)
+                weight_loader(param, loaded_weight)
