9173c266e513d4e1a42f151a328497acb9c4dcc9
wushidonguc <dongws@me.com>
2024-07-16 23:28:49 +0000

    Take care of tie word embedding

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -257,7 +257,8 @@ class LlamaModel(nn.Module):
                       (lora_config.max_loras or 1)) if lora_config else 0
         self.vocab_size = config.vocab_size + lora_vocab
         self.org_vocab_size = config.vocab_size
-        if get_pp_group().is_first_rank:
+        if get_pp_group().is_first_rank or (config.tie_word_embeddings and
+                                            get_pp_group().is_last_rank):
             self.embed_tokens = VocabParallelEmbedding(
                 self.vocab_size,
                 config.hidden_size,
