74e9db6a638322c5d5e67441143a7bd032de8074
DarkLight1337 <tlleungac@connect.ust.hk>
2024-07-16 02:34:56 +0000

    Use consistent format

--- vllm/assets/base.py
+++ vllm/assets/base.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 
-from vllm.envs import VLLM_ASSETS_CACHE
+import vllm.envs as envs
 
 
 def get_cache_dir():
     """Get the path to the cache for storing downloaded assets."""
-    path = Path(VLLM_ASSETS_CACHE)
+    path = Path(envs.VLLM_ASSETS_CACHE)
     path.mkdir(parents=True, exist_ok=True)
 
     return path
