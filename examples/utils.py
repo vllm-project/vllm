b277180575d7d9c85708e2622cc6c32afbc0a383
Andrew Feldman <afeldman@neuralmagic.com>
2024-07-17 07:17:40 -0400

    formatting

--- examples/utils.py
+++ examples/utils.py
@@ -1,14 +1,15 @@
 '''Example code utils'''
 
-from vllm.utils import STR_BACKEND_ENV_VAR
-from typing import Generator
-
 import os
 from contextlib import contextmanager
+from typing import Generator
+
+from vllm.utils import STR_BACKEND_ENV_VAR
+
 
 @contextmanager
-def override_backend_env_var_context_manager(backend_name: str,
-                      ) -> Generator[None, None, None]:
+def override_backend_env_var_context_manager(
+    backend_name: str, ) -> Generator[None, None, None]:
     '''
     Override the environment variable indicating the vLLM backend temporarily,
     in a context where pytest monkeypatch is not available (i.e. *outside*
@@ -29,16 +30,18 @@ def override_backend_env_var_context_manager(backend_name: str,
 
     # Save the current state of the environment variable (if it exists)
     original_value = os.environ.get(key, None)
-    
+
     # Set the new value of the environment variable
     os.environ[key] = backend_name
-    
+
     # Yield control back to the enclosed code block
     try:
         yield
     finally:
         # Revert the environment variable to its original state
         if original_value is None:
-            os.environ.pop(key, None)  # Remove the variable if it wasn't originally set
+            os.environ.pop(
+                key, None)  # Remove the variable if it wasn't originally set
         else:
-            os.environ[key] = original_value  # Revert back to the original value
+            os.environ[
+                key] = original_value  # Revert back to the original value
