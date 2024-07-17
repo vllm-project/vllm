bc3b0ecd6d0ce5dbf08ccbc9da1266ace59a83b1
jeejeeli <pandaleefree@gmail.com>
2024-07-17 10:14:39 +0800

    test_gemma

--- tests/lora/test_gemma.py
+++ tests/lora/test_gemma.py
@@ -18,7 +18,7 @@ def do_sample(llm: vllm.LLM, lora_path: str, lora_id: int) -> List[str]:
         sampling_params,
         lora_request=LoRARequest(str(lora_id), lora_id, lora_path)
         if lora_id else None)
-    # Print the outputs.
+    # Print the outputs
     generated_texts: List[str] = []
     for output in outputs:
         prompt = output.prompt
