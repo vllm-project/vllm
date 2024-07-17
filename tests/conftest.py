f15eacf140810512335a7ac422b09788a1c1964e
Andrew Feldman <afeldman@neuralmagic.com>
2024-07-17 10:55:46 -0400

    wip

--- tests/conftest.py
+++ tests/conftest.py
@@ -433,6 +433,11 @@ class HfRunner:
                                                return_tensors="pt").input_ids
             decoder_input_ids = self.tokenizer(decoder_prompt,
                                                return_tensors="pt").input_ids
+            generation_config = GenerationConfig.from_model_config(self.model.config)
+            generation_config.do_sample = False
+            generation_config.top_k = None
+            generation_config.num_beams = 1
+
             output = self.model.generate(
                 self.wrap_device(encoder_input_ids),
                 decoder_input_ids=self.wrap_device(decoder_input_ids),
@@ -441,6 +446,7 @@ class HfRunner:
                 max_new_tokens=max_tokens,
                 output_hidden_states=True,
                 return_dict_in_generate=True,
+                generation_config=generation_config,
             )
 
             seq_logprobs: List[torch.Tensor] = []
