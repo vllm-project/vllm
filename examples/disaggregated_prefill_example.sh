3db988c713691052f0517d877f76d100a9c015f6
Kuntai Du <kuntai@uchicago.edu>
2024-07-16 23:49:59 -0700

    add example usage for disaggregated prefill

---
+++ examples/disaggregated_prefill_example.sh
@@ -0,0 +1,22 @@
+
+
+# prefilling instance
+VLLM_DISAGG_PREFILL_ROLE=prefill CUDA_VISIBLE_DEVICES=0,1,2,3 python3 \
+    -m vllm.entrypoints.openai.api_server \
+    --model neuralmagic/Meta-Llama-3-70B-Instruct-FP8 \
+    --port 8100 \
+    -tp 4 \
+    --disable-log-stats \
+    --disable-log-requests \
+    --enable-chunked-prefill &
+
+sleep 2
+
+VLLM_DISAGG_PREFILL_ROLE=decode CUDA_VISIBLE_DEVICES=4,5,6,7 python3 \
+    -m vllm.entrypoints.openai.api_server \
+    --model neuralmagic/Meta-Llama-3-70B-Instruct-FP8 \
+    --port 8200 \
+    -tp 4 \
+    --disable-log-stats \
+    --disable-log-requests \
+    --enable-chunked-prefill &
