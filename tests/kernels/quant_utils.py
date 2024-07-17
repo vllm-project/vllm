2859b62db31d23067270f091c3312bf3990c8951
Varun Sundar Rabindranath <varun@neuralmagic.com>
2024-07-17 13:34:41 +0000

    refactor quant utils

--- tests/kernels/quant_utils.py
+++ tests/kernels/quant_utils.py
@@ -1,5 +1,8 @@
 import torch
-from typing import Tuple
+from typing import Tuple, Union
+
+def as_float32_tensor(x: [float, torch.tensor]) -> torch.tensor:
+    return torch.as_tensor(x, dtype=torch.float32, device='cuda')
 
 def ref_dynamic_per_token_quant(x: torch.tensor,
                                 quant_dtype: torch.dtype) \
@@ -8,20 +11,19 @@ def ref_dynamic_per_token_quant(x: torch.tensor,
     assert quant_dtype in [torch.int8, torch.float8_e4m3fn]
     qtype_traits = torch.iinfo(quant_dtype) if quant_dtype == torch.int8 \
             else torch.finfo(quant_dtype)
+    qtype_max = as_float32_tensor(qtype_traits.max)
 
     # For fp8, inorder to match the cuda kernel output, we have to do the same operations
     # to prevent rounding errors.
 
     # Compute scales
     x_token_max, _ = x.abs().max(dim=-1)
-    x_token_max = x_token_max.to(dtype=torch.float32)
-    scales = x_token_max / torch.as_tensor([qtype_traits.max], dtype=torch.float32, device='cuda')
-    scales = scales[:, None]
+    x_token_max = as_float32_tensor(x_token_max)
+    scales = (x_token_max / qtype_max)[:, None]
 
     # Quant
-    iscales = torch.as_tensor([qtype_traits.max], dtype=torch.float32, device='cuda') / x_token_max
-    iscales = iscales[:, None]
-    torch_out = (x.to(dtype=torch.float32) * iscales).to(device="cuda", dtype=torch.float32)
+    iscales = (qtype_max / x_token_max)[:, None]
+    torch_out = as_float32_tensor(x) * iscales
     torch_out = torch_out.round() if quant_dtype == torch.int8 else torch_out
     torch_out = torch_out.clamp(qtype_traits.min, qtype_traits.max).to(quant_dtype)
 
@@ -34,14 +36,14 @@ def ref_dynamic_per_tensor_fp8_quant(x: torch.tensor) \
                     -> Tuple[torch.tensor, torch.tensor]:
 
     fp8_traits = torch.finfo(torch.float8_e4m3fn)
-    fp8_max = torch.as_tensor([fp8_traits.max], dtype=torch.float32, device='cuda') 
-    one = torch.as_tensor([1.0], dtype=torch.float32, device='cuda')
+    fp8_max = as_float32_tensor(fp8_traits.max)
+    one = as_float32_tensor(1.0)
 
     # For fp8, inorder to match the cuda kernel output, we have to do the same operations
     # to prevent rounding errors.
 
-    x_max = x.abs().max().to(dtype=torch.float32) 
+    x_max = as_float32_tensor(x.abs().max())
     ref_scale = x_max / fp8_max 
     ref_iscale =  one / ref_scale 
-    ref_out = (x.to(dtype=torch.float32) * ref_iscale).clamp(fp8_traits.min, fp8_traits.max).to(dtype=torch.float8_e4m3fn)
+    ref_out = (as_float32_tensor(x) * ref_iscale).clamp(fp8_traits.min, fp8_traits.max).to(dtype=torch.float8_e4m3fn)
     return ref_out, ref_scale
