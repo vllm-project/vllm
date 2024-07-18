7fcac7861981be15c9ad00d900bd7afe117f3d20
youkaichao <youkaichao@126.com>
2024-07-18 13:16:16 -0700

    add quantization tests

--- tests/basic_correctness/test_cpu_offload.py
+++ tests/basic_correctness/test_cpu_offload.py
@@ -4,3 +4,5 @@ from ..utils import compare_two_settings
 def test_cpu_offload():
     compare_two_settings("meta-llama/Llama-2-7b-hf", [],
                          ["--cpu-offload-gb", "4"])
+    compare_two_settings("nm-testing/llama7b-one-shot-2_4-w4a16-marlin24-t",
+                         [], ["--cpu-offload-gb", "1"])
