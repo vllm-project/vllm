e6d1aeb470cbc10c056b1aeba37aa7cd832f284a
刘衍聪_Yancong <sp.yancong.liu@enflame.cn>
2024-07-18 10:26:06 +0800

    update example

--- examples/deepseek_vl_example.py
+++ examples/deepseek_vl_example.py
@@ -1,41 +1,43 @@
-import os
-import subprocess
-
-import torch
-from PIL import Image
-
-# The assets are located at `s3://air-example-data-2/vllm_opensource_llava/`.
-# You can use `.buildkite/download-images.sh` to download them
+from vllm.assets.image import ImageAsset
 from vllm import LLM, SamplingParams
 
 sample_params = SamplingParams(temperature=0, max_tokens=1024)
 model = "deepseek-ai/deepseek-vl-7b-chat"
 model = "deepseek-ai/deepseek-vl-1.3b-chat"
-prompt = "You are a helpful language and vision assistant." \
-    "You are able to understand the visual content that the user provides," \
-    "and assist the user with a variety of tasks using natural language.\n" \
-    "User: <image_placeholder> Describe the content of this image.\nAssistant:"
+prompt_one = "You are a helpful language and vision assistant." \
+            "You are able to understand the visual content that the user provides," \
+            "and assist the user with a variety of tasks using natural language.\n" \
+            "User: <image_placeholder> Describe the content of this image.\nAssistant:"\
+
+
+prompt_two = "You are a helpful language and vision assistant. You are able to " \
+            "understand the visual content that the user provides, and assist the "  \
+            "user with a variety of tasks using natural language.\n User: " \
+            "<image_placeholder>What is the season?\nAssistant:"
 
 
 def run_deepseek_vl():
-    llm = LLM(model=model,
-              max_model_len=3072,
-              enforce_eager=True,
-              dtype=torch.bfloat16)
-
-    image = Image.open("images/stop_sign.jpg")
-
+    llm = LLM(model=model)
+    stop_sign_image = ImageAsset("stop_sign").pil_image
+    cherry_blossom_image = ImageAsset("cherry_blossom").pil_image
     outputs = llm.generate(
-        {
-            "prompt": prompt,
+        [{
+            "prompt": prompt_one,
             "multi_modal_data": {
-                "image": image
+                "image": stop_sign_image
             },
-        },
-        sampling_params=sample_params)
+        }, {
+            "prompt": prompt_two,
+            "multi_modal_data": {
+                "image": cherry_blossom_image
+            }
+        }],
+        sampling_params=sample_params,
+    )
 
     for o in outputs:
         generated_text = o.outputs[0].text
+        print("------------------")
         print(generated_text)
 
 
@@ -44,20 +46,4 @@ def main():
 
 
 if __name__ == "__main__":
-    # Download from s3
-    s3_bucket_path = "s3://air-example-data-2/vllm_opensource_llava/"
-    local_directory = "images"
-
-    # Make sure the local directory exists or create it
-    os.makedirs(local_directory, exist_ok=True)
-
-    # Use AWS CLI to sync the directory, assume anonymous access
-    subprocess.check_call([
-        "aws",
-        "s3",
-        "sync",
-        s3_bucket_path,
-        local_directory,
-        "--no-sign-request",
-    ])
     main()
