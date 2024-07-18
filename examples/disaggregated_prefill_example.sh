04d319a08ae484934813ee525c8d73230f49d99c
Kuntai Du <kuntai@uchicago.edu>
2024-07-17 23:09:34 -0700

    continue disaggregated prefill debugging

--- examples/disaggregated_prefill_example.sh
+++ examples/disaggregated_prefill_example.sh
@@ -2,34 +2,24 @@
 export VLLM_LOGGING_LEVEL=DEBUG
 export VLLM_PORT=12345
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python3 \
+# prefilling instance
+VLLM_DISAGG_PREFILL_ROLE=prefill CUDA_VISIBLE_DEVICES=0,1,2,3 python3 \
     -m vllm.entrypoints.openai.api_server \
     --model neuralmagic/Meta-Llama-3-70B-Instruct-FP8 \
     --port 8100 \
     -tp 4 \
     --disable-log-stats \
     --disable-log-requests \
-    --enable-chunked-prefill 
+    --enable-chunked-prefill &
 
-# # prefilling instance
-# VLLM_DISAGG_PREFILL_ROLE=prefill CUDA_VISIBLE_DEVICES=0,1,2,3 python3 \
-#     -m vllm.entrypoints.openai.api_server \
-#     --model neuralmagic/Meta-Llama-3-70B-Instruct-FP8 \
-#     --port 8100 \
-#     -tp 4 \
-#     --disable-log-stats \
-#     --disable-log-requests \
-#     --enable-chunked-prefill &
-
-# sleep 10
-
-# VLLM_DISAGG_PREFILL_ROLE=decode CUDA_VISIBLE_DEVICES=4,5,6,7 python3 \
-#     -m vllm.entrypoints.openai.api_server \
-#     --model neuralmagic/Meta-Llama-3-70B-Instruct-FP8 \
-#     --port 8200 \
-#     -tp 4 \
-#     --disable-log-stats \
-#     --disable-log-requests \
-#     --enable-chunked-prefill
+# decoding instance
+VLLM_DISAGG_PREFILL_ROLE=decode CUDA_VISIBLE_DEVICES=4,5,6,7 python3 \
+    -m vllm.entrypoints.openai.api_server \
+    --model neuralmagic/Meta-Llama-3-70B-Instruct-FP8 \
+    --port 8200 \
+    -tp 4 \
+    --disable-log-stats \
+    --disable-log-requests \
+    --enable-chunked-prefill
 
 
