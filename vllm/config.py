e82954709b612189e8fc76d3ce32476f41ad07aa
simon-mo <simon.mo@hey.com>
2024-07-18 22:24:26 +0000

    comments

--- vllm/config.py
+++ vllm/config.py
@@ -150,7 +150,8 @@ class ModelConfig:
         self.hf_text_config = get_hf_text_config(self.hf_config)
         self.dtype = _get_and_verify_dtype(self.hf_text_config, dtype)
 
-        if getattr(self.hf_config, "max_position_embeddings", 0) == 131072:
+        if (getattr(self.hf_config, "max_position_embeddings", 0) == 131072
+                and getattr(self.hf_config, "role_scaling", None) is None):
             # Note(simon): this is a special case for a model that doesn't
             # supply rope_scaling. We should remove this once the model is
             # updated.
