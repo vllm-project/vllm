8ac9266a711ca9e0ceff8abcc171b9896232ee70
Kuntai Du <kuntai@uchicago.edu>
2024-07-19 01:08:49 -0700

    init DISAGG first

--- vllm/distributed/parallel_state.py
+++ vllm/distributed/parallel_state.py
@@ -996,6 +996,24 @@ def initialize_model_parallel(
             group_ranks, 
             world_size
         )
+        
+    if envs.VLLM_DISAGG_PREFILL_ROLE is not None:
+        assert envs.VLLM_DISAGG_PREFILL_ROLE in ["prefill", "decode"], (
+            "VLLM_DISAGG_PREFILL_ROLE should be either prefill or decode")
+        logger.debug("Disaggregated prefill enabled, create _DISAGG group")
+        group_ranks = []
+        for i in range(world_size):
+            # prefill local rank: i
+            # decode global rank: i + world_size
+            group_ranks.append([i, i + world_size])
+        logger.debug("Distributed group is %s", str(group_ranks))
+        _DISAGG = init_model_parallel_group(
+            group_ranks,
+            int(envs.VLLM_DISAGG_PREFILL_ROLE == "decode"),
+            backend,
+            use_custom_allreduce=False)
+        
+    time.sleep(1000)
 
     # message queue broadcaster is only used in tensor model parallel group
     _TP = init_model_parallel_group(group_ranks,
@@ -1025,22 +1043,6 @@ def initialize_model_parallel(
                                     get_world_group().local_rank,
                                     backend,
                                     use_custom_allreduce=False)
-    
-    if envs.VLLM_DISAGG_PREFILL_ROLE is not None:
-        assert envs.VLLM_DISAGG_PREFILL_ROLE in ["prefill", "decode"], (
-            "VLLM_DISAGG_PREFILL_ROLE should be either prefill or decode")
-        logger.debug("Disaggregated prefill enabled, create _DISAGG group")
-        group_ranks = []
-        for i in range(world_size):
-            # prefill local rank: i
-            # decode global rank: i + world_size
-            group_ranks.append([i, i + world_size])
-        logger.debug("Distributed group is %s", str(group_ranks))
-        _DISAGG = init_model_parallel_group(
-            group_ranks,
-            int(envs.VLLM_DISAGG_PREFILL_ROLE == "decode"),
-            backend,
-            use_custom_allreduce=False)
 
 
 def ensure_model_parallel_initialized(
