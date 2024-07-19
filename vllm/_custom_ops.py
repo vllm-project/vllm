a6c0aeb66337183ba1974a73f60d1bea016ed048
Alexander Matveev <alexm@neuralmagic.com>
2024-07-19 17:44:58 +0000

    sync

--- vllm/_custom_ops.py
+++ vllm/_custom_ops.py
@@ -280,11 +280,12 @@ def gptq_marlin_gemm(a: torch.Tensor, b_q_weight: torch.Tensor,
                      b_scales: torch.Tensor, b_zeros: torch.Tensor,
                      g_idx: torch.Tensor, perm: torch.Tensor,
                      workspace: torch.Tensor, num_bits: int, size_m: int,
-                     size_n: int, size_k: int,
-                     is_k_full: bool, has_zp: bool) -> torch.Tensor:
+                     size_n: int, size_k: int, is_k_full: bool,
+                     has_zp: bool) -> torch.Tensor:
     return torch.ops._C.gptq_marlin_gemm(a, b_q_weight, b_scales, b_zeros,
                                          g_idx, perm, workspace, num_bits,
-                                         size_m, size_n, size_k, is_k_full, has_zp)
+                                         size_m, size_n, size_k, is_k_full,
+                                         has_zp)
 
 
 # fp8 marlin
