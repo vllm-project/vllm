3a2bcfaf4b9e646dadfd376270afbf0d756fdcf4
ezliu <evanliu@stanford.edu>
2024-07-21 16:13:15 -0700

    Add arg to argparser

--- vllm/entrypoints/openai/cli_args.py
+++ vllm/entrypoints/openai/cli_args.py
@@ -128,6 +128,12 @@ def make_arg_parser(parser: FlexibleArgumentParser) -> FlexibleArgumentParser:
         "using @app.middleware('http'). "
         "If a class is provided, vLLM will add it to the server "
         "using app.add_middleware(). ")
+    parser.add_argument(
+        "--return-tokens-as-token-ids",
+        action="store_true",
+        help="When --max-logprobs is specified, represents single tokens as"
+             "strings of the form 'token_id:{token_id}' so that tokens that"
+             "are not JSON-encodable can be identified.")
 
     parser = AsyncEngineArgs.add_cli_args(parser)
     return parser
