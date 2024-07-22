832930eb1131b52d57daf55a2351f4b3999f9757
kevin <kevin@anyscale.com>
2024-07-22 19:01:54 +0000

    p
    Signed-off-by: kevin <kevin@anyscale.com>

--- example.py
+++
@@ -1,5 +0,0 @@
-def add(a, b):
-    return a + b
-
-def subtract(a, b):
-    return a - b
--- test_example.py
+++
@@ -1,7 +0,0 @@
-from example import add, subtract
-
-def test_add():
-    assert add(2, 3) == 5
-
-def test_subtract():
-    assert subtract(5, 2) == 3
