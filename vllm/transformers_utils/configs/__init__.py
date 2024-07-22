071f29e36295a4cc3fc129455a63077f09edb93f
Roger Wang <136131678+ywang96@users.noreply.github.com>
2024-07-21 17:37:51 -0700

    [Model] Initial Support for Chameleon (#5770)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/transformers_utils/configs/__init__.py
+++ vllm/transformers_utils/configs/__init__.py
@@ -1,3 +1,4 @@
+from vllm.transformers_utils.configs.chameleon import ChameleonConfig
 from vllm.transformers_utils.configs.chatglm import ChatGLMConfig
 from vllm.transformers_utils.configs.dbrx import DbrxConfig
 # RWConfig is for the original tiiuae/falcon-40b(-instruct) and
@@ -10,6 +11,7 @@ from vllm.transformers_utils.configs.mlp_speculator import MLPSpeculatorConfig
 from vllm.transformers_utils.configs.mpt import MPTConfig
 
 __all__ = [
+    "ChameleonConfig",
     "ChatGLMConfig",
     "DbrxConfig",
     "MPTConfig",
