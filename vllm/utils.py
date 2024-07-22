009ceccedff162e355350af157a88c8b600fc6aa
Joe G <joseph.granados@h2o.ai>
2024-07-22 07:20:00 +0000

    Test fix for fp8 failures
    fp8 kv cache values are encoded as uint8. The element size of uint8 is 1.
    16 divided by any int over 1 is going to be 8 or less which is
    compatible with head size 120. But that doesn't happen with fp8, which
    leads to test failures for head size 120.

--- vllm/utils.py
+++ vllm/utils.py
@@ -520,7 +520,7 @@ def create_kv_caches_with_random(
     torch_dtype = get_kv_cache_torch_dtype(cache_dtype, model_dtype)
 
     scale = head_size**-0.5
-    x = 16 // torch.tensor([], dtype=torch_dtype).element_size()
+    x = 8 // torch.tensor([], dtype=torch_dtype).element_size()
     key_cache_shape = (num_blocks, num_heads, head_size // x, block_size, x)
     key_caches: List[torch.Tensor] = []
     for _ in range(num_layers):
