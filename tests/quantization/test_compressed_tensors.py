86514c2201fff408996e7e7cf5eaa4d591197276
Michael Goin <michael@neuralmagic.com>
2024-07-23 00:11:50 -0400

    [Misc] Support FP8 kv cache scales from compressed-tensors (#6528)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- tests/quantization/test_compressed_tensors.py
+++ tests/quantization/test_compressed_tensors.py
@@ -150,3 +150,10 @@ def test_compressed_tensors_fp8(vllm_runner):
 
         output = llm.generate_greedy("Hello my name is", max_tokens=20)
         assert output
+
+
+def test_compressed_tensors_kv_cache(vllm_runner):
+    model_path = "nm-testing/TinyLlama-1.1B-compressed-tensors-kv-cache-scheme"
+    with vllm_runner(model_path, kv_cache_dtype="fp8") as llm:
+        output = llm.generate_greedy("Hello world!", max_tokens=20)
+        assert output
