2177737e44f0d855bc8c9d0d4e808463ad28122e
Kuntai Du <kuntai@uchicago.edu>
2024-07-22 20:44:16 -0700

    turn on logging

--- examples/disaggregated_prefill_example.sh
+++ examples/disaggregated_prefill_example.sh
@@ -11,8 +11,6 @@ VLLM_DISAGG_PREFILL_ROLE=prefill CUDA_VISIBLE_DEVICES=0,1,2,3 python3 \
     --model neuralmagic/Meta-Llama-3-70B-Instruct-FP8 \
     --port 8100 \
     -tp 4 \
-    --disable-log-stats \
-    --disable-log-requests \
     --enable-prefix-caching &
 
 # decoding instance
@@ -21,8 +19,6 @@ VLLM_DISAGG_PREFILL_ROLE=decode CUDA_VISIBLE_DEVICES=4,5,6,7 python3 \
     --model neuralmagic/Meta-Llama-3-70B-Instruct-FP8 \
     --port 8200 \
     -tp 4 \
-    --disable-log-stats \
-    --disable-log-requests \
     --enable-prefix-caching &
 
 
