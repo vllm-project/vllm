11850d57a739c94b1e01a3b93baff1b2e27180a1
Kuntai Du <kuntai@uchicago.edu>
2024-07-23 22:22:21 -0700

    reduce gpu memory usage

--- examples/disagg_prefill/disagg_prefill_example.sh
+++ examples/disagg_prefill/disagg_prefill_example.sh
@@ -23,7 +23,8 @@ VLLM_DISAGG_PREFILL_ROLE=prefill CUDA_VISIBLE_DEVICES=0,1,2,3 python3 \
     --model neuralmagic/Meta-Llama-3-70B-Instruct-FP8 \
     --port 8100 \
     -tp 4 \
-    --enable-prefix-caching &
+    --enable-prefix-caching \
+    --gpu-memory-utilization 0.8 &
 
 # decoding instance
 VLLM_DISAGG_PREFILL_ROLE=decode CUDA_VISIBLE_DEVICES=4,5,6,7 python3 \
@@ -31,7 +32,7 @@ VLLM_DISAGG_PREFILL_ROLE=decode CUDA_VISIBLE_DEVICES=4,5,6,7 python3 \
     --model neuralmagic/Meta-Llama-3-70B-Instruct-FP8 \
     --port 8200 \
     -tp 4 \
-    --enable-prefix-caching &
+    --enable-prefix-caching 0.8 &
 
 
 wait_for_server 8100
