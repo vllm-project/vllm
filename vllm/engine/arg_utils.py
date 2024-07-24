067a24329e1af357a48d82a57325b821a15f437b
Michal Szutenberg <37601244+szutenberg@users.noreply.github.com>
2024-07-24 07:38:53 +0200

    Allow block size 256 (#121)

--- vllm/engine/arg_utils.py
+++ vllm/engine/arg_utils.py
@@ -257,7 +257,7 @@ class EngineArgs:
         parser.add_argument('--block-size',
                             type=int,
                             default=EngineArgs.block_size,
-                            choices=[8, 16, 32, 64, 128],
+                            choices=[8, 16, 32, 64, 128, 256],
                             help='Token block size for contiguous chunks of '
                             'tokens.')
 
