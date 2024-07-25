d615870a58deee0feb98254ce741474f78e070ef
刘衍聪_Yancong <sp.yancong.liu@enflame.cn>
2024-07-25 14:12:45 +0800

    fix conflict

--- vllm/transformers_utils/config.py
+++ vllm/transformers_utils/config.py
@@ -5,11 +5,11 @@ from transformers import GenerationConfig, PretrainedConfig
 
 from vllm.envs import VLLM_USE_MODELSCOPE
 from vllm.logger import init_logger
-from vllm.transformers_utils.configs import (ChameleonConfig, ChatGLMConfig,
+from vllm.transformers_utils.configs import (ChatGLMConfig, DbrxConfig,
                                              DeepSeekMultiModalityConfig,
-                                             DbrxConfig, JAISConfig,
-                                             MedusaConfig, MLPSpeculatorConfig,
-                                             MPTConfig, RWConfig)
+                                             JAISConfig, MedusaConfig,
+                                             MLPSpeculatorConfig, MPTConfig,
+                                             RWConfig)
 
 if VLLM_USE_MODELSCOPE:
     from modelscope import AutoConfig
@@ -19,7 +19,6 @@ else:
 logger = init_logger(__name__)
 
 _CONFIG_REGISTRY: Dict[str, Type[PretrainedConfig]] = {
-    "chameleon": ChameleonConfig,
     "chatglm": ChatGLMConfig,
     "dbrx": DbrxConfig,
     "mpt": MPTConfig,
