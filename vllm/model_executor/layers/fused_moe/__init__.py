b9bb0b4a37369ce316638ae2a7a02e4adf7ac721
Thomas Parnell <tpa@zurich.ibm.com>
2024-07-26 13:59:58 -0400

    Improved handling of ruff in __init__.py
    Signed-off-by: Thomas Parnell <tpa@zurich.ibm.com>

--- vllm/model_executor/layers/fused_moe/__init__.py
+++ vllm/model_executor/layers/fused_moe/__init__.py
@@ -2,21 +2,21 @@ from vllm.model_executor.layers.fused_moe.layer import (FusedMoE,
                                                         FusedMoEMethodBase)
 from vllm.triton_utils import HAS_TRITON
 
+__all__ = [
+    "FusedMoE",
+    "FusedMoEMethodBase",
+]
+
 if HAS_TRITON:
+
     from vllm.model_executor.layers.fused_moe.fused_moe import (
         fused_experts, fused_moe, fused_topk, get_config_file_name,
         grouped_topk)
 
-__all__ = [
-    "FusedMoE",
-    "FusedMoEMethodBase",
-    "fused_moe",
-    "fused_topk",
-    "fused_experts",
-    "get_config_file_name",
-    "grouped_topk",
-]
-
-if not HAS_TRITON:
-    # need to do it like this other ruff complains
-    __all__ = __all__[:2]
+    __all__ += [
+        "fused_moe",
+        "fused_topk",
+        "fused_experts",
+        "get_config_file_name",
+        "grouped_topk",
+    ]
