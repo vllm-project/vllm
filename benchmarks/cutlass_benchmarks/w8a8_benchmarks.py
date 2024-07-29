03e3cbda7306f66210fa38cd70fac790b17bb83a
Varun Sundar Rabindranath <varunsundar08@gmail.com>
2024-07-29 11:42:35 -0400

    [Kernel] Tuned FP8 Kernels for Ada Lovelace (#6677)
    Co-authored-by: Varun Sundar Rabindranath <varun@neuralmagic.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- benchmarks/cutlass_benchmarks/w8a8_benchmarks.py
+++ benchmarks/cutlass_benchmarks/w8a8_benchmarks.py
@@ -13,7 +13,7 @@ from weight_shapes import WEIGHT_SHAPES
 from vllm import _custom_ops as ops
 from vllm.utils import FlexibleArgumentParser
 
-DEFAULT_MODELS = list(WEIGHT_SHAPES.keys())[1:]
+DEFAULT_MODELS = list(WEIGHT_SHAPES.keys())
 DEFAULT_BATCH_SIZES = [1, 16, 32, 64, 128, 256, 512]
 DEFAULT_TP_SIZES = [1]
 
