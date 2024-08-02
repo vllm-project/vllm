32e030b956b4db61dd6ed8446d56ed38e2bd24b7
Maxime Fournioux <55544262+mfournioux@users.noreply.github.com>
2024-08-02 17:23:49 +0200

    update some typo

--- tests/entrypoints/openai/test_basic.py
+++ tests/entrypoints/openai/test_basic.py
@@ -64,7 +64,9 @@ async def test_log_metrics(client: openai.AsyncOpenAI):
 def test_get_liveness(client: openai.AsyncOpenAI):
     """Test the technical route /liveness"""
     base_url = str(client.base_url)[:-3].strip("/")
+
     response = requests.get(base_url + "/liveness")
+
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"alive": "ok"}
 
@@ -72,7 +74,9 @@ def test_get_liveness(client: openai.AsyncOpenAI):
 def test_get_readiness_ko(client: openai.AsyncOpenAI):
     """Test the technical route /readiness when the model is not loaded"""
     base_url = str(client.base_url)[:-3].strip("/")
+
     response = requests.get(base_url + "/readiness")
+
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"ready": "ko"}
 
@@ -80,6 +84,8 @@ def test_get_readiness_ko(client: openai.AsyncOpenAI):
 def test_get_readiness_ok(client: openai.AsyncOpenAI):
     """Test the technical route /readiness when the model is fully loaded"""
     base_url = str(client.base_url)[:-3].strip("/")
+
     response = requests.get(base_url + "/readiness")
+    
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"ready": "ok"}
