41542a256a841faa99240c812a1985436b149e9b
caishangming.csm <caishangming.csm@alibaba-inc.com>
2024-08-02 14:42:02 +0800

    fix

--- vllm/spec_decode/spec_decode_params.py
+++ vllm/spec_decode/spec_decode_params.py
@@ -1,24 +1,13 @@
 """Parameters for speculative deocding."""
-from typing import Any, Optional
-
 import copy
-import vllm.envs as envs
-from vllm.logger import init_logger
 
-logger = init_logger(__name__)
 
 class SpecDecodeParams:
     """
     Parameters for Speculative Decoding choices and future features.
 
-    Note that this class should be used internally. For online
-    serving, it is recommended to not allow users to use this class but
-    instead provide another layer of abstraction to prevent users from
-    accessing unauthorized Speculative Decoding proposers.
-
     Args:
-        proposer_name: Name of proposer to used for SpecDecodeWorker.
-        proposer_id: The id of proposer to used for SpecDecodeWorker.
+        proposer_name: Name of proposer to be used for SpecDecodeWorker.
     """
 
     def __init__(
@@ -27,11 +16,11 @@ class SpecDecodeParams:
     ) -> None:
         self.proposer_name = proposer_name
         self._verify_args()
-        
-    
+
     def _verify_args(self) -> None:
-        if self.proposer_name is None:
-            raise ValueError(f"proposer_name must be provided.")
+        if not isinstance(self.proposer_name, str) or not self.proposer_name:
+            raise ValueError(f"proposer_name (a non-empty string) must be "
+                             f"provided.")
     def clone(self) -> "SpecDecodeParams":
         return copy.deepcopy(self)
     
@@ -39,8 +28,11 @@ class SpecDecodeParams:
         return self.proposer_name
     
     def set_proposer(self, proposer_name: str) -> None:
+        if not isinstance(proposer_name, str) or not proposer_name:
+            raise ValueError(f"proposer_name (a non-empty string) must be "
+                             f"provided.")
         self.proposer_name = proposer_name
 
     def __repr__(self) -> str:
-        return (
-            f"SpecDecodeParams(proposer_name={self.proposer_name})")
+        return (f"SpecDecodeParams(proposer_name={self.proposer_name})")
+
