a92265b6d3d2e510e0cdc2894648b3bbbce28393
Michael Goin <michael@neuralmagic.com>
2024-08-02 16:51:22 -0400

    [CI/Build] Add support for Python 3.12 (#7035)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- setup.py
+++ setup.py
@@ -465,6 +465,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: Apache Software License",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
