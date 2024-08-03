34a279365eb3ecc4383fc497f2d8bb171b618678
youkaichao <youkaichao@126.com>
2024-08-02 19:56:45 -0700

    merge test

--- tests/distributed/test_basic_distributed_correctness.py
+++ tests/distributed/test_basic_distributed_correctness.py
@@ -6,10 +6,7 @@ Run:
 ```sh
 cd $VLLM_PATH/tests
 
-TEST_DIST_MODEL=facebook/opt-125m pytest \
-    distributed/test_basic_distributed_correctness.py
-TEST_DIST_MODEL=meta-llama/Llama-2-7b-hf \
-    distributed/test_basic_distributed_correctness.py
+pytest distributed/test_basic_distributed_correctness.py
 ```
 """
 import os
@@ -21,15 +18,21 @@ from vllm.utils import cuda_device_count_stateless
 from ..models.utils import check_outputs_equal
 from ..utils import fork_new_process_for_each_test
 
+TARGET_TEST_SUITE = os.environ.get("TARGET_TEST_SUITE", "L4")
+
 
 @pytest.mark.skipif(cuda_device_count_stateless() < 2,
                     reason="Need at least 2 GPUs to run the test.")
 @pytest.mark.parametrize(
-    "model, distributed_executor_backend, attention_backend", [
-        ("facebook/opt-125m", "ray", ""),
-        ("facebook/opt-125m", "mp", ""),
-        ("facebook/opt-125m", "mp", "FLASHINFER"),
-        ("meta-llama/Meta-Llama-3-8B", "ray", "FLASHINFER"),
+    "model, distributed_executor_backend, attention_backend, test_suit", [
+        ("facebook/opt-125m", "ray", "", "L4"),
+        ("facebook/opt-125m", "mp", "", "L4"),
+        ("meta-llama/Llama-2-7b-hf", "ray", "", "L4"),
+        ("meta-llama/Llama-2-7b-hf", "mp", "", "L4"),
+        ("facebook/opt-125m", "ray", "", "A100"),
+        ("facebook/opt-125m", "mp", "", "A100"),
+        ("facebook/opt-125m", "mp", "FLASHINFER", "A100"),
+        ("meta-llama/Meta-Llama-3-8B", "ray", "FLASHINFER", "A100"),
     ])
 @fork_new_process_for_each_test
 def test_models(
@@ -39,7 +42,17 @@ def test_models(
     model: str,
     distributed_executor_backend: str,
     attention_backend: str,
+    test_suite: str,
 ) -> None:
+
+    if test_suite != TARGET_TEST_SUITE:
+        pytest.skip(f"Skip test for {test_suite}")
+
+    if model == "meta-llama/Llama-2-7b-hf" and distributed_executor_backend == "ray" and attention_backend == "" and test_suite == "L4":  # noqa
+        # test ray adag
+        os.environ['VLLM_USE_RAY_SPMD_WORKER'] = "1"
+        os.environ['VLLM_USE_RAY_COMPILED_DAG'] = "1"
+
     if attention_backend:
         os.environ["VLLM_ATTENTION_BACKEND"] = attention_backend
 
