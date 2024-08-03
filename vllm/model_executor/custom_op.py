a734ebc93e4b76a927fea4ecbd17d49cee05c4e2
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2024-08-03 04:06:00 -0700

    Add guard

--- vllm/model_executor/custom_op.py
+++ vllm/model_executor/custom_op.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+import vllm.envs as envs
 from vllm.utils import is_cpu, is_hip, is_tpu, is_xpu
 
 
@@ -16,18 +17,32 @@ class CustomOp(nn.Module):
 
     @staticmethod
     def forward_static(*args, **kwargs):
+        """PyTorch-native implementation of the forward method.
+
+        This method is optional. If implemented, it can be used with
+        `torch.compile` to generate the custom kernel for the op.
+        This function is necessary because any method that uses `self`
+        triggers re-compilation in `torch.compile`.
+        """
         raise NotImplementedError
 
     def forward_native(self, *args, **kwargs):
         """PyTorch-native implementation of the forward method.
 
-        This method is optional. If implemented, it can be used with
-        `torch.compile`.
+        This method is expected to invoke the forward_static method
+        with the input arguments and the attributes of `self`.
+        By default, we assume that forward_static does not need any
+        attributes of `self`.
         """
         return self.forward_static(*args, **kwargs)
 
     def forward_compile(self, *args, **kwargs):
-        if not self._is_compiled:
+        """Runs a torch-compiled version of forward_native.
+
+        This method hooks into the forward_native method and compiles
+        `forward_static` using `torch.compile` if it has not been compiled yet.
+        """
+        if not self._is_compiled and not envs.VLLM_TEST_DYNAMO_GRAPH_CAPTURE:
             self.forward_static = torch.compile(  # type: ignore
                 self.forward_static)
             self._is_compiled = True
@@ -63,7 +78,7 @@ class CustomOp(nn.Module):
         By default, we use torch.compile to optimize the op. However, we can
         override this method to use a custom CPU implementation if needed.
         """
-        if not self._is_compiled:
+        if not self._is_compiled and not envs.VLLM_TEST_DYNAMO_GRAPH_CAPTURE:
             self.forward_static = torch.compile(  # type: ignore
                 self.forward_static,
                 options={
