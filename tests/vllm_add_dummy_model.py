d07592918d317572c3c8c9d9aa2f6ccd18825e6f
youkaichao <youkaichao@126.com>
2024-08-04 15:06:53 -0700

    add plugins

---
+++ tests/vllm_add_dummy_model.py
@@ -0,0 +1,19 @@
+import torch
+
+from vllm import ModelRegistry
+from vllm.model_executor.models.opt import OPTForCausalLM
+from vllm.model_executor.sampling_metadata import SamplingMetadata
+
+
+class MyOPTForCausalLM(OPTForCausalLM):
+
+    def compute_logits(self, hidden_states: torch.Tensor,
+                       sampling_metadata: SamplingMetadata) -> torch.Tensor:
+        # this dummy model always predicts the first token
+        logits = super().compute_logits(hidden_states, sampling_metadata)
+        logits.zero_()
+        logits[:, 0] += 1.0
+        return logits
+
+# register our dummy model
+ModelRegistry.register_model("OPTForCausalLM", MyOPTForCausalLM)
