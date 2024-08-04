1ecec38b06da8138781633c94b1611ff5d36c308
Felix Zhu <felixzhu555@gmail.com>
2024-08-04 01:12:15 +0000

    small

--- temp/setup.sh
+++ temp/setup.sh
@@ -3,4 +3,6 @@
 read -p "Paste HF token: " token
 export HF_TOKEN="$token"
 export MAX_JOBS=8
+git config --global user.email "felixzhu555@gmail.com"
+git config --global user.name "Felix Zhu"
 pip install -e .
