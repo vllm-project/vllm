71456e7804b40158e5d7b3612fbd1d7aff5ab4e2
Andrew Feldman <afeldman@neuralmagic.com>
2024-08-06 03:21:27 -0400

    wip

--- examples/offline_inference_encoder_decoder.py
+++ examples/offline_inference_encoder_decoder.py
@@ -27,7 +27,7 @@ tokenizer = llm.llm_engine.get_tokenizer_group()
 text_prompt_raw = "Hello, my name is"
 text_prompt = TextPrompt(prompt="The president of the United States is")
 tokens_prompt = TokensPrompt(
-    prompt_token_ids=tokenizer.encode(prompt="The capital of France is", ))
+    prompt_token_ids=tokenizer.encode(prompt="The capital of France is"))
 # - Pass a single prompt to encoder/decoder model
 #   (implicitly encoder input prompt);
 #   decoder input prompt is assumed to be None
