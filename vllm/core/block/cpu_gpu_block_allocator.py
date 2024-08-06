b00fac530adf09f6459772cda668565fb1e48064
Karol Damaszke <kdamaszke@habana.ai>
2024-08-06 10:09:09 +0200

    Allocate blocks from id=1 (#155)

--- vllm/core/block/cpu_gpu_block_allocator.py
+++ vllm/core/block/cpu_gpu_block_allocator.py
@@ -52,7 +52,8 @@ class CpuGpuBlockAllocator(DeviceAwareBlockAllocator):
             - The block IDs are assigned contiguously, with GPU block IDs coming
                 before CPU block IDs.
         """
-        block_ids = list(range(num_gpu_blocks + num_cpu_blocks))
+        # Block ids cannot be equal to 0
+        block_ids = list(range(1, num_gpu_blocks + num_cpu_blocks))
         gpu_block_ids = block_ids[:num_gpu_blocks]
         cpu_block_ids = block_ids[num_gpu_blocks:]
 
