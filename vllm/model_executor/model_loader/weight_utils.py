cadb2548ddadcdffa3fe1b212c448cc6dd6fb89b
isotr0py <2037008807@qq.com>
2024-08-06 13:55:54 +0800

    code format

--- vllm/model_executor/model_loader/weight_utils.py
+++ vllm/model_executor/model_loader/weight_utils.py
@@ -125,9 +125,9 @@ def get_quant_config(model_config: ModelConfig,
     hf_quant_config = getattr(model_config.hf_config, "quantization_config",
                               None)
     # some vision model may keep quantization_config in their text_config
-    if hf_quant_config is None and model_config.hf_config.text_config is not None:
-        hf_quant_config = getattr(model_config.hf_config.text_config, "quantization_config",
-                              None)
+    hf_text_config = model_config.hf_config.text_config
+    if hf_quant_config is None and hf_text_config is not None:
+        hf_quant_config = getattr(hf_text_config, "quantization_config", None)
     if hf_quant_config is None:
         # compressed-tensors uses a compressions_config
         hf_quant_config = getattr(model_config.hf_config, "compression_config",
