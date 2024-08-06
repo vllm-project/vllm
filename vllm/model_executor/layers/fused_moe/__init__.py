5666fcbc6c3e12952ccae4b63dc02a0a63c7a97f
Dipika Sikka <dipikasikka1@gmail.com>
2024-08-06 20:05:33 +0000

    fix triton import

--- vllm/model_executor/layers/fused_moe/__init__.py
+++ vllm/model_executor/layers/fused_moe/__init__.py
@@ -1,22 +1,18 @@
-from vllm.model_executor.layers.fused_moe.fused_moe_awq import (
-    fused_experts_awq)
-from vllm.model_executor.layers.fused_moe.layer import (FusedMoE,
-                                                        FusedMoEMethodBase)
 from vllm.triton_utils import HAS_TRITON
 
-__all__ = [
-    "fused_experts_awq",
-    "FusedMoE",
-    "FusedMoEMethodBase",
-]
-
 if HAS_TRITON:
-
     from vllm.model_executor.layers.fused_moe.fused_moe import (
         fused_experts, fused_moe, fused_topk, get_config_file_name,
         grouped_topk)
+    from vllm.model_executor.layers.fused_moe.fused_moe_awq import (
+        fused_experts_awq)
+    from vllm.model_executor.layers.fused_moe.layer import (FusedMoE,
+                                                            FusedMoEMethodBase)
 
-    __all__ += [
+    __all__ = [
+        "FusedMoE",
+        "FusedMoEMethodBase",
+        "fused_experts_awq",
         "fused_moe",
         "fused_experts",
         "fused_topk",
