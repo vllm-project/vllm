7fc67e5bac2e6c36cad5f7b392a4da0ed79a7e6d
Kyle Mistele <kyle@constellate.ai>
2024-08-06 23:00:33 -0500

    chore: remove print

--- tests/async_engine/test_openapi_server_ray.py
+++ tests/async_engine/test_openapi_server_ray.py
@@ -88,7 +88,6 @@ async def test_single_chat_session(client: openai.AsyncOpenAI):
 
     choice = chat_completion.choices[0]
     assert choice.finish_reason == "length"
-    print('USAGE', chat_completion.usage)
     assert chat_completion.usage == openai.types.CompletionUsage(
         completion_tokens=10, prompt_tokens=55, total_tokens=65)
 
