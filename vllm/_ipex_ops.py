f4709ff42f81632f832eec7e7dba257c90706196
ivy-lv11 <zhicunlv@gmail.com>
2024-08-07 15:22:39 +0800

    fix

--- vllm/_ipex_ops.py
+++ vllm/_ipex_ops.py
@@ -131,10 +131,9 @@ class ipex_ops:
         vllm._C.ops.paged_attention_v2(out, exp_sum, max_logits, tmp_out,
                                      query.contiguous(),
                                      key_cache.view_as(value_cache),
-                                     value_cache, head_mapping, block_tables,
-                                     context_lens, scale, block_size,
-                                     max_context_len, alibi_slopes,
-                                     kv_cache_dtype, k_scale)
+                                     value_cache, num_kv_heads, scale, block_tables,
+                                     context_lens, block_size,
+                                     max_context_len, alibi_slopes,kv_cache_dtype, k_scale)
 
     @staticmethod
     def rotary_embedding(
