3d6aadecc4c9e1b82e2a4ce926390eff52528b3c
rshaw@neuralmagic.com <rshaw@neuralmagic.com>
2024-08-08 23:04:05 +0000

    format

--- examples/openai_completion_client.py
+++ examples/openai_completion_client.py
@@ -19,9 +19,7 @@ completion = client.completions.create(
     model=model,
     prompt="A robot may not injure a human being",
     echo=False,
-    n=2,
-    stream=stream,
-    logprobs=3)
+)
 
 print("Completion results:")
 if stream:
