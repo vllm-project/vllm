629704025b4ca3194a6772b2c28bd3e052cc287c
Alexander Matveev <alexm@neuralmagic.com>
2024-08-09 14:12:59 +0000

    Nick's comment

--- vllm/core/block_manager_v1.py
+++ vllm/core/block_manager_v1.py
@@ -336,7 +336,7 @@ class BlockSpaceManagerV1(BlockSpaceManager):
 
         # Assign the self-attention block tables for each sequence.
         if len(wait_seqs) == 1:
-            self.block_tables[wait_seqs[0].seq_id] = block_table
+            self.block_tables[seq.seq_id] = block_table
         else:
             for seq in wait_seqs:
                 self.block_tables[seq.seq_id] = block_table.copy()
