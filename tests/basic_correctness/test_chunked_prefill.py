de6c8ca2ee826981df62e50c2c7c864c1342b862
jon-chuang <9093549+jon-chuang@users.noreply.github.com>
2024-08-09 11:38:36 -0700

    fmt

--- tests/basic_correctness/test_chunked_prefill.py
+++ tests/basic_correctness/test_chunked_prefill.py
@@ -105,9 +105,11 @@ def test_models_log_probs(
     / numerics (e.g. when using lower-precision types like FP8).
     """
     if kv_cache_dtype == "fp8_e5m2" and model not in E5M2_KV_MODELS:
-        pytest.skip(f"{model} not in {E5M2_KV_MODELS} requiring {kv_cache_dtype}")
+        pytest.skip(
+            f"{model} not in {E5M2_KV_MODELS} requiring {kv_cache_dtype}")
     elif "fp8" in kv_cache_dtype and model not in E4M3_KV_MODELS:
-        pytest.skip(f"{model} not in {E4M3_KV_MODELS} requiring {kv_cache_dtype}")
+        pytest.skip(
+            f"{model} not in {E4M3_KV_MODELS} requiring {kv_cache_dtype}")
 
     NUM_LOG_PROBS = 8
     NUM_OUTPUT_TOKENS = 4
