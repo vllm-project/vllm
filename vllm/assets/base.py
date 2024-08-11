aa28892e26e404586e330d10b3b4b6a55244e5e0
Roger Wang <ywang@roblox.com>
2024-08-11 16:50:06 -0700

    iterate

--- vllm/assets/base.py
+++ vllm/assets/base.py
@@ -5,6 +5,8 @@ import vllm.envs as envs
 from vllm.connections import global_http_connection
 from vllm.envs import VLLM_IMAGE_FETCH_TIMEOUT
 
+vLLM_S3_BUCKET_URL = "https://vllm-public-assets.s3.us-west-2.amazonaws.com"
+
 
 def get_cache_dir():
     """Get the path to the cache for storing downloaded assets."""
@@ -26,10 +28,9 @@ def get_vllm_public_assets(filename: str) -> str:
 
     asset_path = asset_directory / filename
     if not asset_path.exists():
-        base_url = "https://vllm-public-assets.s3.us-west-2.amazonaws.com"
-
-        global_http_connection.download_file(f"{base_url}/{filename}",
-                                             asset_path,
-                                             timeout=VLLM_IMAGE_FETCH_TIMEOUT)
+        global_http_connection.download_file(
+            f"{vLLM_S3_BUCKET_URL}/{filename}",
+            asset_path,
+            timeout=VLLM_IMAGE_FETCH_TIMEOUT)
 
     return asset_path
