96d38b49b1f3811e06728491bb2d33ac71298c12
KuntaiDu <kuntai@uchicago.edu>
2024-08-11 05:29:31 +0000

    add visualization script

--- benchmarks/disagg_benchmarks/visualize_benchmark_results.py
+++ benchmarks/disagg_benchmarks/visualize_benchmark_results.py
@@ -2,72 +2,46 @@
 import matplotlib.pyplot as plt
 import yaml
 import pandas as pd
-from tabulate import tabulate
+import json
 
-
-def stringify(x):
-    return [str(i) for i in x]
 
 
 if __name__ == "__main__":
-    
-    with open("results/chunk_vs_disagg.yaml", "r") as f:
-        data = yaml.load(f, Loader=yaml.FullLoader)
+
+    data = []
+    for name in ['disagg_prefill', 'chunked_prefill']:
+        for qps in [2,4,6,8]:
+            with open(f"results/{name}-qps-{qps}.json", "r") as f:
+                x = json.load(f)
+                x['name'] = name
+                x['qps'] = qps
+                data.append(x)
+                
     df = pd.DataFrame.from_dict(data)
-
-    print_df = df.copy()
-    print_df.drop(columns=[
-        "ttft_ratio",
-        "itl_ratio",
-        "prefill_decode_ratio",
-    ], inplace=True)
-    print_df.to_csv('results/chunk_vs_disagg.csv', index=False)
-
-    df["chunk_e2e"] = df["chunk_ttft"] + df["chunk_itl"] * df["output_len"]
-    df["disagg_e2e"] = df["disagg_ttft"] + df["disagg_itl"] * df["output_len"]
-    df["e2e_ratio"] = df["chunk_e2e"] / df["disagg_e2e"]
+    dis_df = df[df['name'] == 'disagg_prefill']
+    chu_df = df[df['name'] == 'chunked_prefill']
     
+    plt.style.use('bmh')
     plt.rcParams['font.size'] = 20
-    
-    
-    # qps vs performance
-    qps_df = df[df["output_len"] == 150].copy()
-    qps_df.drop(columns=[
-        "chunk_itl",
-        "chunk_ttft",
-        "disagg_itl",
-        "disagg_ttft",
-        "output_len",
-        "prefill_decode_ratio", 
-    ], inplace=True)
-    fig, ax = plt.subplots(figsize=(10, 7))
-    qps_df.plot(
-        ax=ax,
-        kind="bar",
-        x="qps",
-        y=["ttft_ratio", "itl_ratio", "e2e_ratio"],
-        ylabel="$T_{chunked}~/~T_{disagg}$",
-        rot=0,
-    )
-    ax.hlines(1, -1, 5, color='black')
-    fig.savefig('results/qps.png')
-    plt.close(fig)
 
     
-    # prefill decode ratio vs performance
-    tokens_df = df[df["output_len"] != 12]
-    fig, ax = plt.subplots(figsize=(10, 7))
-    tokens_df.plot(
-        ax=ax,
-        kind="bar",
-        x="output_len",
-        xlabel="# of output tokens",
-        y=["ttft_ratio", "itl_ratio", "e2e_ratio", "prefill_decode_ratio"],
-        ylabel="$T_{chunked}~/~T_{disagg}$",
-        rot=0,
-    )
-    ax.hlines(1, -1, 5, color='black')
-    fig.savefig('results/tokens.png')
-    plt.close(fig)
+    for key in ['mean_ttft_ms', 
+                'median_ttft_ms',
+                'p99_ttft_ms',
+                'mean_itl_ms',
+                'median_itl_ms',
+                'p99_itl_ms']:
+        
+        fig, ax = plt.subplots(figsize=(11, 7))
+        plt.plot(dis_df['qps'], dis_df[key], label='disagg_prefill', marker='o', linewidth=4)
+        plt.plot(chu_df['qps'], chu_df[key], label='chunked_prefill', marker='o', linewidth=4)
+        ax.legend()
+
+        ax.set_xlabel('QPS')
+        ax.set_ylabel(key)
+        ax.set_ylim(bottom=0)
+        fig.savefig(f'results/{key}.png')
+        plt.close(fig)
+
     
     
