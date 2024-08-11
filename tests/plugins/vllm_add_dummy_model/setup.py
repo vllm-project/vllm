dc33035bc4d24996ca7cbf67a00dc4b3f2eabb65
youkaichao <youkaichao@126.com>
2024-08-11 16:42:44 -0700

    add

---
+++ tests/plugins/vllm_add_dummy_model/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='vllm_add_dummy_model',
+    version='0.1',
+    packages=['vllm_add_dummy_model'],
+    entry_points={
+        'vllm.general_plugins': [
+            "add_dummy_model = vllm_add_dummy_model:register"
+        ]
+    }
+)
