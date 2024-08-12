ac39c71e858d7b1e6197703e3486ca5672785baf
jon-chuang <9093549+jon-chuang@users.noreply.github.com>
2024-08-12 08:30:04 -0700

    minor

--- tests/kernels/test_prefix_prefill.py
+++ tests/kernels/test_prefix_prefill.py
@@ -465,7 +465,5 @@ def test_contexted_kv_attention_alibi(
     torch.cuda.synchronize()
     end_time = time.time()
     print(f"xformers Time: {(end_time - start_time)*1000:.2f} ms")
-    if "fp8" in kv_cache_dtype:
-        torch.testing.assert_close(output, output_ref, atol=1e-3, rtol=0)
-    else:
-        torch.testing.assert_close(output, output_ref, atol=1e-6, rtol=0)
+    atol = 1e-3 if "fp8" in kv_cache_dtype else 1e-6
+    torch.testing.assert_close(output, output_ref, atol=atol, rtol=0)
