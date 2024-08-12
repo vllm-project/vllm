d2c4754df8418439acb7406acbb51e66461b6aa2
DhruvaBansal00 <dhruva.bansal00@gmail.com>
2024-08-12 14:59:35 -0700

    replace wiehgt name with param name

--- vllm/model_executor/models/mixtral.py
+++ vllm/model_executor/models/mixtral.py
@@ -457,7 +457,7 @@ class MixtralForCausalLM(nn.Module, SupportsLoRA):
                     weight_loader = param.weight_loader
                     weight_loader(param,
                                   loaded_weight,
-                                  weight_name,
+                                  param_name,
                                   shard_id=shard_id,
                                   expert_id=expert_id,
                                   is_quantized=True)
