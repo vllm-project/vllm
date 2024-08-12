7d4b1f0997ae851e5a8a6d2349ca3461ee6af43a
Kuntai Du <kuntai@uchicago.edu>
2024-08-11 23:11:43 -0700

    avoid pip upgrade transformers -- no longer needed

--- .buildkite/nightly-benchmarks/scripts/run-trt-nightly.sh
+++ .buildkite/nightly-benchmarks/scripts/run-trt-nightly.sh
@@ -200,9 +200,6 @@ main() {
   mkdir -p $RESULTS_FOLDER
   BENCHMARK_ROOT=../.buildkite/nightly-benchmarks/
 
-  # update transformers package, to make sure mixtral tokenizer is available
-  python -m pip install transformers -U
-
   export CURRENT_LLM_SERVING_ENGINE=trt
   run_serving_tests $BENCHMARK_ROOT/tests/nightly-tests.json
   python -m pip install tabulate pandas
