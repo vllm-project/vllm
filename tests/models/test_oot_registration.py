a2e128062921c259fb08260eba2a8e542794f998
youkaichao <youkaichao@126.com>
2024-08-11 17:01:45 -0700

    update

--- tests/models/test_oot_registration.py
+++ tests/models/test_oot_registration.py
@@ -4,7 +4,7 @@ from vllm import LLM, SamplingParams
 def test_oot_registration(dummy_opt_path):
     prompts = ["Hello, my name is", "The text does not matter"]
     sampling_params = SamplingParams(temperature=0)
-    llm = LLM(model=dummy_opt_path)
+    llm = LLM(model=dummy_opt_path, load_format="dummy")
     first_token = llm.get_tokenizer().decode(0)
     outputs = llm.generate(prompts, sampling_params)
 
