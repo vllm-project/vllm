1ebb5d7eec62c1b9bc6b0e23ae95362fe88d8fa5
mzusman <mor.zusmann@gmail.com>
2024-08-12 09:27:03 +0300

    Add test

--- tests/models/test_experts_int8.py
+++ tests/models/test_experts_int8.py
@@ -3,3 +3,24 @@
 doesn't test correctness
 """
 
+import pytest
+
+
+MODELS = ["ai21labs/Jamba-tiny-random"]
+
+@pytest.mark.parametrize("model", MODELS)
+@pytest.mark.parametrize("dtype", ["bfloat16"])
+@pytest.mark.parametrize("max_tokens", [10])
+def test_model_experts_int8_startup(
+    hf_runner,
+    vllm_runner,
+    example_prompts,
+    model: str,
+    dtype: str,
+    max_tokens: int,
+) -> None:
+
+    with vllm_runner(model, dtype=dtype,
+                     quantization="experts_int8") as vllm_model:
+        vllm_model.generate_greedy(example_prompts, max_tokens)
+
