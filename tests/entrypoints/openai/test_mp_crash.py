68e48ac0097aa778d5c87321951fa3c9e4555922
youkaichao <youkaichao@126.com>
2024-08-13 14:32:49 -0700

    add tests

--- tests/entrypoints/openai/test_mp_crash.py
+++ tests/entrypoints/openai/test_mp_crash.py
@@ -6,7 +6,7 @@ from vllm.utils import FlexibleArgumentParser
 
 
 @pytest.mark.asyncio
-async def test_mp_crash_detection(monkeypatch):
+async def test_mp_crash_detection():
 
     with pytest.raises(RuntimeError) as excinfo:
         parser = FlexibleArgumentParser(
@@ -21,3 +21,20 @@ async def test_mp_crash_detection(monkeypatch):
             pass
     assert "The server process died before responding to the readiness probe"\
           in str(excinfo.value)
+
+
+@pytest.mark.asyncio
+async def test_mp_cuda_init():
+    # it should not crash, when cuda is initialized
+    # in the API server process
+    import torch
+    torch.cuda.init()
+    parser = FlexibleArgumentParser(description="vLLM's remote OpenAI server.")
+    parser = make_arg_parser(parser)
+    args = parser.parse_args([])
+    # use an invalid tensor_parallel_size to trigger the
+    # error in the server
+    args.tensor_parallel_size = 65536
+
+    async with build_async_engine_client(args):
+        pass
