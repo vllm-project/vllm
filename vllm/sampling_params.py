92681350667b805316a39757dc8618b04e025e22
Chang Su <chang.s.su@oracle.com>
2024-08-13 22:31:44 -0700

    Fix empty output when temp is too low (#2937)
    Co-authored-by: Cyrus Leung <tlleungac@connect.ust.hk>
    Signed-off-by: Alvant <alvasian@yandex.ru>

--- vllm/sampling_params.py
+++ vllm/sampling_params.py
@@ -13,6 +13,7 @@ from vllm.logits_process import LogitsProcessor, NoBadWordsLogitsProcessor
 logger = init_logger(__name__)
 
 _SAMPLING_EPS = 1e-5
+_MAX_TEMP = 1e-2
 
 
 class SamplingType(IntEnum):
@@ -140,6 +141,12 @@ class SamplingParams:
         self.presence_penalty = presence_penalty
         self.frequency_penalty = frequency_penalty
         self.repetition_penalty = repetition_penalty
+        if 0 < temperature < _MAX_TEMP:
+            logger.warning(
+                "temperature %s is less than %s, which may cause numerical "
+                "errors nan or inf in tensors. We have maxed it out to %s.",
+                temperature, _MAX_TEMP, _MAX_TEMP)
+            temperature = max(temperature, _MAX_TEMP)
         self.temperature = temperature
         self.top_p = top_p
         self.top_k = top_k
