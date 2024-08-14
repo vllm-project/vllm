3e40b53f7df9579bbd344af21b68c5e7a42051ca
Kuntai Du <kuntai@uchicago.edu>
2024-08-13 20:16:53 -0700

    update so that we kill vllm together with zeromq server

--- .buildkite/nightly-benchmarks/run-benchmarks-suite.sh
+++ .buildkite/nightly-benchmarks/run-benchmarks-suite.sh
@@ -70,24 +70,11 @@ wait_for_server() {
 
 kill_gpu_processes() {
   # kill all processes on GPU.
-  pids=$(nvidia-smi --query-compute-apps=pid --format=csv,noheader)
-  if [ -z "$pids" ]; then
-      echo "No GPU processes found."
-  else
-      for pid in $pids; do
-          kill -9 "$pid"
-          echo "Killed process with PID: $pid"
-      done
 
-      echo "All GPU processes have been killed."
-  fi
+  pkill -f python3
+  ps -e | grep pt_main_thread | awk '{print $1}' | xargs kill -9
 
-  # waiting for GPU processes to be fully killed
-  # loop while nvidia-smi returns any processes
-  while [ -n "$(nvidia-smi --query-compute-apps=pid --format=csv,noheader)" ]; do
-    sleep 1
-    echo "Waiting for GPU processes to be killed"
-  done
+  sleep 5
 
   # remove vllm config file
   rm -rf ~/.config/vllm
