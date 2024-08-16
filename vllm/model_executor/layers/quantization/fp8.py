10e04ada4d2b5af34a1ee41db24dc5e5591cdf15
Dipika <dipikasikka1@gmail.com>
2024-08-16 19:35:07 +0000

    add fp8 models for weight loading test

--- vllm/model_executor/layers/quantization/fp8.py
+++ vllm/model_executor/layers/quantization/fp8.py
@@ -208,6 +208,9 @@ class Fp8LinearMethod(LinearMethodBase):
         else:
             layer.weight_scale = torch.nn.Parameter(layer.weight_scale.data,
                                                     requires_grad=False)
+            if self.quant_config.activation_scheme == "static":
+                layer.input_scale = torch.nn.Parameter(layer.input_scale.data,
+                                                       requires_grad=False)
             # If using marlin (w8a16), kernel uses channelwise weights,
             # so extend the weight scales to be channelwise.
             if self.use_marlin:
