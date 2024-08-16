eb497df703cccb3ad4e86c45e088d20fd8f6b310
Jee Jee Li <pandaleefree@gmail.com>
2024-08-17 00:50:58 +0800

    Address unit test error

--- tests/lora/test_punica_variation.py
+++ tests/lora/test_punica_variation.py
@@ -291,9 +291,9 @@ def test_punica_expand_nslices(
                 lora_indices_tensor,
                 batches,
                 max_seq_length,
+                token_nums,
                 slice_offset,
                 hidden_size,
-                token_nums,
                 add_inputs=True,
             )
         else:
