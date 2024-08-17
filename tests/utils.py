ff6de47458aa437e0c55dd4ec7f9bcf023c3aab9
youkaichao <youkaichao@gmail.com>
2024-08-17 11:26:38 -0700

    [ci][test] allow longer wait time for api server (#7629)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- tests/utils.py
+++ tests/utils.py
@@ -56,7 +56,7 @@ VLLM_PATH = Path(__file__).parent.parent
 
 class RemoteOpenAIServer:
     DUMMY_API_KEY = "token-abc123"  # vLLM's OpenAI server does not need API key
-    MAX_START_WAIT_S = 120  # wait for server to start for 120 seconds
+    MAX_START_WAIT_S = 240  # wait for server to start for 240 seconds
 
     def __init__(
         self,
