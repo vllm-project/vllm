dfacb0e05b9bcff94e552f489e161d1aa0c0bd2d
Bill Nell <bill@neuralmagic.com>
2024-08-20 19:03:32 +0000

    comment

--- csrc/core/torch_bindings.cpp
+++ csrc/core/torch_bindings.cpp
@@ -13,6 +13,16 @@ TORCH_LIBRARY_EXPAND(TORCH_EXTENSION_NAME, lib) {
   vllm::ScalarTypeTorch::bind_class(lib);
 }
 
+// Note on op signatures:
+// The X_meta signatures are for the meta functions corresponding to op X.
+// They must be kept in sync with the signature for X. Generally, only
+// functions that return Tensors require a meta function.
+//
+// See the following links for detailed docs on op registration and function
+// schemas.
+// https://docs.google.com/document/d/1_W62p8WJOQQUzPsJYa7s701JXt0qf2OfLub2sbkHOaU/edit#heading=h.ptttacy8y1u9
+// https://github.com/pytorch/pytorch/blob/main/aten/src/ATen/native/README.md#annotations
+
 TORCH_LIBRARY_EXPAND(_C, ops) {
   // vLLM custom ops
 
