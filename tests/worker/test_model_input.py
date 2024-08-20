c3e4f12a38212ed695b993d913ccf340c1af33be
Antoni Baum <antoni.baum@protonmail.com>
2024-08-20 10:07:41 -0700

    Update test_model_input.py

--- tests/worker/test_model_input.py
+++ tests/worker/test_model_input.py
@@ -5,9 +5,9 @@ import torch
 
 from vllm.attention import AttentionMetadata, AttentionMetadataBuilder
 from vllm.attention.backends.abstract import AttentionBackend
+from vllm.attention.backends.utils import CommonAttentionState
 from vllm.model_executor import SamplingMetadata
 from vllm.model_executor.pooling_metadata import PoolingMetadata
-from vllm.attention.backends.utils import CommonAttentionState
 from vllm.worker.embedding_model_runner import (
     ModelInputForGPUWithPoolingMetadata)
 from vllm.worker.model_runner import ModelInputForGPUWithSamplingMetadata
