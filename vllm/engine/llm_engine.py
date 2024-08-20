a0acc4433178f49f8207475bcd46165182257925
Ubuntu <zhn@zhn-a100.loq3xmynzuheffxht14poc5q4c.lx.internal.cloudapp.net>
2024-08-20 07:26:18 +0000

    fix merge

--- vllm/engine/llm_engine.py
+++ vllm/engine/llm_engine.py
@@ -742,6 +742,10 @@ class LLMEngine:
                     request_id=request_id,
                     lora_request=lora_request,
                 )
+                
+            if hasattr(self.model_config.hf_config, "num_output_head"):
+                # duplicate the prompt_token_ids for each head
+                prompt_token_ids = [[i] *  self.model_config.hf_config.num_output_head for i in prompt_token_ids]
 
             multi_modal_data = inputs.get("multi_modal_data")
         else:
@@ -947,11 +951,6 @@ class LLMEngine:
                 request_id=request_id,
             )
         else:
-            prompt_token_ids = inputs["prompt_token_ids"]
-            
-            if hasattr(self.model_config.hf_config, "num_output_head"):
-                # duplicate the prompt_token_ids for each head
-                prompt_token_ids = [[i] *  self.model_config.hf_config.num_output_head for i in prompt_token_ids]
             if is_explicit_encoder_decoder_prompt(inputs):
                 raise ValueError("Cannot pass encoder-decoder prompt "
                                  "to decoder-only models")
