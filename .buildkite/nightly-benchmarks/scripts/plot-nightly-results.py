02f22f95169eadf6f8d3a5442fe57528c2d46251
Kuntai Du <kuntai@uchicago.edu>
2024-08-19 23:35:30 -0700

    update plotting script

--- .buildkite/nightly-benchmarks/scripts/plot-nightly-results.py
+++ .buildkite/nightly-benchmarks/scripts/plot-nightly-results.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import pandas as pd
 from tabulate import tabulate
+import numpy as np
 
 
 def parse_arguments():
@@ -20,13 +21,50 @@ def parse_arguments():
                         type=str,
                         required=True,
                         help='Description of the results.')
+    parser.add_argument('--dataset',
+                        type=str,
+                        required=True,
+                        help="The dataset used for the benchmark.")
 
     args = parser.parse_args()
     return args
 
+    
+def get_perf(df, method, model, metric):
+    
+    means = []
+    
+    for qps in [2,4,8,16,"inf"]:
+        target = df['Test name'].str.contains(model)
+        target = target & df['Engine'].str.contains(method)
+        target = target & df['Test name'].str.contains("qps_" + str(qps))
+        filtered_df = df[target]
+
+        if filtered_df.empty:
+            means.append(0.)
+        else:
+            means.append(filtered_df[metric].values[0])
+
+    return np.array(means)
+
+def get_perf_w_std(df, method, model, metric):
+    
+    if metric in ["TTFT", "ITL"]:
+        mean = get_perf(df, method, model, "Mean " + metric + " (ms)")
+        std = get_perf(df, method, model, "Std " + metric + " (ms)")
+        success = get_perf(df, method, model, "Successful req.")
+        if std is not None:
+            std = std / np.sqrt(success)
+
+    else:
+        assert metric == "Tput"
+        mean = get_perf(df, method, model, "Input Tput (tok/s)") + get_perf(df, method, model, "Output Tput (tok/s)")
+        std = None
+
+    return mean, std
+
 
 def main(args):
-    bar_colors = ['#56B4E9', '#009E73', '#D55E00', '#E69F00']
     results_folder = Path(args.results_folder)
 
     results = []
@@ -38,6 +76,7 @@ def main(args):
 
     # generate markdown table
     df = pd.DataFrame.from_dict(results)
+    df = df[df["Test name"].str.contains(args.dataset)]
 
     md_table = tabulate(df, headers='keys', tablefmt='pipe', showindex=False)
 
@@ -50,84 +89,40 @@ def main(args):
     with open("nightly_results.md", "w") as f:
         f.write(description)
 
-    plt.rcParams.update({'font.size': 20})
+    plt.rcParams.update({'font.size': 18})
+    plt.set_cmap("cividis")
 
     # plot results
     fig, axes = plt.subplots(3, 3, figsize=(16, 14))
     fig.subplots_adjust(hspace=1)
-    methods = ["vllm", "trt", "lmdeploy", "tgi"]
+    methods = ["vllm", "trtllm", "sglang", "lmdeploy", "tgi"]
     for i, model in enumerate(["llama8B", "llama70B", "mixtral8x7B"]):
-        for j, metric in enumerate(["TTFT", "ITL"]):
-            means, stds = [], []
-            for method in methods:
-                target = df['Test name'].str.contains(model)
-                target = target & df['Engine'].str.contains(method)
-                filtered_df = df[target]
-
-                if filtered_df.empty:
-                    means.append(0.)
-                    stds.append(0.)
-                else:
-                    means.append(filtered_df[f"Mean {metric} (ms)"].values[0])
-                    std = filtered_df[f"Std {metric} (ms)"].values[0]
-                    success = filtered_df["Successful req."].values[0]
-                    stds.append(std / math.sqrt(success))
-
-            print(model, metric)
-            print(means, stds)
-
-            ax = axes[i, j + 1]
-
-            bars = ax.bar(
-                ["vllm", "trt", "lmdeploy", "tgi"],
-                means,
-                yerr=stds,
-                capsize=10,
-            )
-            for idx, bar in enumerate(bars):
-                bar.set_color(bar_colors[idx])
-            ax.set_ylim(bottom=0)
-
-            ax.set_ylabel(f"{metric} (ms)")
-            ax.set_title(f"{model} {metric}")
-            ax.grid(axis='y')
-
-        metric = "Tput"
-        j = 0
-        if True:
-            tputs = []
-            for method in methods:
-                target = df['Test name'].str.contains(model)
-                target = target & df['Engine'].str.contains(method)
-                filtered_df = df[target]
-
-                if filtered_df.empty:
-                    tputs.append(0.)
-                else:
-                    input_tput = filtered_df["Input Tput (tok/s)"].values[0]
-                    output_tput = filtered_df["Output Tput (tok/s)"].values[0]
-                    tputs.append(input_tput + output_tput)
-
-            print(model, metric)
-            print(tputs)
-
+        for j, metric in enumerate(["TTFT", "ITL", "Tput"]):
             ax = axes[i, j]
-
-            bars = ax.bar(
-                ["vllm", "trt", "lmdeploy", "tgi"],
-                tputs,
-            )
-            for idx, bar in enumerate(bars):
-                bar.set_color(bar_colors[idx])
-
             ax.set_ylim(bottom=0)
-
-            ax.set_ylabel("Tput (token/s)")
+            if metric in ["TTFT", "ITL"]:
+                ax.set_ylabel(f"{metric} (ms)")
+            else:
+                ax.set_ylabel(f"{metric} (tok/s)")
+            ax.set_xlabel("QPS")
             ax.set_title(f"{model} {metric}")
             ax.grid(axis='y')
+            print("New line")
+            print(model, metric)
+            for k, method in enumerate(methods):
+                mean, std = get_perf_w_std(df, method, model, metric)
+                print(method, mean, std)
+                ax.errorbar(["2", "4", "8", "16", "inf"], 
+                            mean, 
+                            yerr=std, 
+                            capsize=10, 
+                            label=method,)
+
+            ax.legend()
+
 
     fig.tight_layout()
-    fig.savefig("nightly_results.png", bbox_inches='tight', dpi=400)
+    fig.savefig(f"nightly_results_{args.dataset}.png", bbox_inches='tight', dpi=400)
 
 
 if __name__ == '__main__':
