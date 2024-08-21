8678a69ab51956031e3bb70bdf1a781a8652e67d
Dipika Sikka <dipikasikka1@gmail.com>
2024-08-21 19:17:10 -0400

    [Kernel]  Expand MoE weight loading + Add Fused Marlin MoE Kernel (#7527)
    Co-authored-by: ElizaWszola <eliza@neuralmagic.com>

--- vllm/model_executor/model_loader/utils.py
+++ vllm/model_executor/model_loader/utils.py
@@ -23,11 +23,11 @@ def get_model_architecture(
     architectures = getattr(model_config.hf_config, "architectures", [])
     # Special handling for quantized Mixtral.
     # FIXME(woosuk): This is a temporary hack.
+    mixtral_supported = ["fp8", "compressed-tensors"]
     if (model_config.quantization is not None
-            and model_config.quantization != "fp8"
+            and model_config.quantization not in mixtral_supported
             and "MixtralForCausalLM" in architectures):
         architectures = ["QuantMixtralForCausalLM"]
-
     return ModelRegistry.resolve_model_cls(architectures)
 
 
