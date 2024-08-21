3aeb63d8bc7eb3def5eb775f9134aa0d69e86488
Umesh Deshpande <udeshpa@us.ibm.com>
2024-08-21 02:57:32 -0400

    Removed assert for lora check

--- vllm/lora/utils.py
+++ vllm/lora/utils.py
@@ -106,8 +106,8 @@ def parse_fine_tuned_lora_name(name: str) -> Tuple[str, bool, bool]:
     assert parts[0] == "base_model"
     assert parts[1] == "model"
     if parts[-1] == "weight":
-        assert parts[-2] == "lora_A" or parts[-2] == "lora_B"
-        return ".".join(parts[2:-2]), parts[-2] == "lora_A", False
+        if parts[-2] == "lora_A" or parts[-2] == "lora_B":
+            return ".".join(parts[2:-2]), parts[-2] == "lora_A", False
 
     if parts[-1] == "lora_embedding_A" or parts[-1] == "lora_embedding_B":
         return ".".join(parts[2:-1]), parts[-1] == "lora_embedding_A", False
