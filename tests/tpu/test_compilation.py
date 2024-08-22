381023e020eeeb7eab71841627c5091c45a07b03
youkaichao <youkaichao@126.com>
2024-08-22 15:34:17 -0700

    add test

--- tests/tpu/test_compilation.py
+++ tests/tpu/test_compilation.py
@@ -1,15 +1,17 @@
-import depyf
+import glob
+import os
 import runpy
 import tempfile
-import os
-import glob
+
+import depyf
 
 temp_dir = tempfile.mkdtemp()
 with depyf.prepare_debug(temp_dir):
     cur_dir = os.path.basename(__file__)
     parent_dir = os.path.dirname(cur_dir)
     root_dir = os.path.dirname(parent_dir)
-    example_file = os.path.join(root_dir, "examples", "offline_inference_tpu.py")
+    example_file = os.path.join(root_dir, "examples",
+                                "offline_inference_tpu.py")
     runpy.run_path(example_file)
 
 compiled_code = glob.glob(os.path.join(temp_dir, "__transformed_code*.py"))
