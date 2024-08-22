dcbbfca43838dd92a8bceec4f7049bcf1f423f54
youkaichao <youkaichao@126.com>
2024-08-22 15:32:29 -0700

    add compilation check

--- .buildkite/run-tpu-test.sh
+++ .buildkite/run-tpu-test.sh
@@ -13,4 +13,4 @@ remove_docker_container
 source /etc/environment
 # Run a simple end-to-end example.
 docker run --privileged --net host --shm-size=16G -it -e HF_TOKEN=$HF_TOKEN --name tpu-test vllm-tpu \
-    python3 /workspace/vllm/examples/offline_inference_tpu.py
+    python3 /workspace/vllm/tests/tpu/test_compilation.py
---
+++ tests/tpu/test_compilation.py
@@ -0,0 +1,16 @@
+import depyf
+import runpy
+import tempfile
+import os
+import glob
+
+temp_dir = tempfile.mkdtemp()
+with depyf.prepare_debug(temp_dir):
+    cur_dir = os.path.basename(__file__)
+    parent_dir = os.path.dirname(cur_dir)
+    root_dir = os.path.dirname(parent_dir)
+    example_file = os.path.join(root_dir, "examples", "offline_inference_tpu.py")
+    runpy.run_path(example_file)
+
+compiled_code = glob.glob(os.path.join(temp_dir, "__transformed_code*.py"))
+assert len(compiled_code) == 2
