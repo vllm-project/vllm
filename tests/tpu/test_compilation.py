db73048e1f4c555608043ffa72cf6699e9e8d28b
youkaichao <youkaichao@126.com>
2024-08-23 09:47:03 -0700

    add comments

--- tests/tpu/test_compilation.py
+++ tests/tpu/test_compilation.py
@@ -15,4 +15,9 @@ with depyf.prepare_debug(temp_dir):
     runpy.run_path(example_file)
 
 compiled_code = glob.glob(os.path.join(temp_dir, "__transformed_code*.py"))
+# we should only trigger Dynamo compilation twice,
+# one for the prefill phase, and one for the decode phase.
+# the graphs will have symbolic shapes, and later calls should
+# not trigger Dynamo compilation again.
+# NOTE: it might still trigger XLA compilation.
 assert len(compiled_code) == 2
