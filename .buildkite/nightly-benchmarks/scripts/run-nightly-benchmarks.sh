0ee068810499788ba08b0c1d9d57debbb205e5fd
Kuntai Du <kuntai@uchicago.edu>
2024-08-23 17:01:08 -0700

    fix vllm import issue

--- .buildkite/nightly-benchmarks/scripts/run-nightly-benchmarks.sh
+++ .buildkite/nightly-benchmarks/scripts/run-nightly-benchmarks.sh
@@ -71,6 +71,8 @@ get_current_llm_serving_engine() {
 
   if [ -e /vllm-workspace ]; then
     echo "Container: vllm"
+    # move to a completely irrelevant directory, to avoid import vllm from current folder
+    cd /dev
     export CURRENT_LLM_SERVING_ENGINE=vllm_$(python3 -c "import vllm; print(vllm.__version__.replace('.', ''))")
     
     return
