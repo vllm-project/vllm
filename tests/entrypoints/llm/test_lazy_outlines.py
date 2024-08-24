fe1f35c71fe51b23c4c346e31ebcac7ea3d82220
youkaichao <youkaichao@126.com>
2024-08-24 00:29:07 -0700

    add lm-format-enforcer test

--- tests/entrypoints/llm/test_lazy_outlines.py
+++ tests/entrypoints/llm/test_lazy_outlines.py
@@ -3,7 +3,7 @@ import sys
 from vllm import LLM, SamplingParams
 
 
-def test_lazy_outlines():
+def test_lazy_outlines(sample_regex):
     """If users don't use guided decoding, outlines should not be imported.
     """
     prompts = [
@@ -21,5 +21,25 @@ def test_lazy_outlines():
         generated_text = output.outputs[0].text
         print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
 
-    # check if outlines is imported
+    # make sure outlines is not imported
+    assert 'outlines' not in sys.modules
+
+    llm = LLM(model="facebook/opt-125m",
+              enforce_eager=True,
+              guided_decoding_backend="lm-format-enforcer")
+    sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+    outputs = llm.generate(
+        prompts=[
+            f"Give an example IPv4 address with this regex: {sample_regex}"
+        ] * 2,
+        sampling_params=sampling_params,
+        use_tqdm=True,
+        guided_options_request=dict(guided_regex=sample_regex))
+
+    for output in outputs:
+        prompt = output.prompt
+        generated_text = output.outputs[0].text
+        print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+
+    # make sure outlines is not imported
     assert 'outlines' not in sys.modules
