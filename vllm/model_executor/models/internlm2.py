d7f2d545a6b652d070b5fb9649bc2e31c54b6074
manikandan-tm <manikandan.tm@zucisystems.com>
2024-08-26 10:50:06 +0530

    refactor

--- vllm/model_executor/models/internlm2.py
+++ vllm/model_executor/models/internlm2.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from typing import Any, Dict, Iterable, List, Optional, Tuple, Union
+from typing import Any, Dict, Iterable, List, Optional, Tuple
 
 import torch
 from torch import nn
