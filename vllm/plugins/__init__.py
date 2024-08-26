a7dd9da30b98d0aaacb96fe0ee59ef5791463bba
youkaichao <youkaichao@126.com>
2024-08-26 11:46:14 -0700

    add get_executor_cls

--- vllm/plugins/__init__.py
+++ vllm/plugins/__init__.py
@@ -1,6 +1,8 @@
 import logging
+from typing import Optional, Type
 
 import vllm.envs as envs
+from vllm.executor.executor_base import ExecutorBase
 
 logger = logging.getLogger(__name__)
 
@@ -29,3 +31,28 @@ def load_general_plugins():
             except Exception:
                 logger.exception("Failed to load general plugin: %s",
                                  plugin.name)
+
+
+_executor_cls_to_use: Optional[Type[ExecutorBase]] = None
+
+
+def set_executor_cls(executor_cls: Type[ExecutorBase]):
+    """
+    Set the executor class to use. This is used by plugins to set the executor
+    """
+    global _executor_cls_to_use
+    if _executor_cls_to_use is not None and \
+        _executor_cls_to_use is not executor_cls:
+        logger.warning(
+            "Executor class has already been set to %s by other plugins."
+            "Changing the executor class to %s now.", _executor_cls_to_use,
+            executor_cls)
+    _executor_cls_to_use = executor_cls
+
+
+def get_executor_cls() -> Optional[Type[ExecutorBase]]:
+    """
+    Get the executor class to use. This is used by the main code to get the
+    executor class to use.
+    """
+    return _executor_cls_to_use
