7e8ef5cfe6dad71c9240c1a2d96e3fd31f89c44e
manikandan-tm <manikandan.tm@zucisystems.com>
2024-08-26 11:54:37 +0530

    refactor

--- vllm/model_executor/models/internlm2.py
+++ vllm/model_executor/models/internlm2.py
@@ -23,9 +23,9 @@ from vllm.model_executor.layers.vocab_parallel_embedding import (
 from vllm.model_executor.model_loader.weight_utils import default_weight_loader
 from vllm.model_executor.sampling_metadata import SamplingMetadata
 from vllm.sequence import IntermediateTensors, SamplerOutput
+
 from .utils import (is_pp_missing_parameter,
                     make_empty_intermediate_tensors_factory, make_layers)
-
 
 
 class InternLM2MLP(nn.Module):
