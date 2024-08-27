1a9f0d2de5269448fea79ece57b8d162163f7246
youkaichao <youkaichao@126.com>
2024-08-26 21:05:58 -0700

    update tests

--- tests/tpu/test_compilation.py
+++ tests/tpu/test_compilation.py
@@ -14,10 +14,21 @@ with depyf.prepare_debug(temp_dir):
                                 "offline_inference_tpu.py")
     runpy.run_path(example_file)
 
-compiled_code = glob.glob(os.path.join(temp_dir, "__transformed_code*.py"))
-# we should only trigger Dynamo compilation twice,
-# one for the prefill phase, and one for the decode phase.
-# the graphs will have symbolic shapes, and later calls should
-# not trigger Dynamo compilation again.
+compiled_code = sorted(
+    glob.glob(os.path.join(temp_dir, "__transformed_code*.py")))
+full_code = glob.glob(os.path.join(temp_dir, "full_code*.py"))[0]
+# we should only trigger Dynamo compilation three times:
+# one for the profiling phase (and the compiled artifact will be discarded)
+# one for the prefill phase with symbolic shapes
+# one for the decode phase with symbolic shapes
+# and later calls should not trigger Dynamo compilation again.
 # NOTE: it might still trigger XLA compilation.
-assert len(compiled_code) == 2
+
+# check we have three compiled code
+assert len(compiled_code) == 3
+
+# check the first compilation is discarded
+with open(full_code) as f:
+    full_code_content = f.read()
+    profile_function = compiled_code[0].split(".")[0]
+    assert profile_function not in full_code_content
