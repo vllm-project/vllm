31e9e7b27fb4fd21239cda662a96c25b3ebb2e57
youkaichao <youkaichao@126.com>
2024-08-27 09:53:53 -0700

    update tests

--- tests/compile/test_wrapper.py
+++ tests/compile/test_wrapper.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import torch
 
 from vllm.compilation.wrapper import TorchCompileWrapperWithCustomDispacther
@@ -5,8 +7,10 @@ from vllm.compilation.wrapper import TorchCompileWrapperWithCustomDispacther
 
 class MyMod(torch.nn.Module):
 
-    def forward(self, x: torch.Tensor, a: int):
-        return (x + a) * 2
+    def forward(self, x: torch.Tensor, cache: Optional[torch.Tensor] = None):
+        if cache is not None:
+            return x + cache
+        return x * 2
 
 
 class MyWrapper(TorchCompileWrapperWithCustomDispacther):
@@ -16,25 +20,31 @@ class MyWrapper(TorchCompileWrapperWithCustomDispacther):
         compiled_callable = torch.compile(self.forward, backend="eager")
         super().__init__(compiled_callable)
 
-    def forward(self, x: torch.Tensor, a: int):
+    def forward(self, x: torch.Tensor, cache: Optional[torch.Tensor] = None):
         # this is the function to be compiled
-        return self.model(x, a)
+        return self.model(x, cache)
 
-    def __call__(self, x: torch.Tensor, a: int):
+    def __call__(self, x: torch.Tensor, cache: Optional[torch.Tensor] = None):
         # let torch.compile compile twice
-        if len(self.compiled_codes) >= 2:
-            with self.dispatch_to_code(0):
-                return self.forward(x, a)
+        if len(self.compiled_codes) == 2:
+            dispatch_id = 0 if cache is None else 1
+            with self.dispatch_to_code(dispatch_id):
+                return self.forward(x, cache)
         else:
-            return self.compiled_callable(x, a)
+            return self.compiled_callable(x, cache)
 
 
 def test_torch_compile_wrapper():
     mod = MyMod()
     wrapper = MyWrapper(mod)
-    x = torch.tensor([1.0])
-    wrapper(x, 0)  # first time, compile
-    wrapper(x, 1)  # second time, compile
-    wrapper(x, 2)  # third time, dispatch to the first compiled code
-    assert len(wrapper.compiled_codes) == 2
+    x = torch.tensor([1])
+    cache = torch.tensor([2])
+    wrapper(x, None)  # first time, compile
+    wrapper(x, cache)  # second time, compile
 
+    new_x = torch.tensor([3])
+    assert wrapper(new_x,
+                   None).item() == 6  # dispatch to the first compiled code
+    assert wrapper(new_x,
+                   cache).item() == 5  # dispatch to the second compiled code
+    assert len(wrapper.compiled_codes) == 2
