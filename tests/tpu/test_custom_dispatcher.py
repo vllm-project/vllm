b9fff4c6421545927262bd6b3bcc8a28c590dcc4
youkaichao <youkaichao@126.com>
2024-08-27 11:01:35 -0700

    update tests

--- tests/tpu/test_custom_dispatcher.py
+++ tests/tpu/test_custom_dispatcher.py
@@ -2,5 +2,8 @@ from ..utils import compare_two_settings
 
 
 def test_custom_dispatcher():
-    compare_two_settings("google/gemma-2b", [], [],
-                         {"VLLM_DYNAMO_USE_CUSTOM_DISPATCHER": "0"})
+    compare_two_settings("google/gemma-2b",
+                         arg1=["--enforce-eager"],
+                         arg2=["--enforce-eager"],
+                         env1={"VLLM_DYNAMO_USE_CUSTOM_DISPATCHER": "0"},
+                         env2={})
