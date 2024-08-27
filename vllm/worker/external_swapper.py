3ccab0f0e214f562e852f677e2ada673394f9724
Changqi Lu <luchangqi.123@bytedance.com>
2024-08-27 10:54:08 +0800

    [Core] Abstract external swapper interface
    The ExternalSwapperBase interface is extracted,
    and users can implement their own ExternalSwapper.
    ExternalSwapperBase corresponds one-to-one with cache_engine
    and The interaction between cache_engine and external swapper
    can be called into the specific implementation class of the
    corresponding external swapper.
    
    Change-Id: I585520bda1298bcb32e501d5f090299e4a21e1ad
    Signed-off-by: Changqi Lu <luchangqi.123@bytedance.com>

---
+++ vllm/worker/external_swapper.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+from typing import (Tuple, List)
+from functools import reduce
+import operator
+import torch
+import os
+
+from vllm.config import CacheConfig, ModelConfig, ParallelConfig
+from vllm.utils import get_dtype_size
+from vllm.attention.backends.abstract import AttentionBackend
+
+
+class ExternalSwapperBase(ABC):
+    """Base class for external swapper."""
+
+    @staticmethod
+    def get_external_swapper_class(external_swapper: str):
+        external_swapper = external_swapper.lower()
+
+        raise ValueError(f"Unknown external_swapper_type {external_swapper=}")
+
+    @abstractmethod
+    def _allocate_kv_cache(self) -> List[int]:
+        """Allocate KV cache."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def swap_out(self, src_to_dst: torch.Tensor) -> None:
+        """Swap out blocks from GPU -> NVMf."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def swap_in(self, src_to_dst: torch.Tensor) -> None:
+        """Swap in blocks from NVMf -> GPU."""
+        raise NotImplementedError
