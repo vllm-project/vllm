38f9de93322480c240068cc55993a0013d305f89
Krishna Mandal <krishna@mandal.us>
2024-08-28 20:59:31 +0000

    updating to main, removing extra files

--- install_config.sh
+++
@@ -1,6 +0,0 @@
-export MAX_JOBS=6 
-echo "MAX_JOBS is set to: $MAX_JOBS" 
-export CUDA_HOME=/usr/local/cuda 
-export PATH="${CUDA_HOME}/bin:$PATH" 
-nvcc --version # verify that nvcc is in your PATH 
-${CUDA_HOME}/bin/nvcc --version # verify that nvcc is in your CUDA_HOMEa
--- ssh_add.sh
+++
@@ -1,3 +0,0 @@
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/krishna
-ssh-add -l
