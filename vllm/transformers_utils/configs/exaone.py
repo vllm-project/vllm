862c86ecce0b4fa9282f3aefcd0da24c67c2e60f
nayohan <nayohan13@gmail.com>
2024-08-28 13:33:02 +0900

    [MOD] reformat import

--- vllm/transformers_utils/configs/exaone.py
+++ vllm/transformers_utils/configs/exaone.py
@@ -17,9 +17,9 @@
 """Exaone model configuration"""
 
 from typing import Dict
+
 from transformers.configuration_utils import PretrainedConfig
 from transformers.utils import logging
-
 
 logger = logging.get_logger(__name__)
 
