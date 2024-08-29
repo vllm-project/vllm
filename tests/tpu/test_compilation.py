15b083a2de4b55613ae4253ffcd4f51fbdb0e6b1
youkaichao <youkaichao@126.com>
2024-08-28 17:13:30 -0700

    add more tests

--- tests/tpu/test_compilation.py
+++ tests/tpu/test_compilation.py
@@ -31,3 +31,25 @@ compiled_code = sorted(
 # check we have three compiled code
 # this is the assumption when we use the custom dispatcher
 assert len(compiled_code) == 3
+
+# check all the compilations are as expected
+compiled_fn = sorted(
+    glob.glob(os.path.join(temp_dir, "__compiled_fn*Captured*.py")))
+
+# the first compilation is the profiling phase,
+# it should not have any kv cache
+with open(compiled_fn[0]) as f:
+    content = f.read()
+    assert "kv_caches" not in content
+
+# the second compilation is the prefill phase,
+# it should have kv cache and the flash_attention op
+with open(compiled_fn[1]) as f:
+    content = f.read()
+    assert "kv_caches" in content and "torch.ops.xla.flash_attention" in content
+
+# the third compilation is the decode phase,
+# it should have kv cache and the paged_attention op
+with open(compiled_fn[2]) as f:
+    content = f.read()
+    assert "kv_caches" in content and "torch.ops.xla.paged_attention" in content
