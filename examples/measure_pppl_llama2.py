40a869e7407ff058bde4c5cb2e9972d0ec27fa5d
Alexei V. Ivanov <alexei.ivanov@amd.com>
2024-08-29 19:07:28 +0000

    .

--- examples/measure_pppl_llama2.py
+++ examples/measure_pppl_llama2.py
@@ -45,8 +45,8 @@ import argparse
 import datetime
 import math
 import os
-from huggingface_hub import hf_hub_download
 
+from huggingface_hub import hf_hub_download
 from transformers import LlamaTokenizer
 
 from vllm import LLM, SamplingParams
