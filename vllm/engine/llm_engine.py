9ec858c8a3086eaf63c5174875770c7a72f73eba
DarkLight1337 <tlleungac@connect.ust.hk>
2024-08-29 14:56:29 +0000

    Provide more detailed error message

--- vllm/engine/llm_engine.py
+++ vllm/engine/llm_engine.py
@@ -1963,6 +1963,8 @@ class LLMEngine:
                 raise ValueError(
                     "The prompt with multimodal tokens (total length "
                     f"{len(prompt_ids)}) is too long to fit into the model "
-                    f"(context length {max_prompt_len}). Make sure that"
-                    "`max_model_len` is no smaller than the size of the "
-                    "multimodal features.")
+                    f"(context length {max_prompt_len}). Make sure that "
+                    "`max_model_len` is no smaller than the number of "
+                    "multimodal tokens. For images, the number of multimodal "
+                    "tokens depends on the number of images passed to the same "
+                    "prompt, and possibly the aspect ratio of each image.")
