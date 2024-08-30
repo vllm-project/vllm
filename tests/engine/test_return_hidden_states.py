Fixed concatenation along wrong axis, made tests more precise

tests/engine/test_return_hidden_states.py
@@ -61,9 +61,11 @@ def test_return_hidden_states(model: str):
     # Ensure step 1 hidden states match numerically what we would expect.
     assert not torch.equal(request1_out.outputs[0].hidden_states,
                            request2_out.outputs[0].hidden_states)
-    request1_step1_hidden_states = request1_out.outputs[0].hidden_states.clone(
+    request1_hidden_states = (
+        request1_out.outputs[0].hidden_states.clone()
     )
-    request2_step1_hidden_states = request2_out.outputs[0].hidden_states.clone(
+    request2_hidden_states = (
+        request2_out.outputs[0].hidden_states.clone()
     )
 
     step2_out = engine.step()
@@ -77,6 +79,39 @@ def test_return_hidden_states(model: str):
             and request2_out.outputs[0].hidden_states is not None)
     # Ensure hidden states are being accumulated correctly.
     assert request1_out.outputs[0].hidden_states.shape[
-        0] > request1_step1_hidden_states.shape[0]
+        0] == 2
+    assert torch.equal(
+        request1_hidden_states, 
+        request1_out.outputs[0].hidden_states[:1])
     assert request2_out.outputs[0].hidden_states.shape[
-        0] > request2_step1_hidden_states.shape[0]
+        0] == 2 
+    assert torch.equal(
+        request2_hidden_states, 
+        request2_out.outputs[0].hidden_states[:1])
+    request1_hidden_states = (
+        request1_out.outputs[0].hidden_states.clone()
+    )
+    request2_hidden_states = (
+        request2_out.outputs[0].hidden_states.clone()
+    )
+    step3_out = engine.step()
+    assert isinstance(step3_out, list)
+    assert isinstance(step3_out[0], RequestOutput)
+    request1_out, request2_out = step3_out
+    assert (isinstance(request1_out, RequestOutput)
+            and isinstance(request2_out, RequestOutput))
+    # Ensure hidden states are being accumulated.
+    assert (request1_out.outputs[0].hidden_states is not None
+            and request2_out.outputs[0].hidden_states is not None)
+    # Ensure hidden states are being accumulated correctly.
+    assert request1_out.outputs[0].hidden_states.shape[
+        0] == 3
+    assert torch.equal(
+            request1_hidden_states, 
+            request1_out.outputs[0].hidden_states[:2])
+    assert request2_out.outputs[0].hidden_states.shape[
+        0] == 3
+    assert torch.equal(
+            request2_hidden_states, 
+            request2_out.outputs[0].hidden_states[:2])
+

vllm/engine/output_processor/single_step.py
@@ -69,9 +69,10 @@ class SingleStepOutputProcessor(SequenceGroupOutputProcessor):
             seq_group.prompt_hidden_states = output.prompt_hidden_states
         # Each output contains the hidden state from the last decode step,
         # so they need to be accumulated on the sequence.
-        seq.hidden_states = (torch.cat([
-            seq.hidden_states, output.hidden_state
-        ]) if seq.hidden_states is not None else output.hidden_state)
+        seq.hidden_states = (torch.cat(
+            [seq.hidden_states, output.hidden_state.unsqueeze(0)]) 
+            if seq.hidden_states is not None 
+            else output.hidden_state.unsqueeze(0))
 
     def process_prompt_logprob(self, seq_group: SequenceGroup,
                                outputs: List[SequenceGroupOutput]) -> None: