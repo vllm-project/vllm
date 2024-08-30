042afc564acad60236892c5903ceeb2a43273b81
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2024-08-30 09:01:26 -0700

    [TPU][Bugfix] Fix tpu type api (#8035)
    Signed-off-by: Alvant <alvasian@yandex.ru>

--- vllm/attention/backends/pallas.py
+++ vllm/attention/backends/pallas.py
@@ -124,7 +124,10 @@ class PallasAttentionBackendImpl(AttentionImpl):
 
         self.megacore_mode = None
         tpu_env = torch_xla.tpu.get_tpu_env()
-        tpu_type = tpu_env.get("TYPE") or tpu_env.get("ACCELERATOR_TYPE")
+        tpu_type = (tpu_env.get("ACCELERATOR_TYPE", None)
+                    or tpu_env.get("TYPE", None)
+                    or tpu_env.get("TPU_ACCELERATOR_TYPE", None))
+        assert tpu_type is not None
         tpu_type = tpu_type.lower()
 
         if "lite" not in tpu_type:
