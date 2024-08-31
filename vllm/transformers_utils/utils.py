fb756c8be49695f000896756dddef97e52973a33
Isotr0py <2037008807@qq.com>
2024-08-31 19:05:58 +0800

    accept gguf model without .gguf extension

---
+++ vllm/transformers_utils/utils.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+def check_gguf_file(model: Path) -> bool:
+    """
+    Check if the file is a GGUF model and extract the file name
+    """
+    if model.is_file():
+        with open(model, "rb") as f:
+            header = f.read(4)
+        return header == b"GGUF"
+    return False
