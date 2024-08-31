bfcf66069d62014e78602da4fea885a2376b9859
Dipika <dipikasikka1@gmail.com>
2024-08-31 00:57:57 +0000

    update

--- vllm/model_executor/layers/quantization/awq.py
+++ vllm/model_executor/layers/quantization/awq.py
@@ -1,13 +1,14 @@
-from typing import Any, Dict, List, Optional
+from typing import Callable, Any, Dict, List, Optional
 
 import torch
 
+from torch.nn import Parameter
 from vllm import _custom_ops as ops
 from vllm.model_executor.layers.fused_moe import FusedMoE, FusedMoEMethodBase
 from vllm.model_executor.layers.linear import LinearBase, LinearMethodBase
 from vllm.model_executor.layers.quantization.base_config import (
-    QuantizationConfi, QuantizeMethodBase)
-from vllm.model_executor.layerds
+    QuantizationConfig, QuantizeMethodBase)
+from vllm.model_executor.utils import set_weight_attrs
 from vllm.model_executor.parameter import (GroupQuantScaleParameter,
                                            PackedvLLMParameter)
 
@@ -175,11 +176,15 @@ class AWQLinearMethod(LinearMethodBase):
             out.add_(bias)
         return out.reshape(out_shape)
 
+
 class AWQMoEMethod(FusedMoEMethodBase):
+
     def __init__(self, quant_config: AWQConfig):
         self.quant_config = quant_config
 
-    def create_weights(self):
+    def create_weights(self, layer: torch.nn.Module, num_experts: int,
+                       hidden_size: int, intermediate_size: int,
+                       params_dtype: torch.dtype, **extra_weight_attrs):
         extra_weight_attrs.update({
             "is_transposed": True,
             "quant_method": "group",
@@ -192,12 +197,7 @@ class AWQMoEMethod(FusedMoEMethodBase):
                                             dtype=torch.int32),
                                 requires_grad=False)
         layer.register_parameter("w13_qweight", w13_qweight)
-        set_weight_attrs(
-            w13_qweight, {
-                "packed_dim": 1,
-                "pack_factor": self.quant_config.pack_factor,
-                **extra_weight_attrs
-            })
+        set_weight_attrs(w13_qweight, extra_weight_attrs)
 
         w2_qweight = Parameter(torch.empty(num_experts,
                                            intermediate_size,
@@ -206,16 +206,11 @@ class AWQMoEMethod(FusedMoEMethodBase):
                                            dtype=torch.int32),
                                requires_grad=False)
         layer.register_parameter("w2_qweight", w2_qweight)
-        set_weight_attrs(
-            w2_qweight, {
-                "packed_dim": 1,
-                "pack_factor": self.quant_config.pack_factor,
-                **extra_weight_attrs
-            })
+        set_weight_attrs(w2_qweight, extra_weight_attrs)
 
-        num_groups_w13 = hidden_size // self.group_size
-        num_groups_w2 = intermediate_size // self.group_size
-    
+        num_groups_w13 = hidden_size // self.quant_config.group_size
+        num_groups_w2 = intermediate_size // self.quant_config.group_size
+
         # WEIGHT_SCALES
         # Allocate 2 scales for w1 and w3 respectively.
         w13_scales = Parameter(torch.empty(num_experts,
@@ -236,30 +231,57 @@ class AWQMoEMethod(FusedMoEMethodBase):
 
         # WEIGHT_ZERO_POINT
         # Allocate 2 zero points for w1 and w3 respectively.
-        w13_qzeros = Parameter(torch.empty(
-            num_experts,
-            num_groups_w13,
-            2 * intermediate_size // self.quant_config.pack_factor,
-            dtype=torch.int32),
+        w13_qzeros = Parameter(torch.empty(num_experts,
+                                           num_groups_w13,
+                                           2 * intermediate_size //
+                                           self.quant_config.pack_factor,
+                                           dtype=torch.int32),
                                requires_grad=False)
         layer.register_parameter("w13_qzeros", w13_qzeros)
-        set_weight_attrs(
-            w13_qzeros, {
-                "packed_dim": 1,
-                "pack_factor": self.quant_config.pack_factor,
-                **extra_weight_attrs
-            })
+        set_weight_attrs(w13_qzeros, extra_weight_attrs)
 
-        w2_qzeros = Parameter(torch.empty(
-            num_experts,
-            num_groups_w2,
-            hidden_size // self.quant_config.pack_factor,
-            dtype=torch.int32),
+        w2_qzeros = Parameter(torch.empty(num_experts,
+                                          num_groups_w2,
+                                          hidden_size //
+                                          self.quant_config.pack_factor,
+                                          dtype=torch.int32),
                               requires_grad=False)
         layer.register_parameter("w2_qzeros", w2_qzeros)
-        set_weight_attrs(
-            w2_qzeros, {
-                "packed_dim": 1,
-                "pack_factor": self.quant_config.pack_factor,
-                **extra_weight_attrs
-            })
+        set_weight_attrs(w2_qzeros, extra_weight_attrs)
+
+    def apply(
+        self,
+        layer: torch.nn.Module,
+        x: torch.Tensor,
+        router_logits: torch.Tensor,
+        top_k: int,
+        renormalize: bool = True,
+        use_grouped_topk: bool = False,
+        num_expert_group: Optional[int] = None,
+        topk_group: Optional[int] = None,
+        custom_routing_function: Optional[Callable] = None,
+    ) -> torch.Tensor:
+
+        from vllm.model_executor.layers.fused_moe.fused_moe_awq import fused_experts_awq
+
+        topk_weights, topk_ids = FusedMoE.select_experts(
+            hidden_states=x,
+            router_logits=router_logits,
+            use_grouped_topk=use_grouped_topk,
+            top_k=top_k,
+            renormalize=renormalize,
+            topk_group=topk_group,
+            num_expert_group=num_expert_group,
+            custom_routing_function=custom_routing_function)
+
+        print("running fused")
+        return fused_experts_awq(hidden_states=x,
+                                 w1=layer.w13_qweight,
+                                 w2=layer.w2_qweight,
+                                 w1_scales=layer.w13_scales,
+                                 w2_scales=layer.w2_scales,
+                                 w1_qzeros=layer.w13_qzeros,
+                                 w2_qzeros=layer.w2_qzeros,
+                                 topk_weights=topk_weights,
+                                 topk_ids=topk_ids,
+                                 pack_factor=self.quant_config.pack_factor)
