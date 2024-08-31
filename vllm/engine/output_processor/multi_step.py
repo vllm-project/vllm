8a448d9e070ca28e90ee3971677bbaecbeb2af28
Robert Shaw <114415538+robertgshaw2-neuralmagic@users.noreply.github.com>
2024-08-31 15:44:03 -0400

    [BugFix][Core] Multistep Fix Crash on Request Cancellation (#8059)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/engine/output_processor/multi_step.py
+++ vllm/engine/output_processor/multi_step.py
@@ -88,9 +88,15 @@ class MultiStepOutputProcessor(SequenceGroupOutputProcessor):
         # TODO: Add support for async if necessary
         assert not is_async
 
+        # Sequences can be in RUNNING or FINISHED_ABORTED state
+        # once scheduled, as a sequence is moved to FINSIHED_ABORTED
+        # if a client disconnects from the api server.
         seqs = sequence_group.get_seqs(status=SequenceStatus.RUNNING)
+        if seqs is None:
+            seqs = sequence_group.get_seqs(
+                status=SequenceStatus.FINISHED_ABORTED)
 
-        assert seqs, "expected running sequences"
+        assert seqs, "Expected RUNNING or FINISHED_ABORTED sequences"
         assert len(seqs) == 1, (
             "Beam search not supported in multi-step decoding.")
         seq = seqs[0]
