a17a28bbf6c36af341b32465146b0d7a57b386c6
Isotr0py <2037008807@qq.com>
2024-08-31 21:57:35 +0800

    add optimisation as suggest

--- vllm/transformers_utils/utils.py
+++ vllm/transformers_utils/utils.py
@@ -6,7 +6,9 @@ from typing import Union
 def check_gguf_file(model: Union[str, PathLike]) -> bool:
     """Check if the file is a GGUF model."""
     model = Path(model)
-    if model.is_file():
+    if model.is_file() and model.suffix == ".gguf":
+        return True
+    elif model.is_file():
         with open(model, "rb") as f:
             header = f.read(4)
         return header == b"GGUF"
