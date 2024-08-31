8d73c77b10573abb663205e836d955bccdd68fbf
Isotr0py <2037008807@qq.com>
2024-08-31 19:09:24 +0800

    code format

--- vllm/transformers_utils/utils.py
+++ vllm/transformers_utils/utils.py
@@ -1,9 +1,8 @@
 from pathlib import Path
 
+
 def check_gguf_file(model: Path) -> bool:
-    """
-    Check if the file is a GGUF model and extract the file name
-    """
+    """Check if the file is a GGUF model."""
     if model.is_file():
         with open(model, "rb") as f:
             header = f.read(4)
