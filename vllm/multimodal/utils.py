f091380dec215c412b426a87b2cb1af83dcfce06
yangshen <yangshen.d@outlook.com>
2024-09-01 16:45:17 +0000

    Fix formatting

--- vllm/multimodal/utils.py
+++ vllm/multimodal/utils.py
@@ -187,6 +187,7 @@ def rescale_image_size(image: Image.Image,
         image = image.transpose(Image.Transpose(transpose))
     return image
 
+
 def try_import_video_packages() -> Any:
     try:
         import cv2
@@ -195,9 +196,10 @@ def try_import_video_packages() -> Any:
             "Please install vllm[video] for video support.") from None
     return cv2
 
+
 def resize_video(frames: npt.NDArray, size: Tuple[int, int]) -> npt.NDArray:
     cv2 = try_import_video_packages()
-    
+
     num_frames, _, _, channels = frames.shape
     new_height, new_width = size
     resized_frames = np.empty((num_frames, new_height, new_width, channels),
