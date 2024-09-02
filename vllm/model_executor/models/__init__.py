b21f5757e1681e8d38c4f197688d7ed78753366d
Shawn Tan <shawn@wtf.sg>
2024-09-01 21:37:18 -0400

    [Model] Add Granite model (#7436)
    Co-authored-by: Nick Hill <nickhill@us.ibm.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/model_executor/models/__init__.py
+++ vllm/model_executor/models/__init__.py
@@ -65,6 +65,7 @@ _GENERATION_MODELS = {
     "EAGLEModel": ("eagle", "EAGLE"),
     "MLPSpeculatorPreTrainedModel": ("mlp_speculator", "MLPSpeculator"),
     "JambaForCausalLM": ("jamba", "JambaForCausalLM"),
+    "GraniteForCausalLM": ("granite", "GraniteForCausalLM")
 }
 
 _EMBEDDING_MODELS = {
