d667d13da052d612caf3d1380a85dfa29a538db5
Isotr0py <2037008807@qq.com>
2024-09-02 20:43:26 +0800

    [Core][Bugfix] Accept GGUF model without .gguf extension (#8056)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

---
+++ vllm/transformers_utils/utils.py
@@ -0,0 +1,16 @@
+from os import PathLike
+from pathlib import Path
+from typing import Union
+
+
+def check_gguf_file(model: Union[str, PathLike]) -> bool:
+    """Check if the file is a GGUF model."""
+    model = Path(model)
+    if not model.is_file():
+        return False
+    elif model.suffix == ".gguf":
+        return True
+
+    with open(model, "rb") as f:
+        header = f.read(4)
+    return header == b"GGUF"
