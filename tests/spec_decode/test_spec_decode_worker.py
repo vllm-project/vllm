54ecae9a5c63ed6eb5a20d206dca2e4893c45f51
Lily Liu <lilyliupku@gmail.com>
2024-09-01 21:23:29 -0700

    [SpecDecode][Kernel] Flashinfer Rejection Sampling (#7244)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- tests/spec_decode/test_spec_decode_worker.py
+++ tests/spec_decode/test_spec_decode_worker.py
@@ -230,9 +230,8 @@ def test_correctly_calls_spec_decode_sampler(k: int, batch_size: int,
 
     assert torch.equal(actual.bonus_token_ids,
                        target_token_ids.reshape(batch_size, k + 1)[:, -1:])
-    assert torch.equal(
-        actual.target_probs,
-        target_token_probs.reshape(batch_size, k + 1, -1)[:, :-1])
+    assert torch.equal(actual.target_with_bonus_probs,
+                       target_token_probs.reshape(batch_size, k + 1, -1))
     assert torch.equal(actual.draft_token_ids, proposal_token_ids)
     assert torch.equal(actual.draft_probs, proposal_probs)
 
