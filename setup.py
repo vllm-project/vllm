dd4a388affcbc278070262194cb1f6d1e0aaffc3
Tomer Asida <tomera@ai21.com>
2024-09-03 21:39:53 +0300

    make the code more explicit

--- setup.py
+++ setup.py
@@ -362,7 +362,7 @@ def get_vllm_version() -> str:
     version = find_version(get_path("vllm", "version.py"))
 
     if _no_device():
-        if VLLM_TARGET_DEVICE == envs.VLLM_TARGET_DEVICE:
+        if envs.VLLM_TARGET_DEVICE == "empty":
             version += "+empty"
     elif _is_cuda():
         cuda_version = str(get_nvcc_cuda_version())
