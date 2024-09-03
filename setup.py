534e75c1193165605aee6d741deb4a1536c58ff1
Tomer Asida <tomera@ai21.com>
2024-09-03 16:46:16 +0300

    fix: Add the +empty tag to the version only when the VLLM_TARGET_DEVICE envvar was explicitly set to "empty"

--- setup.py
+++ setup.py
@@ -362,7 +362,8 @@ def get_vllm_version() -> str:
     version = find_version(get_path("vllm", "version.py"))
 
     if _no_device():
-        version += "+empty"
+        if VLLM_TARGET_DEVICE == envs.VLLM_TARGET_DEVICE:
+            version += "+empty"
     elif _is_cuda():
         cuda_version = str(get_nvcc_cuda_version())
         if cuda_version != MAIN_CUDA_VERSION:
