25af32f4497703affed96c45efac04347cd14f2b
Philippe Lelievre <philippe.lelievre@unity3d.com>
2024-09-04 16:05:04 -0400

    fix: lru cache outlines guide

--- vllm/model_executor/guided_decoding/outlines_logits_processors.py
+++ vllm/model_executor/guided_decoding/outlines_logits_processors.py
@@ -87,6 +87,7 @@ class BaseLogitsProcessor:
 class RegexLogitsProcessor(BaseLogitsProcessor):
 
     @classmethod
+    @lru_cache(maxsize=32)
     @cache()
     def _get_guide(cls, regex_string: str,
                    tokenizer: PreTrainedTokenizerBase) -> Guide:
@@ -146,6 +147,7 @@ class JSONLogitsProcessor(RegexLogitsProcessor):
 class CFGLogitsProcessor(BaseLogitsProcessor):
 
     @classmethod
+    @lru_cache(maxsize=32)
     @cache()
     def _get_guide(cls, cfg: str, tokenizer: PreTrainedTokenizerBase) -> Guide:
         tokenizer = _adapt_tokenizer(tokenizer)
