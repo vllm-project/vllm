039123f53b2c76969d725f9dff5041e925bd52ca
Dipika <dipikasikka1@gmail.com>
2024-09-06 16:29:39 +0000

    remove squeezellm from docs; remove gemms

--- vllm/entrypoints/llm.py
+++ vllm/entrypoints/llm.py
@@ -55,7 +55,7 @@ class LLM:
             However, if the `torch_dtype` in the config is `float32`, we will
             use `float16` instead.
         quantization: The method used to quantize the model weights. Currently,
-            we support "awq", "gptq", "squeezellm", and "fp8" (experimental).
+            we support "awq", "gptq", and "fp8" (experimental).
             If None, we first check the `quantization_config` attribute in the
             model config file. If that is None, we assume the model weights are
             not quantized and use `dtype` to determine the data type of
