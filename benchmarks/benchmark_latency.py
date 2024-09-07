7d0f0e76357111fe729ecd025197b2ff239f38de
s.m.kochetkov <s.m.kochetkov@tcsbank.com>
2024-09-07 12:29:10 +0000

    benchmark_latency
    Signed-off-by: s.kochetkov <s.m.kochetkov@tcsbank.ru>

--- benchmarks/benchmark_latency.py
+++ benchmarks/benchmark_latency.py
@@ -14,16 +14,54 @@ from vllm import LLM, SamplingParams
 from vllm.engine.arg_utils import EngineArgs
 from vllm.inputs import PromptType
 from vllm.utils import FlexibleArgumentParser
+from vllm.lora.request import LoRARequest
+from vllm.model_executor.layers.quantization import QUANTIZATION_METHODS
 
 
 def main(args: argparse.Namespace):
-    print(args)
-
+    
     engine_args = EngineArgs.from_cli_args(args)
 
     # NOTE(woosuk): If the request cannot be processed in a single batch,
     # the engine will automatically process the request in multiple batches.
     llm = LLM(**dataclasses.asdict(engine_args))
+    lora_kwargs={}
+    if args.lora_path is not None:
+        lora_kwargs['enable_lora']=True
+        lora_kwargs['max_loras']=args.max_loras
+        lora_kwargs['max_lora_rank']=args.max_lora_rank
+
+    # NOTE(woosuk): If the request cannot be processed in a single batch,
+    # the engine will automatically process the request in multiple batches.
+    
+    llm = LLM(
+        model=args.model,
+        speculative_model=args.speculative_model,
+        num_speculative_tokens=args.num_speculative_tokens,
+        speculative_draft_tensor_parallel_size=\
+            args.speculative_draft_tensor_parallel_size,
+        tokenizer=args.tokenizer,
+        quantization=args.quantization,
+        tensor_parallel_size=args.tensor_parallel_size,
+        trust_remote_code=args.trust_remote_code,
+        dtype=args.dtype,
+        max_model_len=args.max_model_len,
+        enforce_eager=args.enforce_eager,
+        kv_cache_dtype=args.kv_cache_dtype,
+        quantization_param_path=args.quantization_param_path,
+        device=args.device,
+        ray_workers_use_nsight=args.ray_workers_use_nsight,
+        use_v2_block_manager=args.use_v2_block_manager,
+        enable_chunked_prefill=args.enable_chunked_prefill,
+        download_dir=args.download_dir,
+        block_size=args.block_size,
+        gpu_memory_utilization=args.gpu_memory_utilization,
+        load_format=args.load_format,
+        distributed_executor_backend=args.distributed_executor_backend,
+        otlp_traces_endpoint=args.otlp_traces_endpoint,
+        enable_prefix_caching=args.enable_prefix_caching,
+        **lora_kwargs
+    )
 
     sampling_params = SamplingParams(
         n=args.n,
@@ -32,7 +70,9 @@ def main(args: argparse.Namespace):
         ignore_eos=True,
         max_tokens=args.output_len,
     )
-    print(sampling_params)
+    
+    lora_request=LoRARequest(lora_name='lora', lora_int_id=0, lora_path=args.lora_path) if args.lora_path else None
+
     dummy_prompt_token_ids = np.random.randint(10000,
                                                size=(args.batch_size,
                                                      args.input_len))
@@ -51,7 +91,8 @@ def main(args: argparse.Namespace):
                         str(profile_dir))) as p:
                 llm.generate(dummy_prompts,
                              sampling_params=sampling_params,
-                             use_tqdm=False)
+                             use_tqdm=False,
+                             lora_request=lora_request)
             print(p.key_averages())
         else:
             start_time = time.perf_counter()
@@ -102,6 +143,22 @@ if __name__ == '__main__':
     parser = FlexibleArgumentParser(
         description='Benchmark the latency of processing a single batch of '
         'requests till completion.')
+    parser.add_argument('--model', type=str, default='facebook/opt-125m')
+    parser.add_argument('--lora-path', type=str, default=None)
+    parser.add_argument('--max-loras', type=int, default=4)
+    parser.add_argument('--max-lora-rank', type=int, default=64)
+    parser.add_argument('--speculative-model', type=str, default=None)
+    parser.add_argument('--num-speculative-tokens', type=int, default=None)
+    parser.add_argument('--speculative-draft-tensor-parallel-size',
+                        '-spec-draft-tp',
+                        type=int,
+                        default=None)
+    parser.add_argument('--tokenizer', type=str, default=None)
+    parser.add_argument('--quantization',
+                        '-q',
+                        choices=[*QUANTIZATION_METHODS, None],
+                        default=None)
+    parser.add_argument('--tensor-parallel-size', '-tp', type=int, default=1)
     parser.add_argument('--input-len', type=int, default=32)
     parser.add_argument('--output-len', type=int, default=128)
     parser.add_argument('--batch-size', type=int, default=8)
