982f9116104ab3cb7ec696b9b6569f9a9f55fccd
harrywu <904714159@qq.com>
2024-09-08 19:00:44 +0800

    first_scheduled_time can be None

--- vllm/engine/llm_engine.py
+++ vllm/engine/llm_engine.py
@@ -1875,16 +1875,18 @@ class LLMEngine:
                     # Latency timings
                     time_e2e_requests.append(now -
                                              seq_group.metrics.arrival_time)
-                    time_queue_requests.append(
-                        seq_group.metrics.first_scheduled_time -
-                        seq_group.metrics.arrival_time)
-                    time_prefill_requests.append(
-                        seq_group.metrics.first_token_time -
-                        seq_group.metrics.first_scheduled_time)
-                    time_decode_requests.append(
-                        now - seq_group.metrics.first_token_time)
-                    time_inference_requests.append(
-                        now - seq_group.metrics.first_scheduled_time)
+                    if (seq_group.metrics.first_scheduled_time is not None and
+                            seq_group.metrics.first_token_time is not None):
+                        time_queue_requests.append(
+                            seq_group.metrics.first_scheduled_time -
+                            seq_group.metrics.arrival_time)
+                        time_prefill_requests.append(
+                            seq_group.metrics.first_token_time -
+                            seq_group.metrics.first_scheduled_time)
+                        time_decode_requests.append(
+                            now - seq_group.metrics.first_token_time)
+                        time_inference_requests.append(
+                            now - seq_group.metrics.first_scheduled_time)
                     # Metadata
                     num_prompt_tokens_requests.append(
                         len(seq_group.prompt_token_ids))
