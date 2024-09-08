dca877ab7def76ada27324d4729eb8a3832a24e8
KuntaiDu <kuntai@uchicago.edu>
2024-09-08 08:52:07 +0000

    update benchmark: use small model for quick iteration

--- benchmarks/disagg_benchmarks/disagg_overhead_benchmark.sh
+++ benchmarks/disagg_benchmarks/disagg_overhead_benchmark.sh
@@ -50,29 +50,44 @@ benchmark() {
   # compare chunked prefill with disaggregated prefill
 
   results_folder="./results"
-  model="meta-llama/Meta-Llama-3.1-70B-Instruct"
+  model="meta-llama/Meta-Llama-3.1-8B-Instruct"
   dataset_name="sonnet"
   dataset_path="../sonnet_4x.txt"
-  num_prompts=50
+  num_prompts=20
   qps=$1
   prefix_len=50
   input_len=2048
   output_len=$2
 
   # large model
-  VLLM_RPC_PORT=5570 VLLM_DISAGG_PREFILL_ROLE=prefill CUDA_VISIBLE_DEVICES=0,1,2,3 python3 \
-      -m vllm.entrypoints.openai.api_server \
-      --model $model \
-      --port 8100 \
-      -tp 4 \
-      --max-model-len 30000 \
-      --gpu-memory-utilization 0.8 &
-  VLLM_RPC_PORT=5580 VLLM_DISAGG_PREFILL_ROLE=decode CUDA_VISIBLE_DEVICES=4,5,6,7 python3 \
+  # VLLM_RPC_PORT=5570 VLLM_DISAGG_PREFILL_ROLE=prefill CUDA_VISIBLE_DEVICES=0,1,2,3 python3 \
+  #     -m vllm.entrypoints.openai.api_server \
+  #     --model $model \
+  #     --port 8100 \
+  #     -tp 4 \
+  #     --max-model-len 30000 \
+  #     --gpu-memory-utilization 0.8 &
+  # VLLM_RPC_PORT=5580 VLLM_DISAGG_PREFILL_ROLE=decode CUDA_VISIBLE_DEVICES=4,5,6,7 python3 \
+  #   -m vllm.entrypoints.openai.api_server \
+  #   --model $model \
+  #   --port 8200 \
+  #   -tp 4 \
+  #   --max-model-len 30000 \
+  #   --gpu-memory-utilization 0.8 &
+
+  VLLM_RPC_PORT=5570 VLLM_DISAGG_PREFILL_ROLE=prefill CUDA_VISIBLE_DEVICES=0 python3 \
     -m vllm.entrypoints.openai.api_server \
-    --model $model \
+    --model meta-llama/Meta-Llama-3.1-8B-Instruct \
+    --port 8100 \
+    --max-model-len 10000 \
+    --gpu-memory-utilization 0.8 &
+
+# decoding instance
+VLLM_RPC_PORT=5580 VLLM_DISAGG_PREFILL_ROLE=decode CUDA_VISIBLE_DEVICES=1 python3 \
+    -m vllm.entrypoints.openai.api_server \
+    --model meta-llama/Meta-Llama-3.1-8B-Instruct \
     --port 8200 \
-    -tp 4 \
-    --max-model-len 30000 \
+    --max-model-len 10000 \
     --gpu-memory-utilization 0.8 &
 
   wait_for_server 8100
@@ -92,7 +107,7 @@ benchmark() {
           --save-result \
           --result-dir $results_folder \
           --result-filename disagg_prefill_2xtp4.json \
-          --request-rate $qps
+          --request-rate "inf"
 
 
   # send the request to decode.
