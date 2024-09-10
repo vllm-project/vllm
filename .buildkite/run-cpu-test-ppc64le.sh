2a857066e69fdd25c0afe8bb34339aca52430c5b
sumitd2 <91451282+sumitd2@users.noreply.github.com>
2024-09-10 13:54:47 +0530

    Update run-cpu-test-ppc64le.sh

--- .buildkite/run-cpu-test-ppc64le.sh
+++ .buildkite/run-cpu-test-ppc64le.sh
@@ -12,7 +12,7 @@ remove_docker_container
 
 # Run the image, setting --shm-size=4g for tensor parallel.
 #docker run -itd --entrypoint /bin/bash -v ~/.cache/huggingface:/root/.cache/huggingface --privileged=true --network host -e HF_TOKEN --env VLLM_CPU_KVCACHE_SPACE=4 --shm-size=4g --name cpu-test cpu-test
-docker run -itd --entrypoint /bin/bash -v ~/.cache/huggingface:/root/.cache/huggingface --privileged=true --network host -e HF_TOKEN --name cpu-test cpu-test
+docker run -itd --entrypoint /bin/bash -v ~/.cache/huggingface:/root/.cache/huggingface --privileged=true --network host -e HF_TOKEN=$HF_TOKEN --name cpu-test cpu-test
 
 # Run basic model test
 docker exec cpu-test bash -c "
