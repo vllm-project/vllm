f51e27fa40a36f21b9fafeab3d5890157d44c9db
Alexander Matveev <alexm@neuralmagic.com>
2024-09-10 17:30:54 +0000

    format

--- examples/offline_inference.py
+++ examples/offline_inference.py
@@ -11,9 +11,7 @@ prompts = [
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=8)
 
 # Create an LLM.
-llm = LLM(model="facebook/opt-125m",
-          max_seq_len_to_capture=15,
-          num_scheduler_steps=64)
+llm = LLM(model="facebook/opt-125m")
 # Generate texts from the prompts. The output is a list of RequestOutput objects
 # that contain the prompt, generated text, and other information.
 outputs = llm.generate(prompts, sampling_params)
