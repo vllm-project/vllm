a38ff718c6b25b7af3f6e56643315a5b55f92f6d
Alexander Matveev <alexm@neuralmagic.com>
2024-09-10 17:33:50 +0000

    format

--- examples/offline_inference.py
+++ examples/offline_inference.py
@@ -8,7 +8,7 @@ prompts = [
     "The future of AI is",
 ]
 # Create a sampling params object.
-sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=8)
+sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
 # Create an LLM.
 llm = LLM(model="facebook/opt-125m")
