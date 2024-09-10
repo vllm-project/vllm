b9bcec1e4055ef59e7193467534cad7d2eb4954d
Prashant Gupta <prashantgupta@us.ibm.com>
2024-09-10 14:21:56 -0700

    [Misc] remove peft as dependency for prompt models (#8162)
    Signed-off-by: Alvant <alvasian@yandex.ru>

--- vllm/config.py
+++ vllm/config.py
@@ -1558,14 +1558,6 @@ class PromptAdapterConfig:
     prompt_adapter_dtype: Optional[torch.dtype] = None
 
     def __post_init__(self):
-        library_name = 'peft'
-        try:
-            __import__(library_name)
-        except ImportError as e:
-            raise ImportError(
-                f"'{library_name}' is not installed for prompt adapter support."
-                f"Please install it using 'pip install {library_name}'."
-            ) from e
 
         if self.max_prompt_adapters < 1:
             raise ValueError(f"max_prompt_adapters "
