75c6157ed68dc3d23a4540bf64d0413f0e8e9ff3
rshaw@neuralmagic.com <rshaw@neuralmagic.com>
2024-09-10 16:20:11 +0000

    fixed magicmock errored

--- vllm/entrypoints/openai/serving_chat.py
+++ vllm/entrypoints/openai/serving_chat.py
@@ -109,7 +109,7 @@ class OpenAIServingChat(OpenAIServing):
         # success status before we actually start generating text :).
         if self.engine_client.errored:
             raise self.engine_client.dead_error
-
+        
         try:
             (
                 lora_request,
@@ -207,6 +207,7 @@ class OpenAIServingChat(OpenAIServing):
                     and contains_trace_headers(raw_request.headers)):
                 log_tracing_disabled_warning()
 
+            print("calling generate")
             result_generator = self.engine_client.generate(
                 engine_inputs,
                 sampling_params,
