4052bdb728ba3bbddca82af1a71574c8db706179
Konrad Zawora <kzawora@habana.ai>
2024-09-10 15:04:34 +0200

    Add disable_tensor_cache=True to HPUGraph capture (#252)
    RuntimeErrors are not observed anymore on habana_main when
    disable_tensor_cache is used. This PR enables disable_tensor_cache.

--- vllm/worker/habana_model_runner.py
+++ vllm/worker/habana_model_runner.py
@@ -576,8 +576,6 @@ class HabanaModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
                 htcore.mark_step()
             torch.hpu.synchronize()
 
-            # FIXME: Running with disable_tensor_cache=True causes
-            # RuntimeErrors. This needs to be debugged
             with HabanaMemoryProfiler() as m_wrap:
                 self.model = _maybe_wrap_in_hpu_graph(
                     self.model,
@@ -1576,10 +1574,9 @@ class HabanaModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
 
 
 def _maybe_wrap_in_hpu_graph(*args, **kwargs):
-    return htorch.hpu.wrap_in_hpu_graph(HpuModelAdapter(
-        *args, **
-        kwargs)) if htorch.utils.internal.is_lazy() else HpuModelAdapter(
-            *args, **kwargs)
+    return htorch.hpu.wrap_in_hpu_graph(
+        HpuModelAdapter(*args, **kwargs), disable_tensor_cache=True
+    ) if htorch.utils.internal.is_lazy() else HpuModelAdapter(*args, **kwargs)
 
 
 class HabanaProfilerCounterHelper():
