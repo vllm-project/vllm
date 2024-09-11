282c66ae4e6808981374a58b839a61b7297fb13e
DarkLight1337 <tlleungac@connect.ust.hk>
2024-09-11 03:02:19 +0000

    format

--- tests/models/test_registry.py
+++ tests/models/test_registry.py
@@ -7,7 +7,7 @@ from vllm.model_executor.models import _MODELS, ModelRegistry
 @pytest.mark.parametrize("model_cls", _MODELS)
 def test_registry_imports(model_cls):
     if (model_cls == "Qwen2VLForConditionalGeneration"
-        and transformers.__version__ < "4.45"):
+            and transformers.__version__ < "4.45"):
         pytest.skip("Waiting for next transformers release")
 
     # Ensure all model classes can be imported successfully
