5cf1c75ebc5bbd008bf5cf0bd6ce7dca4b1398a9
Joe Shajrawi <17753158+shajrawi@users.noreply.github.com>
2024-09-11 15:50:21 -0500

    Support commandr on ROCm (#180)
    Disable torch compile

--- vllm/model_executor/models/commandr.py
+++ vllm/model_executor/models/commandr.py
@@ -50,7 +50,6 @@ from vllm.sequence import IntermediateTensors
 from .interfaces import SupportsLoRA
 
 
-@torch.compile
 def layer_norm_func(hidden_states, weight, variance_epsilon):
     input_dtype = hidden_states.dtype
     hidden_states = hidden_states.to(torch.float32)
