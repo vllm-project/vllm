4c537ad27d7ecf7af21cd3c7f9dbcbb6cd291a0b
Li, Jiang <jiang1.li@intel.com>
2024-09-12 00:46:46 +0800

    [Hardware][Intel] Support compressed-tensor W8A8 for CPU backend (#7257)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/model_executor/model_loader/loader.py
+++ vllm/model_executor/model_loader/loader.py
@@ -95,8 +95,9 @@ def _get_quantization_config(
     """Get the quantization config."""
     if model_config.quantization is not None:
         quant_config = get_quant_config(model_config, load_config)
-        if not current_platform.is_tpu():
-            capability = current_platform.get_device_capability()
+        capability = current_platform.get_device_capability()  # type: ignore
+
+        if capability is not None:
             capability = capability[0] * 10 + capability[1]
             if capability < quant_config.get_min_capability():
                 raise ValueError(
--- vllm/platforms/tpu.py
+++ vllm/platforms/tpu.py
@@ -1,5 +1,3 @@
-from typing import Tuple
-
 import torch
 
 from .interface import Platform, PlatformEnum
@@ -7,10 +5,6 @@ from .interface import Platform, PlatformEnum
 
 class TpuPlatform(Platform):
     _enum = PlatformEnum.TPU
-
-    @staticmethod
-    def get_device_capability(device_id: int = 0) -> Tuple[int, int]:
-        raise RuntimeError("TPU does not have device capability.")
 
     @staticmethod
     def inference_mode():
