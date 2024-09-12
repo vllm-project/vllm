b3b2136405d49f17cd4d498181716fc363430d03
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2024-09-11 20:31:51 -0700

    [TPU] Use Ray for default distributed backend (#8389)
    Signed-off-by: Alvant <alvasian@yandex.ru>

--- vllm/config.py
+++ vllm/config.py
@@ -869,6 +869,13 @@ class ParallelConfig:
                                  f"distributed executor backend "
                                  f"'{self.distributed_executor_backend}'.")
 
+        if current_platform.is_tpu() and self.world_size > 1:
+            if self.distributed_executor_backend is None:
+                self.distributed_executor_backend = "ray"
+            if self.distributed_executor_backend != "ray":
+                raise ValueError(
+                    "TPU backend only supports Ray for distributed inference.")
+
         if self.distributed_executor_backend is None and self.world_size > 1:
             # We use multiprocessing by default if world_size fits on the
             # current node and we aren't in a ray placement group.
