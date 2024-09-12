a51e4b289cd46a8300329a25b27201452da78521
Bill Nell <bill@neuralmagic.com>
2024-09-12 20:39:16 +0000

    fix

--- tests/compile/test_full_graph.py
+++ tests/compile/test_full_graph.py
@@ -1,33 +1,38 @@
 import os
 
 import pytest
+import torch
 
-from vllm.utils import cuda_device_count_stateless
+from vllm import LLM, SamplingParams
 
-from ..utils import fork_new_process_for_each_test
-
-TEST_MODELS=[
-    ("facebook/opt-125m", {})
-    ("nm-testing/tinyllama-oneshot-w8w8-test-static-shape-change", {"dtype":torch.float16, "quantization":"compressed-tensors"})
-    ("neuralmagic/Meta-Llama-3-8B-Instruct-FP8", {"dtype":torch.float16, "quantization":"fp8"})
-    ("nm-testing/Meta-Llama-3-8B-Instruct-W8A8-Dyn-Per-Token-2048-Samples", {"quantization":"compressed-tensors"})
-    ("meta-llama/Meta-Llama-3-8B", {})
+TEST_MODELS = [
+    ("facebook/opt-125m", {}),
+    ("nm-testing/tinyllama-oneshot-w8w8-test-static-shape-change", {
+        "dtype": torch.float16,
+        "quantization": "compressed-tensors"
+    }),
+    ("neuralmagic/Meta-Llama-3-8B-Instruct-FP8", {
+        "dtype": torch.float16,
+        "quantization": "fp8"
+    }),
+    ("nm-testing/Meta-Llama-3-8B-Instruct-W8A8-Dyn-Per-Token-2048-Samples", {
+        "quantization": "compressed-tensors"
+    }),
+    ("meta-llama/Meta-Llama-3-8B", {
+        "load_format": "dummy"
+    }),
 ]
 
-@pytest.mark.parametrize("model", TEST_MODELS)
-@pytest.mark.parametrize("tp_size", [1, 2])
-@fork_new_process_for_each_test
-def test_full_graph(model, tp_size):
 
-    # Skip the test if there are not enough CUDA devices.
-    if cuda_device_count_stateless() < tp_size:
-        pytest.skip("Not enough CUDA devices for the test.")
-
+@pytest.mark.parametrize("model_info", TEST_MODELS)
+def test_full_graph(model_info):
     # make sure these models can be captured in full graph mode
     if "VLLM_TEST_DYNAMO_GRAPH_CAPTURE" not in os.environ:
         os.environ["VLLM_TEST_DYNAMO_GRAPH_CAPTURE"] = "1"
 
-    from vllm import LLM, SamplingParams
+    model = model_info[0]
+    model_kwargs = model_info[1]
+
     prompts = [
         "Hello, my name is",
         "The president of the United States is",
@@ -35,15 +40,8 @@ def test_full_graph(model, tp_size):
         "The future of AI is",
     ]
     sampling_params = SamplingParams(temperature=0)
-    llm = LLM(model=model,
-              enforce_eager=True,
-              load_format="dummy",
-              *model_kwargs)
+    llm = LLM(model=model, enforce_eager=True, **model_kwargs)
+    llm.generate(prompts, sampling_params)
 
-    outputs = llm.generate(prompts, sampling_params)
-
-    # Print the outputs.
-    for output in outputs:
-        prompt = output.prompt
-        generated_text = output.outputs[0].text
-        print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+    # Runs insanely slow w/o this
+    torch._dynamo.reset()
