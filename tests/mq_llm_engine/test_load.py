a4997339690eeaa2a6ced575078803a872bbd23f
Alexander Matveev <alexm@neuralmagic.com>
2024-09-12 16:33:28 +0000

    format

--- tests/mq_llm_engine/test_load.py
+++ tests/mq_llm_engine/test_load.py
@@ -7,7 +7,6 @@ import uuid
 import pytest
 
 from tests.mq_llm_engine.utils import RemoteMQLLMEngine
-
 from vllm import SamplingParams
 from vllm.engine.arg_utils import AsyncEngineArgs
 from vllm.engine.multiprocessing.client import MQLLMEngineClient
@@ -20,26 +19,28 @@ NUM_REQUESTS = 10000
 # Scenarios to test for num generated token.
 ENGINE_ARGS = AsyncEngineArgs(model=MODEL, disable_log_requests=True)
 
+
 @pytest.fixture(scope="function")
 def tmp_socket():
     with tempfile.TemporaryDirectory() as td:
         yield f"ipc://{td}/{uuid.uuid4()}"
 
-async def run_to_completion(client: MQLLMEngineClient,
-                            request_id: str):
-    
+
+async def run_to_completion(client: MQLLMEngineClient, request_id: str):
+
     count = 0
     async for out in client.generate(inputs=PROMPT,
-                                    sampling_params=SamplingParams(
-                                        max_tokens=NUM_EXPECTED_TOKENS,
-                                        temperature=0),
-                                    request_id=request_id):
-        
+                                     sampling_params=SamplingParams(
+                                         max_tokens=NUM_EXPECTED_TOKENS,
+                                         temperature=0),
+                                     request_id=request_id):
+
         count += 1
         await asyncio.sleep(0.)
 
     # Confirm we generated all the tokens we expected.
     return count, request_id
+
 
 @pytest.mark.asyncio
 async def test_generation(tmp_socket):
@@ -53,16 +54,16 @@ async def test_generation(tmp_socket):
         # Create concurrent requests.
         tasks = []
         for request_id in request_ids:
-            tasks.append(asyncio.create_task(run_to_completion(client, 
-                                                               request_id)))
+            tasks.append(
+                asyncio.create_task(run_to_completion(client, request_id)))
 
         # Confirm that we got all the EXPECTED tokens from the requests.
         failed_request_id = None
         tokens = None
         for task in tasks:
             num_generated_tokens, request_id = await task
-            if (num_generated_tokens != NUM_EXPECTED_TOKENS and 
-                failed_request_id is None):
+            if (num_generated_tokens != NUM_EXPECTED_TOKENS
+                    and failed_request_id is None):
                 failed_request_id = request_id
                 tokens = num_generated_tokens
 
