f06e1b269b6a7814124cec5455c1a273cb361fd4
Wenxiang <8460860+wenxcs@users.noreply.github.com>
2024-09-13 07:25:00 +0800

    [Bugfix] Fix weight loading issue by rename variable. (#8293)
    Signed-off-by: Alvant <alvasian@yandex.ru>

--- vllm/model_executor/models/phimoe.py
+++ vllm/model_executor/models/phimoe.py
@@ -600,7 +600,7 @@ class PhiMoEForCausalLM(nn.Module, SupportsLoRA):
                     weight_loader(
                         param,
                         loaded_weight,
-                        weight_name,
+                        name,
                         shard_id=shard_id,
                         expert_id=expert_id,
                     )
