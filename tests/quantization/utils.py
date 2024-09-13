cac0430bebc83c7044a54a5cdeecd01cf116d030
youkaichao <youkaichao@gmail.com>
2024-09-13 02:20:14 -0700

    [misc][ci] fix quant test (#8449)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- tests/quantization/utils.py
+++ tests/quantization/utils.py
@@ -1,12 +1,10 @@
-import torch
-
 from vllm.model_executor.layers.quantization import QUANTIZATION_METHODS
 from vllm.platforms import current_platform
 
 
 def is_quant_method_supported(quant_method: str) -> bool:
     # Currently, all quantization methods require Nvidia or AMD GPUs
-    if not torch.cuda.is_available():
+    if not (current_platform.is_cuda() or current_platform.is_rocm()):
         return False
 
     capability = current_platform.get_device_capability()
