9f9c0799a135c4d1d15cec47c793d46ddb64efb0
isotr0py <2037008807@qq.com>
2024-09-14 13:15:33 +0800

    code format

--- tests/kernels/test_gguf.py
+++ tests/kernels/test_gguf.py
@@ -10,10 +10,13 @@ from huggingface_hub import snapshot_download
 import vllm._custom_ops as ops
 
 
-def get_gguf_sample_tensors(hidden_size:int, quant_type:GGMLQuantizationType)->List[ReaderTensor]:
+def get_gguf_sample_tensors(
+        hidden_size: int,
+        quant_type: GGMLQuantizationType) -> List[ReaderTensor]:
     # sample_dir = snapshot_download("Isotr0py/test-gguf-sample")
     sample_dir = "/mnt/g/libtorch_develop/ggml-scripts"
-    sample_file = Path(sample_dir) / f"Quant_{quant_type.name}_{hidden_size}.gguf"
+    filename = f"Quant_{quant_type.name}_{hidden_size}.gguf"
+    sample_file = Path(sample_dir) / filename
     return GGUFReader(sample_file).tensors
 
 
@@ -43,16 +46,16 @@ QUANT_TYPE = [
 @pytest.mark.parametrize("dtype", DTYPES)
 @pytest.mark.parametrize("quant_type", QUANT_TYPE)
 @torch.inference_mode()
-def test_dequantize(hidden_size: int,
-                    dtype: torch.dtype, quant_type: GGMLQuantizationType):
+def test_dequantize(hidden_size: int, dtype: torch.dtype,
+                    quant_type: GGMLQuantizationType):
     tensors = get_gguf_sample_tensors(hidden_size, quant_type)
     for tensor in tensors:
         shape_str = tensor.name.split("_")[-1]
         shape = map(int, shape_str.split("x"))
 
-        ref_output = torch.tensor(dequantize(tensor.data, quant_type), device="cuda").to(dtype)
-        output = ops.ggml_dequantize(torch.tensor(tensor.data, device="cuda"), quant_type, *list(shape)).to(dtype)
+        ref_output = torch.tensor(dequantize(tensor.data, quant_type),
+                                  device="cuda").to(dtype)
+        output = ops.ggml_dequantize(torch.tensor(tensor.data, device="cuda"),
+                                     quant_type, *list(shape)).to(dtype)
 
-        torch.testing.assert_close(output, ref_output, atol=1e-2, rtol=4e-2), \
-        f" Error: {torch.max(torch.abs(output - ref_output))}, Size: {shape_str}"
-    
+        torch.testing.assert_close(output, ref_output, atol=1e-2, rtol=4e-2)
