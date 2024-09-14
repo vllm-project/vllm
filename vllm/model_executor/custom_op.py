47790f3e328f1fbf250d8f858b6390496c1e61c0
youkaichao <youkaichao@gmail.com>
2024-09-14 13:07:16 -0700

    [torch.compile] add a flag to disable custom op (#8488)

--- vllm/model_executor/custom_op.py
+++ vllm/model_executor/custom_op.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 
+import vllm.envs as envs
 from vllm.platforms import current_platform
 from vllm.utils import is_cpu, is_hip, is_xpu
 
@@ -53,6 +54,10 @@ class CustomOp(nn.Module):
     def dispatch_forward(self):
         # NOTE(woosuk): Here we assume that vLLM was built for only one
         # specific backend. Currently, we do not support dynamic dispatching.
+
+        if envs.VLLM_TEST_COMPILE_NO_CUSTOM_OPS:
+            return self.forward_native
+
         if is_hip():
             return self.forward_hip
         elif is_cpu():
