784d9058d5a8f483024d23887942583bac2ff238
ApostaC <jc4xvyp@outlook.com>
2024-09-15 22:35:12 +0000

    update the example

--- examples/disagg_prefill/disagg_prefill_example.sh
+++ examples/disagg_prefill/disagg_prefill_example.sh
@@ -16,7 +16,7 @@ wait_for_server() {
 }
 
 # prefilling instance
-VLLM_RPC_PORT=5570 VLLM_DISAGG_PREFILL_ROLE=prefill CUDA_VISIBLE_DEVICES=0 python3 \
+VLLM_DISAGG_PREFILL_ROLE=prefill CUDA_VISIBLE_DEVICES=0 python3 \
     -m vllm.entrypoints.openai.api_server \
     --model meta-llama/Meta-Llama-3.1-8B-Instruct \
     --port 8100 \
@@ -24,7 +24,7 @@ VLLM_RPC_PORT=5570 VLLM_DISAGG_PREFILL_ROLE=prefill CUDA_VISIBLE_DEVICES=0 pytho
     --gpu-memory-utilization 0.8 &
 
 # decoding instance
-VLLM_RPC_PORT=5580 VLLM_DISAGG_PREFILL_ROLE=decode CUDA_VISIBLE_DEVICES=1 python3 \
+VLLM_DISAGG_PREFILL_ROLE=decode CUDA_VISIBLE_DEVICES=1 python3 \
     -m vllm.entrypoints.openai.api_server \
     --model meta-llama/Meta-Llama-3.1-8B-Instruct \
     --port 8200 \
@@ -36,18 +36,39 @@ wait_for_server 8100
 wait_for_server 8200
 
 # launch a proxy server that opens the service at port 8000
+# the workflow of this proxy:
+# - send the request to prefill vLLM instance (port 8100), change max_tokens to 1
+# - after the prefill vLLM finishes prefill, send the request to decode vLLM instance
 python3 ../../benchmarks/disagg_benchmarks/disagg_prefill_proxy_server.py &
 sleep 1
 
-# serve an example request
-curl http://localhost:8000/v1/completions \
+# serve two example requests
+output1=$(curl -s http://localhost:8000/v1/completions \
 -H "Content-Type: application/json" \
 -d '{
 "model": "meta-llama/Meta-Llama-3.1-8B-Instruct",
 "prompt": "San Francisco is a",
 "max_tokens": 10,
 "temperature": 0
-}'
+}')
 
-# clean up
-ps -e | grep pt_main_thread | awk '{print $1}' | xargs kill -9
+output2=$(curl -s http://localhost:8000/v1/completions \
+-H "Content-Type: application/json" \
+-d '{
+"model": "meta-llama/Meta-Llama-3.1-8B-Instruct",
+"prompt": "Santa Clara is a",
+"max_tokens": 10,
+"temperature": 0
+}')
+
+# Print the outputs of the curl requests
+echo ""
+echo "Output of first request: $output1"
+echo "Output of second request: $output2"
+
+echo "Successfully finished 2 test requests!"
+echo ""
+
+# Cleanup commands, suppressing their output
+ps -e | grep pt_main_thread | awk '{print $1}' | xargs kill -9 > /dev/null 2>&1
+pkill -f python3 > /dev/null 2>&1
