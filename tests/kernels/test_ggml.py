fe6a49da4404ee0c4a51d38713d47a4a0c57dd6f
Bill Nell <bill@neuralmagic.com>
2024-09-15 00:35:51 +0000

    add ggml_mul_mat opcheck tests

--- tests/kernels/test_ggml.py
+++ tests/kernels/test_ggml.py
@@ -3,10 +3,12 @@ import torch
 import gguf
 
 from tests.kernels.utils import opcheck
-from vllm import _custom_ops as ops
+from vllm import _custom_ops as ops  # noqa: F401
 
+
+# TODO: add more shapes?
 @pytest.mark.parametrize("quant_type", [12])
-def test_gglm_dequantize_opcheck(quant_type):
+def test_ggml_opcheck(quant_type):
     block_size, type_size = gguf.GGML_QUANT_SIZES[quant_type]
     shape = [256, 1152]
     qweight = torch.randint(0, 100, shape, device='cuda', dtype=torch.uint8)
@@ -14,3 +16,8 @@ def test_gglm_dequantize_opcheck(quant_type):
     n = qweight.shape[1] // type_size * block_size
     opcheck(torch.ops._C.ggml_dequantize, (qweight, quant_type, m, n))
 
+    x = torch.rand((m, 512), device='cuda', dtype=torch.float16)
+    opcheck(torch.ops._C.ggml_mul_mat_a8,
+            (qweight, x, quant_type, qweight.shape[0]))
+    opcheck(torch.ops._C.ggml_mul_mat_vec_a8,
+            (qweight, x, quant_type, qweight.shape[0]))
