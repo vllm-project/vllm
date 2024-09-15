9256f170504d77aa16702ff01a4f689cd607ae00
litianjian <litianjian@bytedance.com>
2024-09-15 07:12:40 +0000

    format codes

--- vllm/model_executor/models/llava_onevision.py
+++ vllm/model_executor/models/llava_onevision.py
@@ -863,12 +863,8 @@ class LlavaOnevisionForConditionalGeneration(nn.Module, SupportsMultiModal):
 
     def load_weights(self, weights: Iterable[Tuple[str, torch.Tensor]]):
         # prepare weight iterators
-        (
-            vit_weights,
-            mlp_weights,
-            newline_weights,
-            llm_weights
-        ) = itertools.tee(weights, 4)
+        (vit_weights, mlp_weights, newline_weights,
+         llm_weights) = itertools.tee(weights, 4)
         # load vision encoder
         vit_weights = filter_weights(vit_weights, "vision_tower")
         self.vision_tower.load_weights(vit_weights)
