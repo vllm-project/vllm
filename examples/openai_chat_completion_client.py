add68ee763c54fc325beca4e49fb2a3ed664756b
Robert Shaw <114415538+robertgshaw2-neuralmagic@users.noreply.github.com>
2024-09-17 08:22:56 -0400

    Update examples/openai_chat_completion_client.py
    Co-authored-by: Simon Mo <simon.mo@hey.com>

--- examples/openai_chat_completion_client.py
+++ examples/openai_chat_completion_client.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 
 # Modify OpenAI's API key and API base to use vLLM's API server.
 openai_api_key = "EMPTY"
-openai_api_base = "http://localhost:8001/v1"
+openai_api_base = "http://localhost:8000/v1"
 
 client = OpenAI(
     # defaults to os.environ.get("OPENAI_API_KEY")
