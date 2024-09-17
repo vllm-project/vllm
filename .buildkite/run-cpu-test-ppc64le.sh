a28c013fccb71338539cfb29e3dd765012eb2efc
Sumit Dubey <sumit.dubey2@ibm.com>
2024-09-16 22:32:34 -0400

    Dockerfile and  CI fix

--- .buildkite/run-cpu-test-ppc64le.sh
+++ .buildkite/run-cpu-test-ppc64le.sh
@@ -2,13 +2,13 @@
 # It serves a sanity check for compilation and basic model usage.
 set -ex
 
-# Try building the docker image
-docker build -t cpu-test -f Dockerfile.ppc64le .
-
 # Setup cleanup
-remove_docker_container() { docker rm -f cpu-test || true; }
+remove_docker_container() { docker rm -f cpu-test || true; docker system prune -f; }
 trap remove_docker_container EXIT
 remove_docker_container
+
+# Try building the docker image
+docker build -t cpu-test -f Dockerfile.ppc64le .
 
 # Run the image, setting --shm-size=4g for tensor parallel.
 source /etc/environment
@@ -17,14 +17,23 @@ docker run -itd --entrypoint /bin/bash -v ~/.cache/huggingface:/root/.cache/hugg
 
 # Run basic model test
 docker exec cpu-test bash -c "
-  pip install pytest matplotlib einops transformers_stream_generator
-  pytest -v -s tests/models -m \"not vlm\" --ignore=tests/models/test_embedding.py --ignore=tests/models/test_oot_registration.py --ignore=tests/models/test_registry.py --ignore=tests/models/test_jamba.py --ignore=tests/models/test_danube3_4b.py" # Mamba and Danube3-4B on CPU is not supported
+  pip install pytest matplotlib einops transformers_stream_generator datamodel_code_generator
+  pytest -v -s tests/models/decoder_only/language \
+    --ignore=tests/models/test_fp8.py \
+    --ignore=tests/models/decoder_only/language/test_jamba.py \
+    --ignore=tests/models/decoder_only/language/test_danube3_4b.py" # Mamba and Danube3-4B on CPU is not supported
+
+# Run compressed-tensor test
+#docker exec cpu-test bash -c "
+#  pytest -s -v \
+#  tests/quantization/test_compressed_tensors.py::test_compressed_tensors_w8a8_static_setup \
+#  tests/quantization/test_compressed_tensors.py::test_compressed_tensors_w8a8_dynanmic_per_token"
 
 # online inference
 docker exec cpu-test bash -c "
-  python3 -m vllm.entrypoints.openai.api_server --model facebook/opt-125m & 
+  /opt/conda/bin/python3 -m vllm.entrypoints.openai.api_server --model facebook/opt-125m & 
   timeout 600 bash -c 'until curl localhost:8000/v1/models; do sleep 1; done' || exit 1
-  python3 benchmarks/benchmark_serving.py \
+  /opt/conda/bin/python3 benchmarks/benchmark_serving.py \
     --backend vllm \
     --dataset-name random \
     --model facebook/opt-125m \
