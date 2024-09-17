ef7c637679d27ec51f81e53482a368f8797f5065
Alex Brooks <alex.brooks@ibm.com>
2024-09-16 22:17:32 -0600

    [Frontend] Improve Nullable kv Arg Parsing (#8525)
    Signed-off-by: Alex-Brooks <Alex.Brooks@ibm.com>
    Signed-off-by: Sumit Dubey <sumit.dubey2@ibm.com>

--- tests/engine/test_arg_utils.py
+++ tests/engine/test_arg_utils.py
@@ -1,6 +1,8 @@
+from argparse import ArgumentTypeError
+
 import pytest
 
-from vllm.engine.arg_utils import EngineArgs
+from vllm.engine.arg_utils import EngineArgs, nullable_kvs
 from vllm.utils import FlexibleArgumentParser
 
 
@@ -13,6 +15,10 @@ from vllm.utils import FlexibleArgumentParser
         "image": 16,
         "video": 2
     }),
+    ("Image=16, Video=2", {
+        "image": 16,
+        "video": 2
+    }),
 ])
 def test_limit_mm_per_prompt_parser(arg, expected):
     parser = EngineArgs.add_cli_args(FlexibleArgumentParser())
@@ -22,3 +28,15 @@ def test_limit_mm_per_prompt_parser(arg, expected):
         args = parser.parse_args(["--limit-mm-per-prompt", arg])
 
     assert args.limit_mm_per_prompt == expected
+
+
+@pytest.mark.parametrize(
+    ("arg"),
+    [
+        "image",  # Missing =
+        "image=4,image=5",  # Conflicting values
+        "image=video=4"  # Too many = in tokenized arg
+    ])
+def test_bad_nullable_kvs(arg):
+    with pytest.raises(ArgumentTypeError):
+        nullable_kvs(arg)
