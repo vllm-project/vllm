fd925c28c7329ee9a394638e3ab644e2e6a30bce
youkaichao <youkaichao@gmail.com>
2024-09-16 22:57:57 -0700

    [torch.compile] register allreduce operations as custom ops (#8526)
    Signed-off-by: Alvant <alvasian@yandex.ru>

--- csrc/torch_bindings.cpp
+++ csrc/torch_bindings.cpp
@@ -411,11 +411,6 @@ TORCH_LIBRARY_EXPAND(CONCAT(TORCH_EXTENSION_NAME, _custom_ar), custom_ar) {
       "bool full_nvlink) -> int");
   custom_ar.impl("init_custom_ar", torch::kCUDA, &init_custom_ar);
 
-  custom_ar.def(
-      "should_custom_ar(Tensor inp, int max_size, int world_size, "
-      "bool full_nvlink) -> bool");
-  custom_ar.impl("should_custom_ar", torch::kCUDA, &should_custom_ar);
-
   custom_ar.def("all_reduce_reg(int fa, Tensor inp, Tensor! out) -> ()");
   custom_ar.impl("all_reduce_reg", torch::kCUDA, &all_reduce_reg);
 
