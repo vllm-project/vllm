27b64d4b532cfcbf0a6388d2bfb38977851c3601
Gregory Shtrasberg <156009573+gshtras@users.noreply.github.com>
2024-09-18 13:41:08 -0400

    [AMD][ROCm]Quantization methods on ROCm; Fix _scaled_mm call (#8380)
    Co-authored-by: Alexei-V-Ivanov-AMD <156011006+Alexei-V-Ivanov-AMD@users.noreply.github.com>
    Co-authored-by: Michael Goin <michael@neuralmagic.com>
    Signed-off-by: Sumit Dubey <sumit.dubey2@ibm.com>

--- vllm/config.py
+++ vllm/config.py
@@ -255,7 +255,10 @@ class ModelConfig:
 
     def _verify_quantization(self) -> None:
         supported_quantization = [*QUANTIZATION_METHODS]
-        rocm_supported_quantization = ["awq", "gptq", "fp8"]
+        rocm_supported_quantization = [
+            "awq", "gptq", "fp8", "compressed_tensors", "compressed-tensors",
+            "fbgemm_fp8"
+        ]
         optimized_quantization_methods = [
             "fp8", "marlin", "modelopt", "gptq_marlin_24", "gptq_marlin",
             "awq_marlin", "fbgemm_fp8", "compressed_tensors",
