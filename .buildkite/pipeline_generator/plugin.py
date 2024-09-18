c596405949818720458a47efe8372eb29baa74f3
kevin <kevin@anyscale.com>
2024-09-18 23:04:36 +0000

    p
    Signed-off-by: kevin <kevin@anyscale.com>

--- .buildkite/pipeline_generator/plugin.py
+++ .buildkite/pipeline_generator/plugin.py
@@ -28,7 +28,7 @@ def get_kubernetes_plugin_config(docker_image_path: str, test_bash_command: List
     pod_spec = KubernetesPodSpec(
         containers=[{
             "image": docker_image_path, 
-            "command": test_bash_command[0],
+            "command": [test_bash_command[0]],
             "args": test_bash_command[1:],
             "resources": {
                 "limits": {
