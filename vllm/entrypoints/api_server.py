c102ed5b280f16f8350acdedd580aa575d556123
Pastel！ <1627301104@qq.com>
2024-09-20 14:34:46 +0800

    Update api_server.py

--- vllm/entrypoints/api_server.py
+++ vllm/entrypoints/api_server.py
@@ -121,7 +121,6 @@ async def run_server(args: Namespace,
 
     shutdown_task = await serve_http(
         app,
-        limit_concurrency=engine.limit_concurrency,
         host=args.host,
         port=args.port,
         log_level=args.log_level,
