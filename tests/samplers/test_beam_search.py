f11d54907fe65aa0d8fc433e2c020379153396f1
youkaichao <youkaichao@126.com>
2024-09-20 16:58:07 -0700

    restore

--- tests/samplers/test_beam_search.py
+++ tests/samplers/test_beam_search.py
@@ -27,7 +27,7 @@ def test_beam_search_single_input(
     max_tokens: int,
     beam_width: int,
 ) -> None:
-    example_prompts = example_prompts[1:2]
+    example_prompts = example_prompts[:1]
     with hf_runner(model, dtype=dtype) as hf_model:
         hf_outputs = hf_model.generate_beam_search(example_prompts, beam_width,
                                                    max_tokens)
