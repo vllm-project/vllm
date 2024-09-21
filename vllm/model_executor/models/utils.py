f54562c4ae13a3face5a12cedafe4a8995f79d3b
Isotr0py <2037008807@qq.com>
2024-09-21 13:41:01 +0800

    refactor component weight loading

--- vllm/model_executor/models/utils.py
+++ vllm/model_executor/models/utils.py
@@ -1,3 +1,4 @@
+import itertools
 from typing import (Dict, Iterable, List, Literal, Optional, Protocol, Tuple,
                     Union, overload)
 
@@ -16,7 +17,8 @@ from vllm.sequence import IntermediateTensors
 from vllm.utils import is_pin_memory_available
 
 
-def filter_weights(weights: Iterable[Tuple[str, torch.Tensor]], prefix: str):
+def filter_weights(weights: Iterable[Tuple[str, torch.Tensor]],
+                   prefix: str) -> Iterable[Tuple[str, torch.Tensor]]:
     """
     Helper function to load weights for inner vLLM models.
 
@@ -30,18 +32,23 @@ def filter_weights(weights: Iterable[Tuple[str, torch.Tensor]], prefix: str):
             yield name, loaded_weight
 
 
-def check_filter_available(weights: Iterable[Tuple[str, torch.Tensor]], prefix: List[str]):
+def group_weights_with_prefix(
+    weights: Iterable[Tuple[str, torch.Tensor]]
+) -> Dict[str, Iterable[Tuple[str, torch.Tensor]]]:
     """
-    Helper function to check if the filter is available for the loaded weights
+    Helper function to group weights with prefix
     """
-    unexpected_name = set()
-    for name, _ in weights:
-        weight_prefix = name.split(".")[0]
-        if weight_prefix not in prefix:
-            unexpected_name.add(name)
+    init_weights, repeated_weights = itertools.tee(weights, 2)
 
-    if unexpected_name:
-        raise ValueError(f"Loaded weights contents unexpected weight: {unexpected_name}")
+    prefix = set(map(lambda x: x[0].split(".")[0], init_weights))
+
+    repeated_weights = itertools.tee(repeated_weights, len(prefix))
+
+    grouped_weights = {}
+    for weight_component, weight_prefix in zip(repeated_weights, prefix):
+        grouped_weights[weight_prefix] = filter_weights(
+            weight_component, weight_prefix)
+    return grouped_weights
 
 
 def init_vllm_registered_model(
