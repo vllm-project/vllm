b1a80930b80d34b728b8851da3a568fd6a3cc3f8
liuyanyi <wolfsonliu@163.com>
2024-09-22 19:12:48 +0800

    Fix tool_parser

--- vllm/entrypoints/openai/serving_chat.py
+++ vllm/entrypoints/openai/serving_chat.py
@@ -275,9 +275,6 @@ class OpenAIServingChat(OpenAIServing):
 
         num_prompt_tokens = 0
 
-        tool_parser: Optional[ToolParser] = self.tool_parser(
-            tokenizer) if self.tool_parser else None
-
         if isinstance(request.tool_choice, ChatCompletionNamedToolChoiceParam):
             tool_choice_function_name = request.tool_choice.function.name
         else:
@@ -295,6 +292,19 @@ class OpenAIServingChat(OpenAIServing):
             all_previous_token_ids = [[]] * num_choices
         else:
             previous_texts, all_previous_token_ids = None, None
+
+        # Prepare the tool parser if it's needed
+        try:
+            if tool_choice_auto and self.tool_parser:
+                tool_parser: Optional[ToolParser] = self.tool_parser(tokenizer)
+            else:
+                tool_parser = None
+        except RuntimeError as e:
+            logger.error("Error in tool parser creation: %s", e)
+            data = self.create_streaming_error_response(str(e))
+            yield f"data: {data}\n\n"
+            yield "data: [DONE]\n\n"
+            return
 
         try:
             async for res in result_generator:
@@ -664,7 +674,12 @@ class OpenAIServingChat(OpenAIServing):
                     or request.tool_choice is None) and self.enable_auto_tools \
                     and self.tool_parser:
 
-                tool_parser = self.tool_parser(tokenizer)
+                try:
+                    tool_parser = self.tool_parser(tokenizer)
+                except RuntimeError as e:
+                    logger.error("Error in tool parser creation: %s", e)
+                    return self.create_error_response(str(e))
+
                 tool_call_info = tool_parser.extract_tool_calls(output.text)
                 tools_called = tool_call_info.tools_called
                 if tool_call_info.tools_called:
