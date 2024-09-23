cbbd0f4954a9a9734687aa4bb436d82342bd53a1
tylertitsworth <tyler.titsworth@intel.com>
2024-09-23 11:37:58 -0700

    fix lint and build errors
    Signed-off-by: tylertitsworth <tyler.titsworth@intel.com>

--- vllm/platforms/xpu.py
+++ vllm/platforms/xpu.py
@@ -2,15 +2,18 @@ from typing import Tuple
 
 import torch
 
-from .interface import Platform, PlatformEnum
+from .interface import Platform, PlatformEnum, DeviceCapability
 
 
 class XPUPlatform(Platform):
     _enum = PlatformEnum.XPU
 
     @staticmethod
-    def get_device_capability(device_id: int = 0) -> Tuple[int, int]:
-        return torch.xpu.get_device_capability(device_id)
+    def get_device_capability(device_id: int = 0) -> DeviceCapability:
+        return DeviceCapability(
+            major=int(torch.xpu.get_device_capability(device_id)['version'].split('.')[0]),
+            minor=int(torch.xpu.get_device_capability(device_id)['version'].split('.')[1])
+        )
 
     @staticmethod
     def get_device_name(device_id: int = 0) -> str:
