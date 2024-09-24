f224c71f0b2de554a199d334ab0186895b566c2b
KuntaiDu <kuntai@uchicago.edu>
2024-09-24 06:54:25 +0000

    make format checker happy

--- vllm/utils.py
+++ vllm/utils.py
@@ -31,10 +31,10 @@ import yaml
 from packaging.version import Version
 from typing_extensions import ParamSpec, TypeIs, assert_never
 
+import vllm.distributed.kv_transfer.vllm_adapter as dist_kv
 import vllm.envs as envs
 from vllm.logger import enable_trace_function_call, init_logger
 from vllm.platforms import current_platform
-import vllm.distributed.kv_transfer.vllm_adapter as dist_kv
 
 logger = init_logger(__name__)
 
@@ -546,14 +546,14 @@ def get_open_zmq_ipc_path() -> str:
 
 def get_open_port(force: bool = False) -> int:
     port = envs.VLLM_PORT
-    
+
     if force:
         # This flag will only be True in disaggregated prefill scenario
         # and it has to be set so that vLLM can connect prefill vLLM instance
         # and decode vLLM instance.
         assert port is not None, "Please set environment variable VLLM_PORT in"
         " order to use disaggregated prefill and distributed KV cache transfer"
-        
+
         # For prefill vLLM instance (KV producer) this port must be empty.
         # For decode vLLM instance this port can be non-empty.
         if dist_kv.IS_KV_PRODUCER:
@@ -562,14 +562,15 @@ def get_open_port(force: bool = False) -> int:
                     s.bind(("", port))
                     return port
             except OSError as e:
-                logger.error("Port %d must be empty so that prefill vLLM "
-                             "instance can use this port to initialize "
-                             "distributed KV communication with decode "
-                             "vLLM instance.", port)
+                logger.error(
+                    "Port %d must be empty so that prefill vLLM "
+                    "instance can use this port to initialize "
+                    "distributed KV communication with decode "
+                    "vLLM instance.", port)
                 raise e
         else:
             return port
-        
+
     if port is not None:
         while True:
             try:
