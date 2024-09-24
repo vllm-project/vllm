6d48951e98a51af897ce8e600e91ffc876cd1fdc
Sourashis Roy <sroy@roblox.com>
2024-09-24 18:59:22 +0000

    Fix format

--- tests/core/test_scheduler.py
+++ tests/core/test_scheduler.py
@@ -746,20 +746,23 @@ def test_schedule_decode_blocks_to_copy_update(use_v2_block_manager: bool):
     # applied.
     assert output.blocks_to_copy == [(2, 3)]
 
+
 @pytest.mark.parametrize('use_v2_block_manager', [True, False])
-def test_schedule_swapped_simple(use_v2_block_manager:bool):
-    block_size=4
-    scheduler = initialize_scheduler(
-        use_v2_block_manager=use_v2_block_manager, block_size=block_size)
+def test_schedule_swapped_simple(use_v2_block_manager: bool):
+    block_size = 4
+    scheduler = initialize_scheduler(use_v2_block_manager=use_v2_block_manager,
+                                     block_size=block_size)
     curr_loras = None
     blocks_to_swap_out: List[Tuple[int, int]] = []
-    _, seq_group = create_dummy_prompt(
-        "1", prompt_length=4, best_of=2, block_size=block_size)
+    _, seq_group = create_dummy_prompt("1",
+                                       prompt_length=4,
+                                       best_of=2,
+                                       block_size=block_size)
     scheduler._allocate_and_set_running(seq_group)
     append_new_token_seq_group(4, seq_group, 1)
     scheduler._swap_out(seq_group, blocks_to_swap_out)
     scheduler._add_seq_group_to_swapped(seq_group)
-    
+
     budget = create_token_budget()
     output = scheduler._schedule_swapped(budget, curr_loras)
     remaining_swapped = scheduler.swapped
