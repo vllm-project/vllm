73f4b48905f75ebbed035ab5e7abde2b64e701c3
Konrad Zawora <kzawora@habana.ai>
2024-09-24 14:22:16 +0200

    Fix INC FP8 inference after rebase (#333)
    This PR fixes the "RuntimeError: HPU does not have device capability."
    error introduced after rebase & fixes loading weights on CPU for
    quantization.

--- vllm/platforms/hpu.py
+++ vllm/platforms/hpu.py
@@ -1,17 +1,10 @@
-from typing import Optional
-
 import torch
 
-from .interface import DeviceCapability, Platform, PlatformEnum
+from .interface import Platform, PlatformEnum
 
 
 class HpuPlatform(Platform):
     _enum = PlatformEnum.HPU
-
-    @staticmethod
-    def get_device_capability(
-            device_id: int = 0) -> Optional[DeviceCapability]:
-        raise RuntimeError("HPU does not have device capability.")
 
     @staticmethod
     def inference_mode():
