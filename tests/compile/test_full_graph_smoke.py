62c33bef021af1418dd47ed10db7252280663593
Bill Nell <bill@neuralmagic.com>
2024-09-24 01:48:56 +0000

    split up fullgraph tests

---
+++ tests/compile/test_full_graph_smoke.py
@@ -0,0 +1,11 @@
+import pytest
+
+from .utils import TEST_MODELS_SMOKE, check_full_graph_support
+
+
+@pytest.mark.parametrize("model_info", TEST_MODELS_SMOKE)
+@pytest.mark.parametrize("backend", ["eager", "inductor"])
+def test_full_graph(model_info, backend):
+    model = model_info[0]
+    model_kwargs = model_info[1]
+    check_full_graph_support(model, model_kwargs, backend, tp_size=1)
