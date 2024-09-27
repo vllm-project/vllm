1305f8d9a8b56e7c2addafa418e6c1dcd7d443bf
Salar Hosseini <skhorasgani@tenstorrent.com>
2024-09-27 17:20:46 +0000

    Add global prefill and decode stat tracking to llm_engine
    Signed-off-by: Salar Hosseini <skhorasgani@tenstorrent.com>

--- vllm/engine/llm_engine.py
+++ vllm/engine/llm_engine.py
@@ -379,7 +379,8 @@ class LLMEngine:
                 # before prometheus_client is imported.
                 # See https://prometheus.github.io/client_python/multiprocess/
                 from vllm.engine.metrics import (LoggingStatLogger,
-                                                 PrometheusStatLogger)
+                                                 PrometheusStatLogger,
+                                                 GlobalStatLogger)
 
                 self.stat_loggers = {
                     "logging":
@@ -392,6 +393,7 @@ class LLMEngine:
                         labels=dict(
                             model_name=self.model_config.served_model_name),
                         vllm_config=vllm_config),
+                    "global": GlobalStatLogger(),
                 }
                 self.stat_loggers["prometheus"].info("cache_config",
                                                      self.cache_config)
