90083c3485611cfb948e1063382b5a617875d25a
jiqing-feng <jiqing.feng@intel.com>
2024-09-27 12:20:58 -0400

    fix ruff

--- vllm/spec_decode/spec_decode_worker.py
+++ vllm/spec_decode/spec_decode_worker.py
@@ -554,7 +554,6 @@ class SpecDecodeWorker(LoraNotSupportedWorkerBase):
         not called, meaning that the kv-cache in proposer for requests is not
         updated, so they cannot enable spec decode in the rest decoding.
         """
-        import pdb; pdb.set_trace()
         sampler_output = self.scorer_worker.execute_model(execute_model_req)
         assert len(sampler_output) == 1
         sampler_output = sampler_output[0]
