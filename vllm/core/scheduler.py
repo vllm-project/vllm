b4650b61b49a9a2e49eb80499f85b69350149806
Varun Sundar Rabindranath <varun@neuralmagic.com>
2024-09-27 17:02:23 +0000

    make can_append_slots assert stronger

--- vllm/core/scheduler.py
+++ vllm/core/scheduler.py
@@ -1190,16 +1190,17 @@ class Scheduler:
             return False
 
         is_prefill = seq_group.is_prefill()
+        num_lookahead_slots = self._get_num_lookahead_slots(is_prefill,
+                                                             enable_chunking)
 
-        # Appending prefill slots only happens chunked prefill is enabled.
-        assert self.scheduler_config.chunked_prefill_enabled or \
-                    not is_prefill
+        if is_prefill and num_lookahead_slots > 0:
+            # Appending prefill slots only happens multi-step and
+            # chunked-prefill are enabled together.
+            assert self.scheduler_config.is_multi_step and enable_chunking
 
         return self.block_manager.can_append_slots(
             seq_group=seq_group,
-            num_lookahead_slots=self._get_num_lookahead_slots(
-                is_prefill, enable_chunking),
-        )
+            num_lookahead_slots=num_lookahead_slots)
 
     def _allow_async_output_proc(self, seq_group: SequenceGroup) -> bool:
         no_beam_search = seq_group.sampling_params is None or (
