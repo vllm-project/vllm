966cb9bc8f71447e0c725c8dcb7703101377f8ea
yan ma <yan.ma@intel.com>
2024-09-27 19:34:05 +0800

    avoid triton import for xpu

--- vllm/triton_utils/importing.py
+++ vllm/triton_utils/importing.py
@@ -1,11 +1,12 @@
 from importlib.util import find_spec
 
 from vllm.logger import init_logger
+from vllm.platforms import current_platform
 
 logger = init_logger(__name__)
 
-HAS_TRITON = find_spec("triton") is not None
+HAS_TRITON = find_spec("triton") is not None and not current_platform.is_xpu()
 
 if not HAS_TRITON:
-    logger.info("Triton not installed; certain GPU-related functions"
-                " will not be available.")
+    logger.info("Triton not installed or not compatible; certain GPU-related"
+                " functions will not be available.")
