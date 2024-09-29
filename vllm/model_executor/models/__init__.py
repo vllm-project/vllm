14a7070ad2500723218427460b4528da85a3de35
Zilin Zhu <zilinzhu@tencent.com>
2024-09-29 12:19:39 +0800

    [Model] Support Qwen2.5-Math-RM-72B (#8896)
    Signed-off-by: Sumit Dubey <sumit.dubey2@ibm.com>

--- vllm/model_executor/models/__init__.py
+++ vllm/model_executor/models/__init__.py
@@ -74,6 +74,7 @@ _GENERATION_MODELS = {
 
 _EMBEDDING_MODELS = {
     "MistralModel": ("llama_embedding", "LlamaEmbeddingModel"),
+    "Qwen2ForRewardModel": ("qwen2_rm", "Qwen2ForRewardModel"),
 }
 
 _MULTIMODAL_MODELS = {
