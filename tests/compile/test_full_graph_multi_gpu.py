fad55cb07d2c02af01aed2f0c2cbeae3330142fd
youkaichao <youkaichao@126.com>
2024-09-29 17:12:42 -0700

    fix tests

--- tests/compile/test_full_graph_multi_gpu.py
+++ tests/compile/test_full_graph_multi_gpu.py
@@ -1,6 +1,5 @@
 import pytest
 
-from vllm.compilation.backends import vllm_backend
 from vllm.utils import cuda_device_count_stateless
 
 from ..utils import fork_new_process_for_each_test
@@ -9,9 +8,9 @@ from .utils import TEST_MODELS_SMOKE, check_full_graph_support
 
 @pytest.mark.parametrize("model_info", TEST_MODELS_SMOKE)
 @pytest.mark.parametrize("tp_size", [2])
-@pytest.mark.parametrize("backend", ["eager", vllm_backend])
+@pytest.mark.parametrize("optimization_level", [1, 2])
 @fork_new_process_for_each_test
-def test_full_graph_multi_gpu(model_info, tp_size, backend):
+def test_full_graph_multi_gpu(model_info, tp_size, optimization_level):
     model = model_info[0]
     model_kwargs = model_info[1]
 
@@ -19,4 +18,7 @@ def test_full_graph_multi_gpu(model_info, tp_size, backend):
     if cuda_device_count_stateless() < tp_size:
         pytest.skip("Not enough CUDA devices for the test.")
 
-    check_full_graph_support(model, model_kwargs, backend, tp_size=tp_size)
+    check_full_graph_support(model,
+                             model_kwargs,
+                             optimization_level,
+                             tp_size=tp_size)
--- tests/compile/utils.py
+++ tests/compile/utils.py
@@ -4,7 +4,6 @@ import torch
 
 from tests.quantization.utils import is_quant_method_supported
 from vllm import LLM, SamplingParams
-from vllm.plugins import set_torch_compile_backend
 from vllm.utils import is_hip
 
 TEST_MODELS_SMOKE = [
@@ -68,19 +67,19 @@ if not is_hip() and is_quant_method_supported("awq"):
     }))
 
 
-def check_full_graph_support(model, model_kwargs, backend, tp_size=1):
+def check_full_graph_support(model,
+                             model_kwargs,
+                             optimization_level,
+                             tp_size=1):
     # make sure these models can be captured in full graph mode
-    if "VLLM_TORCH_COMPILE_LEVEL" not in os.environ:
-        os.environ["VLLM_TORCH_COMPILE_LEVEL"] = "1"
-        os.environ["VLLM_TEST_DYNAMO_FULLGRAPH_CAPTURE"] = "1"
+    os.environ["VLLM_TORCH_COMPILE_LEVEL"] = str(optimization_level)
+    os.environ["VLLM_TEST_DYNAMO_FULLGRAPH_CAPTURE"] = "1"
 
     # Inductor doesn't support fp8/gptq_marlin_24 yet.
     quantization = model_kwargs.get("quantization")
     if (quantization == "fp8" or quantization == "gptq_marlin"
-            or quantization == "gptq_marlin_24") and backend != "eager":
+            or quantization == "gptq_marlin_24") and optimization_level > 1:
         return
-
-    set_torch_compile_backend(backend)
 
     prompts = [
         "Hello, my name is",
