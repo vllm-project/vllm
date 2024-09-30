d2f1b97d4dad32e6839f73f3b6b638102158986f
youkaichao <youkaichao@126.com>
2024-09-29 19:13:57 -0700

    prepare for tp test

--- tests/compile/test_basic_correctness.py
+++ tests/compile/test_basic_correctness.py
@@ -10,18 +10,17 @@ from .utils import TEST_MODELS_SMOKE
 
 @pytest.mark.parametrize("model_info", TEST_MODELS_SMOKE)
 @pytest.mark.parametrize("pp_size", [1, 2])
-def test_compile_correctness(model_info, pp_size):
+@pytest.mark.parametrize("tp_size", [1])
+def test_compile_correctness(model_info, pp_size, tp_size):
     # this test is run under multiple suits, with different GPUs.
     # make sure we only run the test with correct CUDA devices.
     # don't use "<", as it will duplicate the tests.
-    if cuda_device_count_stateless() != pp_size:
+    if cuda_device_count_stateless() != pp_size * tp_size:
         pytest.skip("Not correct CUDA devices for the test.")
     model = model_info[0]
     model_args = model_info[1]
-    all_args = [
-        ["--enforce-eager"] + model_args + ["--max_model_len", "1024"] +
-        ["-pp", str(pp_size)],
-    ] * 3
+    all_args = [["--enforce-eager"] + model_args + ["--max_model_len", "1024"]
+                + ["-pp", str(pp_size)] + ["-tp", str(tp_size)]] * 3
     all_envs: List[Optional[Dict[str, str]]] = [{
         "VLLM_TORCH_COMPILE_LEVEL":
         str(i)
