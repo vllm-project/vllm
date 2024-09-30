f2330876f71306fdfb00c767c5720df8c66c2759
youkaichao <youkaichao@126.com>
2024-09-29 19:12:14 -0700

    update tests

--- tests/compile/test_basic_correctness.py
+++ tests/compile/test_basic_correctness.py
@@ -11,8 +11,11 @@ from .utils import TEST_MODELS_SMOKE
 @pytest.mark.parametrize("model_info", TEST_MODELS_SMOKE)
 @pytest.mark.parametrize("pp_size", [1, 2])
 def test_compile_correctness(model_info, pp_size):
-    if cuda_device_count_stateless() < pp_size:
-        pytest.skip("Not enough CUDA devices for the test.")
+    # this test is run under multiple suits, with different GPUs.
+    # make sure we only run the test with correct CUDA devices.
+    # don't use "<", as it will duplicate the tests.
+    if cuda_device_count_stateless() != pp_size:
+        pytest.skip("Not correct CUDA devices for the test.")
     model = model_info[0]
     model_args = model_info[1]
     all_args = [
