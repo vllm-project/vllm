a8644754ac97e6ef1204f30795badb4a326316cb
youkaichao <youkaichao@126.com>
2024-09-29 19:07:53 -0700

    add tests

--- tests/compile/test_full_graph_smoke.py
+++ tests/compile/test_full_graph_smoke.py
@@ -2,19 +2,23 @@ from typing import Dict, List, Optional
 
 import pytest
 
+from vllm.utils import cuda_device_count_stateless
+
 from ..utils import compare_all_settings
 from .utils import TEST_MODELS_SMOKE
 
 
 @pytest.mark.parametrize("model_info", TEST_MODELS_SMOKE)
-def test_compile_correctness(model_info):
+@pytest.mark.parametrize("pp_size", [1, 2])
+def test_compile_correctness(model_info, pp_size):
+    if cuda_device_count_stateless() < pp_size:
+        pytest.skip("Not enough CUDA devices for the test.")
     model = model_info[0]
     model_args = model_info[1]
     all_args = [
-        ["--enforce-eager"] + model_args,
-        ["--enforce-eager"] + model_args,
-        ["--enforce-eager"] + model_args,
-    ]
+        ["--enforce-eager"] + model_args + ["--max_model_len", "1024"] +
+        ["-pp", str(pp_size)],
+    ] * 3
     all_envs: List[Optional[Dict[str, str]]] = [{
         "VLLM_TORCH_COMPILE_LEVEL":
         str(i)
