3b49f43b8f665561f22581c3e8409ddd14e9abda
Varun Sundar Rabindranath <varun@neuralmagic.com>
2024-09-30 17:01:46 +0000

    fix formatting

--- vllm/worker/model_runner.py
+++ vllm/worker/model_runner.py
@@ -859,10 +859,7 @@ class ModelInputForGPUBuilder(ModelRunnerInputBuilderBase[ModelInputForGPU]):
 
         # Attention metadata.
         attn_metadata = self.attn_metadata_builder.build(
-            seq_lens,
-            query_lens,
-            cuda_graph_pad_size,
-            batch_size)
+            seq_lens, query_lens, cuda_graph_pad_size, batch_size)
 
         # LoRA data.
         lora_requests = set()
