fbd3231c3796a87ebbe1335781d0728c996ca32c
youkaichao <youkaichao@126.com>
2024-09-29 17:31:04 -0700

    change tests

--- tests/compile/test_correctness.py
+++ tests/compile/test_correctness.py
@@ -1,32 +1,16 @@
-import os
+from typing import Dict, List, Optional
 
-from vllm import LLM, SamplingParams
+from ..utils import compare_all_settings
 
 
 def test_compile_correctness():
-    prompts = [
-        "Hello, my name is",
-        "The president of the United States is",
-        "The capital of France is",
-        "The future of AI is",
+    all_args = [
+        ["--enforce-eager"],
+        ["--enforce-eager"],
+        ["--enforce-eager"],
     ]
-    sampling_params = SamplingParams(temperature=0)
-
-    all_outputs = []
-    all_levels = [0, 1, 2]
-    for level in all_levels:
-        os.environ["VLLM_TORCH_COMPILE_LEVEL"] = str(level)
-        llm = LLM(model="meta-llama/Meta-Llama-3-8B",
-                  enforce_eager=True,
-                  tensor_parallel_size=1,
-                  disable_custom_all_reduce=True,
-                  gpu_memory_utilization=0.3)
-        outputs = llm.generate(prompts, sampling_params)
-        all_outputs.append(outputs)
-    reference_outputs = all_outputs[0]
-    for level, outputs in zip(all_levels[1:], all_outputs[1:]):
-        for ref_output, output in zip(reference_outputs, outputs):
-            prompt = output.prompt
-            generated_text = output.outputs[0].text
-            ref_generated_text = ref_output.outputs[0].text
-            assert generated_text == ref_generated_text, f"level: {level}, prompt: {prompt}, generated_text: {generated_text}, ref_generated_text: {ref_generated_text}"  # noqa
+    all_envs: List[Optional[Dict[str, str]]] = [{
+        "VLLM_TORCH_COMPILE_LEVEL":
+        str(i)
+    } for i in range(3)]
+    compare_all_settings("meta-llama/Meta-Llama-3-8B", all_args, all_envs)
