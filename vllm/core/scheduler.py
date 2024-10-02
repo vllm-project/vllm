b5277c9f8b33e1e585d4c8b03d7722fb363fba46
Andrew Feldman <afeld2012@gmail.com>
2024-10-02 01:48:55 -0400

    refactoring scheduler improvements

--- vllm/core/scheduler.py
+++ vllm/core/scheduler.py
@@ -1607,37 +1607,16 @@ class Scheduler:
         # in a decode phase. Do not chunk.
         if enable_chunking and len(seqs) == 1:
             remaining_token_budget = budget.remaining_token_budget()
-            if self.cache_config.enable_prefix_caching:
-                if self.scheduler_config.is_multi_step:
-                    # The current multi-step + chunked prefill capability does
-                    # not actually support chunking prompts.
-                    if num_new_tokens > self._get_prompt_limit(seq_group):
-                        # If the seq_group is in prompt-stage, pass the
-                        # num_new_tokens as-is so the caller can ignore
-                        # the sequence.
-                        pass
-                    else:
-                        num_new_tokens = 0 \
-                            if num_new_tokens > remaining_token_budget \
-                            else num_new_tokens
-                else:
-                    # When prefix caching is enabled, we always allocate
-                    # the number of new tokens that is dividable by the block
-                    # size to avoid partial block matching.
-                    block_size = self.cache_config.block_size
-                    remainder = budget.token_budget % block_size
-                    if remainder != 0:
-                        raise ValueError(
-                            "When enabling chunked prefill and "
-                            "prefix caching, max_num_batched_tokens "
-                            "(chunk size) must be dividable by "
-                            "block size, but got chunk_size "
-                            f"({budget.token_budget}) % block_size "
-                            f"({block_size}) = {remainder}")
-                    if remaining_token_budget < num_new_tokens:
-                        num_new_tokens = (remaining_token_budget //
-                                          block_size) * block_size
-            elif self.scheduler_config.is_multi_step:
+            if self.scheduler_config.is_multi_step:
+                # The current multi-step + chunked prefill capability does
+                # not actually support chunking prompts.
+                #
+                # Therefore, `num_new_tokens` is computed in the same fashion
+                # for both multi-step+chunked-prefill &
+                # multi-step+chunked-prefill+APC
+                #
+                # Prompts with more tokens than the current remaining budget
+                # are postponed to future scheduler steps
                 if num_new_tokens > self._get_prompt_limit(seq_group):
                     # If the seq_group is in prompt-stage, pass the
                     # num_new_tokens as-is so the caller can ignore
@@ -1647,6 +1626,22 @@ class Scheduler:
                     num_new_tokens = 0 \
                         if num_new_tokens > remaining_token_budget \
                         else num_new_tokens
+            elif self.cache_config.enable_prefix_caching:
+                # When prefix caching is enabled, we always allocate
+                # the number of new tokens that is dividable by the block
+                # size to avoid partial block matching.
+                block_size = self.cache_config.block_size
+                remainder = budget.token_budget % block_size
+                if remainder != 0:
+                    raise ValueError("When enabling chunked prefill and "
+                                     "prefix caching, max_num_batched_tokens "
+                                     "(chunk size) must be dividable by "
+                                     "block size, but got chunk_size "
+                                     f"({budget.token_budget}) % block_size "
+                                     f"({block_size}) = {remainder}")
+                if remaining_token_budget < num_new_tokens:
+                    num_new_tokens = (remaining_token_budget //
+                                      block_size) * block_size
             else:
                 num_new_tokens = min(num_new_tokens, remaining_token_budget)
         return num_new_tokens
