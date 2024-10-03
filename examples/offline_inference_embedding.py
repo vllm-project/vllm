1e04160a329a2e83e1d2317da503438d10c75ac5
rshaw@neuralmagic.com <rshaw@neuralmagic.com>
2024-10-03 22:54:27 +0000

    revert changes to e5-mistral

--- examples/offline_inference_embedding.py
+++ examples/offline_inference_embedding.py
@@ -1,22 +1,15 @@
 from vllm import LLM
-from vllm.inputs import build_decoder_prompts
 
 # Sample prompts.
-prompts = build_decoder_prompts([
+prompts = [
     "Hello, my name is",
     "The president of the United States is",
     "The capital of France is",
     "The future of AI is",
-])
+]
 
 # Create an LLM.
-model = LLM(
-    model="intfloat/e5-mistral-7b-instruct",
-    enforce_eager=True,
-    # NOTE: sliding_window is not supported by encoder_decoder_model
-    disable_sliding_window=True,
-    gpu_memory_utilization=0.95,
-)
+model = LLM(model="intfloat/e5-mistral-7b-instruct", enforce_eager=True)
 # Generate embedding. The output is a list of EmbeddingRequestOutputs.
 outputs = model.encode(prompts)
 # Print the outputs.
