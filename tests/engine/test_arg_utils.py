63dcb6c886dba16f21507dbac70e093dc6a5cde6
Jerzy Zagorski <jzagorsk@amazon.com>
2024-10-03 15:50:55 +0200

    Prevent autoformating of test parameters

--- tests/engine/test_arg_utils.py
+++ tests/engine/test_arg_utils.py
@@ -42,24 +42,38 @@ def test_bad_nullable_kvs(arg):
         nullable_kvs(arg)
 
 
+# yapf: disable
 @pytest.mark.parametrize(("arg", "expected", "option"), [
     (None, None, "mm-processor-kwargs"),
     ("{}", {}, "mm-processor-kwargs"),
-    ('{"num_crops": 4}', {
-        "num_crops": 4
-    }, "mm-processor-kwargs"),
-    ('{"foo": {"bar": "baz"}}', {
-        "foo": {
-            "bar": "baz"
-        }
-    }, "mm-processor-kwargs"),
-    ('{"cast_logits_dtype":"bfloat16","sequence_parallel_norm":true,"sequence_parallel_norm_threshold":2048}',
-     {
-         "cast_logits_dtype": "bfloat16",
-         "sequence_parallel_norm": True,
-         "sequence_parallel_norm_threshold": 2048,
-     }, "override-neuron-config"),
+    (
+        '{"num_crops": 4}',
+        {
+            "num_crops": 4
+        },
+        "mm-processor-kwargs"
+    ),
+    (
+        '{"foo": {"bar": "baz"}}',
+        {
+            "foo":
+            {
+                "bar": "baz"
+            }
+        },
+        "mm-processor-kwargs"
+    ),
+    (
+        '{"cast_logits_dtype":"bfloat16","sequence_parallel_norm":true,"sequence_parallel_norm_threshold":2048}',
+        {
+            "cast_logits_dtype": "bfloat16",
+            "sequence_parallel_norm": True,
+            "sequence_parallel_norm_threshold": 2048,
+        },
+        "override-neuron-config"
+    ),
 ])
+# yapf: enable
 def test_composite_arg_parser(arg, expected, option):
     parser = EngineArgs.add_cli_args(FlexibleArgumentParser())
     if arg is None:
