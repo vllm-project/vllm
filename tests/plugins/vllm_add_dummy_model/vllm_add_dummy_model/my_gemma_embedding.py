7a7272c9eca4c25d88e525923c73967396564017
DarkLight1337 <tlleungac@connect.ust.hk>
2024-10-06 15:24:27 +0000

    Add test

---
+++ tests/plugins/vllm_add_dummy_model/vllm_add_dummy_model/my_gemma_embedding.py
@@ -0,0 +1,14 @@
+import torch
+
+from vllm.model_executor.models.gemma2_embedding import Gemma2EmbeddingModel
+
+
+class MyGemma2Embedding(Gemma2EmbeddingModel):
+
+    def forward(self, *args, **kwargs) -> torch.Tensor:
+        hidden_states = super().forward(*args, **kwargs)
+
+        # We assume PP isn't used in the test
+        assert isinstance(hidden_states, torch.Tensor)
+
+        return torch.zeros_like(hidden_states)
