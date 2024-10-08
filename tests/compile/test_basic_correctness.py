388d5639ecd07349a0a02e0174c98ee72dad6fb5
youkaichao <youkaichao@126.com>
2024-10-08 15:53:33 -0700

    llava uses fullgraph=false

--- tests/compile/test_basic_correctness.py
+++ tests/compile/test_basic_correctness.py
@@ -11,18 +11,19 @@ from ..utils import compare_all_settings
 # we cannot afford testing the full Catesian product
 # of all models and all levels
 @pytest.mark.parametrize(
-    "model, model_args, pp_size, tp_size, attn_backend, method",
+    "model, model_args, pp_size, tp_size, attn_backend, method, fullgraph",
     [
-        ("meta-llama/Meta-Llama-3-8B", [], 2, 2, "FLASH_ATTN", "generate"),
+        ("meta-llama/Meta-Llama-3-8B", [], 2, 2, "FLASH_ATTN", "generate",
+         True),
         ("nm-testing/Meta-Llama-3-8B-Instruct-W8A8-Dyn-Per-Token-2048-Samples",
          ["--quantization", "compressed-tensors"
-          ], 1, 1, "FLASH_ATTN", "generate"),
-        ("google/gemma-2-2b-it", [], 1, 2, "FLASHINFER", "generate"),
+          ], 1, 1, "FLASH_ATTN", "generate", True),
+        ("google/gemma-2-2b-it", [], 1, 2, "FLASHINFER", "generate", True),
         # TODO: add multi-modality test for llava
-        ("llava-hf/llava-1.5-7b-hf", [], 2, 1, "FLASHINFER", "generate")
+        ("llava-hf/llava-1.5-7b-hf", [], 2, 1, "FLASHINFER", "generate", False)
     ])
 def test_compile_correctness(model, model_args, pp_size, tp_size, attn_backend,
-                             method):
+                             method, fullgraph):
     # this test is run under multiple suits, with different GPUs.
     # make sure we only run the test with correct CUDA devices.
     # don't use "<", as it will duplicate the tests.
@@ -30,6 +31,8 @@ def test_compile_correctness(model, model_args, pp_size, tp_size, attn_backend,
         pytest.skip("Not correct CUDA devices for the test.")
     import os
     os.environ["VLLM_ATTENTION_BACKEND"] = attn_backend
+    if not fullgraph:
+        os.environ["VLLM_TEST_DYNAMO_FULLGRAPH_CAPTURE"] = "0"
     all_args = [["--enforce-eager"] + model_args + ["--max_model_len", "1024"]
                 + ["-pp", str(pp_size)] + ["-tp", str(tp_size)]] * 3
     # don't test VLLM_TORCH_COMPILE_LEVEL == 3 case
