e1f07fab8c778c89b659b8f6423b63aa2ff5cc3b
yan ma <yan.ma@intel.com>
2024-10-08 17:50:13 +0800

    add doc for tp+pp support
    Signed-off-by: yan ma <yan.ma@intel.com>

--- vllm/platforms/xpu.py
+++ vllm/platforms/xpu.py
@@ -24,8 +24,3 @@ class XPUPlatform(Platform):
     @staticmethod
     def inference_mode():
         return torch.no_grad()
-
-    @classmethod
-    def get_device_total_memory(cls, device_id: int = 0) -> int:
-        device_props = torch.xpu.get_device_properties(device_id)
-        return device_props.total_memory
