b934f656d716dfab75899dfe81010dca56113a39
noooop <noooop@126.com>
2024-10-08 15:01:19 +0800

    prefill only gpu executor

--- vllm/model_executor/prefill_only/execute_io.py
+++ vllm/model_executor/prefill_only/execute_io.py
@@ -23,7 +23,11 @@ class ExecuteInput:
 
 
 class ExecuteOutput:
-    pass
+
+    def to(self, target_device, non_blocking=False):
+        for k in self.__dict__:
+            self.__dict__[k] = self.__dict__[k].to(device=target_device,
+                                                   non_blocking=non_blocking)
 
 
 @dataclass
