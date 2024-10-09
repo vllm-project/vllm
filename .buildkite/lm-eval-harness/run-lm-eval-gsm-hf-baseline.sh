3138eca6ae9e16296656441ee7b2c484b1f3db7f
Michael Goin <michael@neuralmagic.com>
2024-10-08 21:22:31 -0400

    Add `lm-eval` directly to requirements-test.txt (#9161)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- .buildkite/lm-eval-harness/run-lm-eval-gsm-hf-baseline.sh
+++ .buildkite/lm-eval-harness/run-lm-eval-gsm-hf-baseline.sh
@@ -2,7 +2,7 @@
 # We can use this script to compute baseline accuracy on GSM for transformers.
 #
 # Make sure you have lm-eval-harness installed:
-#   pip install git+https://github.com/EleutherAI/lm-evaluation-harness.git@9516087b81a61d0e220b22cc1b75be76de23bc10
+#   pip install lm-eval==0.4.4
 
 usage() {
     echo``
