47dd49ef25e09ca6313d2ba64a4ab442c72f9e95
youkaichao <youkaichao@gmail.com>
2024-10-10 15:54:23 -0700

    [torch.compile] generic decorators (#9258)
    Signed-off-by: Sumit Dubey <sumit.dubey2@ibm.com>

--- vllm/model_executor/models/gemma2.py
+++ vllm/model_executor/models/gemma2.py
@@ -21,7 +21,7 @@ from torch import nn
 from transformers import Gemma2Config
 
 from vllm.attention import Attention, AttentionMetadata
-from vllm.compilation.decorators import support_compile_llama_style
+from vllm.compilation.decorators import support_torch_compile
 from vllm.config import CacheConfig, LoRAConfig
 from vllm.distributed import get_pp_group, get_tensor_model_parallel_world_size
 from vllm.logger import init_logger
@@ -239,7 +239,13 @@ class Gemma2DecoderLayer(nn.Module):
         return hidden_states, residual
 
 
-@support_compile_llama_style
+@support_torch_compile(
+    dynamic_arg_dims={
+        "input_ids": 0,
+        "positions": 0,
+        "inputs_embeds": 0,
+        "intermediate_tensors": 0,
+    })
 class Gemma2Model(nn.Module):
 
     def __init__(
