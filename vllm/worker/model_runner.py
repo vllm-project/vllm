c5bdf64887725a5719bfb304419318fdc3f49ef0
Kuntai Du <kuntai@uchicago.edu>
2024-10-10 09:35:48 -0700

    Adjust to latest changes of `kv_caches`: it is now always a tensor.

--- vllm/worker/model_runner.py
+++ vllm/worker/model_runner.py
@@ -1774,7 +1774,7 @@ class ModelRunner(GPUModelRunnerBase[ModelInputForGPUWithSamplingMetadata]):
         prefill_meta = model_input.attn_metadata.prefill_metadata
 
         # check if the current run is profiling
-        is_profile_run = (kv_caches is None) or (kv_caches[0] is None)
+        is_profile_run = (kv_caches.numel() == 0)
         # check if the current run is prefill
         is_prefill_run = prefill_meta is not None
 
@@ -1796,7 +1796,7 @@ class ModelRunner(GPUModelRunnerBase[ModelInputForGPUWithSamplingMetadata]):
         prefill_meta = model_input.attn_metadata.prefill_metadata
 
         # check if the current run is profiling
-        is_profile_run = (kv_caches is None) or (kv_caches[0] is None)
+        is_profile_run = (kv_caches.numel() == 0)
         # check if the current run is prefill
         is_prefill_run = prefill_meta is not None
 
