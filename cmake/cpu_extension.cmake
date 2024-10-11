e3c67400442ea7231c7bfb5cf11e4375761e5383
Sanket Kale <sanket.kale@fujitsu.com>
2024-10-11 09:35:08 +0530

    fixed typos
    Signed-off-by: Sanket Kale <sanketk.kale@fujitsu.com>

--- cmake/cpu_extension.cmake
+++ cmake/cpu_extension.cmake
@@ -100,7 +100,7 @@ elseif (ASIMD_FOUND)
         )
         
 else()
-    message(FATAL_ERROR "vLLM CPU backend requires AVX512, AVX2, Power9+, or ARMv8.")
+    message(FATAL_ERROR "vLLM CPU backend requires AVX512, AVX2, Power9+ ISA or ARMv8 support.")
 endif()
 
 #
