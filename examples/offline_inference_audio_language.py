3fa3e832eadebfc155e704571a3ba140994f72d0
云楚 <fay.cyf@alibaba-inc.com>
2024-10-12 12:27:45 +0800

    update BaseMultiModalItemTracker._placeholder_str and offline_inference_audio_language

--- examples/offline_inference_audio_language.py
+++ examples/offline_inference_audio_language.py
@@ -41,9 +41,27 @@ def run_ultravox(question, audio_count):
     stop_token_ids = None
     return llm, prompt, stop_token_ids
 
+# Qwen2-Audio
+def run_qwen2_audio(question, audio_count): 
+    model_name = "Qwen/Qwen2-Audio-7B-Instruct"
+
+    llm = LLM(
+        model=model_name,
+        max_model_len=4096,
+        max_num_seqs=5,
+        limit_mm_per_prompt={"audio": audio_count}
+    )
+
+    prompt = ("<|im_start|>system\nYou are a helpful assistant.<|im_end|>\n"
+              "<|im_start|>user\nAudio 1: <|audio_bos|><|AUDIO|><|audio_eos|>"
+              f"{question}<|im_end|>\n"
+              "<|im_start|>assistant\n")
+    stop_token_ids = None
+    return llm, prompt, stop_token_ids
 
 model_example_map = {
     "ultravox": run_ultravox,
+    "qwen2_audio": run_qwen2_audio
 }
 
 
