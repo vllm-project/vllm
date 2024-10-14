c7323ca70deca14d639eed2c8f3b5910f23af69f
Bill Nell <bill@neuralmagic.com>
2024-10-14 20:08:28 +0000

    work w/torch.compile

--- vllm/distributed/parallel_state.py
+++ vllm/distributed/parallel_state.py
@@ -46,7 +46,7 @@ from vllm.platforms import current_platform
 from vllm.utils import supports_custom_op
 
 
-torch._inductor.config._micro_pipeline_tp = True
+#torch._inductor.config._micro_pipeline_tp = True
 
 
 @dataclass
@@ -952,12 +952,12 @@ def graph_capture():
 
 logger = init_logger(__name__)
 
-_ENABLE_CUSTOM_ALL_REDUCE = False # True
+_ENABLE_CUSTOM_ALL_REDUCE = True
 
 
 def set_custom_all_reduce(enable: bool):
     global _ENABLE_CUSTOM_ALL_REDUCE
-    _ENABLE_CUSTOM_ALL_REDUCE = False #enable
+    _ENABLE_CUSTOM_ALL_REDUCE = enable
 
 
 def init_distributed_environment(
