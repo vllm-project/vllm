329f4afeeeab313f0c06659007ec9b819351f0d2
luka <luka@neuralmagic.com>
2024-10-15 16:36:21 -0400

    Reformat test

--- tests/model_executor/test_enabled_custom_ops.py
+++ tests/model_executor/test_enabled_custom_ops.py
@@ -20,18 +20,30 @@ from vllm.model_executor.layers.layernorm import RMSNorm
         ("", 3, [False] * 4, False),
         ("", 4, [False] * 4, False),  # None by default
         # Explicitly enabling/disabling
+        #
         # Default: all
-        ("+rms_norm,-silu_and_mul", 0, [True, False, True, True], True),  # All but SiluAndMul
-        ("none,-rms_norm,+relu2", 0, [False, False, False, True], False),  # Only ReLUSquaredActivation
-        ("all,-silu_and_mul", 1, [True, False, True, True], True),  # All but SiluAndMul
-        ("-relu2", 1, [True, True, True, False], True),  # All but ReLUSquaredActivation
-        ("none,-relu2,+gelu_and_mul,+silu_and_mul", 2,[False, True, True, False], False),  # GeluAndMul and SiluAndMul
-        ("-rms_norm", 2, [False, True, True, True], True),  # All but RMSNorm
+        #
+        # All but SiluAndMul
+        ("+rms_norm,-silu_and_mul", 0, [1, 0, 1, 1], True),
+        # Only ReLUSquaredActivation
+        ("none,-rms_norm,+relu2", 0, [0, 0, 0, 1], False),
+        # All but SiluAndMul
+        ("all,-silu_and_mul", 1, [1, 0, 1, 1], True),
+        # All but ReLUSquaredActivation
+        ("-relu2", 1, [1, 1, 1, 0], True),
+        # GeluAndMul and SiluAndMul
+        ("none,-relu2,+gelu_and_mul,+silu_and_mul", 2, [0, 1, 1, 0], False),
+        # All but RMSNorm
+        ("-rms_norm", 2, [0, 1, 1, 1], True),
+        #
         # Default: none
-        ("-silu_and_mul,+relu2", 3, [False, False, False, True], False),  # Only ReLUSquaredActivation
-        ("all,-rms_norm", 4, [False, True, True, True], True),  # All but RMSNorm
+        #
+        # Only ReLUSquaredActivation
+        ("-silu_and_mul,+relu2", 3, [0, 0, 0, 1], False),
+        # All but RMSNorm
+        ("all,-rms_norm", 4, [0, 1, 1, 1], True),
     ])
-def test_enabled_ops(env: str, torch_level: int, ops_enabled: List[bool],
+def test_enabled_ops(env: str, torch_level: int, ops_enabled: List[int],
                      default_on: bool):
     os.environ["VLLM_CUSTOM_OPS"] = env
     os.environ["VLLM_TORCH_COMPILE_LEVEL"] = str(torch_level)
@@ -41,10 +53,10 @@ def test_enabled_ops(env: str, torch_level: int, ops_enabled: List[bool],
 
     assert CustomOp.default_on() == default_on
 
-    assert RMSNorm(1024)._enabled() == ops_enabled[0]
-    assert SiluAndMul()._enabled() == ops_enabled[1]
-    assert GeluAndMul()._enabled() == ops_enabled[2]
-    assert ReLUSquaredActivation()._enabled() == ops_enabled[3]
+    assert RMSNorm(1024)._enabled() == bool(ops_enabled[0])
+    assert SiluAndMul()._enabled() == bool(ops_enabled[1])
+    assert GeluAndMul()._enabled() == bool(ops_enabled[2])
+    assert ReLUSquaredActivation()._enabled() == bool(ops_enabled[3])
 
 
 @pytest.mark.parametrize(
