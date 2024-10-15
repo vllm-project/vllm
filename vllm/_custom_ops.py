f4e7d4720955839c66f0dd204b496bb2594a2459
charlifu <charlifu@amd.com>
2024-10-15 17:53:22 +0000

    add moe_sum and improve the thread number of moe align

--- vllm/_custom_ops.py
+++ vllm/_custom_ops.py
@@ -830,11 +830,14 @@ def selective_scan_fwd(
 
 
 # moe
+def moe_sum(input: torch.Tensor, output: torch.Tensor):
+    torch.ops._moe_C.moe_sum(input, output)
+
 def moe_align_block_size(topk_ids: torch.Tensor, num_experts: int,
                          block_size: int, sorted_token_ids: torch.Tensor,
                          experts_ids: torch.Tensor,
                          num_tokens_post_pad: torch.Tensor) -> None:
-    torch.ops._C.moe_align_block_size(topk_ids, num_experts, block_size,
+    torch.ops._moe_C.moe_align_block_size(topk_ids, num_experts, block_size,
                                       sorted_token_ids, experts_ids,
                                       num_tokens_post_pad)
 
