c5b83ae238e7bf7eb3699381483dd6edf34cc2e5
Alex-Brooks <Alex.Brooks@ibm.com>
2024-10-15 07:24:57 -0400

    Reenable single image test for minicpmv

--- tests/models/decoder_only/vision_language/test_models.py
+++ tests/models/decoder_only/vision_language/test_models.py
@@ -175,7 +175,7 @@ VLM_TEST_SETTINGS = {
     ),
     "minicpmv": VLMTestInfo(
         models="openbmb/MiniCPM-Llama3-V-2_5",
-        test_type=(VLMTestType.MULTI_IMAGE),
+        test_type=(VLMTestType.IMAGE, VLMTestType.MULTI_IMAGE),
         prompt_formatter=lambda img_prompt: f"<|begin_of_text|><|start_header_id|>user<|end_header_id|>\n\n{img_prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",  # noqa: E501
         img_idx_to_prompt=lambda idx: "(<image>./</image>)\n",
         max_model_len=4096,
