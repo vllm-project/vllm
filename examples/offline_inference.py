5cf508c04edd967973d9f0be7ed1cbe9b9e9f310
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2024-10-15 10:06:52 -0700

    Revert

--- examples/offline_inference.py
+++ examples/offline_inference.py
@@ -11,7 +11,7 @@ prompts = [
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
 # Create an LLM.
-llm = LLM(model="meta-llama/Llama-3.1-8B")
+llm = LLM(model="facebook/opt-125m")
 # Generate texts from the prompts. The output is a list of RequestOutput objects
 # that contain the prompt, generated text, and other information.
 outputs = llm.generate(prompts, sampling_params)
