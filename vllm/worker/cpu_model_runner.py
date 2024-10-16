0b9caedfb0f3daf0f41a7df61520dfde35230d93
Chen Zhang <zhangch99@outlook.com>
2024-10-15 22:05:26 -0700

    support sliding window in flash attention backend

--- vllm/worker/cpu_model_runner.py
+++ vllm/worker/cpu_model_runner.py
@@ -419,7 +419,6 @@ class CPUModelRunner(ModelRunnerBase[ModelInputForCPU]):
         self.block_size = cache_config.block_size
         self.attn_backend = get_attn_backend(
             self.model_config.get_head_size(),
-            self.model_config.get_sliding_window(),
             self.model_config.dtype,
             self.kv_cache_dtype,
             self.block_size,
