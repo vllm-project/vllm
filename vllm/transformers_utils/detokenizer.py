f4f573b760b8046fde710f8b6d53cfbe6d0472f2
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2024-10-18 09:56:16 -0700

    yapf

--- vllm/transformers_utils/detokenizer.py
+++ vllm/transformers_utils/detokenizer.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, List, Optional
 
 from vllm.sequence import (VLLM_INVALID_TOKEN_ID, Logprob, SamplingParams,
                            Sequence, SequenceGroup)
