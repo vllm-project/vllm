3e0320602141974507e7460f3f753c3644f2d0dd
Cyrus Leung <tlleungac@connect.ust.hk>
2024-10-19 02:31:58 +0800

    [Model] Add user-configurable task for models that support both generation and embedding (#9424)
    Signed-off-by: Alvant <alvasian@yandex.ru>

--- vllm/engine/llm_engine.py
+++ vllm/engine/llm_engine.py
@@ -344,7 +344,7 @@ class LLMEngine:
             observability_config=self.observability_config,
         )
 
-        if not self.model_config.embedding_mode:
+        if self.model_config.task != "embedding":
             self._initialize_kv_caches()
 
         # If usage stat is enabled, collect relevant info.
@@ -1163,7 +1163,7 @@ class LLMEngine:
                             seq_group.metrics.model_execute_time = (
                                 o.model_execute_time)
 
-            if self.model_config.embedding_mode:
+            if self.model_config.task == "embedding":
                 self._process_sequence_group_outputs(seq_group, output)
             else:
                 self.output_processor.process_prompt_logprob(seq_group, output)
@@ -1901,9 +1901,6 @@ class LLMEngine:
 
     def is_encoder_decoder_model(self):
         return self.input_preprocessor.is_encoder_decoder_model()
-
-    def is_embedding_model(self):
-        return self.model_config.is_embedding_model
 
     def _validate_model_inputs(self, inputs: Union[DecoderOnlyInputs,
                                                    EncoderDecoderInputs]):
