4239dfe420cec9a28c84f6f6370dec2cef28d083
Lucas Wilkinson <lwilkinson@neuralmagic.com>
2024-10-18 04:17:14 +0000

    remove extra check

--- csrc/quantization/cutlass_w8a8/scaled_mm_entry.cu
+++ csrc/quantization/cutlass_w8a8/scaled_mm_entry.cu
@@ -139,7 +139,6 @@ void cutlass_scaled_mm(torch::Tensor& c, torch::Tensor const& a,
 
   if (version_num >= 75) {
     // Turing
-    TORCH_CHECK(version_num >= 75);
     cutlass_scaled_mm_sm75(c, a, b, a_scales, b_scales, bias);
     return;
   }
