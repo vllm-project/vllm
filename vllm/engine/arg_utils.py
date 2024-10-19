454ce16b942b5b409aef6c09470d02a204e47e16
Joe Runde <Joseph.Runde@ibm.com>
2024-10-18 22:26:36 -0500

    [Doc] update gpu-memory-utilization flag docs (#9507)
    Signed-off-by: Joe Runde <Joseph.Runde@ibm.com>
    Signed-off-by: Sumit Dubey <sumit.dubey2@ibm.com>

--- vllm/engine/arg_utils.py
+++ vllm/engine/arg_utils.py
@@ -428,7 +428,11 @@ class EngineArgs:
             help='The fraction of GPU memory to be used for the model '
             'executor, which can range from 0 to 1. For example, a value of '
             '0.5 would imply 50%% GPU memory utilization. If unspecified, '
-            'will use the default value of 0.9.')
+            'will use the default value of 0.9. This is a global gpu memory '
+            'utilization limit, for example if 50%% of the gpu memory is '
+            'already used before vLLM starts and --gpu-memory-utilization is '
+            'set to 0.9, then only 40%% of the gpu memory will be allocated '
+            'to the model executor.')
         parser.add_argument(
             '--num-gpu-blocks-override',
             type=int,
