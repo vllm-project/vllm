c84873521c80d79d8dc2fc57031eb483c3a16ed4
Robert Shaw <rshaw@neuralmagic.com>
2024-10-20 17:55:48 +0000

    proper merge

--- vllm/model_executor/layers/quantization/kernels/scaled_mm/__init__.py
+++ vllm/model_executor/layers/quantization/kernels/scaled_mm/__init__.py
@@ -1,69 +1,64 @@
-import os
-from typing import Dict, List, Optional, Type
+from typing import List, Optional, Type
 
-from vllm.model_executor.layers.quantization.kernels.scaled_mm.cutlass import (
-    CutlassScaledMMLinearKernel)
-from vllm.model_executor.layers.quantization.kernels.scaled_mm.ScaledMMLinearKernel import (  # noqa: E501
-    ScaledMMLinearKernel, ScaledMMLinearLayerConfig)
-from vllm.model_executor.layers.quantization.kernels.scaled_mm.xla import (
-    XLAScaledMMLinearKernel)
-from vllm.platforms import PlatformEnum, current_platform
+import vllm.envs as envs
+from vllm.model_executor.layers.quantization.kernels.exllama import (
+    ExllamaLinearKernel)
+from vllm.model_executor.layers.quantization.kernels.machete import (
+    MacheteLinearKernel)
+from vllm.model_executor.layers.quantization.kernels.marlin import (
+    MarlinLinearKernel)
+from vllm.model_executor.layers.quantization.kernels.MPLinearKernel import (
+    MPLinearKernel, MPLinearLayerConfig)
+from vllm.platforms import current_platform
 
 # in priority/performance order (when available)
-_POSSIBLE_KERNELS: Dict[PlatformEnum, List[Type[ScaledMMLinearKernel]]] = {
-    PlatformEnum.CPU: [CutlassScaledMMLinearKernel],
-    PlatformEnum.CUDA: [CutlassScaledMMLinearKernel],
-    PlatformEnum.TPU: [XLAScaledMMLinearKernel],
-}
+_POSSIBLE_KERNELS: List[Type[MPLinearKernel]] = [
+    MacheteLinearKernel,
+    MarlinLinearKernel,
+    ExllamaLinearKernel,
+]
 
 
-def choose_scaled_mm_linear_kernel(
-        config: ScaledMMLinearLayerConfig,
-        compute_capability: Optional[int] = None
-) -> Type[ScaledMMLinearKernel]:
+def choose_mp_linear_kernel(
+        config: MPLinearLayerConfig,
+        compute_capability: Optional[int] = None) -> Type[MPLinearKernel]:
     """
-    Choose an ScalledMMLinearKernel that can implement the given config for the 
-    given compute capability. Attempts to choose the best kernel in terms of 
-    performance.
+    Choose an MPLinearKernel that can implement the given config for the given
+     compute capability. Attempts to choose the best kernel in terms of 
+     performance.
 
     Args:
-        config (ScaledMMLinearLayerConfig): Description of the linear layer 
-            to be implemented.
+        config (MPLinearLayerConfig): Description of the linear layer to be 
+          implemented.
         compute_capability (Optional[int], optional): The compute capability of
-            the target device, if None uses `current_platform` to get the 
-            compute capability. Defaults to None.
+          the target device, if None uses `current_platform` to get the compute 
+          capability. Defaults to None.
 
     Raises:
         ValueError: If no kernel can implement the given config.
 
     Returns:
-        Type[ScaledMMLinearKernel]: Chosen kernel.
+        Type[MPLinearKernel]: Chosen kernel.
     """
-
     if compute_capability is None:
+        if current_platform is None:
+            raise ValueError("Cannot determine compute capability")
         _cc = current_platform.get_device_capability()
-        if _cc is not None:
-            compute_capability = _cc[0] * 10 + _cc[1]
+        compute_capability = _cc[0] * 10 + _cc[1]
 
     failure_reasons = []
-    for kernel in _POSSIBLE_KERNELS[current_platform._enum]:
-        if kernel.__name__ in os.environ.get("VLLM_DISABLED_KERNELS", "")\
-            .split(","):
+    for kernel in _POSSIBLE_KERNELS:
+        if kernel.__name__ in envs.VLLM_DISABLED_KERNELS:
             failure_reasons.append(
                 f' {kernel.__name__} disabled by environment variable')
             continue
 
-        # If the current platform uses compute_capability,
-        # make sure the kernel supports the compute cability.
-        if compute_capability is not None:
-            kernel_min_capability = kernel.get_min_capability()
-            if (kernel_min_capability is not None
-                    and kernel_min_capability > compute_capability):
-                failure_reasons.append(
-                    f"{kernel.__name__} requires capability "
-                    f"{kernel_min_capability}, current compute capability "
-                    f"is {compute_capability}")
-                continue
+        if kernel.get_min_capability() > compute_capability:
+            failure_reasons.append(
+                f"{kernel.__name__} requires capability "
+                f"{kernel.get_min_capability()}, current compute capability "
+                f"is {compute_capability}")
+            continue
 
         can_implement, failure_reason = kernel.can_implement(config)
         if can_implement:
@@ -75,5 +70,5 @@ def choose_scaled_mm_linear_kernel(
 
     raise ValueError(
         "Failed to find a kernel that can implement the "\
-        "ScaledMM linear layer. Reasons: \n"
+        "WNA16 linear layer. Reasons: \n"
         + '\n'.join(failure_reasons))
