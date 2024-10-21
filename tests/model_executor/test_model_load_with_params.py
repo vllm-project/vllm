7c051e46a89695eaf84ad4136f485788b4d49cc5
Flavia Beo <flavia.beo@ibm.com>
2024-10-21 16:25:21 -0300

    Adds pooling config test
    Signed-off-by: Flavia Beo <flavia.beo@ibm.com>

--- tests/model_executor/test_model_load_with_params.py
+++ tests/model_executor/test_model_load_with_params.py
@@ -6,6 +6,7 @@ MAX_MODEL_LEN = 128
 MODEL_NAME = os.environ.get("MODEL_NAME",
                             "sentence-transformers/all-MiniLM-L12-v2")
 REVISION = os.environ.get("REVISION", "main")
+QUANTIZATION = os.environ.get("QUANTIZATION", "auto")
 
 
 def test_model_loading_with_params(vllm_runner):
@@ -15,9 +16,8 @@ def test_model_loading_with_params(vllm_runner):
     with vllm_runner(model_name=MODEL_NAME,
                      revision=REVISION,
                      dtype=torch.half if QUANTIZATION == "gptq" else "auto",
-                     max_model_len=MAX_MODEL_LEN,
-                     tensor_parallel_size=2) as model:
-
+                     max_model_len=MAX_MODEL_LEN
+                     ) as model:
         output = model.encode("Write a short story about a robot that dreams for the first time.\n")
         print(output)
         assert output
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,7 @@
 import pytest
 
 from vllm.config import ModelConfig
+from vllm.model_executor.layers.pooler import PoolingConfig
 
 
 @pytest.mark.parametrize(("model_id", "expected_task"), [
@@ -100,6 +101,23 @@ def test_get_sliding_window():
 
     mistral_model_config.hf_config.sliding_window = TEST_SLIDING_WINDOW
     assert mistral_model_config.get_sliding_window() == TEST_SLIDING_WINDOW
+
+
+def test_get_pooling_config():
+    minilm_model_config = ModelConfig(
+        "sentence-transformers/all-MiniLM-L12-v2",
+        "sentence-transformers/all-MiniLM-L12-v2",
+        tokenizer_mode="auto",
+        trust_remote_code=False,
+        seed=0,
+        dtype="float16",
+        revision=None,
+    )
+
+    minilm_pooling_config = minilm_model_config.get_pooling_config()
+
+    assert isinstance(minilm_model_config.pooling_config, PoolingConfig)
+    assert minilm_pooling_config.normalize  
 
 
 def test_rope_customization():
