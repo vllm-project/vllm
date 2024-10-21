3c195471bb194af38432845cd12a0fa893214d4a
Yunmeng Chen <imanchen103@gmail.com>
2024-10-21 04:22:56 -0700

    remove docker exec/run

--- .buildkite/run-code-dev-test.sh
+++ .buildkite/run-code-dev-test.sh
@@ -3,41 +3,37 @@
 set -ex
 BUILDKITE_COMMIT=$BUILDKITE_COMMIT
 
-# Setup cleanup
-remove_docker_container() { docker rm -f vllm-dev-test || true; }
-trap remove_docker_container EXIT
-remove_docker_container
+# Assume this test is in vllm ci test image which already install vllm
+# Uninstall first for testing
+pip uninstall -y vllm
 
-# Use docker from building wheel stage
-DOCKER_BUILDKIT=1 docker build --build-arg max_jobs=16 --build-arg USE_SCCACHE=1 --tag vllm-ci:dev --target base --progress plain .
-docker run -dit --entrypoint /bin/bash --privileged=true --network host --name vllm-dev-test vllm-ci:dev
+# Test directory
+TEST_DIR=/tmp/vllm_test
+mkdir -p ${TEST_DIR}
 
-# Install vllm dev wheel (latest nightly wheel)
-docker exec vllm-dev-test bash -c "
-    pip install https://vllm-wheels.s3.us-west-2.amazonaws.com/${BUILDKITE_COMMIT}/vllm-1.0.0.dev-cp38-abi3-manylinux1_x86_64.whl && \
-    pip install setuptools-scm"
+# Install vllm dev wheel
+pip install https://vllm-wheels.s3.us-west-2.amazonaws.com/${BUILDKITE_COMMIT}/vllm-1.0.0.dev-cp38-abi3-manylinux1_x86_64.whl && \
+pip install setuptools-scm # Hack to generate __version.py
 
 # Test python_only_dev.py
-docker exec vllm-dev-test bash -c '
-    git clone https://github.com/vllm-project/vllm.git --no-checkout && \
-    cd vllm && \
-    git checkout ${BUILDKITE_COMMIT} && \
-    python3 python_only_dev.py && \
-    cd / && \
-    python3 -c "import vllm; print(vllm.__file__)"'
+cd ${TEST_DIR} && \
+git clone https://github.com/vllm-project/vllm.git --no-checkout && \
+cd vllm && \
+git checkout ${BUILDKITE_COMMIT} && \
+python3 python_only_dev.py && \
+cd / && \
+python3 -c "import vllm; print(vllm.__file__)"
 
 # Test code change
-docker exec vllm-dev-test bash -c '
-    cd vllm && \
-    echo "test_var=123456" > vllm/fake_module.py && \
-    cd / && \
-    python3 -c "import vllm.fake_module; print(vllm.fake_module.test_var)"'
+cd ${TEST_DIR}/vllm && \
+echo "test_var=123456" > vllm/fake_module.py && \
+cd / && \
+python3 -c "import vllm.fake_module; print(vllm.fake_module.test_var)"
 
 # Test uninstall/updateing condition
-docker exec vllm-dev-test bash -c "
-    cd vllm && \
-    python3 python_only_dev.py --quit-dev && \
-    cd / && \
-    pip uninstall -y vllm && \
-    pip install https://vllm-wheels.s3.us-west-2.amazonaws.com/${BUILDKITE_COMMIT}/vllm-1.0.0.dev-cp38-abi3-manylinux1_x86_64.whl && \
-    pip show vllm"
+cd ${TEST_DIR}/vllm && \
+python3 python_only_dev.py --quit-dev && \
+cd / && \
+pip uninstall -y vllm && \
+pip install https://vllm-wheels.s3.us-west-2.amazonaws.com/${BUILDKITE_COMMIT}/vllm-1.0.0.dev-cp38-abi3-manylinux1_x86_64.whl && \
+pip show vllm
