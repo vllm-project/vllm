0a551cbd546d67d35fb40451731c1763ca9cb86d
KuntaiDu <kuntai@uchicago.edu>
2024-10-22 02:21:01 +0000

    Remove evictor_v1 as evictor_v1 is for block manager v1, which has been removed.

--- examples/offline_inference.py
+++ examples/offline_inference.py
@@ -11,7 +11,7 @@ prompts = [
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
 # Create an LLM.
-llm = LLM(model="facebook/opt-125m")
+llm = LLM(model="facebook/opt-125m", enable_prefix_caching=True)
 # Generate texts from the prompts. The output is a list of RequestOutput objects
 # that contain the prompt, generated text, and other information.
 outputs = llm.generate(prompts, sampling_params)
