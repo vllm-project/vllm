25adeb1a8004f709c60b3dbc1e99302e80f368b3
xendo <xendoo@gmail.com>
2024-10-22 14:51:41 +0200

    [Neuron] [Bugfix] Fix neuron startup (#9374)
    Co-authored-by: Jerzy Zagorski <jzagorsk@amazon.com>
    Signed-off-by: Sumit Dubey <sumit.dubey2@ibm.com>

--- vllm/triton_utils/importing.py
+++ vllm/triton_utils/importing.py
@@ -1,10 +1,13 @@
 from importlib.util import find_spec
 
 from vllm.logger import init_logger
+from vllm.platforms import current_platform
 
 logger = init_logger(__name__)
 
-HAS_TRITON = find_spec("triton") is not None
+# neuron has too old torch
+HAS_TRITON = find_spec(
+    "triton") is not None and not current_platform.is_neuron()
 
 if not HAS_TRITON:
     logger.info("Triton not installed; certain GPU-related functions"
