d63763fe8fea29e127ec81ce647cd751d90240b7
Cyrus Leung <cyrus.tl.leung@gmail.com>
2024-10-23 14:20:19 +0800

    Fix indent

--- vllm/triton_utils/importing.py
+++ vllm/triton_utils/importing.py
@@ -6,9 +6,9 @@ from vllm.platforms import current_platform
 logger = init_logger(__name__)
 
 HAS_TRITON = (
-  find_spec("triton") is not None
-  and not current_platform.is_xpu()  # Not compatible
-  and not current_platform.is_neuron()  # neuron has too old torch
+    find_spec("triton") is not None
+    and not current_platform.is_xpu()  # Not compatible
+    and not current_platform.is_neuron()  # neuron has too old torch
 )
 
 if not HAS_TRITON:
