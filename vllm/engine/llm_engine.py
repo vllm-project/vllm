7d193f32030dd52116efdac212335a51618e088f
yangsijia.614 <yangsijia.614@bytedance.com>
2024-10-25 00:31:41 +0800

    fix: rebase, add LLMEngine.is_embedding_model()
    Signed-off-by: yangsijia.614 <yangsijia.614@bytedance.com>

--- vllm/engine/llm_engine.py
+++ vllm/engine/llm_engine.py
@@ -21,7 +21,8 @@ from vllm.config import (CacheConfig, DecodingConfig, DeviceConfig,
 from vllm.core.scheduler import (ScheduledSequenceGroup, Scheduler,
                                  SchedulerOutputs)
 from vllm.engine.arg_utils import EngineArgs
-from vllm.engine.metrics_types import StatLoggerBase, Stats, GeneralStats, EmbeddingStats
+from vllm.engine.metrics_types import (StatLoggerBase, Stats,
+                                       GeneralStats, EmbeddingStats)
 from vllm.engine.output_processor.interfaces import (
     SequenceGroupOutputProcessor)
 from vllm.engine.output_processor.stop_checker import StopChecker
@@ -1952,6 +1953,9 @@ class LLMEngine:
 
     def is_encoder_decoder_model(self):
         return self.input_preprocessor.is_encoder_decoder_model()
+    
+    def is_embedding_model(self):
+        return self.model_config.task == "embedding"
 
     def _validate_model_inputs(self, inputs: Union[DecoderOnlyInputs,
                                                    EncoderDecoderInputs]):
