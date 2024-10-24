28fb4f5ae7cb65743cf3c6e9822b50919ff68bfb
crzbulabula <532741407@qq.com>
2024-10-24 13:43:06 +0800

    success testing some models

--- vllm/model_executor/models/orion.py
+++ vllm/model_executor/models/orion.py
@@ -11,6 +11,7 @@ from torch import nn
 from transformers import PretrainedConfig
 
 from vllm.attention import Attention, AttentionMetadata
+from vllm.compilation.decorators import support_torch_compile
 from vllm.config import CacheConfig
 from vllm.distributed import get_pp_group, get_tensor_model_parallel_world_size
 from vllm.model_executor.layers.activation import SiluAndMul
@@ -206,6 +207,7 @@ class OrionDecoderLayer(nn.Module):
         return hidden_states, None
 
 
+@support_torch_compile
 class OrionModel(nn.Module):
 
     def __init__(
--- vllm/model_executor/models/xverse.py
+++ vllm/model_executor/models/xverse.py
@@ -27,6 +27,7 @@ from torch import nn
 from transformers import PretrainedConfig
 
 from vllm.attention import Attention, AttentionMetadata
+from vllm.compilation.decorators import support_torch_compile
 from vllm.config import CacheConfig, LoRAConfig
 from vllm.distributed import get_pp_group, get_tensor_model_parallel_world_size
 from vllm.model_executor.layers.activation import SiluAndMul
@@ -220,6 +221,7 @@ class XverseDecoderLayer(nn.Module):
         return hidden_states, residual
 
 
+@support_torch_compile
 class XverseModel(nn.Module):
 
     def __init__(
