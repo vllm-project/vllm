ea27fc17b9a1942c6bc26ccc8360903d90307878
youkaichao <youkaichao@gmail.com>
2024-10-26 09:59:49 -0700

    add tests
    Signed-off-by: youkaichao <youkaichao@gmail.com>

--- tests/compile/piecewise/test_simple.py
+++ tests/compile/piecewise/test_simple.py
@@ -1,35 +1,41 @@
-import torch
-from vllm.compilation.decorators import support_torch_compile
-from vllm.compilation.compile_context import set_compile_context
-from vllm.plugins import set_non_cudagraph_ops
-set_non_cudagraph_ops(["silly.attention"])
+import os
 
 import torch
 from torch import nn
 
+from vllm.compilation.compile_context import set_compile_context
+from vllm.compilation.decorators import support_torch_compile
+from vllm.compilation.levels import CompilationLevel
+from vllm.plugins import set_non_cudagraph_ops
+
+set_non_cudagraph_ops(["silly.attention"])
+
 global_counter = 0
 
+
 @torch.library.custom_op("silly::attention", mutates_args=["out"])
-def silly_attention(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, out: torch.Tensor) -> None:
+def silly_attention(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor,
+                    out: torch.Tensor) -> None:
     global global_counter
     global_counter += 1
     print(f"{global_counter=}")
     out.copy_(q)
     out[0] += 1
 
+
 @silly_attention.register_fake
-def _(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, out: torch.Tensor) -> None:
+def _(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor,
+      out: torch.Tensor) -> None:
     return
+
 
 @support_torch_compile
 class SillyModel(nn.Module):
+
     def __init__(self) -> None:
         super().__init__()
 
-    def forward(
-        self,
-        x: torch.Tensor
-    ) -> torch.Tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         Overall effect:
         x += 1
@@ -49,7 +55,10 @@ class SillyModel(nn.Module):
         x = x + 1
         return x
 
+
 def test_simple_piecewise_compile():
+
+    os.environ["VLLM_TORCH_COMPILE_LEVEL"] = str(CompilationLevel.PIECEWISE)
 
     model = SillyModel()
 
