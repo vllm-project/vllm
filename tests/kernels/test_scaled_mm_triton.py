cb3c211bf20293e7ded043d51d108596b684eedc
Randall Smith <Randall.Smith@amd.com>
2024-10-25 21:51:12 -0500

    noqa
    Signed-off-by: Randall Smith <Randall.Smith@amd.com>

--- tests/kernels/test_scaled_mm_triton.py
+++ tests/kernels/test_scaled_mm_triton.py
@@ -7,8 +7,7 @@ from typing import Optional, Type
 import pytest
 import torch
 
-from vllm.model_executor.layers.quantization.compressed_tensors.\
-     scaled_mm_triton import scaled_mm_triton # noqa
+from vllm.model_executor.layers.quantization.compressed_tensors.scaled_mm_triton import scaled_mm_triton # noqa
 from vllm.utils import seed_everything
 
 device = "cuda"
