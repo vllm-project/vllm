ac870491191887c79f122c50d0709c95a2d55f53
Shashwat Srijan <sssrijan@amazon.com>
2024-10-29 10:18:44 +0000

    Support speculation with neuronx-distributed-inference for batch 1
    cr: https://code.amazon.com/reviews/CR-157029558

---
+++ vllm/worker/multi_step_neuronx_distributed_model_runner.py
@@ -0,0 +1,54 @@
+from importlib.util import find_spec
+import torch
+from typing import List, Optional
+from vllm.config import (DeviceConfig, ModelConfig, ParallelConfig,
+                        SchedulerConfig, SpeculativeConfig)
+from vllm.model_executor.layers.sampler import SamplerOutput
+from vllm.multimodal import MultiModalInputs
+from vllm.sequence import IntermediateTensors
+from vllm.worker.neuronx_distributed_model_runner import NeuronxDistributedModelRunner
+
+class MultiStepNeuronModelRunner(NeuronxDistributedModelRunner):
+
+    def __init__(
+        self,
+        model_config: ModelConfig,
+        parallel_config: ParallelConfig,
+        scheduler_config: SchedulerConfig,
+        device_config: DeviceConfig,
+        speculation_config: SpeculativeConfig,
+    ):
+        super().__init__(model_config, parallel_config, scheduler_config, device_config)
+        self.speculation_config = speculation_config
+
+    def load_model(self) -> None:
+        from vllm.model_executor.model_loader.neuronx_distributed import get_neuron_speculation_model
+        assert self.scheduler_config.max_num_seqs == 1, "Only batch size 1 is currently supported for speculation using neuronx-distributed-inference." 
+        self.model = get_neuron_speculation_model(
+            self.model_config,
+            parallel_config=self.parallel_config,
+            scheduler_config=self.scheduler_config,
+            speculation_config=self.speculation_config)
+
+    @torch.inference_mode()
+    def execute_model(
+        self,
+        model_input,
+        kv_caches: Optional[List[torch.Tensor]] = None,
+        intermediate_tensors: Optional[IntermediateTensors] = None,
+        num_steps: int = 1,
+    ) -> Optional[List[SamplerOutput]]:
+        logits = self.model(
+            input_ids=model_input.input_tokens,
+            positions=model_input.input_positions,
+            input_block_ids=model_input.input_block_ids,
+            **MultiModalInputs.as_kwargs(model_input.multi_modal_kwargs or {},
+                                        device=self.device),
+        )
+
+        output = self.model.sample(
+            logits=logits,
+            sampling_metadata=model_input.sampling_metadata,
+        )
+        return output
+
