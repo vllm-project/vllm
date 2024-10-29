c4db4f8de0025c0b972073d120005c95c7c0f6cb
科英 <abatom@163.com>
2024-10-30 01:32:56 +0800

    [Misc] Add metrics for request queue time, forward time, and execute time (#9659)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/engine/metrics_types.py
+++ vllm/engine/metrics_types.py
@@ -46,6 +46,9 @@ class Stats:
     # Request stats (should have _requests suffix)
     #   Latency
     time_e2e_requests: List[float]
+    time_in_queue_requests: List[float]
+    model_forward_time_requests: List[float]
+    model_execute_time_requests: List[float]
     #   Metadata
     num_prompt_tokens_requests: List[int]
     num_generation_tokens_requests: List[int]
