e2e9811871a02dc125abf6d54b7fcfd2de82df3b
Jee Jee Li <pandaleefree@gmail.com>
2024-10-29 03:41:57 +0000

    Modify model code
    Signed-off-by: Jee Jee Li <pandaleefree@gmail.com>

--- vllm/model_executor/models/idefics3.py
+++ vllm/model_executor/models/idefics3.py
@@ -15,6 +15,8 @@
 """Inference-only Idefics3 model compatible with HuggingFace weights."""
 
 from typing import (
+    Any,
+    Callable,
     Iterable,
     List,
     Literal,
@@ -29,7 +31,7 @@ import torch
 import torch.utils.checkpoint
 from torch import nn
 
-from transformers import Idefics3Config
+from transformers import PretrainedConfig
 
 from vllm.logger import init_logger
 from vllm.attention import AttentionMetadata
@@ -37,25 +39,19 @@ from vllm.config import CacheConfig, MultiModalConfig
 from vllm.inputs import INPUT_REGISTRY, InputContext, LLMInputs
 from vllm.model_executor.layers.logits_processor import LogitsProcessor
 
+from vllm.model_executor.layers.linear import ReplicatedLinear
 from vllm.model_executor.layers.quantization import QuantizationConfig
 from vllm.model_executor.layers.sampler import Sampler, SamplerOutput
-
+from vllm.model_executor.layers.vocab_parallel_embedding import ParallelLMHead
 from vllm.model_executor.sampling_metadata import SamplingMetadata
 from vllm.multimodal import MULTIMODAL_REGISTRY
 from vllm.sequence import IntermediateTensors
-
 
 from .llama import LlamaModel
 from .idefics2_vision_model import (
     Idefics2VisionTransformer as Idefics3VisionTransformer, )
 from .interfaces import SupportsMultiModal
-from .siglip import (
-    SiglipVisionModel,
-    dummy_image_for_siglip,
-    dummy_seq_data_for_siglip,
-    get_max_siglip_image_tokens,
-    input_processor_for_siglip,
-)
+
 from .utils import (
     AutoWeightsLoader,
     flatten_bn,
@@ -83,67 +79,6 @@ class ImageEmbeddingInputs(TypedDict):
 ImageInputs = Union[ImagePixelInputs, ImageEmbeddingInputs]
 
 
-def input_processor_for_idefics3(ctx: InputContext, llm_inputs: LLMInputs):
-    multi_modal_data = llm_inputs.get("multi_modal_data")
-    if multi_modal_data is None or "image" not in multi_modal_data:
-        return llm_inputs
-    model_config = ctx.model_config
-    version = get_version_by_config(model_config.hf_config)
-    tokenizer = cached_get_tokenizer(
-        model_config.tokenizer,
-        trust_remote_code=model_config.trust_remote_code)
-    image_processor = cached_get_image_processor(model_config.tokenizer)
-
-    def get_placeholder(image_size: Tuple[int, int], num_image: int):
-        if version == (2, 0) or version == (2, 5):
-            return image_processor. \
-                get_slice_image_placeholder(image_size)
-        return image_processor. \
-            get_slice_image_placeholder(image_size, num_image)
-
-    prompt = llm_inputs.get("prompt")
-    token_ids = llm_inputs.get("prompt_token_ids")
-    if prompt is None:
-        prompt = tokenizer.decode(token_ids)
-
-    pattern = "(<image>./</image>)"
-    images = multi_modal_data["image"]
-    image_tags = re.findall(pattern, prompt)
-    if len(image_tags) == 0:
-        new_token_ids = token_ids
-        new_prompt = prompt
-    else:
-        if isinstance(images, dict):
-            image_size_list = images.get("image_size_list")
-            images = [images.get("image_embeds")]
-        else:
-            if isinstance(images, Image.Image):
-                images = [images]
-            image_size_list = [image.size for image in images]
-
-        text_chunks = prompt.split(pattern)
-        new_prompt_chunks: List[str] = []
-        for i in range(len(image_size_list)):
-            new_prompt_chunks += [
-                text_chunks[i],
-                get_placeholder(image_size_list[i], i)
-            ]
-        new_prompt_chunks.append(text_chunks[-1])
-        new_prompt = "".join(new_prompt_chunks)
-        new_token_ids = tokenizer.encode(new_prompt)
-
-    multi_modal_data["image"] = [
-        _build_image_input(ctx, image) for image in images
-    ]
-
-    llm_inputs = LLMInputs(
-        prompt_token_ids=new_token_ids,
-        prompt=new_prompt,
-        multi_modal_data=multi_modal_data,
-    )
-    return llm_inputs
-
-
 class Idefics3SimpleMLP(nn.Module):
 
     def __init__(self, config):
@@ -151,50 +86,11 @@ class Idefics3SimpleMLP(nn.Module):
         input_size = config.vision_config.hidden_size * (config.scale_factor**
                                                          2)
         output_size = config.text_config.hidden_size
-        self.proj = nn.Linear(input_size, output_size, bias=False)
+        self.proj = ReplicatedLinear(input_size, output_size, bias=False)
 
-    def forward(self, x):
-        return self.proj(x)
-
-
-# Copied from transformers.models.llama.modeling_llama.repeat_kv
-def repeat_kv(hidden_states: torch.Tensor, n_rep: int) -> torch.Tensor:
-    """
-    This is the equivalent of torch.repeat_interleave(x, dim=1, repeats=n_rep). The hidden states go from (batch,
-    num_key_value_heads, seqlen, head_dim) to (batch, num_attention_heads, seqlen, head_dim)
-    """
-    batch, num_key_value_heads, slen, head_dim = hidden_states.shape
-    if n_rep == 1:
-        return hidden_states
-    hidden_states = hidden_states[:, :,
-                                  None, :, :].expand(batch,
-                                                     num_key_value_heads,
-                                                     n_rep, slen, head_dim)
-    return hidden_states.reshape(batch, num_key_value_heads * n_rep, slen,
-                                 head_dim)
-
-
-# Copied from transformers.models.llama.modeling_llama.LlamaRMSNorm with Llama->Idefics3
-class Idefics3RMSNorm(nn.Module):
-
-    def __init__(self, hidden_size, eps=1e-6):
-        """
-        Idefics3RMSNorm is equivalent to T5LayerNorm
-        """
-        super().__init__()
-        self.weight = nn.Parameter(torch.ones(hidden_size))
-        self.variance_epsilon = eps
-
-    def forward(self, hidden_states):
-        input_dtype = hidden_states.dtype
-        hidden_states = hidden_states.to(torch.float32)
-        variance = hidden_states.pow(2).mean(-1, keepdim=True)
-        hidden_states = hidden_states * torch.rsqrt(variance +
-                                                    self.variance_epsilon)
-        return self.weight * hidden_states.to(input_dtype)
-
-    def extra_repr(self):
-        return f"{tuple(self.weight.shape)}, eps={self.variance_epsilon}"
+    def forward(self, x) -> torch.Tensor:
+        out, _ = self.proj(x)
+        return out
 
 
 class Idefics3Connector(nn.Module):
@@ -233,7 +129,7 @@ class Idefics3Model(nn.Module):
 
     def __init__(
         self,
-        config: Idefics3Config,
+        config: PretrainedConfig,
         cache_config: Optional[CacheConfig] = None,
         quant_config: Optional[QuantizationConfig] = None,
     ):
@@ -242,7 +138,8 @@ class Idefics3Model(nn.Module):
         self.padding_idx = self.config.text_config.pad_token_id
         self.vocab_size = self.config.text_config.vocab_size
 
-        self.vision_model = Idefics3VisionTransformer(config.vision_config, )
+        self.vision_model = Idefics3VisionTransformer(config.vision_config,
+                                                      quant_config)
         self.connector = Idefics3Connector(config)
         self.text_model = LlamaModel(config.text_config, cache_config,
                                      quant_config)
@@ -308,10 +205,9 @@ class Idefics3Model(nn.Module):
 
     def _image_pixels_to_features(
         self,
-        vision_tower: Union[SiglipVisionModel],
+        vision_tower: Idefics3VisionTransformer,
         pixel_values: torch.Tensor,
     ) -> torch.Tensor:
-
         # NOTE: we skip the step to select the vision feature layer since
         # this is already done inside the vision tower
         image_features = vision_tower(pixel_values)
@@ -329,7 +225,6 @@ class Idefics3Model(nn.Module):
         return self._image_pixels_to_features(self.vision_tower, pixel_values)
 
     def _process_image_input(self, image_input: ImageInputs) -> torch.Tensor:
-
         if image_input["type"] == "image_embeds":
             return image_input["data"]
 
@@ -363,68 +258,36 @@ class Idefics3Model(nn.Module):
                     input_ids)
 
                 inputs_embeds = merge_multimodal_embeddings(
-                    input_ids, inputs_embeds, vision_embeddings,
-                    self.config.image_token_index)
+                    input_ids,
+                    inputs_embeds,
+                    vision_embeddings,
+                    self.config.image_token_index,
+                )
             else:
                 inputs_embeds = self.language_model.model.get_input_embeddings(
                     input_ids)
             input_ids = None
 
-        hidden_states = self.text_model(input_ids,
-                                        positions,
-                                        kv_caches,
-                                        attn_metadata,
-                                        intermediate_tensors,
-                                        inputs_embeds=inputs_embeds)
-
+        hidden_states = self.text_model(
+            input_ids,
+            positions,
+            kv_caches,
+            attn_metadata,
+            intermediate_tensors,
+            inputs_embeds=inputs_embeds,
+        )
         return hidden_states
-
-
-def dummy_data_for_idefics3(ctx: InputContext, seq_len: int,
-                            mm_counts: Mapping[str, int]):
-    hf_config = ctx.get_hf_config(Idefics3Config)
-    vision_config = hf_config.vision_config
-    num_images = mm_counts["image"]
-
-    image_feature_size = get_max_llava_image_tokens(ctx)
-
-    if isinstance(vision_config, CLIPVisionConfig):
-        seq_data = dummy_seq_data_for_clip(
-            vision_config,
-            seq_len,
-            num_images,
-            image_token_id=hf_config.image_token_index,
-            image_feature_size_override=image_feature_size,
-        )
-
-        mm_data = dummy_image_for_clip(vision_config, num_images)
-        return seq_data, mm_data
-    elif isinstance(vision_config, SiglipVisionConfig):
-        seq_data = dummy_seq_data_for_siglip(
-            vision_config,
-            seq_len,
-            num_images,
-            image_token_id=hf_config.image_token_index,
-            image_feature_size_override=image_feature_size,
-        )
-
-        mm_data = dummy_image_for_siglip(vision_config, num_images)
-        return seq_data, mm_data
-
-    msg = f"Unsupported vision config: {type(vision_config)}"
-    raise NotImplementedError(msg)
 
 
 @MULTIMODAL_REGISTRY.register_image_input_mapper()
 @MULTIMODAL_REGISTRY.register_max_image_tokens()
-@INPUT_REGISTRY.register_dummy_data(dummy_data_for_idefics3)
-@INPUT_REGISTRY.register_input_processor(input_processor_for_idefics3)
+@INPUT_REGISTRY.register_dummy_data()
+@INPUT_REGISTRY.register_input_processor()
 class Idefics3ForConditionalGeneration(nn.Module, SupportsMultiModal):
-    _tied_weights_keys = ["lm_head.weight"]
 
     def __init__(
         self,
-        config: Idefics3Config,
+        config: PretrainedConfig,
         multimodal_config: MultiModalConfig,
         cache_config: Optional[CacheConfig] = None,
         quant_config: Optional[QuantizationConfig] = None,
@@ -437,14 +300,14 @@ class Idefics3ForConditionalGeneration(nn.Module, SupportsMultiModal):
         self.model = Idefics3Model(config, cache_config, quant_config)
         self.image_token_id = self.config.image_token_id
 
-        self.lm_head = nn.Linear(
+        self.lm_head = ParallelLMHead(
             config.text_config.hidden_size,
             config.text_config.vocab_size,
-            bias=False,
+            quant_config=quant_config,
         )
-        self.vocab_size = config.text_config.vocab_size
-
-        self.logits_processor = LogitsProcessor(self.vocab_size)
+        if self.config.text_config.tie_word_embeddings:
+            self.lm_head.weight = self.model.text_model.wte.weight
+        self.logits_processor = LogitsProcessor(config.text_config.vocab_size)
         self.sampler = Sampler()
 
     def forward(
@@ -455,12 +318,8 @@ class Idefics3ForConditionalGeneration(nn.Module, SupportsMultiModal):
         attn_metadata: AttentionMetadata,
         intermediate_tensors: Optional[IntermediateTensors] = None,
         **kwargs: object,
-    ) -> SamplerOutput:
-        r"""
-        TODO
-        ```"""
-
-        outputs = self.model(
+    ) -> Union[torch.Tensor, IntermediateTensors]:
+        hidden_states = self.model(
             input_ids,
             positions,
             kv_caches,
@@ -468,9 +327,6 @@ class Idefics3ForConditionalGeneration(nn.Module, SupportsMultiModal):
             intermediate_tensors,
             **kwargs,
         )
-
-        hidden_states = outputs[0]
-
         return hidden_states
 
     def compute_logits(self, hidden_states: torch.Tensor,
