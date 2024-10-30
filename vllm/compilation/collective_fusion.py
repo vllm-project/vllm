42ebfa922d056fc941c58d65995bbb621bd3809c
Bill Nell <bill@neuralmagic.com>
2024-10-30 19:20:41 +0000

    move collective fusion to separate file
    Signed-off-by: Bill Nell <bill@neuralmagic.com>

--- vllm/compilation/collective_fusion.py
+++ vllm/compilation/collective_fusion.py
@@ -178,7 +178,7 @@ def match_final(arg227_1, getitem_1022, getitem_1020, arg228_1):
     getitem_1026 = auto_functionalized_225[1]
     return getitem_1026
 
-
+# TODO: wrap in custom op to prevent infinite recursion in inductor logging statement?
 def replace_final(arg227_1, getitem_1215, getitem_1209, arg228_1):
     tp_group_name = "tp:0" # f"tp:{group_name}" # TODO: not same as group name
 
@@ -188,8 +188,8 @@ def replace_final(arg227_1, getitem_1215, getitem_1209, arg228_1):
     getitem_1217 = auto_functionalized_161[1]
 
     if should_slice(getitem_1209.shape):
-        group_name = torch.distributed.group.WORLD.group_name # factor out?
-        world_size = 2 # factor out
+        group_name = torch.distributed.group.WORLD.group_name # TODO: factor out
+        world_size = get_tp_group().world_size                # TODO: factor out
         all_gather_into_tensor = torch.ops._c10d_functional.all_gather_into_tensor.default(getitem_1209, world_size, group_name)
         wait_tensor = torch.ops._c10d_functional.wait_tensor.default(all_gather_into_tensor)
     else:
@@ -208,7 +208,6 @@ def get_matches():
     global my_patterns, my_patterns2, matches
 
     def record_match_fn(match: Match):
-        #print(f"MATCHED {len(matches)}, {id(matches)}")
         matches.append(match)
         return False
 
@@ -312,170 +311,6 @@ def process_matches(graph: fx.Graph, matches):
     # Finally, remove matched nodes
     graph.eliminate_dead_code()
     assert all(node not in graph.nodes for match in matches for node in match.nodes)
-
-
-def dump_graph(graph: torch.fx.Graph, stage: str):
-    logger.info("Printing graph to %s", f"{stage}.py")
-    with open(f"{stage}.py", "w") as f:
-        print(graph.python_code(root_module="self", verbose=True).src, file=f)
-
-
-def async_rewrite(graph: fx.Graph):
-    global matches
-    rank = get_tensor_model_parallel_rank()
-    get_matches()
-    matches.clear()
-
-    count = my_patterns.apply(graph)
-    print(f"fused gemm match count = {len(matches)} {id(matches)}")
-
-    # a bit hacky
-    if len(matches) > 0:
-        count = my_patterns2.apply(graph)
-        print(f"final match count = {count}")
-        process_matches(graph, matches)
-
-    return graph
-
-
-def fix_functionalization(graph: fx.Graph):
-    """
-    Rewrite the graph module to replace the pattern involving
-    torch._higher_order_ops.auto_functionalize.auto_functionalized
-    with a direct call to the inplace custom op.
-
-    # TODO: check if PyTorch nightly has fixed this issue
-    """
-
-    # debug code, if we want to see the graph before the transformation
-    # with open("before.py", "w") as f:
-    #     print(graph.python_code(root_module="self", verbose=True).src, file=f)
-
-    nodes_to_remove = []
-
-    for node in graph.nodes:
-        # Identify the auto_functionalized node
-        if node.op == 'call_function' and node.target == torch._higher_order_ops.auto_functionalize.auto_functionalized:  # noqa
-            if node.args[0] == torch.ops._C.rotary_embedding.default:
-                # manual replace for rotary_embedding
-
-                # Now, collect the arguments
-                kwargs = node.kwargs
-
-                query = kwargs['query']
-                mm_node = query.args[0].args[0]
-
-                # Create a new call to torch.ops._C.rotary_embedding.default
-                with graph.inserting_before(node):
-                    # just insert the call to the custom op
-                    # NOTE: don't run dead code elimination,
-                    # otherwise this op will be removed
-                    graph.call_function(torch.ops._C.rotary_embedding.default,
-                                        kwargs=kwargs)
-
-                # Remove the auto_functionalized node
-                # Since the node may have outputs, we need to handle its users
-                # Replace uses of the outputs (getitem nodes) with mm_node
-                for user in list(node.users):
-                    if user.op == 'call_function' and user.target == operator.getitem:  # noqa
-                        # Remove the getitem node
-                        for getitem_user in list(user.users):
-                            if (getitem_user.op == 'call_function'
-                                    and getitem_user.target
-                                    == torch.ops.aten.slice_scatter.default):
-                                # Replace the uses of slice_scatter node
-                                # with mm_node
-                                getitem_user.replace_all_uses_with(mm_node)
-                                nodes_to_remove.append(getitem_user)
-                        nodes_to_remove.append(user)
-                nodes_to_remove.append(node)
-
-            elif node.args[0] == torch.ops._C.fused_add_rms_norm.default:
-                # manual replace for fused_add_rms_norm
-                # this is the most effective optimization for llama
-                # failing to do this will result in many unnecessary copies
-
-                kwargs = node.kwargs
-
-                input = kwargs['input']
-                residual = kwargs['residual']
-
-                # Create a new call to torch.ops._C.rotary_embedding.default
-                with graph.inserting_before(node):
-                    # just insert the call to the custom op
-                    # NOTE: don't run dead code elimination,
-                    # otherwise this op will be removed
-                    graph.call_function(
-                        torch.ops._C.fused_add_rms_norm.default, kwargs=kwargs)
-
-                for user in list(node.users):
-                    if user.op == 'call_function' and user.target == operator.getitem:  # noqa
-                        # Remove the getitem node
-                        if user.args[1] == 1:
-                            replace_node = input
-                        elif user.args[1] == 2:
-                            replace_node = residual
-                        user.replace_all_uses_with(replace_node)
-                        nodes_to_remove.append(user)
-                nodes_to_remove.append(node)
-
-            elif node.args[0] == torch.ops._C.rms_norm.default:
-                # manual replace for rms_norm
-
-                kwargs = node.kwargs
-
-                input = kwargs['input']
-                out = kwargs['out']
-                weight = kwargs['weight']
-                epsilon = kwargs['epsilon']
-                # Create a new call to torch.ops._C.rotary_embedding.default
-                # cannot use kwargs, because we have an `out`, see https://github.com/pytorch/pytorch/blob/a00faf440888ffb724bad413f329a49e2b6388e7/torch/_inductor/lowering.py#L351 # noqa
-                with graph.inserting_before(node):
-                    # just insert the call to the custom op
-                    # NOTE: don't run dead code elimination,
-                    # otherwise this op will be removed
-                    graph.call_function(
-                        torch.ops._C.rms_norm.default,
-                        args=(out, input, weight, epsilon),
-                    )
-
-                replace_node = out
-
-                for user in list(node.users):
-                    if user.op == 'call_function' and user.target == operator.getitem:  # noqa
-                        user.replace_all_uses_with(replace_node)
-                        nodes_to_remove.append(user)
-                nodes_to_remove.append(node)
-
-            elif node.args[0] == torch.ops._C.silu_and_mul.default:
-                # manual replace for silu_and_mul
-
-                kwargs = node.kwargs
-
-                input = kwargs['input']
-                out = kwargs['out']
-
-                # Create a new call to torch.ops._C.rotary_embedding.default
-                # cannot use kwargs, because we have an `out`, see https://github.com/pytorch/pytorch/blob/a00faf440888ffb724bad413f329a49e2b6388e7/torch/_inductor/lowering.py#L351 # noqa
-                with graph.inserting_before(node):
-                    # just insert the call to the custom op
-                    # NOTE: don't run dead code elimination,
-                    # otherwise this op will be removed
-                    graph.call_function(
-                        torch.ops._C.silu_and_mul.default,
-                        args=(out, input),
-                    )
-                replace_node = out
-
-                for user in list(node.users):
-                    if user.op == 'call_function' and user.target == operator.getitem:  # noqa
-                        user.replace_all_uses_with(replace_node)
-                        nodes_to_remove.append(user)
-                nodes_to_remove.append(node)
-
-    # Remove the nodes all at once
-    for node in nodes_to_remove:
-        graph.erase_node(node)
 
 
 def collective_fusion(graph: fx.Graph):
