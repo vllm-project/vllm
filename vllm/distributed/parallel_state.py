b80dc8b1fb00f0cd5c32bba7ed57e9c10561d715
yan ma <yan.ma@intel.com>
2024-10-30 17:33:19 +0800

    address comments
    Signed-off-by: yan ma <yan.ma@intel.com>

--- vllm/distributed/parallel_state.py
+++ vllm/distributed/parallel_state.py
@@ -452,24 +452,21 @@ class GroupCoordinator:
                                                       input_size[dim + 1:])
             else:
                 output_tensor = None
+            return output_tensor
+        # Allocate output tensor.
+        if self.rank_in_group == dst:
+            gather_list = [torch.empty_like(input_) for _ in range(world_size)]
         else:
-            # Allocate output tensor.
-            if self.rank_in_group == dst:
-                gather_list = [
-                    torch.empty_like(input_) for _ in range(world_size)
-                ]
-            else:
-                gather_list = None
-            # Gather.
-            torch.distributed.gather(input_,
-                                     gather_list,
-                                     dst=self.ranks[dst],
-                                     group=self.device_group)
-            if self.rank_in_group == dst:
-                output_tensor = torch.cat(gather_list, dim=dim)
-            else:
-                output_tensor = None
-
+            gather_list = None
+        # Gather.
+        torch.distributed.gather(input_,
+                                 gather_list,
+                                 dst=self.ranks[dst],
+                                 group=self.device_group)
+        if self.rank_in_group == dst:
+            output_tensor = torch.cat(gather_list, dim=dim)
+        else:
+            output_tensor = None
         return output_tensor
 
     def broadcast(self, input_: torch.Tensor, src: int = 0):
