335b9c6c4406e98b17f88b93383d4343ad9cc013
Gene Su <e870252314@gmail.com>
2024-11-01 13:18:30 -0700

    [Fix] PicklingError on RayTaskError
    Signed-off-by: Gene Su <e870252314@gmail.com>

--- vllm/engine/multiprocessing/engine.py
+++ vllm/engine/multiprocessing/engine.py
@@ -1,6 +1,7 @@
 import pickle
 import signal
 from contextlib import contextmanager
+from ray.exceptions import RayTaskError
 from typing import Iterator, List, Optional, Union
 
 import cloudpickle
@@ -43,12 +44,12 @@ class MQLLMEngine:
     """A multiprocessing wrapper for :class:`LLMEngine`.
 
     This class is used to wrap the :class:`LLMEngine` class to enable use
-    in concurrnet manner. It runs a background loop and uses zeromq to 
+    in concurrnet manner. It runs a background loop and uses zeromq to
     receive new requests and stream outputs incrementally via ipc.
-    
+
     The :class:`LLMEngine` generate or encode process is kicked off when a new
     RPCProcessRequest is received by the input_socket.
-    
+
     The self.engine_loop checks the input_socket for new requests,
     adds them to the LLMEngine if there are any, calls the internal
     :class:`LLMEngine.step()`, and sends the RequestOutputs back over
@@ -310,6 +311,13 @@ class MQLLMEngine:
     def _send_outputs(self, outputs: REQUEST_OUTPUTS_T):
         """Send List of RequestOutput to RPCClient."""
         if outputs:
+            # RayTaskError might not pickelable here. We need to unpack the underlying
+            # Exception as the real exception in the output.
+            if (
+                isinstance(outputs, RPCError)
+                and isinstance(outputs.exception, RayTaskError)
+            ):
+                outputs.exception = outputs.exception.cause
             output_bytes = pickle.dumps(outputs)
             self.output_socket.send_multipart((output_bytes, ), copy=False)
 
