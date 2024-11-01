8a203df1368148fa855e678f9a5d6faa4b443b12
youkaichao <youkaichao@gmail.com>
2024-11-01 13:51:57 -0700

    [1/N] pass the complete config from engine to executor (#9933)
    Signed-off-by: youkaichao <youkaichao@gmail.com>

--- vllm/engine/async_llm_engine.py
+++ vllm/engine/async_llm_engine.py
@@ -680,7 +680,7 @@ class AsyncLLMEngine(EngineClient):
 
         # Create the async LLM engine.
         engine = cls(
-            **engine_config.to_dict(),
+            vllm_config=engine_config,
             executor_class=executor_class,
             log_requests=not engine_args.disable_log_requests,
             log_stats=not engine_args.disable_log_stats,
