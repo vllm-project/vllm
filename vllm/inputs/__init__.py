0a549e54ac9524a08b07ccd4846f96bf2b2ffb25
DarkLight1337 <tlleungac@connect.ust.hk>
2024-11-03 03:28:10 +0000

    Add missing export
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- vllm/inputs/__init__.py
+++ vllm/inputs/__init__.py
@@ -22,6 +22,7 @@ __all__ = [
     "ExplicitEncoderDecoderPrompt",
     "TokenInputs",
     "token_inputs",
+    "DecoderOnlyInputs",
     "EncoderDecoderInputs",
     "ProcessorInputs",
     "SingletonInputs",
