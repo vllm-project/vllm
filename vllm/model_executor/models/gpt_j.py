1490fbcab4086f2771a26a810ef5abe91b815c22
Alex Rakowski <182798202+arakowsk-amd@users.noreply.github.com>
2024-11-05 16:25:17 -0800

    formatting
    Signed-off-by: Alex Rakowski <182798202+arakowsk-amd@users.noreply.github.com>

--- vllm/model_executor/models/gpt_j.py
+++ vllm/model_executor/models/gpt_j.py
@@ -36,7 +36,8 @@ from vllm.model_executor.layers.rotary_embedding import get_rope
 from vllm.model_executor.layers.sampler import Sampler, SamplerOutput
 from vllm.model_executor.layers.vocab_parallel_embedding import (
     ParallelLMHead, VocabParallelEmbedding)
-from vllm.model_executor.model_loader.weight_utils import (default_weight_loader, maybe_remap_kv_scale_name)
+from vllm.model_executor.model_loader.weight_utils import (
+    default_weight_loader, maybe_remap_kv_scale_name)
 from vllm.model_executor.sampling_metadata import SamplingMetadata
 from vllm.sequence import IntermediateTensors
 
