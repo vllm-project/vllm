f97eacfae851ca40b7acc353f1bb26562a62f9bc
Joe Runde <Joseph.Runde@ibm.com>
2024-11-06 16:15:34 -0700

    :bug: fix multi-chunked-prefill sampler bug
    Signed-off-by: Joe Runde <Joseph.Runde@ibm.com>

--- vllm/model_executor/layers/sampler.py
+++ vllm/model_executor/layers/sampler.py
@@ -995,7 +995,9 @@ def get_logprobs(
     if len(query_indices) == 0:
         empty_sampled_logprob: SampleLogprobs = []
         empty_prompt_logprob: Optional[PromptLogprobs] = None
-        return [empty_prompt_logprob], [empty_sampled_logprob]
+        num_seq_groups = len(sampling_metadata.seq_groups)
+        return [empty_prompt_logprob
+                ] * num_seq_groups, [empty_sampled_logprob] * num_seq_groups
 
     selected_logprobs, ranks = None, None
     top_logprobs, top_token_ids = None, None
