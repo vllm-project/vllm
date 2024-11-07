e9273e1ea290837c0127084775bcd4421c076c5b
Maximilien de Bayser <mbayser@br.ibm.com>
2024-11-07 12:09:02 -0300

    [Frontend] Tool calling parser for Granite 3.0 models (#9027)
    Signed-off-by: Max de Bayser <mbayser@br.ibm.com>
    Signed-off-by: Isotr0py <2037008807@qq.com>

--- vllm/entrypoints/openai/tool_parsers/__init__.py
+++ vllm/entrypoints/openai/tool_parsers/__init__.py
@@ -1,5 +1,6 @@
 from .abstract_tool_parser import ToolParser, ToolParserManager
 from .granite_20b_fc_tool_parser import Granite20bFCToolParser
+from .granite_tool_parser import GraniteToolParser
 from .hermes_tool_parser import Hermes2ProToolParser
 from .internlm2_tool_parser import Internlm2ToolParser
 from .jamba_tool_parser import JambaToolParser
@@ -8,6 +9,6 @@ from .mistral_tool_parser import MistralToolParser
 
 __all__ = [
     "ToolParser", "ToolParserManager", "Granite20bFCToolParser",
-    "Hermes2ProToolParser", "MistralToolParser", "Internlm2ToolParser",
-    "Llama3JsonToolParser", "JambaToolParser"
+    "GraniteToolParser", "Hermes2ProToolParser", "MistralToolParser",
+    "Internlm2ToolParser", "Llama3JsonToolParser", "JambaToolParser"
 ]
