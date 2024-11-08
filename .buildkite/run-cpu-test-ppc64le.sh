431a5c8cd13827f36fc398bd70f06d3347bdf558
DarkLight1337 <tlleungac@connect.ust.hk>
2024-11-08 04:46:27 +0000

    Embedding models are not supported for CPU backend
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- .buildkite/run-cpu-test-ppc64le.sh
+++ .buildkite/run-cpu-test-ppc64le.sh
@@ -22,7 +22,8 @@ docker exec cpu-test bash -c "
     einops librosa peft Pillow sentence-transformers soundfile \
     transformers_stream_generator matplotlib datamodel_code_generator
   pip install torchvision --index-url https://download.pytorch.org/whl/cpu
-  pytest -v -s tests/models/embedding/language
+  # Embedding models are not supported on CPU yet
+  # pytest -v -s tests/models/embedding/language
   pytest -v -s tests/models/encoder_decoder/language
   pytest -v -s tests/models/decoder_only/language/test_models.py
   pytest -v -s tests/models/decoder_only/audio_language -m core_model
