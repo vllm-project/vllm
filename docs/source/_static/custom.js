93bc3f4d50fcd068e9176b98d2a383ff774f1b53
Rafael Vasquez <rafvasq21@gmail.com>
2024-11-08 12:51:04 -0500

    [Misc] Improve Web UI (#10090)
    Signed-off-by: Rafael Vasquez <rafvasq21@gmail.com>

--- docs/source/_static/custom.js
+++ docs/source/_static/custom.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
     script.setAttribute("version", "stable");
     script.setAttribute("runllm-keyboard-shortcut", "Mod+j"); // cmd-j or ctrl-j to open the widget.
     script.setAttribute("runllm-name", "vLLM");
-    script.setAttribute("runllm-position", "BOTTOM_RIGHT");
+    script.setAttribute("runllm-position", "BOTTOM_LEFT");
     script.setAttribute("runllm-assistant-id", "207");
   
     script.async = true;
