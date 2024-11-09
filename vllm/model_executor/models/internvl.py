816398c349a7230bcc2d7f32b7be4f950f086155
Roger Wang <ywang@roblox.com>
2024-11-08 21:49:04 -0800

    fix
    Signed-off-by: Roger Wang <ywang@roblox.com>

--- vllm/model_executor/models/internvl.py
+++ vllm/model_executor/models/internvl.py
@@ -26,7 +26,7 @@ from vllm.model_executor.models.intern_vit import (InternVisionModel,
                                                    InternVisionPatchModel)
 from vllm.model_executor.sampling_metadata import SamplingMetadata
 from vllm.multimodal import MULTIMODAL_REGISTRY
-from vllm.multimodal.base import MultiModalInputs, PlaceholderRange
+from vllm.multimodal.base import MultiModalKwargs, PlaceholderRange
 from vllm.multimodal.utils import cached_get_tokenizer
 from vllm.sequence import IntermediateTensors
 from vllm.utils import is_list_of
