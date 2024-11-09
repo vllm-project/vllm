a4e4b1498fafa71d340b719fb83245406183cf65
Li, Jiang <jiang1.li@intel.com>
2024-11-09 11:27:11 +0800

    [CI/Build] Adding timeout in CPU CI to avoid CPU test queue blocking (#6892)
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>
    Co-authored-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- .buildkite/run-cpu-test-ppc64le.sh
+++ .buildkite/run-cpu-test-ppc64le.sh
@@ -17,6 +17,7 @@ source /etc/environment
 #docker run -itd --entrypoint /bin/bash -v ~/.cache/huggingface:/root/.cache/huggingface --privileged=true --network host -e HF_TOKEN --env VLLM_CPU_KVCACHE_SPACE=4 --shm-size=4g --name cpu-test cpu-test
 docker run -itd --entrypoint /bin/bash -v ~/.cache/huggingface:/root/.cache/huggingface --privileged=true --network host -e HF_TOKEN="$HF_TOKEN" --name cpu-test cpu-test
 
+<<<<<<< HEAD
 # Run basic model test
 docker exec cpu-test bash -c "
 <<<<<<< HEAD
@@ -56,16 +57,37 @@ docker exec cpu-test bash -c "
   # pytest -v -s tests/models/decoder_only/audio_language -m cpu_model
   pytest -v -s tests/models/decoder_only/vision_language -m cpu_model"
 >>>>>>> b489fc3c... [CI/Build] Update CPU tests to include all "standard" tests (#5481)
+=======
+function cpu_tests() {
+  # Run basic model test
+  docker exec cpu-test bash -c "
+    set -e
+    pip install pytest pytest-asyncio \
+      decord einops librosa peft Pillow sentence-transformers soundfile \
+      transformers_stream_generator matplotlib datamodel_code_generator
+    pip install torchvision --index-url https://download.pytorch.org/whl/cpu
+    # Embedding models are not supported for CPU yet
+    # pytest -v -s tests/models/embedding/language
+    pytest -v -s tests/models/encoder_decoder/language
+    pytest -v -s tests/models/decoder_only/language/test_models.py
+    pytest -v -s tests/models/decoder_only/audio_language -m cpu_model
+    pytest -v -s tests/models/decoder_only/vision_language -m cpu_model"
+>>>>>>> d7edca1d... [CI/Build] Adding timeout in CPU CI to avoid CPU test queue blocking (#6892)
 
-# online inference
-docker exec cpu-test bash -c "
-  set -e
-  python3 -m vllm.entrypoints.openai.api_server --model facebook/opt-125m & 
-  timeout 600 bash -c 'until curl localhost:8000/v1/models; do sleep 1; done' || exit 1
-  python3 benchmarks/benchmark_serving.py \
-    --backend vllm \
-    --dataset-name random \
-    --model facebook/opt-125m \
-    --num-prompts 20 \
-    --endpoint /v1/completions \
-    --tokenizer facebook/opt-125m"
+  # online inference
+  docker exec cpu-test bash -c "
+    set -e
+    python3 -m vllm.entrypoints.openai.api_server --model facebook/opt-125m & 
+    timeout 600 bash -c 'until curl localhost:8000/v1/models; do sleep 1; done' || exit 1
+    python3 benchmarks/benchmark_serving.py \
+      --backend vllm \
+      --dataset-name random \
+      --model facebook/opt-125m \
+      --num-prompts 20 \
+      --endpoint /v1/completions \
+      --tokenizer facebook/opt-125m"
+}
+
+# All of CPU tests are expected to be finished less than 25 mins.
+export -f cpu_tests
+timeout 25m bash -c "cpu_tests"
