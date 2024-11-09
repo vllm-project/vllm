06162b91e631179d04a989fc50e4f084ff3d1835
Umesh Deshpande <udeshpa@us.ibm.com>
2024-11-08 20:18:27 -0500

    Formatting fix
    Signed-off-by: Umesh Deshpande <udeshpa@us.ibm.com>

--- vllm/lora/models.py
+++ vllm/lora/models.py
@@ -393,7 +393,8 @@ class LoRAModelManager(AdapterModelManager):
             if module_lora:
                 module_lora.optimize()
                 # Bias is not explicitly enabled with the flag enable_lora_bias.
-                if module_lora.bias is not None and not self.lora_config.bias_enabled:
+                if (module_lora.bias is not None
+                     and not self.lora_config.bias_enabled):
                     module_lora.bias = None
                     raise ValueError(
                         f"Adapter bias cannot be used for {module_name}"
