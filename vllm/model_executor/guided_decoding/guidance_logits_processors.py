1a0bb1c3ce887dbfe0bae87855737bf47d4a9700
Loc Huynh <jc1da.3011@gmail.com>
2024-11-09 23:12:19 -0800

    Move _initialize call into constructor
    Signed-off-by: Loc Huynh <jc1da.3011@gmail.com>

--- vllm/model_executor/guided_decoding/guidance_logits_processors.py
+++ vllm/model_executor/guided_decoding/guidance_logits_processors.py
@@ -1,19 +1,20 @@
-import os
-import guidance
-import torch
-import json
 import inspect
-import numpy as np
+import json
+import os
+from typing import Any, List, Type, Union
+
+import guidance
 import llguidance  # type: ignore[import-untyped]
-from pydantic import BaseModel
-from typing import Type, Union, List
+import numpy as np
+import torch
 from guidance._schema import LLInterpreterResponse
 from guidance.models import TransformersTokenizer
+from pydantic import BaseModel
 from transformers import PreTrainedTokenizerBase
-from typing import Union
+
 
 class GuidanceLogitsProcessor:
-    metadata = {}
+    metadata: dict[str, Any] = {}
 
     def __init__(
         self,
@@ -26,17 +27,18 @@ class GuidanceLogitsProcessor:
 
         Args:
             mode (str)
-                guided generation mode. Must be one of "json", "regex", "choice", "grammar"
+                guided generation mode. 
+                Must be one of "json", "regex", "choice", "grammar"
             guide (Union[dict, Type[BaseModel], str])
                 guide for guided generation
             tokenizer (PreTrainedTokenizerBase)
                 model's tokenizer
             whitespace_pattern (Union[str, None], optional)
-                Json-string to indicate pattern to use for JSON syntactic whitespace
+                Json-string to indicate pattern to use \
+                    for JSON syntactic whitespace
                 Example: '{"whitespace_flexible":true}'
         """
         self.mode = mode
-        self.initialized = False
         self.guide = guide
         self.tokenizer = tokenizer
         self.tokenizer_name = tokenizer.name_or_path
@@ -46,16 +48,9 @@ class GuidanceLogitsProcessor:
         self.pending_ff_tokens: list[int] = []
         self.new_sampling = False
 
-        self.schema = None
-        self.guidance_tokenizer = None
-        self.ll_tokenizer = None
-        self.ll_interpreter = None
-        self.serialized_grammar = None
+        self._initialize()
 
-    def initialize(self):
-        if self.initialized:
-            return
-
+    def _initialize(self):
         if self.mode.lower() == "json":
             if isinstance(self.guide, str):
                 schema = json.loads(self.guide)
@@ -65,29 +60,25 @@ class GuidanceLogitsProcessor:
                 schema = self.guide
 
             whitespaces_config = {}
-            try:
+            if isinstance(self.whitespace_pattern, str):
                 whitespaces_config = json.loads(self.whitespace_pattern)
-            except:
-                pass
 
             args = {
-                "schema" : schema,
-                "temperature" : 0.0,
+                "schema": schema,
+                "temperature": 0.0,
             }
 
             json_func_sigs = inspect.signature(guidance.json).parameters
             if "whitespace_flexible" in json_func_sigs:
                 # whitespace_flexible is available in main-repo or later version
                 args["whitespace_flexible"] = whitespaces_config.get(
-                    "whitespace_flexible", False
-                )
+                    "whitespace_flexible", False)
 
             self.schema = guidance.json(**args)
             self.serialized_grammar = self.schema.ll_serialize()
         elif self.mode.lower() in ["regex", "choice"]:
             self.serialized_grammar = guidance.gen(
-                regex=self.guide, temperature=0.0
-            ).ll_serialize()
+                regex=self.guide, temperature=0.0).ll_serialize()
         elif self.mode.lower() == "grammar":
             serialized_grammar = self.guide
             if isinstance(self.guide, str):
@@ -95,16 +86,20 @@ class GuidanceLogitsProcessor:
             self.serialized_grammar = serialized_grammar
 
         if f"guidance_tokenizer_{self.tokenizer_name}" not in self.metadata:
-            self.metadata[f"guidance_tokenizer_{self.tokenizer_name}"] = TransformersTokenizer(
-                model=self.tokenizer.name_or_path, transformers_tokenizer=self.tokenizer
-            )
-        self.guidance_tokenizer = self.metadata[f"guidance_tokenizer_{self.tokenizer_name}"]
+            self.metadata[
+                f"guidance_tokenizer_{self.tokenizer_name}"] = \
+                    TransformersTokenizer( \
+                        model=self.tokenizer.name_or_path,
+                        transformers_tokenizer=self.tokenizer)
+        self.guidance_tokenizer = self.metadata[
+            f"guidance_tokenizer_{self.tokenizer_name}"]
 
         if f"ll_tokenizer_{self.tokenizer_name}" not in self.metadata:
-            self.metadata[f"ll_tokenizer_{self.tokenizer_name}"] = llguidance.LLTokenizer(
-                llguidance.TokenizerWrapper(self.guidance_tokenizer)
-            )
-        self.ll_tokenizer = self.metadata[f"ll_tokenizer_{self.tokenizer_name}"]
+            self.metadata[
+                f"ll_tokenizer_{self.tokenizer_name}"] = llguidance.LLTokenizer(
+                    llguidance.TokenizerWrapper(self.guidance_tokenizer))
+        self.ll_tokenizer = self.metadata[
+            f"ll_tokenizer_{self.tokenizer_name}"]
 
         self.ll_interpreter = llguidance.LLInterpreter(
             self.ll_tokenizer,
@@ -121,8 +116,6 @@ class GuidanceLogitsProcessor:
         past_tokens_ids: list[int],
         logits: torch.Tensor,
     ) -> torch.Tensor:
-        self.initialize()
-
         if self.is_stopped:
             return logits
 
@@ -132,8 +125,7 @@ class GuidanceLogitsProcessor:
 
             if self.new_sampling and len(past_tokens_ids) > 0:
                 backtrack, ff_tokens = self.ll_interpreter.post_process(
-                    past_tokens_ids[-1]
-                )
+                    past_tokens_ids[-1])
                 if len(ff_tokens) > 0 and backtrack == 0:
                     ff_tokens = ff_tokens[1:]
                 self.pending_ff_tokens.extend(ff_tokens)
@@ -143,11 +135,12 @@ class GuidanceLogitsProcessor:
                 # assert backtrack == 0
 
             if len(self.pending_ff_tokens) > 0:
-                # if we have pending fast-forward tokens, just return them immediately
+                # if we have pending fast-forward tokens,
+                # just return them immediately
                 ff_token = self.pending_ff_tokens.pop(0)
-                masked_logits = torch.zeros_like(
-                    logits, dtype=logits.dtype, device=logits.device
-                )
+                masked_logits = torch.zeros_like(logits,
+                                                 dtype=logits.dtype,
+                                                 device=logits.device)
                 masked_logits[ff_token] = 200.0
                 return masked_logits
 
@@ -155,34 +148,34 @@ class GuidanceLogitsProcessor:
             r = LLInterpreterResponse.model_validate_json(resp)
 
             if r.stop:
-                mask = torch.zeros_like(
-                    logits, dtype=logits.dtype, device=logits.device
-                )
+                mask = torch.zeros_like(logits,
+                                        dtype=logits.dtype,
+                                        device=logits.device)
                 if self.guidance_tokenizer.eos_token_id is not None:
                     mask[self.guidance_tokenizer.eos_token_id] = 200.0
                 self.is_stopped = True
             elif mask is None:
                 # NOTE: mask should not be None unless r.stop is True
-                # However, we are handling this case just in case llguidance allows free-style generation
-                mask = torch.zeros_like(
-                    logits, dtype=logits.dtype, device=logits.device
-                )
+                # However, we are handling this case just in case
+                # llguidance allows free-style generation
+                mask = torch.zeros_like(logits,
+                                        dtype=logits.dtype,
+                                        device=logits.device)
             else:
                 mask = np.frombuffer(mask, dtype=np.uint8)
-                mask = torch.tensor(mask, dtype=logits.dtype, device=logits.device)
+                mask = torch.tensor(mask,
+                                    dtype=logits.dtype,
+                                    device=logits.device)
 
             if mask.shape[0] != logits.shape[0]:
                 # Some model has extra tokens that are not in the vocabulary
-                mask = torch.cat(
-                    [
-                        mask,
-                        torch.ones(
-                            logits.shape[0] - mask.shape[0],
-                            device=logits.device,
-                        )
-                        * 200.0,
-                    ]
-                )
+                mask = torch.cat([
+                    mask,
+                    torch.ones(
+                        logits.shape[0] - mask.shape[0],
+                        device=logits.device,
+                    ) * 200.0,
+                ])
 
             masked_logits = logits + mask
             self.new_sampling = True
