74145f267f75aa4ba86825bcacaa49f7a4d2612f
youkaichao <youkaichao@gmail.com>
2024-11-10 22:52:22 -0800

    update cpu tests
    Signed-off-by: youkaichao <youkaichao@gmail.com>

--- tests/distributed/test_utils.py
+++ tests/distributed/test_utils.py
@@ -43,23 +43,20 @@ def test_cuda_device_count_stateless():
 def cpu_worker(rank, WORLD_SIZE):
     pg1 = stateless_init_process_group(init_method="tcp://127.0.0.1:29500",
                                        rank=rank,
-                                       world_size=WORLD_SIZE,
-                                       backend="gloo")
+                                       world_size=WORLD_SIZE)
     if rank <= 2:
         pg2 = stateless_init_process_group(init_method="tcp://127.0.0.1:29501",
                                            rank=rank,
-                                           world_size=3,
-                                           backend="gloo")
+                                           world_size=3)
     data = torch.tensor([rank])
-    dist.all_reduce(data, op=dist.ReduceOp.SUM, group=pg1)
+    data = pg1.broadcast_obj(data, src=2)
+    assert data.item() == 2
     if rank <= 2:
-        dist.all_reduce(data, op=dist.ReduceOp.SUM, group=pg2)
-    item = data[0].item()
-    print(f"rank: {rank}, item: {item}")
-    if rank == 3:
-        assert item == 6
-    else:
-        assert item == 18
+        data = torch.tensor([rank + 1])
+        data = pg2.broadcast_obj(data, src=2)
+        assert data.item() == 3
+        pg2.barrier()
+    pg1.barrier()
 
 
 def gpu_worker(rank, WORLD_SIZE):
