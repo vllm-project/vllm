d2027462db70e5b959b5c782a382b322ac24d571
youkaichao <youkaichao@gmail.com>
2024-11-10 21:19:32 -0800

    fix mllama
    Signed-off-by: youkaichao <youkaichao@gmail.com>

--- vllm/model_executor/models/mllama.py
+++ vllm/model_executor/models/mllama.py
@@ -943,7 +943,7 @@ class MllamaTextModel(nn.Module):
     def __init__(self, *, vllm_config: VllmConfig, prefix: str = ""):
         super().__init__()
 
-        config = vllm_config.model_config.hf_config
+        config = vllm_config.model_config.hf_config.text_config
         cache_config = vllm_config.cache_config
         quant_config = vllm_config.quant_config
 
