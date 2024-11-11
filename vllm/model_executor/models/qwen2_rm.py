e3e2422084a22f8d75b467b63edb4fe1c27bb32f
DarkLight1337 <tlleungac@connect.ust.hk>
2024-11-11 07:17:50 +0000

    lint
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- vllm/model_executor/models/qwen2_rm.py
+++ vllm/model_executor/models/qwen2_rm.py
@@ -58,7 +58,6 @@ class Qwen2ForRewardModel(nn.Module, SupportsLoRA, SupportsPP):
     def __init__(self, *, vllm_config: VllmConfig, prefix: str = ""):
         super().__init__()
         config = vllm_config.model_config.hf_config
-        cache_config = vllm_config.cache_config
         quant_config = vllm_config.quant_config
         lora_config = vllm_config.lora_config
         pooler_config = vllm_config.model_config.pooler_config
