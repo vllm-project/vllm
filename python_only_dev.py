077d38c8edd172a6179ade0f1355655aa86ee2d9
Nikolai Shcheglov <ndnd@mail.ru>
2024-11-11 16:29:19 -0600

    Make shutil rename in python_only_dev (#10233)
    Signed-off-by: shcheglovnd <shcheglovnd@avride.ai>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- python_only_dev.py
+++ python_only_dev.py
@@ -69,7 +69,8 @@ if not args.quit_dev:
     current_vllm_path = os.path.join(cwd, "vllm")
 
     print(f"Renaming {pre_built_vllm_path} to {tmp_path} for backup")
-    os.rename(pre_built_vllm_path, tmp_path)
+    shutil.copytree(pre_built_vllm_path, tmp_path)
+    shutil.rmtree(pre_built_vllm_path)
 
     print(f"Linking {current_vllm_path} to {pre_built_vllm_path}")
     os.symlink(current_vllm_path, pre_built_vllm_path)
