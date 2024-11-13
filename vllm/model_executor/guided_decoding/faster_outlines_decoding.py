ae16cafede13bd1c5fbd0e76c35a3b3a3bbb3381
unaidedelf8777 <thwackyy.y@gmail.com>
2024-11-12 19:01:21 -0600

    run format.sh, isort

--- vllm/model_executor/guided_decoding/faster_outlines_decoding.py
+++ vllm/model_executor/guided_decoding/faster_outlines_decoding.py
@@ -1,38 +1,33 @@
 import math
-import torch
-from re import escape as regex_escape
 from json import dumps as json_dumps
+from re import escape as regex_escape
 from typing import Dict, List, Union
 
-from faster_outlines.fsm import (
-    LazyVLLMRegexGuide, 
-    Write,
-    Generate,
-    TokenVocabulary
-)
-from transformers import PreTrainedTokenizerBase
-from vllm.sampling_params import GuidedDecodingParams
+import torch
+from faster_outlines.fsm import (Generate, LazyVLLMRegexGuide, TokenVocabulary,
+                                 Write)
 from outlines.fsm.json_schema import build_regex_from_schema
+from transformers import PreTrainedTokenizerBase
+
+from vllm.sampling_params import GuidedDecodingParams
 
 TOKENIZER_CACHE: Dict[str, TokenVocabulary] = {}
+
 
 class BaseLogitsProcessor:
 
     def __init__(self, guide):
         self._guide = guide
         self.state = 0
-    
+
     def __call__(self, input_ids: List[int],
                  scores: torch.Tensor) -> torch.Tensor:
         """Use the FSM to bias the logits before sampling the next token."""
         if len(input_ids) > 0:
-            self.state = self._guide.get_next_state(
-                state=self.state,
-                token_id=input_ids[-1]
-            )
+            self.state = self._guide.get_next_state(state=self.state,
+                                                    token_id=input_ids[-1])
 
-        instruction = self._guide.get_next_instruction(
-            state=self.state)
+        instruction = self._guide.get_next_instruction(state=self.state)
         if type(instruction) == Generate:  # noqa: E721
             allowed_tokens = instruction.tokens
         elif type(instruction) == Write:  # noqa: E721
@@ -53,8 +48,7 @@ class BaseLogitsProcessor:
 class RegexLogitsProcessor(BaseLogitsProcessor):
 
     @classmethod
-    def _get_guide(cls, regex_string: str,
-                   tokenizer: PreTrainedTokenizerBase):
+    def _get_guide(cls, regex_string: str, tokenizer: PreTrainedTokenizerBase):
         vocab = _adapt_tokenizer(tokenizer)
         return LazyVLLMRegexGuide(regex_string, vocab)
 
@@ -72,6 +66,7 @@ class RegexLogitsProcessor(BaseLogitsProcessor):
         super().__init__(
             RegexLogitsProcessor._get_guide(regex_string, tokenizer))
 
+
 def _adapt_tokenizer(tokenizer: PreTrainedTokenizerBase):
     """
     Adapt VLLM's tokenizer into a TokenVocabulary, readable by Rust.
@@ -80,30 +75,27 @@ def _adapt_tokenizer(tokenizer: PreTrainedTokenizerBase):
 
         return TOKENIZER_CACHE[tokenizer.name_or_path]
 
-    token_vocab = TokenVocabulary(
-        tokenizer.get_vocab(),
-        tokenizer.eos_token_id,
-        set(tokenizer.all_special_tokens)
-    )
+    token_vocab = TokenVocabulary(tokenizer.get_vocab(),
+                                  tokenizer.eos_token_id,
+                                  set(tokenizer.all_special_tokens))
 
     TOKENIZER_CACHE[tokenizer.name_or_path] = token_vocab
 
     return token_vocab
 
+
 def get_local_faster_outlines_guided_decoding_logits_processor(
-    guided_params: GuidedDecodingParams, 
-    tokenizer: PreTrainedTokenizerBase
+        guided_params: GuidedDecodingParams, tokenizer: PreTrainedTokenizerBase
 ) -> Union[RegexLogitsProcessor, None]:
     regex = _get_regex(guided_params)
 
     if not regex:
         return None
-    
+
     return RegexLogitsProcessor(regex, tokenizer)
 
-def _get_regex(
-    guided_params: GuidedDecodingParams
-) -> Union[str, None]:
+
+def _get_regex(guided_params: GuidedDecodingParams) -> Union[str, None]:
     if guided_params.json:
         if isinstance(guided_params.json, dict):
             # turn dict into hashable string
