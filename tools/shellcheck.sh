1e0d209e4d3986dca15f14ac3fa2a46feb19b7d0
DarkLight1337 <tlleungac@connect.ust.hk>
2024-11-13 15:58:38 +0000

    Fix `format.sh` not failing when shellcheck fails
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- tools/shellcheck.sh
+++ tools/shellcheck.sh
@@ -19,4 +19,4 @@ if ! [ -x "$(command -v shellcheck)" ]; then
 fi
 
 # TODO - fix warnings in .buildkite/run-amd-test.sh
-find . -name "*.sh" -not -path "./.buildkite/run-amd-test.sh" -exec sh -c 'git check-ignore -q $1 || shellcheck $1' _ {} \;
+find . -name "*.sh" -not -path "./.buildkite/run-amd-test.sh" -print0 | xargs -0 -I {} sh -c 'git check-ignore -q "{}" || shellcheck "{}"'
