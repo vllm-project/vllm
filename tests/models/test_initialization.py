3dae7a93c89282c3d0e7a8414cb0671cc1862a50
DarkLight1337 <tlleungac@connect.ust.hk>
2024-11-14 09:38:17 +0000

    Enable conditional HF override
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- tests/models/test_initialization.py
+++ tests/models/test_initialization.py
@@ -1,6 +1,7 @@
 from unittest.mock import patch
 
 import pytest
+from transformers import PretrainedConfig
 
 from vllm import LLM
 
@@ -14,13 +15,21 @@ def test_can_initialize(model_arch):
         pytest.skip("Model is not available online")
 
     # Avoid OOM
-    text_config_overrides = {
-        "num_layers": 1,
-        "num_hidden_layers": 1,
-        "num_experts": 2,
-        "num_experts_per_tok": 2,
-        "num_local_experts": 2,
-    }
+    def hf_overrides(hf_config: PretrainedConfig) -> PretrainedConfig:
+        if hasattr(hf_config, "text_config"):
+            text_config: PretrainedConfig = hf_config.text_config
+        else:
+            text_config = hf_config
+
+        text_config.update({
+            "num_layers": 1,
+            "num_hidden_layers": 1,
+            "num_experts": 2,
+            "num_experts_per_tok": 2,
+            "num_local_experts": 2,
+        })
+
+        return text_config
 
     # Avoid calling model.forward()
     def _initialize_kv_caches(self) -> None:
@@ -33,8 +42,5 @@ def test_can_initialize(model_arch):
             model_info.default,
             trust_remote_code=model_info.trust_remote_code,
             load_format="dummy",
-            hf_overrides={
-                **text_config_overrides,
-                "text_config": text_config_overrides,  # For multi-modal models
-            },
+            hf_overrides=hf_overrides,
         )
