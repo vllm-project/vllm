06da0050c8f04caea3f097e07e27d027a62608aa
Maximilien de Bayser <mbayser@br.ibm.com>
2024-11-14 18:23:29 -0300

    Support Roberta embedding models (#9387)
    Signed-off-by: Max de Bayser <mbayser@br.ibm.com>
    Signed-off-by: Flavia Beo <flavia.beo@ibm.com>
    Co-authored-by: Flavia Beo <flavia.beo@ibm.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- csrc/attention/paged_attention_v2.cu
+++ csrc/attention/paged_attention_v2.cu
@@ -104,6 +104,9 @@ void paged_attention_v2_launcher(
     // NOTE(woosuk): To reduce the compilation time, we only compile for the
     // head sizes that we use in the model. However, we can easily extend this
     // to support any head size which is a multiple of 16.
+    case 32:
+      LAUNCH_PAGED_ATTENTION_V2(32);
+      break;
     case 64:
       LAUNCH_PAGED_ATTENTION_V2(64);
       break;
