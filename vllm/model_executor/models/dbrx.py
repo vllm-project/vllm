504ac53d18fc057d2a98741fa27d89df9054422d
youkaichao <youkaichao@gmail.com>
2024-11-13 18:55:39 -0800

    [misc] error early for old-style class (#10304)
    Signed-off-by: youkaichao <youkaichao@gmail.com>

--- vllm/model_executor/models/dbrx.py
+++ vllm/model_executor/models/dbrx.py
@@ -350,11 +350,7 @@ class DbrxModel(nn.Module):
 
 class DbrxForCausalLM(nn.Module, SupportsPP):
 
-    def __init__(
-        self,
-        vllm_config: VllmConfig,
-        prefix: str = "",
-    ):
+    def __init__(self, *, vllm_config: VllmConfig, prefix: str = ""):
         super().__init__()
         config = vllm_config.model_config.hf_config
         quant_config = vllm_config.quant_config
