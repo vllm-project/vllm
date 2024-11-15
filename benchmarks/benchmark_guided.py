a0c46f18667a36efb6c4192382d2a93b881e9911
Aaron Pham <contact@aarnphm.xyz>
2024-11-15 15:33:15 -0500

    chore: add @mgoin's suggestion
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- benchmarks/benchmark_guided.py
+++ benchmarks/benchmark_guided.py
@@ -3,12 +3,12 @@ import argparse
 import json
 import random
 import time
-from typing import List, Optional, Tuple
+from typing import List
 
 import uvloop
 from transformers import AutoTokenizer
 
-from vllm.engine.arg_utils import DEVICE_OPTIONS, AsyncEngineArgs, EngineArgs
+from vllm.engine.arg_utils import AsyncEngineArgs, EngineArgs
 from vllm.entrypoints.openai.api_server import (
     build_async_engine_client_from_engine_args)
 from vllm.sampling_params import GuidedDecodingParams
@@ -128,52 +128,15 @@ SCHEMA = {
     ["userId", "personalInfo", "address", "accountStatus", "registrationDate"]
 }
 
-
 def run_vllm(
-    requests: List[Tuple[str, int, int]],
-    model: str,
-    tokenizer: str,
-    tensor_parallel_size: int,
-    seed: int,
+    requests: List[tuple[str, int, int]],
+    engine_args: EngineArgs,
     n: int,
-    trust_remote_code: bool,
-    dtype: str,
-    max_model_len: Optional[int],
-    enforce_eager: bool,
-    kv_cache_dtype: str,
-    device: str,
-    enable_prefix_caching: bool,
-    enable_chunked_prefill: bool,
-    max_num_batched_tokens: int,
-    distributed_executor_backend: Optional[str],
-    gpu_memory_utilization: float = 0.9,
-    num_scheduler_steps: int = 1,
-    disable_async_output_proc: bool = False,
     guided_decoding: bool = False,
     warmup: bool = False,
 ) -> float:
     from vllm import LLM, SamplingParams
-    llm = LLM(
-        model=model,
-        tokenizer=tokenizer,
-        tensor_parallel_size=tensor_parallel_size,
-        seed=seed,
-        trust_remote_code=trust_remote_code,
-        dtype=dtype,
-        max_model_len=max_model_len,
-        gpu_memory_utilization=gpu_memory_utilization,
-        enforce_eager=enforce_eager,
-        kv_cache_dtype=kv_cache_dtype,
-        device=device,
-        enable_prefix_caching=enable_prefix_caching,
-        enable_chunked_prefill=enable_chunked_prefill,
-        max_num_batched_tokens=max_num_batched_tokens,
-        distributed_executor_backend=distributed_executor_backend,
-        load_format=EngineArgs.load_format,
-        num_scheduler_steps=num_scheduler_steps,
-        disable_async_output_proc=disable_async_output_proc,
-        guided_decoding_backend="outlines",
-    )
+    llm = LLM(**vars(engine_args))
 
     # Add the requests to the engine.
     prompts: List[str] = []
@@ -196,55 +159,15 @@ def run_vllm(
     end = time.perf_counter()
     return end - start
 
-
 async def run_vllm_async(
-    requests: List[Tuple[str, int, int]],
-    model: str,
-    tokenizer: str,
-    tensor_parallel_size: int,
-    seed: int,
+    requests: List[tuple[str, int, int]],
+    engine_args: AsyncEngineArgs,
     n: int,
-    trust_remote_code: bool,
-    dtype: str,
-    max_model_len: Optional[int],
-    enforce_eager: bool,
-    kv_cache_dtype: str,
-    device: str,
-    enable_prefix_caching: bool,
-    enable_chunked_prefill: bool,
-    max_num_batched_tokens: int,
-    distributed_executor_backend: Optional[str],
-    gpu_memory_utilization: float = 0.9,
-    num_scheduler_steps: int = 1,
-    disable_async_output_proc: bool = False,
     guided_decoding: bool = False,
     warmup: bool = False,
     disable_frontend_multiprocessing: bool = False,
 ) -> float:
     from vllm import SamplingParams
-    engine_args = AsyncEngineArgs(
-        model=model,
-        tokenizer=tokenizer,
-        tensor_parallel_size=tensor_parallel_size,
-        seed=seed,
-        trust_remote_code=trust_remote_code,
-        dtype=dtype,
-        max_model_len=max_model_len,
-        gpu_memory_utilization=gpu_memory_utilization,
-        enforce_eager=enforce_eager,
-        kv_cache_dtype=kv_cache_dtype,
-        device=device,
-        enable_prefix_caching=enable_prefix_caching,
-        enable_chunked_prefill=enable_chunked_prefill,
-        max_num_batched_tokens=max_num_batched_tokens,
-        distributed_executor_backend=distributed_executor_backend,
-        load_format=EngineArgs.load_format,
-        num_scheduler_steps=num_scheduler_steps,
-        disable_async_output_proc=disable_async_output_proc,
-        worker_use_ray=False,
-        disable_log_requests=False,
-        guided_decoding_backend="outlines",
-    )
 
     async with build_async_engine_client_from_engine_args(
             engine_args, disable_frontend_multiprocessing) as llm:
@@ -273,7 +196,6 @@ async def run_vllm_async(
             for i, (prompt, sp) in enumerate(zip(prompts, sampling_params)):
                 generator = llm.generate(prompt, sp, request_id=f"test{i}")
                 generators.append(generator)
-            all_gens = merge_async_iterators(*generators)
             all_gens = merge_async_iterators(*generators)
             async for i, res in all_gens:
                 pass
@@ -304,7 +226,6 @@ async def run_vllm_async(
         end = time.perf_counter()
         return end - start
 
-
 def main(args: argparse.Namespace):
     print(args)
     random.seed(args.seed)
@@ -312,44 +233,40 @@ def main(args: argparse.Namespace):
     # Synthesize a prompt with the given input length.
     tokenizer = AutoTokenizer.from_pretrained(
         args.tokenizer, trust_remote_code=args.trust_remote_code)
-    prompt = f"Generate an example of a user profile given the following schema: {json.dumps(SCHEMA)}"  # noqa: E501
+    prompt = f"Generate an example of a user profile given the following schema: {json.dumps(SCHEMA)}"
     input_len = len(tokenizer(prompt).input_ids)
+    print(f"Input length of the prompt: {input_len} tokens")
     requests = [(prompt, input_len, args.output_len)
                 for _ in range(args.num_prompts)]
 
-    run_args = [
-        requests,
-        args.model,
-        args.tokenizer,
-        args.tensor_parallel_size,
-        args.seed,
-        args.n,
-        args.trust_remote_code,
-        args.dtype,
-        args.max_model_len,
-        args.enforce_eager,
-        args.kv_cache_dtype,
-        args.device,
-        args.enable_prefix_caching,
-        args.enable_chunked_prefill,
-        args.max_num_batched_tokens,
-        args.distributed_executor_backend,
-        args.gpu_memory_utilization,
-        args.num_scheduler_steps,
-        args.disable_async_output_proc,
-        args.guided_decoding,
-        args.warmup,
-    ]
-
     if args.async_engine:
-        run_args.append(args.disable_frontend_multiprocessing)
-        elapsed_time = uvloop.run(run_vllm_async(*run_args))
+        engine_args = AsyncEngineArgs.from_cli_args(args)
+        elapsed_time = uvloop.run(
+            run_vllm_async(
+                requests,
+                engine_args,
+                args.n,
+                args.guided_decoding,
+                args.warmup,
+                args.disable_frontend_multiprocessing,
+            ))
     else:
-        elapsed_time = run_vllm(*run_args)
+        engine_args = EngineArgs.from_cli_args(args)
+        elapsed_time = run_vllm(
+            requests,
+            engine_args,
+            args.n,
+            args.guided_decoding,
+            args.warmup,
+        )
+
     total_num_tokens = sum(prompt_len + output_len
                            for _, prompt_len, output_len in requests)
+    total_output_tokens = sum(output_len
+                              for _, _, output_len in requests)
     print(f"Throughput: {len(requests) / elapsed_time:.2f} requests/s, "
-          f"{total_num_tokens / elapsed_time:.2f} tokens/s")
+          f"{total_num_tokens / elapsed_time:.2f} total tokens/s, "
+          f"{total_output_tokens / elapsed_time:.2f} output tokens/s")
 
     # Output JSON results if specified
     if args.output_json:
@@ -363,17 +280,14 @@ def main(args: argparse.Namespace):
         with open(args.output_json, "w") as f:
             json.dump(results, f, indent=4)
 
-
 if __name__ == "__main__":
     parser = FlexibleArgumentParser(description="Benchmark guided decoding.")
+    parser = AsyncEngineArgs.add_cli_args(parser)
+
     parser.add_argument("--output-len",
                         type=int,
-                        default=None,
                         help="Output length for each request. Overrides the "
                         "output length from the dataset.")
-    parser.add_argument("--model", type=str, default="facebook/opt-125m")
-    parser.add_argument("--tokenizer", type=str, default=None)
-    parser.add_argument("--tensor-parallel-size", "-tp", type=int, default=1)
     parser.add_argument("--n",
                         type=int,
                         default=1,
@@ -382,81 +296,10 @@ if __name__ == "__main__":
                         type=int,
                         default=10,
                         help="Number of prompts to process.")
-    parser.add_argument("--seed", type=int, default=0)
-    parser.add_argument('--trust-remote-code',
-                        action='store_true',
-                        help='trust remote code from huggingface')
-    parser.add_argument(
-        '--max-model-len',
-        type=int,
-        default=None,
-        help='Maximum length of a sequence (including prompt and output). '
-        'If None, will be derived from the model.')
-    parser.add_argument(
-        '--dtype',
-        type=str,
-        default='auto',
-        choices=['auto', 'half', 'float16', 'bfloat16', 'float', 'float32'],
-        help='data type for model weights and activations. '
-        'The "auto" option will use FP16 precision '
-        'for FP32 and FP16 models, and BF16 precision '
-        'for BF16 models.')
-    parser.add_argument('--gpu-memory-utilization',
-                        type=float,
-                        default=0.9,
-                        help='the fraction of GPU memory to be used for '
-                        'the model executor, which can range from 0 to 1.'
-                        'If unspecified, will use the default value of 0.9.')
-    parser.add_argument("--enforce-eager",
-                        action="store_true",
-                        help="enforce eager execution")
-    parser.add_argument(
-        '--kv-cache-dtype',
-        type=str,
-        choices=['auto', 'fp8', 'fp8_e5m2', 'fp8_e4m3'],
-        default="auto",
-        help='Data type for kv cache storage. If "auto", will use model '
-        'data type. CUDA 11.8+ supports fp8 (=fp8_e4m3) and fp8_e5m2. '
-        'ROCm (AMD GPU) supports fp8 (=fp8_e4m3)')
-    parser.add_argument("--device",
+    parser.add_argument('--output-json',
                         type=str,
-                        default="auto",
-                        choices=DEVICE_OPTIONS,
-                        help='device type for vLLM execution')
-    parser.add_argument(
-        "--num-scheduler-steps",
-        type=int,
-        default=1,
-        help="Maximum number of forward steps per scheduler call.")
-    parser.add_argument(
-        "--enable-prefix-caching",
-        action='store_true',
-        help="Enable automatic prefix caching for vLLM backend.")
-    parser.add_argument("--enable-chunked-prefill",
-                        action='store_true',
-                        help="enable chunked prefill for vLLM backend.")
-    parser.add_argument('--max-num-batched-tokens',
-                        type=int,
                         default=None,
-                        help='maximum number of batched tokens per '
-                        'iteration')
-    parser.add_argument(
-        '--output-json',
-        type=str,
-        default=None,
-        help='Path to save the throughput results in JSON format.')
-    parser.add_argument(
-        '--distributed-executor-backend',
-        choices=['ray', 'mp'],
-        default=None,
-        help='Backend to use for distributed serving. When more than 1 GPU '
-        'is used, will be automatically set to "ray" if installed '
-        'or "mp" (multiprocessing) otherwise.')
-    parser.add_argument(
-        "--disable-async-output-proc",
-        action='store_true',
-        default=False,
-        help="Disable async output processor for vLLM backend.")
+                        help='Path to save the throughput results in JSON format.')
     parser.add_argument("--async-engine",
                         action='store_true',
                         default=False,
@@ -469,7 +312,10 @@ if __name__ == "__main__":
                         action='store_true',
                         default=False,
                         help="Disable decoupled async engine frontend.")
-    parser.add_argument("--warmup", action="store_true", default=False)
+    parser.add_argument("--warmup",
+                        action="store_true",
+                        default=False,
+                        help="Run warmup prompts before benchmark.")
     args = parser.parse_args()
     if args.tokenizer is None:
         args.tokenizer = args.model
