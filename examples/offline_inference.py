fd2e6fed1f9ab57b302c0154c72782020235f445
wchen61 <wchen61@foxmail.com>
2024-11-17 18:22:50 +0800

    Group sampling parameters
    Signed-off-by: wchen61 <wchen61@foxmail.com>

--- examples/offline_inference.py
+++ examples/offline_inference.py
@@ -5,7 +5,7 @@ from vllm.engine.arg_utils import EngineArgs
 from vllm.utils import FlexibleArgumentParser
 
 
-def get_prompts(args):
+def get_prompts(num_prompts: int):
     # The default sample prompts.
     prompts = [
         "Hello, my name is",
@@ -14,16 +14,15 @@ def get_prompts(args):
         "The future of AI is",
     ]
 
-    if args.num_prompts != len(prompts):
-        prompts = (prompts *
-                   ((args.num_prompts // len(prompts)) + 1))[:args.num_prompts]
+    if num_prompts != len(prompts):
+        prompts = (prompts * ((num_prompts // len(prompts)) + 1))[:num_prompts]
 
     return prompts
 
 
 def main(args):
     # Create prompts
-    prompts = get_prompts(args)
+    prompts = get_prompts(args.num_prompts)
 
     # Create a sampling params object.
     sampling_params = SamplingParams(n=args.n,
@@ -50,31 +49,32 @@ def main(args):
 
 if __name__ == '__main__':
     parser = FlexibleArgumentParser()
-    parser.add_argument("--num-prompts",
-                        type=int,
-                        default=4,
-                        help="Number of prompts used for inference")
-    parser.add_argument("--max-tokens",
-                        type=int,
-                        default=16,
-                        help="Generated output length for sampling")
-    parser.add_argument('--n',
-                        type=int,
-                        default=1,
-                        help='Number of generated sequences per prompt')
-    parser.add_argument('--temperature',
-                        type=float,
-                        default=0.8,
-                        help='Temperature for text generation')
-    parser.add_argument('--top-p',
-                        type=float,
-                        default=0.95,
-                        help='top_p for text generation')
-    parser.add_argument('--top-k',
-                        type=int,
-                        default=-1,
-                        help='top_k for text generation')
+    parser = EngineArgs.add_cli_args(parser)
+    group = parser.add_argument_group("SamplingParams options")
+    group.add_argument("--num-prompts",
+                       type=int,
+                       default=4,
+                       help="Number of prompts used for inference")
+    group.add_argument("--max-tokens",
+                       type=int,
+                       default=16,
+                       help="Generated output length for sampling")
+    group.add_argument('--n',
+                       type=int,
+                       default=1,
+                       help='Number of generated sequences per prompt')
+    group.add_argument('--temperature',
+                       type=float,
+                       default=0.8,
+                       help='Temperature for text generation')
+    group.add_argument('--top-p',
+                       type=float,
+                       default=0.95,
+                       help='top_p for text generation')
+    group.add_argument('--top-k',
+                       type=int,
+                       default=-1,
+                       help='top_k for text generation')
 
-    EngineArgs.add_cli_args(parser)
     args = parser.parse_args()
     main(args)
