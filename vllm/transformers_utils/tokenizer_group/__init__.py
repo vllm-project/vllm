9158549c8ec22b447352613fcf6cffd7b6352d94
KuntaiDu <kuntai@uchicago.edu>
2024-11-18 07:17:52 +0000

    clean up wierd spaces in tokenizer groups

--- vllm/transformers_utils/tokenizer_group/__init__.py
+++ vllm/transformers_utils/tokenizer_group/__init__.py
@@ -26,9 +26,9 @@ def init_tokenizer_from_configs(model_config: ModelConfig,
                        revision=model_config.tokenizer_revision)
 
     if (model_config.encoder_config is not None
-            and "do_lower_case" in model_config.encoder_config):	
-        init_kwargs["do_lower_case"] = model_config.encoder_config[	
-            "do_lower_case"]	
+            and "do_lower_case" in model_config.encoder_config):
+        init_kwargs["do_lower_case"] = model_config.encoder_config[
+            "do_lower_case"]
 
     return get_tokenizer_group(parallel_config.tokenizer_pool_config,
                                **init_kwargs)
