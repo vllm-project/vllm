b1d6a6aea7ea5f962a582ce6b9ad3161938f28ef
Isotr0py <2037008807@qq.com>
2024-11-18 21:45:21 +0800

    [Model] Remove transformers attention porting in VITs (#10414)
    Signed-off-by: Isotr0py <2037008807@qq.com>

--- vllm/model_executor/models/molmo.py
+++ vllm/model_executor/models/molmo.py
@@ -187,7 +187,7 @@ class MultiHeadDotProductAttention(nn.Module):
         )
 
         # Detect attention implementation.
-        self.attn_backend: _Backend = get_vit_attn_backend()
+        self.attn_backend: _Backend = get_vit_attn_backend(support_fa=True)
         if self.attn_backend not in {
                 _Backend.FLASH_ATTN, _Backend.TORCH_SDPA, _Backend.XFORMERS
         }:
