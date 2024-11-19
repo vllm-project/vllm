b45c158f63827e0392dd9d9625bca5647d4204cf
rshaw@neuralmagic.com <rshaw@neuralmagic.com>
2024-11-19 19:52:42 +0000

    updated

--- benchmarks/benchmark_throughput.py
+++ benchmarks/benchmark_throughput.py
@@ -302,7 +302,9 @@ def main(args: argparse.Namespace):
                               for request in requests)
     print(f"Throughput: {len(requests) / elapsed_time:.2f} requests/s, "
           f"{total_num_tokens / elapsed_time:.2f} total tokens/s, "
-          f"{total_output_tokens / elapsed_time:.2f} output tokens/s")
+          f"{total_output_tokens / elapsed_time:.2f} output tokens/s, "
+          f"{total_num_tokens=} | {total_output_tokens=}"
+    )
 
     # Output JSON results if specified
     if args.output_json:
--- vllm/model_executor/layers/quantization/compressed_tensors/compressed_tensors.py
+++ vllm/model_executor/layers/quantization/compressed_tensors/compressed_tensors.py
@@ -359,8 +359,7 @@ class CompressedTensorsConfig(QuantizationConfig):
         """
         scheme = CompressedTensors24(
             model_compressor=self.model_compressor,
-            layer_name=layer_name
-        )
+            layer_name=layer_name)
         # scheme = CompressedTensorsW8A8Fp8(
         #     strategy=QuantizationStrategy.CHANNEL,
         #     is_static_input_scheme=False)
