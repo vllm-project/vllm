069f8cad4e233f7cd7ee7a3691a36fcd5f7ad666
Kunjan Patel <kunjan@ucla.edu>
2024-11-20 05:07:37 +0000

    Revert "Revert "Add orca format flag and header adding""
    This reverts commit 8c6f2a281d4c612ae2025d7696d50e2b9e567109.

--- vllm/entrypoints/openai/api_server.py
+++ vllm/entrypoints/openai/api_server.py
@@ -6,13 +6,15 @@ import os
 import re
 import signal
 import socket
+import base64
+import json
 import tempfile
 import uuid
 from argparse import Namespace
 from contextlib import asynccontextmanager
 from functools import partial
 from http import HTTPStatus
-from typing import AsyncIterator, Optional, Set, Tuple
+from typing import AsyncIterator, Optional, Set, Tuple, Dict, List, Mapping
 
 import uvloop
 from fastapi import APIRouter, FastAPI, Request
@@ -24,6 +26,7 @@ from starlette.routing import Mount
 from typing_extensions import assert_never
 
 import vllm.envs as envs
+import vllm.entrypoints.openai.orca_load_report_pb2 as orca_load_report_pb2
 from vllm.config import ModelConfig
 from vllm.engine.arg_utils import AsyncEngineArgs
 from vllm.engine.multiprocessing.client import MQLLMEngineClient
@@ -47,7 +50,8 @@ from vllm.entrypoints.openai.protocol import (ChatCompletionRequest,
                                               LoadLoraAdapterRequest,
                                               TokenizeRequest,
                                               TokenizeResponse,
-                                              UnloadLoraAdapterRequest)
+                                              UnloadLoraAdapterRequest,
+                                              EngineMetrics)
 # yapf: enable
 from vllm.entrypoints.openai.serving_chat import OpenAIServingChat
 from vllm.entrypoints.openai.serving_completion import OpenAIServingCompletion
@@ -346,7 +350,6 @@ async def create_chat_completion(request: ChatCompletionRequest,
             message="The model does not support Chat Completions API")
 
     generator = await handler.create_chat_completion(request, raw_request)
-
     if isinstance(generator, ErrorResponse):
         return JSONResponse(content=generator.model_dump(),
                             status_code=generator.code)
@@ -356,6 +359,79 @@ async def create_chat_completion(request: ChatCompletionRequest,
 
     return StreamingResponse(content=generator, media_type="text/event-stream")
 
+def create_orca_header(
+    format: str, named_metrics: List[Tuple[str, float]], metadata_fields=None
+) -> Optional[Mapping[str,str]]:
+    """
+    Creates ORCA headers named 'endpoint-load-metrics' in the specified format and adds custom metrics to named_metrics.
+    ORCA headers format description: https://docs.google.com/document/d/1C1ybMmDKJIVlrbOLbywhu9iRYo4rilR-cT50OTtOFTs/edit?tab=t.0
+    ORCA proto https://github.com/cncf/xds/blob/main/xds/data/orca/v3/orca_load_report.proto
+
+    Parameters:
+    - format (str): The format of the header ('BIN', 'TEXT', 'JSON').
+    - named_metrics (List[Tuple[str, float]]): List of tuples with metric names and their corresponding double values.
+    - metadata_fields (list): List of additional metadata fields (currently unsupported).
+
+    Returns:
+    - Optional[Mapping[str,str]]: A dictionary with header key as 'endpoint-load-metrics' and values as the ORCA header strings with format prefix and data in  with named_metrics in.
+    """
+        
+    if metadata_fields:
+        logger.warning(
+            "Warning: `metadata_fields` are not supported in the ORCA response header yet."
+        )
+        
+    if format not in ["BIN","TEXT","JSON"]:
+        logger.warning(
+            "Warning: `%s` format is not supported in the ORCA response header",
+            format
+        )
+        return 
+
+    header = {}
+    orca_report = {
+        "named_metrics": {
+            metric_name: value for metric_name, value in named_metrics 
+            if isinstance(metric_name,str) and isinstance(value, float)
+        }
+    }
+    # output example:
+    # endpoint-load-metrics: BIN
+    # CZqZmZmZmbk/MQAAAAAAAABAQg4KA2ZvbxGamZmZmZm5P0IOCgNiYXIRmpmZmZmZyT8=
+    if format == "BIN":
+        load_proto = orca_load_report_pb2.OrcaLoadReport()
+        for metric_name, value in named_metrics:
+            if isinstance(metric_name,str) and isinstance(value, float):
+                load_proto.named_metrics[metric_name] = value
+        binary_data = load_proto.SerializeToString()
+        base64_encoded_data = base64.b64encode(binary_data).decode("utf-8")
+        header["endpoint-load-metrics"] = f"BIN {base64_encoded_data}"
+
+    # output example:
+    # endpoint-load-metrics: TEXT named_metrics.kv_cache_utilization=0.4
+    elif format == "TEXT":
+        native_http_header = ", ".join(
+            [
+                f"named_metrics.{metric_name}={value}"
+                for  metric_name, value in named_metrics 
+                if isinstance(metric_name,str) and isinstance(value, float)
+            ]
+        )
+        header["endpoint-load-metrics"] = f"TEXT {native_http_header}"
+
+    # output example:
+    # endpoint-load-metrics: JSON “named_metrics”: {“custom-metric-util”: 0.4}
+    elif format == "JSON":
+        header["endpoint-load-metrics"] = f"JSON {json.dumps(orca_report)}"
+
+    return header
+
+def metrics_header (m:EngineMetrics) -> Optional[Mapping[str, str]]:
+    named_metrics = []
+    for metric, val in vars(m).items():
+        if isinstance(val, float) and metric != "format":
+            named_metrics.append([metric,val])
+    return create_orca_header(m.format, named_metrics)
 
 @router.post("/v1/completions")
 async def create_completion(request: CompletionRequest, raw_request: Request):
@@ -369,7 +445,8 @@ async def create_completion(request: CompletionRequest, raw_request: Request):
         return JSONResponse(content=generator.model_dump(),
                             status_code=generator.code)
     elif isinstance(generator, CompletionResponse):
-        return JSONResponse(content=generator.model_dump())
+        header = metrics_header(generator.metrics)
+        return JSONResponse(content=generator.model_dump(), headers= header)
 
     return StreamingResponse(content=generator, media_type="text/event-stream")
 
@@ -553,6 +630,7 @@ def init_app_state(
         model_config,
         base_model_paths,
         lora_modules=args.lora_modules,
+        orca_format=args.orca_format,
         prompt_adapters=args.prompt_adapters,
         request_logger=request_logger,
         return_tokens_as_token_ids=args.return_tokens_as_token_ids,
