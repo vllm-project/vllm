d6ebd1ff69dce8dc55ef8cd65dec33592cad685f
Isotr0py <2037008807@qq.com>
2024-11-20 11:19:35 +0800

    revert mamba
    Signed-off-by: Isotr0py <2037008807@qq.com>

--- vllm/model_executor/models/mamba.py
+++ vllm/model_executor/models/mamba.py
@@ -48,7 +48,7 @@ class MambaDecoderLayer(nn.Module):
                                 time_step_rank=config.time_step_rank,
                                 use_conv_bias=config.use_conv_bias,
                                 use_bias=config.use_bias,
-                                use_rms_norm=False,
+                                use_rms_norm=self.is_falcon_mamba,
                                 rms_norm_eps=mixer_rms_eps,
                                 activation=config.hidden_act)
 
@@ -246,7 +246,6 @@ class MambaForCausalLM(nn.Module, HasInnerState, IsAttentionFree):
     def load_weights(self, weights: Iterable[Tuple[str,
                                                    torch.Tensor]]) -> Set[str]:
         params_dict = dict(self.named_parameters())
-        loaded_params: Set[str] = set()
         for name, loaded_weight in weights:
             if "A_log" in name:
                 name = name.replace("A_log", "A")
@@ -258,5 +257,3 @@ class MambaForCausalLM(nn.Module, HasInnerState, IsAttentionFree):
             weight_loader = getattr(param, "weight_loader",
                                     default_weight_loader)
             weight_loader(param, loaded_weight)
-            loaded_params.add(name)
-        return loaded_params
