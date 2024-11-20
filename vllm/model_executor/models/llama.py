80455586c9f6a69bfca248098865e723e53a15cf
andoorve <37849411+andoorve@users.noreply.github.com>
2024-11-20 04:14:48 +0000

    Example to increase llama blocks
    Signed-off-by: andoorve <37849411+andoorve@users.noreply.github.com>

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -89,12 +89,6 @@ class LlamaMLP(nn.Module):
                              "Only silu is supported for now.")
         self.act_fn = SiluAndMul()
 
-    def forward(self, x):
-        x, _ = self.gate_up_proj(x)
-        x = self.act_fn(x)
-        x, _ = self.down_proj(x)
-        return x
-
 
 class LlamaAttention(nn.Module):
 
@@ -176,20 +170,6 @@ class LlamaAttention(nn.Module):
             quant_config=quant_config,
         )
 
-    def forward(
-        self,
-        positions: torch.Tensor,
-        hidden_states: torch.Tensor,
-        kv_cache: torch.Tensor,
-        attn_metadata: AttentionMetadata,
-    ) -> torch.Tensor:
-        qkv, _ = self.qkv_proj(hidden_states)
-        q, k, v = qkv.split([self.q_size, self.kv_size, self.kv_size], dim=-1)
-        q, k = self.rotary_emb(positions, q, k)
-        attn_output = self.attn(q, k, v, kv_cache, attn_metadata)
-        output, _ = self.o_proj(attn_output)
-        return output
-
 
 class LlamaDecoderLayer(nn.Module):
 
@@ -240,32 +220,6 @@ class LlamaDecoderLayer(nn.Module):
                                        eps=config.rms_norm_eps)
         self.post_attention_layernorm = RMSNorm(config.hidden_size,
                                                 eps=config.rms_norm_eps)
-
-    def forward(
-        self,
-        positions: torch.Tensor,
-        hidden_states: torch.Tensor,
-        kv_cache: torch.Tensor,
-        attn_metadata: AttentionMetadata,
-        residual: Optional[torch.Tensor],
-    ) -> Tuple[torch.Tensor, torch.Tensor]:
-        # Self Attention
-        if residual is None:
-            residual = hidden_states
-            hidden_states = self.input_layernorm(hidden_states)
-        else:
-            hidden_states, residual = self.input_layernorm(
-                hidden_states, residual)
-        hidden_states = self.self_attn(positions=positions,
-                                       hidden_states=hidden_states,
-                                       kv_cache=kv_cache,
-                                       attn_metadata=attn_metadata)
-
-        # Fully Connected
-        hidden_states, residual = self.post_attention_layernorm(
-            hidden_states, residual)
-        hidden_states = self.mlp(hidden_states)
-        return hidden_states, residual
 
 
 @support_torch_compile
@@ -337,9 +291,31 @@ class LlamaModel(nn.Module):
 
         for i in range(self.start_layer, self.end_layer):
             layer = self.layers[i]
-            hidden_states, residual = layer(positions, hidden_states,
-                                            kv_caches[i - self.start_layer],
-                                            attn_metadata, residual)
+            # Self Attention
+            if residual is None:
+                residual = hidden_states
+                hidden_states = layer.input_layernorm(hidden_states)
+            else:
+                hidden_states, residual = layer.input_layernorm(
+                    hidden_states, residual)
+            qkv, _ = layer.self_attn.qkv_proj(hidden_states)
+            q, k, v = qkv.split([
+                layer.self_attn.q_size, layer.self_attn.kv_size,
+                layer.self_attn.kv_size
+            ],
+                                dim=-1)
+            del qkv
+            q, k = layer.self_attn.rotary_emb(positions, q, k)
+            hidden_states = layer.self_attn.attn(
+                q, k, v, kv_caches[i - self.start_layer], attn_metadata)
+            del q, k, v
+            hidden_states, _ = layer.self_attn.o_proj(hidden_states)
+            # Fully Connected
+            hidden_states, residual = layer.post_attention_layernorm(
+                hidden_states, residual)
+            hidden_states, _ = layer.mlp.gate_up_proj(hidden_states)
+            hidden_states = layer.mlp.act_fn(hidden_states)
+            hidden_states, _ = layer.mlp.down_proj(hidden_states)
 
         if not get_pp_group().is_last_rank:
             return IntermediateTensors({
