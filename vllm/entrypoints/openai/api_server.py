1cce30140aead5fd59a714ffe0cc572decd3f463
Kunjan Patel <kunjan@ucla.edu>
2024-11-20 05:16:46 +0000

    Support chat completion
    Signed-off-by: Kunjan Patel <kunjan@ucla.edu>

--- vllm/entrypoints/openai/api_server.py
+++ vllm/entrypoints/openai/api_server.py
@@ -6,8 +6,6 @@ import os
 import re
 import signal
 import socket
-import base64
-import json
 import tempfile
 import uuid
 from argparse import Namespace
@@ -26,13 +24,13 @@ from starlette.routing import Mount
 from typing_extensions import assert_never
 
 import vllm.envs as envs
-import vllm.entrypoints.openai.orca_load_report_pb2 as orca_load_report_pb2
 from vllm.config import ModelConfig
 from vllm.engine.arg_utils import AsyncEngineArgs
 from vllm.engine.multiprocessing.client import MQLLMEngineClient
 from vllm.engine.multiprocessing.engine import run_mp_engine
 from vllm.engine.protocol import EngineClient
 from vllm.entrypoints.chat_utils import load_chat_template
+from vllm.entrypoints.openai.orca_header import metrics_header
 from vllm.entrypoints.launcher import serve_http
 from vllm.entrypoints.logger import RequestLogger
 from vllm.entrypoints.openai.cli_args import (make_arg_parser,
@@ -51,7 +49,7 @@ from vllm.entrypoints.openai.protocol import (ChatCompletionRequest,
                                               TokenizeRequest,
                                               TokenizeResponse,
                                               UnloadLoraAdapterRequest,
-                                              EngineMetrics)
+                                              )
 # yapf: enable
 from vllm.entrypoints.openai.serving_chat import OpenAIServingChat
 from vllm.entrypoints.openai.serving_completion import OpenAIServingCompletion
@@ -355,83 +353,10 @@ async def create_chat_completion(request: ChatCompletionRequest,
                             status_code=generator.code)
 
     elif isinstance(generator, ChatCompletionResponse):
-        return JSONResponse(content=generator.model_dump())
+        header = metrics_header(generator.metrics)
+        return JSONResponse(content=generator.model_dump(), headers= header)
 
     return StreamingResponse(content=generator, media_type="text/event-stream")
-
-def create_orca_header(
-    format: str, named_metrics: List[Tuple[str, float]], metadata_fields=None
-) -> Optional[Mapping[str,str]]:
-    """
-    Creates ORCA headers named 'endpoint-load-metrics' in the specified format and adds custom metrics to named_metrics.
-    ORCA headers format description: https://docs.google.com/document/d/1C1ybMmDKJIVlrbOLbywhu9iRYo4rilR-cT50OTtOFTs/edit?tab=t.0
-    ORCA proto https://github.com/cncf/xds/blob/main/xds/data/orca/v3/orca_load_report.proto
-
-    Parameters:
-    - format (str): The format of the header ('BIN', 'TEXT', 'JSON').
-    - named_metrics (List[Tuple[str, float]]): List of tuples with metric names and their corresponding double values.
-    - metadata_fields (list): List of additional metadata fields (currently unsupported).
-
-    Returns:
-    - Optional[Mapping[str,str]]: A dictionary with header key as 'endpoint-load-metrics' and values as the ORCA header strings with format prefix and data in  with named_metrics in.
-    """
-        
-    if metadata_fields:
-        logger.warning(
-            "Warning: `metadata_fields` are not supported in the ORCA response header yet."
-        )
-        
-    if format not in ["BIN","TEXT","JSON"]:
-        logger.warning(
-            "Warning: `%s` format is not supported in the ORCA response header",
-            format
-        )
-        return 
-
-    header = {}
-    orca_report = {
-        "named_metrics": {
-            metric_name: value for metric_name, value in named_metrics 
-            if isinstance(metric_name,str) and isinstance(value, float)
-        }
-    }
-    # output example:
-    # endpoint-load-metrics: BIN
-    # CZqZmZmZmbk/MQAAAAAAAABAQg4KA2ZvbxGamZmZmZm5P0IOCgNiYXIRmpmZmZmZyT8=
-    if format == "BIN":
-        load_proto = orca_load_report_pb2.OrcaLoadReport()
-        for metric_name, value in named_metrics:
-            if isinstance(metric_name,str) and isinstance(value, float):
-                load_proto.named_metrics[metric_name] = value
-        binary_data = load_proto.SerializeToString()
-        base64_encoded_data = base64.b64encode(binary_data).decode("utf-8")
-        header["endpoint-load-metrics"] = f"BIN {base64_encoded_data}"
-
-    # output example:
-    # endpoint-load-metrics: TEXT named_metrics.kv_cache_utilization=0.4
-    elif format == "TEXT":
-        native_http_header = ", ".join(
-            [
-                f"named_metrics.{metric_name}={value}"
-                for  metric_name, value in named_metrics 
-                if isinstance(metric_name,str) and isinstance(value, float)
-            ]
-        )
-        header["endpoint-load-metrics"] = f"TEXT {native_http_header}"
-
-    # output example:
-    # endpoint-load-metrics: JSON “named_metrics”: {“custom-metric-util”: 0.4}
-    elif format == "JSON":
-        header["endpoint-load-metrics"] = f"JSON {json.dumps(orca_report)}"
-
-    return header
-
-def metrics_header (m:EngineMetrics) -> Optional[Mapping[str, str]]:
-    named_metrics = []
-    for metric, val in vars(m).items():
-        if isinstance(val, float) and metric != "format":
-            named_metrics.append([metric,val])
-    return create_orca_header(m.format, named_metrics)
 
 @router.post("/v1/completions")
 async def create_completion(request: CompletionRequest, raw_request: Request):
