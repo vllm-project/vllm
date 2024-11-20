8c3f56a8622547ec195170da8460af5d9f5615ec
Michał Kuligowski <mkuligowski@habana.ai>
2024-11-20 12:50:39 +0100

    Update ray_hpu_executor.py (#522)

--- vllm/executor/ray_hpu_executor.py
+++ vllm/executor/ray_hpu_executor.py
@@ -71,10 +71,13 @@ class RayHPUExecutor(DistributedGPUExecutor):
             Optional[List[SamplerOutput]])
 
     def shutdown(self) -> None:
+        for worker in self.workers:
+            worker.__ray_terminate__.remote()
         if hasattr(self, "forward_dag") and self.forward_dag is not None:
             self.forward_dag.teardown()
+            import ray
             for worker in self.workers:
-                worker.__ray_terminate__.remote()
+                ray.kill(worker)
             self.forward_dag = None
 
     def finish_measurements(self):
