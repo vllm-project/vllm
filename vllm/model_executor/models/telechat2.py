6b1c2c4619d73385e52fbee4767608da21c70e02
xiangw2 <xiangw2@chinatelecom.cn>
2024-11-20 15:27:15 +0800

    fix

--- vllm/model_executor/models/telechat2.py
+++ vllm/model_executor/models/telechat2.py
@@ -22,15 +22,15 @@ from typing import Any, Dict, Iterable, List, Optional, Set, Tuple, Union
 import torch
 from torch import nn
 from transformers import PretrainedConfig
-from vllm.config import CacheConfig, VllmConfig
+
 from vllm.attention import Attention, AttentionMetadata
+from vllm.config import CacheConfig, VllmConfig
+from vllm.distributed import get_tensor_model_parallel_world_size
 from vllm.model_executor.layers.activation import SiluAndMul
 from vllm.model_executor.layers.layernorm import RMSNorm
 from vllm.model_executor.layers.linear import (MergedColumnParallelLinear,
                                                QKVParallelLinear,
                                                RowParallelLinear)
-from vllm.distributed import (get_tensor_model_parallel_world_size)
-
 from vllm.model_executor.layers.logits_processor import LogitsProcessor
 from vllm.model_executor.layers.quantization.base_config import (
     QuantizationConfig)
@@ -41,8 +41,8 @@ from vllm.model_executor.layers.vocab_parallel_embedding import (
 from vllm.model_executor.model_loader.weight_utils import (
     default_weight_loader)
 from vllm.model_executor.sampling_metadata import SamplingMetadata
-from vllm.sequence import IntermediateTensors
 from vllm.model_executor.layers.sampler import SamplerOutput
+from vllm.sequence import IntermediateTensors
 from .utils import AutoWeightsLoader, make_layers, maybe_prefix
 
 class TeleChat2MLP(nn.Module):
