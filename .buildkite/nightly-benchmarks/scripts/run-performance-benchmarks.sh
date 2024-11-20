474a3cefb23115883d6810a6ba9bc3490d0bd7a8
simon-mo <simon.mo@hey.com>
2024-11-19 16:30:08 -0800

    carry on

--- .buildkite/nightly-benchmarks/scripts/run-performance-benchmarks.sh
+++ .buildkite/nightly-benchmarks/scripts/run-performance-benchmarks.sh
@@ -6,7 +6,7 @@
 
 # Do not set -e, as the mixtral 8x22B model tends to crash occasionally
 # and we still want to see other benchmarking results even when mixtral crashes.
-set -ex
+set -x
 set -o pipefail
 
 check_gpus() {
