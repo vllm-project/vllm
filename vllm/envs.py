93274b5a5ae10c742c905f15b6c3ac7b5940c394
Cyrus Leung <tlleungac@connect.ust.hk>
2024-11-21 15:06:42 +0800

    [Misc] Increase default video fetch timeout (#10495)
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- vllm/envs.py
+++ vllm/envs.py
@@ -49,7 +49,7 @@ if TYPE_CHECKING:
     VLLM_WORKER_MULTIPROC_METHOD: str = "fork"
     VLLM_ASSETS_CACHE: str = os.path.join(VLLM_CACHE_ROOT, "assets")
     VLLM_IMAGE_FETCH_TIMEOUT: int = 5
-    VLLM_VIDEO_FETCH_TIMEOUT: int = 15
+    VLLM_VIDEO_FETCH_TIMEOUT: int = 30
     VLLM_AUDIO_FETCH_TIMEOUT: int = 10
     VLLM_TARGET_DEVICE: str = "cuda"
     MAX_JOBS: Optional[str] = None
