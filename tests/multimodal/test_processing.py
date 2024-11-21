3baa22a800bb220516f55de251e43ca821305da4
DarkLight1337 <tlleungac@connect.ust.hk>
2024-11-21 18:06:06 +0000

    Setup tests
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- tests/multimodal/test_processing.py
+++ tests/multimodal/test_processing.py
@@ -1,6 +1,14 @@
-import pytest
+from typing import cast
 
-from vllm.multimodal.processing import iter_token_matches, iter_token_runs
+import pytest
+from transformers import BatchFeature
+
+from vllm.config import ModelConfig
+from vllm.multimodal.processing import (InputProcessingContext,
+                                        ModalityProcessingMetadata,
+                                        MultiModalProcessor, PromptReplacement,
+                                        iter_token_matches, iter_token_runs)
+from vllm.multimodal.utils import cached_get_tokenizer
 
 
 # yapf: disable
@@ -98,3 +106,113 @@ def test_iter_token_matches(token_ids, match_ids, expected):
     match_lens = [end - start for start, end in result]
     print("match_lens:", match_lens)  # Only displayed on error
     assert all(match_len == len(match_ids) for match_len in match_lens)
+
+
+@pytest.mark.parametrize("tokenizer_id", [
+    "llava-hf/llava-1.5-7b-hf",
+    "meta-llama/Llama-3.2-11B-Vision-Instruct",
+    "microsoft/Phi-3.5-vision-instruct",
+    "Qwen/Qwen2-VL-2B-Instruct",
+])
+@pytest.mark.parametrize(
+    "prompt_str",
+    [
+        "What is in this image?",
+        # LLaVA
+        "<image>What is in this image?",
+        "What is<image>in this image?",
+        "What is in this image?<image>",
+        # LLama-3.2
+        "<|image|>What is in this image?",
+        "What is<|image|>in this image?",
+        "What is in this image?<|image|>",
+        # Phi-3-vision
+        "<image_1>What is in this image?",
+        "What is<image_1>in this image?",
+        "What is in this image?<image_1>",
+        # Qwen2-VL
+        "<|vision_start|><|image_pad|><|vision_end|>What is in this image?",
+        "What is<|vision_start|><|image_pad|><|vision_end|>in this image?",
+        "What is in this image?<|vision_start|><|image_pad|><|vision_end|>",
+    ])
+@pytest.mark.parametrize(
+    "repl_target_str",
+    [
+        # No match
+        "No",
+        # Has match
+        "i",
+        "image",
+        "image?",
+        "<image>",
+        "<|image|>",
+        "<image_1>",
+        "<|vision_start|><|image_pad|><|vision_end|>",
+        "<s>",
+    ])
+@pytest.mark.parametrize(
+    "repl_unit_str",
+    [
+        # No match
+        "No",
+        # Has match
+        "i",
+        "image",
+        "image?",
+        "<image>",
+        "<|image|>",
+        "<image_1>",
+        "<|vision_start|><|image_pad|><|vision_end|>",
+        "<s>",
+    ])
+@pytest.mark.parametrize("repl_count", [0, 1, 2])
+@pytest.mark.parametrize("mm_count", [0, 1, 2])
+def test_processor_prompt_replacements(
+    tokenizer_id,
+    prompt_str,
+    repl_target_str,
+    repl_unit_str,
+    repl_count,
+    mm_count,
+):
+    model_config = cast(ModelConfig, object())
+    tokenizer = cached_get_tokenizer(tokenizer_id)
+
+    ctx = InputProcessingContext(model_config, tokenizer)
+    metadata = ModalityProcessingMetadata(prompt_repls=[
+        PromptReplacement(repl_target_str, repl_unit_str,
+                          lambda *args, **kwargs: repl_count),
+    ])
+    modality = "image"
+
+    dummy_metadata = {modality: metadata}
+    dummy_mm_data = {modality: list(range(mm_count))}
+    prompt_ids = tokenizer.encode(prompt_str)
+
+    processor = MultiModalProcessor(ctx, dummy_metadata)
+    new_token_ids, placeholder_ranges = processor._apply_prompt_replacements(
+        dummy_mm_data,
+        BatchFeature(),
+        prompt_ids,
+    )
+
+    repl_unit_ids = tokenizer.encode(repl_unit_str)
+
+    # Only displayed on error
+    print("prompt_ids:", prompt_ids)
+    print("repl_unit_ids:", repl_unit_ids)
+    print("new_token_ids:", new_token_ids)
+    print("placeholder_ranges:", placeholder_ranges)
+
+    # Invariants
+    if mm_count == 0:
+        assert new_token_ids == prompt_ids
+
+    assert len(placeholder_ranges) <= mm_count
+
+    for placeholder_range in placeholder_ranges:
+        repl_offset = placeholder_range["offset"]
+        repl_len = placeholder_range["length"]
+
+        assert new_token_ids[repl_offset:repl_offset +
+                             repl_len] == repl_unit_ids * repl_count
