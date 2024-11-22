984f0470c3a48ee06d22b73599612b8691cae1db
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2024-11-21 19:38:16 -0800

    fix
    Signed-off-by: Woosuk Kwon <woosuk.kwon@berkeley.edu>

--- vllm/entrypoints/llm.py
+++ vllm/entrypoints/llm.py
@@ -110,9 +110,8 @@ class LLM:
             HuggingFace config. If a callable, it is called to update the
             HuggingFace config.
         compilation_config: Either an integer or a dictionary. If it is an
-            integer, it is used as the level of compilation optimization.
-            If it is a dictionary, it can specify the full compilation
-            configuration.
+            integer, it is used as the level of compilation optimization. If it
+            is a dictionary, it can specify the full compilation configuration.
         **kwargs: Arguments for :class:`~vllm.EngineArgs`. (See
             :ref:`engine_args`)
 
