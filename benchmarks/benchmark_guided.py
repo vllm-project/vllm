18c245cbd483cb3dc7b4b60d03403e744b81b031
Chendi Xue <chendi.xue@intel.com>
2024-11-22 01:00:16 -0500

    Add xgrammar similiar dataset
    Signed-off-by: Chendi Xue <chendi.xue@intel.com>

--- benchmarks/benchmark_guided.py
+++ benchmarks/benchmark_guided.py
@@ -1,12 +1,14 @@
 """Benchmark guided decoding throughput."""
 import argparse
+import dataclasses
 import json
 import random
 import time
 from typing import List
 
+import datasets
 import uvloop
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, PreTrainedTokenizerBase
 
 from vllm.engine.arg_utils import AsyncEngineArgs, EngineArgs
 from vllm.entrypoints.openai.api_server import (
@@ -129,46 +131,74 @@ SCHEMA = {
 }
 
 
-def run_vllm(
-    requests: List[tuple[str, int, int]],
-    engine_args: EngineArgs,
-    n: int,
-    guided_decoding: bool = False,
-    warmup: bool = False,
-) -> float:
+@dataclasses.dataclass
+class SampleRequest:
+    """A class representing a single inference request for benchmarking.
+
+    Attributes:
+        prompt: The input text prompt for the model.
+        multi_modal_data: Optional dictionary containing multi-modal data (e.g.
+            images).
+        prompt_len: The length of the prompt in tokens.
+        expected_output_len: The expected length of the output in tokens.
+    """
+    prompt: str
+    prompt_len: int
+    expected_output_len: int
+    schema: dict
+    completion: str = None
+
+
+def run_vllm(requests: List[SampleRequest],
+             engine_args: EngineArgs,
+             n: int,
+             guided_decoding: bool = False,
+             warmup: bool = False,
+             result_file_name: str = None) -> float:
     from vllm import LLM, SamplingParams
     llm = LLM(**vars(engine_args))
 
     # Add the requests to the engine.
     prompts: List[str] = []
     sampling_params: List[SamplingParams] = []
-    for prompt, _, output_len in requests:
-        prompts.append(prompt)
+    for request in requests:
+        prompts.append(request.prompt)
         sampling_params.append(
             SamplingParams(
                 n=n,
                 temperature=1.0,
                 top_p=1.0,
                 ignore_eos=True,
-                max_tokens=output_len,
+                max_tokens=request.expected_output_len,
                 guided_decoding=GuidedDecodingParams(
-                    json=SCHEMA) if guided_decoding else None,
+                    json=request.schema) if guided_decoding else None,
             ))
 
     start = time.perf_counter()
-    llm.generate(prompts, sampling_params, use_tqdm=True)
+    outputs = llm.generate(prompts, sampling_params, use_tqdm=True)
+    ret = []
+    for output, request in zip(outputs, requests):
+        generated_text = output.outputs[0].text
+        ret.append({
+            "generated": generated_text,
+            "expected": request.completion
+        })
     end = time.perf_counter()
+    # save ret list into a json
+    if result_file_name:
+        with open(result_file_name, 'w') as f:
+            json.dump(ret, f, indent=4)
+            f.write("\n")
     return end - start
 
 
-async def run_vllm_async(
-    requests: List[tuple[str, int, int]],
-    engine_args: AsyncEngineArgs,
-    n: int,
-    guided_decoding: bool = False,
-    warmup: bool = False,
-    disable_frontend_multiprocessing: bool = False,
-) -> float:
+async def run_vllm_async(requests: List[SampleRequest],
+                         engine_args: AsyncEngineArgs,
+                         n: int,
+                         guided_decoding: bool = False,
+                         warmup: bool = False,
+                         disable_frontend_multiprocessing: bool = False,
+                         result_file_name: str = None) -> float:
     from vllm import SamplingParams
 
     async with build_async_engine_client_from_engine_args(
@@ -182,17 +212,17 @@ async def run_vllm_async(
             # We setup the first 5 requests to warmup FSM
             warmup_requests = requests[:5]
             requests = requests[5:]
-            for prompt, _, output_len in warmup_requests:
-                prompts.append(prompt)
+            for request in warmup_requests:
+                prompts.append(request.prompt)
                 sampling_params.append(
                     SamplingParams(
                         n=n,
                         temperature=1.0,
                         top_p=1.0,
                         ignore_eos=True,
-                        max_tokens=output_len,
+                        max_tokens=request.expected_output_len,
                         guided_decoding=GuidedDecodingParams(
-                            json=SCHEMA) if guided_decoding else None,
+                            json=request.schema) if guided_decoding else None,
                     ))
             generators = []
             for i, (prompt, sp) in enumerate(zip(prompts, sampling_params)):
@@ -204,17 +234,17 @@ async def run_vllm_async(
 
         prompts = []
         sampling_params = []
-        for prompt, _, output_len in requests:
-            prompts.append(prompt)
+        for request in requests:
+            prompts.append(request.prompt)
             sampling_params.append(
                 SamplingParams(
                     n=n,
                     temperature=1.0,
                     top_p=1.0,
                     ignore_eos=True,
-                    max_tokens=output_len,
+                    max_tokens=request.expected_output_len,
                     guided_decoding=GuidedDecodingParams(
-                        json=SCHEMA) if guided_decoding else None,
+                        json=request.schema) if guided_decoding else None,
                 ))
 
         generators = []
@@ -223,24 +253,78 @@ async def run_vllm_async(
             generator = llm.generate(prompt, sp, request_id=f"test{i}")
             generators.append(generator)
         all_gens = merge_async_iterators(*generators)
+        ret = []
         async for i, res in all_gens:
-            pass
+            generated_text = res.outputs[0].text
+            ret.append(generated_text)
         end = time.perf_counter()
+        if result_file_name:
+            with open(result_file_name, 'w') as f:
+                json.dump(ret, f, indent=4)
+                f.write("\n")
         return end - start
+
+
+def sample_requests(tokenizer: PreTrainedTokenizerBase,
+                    args: argparse.Namespace) -> List[SampleRequest]:
+    if args.dataset == 'single_schema':
+        prompt = f"Generate an example of a user profile given the following schema: {json.dumps(SCHEMA)}"  # noqa: E501
+        input_len = len(tokenizer(prompt).input_ids)
+        print(f"Input length of the prompt: {input_len} tokens")
+        requests = [
+            SampleRequest(prompt=prompt,
+                          prompt_len=input_len,
+                          expected_output_len=args.output_len,
+                          schema=SCHEMA) for _ in range(args.num_prompts)
+        ]
+
+    elif args.dataset == "xgrammar_bench":
+        requests: List[SampleRequest] = []
+        dataset = datasets.load_dataset("NousResearch/json-mode-eval",
+                                        split="train")
+        print(f"dataset has {len(dataset)} entries")
+        len_dataset = len(dataset)
+        for data_point_idx in range(args.num_prompts):
+            idx = data_point_idx
+            while idx >= len_dataset:
+                idx -= len_dataset
+            schema = dataset["schema"][idx]
+            prompt = tokenizer.apply_chat_template(dataset["prompt"][idx],
+                                                   tokenize=False)
+            input_len = len(tokenizer(prompt).input_ids)
+            completion = dataset["completion"][idx]
+
+            requests.append(
+                SampleRequest(prompt=prompt,
+                              prompt_len=input_len,
+                              expected_output_len=args.output_len,
+                              schema=schema,
+                              completion=completion))
+
+    return requests
 
 
 def main(args: argparse.Namespace):
     print(args)
     random.seed(args.seed)
 
+    if args.save_results:
+        result_file_name = 'guided' if args.guided_decoding else 'no_guided'
+        result_file_name += f"_{args.model.split('/')[-1]}"
+        result_file_name += f"_{args.dataset}"
+        result_file_name += f"_{args.num_prompts}"
+        result_file_name += f"_out{args.output_len}"
+        result_file_name += f"_async{args.async_engine}"
+        result_file_name += f"_warmup{args.warmup}"
+        result_file_name += f"_chunkedprefill{args.enable_chunked_prefill}"
+        result_file_name += ".txt"
+    else:
+        result_file_name = None
+
     # Synthesize a prompt with the given input length.
     tokenizer = AutoTokenizer.from_pretrained(
         args.tokenizer, trust_remote_code=args.trust_remote_code)
-    prompt = f"Generate an example of a user profile given the following schema: {json.dumps(SCHEMA)}"  # noqa: E501
-    input_len = len(tokenizer(prompt).input_ids)
-    print(f"Input length of the prompt: {input_len} tokens")
-    requests = [(prompt, input_len, args.output_len)
-                for _ in range(args.num_prompts)]
+    requests = sample_requests(tokenizer, args)
 
     if args.async_engine:
         engine_args = AsyncEngineArgs.from_cli_args(args)
@@ -252,6 +336,7 @@ def main(args: argparse.Namespace):
                 args.guided_decoding,
                 args.warmup,
                 args.disable_frontend_multiprocessing,
+                result_file_name,
             ))
     else:
         engine_args = EngineArgs.from_cli_args(args)
@@ -261,26 +346,41 @@ def main(args: argparse.Namespace):
             args.n,
             args.guided_decoding,
             args.warmup,
+            result_file_name,
         )
 
-    total_num_tokens = sum(prompt_len + output_len
-                           for _, prompt_len, output_len in requests)
-    total_output_tokens = sum(output_len for _, _, output_len in requests)
+    total_num_tokens = sum(request.prompt_len + request.expected_output_len
+                           for request in requests)
+    total_output_tokens = sum(request.expected_output_len
+                              for request in requests)
     print(f"Throughput: {len(requests) / elapsed_time:.2f} requests/s, "
           f"{total_num_tokens / elapsed_time:.2f} total tokens/s, "
           f"{total_output_tokens / elapsed_time:.2f} output tokens/s")
 
     # Output JSON results if specified
-    if args.output_json:
+    if args.output_json or result_file_name:
         results = {
-            "elapsed_time": elapsed_time,
-            "num_requests": len(requests),
-            "total_num_tokens": total_num_tokens,
-            "requests_per_second": len(requests) / elapsed_time,
-            "tokens_per_second": total_num_tokens / elapsed_time,
+            "elapsed_time":
+            elapsed_time,
+            "num_requests":
+            len(requests),
+            "total_num_tokens":
+            total_num_tokens,
+            "total_output_tokens":
+            total_output_tokens,
+            "requests_per_second":
+            len(requests) / elapsed_time,
+            "tokens_per_second":
+            f"{total_num_tokens / elapsed_time:.2f}",
+            "output_tokens_per_second":
+            f"{total_output_tokens / elapsed_time:.2f}",
         }
-        with open(args.output_json, "w") as f:
-            json.dump(results, f, indent=4)
+        if args.output_json:
+            with open(args.output_json, "w") as f:
+                json.dump(results, f, indent=4)
+        elif result_file_name:
+            with open(result_file_name, "a") as f:
+                json.dump(results, f, indent=4)
 
 
 if __name__ == "__main__":
@@ -291,6 +391,8 @@ if __name__ == "__main__":
                         type=int,
                         help="Output length for each request. Overrides the "
                         "output length from the dataset.")
+    parser.add_argument("--dataset",
+                        choices=['single_schema', 'xgrammar_bench'])
     parser.add_argument("--n",
                         type=int,
                         default=1,
@@ -320,6 +422,10 @@ if __name__ == "__main__":
                         action="store_true",
                         default=False,
                         help="Run warmup prompts before benchmark.")
+    parser.add_argument("--save-results",
+                        action="store_true",
+                        default=False,
+                        help="save output results.")
     args = parser.parse_args()
     if args.tokenizer is None:
         args.tokenizer = args.model
