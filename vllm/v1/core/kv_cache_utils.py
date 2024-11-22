bf465ecc394e99160d306269104519d8785fe02e
Ricky Xu <rickyx@anyscale.com>
2024-11-22 15:27:25 -0800

    [v1] Refactor KVCacheManager for more hash input than token ids (#10507)
    Signed-off-by: rickyx <rickyx@anyscale.com>
    Signed-off-by: Cody Yu <hao.yu.cody@gmail.com>
    Co-authored-by: Cody Yu <hao.yu.cody@gmail.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/v1/core/kv_cache_utils.py
+++ vllm/v1/core/kv_cache_utils.py
@@ -1,6 +1,6 @@
 """KV-Cache Utilities."""
-from dataclasses import dataclass, field
-from typing import List, Optional, Tuple, Union
+from dataclasses import dataclass
+from typing import List, Optional, Tuple
 
 from vllm.logger import init_logger
 
@@ -16,27 +16,34 @@ class KVCacheBlock:
     block_id: int
     # Reference count.
     ref_cnt: int = 0
-    # Token IDs in the block. When the block is full, the type of token_ids
-    # should be Tuple[int] for fast matching.
-    token_ids: Union[List[int], Tuple[int]] = field(default_factory=list)
     # The hash of the block composed of (block hash, tuple of token IDs).
     # It is only available when the block is full.
-    block_hash: Optional[BlockHashType] = None
-    # The number of hashed tokens. More hashed tokens means the block
-    # is closer to the end of a prompt and more likely to be evicted.
-    num_hashed_tokens: int = 0
+    _block_hash: Optional[BlockHashType] = None
 
     # Used to construct a doubly linked list for free blocks.
     # These two attributes should only be manipulated by FreeKVCacheBlockQueue.
     prev_free_block: Optional["KVCacheBlock"] = None
     next_free_block: Optional["KVCacheBlock"] = None
 
-    def reset(self):
-        """Reset the block metadata."""
-        self.ref_cnt = 0
-        self.token_ids = []
-        self.block_hash = None
-        self.num_hashed_tokens = 0
+    def incr_ref(self):
+        self.ref_cnt += 1
+
+    def decr_ref(self):
+        self.ref_cnt -= 1
+
+    @property
+    def block_hash(self) -> Optional[BlockHashType]:
+        return self._block_hash
+
+    @block_hash.setter
+    def block_hash(self, block_hash: BlockHashType):
+        assert self.block_hash is None, (
+            "The block already has a hash. This should not happen.")
+        self._block_hash = block_hash
+
+    def reset_hash(self):
+        """Reset the block hash when the block is evicted."""
+        self._block_hash = None
 
 
 class FreeKVCacheBlockQueue:
