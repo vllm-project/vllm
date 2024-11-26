70a631059b457c5570b7a59a0ca0109bbeb7d093
xiangw2 <xiangw2@chinatelecom.cn>
2024-11-26 09:48:33 +0800

    fix

--- vllm/model_executor/models/telechat2.py
+++ vllm/model_executor/models/telechat2.py
@@ -32,8 +32,8 @@ from vllm.model_executor.layers.quantization.base_config import (
 from vllm.model_executor.layers.sampler import Sampler, SamplerOutput
 from vllm.model_executor.layers.vocab_parallel_embedding import ParallelLMHead
 from vllm.model_executor.model_loader.weight_utils import default_weight_loader
-from vllm.model_executor.models.llama import (LlamaAttention, 
-                                              LlamaDecoderLayer, 
+from vllm.model_executor.models.llama import (LlamaAttention,
+                                              LlamaDecoderLayer,
                                               LlamaForCausalLM, LlamaMLP,
                                               LlamaModel)
 from vllm.model_executor.sampling_metadata import SamplingMetadata
