11358efc66bc9e3ac10d074d5c26206b447f79f6
Ricky Xu <rickyx@anyscale.com>
2024-11-27 23:59:28 -0800

    [Bug][CLI] Allow users to disable prefix caching explicitly (#10724)
    Signed-off-by: rickyx <rickyx@anyscale.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- tests/engine/test_arg_utils.py
+++ tests/engine/test_arg_utils.py
@@ -59,6 +59,25 @@ def test_compilation_config():
     assert args.compilation_config.level == 3
 
 
+def test_prefix_cache_default():
+    parser = EngineArgs.add_cli_args(FlexibleArgumentParser())
+    args = parser.parse_args([])
+
+    engine_args = EngineArgs.from_cli_args(args=args)
+    assert (not engine_args.enable_prefix_caching
+            ), "prefix caching defaults to off."
+
+    # with flag to turn it on.
+    args = parser.parse_args(["--enable-prefix-caching"])
+    engine_args = EngineArgs.from_cli_args(args=args)
+    assert engine_args.enable_prefix_caching
+
+    # with disable flag to turn it off.
+    args = parser.parse_args(["--no-enable-prefix-caching"])
+    engine_args = EngineArgs.from_cli_args(args=args)
+    assert not engine_args.enable_prefix_caching
+
+
 def test_valid_pooling_config():
     parser = EngineArgs.add_cli_args(FlexibleArgumentParser())
     args = parser.parse_args([
