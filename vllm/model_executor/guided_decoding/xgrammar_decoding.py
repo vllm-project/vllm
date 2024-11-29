5aa015ab2c4265196521051394ed5913824f7b98
Aaron Pham <contact@aarnphm.xyz>
2024-11-29 23:44:02 +0000

    hack: hmm it is a tuple
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- vllm/model_executor/guided_decoding/xgrammar_decoding.py
+++ vllm/model_executor/guided_decoding/xgrammar_decoding.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 
-import json
+import json, torch
 
+from transformers import PreTrainedTokenizerFast
 from dataclasses import dataclass
-from typing import TYPE_CHECKING, Dict, Any, Optional
+from typing import TYPE_CHECKING, Dict, Any, Optional, List
 
 try:
     import xgrammar as xgr
+    from xgrammar.base import _core as xgr_core
 except ImportError:
     pass
 
@@ -23,22 +25,62 @@ def get_local_xgrammar_guided_decoding_logits_processor(
         model_config: ModelConfig,
         max_threads=8):
     config = GrammarConfig.from_guided_params(
-        guided_params, model_config, max_threads)
-    return XGrammarLogitsProcessor(config, tokenizer)
+        guided_params=guided_params, model_config=model_config, tokenizer=tokenizer, max_threads=max_threads)
+    return XGrammarLogitsProcessor(config)
 
 @dataclass
 class GrammarConfig:
     """Serializable configuration for grammar compilation"""
-    json_str: Optional[str] = None
-    grammar_str: Optional[str] = None
     vocab_size: int = 0
     max_threads: int = 8
+    json_str: Optional[str] = None
+    grammar_str: Optional[str] = None
+    encoded_vocab: Optional[Dict[str, int]] = None
+    stop_token_ids: Optional[List[int]] = None
+    backend_str: str = ""
 
     @classmethod
     def from_guided_params(cls,
                           guided_params: GuidedDecodingParams,
                           model_config: ModelConfig,
+                          tokenizer: PreTrainedTokenizer,
                           max_threads: int = 8) -> GrammarConfig:
+
+        # Vendorred from xgrammar logics
+        try:
+            encoded_vocab = tokenizer.get_vocab()
+            encoded_vocab = [
+                token for token, _ in sorted(encoded_vocab.items(), key=lambda x: x[1])
+            ]
+        except AttributeError as e:
+            msg = (
+                f"Cannot get the vocabulary of the tokenizer {type(tokenizer)}. The tokenizer "
+                "should have a get_vocab method."
+            )
+            raise ValueError(msg) from e
+
+        stop_token_ids = None
+        backend_str = xgr.VocabType.RAW
+        if isinstance(tokenizer, PreTrainedTokenizerFast):
+            # huggingface fast tokenizer
+            # - the vocabulary is directly obtained from tokenizer.get_vocab()
+            #   (tokenizer.backend_tokenizer.to_str() may not contain the full vocab, special
+            #   tokens may be omitted)
+            # - the vocab size is obtained from len(tokenizer.get_vocab()) or provided by user
+            # - the vocab type and prepend_space_in_tokenization are obtained from
+            #   tokenizer.backend_tokenizer.to_str()
+            # - stop token id is provided by user, or auto detected.
+            backend_str = tokenizer.backend_tokenizer.to_str()
+            if stop_token_ids is None:
+                if hasattr(tokenizer, "eos_token_id") and tokenizer.eos_token_id is not None:
+                    stop_token_ids = [tokenizer.eos_token_id]
+                else:
+                    logger.warning(
+                        "When constructing TokenizerInfo from a huggingface tokenizer, "
+                        "stop_token_ids is neither provided by user nor found from the tokenizer. "
+                        "It will be automatically detected."
+                    )
+
         if guided_params.json:
             if not isinstance(guided_params.json, str):
                 json_str = json.dumps(guided_params.json)
@@ -47,22 +89,34 @@ class GrammarConfig:
             return cls(
                 json_str=json_str,
                 vocab_size=model_config.hf_config.vocab_size,
-                max_threads=max_threads
+                max_threads=max_threads,
+                encoded_vocab=encoded_vocab,
+                stop_token_ids=stop_token_ids,
+                backend_str=backend_str
             )
         elif guided_params.grammar:
             return cls(
                 grammar_str=guided_params.grammar,
                 vocab_size=model_config.hf_config.vocab_size,
-                max_threads=max_threads
+                max_threads=max_threads,
+                encoded_vocab=encoded_vocab,
+                stop_token_ids=stop_token_ids,
+                backend_str=backend_str
             )
         else:
             raise ValueError("Currently only support JSON and EBNF grammar mode for xgrammar")
 
+    def create_tokenizer_info(self):
+        return xgr.TokenizerInfo._create_from_handle(
+            xgr_core.TokenizerInfo.from_huggingface(
+                self.encoded_vocab, self.backend_str, self.vocab_size, self.stop_token_ids
+            )
+        )
+
 class XGrammarLogitsProcessor:
-    """Wrapper class that rebuilds CompiledGrammar in each worker process"""
-    def __init__(self, config: GrammarConfig, tokenizer: PreTrainedTokenizer):
+    """Wrapper class to support pickle protocol"""
+    def __init__(self, config: GrammarConfig):
         self.config = config
-        self.tokenizer = tokenizer
         self._processor = None
 
     def __getstate__(self) -> Dict[str, Any]: return {'config': self.config}
@@ -74,8 +128,7 @@ class XGrammarLogitsProcessor:
     def _ensure_processor(self):
         """Lazily initialize the processor in the worker process"""
         if self._processor is None:
-            tokenizer_info = xgr.TokenizerInfo.from_huggingface(
-                self.tokenizer, vocab_size=self.config.vocab_size)
+            tokenizer_info = self.config.create_tokenizer_info()
             compiler = xgr.GrammarCompiler(
                 tokenizer_info, max_threads=self.config.max_threads)
 
@@ -86,7 +139,7 @@ class XGrammarLogitsProcessor:
 
             self._processor = xgr.contrib.hf.LogitsProcessor(ctx)
 
-    def __call__(self, *args, **kwargs):
-        """Delegate to underlying processor after ensuring it's initialized"""
-        if self._processor is None: self._ensure_processor()
-        return self._processor(*args, **kwargs)
+    def __call__(self, input_ids: List[int], scores: torch.Tensor) -> torch.Tensor:
+      if self._processor is None: self._ensure_processor()
+      if not input_ids: return scores
+      return self._processor(input_ids, scores)
