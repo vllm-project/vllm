7d3631f983aa84cd98b23fa8883868acd1e2bc06
DarkLight1337 <tlleungac@connect.ust.hk>
2024-11-30 15:16:16 +0000

    Try adding back prefix
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -512,7 +512,9 @@ class LlamaForCausalLM(nn.Module, SupportsLoRA, SupportsPP):
         self.config = config
         self.lora_config = lora_config
 
-        self.model = self._init_model(vllm_config=vllm_config, prefix=prefix)
+        self.model = self._init_model(vllm_config=vllm_config,
+                                      prefix=maybe_prefix(prefix, "model"))
+    
         if get_pp_group().is_last_rank:
             self.unpadded_vocab_size = config.vocab_size
             if lora_config:
