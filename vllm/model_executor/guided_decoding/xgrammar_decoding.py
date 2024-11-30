dd329504198ae036015c3a34cb741adafd4d4358
Aaron Pham <contact@aarnphm.xyz>
2024-11-30 00:08:51 +0000

    fix: correct use apply_token_bitmask interface
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- vllm/model_executor/guided_decoding/xgrammar_decoding.py
+++ vllm/model_executor/guided_decoding/xgrammar_decoding.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import json, torch
 
 from transformers import PreTrainedTokenizerFast
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import TYPE_CHECKING, Dict, Any, Optional, List
 
 try:
@@ -24,9 +24,12 @@ def get_local_xgrammar_guided_decoding_logits_processor(
         tokenizer: PreTrainedTokenizer,
         model_config: ModelConfig,
         max_threads=8):
-    config = GrammarConfig.from_guided_params(
-        guided_params=guided_params, model_config=model_config, tokenizer=tokenizer, max_threads=max_threads)
+    config = GrammarConfig.from_guided_params(guided_params=guided_params,
+                                              model_config=model_config,
+                                              tokenizer=tokenizer,
+                                              max_threads=max_threads)
     return XGrammarLogitsProcessor(config)
+
 
 @dataclass
 class GrammarConfig:
@@ -41,22 +44,22 @@ class GrammarConfig:
 
     @classmethod
     def from_guided_params(cls,
-                          guided_params: GuidedDecodingParams,
-                          model_config: ModelConfig,
-                          tokenizer: PreTrainedTokenizer,
-                          max_threads: int = 8) -> GrammarConfig:
+                           guided_params: GuidedDecodingParams,
+                           model_config: ModelConfig,
+                           tokenizer: PreTrainedTokenizer,
+                           max_threads: int = 8) -> GrammarConfig:
 
         # Vendorred from xgrammar logics
         try:
             encoded_vocab = tokenizer.get_vocab()
             encoded_vocab = [
-                token for token, _ in sorted(encoded_vocab.items(), key=lambda x: x[1])
+                token for token, _ in sorted(encoded_vocab.items(),
+                                             key=lambda x: x[1])
             ]
         except AttributeError as e:
             msg = (
                 f"Cannot get the vocabulary of the tokenizer {type(tokenizer)}. The tokenizer "
-                "should have a get_vocab method."
-            )
+                "should have a get_vocab method.")
             raise ValueError(msg) from e
 
         stop_token_ids = None
@@ -72,74 +75,97 @@ class GrammarConfig:
             # - stop token id is provided by user, or auto detected.
             backend_str = tokenizer.backend_tokenizer.to_str()
             if stop_token_ids is None:
-                if hasattr(tokenizer, "eos_token_id") and tokenizer.eos_token_id is not None:
+                if hasattr(
+                        tokenizer,
+                        "eos_token_id") and tokenizer.eos_token_id is not None:
                     stop_token_ids = [tokenizer.eos_token_id]
                 else:
                     logger.warning(
                         "When constructing TokenizerInfo from a huggingface tokenizer, "
                         "stop_token_ids is neither provided by user nor found from the tokenizer. "
-                        "It will be automatically detected."
-                    )
+                        "It will be automatically detected.")
 
         if guided_params.json:
             if not isinstance(guided_params.json, str):
                 json_str = json.dumps(guided_params.json)
             else:
                 json_str = guided_params.json
-            return cls(
-                json_str=json_str,
-                vocab_size=model_config.hf_config.vocab_size,
-                max_threads=max_threads,
-                encoded_vocab=encoded_vocab,
-                stop_token_ids=stop_token_ids,
-                backend_str=backend_str
-            )
+            return cls(json_str=json_str,
+                       vocab_size=model_config.hf_config.vocab_size,
+                       max_threads=max_threads,
+                       encoded_vocab=encoded_vocab,
+                       stop_token_ids=stop_token_ids,
+                       backend_str=backend_str)
         elif guided_params.grammar:
-            return cls(
-                grammar_str=guided_params.grammar,
-                vocab_size=model_config.hf_config.vocab_size,
-                max_threads=max_threads,
-                encoded_vocab=encoded_vocab,
-                stop_token_ids=stop_token_ids,
-                backend_str=backend_str
-            )
+            return cls(grammar_str=guided_params.grammar,
+                       vocab_size=model_config.hf_config.vocab_size,
+                       max_threads=max_threads,
+                       encoded_vocab=encoded_vocab,
+                       stop_token_ids=stop_token_ids,
+                       backend_str=backend_str)
         else:
-            raise ValueError("Currently only support JSON and EBNF grammar mode for xgrammar")
+            raise ValueError(
+                "Currently only support JSON and EBNF grammar mode for xgrammar"
+            )
 
     def create_tokenizer_info(self):
         return xgr.TokenizerInfo._create_from_handle(
-            xgr_core.TokenizerInfo.from_huggingface(
-                self.encoded_vocab, self.backend_str, self.vocab_size, self.stop_token_ids
-            )
-        )
+            xgr_core.TokenizerInfo.from_huggingface(self.encoded_vocab,
+                                                    self.backend_str,
+                                                    self.vocab_size,
+                                                    self.stop_token_ids))
 
+
+@dataclass
 class XGrammarLogitsProcessor:
     """Wrapper class to support pickle protocol"""
-    def __init__(self, config: GrammarConfig):
-        self.config = config
-        self._processor = None
+    config: GrammarConfig
 
-    def __getstate__(self) -> Dict[str, Any]: return {'config': self.config}
+    ctx: Optional[xgr.CompiledGrammar] = None
+    matchers: List[xgr.GrammarMatcher] = field(default_factory=list)
+    batch_size: int = 1
+    token_bitmask: Optional[torch.Tensor]  = None
+
+    def __getstate__(self) -> Dict[str, Any]:
+        return {'config': self.config}
 
     def __setstate__(self, state: Dict[str, Any]):
         self.config = state['config']
-        self._processor = None
 
-    def _ensure_processor(self):
+        self.ctx = None
+        self.matchers = []
+        self.batch_size = 1
+        self.token_bitmask = None
+
+    def _ensure_ctx(self):
         """Lazily initialize the processor in the worker process"""
-        if self._processor is None:
-            tokenizer_info = self.config.create_tokenizer_info()
-            compiler = xgr.GrammarCompiler(
-                tokenizer_info, max_threads=self.config.max_threads)
+        if self.ctx is None:
+            compiler = xgr.GrammarCompiler(self.config.create_tokenizer_info(),
+                                           max_threads=self.config.max_threads)
 
             if self.config.json_str is not None:
-                ctx = compiler.compile_json_schema(self.config.json_str)
+                self.ctx = compiler.compile_json_schema(self.config.json_str)
             else:
-                ctx = compiler.compile_grammar(self.config.grammar_str)
+                self.ctx = compiler.compile_grammar(self.config.grammar_str)
 
-            self._processor = xgr.contrib.hf.LogitsProcessor(ctx)
+    def __call__(self, input_ids: List[int],
+                 scores: torch.Tensor) -> torch.Tensor:
+        if self.ctx is None: self._ensure_ctx()
+        if len(self.matchers) == 0:
+            self.matchers = [
+                xgr.GrammarMatcher(self.ctx) for _ in range(self.batch_size)
+            ]
+            self.token_bitmask = xgr.allocate_token_bitmask(
+                self.batch_size, self.config.vocab_size)
 
-    def __call__(self, input_ids: List[int], scores: torch.Tensor) -> torch.Tensor:
-      if self._processor is None: self._ensure_processor()
-      if not input_ids: return scores
-      return self._processor(input_ids, scores)
+        for i, matcher in enumerate(self.matchers):
+            if not matcher.is_terminated():
+                matcher.fill_next_token_bitmask(self.token_bitmask, i)
+
+        device_type = scores.device.type
+        if device_type != "cuda": scores = scores.to("cpu")
+        xgr.apply_token_bitmask_inplace(scores,
+                                        self.token_bitmask.to(scores.device))
+        if device_type != "cuda": scores = scores.to(device_type)
+
+        return scores
