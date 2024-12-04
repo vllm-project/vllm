31514a03cf267398daee37f947aeca2c384290a4
Shangming Cai <caishangming@linux.alibaba.com>
2024-12-04 15:00:04 +0800

    Add solution for ports conflict on the same node.
    Signed-off-by: Shangming Cai <caishangming@linux.alibaba.com>

--- vllm/distributed/kv_transfer/kv_pipe/mooncake_pipe.py
+++ vllm/distributed/kv_transfer/kv_pipe/mooncake_pipe.py
@@ -74,6 +74,12 @@ class MooncakeTransferEngine:
             raise
         prefill_host, base_prefill_port = self.config.prefill_url.split(':')
         decode_host, base_decode_port = self.config.decode_url.split(':')
+
+        # Avoid ports conflict when running prefill and decode on the same node
+        if prefill_host == decode_host and \
+                base_prefill_port == base_decode_port:
+            base_decode_port = int(base_decode_port) + 100
+
         prefill_port = int(base_prefill_port) + self.local_rank
         decode_port = int(base_decode_port) + self.local_rank
         self.prefill_url = ':'.join([prefill_host, str(prefill_port)])
