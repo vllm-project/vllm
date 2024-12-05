da0c5f43eee889d61d702b00904ab8a7a389b271
youkaichao <youkaichao@gmail.com>
2024-12-04 21:33:40 -0800

    add tests
    Signed-off-by: youkaichao <youkaichao@gmail.com>

---
+++ tests/standalone_tests/python_only_compile.sh
@@ -0,0 +1,24 @@
+# This script tests if the python only compilation works correctly
+# for users who do not have compilers installed on their system
+
+# uninstall vllm
+pip3 uninstall -y vllm
+# restore the original files
+mv test_docs/vllm ./vllm
+
+# remove all compilers
+unlink $(which gcc)
+unlink $(which g++)
+
+echo 'import os; os.system("touch /tmp/changed.file")' >> vllm/__init__.py
+
+VLLM_USE_PRECOMPILED=1 pip3 install -vvv -e .
+
+# Run the script
+python3 -c 'import vllm'
+
+# Check if the clangd log file was created
+if [ ! -f /tmp/changed.file ]; then
+    echo "changed.file was not created, python only compilation failed"
+    exit 1
+fi
