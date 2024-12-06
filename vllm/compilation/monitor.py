bde6bb3f1d2b0d95dc3b995a803e890014c5c39a
youkaichao <youkaichao@gmail.com>
2024-12-06 11:34:33 -0800

    add depyf
    Signed-off-by: youkaichao <youkaichao@gmail.com>

--- vllm/compilation/monitor.py
+++ vllm/compilation/monitor.py
@@ -1,11 +1,23 @@
+import os
+
 from vllm.config import CompilationConfig, CompilationLevel, VllmConfig
 from vllm.logger import init_logger
 
 logger = init_logger(__name__)
 
+context_manager = None
+
 
 def start_monitoring_torch_compile(vllm_config: VllmConfig):
-    pass
+    compilation_config: CompilationConfig = vllm_config.compilation_config
+    if compilation_config.level == CompilationLevel.PIECEWISE and \
+        compilation_config.debug_dump_path:
+        import depyf
+        path = os.path.join(compilation_config.debug_dump_path,
+                            f"rank_{vllm_config.parallel_config.rank}")
+        global context_manager
+        context_manager = depyf.prepare_debug(path)
+        context_manager.__enter__()
 
 
 def end_monitoring_torch_compile(vllm_config: VllmConfig):
@@ -13,3 +25,7 @@ def end_monitoring_torch_compile(vllm_config: VllmConfig):
     if compilation_config.level == CompilationLevel.PIECEWISE:
         logger.info("graph compilation takes %.2f s in total",
                     compilation_config.compilation_time)
+        global context_manager
+        if context_manager is not None:
+            context_manager.__exit__(None, None, None)
+            context_manager = None
