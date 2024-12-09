83ea81cd247669a8f7449f3b6b07b9c7ce7025aa
Wallas Santos <wallashss@ibm.com>
2024-12-09 14:29:59 -0300

    [Bugfix] Fix guided decoding with tokenizer mode mistral
    Signed-off-by: Wallas Santos <wallashss@ibm.com>

--- vllm/engine/llm_engine.py
+++ vllm/engine/llm_engine.py
@@ -2008,9 +2008,7 @@ class LLMEngine:
                 self.decoding_config.guided_decoding_backend
 
             processor = get_local_guided_decoding_logits_processor(
-                guided_params=guided_decoding,
-                tokenizer=tokenizer,
-                model_config=self.model_config)
+                guided_params=guided_decoding, tokenizer=tokenizer)
             if processor:
                 logits_processors.append(processor)
 
