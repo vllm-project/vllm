c269144d2bd88fa33693cb0355387f9e0b0f44be
Jee Jee Li <pandaleefree@gmail.com>
2024-12-10 04:54:44 +0800

    [Misc][LoRA] Abstract PunicaWrapper (#10955)
    Signed-off-by: Jee Jee Li <pandaleefree@gmail.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

---
+++ vllm/lora/punica_wrapper/__init__.py
@@ -0,0 +1,7 @@
+from vllm.lora.punica_wrapper.punica_base import PunicaWrapperBase
+from vllm.lora.punica_wrapper.punica_selector import get_punica_wrapper
+
+__all__ = [
+    "PunicaWrapperBase",
+    "get_punica_wrapper",
+]
