980ad394a83a6f12c576a035922db3c2e743beff
Joe Runde <Joseph.Runde@ibm.com>
2024-12-09 22:46:29 -0700

    [Frontend] Use request id from header (#10968)
    Signed-off-by: Joe Runde <Joseph.Runde@ibm.com>

--- vllm/entrypoints/openai/serving_score.py
+++ vllm/entrypoints/openai/serving_score.py
@@ -15,7 +15,7 @@ from vllm.inputs.data import TokensPrompt
 from vllm.logger import init_logger
 from vllm.outputs import PoolingRequestOutput
 from vllm.transformers_utils.tokenizers.mistral import MistralTokenizer
-from vllm.utils import make_async, merge_async_iterators, random_uuid
+from vllm.utils import make_async, merge_async_iterators
 
 logger = init_logger(__name__)
 
@@ -102,7 +102,7 @@ class OpenAIServingScores(OpenAIServing):
             return error_check_ret
 
         model_name = request.model
-        request_id = f"score-{random_uuid()}"
+        request_id = f"score-{self._base_request_id(raw_request)}"
         created_time = int(time.monotonic())
         truncate_prompt_tokens = request.truncate_prompt_tokens
 
