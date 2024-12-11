4e6b081f28ac21bb1922b0bad099abfde99e4ef5
Tyler Michael Smith <tyler@neuralmagic.com>
2024-12-10 19:16:22 -0500

    [Bugfix] Fix Mamba multistep (#11071)
    Signed-off-by: Tyler Michael Smith <tyler@neuralmagic.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/worker/multi_step_model_runner.py
+++ vllm/worker/multi_step_model_runner.py
@@ -29,7 +29,9 @@ if TYPE_CHECKING:
 
 logger = init_logger(__name__)
 
-MULTI_STEP_ATTENTION_BACKENDS = ["FLASH_ATTN", "ROCM_FLASH", "FLASHINFER"]
+MULTI_STEP_ATTENTION_BACKENDS = [
+    "FLASH_ATTN", "ROCM_FLASH", "FLASHINFER", "NO_ATTENTION"
+]
 MULTI_STEP_CHUNKED_PREFILL_ATTENTION_BACKENDS = ["FLASH_ATTN"]
 
 def _get_supported_attention_backends(chunked_prefill_enabled: bool) \
