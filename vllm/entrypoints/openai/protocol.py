3b34edb46632fe38df2547f2d66de0b1a9f941e4
Brad Hilton <brad.hilton.nw@gmail.com>
2024-12-12 15:31:10 -0700

    fix: Handle missing args/kwargs
    Signed-off-by: Brad Hilton <brad.hilton.nw@gmail.com>

--- vllm/entrypoints/openai/protocol.py
+++ vllm/entrypoints/openai/protocol.py
@@ -343,11 +343,10 @@ class ChatCompletionRequest(OpenAIBaseModel):
             prompt_logprobs = self.top_logprobs
 
         if self.logits_processors:
-            logits_processors = [
-                (resolve_obj_by_qualname(p) if isinstance(p, str) else
-                 resolve_obj_by_qualname(p.qualname)(*p.args, **p.kwargs))
-                for p in self.logits_processors
-            ]
+            logits_processors = [(resolve_obj_by_qualname(p) if isinstance(
+                p, str) else resolve_obj_by_qualname(p.qualname)(
+                    *p.args or [], **p.kwargs or {}))
+                                 for p in self.logits_processors]
         else:
             logits_processors = None
 
@@ -671,11 +670,10 @@ class CompletionRequest(OpenAIBaseModel):
         echo_without_generation = self.echo and self.max_tokens == 0
 
         if self.logits_processors:
-            logits_processors = [
-                (resolve_obj_by_qualname(p) if isinstance(p, str) else
-                 resolve_obj_by_qualname(p.qualname)(*p.args, **p.kwargs))
-                for p in self.logits_processors
-            ]
+            logits_processors = [(resolve_obj_by_qualname(p) if isinstance(
+                p, str) else resolve_obj_by_qualname(p.qualname)(
+                    *p.args or [], **p.kwargs or {}))
+                                 for p in self.logits_processors]
         else:
             logits_processors = None
 
