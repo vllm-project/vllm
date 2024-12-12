47c3557f0c467464ce86a30a29ba1666090afb9c
Dahai Tang <tangdahai.tdh@alibaba-inc.com>
2024-12-12 04:27:22 +0000

    Fix: lint checker
    Signed-off-by: Dahai Tang <tangdahai.tdh@alibaba-inc.com>

--- vllm/worker/openvino_model_runner.py
+++ vllm/worker/openvino_model_runner.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from typing import Dict, List, NamedTuple, Optional, Tuple
+from typing import Any, Dict, List, NamedTuple, Optional, Tuple
 
 import openvino as ov
 import torch
