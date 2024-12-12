ec684636c5930c47cea808633a8dbd14eb181edc
rickyx <rickyx@anyscale.com>
2024-12-12 23:55:06 +0000

    updates
    Signed-off-by: rickyx <rickyx@anyscale.com>

--- tests/v1/test_stats.py
+++ tests/v1/test_stats.py
@@ -1,10 +1,39 @@
-from typing import Optional
-
+from typing import Any
 import pytest
-
 from vllm.sampling_params import SamplingParams
-from vllm.v1.engine import EngineCoreRequest
 from vllm.v1.stats.common import RequestStats, RequestStatsUpdate
+
+
+def test_invalid_request_update():
+    request_id = "test_request"
+    update_specific_required_fields = {
+        RequestStatsUpdate.Type.INPUT_PROCESSED: [
+            "sampling_params",
+            "num_prompt_tokens",
+        ],
+        RequestStatsUpdate.Type.PREFILLING: [
+            "num_computed_tokens",
+            "num_cached_tokens",
+        ],
+        RequestStatsUpdate.Type.DETOKENIZED: ["num_new_tokens"],
+        RequestStatsUpdate.Type.FINISHED: ["finish_reason"],
+    }
+
+    # Missing a required field should raise an assertion error.
+    for update_type in RequestStatsUpdate.Type:
+        required_fields = update_specific_required_fields.get(update_type, [])
+
+        # Try to miss one of the required fields.
+        kwargs = {field: object() for field in required_fields}
+        for field in required_fields:
+            copy_kwargs = kwargs.copy()
+            copy_kwargs.pop(field)
+            with pytest.raises(ValueError):
+                RequestStatsUpdate(
+                    request_id=request_id,
+                    type=update_type,
+                    **copy_kwargs,
+                )
 
 
 def test_lifecycle_updates():
@@ -15,15 +44,16 @@ def test_lifecycle_updates():
     arrived_ts = 0
     input_processed_ts = 1
     queued_ts = 2
-    running_ts = 3
-    running_2_ts = 4
+    prefilling_ts = 3
     decoded_ts = 5
     detokenized_ts = 6
     decoded_2_ts = 7
-    preempted_ts = 8
-    resumed_ts = 9
-    decoded_3_ts = 10
-    finished_ts = 11
+    detokenized_2_ts = 8
+    preempted_ts = 9
+    resumed_ts = 10
+    decoded_3_ts = 11
+    detokenized_3_ts = 12
+    finished_ts = 13
 
     # Test ARRIVED
     arrived_update = RequestStatsUpdate(
@@ -37,26 +67,15 @@ def test_lifecycle_updates():
 
     # Test INPUT_PROCESSED
     sampling_params = SamplingParams(n=1)
-    engine_request = EngineCoreRequest(
-        prompt_token_ids=[1, 2, 3, 4, 5, 6],
-        sampling_params=sampling_params,
-        request_id=request_id,
-        prompt="test_prompt",
-        mm_inputs=None,
-        mm_placeholders=None,
-        eos_token_id=None,
-        arrival_time=arrived_ts,
-        lora_request=None,
-    )
     input_processed_update = RequestStatsUpdate(
         request_id=request_id,
         type=RequestStatsUpdate.Type.INPUT_PROCESSED,
         monotonic_ts_s=input_processed_ts,
-        engine_request=engine_request,
+        sampling_params=sampling_params,
+        num_prompt_tokens=6,
     )
     stats.update_from(input_processed_update)
     assert stats.input_processor_end_ts_s == input_processed_ts
-    assert stats.engine_request == engine_request
     assert stats.last_updated_ts_s == input_processed_ts
     assert stats.num_prompt_tokens == 6
     assert stats.sampling_params == sampling_params
@@ -74,78 +93,61 @@ def test_lifecycle_updates():
     assert stats.queued_ts_s == queued_ts
     assert stats.last_updated_ts_s == queued_ts
 
-    # Test RUNNING
-    running_update = RequestStatsUpdate(
+    # Test PREFILLING
+    prefilling_update = RequestStatsUpdate(
         request_id=request_id,
-        type=RequestStatsUpdate.Type.RUNNING,
-        monotonic_ts_s=running_ts,
-        new_prefill=True,
+        type=RequestStatsUpdate.Type.PREFILLING,
+        monotonic_ts_s=prefilling_ts,
         num_computed_tokens=3,
         num_cached_tokens=1,
     )
-    stats.update_from(running_update)
-    assert stats.prefill_ts_s == running_ts
+    stats.update_from(prefilling_update)
+    assert stats.prefill_ts_s == prefilling_ts
     assert stats.num_computed_tokens == 3
     assert stats.num_cached_tokens == 1
-    assert stats.queue_duration_s == running_ts - queued_ts
+    assert stats.queue_duration_s == prefilling_ts - queued_ts
 
-    # Test RUNNING again shouldn't update prefill_ts_s
-    running_update = RequestStatsUpdate(
-        request_id=request_id,
-        type=RequestStatsUpdate.Type.RUNNING,
-        monotonic_ts_s=running_2_ts,
-        new_prefill=False,
-        num_computed_tokens=6,
-        num_cached_tokens=0,
-    )
-    stats.update_from(running_update)
-    assert stats.prefill_ts_s == running_ts
-    assert stats.num_computed_tokens == 6
-    # num_cached_tokens is not updated
-    assert stats.num_cached_tokens == 1
-    assert stats.last_updated_ts_s == running_2_ts
-    # prefill_start_ts_s_lst should only contain the first running/resumed
-    # running prefill update.
-    assert stats.prefill_start_ts_s_lst == [
-        running_ts,
-    ]
-
-    # Test DECODED
+    # Test DECODING
     decoded_update = RequestStatsUpdate(
         request_id=request_id,
-        type=RequestStatsUpdate.Type.DECODED,
+        type=RequestStatsUpdate.Type.DECODING,
         monotonic_ts_s=decoded_ts,
-        num_new_tokens=1,
-        token_perf_ts_ns=decoded_ts * 1e9,
     )
     stats.update_from(decoded_update)
     assert stats.last_updated_ts_s == decoded_ts
-    # Since arrival
-    assert stats.first_token_latency_s == decoded_ts - arrived_ts
-    assert stats.num_output_tokens == 1
-    # Since first scheduled
-    assert stats.prefill_latency_s == 2
 
     # Test DETOKENIZED
     detokenized_update = RequestStatsUpdate(
         request_id=request_id,
         type=RequestStatsUpdate.Type.DETOKENIZED,
         monotonic_ts_s=detokenized_ts,
+        num_new_tokens=1,
     )
     stats.update_from(detokenized_update)
     assert stats.last_updated_ts_s == detokenized_ts
+    assert stats.num_output_tokens == 1
+    # Since arrival
+    assert stats.first_token_latency_s == detokenized_ts - arrived_ts
+    # Since first scheduled
+    assert stats.prefill_latency_s == detokenized_ts - prefilling_ts
 
-    # Test another DECODE should yield correct inter token latency
+    # Test another DECODING and DETOKENIZED should yield correct inter token latency
     decoded_update = RequestStatsUpdate(
         request_id=request_id,
-        type=RequestStatsUpdate.Type.DECODED,
+        type=RequestStatsUpdate.Type.DECODING,
         monotonic_ts_s=decoded_2_ts,
-        num_new_tokens=1,
-        token_perf_ts_ns=decoded_2_ts * 1e9,
     )
     stats.update_from(decoded_update)
+
+    detokenized_update = RequestStatsUpdate(
+        request_id=request_id,
+        type=RequestStatsUpdate.Type.DETOKENIZED,
+        monotonic_ts_s=detokenized_2_ts,
+        num_new_tokens=1,
+    )
+    stats.update_from(detokenized_update)
     assert stats.output_token_latency_s_lst == [
-        decoded_2_ts - decoded_ts,
+        detokenized_2_ts - detokenized_ts,
     ]
     assert stats.num_output_tokens == 2
 
@@ -161,121 +163,73 @@ def test_lifecycle_updates():
     # States should be reset
     assert stats.num_computed_tokens == 0
     assert stats.num_cached_tokens == 0
-    assert stats.num_output_tokens == 0
-    assert stats.output_token_latency_s_lst == []
+    # These states should not be reset
+    assert stats.num_output_tokens == 2
+    assert stats.output_token_latency_s_lst == [
+        detokenized_2_ts - detokenized_ts,
+    ]
+    assert stats.prefill_latency_s == prefilling_ts - arrived_ts
+    assert stats.num_prompt_tokens == 6
+    assert stats.prefill_start_ts_s_lst == [prefilling_ts]
 
     # Test resumed
     resumed_update = RequestStatsUpdate(
         request_id=request_id,
-        type=RequestStatsUpdate.Type.RUNNING,
+        type=RequestStatsUpdate.Type.PREFILLING,
         monotonic_ts_s=resumed_ts,
-        new_prefill=True,
         num_computed_tokens=6,
         num_cached_tokens=2,
     )
     stats.update_from(resumed_update)
-    # Resumed prefill timestamp should be updated
-    assert stats.prefill_ts_s == resumed_ts
+    # prefill timestamp should not be updated since it's a resumed prefill
+    assert stats.prefill_ts_s == prefilling_ts
     assert stats.num_computed_tokens == 6
     assert stats.num_cached_tokens == 2
     assert stats.prefill_start_ts_s_lst == [
-        running_ts,
+        prefilling_ts,
         resumed_ts,
     ]
     assert stats.last_updated_ts_s == resumed_ts
 
-    # Test another DECODED should yield correct first token latency.
+    # Test another DECODED/DETOKENIZED should yield correct first token latency.
     decoded_update = RequestStatsUpdate(
         request_id=request_id,
-        type=RequestStatsUpdate.Type.DECODED,
+        type=RequestStatsUpdate.Type.DECODING,
         monotonic_ts_s=decoded_3_ts,
+    )
+    detokenized_update = RequestStatsUpdate(
+        request_id=request_id,
+        type=RequestStatsUpdate.Type.DETOKENIZED,
+        monotonic_ts_s=detokenized_3_ts,
         num_new_tokens=1,
-        token_perf_ts_ns=decoded_3_ts * 1e9,
     )
     stats.update_from(decoded_update)
-    assert stats.first_token_ts_s == decoded_3_ts
-    assert stats.num_output_tokens == 1
+    stats.update_from(detokenized_update)
+    assert stats.first_token_ts_s == detokenized_ts - arrived_ts
+    assert stats.num_output_tokens == 3
+    assert stats.output_token_latency_s_lst == [
+        detokenized_2_ts - detokenized_ts,
+        detokenized_3_ts - detokenized_2_ts,
+    ]
 
     # Test FINISHED
     finished_update = RequestStatsUpdate(
         request_id=request_id,
-        type=RequestStatsUpdate.Type.DETOKENIZED,
+        type=RequestStatsUpdate.Type.FINISHED,
         monotonic_ts_s=finished_ts,
         finish_reason="test_reason",
     )
     stats.update_from(finished_update)
     assert stats.last_updated_ts_s == finished_ts
     assert stats.e2e_latency_s == finished_ts - arrived_ts
-    assert stats.inference_latency_s == finished_ts - resumed_ts
-    assert stats.decode_latency_s == finished_ts - decoded_3_ts
+    assert stats.inference_latency_s == finished_ts - prefilling_ts
+    assert stats.prefill_latency_s == detokenized_ts - prefilling_ts
+    assert stats.decode_latency_s == finished_ts - detokenized_ts
+    assert stats.first_token_latency_s == detokenized_ts - arrived_ts
+    assert stats.queue_duration_s == prefilling_ts - queued_ts
     assert stats.is_finished
     assert stats.finish_reason == "test_reason"
 
-
-@pytest.mark.parametrize("finish_reason",
-                         ["test-decode", "test-detokenize", None])
-def test_finish_reason(finish_reason: Optional[str]):
-    """
-    Test that a request could be finished when decoded and detokenized
-    at different times.
-    """
-    request_id = "test_request"
-    r = RequestStats(request_id=request_id)
-
-    # Test FINISHED
-    updates = [
-        RequestStatsUpdate(
-            request_id=request_id,
-            type=RequestStatsUpdate.Type.ARRIVED,
-            monotonic_ts_s=0,
-        ),
-        RequestStatsUpdate(
-            request_id=request_id,
-            type=RequestStatsUpdate.Type.INPUT_PROCESSED,
-            monotonic_ts_s=1,
-        ),
-        RequestStatsUpdate(
-            request_id=request_id,
-            type=RequestStatsUpdate.Type.QUEUED,
-            monotonic_ts_s=2,
-        ),
-        RequestStatsUpdate(
-            request_id=request_id,
-            type=RequestStatsUpdate.Type.RUNNING,
-            new_prefill=True,
-            monotonic_ts_s=3,
-            num_computed_tokens=3,
-        ),
-    ]
-
-    if finish_reason is not None:
-        if finish_reason == "test-decode":
-            updates.append(
-                RequestStatsUpdate(
-                    request_id=request_id,
-                    type=RequestStatsUpdate.Type.DECODED,
-                    monotonic_ts_s=4,
-                    finish_reason=finish_reason,
-                    token_perf_ts_ns=4 * 1e9,
-                    num_new_tokens=1,
-                ))
-        elif finish_reason == "test-detokenize":
-            updates.append(
-                RequestStatsUpdate(
-                    request_id=request_id,
-                    type=RequestStatsUpdate.Type.DETOKENIZED,
-                    monotonic_ts_s=4,
-                    finish_reason=finish_reason,
-                ))
-
-    for update in updates:
-        r.update_from(update)
-
-    if finish_reason is not None:
-        assert r.finish_reason == finish_reason
-        assert r.is_finished
-        assert r.e2e_latency_s == 4
-    else:
-        assert r.finish_reason is None
-        assert not r.is_finished
-        assert r.e2e_latency_s is None
+    # TODO(rickyx): Add model forward/execute time.
+    assert stats.model_forward_duration_s == 0.0
+    assert stats.model_execute_duration_s == 0.0
