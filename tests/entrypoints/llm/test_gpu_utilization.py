065eb4f5994675d4409f79c390cacd0b347f6023
youkaichao <youkaichao@gmail.com>
2024-12-13 19:50:47 -0800

    fix format
    Signed-off-by: youkaichao <youkaichao@gmail.com>

--- tests/entrypoints/llm/test_gpu_utilization.py
+++ tests/entrypoints/llm/test_gpu_utilization.py
@@ -1,5 +1,6 @@
 from vllm import LLM, SamplingParams
 
+
 def test_gpu_memory_utilization():
     prompts = [
         "Hello, my name is",
@@ -11,7 +12,11 @@ def test_gpu_memory_utilization():
 
     # makes sure gpu_memory_utilization is per-instance limit,
     # not a global limit
-    llms = [LLM(model="facebook/opt-125m", gpu_memory_utilization=0.3, enforce_eager=True) for i in range(3)]
+    llms = [
+        LLM(model="facebook/opt-125m",
+            gpu_memory_utilization=0.3,
+            enforce_eager=True) for i in range(3)
+    ]
     for llm in llms:
         outputs = llm.generate(prompts, sampling_params)
         for output in outputs:
