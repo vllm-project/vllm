623046d2af6d049d9b98cc35c48bc8242c803114
Erez Schwartz <erezs@ai21.com>
2024-12-15 18:29:50 +0200

    removed commented code, formatting

--- vllm/model_executor/models/jamba.py
+++ vllm/model_executor/models/jamba.py
@@ -25,8 +25,6 @@ from vllm.model_executor.model_loader.weight_utils import default_weight_loader
 from vllm.model_executor.models.mamba_cache import (MambaCacheManager,
                                                     MambaCacheParams)
 from vllm.model_executor.sampling_metadata import SamplingMetadata
-# from vllm.worker.model_runner import (_BATCH_SIZES_TO_CAPTURE,
-#                                       _get_graph_batch_size)
 from vllm.sequence import IntermediateTensors
 from vllm.utils import LayerBlockType
 
