bb995cd33076b78f6df5daaea3b77779dda79718
youkaichao <youkaichao@gmail.com>
2024-12-15 15:55:30 -0800

    modify tests
    Signed-off-by: youkaichao <youkaichao@gmail.com>

--- examples/offline_inference.py
+++ examples/offline_inference.py
@@ -3,12 +3,12 @@ import math
 
 llm = LLM(model="meta-llama/Meta-Llama-3-8B", enforce_eager=True)
 
-bs = 4
-# in the beginning, we have bs number of sequences, in total about 512 tokens for the prefill
-prompt_token_ids = [[0] * math.floor(512 / bs)] * bs
+for bs in [1, 2, 4, ] + list(range(8, 264, 8)):
+    # in the beginning, we have bs number of sequences, in total about 512 tokens for the prefill
+    prompt_token_ids = [[0] * math.floor(512 / bs)] * bs
 
-# all sequence generates 30 tokens
-sampling_params = SamplingParams(temperature=0, max_tokens=2, ignore_eos=True)
+    # all sequence generates 4 tokens
+    sampling_params = SamplingParams(temperature=0, max_tokens=4, ignore_eos=True)
 
-outputs = llm.generate(prompt_token_ids=prompt_token_ids, sampling_params=sampling_params)
-outputs = llm.generate(prompt_token_ids=prompt_token_ids, sampling_params=sampling_params)
+    outputs = llm.generate(prompt_token_ids=prompt_token_ids, sampling_params=sampling_params, use_tqdm=False)
+    outputs = llm.generate(prompt_token_ids=prompt_token_ids, sampling_params=sampling_params, use_tqdm=False)
