43aae7025b24cef82adf8c758e3204c7e453836d
Jee Jee Li <pandaleefree@gmail.com>
2024-12-16 15:38:37 +0000

    Delete unused code
    Signed-off-by: Jee Jee Li <pandaleefree@gmail.com>

--- vllm/lora/punica_wrapper/punica_gpu.py
+++ vllm/lora/punica_wrapper/punica_gpu.py
@@ -15,7 +15,6 @@ if HAS_TRITON:
     from vllm.lora.ops.bgmv_expand import bgmv_expand
     from vllm.lora.ops.bgmv_expand_slice import bgmv_expand_slice
     from vllm.lora.ops.bgmv_shrink import bgmv_shrink
-    # from vllm.lora.ops.sgmv_expand import sgmv_expand
     from vllm.lora.ops.sgmv_expand import sgmv_expand
     from vllm.lora.ops.sgmv_shrink import sgmv_shrink
 
