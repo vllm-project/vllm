d6bf50eeadbaafe2bd539d7eecd4cc8664e7c3cb
Michael Goin <michael@neuralmagic.com>
2024-12-16 22:39:58 -0500

    [CI] Add test case with JSON schema using references + use xgrammar by default with OpenAI parse (#10935)
    Signed-off-by: mgoin <michael@neuralmagic.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- tests/entrypoints/llm/test_guided_generate.py
+++ tests/entrypoints/llm/test_guided_generate.py
@@ -105,6 +105,34 @@ def test_guided_complex_json_completion(sample_complex_json_schema, llm):
 
 
 @pytest.mark.skip_global_cleanup
+def test_guided_definition_json_completion(sample_definition_json_schema, llm):
+    sampling_params = SamplingParams(temperature=1.0,
+                                     max_tokens=1000,
+                                     guided_decoding=GuidedDecodingParams(
+                                         json=sample_definition_json_schema))
+    outputs = llm.generate(prompts=[
+        f"Give an example JSON for solving 8x + 7 = -23 "
+        f"that fits this schema: {sample_definition_json_schema}"
+    ] * 2,
+                           sampling_params=sampling_params,
+                           use_tqdm=True)
+
+    assert outputs is not None
+
+    for output in outputs:
+        assert output is not None
+        assert isinstance(output, RequestOutput)
+        prompt = output.prompt
+
+        generated_text = output.outputs[0].text
+        assert generated_text is not None
+        print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+        output_json = json.loads(generated_text)
+        jsonschema.validate(instance=output_json,
+                            schema=sample_definition_json_schema)
+
+
+@pytest.mark.skip_global_cleanup
 def test_guided_choice_completion(sample_guided_choice, llm):
     sampling_params = SamplingParams(
         temperature=0.8,
