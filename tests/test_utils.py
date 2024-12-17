2d1b9baa8f57fc59912c7bcd07fd630fb9d72c9d
Joe Runde <Joseph.Runde@ibm.com>
2024-12-17 13:26:32 -0700

    [Bugfix] Fix request cancellation without polling (#11190)

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,6 @@
 import asyncio
 import os
 import socket
-from functools import partial
 from typing import AsyncIterator, Tuple
 
 import pytest
@@ -26,10 +25,7 @@ async def test_merge_async_iterators():
             print(f"iterator {idx} cancelled")
 
     iterators = [mock_async_iterator(i) for i in range(3)]
-    merged_iterator = merge_async_iterators(*iterators,
-                                            is_cancelled=partial(asyncio.sleep,
-                                                                 0,
-                                                                 result=False))
+    merged_iterator = merge_async_iterators(*iterators)
 
     async def stream_output(generator: AsyncIterator[Tuple[int, str]]):
         async for idx, output in generator:
