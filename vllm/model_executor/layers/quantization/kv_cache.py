b302bfb103bbcc21ebece4ced6dff15ad9bb3751
Micah Williamson <micah.williamson@amd.com>
2024-12-17 17:43:18 -0600

    Dynamic Scale Factor Calculations for Key/Value Scales With FP8 KV Caching (#317)
    * Changed _k_scale and _v_scale to tensors
    
    * fixed rocm paged attention with tensor kv scales
    
    * Added on the fly scale factor calculation
    
    * trying to fix attn metadata
    
    * fixed AttentionMetadata issue, updated description for calculate-kv-scales flag in arg_utils.py
    
    * Changed K and V scale constants
    
    * Removed unneeded comment
    
    * Changes to pass format.sh, also fixed lingering k_scale/v_scale : float
    
    * Fix for TP > 1
    
    * Ran format.sh
    
    * Removed legacy kv_scale loading from the json file
    
    * Removed the outdated kv cache docs
    
    * Revert some unwanted changes
    
    ---------
    
    Co-authored-by: Gregory Shtrasberg <Gregory.Shtrasberg@amd.com>
    Signed-off-by: Gregory Shtrasberg <Gregory.Shtrasberg@amd.com>

--- vllm/model_executor/layers/quantization/kv_cache.py
+++ vllm/model_executor/layers/quantization/kv_cache.py
@@ -2,7 +2,8 @@ import torch
 
 from vllm.model_executor.layers.quantization.base_config import (
     QuantizationConfig, QuantizeMethodBase)
-from vllm.utils import print_warning_once
+from vllm.platforms import current_platform
+from vllm.utils import is_navi, print_warning_once
 
 
 class BaseKVCacheMethod(QuantizeMethodBase):
@@ -38,11 +39,16 @@ class BaseKVCacheMethod(QuantizeMethodBase):
     def process_weights_after_loading(self, layer: torch.nn.Module) -> None:
         # If the kv-cache dtype is auto, we enforce the k/v_scale to be 1.0
         # regardless whether the kv-scale is available in the checkpoint.
-        if layer.kv_cache_dtype != "auto":
+        # No need to process kv scales after loading if we are going to
+        # calculate them on the fly.
+        if layer.kv_cache_dtype != "auto" and not layer.calculate_kv_scales:
             if layer.k_scale > 0.0 and layer.v_scale > 0.0:
                 # We prefer to use separate k_scale and v_scale if present
                 k_scale = layer.k_scale.to("cpu").tolist()
                 v_scale = layer.v_scale.to("cpu").tolist()
+                if current_platform.is_rocm() and not is_navi():
+                    k_scale *= 2
+                    v_scale *= 2
             elif layer.k_scale < 0.0 and layer.v_scale < 0.0:
                 # If no scales were loaded (both scales are invalid negative
                 # values), use the default value of 1.0
@@ -56,6 +62,9 @@ class BaseKVCacheMethod(QuantizeMethodBase):
                 scale_to_duplicate = max(layer.k_scale, layer.v_scale)
                 k_scale = scale_to_duplicate.to("cpu").tolist()
                 v_scale = scale_to_duplicate.to("cpu").tolist()
+                if current_platform.is_rocm() and not is_navi():
+                    k_scale *= 2
+                    v_scale *= 2
 
             if not isinstance(k_scale, float) or not isinstance(
                     v_scale, float):
@@ -63,9 +72,9 @@ class BaseKVCacheMethod(QuantizeMethodBase):
                                  "for fp8 KV cache")
 
             # These are used in the final Attention.forward()
-            layer._k_scale = k_scale
-            layer._v_scale = v_scale
-            if (layer._k_scale == 1.0 and layer._v_scale == 1.0
+            layer._k_scale.copy_(k_scale)
+            layer._v_scale.copy_(v_scale)
+            if (k_scale == 1.0 and v_scale == 1.0
                     and "e5m2" not in layer.kv_cache_dtype):
                 print_warning_once(
                     "Using KV cache scaling factor 1.0 for fp8_e4m3. This "
