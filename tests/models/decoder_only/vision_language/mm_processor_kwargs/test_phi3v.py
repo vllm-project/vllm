28af2987dcd39fda59c9cf24908160a5e57ded78
Isotr0py <mozf@mail2.sysu.edu.cn>
2024-12-19 02:16:40 +0800

    [Bugfix] Fix broken phi3-v mm_processor_kwargs tests (#11263)
    Signed-off-by: Isotr0py <2037008807@qq.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- tests/models/decoder_only/vision_language/mm_processor_kwargs/test_phi3v.py
+++ tests/models/decoder_only/vision_language/mm_processor_kwargs/test_phi3v.py
@@ -58,16 +58,14 @@ def test_max_tokens_override(get_max_phi3v_image_tokens, model: str,
 
 @pytest.mark.parametrize("model", models)
 @pytest.mark.parametrize(
-    "num_crops,expected_toks_per_img,num_imgs",
+    "num_crops,expected_toks_per_img",
     [
-        (4, 757, 1),
-        (4, 757, 2),
-        (16, 1921, 1),
-        (16, 1921, 2),
+        (4, 757),
+        (16, 1921),
         # the default num_crops of phi-3.5-vision is 4
-        (None, 757, 2),
-        (None, 757, 2),
+        (None, 757),
     ])
+@pytest.mark.parametrize("num_imgs", [1, 2])
 def test_processor_override(processor_for_phi3v, image_assets: _ImageAssets,
                             model: str, num_crops: Optional[int],
                             expected_toks_per_img: int, num_imgs: int):
