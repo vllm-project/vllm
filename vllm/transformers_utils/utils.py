3222bcd2b5b4d96a254432bc87ef9f37481dfe7e
omer-dayan <omer@run.ai>
2024-12-20 18:46:24 +0200

    [Core] Loading model from S3 using RunAI Model Streamer as optional loader (#10192)
    Signed-off-by: OmerD <omer@run.ai>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/transformers_utils/utils.py
+++ vllm/transformers_utils/utils.py
@@ -3,6 +3,10 @@ from pathlib import Path
 from typing import Union
 
 
+def is_s3(model_or_path: str) -> bool:
+    return model_or_path.lower().startswith('s3://')
+
+
 def check_gguf_file(model: Union[str, PathLike]) -> bool:
     """Check if the file is a GGUF model."""
     model = Path(model)
