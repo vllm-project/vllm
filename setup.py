7c7aa37c6933c40a94da0789d0f330a8d89f091b
Daniele <36171005+dtrifiro@users.noreply.github.com>
2024-12-20 17:14:40 +0100

    [CI/Build] fix pre-compiled wheel install for exact tag (#11373)
    Signed-off-by: Daniele Trifirò <dtrifiro@redhat.com>

--- setup.py
+++ setup.py
@@ -466,7 +466,7 @@ def get_vllm_version() -> str:
             version += f"{sep}empty"
     elif _is_cuda():
         if envs.VLLM_USE_PRECOMPILED:
-            version += ".precompiled"
+            version += f"{sep}precompiled"
         else:
             cuda_version = str(get_nvcc_cuda_version())
             if cuda_version != MAIN_CUDA_VERSION:
