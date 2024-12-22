4a9139780ad78a648415f07dd7a5a216fb3f96ab
youkaichao <youkaichao@gmail.com>
2024-12-21 23:53:44 -0800

    [cd][release] add pypi index for every commit and nightly build (#11404)
    Signed-off-by: youkaichao <youkaichao@gmail.com>
    Co-authored-by: Roger Wang <136131678+ywang96@users.noreply.github.com>

---
+++ .buildkite/generate_index.py
@@ -0,0 +1,24 @@
+import argparse
+import os
+
+template = """<!DOCTYPE html>
+<html>
+    <body>
+    <h1>Links for vLLM</h1/>
+        <a href="../{wheel_html_escaped}">{wheel}</a><br/>
+    </body>
+</html>
+"""
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--wheel", help="The wheel path.", required=True)
+args = parser.parse_args()
+
+filename = os.path.basename(args.wheel)
+
+with open("index.html", "w") as f:
+    print(f"Generated index.html for {args.wheel}")
+    # cloudfront requires escaping the '+' character
+    f.write(
+        template.format(wheel=filename,
+                        wheel_html_escaped=filename.replace("+", "%2B")))
