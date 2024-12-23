46c0d981c0a187f7f763cd7c00be39d374a692a7
cenzhiyao <2523403608@qq.com>
2024-12-23 21:02:27 +0800

    rm unused weak_ref_tensor

--- vllm/model_executor/models/utils.py
+++ vllm/model_executor/models/utils.py
@@ -17,8 +17,7 @@ from vllm.model_executor.model_loader.weight_utils import default_weight_loader
 from vllm.multimodal import MultiModalPlaceholderMap, NestedTensors
 from vllm.platforms import _Backend, current_platform
 from vllm.sequence import IntermediateTensors
-from vllm.utils import (is_pin_memory_available, print_warning_once,
-                        weak_ref_tensor)
+from vllm.utils import (is_pin_memory_available, print_warning_once)
 
 logger = init_logger(__name__)
 
