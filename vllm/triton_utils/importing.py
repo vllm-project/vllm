e2f85d1f7cb38ebac97e44f7dd35abc75c8e9c7a
wangxiyuan <wangxiyuan1007@gmail.com>
2024-12-23 09:17:33 +0800

    [DNM] Test triton
    Signed-off-by: wangxiyuan <wangxiyuan1007@gmail.com>

--- vllm/triton_utils/importing.py
+++ vllm/triton_utils/importing.py
@@ -5,11 +5,7 @@ from vllm.platforms import current_platform
 
 logger = init_logger(__name__)
 
-HAS_TRITON = (
-    find_spec("triton") is not None
-    and not current_platform.is_xpu()  # Not compatible
-    and not current_platform.is_neuron()  # neuron has too old torch
-)
+HAS_TRITON = find_spec("triton") is not None
 
 if not HAS_TRITON:
     logger.info("Triton not installed or not compatible; certain GPU-related"
