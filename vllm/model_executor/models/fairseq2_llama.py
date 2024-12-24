0519ddd0290643926b14a3192368532f87fccdc7
Martin Gleize <mgleize@meta.com>
2024-12-24 00:31:50 +0000

    yapf
    Signed-off-by: Martin Gleize <mgleize@meta.com>

--- vllm/model_executor/models/fairseq2_llama.py
+++ vllm/model_executor/models/fairseq2_llama.py
@@ -25,17 +25,18 @@ from .utils import AutoWeightsLoader, WeightsMapper
 
 
 class Fairseq2LlamaForCausalLM(LlamaForCausalLM):
+
     def _init_model(self, vllm_config: VllmConfig, prefix: str = ""):
         return LlamaModel(vllm_config=vllm_config, prefix=prefix)
 
-    def load_weights(
-        self, weights: Iterable[Tuple[str, torch.Tensor]]
-    ) -> Set[str]:
+    def load_weights(self, weights: Iterable[Tuple[str,
+                                                   torch.Tensor]]) -> Set[str]:
         # fairseq2's serialization adds a wrapper to usual .pt state_dict's:
         # { "model_key": my_model_name, "my_model_name": state_dict }
         # which we first need to unpack
         weights_wrapped = dict(weights)
-        weights = weights_wrapped[weights_wrapped["model_key"]].items()  # type: ignore
+        weights = weights_wrapped[
+            weights_wrapped["model_key"]].items()  # type: ignore
 
         hf_to_vllm_mapper = WeightsMapper(
             orig_to_new_prefix={
@@ -55,15 +56,12 @@ class Fairseq2LlamaForCausalLM(LlamaForCausalLM):
         )
         loader = AutoWeightsLoader(
             self,
-            skip_prefixes=(
-                ["lm_head."] if self.config.tie_word_embeddings else None
-            ),
+            skip_prefixes=(["lm_head."]
+                           if self.config.tie_word_embeddings else None),
         )
         return loader.load_weights(
-            (
-                self.reshape_fairseq2_weights(name, loaded_weight)
-                for name, loaded_weight in weights
-            ),
+            (self.reshape_fairseq2_weights(name, loaded_weight)
+             for name, loaded_weight in weights),
             mapper=hf_to_vllm_mapper,
         )
 
@@ -73,25 +71,22 @@ class Fairseq2LlamaForCausalLM(LlamaForCausalLM):
         name: str,
         loaded_weight: torch.Tensor,
     ) -> Tuple[str, torch.Tensor]:
+
         def permute(w: torch.Tensor, n_heads: int):
             attn_in = self.config.head_dim * n_heads
             attn_out = self.config.hidden_size
-            return (
-                w.view(n_heads, attn_in // n_heads // 2, 2, attn_out)
-                .transpose(1, 2)
-                .reshape(attn_in, attn_out)
-            )
+            return (w.view(n_heads, attn_in // n_heads // 2, 2,
+                           attn_out).transpose(1,
+                                               2).reshape(attn_in, attn_out))
 
         modules = name.split(".")
 
         # rotary embeds should be sliced
         if "k_proj" in modules:
-            loaded_weight = permute(
-                loaded_weight, self.config.num_key_value_heads
-            )
+            loaded_weight = permute(loaded_weight,
+                                    self.config.num_key_value_heads)
         elif "q_proj" in modules:
-            loaded_weight = permute(
-                loaded_weight, self.config.num_attention_heads
-            )
+            loaded_weight = permute(loaded_weight,
+                                    self.config.num_attention_heads)
 
         return name, loaded_weight
