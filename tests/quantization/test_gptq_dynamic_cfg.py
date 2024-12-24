74b1d4223e93d45e678a0d6efd5ed3965eedca63
ZX-ModelCloud <zx@modelcloud.ai>
2024-12-24 20:41:00 +0800

    add test_gptq_dynamic_cfg.py

---
+++ tests/quantization/test_gptq_dynamic_cfg.py
@@ -0,0 +1,46 @@
+"""Tests whether gptq models with dynamic_cfg quantized can be loaded.
+
+Run `pytest tests/quantization/test_gptq_dynamic_cfg.py --forked`.
+"""
+
+import pytest
+import torch
+
+from vllm.model_executor.layers.quantization.gptq_marlin import (
+    GPTQMarlinLinearMethod)
+from vllm.model_executor.layers.linear import UnquantizedLinearMethod
+
+PROMPT = "On the surface of Mars, we found"
+
+MODELS_QUANT = ["ModelCloud/TinyLlama-1.1B-Chat-v1.0-GPTQ-4bits-dynamic-cfg"]
+
+
+@pytest.mark.parametrize("model_id", MODELS_QUANT)
+def test_gptq_with_dynamic_cfg(vllm_runner, model_id: str):
+    vllm_model = vllm_runner(model_id, dtype=torch.float16, max_model_len=2048)
+
+    for name, submodule in (vllm_model.model.llm_engine.model_executor.
+                            driver_worker.model_runner.model.named_modules()):
+        if name == 'model.model.layers.0.self_attn.qkv_proj':
+            # The first layer is quantized using bits=4, group_size=128,
+            # desc_act=True
+            assert isinstance(submodule, GPTQMarlinLinearMethod)
+            assert submodule.quant_config.bits == 4
+            assert submodule.quant_config.group_size == 128
+            assert submodule.quant_config.desc_act
+        elif name == 'model.model.layers.1.self_attn.qkv_proj':
+            # The second layer is quantized using bits=8, group_size=32,
+            # desc_act=False
+            assert isinstance(submodule, GPTQMarlinLinearMethod)
+            assert submodule.quant_config.bits == 8
+            assert submodule.quant_config.group_size == 32
+            assert not submodule.quant_config.desc_act
+        elif (name == 'model.model.layers.2.self_attn.qkv_proj'
+              or name == 'model.model.layers.2.mlp.gate_up_proj'):
+            # Other layers are not quantized.
+            assert isinstance(submodule, UnquantizedLinearMethod)
+
+    print(
+        vllm_model.generate_greedy(prompts=["Hello my name is"],
+                                   max_tokens=10)[0][1])
+    del vllm_model
