1749da1210a2bb60cdfaf07675d8aac75326d62e
rshaw@neuralmagic.com <rshaw@neuralmagic.com>
2024-12-27 01:17:09 +0000

    updated

--- vllm/v1/engine/async_llm.py
+++ vllm/v1/engine/async_llm.py
@@ -242,7 +242,6 @@ class AsyncLLM(EngineClient):
         # generate() task will be canceled. So, we abort the
         # request if we end up here.
         except asyncio.CancelledError:
-            print("calling abort")
             await self.abort(request_id)
             raise
 
