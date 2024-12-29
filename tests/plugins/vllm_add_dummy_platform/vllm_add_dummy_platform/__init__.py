5cdd326aa2f666d5f0012a9838da8ed0372e5e26
youkaichao <youkaichao@gmail.com>
2024-12-29 16:36:24 +0800

    add dummy platform tests
    Signed-off-by: youkaichao <youkaichao@gmail.com>

---
+++ tests/plugins/vllm_add_dummy_platform/vllm_add_dummy_platform/__init__.py
@@ -0,0 +1,3 @@
+def dummy_platform_plugin() -> Tuple[bool, str]:
+    is_dummy = True
+    return is_dummy, "vllm_add_dummy_platform.dummy_platform.DummyPlatform"
---
+++ tests/plugins/vllm_add_dummy_platform/vllm_add_dummy_platform/dummy_platform.py
@@ -0,0 +1,8 @@
+from vllm.platforms import Platform, PlatformEnum
+
+class DummyPlatform(Platform):
+    _enum = PlatformEnum.UNSPECIFIED
+    device_name = "DummyDevice"
+    device_type = "DummyType"
+    dispatch_key = "DUMMY"
+    supported_quantization = ["dummy_quantization"]
