2caec1230b98d74087375ce3c101bc0c292c9afc
youkaichao <youkaichao@gmail.com>
2024-12-30 08:59:02 +0800

    collect all files
    Signed-off-by: youkaichao <youkaichao@gmail.com>

--- vllm/config.py
+++ vllm/config.py
@@ -2751,6 +2751,7 @@ class CompilationConfig(BaseModel):
     # keep track of enabled and disabled custom ops
     enabled_custom_ops: Counter[str] = PrivateAttr
     disabled_custom_ops: Counter[str] = PrivateAttr
+    traced_files: Set[str] = PrivateAttr
     compilation_time: float = PrivateAttr
     # should be InductorHashCache, but Pydantic does not support it
     inductor_hash_cache: Any = PrivateAttr
