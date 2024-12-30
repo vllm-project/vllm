2e8a87f11112c94c888141dae192e82be25a65c8
Nathan Azrak <nathan.azrak@gmail.com>
2024-12-30 15:40:07 +1100

    use process subsitution instead of pipelining to fix local variable issues
    Signed-off-by: Nathan Azrak <nathan.azrak@gmail.com>

--- sagemaker-entrypoint.sh
+++ sagemaker-entrypoint.sh
@@ -9,16 +9,16 @@ ARG_PREFIX="--"
 ARGS=(--port 8080)
 
 # Loop through all environment variables
-env | grep "^${PREFIX}" | while IFS='=' read -r key value; do
+while IFS='=' read -r key value; do
     # Remove the prefix from the key, convert to lowercase, and replace underscores with dashes
-    arg_name=$(echo "${key#${PREFIX}}" | tr '[:upper:]' '[:lower:]' | tr '_' '-')
+    arg_name=$(echo "${key#"${PREFIX}"}" | tr '[:upper:]' '[:lower:]' | tr '_' '-')
 
     # Add the argument name and value to the ARGS array
     ARGS+=("${ARG_PREFIX}${arg_name}")
     if [ -n "$value" ]; then
         ARGS+=("$value")
     fi
-done
+done < <(env | grep "^${PREFIX}")
 
 # Pass the collected arguments to the main entrypoint
 exec python3 -m vllm.entrypoints.openai.api_server "${ARGS[@]}"
