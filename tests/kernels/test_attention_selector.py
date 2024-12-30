6fb847858c2e69f837833d625449ef9cc0b7d8f9
wangxiyuan <wangxiyuan1007@gmail.com>
2024-12-30 11:27:52 +0800

    [platform] Allow platform specify attention backend
    Signed-off-by: wangxiyuan <wangxiyuan1007@gmail.com>

--- tests/kernels/test_attention_selector.py
+++ tests/kernels/test_attention_selector.py
@@ -16,9 +16,7 @@ from vllm.utils import STR_FLASH_ATTN_VAL, STR_INVALID_VAL
     "name", ["TORCH_SDPA", "ROCM_FLASH", "XFORMERS", "FLASHINFER", "OPENVINO"])
 @pytest.mark.parametrize("device", ["cpu", "openvino", "hip", "cuda"])
 def test_env(name: str, device: str, monkeypatch):
-    """Test that the attention selector can be set via environment variable.
-    Note that we do not test FlashAttn because it is the default backend.
-    """
+    """Test that the attention selector can be set via environment variable."""
 
     override_backend_env_variable(monkeypatch, name)
 
@@ -26,23 +24,28 @@ def test_env(name: str, device: str, monkeypatch):
         with patch("vllm.attention.selector.current_platform", CpuPlatform()):
             backend = which_attn_to_use(16, torch.float16, torch.float16, 16,
                                         False)
-        assert backend.name == "TORCH_SDPA"
+        assert backend == "vllm.attention.backends.torch_sdpa.TorchSDPABackend"
     elif device == "hip":
         with patch("vllm.attention.selector.current_platform", RocmPlatform()):
             backend = which_attn_to_use(16, torch.float16, torch.float16, 16,
                                         False)
-        assert backend.name == "ROCM_FLASH"
+        assert backend == "vllm.attention.backends.rocm_flash_attn.ROCmFlashAttentionBackend"  # noqa: E501
     elif device == "openvino":
         with patch("vllm.attention.selector.current_platform",
                    OpenVinoPlatform()):
             backend = which_attn_to_use(16, torch.float16, torch.float16, 16,
                                         False)
-        assert backend.name == "OPENVINO"
+        assert backend == "vllm.attention.backends.openvino.OpenVINOAttentionBackend"  # noqa: E501
     else:
         with patch("vllm.attention.selector.current_platform", CudaPlatform()):
             backend = which_attn_to_use(16, torch.float16, torch.float16, 16,
                                         False)
-        assert backend.name == name
+        if name == "FLASHINFER":
+            assert backend == "vllm.attention.backends.flashinfer.FlashInferBackend"  # noqa: E501
+        if name == "XFORMERS":
+            assert backend == "vllm.attention.backends.xformers.XFormersBackend"
+        else:
+            assert backend == "vllm.attention.backends.flash_attn.FlashAttentionBackend"  # noqa: E501
 
 
 def test_flash_attn(monkeypatch):
@@ -55,32 +58,32 @@ def test_flash_attn(monkeypatch):
     # Unsupported CUDA arch
     with patch("torch.cuda.get_device_capability", return_value=(7, 5)):
         backend = which_attn_to_use(16, torch.float16, None, 16, False)
-        assert backend.name != STR_FLASH_ATTN_VAL
+        assert backend != "vllm.attention.backends.flash_attn.FlashAttentionBackend"  # noqa: E501
 
     # Unsupported data type
     backend = which_attn_to_use(16, torch.float8_e4m3fn, None, 16, False)
-    assert backend.name != STR_FLASH_ATTN_VAL
+    assert backend != "vllm.attention.backends.flash_attn.FlashAttentionBackend"
 
     # Unsupported kv cache data type
     backend = which_attn_to_use(16, torch.float16, "fp8", 16, False)
-    assert backend.name != STR_FLASH_ATTN_VAL
+    assert backend != "vllm.attention.backends.flash_attn.FlashAttentionBackend"
 
     # Unsupported block size
     backend = which_attn_to_use(16, torch.float16, None, 8, False)
-    assert backend.name != STR_FLASH_ATTN_VAL
+    assert backend != "vllm.attention.backends.flash_attn.FlashAttentionBackend"
 
     # flash-attn is not installed
     with patch.dict('sys.modules', {'vllm_flash_attn': None}):
         backend = which_attn_to_use(16, torch.float16, None, 16, False)
-        assert backend.name != STR_FLASH_ATTN_VAL
+        assert backend != "vllm.attention.backends.flash_attn.FlashAttentionBackend"  # noqa: E501
 
     # Unsupported head size
     backend = which_attn_to_use(17, torch.float16, None, 16, False)
-    assert backend.name != STR_FLASH_ATTN_VAL
+    assert backend != "vllm.attention.backends.flash_attn.FlashAttentionBackend"
 
     # Attention-free models should bypass env and use PlaceholderAttention
     backend = which_attn_to_use(16, torch.float16, torch.float16, 16, True)
-    assert backend.name != STR_FLASH_ATTN_VAL
+    assert backend != "vllm.attention.backends.flash_attn.FlashAttentionBackend"
 
 
 def test_invalid_env(monkeypatch):
