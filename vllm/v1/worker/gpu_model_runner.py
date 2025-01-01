8b3291d623247c39476a09b85547629c05e04d0a
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2025-01-01 02:44:24 -0800

    Add e2e test
    Signed-off-by: Woosuk Kwon <woosuk.kwon@berkeley.edu>

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -426,7 +426,6 @@ class GPUModelRunner:
             )
 
         if use_cascade:
-            logger.debug("Using cascade attention.")
             # TODO: Optimize.
             cu_prefix_query_lens = torch.tensor(
                 [0, total_num_scheduled_tokens],
