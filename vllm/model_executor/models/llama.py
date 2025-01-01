c4358517fe78e60c35786e828fb2a022885afa66
Sungjae Lee <33976427+llsj14@users.noreply.github.com>
2025-01-01 17:06:23 +0000

    remove modification on llama model
    Signed-off-by: Sungjae Lee <33976427+llsj14@users.noreply.github.com>

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -315,12 +315,6 @@ class LlamaModel(nn.Module):
             )
         else:
             self.embed_tokens = PPMissingLayer()
-
-        if config.num_hidden_layers==1:
-            self.eagle = True
-        else:
-            self.eagle = False
-
         self.start_layer, self.end_layer, self.layers = make_layers(
             config.num_hidden_layers,
             lambda prefix: layer_type(config=config,
@@ -374,8 +368,6 @@ class LlamaModel(nn.Module):
             })
 
         hidden_states, _ = self.norm(hidden_states, residual)
-        if self.eagle:
-            hidden_states = residual + hidden_states
         return hidden_states
 
     def load_weights(self, weights: Iterable[Tuple[str,
