555861e42da0a773d73c0b679c7c6c483248f10b
rshaw@neuralmagic.com <rshaw@neuralmagic.com>
2025-01-02 13:33:24 +0000

    remove spurious change

--- vllm/v1/engine/async_llm.py
+++ vllm/v1/engine/async_llm.py
@@ -164,14 +164,8 @@ class AsyncLLM(EngineClient):
 
         # 2) Convert input --> DetokenizerRequest / EngineCoreRequest.
         detokenizer_req, engine_core_req = self.processor.process_inputs(
-            request_id=request_id,
-            prompt=prompt,
-            params=params,
-            arrival_time=arrival_time,
-            lora_request=lora_request,
-            trace_headers=trace_headers,
-            prompt_adapter_request=prompt_adapter_request,
-            priority=priority)
+            request_id, prompt, params, arrival_time, lora_request,
+            trace_headers, prompt_adapter_request, priority)
 
         # 3) Add the request to Detokenizer (this process).
         self.detokenizer.add_request(detokenizer_req)
