0347baaf2bb7656d461817e1f00f3b1cbaecf2a4
Robert Shaw <114415538+robertgshaw2-neuralmagic@users.noreply.github.com>
2025-01-03 14:09:50 -0500

    Update vllm/v1/executor/multiproc_executor.py
    Co-authored-by: Tyler Michael Smith <tyler@neuralmagic.com>

--- vllm/v1/executor/multiproc_executor.py
+++ vllm/v1/executor/multiproc_executor.py
@@ -350,7 +350,7 @@ class WorkerProc:
             logger.debug("Worker interrupted.")
 
         except Exception:
-            # worker_busy_loop sends exceptions exceptons to Executor
+            # worker_busy_loop sends exceptons to Executor
             # for shutdown, but if there is an error in startup or an
             # error with IPC itself, we need to alert the parent.
             psutil.Process().parent().send_signal(signal.SIGQUIT)
