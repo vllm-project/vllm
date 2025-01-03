6f760c0ab59a80c40c88ceb27bdca7370ce72d7c
s.kochetkov <s.m.kochetkov@tbank.ru>
2025-01-03 11:53:29 +0000

    format
    Signed-off-by: s.kochetkov <s.m.kochetkov@tbank.ru>

--- vllm/engine/arg_utils.py
+++ vllm/engine/arg_utils.py
@@ -648,10 +648,10 @@ class EngineArgs:
             help=('Data type for LoRA. If auto, will default to '
                   'base model dtype.'))
         parser.add_argument(
-            '--enable_lora_modules_to_save', type=bool, 
+            '--enable_lora_modules_to_save',
+            type=bool,
             default=EngineArgs.enable_lora_modules_to_save,
-            help='enable fully trained lm_head and embed_tokens'
-        )
+            help='enable fully trained lm_head and embed_tokens')
         parser.add_argument(
             '--long-lora-scaling-factors',
             type=nullable_str,
