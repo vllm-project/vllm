de75cc44a2a4162a2ef398ad0f729d139e78c96c
rshaw@neuralmagic.com <rshaw@neuralmagic.com>
2025-01-04 20:16:35 +0000

    cruft

--- vllm/v1/engine/core.py
+++ vllm/v1/engine/core.py
@@ -209,7 +209,7 @@ class EngineCoreProc(EngineCore):
     def run_busy_loop(self):
         """Core busy loop of the EngineCore."""
 
-        # Loop until process is sent a SIGINT or SIGTERM.
+        # Loop until process is sent a SIGINT or SIGTERM
         while True:
             # 1) Poll the input queue until there is work to do.
             if not self.scheduler.has_unfinished_requests():
