92f6a35bacdf02064c9006a515947497f991cfd5
Cyrus Leung <tlleungac@connect.ust.hk>
2025-01-04 23:46:21 +0800

    [Model] Remove unnecessary weight initialization logic (#11736)
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>
    Signed-off-by: Isotr0py <2037008807@qq.com>
    Co-authored-by: Isotr0py <2037008807@qq.com>

--- vllm/model_executor/models/minicpmv.py
+++ vllm/model_executor/models/minicpmv.py
@@ -141,8 +141,6 @@ class Resampler2_5(BaseResampler):
         self.max_size = max_size
         self._set_2d_pos_cache(self.max_size)
 
-        self.apply(self._init_weights)
-
     def _set_2d_pos_cache(self,
                           max_size: Tuple[int, int],
                           device: torch.types.Device = "cpu") -> None:
