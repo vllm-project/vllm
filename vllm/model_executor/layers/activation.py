6a813e1f6546f060465e471e295e80a94cb48d56
youkaichao <youkaichao@gmail.com>
2025-01-06 00:28:11 +0800

    lint
    Signed-off-by: youkaichao <youkaichao@gmail.com>

--- vllm/model_executor/layers/activation.py
+++ vllm/model_executor/layers/activation.py
@@ -10,8 +10,8 @@ from vllm.distributed import (divide, get_tensor_model_parallel_rank,
                               get_tensor_model_parallel_world_size)
 from vllm.model_executor.custom_op import CustomOp
 from vllm.model_executor.utils import set_weight_attrs
-from vllm.utils import LazyDict
 from vllm.platforms import current_platform
+from vllm.utils import LazyDict
 
 
 @CustomOp.register("fatrelu_and_mul")
