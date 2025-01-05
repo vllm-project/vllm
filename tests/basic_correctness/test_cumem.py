5189a29e561e4db345c8ed604181cf1e98fd4447
youkaichao <youkaichao@gmail.com>
2025-01-05 13:39:50 +0800

    add basic tests
    Signed-off-by: youkaichao <youkaichao@gmail.com>

---
+++ tests/basic_correctness/test_cumem.py
@@ -0,0 +1,33 @@
+from vllm.device_allocator.cumem import CuMemAllocator, CuMemMode
+import torch
+
+def test_basic_cumem():
+    # some tensors from default memory pool
+    shape = (1024, 1024)
+    x = torch.empty(shape, device='cuda')
+    x.zero_()
+
+    # some tensors from custom memory pool
+    allocator = CuMemAllocator.get_instance()
+    with allocator.use_memory_pool(mode=CuMemMode.OFFLOAD):
+        # custom memory pool
+        y = torch.empty(shape, device='cuda')
+        y.zero_()
+        y += 1
+        z = torch.empty(shape, device='cuda')
+        z.zero_()
+        z += 2
+
+    # they can be used together
+    output = x + y + z
+    assert torch.allclose(output, torch.ones_like(output) * 3)
+
+    free_bytes = torch.cuda.mem_get_info()[0]
+    allocator.sleep()
+    free_bytes_after_sleep = torch.cuda.mem_get_info()[0]
+    assert free_bytes_after_sleep < free_bytes
+    allocator.wake_up()
+
+    # they can be used together
+    output = x + y + z
+    assert torch.allclose(output, torch.ones_like(output) * 3)
