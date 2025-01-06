6e57de4f4020fd7cb0466890d926943ea5ad3fe0
Andrew Feldman <afeldman@neuralmagic.com>
2025-01-06 03:53:45 +0000

    wip
    Signed-off-by: Andrew Feldman <afeldman@neuralmagic.com>

--- vllm/v1/serial_utils.py
+++ vllm/v1/serial_utils.py
@@ -4,7 +4,9 @@ from typing import Any
 import torch
 from msgspec import msgpack
 
-CUSTOM_TYPE_CODE_PICKLE = 1
+from vllm.v1.engine import EngineCoreOutputs
+
+CUSTOM_TYPE_CODE_PICKLE = 100
 
 
 class PickleEncoder:
@@ -23,9 +25,16 @@ class MsgpackEncoder:
         self.encoder = msgpack.Encoder(enc_hook=custom_enc_hook)
 
     def encode(self, obj: Any) -> bytes:
+        # print("\n\nencode() obj:",obj,"\n\n")
+        # dec=msgpack.Decoder(EngineCoreOutputs, ext_hook=custom_ext_hook)
+        # dec.decode(self.encoder.encode(obj))
         return self.encoder.encode(obj)
 
     def encode_into(self, obj: Any, buf: bytearray) -> None:
+        print("\n\nbanana:",self.encoder.encode(obj),"\n\n")
+        # print(f"\n\nencode_into obj:{obj}","\n\n")
+        # dec=msgpack.Decoder(EngineCoreOutputs, ext_hook=custom_ext_hook)
+        # dec.decode(self.encoder.encode(obj))
         self.encoder.encode_into(obj, buf)
 
 
@@ -44,7 +53,8 @@ def custom_enc_hook(obj: Any) -> Any:
         # NOTE(rob): it is fastest to use numpy + pickle
         # when serializing torch tensors.
         # https://gist.github.com/tlrmchlsmth/8067f1b24a82b6e2f90450e7764fa103 # noqa: E501
-        return msgpack.Ext(CUSTOM_TYPE_CODE_PICKLE, pickle.dumps(obj.numpy()))
+        #return msgpack.Ext(CUSTOM_TYPE_CODE_PICKLE, pickle.dumps(obj.numpy()))
+        return msgpack.Ext(CUSTOM_TYPE_CODE_PICKLE, msgpack.Encoder().encode(obj.numpy()))
     else:
         raise NotImplementedError(
             f"Objects of type {type(obj)} are not supported")
