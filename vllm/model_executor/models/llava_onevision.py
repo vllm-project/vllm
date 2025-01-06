Fix LLaVA-NeXT feature size calculation (for real)
Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

vllm/model_executor/models/llava_onevision.py
@@ -104,30 +104,29 @@ class LlavaOnevisionProcessingMixin(LlavaNextProcessingMixin):
         num_patch_height: int,
         num_patch_width: int,
     ) -> tuple[int, int]:
-        current_height = npatches * num_patch_height
-        current_width = npatches * num_patch_width
-
         # NOTE: Use float32 to remain consistent with HF output
-        original_aspect_ratio = np.array(original_width / original_height,
-                                         dtype=np.float32)
-        current_aspect_ratio = np.array(current_width / current_height,
-                                        dtype=np.float32)
+        current_height = np.float32(npatches * num_patch_height)
+        current_width = np.float32(npatches * num_patch_width)
+
+        original_width = np.float32(original_width)  # type: ignore
+        original_height = np.float32(original_height)  # type: ignore
+
+        original_aspect_ratio = original_width / original_height
+        current_aspect_ratio = current_width / current_height
 
         if original_aspect_ratio > current_aspect_ratio:
-            scale_factor = np.array(current_width / original_width,
-                                    dtype=np.float32)
+            scale_factor = current_width / original_width
             new_height = int(original_height * scale_factor)
             padding = (current_height - new_height) // 2
             current_height -= 2 * padding
         else:
-            scale_factor = np.array(current_height / original_height,
-                                    dtype=np.float32)
+            scale_factor = current_height / original_height
             new_width = int(original_width * scale_factor)
             padding = (current_width - new_width) // 2
             current_width -= 2 * padding
 
-        unpadded_features = current_height * current_width
-        newline_features = current_height
+        unpadded_features = int(current_height * current_width)
+        newline_features = int(current_height)
 
         ratio = math.sqrt(current_height * current_width / (9 * npatches**2))
         if ratio > 1.1: