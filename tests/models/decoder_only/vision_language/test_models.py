a38c64f45e9ebd77aa9e81f8fb628c526f0c6ae2
Jee Jee Li <pandaleefree@gmail.com>
2025-01-06 09:28:50 +0000

    Fix format
    Signed-off-by: Jee Jee Li <pandaleefree@gmail.com>

--- tests/models/decoder_only/vision_language/test_models.py
+++ tests/models/decoder_only/vision_language/test_models.py
@@ -349,8 +349,7 @@ VLM_TEST_SETTINGS = {
         max_num_seqs=2,
         image_size_factors=[(),(1.0, 1.0, 1.0)],
         patch_hf_runner=model_utils.mlomo_patch_hf_runner,
-        postprocess_inputs=model_utils.molmo_post_processor
-
+        postprocess_inputs=model_utils.molmo_post_processor,
     ),
     # Tests for phi3v currently live in another file because of a bug in
     # transformers. Once this issue is fixed, we can enable them here instead.
