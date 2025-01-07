2a359ef78dacc562c44cfad56abb1f0ce064981a
rshaw@neuralmagic.com <rshaw@neuralmagic.com>
2025-01-07 02:38:06 +0000

    stash

--- vllm/model_executor/layers/quantization/kernels/scaled_mm/cutlass.py
+++ vllm/model_executor/layers/quantization/kernels/scaled_mm/cutlass.py
@@ -88,11 +88,11 @@ class CutlassScaledMMLinearKernel(ScaledMMLinearKernel):
         # For more details, see csrc/quantization/cutlass_w8a8/Epilogues.md
         # https://github.com/vllm-project/vllm/blob/8d59dbb00044a588cab96bcdc028006ed922eb06/csrc/quantization/cutlass_w8a8/Epilogues.md
         if not self.config.input_symmetric:
-            layer.azp_adj = layer.weight.sum(dim=0,
-                                             keepdim=True,
-                                             dtype=torch.int32)
+            azp_adj = layer.weight.sum(dim=0, keepdim=True, dtype=torch.int32)
+            setattr(layer, self.azp_adj_name, 
+                    torch.nn.Parameter(azp_adj, requires_grad=False))
         else:
-            layer.azp_adj = None
+            setattr(layer, self.azp_adj_name, None)
 
     def apply_weights(self,
                       layer: torch.nn.Module,
