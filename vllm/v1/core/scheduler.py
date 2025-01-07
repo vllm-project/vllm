d21b446d2b1eb87703518f26d0bddb2c08d55347
WangErXiao <863579016@qq.com>
2025-01-07 15:48:21 +0800

    [V1][Misc] Remove the duplicate imports of MultiModalKwargs and PlaceholderRange
    Remove the duplicate imports of MultiModalKwargs and PlaceholderRange, as they are only used for type hints.

--- vllm/v1/core/scheduler.py
+++ vllm/v1/core/scheduler.py
@@ -5,8 +5,6 @@ from typing import (TYPE_CHECKING, Deque, Dict, Iterable, List, Optional, Set,
 
 from vllm.config import CacheConfig, LoRAConfig, SchedulerConfig
 from vllm.logger import init_logger
-from vllm.multimodal import MultiModalKwargs
-from vllm.multimodal.base import PlaceholderRange
 from vllm.sampling_params import SamplingParams
 from vllm.v1.core.encoder_cache_manager import EncoderCacheManager
 from vllm.v1.core.kv_cache_manager import KVCacheManager
