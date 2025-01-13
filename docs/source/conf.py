024803f974a122b13af05bffbf0edf6e1c49077d
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-01-13 12:32:20 +0000

    Resolve conflict
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,7 +55,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns: list[str] = ["**/*.template.md"]
+exclude_patterns: list[str] = ["**/*.template.md", "**/*.inc.md"]
 
 # Exclude the prompt "$" when copying code
 copybutton_prompt_text = r"\$ "
