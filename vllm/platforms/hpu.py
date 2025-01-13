458e63a2c6b18e7febfa30cecb59461f96fbe324
youkaichao <youkaichao@gmail.com>
2025-01-13 20:59:09 +0800

    [platform] add device_control env var (#12009)
    Signed-off-by: youkaichao <youkaichao@gmail.com>

--- vllm/platforms/hpu.py
+++ vllm/platforms/hpu.py
@@ -20,6 +20,7 @@ class HpuPlatform(Platform):
     device_type: str = "hpu"
     dispatch_key: str = "HPU"
     ray_device_key: str = "HPU"
+    device_control_env_var: str = "HABANA_VISIBLE_MODULES"
 
     @classmethod
     def get_attn_backend_cls(cls, selected_backend: _Backend, head_size: int,
