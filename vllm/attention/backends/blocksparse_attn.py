7f6bab092b1493b149a4479f27bbfdf38d1617f5
Chen Zhang <zhangch99@outlook.com>
2025-01-14 05:03:39 -0800

    fix blocksparse attention
    Signed-off-by: Chen Zhang <zhangch99@outlook.com>

--- vllm/attention/backends/blocksparse_attn.py
+++ vllm/attention/backends/blocksparse_attn.py
@@ -90,7 +90,7 @@ class BlocksparseFlashAttentionBackend(AttentionBackend):
     @staticmethod
     def get_name() -> str:
         # For attention layer compatibility
-        return "FLASH_ATTN"
+        return "BLOCK_SPARSE_FLASH_ATTN"
 
     @staticmethod
     def get_impl_cls() -> Type["BlocksparseFlashAttentionImpl"]:
