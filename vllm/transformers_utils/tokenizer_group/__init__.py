b291b775531e37b288d55c75d3ebc8c297c34c45
Joe Runde <Joseph.Runde@ibm.com>
2025-01-15 09:31:01 -0700

    [Bugfix] use right truncation for non-generative tasks (#12050)
    Signed-off-by: Joe Runde <Joseph.Runde@ibm.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/transformers_utils/tokenizer_group/__init__.py
+++ vllm/transformers_utils/tokenizer_group/__init__.py
@@ -24,7 +24,8 @@ def init_tokenizer_from_configs(model_config: ModelConfig,
                        max_input_length=None,
                        tokenizer_mode=model_config.tokenizer_mode,
                        trust_remote_code=model_config.trust_remote_code,
-                       revision=model_config.tokenizer_revision)
+                       revision=model_config.tokenizer_revision,
+                       truncation_side=model_config.truncation_side)
 
     return get_tokenizer_group(parallel_config.tokenizer_pool_config,
                                **init_kwargs)
