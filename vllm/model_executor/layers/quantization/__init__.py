61d6ca67182ddabf74697a058bb49b51d505eafc
maktukmak <mehmet.aktukmak@intel.com>
2025-01-15 22:54:37 +0000

    rebase and conflict resolve

--- vllm/model_executor/layers/quantization/__init__.py
+++ vllm/model_executor/layers/quantization/__init__.py
@@ -107,6 +107,7 @@ def get_quantization_config(quantization: str) -> Type[QuantizationConfig]:
     from .ptpc_fp8 import PTPCFp8Config
     from .qqq import QQQConfig
     from .tpu_int8 import Int8TpuConfig
+    from vllm_hpu_extension.awq_hpu import AWQHPUConfig
 
     method_to_config: Dict[str, Type[QuantizationConfig]] = {
         "aqlm": AQLMConfig,
