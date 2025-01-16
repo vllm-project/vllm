988586d5c17b66a2de2341379c7a56e52ea478c9
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-01-16 19:08:57 +0100

    Undo changes to `chat.py`
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- examples/offline_inference/chat.py
+++ examples/offline_inference/chat.py
@@ -1,7 +1,7 @@
 from vllm import LLM, SamplingParams
 
-# llm = LLM(model="meta-llama/Llama-3.2-1B-Instruct")
-sampling_params = SamplingParams(temperature=0.5, top_k=8, max_tokens=300)
+llm = LLM(model="meta-llama/Meta-Llama-3-8B-Instruct")
+sampling_params = SamplingParams(temperature=0.5)
 
 
 def print_outputs(outputs):
@@ -14,14 +14,9 @@ def print_outputs(outputs):
 
 print("=" * 80)
 
-# outputs = llm.generate(["The theory of relativity states that"],
-#                    sampling_params=sampling_params,
-#                    use_tqdm=False)
-# print_outputs(outputs)
+# In this script, we demonstrate how to pass input to the chat method:
 
-# # In this script, we demonstrate how to pass input to the chat method:
-llm = LLM(model="meta-llama/Llama-3.1-8B-Instruct", tensor_parallel_size=1)
-conversation1 = [
+conversation = [
     {
         "role": "system",
         "content": "You are a helpful assistant"
@@ -36,45 +31,40 @@ conversation1 = [
     },
     {
         "role": "user",
-        "content": "Write a short essay about the importance of higher education.",
+        "content": "Write an essay about the importance of higher education.",
     },
 ]
-
-conversations = [conversation1 for _ in range(100)]
-import time
-
-start = time.time()
-outputs = llm.chat(conversations,sampling_params=sampling_params,use_tqdm=True)
+outputs = llm.chat(conversation,
+                   sampling_params=sampling_params,
+                   use_tqdm=False)
 print_outputs(outputs)
-end = time.time()
 
-print(end-start)
 # You can run batch inference with llm.chat API
-# conversation2 = [
-#     {
-#         "role": "system",
-#         "content": "You are a helpful assistant"
-#     },
-#     {
-#         "role": "user",
-#         "content": "Hello"
-#     },
-#     {
-#         "role": "assistant",
-#         "content": "Hello! How can I assist you today?"
-#     },
-#     {
-#         "role": "user",
-#         "content": "Write an essay about the importance of playing video games!",
-#     },
-# ]
-# conversations = [conversation1, conversation2]
+conversation = [
+    {
+        "role": "system",
+        "content": "You are a helpful assistant"
+    },
+    {
+        "role": "user",
+        "content": "Hello"
+    },
+    {
+        "role": "assistant",
+        "content": "Hello! How can I assist you today?"
+    },
+    {
+        "role": "user",
+        "content": "Write an essay about the importance of higher education.",
+    },
+]
+conversations = [conversation for _ in range(10)]
 
 # We turn on tqdm progress bar to verify it's indeed running batch inference
-# outputs = llm.chat(messages=conversations,
-#                    sampling_params=sampling_params,
-#                    use_tqdm=True)
-# print_outputs(outputs)
+outputs = llm.chat(messages=conversations,
+                   sampling_params=sampling_params,
+                   use_tqdm=True)
+print_outputs(outputs)
 
 # A chat template can be optionally supplied.
 # If not, the model will use its default chat template.
