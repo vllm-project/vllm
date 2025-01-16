05d6595942811246e5d22b396c67feddbb5295a4
youkaichao <youkaichao@gmail.com>
2025-01-16 10:25:33 +0800

    tutorial style
    Signed-off-by: youkaichao <youkaichao@gmail.com>

--- examples/offline_inference/torchrun_example.py
+++ examples/offline_inference/torchrun_example.py
@@ -1,19 +1,13 @@
+# experimental example for tensor-parallel inference with torchrun,
 # see https://github.com/vllm-project/vllm/issues/11400 for
 # the motivation and use case for this example.
-# this is a specialized implementation of tensor-parallel inference.
 # run the script with `torchrun --nproc-per-node=2 torchrun_example.py`,
 # the argument 2 should match the `tensor_parallel_size` below.
-
-import torch.distributed as dist
+# see `tests/distributed/test_torchrun_example.py` for the unit test.
 
 from vllm import LLM, SamplingParams
-from vllm.distributed.parallel_state import get_world_group
 
-dist.init_process_group(backend="nccl")
-
-torch_rank = dist.get_rank()
-
-# Create prompts
+# Create prompts, the same across all ranks
 prompts = [
     "Hello, my name is",
     "The president of the United States is",
@@ -21,51 +15,21 @@ prompts = [
     "The future of AI is",
 ]
 
+# Create sampling parameters, the same across all ranks
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
-# important: use `distributed_executor_backend="external_launcher"` so that
+# Use `distributed_executor_backend="external_launcher"` so that
 # this llm engine/instance only creates one worker.
-# set different `gpu_memory_utilization` and `swap_space` for different ranks,
-# to test if all ranks agree on the same kv cache configuration.
 llm = LLM(model="facebook/opt-125m",
           tensor_parallel_size=2,
           distributed_executor_backend="external_launcher",
-          gpu_memory_utilization=0.9 if torch_rank == 0 else 0.7,
-          swap_space=3 if torch_rank == 0 else 4)
+        )
 
-# important: prompts should be the same across all ranks
-# important: scheduling decisions should be deterministic
-# and should be the same across all ranks
 outputs = llm.generate(prompts, sampling_params)
 
-# it is recommended to use this `cpu_group` to communicate
-# control messages across all ranks, to avoid interference
-# with the model's device group communication.
-cpu_group = get_world_group().cpu_group
-
-
-def test_consistent_across_ranks(obj):
-    if torch_rank == 0:
-        dist.broadcast_object_list([obj], src=0, group=cpu_group)
-    else:
-        container = [None]
-        dist.broadcast_object_list(container, src=0, group=cpu_group)
-        assert container[0] == obj
-
-
-test_consistent_across_ranks(
-    llm.llm_engine.vllm_config.cache_config.num_cpu_blocks)
-test_consistent_across_ranks(
-    llm.llm_engine.vllm_config.cache_config.num_gpu_blocks)
-
-# all ranks should have the same outputs
+# all ranks will have the same outputs
 for output in outputs:
     prompt = output.prompt
     generated_text = output.outputs[0].text
-    test_consistent_across_ranks(prompt)
-    test_consistent_across_ranks(generated_text)
-    print(f"Rank {torch_rank}, Prompt: {prompt!r}, "
+    print(f"Prompt: {prompt!r}, "
           f"Generated text: {generated_text!r}")
-
-# all ranks can access the model directly
-# via `llm.llm_engine.model_executor.driver_worker.worker.model_runner.model`
