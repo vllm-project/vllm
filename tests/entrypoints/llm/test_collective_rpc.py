8519d2e51ff7acf3049dc57f51cb25c4a3250b7d
youkaichao <youkaichao@gmail.com>
2025-01-17 16:32:48 +0800

    simplify tests
    Signed-off-by: youkaichao <youkaichao@gmail.com>

--- tests/entrypoints/llm/test_collective_rpc.py
+++ tests/entrypoints/llm/test_collective_rpc.py
@@ -18,9 +18,8 @@ class MyWorker(Worker):
 
 @pytest.mark.parametrize("tp_size", [1, 2])
 @pytest.mark.parametrize("backend", ["mp", "ray"])
-@pytest.mark.parametrize("method", ["echo_rank", echo_rank])
 @fork_new_process_for_each_test
-def test_collective_rpc(tp_size, backend, method):
+def test_collective_rpc(tp_size, backend):
     if tp_size == 1 and backend == "ray":
         pytest.skip("Skip duplicate test case")
     if tp_size == 1:
@@ -30,4 +29,5 @@ def test_collective_rpc(tp_size, backend, method):
               tensor_parallel_size=tp_size,
               distributed_executor_backend=backend,
               worker_cls=MyWorker)
-    assert llm.collective_rpc(method) == list(range(tp_size))
+    for method in ["echo_rank", echo_rank]:
+        assert llm.collective_rpc(method) == list(range(tp_size))
