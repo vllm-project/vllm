c062da6f77023e639a9fa0ce92dfc98a2be651d4
Chen Zhang <zhangch99@outlook.com>
2025-01-17 13:33:22 +0800

    [BugFix] add more `is not None` check in VllmConfig.__post_init__ (#12138)
    Signed-off-by: Chen Zhang <zhangch99@outlook.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/config.py
+++ vllm/config.py
@@ -3174,7 +3174,8 @@ class VllmConfig:
 
         if self.compilation_config is None:
             self.compilation_config = CompilationConfig()
-        if envs.VLLM_USE_V1 and not self.model_config.enforce_eager:
+        if envs.VLLM_USE_V1 and self.model_config is not None and \
+            not self.model_config.enforce_eager:
             # NOTE(woosuk): Currently, we use inductor because the piecewise
             # CUDA graphs do not work properly with the custom CUDA kernels.
             # FIXME(woosuk): Disable inductor to reduce the compilation time
