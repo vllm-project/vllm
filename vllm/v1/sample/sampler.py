8ca382dbdad201a5a33bc3ff1f11d8ec10fa207d
Keyun Tong <tongkeyun@gmail.com>
2025-01-17 10:11:37 -0800

    remove outdated comment
    Signed-off-by: Keyun Tong <tongkeyun@gmail.com>

--- vllm/v1/sample/sampler.py
+++ vllm/v1/sample/sampler.py
@@ -50,7 +50,6 @@ class Sampler(nn.Module):
         # Use int32 to reduce the tensor size.
         sampled = sampled.to(torch.int32)
 
-        # NOTE: CPU-GPU synchronization happens here.
         sampler_output = SamplerOutput(
             sampled_token_ids=sampled,
             logprob_token_ids=topk_indices,
