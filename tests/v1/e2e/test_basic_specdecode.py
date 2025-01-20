d5ee0812820c0c1593d887999418a06c6223acf1
LiuXiaoxuanPKU <lilyliupku@gmail.com>
2025-01-20 12:09:26 -0800

    minor

--- tests/v1/e2e/test_basic_specdecode.py
+++ tests/v1/e2e/test_basic_specdecode.py
@@ -8,30 +8,25 @@ prompts = [
 sampling_params = SamplingParams(temperature=0)
 
 
-
 def test_basic_specdecode(monkeypatch):
     '''
     Compare the outputs of a original LLM and a speculative LLM
     should be the same.
     '''
-    prompts = [ "The future of AI is",
-                "This is a basic spec decode test",]
     with monkeypatch.context() as m:
         m.setenv("VLLM_USE_V1", "1")
         model = "meta-llama/Meta-Llama-3-8B-Instruct"
-        
+
         ref_llm = LLM(model=model)
         ref_outputs = ref_llm.generate(prompts, sampling_params)
         del ref_llm
         # print(ref_outputs.outputs[0].text)
 
-        spec_llm = LLM(
-            model=model,
-            speculative_model='[ngram]',
-            ngram_prompt_lookup_max=5,
-            ngram_prompt_lookup_min=3,
-            num_speculative_tokens=3
-        )
+        spec_llm = LLM(model=model,
+                       speculative_model='[ngram]',
+                       ngram_prompt_lookup_max=5,
+                       ngram_prompt_lookup_min=3,
+                       num_speculative_tokens=3)
         spec_outputs = spec_llm.generate(prompts, sampling_params)
         for ref_output, spec_output in zip(ref_outputs, spec_outputs):
             assert ref_output.outputs[0].text == spec_output.outputs[0].text, \
