c885a40fe8950354d078b67b7c25441180852fbd
Chen Zhang <zhangch99@outlook.com>
2025-01-20 07:13:07 -0800

    fix_pre_commit
    Signed-off-by: Chen Zhang <zhangch99@outlook.com>

--- tools/mypy.sh
+++ tools/mypy.sh
@@ -7,6 +7,10 @@ if [ "$CI" -eq 1 ]; then
     set -e
 fi
 
+if [ $PYTHON_VERSION == "local" ]; then
+    PYTHON_VERSION=$(python -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')
+fi
+
 run_mypy() {
     echo "Running mypy on $1"
     if [ "$CI" -eq 1 ] && [ -z "$1" ]; then
