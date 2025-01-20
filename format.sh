8bd9ecae10a42cd8515dbb16ae0b70cc0a38b8ef
youkaichao <youkaichao@gmail.com>
2025-01-20 15:57:23 +0800

    add placeholder format.sh
    Signed-off-by: youkaichao <youkaichao@gmail.com>

---
+++ format.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "vLLM linting system has been moved from format.sh to pre-commit hook."
+echo "Please run 'pip install -r requirements-lint.txt' and 'pre-commit install' to install the pre-commit hook."
+echo "Then linters will run automatically before each commit."
