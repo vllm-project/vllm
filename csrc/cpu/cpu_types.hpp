764e790caa0d1dc9ea897cfda764b77602c1002a
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-01-20 06:58:01 +0000

    Move linting to `pre-commit` (#11975)
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- csrc/cpu/cpu_types.hpp
+++ csrc/cpu/cpu_types.hpp
@@ -2,13 +2,13 @@
 #define CPU_TYPES_HPP
 
 #if defined(__x86_64__)
-  //x86 implementation
+  // x86 implementation
   #include "cpu_types_x86.hpp"
 #elif defined(__POWER9_VECTOR__)
-  //ppc implementation
+  // ppc implementation
   #include "cpu_types_vsx.hpp"
 #elif defined(__aarch64__)
-  //arm implementation
+  // arm implementation
   #include "cpu_types_arm.hpp"
 #else
   #warning "unsupported vLLM cpu implementation"
