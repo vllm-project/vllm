f0af1d9c2f0143ab28b26bbbca2815f46930d595
Lu Fang <lufang@fb.com>
2025-01-20 14:44:32 -0800

    Move find_loaded_library to platform_aware_utils.py
    Signed-off-by: Lu Fang <lufang@fb.com>

---
+++ vllm/platform_aware_utils.py
@@ -0,0 +1,31 @@
+"""This file contains all the platform related utility functions.
+Please ensure we only add new functions to this file when necessary.
+"""
+from typing import Optional
+import subprocess
+
+
+def find_loaded_library(lib_name) -> Optional[str]:
+    """
+    According to according to https://man7.org/linux/man-pages/man5/proc_pid_maps.5.html,
+    the file `/proc/self/maps` contains the memory maps of the process, which includes the
+    shared libraries loaded by the process. We can use this file to find the path of the
+    a loaded library.
+    """ # noqa
+    found = False
+    with open("/proc/self/maps") as f:
+        for line in f:
+            if lib_name in line:
+                found = True
+                break
+    if not found:
+        # the library is not loaded in the current process
+        return None
+    # if lib_name is libcudart, we need to match a line with:
+    # address /path/to/libcudart-hash.so.11.0
+    start = line.index("/")
+    path = line[start:].strip()
+    filename = path.split("/")[-1]
+    assert filename.rpartition(".so")[0].startswith(lib_name), \
+        f"Unexpected filename: {filename} for library {lib_name}"
+    return path
