3105f0c4d47633a7c19831c8d6b2406354274b7c
Thomas Parnell <tpa@zurich.ibm.com>
2025-01-21 09:56:03 +0100

    use_existing_torch.py: filter out comments
    Signed-off-by: Thomas Parnell <tpa@zurich.ibm.com>

--- use_existing_torch.py
+++ use_existing_torch.py
@@ -6,6 +6,8 @@ for file in requires_files:
     print(f">>> cleaning {file}")
     with open(file) as f:
         lines = f.readlines()
+    # filter out comments
+    lines = [ line.split(" #")[0] for line in lines ]
     if "torch" in "".join(lines).lower():
         print("removed:")
         with open(file, 'w') as f:
