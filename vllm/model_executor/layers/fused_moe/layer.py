dae7f3235979826498c93514a7c0b2b7ae5c2758
charlifu <charlifu@amd.com>
2025-01-22 04:58:52 +0000

    replace fp moe kernel with aiter kernel

--- vllm/model_executor/layers/fused_moe/layer.py
+++ vllm/model_executor/layers/fused_moe/layer.py
@@ -17,6 +17,9 @@ from vllm.model_executor.utils import set_weight_attrs
 from vllm.platforms import current_platform
 from vllm.platforms.interface import CpuArchEnum
 
+from aiter.fused_moe_bf16_asm import asm_moe
+from aiter.ops.shuffle import shuffle_weight
+
 if current_platform.is_cuda_alike():
     from .fused_moe import fused_experts
 else:
@@ -164,12 +167,18 @@ class UnquantizedFusedMoEMethod(FusedMoEMethodBase, CustomOp):
             scoring_func=scoring_func,
             e_score_correction_bias=e_score_correction_bias)
 
-        return fused_experts(hidden_states=x,
-                             w1=layer.w13_weight,
-                             w2=layer.w2_weight,
-                             topk_weights=topk_weights,
-                             topk_ids=topk_ids,
-                             inplace=True)
+        # return fused_experts(hidden_states=x,
+        #                      w1=layer.w13_weight,
+        #                      w2=layer.w2_weight,
+        #                      topk_weights=topk_weights,
+        #                      topk_ids=topk_ids,
+        #                      inplace=True)
+        
+        return asm_moe(hidden_states=x,
+                         w1=layer.w13_weight,
+                         w2=layer.w2_weight,
+                         topk_weight=topk_weights,
+                         topk_ids=topk_ids)
 
     def forward_cpu(
         self,
