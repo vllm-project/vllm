900c257640c59a6004564c9edfa5f2945c4618ca
youkaichao <youkaichao@gmail.com>
2025-01-22 14:20:30 +0800

    use found_line
    Signed-off-by: youkaichao <youkaichao@gmail.com>

--- vllm/device_allocator/cumem.py
+++ vllm/device_allocator/cumem.py
@@ -21,19 +21,19 @@ def find_loaded_library(lib_name) -> Optional[str]:
     shared libraries loaded by the process. We can use this file to find the path of the
     a loaded library.
     """ # noqa
-    found = False
+    found_line = None
     with open("/proc/self/maps") as f:
         for line in f:
             if lib_name in line:
-                found = True
+                found_line = line
                 break
-    if not found:
+    if found_line is None:
         # the library is not loaded in the current process
         return None
     # if lib_name is libcudart, we need to match a line with:
     # address /path/to/libcudart-hash.so.11.0
-    start = line.index("/")
-    path = line[start:].strip()
+    start = found_line.index("/")
+    path = found_line[start:].strip()
     filename = path.split("/")[-1]
     assert filename.rpartition(".so")[0].startswith(lib_name), \
         f"Unexpected filename: {filename} for library {lib_name}"
