ba56387a9bdec32ccb96b52c8e16c343ecbf710a
Shangming Cai <caishangming@linux.alibaba.com>
2025-01-22 15:16:13 +0800

    fix
    Signed-off-by: Shangming Cai <caishangming@linux.alibaba.com>

--- vllm/spec_decode/smaller_tp_proposer_worker.py
+++ vllm/spec_decode/smaller_tp_proposer_worker.py
@@ -180,7 +180,7 @@ class SmallerTpProposerWorker(ProposerWorkerBase):
         lm_head_weight: torch.Tensor,
     ) -> None:
         if self._is_dummy:
-            return []
+            return
 
         with self._patch_tensor_parallel_group():
             weight_loader = getattr(
