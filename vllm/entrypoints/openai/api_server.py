e59841b8c62f08efa96d7045d8c3bc638b11a795
Varun Sundar Rabindranath <vsundarr@redhat.com>
2025-01-23 14:06:50 -0500

    whisper-async working poc

--- vllm/entrypoints/openai/api_server.py
+++ vllm/entrypoints/openai/api_server.py
@@ -14,10 +14,10 @@ from argparse import Namespace
 from contextlib import asynccontextmanager
 from functools import partial
 from http import HTTPStatus
-from typing import AsyncIterator, Dict, Optional, Set, Tuple, Union
+from typing import AsyncIterator, Dict, Optional, Set, Tuple, Union, List, Annotated
 
 import uvloop
-from fastapi import APIRouter, FastAPI, HTTPException, Request
+from fastapi import APIRouter, FastAPI, HTTPException, Request, Form
 from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse, Response, StreamingResponse
@@ -58,7 +58,11 @@ from vllm.entrypoints.openai.protocol import (ChatCompletionRequest,
                                               ScoreRequest, ScoreResponse,
                                               TokenizeRequest,
                                               TokenizeResponse,
-                                              UnloadLoraAdapterRequest)
+                                              UnloadLoraAdapterRequest,
+                                              TranscriptionRequest,
+                                              TranscriptionResponse,
+                                              AudioResponseFormat,
+                                              )
 # yapf: enable
 from vllm.entrypoints.openai.serving_chat import OpenAIServingChat
 from vllm.entrypoints.openai.serving_completion import OpenAIServingCompletion
@@ -70,6 +74,7 @@ from vllm.entrypoints.openai.serving_pooling import OpenAIServingPooling
 from vllm.entrypoints.openai.serving_score import OpenAIServingScores
 from vllm.entrypoints.openai.serving_tokenization import (
     OpenAIServingTokenization)
+from vllm.entrypoints.openai.serving_transcription import OpenAIServingTranscription
 from vllm.entrypoints.openai.tool_parsers import ToolParserManager
 from vllm.entrypoints.utils import with_cancellation
 from vllm.logger import init_logger
@@ -303,6 +308,9 @@ def score(request: Request) -> Optional[OpenAIServingScores]:
 def tokenization(request: Request) -> OpenAIServingTokenization:
     return request.app.state.openai_serving_tokenization
 
+def transcription(request: Request) -> OpenAIServingTranscription:
+    return request.app.state.openai_serving_transcription
+
 
 def engine_client(request: Request) -> EngineClient:
     return request.app.state.engine_client
@@ -495,6 +503,29 @@ async def create_score_v1(request: ScoreRequest, raw_request: Request):
 
     return await create_score(request, raw_request)
 
+@router.post("/v1/audio/transcriptions")
+@with_cancellation
+async def create_transcriptions(request: Annotated[TranscriptionRequest, Form()],
+                                raw_request: Request):
+
+    audio_data = await request.file.read()
+
+    handler = transcription(raw_request)
+    if handler is None:
+        return base(raw_request).create_error_response(
+            message="The model does not support Transcriptions API")
+
+    generator = await handler.create_transcription(audio_data, request, raw_request)
+
+    if isinstance(generator, ErrorResponse):
+        return JSONResponse(content=generator.model_dump(),
+                            status_code=generator.code)
+
+    elif isinstance(generator, TranscriptionResponse):
+        return JSONResponse(content=generator.model_dump())
+
+    return StreamingResponse(content=generator, media_type="text/event-stream")
+
 
 TASK_HANDLERS: Dict[str, Dict[str, tuple]] = {
     "generate": {
@@ -682,6 +713,7 @@ async def init_app_state(
     state: State,
     args: Namespace,
 ) -> None:
+
     if args.served_model_name is not None:
         served_model_names = args.served_model_name
     else:
@@ -760,6 +792,12 @@ async def init_app_state(
         request_logger=request_logger,
         chat_template=resolved_chat_template,
         chat_template_content_format=args.chat_template_content_format,
+    )
+    state.openai_serving_transcription = OpenAIServingTranscription(
+        engine_client,
+        model_config,
+        state.openai_serving_models,
+        request_logger=request_logger,
     )
     state.task = model_config.task
 
---
+++ vllm/entrypoints/openai/serving_transcription.py
@@ -0,0 +1,137 @@
+import asyncio
+import io
+#import time
+from typing import Any, AsyncGenerator, Dict, Optional, Union
+
+## TODO (varun) : This is used for testing.. use pydub instead ?????
+import librosa
+from fastapi import Request
+
+from vllm.config import ModelConfig
+from vllm.engine.protocol import EngineClient
+from vllm.entrypoints.logger import RequestLogger
+from vllm.entrypoints.openai.protocol import (RequestResponseMetadata,
+                                              TranscriptionRequest,
+                                              TranscriptionResponse,
+                                              TranscriptionResponseVerbose)
+from vllm.entrypoints.openai.serving_engine import OpenAIServing
+from vllm.entrypoints.openai.serving_models import OpenAIServingModels
+from vllm.logger import init_logger
+from vllm.outputs import RequestOutput
+
+logger = init_logger(__name__)
+
+
+class OpenAIServingTranscription(OpenAIServing):
+
+    def __init__(
+        self,
+        engine_client: EngineClient,
+        model_config: ModelConfig,
+        models: OpenAIServingModels,
+        *,
+        request_logger: Optional[RequestLogger],
+        return_tokens_as_token_ids: bool = False,
+    ):
+        super().__init__(engine_client=engine_client,
+                         model_config=model_config,
+                         models=models,
+                         request_logger=request_logger,
+                         return_tokens_as_token_ids=return_tokens_as_token_ids)
+
+        diff_sampling_param = self.model_config.get_diff_sampling_param()
+        if diff_sampling_param:
+            logger.info(
+                "Overwriting default completion sampling param with: %s",
+                diff_sampling_param)
+
+    # TODO (varun) : pass in a tokenizer and return tokenized values !!
+    async def _preprocess_transcription(
+            self, audio_data: bytes,
+            request: TranscriptionRequest) -> Dict[Any, Any]:
+        return {
+            "encoder_prompt": {
+                "prompt": "",
+                "multi_modal_data": {
+                    "audio": librosa.load(io.BytesIO(audio_data)),
+                },
+            },
+            # TODO (Varun) : Should this instead be encoder prompt ???
+            "decoder_prompt": f"{request.prompt}",
+        }
+
+    # TODO (varun) : Make verbose response work !
+    async def create_transcription(
+        self, audio_data: bytes, request: TranscriptionRequest,
+        raw_request: Request
+    ) -> Union[TranscriptionResponse, TranscriptionResponseVerbose]:
+        """Completion API similar to OpenAI's API.
+
+        See https://platform.openai.com/docs/api-reference/audio/createTranscription
+        for the API specification. This API mimics the OpenAI completion API.
+        """
+
+        assert request.response_format in ['text', 'json']
+
+        error_check_ret = await self._check_model(request)
+        if error_check_ret is not None:
+            return error_check_ret
+
+        # If the engine is dead, raise the engine's DEAD_ERROR.
+        # This is required for the streaming case, where we return a
+        # success status before we actually start generating text :).
+        if self.engine_client.errored:
+            raise self.engine_client.dead_error
+
+        request_id = f"cmpl-{self._base_request_id(raw_request)}"
+        # TODO (varun) : other serving_* files use this -- we should use
+        # it as well.
+        #created_time = int(time.time())
+
+        request_metadata = RequestResponseMetadata(request_id=request_id)
+        if raw_request:
+            raw_request.state.request_metadata = request_metadata
+
+        # TODO (varun) : Does Whisper have LoRA ?
+        #tokenizer = await self.engine_client.get_tokenizer(None)
+
+        prompt = await self._preprocess_transcription(audio_data, request)
+
+        default_sampling_params = (self.model_config.get_diff_sampling_param())
+
+        # TODO (Varun) : figure out default_max_tokens by tokenizing first
+        default_max_tokens = 200
+        sampling_params = request.to_sampling_params(default_max_tokens,
+                                                     default_sampling_params)
+
+        self._log_inputs(
+            request_id,
+            prompt['decoder_prompt'],
+            params=sampling_params,
+            lora_request=None,
+            prompt_adapter_request=None,
+        )
+
+        generator: AsyncGenerator[RequestOutput, None] = None
+        try:
+            generator = self.engine_client.generate(
+                prompt,
+                sampling_params,
+                request_id,
+            )
+        except ValueError as e:
+            # TODO: Use a vllm-specific Validation Error
+            return self.create_error_response(str(e))
+
+        # Non-streaming response
+        result: Optional[RequestOutput] = None
+
+        try:
+            async for op in generator:
+                result = op
+            return TranscriptionResponse(text=result.outputs[0].text)
+        except asyncio.CancelledError:
+            return self.create_error_response("Client disconnected")
+        except ValueError as e:
+            # TODO: Use a vllm-specific Validation Error
+            return self.create_error_response(str(e))
