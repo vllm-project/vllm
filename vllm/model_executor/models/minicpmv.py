c15228b88bb803ed906ecc8c3f66951c9e71d722
hzh <hezhihui_thu@163.com>
2025-01-23 09:05:31 +0000

    format

--- vllm/model_executor/models/minicpmv.py
+++ vllm/model_executor/models/minicpmv.py
@@ -19,7 +19,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Inference-only MiniCPM-3-O model compatible with HuggingFace weights."""
+"""Inference-only MiniCPM-V model compatible with HuggingFace weights."""
 import math
 import re
 from functools import cached_property, partial
