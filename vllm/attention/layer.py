e8676c89130edc6c3ddb6a33d3ccb0144edfca6a
Isotr0py <2037008807@qq.com>
2025-01-24 22:46:32 +0800

    add mha kernel test
    Signed-off-by: Isotr0py <2037008807@qq.com>

--- vllm/attention/layer.py
+++ vllm/attention/layer.py
@@ -186,6 +186,9 @@ class MultiHeadAttention(nn.Module):
         self.scale = scale
         self.num_kv_heads = num_heads if num_kv_heads is None else num_kv_heads
 
+        assert self.num_heads % self.num_kv_heads == 0
+        self.num_queries_per_kv = self.num_heads // self.num_kv_heads
+
         dtype = torch.get_default_dtype()
         attn_backend = get_attn_backend(head_size,
                                         dtype,
@@ -214,6 +217,11 @@ class MultiHeadAttention(nn.Module):
         query = query.view(bsz, q_len, self.num_heads, self.head_size)
         key = key.view(bsz, kv_len, self.num_kv_heads, self.head_size)
         value = value.view(bsz, kv_len, self.num_kv_heads, self.head_size)
+
+        if (num_repeat := self.num_queries_per_kv) > 1:
+            # Handle MQA and GQA
+            key = torch.repeat_interleave(key, num_repeat, dim=2)
+            value = torch.repeat_interleave(value, num_repeat, dim=2)
 
         if self.attn_backend in {
                 _Backend.FLASH_ATTN,
