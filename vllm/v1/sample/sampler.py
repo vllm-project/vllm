naive prompt logprobs fix doesn't work
Signed-off-by: Andrew Feldman <afeldman@neuralmagic.com>

vllm/v1/sample/sampler.py
@@ -140,7 +140,7 @@ class Sampler(nn.Module):
         topk_indices = topk_indices.to(torch.int32)
 
         # Concatenate with the token_ids
-        token_ids = token_ids.unsqueeze(-1)
+        token_ids = token_ids.unsqueeze(-1).to(logprobs.device)
         sampled_logprobs = logprobs.gather(-1, token_ids.to(torch.int64))
         topk_indices = torch.cat((token_ids, topk_indices), dim=1)
         topk_logprobs = torch.cat((sampled_logprobs, topk_logprobs), dim=1)