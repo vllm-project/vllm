635c79878e5c0dcea4d98500e209572cdbc109ce
Sage Moore <sage@neuralmagic.com>
2025-01-27 21:22:54 +0000

    format
    Signed-off-by: Sage Moore <sage@neuralmagic.com>

--- csrc/core/math.hpp
+++ csrc/core/math.hpp
@@ -16,15 +16,13 @@ static inline constexpr auto div_ceil(A a, B b) {
 // Round a down to the next multiple of b. The caller is responsible for making
 // sure that b is non-zero
 template <typename T>
-inline constexpr T round_to_previous_multiple_of(T a, T b)
-{
-    return a % b == 0 ? a : (a / b) * b;
+inline constexpr T round_to_previous_multiple_of(T a, T b) {
+  return a % b == 0 ? a : (a / b) * b;
 }
 
 // Round a up to the next multiple of b. The caller is responsible for making
 // sure that b is non-zero
 template <typename T>
-inline constexpr T round_to_next_multiple_of(T a, T b)
-{
-    return a % b == 0 ? a : ((a / b) + 1) * b;
+inline constexpr T round_to_next_multiple_of(T a, T b) {
+  return a % b == 0 ? a : ((a / b) + 1) * b;
 }
