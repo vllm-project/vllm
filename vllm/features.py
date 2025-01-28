ddc6ebae9461512e43b39d99d959a5d3d5a3a4e8
Russell Bryant <rbryant@redhat.com>
2025-01-28 10:29:28 -0500

    Add instructions for edits to vllm.features
    Signed-off-by: Russell Bryant <rbryant@redhat.com>

--- vllm/features.py
+++ vllm/features.py
@@ -1,5 +1,27 @@
 from typing import Dict, Optional
 
+#
+# Instructions:
+#
+# To add a new feature:
+#
+# 1) Reserve a bit by defining a new constant, FEATURE_<NAME>, with a value of
+#    1 << n, where n is the next available bit in the range 0-63.
+#
+# 2) Add a string representation of the feature to FEATURE_STR.
+#
+# 3) Add the feature to INCOMPATIBILITY_MATRIX. The key should be the feature
+#    and the value should be a bit mask of all the features that are
+#    incompatible with it. The feature should be added to the value of any other
+#    feature that it is incompatible with, as well.
+#
+# To change the compatibility matrix for existing features,
+# update INCOMPATIBILITY_MATRIX.
+#
+# This should match what in the feature compatibility matrix in the docs:
+# https://docs.vllm.ai/en/latest/features/compatibility_matrix.html
+#
+
 # This range must not go above 63 as they represent the bits in the feature
 # usage bit field. The implementation will have to change if we need to
 # calculate compatibility between more than 64 features.
