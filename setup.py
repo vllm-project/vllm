acb3dc79e99704e7d8e0e1109837ded296a47808
Luka Govedič <lgovedic@redhat.com>
2025-01-29 23:26:03 +0000

    Add vllm-flash-attn build from source on AMD

--- setup.py
+++ setup.py
@@ -596,9 +596,9 @@ if _is_cuda() or _is_hip():
 if _is_hip():
     ext_modules.append(CMakeExtension(name="vllm._rocm_C"))
 
-if _is_cuda():
+if _is_cuda() or _is_hip():
     ext_modules.append(CMakeExtension(name="vllm.vllm_flash_attn._vllm_fa2_C"))
-    if envs.VLLM_USE_PRECOMPILED or get_nvcc_cuda_version() >= Version("12.0"):
+    if _is_cuda() and (envs.VLLM_USE_PRECOMPILED or get_nvcc_cuda_version() >= Version("12.0")):
         # FA3 requires CUDA 12.0 or later
         ext_modules.append(
             CMakeExtension(name="vllm.vllm_flash_attn._vllm_fa3_C"))
