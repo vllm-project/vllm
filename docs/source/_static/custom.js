27aa21055fadd117cda2b5d0caf372d7308c367c
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-01-29 18:31:22 +0000

    Set `?device={device}` when changing tab in installation guides
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- docs/source/_static/custom.js
+++ docs/source/_static/custom.js
@@ -16,3 +16,21 @@ document.addEventListener("DOMContentLoaded", function () {
     script.async = true;
     document.head.appendChild(script);
   });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const tabs = document.querySelectorAll(".sd-tab-label");
+
+    function updateURL(tab) {
+      const syncGroup = tab.getAttribute("data-sync-group");
+      const syncId = tab.getAttribute("data-sync-id");
+      if (syncGroup && syncId) {
+          const url = new URL(window.location);
+          url.searchParams.set(syncGroup, syncId);
+          window.history.replaceState(null, "", url);
+      }
+    }
+
+    tabs.forEach(tab => {
+        tab.addEventListener("click", () => updateURL(tab));
+    });
+});
