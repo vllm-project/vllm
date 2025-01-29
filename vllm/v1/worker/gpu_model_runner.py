3efb2df87d1f87fc71807fd4324ea7a29796c2d1
Andrew Feldman <afeldman@neuralmagic.com>
2025-01-29 17:15:16 +0000

    more encapsulated prompt logprobs approach
    Signed-off-by: Andrew Feldman <afeldman@neuralmagic.com>

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -824,37 +824,41 @@ class GPUModelRunner:
         for (request_id, num_prompt_logprobs
              ) in self.input_batch.num_prompt_logprobs.items():
 
-            # Compute the positions of the prompt tokens
+            request = self.requests[request_id]
+            req_idx = self.input_batch.req_id_to_index[request_id]
+            prompt_token_ids = torch.tensor(request.prompt_token_ids)
+            is_partial_req = request_id == scheduler_output.partial_req_id
+
+            # Get number of prompt logits generated.
             num_scheduled_tokens = scheduler_output.num_scheduled_tokens[
                 request_id]
-            is_partial_req = request_id == scheduler_output.partial_req_id
-            req_idx = self.input_batch.req_id_to_index[request_id]
-            # Index of this request's first scheduled prompt token within
-            # model input token vector
-            start_idx_in_inp_ids = self.query_start_loc_np[req_idx].item()
-            # Unless request is partial,
-            num_scheduled_prompt_tokens = (
-                num_scheduled_tokens if is_partial_req
-                # Exclude the sample token if there is one.
-                else num_scheduled_tokens - 1)
-            end_idx_in_inp_ids = (start_idx_in_inp_ids +
-                                  num_scheduled_prompt_tokens)
-            prompt_hidden_states = (
-                hidden_states[start_idx_in_inp_ids:end_idx_in_inp_ids])
+            num_prompt_logits = (num_scheduled_tokens if is_partial_req else
+                                 num_scheduled_tokens - 1)
+
+            # Get start and end indices of the hidden states
+            # corresponding to prompt logits
+            start_idx_in_hidden_states = self.query_start_loc_np[req_idx].item(
+            )
+            end_idx_in_hidden_states = (start_idx_in_hidden_states +
+                                        num_prompt_logits)
+
+            # Compute prompt logits from prompt hidden states
+            prompt_hidden_states = (hidden_states[
+                start_idx_in_hidden_states:end_idx_in_hidden_states])
             logits = self.model.compute_logits(prompt_hidden_states, None)
-            # - Offset `prompt_indices` by 1 because (in general) the logprob
-            #   distribution at sequence position i is predicting position i+1.
-            #   Note: for partial prefill chunks, `end_idx_in_inp_idx + 1`
-            #   points to the "peek token" index i.e. the first token of the
-            #   next chunk, which is what is predicted by the prompt logprobs
-            #   of the last token in the current chunk.
-            chunk_prompt_token_ids = self.input_ids[start_idx_in_inp_ids +
-                                                    1:end_idx_in_inp_ids + 1]
+
+            # Get start and end indices of the prompt token ids
+            # corresponding to the prompt logits
+            start_idx_in_tokens = request.num_computed_tokens + 1
+            end_idx_in_tokens = start_idx_in_tokens + num_prompt_logits
+
             # Compute prompt logprobs.
             prompt_logprobs_dict[request_id] = self.model.sampler.get_logprobs(
                 logits,
                 num_prompt_logprobs,
-                token_ids=chunk_prompt_token_ids  #type: ignore
+                token_ids=prompt_token_ids[
+                    start_idx_in_tokens:end_idx_in_tokens].to(
+                        self.device)  # copy to gpu
             )
 
         # TODO(woosuk): The following loop can be slow since it iterates over
