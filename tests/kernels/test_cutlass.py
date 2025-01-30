0d67e859510e405c6647eeb14d76f835361726a4
Lucas Wilkinson <lwilkinson@neuralmagic.com>
2025-01-30 17:04:12 +0000

    fix pre-commit
    Signed-off-by: Lucas Wilkinson <lwilkinson@neuralmagic.com>

--- tests/kernels/test_cutlass.py
+++ tests/kernels/test_cutlass.py
@@ -2,7 +2,7 @@
 
 Run `pytest tests/kernels/test_cutlass.py`.
 """
-from typing import Optional, Type
+from typing import Type
 
 import pytest
 import torch
@@ -51,48 +51,6 @@ capability = capability[0] * 10 + capability[1]
 
 def rand_int8(shape: tuple, device: str = "cuda"):
     return to_int8(torch.rand(shape, device=device) * 255 - 128)
-
-
-def baseline_scaled_mm(a: torch.Tensor,
-                       b: torch.Tensor,
-                       scale_a: torch.Tensor,
-                       scale_b: torch.Tensor,
-                       out_dtype: Type[torch.dtype],
-                       bias: Optional[torch.Tensor] = None) -> torch.Tensor:
-
-    # We treat N-dimensional group scaling as extended numpy-style broadcasting
-    # in numpy simply stretches dimensions with an extent of 1 to match the
-    # the target shape by repeating the data along that dimension (broadcasting)
-    # , we extend these semantics to say if the extent of a dimension in the
-    # source shape is not 1 and does not match the target shape we repeat each
-    # element along that dimension src_shape[dim] // target_shape[dim] times
-    # example if we have:
-    #       a = [[1, 2], and target_shape = (2, 4)
-    #            [3, 4]]
-    # then we would expand a to:
-    #       a = [[1, 1, 2, 2],
-    #            [3, 3, 4, 4]]
-    # NOTE this function this function does not explicitly broadcast dimensions
-    # with an extent of 1, since this can be done implicitly by pytorch
-    def group_broadcast(t, shape):
-        for i, s in enumerate(shape):
-            if t.shape[i] != s and t.shape[i] != 1:
-                assert s % t.shape[i] == 0
-                t = t.unsqueeze(i + 1)\
-                  .expand(*t.shape[:i+1], s // t.shape[i], *t.shape[i+1:])\
-                  .flatten(i, i + 1)
-        return t
-
-    scale_a = group_broadcast(scale_a, a.shape)
-    scale_b = group_broadcast(scale_b, b.shape)
-
-    output = torch.mm((scale_a * a.to(dtype=torch.float32)),
-                      (scale_b * b.to(dtype=torch.float32))).to(out_dtype)
-
-    if bias is not None:
-        output = output + bias
-
-    return output
 
 
 def group_scale_helper(shape, group_shape):
