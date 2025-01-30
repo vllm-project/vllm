2624faf46ec51d5d79c57c6cee6b9f0528908c9b
simon-mo <xmo@berkeley.edu>
2025-01-29 20:14:58 -0800

    stash

--- vllm/model_executor/layers/quantization/utils/fp8_utils.py
+++ vllm/model_executor/layers/quantization/utils/fp8_utils.py
@@ -1,17 +1,4 @@
-# Copyright 2024 SGLang Team
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
+# Adapted from https://github.com/sgl-project/sglang/pull/2575
 import functools
 import json
 import os
