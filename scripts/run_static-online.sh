655a50002a6bd4f138ee46cde94c3a2dffb93531
Chendi.Xue <chendi.xue@intel.com>
2025-01-30 21:17:42 -0600

    Deepseek r1 (#765)
    1. move block_fp8 pad to load_weight
    2. move moe fp8 linear out of loop
    3. remove permute and reshape
    
    ---------
    
    Signed-off-by: Chendi Xue <chendi.xue@intel.com>
    Co-authored-by: root <root@g3lc-srv32-c03d-idc.idc9.habana-labs.com>

--- scripts/run_static-online.sh
+++ scripts/run_static-online.sh
@@ -6,8 +6,10 @@ out_len=1024
 multi_step=1
 total_len=$((in_len + out_len))
 VLLM_DECODE_BLOCK_BUCKET_MIN=$((in_len * bs / 128))
-VLLM_DECODE_BLOCK_BUCKET_MAX=$((total_len * bs / 128))
+VLLM_DECODE_BLOCK_BUCKET_MAX=$((total_len * bs / 128 + 128))
 
+#model="/data/models/DeepSeek-R1/"
+#tokenizer="/data/models/DeepSeek-R1/"
 model="/software/data/DeepSeek-R1/"
 tokenizer="/software/data/DeepSeek-R1/"
 model_name="DeepSeek-R1"
@@ -19,7 +21,7 @@ RAY_IGNORE_UNHANDLED_ERRORS="1" \
 VLLM_PROMPT_BS_BUCKET_MIN=1 \
 VLLM_PROMPT_BS_BUCKET_MAX=${bs} \
 VLLM_PROMPT_SEQ_BUCKET_MIN=${in_len} \
-VLLM_PROMPT_SEQ_BUCKET_MAX=${in_len} \
+VLLM_PROMPT_SEQ_BUCKET_MAX=${out_len} \
 VLLM_DECODE_BS_BUCKET_MIN=${bs} \
 VLLM_DECODE_BS_BUCKET_MAX=${bs} \
 VLLM_DECODE_BLOCK_BUCKET_MIN=${VLLM_DECODE_BLOCK_BUCKET_MIN} \
@@ -36,6 +38,7 @@ python -m vllm.entrypoints.openai.api_server \
     --max-model-len 2048 \
     --max-num-batched-tokens 2048 \
     --distributed_executor_backend ray \
+    --gpu_memory_utilization 0.9 \
     --trust_remote_code 2>&1 | tee benchmark_logs/serving.log &
 pid=$(($!-1))
 
@@ -54,7 +57,7 @@ request_rate=1
 start_time=$(date +%s)
 echo "Start to benchmark"
 python benchmarks/benchmark_serving.py --backend vllm --model ${model} --tokenizer ${tokenizer} --dataset-name sonnet --dataset-path benchmarks/sonnet.txt --request-rate ${request_rate} --num-prompts ${num_prompts} --port 8080 --sonnet-input-len ${in_len} --sonnet-output-len ${out_len} --sonnet-prefix-len 100 \
---save-result| tee benchmark_logs/static-online-gaudi3-TPparallel${tp_parrallel}-multistep${multi_step}_nprompt${num_prompts}_rrate${request_rate}_bs${bs}_i${in_len}_o${out_len}.log
+--save-result 2>&1 | tee benchmark_logs/static-online-gaudi3-0.9util-TPparallel${tp_parrallel}-multistep${multi_step}_nprompt${num_prompts}_rrate${request_rate}_bs${bs}_i${in_len}_o${out_len}_prepad.log
 end_time=$(date +%s)
 echo "Time elapsed: $((end_time - start_time))s"
 
