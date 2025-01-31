47c05d95f0a9e59fd101dcba41babf459031f1e4
Cody Yu <hao.yu.cody@gmail.com>
2025-01-31 12:30:33 -0800

    [Git] Automatically sign-off commits (#12595)
    It's very annoying when I forgot to add `-s` in `git commit` to
    sign-off, because I then need to `git rebase HEAD~1 --signoff` and `git
    push -f` to fix the DCO. This PR adds a hook to sign off commits
    automatically when `-s` is missing to solve this problem. The only
    change from the user side is now users have to install 2 hooks, so
    instead of just
    
    ```
    pre-commit install
    ```
    
    Now we need to
    
    ```
    pre-commit install --hook-type pre-commit --hook-type commit-msg
    ```
    
    Note that even if users still only install the pre-commit hook, they
    won't get any error in `git commit`. Just the sign-off hook won't run.
    
    cc @hmellor @youkaichao
    
    ---------
    
    Signed-off-by: Cody Yu <hao.yu.cody@gmail.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- format.sh
+++ format.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "vLLM linting system has been moved from format.sh to pre-commit hook."
-echo "Please run 'pip install -r requirements-lint.txt' and 'pre-commit install' to install the pre-commit hook."
+echo "Please run 'pip install -r requirements-lint.txt', followed by"
+echo "'pre-commit install --hook-type pre-commit --hook-type commit-msg' to install the pre-commit hook."
 echo "Then linters will run automatically before each commit."
