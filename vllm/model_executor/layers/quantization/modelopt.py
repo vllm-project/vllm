edf734823f289b8ab524215aabab6f2cfa4a8776
Pavani Majety <pmajety@nvidia.com>
2025-01-31 22:45:38 +0000

    NFC
    Signed-off-by: Pavani Majety <pmajety@nvidia.com>

--- vllm/model_executor/layers/quantization/modelopt.py
+++ vllm/model_executor/layers/quantization/modelopt.py
@@ -195,8 +195,7 @@ class ModelOptNvFp4Config(QuantizationConfig):
 
     @classmethod
     def get_min_capability(cls) -> int:
-        # TODO : pavanimajety: Move to 100 after blackwell is more accessible
-        return 89
+        return 100
 
     @classmethod
     def get_config_filenames(cls) -> List[str]:
