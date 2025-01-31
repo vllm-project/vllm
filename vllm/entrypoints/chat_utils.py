022387ee4cc7fda12062bc06342b61529d23f532
Roger Wang <ywang@roblox.com>
2025-01-31 00:57:01 -0800

    add to chat utils
    Co-authored-by: Yixuan Qiao <yixqiao@gmail.com>
    Signed-off-by: Roger Wang <ywang@roblox.com>

--- vllm/entrypoints/chat_utils.py
+++ vllm/entrypoints/chat_utils.py
@@ -408,7 +408,7 @@ class BaseMultiModalItemTracker(ABC, Generic[_T]):
                 return "<image>"
             if model_type == "mllama":
                 return "<|image|>"
-            if model_type == "qwen2_vl":
+            if model_type in ("qwen2_vl", "qwen_2_5_vl"):
                 return "<|vision_start|><|image_pad|><|vision_end|>"
             if model_type == "molmo":
                 return ""
@@ -428,7 +428,7 @@ class BaseMultiModalItemTracker(ABC, Generic[_T]):
                 return "(<audio>./</audio>)"
             raise TypeError(f"Unknown model type: {model_type}")
         elif modality == "video":
-            if model_type == "qwen2_vl":
+            if model_type in ("qwen2_vl", "qwen_2_5_vl"):
                 return "<|vision_start|><|video_pad|><|vision_end|>"
             if model_type in ("minicpmo", "minicpmv"):
                 return "(<video>./</video>)"
