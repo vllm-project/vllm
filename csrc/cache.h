a7e74cd1571c52dbdd111f859327b0063eefc128
Lucas Wilkinson <LucasWilkinson@users.noreply.github.com>
2025-01-31 02:49:37 -0500

    [Attention] MLA decode optimizations (#12528)
    Signed-off-by: Lucas Wilkinson <lwilkinson@neuralmagic.com>
    Signed-off-by: simon-mo <xmo@berkeley.edu>
    Co-authored-by: Woosuk Kwon <woosuk.kwon@berkeley.edu>
    Co-authored-by: simon-mo <simon.mo@hey.com>
    Co-authored-by: Michael Goin <mgoin64@gmail.com>
    Co-authored-by: Zhuohan Li <zhuohan123@gmail.com>
    Co-authored-by: Tyler Michael Smith <tysmith@redhat.com>
    Co-authored-by: Alexander Matveev <59768536+alexm-neuralmagic@users.noreply.github.com>
    Co-authored-by: simon-mo <xmo@berkeley.edu>

--- csrc/cache.h
+++ csrc/cache.h
@@ -28,6 +28,11 @@ void reshape_and_cache_flash(torch::Tensor& key, torch::Tensor& value,
                              const std::string& kv_cache_dtype,
                              torch::Tensor& k_scale, torch::Tensor& v_scale);
 
+void concat_and_cache_mla(torch::Tensor& kv_c, torch::Tensor& k_pe,
+                          torch::Tensor& kv_cache, torch::Tensor& slot_mapping,
+                          const std::string& kv_cache_dtype,
+                          torch::Tensor& scale);
+
 // Just for unittest
 void convert_fp8(torch::Tensor& dst_cache, torch::Tensor& src_cache,
                  const double scale, const std::string& kv_cache_dtype);
