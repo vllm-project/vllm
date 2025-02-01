bb942601c595f2908e87054fba60c93712ff46d4
Lucas Wilkinson <LucasWilkinson@users.noreply.github.com>
2025-02-01 00:52:51 -0500

    [Attention] Deepseek v3 MLA support with FP8 compute (#12601)
    This PR implements the Deepseek V3 support by performing matrix absorption the fp8 weights
    
    ---------
    
    Signed-off-by: Lucas Wilkinson <lwilkinson@neuralmagic.com>
    Co-authored-by: Woosuk Kwon <woosuk.kwon@berkeley.edu>
    Co-authored-by: simon-mo <simon.mo@hey.com>
    Co-authored-by: Michael Goin <mgoin64@gmail.com>
    Co-authored-by: Zhuohan Li <zhuohan123@gmail.com>
    Co-authored-by: Tyler Michael Smith <tysmith@redhat.com>
    Co-authored-by: Alexander Matveev <59768536+alexm-neuralmagic@users.noreply.github.com>
    Signed-off-by: Srikanth Srinivas <srikanth@astrum.ai>

--- vllm/attention/backends/triton_mla.py
+++ vllm/attention/backends/triton_mla.py
@@ -57,14 +57,12 @@ class TritonMLABackend(AttentionBackend):
 
     @staticmethod
     def get_kv_cache_shape(
-            num_blocks: int,
-            block_size: int,
-            num_kv_heads: int,  # assumed to be 1 for MLA
-            kv_lora_rank: int,  # passed via head_size
+        num_blocks: int,
+        block_size: int,
+        num_kv_heads: int,  # assumed to be 1 for MLA
+        head_size: int,
     ) -> Tuple[int, ...]:
-        # TODO(lucas): remove hardcoding k_pe size as 1/8th of kv_lora_rank
-        k_pe_size = kv_lora_rank // 8
-        return (num_blocks, block_size, kv_lora_rank + k_pe_size)
+        return (num_blocks, block_size, head_size)
 
     @staticmethod
     def swap_blocks(
@@ -83,7 +81,7 @@ class TritonMLABackend(AttentionBackend):
 
     @staticmethod
     def get_supported_head_sizes() -> List[int]:
-        return [512]
+        return [576]
 
 
 class TritonMLAState(AttentionState):
@@ -624,8 +622,6 @@ class TritonMLAMetadataBuilder(AttentionMetadataBuilder[TritonMLAMetadata]):
             self.multimodal_placeholder_maps.items()
         }
 
-        num_kv_splits = 8
-
         return TritonMLAMetadata(
             num_prefills=self.num_prefills,
             slot_mapping=slot_mapping_tensor,
@@ -645,7 +641,7 @@ class TritonMLAMetadataBuilder(AttentionMetadataBuilder[TritonMLAMetadata]):
             context_lens_tensor=context_lens_tensor,
             block_tables=block_tables,
             use_cuda_graph=use_captured_graph,
-            num_kv_splits=num_kv_splits,
+            num_kv_splits=4,  # TODO(lucas) add heuristic
             head_dim=self.runner.model_config.get_head_size(),
         )
 
