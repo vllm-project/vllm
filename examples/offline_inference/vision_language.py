c5a056cf6ce3b03dc60fc648348b26078dbb6fda
Roger Wang <ywang@roblox.com>
2025-02-02 14:13:38 -0800

    add fps
    Signed-off-by: Roger Wang <ywang@roblox.com>

--- examples/offline_inference/vision_language.py
+++ examples/offline_inference/vision_language.py
@@ -542,6 +542,7 @@ def run_qwen2_5_vl(question: str, modality: str):
         mm_processor_kwargs={
             "min_pixels": 28 * 28,
             "max_pixels": 1280 * 28 * 28,
+            "fps": 1,
         },
         disable_mm_preprocessor_cache=args.disable_mm_preprocessor_cache,
         limit_mm_per_prompt={
