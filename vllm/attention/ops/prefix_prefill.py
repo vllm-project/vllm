afa223eb676c2b4d2a0c8565b9600edc26b58704
Aleksandr Malyshev <164964928+maleksan85@users.noreply.github.com>
2025-02-04 19:58:22 -0800

    [ROCM][AMD][TRITON] Halving warps number for fw_prefill to reduce spilling (#12713)
    Signed-off-by: Aleksandr Malyshev <maleksan@amd.com>
    Co-authored-by: Aleksandr Malyshev <maleksan@amd.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/attention/ops/prefix_prefill.py
+++ vllm/attention/ops/prefix_prefill.py
@@ -11,7 +11,7 @@ from vllm.platforms import current_platform
 
 # Static kernels parameters
 BASE_BLOCK = 128 if current_platform.has_device_capability(80) else 64
-NUM_WARPS = 8
+NUM_WARPS = 4 if current_platform.is_rocm() else 8
 
 # To check compatibility
 IS_TURING = current_platform.get_device_capability() == (7, 5)
