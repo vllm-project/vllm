e469d82bd1daa9ab8f85ef84c82de234dc0ed9e2
Christian Pinto <christian.pinto@ibm.com>
2025-02-05 11:53:58 +0000

    Added missing inter_data_list in cpu_model_runner.py
    Signed-off-by: Christian Pinto <christian.pinto@ibm.com>

--- vllm/worker/cpu_model_runner.py
+++ vllm/worker/cpu_model_runner.py
@@ -167,6 +167,7 @@ class ModelInputForCPUBuilder(ModelRunnerInputBuilderBase[ModelInputForCPU]):
         self.input_data = ModelInputForCPUBuilder.ModelInputData(
             self.runner.model_config.uses_mrope)
         self.att_metadata_builder.prepare()
+        self.inter_data_list = []
 
     def add_seq_group(self, seq_group_metadata: SequenceGroupMetadata):
         self.seq_group_metadata_list.append(seq_group_metadata)
