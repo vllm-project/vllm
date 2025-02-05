d92413c5adcaa9a51b496804a250c0187676b018
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-02-05 06:42:46 +0000

    [Bugfix] Fix OpenVINO model runner (#12750)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/attention/backends/openvino.py
+++ vllm/attention/backends/openvino.py
@@ -140,3 +140,7 @@ class OpenVINOAttentionMetadata:
     # `model_executable`.
     multi_modal_placeholder_index_maps: Optional[Dict[
         str, MultiModalPlaceholderMap.IndexMap]]
+
+    # Enable/disable KV scales calculation. This is so that we can disable the
+    # calculation until after prefill and cuda graph capture.
+    enable_kv_scales_calculation: bool
