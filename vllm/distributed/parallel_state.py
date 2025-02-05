c9451cf2b87018582126a945b4dcdeac672ca717
Akash kaothalkar <61960177+Akashcodes732@users.noreply.github.com>
2025-02-05 12:41:02 +0530

    [Bugfix] Fix 'ModuleNotFoundError: No module named 'intel_extension_for_pytorch'' for --tensor-parallel-size more than 1  (#12546)
    Signed-off-by: Louis Ulmer <ulmerlouis@gmail.com>

--- vllm/distributed/parallel_state.py
+++ vllm/distributed/parallel_state.py
@@ -329,9 +329,17 @@ class GroupCoordinator:
             return input_
 
         if input_.is_cpu:
-            import intel_extension_for_pytorch as ipex
-            ipex.distributed.all_reduce(input_, group=self.device_group)
-            return input_
+            try:
+                import intel_extension_for_pytorch as ipex
+                ipex.distributed.all_reduce(input_, group=self.device_group)
+                return input_
+            except ImportError:
+                """
+                Intel IPEX not found. Falling back to PyTorch native 
+                all_reduce for CPU
+                """
+                torch.distributed.all_reduce(input_, group=self.device_group)
+                return input_
 
         if self.tpu_communicator is not None and \
             not self.tpu_communicator.disabled:
