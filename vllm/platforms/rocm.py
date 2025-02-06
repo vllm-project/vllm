f563276362f8ca7f1e2a0a4c957aa33852be91b1
Sage Moore <sage@neuralmagic.com>
2025-02-06 15:24:19 +0000

    move rocm logic to its own attention backend
    Signed-off-by: Sage Moore <sage@neuralmagic.com>

--- vllm/platforms/rocm.py
+++ vllm/platforms/rocm.py
@@ -74,7 +74,7 @@ class RocmPlatform(Platform):
                             == _Backend.FLASH_ATTN else selected_backend)
         if "VLLM_USE_V1" in os.environ:
             logger.info("Using Flash Attention backend on V1 engine.")
-            return "vllm.v1.attention.backends.flash_attn.FlashAttentionBackend"
+            return "vllm.v1.attention.backends.rocm_attn.ROCmAttentionBackend"
         if selected_backend == _Backend.ROCM_FLASH:
             if not cls.has_device_capability(90):
                 # not Instinct series GPUs.
