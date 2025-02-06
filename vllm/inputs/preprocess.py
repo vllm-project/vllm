bf5e005ce41a70886b19d56bc66542e439407e8d
Cyrus Leung <tlleungac@connect.ust.hk>
2025-02-07 00:45:44 +0800

    [Misc] Remove unnecessary decode call (#12833)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/inputs/preprocess.py
+++ vllm/inputs/preprocess.py
@@ -260,8 +260,6 @@ class InputPreprocessor:
         mm_processor = self.mm_registry.create_processor(
             self.model_config, tokenizer)
 
-        if isinstance(prompt, list):
-            prompt = tokenizer.decode(prompt)
         if mm_processor_kwargs is None:
             mm_processor_kwargs = {}
 
