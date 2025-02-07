ad9aa9270f328663b3fc25985619c0c9f098b9b3
Cyrus Leung <tlleungac@connect.ust.hk>
2025-02-07 22:21:17 +0800

    [Misc] Remove unnecessary detokenization in multimodal processing (#12868)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- tests/entrypoints/openai/test_vision.py
+++ tests/entrypoints/openai/test_vision.py
@@ -92,7 +92,7 @@ async def test_single_chat_session_image(client: openai.AsyncOpenAI,
     choice = chat_completion.choices[0]
     assert choice.finish_reason == "length"
     assert chat_completion.usage == openai.types.CompletionUsage(
-        completion_tokens=10, prompt_tokens=775, total_tokens=785)
+        completion_tokens=10, prompt_tokens=774, total_tokens=784)
 
     message = choice.message
     message = chat_completion.choices[0].message
@@ -185,7 +185,7 @@ async def test_single_chat_session_image_base64encoded(
     choice = chat_completion.choices[0]
     assert choice.finish_reason == "length"
     assert chat_completion.usage == openai.types.CompletionUsage(
-        completion_tokens=10, prompt_tokens=775, total_tokens=785)
+        completion_tokens=10, prompt_tokens=774, total_tokens=784)
 
     message = choice.message
     message = chat_completion.choices[0].message
