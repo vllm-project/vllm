4bdf7de3d32cec0226827a0d7f973b55ffd0f76d
Sage Moore <sage@neuralmagic.com>
2025-02-07 00:49:06 +0000

    more rocm refactoring
    Signed-off-by: Sage Moore <sage@neuralmagic.com>

--- vllm/platforms/rocm.py
+++ vllm/platforms/rocm.py
@@ -119,16 +119,26 @@ class RocmPlatform(Platform):
         parallel_config = vllm_config.parallel_config
         scheduler_config = vllm_config.scheduler_config
         if parallel_config.worker_cls == "auto":
-            # if scheduler_config.is_multi_step:
-            #     parallel_config.worker_cls = \
-            #         "vllm.worker.multi_step_worker.MultiStepWorker"
-            # elif vllm_config.speculative_config:
-            #     parallel_config.worker_cls = \
-            #         "vllm.spec_decode.spec_decode_worker.create_spec_worker"
-            #     parallel_config.sd_worker_cls = \
-            #         "vllm.worker.worker.Worker"
-            # else:
-            parallel_config.worker_cls = "vllm.v1.worker.gpu_worker.Worker"
+            if scheduler_config.is_multi_step:
+                if envs.VLLM_USE_V1:
+                    raise NotImplementedError
+                else:
+                    parallel_config.worker_cls = \
+                        "vllm.worker.multi_step_worker.MultiStepWorker"
+            elif vllm_config.speculative_config:
+                if envs.VLLM_USE_V1:
+                    raise NotImplementedError
+                else:
+                    parallel_config.worker_cls = \
+                        "vllm.spec_decode.spec_decode_worker.create_spec_worker"
+                    parallel_config.sd_worker_cls = \
+                        "vllm.worker.worker.Worker"
+            else:
+                if envs.VLLM_USE_V1:
+                    parallel_config.worker_cls = \
+                            "vllm.v1.worker.gpu_worker.Worker"
+                else:
+                    parallel_config.worker_cls = "vllm.worker.worker.Worker"
 
     @classmethod
     def verify_model_arch(cls, model_arch: str) -> None:
