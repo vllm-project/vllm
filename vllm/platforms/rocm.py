3f610f04570bf0098ae484e1e48f303ec093cf4f
Divakar Verma <137818590+divakar-amd@users.noreply.github.com>
2025-02-07 10:34:26 -0600

    fix rocm get_device name for moe configs (#359)
    * fix rocm get_device name
    
    use 'market_name'
    hard-code names for mi308 & mi300
    
    * use gfx and num_CU for device name
    
    * using market_name
    
    * rename MI325_OAM to MI325X
    
    * rm (duplicate) MI300X_OAM
    
    * rename mi308

--- vllm/platforms/rocm.py
+++ vllm/platforms/rocm.py
@@ -5,7 +5,7 @@ from functools import lru_cache, wraps
 from typing import TYPE_CHECKING, Dict, List, Optional
 
 import torch
-from amdsmi import (AmdSmiException, amdsmi_get_gpu_board_info,
+from amdsmi import (AmdSmiException, amdsmi_get_gpu_asic_info,
                     amdsmi_get_processor_handles, amdsmi_init,
                     amdsmi_shut_down, amdsmi_topo_get_link_type)
 
@@ -167,9 +167,8 @@ class RocmPlatform(Platform):
     def get_device_name(cls, device_id: int = 0) -> str:
         physical_device_id = device_id_to_physical_device_id(device_id)
         handle = amdsmi_get_processor_handles()[physical_device_id]
-        # Note: this may not be exactly the same as the torch device name
-        # E.g. `AMD Instinct MI300X OAM` vs `AMD Instinct MI300X`
-        return amdsmi_get_gpu_board_info(handle)["product_name"]
+        # Using market_name to distinguish MI300 & MI308
+        return amdsmi_get_gpu_asic_info(handle)["market_name"]
 
     @classmethod
     def get_device_total_memory(cls, device_id: int = 0) -> int:
