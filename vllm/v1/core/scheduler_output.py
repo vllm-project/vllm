b0c3b0c9b28fa6f641de7ac95514b83b4041717b
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2025-02-10 15:57:57 -0800

    comments
    Signed-off-by: Woosuk Kwon <woosuk.kwon@berkeley.edu>

--- vllm/v1/core/scheduler_output.py
+++ vllm/v1/core/scheduler_output.py
@@ -1,4 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
+
 from dataclasses import dataclass
 from typing import TYPE_CHECKING, Dict, List, Optional, Set, Tuple
 
@@ -75,13 +76,33 @@ class CachedRequestData:
 @dataclass
 class SchedulerOutput:
 
+    # List of the requests that are scheduled for the first time.
+    # We cache the request's data in each worker process, so that we don't
+    # need to re-send it every scheduling step.
     scheduled_new_reqs: List[NewRequestData]
+    # List of the requests that have been scheduled before.
+    # Since the request's data is already cached in the worker processes,
+    # we only send the diff to minimize the communication cost.
     scheduled_cached_reqs: List[CachedRequestData]
 
+    # req_id -> num_scheduled_tokens
+    # Number of tokens scheduled for each request.
     num_scheduled_tokens: Dict[str, int]
+    # Total number of tokens scheduled for all requests.
+    # Equal to sum(num_scheduled_tokens.values())
     total_num_scheduled_tokens: int
+    # req_id -> indices of encoder inputs scheduled for each request
+    # E.g., [0, 1] means that 0-th and 1-th images are scheduled to run on
+    # the vision encoder at the current step.
     scheduled_encoder_inputs: Dict[str, List[int]]
+    # Number of common prefix blocks for all requests.
+    # This can be used for cascade attention.
     num_common_prefix_blocks: int
 
+    # Request IDs that are finished in between the previous and the current
+    # steps. This is used to notify the workers about the finished requests
+    # so that they can free the cached states for those requests.
     finished_req_ids: Set[str]
+    # List of (req_id, encoder_input_index) tuples.
+    # Used to free the encoder cache.
     free_encoder_input_ids: List[Tuple[str, int]]
