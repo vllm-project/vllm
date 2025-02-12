20506dd41da5a33e3ad684075274a9123a9b8a63
NickLucche <nlucches@redhat.com>
2025-02-12 08:29:32 +0000

    stream with own chunk type + usage info
    Signed-off-by: NickLucche <nlucches@redhat.com>

--- examples/online_serving/openai_chat_completion_client.py
+++ examples/online_serving/openai_chat_completion_client.py
@@ -15,24 +15,20 @@ client = OpenAI(
 models = client.models.list()
 model = models.data[0].id
 
-chat_completion = client.chat.completions.create(
-    messages=[{
-        "role": "system",
-        "content": "You are a helpful assistant."
-    }, {
-        "role": "user",
-        "content": "Who won the world series in 2020?"
-    }, {
-        "role":
-        "assistant",
-        "content":
-        "The Los Angeles Dodgers won the World Series in 2020."
-    }, {
-        "role": "user",
-        "content": "Where was it played?"
-    }],
-    model=model,
-)
+chat_completion = client.chat.completions.create(messages=[{
+    "role":
+    "system",
+    "content":
+    "You are a helpful assistant."
+}, {
+    "role": "user",
+    "content": "Say Hi"
+}],
+                                                 model=model,
+                                                 stream=True)
 
 print("Chat completion results:")
-print(chat_completion)
+# print(chat_completion)
+for chunk in chat_completion:
+    print(chunk, '\n\n')
+    # print(chunk.choices[0].delta.content)
