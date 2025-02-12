fb4b441183d17c97ecfe7491b52693aaafc1299b
Bill Nell <bnell@redhat.com>
2025-02-12 20:10:32 +0000

    review comments, fix events
    Signed-off-by: Bill Nell <bnell@redhat.com>

--- vllm/v1/engine/core.py
+++ vllm/v1/engine/core.py
@@ -130,7 +130,7 @@ class EngineCore:
                 new_logprobs={},
                 new_prompt_logprobs_tensors={},
                 finish_reason={},
-                events={},
+                events=None,
                 scheduler_stats=self.scheduler.make_stats())
 
         scheduler_output = self.scheduler.schedule()
@@ -298,6 +298,5 @@ class EngineCoreProc(EngineCore):
         with zmq_socket_ctx(output_path, zmq.constants.PUSH) as socket:
             while True:
                 outputs = self.output_queue.get()
-                print(outputs)
                 encoder.encode_into(outputs, buffer)
                 socket.send_multipart((buffer, ), copy=False)
