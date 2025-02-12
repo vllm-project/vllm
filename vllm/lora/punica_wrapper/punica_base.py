236389fc9fab8c10571e173ccbb4e721ab7be5b9
Jee Jee Li <pandaleefree@gmail.com>
2025-02-13 00:58:24 +0800

    [Misc] Delete unused LoRA modules (#13151)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/lora/punica_wrapper/punica_base.py
+++ vllm/lora/punica_wrapper/punica_base.py
@@ -147,7 +147,7 @@ class PunicaWrapperBase(PunicaWrapperABC):
                                               dtype=torch.long,
                                               device=device)
 
-        # 5 is the number of indicies tensors.
+        # 5 is the number of indices tensors.
         # base_indices, sampler_indices, sampler_indices_padded,
         # embeddings_indices,long_lora_indices
         self.indices_len: List[Optional[int]] = [None] * 5
