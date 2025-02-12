7590b0e37dfb2837eef1073f4f40daf8de4e707c
Akshat Tripathi <akshat@krai.ai>
2025-02-12 17:51:19 +0000

    Fix bug
    Signed-off-by: Akshat Tripathi <akshat@krai.ai>

--- vllm/lora/ops/xla_ops/__init__.py
+++ vllm/lora/ops/xla_ops/__init__.py
@@ -1,15 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
-from vllm.lora.ops.xla_ops.lora_ops import bgmv_expand  # noqa: F401
-from vllm.lora.ops.xla_ops.lora_ops import (bgmv_expand_slice, bgmv_shrink,
-                                            sgmv_expand, sgmv_expand_slice,
-                                            sgmv_shrink)
+from vllm.lora.ops.xla_ops.lora_ops import (bgmv_expand, bgmv_expand_slice, bgmv_shrink)
 
 __all__ = [
     "bgmv_expand",
     "bgmv_expand_slice",
-    "bgmv_shrink",
-    "sgmv_expand",
-    "sgmv_expand_slice",
-    "sgmv_shrink",
+    "bgmv_shrink"
 ]
