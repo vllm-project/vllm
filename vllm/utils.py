c7dc9bd7269db1860f6cbe04103c9e8b486bd31b
Gregory Shtrasberg <Gregory.Shtrasberg@amd.com>
2025-02-13 22:06:18 +0000

    Adding explanation for stream creation
    Signed-off-by: Gregory Shtrasberg <Gregory.Shtrasberg@amd.com>

--- vllm/utils.py
+++ vllm/utils.py
@@ -947,6 +947,9 @@ def current_stream() -> torch.cuda.Stream:
     if _current_stream is None:
         # when this function is called before any stream is set,
         # we return the default stream.
+        # On ROCm using the default 0 stream in combination with RCCL
+        # is hurting performance. Therefore creating a dedicated stream
+        # per process
         _current_stream = torch.cuda.Stream() if current_platform.is_rocm(
         ) else torch.cuda.current_stream()
     return _current_stream
