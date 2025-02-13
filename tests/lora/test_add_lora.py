11f1cb44832cedce934c1ecc46ae594e1d737ea5
Varun Sundar Rabindranath <varun@neuralmagic.com>
2025-02-13 15:44:40 -0500

    reuse llm instance
    Signed-off-by: Varun Sundar Rabindranath <varun@neuralmagic.com>

--- tests/lora/test_add_lora.py
+++ tests/lora/test_add_lora.py
@@ -2,13 +2,12 @@
 import asyncio
 import time
 from pathlib import Path
-from typing import Any, Callable, List, Optional
+from typing import List
 
 import pytest
 from huggingface_hub import snapshot_download
 
 from vllm.engine.arg_utils import AsyncEngineArgs
-from vllm.engine.protocol import EngineClient
 from vllm.inputs import TextPrompt
 from vllm.lora.request import LoRARequest
 from vllm.sampling_params import SamplingParams
@@ -17,7 +16,7 @@ from vllm.utils import merge_async_iterators
 MODEL_PATH = "meta-llama/Llama-2-7b-hf"
 LORA_MODULE_DOWNLOAD_PATH = None  # Populated by download_and_prepare_lora_module #noqa
 LORA_RANK = 8
-DEFAULT_MAX_LORAS = 64
+DEFAULT_MAX_LORAS = 192
 
 
 def download_and_prepare_lora_module():
@@ -53,78 +52,6 @@ def v1(run_with_both_engines_lora):
     pass
 
 
-async def add_lora_callable(llm: EngineClient,
-                            lora_requests: List[LoRARequest]):
-    for lr in lora_requests:
-        await llm.add_lora(lr)
-
-
-async def requests_processing_time(
-    lora_requests=List[LoRARequest],
-    warmup_function: Optional[Callable[[EngineClient, List[LoRARequest]],
-                                       Any]] = None
-) -> float:
-    """
-    Utility function to measure LoRA requests processing time. The primary
-    usecase is to test the difference between a cold start
-    (no warmup functions) vs a hot start.
-    """
-
-    max_loras = len(set([lr.lora_int_id for lr in lora_requests]))
-    # Create engine in eager-mode. Due to high max_loras, the CI can
-    # OOM during cuda-graph capture.
-    engine_args = AsyncEngineArgs(
-        model=MODEL_PATH,
-        enable_lora=True,
-        max_loras=max_loras,
-        max_lora_rank=LORA_RANK,
-        max_model_len=128,
-        gpu_memory_utilization=0.8,  #avoid OOM
-        enforce_eager=True)
-    sampling_params = SamplingParams(n=1,
-                                     temperature=0.0,
-                                     top_p=1.0,
-                                     ignore_eos=True,
-                                     max_tokens=1)
-
-    # The run_with_both_engines_lora fixture sets up the `VLLM_USE_V1`
-    # environment variable. reload vllm.enging.async_llm_engine as
-    # vllm.engine.async_llm_engine.AsyncLLMEgnine changes depending on the
-    # env var
-    import importlib
-
-    import vllm.engine.async_llm_engine
-    importlib.reload(vllm.engine.async_llm_engine)
-    from vllm.entrypoints.openai.api_server import (
-        build_async_engine_client_from_engine_args)
-
-    async with build_async_engine_client_from_engine_args(engine_args) as llm:
-
-        if warmup_function:
-            await warmup_function(llm, lora_requests)
-            # Wait for the warmup functions complete
-            await asyncio.sleep(30)
-
-        generators = []
-        start = time.perf_counter()
-
-        for idx, lora_request in enumerate(lora_requests):
-            generator = llm.generate(
-                prompt=TextPrompt(prompt=f"hello {idx}",
-                                  multi_modal_data=None),  # type: ignore 
-                sampling_params=sampling_params,
-                lora_request=lora_request,
-                request_id=f"test{idx}")
-            generators.append(generator)
-
-        all_gens = merge_async_iterators(*generators)
-        async for i, res in all_gens:
-            pass
-
-        end = time.perf_counter()
-        return end - start
-
-
 def get_lora_requests() -> List[LoRARequest]:
     lora_requests: List[LoRARequest] = [
         LoRARequest(lora_name=f"{i}",
@@ -132,8 +59,37 @@ def get_lora_requests() -> List[LoRARequest]:
                     lora_path=LORA_MODULE_DOWNLOAD_PATH)
         for i in range(1, DEFAULT_MAX_LORAS + 1)
     ]
-
     return lora_requests
+
+
+async def requests_processing_time(llm,
+                                   lora_requests: List[LoRARequest]) -> float:
+
+    sampling_params = SamplingParams(n=1,
+                                     temperature=0.0,
+                                     top_p=1.0,
+                                     ignore_eos=True,
+                                     max_tokens=1)
+
+    generators = []
+    start = time.perf_counter()
+
+    for lora_request in lora_requests:
+        lora_int_id = lora_request.lora_int_id
+        generator = llm.generate(
+            prompt=TextPrompt(prompt=f"hello {lora_int_id}",
+                              multi_modal_data=None),  # type: ignore 
+            sampling_params=sampling_params,
+            lora_request=lora_request,
+            request_id=f"test{lora_int_id}")
+        generators.append(generator)
+
+    all_gens = merge_async_iterators(*generators)
+    async for i, res in all_gens:
+        pass
+
+    end = time.perf_counter()
+    return end - start
 
 
 @pytest.mark.asyncio
@@ -152,16 +108,52 @@ async def test_add_lora():
 
     lora_requests: List[LoRARequest] = get_lora_requests()
 
-    # Dummy run - So any 1-time functionality like triton kernel compilation
-    # is complete here.
-    await requests_processing_time(lora_requests)
+    max_loras = len(set([lr.lora_int_id for lr in lora_requests]))
+    # Create engine in eager-mode. Due to high max_loras, the CI can
+    # OOM during cuda-graph capture.
+    engine_args = AsyncEngineArgs(
+        model=MODEL_PATH,
+        enable_lora=True,
+        max_loras=max_loras,
+        max_lora_rank=LORA_RANK,
+        max_model_len=128,
+        gpu_memory_utilization=0.8,  #avoid OOM
+        enforce_eager=True)
 
-    # Run with warmup
-    time_with_add_lora = await requests_processing_time(
-        lora_requests, warmup_function=add_lora_callable)
+    # The run_with_both_engines_lora fixture sets up the `VLLM_USE_V1`
+    # environment variable. reload vllm.enging.async_llm_engine as
+    # vllm.engine.async_llm_engine.AsyncLLMEgnine changes depending on the
+    # env var.
+    import importlib
 
-    # Run without any warmup
-    time_cold_start = await requests_processing_time(lora_requests)
+    import vllm.engine.async_llm_engine
+    importlib.reload(vllm.engine.async_llm_engine)
+    from vllm.entrypoints.openai.api_server import (
+        build_async_engine_client_from_engine_args)
+
+    # split lora_requests into 3 parts
+    part_size = len(lora_requests) // 3
+    dummy_run_requests = lora_requests[:part_size]
+    warmup_run_requests = lora_requests[part_size:part_size * 2]
+    cold_run_requests = lora_requests[part_size * 2:]
+
+    async with build_async_engine_client_from_engine_args(engine_args) as llm:
+
+        # Dummy run - So any 1-time functionality like triton kernel compilation
+        # is complete here.
+        await requests_processing_time(llm, dummy_run_requests)
+
+        # Run with warmup
+        for lr in warmup_run_requests:
+            await llm.add_lora(lr)
+        # Wait for the add_lora function to complete on the server side.
+        await asyncio.sleep(30)
+        time_with_add_lora = await requests_processing_time(
+            llm, warmup_run_requests)
+
+        # Run without any warmup
+        time_cold_start = await requests_processing_time(
+            llm, cold_run_requests)
 
     print(f"time hot-start {time_with_add_lora} vs "
           f"time cold-start {time_cold_start} ")
@@ -170,5 +162,4 @@ async def test_add_lora():
         f"time_with_add_lora={time_with_add_lora}, "
         f"time_cold_start={time_cold_start}"
         "The engine request processing time with LoRA pre-loading "
-        "must be less than the version that does on-demand LoRA loading."
-        f"time with add_lora ={time}")
+        "must be less than the version that does on-demand LoRA loading.")
