d56195ac94fcf0e5d2ba3b61bad498d2a5d6a801
Rafael Vasquez <rafvasq21@gmail.com>
2025-02-13 10:10:25 -0500

    Dict type to avoid mypy errors
    Signed-off-by: Rafael Vasquez <rafvasq21@gmail.com>

--- vllm/logger.py
+++ vllm/logger.py
@@ -10,7 +10,7 @@ from logging import Logger
 from logging.config import dictConfig
 from os import path
 from types import MethodType
-from typing import Any, Optional, cast
+from typing import Any, Dict, Optional, cast
 
 import vllm.envs as envs
 
@@ -24,7 +24,7 @@ _FORMAT = (f"{VLLM_LOGGING_PREFIX}%(levelname)s %(asctime)s "
            "%(filename)s:%(lineno)d] %(message)s")
 _DATE_FORMAT = "%m-%d %H:%M:%S"
 
-DEFAULT_LOGGING_CONFIG = {
+DEFAULT_LOGGING_CONFIG: Dict[str, Any] = {
     "formatters": {
         "vllm": {
             "class": "vllm.logging_utils.NewLineFormatter",
@@ -130,7 +130,6 @@ def _configure_vllm_root_logger() -> None:
     else:
         log_filter_patterns = []
 
-    # Update the DEFAULT_LOGGING_CONFIG with the patterns
     DEFAULT_LOGGING_CONFIG["filters"]["vllm_redact"][
         "patterns"] = log_filter_patterns
 
