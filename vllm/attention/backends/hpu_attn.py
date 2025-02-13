0d3a5f5efe023de2d1853b3b14550734c6b5d6c8
Konrad Zawora <kzawora@habana.ai>
2025-02-13 18:40:09 +0200

    add mllama prefill workaround

--- vllm/attention/backends/hpu_attn.py
+++ vllm/attention/backends/hpu_attn.py
@@ -326,11 +326,13 @@ class HPUAttentionImpl(AttentionImpl, torch.nn.Module):
         if attn_metadata.is_prompt:
             batch_size = attn_metadata.num_prefills
             batched_tokens, _ = query.shape
-            batched_kv_tokens, _, _ = key.shape
+            if key is not None:
+                batched_kv_tokens, _, _ = key.shape
             assert batch_size > 0, (
                 "In prefill stage the num_prefills should be > 0")
             assert batched_tokens % batch_size == 0
-            assert batched_kv_tokens % batch_size == 0
+            if key is not None:
+                assert batched_kv_tokens % batch_size == 0
             seq_len = batched_tokens // batch_size
 
         query = query.view(-1, self.num_heads, self.head_size)
@@ -371,6 +373,13 @@ class HPUAttentionImpl(AttentionImpl, torch.nn.Module):
             attn_bias = torch.zeros((batch_size, 1, 1, 1),
                                     device=query.device,
                                     dtype=torch.bool)
+
+            # NOTE(kzawora): mllama prefill fwd pass doesn't pass keys and
+            # values in profile_run. No idea why. This is a dirty workaround.
+            if key is None:
+                key = query
+            if value is None:
+                value = query
             out = ops.prompt_attention(
                 query.view(query_shape),
                 key.view(kv_shape),
