09dc628f9ab2d8a67069cdd6c2b2efa95459bb1c
Wang Ran (汪然) <wrran@outlook.com>
2025-02-14 14:07:25 +0800

    [Misc] Remove redundant statements in scheduler.py (#13229)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/core/scheduler.py
+++ vllm/core/scheduler.py
@@ -770,7 +770,6 @@ class Scheduler:
             swapped_queue.popleft()
             self._swap_in(seq_group, blocks_to_swap_in)
             self._append_slots(seq_group, blocks_to_copy, enable_chunking)
-            is_prefill = seq_group.is_prefill()
             if is_prefill:
                 prefill_seq_groups.append(
                     ScheduledSequenceGroup(
