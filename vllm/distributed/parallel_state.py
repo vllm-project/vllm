56f98285e15e3198bdbd2dafa63e037134799489
Jongseok Park <37990712+cakeng@users.noreply.github.com>
2025-02-14 10:33:38 -0800

    Update parallel_state.py
    Fix removed 'backend' arg

--- vllm/distributed/parallel_state.py
+++ vllm/distributed/parallel_state.py
@@ -1096,7 +1096,7 @@ def ensure_model_parallel_initialized(
         get_world_group().device_group)
     if not model_parallel_is_initialized():
         initialize_model_parallel(tensor_model_parallel_size,
-                                  pipeline_model_parallel_size)
+                                  pipeline_model_parallel_size, backend)
         return
 
     assert (
