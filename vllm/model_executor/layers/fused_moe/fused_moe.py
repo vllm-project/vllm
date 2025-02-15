c37abcbae0807f6a4dac43c1e200e9b143f68d62
Jinzhen Lin <linjinzhen@hotmail.com>
2025-02-16 01:09:30 +0800

    fix typo
    Signed-off-by: Jinzhen Lin <linjinzhen@hotmail.com>

--- vllm/model_executor/layers/fused_moe/fused_moe.py
+++ vllm/model_executor/layers/fused_moe/fused_moe.py
@@ -960,7 +960,7 @@ def try_get_optimal_moe_config(
     else:
         # First try to load optimal config from the file
         E, _, N = w2_shape
-        if dtype == "int4_w8a8":
+        if dtype == "int4_w8a16":
             N = N * 2
         block_n = block_shape[0] if block_shape else 0
         block_k = block_shape[1] if block_shape else 0
