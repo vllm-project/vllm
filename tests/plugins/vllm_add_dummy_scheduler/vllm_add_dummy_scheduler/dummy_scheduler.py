e5d8805dde62da535f47d2314a329706e218ef68
Yannick Schnider <Yannick.Schnider1@ibm.com>
2025-02-17 18:20:43 +0000

    allow scheduler_cls to be string or class directly, update testing
    Signed-off-by: Yannick Schnider <Yannick.Schnider1@ibm.com>

--- tests/plugins/vllm_add_dummy_scheduler/vllm_add_dummy_scheduler/dummy_scheduler.py
+++
@@ -1,9 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-
-from vllm.core.scheduler import Scheduler
-
-
-class DummyScheduler(Scheduler):
-
-    def schedule(self):
-        raise Exception("Exception raised by DummyScheduler")
--- tests/plugins_tests/test_scheduler_plugins.py
+++ tests/plugins_tests/test_scheduler_plugins.py
@@ -1,18 +1,37 @@
 # SPDX-License-Identifier: Apache-2.0
 
+from vllm.core.scheduler import Scheduler
+
+
+class DummyScheduler(Scheduler):
+
+    def schedule(self):
+        raise Exception("Exception raised by DummyScheduler")
+
 
 def test_scheduler_plugins():
-    # simulate workload by running an example
-    import runpy
-
     import pytest
 
-    current_file = __file__
-    import os
-    example_file = os.path.join(
-        os.path.dirname(os.path.dirname(os.path.dirname(current_file))),
-        "examples", "offline_inference/basic.py")
+    from vllm.engine.arg_utils import EngineArgs
+    from vllm.engine.llm_engine import LLMEngine
+    from vllm.executor.uniproc_executor import UniProcExecutor
+    from vllm.sampling_params import SamplingParams
 
     with pytest.raises(Exception) as exception_info:
-        runpy.run_path(example_file)
+
+        engine_args = EngineArgs(
+            model="facebook/opt-125m",
+            enforce_eager=True,  # reduce test time
+        )
+        vllm_config = engine_args.create_engine_config()
+        vllm_config.parallel_config.scheduler_cls = DummyScheduler
+
+        engine = LLMEngine(vllm_config=vllm_config,
+                           executor_class=UniProcExecutor,
+                           log_stats=False)
+
+        sampling_params = SamplingParams(max_tokens=1)
+        engine.add_request("0", "foo", sampling_params)
+        engine.step()
+
     assert str(exception_info.value) == "Exception raised by DummyScheduler"
