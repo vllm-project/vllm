8e5bbb30c3c17a0a1787028c0e787c9ea82ed3c5
Xiongfei Wei <isaacwxf23@gmail.com>
2025-02-17 06:03:36 +0000

    merge prompt and decode

--- vllm/model_executor/models/qwen2.py
+++ vllm/model_executor/models/qwen2.py
@@ -258,15 +258,17 @@ class Qwen2DecoderLayer(nn.Module):
         return hidden_states, residual
 
 
-@support_torch_compile(
-    dynamic_arg_dims={
-        "input_ids": 0,
-        # positions is of shape (3, seq_len) if mrope is enabled for qwen2-vl,
-        # otherwise (seq_len, ).
-        "positions": -1,
-        "intermediate_tensors": 0,
-        "inputs_embeds": 0,
-    })
+# TODO(xw32): revert the change before merging the code.
+# xw32 turns off dynamo
+# @support_torch_compile(
+#     dynamic_arg_dims={
+#         "input_ids": 0,
+#         # positions is of shape (3, seq_len) if mrope is enabled for qwen2-vl,
+#         # otherwise (seq_len, ).
+#         "positions": -1,
+#         "intermediate_tensors": 0,
+#         "inputs_embeds": 0,
+#     })
 class Qwen2Model(nn.Module):
 
     def __init__(self, *, vllm_config: VllmConfig, prefix: str = ""):
