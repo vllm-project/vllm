2ee6281eb1a35e5b5c88856e4184c64a5073b601
Cody Yu <hao.yu.cody@gmail.com>
2025-02-18 14:29:55 -0800

    msg
    Signed-off-by: Cody Yu <hao.yu.cody@gmail.com>

--- vllm/executor/ray_distributed_executor.py
+++ vllm/executor/ray_distributed_executor.py
@@ -101,6 +101,10 @@ class RayDistributedExecutor(DistributedExecutorBase):
                 self.driver_worker.execute_method)
 
     def shutdown(self) -> None:
+        logger.info(
+            "Shutdown Ray executor. If you see the error log above this "
+            "line about SIGTERM received, please ignore because this is "
+            "the expected termination process in Ray")
         if hasattr(self, "forward_dag") and self.forward_dag is not None:
             self.forward_dag.teardown()
             import ray
