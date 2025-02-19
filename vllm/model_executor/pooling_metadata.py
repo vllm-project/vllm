cdcf576627b3216cffbccce73cb5b199f57672eb
Libin Tang <litang@habana.ai>
2025-02-19 06:12:06 -0800

    Initial enablement for text-embedding (#758)
    This is the initial PR for text-embedding with --dtype bfloat16. The
    following models are tested with 1x and 2x mpi/ray
    
    model
    BAAI/bge-base-en-v1.5
    BAAI/bge-multilingual-gemma2
    intfloat/e5-mistral-7b-instruct
    ssmits/Qwen2-7B-Instruct-embed-base
    Alibaba-NLP/gte-Qwen2-7B-instruct
    
    There is no Lora support yet.
    has dependency with
    https://github.com/HabanaAI/vllm-hpu-extension/pull/87
    
    offline test use script from
    https://docs.vllm.ai/en/latest/getting_started/examples/embedding.html
    with adding dtype="bfloat16" in LLM call
    sample cmds such as
    python testscript.py
    VLLM_SKIP_WARMUP=true python testscript.py
    
    online test use client script from
    https://docs.vllm.ai/en/latest/getting_started/examples/openai_embedding_client.html
    server sample cmds such as
    vllm serve Alibaba-NLP/gte-Qwen2-7B-instruct --task embed --dtype
    bfloat16
    VLLM_SKIP_WARMUP=true VLLM_PROMPT_USE_FUSEDSDPA=true vllm serve
    Alibaba-NLP/gte-Qwen2-7B-instruct --task embed --dtype bfloat16
    
    ---------
    
    Co-authored-by: Konrad Zawora <kzawora@habana.ai>

--- vllm/model_executor/pooling_metadata.py
+++ vllm/model_executor/pooling_metadata.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from dataclasses import dataclass
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict, List, Optional, Tuple
 
 import torch
 
@@ -19,6 +19,8 @@ class PoolingMetadata:
         seq_groups: List of (seq_ids, pooling_params).
         seq_data: A mapping of sequence ID to additional sequence data.
         prompt_lens: List of the lengths of each prompt.
+        prompt_offsets: List of prompt start offsets for each prompt
+        when flat out with padding
     """
 
     def __init__(
@@ -26,16 +28,19 @@ class PoolingMetadata:
         seq_groups: List[Tuple[List[int], PoolingParams]],
         seq_data: Dict[int, Any],  # Specific data related to sequences
         prompt_lens: List[int],
+        prompt_offsets: Optional[List[int]] = None,
     ) -> None:
         self.seq_groups = seq_groups
         self.seq_data = seq_data
         self.prompt_lens = prompt_lens
+        self.prompt_offsets = prompt_offsets
 
     def __repr__(self) -> str:
         return ("PoolingMetadata("
                 f"seq_groups={self.seq_groups}, "
                 f"seq_data={self.seq_data}, "
-                f"prompt_lens={self.prompt_lens})")
+                f"prompt_lens={self.prompt_lens}, "
+                f"prompt_offsets={self.prompt_offsets})")
 
 
 @dataclass
@@ -43,6 +48,7 @@ class PoolingTensors:
     """Tensors for pooling."""
 
     prompt_lens: torch.Tensor
+    prompt_offsets: torch.Tensor
 
     @classmethod
     def from_pooling_metadata(
@@ -66,6 +72,15 @@ class PoolingTensors:
             dtype=torch.long,
             pin_memory=pin_memory,
         )
-
+        if pooling_metadata.prompt_offsets is not None:
+            prompt_offsets_t = torch.tensor(
+                pooling_metadata.prompt_offsets,
+                device="cpu",
+                dtype=torch.long,
+                pin_memory=pin_memory,
+            ).to(device=device, non_blocking=True)
+        else:
+            prompt_offsets_t = None
         return cls(prompt_lens=prompt_lens_t.to(device=device,
-                                                non_blocking=True), )
+                                                non_blocking=True),
+                   prompt_offsets=prompt_offsets_t)
