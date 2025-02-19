a4693bb94f82a8ac64298a5ca78435dcbf9d7dd2
Yi Liu <yiliu4@habana.ai>
2025-02-19 04:44:56 +0200

    add more inc to hpu
    Signed-off-by: Yi Liu <yiliu4@habana.ai>

--- vllm/platforms/hpu.py
+++ vllm/platforms/hpu.py
@@ -23,7 +23,7 @@ class HpuPlatform(Platform):
     dispatch_key: str = "HPU"
     ray_device_key: str = "HPU"
     device_control_env_var: str = "HABANA_VISIBLE_MODULES"
-    supported_quantization: list[str] = ["fp8", "inc", "awq_hpu", "gptq_hpu"]
+    supported_quantization: list[str] = ["fp8", "inc", "awq_hpu", "gptq_hpu", "inc_p", "inc_q"]
 
     @classmethod
     def get_attn_backend_cls(cls, selected_backend: _Backend, head_size: int,
