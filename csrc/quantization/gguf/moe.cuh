85a6140a37edaee2e6cb6dfb1a9f592c04da6108
SzymonOzog <szymon.ozog@aleph-alpha.com>
2025-02-19 18:25:42 +0000

    Working messed up version

--- csrc/quantization/gguf/moe.cuh
+++ csrc/quantization/gguf/moe.cuh
@@ -25,22 +25,34 @@ static __device__ __forceinline__ void moe_q(
   int token_offs[mmq_x / nwarps];
   for (int i = 0; i < mmq_x; i += nwarps) {
     token_offs[i / nwarps] = sorted_token_ids[col_dst_0 + threadIdx.y + i];
-    token_ids[i / nwarps] = token_offs[i / nwarps] / top_k;
+    // printf("thread %d/%d, %d tok id %d, tok offset %d \n",
+    //         threadIdx.x, threadIdx.y, i / nwarps,
+    //        token_ids[i / nwarps], token_offs[i / nwarps]);
+  }
+
+  for (int i = 0; i < mmq_x; i += nwarps * QI8_1) {
+    const int ids =
+        (i + threadIdx.y * QI8_1 + threadIdx.x / (WARP_SIZE_GGUF / QI8_1)) %
+        mmq_x;
+    token_ids[i / (nwarps * QI8_1)] = token_offs[ids] / top_k;
   }
   const int exp_idx = expert_ids[blockIdx.y];
+  if (exp_idx > 255 || exp_idx < 0) return;
 
-  if (blockIdx.x == 2 && blockIdx.y == 0 && threadIdx.x == 3 &&
-      threadIdx.y == 1) {
-    printf(
-        "running kernel for expert id %d, row_dst %d \
-blocks per warp %d \n",
-        exp_idx, row_dst_0, blocks_per_warp);
-    for (int i = 0; i < mmq_x; i += nwarps) {
-      printf("%d tok id %d, tok offset %d \n", i / nwarps,
-             token_ids[i / nwarps], token_offs[i / nwarps]);
-    }
-    printf("sum configured to %d %d\n", mmq_y / WARP_SIZE_GGUF, mmq_x / nwarps);
-  }
+  // if ((blockIdx.y == 119 || blockIdx.y == 119)&& blockIdx.x == 9 &&
+  // threadIdx.x == 3 && threadIdx.y == 3)
+  //     if (threadIdx.x == 0 && threadIdx.y == 0)
+  //     {
+  //     printf(
+  //         "running kernel for expert id %d, row_dst %d \
+// blocks per warp %d \n",
+  //         exp_idx, row_dst_0, blocks_per_warp);
+  //     for (int i = 0; i < mmq_x; i += nwarps) {
+  //       printf("block %d, %d/%d, %d tok id %d, tok offset %d
+  //       \n",blockIdx.y,threadIdx.x, threadIdx.y, i / nwarps,
+  //              token_ids[i / nwarps], token_offs[i / nwarps]);
+  //     }
+  //   }
 
   const block_q_t* x = (const block_q_t*)((char*)vx + exp_idx * exp_stride);
   const block_q8_1* y = (const block_q8_1*)(vy);
@@ -58,6 +70,11 @@ blocks per warp %d \n",
   float sum[mmq_y / WARP_SIZE_GGUF][mmq_x / nwarps] = {{0.0f}};
 
   for (int ib0 = 0; ib0 < blocks_per_row_x; ib0 += blocks_per_warp) {
+    // if ((blockIdx.y == 0 || blockIdx.y == 1)&& blockIdx.x == 0 &&
+    // (threadIdx.x == 0 || threadIdx.x == 0))
+    // {
+    //   printf("stepping tile");
+    // }
     load_tiles(x + row_x_0 * blocks_per_row_x + ib0, tile_x_ql, tile_x_dm,
                tile_x_qh, tile_x_sc, threadIdx.y, nrows_x - row_x_0 - 1,
                threadIdx.x, blocks_per_row_x);
@@ -84,8 +101,9 @@ blocks per warp %d \n",
         // 0)
         // {
         //     printf("thread %d/%d loading data from col eff %d, idx %d, val
-        //     %d\n", threadIdx.x, threadIdx.y, col_y_eff, index_y,
-        //     tile_y_qs[index_y]);
+        //     %d\n",
+        //             threadIdx.x, threadIdx.y, col_y_eff, index_y,
+        //             tile_y_qs[index_y]);
         // }
       }
 
@@ -95,8 +113,20 @@ blocks per warp %d \n",
                          threadIdx.x / (WARP_SIZE_GGUF / QI8_1)) %
                         mmq_x;
         const int kby = threadIdx.x % (WARP_SIZE_GGUF / QI8_1);
-        const int col_y_eff =
-            min(token_ids[ids0 / nwarps * QI8_1] + ids, ncols_y - 1);
+        const int col_y_eff = min(token_ids[ids], ncols_y - 1);
+        // if ((blockIdx.y == 0 || blockIdx.y == 1)&& blockIdx.x == 0 &&
+        // (threadIdx.x == 0 || threadIdx.x == 0)) if ((blockIdx.y == 0 ||
+        // blockIdx.y == 0)&& blockIdx.x == 0)
+        // {
+        //     printf("block %d thread %d/%d, ids %d loading next scale %d, %d,
+        //     %d, %d, %d, to %d/%d\n",
+        //             blockIdx.y,threadIdx.x, threadIdx.y, ids, col_y_eff, kby,
+        //             ib0, ir, col_y_eff * blocks_per_col_y + ib0 * (qk /
+        //             QK8_1) + ir * (WARP_SIZE_GGUF / QI8_1) + kby, ids *
+        //             (WARP_SIZE_GGUF / QI8_1) + kby,
+        //             mmq_x*WARP_SIZE_GGUF/QI8_1
+        //             );
+        // }
         // if (blockIdx.x == 0 && blockIdx.y == 0 && threadIdx.x == 0 &&
         // threadIdx.y == 0)
         // {
@@ -113,7 +143,9 @@ blocks per warp %d \n",
             &y[col_y_eff * blocks_per_col_y + ib0 * (qk / QK8_1) +
                ir * (WARP_SIZE_GGUF / QI8_1) + kby]
                  .ds;
-        half2* dsi_dst = &tile_y_ds[ids * (WARP_SIZE_GGUF / QI8_1) + kby];
+        half2* dsi_dst =
+            &tile_y_ds[threadIdx.y * (WARP_SIZE_GGUF / QI8_1) + kby];
+        // half2* dsi_dst = &tile_y_ds[threadIdx.y];
         if (need_sum) {
           *dsi_dst = *dsi_src;
         } else {
@@ -134,6 +166,13 @@ blocks per warp %d \n",
             sum[i / WARP_SIZE_GGUF][j / nwarps] +=
                 vec_dot(tile_x_ql, tile_x_dm, tile_x_qh, tile_x_sc, tile_y_qs,
                         tile_y_ds, threadIdx.x + i, threadIdx.y + j, k);
+            // if ((blockIdx.y == 0 || blockIdx.y == 1)&& blockIdx.x == 0 &&
+            // (threadIdx.x == 0 || threadIdx.x == 0))
+            // {
+            //     printf("block %d thread %d/%d doing next sum %f \n",
+            //     blockIdx.y,threadIdx.x, threadIdx.y, sum[i /
+            //     WARP_SIZE_GGUF][j / nwarps]);
+            // }
           }
         }
       }
@@ -155,10 +194,11 @@ blocks per warp %d \n",
       if (row_dst >= nrows_dst) {
         continue;
       }
-      if (blockIdx.x == 0 && blockIdx.y == 0) {
-        printf("saving %f tocol %d, row %d\n",
-               sum[i / WARP_SIZE_GGUF][j / nwarps], col_dst, row_dst);
-      }
+      // if (blockIdx.x == 0 && blockIdx.y == 0) {
+      //   printf("thread %d/%d saving %f tocol %d, row %d\n", threadIdx.x,
+      //   threadIdx.y,
+      //          sum[i / WARP_SIZE_GGUF][j / nwarps], col_dst, row_dst);
+      // }
       dst[col_dst * nrows_dst + row_dst] =
           __float2half(sum[i / WARP_SIZE_GGUF][j / nwarps]);
     }
@@ -202,15 +242,19 @@ static void ggml_moe_q2_K_q8_1_cuda(const void* inp, const void* w, half* dst,
                                     const int ncols_x, const int nrows_x,
                                     const int ncols_y, const int nrows_y,
                                     const int nrows_dst, const int top_k,
+                                    const int tokens_post_padded,
                                     cudaStream_t stream) {
   const int mmq_x = MMQ_X_Q2_K;
   const int mmq_y = MMQ_Y_Q2_K;
   const int nwarps = NWARPS_Q2_K;
 
   const int block_num_x = (nrows_x + mmq_y - 1) / mmq_y;
-  const int block_num_y = (ncols_y * top_k + mmq_x - 1) / mmq_x;
+  // const int block_num_y = (ncols_y * top_k + mmq_x - 1) / mmq_x;
+  const int block_num_y = (tokens_post_padded) / mmq_x;
   const dim3 block_nums(block_num_x, block_num_y, 1);
   const dim3 block_dims(WARP_SIZE_GGUF, nwarps, 1);
+  // printf("running block size %d %d, grid size %d %d \n", WARP_SIZE_GGUF,
+  // nwarps, block_num_x, block_num_y);
 
   if (nrows_x % mmq_y == 0) {
     constexpr bool need_check = false;
