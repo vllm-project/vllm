9daac1d1654a1c176c7d9dfe6ec0eb6a205a85fe
albert <xzr@xzr.com>
2025-02-19 16:16:30 +0800

    test: test transcription without given language
    Signed-off-by: albert <xzr@xzr.com>

--- tests/entrypoints/openai/correctness/test_transcription_api_correctness.py
+++ tests/entrypoints/openai/correctness/test_transcription_api_correctness.py
@@ -30,7 +30,7 @@ def to_bytes(y, sr):
     return buffer
 
 
-async def transcribe_audio(client, tokenizer, y, sr):
+async def transcribe_audio(client, tokenizer, y, sr, language):
     # Send loaded audio directly instead of loading from disk,
     # dont account for that time though
     with to_bytes(y, sr) as f:
@@ -38,40 +38,42 @@ async def transcribe_audio(client, tokenizer, y, sr):
         transcription = await client.audio.transcriptions.create(
             file=f,
             model=tokenizer.name_or_path,
-            language="en",
+            language=language,
             temperature=0.0,
         )
         end_time = time.perf_counter()
         # NOTE there's no streaming in transcriptions, can't measure ttft
     latency = end_time - start_time
     num_output_tokens = len(
-        tokenizer(transcription.text, add_special_tokens=False).input_ids)
+        tokenizer(transcription.text, add_special_tokens=False).input_ids
+    )
     return latency, num_output_tokens, transcription.text
 
 
-async def bound_transcribe(model_name, sem, client, audio, reference):
+async def bound_transcribe(model_name, sem, client, audio, reference, language):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     # Use semaphore to limit concurrent requests.
     async with sem:
-        result = await transcribe_audio(client, tokenizer, *audio)
+        result = await transcribe_audio(client, tokenizer, *audio, language)
         # Normalize *english* output/reference for evaluation.
         out = tokenizer.normalize(result[2])
         ref = tokenizer.normalize(reference)
         return result[:2] + (out, ref)
 
 
-async def process_dataset(model, client, data, concurrent_request):
+async def process_dataset(model, client, data, concurrent_request, language):
     sem = asyncio.Semaphore(concurrent_request)
 
     # Warmup call as the first `librosa.load` server-side is quite slow.
     audio, sr = data[0]["audio"]["array"], data[0]["audio"]["sampling_rate"]
-    _ = await bound_transcribe(model, sem, client, (audio, sr), "")
+    _ = await bound_transcribe(model, sem, client, (audio, sr), "", language)
 
     tasks: List[asyncio.Task] = []
     for sample in data:
         audio, sr = sample["audio"]["array"], sample["audio"]["sampling_rate"]
         task = asyncio.create_task(
-            bound_transcribe(model, sem, client, (audio, sr), sample["text"]))
+            bound_transcribe(model, sem, client, (audio, sr), sample["text"], language)
+        )
         tasks.append(task)
     return await asyncio.gather(*tasks)
 
@@ -95,34 +97,38 @@ def print_performance_metrics(results, total_time):
 
 
 def add_duration(sample):
-    y, sr = sample['audio']["array"], sample['audio']["sampling_rate"]
-    sample['duration_ms'] = librosa.get_duration(y=y, sr=sr) * 1000
+    y, sr = sample["audio"]["array"], sample["audio"]["sampling_rate"]
+    sample["duration_ms"] = librosa.get_duration(y=y, sr=sr) * 1000
     return sample
 
 
-def load_hf_dataset(dataset_repo: str, split='validation', **hf_kwargs):
+def load_hf_dataset(dataset_repo: str, split="validation", **hf_kwargs):
     ## Load and filter the dataset
     dataset = load_dataset(dataset_repo, split=split, **hf_kwargs)
-    if 'duration_ms' not in dataset[0]:
+    if "duration_ms" not in dataset[0]:
         # compute duration to filter
         dataset = dataset.map(add_duration)
 
     # Whisper max supported duration
-    dataset = dataset.filter(lambda example: example['duration_ms'] < 30000)
+    dataset = dataset.filter(lambda example: example["duration_ms"] < 30000)
     return dataset
 
 
-def run_evaluation(model: str,
-                   client,
-                   dataset,
-                   max_concurrent_reqs: int,
-                   n_examples: int = -1,
-                   print_metrics: bool = True):
+def run_evaluation(
+    model: str,
+    client,
+    dataset,
+    max_concurrent_reqs: int,
+    language="en",
+    n_examples: int = -1,
+    print_metrics: bool = True,
+):
     if n_examples > 0:
         dataset = dataset.select(range(n_examples))
     start = time.perf_counter()
     results = asyncio.run(
-        process_dataset(model, client, dataset, max_concurrent_reqs))
+        process_dataset(model, client, dataset, max_concurrent_reqs, language=language)
+    )
     end = time.perf_counter()
     total_time = end - start
     print(f"Total Test Time: {total_time:.4f} seconds")
@@ -132,8 +138,7 @@ def run_evaluation(model: str,
     predictions = [res[2] for res in results]
     references = [res[3] for res in results]
     wer = load("wer")
-    wer_score = 100 * wer.compute(references=references,
-                                  predictions=predictions)
+    wer_score = 100 * wer.compute(references=references, predictions=predictions)
     print("WER:", wer_score)
     return wer_score
 
@@ -142,25 +147,59 @@ def run_evaluation(model: str,
 @pytest.mark.parametrize("model_name", ["openai/whisper-large-v3"])
 # Original dataset is 20GB+ in size, hence we use a pre-filtered slice.
 @pytest.mark.parametrize(
-    "dataset_repo", ["D4nt3/esb-datasets-earnings22-validation-tiny-filtered"])
+    "dataset_repo", ["D4nt3/esb-datasets-earnings22-validation-tiny-filtered"]
+)
 # NOTE: Expected WER measured with equivalent hf.transformers args:
 # whisper-large-v3 + esb-datasets-earnings22-validation-tiny-filtered.
 @pytest.mark.parametrize("expected_wer", [12.744980])
-def test_wer_correctness(model_name,
-                         dataset_repo,
-                         expected_wer,
-                         n_examples=-1,
-                         max_concurrent_request=None):
-    with RemoteOpenAIServer(model_name, ['--enforce-eager']) as remote_server:
+def test_wer_correctness(
+    model_name, dataset_repo, expected_wer, n_examples=-1, max_concurrent_request=None
+):
+    with RemoteOpenAIServer(model_name, ["--enforce-eager"]) as remote_server:
         dataset = load_hf_dataset(dataset_repo)
 
         if not max_concurrent_request:
             # No max concurrency
-            max_concurrent_request = n_examples if n_examples > 0\
-                else len(dataset)
+            max_concurrent_request = n_examples if n_examples > 0 else len(dataset)
 
         client = remote_server.get_async_client()
-        wer = run_evaluation(model_name, client, dataset,
-                             max_concurrent_request, n_examples)
+        wer = run_evaluation(
+            model_name, client, dataset, max_concurrent_request, n_examples=n_examples
+        )
         if expected_wer:
             torch.testing.assert_close(wer, expected_wer, atol=1e-1, rtol=1e-2)
+
+
+@pytest.mark.parametrize("model_name", ["openai/whisper-large-v3"])
+@pytest.mark.parametrize(
+    "dataset_repo", ["D4nt3/esb-datasets-earnings22-validation-tiny-filtered"]
+)
+@pytest.mark.parametrize("expected_wer", [12.744980])
+@pytest.mark.parametrize("language", [None])
+def test_wer_correctness_with_lang_detection(
+    model_name,
+    dataset_repo,
+    expected_wer,
+    language,
+    n_examples=-1,
+    max_concurrent_request=None,
+):
+    with RemoteOpenAIServer(model_name, ["--enforce-eager"]) as remote_server:
+        dataset = load_hf_dataset(dataset_repo)
+
+        if not max_concurrent_request:
+            # No max concurrency
+            max_concurrent_request = n_examples if n_examples > 0 else len(dataset)
+
+        client = remote_server.get_async_client()
+        wer = run_evaluation(
+            model_name,
+            client,
+            dataset,
+            max_concurrent_request,
+            language=language,
+            n_examples=n_examples,
+        )
+        if expected_wer:
+            # increase tolerance for language detection
+            torch.testing.assert_close(wer, expected_wer, atol=0.3, rtol=0.05)
