2110a81bd9e641e7e11663a2e3299c9f30270e6b
Yi Liu <yiliu4@habana.ai>
2025-02-19 05:34:11 +0200

    print model
    Signed-off-by: Yi Liu <yiliu4@habana.ai>

--- vllm/worker/hpu_model_runner.py
+++ vllm/worker/hpu_model_runner.py
@@ -777,6 +777,7 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
                     htcore.hpu_initialize(self.model,
                                           mark_only_scales_as_const=True)
                 self.inc_initialized_successfully = True
+                logger.info(f"INC MODEL: \n{self.model}")
                 logger.info("Preparing model with INC took %s",
                             m_inc.get_summary_string())
             elif not is_fake_hpu():
