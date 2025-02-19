92e474483709a5d5295f95496dcbd5ba595040eb
Cody Yu <hao.yu.cody@gmail.com>
2025-02-19 09:40:50 -0800

    [MISC] Logging the message about Ray teardown (#13502)
    Signed-off-by: Cody Yu <hao.yu.cody@gmail.com>
    Co-authored-by: Rui Qiao <161574667+ruisearch42@users.noreply.github.com>
    Signed-off-by: Louis Ulmer <ulmerlouis@gmail.com>

--- vllm/executor/ray_distributed_executor.py
+++ vllm/executor/ray_distributed_executor.py
@@ -101,6 +101,10 @@ class RayDistributedExecutor(DistributedExecutorBase):
                 self.driver_worker.execute_method)
 
     def shutdown(self) -> None:
+        logger.info(
+            "Shutting down Ray distributed executor. If you see error log "
+            "from logging.cc regarding SIGTERM received, please ignore because "
+            "this is the expected termination process in Ray.")
         if hasattr(self, "forward_dag") and self.forward_dag is not None:
             self.forward_dag.teardown()
             import ray
