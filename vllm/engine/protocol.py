1ca84e9f65ea4e5b5bf77af3d206e58ec6b17380
youkaichao <youkaichao@gmail.com>
2025-02-20 12:41:17 +0800

    [core] add sleep and wake up endpoint and v1 support (#12987)
    Signed-off-by: youkaichao <youkaichao@gmail.com>
    Signed-off-by: cennn <2523403608@qq.com>
    Co-authored-by: cennn <2523403608@qq.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/engine/protocol.py
+++ vllm/engine/protocol.py
@@ -279,6 +279,16 @@ class EngineClient(ABC):
         ...
 
     @abstractmethod
+    async def sleep(self, level: int = 1) -> None:
+        """Sleep the engine"""
+        ...
+
+    @abstractmethod
+    async def wake_up(self) -> None:
+        """Wake up the engine"""
+        ...
+
+    @abstractmethod
     async def add_lora(self, lora_request: LoRARequest) -> None:
         """Load a new LoRA adapter into the engine for future requests."""
         ...
