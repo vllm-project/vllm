0511191d2d3e239755359a846def930b0fb95a22
Varun Sundar Rabindranath <varun@neuralmagic.com>
2025-02-20 09:16:56 -0500

    fix isort
    Signed-off-by: Varun Sundar Rabindranath <varun@neuralmagic.com>

--- vllm/lora/ops/triton_ops/v1/__init__.py
+++ vllm/lora/ops/triton_ops/v1/__init__.py
@@ -1,8 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from vllm.lora.ops.triton_ops.v1.v1_expand import v1_expand
-from vllm.lora.ops.triton_ops.v1.v1_shrink import v1_shrink
 from vllm.lora.ops.triton_ops.v1.v1_kernel_metadata import V1KernelMeta
+from vllm.lora.ops.triton_ops.v1.v1_shrink import v1_shrink
 
 __all__ = [
     "v1_expand",
