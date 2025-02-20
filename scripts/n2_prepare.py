b9707ecce5f6954ca9ba96901f5d435f6806de43
Yi Liu <yiliu4@habana.ai>
2025-02-20 10:29:21 +0200

    upadte the gen
    Signed-off-by: Yi Liu <yiliu4@habana.ai>

--- scripts/n2_prepare.py
+++ scripts/n2_prepare.py
@@ -221,7 +221,9 @@ if __name__ == "__main__":
 
     # Generate texts from the prompts. The output is a list of RequestOutput objects
     # that contain the prompt, generated text, and other information.
-    outputs = llm.generate(prompts, sampling_params)
+    outputs = llm.generate(
+        prompts=None, sampling_params=sampling_params, prompt_token_ids=prompt_token_ids
+    )
     # Print the outputs.
     for output_i in range(len(outputs)):
         output = outputs[output_i]
