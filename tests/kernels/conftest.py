551e9bcac434469a564b26d5b785201d0bfd2840
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-02-20 14:52:18 +0100

    Use `pytest-xdist` to parallelise kernel tests
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- tests/kernels/conftest.py
+++ tests/kernels/conftest.py
@@ -1,9 +1,15 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import pytest
+import torch
 
 from vllm.utils import (create_kv_caches_with_random,
                         create_kv_caches_with_random_flash)
+
+
+def pytest_configure(config: pytest.Config):
+    if torch.cuda.is_available():
+        torch.cuda.set_per_process_memory_fraction(1 / 8)
 
 
 @pytest.fixture()
