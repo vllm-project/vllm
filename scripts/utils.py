d4337de0b083b90e398e302390a99dd0100c6648
Yi Liu <yiliu4@habana.ai>
2025-02-20 10:18:17 +0200

    add utils
    Signed-off-by: Yi Liu <yiliu4@habana.ai>

---
+++ scripts/utils.py
@@ -0,0 +1,21 @@
+def get_prompts():
+    filename = "pile.txt"
+    with open(filename, "r") as f:
+        prompts = f.readlines()
+        print(f"Number of prompts: {len(prompts)}")
+    return prompts
+
+def get_prompt_token_ids(model_path, prompts, max_length=1024):
+    from transformers import AutoTokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    prompt_token_ids = []
+    for prompt in prompts:
+        tokens = tokenizer(
+            prompt,
+            return_tensors="pt",
+            truncation=True,
+            max_length=max_length,
+        )
+        if len(tokens.input_ids[0]) < max_length:
+            continue
+        prompt_token_ids.append([x.item() for x in tokens.input_ids[0]])
