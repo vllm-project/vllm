f533bc817934175e1ecec4d979d4ceb45947b8bd
Kevin H. Luu <kevin@anyscale.com>
2025-02-19 18:12:30 -0800

    [3/n][CI] Load Quantization test models with S3 (#13570)
    Signed-off-by: <>
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-20-117.us-west-2.compute.internal>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/model_executor/model_loader/weight_utils.py
+++ vllm/model_executor/model_loader/weight_utils.py
@@ -27,8 +27,6 @@ from vllm.model_executor.layers.quantization import (QuantizationConfig,
 from vllm.platforms import current_platform
 from vllm.utils import PlaceholderModule
 
-logger = init_logger(__name__)
-
 try:
     from runai_model_streamer import SafetensorsStreamer
 except (ImportError, OSError):
@@ -38,6 +36,8 @@ except (ImportError, OSError):
         "runai_model_streamer")  # type: ignore[assignment]
     SafetensorsStreamer = runai_model_streamer.placeholder_attr(
         "SafetensorsStreamer")
+
+logger = init_logger(__name__)
 
 # use system-level temp directory for file locks, so that multiple users
 # can share the same lock without error.
