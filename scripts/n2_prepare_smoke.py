b5fe4ac50ff6348f24e9160ca0ab9ed8c3685cde
Yi Liu <yiliu4@habana.ai>
2025-02-21 11:53:12 +0200

    remove low cpu mem in prepare
    Signed-off-by: Yi Liu <yiliu4@habana.ai>

--- scripts/n2_prepare_smoke.py
+++ scripts/n2_prepare_smoke.py
@@ -227,7 +227,6 @@ if __name__ == "__main__":
         distributed_executor_backend='ray',
         trust_remote_code=True,
         quantization='inc_p',
-        weights_load_device="cpu",
         max_model_len=16384,
         dtype="bfloat16",
     )
