2114e7da1b6c89f28c6918869c12e231dd305373
youkaichao <youkaichao@gmail.com>
2025-02-21 14:03:21 +0800

    relax?
    Signed-off-by: youkaichao <youkaichao@gmail.com>

--- vllm/executor/executor_base.py
+++ vllm/executor/executor_base.py
@@ -282,7 +282,6 @@ class DistributedExecutorBase(ExecutorBase):
 
         # Only the driver worker returns the sampling results.
         driver_outputs = self._driver_execute_model(execute_model_req)
-        assert driver_outputs is not None
         return driver_outputs
 
     def stop_remote_worker_execution_loop(self) -> None:
