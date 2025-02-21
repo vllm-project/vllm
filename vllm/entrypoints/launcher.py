68aa1806d853e0e3e5e059869268d202e78a1643
Keyun Tong <tongkeyun@gmail.com>
2025-02-20 22:25:36 -0800

    Modularize SSLCertRefresher
    Signed-off-by: Keyun Tong <tongkeyun@gmail.com>

--- vllm/entrypoints/launcher.py
+++ vllm/entrypoints/launcher.py
@@ -4,35 +4,24 @@ import asyncio
 import signal
 import socket
 from http import HTTPStatus
-from typing import Any, Callable, Optional
+from typing import Any, Optional
 
 import uvicorn
 from fastapi import FastAPI, Request, Response
-from watchfiles import Change, awatch
 
 from vllm import envs
 from vllm.engine.async_llm_engine import AsyncEngineDeadError
 from vllm.engine.multiprocessing import MQEngineDeadError
+from vllm.entrypoints.ssl import SSLCertRefresher
 from vllm.logger import init_logger
 from vllm.utils import find_process_using_port
 
 logger = init_logger(__name__)
 
 
-async def watch_files(paths, fun: Callable[[Change, str], None]) -> None:
-    """Watch multiple file paths asynchronously."""
-    logger.info("Watching files: %s", paths)
-    async for changes in awatch(*paths):
-        try:
-            for change, file_path in changes:
-                logger.info("File change detected: %s - %s", change.name,
-                            file_path)
-                fun(change, file_path)
-        except Exception as e:
-            logger.error("File watcher failed with error: %s", e)
-
-
-async def serve_http(app: FastAPI, sock: Optional[socket.socket],
+async def serve_http(app: FastAPI,
+                     sock: Optional[socket.socket],
+                     enable_ssl_refresh: bool = False,
                      **uvicorn_kwargs: Any):
     logger.info("Available routes are:")
     for route in app.routes:
@@ -45,6 +34,7 @@ async def serve_http(app: FastAPI, sock: Optional[socket.socket],
         logger.info("Route: %s, Methods: %s", path, ', '.join(methods))
 
     config = uvicorn.Config(app, **uvicorn_kwargs)
+    config.load()
     server = uvicorn.Server(config)
     _add_shutdown_handlers(app, server)
 
@@ -53,32 +43,17 @@ async def serve_http(app: FastAPI, sock: Optional[socket.socket],
     server_task = loop.create_task(
         server.serve(sockets=[sock] if sock else None))
 
-    def update_ssl_cert_chain(change: Change, file_path: str) -> None:
-        logger.info("Reloading SSL certificate chain")
-        config.ssl.load_cert_chain(config.ssl_certfile, config.ssl_keyfile)
-
-    def update_ssl_ca(change: Change, file_path: str) -> None:
-        logger.info("Reloading SSL CA certificates")
-        config.ssl.load_verify_locations(config.ssl_ca_certs)
-
-    watch_ssl_cert_task = None
-    if config.ssl_keyfile and config.ssl_certfile:
-        watch_ssl_cert_task = loop.create_task(
-            watch_files([config.ssl_keyfile, config.ssl_certfile],
-                        update_ssl_cert_chain))
-
-    watch_ssl_ca_task = None
-    if config.ssl_ca_certs:
-        watch_ssl_ca_task = loop.create_task(
-            watch_files([config.ssl_ca_certs], update_ssl_ca))
+    ssl_cert_refresher = None if not enable_ssl_refresh else SSLCertRefresher(
+        ssl_context=config.ssl,
+        key_path=config.ssl_keyfile,
+        cert_path=config.ssl_certfile,
+        ca_path=config.ssl_ca_certs)
 
     def signal_handler() -> None:
         # prevents the uvicorn signal handler to exit early
         server_task.cancel()
-        if watch_ssl_cert_task:
-            watch_ssl_cert_task.cancel()
-        if watch_ssl_ca_task:
-            watch_ssl_ca_task.cancel()
+        if ssl_cert_refresher:
+            ssl_cert_refresher.stop()
 
     async def dummy_shutdown() -> None:
         pass
