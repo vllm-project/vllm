82c8d66a59d00a5868683812460a7f6ffed0ec5e
Yi Liu <yiliu4@habana.ai>
2025-02-22 09:19:16 +0200

    eval bf16 model
    Signed-off-by: Yi Liu <yiliu4@habana.ai>

--- scripts/run_lm_eval.py
+++ scripts/run_lm_eval.py
@@ -7,29 +7,29 @@ import json
 model_path = "/data/models/DeepSeek-R1/"
 #model_path = "/mnt/workdisk/dohayon/Projects/R1/DeepSeek-R1-fp8/"
 # model_path = "deepseek-ai/DeepSeek-V2-Lite"
-
+model_path = "/mnt/disk5/hf_models/DeepSeek-R1-BF16"
 # Parse the command-line arguments.
 parser = argparse.ArgumentParser()
 parser.add_argument("--model", type=str, default=model_path, help="The model path.")
 parser.add_argument("--task", type=str, default="gsm8k", help="The model path.")
 parser.add_argument("--tokenizer", type=str, default=model_path, help="The model path.")
-parser.add_argument("--tp_size", type=int, default=8, help="Tensor Parallelism size.")
-parser.add_argument("--ep_size", type=int, default=4, help="Expert Parallelism size.")
+parser.add_argument("--tp_size", type=int, default=16, help="Tensor Parallelism size.")
+parser.add_argument("--ep_size", type=int, default=16, help="Expert Parallelism size.")
 parser.add_argument("-l", "--limit", type=int, default=16, help="test request counts.")
 args = parser.parse_args()
 
-os.environ["VLLM_SKIP_WARMUP"] = "true"
-os.environ["HABANA_VISIBLE_DEVICES"] = "ALL"
-os.environ["PT_HPU_ENABLE_LAZY_COLLECTIVES"] = "true"
-if args.ep_size > 1:
-    os.environ["VLLM_MOE_N_SLICE"] = "1"
-    os.environ["VLLM_EP_SIZE"] = f"{args.ep_size}"
-else:
-    os.environ["VLLM_MOE_N_SLICE"] = "4"
-    os.environ["VLLM_EP_SIZE"] = "1"
+# os.environ["VLLM_SKIP_WARMUP"] = "true"
+# os.environ["HABANA_VISIBLE_DEVICES"] = "ALL"
+# os.environ["PT_HPU_ENABLE_LAZY_COLLECTIVES"] = "true"
+# if args.ep_size > 1:
+#     os.environ["VLLM_MOE_N_SLICE"] = "1"
+#     os.environ["VLLM_EP_SIZE"] = f"{args.ep_size}"
+# else:
+#     os.environ["VLLM_MOE_N_SLICE"] = "4"
+#     os.environ["VLLM_EP_SIZE"] = "1"
 
-os.environ["VLLM_MLA_DISABLE_REQUANTIZATION"] = "1"
-os.environ["PT_HPU_WEIGHT_SHARING"] = "0"
+# os.environ["VLLM_MLA_DISABLE_REQUANTIZATION"] = "1"
+# os.environ["PT_HPU_WEIGHT_SHARING"] = "0"
 
 if __name__ == "__main__":
 
@@ -51,7 +51,7 @@ if __name__ == "__main__":
             pretrained=model, 
             tokenizer=args.tokenizer,
             tensor_parallel_size=args.tp_size,
-            distributed_executor_backend='mp',
+            distributed_executor_backend='ray',
             trust_remote_code=True,
             max_model_len=4096,
             dtype="bfloat16",
