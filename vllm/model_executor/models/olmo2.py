f279cae620c8aa6a1341d4b15d03f3dd3771752e
Shane A <shanea@allenai.org>
2025-02-21 22:07:45 -0800

    [Bugfix][Model] OLMo 2: split qkv correctly for GQA and MQA (#13687)
    Signed-off-by: Louis Ulmer <ulmerlouis@gmail.com>

--- vllm/model_executor/models/olmo2.py
+++ vllm/model_executor/models/olmo2.py
@@ -157,7 +157,7 @@ class Olmo2Attention(nn.Module):
         attn_metadata: AttentionMetadata,
     ) -> torch.Tensor:
         qkv, _ = self.qkv_proj(hidden_states)
-        q, k, v = qkv.chunk(chunks=3, dim=-1)
+        q, k, v = qkv.split([self.q_size, self.kv_size, self.kv_size], dim=-1)
         q, k = self._apply_qk_norm(q, k)
         q, k = self.rotary_emb(positions, q, k)
         attn_output = self.attn(q, k, v, kv_cache, attn_metadata)
