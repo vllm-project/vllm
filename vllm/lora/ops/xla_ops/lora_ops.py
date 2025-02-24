89b062e568c8feed11f324be8bcd0715b19134c7
Akshat Tripathi <akshat@krai.ai>
2025-02-24 14:53:06 +0000

    Integrated bgmv kernel
    Signed-off-by: Akshat Tripathi <akshat@krai.ai>

--- vllm/lora/ops/xla_ops/lora_ops.py
+++ vllm/lora/ops/xla_ops/lora_ops.py
@@ -8,15 +8,10 @@ def bgmv_expand(inputs: torch.Tensor,
                 output_tensor: torch.Tensor,
                 lora_indices_tensor: torch.Tensor,
                 add_inputs: bool = True):
-    selected_loras = lora_b_weights[lora_indices_tensor].to(
-        dtype=output_tensor.dtype)
-    if len(selected_loras.shape) == 4:
-        selected_loras = selected_loras.squeeze(dim=1)
     inputs = inputs.to(dtype=output_tensor.dtype)
-    # outputs = torch.einsum("bi, boi -> bo", inputs, selected_loras)
-    batch_size, output_size, input_size = selected_loras.shape
-    outputs = (selected_loras @ inputs.reshape(
-        (batch_size, input_size, 1))).reshape((batch_size, output_size))
+
+    batch_size = outputs.size(0)
+    outputs = torch.ops.xla.bgmv(inputs, lora_b_weights, lora_indices_tensor)
 
     limit = output_tensor.shape[0]
     if outputs.shape[0] == 1 and output_tensor.shape[0] != 1:
@@ -52,18 +47,10 @@ def bgmv_expand_slice(inputs: torch.Tensor,
                       slice_size: int,
                       add_inputs: bool = True):
 
-    selected_loras = lora_b_weights[lora_indices_tensor].to(
-        dtype=output_tensor.dtype)
-
     inputs = inputs.to(dtype=output_tensor.dtype)
 
-    if len(selected_loras.shape) == 4:
-        selected_loras = selected_loras.squeeze(dim=1)
-
-    batch_size, output_size, input_size = selected_loras.shape
-
-    outputs = (selected_loras @ inputs.reshape(
-        (batch_size, input_size, 1))).reshape((batch_size, output_size))
+    batch_size = outputs.size(0)
+    outputs = torch.ops.xla.bgmv(inputs, lora_b_weights, lora_indices_tensor)
 
     outputs = torch.cat((
         torch.zeros((batch_size, slice_offset), device=outputs.device),
