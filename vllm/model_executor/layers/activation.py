b85761ebe367f269880e8a2d9d27c2f4a1d23ac7
Ho-Ren (Jack) Chuang <horenchuang@bytedance.com>
2025-02-26 18:53:37 +0800

    [Triton][Kernel] Add Triton SiLUandMul (#13319)
    Implement forward_triton in SiluAndMul (CustomOp), using silu_and_mul.
    
    Signed-off-by: Ho-Ren (Jack) Chuang <horenchuang@bytedance.com>
    Co-authored-by: Tony Zhang <tony.zhang@bytedance.com>

--- vllm/model_executor/layers/activation.py
+++ vllm/model_executor/layers/activation.py
@@ -14,6 +14,41 @@ from vllm.model_executor.utils import set_weight_attrs
 from vllm.platforms import current_platform
 from vllm.utils import LazyDict
 
+import triton
+import triton.language as tl
+
+@triton.jit
+def silu_and_mul_kernel(out_ptr, x_ptr, d: tl.constexpr, d_padded: tl.constexpr):
+    token_idx = tl.program_id(0)
+    idx = tl.arange(0, d_padded)
+
+    # Load A and B from x
+    mask = idx < d  # Mask for values within the original range of d
+    x_a = tl.load(x_ptr + token_idx * 2 * d + idx, mask=mask, other=0.0)
+    x_b = tl.load(x_ptr + token_idx * 2 * d + d + idx, mask=mask, other=0.0)
+
+    # SiLU activation on A and element-wise multiplication with B
+    x_a_fp32 = x_a.to(tl.float32)
+    x_silu = x_a_fp32 / (1.0 + tl.exp(-x_a_fp32))
+    result = x_silu * x_b
+
+    # Write result to output
+    tl.store(out_ptr + token_idx * d + idx, result, mask=mask)
+
+
+class _SiluAndMul(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, out, x):
+        d = x.shape[-1] // 2
+        d_padded = 1 << (d - 1).bit_length()  # Calculate d_padded in Python
+        num_tokens = x.shape[0]
+
+        # Launch the Triton kernel
+        silu_and_mul_kernel[(num_tokens,)](out, x, d=d, d_padded=d_padded)
+
+
+def _silu_and_mul(out, x):
+    _SiluAndMul.apply(out, x)
 
 @CustomOp.register("fatrelu_and_mul")
 class FatreluAndMul(CustomOp):
@@ -95,6 +130,13 @@ class SiluAndMul(CustomOp):
         s = x_reshaped[:, :d] * F.sigmoid(x_reshaped[:, :d])
         result = s * x_reshaped[:, d:]
         return result.view(*x.shape[:-1], d)
+
+    def forward_triton(self, x: torch.Tensor) -> torch.Tensor:
+        d = x.shape[-1] // 2
+        output_shape = (x.shape[:-1] + (d, ))
+        out = torch.empty(output_shape, dtype=x.dtype, device=x.device)
+        _silu_and_mul(out, x)
+        return out
 
 
 @CustomOp.register("mul_and_silu")
