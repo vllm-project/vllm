7787db2a27bd499a2367fae9483682ee1bdb8ca4
Brayden Zhong <b8zhong@uwaterloo.ca>
2025-02-27 03:30:54 +0000

    attempt to retrigger docs: mock w/ loguru
    Signed-off-by: Brayden Zhong <b8zhong@uwaterloo.ca>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -198,29 +198,10 @@ def linkcode_resolve(domain, info):
 
 # Mock out external dependencies here, otherwise the autodoc pages may be blank.
 autodoc_mock_imports = [
-    "blake3",
-    "compressed_tensors",
-    "cpuinfo",
-    "cv2",
-    "torch",
-    "transformers",
-    "psutil",
-    "prometheus_client",
-    "sentencepiece",
-    "vllm._C",
-    "PIL",
-    "numpy",
-    'triton',
-    "tqdm",
-    "tensorizer",
-    "pynvml",
-    "outlines",
-    "xgrammar",
-    "librosa",
-    "soundfile",
-    "gguf",
-    "lark",
-    "decord",
+    "blake3", "compressed_tensors", "cpuinfo", "cv2", "torch", "transformers",
+    "psutil", "prometheus_client", "sentencepiece", "vllm._C", "PIL", "numpy",
+    'triton', "tqdm", "tensorizer", "pynvml", "outlines", "xgrammar",
+    "librosa", "soundfile", "gguf", "lark", "decord", "loguru"
 ]
 
 for mock_target in autodoc_mock_imports:
