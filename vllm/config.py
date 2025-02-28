2188480d356810193041a49aa68fca30e3073022
Tyler Michael Smith <tyler@neuralmagic.com>
2025-02-28 16:36:40 +0000

    Back out reduce_scatter changes
    Signed-off-by: Tyler Michael Smith <tyler@neuralmagic.com>

--- vllm/config.py
+++ vllm/config.py
@@ -3407,7 +3407,7 @@ class VllmConfig:
             # FIXME(woosuk): Disable inductor to reduce the compilation time
             # and avoid any potential issues with the inductor.
             self.compilation_config.custom_ops = ["none"]
-            self.compilation_config.use_cudagraph = False
+            self.compilation_config.use_cudagraph = True
             self.compilation_config.use_inductor = True
             self.compilation_config.cudagraph_num_of_warmups = 1
             self.compilation_config.pass_config.enable_fusion = False
