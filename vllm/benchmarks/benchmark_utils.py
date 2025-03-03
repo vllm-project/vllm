ff38453528814e578b7270c2dfbbdabf1bc8af19
Randy Chen <acad.randyjhc@gmail.com>
2025-03-03 15:35:35 -0800

    Revert "Compacted utility functions (#13840)"
    This reverts commit d5bc88b4cb58779818c2740d9444a38cd05e6ee9.
    
    Signed-off-by: Randy Chen <acad.randyjhc@gmail.com>

--- vllm/benchmarks/benchmark_utils.py
+++ vllm/benchmarks/benchmark_utils.py
@@ -49,6 +49,21 @@ def convert_to_pytorch_benchmark_format(args: argparse.Namespace,
     return records
 
 
+class InfEncoder(json.JSONEncoder):
+
+    def clear_inf(self, o: Any):
+        if isinstance(o, dict):
+            return {k: self.clear_inf(v) for k, v in o.items()}
+        elif isinstance(o, list):
+            return [self.clear_inf(v) for v in o]
+        elif isinstance(o, float) and math.isinf(o):
+            return "inf"
+        return o
+
+    def iterencode(self, o: Any, *args, **kwargs) -> Any:
+        return super().iterencode(self.clear_inf(o), *args, **kwargs)
+
+
 def write_to_json(filename: str, records: list) -> None:
     with open(filename, "w") as f:
         json.dump(records, f, cls=InfEncoder)
