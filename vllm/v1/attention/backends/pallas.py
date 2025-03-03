1ce8b587f2122a376e2a71526558309301885f98
Michael Goin <mgoin64@gmail.com>
2025-03-03 13:44:07 -0700

    Fix comment

--- vllm/v1/attention/backends/pallas.py
+++ vllm/v1/attention/backends/pallas.py
@@ -155,6 +155,9 @@ class PallasAttentionBackendImpl(AttentionImpl):
             write_to_kv_cache(key, value, key_cache, value_cache, slot_mapping)
 
         query = query * self.scale
+        # use_kernel switches between using kernel or reference implementation
+        # (non kernel: https://github.com/pytorch/xla/blob/cee0820e78fc9675e2d0511db891fd44342e890d/torch_xla/experimental/custom_kernel.py#L890).
+        use_kernel = False
         output = torch.ops.xla.ragged_paged_attention(
             query,
             key_cache,
@@ -165,9 +168,7 @@ class PallasAttentionBackendImpl(AttentionImpl):
             attn_metadata.num_seqs,
             num_kv_pages_per_block=NUM_KV_PAGES_PER_BLOCK,
             num_queries_per_block=NUM_QUERIES_PER_BLOCK,
-            # use_kernel switches between using kernel or the reference implementation
-            # (non kernel: https://github.com/pytorch/xla/blob/cee0820e78fc9675e2d0511db891fd44342e890d/torch_xla/experimental/custom_kernel.py#L890).
-            use_kernel=False,
+            use_kernel=use_kernel,
         )
 
         return output.reshape(num_tokens, hidden_size)
