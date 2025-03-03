5fecb39c860038d737815823bf07f73565faf537
noooop <noooop@126.com>
2025-03-03 14:49:37 +0800

    model_overwrite

--- vllm/envs.py
+++ vllm/envs.py
@@ -96,6 +96,7 @@ if TYPE_CHECKING:
     VLLM_DP_SIZE: int = 1
     VLLM_DP_MASTER_IP: str = ""
     VLLM_DP_MASTER_PORT: int = 0
+    VLLM_MODEL_OVERWRITE_PATH: Optional[str] = None
 
 
 def get_default_cache_root():
@@ -630,6 +631,11 @@ environment_variables: dict[str, Callable[[], Any]] = {
     # Whether to use S3 path for model loading in CI via RunAI Streamer
     "VLLM_CI_USE_S3":
     lambda: os.environ.get("VLLM_CI_USE_S3", "0") == "1",
+
+    # Use model_overwrite to redirect the model name to a local folder
+    # while keeping the model name in the test file without being hardcoded.
+    "VLLM_MODEL_OVERWRITE_PATH":
+    lambda: os.environ.get("VLLM_MODEL_OVERWRITE_PATH", None),
 }
 
 # end-env-vars-definition
