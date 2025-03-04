e09841c7d6a0383376e8d88c8a13180d1a10f003
Lucas Wilkinson <lwilkins@redhat.com>
2025-03-04 21:25:11 +0000

    bugfix
    Signed-off-by: Lucas Wilkinson <lwilkins@redhat.com>

--- vllm/v1/attention/backends/mla/common.py
+++ vllm/v1/attention/backends/mla/common.py
@@ -382,7 +382,7 @@ class MLACommonMetadata(Generic[D]):
 
         # Pre-compute prefill/decode tensor slices and other stats
         if self.num_prefills is not None and self.num_prefills > 0:
-            assert self.num_decodes is not None and self.num_decodes > 0
+            assert self.num_decodes is not None
             start = self.num_decodes  # prefill_start
             self.prefill_query_start_loc = \
                 self.query_start_loc[start:] - self.query_start_loc[start]
