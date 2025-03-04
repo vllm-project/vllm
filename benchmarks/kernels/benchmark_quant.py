682da0658216a9e8aef1e26cd3c85874b35181b2
Brayden Zhong <b8zhong@uwaterloo.ca>
2025-03-04 03:01:32 +0000

    fix: remove double instance of cudaProfilerStart()
    Signed-off-by: Brayden Zhong <b8zhong@uwaterloo.ca>

--- benchmarks/kernels/benchmark_quant.py
+++ benchmarks/kernels/benchmark_quant.py
@@ -40,7 +40,7 @@ def main(num_tokens: int,
 
         end_time = time.perf_counter()
         if profile:
-            torch.cuda.cudart().cudaProfilerStart()
+            torch.cuda.cudart().cudaProfilerStop()
         return (end_time - start_time) / num_iters
 
     # Warmup.
