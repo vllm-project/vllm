3c78e5d7ff9544a146129f66b5522ec3c687ec81
Huy Do <huydhn@gmail.com>
2025-03-05 10:42:39 -0800

    [Benchmark] Unset best_of when running v1 benchmark
    The check is introduced by https://github.com/vllm-project/vllm/pull/14159
    and it will fail v1 benchmark with `VLLM V1 does not yet support
    best_of` error as the parameter is not yet supported
    
    Signed-off-by: Huy Do <huydhn@gmail.com>

--- benchmarks/backend_request_func.py
+++ benchmarks/backend_request_func.py
@@ -5,7 +5,7 @@ import os
 import sys
 import time
 import traceback
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, make_dataclass
 from typing import Optional, Union
 
 import aiohttp
@@ -18,20 +18,24 @@ from vllm.model_executor.model_loader.weight_utils import get_lock
 
 AIOHTTP_TIMEOUT = aiohttp.ClientTimeout(total=6 * 60 * 60)
 
-
-@dataclass
-class RequestFuncInput:
-    prompt: str
-    api_url: str
-    prompt_len: int
-    output_len: int
-    model: str
-    model_name: Optional[str] = None
-    best_of: int = 1
-    logprobs: Optional[int] = None
-    extra_body: Optional[dict] = None
-    multi_modal_content: Optional[dict] = None
-    ignore_eos: bool = False
+RequestFuncInput = make_dataclass(
+    "RequestFuncInput",
+    [
+        ("prompt", str),
+        ("api_url", str),
+        ("prompt_len", int),
+        ("output_len", int),
+        ("model", str),
+        ("model_name", Optional[str], field(default=None)),
+        ("logprobs", Optional[int], field(default=None)),
+        ("extra_body", Optional[dict], field(default=None)),
+        ("multi_modal_content", Optional[dict], field(default=None)),
+        ("ignore_eos", bool, field(default=False)),
+    ].extend(
+        # From https://github.com/vllm-project/vllm/pull/14159, v1 doesn't yet
+        # support best_of parameter
+        [] if os.environ.get("VLLM_USE_V1", 0) else [("best_of", int,
+                                                      field(default=1))]))
 
 
 @dataclass
