c2b3649b33fa14c9fd06fe76ca64e2eea5a84268
Michael Goin <mgoin64@gmail.com>
2025-03-05 03:30:53 +0000

    [TPU][V1] Capture multimodal encoder during model compilation
    Signed-off-by: Michael Goin <mgoin64@gmail.com>

--- vllm/v1/worker/tpu_worker.py
+++ vllm/v1/worker/tpu_worker.py
@@ -156,8 +156,10 @@ class TPUWorker:
             self.vllm_config.compilation_config.static_forward_context,
             runner_kv_caches)
 
-        self.model_runner._dummy_run(
-            self.scheduler_config.max_num_batched_tokens)
+        self.model_runner.profile_run(
+            runner_kv_caches,
+            num_tokens=self.scheduler_config.max_num_batched_tokens,
+        )
 
         # Synchronize before measuring the memory usage.
         xm.wait_device_ops()
