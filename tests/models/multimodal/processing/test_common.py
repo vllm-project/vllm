582d1b43555c5f2ac6af52b16420f39bf1ef05cd
kYLe <kylhuang@nvidia.com>
2025-03-06 02:31:38 -0600

    [Model] Update Paligemma multimodal processing with PromptUpdate  (#14015)
    Signed-off-by: Kyle Huang <kylhuang@nvidia.com>
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>
    Co-authored-by: Cyrus Leung <tlleungac@connect.ust.hk>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- tests/models/multimodal/processing/test_common.py
+++ tests/models/multimodal/processing/test_common.py
@@ -175,6 +175,8 @@ def _test_processing_correctness(
     "Qwen/Qwen2-Audio-7B-Instruct",
     "fixie-ai/ultravox-v0_4",
     "openai/whisper-large-v3",
+    "google/paligemma-3b-mix-224",
+    "google/paligemma2-3b-ft-docci-448",
 ])
 @pytest.mark.parametrize("hit_rate", [0.3, 0.5, 1.0])
 @pytest.mark.parametrize("num_batches", [32])
