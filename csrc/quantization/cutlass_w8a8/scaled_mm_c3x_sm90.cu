512b0cd645c71b6e7ecf7d7eaa87a142c8d40949
Lucas Wilkinson <LucasWilkinson@users.noreply.github.com>
2025-03-08 03:11:56 -0500

    [Build/BugFix] Fix hopper 12.8 build (#14354)
    Signed-off-by: Lucas Wilkinson <lwilkins@redhat.com>
    Signed-off-by: Lucas Wilkinson <lwilkinson@neuralmagic.com>
    Signed-off-by: Tyler Michael Smith <tyler@neuralmagic.com>
    Co-authored-by: Tyler Michael Smith <tyler@neuralmagic.com>
    Signed-off-by: Louis Ulmer <ulmerlouis@gmail.com>

--- csrc/quantization/cutlass_w8a8/scaled_mm_c3x_sm90.cu
+++ csrc/quantization/cutlass_w8a8/scaled_mm_c3x_sm90.cu
@@ -5,8 +5,10 @@
 
 /*
    This file defines quantized GEMM operations using the CUTLASS 3.x API, for
-   NVIDIA GPUs with sm90a (Hopper) or later.
+   NVIDIA GPUs with sm90a (Hopper).
 */
+
+#if defined ENABLE_SCALED_MM_SM90 && ENABLE_SCALED_MM_SM90
 
 void cutlass_scaled_mm_sm90(torch::Tensor& c, torch::Tensor const& a,
                             torch::Tensor const& b,
@@ -70,29 +72,6 @@ void cutlass_scaled_mm_azp_sm90(torch::Tensor& out, torch::Tensor const& a,
 
   vllm::cutlass_scaled_mm_azp_sm90_int8(out, a, b, a_scales, b_scales, azp_adj,
                                         azp, bias);
-}
-
-#if defined CUDA_VERSION && CUDA_VERSION >= 12080
-
-void cutlass_scaled_mm_sm100(torch::Tensor& c, torch::Tensor const& a,
-                             torch::Tensor const& b,
-                             torch::Tensor const& a_scales,
-                             torch::Tensor const& b_scales,
-                             std::optional<torch::Tensor> const& bias) {
-  TORCH_CHECK(a_scales.dtype() == torch::kFloat32);
-  TORCH_CHECK(b_scales.dtype() == torch::kFloat32);
-
-  int M = a.size(0), N = b.size(1), K = a.size(1);
-  TORCH_CHECK(
-      (a_scales.numel() == 1 || a_scales.numel() == a.size(0)) &&
-          (b_scales.numel() == 1 || b_scales.numel() == b.size(1)),
-      "Currently, block scaled fp8 gemm is not implemented for Blackwell");
-
-  // Standard per-tensor/per-token/per-channel scaling
-  TORCH_CHECK(a_scales.is_contiguous() && b_scales.is_contiguous());
-  TORCH_CHECK(a.dtype() == torch::kFloat8_e4m3fn,
-              "Currently, only fp8 gemm is implemented for Blackwell");
-  vllm::cutlass_scaled_mm_sm100_fp8(c, a, b, a_scales, b_scales, bias);
 }
 
 #endif
