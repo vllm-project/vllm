83ee6d5b0b529ba88785d51fbcdd9bad5b9c11f2
rshaw@neuralmagic.com <rshaw@neuralmagic.com>
2025-03-08 10:47:02 +0000

    cleaning
    Signed-off-by: rshaw@neuralmagic.com <rshaw@neuralmagic.com>

--- vllm/attention/layer.py
+++ vllm/attention/layer.py
@@ -71,15 +71,6 @@ class Attention(nn.Module):
             block_size = 16
             is_attention_free = False
             calculate_kv_scales = False
-            if not envs.is_set("VLLM_USE_V1"):
-                raise NotImplementedError(
-                    "VLLM_USE_V1 is not set and your model does "
-                    "not pass a CacheConfig to the Attention layer. "
-                    "As a result, we cannot detect whether to use V0 "
-                    "or V1 attention. Please set VLLM_USE_V1=0 or "
-                    "VLLM_USE_V1=1 and file an issue on Github so "
-                    "we can address it.")
-
         if num_kv_heads is None:
             num_kv_heads = num_heads
 
