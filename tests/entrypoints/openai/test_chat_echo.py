28e6b5cee14e98ed98059e9901aaa3dbd51c45d9
Cyrus Leung <tlleungac@connect.ust.hk>
2025-03-08 19:30:09 +0800

    [CI/Build] Use a fixed seed to avoid flaky tests (#14480)
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>
    Signed-off-by: Louis Ulmer <ulmerlouis@gmail.com>

--- tests/entrypoints/openai/test_chat_echo.py
+++ tests/entrypoints/openai/test_chat_echo.py
@@ -24,8 +24,6 @@ def server():
         "4080",
         "--chat-template",
         DUMMY_CHAT_TEMPLATE,
-        "--seed",
-        "0",
     ]
 
     with RemoteOpenAIServer(MODEL_NAME, args) as remote_server:
