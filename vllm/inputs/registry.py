91b304d0646bbdb63b8a4a491e2727745ba5f3f2
Isotr0py <mozf@mail2.sysu.edu.cn>
2025-03-09 00:52:34 +0800

    [Bugfix] Fix profiling OOM and decouple encoder multimodal profiling (#14361)
    Signed-off-by: Isotr0py <2037008807@qq.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/inputs/registry.py
+++ vllm/inputs/registry.py
@@ -335,8 +335,10 @@ class InputRegistry:
                                                      tokenizer,
                                                      disable_cache=True)
             profiler = MultiModalProfiler(processor)
-            dummy_data = profiler.get_dummy_data(
-                seq_len, is_encoder_data=is_encoder_data)
+            dummy_data_factory = (profiler.get_encoder_dummy_data
+                                  if is_encoder_data else
+                                  profiler.get_decoder_dummy_data)
+            dummy_data = dummy_data_factory(seq_len)
         else:
             model_cls, _ = get_model_architecture(model_config)
             if is_encoder_data:
