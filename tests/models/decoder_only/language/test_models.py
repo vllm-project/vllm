1dd14479199834083a26b754ddcc9812e3b358ed
rshaw@neuralmagic.com <rshaw@neuralmagic.com>
2025-03-09 16:37:24 +0000

    fixed language model test
    Signed-off-by: rshaw@neuralmagic.com <rshaw@neuralmagic.com>

--- tests/models/decoder_only/language/test_models.py
+++ tests/models/decoder_only/language/test_models.py
@@ -93,13 +93,6 @@ def test_models(
         vllm_outputs = vllm_model.generate_greedy_logprobs(
             example_prompts, max_tokens, num_logprobs)
 
-        # This test is for verifying whether the model's extra_repr
-        # can be printed correctly.
-        def print_model(model):
-            print(model)
-
-        vllm_model.apply_model(print_model)
-
     check_logprobs_close(
         outputs_0_lst=hf_outputs,
         outputs_1_lst=vllm_outputs,
