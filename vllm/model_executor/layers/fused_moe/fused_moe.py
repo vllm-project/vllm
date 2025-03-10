4b740b0d28bd623f58dfe9e4e45ce2ae638fe3e7
Jinzhen Lin <linjinzhen@hotmail.com>
2025-03-11 01:29:45 +0800

    update condition
    Signed-off-by: Jinzhen Lin <linjinzhen@hotmail.com>

--- vllm/model_executor/layers/fused_moe/fused_moe.py
+++ vllm/model_executor/layers/fused_moe/fused_moe.py
@@ -940,7 +940,7 @@ def get_moe_wna16_block_config(config: Dict[str,
 def should_moe_wna16_use_cuda(num_valid_tokens: int, group_size: int,
                               num_experts: int, bit: int):
     return bit == 4 and group_size in [32, 64, 128] and \
-        num_valid_tokens / num_experts <= 8
+        num_valid_tokens / num_experts <= 6
 
 
 def get_default_config(
