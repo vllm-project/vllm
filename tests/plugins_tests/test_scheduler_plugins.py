3ab158f87d280872b91a66b33ee2f524de701529
Joe Runde <Joseph.Runde@ibm.com>
2025-03-10 14:56:48 -0600

    :white_check_mark: run v1 scheduler plugin test in-process
    Signed-off-by: Joe Runde <Joseph.Runde@ibm.com>

--- tests/plugins_tests/test_scheduler_plugins.py
+++ tests/plugins_tests/test_scheduler_plugins.py
@@ -1,7 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
-from unittest import mock
-
 import pytest
 
 from vllm.core.scheduler import Scheduler
@@ -12,26 +10,26 @@ from vllm.v1.core.scheduler import Scheduler as V1Scheduler
 from vllm.v1.engine.llm_engine import LLMEngine as V1LLMEngine
 
 
-class DummyScheduler(Scheduler):
+class DummyV0Scheduler(Scheduler):
 
     def schedule(self):
-        raise Exception("Exception raised by DummyScheduler")
+        raise Exception("Exception raised by DummyV0Scheduler")
 
 
 class DummyV1Scheduler(V1Scheduler):
 
     def schedule(self):
-        raise Exception("Exception raised by DummyScheduler")
+        raise Exception("Exception raised by DummyV1Scheduler")
 
 
-def test_scheduler_plugins(monkeypatch):
+def test_scheduler_plugins_v0(monkeypatch):
     monkeypatch.setenv("VLLM_USE_V1", "0")
     with pytest.raises(Exception) as exception_info:
 
         engine_args = EngineArgs(
             model="facebook/opt-125m",
             enforce_eager=True,  # reduce test time
-            scheduler_cls=DummyScheduler,
+            scheduler_cls=DummyV0Scheduler,
         )
 
         engine = LLMEngine.from_engine_args(engine_args=engine_args)
@@ -40,31 +38,27 @@ def test_scheduler_plugins(monkeypatch):
         engine.add_request("0", "foo", sampling_params)
         engine.step()
 
-    assert str(exception_info.value) == "Exception raised by DummyScheduler"
+    assert str(exception_info.value) == "Exception raised by DummyV0Scheduler"
 
 
 def test_scheduler_plugins_v1(monkeypatch):
     monkeypatch.setenv("VLLM_USE_V1", "1")
+    # Explicitly turn off engine multiprocessing so that the scheduler runs in
+    # this process
+    monkeypatch.setenv("VLLM_ENABLE_V1_MULTIPROCESSING", "0")
 
-    # V1 engine has more redirection- the worker process will raise with our
-    # dummy scheduler error but then client in this process will try to kill
-    # the process tree when the worker fails.
-    with mock.patch(
-            "vllm.v1.engine.core_client.kill_process_tree") as mock_kill:
-        mock_kill.side_effect = Exception("kill_process_tree was called")
+    with pytest.raises(Exception) as exception_info:
 
-        with pytest.raises(Exception) as exception_info:
+        engine_args = EngineArgs(
+            model="facebook/opt-125m",
+            enforce_eager=True,  # reduce test time
+            scheduler_cls=DummyV1Scheduler,
+        )
 
-            engine_args = EngineArgs(
-                model="facebook/opt-125m",
-                enforce_eager=True,  # reduce test time
-                scheduler_cls=DummyV1Scheduler,
-            )
+        engine = V1LLMEngine.from_engine_args(engine_args=engine_args)
 
-            engine = V1LLMEngine.from_engine_args(engine_args=engine_args)
+        sampling_params = SamplingParams(max_tokens=1)
+        engine.add_request("0", "foo", sampling_params)
+        engine.step()
 
-            sampling_params = SamplingParams(max_tokens=1)
-            engine.add_request("0", "foo", sampling_params)
-            engine.step()
-
-    assert str(exception_info.value) == "kill_process_tree was called"
+    assert str(exception_info.value) == "Exception raised by DummyV1Scheduler"
