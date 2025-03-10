e696144886b7c15aa5b2548f0c5deb72112c6039
Akshat Tripathi <akshat@krai.ai>
2025-03-10 15:57:00 +0000

    Fixed bug with higher batch sizes
    Signed-off-by: Akshat Tripathi <akshat@krai.ai>

--- vllm/v1/worker/tpu_model_runner.py
+++ vllm/v1/worker/tpu_model_runner.py
@@ -438,8 +438,8 @@ class TPUModelRunner(LoRAModelRunnerMixin):
         seq_lens = self.seq_lens_cpu[:self.max_num_reqs].to(self.device)
 
         if self.lora_config is not None:
-            self.set_active_loras(self.input_batch,
-                                  np.array(padded_total_num_scheduled_tokens))
+            self.set_active_loras(self.input_batch, 
+                                  num_scheduled_tokens_per_req)
 
         attn_metadata = PallasMetadata(
             slot_mapping=slot_mapping,
