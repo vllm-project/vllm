c8d04be662f2fbd6fb6467e77fd52ba15c17a997
Chendi Xue <chendi.xue@intel.com>
2025-03-10 21:17:02 +0000

    update profile.sh
    Signed-off-by: Chendi Xue <chendi.xue@intel.com>

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cur_path=$(pwd)
-HABANA_PROFILE_WRITE_HLTV=1 HABANA_PROFILE=profile_api \
-VLLM_PT_PROFILE=decode_128_2048_t \
-python ${cur_path}/run_example_tp.py
+HABANA_PROFILE_WRITE_HLTV=1 HABANA_PROFILE=1 \
+HABANA_PROF_CONFIG=${cur_path}/profile_api_trace_analyzer.json \
+VLLM_PT_PROFILE=decode_16_4000_t \
+python ${cur_path}/run_hpu_example.py
