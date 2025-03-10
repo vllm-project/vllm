8525578b0fa2ffbb96eda1316a47ac38c167c954
Caleb_Du <Caleb_Du@zju.edu.cn>
2025-03-10 02:25:44 -0900

    implement moe permute kernel
    Signed-off-by: Caleb_Du <Caleb_Du@zju.edu.cn>

---
+++ csrc/moe/permute_unpermute_kernels/dispatch.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#define MOE_SWITCH(TYPE, ...)                       \
+  at::ScalarType _st = ::detail::scalar_type(TYPE); \
+  switch (_st) {                                    \
+    __VA_ARGS__                                     \
+    default:                                        \
+      TORCH_CHECK(false, "dispatch fail!")          \
+  }
+
+#define MOE_DISPATCH_CASE(enum_type, ...)                  \
+  case enum_type: {                                        \
+    std::cout << "switch:" << enum_type << std::endl;      \
+    using scalar_t = ScalarType2CudaType<enum_type>::type; \
+    return __VA_ARGS__();                                  \
+  }
+#define MOE_DISPATCH_FLOAT_CASE(...)                          \
+  MOE_DISPATCH_CASE(at::ScalarType::Float, __VA_ARGS__)       \
+  MOE_DISPATCH_CASE(at::ScalarType::Half, __VA_ARGS__)        \
+  MOE_DISPATCH_CASE(at::ScalarType::BFloat16, __VA_ARGS__)    \
+  MOE_DISPATCH_CASE(at::ScalarType::Float8_e5m2, __VA_ARGS__) \
+  MOE_DISPATCH_CASE(at::ScalarType::Float8_e4m3fn, __VA_ARGS__)
+
+#define MOE_DISPATCH(TYPE, ...) \
+  MOE_SWITCH(TYPE, MOE_DISPATCH_FLOAT_CASE(__VA_ARGS__))
---
+++ vllm/model_executor/layers/fused_moe/moe_permute_unpermute.py
@@ -0,0 +1,44 @@
+import torch
+from typing import List
+from vllm import _custom_ops as ops
+from vllm.model_executor.layers.fused_moe.fused_moe import fused_topk
+
+
+def moe_permute(
+    hidden_states: torch.Tensor,
+    topk_weights: torch.Tensor,
+    topk_ids: torch.Tensor,
+    token_expert_indices: torch.Tensor,
+    topk: int,
+    n_expert: int,
+) -> List[torch.Tensor]:
+    n_token, n_hidden = hidden_states.shape
+    permuted_hidden_states = torch.empty(
+        (n_token * topk, n_hidden),
+        dtype=hidden_states.dtype,
+        device=hidden_states.device,
+    )
+    expert_first_token_offset = torch.empty(
+        n_expert + 1, dtype=torch.int64, device=hidden_states.device
+    )
+    src_row_id2dst_row_id_map = torch.empty(
+        (n_token, topk), dtype=torch.int32, device=hidden_states.device
+    )
+    torch.ops._moe_C.moe_permute(
+        hidden_states,
+        topk_weights,
+        topk_ids,
+        token_expert_indices,
+        n_expert,
+        topk,
+        permuted_hidden_states,
+        expert_first_token_offset,
+        src_row_id2dst_row_id_map,
+    )
+    return [
+        permuted_hidden_states,
+        expert_first_token_offset,
+        src_row_id2dst_row_id_map,
+    ]
+
+
