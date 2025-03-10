45884cf0a36524daebc98926bd0fe5cdf82edf4b
Caleb_Du <Caleb_Du@zju.edu.cn>
2025-03-10 04:37:42 -0900

    implement moe unpermute
    Signed-off-by: Caleb_Du <Caleb_Du@zju.edu.cn>

--- csrc/moe/moe_permute_unpermute_op.cu
+++ csrc/moe/moe_permute_unpermute_op.cu
@@ -5,7 +5,7 @@
 #include "core/registration.h"
 
 void moe_permute(torch::Tensor& input,                  // [n_token, hidden]
-                 torch::Tensor& topk_weight,            //[n_token, topk]
+                 torch::Tensor& topk_weights,           //[n_token, topk]
                  torch::Tensor& topk_ids,               // [n_token, topk]
                  torch::Tensor& token_expert_indicies,  // [n_token, topk]
                  int64_t n_expert, int64_t topk,
@@ -13,8 +13,8 @@ void moe_permute(torch::Tensor& input,                  // [n_token, hidden]
                  torch::Tensor& expert_first_token_offset,    // [expert + 1]
                  torch::Tensor& src_row_id2dst_row_id_map) {  // [n_token, topk]
 
-  TORCH_CHECK(topk_weight.scalar_type() == at::ScalarType::Float,
-              "topk_weight must be float32");
+  TORCH_CHECK(topk_weights.scalar_type() == at::ScalarType::Float,
+              "topk_weights must be float32");
   TORCH_CHECK(expert_first_token_offset.scalar_type() == at::ScalarType::Long,
               "expert_first_token_offset must be int64");
   TORCH_CHECK(topk_ids.scalar_type() == at::ScalarType::Int,
@@ -54,13 +54,44 @@ void moe_permute(torch::Tensor& input,                  // [n_token, hidden]
   MOE_DISPATCH(input.scalar_type(), [&] {
     expandInputRowsKernelLauncher<scalar_t>(
         get_ptr<scalar_t>(input), get_ptr<scalar_t>(permuted_input),
-        get_ptr<float>(topk_weight), nullptr,
+        get_ptr<float>(topk_weights), nullptr,
         get_ptr<int>(dst_row_id2src_row_id_map),
         get_ptr<int>(src_row_id2dst_row_id_map), n_token, nullptr, n_hidden,
         topk, stream);
   });
 }
 
+void moe_unpermute(
+    torch::Tensor& permuted_hidden_states,     // [n_token * topk, hidden]
+    torch::Tensor& topk_weights,               //[n_token, topk]
+    torch::Tensor& topk_ids,                   // [n_token, topk]
+    torch::Tensor& src_row_id2dst_row_id_map,  // [n_token, topk]
+    int64_t n_expert, int64_t topk,
+    torch::Tensor& hidden_states  // [n_token, hidden]
+) {
+  TORCH_CHECK(src_row_id2dst_row_id_map.sizes() == topk_ids.sizes(),
+              "topk_ids shape must be same as src_row_id2dst_row_id_map");
+  TORCH_CHECK(topk_ids.scalar_type() == at::ScalarType::Int,
+              "topk_ids must be int32");
+  TORCH_CHECK(
+      permuted_hidden_states.scalar_type() == hidden_states.scalar_type(),
+      "topk_ids dtype must be same as src_row_id2dst_row_id_map");
+  TORCH_CHECK(permuted_hidden_states.size(0) == hidden_states.size(0) * topk,
+              "permuted_hidden_states must be [n_token * topk, n_hidden],"
+              "hidden_states must be [n_token, n_hidden]");
+  auto n_token = hidden_states.size(0);
+  auto n_hidden = hidden_states.size(1);
+  auto stream = at::cuda::getCurrentCUDAStream().stream();
+  MOE_DISPATCH(hidden_states.scalar_type(), [&] {
+    finalizeMoeRoutingKernelLauncher<scalar_t, scalar_t>(
+        get_ptr<scalar_t>(permuted_hidden_states),
+        get_ptr<scalar_t>(hidden_states), get_ptr<float>(topk_weights),
+        get_ptr<int>(src_row_id2dst_row_id_map), get_ptr<int>(topk_ids),
+        n_token, n_hidden, topk, nullptr, stream);
+  });
+}
+
 TORCH_LIBRARY_IMPL_EXPAND(TORCH_EXTENSION_NAME, CUDA, m) {
   m.impl("moe_permute", &moe_permute);
+  m.impl("moe_unpermute", &moe_unpermute);
 }
