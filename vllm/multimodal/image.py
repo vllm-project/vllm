afe93ce6115e83e25787aaaae454eded75602159
Chauncey <chaunceyjiang@gmail.com>
2025-03-10 20:36:03 +0800

    [Frontend] support image embeds (#13955)
    Signed-off-by: chaunceyjiang <chaunceyjiang@gmail.com>
    Signed-off-by: Louis Ulmer <ulmerlouis@gmail.com>

--- vllm/multimodal/image.py
+++ vllm/multimodal/image.py
@@ -134,3 +134,22 @@ class ImageMediaIO(MediaIO[Image.Image]):
             data = buffer.getvalue()
 
         return base64.b64encode(data).decode('utf-8')
+
+
+class ImageEmbeddingMediaIO(MediaIO[torch.Tensor]):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def load_bytes(self, data: bytes) -> torch.Tensor:
+        buffer = BytesIO(data)
+        return torch.load(buffer, weights_only=True)
+
+    def load_base64(self, media_type: str, data: str) -> torch.Tensor:
+        return self.load_bytes(base64.b64decode(data))
+
+    def load_file(self, filepath: Path) -> torch.Tensor:
+        return torch.load(filepath)
+
+    def encode_base64(self, media: torch.Tensor) -> str:
+        return base64.b64encode(media.numpy()).decode('utf-8')
