6da930d28c9c372ae1f97527898bcbe38ba78f5a
Cyrus Leung <tlleungac@connect.ust.hk>
2025-03-11 22:59:43 +0800

    [Bugfix] Update `--hf-overrides` for `Alibaba-NLP/gte-Qwen2` (#14609)
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>
    Signed-off-by: Mu Huai <tianbowen.tbw@antgroup.com>

--- tests/models/embedding/language/test_embedding.py
+++ tests/models/embedding/language/test_embedding.py
@@ -42,8 +42,8 @@ def test_models(
     if model == "ssmits/Qwen2-7B-Instruct-embed-base":
         vllm_extra_kwargs["override_pooler_config"] = \
             PoolerConfig(pooling_type="MEAN")
-    if model == "Alibaba-NLP/gte-Qwen2-7B-instruct":
-        vllm_extra_kwargs["hf_overrides"] = {"is_causal": False}
+    if model == "Alibaba-NLP/gte-Qwen2-1.5B-instruct":
+        vllm_extra_kwargs["hf_overrides"] = {"is_causal": True}
 
     # The example_prompts has ending "\n", for example:
     # "Write a short story about a robot that dreams for the first time.\n"
