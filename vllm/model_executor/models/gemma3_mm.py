f30789a701e377ed1901fabaf25f145b80a23f1a
DarkLight1337 <tlleungac@connect.ust.hk>
2025-03-13 07:22:33 +0000

    Fix
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- vllm/model_executor/models/gemma3_mm.py
+++ vllm/model_executor/models/gemma3_mm.py
@@ -235,7 +235,7 @@ class Gemma3DummyInputsBuilder(BaseDummyInputsBuilder[Gemma3ProcessingInfo]):
         }
 
         return ProcessorInputs(
-            prompt_text=image_token * num_images,
+            prompt_text=" ".join([image_token] * num_images),
             mm_data=mm_data,
         )
 
