be625bf94f53f9ba0f653974d73f2053935bac13
qscqesze <475517977@qq.com>
2025-03-14 00:47:38 +0800

    [Refactor][LightningAttention] Improve code readability and consistency by restructuring pointer calculations and simplifying expressions in the lightning attention implementation.
    Signed-off-by: qscqesze <475517977@qq.com>

--- vllm/model_executor/layers/lightning_attn.py
+++ vllm/model_executor/layers/lightning_attn.py
@@ -1,9 +1,7 @@
-# SPDX-License-Identifier: Apache-2.0
 import torch
 import triton
 import triton.language as tl
 from einops import rearrange
-
 
 @triton.jit
 def _fwd_diag_kernel(
@@ -42,18 +40,36 @@ def _fwd_diag_kernel(
     q_cblock_offset = cblock_offset * d
     o_cblock_offset = cblock_offset * e
 
-    Q_block_ptr = (Q + qk_offset + qk_block_offset + q_cblock_offset +
-                   tl.arange(0, CBLOCK)[:, None] * d +
-                   tl.arange(0, d)[None, :])
-    K_trans_block_ptr = (K + qk_offset + qk_block_offset +
-                         tl.arange(0, CBLOCK)[None, :] * d +
-                         tl.arange(0, d)[:, None])
-    V_block_ptr = (V + v_offset + v_block_offset +
-                   tl.arange(0, CBLOCK)[:, None] * e +
-                   tl.arange(0, e)[None, :])
-    O_block_ptr = (Out + o_offset + o_block_offset + o_cblock_offset +
-                   tl.arange(0, CBLOCK)[:, None] * e +
-                   tl.arange(0, e)[None, :])
+    Q_block_ptr = (
+        Q
+        + qk_offset
+        + qk_block_offset
+        + q_cblock_offset
+        + tl.arange(0, CBLOCK)[:, None] * d
+        + tl.arange(0, d)[None, :]
+    )
+    K_trans_block_ptr = (
+        K
+        + qk_offset
+        + qk_block_offset
+        + tl.arange(0, CBLOCK)[None, :] * d
+        + tl.arange(0, d)[:, None]
+    )
+    V_block_ptr = (
+        V
+        + v_offset
+        + v_block_offset
+        + tl.arange(0, CBLOCK)[:, None] * e
+        + tl.arange(0, e)[None, :]
+    )
+    O_block_ptr = (
+        Out
+        + o_offset
+        + o_block_offset
+        + o_cblock_offset
+        + tl.arange(0, CBLOCK)[:, None] * e
+        + tl.arange(0, e)[None, :]
+    )
 
     S_block_ptr = S + off_h
     s = tl.load(S_block_ptr)
@@ -61,9 +77,9 @@ def _fwd_diag_kernel(
     i = off_cblock
     q_index = tl.arange(0, CBLOCK) + i * CBLOCK
 
-    q = tl.load(Q_block_ptr,
-                mask=block_offset + q_index[:, None] < n,
-                other=0.0).to(tl.float32)
+    q = tl.load(Q_block_ptr, mask=block_offset + q_index[:, None] < n, other=0.0).to(
+        tl.float32
+    )
 
     qkv = tl.zeros([CBLOCK, e], dtype=tl.float32)
     # none diag
@@ -76,13 +92,13 @@ def _fwd_diag_kernel(
         decay = tl.exp(s_index)
 
         k_trans = tl.load(
-            K_trans_block_ptr,
-            mask=block_offset + kv_index[None, :] < n,
+            K_trans_block_ptr, 
+            mask=block_offset + kv_index[None, :] < n, 
             other=0.0,
         ).to(tl.float32)
         v = tl.load(
-            V_block_ptr,
-            mask=block_offset + kv_index[:, None] < n,
+            V_block_ptr, 
+            mask=block_offset + kv_index[:, None] < n, 
             other=0.0,
         ).to(tl.float32)
 
@@ -121,11 +137,11 @@ def _fwd_kv_parallel(
 ):
     off_bh = tl.program_id(0)
     off_block = tl.program_id(1)
-    # off_de = tl.program_id(2)
+    off_de = tl.program_id(2)
 
     off_h = off_bh % h
-    # off_d = off_de // NUM_FBLOCK
-    # off_e = off_de % NUM_FBLOCK
+    off_d = off_de // NUM_FBLOCK
+    off_e = off_de % NUM_FBLOCK
 
     block_offset = off_block * BLOCK
 
@@ -136,23 +152,37 @@ def _fwd_kv_parallel(
     k_offset = off_bh * n * d
     v_offset = off_bh * n * e
     kv_offset = off_bh * NUM_BLOCK * d * e
-    # d_offset = off_d * D_FBLOCK
-    # e_offset = off_e * E_FBLOCK
+    d_offset = off_d * D_FBLOCK
+    e_offset = off_e * E_FBLOCK
 
     # (CBLOCK, FBLOCK)
     K_trans_block_ptr = (
-        K + k_offset + k_block_offset +
-        tl.arange(0, CBLOCK)[None, :] * d  # d x c
-        + tl.arange(0, D_FBLOCK)[:, None])
+        K
+        + k_offset
+        + k_block_offset
+        + tl.arange(0, CBLOCK)[None, :] * d  # d x c
+        + tl.arange(0, D_FBLOCK)[:, None]
+    )
     V_block_ptr = (
-        V + v_offset + v_block_offset +
-        tl.arange(0, CBLOCK)[:, None] * e  # c x d
-        + tl.arange(0, E_FBLOCK)[None, :])
-    KV_block_ptr = (KV + kv_offset + kv_block_offset +
-                    tl.arange(0, D_FBLOCK)[:, None] * e +
-                    tl.arange(0, E_FBLOCK)[None, :])
+        V
+        + v_offset
+        + v_block_offset
+        + tl.arange(0, CBLOCK)[:, None] * e  # c x d
+        + tl.arange(0, E_FBLOCK)[None, :]
+    )
+    KV_block_ptr = (
+        KV
+        + kv_offset
+        + kv_block_offset
+        + tl.arange(0, D_FBLOCK)[:, None] * e
+        + tl.arange(0, E_FBLOCK)[None, :]
+    )
 
-    k_decay_ptr = (K_decay + off_h * BLOCK + tl.arange(0, CBLOCK)[None, :])
+    k_decay_ptr = (
+        K_decay
+        + off_h * BLOCK
+        + tl.arange(0, CBLOCK)[None, :]
+    )
 
     # compute block array
     kv_index = tl.arange(0, CBLOCK)
@@ -170,12 +200,16 @@ def _fwd_kv_parallel(
     for j in range(num_blocks):
         # right align k, v with CBLOCK
         left_bound = (1 - j) * left_shift
-        k_trans = tl.load(K_trans_block_ptr - left_shift * d,
-                          mask=kv_index[None, :] >= left_bound,
-                          other=0.0)
-        v = tl.load(V_block_ptr - left_shift * d,
-                    mask=kv_index[:, None] >= left_bound,
-                    other=0.0)
+        k_trans = tl.load(
+            K_trans_block_ptr - left_shift * d, 
+            mask=kv_index[None, :] >= left_bound, 
+            other=0.0
+        )
+        v = tl.load(
+            V_block_ptr - left_shift * d,
+            mask=kv_index[:, None] >= left_bound, 
+            other=0.0
+        )
 
         k_decay = tl.load(k_decay_ptr)
         kv += tl.dot(k_trans * k_decay, v)
@@ -203,35 +237,41 @@ def _fwd_kv_reduce(
     NUM_BLOCK,
     D_FBLOCK: tl.constexpr,
     E_FBLOCK: tl.constexpr,
-    # NUM_FBLOCK: tl.constexpr,
-    # CBLOCK: tl.constexpr,
-    # NUM_CBLOCK: tl.constexpr,
+    NUM_FBLOCK: tl.constexpr,
+    CBLOCK: tl.constexpr,
+    NUM_CBLOCK: tl.constexpr,
 ):
     off_bh = tl.program_id(0)
     off_h = off_bh % h
-    # off_d = tl.program_id(1)
-    # off_e = tl.program_id(2)
+    off_d = tl.program_id(1)
+    off_e = tl.program_id(2)
 
     kv_offset = off_bh * NUM_BLOCK * d * e
-    # d_offset = off_d * D_FBLOCK
-    # e_offset = off_e * E_FBLOCK
+    d_offset = off_d * D_FBLOCK
+    e_offset = off_e * E_FBLOCK
 
     # (CBLOCK, FBLOCK)
-    KV_block_ptr = (KV + kv_offset + tl.arange(0, D_FBLOCK)[:, None] * e +
-                    tl.arange(0, E_FBLOCK)[None, :])
+    KV_block_ptr = (
+        KV
+        + kv_offset
+        + tl.arange(0, D_FBLOCK)[:, None] * e
+        + tl.arange(0, E_FBLOCK)[None, :]
+    )
 
     s_ptrs = S + off_h
     s = tl.load(s_ptrs)
 
     # Initialize kv from KV_HISTORY
     kv_history_offset = off_bh * d * e
-    KV_HISTORY_block_ptr = (KV_HISTORY + kv_history_offset +
-                            tl.arange(0, D_FBLOCK)[:, None] * e +
-                            tl.arange(0, E_FBLOCK)[None, :])
+    KV_HISTORY_block_ptr = (
+        KV_HISTORY + kv_history_offset +
+        tl.arange(0, D_FBLOCK)[:, None] * e +
+        tl.arange(0, E_FBLOCK)[None, :]
+    )
     # compute block array
     # last step
     kv_pre = tl.load(KV_HISTORY_block_ptr).to(tl.float32)
-    for i in range(NUM_BLOCK):
+    for i in range (NUM_BLOCK):
         block_size = min(n - i * BLOCK, BLOCK)
         block_decay = tl.exp(-s.to(tl.float32) * block_size)
 
@@ -276,18 +316,31 @@ def _fwd_none_diag_kernel(
     c_offset = off_c * CBLOCK
     e_offset = off_e * E_FBLOCK
     block_offset = n_offset + c_offset
+    
 
     q_offset = off_bh * n * d + (n_offset + c_offset) * d
     o_offset = off_bh * n * e + (n_offset + c_offset) * e + e_offset
 
     kv_offset = off_bh * NUM_BLOCK * d * e + off_n * d * e + e_offset
 
-    Q_block_ptr = (Q + q_offset + tl.arange(0, CBLOCK)[:, None] * d +
-                   tl.arange(0, d)[None, :])
-    O_block_ptr = (Out + o_offset + tl.arange(0, CBLOCK)[:, None] * e +
-                   tl.arange(0, E_FBLOCK)[None, :])
-    KV_block_ptr = (KV + kv_offset + tl.arange(0, d)[:, None] * e +
-                    tl.arange(0, E_FBLOCK)[None, :])
+    Q_block_ptr = (
+        Q
+        + q_offset
+        + tl.arange(0, CBLOCK)[:, None] * d
+        + tl.arange(0, d)[None, :]
+    )
+    O_block_ptr = (
+        Out
+        + o_offset
+        + tl.arange(0, CBLOCK)[:, None] * e
+        + tl.arange(0, E_FBLOCK)[None, :]
+    )
+    KV_block_ptr = (
+        KV
+        + kv_offset
+        + tl.arange(0, d)[:, None] * e
+        + tl.arange(0, E_FBLOCK)[None, :]
+    )
     S_block_ptr = S + off_h
     s = tl.load(S_block_ptr)
 
@@ -295,24 +348,18 @@ def _fwd_none_diag_kernel(
 
     kv = tl.load(KV_block_ptr).to(tl.float32)
     q_index = block_offset + tl.arange(0, CBLOCK)
-    q = tl.load(Q_block_ptr, mask=q_index[:, None] < n,
-                other=0.).to(tl.float32)
-
+    q = tl.load(Q_block_ptr, mask=q_index[:, None] < n, other=0.).to(tl.float32)
+    
     q_decay = tl.exp(-s.to(tl.float32) * (off_c * CBLOCK + c_array[:, None]))
     qkv_none_diag = tl.dot(q, kv) * q_decay
-
-    qkv_diag = tl.load(O_block_ptr, mask=q_index[:, None] < n,
-                       other=0.).to(tl.float32)
+    
+    qkv_diag = tl.load(O_block_ptr, mask=q_index[:, None] < n, other=0.).to(tl.float32)
 
     qkv = qkv_diag + qkv_none_diag
 
-    tl.store(O_block_ptr,
-             qkv.to(O_block_ptr.dtype.element_ty),
-             mask=q_index[:, None] < n)
-
+    tl.store(O_block_ptr, qkv.to(O_block_ptr.dtype.element_ty), mask=q_index[:, None] < n)
 
 class _attention(torch.autograd.Function):
-
     @staticmethod
     def forward(ctx, q, k, v, s, kv_history):
         q = q.contiguous()
@@ -323,8 +370,8 @@ class _attention(torch.autograd.Function):
         capability = torch.cuda.get_device_capability()
         if capability[0] < 8:
             raise RuntimeError(
-                "Flash attention currently only supported for compute "
-                "capability >= 80")
+                "Flash attention currently only supported for compute capability >= 80"
+            )
         # shape constraints
         b, h, n, d = q.shape
         e = v.shape[-1]
@@ -336,8 +383,7 @@ class _attention(torch.autograd.Function):
 
         CBLOCK = 64
         CBLOCK = 32
-        NUM_CBLOCK = BLOCK // CBLOCK
-        assert BLOCK % CBLOCK == 0, "BLOCK must be a multiple of CBLOCK"
+        NUM_CBLOCK = BLOCK // CBLOCK; assert BLOCK % CBLOCK == 0, "BLOCK must be a multiple of CBLOCK"
 
         array = torch.arange(0, BLOCK, device=q.device) + 1
         k_decay = torch.exp(-s * (BLOCK - array.reshape(1, -1)))
@@ -361,20 +407,16 @@ class _attention(torch.autograd.Function):
         )
 
         NUM_FBLOCK = 1
-        D_FBLOCK = d // NUM_FBLOCK
-        assert d % NUM_FBLOCK == 0
-        E_FBLOCK = e // NUM_FBLOCK
-        assert e % NUM_FBLOCK == 0
-
+        D_FBLOCK = d // NUM_FBLOCK; assert d % NUM_FBLOCK == 0
+        E_FBLOCK = e // NUM_FBLOCK; assert e % NUM_FBLOCK == 0
+        
         CBLOCK = 64
-        NUM_CBLOCK = BLOCK // CBLOCK
-        assert BLOCK % CBLOCK == 0, "BLOCK must be a multiple of CBLOCK"
+        NUM_CBLOCK = BLOCK // CBLOCK; assert BLOCK % CBLOCK == 0, "BLOCK must be a multiple of CBLOCK"
 
-        kv = torch.empty((b, h, NUM_BLOCK, d, e),
-                         dtype=torch.float32,
-                         device=q.device)
-        # grid = (b * h, NUM_BLOCK, NUM_FBLOCK * NUM_FBLOCK)
-        grid = (b * h, NUM_BLOCK)
+        kv = torch.empty(
+            (b, h, NUM_BLOCK, d, e), dtype=torch.float32, device=q.device
+        )
+        grid = (b * h, NUM_BLOCK, NUM_FBLOCK * NUM_FBLOCK)
         _fwd_kv_parallel[grid](
             k,
             v,
@@ -394,8 +436,7 @@ class _attention(torch.autograd.Function):
             NUM_CBLOCK=NUM_CBLOCK,
         )
 
-        # grid = (b * h, NUM_FBLOCK, NUM_FBLOCK)
-        grid = (b * h, NUM_BLOCK)
+        grid = (b * h, NUM_FBLOCK, NUM_FBLOCK)
         _fwd_kv_reduce[grid](
             k,
             v,
@@ -416,8 +457,7 @@ class _attention(torch.autograd.Function):
             NUM_CBLOCK=NUM_CBLOCK,
         )
 
-        # grid = (b * h, NUM_BLOCK * NUM_CBLOCK, NUM_FBLOCK)
-        grid = (b * h, NUM_BLOCK * NUM_CBLOCK)
+        grid = (b * h, NUM_BLOCK * NUM_CBLOCK, NUM_FBLOCK)
         _fwd_none_diag_kernel[grid](
             q,
             k,
@@ -444,23 +484,22 @@ class _attention(torch.autograd.Function):
 
         return o, torch.cat([kv, kv_history.unsqueeze(2)], dim=2)
 
-
 lightning_attention_ = _attention.apply
-
 
 def lightning_attention(q, k, v, ed, block_size=256, kv_history=None):
     d = q.shape[-1]
     e = v.shape[-1]
-    m = 128 if d >= 128 else 64
+    if d >= 128:
+        m = 128
+    else:
+        m = 64
     arr = [m * i for i in range(d // m + 1)]
     if arr[-1] != d:
         arr.append(d)
     n = len(arr)
     output = 0
     if kv_history is None:
-        kv_history = torch.zeros((q.shape[0], q.shape[1], d, e),
-                                 dtype=torch.float32,
-                                 device=q.device)
+        kv_history = torch.zeros((q.shape[0], q.shape[1], d, e), dtype=torch.float32, device=q.device)
     else:
         # make sure run in functional programming style
         kv_history = kv_history.clone().contiguous()
@@ -475,58 +514,44 @@ def lightning_attention(q, k, v, ed, block_size=256, kv_history=None):
         output = output + o
     return output, kv
 
-
-def lightning_attention2_parallel(q,
-                                  k,
-                                  v,
-                                  ed,
-                                  block_size=256,
-                                  kv_history=None):
+def lightning_attention2_parallel(q, k, v, ed, block_size=256, kv_history=None):
     return lightning_attention(q, k, v, ed, block_size, kv_history)
-
 
 @triton.jit
 def _linear_attn_decode_kernel(
     # Pointers to matrices
-    q_ptr,
-    k_ptr,
-    v_ptr,  # [B, H, 1, D]  
-    kv_cache_ptr,  # [B, H, D, D] 
-    slope_rate,
+    q_ptr, k_ptr, v_ptr,      # [B, H, 1, D]  
+    kv_cache_ptr,             # [B, H, D, D] 
+    slope_rate, 
     slot_idx,
-    output_ptr,  # [B, H, 1, D]
-    B,
-    H,
+    output_ptr,               # [B, H, 1, D]
+    B, H, 
     D: tl.constexpr,
     # Matrix dimensions
-    qkv_b_stride,
-    qkv_h_stride,
-    cache_b_stride,
-    cache_h_stride,
-    cache_d0_stride,
-    cache_d1_stride,
+    qkv_b_stride, qkv_h_stride,
+    cache_b_stride, cache_h_stride, cache_d0_stride, cache_d1_stride,
     BLOCK_SIZE: tl.constexpr,
 ):
 
     pid_b = tl.program_id(0)
     pid_h = tl.program_id(1)
     pid_d = tl.program_id(2)
-
+    
     slot_id = tl.load(slot_idx + pid_b)
 
     # return when padding
     if slot_id == -1:
         return
-
+    
     batch_id = pid_b
     head_id = pid_h
-
+    
     ratio = tl.load(slope_rate + pid_h)
+
 
     qk_d_offsets = tl.arange(0, D)
     v_d_offsets = tl.arange(0, BLOCK_SIZE) + pid_d * BLOCK_SIZE
-    cache_d_offsets = qk_d_offsets[:, None] * cache_d0_stride + v_d_offsets[
-        None, :] * cache_d1_stride
+    cache_d_offsets = qk_d_offsets[:, None] * cache_d0_stride + v_d_offsets[None, :] * cache_d1_stride
 
     q_offset = batch_id * qkv_b_stride + head_id * qkv_h_stride
     k_offset = batch_id * qkv_b_stride + head_id * qkv_h_stride
@@ -536,12 +561,12 @@ def _linear_attn_decode_kernel(
     cache_offset = slot_id * cache_b_stride + head_id * cache_h_stride
 
     qk_mask = qk_d_offsets < D
-    v_mask = v_d_offsets < D
+    v_mask = v_d_offsets < D 
     # load data to shm
     q = tl.load(q_ptr + q_offset + qk_d_offsets, mask=qk_mask, other=0.0)
     k = tl.load(k_ptr + k_offset + qk_d_offsets, mask=qk_mask, other=0.0)
     v = tl.load(v_ptr + v_offset + v_d_offsets, mask=v_mask, other=0.0)
-
+    
     kv_outer = k[:, None] * v[None, :]  # [D, BLOCK_SIZE]
     kv_mask = qk_mask[:, None] & v_mask[None, :]
 
@@ -559,22 +584,23 @@ def _linear_attn_decode_kernel(
     tl.store(output_ptr + q_offset + v_d_offsets, output, mask=v_mask)
 
 
+
 def linear_decode_forward_triton(
-    q: torch.Tensor,  # [B, H, 1, D] 
-    k: torch.Tensor,  # [B, H, 1, D]
-    v: torch.Tensor,  # [B, H, 1, D] 
+    q: torch.Tensor,      # [B, H, 1, D] 
+    k: torch.Tensor,      # [B, H, 1, D]
+    v: torch.Tensor,      # [B, H, 1, D] 
     kv_caches: torch.Tensor,  # [B, H, D, D]
     slope_rate: torch.Tensor,  # float
     slot_idx: torch.Tensor,
     BLOCK_SIZE: int = 32,
 ) -> torch.Tensor:
-
+    
     B, H, _, D = q.shape
     assert k.shape == (B, H, 1, D)
     assert v.shape == (B, H, 1, D)
-
+    
     output = torch.empty_like(q)
-
+    
     grid = (B, H, D // BLOCK_SIZE)
 
     qkv_b_stride = q.stride(0)
@@ -584,26 +610,19 @@ def linear_decode_forward_triton(
     cache_h_stride = kv_caches.stride(1)
     cache_d0_stride = kv_caches.stride(2)
     cache_d1_stride = kv_caches.stride(3)
-
+    
     # launch kernel
     _linear_attn_decode_kernel[grid](
-        q,
-        k,
-        v,
-        kv_caches,
+        q, k, v,
+        kv_caches, 
         slope_rate,
         slot_idx,
         output,
-        B,
-        H,
-        D,
-        qkv_b_stride,
-        qkv_h_stride,
-        cache_b_stride,
-        cache_h_stride,
-        cache_d0_stride,
-        cache_d1_stride,
+        B, H, D,
+        qkv_b_stride, qkv_h_stride,
+        cache_b_stride, cache_h_stride,cache_d0_stride, cache_d1_stride,
         BLOCK_SIZE=BLOCK_SIZE,
     )
     output = rearrange(output, "b h n d -> b n (h d)")
     return output.squeeze(1).contiguous()
+
