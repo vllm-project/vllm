d83f7efb628df811deeb4d4e6629d00be6a53c0a
Isotr0py <2037008807@qq.com>
2025-03-14 22:17:44 +0800

    remove unused pooling head
    Signed-off-by: Isotr0py <2037008807@qq.com>

--- vllm/model_executor/models/idefics2_vision_model.py
+++ vllm/model_executor/models/idefics2_vision_model.py
@@ -193,39 +193,6 @@ class Idefics2VisionMLP(nn.Module):
         return hidden_states
 
 
-class Idefics2MultiheadAttentionPoolingHead(nn.Module):
-    """Multihead Attention Pooling."""
-
-    def __init__(
-        self,
-        config: Idefics2VisionConfig,
-        quant_config: Optional[QuantizationConfig] = None,
-        prefix: str = "",
-    ):
-        super().__init__()
-
-        self.probe = nn.Parameter(torch.randn(1, 1, config.hidden_size))
-        self.attention = torch.nn.MultiheadAttention(
-            config.hidden_size, config.num_attention_heads, batch_first=True)
-        self.layernorm = nn.LayerNorm(config.hidden_size,
-                                      eps=config.layer_norm_eps)
-        self.mlp = Idefics2VisionMLP(config=config,
-                                     quant_config=quant_config,
-                                     prefix=f"{prefix}.mlp")
-
-    def forward(self, hidden_state):
-        batch_size = hidden_state.shape[0]
-        probe = self.probe.repeat(batch_size, 1, 1)
-
-        hidden_state = self.attention(probe, hidden_state, hidden_state)[0]
-
-        residual = hidden_state
-        hidden_state = self.layernorm(hidden_state)
-        hidden_state = residual + self.mlp(hidden_state)
-
-        return hidden_state[:, 0]
-
-
 class Idefics2EncoderLayer(nn.Module):
 
     def __init__(
