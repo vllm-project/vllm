0826ceb743a3ffb7e5df03f0e2e4839fd9bc7243
Thien Tran <gau.nernst@yahoo.com.sg>
2025-03-15 13:28:31 +0800

    fix AVX512 (yay)
    Signed-off-by: Thien Tran <gau.nernst@yahoo.com.sg>

--- csrc/cpu/mla_decode.cpp
+++ csrc/cpu/mla_decode.cpp
@@ -4,41 +4,48 @@
 namespace {
 template <typename scalar_t>
 struct KernelVecType {
-  using load_vec_type = void;
-  using vec_type = void;
+  using qk_load_vec_type = void;
+  using qk_vec_type = void;
+  using v_load_vec_type = void;
 };
 
 template <>
 struct KernelVecType<float> {
-  using load_vec_type = vec_op::FP32Vec16;
-  using vec_type = vec_op::FP32Vec16;
+  using qk_load_vec_type = vec_op::FP32Vec16;
+  using qk_vec_type = vec_op::FP32Vec16;
+  using v_load_vec_type = vec_op::FP32Vec16;
 };
 
 template <>
 struct KernelVecType<c10::Half> {
 #if defined(__powerpc64__) || defined(__s390x__)
   // Power and s390x architecture-specific vector types
-  using load_vec_type = vec_op::FP32Vec16;
+  using qk_load_vec_type = vec_op::FP32Vec16;
+  using qk_vec_type = vec_op::FP32Vec16;
+  using v_load_vec_type = vec_op::FP32Vec16;
 #else
   // Fallback for other architectures, including x86
-  using load_vec_type = vec_op::FP16Vec16;
+  using qk_load_vec_type = vec_op::FP16Vec16;
+  using qk_vec_type = vec_op::FP32Vec16;
+  using v_load_vec_type = vec_op::FP16Vec16;
 #endif
-  using vec_type = vec_op::FP32Vec16;
 };
 
 #ifdef __AVX512BF16__
 template <>
 struct KernelVecType<c10::BFloat16> {
-  using load_vec_type = vec_op::BF16Vec32;
-  using vec_type = vec_op::BF16Vec32;
+  using qk_load_vec_type = vec_op::BF16Vec32;
+  using qk_vec_type = vec_op::BF16Vec32;
+  using v_load_vec_type = vec_op::BF16Vec16;
 };
 #elif defined(__aarch64__) && !defined(ARM_BF16_SUPPORT)
 // pass
 #else
 template <>
 struct KernelVecType<c10::BFloat16> {
-  using load_vec_type = vec_op::BF16Vec16;
-  using vec_type = vec_op::FP32Vec16;
+  using qk_load_vec_type = vec_op::BF16Vec16;
+  using qk_vec_type = vec_op::FP32Vec16;
+  using v_load_vec_type = vec_op::BF16Vec16;
 };
 #endif
 }  // namespace
@@ -52,14 +59,17 @@ void mla_decode_block(
                                   // alignment hint?
     float* __restrict__ acc_lse,  // [num_heads]
     const int num_heads, const float scale, const int num_tokens) {
-  using load_vec_type = typename KernelVecType<scalar_t>::load_vec_type;
-  using vec_type = typename KernelVecType<scalar_t>::vec_type;
+  using qk_load_vec_type = typename KernelVecType<scalar_t>::qk_load_vec_type;
+  using qk_vec_type = typename KernelVecType<scalar_t>::qk_vec_type;
+  using v_load_vec_type = typename KernelVecType<scalar_t>::v_load_vec_type;
   using f32_vec_type = vec_op::FP32Vec16;
-  static_assert(vec_type::VEC_ELEM_NUM == load_vec_type::VEC_ELEM_NUM);
-  constexpr int NUM_ELEM = vec_type::VEC_ELEM_NUM;
+  static_assert(qk_load_vec_type::VEC_ELEM_NUM == qk_vec_type::VEC_ELEM_NUM);
+  static_assert(v_load_vec_type::VEC_ELEM_NUM == f32_vec_type::VEC_ELEM_NUM);
+  constexpr int QK_NUM_ELEM = qk_vec_type::VEC_ELEM_NUM;
+  constexpr int V_NUM_ELEM = v_load_vec_type::VEC_ELEM_NUM;
 
-  const vec_type* k_vecs;
-  const f32_vec_type* v_vecs;
+  const qk_vec_type* k_vecs;
+  const f32_vec_type* v_vecs_f32;
   float* kv_cache_f32 = nullptr;
 
   if constexpr (!std::is_same<scalar_t, float>::value) {
@@ -71,28 +81,28 @@ void mla_decode_block(
     kv_cache_f32 = static_cast<float*>(std::aligned_alloc(64, nbytes));
 
     for (int block_offset = 0; block_offset < num_tokens; ++block_offset)
-      for (int i = 0; i < HEAD_DIM; i += NUM_ELEM) {
-        load_vec_type kv_load_vec(kv_cache + block_offset * HEAD_DIM + i);
-        vec_op::FP32Vec16 kv_vec_f32(kv_load_vec);
+      for (int i = 0; i < HEAD_DIM; i += V_NUM_ELEM) {
+        v_load_vec_type kv_load_vec(kv_cache + block_offset * HEAD_DIM + i);
+        f32_vec_type kv_vec_f32(kv_load_vec);
         kv_vec_f32.save(kv_cache_f32 + block_offset * HEAD_DIM + i);
       }
 
-    if constexpr (std::is_same<load_vec_type, vec_type>::value) {
+    if constexpr (std::is_same<qk_load_vec_type, qk_vec_type>::value) {
       // for AVX512_BF16, Q @ K.T uses BF16 for K (no conversion)
       // NOTE: in this case, we only need to convert the V section to FP32.
       // But for simplicity, we will convert the whole KV block to FP32.
-      k_vecs = reinterpret_cast<const vec_type*>(kv_cache);
+      k_vecs = reinterpret_cast<const qk_vec_type*>(kv_cache);
     } else {
-      k_vecs = reinterpret_cast<const vec_type*>(kv_cache_f32);
+      k_vecs = reinterpret_cast<const qk_vec_type*>(kv_cache_f32);
     }
 
     // attn @ V always use FP32 for V, since attn is FP32.
-    v_vecs = reinterpret_cast<const f32_vec_type*>(kv_cache_f32);
+    v_vecs_f32 = reinterpret_cast<const f32_vec_type*>(kv_cache_f32);
 
   } else {
     // KV cache is FP32. don't need to do anything.
-    k_vecs = reinterpret_cast<const vec_type*>(kv_cache);
-    v_vecs = reinterpret_cast<const f32_vec_type*>(kv_cache);
+    k_vecs = reinterpret_cast<const qk_vec_type*>(kv_cache);
+    v_vecs_f32 = reinterpret_cast<const f32_vec_type*>(kv_cache);
   }
 
   for (int head_idx = 0; head_idx < num_heads; ++head_idx) {
@@ -102,10 +112,10 @@ void mla_decode_block(
     for (int block_offset = 0; block_offset < num_tokens; ++block_offset) {
       // dot product
       f32_vec_type acc_vec;
-      for (int i = 0; i < HEAD_DIM; i += NUM_ELEM) {
-        load_vec_type q_load_vec(q + head_idx * HEAD_DIM + i);
-        vec_type q_vec(q_load_vec);
-        vec_type k_vec(k_vecs[(block_offset * HEAD_DIM + i) / NUM_ELEM]);
+      for (int i = 0; i < HEAD_DIM; i += QK_NUM_ELEM) {
+        qk_load_vec_type q_load_vec(q + head_idx * HEAD_DIM + i);
+        qk_vec_type q_vec(q_load_vec);
+        qk_vec_type k_vec(k_vecs[(block_offset * HEAD_DIM + i) / QK_NUM_ELEM]);
         vec_op::fma(acc_vec, q_vec, k_vec);
       }
       float acc = acc_vec.reduce_sum();
@@ -122,15 +132,16 @@ void mla_decode_block(
       sum_exp += val;
     }
 
-    f32_vec_type this_out[V_HEAD_DIM / NUM_ELEM];
+    f32_vec_type this_out[V_HEAD_DIM / V_NUM_ELEM];
     float inv_sum = 1.0f / sum_exp;
 
     for (int block_offset = 0; block_offset < BLOCK_SIZE; ++block_offset) {
       f32_vec_type scale_(logits[block_offset] * inv_sum);
 
-      for (int i = 0; i < V_HEAD_DIM; i += NUM_ELEM) {
-        f32_vec_type v_vec(v_vecs[(block_offset * HEAD_DIM + i) / NUM_ELEM]);
-        vec_op::fma(this_out[i / NUM_ELEM], v_vec, scale_);
+      for (int i = 0; i < V_HEAD_DIM; i += V_NUM_ELEM) {
+        f32_vec_type v_vec(
+            v_vecs_f32[(block_offset * HEAD_DIM + i) / V_NUM_ELEM]);
+        vec_op::fma(this_out[i / V_NUM_ELEM], v_vec, scale_);
       }
     }
 
@@ -149,9 +160,9 @@ void mla_decode_block(
     f32_vec_type curr_scale(curr_sum_exp / new_sum_exp);
 
     acc_lse[head_idx] = std::log(new_sum_exp) + max_val;
-    for (int i = 0; i < V_HEAD_DIM; i += NUM_ELEM) {
+    for (int i = 0; i < V_HEAD_DIM; i += V_NUM_ELEM) {
       f32_vec_type o_vec(acc_out + head_idx * V_HEAD_DIM + i);
-      o_vec = o_vec * prev_scale + this_out[i / NUM_ELEM] * curr_scale;
+      o_vec = o_vec * prev_scale + this_out[i / V_NUM_ELEM] * curr_scale;
       o_vec.save(acc_out + head_idx * V_HEAD_DIM + i);
     }
   }
@@ -183,14 +194,6 @@ void mla_decode_kvcache_cpu_impl(
   // sub-problem
   //   - chunk according to BLOCK_SIZE for convenience
 
-  using load_vec_type = typename KernelVecType<scalar_t>::load_vec_type;
-  using vec_type = typename KernelVecType<scalar_t>::vec_type;
-  using f32_vec_type = vec_op::FP32Vec16;
-  static_assert(vec_type::VEC_ELEM_NUM == load_vec_type::VEC_ELEM_NUM);
-  constexpr int NUM_ELEM = vec_type::VEC_ELEM_NUM;
-  static_assert(HEAD_DIM % NUM_ELEM == 0);
-  static_assert(V_HEAD_DIM % NUM_ELEM == 0);
-
 #pragma omp parallel for schedule(static, 1)
   for (int seq_idx = 0; seq_idx < num_seqs; ++seq_idx) {
     const int seq_len = seq_lens[seq_idx];
@@ -214,97 +217,12 @@ void mla_decode_kvcache_cpu_impl(
           acc_out, acc_lse.data(), num_heads, scale, num_tokens);
     }
 
-    for (int i = 0; i < num_heads * V_HEAD_DIM; i += NUM_ELEM) {
-      vec_op::FP32Vec16 o_vec_f32(acc_out + i);
-      load_vec_type o_store_vec(o_vec_f32);
-      o_store_vec.save(out + seq_idx * o_stride + i);
+    for (int i = 0; i < num_heads * V_HEAD_DIM; ++i) {
+      vec_op::storeFP32(acc_out[i], out + seq_idx * o_stride + i);
     }
     std::free(acc_out);
   }
   return;
-
-#pragma omp parallel for collapse(2)
-  for (int seq_idx = 0; seq_idx < num_seqs; ++seq_idx) {
-    for (int head_idx = 0; head_idx < num_heads; ++head_idx) {
-      const int seq_len = seq_lens[seq_idx];
-      const int* seq_block_table =
-          block_tables + max_num_blocks_per_seq * seq_idx;
-      const int block_num = (seq_len + BLOCK_SIZE - 1) / BLOCK_SIZE;
-      const int last_block_token_num = seq_len - (block_num - 1) * BLOCK_SIZE;
-
-      const scalar_t* __restrict__ q_ptr =
-          q + seq_idx * q_stride + head_idx * HEAD_DIM;
-
-      std::vector<float> logits(seq_len);
-      float max_val = -FLT_MAX;
-
-      // compute QK.T
-      for (int block_idx = 0; block_idx < block_num; ++block_idx) {
-        const int physical_block_idx = seq_block_table[block_idx];
-        const int num_tokens =
-            block_idx < block_num - 1 ? BLOCK_SIZE : last_block_token_num;
-
-        for (int block_offset = 0; block_offset < num_tokens; ++block_offset) {
-          const scalar_t* __restrict__ k_ptr = kv_cache +
-                                               physical_block_idx * kv_stride +
-                                               block_offset * HEAD_DIM;
-          f32_vec_type acc_vec;
-
-          for (int i = 0; i < HEAD_DIM; i += NUM_ELEM) {
-            load_vec_type q_load_vec(q_ptr + i);
-            load_vec_type k_load_vec(k_ptr + i);
-            vec_type q_vec(q_load_vec);
-            vec_type k_vec(k_load_vec);
-            vec_op::fma(acc_vec, q_vec, k_vec);
-          }
-          float acc = acc_vec.reduce_sum();
-
-          acc *= scale;
-          max_val = std::max(max_val, acc);
-          logits[block_idx * BLOCK_SIZE + block_offset] = acc;
-        }
-      }
-
-      // softmax(QK.T)
-      float sum = 0.0f;
-      for (int tok_idx = 0; tok_idx < seq_len; ++tok_idx) {
-        const float val = std::exp(logits[tok_idx] - max_val);
-        sum += val;
-        logits[tok_idx] = val;
-      }
-      const float inv_sum = 1.0f / sum;
-
-      // multiply with v
-      std::vector<f32_vec_type> out_token(V_HEAD_DIM / NUM_ELEM);
-
-      for (int block_idx = 0; block_idx < block_num; ++block_idx) {
-        const int64_t physical_block_idx = seq_block_table[block_idx];
-        const int num_tokens =
-            block_idx < block_num - 1 ? BLOCK_SIZE : last_block_token_num;
-
-        for (int block_offset = 0; block_offset < num_tokens; ++block_offset) {
-          const scalar_t* __restrict__ v_ptr = kv_cache +
-                                               physical_block_idx * kv_stride +
-                                               block_offset * HEAD_DIM;
-          f32_vec_type scale_(logits[block_idx * BLOCK_SIZE + block_offset] *
-                              inv_sum);
-
-          for (int i = 0; i < V_HEAD_DIM; i += NUM_ELEM) {
-            load_vec_type v_load_vec(v_ptr + i);
-            f32_vec_type v_vec_f32(v_load_vec);
-            vec_op::fma(out_token[i / NUM_ELEM], v_vec_f32, scale_);
-          }
-        }
-      }
-
-      scalar_t* __restrict__ o_ptr =
-          out + seq_idx * o_stride + head_idx * V_HEAD_DIM;
-      for (int i = 0; i < V_HEAD_DIM; i += NUM_ELEM) {
-        load_vec_type o_store(out_token[i / NUM_ELEM]);
-        o_store.save(o_ptr + i);
-      }
-    }
-  }
 }
 
 void mla_decode_kvcache(torch::Tensor& out, torch::Tensor& query,
