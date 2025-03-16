0d6e2fea538d9f6eae41eb93443530c26a69ee50
Kinfey <kinfeylo@microsoft.com>
2025-03-16 21:32:14 +0800

    Add phi-4-mini function support
    Signed-off-by: Kinfey <kinfeylo@microsoft.com>

--- vllm/entrypoints/openai/tool_parsers/phi4mini_tool_parser.py
+++ vllm/entrypoints/openai/tool_parsers/phi4mini_tool_parser.py
@@ -259,7 +259,6 @@ class Phi4MiniJsonToolParser(ToolParser):
                             )
                         ])
                         self.streamed_args_for_tool[self.current_tool_id] += argument_diff
-             
             self.prev_tool_call_arr = tool_call_arr
             return delta
 
