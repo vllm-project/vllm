4df7ed0320e129499967304a924c734681f964b9
tjtanaa <tunjian.tan@embeddedllm.com>
2025-03-17 13:49:16 +0000

    reenable unit tests; update Dockerfile.rocm_base
    Signed-off-by: tjtanaa <tunjian.tan@embeddedllm.com>

--- tests/models/decoder_only/language/test_models.py
+++ tests/models/decoder_only/language/test_models.py
@@ -21,21 +21,21 @@ REQUIRES_V0 = ["microsoft/phi-2", "stabilityai/stablelm-3b-4e1t"]
 @pytest.mark.parametrize(
     "model",
     [
-        # pytest.param(
-        #     "bigscience/bloom-560m",  # bloom - testing alibi slopes
-        #     marks=[pytest.mark.core_model, pytest.mark.cpu_model],
-        # ),
-        # pytest.param(
-        #     "openai-community/gpt2",  # gpt2
-        #     marks=[pytest.mark.core_model, pytest.mark.cpu_model],
-        # ),
-        # pytest.param("Milos/slovak-gpt-j-405M"),  # gptj
-        # pytest.param("bigcode/tiny_starcoder_py"),  # gpt_bigcode
-        # pytest.param("EleutherAI/pythia-70m"),  # gpt_neox
-        # pytest.param(
-        #     "google/gemma-1.1-2b-it",  # gemma
-        #     marks=[pytest.mark.core_model, pytest.mark.cpu_model],
-        # ),
+        pytest.param(
+            "bigscience/bloom-560m",  # bloom - testing alibi slopes
+            marks=[pytest.mark.core_model, pytest.mark.cpu_model],
+        ),
+        pytest.param(
+            "openai-community/gpt2",  # gpt2
+            marks=[pytest.mark.core_model, pytest.mark.cpu_model],
+        ),
+        pytest.param("Milos/slovak-gpt-j-405M"),  # gptj
+        pytest.param("bigcode/tiny_starcoder_py"),  # gpt_bigcode
+        pytest.param("EleutherAI/pythia-70m"),  # gpt_neox
+        pytest.param(
+            "google/gemma-1.1-2b-it",  # gemma
+            marks=[pytest.mark.core_model, pytest.mark.cpu_model],
+        ),
         pytest.param(
             "THUDM/chatglm3-6b",  # chatglm (text-only)
         ),
@@ -43,32 +43,32 @@ REQUIRES_V0 = ["microsoft/phi-2", "stabilityai/stablelm-3b-4e1t"]
             "meta-llama/Llama-3.2-1B-Instruct",  # llama
             marks=[pytest.mark.core_model, pytest.mark.cpu_model],
         ),
-        # pytest.param(
-        #     "openbmb/MiniCPM3-4B",
-        #     # fused_moe not supported on CPU
-        #     marks=[pytest.mark.core_model],
-        # ),
-        # pytest.param(
-        #     "facebook/opt-125m",  # opt
-        #     marks=[pytest.mark.core_model, pytest.mark.cpu_model],
-        # ),
-        # pytest.param(
-        #     "microsoft/phi-2",  # phi
-        #     marks=[pytest.mark.core_model],
-        # ),
-        # pytest.param(
-        #     "Qwen/Qwen-7B",  # qwen (text-only)
-        # ),
-        # pytest.param(
-        #     "Qwen/Qwen2.5-0.5B-Instruct",  # qwen2
-        #     marks=[pytest.mark.core_model],
-        # ),
-        # pytest.param("stabilityai/stablelm-3b-4e1t"),  # stablelm
-        # pytest.param("bigcode/starcoder2-3b"),  # starcoder2
-        # pytest.param(
-        #     "ehristoforu/Falcon3-MoE-2x7B-Insruct",  # mixtral
-        #     marks=[pytest.mark.cpu_model],
-        # )
+        pytest.param(
+            "openbmb/MiniCPM3-4B",
+            # fused_moe not supported on CPU
+            marks=[pytest.mark.core_model],
+        ),
+        pytest.param(
+            "facebook/opt-125m",  # opt
+            marks=[pytest.mark.core_model, pytest.mark.cpu_model],
+        ),
+        pytest.param(
+            "microsoft/phi-2",  # phi
+            marks=[pytest.mark.core_model],
+        ),
+        pytest.param(
+            "Qwen/Qwen-7B",  # qwen (text-only)
+        ),
+        pytest.param(
+            "Qwen/Qwen2.5-0.5B-Instruct",  # qwen2
+            marks=[pytest.mark.core_model],
+        ),
+        pytest.param("stabilityai/stablelm-3b-4e1t"),  # stablelm
+        pytest.param("bigcode/starcoder2-3b"),  # starcoder2
+        pytest.param(
+            "ehristoforu/Falcon3-MoE-2x7B-Insruct",  # mixtral
+            marks=[pytest.mark.cpu_model],
+        )
     ])
 @pytest.mark.parametrize("dtype", ["half"])
 @pytest.mark.parametrize("max_tokens", [32])
