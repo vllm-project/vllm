cf161197842694f6bdcacb351a00e1142fd4ef7f
Manish kumar thota <37763863+manishkumart@users.noreply.github.com>
2025-03-17 09:33:56 -0400

    Update engine.py
    Ppdated the imports at package level

--- vllm/engine/multiprocessing/engine.py
+++ vllm/engine/multiprocessing/engine.py
@@ -8,7 +8,8 @@ from typing import Iterator, List, Optional, Union
 import cloudpickle
 import zmq
 
-from vllm import AsyncEngineArgs, SamplingParams
+from vllm.engine.arg_utils import AsyncEngineArgs
+from vllm.sampling_params import SamplingParams
 from vllm.config import VllmConfig
 from vllm.engine.llm_engine import LLMEngine
 # yapf conflicts with isort for this block
