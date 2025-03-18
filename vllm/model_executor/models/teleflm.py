89574575b92b91339fde4cddbe76475005a4411c
Naitong Yu <ntyu@baai.ac.cn>
2025-03-18 20:05:51 +0800

    reformat code
    Signed-off-by: Naitong Yu <ntyu@baai.ac.cn>
    Signed-off-by: jiangxin <horizon94@outlook.com>

--- vllm/model_executor/models/teleflm.py
+++ vllm/model_executor/models/teleflm.py
@@ -94,20 +94,18 @@ class TeleFLMMLP(nn.Module):
 
 class TeleFLMAttention(nn.Module):
 
-    def __init__(
-        self,
-        config: TeleFLMConfig,
-        hidden_size: int,
-        num_heads: int,
-        num_kv_heads: int,
-        rope_theta: float = 10000,
-        rope_scaling: Optional[Dict[str, Any]] = None,
-        max_position_embeddings: int = 8192,
-        quant_config: Optional[QuantizationConfig] = None,
-        bias: bool = False,
-        cache_config: Optional[CacheConfig] = None,
-        prefix: str = ""
-    ) -> None:
+    def __init__(self,
+                 config: TeleFLMConfig,
+                 hidden_size: int,
+                 num_heads: int,
+                 num_kv_heads: int,
+                 rope_theta: float = 10000,
+                 rope_scaling: Optional[Dict[str, Any]] = None,
+                 max_position_embeddings: int = 8192,
+                 quant_config: Optional[QuantizationConfig] = None,
+                 bias: bool = False,
+                 cache_config: Optional[CacheConfig] = None,
+                 prefix: str = "") -> None:
         super().__init__()
         self.hidden_size = hidden_size
         tp_size = get_tensor_model_parallel_world_size()
