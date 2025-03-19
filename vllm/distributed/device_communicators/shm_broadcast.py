e4b21536ef9fd822f2a3af7f869fc72d6a1bfb63
Yi Liu <yiliu4@habana.ai>
2025-03-19 04:01:12 +0200

    revert change
    Signed-off-by: Yi Liu <yiliu4@habana.ai>

--- vllm/distributed/device_communicators/shm_broadcast.py
+++ vllm/distributed/device_communicators/shm_broadcast.py
@@ -356,15 +356,8 @@ class MessageQueue:
                     # if we wait for a long time, log a message
                     if (time.monotonic() - start_time
                             > VLLM_RINGBUFFER_WARNING_INTERVAL * n_warning):
-                        block_status_info = (
-                            f"The block has been read by {read_count}"
-                            f" out of {self.buffer.n_reader} readers."
-                        )
-                        logger.debug(
-                            "No available block found in %s second. %s",
-                            VLLM_RINGBUFFER_WARNING_INTERVAL,
-                            block_status_info,
-                        )
+                        logger.debug("No available block found in %s second. ",
+                                     VLLM_RINGBUFFER_WARNING_INTERVAL)
                         n_warning += 1
 
                     # if we time out, raise an exception
@@ -421,22 +414,8 @@ class MessageQueue:
                     # if we wait for a long time, log a message
                     if (time.monotonic() - start_time
                             > VLLM_RINGBUFFER_WARNING_INTERVAL * n_warning):
-                        if not written_flag:
-                            block_status_info = (
-                                "This block has not been written yet.")
-                        else:
-                            block_status_info = (
-                                "This block has already been read by "
-                                f"reader {self.local_reader_rank}.")
-                        block_status_info = (f"{block_status_info} "
-                                             f"written_flag: {written_flag}, "
-                                             f"read_flag: {read_flag}.")
-
-                        logger.debug(
-                            "No available block found in %s second. %s",
-                            VLLM_RINGBUFFER_WARNING_INTERVAL,
-                            block_status_info,
-                        )
+                        logger.debug("No available block found in %s second. ",
+                                     VLLM_RINGBUFFER_WARNING_INTERVAL)
                         n_warning += 1
 
                     # if we time out, raise an exception
