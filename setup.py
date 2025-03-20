baa5ab67c3abd7b5c7df652750944eb5f1f2e356
Isotr0py <Isotr0py@outlook.com>
2025-03-21 00:21:22 +0800

    Update setup.py
    Co-authored-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -690,7 +690,7 @@ setup(
         "tensorizer": ["tensorizer>=2.9.0"],
         "runai": ["runai-model-streamer", "runai-model-streamer-s3", "boto3"],
         "audio": ["librosa", "soundfile"],  # Required for audio processing
-        "video": []  # decord is deprecated
+        "video": []  # Kept for backwards compatability
     },
     cmdclass=cmdclass,
     package_data=package_data,
