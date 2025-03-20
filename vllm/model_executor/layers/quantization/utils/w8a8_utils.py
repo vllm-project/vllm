03c29426eb26ee4f59ecf1740e7a958ae08c0c51
Hollow Man <hollowman@opensuse.org>
2025-03-20 21:18:06 +0200

    [ROCm] Pop ROCR/HIP_VISIBLE_DEVICES in RayWorkerWrapper
    From v2.6.0, torch respects ROCR_VISIBLE_DEVICES on AMD GPU
    device discovery: https://github.com/pytorch/pytorch/pull/144026
    So we no longer need always to set
    `RAY_EXPERIMENTAL_NOSET_ROCR_VISIBLE_DEVICES`, but we need to pop
    the `ROCR_VISIBLE_DEVICES` set by ray when we initialize
    `RayWorkerWrapper`, as we still use `CUDA_VISIBLE_DEVICES` for
    ROCm, setting both of them at the same time causes conflict.
    
    In https://github.com/ray-project/ray/commit/3b9e729f6a669ffd85190f901f5e262af79771b0
    Ray replaces AMD device env var with HIP_VISIBLE_DEVICES,
    so now we also need to pop the HIP_VISIBLE_DEVICES.
    
    Signed-off-by: Hollow Man <hollowman@opensuse.org>
    
    [ROCm] Delay setting USE_ROWWISE_TORCH_SCALED_MM
    
    Until the first time it is needed, as initially, when importing,
     CUDA_VISIBLE_DEVICES may not be set properly.
    
    Signed-off-by: Hollow Man <hollowman@opensuse.org>
    
    [ROCm] Avoid CUDA initialized by torch.cuda.get_device_properties
    
    before we set the `CUDA_VISIBLE_DEVICES`.
    
    We use `rocminfo` to get the gcn arch as a workaround
    
    Signed-off-by: Hollow Man <hollowman@opensuse.org>
    
    Address review suggestions
    
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- vllm/model_executor/layers/quantization/utils/w8a8_utils.py
+++ vllm/model_executor/layers/quantization/utils/w8a8_utils.py
@@ -2,6 +2,7 @@
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
 from collections.abc import Callable
+from functools import cache
 
 import torch
 from packaging import version
@@ -19,15 +20,18 @@ from vllm.utils.torch_utils import direct_register_custom_op
 # from pytorch 2.5. Allocating a dummy tensor to pass as input_scale
 TORCH_DEVICE_IDENTITY = None
 
+
 # The condition to determine if it is on a platform that supports
 # torch._scaled_mm rowwise feature.
-# The condition is determined once as the operations
-# are time-consuming.
-USE_ROWWISE_TORCH_SCALED_MM = (
-    current_platform.is_rocm()
-    and version.parse(torch.__version__) >= version.parse("2.7")
-    and current_platform.has_device_capability(94)
-)
+# Cache the result to avoid determining it multiple times, as
+# this is time-consuming.
+@cache
+def use_rowwise_torch_scaled_mm() -> bool:
+    return (
+        current_platform.is_rocm()
+        and version.parse(torch.__version__) >= version.parse("2.7")
+        and current_platform.has_device_capability(94)
+    )
 
 
 def sparse_cutlass_supported() -> bool:
@@ -377,7 +381,7 @@ def dispatch_w8a8_scaled_mm(
     if (
         not per_tensor_weights
         and not per_tensor_activations
-        and USE_ROWWISE_TORCH_SCALED_MM
+        and use_rowwise_torch_scaled_mm()
     ):
         return torch_per_token_w8a8_scaled_mm
     # Normally, torch.scaled_mm supports per tensor weights + activations only
