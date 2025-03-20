63aaaea3d1430b7d0ca4fedcb481919979c00235
chaunceyjiang <chaunceyjiang@gmail.com>
2025-03-20 02:40:18 +0000

    [Misc]fixed disable these http request logs
    Signed-off-by: chaunceyjiang <chaunceyjiang@gmail.com>

--- vllm/entrypoints/openai/cli_args.py
+++ vllm/entrypoints/openai/cli_args.py
@@ -90,7 +90,7 @@ def make_arg_parser(parser: FlexibleArgumentParser) -> FlexibleArgumentParser:
         choices=['debug', 'info', 'warning', 'error', 'critical', 'trace'],
         help="Log level for uvicorn.")
     parser.add_argument("--disable-uvicorn-access-log",
-                        action="store_false",
+                        action="store_true",
                         help="Disable uvicorn access log.")
     parser.add_argument("--allow-credentials",
                         action="store_true",
