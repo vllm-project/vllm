27ad7938a0556e8df569953fa9195fdc588b4ddd
Akshat Tripathi <akshat@krai.ai>
2025-03-20 16:09:44 +0000

    Replaced torch.cat operations with F.pad
    Signed-off-by: Akshat Tripathi <akshat@krai.ai>

--- vllm/lora/ops/xla_ops/lora_ops.py
+++ vllm/lora/ops/xla_ops/lora_ops.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import torch
+import torch.nn.functional as F
 
 # Required to register the custom ops
 import vllm.lora.ops.xla_ops.pallas  # noqa # pylint: disable=unused-import
@@ -12,22 +13,21 @@ def bgmv_expand(inputs: torch.Tensor,
                 lora_indices_tensor: torch.Tensor,
                 add_inputs: bool = True):
     outputs = torch.ops.xla.bgmv(inputs, lora_b_weights, lora_indices_tensor)
-    n_tokens = outputs.size(0)
 
     limit = output_tensor.shape[0]
     if outputs.shape[0] == 1 and output_tensor.shape[0] != 1:
         limit = 1
 
-    outputs = torch.cat(
-        (outputs,
-         torch.zeros((n_tokens, output_tensor.shape[1] - outputs.shape[1]),
-                     device=outputs.device)),
-        dim=1)
+    if output_tensor.shape[1] > outputs.shape[1]:
+        outputs = F.pad(
+            outputs,
+            (0, output_tensor.shape[1] - outputs.shape[1], 0, 0)
+        )
 
     if add_inputs:
-        return output_tensor + outputs[:limit, :]
+        return output_tensor + outputs[:limit, :output_tensor.shape[1]]
     else:
-        return outputs[:limit, :]
+        return outputs[:limit, :output_tensor.shape[1]]
 
 
 def bgmv_shrink(inputs: torch.Tensor,
@@ -48,16 +48,11 @@ def bgmv_expand_slice(inputs: torch.Tensor,
                       slice_size: int,
                       add_inputs: bool = True):
     outputs = torch.ops.xla.bgmv(inputs, lora_b_weights, lora_indices_tensor)
-    n_tokens = outputs.size(0)
 
-    outputs = torch.cat((
-        torch.zeros((n_tokens, slice_offset), device=outputs.device),
+    outputs = F.pad(
         outputs,
-        torch.zeros(
-            (n_tokens, output_tensor.shape[1] - (slice_offset + slice_size)),
-            device=outputs.device),
-    ),
-                        dim=1)
+        (slice_offset, output_tensor.shape[1] - (slice_offset + slice_size), 0, 0)
+    )
 
     if add_inputs:
         return output_tensor + outputs
