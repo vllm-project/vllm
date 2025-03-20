687d085b024c8e7c500ebbf43f2c06f38fb3f4dc
Hiroaki Sugiyama <h.sugi@ieee.org>
2025-03-21 08:57:09 +0900

    change hf_config to hf_text_config
    Co-authored-by: Woosuk Kwon <woosuk.kwon@berkeley.edu>
    Signed-off-by: h-sugi <h.sugi@ieee.org>

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -223,10 +223,8 @@ class GPUModelRunner(LoRAModelRunnerMixin):
                 pin_memory=self.pin_memory)
 
         # Only relevant for models using ALiBi (e.g, MPT)
-        self.use_alibi = (hasattr(model_config, "hf_config")
-                          and hasattr(model_config.hf_config, "attn_config")
-                          and model_config.hf_config.attn_config.get(
-                              "alibi", False))
+        self.use_alibi = (hasattr(model_config.hf_text_config, "attn_config")
+                                    and model_config.hf_text_config.attn_config.get("alibi", False))
 
         self.inputs_embeds = torch.zeros(
             (self.max_num_tokens, self.hidden_size),
