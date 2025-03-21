5e4a79f9b12a223caa45ef474c151982b1a3f844
Randall Smith <Randall.Smith@amd.com>
2025-03-21 16:18:03 -0500

    remove variable redefinitions
    Signed-off-by: Randall Smith <Randall.Smith@amd.com>

--- vllm/attention/backends/abstract.py
+++ vllm/attention/backends/abstract.py
@@ -236,8 +236,6 @@ class AttentionLayer(Protocol):
     _k_scale: torch.Tensor
     _v_scale: torch.Tensor
     _k_scale_float: float
-    _v_scale_float: float
-    _q_scale: torch.Tensor
     _prob_scale: torch.Tensor
     _fp8_out_scale: torch.Tensor
 
