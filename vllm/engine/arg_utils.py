48a4da8f9062e30065aeccafc58092e2449f939c
hijkzzz <janhu9527@gmail.com>
2025-03-22 15:58:04 +0800

    Fix arg_utils.py for OpenRLHF using vLLM V1 engine

--- vllm/engine/arg_utils.py
+++ vllm/engine/arg_utils.py
@@ -1465,11 +1465,6 @@ class EngineArgs:
                                recommend_to_remove=False)
             return False
 
-        if self.worker_extension_cls != EngineArgs.worker_extension_cls:
-            _raise_or_fallback(feature_name="--worker-extension-cls",
-                               recommend_to_remove=False)
-            return False
-
         if self.num_scheduler_steps != EngineArgs.num_scheduler_steps:
             _raise_or_fallback(feature_name="--num-scheduler-steps",
                                recommend_to_remove=True)
