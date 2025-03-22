effacb061159f245a13a3415ee82e18e9f0cf6fd
hijkzzz <janhu9527@gmail.com>
2025-03-22 16:04:15 +0800

    Update arg_utils.py

--- vllm/engine/arg_utils.py
+++ vllm/engine/arg_utils.py
@@ -1460,11 +1460,6 @@ class EngineArgs:
                                recommend_to_remove=False)
             return False
 
-        if self.worker_cls != EngineArgs.worker_cls:
-            _raise_or_fallback(feature_name="--worker-cls",
-                               recommend_to_remove=False)
-            return False
-
         if self.num_scheduler_steps != EngineArgs.num_scheduler_steps:
             _raise_or_fallback(feature_name="--num-scheduler-steps",
                                recommend_to_remove=True)
