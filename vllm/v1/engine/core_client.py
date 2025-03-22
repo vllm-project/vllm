7756bfbf7c81b6695aee655a884be8747a204069
Wang Ran (汪然) <wrran@outlook.com>
2025-03-23 00:05:03 +0800

    [BugFix][Typing] Fix Imprecise Type Annotations (#15208)
    Signed-off-by: Wang Ran (汪然) <wrran@outlook.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/v1/engine/core_client.py
+++ vllm/v1/engine/core_client.py
@@ -212,9 +212,9 @@ class BackgroundResources:
     """Used as a finalizer for clean shutdown, avoiding
     circular reference back to the client object."""
 
-    ctx: Union[zmq.Context] = None
-    output_socket: Union[zmq.Socket, zmq.asyncio.Socket] = None
-    input_socket: Union[zmq.Socket, zmq.asyncio.Socket] = None
+    ctx: zmq.Context
+    output_socket: Optional[Union[zmq.Socket, zmq.asyncio.Socket]] = None
+    input_socket: Optional[Union[zmq.Socket, zmq.asyncio.Socket]] = None
     proc_handle: Optional[BackgroundProcHandle] = None
     shutdown_path: Optional[str] = None
 
