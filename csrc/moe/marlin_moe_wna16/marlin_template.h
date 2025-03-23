dc17c898954252eaac3bb48416e253c4a406e7e9
Jinzhen Lin <linjinzhen@hotmail.com>
2025-03-23 20:56:03 +0800

    fix format error
    Signed-off-by: Jinzhen Lin <linjinzhen@hotmail.com>

--- csrc/moe/marlin_moe_wna16/marlin_template.h
+++ csrc/moe/marlin_moe_wna16/marlin_template.h
@@ -1669,9 +1669,9 @@ __global__ void Marlin(
       int64_t true_idx = sorted_row * c_gl_stride + c_gl_wr % c_gl_stride;
       if (row < block_num_valid_tokens) {
         scalar_t2 topk_weight_score;
-        if (mul_topk_weights) 
+        if (mul_topk_weights)
           topk_weight_score =
-            Dtype::num2num2(Dtype::float2num(block_topk_weights[row]));
+              Dtype::num2num2(Dtype::float2num(block_topk_weights[row]));
         if (use_atomic_add && slice_count > 1 || mul_topk_weights) {
           scalar_t2* C_half2 = reinterpret_cast<scalar_t2*>(&C[true_idx]);
           scalar_t2* sh_red_half2 =
