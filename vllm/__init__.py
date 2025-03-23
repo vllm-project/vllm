1661af5dd4cdd9f49e2386b3d16b7b9de3428d21
Chen-0210 <chenjincong11@gmail.com>
2025-03-23 03:56:48 +0000

    fix
    Signed-off-by: Chen-0210 <chenjincong11@gmail.com>

--- vllm/__init__.py
+++ vllm/__init__.py
@@ -1,24 +1,14 @@
 # SPDX-License-Identifier: Apache-2.0
 """vLLM: a high-throughput and memory-efficient inference engine for LLMs"""
-# The version.py should be independent library, and we always import the
-# version library first.  Such assumption is critical for some customization.
+
 from .version import __version__, __version_tuple__  # isort:skip
 
-import os
-
 import torch
-from typing import TYPE_CHECKING
-
-from vllm.utils import LazyLoader
-
-# set some common config/environment variables that should be set
-# for all processes created by vllm and all processes
-# that interact with vllm workers.
-# they are executed whenever `import vllm` is called.
+import importlib
+import os
 
 # see https://github.com/NVIDIA/nccl/issues/1234
 os.environ['NCCL_CUMEM_ENABLE'] = '0'
-
 # see https://github.com/vllm-project/vllm/issues/10480
 os.environ['TORCHINDUCTOR_COMPILE_THREADS'] = '1'
 # see https://github.com/vllm-project/vllm/issues/10619
@@ -51,54 +41,43 @@ __all__ = [
     "PoolingParams",
 ]
 
-if TYPE_CHECKING:
-    from vllm.engine.arg_utils import AsyncEngineArgs, EngineArgs
-    from vllm.engine.async_llm_engine import AsyncLLMEngine
-    from vllm.engine.llm_engine import LLMEngine
-    from vllm.entrypoints.llm import LLM
-    from vllm.executor.ray_utils import initialize_ray_cluster
-    from vllm.inputs import PromptType, TextPrompt, TokensPrompt
-    from vllm.model_executor.models import ModelRegistry
-    from vllm.outputs import (ClassificationOutput,
-                              ClassificationRequestOutput, CompletionOutput,
-                              EmbeddingOutput, EmbeddingRequestOutput,
-                              PoolingOutput, PoolingRequestOutput,
-                              RequestOutput, ScoringOutput,
-                              ScoringRequestOutput)
-    from vllm.pooling_params import PoolingParams
-    from vllm.sampling_params import SamplingParams
-else:
-    EngineArgs = LazyLoader("EngineArgs", globals(), "vllm.engine.arg_utils")
-    AsyncEngineArgs = LazyLoader("AsyncEngineArgs", globals(),
-                                 "vllm.engine.arg_utils")
-    AsyncLLMEngine = LazyLoader("AsyncLLMEngine", globals(),
-                                "vllm.engine.async_llm_engine")
-    LLMEngine = LazyLoader("LLMEngine", globals(), "vllm.engine.llm_engine")
-    LLM = LazyLoader("LLM", globals(), "vllm.entrypoints.llm")
-    initialize_ray_cluster = LazyLoader("initialize_ray_cluster", globals(),
-                                        "vllm.executor.ray_utils")
-    PromptType = LazyLoader("PromptType", globals(), "vllm.inputs")
-    TextPrompt = LazyLoader("TextPrompt", globals(), "vllm.inputs")
-    TokensPrompt = LazyLoader("TokensPrompt", globals(), "vllm.inputs")
-    ModelRegistry = LazyLoader("ModelRegistry", globals(),
-                               "vllm.model_executor.models")
-    ClassificationOutput = LazyLoader("ClassificationOutput", globals(),
-                                      "vllm.outputs")
-    ClassificationRequestOutput = LazyLoader("ClassificationRequestOutput",
-                                             globals(), "vllm.outputs")
-    CompletionOutput = LazyLoader("CompletionOutput", globals(),
-                                  "vllm.outputs")
-    EmbeddingOutput = LazyLoader("EmbeddingOutput", globals(), "vllm.outputs")
-    EmbeddingRequestOutput = LazyLoader("EmbeddingRequestOutput", globals(),
-                                        "vllm.outputs")
-    PoolingOutput = LazyLoader("PoolingOutput", globals(), "vllm.outputs")
-    PoolingRequestOutput = LazyLoader("PoolingRequestOutput", globals(),
-                                      "vllm.outputs")
-    RequestOutput = LazyLoader("RequestOutput", globals(), "vllm.outputs")
-    ScoringOutput = LazyLoader("ScoringOutput", globals(), "vllm.outputs")
-    ScoringRequestOutput = LazyLoader("ScoringRequestOutput", globals(),
-                                      "vllm.outputs")
-    SamplingParams = LazyLoader("SamplingParams", globals(),
-                                "vllm.sampling_params")
-    PoolingParams = LazyLoader("PoolingParams", globals(),
-                               "vllm.pooling_params")
+_lazy_modules = {
+    "EngineArgs": "vllm.engine.arg_utils:EngineArgs",
+    "AsyncEngineArgs": "vllm.engine.arg_utils:AsyncEngineArgs",
+    "AsyncLLMEngine": "vllm.engine.async_llm_engine:AsyncLLMEngine",
+    "LLMEngine": "vllm.engine.llm_engine:LLMEngine",
+    "LLM": "vllm.entrypoints.llm:LLM",
+    "initialize_ray_cluster": "vllm.executor.ray_utils:initialize_ray_cluster",
+    "PromptType": "vllm.inputs:PromptType",
+    "TextPrompt": "vllm.inputs:TextPrompt",
+    "TokensPrompt": "vllm.inputs:TokensPrompt",
+    "ModelRegistry": "vllm.model_executor.models:ModelRegistry",
+    "ClassificationOutput": "vllm.outputs:ClassificationOutput",
+    "ClassificationRequestOutput": "vllm.outputs:ClassificationRequestOutput",
+    "CompletionOutput": "vllm.outputs:CompletionOutput",
+    "EmbeddingOutput": "vllm.outputs:EmbeddingOutput",
+    "EmbeddingRequestOutput": "vllm.outputs:EmbeddingRequestOutput",
+    "PoolingOutput": "vllm.outputs:PoolingOutput",
+    "PoolingRequestOutput": "vllm.outputs:PoolingRequestOutput",
+    "RequestOutput": "vllm.outputs:RequestOutput",
+    "ScoringOutput": "vllm.outputs:ScoringOutput",
+    "ScoringRequestOutput": "vllm.outputs:ScoringRequestOutput",
+    "SamplingParams": "vllm.sampling_params:SamplingParams",
+    "PoolingParams": "vllm.pooling_params:PoolingParams",
+}
+
+
+def __getattr__(name):
+
+    if name in _lazy_modules:
+        module_spec = _lazy_modules[name]
+        module_path, attr_name = module_spec.split(":")
+        module = importlib.import_module(module_path)
+        value = getattr(module, attr_name)
+        return value
+    
+    raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
+
+
+def __dir__():
+    return __all__ 
