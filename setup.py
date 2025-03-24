5ca446af07296351c3cdf6f5c3ca60d43585114b
Manish Sethi <manish.sethi@gmail.com>
2025-03-24 11:08:02 -0400

    [Core] Integrate `fastsafetensors` loader for loading model weights (#10647)
    Signed-off-by: Manish Sethi <Manish.sethi1@ibm.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- setup.py
+++ setup.py
@@ -680,6 +680,7 @@ setup(
     install_requires=get_requirements(),
     extras_require={
         "tensorizer": ["tensorizer>=2.9.0"],
+        "fastsafetensors": ["fastsafetensors >= 0.1.10"],
         "runai": ["runai-model-streamer", "runai-model-streamer-s3", "boto3"],
         "audio": ["librosa", "soundfile"],  # Required for audio processing
         "video": ["decord"]  # Required for video processing
