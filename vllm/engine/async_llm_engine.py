b1ac0721836e800932275a08d4afee091b7b7c56
Andrew Sansom <andrew@protopia.ai>
2025-03-24 21:04:47 -0500

    styling
    Signed-off-by: Andrew Sansom <andrew@protopia.ai>

--- vllm/engine/async_llm_engine.py
+++ vllm/engine/async_llm_engine.py
@@ -489,11 +489,9 @@ class _AsyncLLMEngine(LLMEngine):
         if arrival_time is None:
             arrival_time = time.time()
 
-        if (
-            isinstance(prompt, dict) 
-            and prompt.get("prompt_embeds", None) is not None 
-            and not prompt.get("prompt_token_ids", None)
-        ):
+        if (isinstance(prompt, dict)
+                and prompt.get("prompt_embeds", None) is not None
+                and not prompt.get("prompt_token_ids", None)):
             prompt["prompt_token_ids"] = [0] * prompt["prompt_embeds"].shape[0]
 
         if self.tokenizer is not None:
