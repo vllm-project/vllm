25f09c756bbe4f2bca3eb2873a47261d7bbd789e
Thien Tran <gau.nernst@yahoo.com.sg>
2025-03-25 17:34:59 +0800

    [Kernel][CPU] CPU MLA (#14744)
    Signed-off-by: Thien Tran <gau.nernst@yahoo.com.sg>
    Signed-off-by: Mu Huai <tianbowen.tbw@antgroup.com>

--- .buildkite/run-cpu-test.sh
+++ .buildkite/run-cpu-test.sh
@@ -38,6 +38,8 @@ function cpu_tests() {
     set -e
     pip install -r vllm/requirements/test.txt
     pip install -r vllm/requirements/cpu.txt
+    pytest -v -s tests/kernels/test_cache.py -m cpu_model
+    pytest -v -s tests/kernels/test_mla_decode_cpu.py -m cpu_model
     pytest -v -s tests/models/decoder_only/language -m cpu_model
     pytest -v -s tests/models/embedding/language -m cpu_model
     pytest -v -s tests/models/encoder_decoder/language -m cpu_model
