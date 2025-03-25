f3efad8c87e99c6fd56d5c1d671df83dd7c708d9
Joe Runde <Joseph.Runde@ibm.com>
2025-03-25 15:00:32 -0400

    [bugfix] add supports_v1 platform interface (#15417)
    Signed-off-by: Joe Runde <Joseph.Runde@ibm.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/platforms/tpu.py
+++ vllm/platforms/tpu.py
@@ -10,8 +10,9 @@ from vllm.logger import init_logger
 from .interface import Platform, PlatformEnum, _Backend
 
 if TYPE_CHECKING:
-    from vllm.config import VllmConfig
+    from vllm.config import ModelConfig, VllmConfig
 else:
+    ModelConfig = None
     VllmConfig = None
 
 logger = init_logger(__name__)
@@ -126,4 +127,9 @@ class TpuPlatform(Platform):
 
     @classmethod
     def use_all_gather(cls) -> bool:
+        return True
+
+    @classmethod
+    def supports_v1(cls, model_config: ModelConfig) -> bool:
+        # V1 support on TPU is experimental
         return True
