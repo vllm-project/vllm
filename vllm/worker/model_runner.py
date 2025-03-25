a1c0cf590c7d545beedaf5f65baa41a16738bd0c
Andrew Sansom <andrew@protopia.ai>
2025-03-25 09:37:04 -0500

    remove unnecessary changes
    Signed-off-by: Andrew Sansom <andrew@protopia.ai>

--- vllm/worker/model_runner.py
+++ vllm/worker/model_runner.py
@@ -1900,6 +1900,9 @@ class ModelRunner(GPUModelRunnerBase[ModelInputForGPUWithSamplingMetadata]):
             kv_caches: vLLM's paged memory
         """
 
+        if self.vllm_config.kv_transfer_config is None:
+            return False
+
         prefill_meta = model_input.attn_metadata.prefill_metadata
 
         # check if the current run is profiling
@@ -1931,9 +1934,6 @@ class ModelRunner(GPUModelRunnerBase[ModelInputForGPUWithSamplingMetadata]):
         is_profile_run = (kv_caches[0].numel() == 0)
         # check if the current run is prefill
         is_prefill_run = prefill_meta is not None
-
-        if self.vllm_config.kv_transfer_config is None:
-            return False
 
         return self.vllm_config.kv_transfer_config.is_kv_producer and (
             not is_profile_run) and is_prefill_run
