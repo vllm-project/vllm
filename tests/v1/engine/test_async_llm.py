7223ef3750831aa9a99ce22a8188cbbeb468837d
Zijing Liu <liuzijing2014@gmail.com>
2025-03-25 18:21:06 -0700

    address comments
    Signed-off-by: Zijing Liu <liuzijing2014@gmail.com>

--- tests/v1/engine/test_async_llm.py
+++ tests/v1/engine/test_async_llm.py
@@ -14,22 +14,21 @@ from vllm.inputs import PromptType
 from vllm.platforms import current_platform
 from vllm.sampling_params import RequestOutputKind
 from vllm.v1.engine.async_llm import AsyncLLM
-from vllm.v1.metrics.loggers import BuiltinLoggerName, LoggingStatLogger, StatLoggerBase
+from vllm.v1.metrics.loggers import LoggingStatLogger, StatLoggerBase
 
 if not current_platform.is_cuda():
-    pytest.skip(reason="V1 currently only supported on CUDA.", allow_module_level=True)
+    pytest.skip(reason="V1 currently only supported on CUDA.",
+                allow_module_level=True)
 
 TEST_LOGGER_NAME = "test_logger"
 
-TEXT_ENGINE_ARGS = AsyncEngineArgs(
-    model="meta-llama/Llama-3.2-1B-Instruct",
-    enforce_eager=True,
-    disable_log_requests=True,
-)
+TEXT_ENGINE_ARGS = AsyncEngineArgs(model="meta-llama/Llama-3.2-1B-Instruct",
+                                   enforce_eager=True,
+                                   disable_log_requests=True)
 
-VISION_ENGINE_ARGS = AsyncEngineArgs(
-    model="Qwen/Qwen2-VL-2B-Instruct", enforce_eager=True, disable_log_requests=True
-)
+VISION_ENGINE_ARGS = AsyncEngineArgs(model="Qwen/Qwen2-VL-2B-Instruct",
+                                     enforce_eager=True,
+                                     disable_log_requests=True)
 
 TEXT_PROMPT = "Hello my name is Robert and"
 
@@ -37,11 +36,12 @@ VISION_PROMPT_TEMPLATE = (
     "<|im_start|>system\nYou are a helpful assistant.<|im_end|>"
     "\n<|im_start|>user\n<|vision_start|><|image_pad|><|vision_end|>"
     "What is in the image?<|im_end|>\n"
-    "<|im_start|>assistant\n"
-)
+    "<|im_start|>assistant\n")
 VISION_PROMPT = {
     "prompt": VISION_PROMPT_TEMPLATE,
-    "multi_modal_data": {"image": ImageAsset("stop_sign").pil_image},
+    "multi_modal_data": {
+        "image": ImageAsset("stop_sign").pil_image
+    },
 }
 
 
@@ -67,9 +67,9 @@ async def generate(
         n=n,
         prompt_logprobs=prompt_logprobs,
     )
-    async for out in engine.generate(
-        request_id=request_id, prompt=prompt, sampling_params=sampling_params
-    ):
+    async for out in engine.generate(request_id=request_id,
+                                     prompt=prompt,
+                                     sampling_params=sampling_params):
 
         num_tokens = sum(len(output.token_ids) for output in out.outputs)
         if output_kind == RequestOutputKind.DELTA:
@@ -83,8 +83,7 @@ async def generate(
 
 
 @pytest.mark.parametrize(
-    "output_kind", [RequestOutputKind.DELTA, RequestOutputKind.FINAL_ONLY]
-)
+    "output_kind", [RequestOutputKind.DELTA, RequestOutputKind.FINAL_ONLY])
 @pytest.mark.parametrize(
     "engine_args,prompt",
     [(TEXT_ENGINE_ARGS, TEXT_PROMPT), (VISION_ENGINE_ARGS, VISION_PROMPT)],
@@ -115,29 +114,25 @@ async def test_load(
         for request_id in request_ids:
             tasks.append(
                 asyncio.create_task(
-                    generate(
-                        engine, request_id, prompt, output_kind, NUM_EXPECTED_TOKENS
-                    )
-                )
-            )
+                    generate(engine, request_id, prompt, output_kind,
+                             NUM_EXPECTED_TOKENS)))
 
         # Confirm that we got all the EXPECTED tokens from the requests.
-        done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_EXCEPTION)
+        done, pending = await asyncio.wait(tasks,
+                                           return_when=asyncio.FIRST_EXCEPTION)
         for task in pending:
             task.cancel()
         for task in done:
             num_generated_tokens, request_id = await task
             assert num_generated_tokens == NUM_EXPECTED_TOKENS, (
                 f"{request_id} generated {num_generated_tokens} but "
-                f"expected {NUM_EXPECTED_TOKENS}"
-            )
+                f"expected {NUM_EXPECTED_TOKENS}")
 
         assert not engine.output_processor.has_unfinished_requests()
 
 
 @pytest.mark.parametrize(
-    "output_kind", [RequestOutputKind.DELTA, RequestOutputKind.FINAL_ONLY]
-)
+    "output_kind", [RequestOutputKind.DELTA, RequestOutputKind.FINAL_ONLY])
 @pytest.mark.parametrize(
     "engine_args,prompt",
     [(TEXT_ENGINE_ARGS, TEXT_PROMPT), (VISION_ENGINE_ARGS, VISION_PROMPT)],
@@ -167,17 +162,14 @@ async def test_abort(
         # Create concurrent requests.
         tasks: list[asyncio.Task] = []
         for idx, request_id in enumerate(request_ids):
-            max_tokens = (
-                NUM_EXPECTED_TOKENS_LONG
-                if (idx in REQUEST_IDS_TO_ABORT)
-                else NUM_EXPECTED_TOKENS
-            )
+            max_tokens = (NUM_EXPECTED_TOKENS_LONG if
+                          (idx
+                           in REQUEST_IDS_TO_ABORT) else NUM_EXPECTED_TOKENS)
             n = 3 if idx in PARALLEL_SAMPLE_REQ_IDS else 1
             tasks.append(
                 asyncio.create_task(
-                    generate(engine, request_id, prompt, output_kind, max_tokens, n)
-                )
-            )
+                    generate(engine, request_id, prompt, output_kind,
+                             max_tokens, n)))
 
         # API server cancels requests when they disconnect.
         for idx in REQUEST_IDS_TO_ABORT:
@@ -197,8 +189,7 @@ async def test_abort(
                 expected_tokens = NUM_EXPECTED_TOKENS * n
                 assert num_generated_tokens == expected_tokens, (
                     f"{request_id} generated {num_generated_tokens} but "
-                    f"expected {expected_tokens}"
-                )
+                    f"expected {expected_tokens}")
 
         # Make sure all aborted requests were really aborted.
         assert not engine.output_processor.has_unfinished_requests()
@@ -206,8 +197,8 @@ async def test_abort(
         # Confirm we can do another generation.
         request_id = f"request-{REQUEST_IDS_TO_ABORT[0]}"
         task = asyncio.create_task(
-            generate(engine, request_id, prompt, output_kind, NUM_EXPECTED_TOKENS)
-        )
+            generate(engine, request_id, prompt, output_kind,
+                     NUM_EXPECTED_TOKENS))
         num_generated_tokens, request_id = await task
         assert num_generated_tokens == NUM_EXPECTED_TOKENS
         assert not engine.output_processor.has_unfinished_requests()
@@ -241,9 +232,9 @@ async def test_finished_flag(
         )
         outputs = [
             out
-            async for out in engine.generate(
-                request_id="request-33", prompt=prompt, sampling_params=sampling_params
-            )
+            async for out in engine.generate(request_id="request-33",
+                                             prompt=prompt,
+                                             sampling_params=sampling_params)
         ]
 
         # Assert only the last output has the finished flag set
@@ -259,7 +250,9 @@ def get_customized_logger_mock() -> StatLoggerBase:
 
 @pytest.mark.parametrize(
     "loggers",
-    [{TEST_LOGGER_NAME: get_customized_logger_mock()}],
+    [{
+        TEST_LOGGER_NAME: get_customized_logger_mock()
+    }],
 )
 @pytest.mark.asyncio
 async def test_customize_loggers(
