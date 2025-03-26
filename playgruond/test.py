164b1f628b827b2404fcb9ac7a0d912682eb910e
tom <1005484344wxy@gmail.com>
2025-03-25 20:04:17 -0700

    make it more fine grained

--- playgruond/test.py
+++ playgruond/test.py
@@ -19,11 +19,11 @@ sampling_params = SamplingParams(temperature=0.8, top_p=0.95,  max_tokens=100)
 # llm = LLM(model="facebook/opt-1.3b", gpu_memory_utilization=0.05, max_model_len=384, preemption_mode="swap", scheduling_policy='priority')
 llm = LLM(model="facebook/opt-2.7b", gpu_memory_utilization=0.1, max_model_len=384, preemption_mode="swap", scheduling_policy='priority')
 
-for factor in np.arange(0, 12, 0.4):
+for factor in np.arange(0, 12, 0.2):
     with open('config.log', 'w') as f:
         f.write(f'{factor}')
     
-    for _ in range(5):
+    for _ in range(10):
         # Generate texts from the prompts. The output is a list of RequestOutput objects
         # that contain the prompt, generated text, and other information.
         outputs = llm.generate(prompts, sampling_params)
--- vllm/core/scheduler.py
+++ vllm/core/scheduler.py
@@ -1556,14 +1556,14 @@ class Scheduler:
         else:
             preemption_mode = PreemptionMode.RECOMPUTE
 
-        if self.num_cumulative_preemption % 50 == 0:
-            logger.warning(
-                "Sequence group %s is preempted by %s mode because there is "
-                "not enough KV cache space. This can affect the end-to-end "
-                "performance. Increase gpu_memory_utilization or "
-                "tensor_parallel_size to provide more KV cache memory. "
-                "total_num_cumulative_preemption=%d", seq_group.request_id,
-                preemption_mode, self.num_cumulative_preemption + 1)
+        # if self.num_cumulative_preemption % 50 == 0:
+        #     logger.warning(
+        #         "Sequence group %s is preempted by %s mode because there is "
+        #         "not enough KV cache space. This can affect the end-to-end "
+        #         "performance. Increase gpu_memory_utilization or "
+        #         "tensor_parallel_size to provide more KV cache memory. "
+        #         "total_num_cumulative_preemption=%d", seq_group.request_id,
+        #         preemption_mode, self.num_cumulative_preemption + 1)
         self.num_cumulative_preemption += 1
 
         if preemption_mode == PreemptionMode.RECOMPUTE:
