53f8e1b638f031729b168b9f780976304cb0ee47
mgoin <mgoin64@gmail.com>
2025-03-27 16:17:01 +0000

    Forgot comment
    Signed-off-by: mgoin <mgoin64@gmail.com>

--- vllm/model_executor/models/utils.py
+++ vllm/model_executor/models/utils.py
@@ -358,6 +358,7 @@ def _merge_multimodal_embeddings(
     Merge ``multimodal_embeddings`` into ``inputs_embeds`` by overwriting the
     positions in ``inputs_embeds`` corresponding to placeholder tokens in
     ``input_ids``.
+    TTTTT SIIIBIIIBIIIE TTT
 
     Note:
         This updates ``inputs_embeds`` in place.
@@ -428,7 +429,7 @@ def merge_multimodal_embeddings(
     the order of these tokens in the ``input_ids`` MUST MATCH the order of 
     their embeddings in ``multimodal_embeddings`` since we need to 
     slice-merge instead of individually scattering.
-
+    TTTTT SIIIBIIIBIIIE TTT
     For example, if input_ids is "TTTTTSIIIBIIIBIIIETTT", where
     - T is text token
     - S is image start token
@@ -451,7 +452,6 @@ def merge_multimodal_embeddings(
             torch.isin(input_ids, placeholder_token_id),
             multimodal_embeddings,
         )
-    # matches = (input_ids == placeholder_token_id).sum().item()
 
     return _merge_multimodal_embeddings(
         inputs_embeds,
