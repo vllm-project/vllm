168cad3a8e028c3fd2f9558067a2bf1655e759d6
Jerry Zhang <jerryzh168@gmail.com>
2025-03-26 17:18:12 -0700

    formatting

--- vllm/model_executor/models/siglip.py
+++ vllm/model_executor/models/siglip.py
@@ -276,7 +276,9 @@ class SiglipMLP(nn.Module):
         self.config = config
         self.activation_fn = get_act_fn(config.hidden_act)
         # Special handling for BNB and torchao quantization
-        if quant_config and quant_config.get_name() in ["bitsandbytes", "torchao"]:
+        if quant_config and quant_config.get_name() in [
+                "bitsandbytes", "torchao"
+        ]:
             quantizable = True
         else:
             # For other quantization, we require the hidden size to be a
