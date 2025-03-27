a9e217d43527442511114c903a786ab9c8af7e65
Gregory Shtrasberg <156009573+gshtras@users.noreply.github.com>
2025-03-27 01:46:12 -0400

    [ROCm] Env variable to trigger custom PA (#15557)
    Signed-off-by: Gregory Shtrasberg <Gregory.Shtrasberg@amd.com>
    Signed-off-by: Mu Huai <tianbowen.tbw@antgroup.com>

--- vllm/attention/backends/rocm_flash_attn.py
+++ vllm/attention/backends/rocm_flash_attn.py
@@ -908,4 +908,5 @@ def _use_rocm_custom_paged_attention(qtype: torch.dtype, head_size: int,
             and (qtype == torch.half or qtype == torch.bfloat16)
             and (head_size == 64 or head_size == 128)
             and (block_size == 16 or block_size == 32)
-            and (gqa_ratio >= 1 and gqa_ratio <= 16) and max_seq_len <= 32768)
+            and (gqa_ratio >= 1 and gqa_ratio <= 16) and max_seq_len <= 32768
+            and envs.VLLM_ROCM_CUSTOM_PAGED_ATTN)
