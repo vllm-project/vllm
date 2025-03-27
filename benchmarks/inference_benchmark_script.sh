27e2993e385f3b34afac53791805073b94153eeb
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2025-03-27 13:17:37 -0700

    Update inference_benchmark_script.sh

--- benchmarks/inference_benchmark_script.sh
+++ benchmarks/inference_benchmark_script.sh
@@ -8,5 +8,5 @@ curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /
 apt-get update && apt-get install -y google-cloud-sdk
 apt-get -y install jq
 export PJRT_DEVICE=TPU
-python inference-benchmark/benchmark_serving.py --save-json-results --port=8009 --dataset=ShareGPT_V3_unfiltered_cleaned_split.json --tokenizer=meta-llama/Meta-Llama-3-8B --request-rate=1 --backend=vllm --num-prompts=300 --max-input-length=1024 --max-output-length=1024 --file-prefix=benchmark --models=meta-llama/Meta-Llama-3-8B --stream-request --output-bucket="gs://manfeipublic"
+python inference-benchmark/benchmark_serving.py --save-json-results --port=8009 --dataset=ShareGPT_V3_unfiltered_cleaned_split.json --tokenizer=meta-llama/Meta-Llama-3-8B --request-rate=1 --backend=vllm --num-prompts=300 --max-input-length=1024 --max-output-length=1024 --file-prefix=benchmark --models=meta-llama/Meta-Llama-3-8B --stream-request --output-bucket="$0"
 ls
