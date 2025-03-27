89a5461676137dfade0ec8db678ddb23c7f1b54e
Andrew Feldman <afeldman@neuralmagic.com>
2025-03-27 23:54:34 +0000

    removed tests of LLM engine without MP
    Signed-off-by: Andrew Feldman <afeldman@neuralmagic.com>

--- tests/v1/shutdown/test_forward_error.py
+++ tests/v1/shutdown/test_forward_error.py
@@ -14,6 +14,8 @@ from vllm.utils import cuda_device_count_stateless
 from vllm.v1.engine.async_llm import AsyncLLM
 from vllm.v1.engine.exceptions import EngineDeadError
 
+MODELS = ["meta-llama/Llama-3.2-1B"]
+
 
 def evil_forward(self, *args, **kwargs):
     """Evil forward method that raise an exception after 10 calls."""
@@ -32,72 +34,73 @@ def evil_forward(self, *args, **kwargs):
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("tensor_parallel_size", [2, 1])
-@pytest.mark.parametrize("enable_multiprocessing", [True, False])
-async def test_async_llm_model_error(monkeypatch, tensor_parallel_size,
-                                     enable_multiprocessing):
+@pytest.mark.parametrize("model", MODELS)
+async def test_async_llm_model_error(monkeypatch, tensor_parallel_size: int,
+                                     model: str) -> None:
+    """Test that AsyncLLM propagates a forward pass error and frees memory.
+    
+    AsyncLLM always uses an MP client.
+    """
 
     if cuda_device_count_stateless() < tensor_parallel_size:
         pytest.skip(reason="Not enough CUDA devices")
 
-    with monkeypatch.context() as m:
-        MP_VALUE = "1" if enable_multiprocessing else "0"
-        m.setenv("VLLM_ENABLE_V1_MULTIPROCESSING", MP_VALUE)
+    # Monkeypatch an error in the model.
+    monkeypatch.setattr(LlamaForCausalLM, "forward", evil_forward)
 
-        # Monkeypatch an error in the model.
-        monkeypatch.setattr(LlamaForCausalLM, "forward", evil_forward)
+    engine_args = AsyncEngineArgs(model=model,
+                                  enforce_eager=True,
+                                  tensor_parallel_size=tensor_parallel_size)
+    async_llm = AsyncLLM.from_engine_args(engine_args)
 
-        engine_args = AsyncEngineArgs(
-            model="meta-llama/Llama-3.2-1B",
-            enforce_eager=True,
-            tensor_parallel_size=tensor_parallel_size)
-        async_llm = AsyncLLM.from_engine_args(engine_args)
+    async def generate(request_id: str):
+        generator = async_llm.generate("Hello my name is",
+                                       request_id=request_id,
+                                       sampling_params=SamplingParams())
+        try:
+            async for _ in generator:
+                pass
+        except Exception as e:
+            return e
 
-        async def generate(request_id: str):
-            generator = async_llm.generate("Hello my name is",
-                                           request_id=request_id,
-                                           sampling_params=SamplingParams())
-            try:
-                async for _ in generator:
-                    pass
-            except Exception as e:
-                return e
+    NUM_REQS = 3
+    tasks = [generate(f"request-{idx}") for idx in range(NUM_REQS)]
+    outputs = await asyncio.gather(*tasks)
 
-        NUM_REQS = 3
-        tasks = [generate(f"request-{idx}") for idx in range(NUM_REQS)]
-        outputs = await asyncio.gather(*tasks)
+    # Every request should get an EngineDeadError.
+    for output in outputs:
+        assert isinstance(output, EngineDeadError)
 
-        # Every request should get an EngineDeadError.
-        for output in outputs:
-            assert isinstance(output, EngineDeadError)
+    # AsyncLLM should be errored.
+    assert async_llm.errored
 
-        # AsyncLLM should be errored.
-        assert async_llm.errored
+    # We should not be able to make another request.
+    with pytest.raises(EngineDeadError):
+        async for _ in async_llm.generate("Hello my name is",
+                                          request_id="abc",
+                                          sampling_params=SamplingParams()):
+            raise Exception("We should not get here.")
 
-        # We should not be able to make another request.
-        with pytest.raises(EngineDeadError):
-            async for _ in async_llm.generate(
-                    "Hello my name is",
-                    request_id="abc",
-                    sampling_params=SamplingParams()):
-                raise Exception("We should not get here.")
+    # Confirm all the processes are cleaned up.
+    wait_for_gpu_memory_to_clear(
+        devices=list(range(tensor_parallel_size)),
+        threshold_bytes=2 * 2**30,
+        timeout_s=60,
+    )
 
-        # Confirm all the processes are cleaned up.
-        wait_for_gpu_memory_to_clear(
-            devices=list(range(tensor_parallel_size)),
-            threshold_bytes=2 * 2**30,
-            timeout_s=60,
-        )
-
-        # NOTE: shutdown is handled by the API Server if an exception
-        # occurs, so it is expected that we would need to call this.
-        async_llm.shutdown()
+    # NOTE: shutdown is handled by the API Server if an exception
+    # occurs, so it is expected that we would need to call this.
+    async_llm.shutdown()
 
 
-@pytest.mark.parametrize("enable_multiprocessing", [True, False])
+@pytest.mark.parametrize("enable_multiprocessing", [True])
 @pytest.mark.parametrize("tensor_parallel_size", [2, 1])
-def test_llm_model_error(monkeypatch, tensor_parallel_size,
-                         enable_multiprocessing):
-
+@pytest.mark.parametrize("model", MODELS)
+def test_llm_model_error(monkeypatch, tensor_parallel_size: int,
+                         enable_multiprocessing: bool, model: str) -> None:
+    """Test that LLM propagates a forward pass error and frees memory.
+    TODO(andy) - LLM without multiprocessing.
+    """
     if cuda_device_count_stateless() < tensor_parallel_size:
         pytest.skip(reason="Not enough CUDA devices")
 
@@ -109,7 +112,7 @@ def test_llm_model_error(monkeypatch, tensor_parallel_size,
         # Monkeypatch an error in the model.
         m.setattr(LlamaForCausalLM, "forward", evil_forward)
 
-        llm = LLM(model="meta-llama/Llama-3.2-1B",
+        llm = LLM(model=model,
                   enforce_eager=True,
                   tensor_parallel_size=tensor_parallel_size)
 
@@ -117,9 +120,9 @@ def test_llm_model_error(monkeypatch, tensor_parallel_size,
                 EngineDeadError if enable_multiprocessing else Exception):
             llm.generate("Hello my name is Robert and I")
 
-            # Confirm all the processes are cleaned up.
-            wait_for_gpu_memory_to_clear(
-                devices=list(range(tensor_parallel_size)),
-                threshold_bytes=2 * 2**30,
-                timeout_s=60,
-            )
+        # Confirm all the processes are cleaned up.
+        wait_for_gpu_memory_to_clear(
+            devices=list(range(tensor_parallel_size)),
+            threshold_bytes=2 * 2**30,
+            timeout_s=60,
+        )
--- tests/v1/shutdown/test_startup_error.py
+++ tests/v1/shutdown/test_startup_error.py
@@ -29,43 +29,44 @@ MODELS = [
 
 @pytest.mark.parametrize("model", MODELS)
 @pytest.mark.parametrize("tensor_parallel_size", [2, 1])
-@pytest.mark.parametrize("enable_multiprocessing", [True, False])
-def test_async_llm_startup_error(monkeypatch, model, tensor_parallel_size,
-                                 enable_multiprocessing):
-
+def test_async_llm_startup_error(monkeypatch, model: str,
+                                 tensor_parallel_size: int) -> None:
+    """Test that AsyncLLM propagates an __init__ error & frees memory.
+    
+    AsyncLLM always uses an MP client.
+    """
     if cuda_device_count_stateless() < tensor_parallel_size:
         pytest.skip(reason="Not enough CUDA devices")
 
-    with monkeypatch.context() as m:
-        MP_VALUE = "1" if enable_multiprocessing else "0"
-        m.setenv("VLLM_ENABLE_V1_MULTIPROCESSING", MP_VALUE)
+    # Monkeypatch an error in the model.
+    monkeypatch.setattr(LlamaForCausalLM, "forward", evil_forward)
 
-        # Monkeypatch an error in the model.
-        monkeypatch.setattr(LlamaForCausalLM, "forward", evil_forward)
+    engine_args = AsyncEngineArgs(model=model,
+                                  enforce_eager=True,
+                                  tensor_parallel_size=tensor_parallel_size)
 
-        engine_args = AsyncEngineArgs(
-            model=model,
-            enforce_eager=True,
-            tensor_parallel_size=tensor_parallel_size)
+    # Confirm we get an exception.
+    with pytest.raises(Exception, match="initialization failed"):
+        _ = AsyncLLM.from_engine_args(engine_args)
 
-        # Confirm we get an exception.
-        with pytest.raises(Exception, match="initialization failed"):
-            _ = AsyncLLM.from_engine_args(engine_args)
-
-        # Confirm all the processes are cleaned up.
-        wait_for_gpu_memory_to_clear(
-            devices=list(range(tensor_parallel_size)),
-            threshold_bytes=2 * 2**30,
-            timeout_s=60,
-        )
+    # Confirm all the processes are cleaned up.
+    wait_for_gpu_memory_to_clear(
+        devices=list(range(tensor_parallel_size)),
+        threshold_bytes=2 * 2**30,
+        timeout_s=60,
+    )
 
 
 @pytest.mark.parametrize("model", MODELS)
 @pytest.mark.parametrize("tensor_parallel_size", [2, 1])
-@pytest.mark.parametrize("enable_multiprocessing", [True, False])
-def test_llm_startup_error(monkeypatch, model, tensor_parallel_size,
-                           enable_multiprocessing):
-
+@pytest.mark.parametrize("enable_multiprocessing", [True])
+def test_llm_startup_error(monkeypatch, model: str, tensor_parallel_size: int,
+                           enable_multiprocessing: bool) -> None:
+    """Test that LLM propagates an __init__ error and frees memory.
+    TODO(andy) - LLM without multiprocessing.
+    """
+    if model != "meta-llama/Llama-3.2-1B":
+        pytest.skip(reason="Only test meta-llama/Llama-3.2-1B")
     if cuda_device_count_stateless() < tensor_parallel_size:
         pytest.skip(reason="Not enough CUDA devices")
 
@@ -81,7 +82,7 @@ def test_llm_startup_error(monkeypatch, model, tensor_parallel_size,
                 Exception,
                 match="initialization failed"
                 if enable_multiprocessing else "Simulated Error in startup!"):
-            _ = LLM(model="meta-llama/Llama-3.2-1B",
+            _ = LLM(model=model,
                     enforce_eager=True,
                     tensor_parallel_size=tensor_parallel_size)
 
