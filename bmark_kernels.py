fbb902ae343d601cecf4f0a5f992babf99e83a83
Akshat Tripathi <akshat@krai.ai>
2025-03-27 21:01:16 +0000

    Updated kernel benchmarking script with lora laning
    Signed-off-by: Akshat Tripathi <akshat@krai.ai>

--- bmark_kernels.py
+++ bmark_kernels.py
@@ -26,32 +26,30 @@ def create_tensors(T, D, L, N, dtype=torch.bfloat16, device='xla'):
     
     return inputs, loras, idxs
 
-# SEQ_LENS = [8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 131072]
-# HIDDEN_DIM = [256, 1024, 4096, 8192, 14336, 28672]
-# LORA_RANKS = [8, 16, 32, 64, 128, 128]
-# N_LORAS = [1, 2, 4, 8]
-
-
-SEQ_LENS = [1024]
-HIDDEN_DIM = [4096]
-LORA_RANKS = [32]
-N_LORAS = [1]
+SEQ_LENS = [8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 131072]
+HIDDEN_DIM = [256, 1024, 4096, 8192, 14336, 28672]
+LORA_RANKS = [8, 16, 32, 64, 128, 128]
+N_LORAS = [1, 2, 4, 8]
 
 @torch.compile(fullgraph=True, dynamic=False, backend="openxla")
 def ref_bgmv(inputs: torch.Tensor, loras: torch.Tensor, idxs: torch.IntTensor):
     return torch.einsum("td,tld->tl", inputs, loras[idxs])
 
 @torch.compile(fullgraph=True, dynamic=False, backend="openxla")
-def bgmv(inputs: torch.Tensor, loras: torch.Tensor, idxs: torch.IntTensor):
-    return torch.ops.xla.bgmv(inputs, loras, idxs)
+def bgmv_shrink(inputs: torch.Tensor, loras: torch.Tensor, idxs: torch.IntTensor):
+    return torch.ops.xla.bgmv_shrink(inputs, loras, idxs)
+
+@torch.compile(fullgraph=True, dynamic=False, backend="openxla")
+def bgmv_expand(inputs: torch.Tensor, loras: torch.Tensor, idxs: torch.IntTensor, enable_laning: bool):
+    return torch.ops.xla.bgmv_expand(inputs, loras, idxs, enable_laning)
 
 @torch.compile(fullgraph=True, dynamic=False, backend="openxla")
 def shrink_and_expand(inputs: torch.Tensor, loras_a: torch.Tensor, loras_b: torch.Tensor, idxs: torch.IntTensor):
-    # TODO: Fuse kernels
-    return bgmv(
-        bgmv(inputs, loras_a, idxs),
+    return bgmv_expand(
+        bgmv_shrink(inputs, loras_a, idxs),
         loras_b,
-        idxs
+        idxs,
+        enable_laning=True
     )
 
 @torch.compile(fullgraph=True, dynamic=False, backend="openxla")
@@ -69,24 +67,23 @@ def run_and_wait_torch(func, *args):
     return out
 
 @pytest.mark.parametrize("T,D,L,N", itertools.product(SEQ_LENS, HIDDEN_DIM, LORA_RANKS, N_LORAS))
-@pytest.mark.parametrize("func", [bgmv, ref_bgmv])
+@pytest.mark.parametrize("func", [bgmv_shrink])
 def test_bmark_shrink(benchmark, T, D, L, N, func):
     inputs, loras, idxs = create_tensors(T, D, L, N)
     
-    benchmark.pedantic(partial(run_and_wait_torch, func), args=(inputs, loras, idxs), rounds=5, warmup_rounds=5, iterations=100)
+    benchmark.pedantic(partial(run_and_wait_torch, func), args=(inputs, loras, idxs), rounds=5, warmup_rounds=5, iterations=10)
 
 @pytest.mark.parametrize("T,D,L,N", itertools.product(SEQ_LENS, LORA_RANKS, HIDDEN_DIM, N_LORAS))
-@pytest.mark.parametrize("func", [bgmv, ref_bgmv])
+@pytest.mark.parametrize("func", [bgmv_expand])
 def test_bmark_expand(benchmark, T, D, L, N, func):
     inputs, loras, idxs = create_tensors(T, D, L, N)
     
-    benchmark.pedantic(partial(run_and_wait_torch, func), args=(inputs, loras, idxs), rounds=5, warmup_rounds=5, iterations=100)
-
+    benchmark.pedantic(partial(run_and_wait_torch, func), args=(inputs, loras, idxs), rounds=5, warmup_rounds=5, iterations=10)
 
 @pytest.mark.parametrize("T,D,L,N", itertools.product(SEQ_LENS, HIDDEN_DIM, LORA_RANKS, N_LORAS))
-@pytest.mark.parametrize("func", [shrink_and_expand, ref_shrink_and_expand])
+@pytest.mark.parametrize("func", [shrink_and_expand])
 def test_bmark_shrink_and_expand(benchmark, T, D, L, N, func):
     inputs, loras_a, idxs = create_tensors(T, D, L, N)
     _, loras_b, _ = create_tensors(T, L, D, N)
     
-    benchmark.pedantic(partial(run_and_wait_torch, func), args=(inputs, loras_a, loras_b, idxs), rounds=5, warmup_rounds=5, iterations=100)
+    benchmark.pedantic(partial(run_and_wait_torch, func), args=(inputs, loras_a, loras_b, idxs), rounds=5, warmup_rounds=5, iterations=10)
