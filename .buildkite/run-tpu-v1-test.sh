fa3aba95bcd1a2b6db25604cc1366af4347f43d9
Nicolò Lucchesi <nlucches@redhat.com>
2025-03-27 20:15:06 +0100

    [Bugfix][TPU][V1] Fix recompilation (#15553)
    Signed-off-by: NickLucche <nlucches@redhat.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- .buildkite/run-tpu-v1-test.sh
+++ .buildkite/run-tpu-v1-test.sh
@@ -32,7 +32,9 @@ docker run --privileged --net host --shm-size=16G -it \
     && echo TEST_5 \
     && python3 /workspace/vllm/examples/offline_inference/tpu.py \
     && echo TEST_6 \
-    && pytest -s -v /workspace/vllm/tests/tpu/worker/test_tpu_model_runner.py" \
+    && pytest -s -v /workspace/vllm/tests/tpu/worker/test_tpu_model_runner.py \
+    && echo TEST_7 \
+    && pytest -s -v /workspace/vllm/tests/v1/tpu/test_sampler.py" \
 
 
 # TODO: This test fails because it uses RANDOM_SEED sampling
