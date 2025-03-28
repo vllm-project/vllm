4780232a05998ad0c694f690f2824dc4dd4b1087
shangmingc <caishangming@linux.alibaba.com>
2025-03-29 01:33:32 +0800

    [Misc] Remove stale func in KVTransferConfig (#14746)
    Signed-off-by: Shangming Cai <caishangming@linux.alibaba.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/config.py
+++ vllm/config.py
@@ -2987,12 +2987,6 @@ class KVTransferConfig(BaseModel):
             self.kv_role in ["kv_producer", "kv_consumer", "kv_both"]
 
     @property
-    def need_kv_parallel_group(self) -> bool:
-        # for those database-based connector, vLLM does not need to create
-        # parallel group, and in that case the kv parallel size will be 1.
-        return self.kv_connector is not None and self.kv_parallel_size > 1
-
-    @property
     def is_kv_producer(self) -> bool:
         return self.kv_connector is not None and \
             self.kv_role in ["kv_producer", "kv_both"]
