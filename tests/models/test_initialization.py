0644ac456f6b2d9287730b3cfca83e3c0dc7fb73
Shangming Cai <caishangming@linux.alibaba.com>
2025-03-30 18:20:26 +0800

    fix model register test
    Signed-off-by: Shangming Cai <caishangming@linux.alibaba.com>

--- tests/models/test_initialization.py
+++ tests/models/test_initialization.py
@@ -55,11 +55,9 @@ def test_can_initialize(model_arch):
             tokenizer=model_info.tokenizer,
             tokenizer_mode=model_info.tokenizer_mode,
             speculative_config={
-                "model":
-                model_info.speculative_model,
-                "num_speculative_tokens":
-                1 if model_info.speculative_model else None,
-            },
+                "model": model_info.speculative_model,
+                "num_speculative_tokens": 1,
+            } if model_info.speculative_model else None,
             trust_remote_code=model_info.trust_remote_code,
             load_format="dummy",
             hf_overrides=hf_overrides,
