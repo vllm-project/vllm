1e9d1aec9806a22a89087f15d0c5c2be6b6fadc7
yihong <zouzou0208@gmail.com>
2025-03-31 01:47:57 +0800

    fix: Comments to English for better dev experience (#15768)
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>
    Signed-off-by: Mu Huai <tianbowen.tbw@antgroup.com>

--- vllm/model_executor/models/deepseek_vl2.py
+++ vllm/model_executor/models/deepseek_vl2.py
@@ -509,7 +509,7 @@ class DeepseekVLV2ForCausalLM(nn.Module, SupportsMultiModal, SupportsPP):
         _, hw, n_dim = images_embeds.shape
         h = w = int(hw**0.5)
 
-        # 根据self.tile_tag & self.global_view_pos填充image token sequence
+        # fill image token based on self.tile_tag & self.global_view_pos
         tile_index = 0
         vision_embeddings = []
         for jdx in range(images_spatial_crop.size(0)):
