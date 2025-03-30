bc98619d0e7e3663fc15439a56c5269d70e1f489
warjiang <1096409085@qq.com>
2025-03-30 10:12:48 -0700

    feat: update allow_pattern
    Signed-off-by: warjiang <1096409085@qq.com>

--- vllm/config.py
+++ vllm/config.py
@@ -458,8 +458,11 @@ class ModelConfig:
         if is_s3(model) or is_s3(tokenizer):
             if is_s3(model):
                 s3_model = S3Model()
-                s3_model.pull_files(
-                    model, allow_pattern=["*.model", "*.py", "*.json"])
+                s3_model.pull_files(model,
+                                    allow_pattern=[
+                                        "*.model", "*.py", "*.json",
+                                        "*.safetensors"
+                                    ])
                 self.model_weights = self.model
                 self.model = s3_model.dir
 
