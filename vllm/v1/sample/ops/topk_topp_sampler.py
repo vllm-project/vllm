0a81a65b461dbb3edd7255a1324d749630655f36
Hyesoo Yang <hyeygit@gmail.com>
2025-03-31 21:44:20 +0000

    [V1][TPU] TPU-optimized top-k implementation (2nd try)
    Previously we found that using torch.topk resulted in significant
    speed up for TPU. Turns out that's not a viable solution because the
    return shape of torch.topk depends on k, which means an XLA recompilation
    is triggered everytime k changes.
    
    Additionally, we realized that torch.scatter was the main bottleneck for
    the original top-k impl on TPU. This PR circumvents both problems by using
    a threshold-based approach to find the top-k set. The algorithm is nearly
    identical to that of top-p; see #15736 for more details.
    
    Signed-off-by: Hyesoo Yang <hyeygit@gmail.com>

--- vllm/v1/sample/ops/topk_topp_sampler.py
+++ vllm/v1/sample/ops/topk_topp_sampler.py
@@ -139,12 +139,22 @@ def apply_top_k_top_p_tpu(
     chance of being chosen during final sampling, so we can consider the tie
     being broken then.
     """
+    probs = logits.softmax(dim=-1)
+    probs_sort, _ = probs.sort(dim=-1, descending=False)
+
     if k is not None:
-        logits = apply_top_k_only(logits, k)
+        top_k_count = probs_sort.size(1) - k.to(torch.long)  # shape: (batch, )
+        top_k_count = top_k_count.unsqueeze(dim=1)
+        top_k_cutoff = probs_sort.gather(-1, top_k_count)
+
+        # Make sure the no top-k rows are no-op.
+        no_top_k_mask = (k == logits.shape[1]).unsqueeze(dim=1)
+        top_k_cutoff.masked_fill_(no_top_k_mask, -float("inf"))
+
+        elements_to_discard = probs < top_k_cutoff
+        logits.masked_fill_(elements_to_discard, -float("inf"))
 
     if p is not None:
-        probs = logits.softmax(dim=-1)
-        probs_sort, _ = probs.sort(dim=-1, descending=False)
         cumprob = torch.cumsum(probs_sort, dim=-1)
         top_p_mask = cumprob <= 1 - p.unsqueeze(dim=1)
         top_p_mask[:, -1] = False  # at least one
