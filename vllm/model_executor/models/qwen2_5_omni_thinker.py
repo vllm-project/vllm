98226adc6f00b56c1a9399bbe8dfbd1a80b82b1f
Roger Wang <ywang@roblox.com>
2025-03-31 00:27:17 -0700

    add TODO
    Signed-off-by: Roger Wang <ywang@roblox.com>

--- vllm/model_executor/models/qwen2_5_omni_thinker.py
+++ vllm/model_executor/models/qwen2_5_omni_thinker.py
@@ -821,6 +821,9 @@ class Qwen2_5OmniThinkerForConditionalGeneration(
     ) -> torch.Tensor:
         inputs_embeds = self.language_model.get_input_embeddings(input_ids)
         if multimodal_embeddings is not None:
+
+            # TODO (ywang96): support overlapping modalitiy embeddings so that
+            # `use_audio_in_video` will work on V1.
             inputs_embeds = merge_multimodal_embeddings(
                 input_ids, inputs_embeds, multimodal_embeddings, [
                     self.config.image_token_id, self.config.video_token_id,
