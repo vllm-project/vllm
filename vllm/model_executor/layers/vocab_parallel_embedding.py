c186d7b8434e4490fad3da52cb45a902b84cc597
Alexey Kiryushin <alexey.a.kiryushin@gmail.com>
2025-04-01 05:57:59 +0000

    [Bugfix]: Fix is_embedding_layer condition in VocabParallelEmbedding  (#15824)
    Signed-off-by: alexwl <alexey.a.kiryushin@gmail.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/model_executor/layers/vocab_parallel_embedding.py
+++ vllm/model_executor/layers/vocab_parallel_embedding.py
@@ -235,7 +235,7 @@ class VocabParallelEmbedding(torch.nn.Module):
         # If we are making an embedding layer, then our quantization linear
         # method must implement the embedding operation. If we are another
         # layer type like ParallelLMHead, this is not important.
-        is_embedding_layer = type(self.__class__) is VocabParallelEmbedding
+        is_embedding_layer = type(self) is VocabParallelEmbedding
         quant_method_implements_embedding = method_has_implemented_embedding(
             type(quant_method))
         if is_embedding_layer and not quant_method_implements_embedding:
