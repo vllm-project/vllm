985034c459883218db2cc4470f504662ba943935
kaixih <kaixih@nvidia.com>
2025-04-01 18:29:02 +0000

    Support cutlass MLA and Upgrade cutlass to 3.9
    Signed-off-by: kaixih <kaixih@nvidia.com>

--- vllm/_custom_ops.py
+++ vllm/_custom_ops.py
@@ -1525,3 +1525,18 @@ def flash_mla_with_kvcache(
         num_splits,
     )
     return out, softmax_lse
+
+
+def cutlass_mla_decode(q_nope_and_q_pe: torch.Tensor,
+                       kv_c_and_k_pe_cache: torch.Tensor,
+                       seq_lens: torch.Tensor,
+                       page_table: torch.Tensor) -> torch.Tensor:
+    B_q, H, _ = q_nope_and_q_pe.shape
+
+    out = torch.empty((B_q, H, 512),
+                      device=q_nope_and_q_pe.device,
+                      dtype=q_nope_and_q_pe.dtype)
+
+    torch.ops._C.cutlass_mla_decode(out, q_nope_and_q_pe, kv_c_and_k_pe_cache,
+                                    seq_lens, page_table)
+    return out
