9b39a58914c79aca6d83c2316eddfdc62434bd7e
付 <fulei@ratutech.com>
2025-04-01 11:19:42 +0800

    update
    Signed-off-by: 付 <fulei@ratutech.com>

--- vllm/entrypoints/openai/tool_parsers/hermes_tool_parser.py
+++ vllm/entrypoints/openai/tool_parsers/hermes_tool_parser.py
@@ -340,8 +340,8 @@ class Hermes2ProToolParser(ToolParser):
 
             # last case -- we have an update to existing arguments.
             elif cur_arguments and prev_arguments:
-                # isinstance(delta_text, str) 判断 delta_text 是不是字符串类型
-                # 多了一个等于号len(delta_text.rstrip()) >= 1 把'}'这种情况给过滤成空了。
+                # isinstance(delta_text, str) determines whether delta_text is of type string
+                # The addition of the equal sign len(delta_text.rstrip()) >= 1 filters out the '}' case.
                 if isinstance(delta_text, str) and len(delta_text.rstrip()) > 1 and delta_text.rstrip()[-1] == '}':
                     if delta_text.replace('\n', '').replace('\r', '') == '}':
                         logger.debug("Exceptional condition: %s", delta_text)
