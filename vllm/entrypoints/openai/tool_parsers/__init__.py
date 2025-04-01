70b130f95da85647a9e41acc14482a20bd30295c
Kinfey <93169410+kinfey@users.noreply.github.com>
2025-04-01 13:50:05 +0800

    [Frontend] Add Phi-4-mini function calling support (#14886)
    Signed-off-by: Kinfey <kinfeylo@microsoft.com>
    Co-authored-by: Cyrus Leung <tlleungac@connect.ust.hk>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/entrypoints/openai/tool_parsers/__init__.py
+++ vllm/entrypoints/openai/tool_parsers/__init__.py
@@ -8,11 +8,12 @@ from .internlm2_tool_parser import Internlm2ToolParser
 from .jamba_tool_parser import JambaToolParser
 from .llama_tool_parser import Llama3JsonToolParser
 from .mistral_tool_parser import MistralToolParser
+from .phi4mini_tool_parser import Phi4MiniJsonToolParser
 from .pythonic_tool_parser import PythonicToolParser
 
 __all__ = [
     "ToolParser", "ToolParserManager", "Granite20bFCToolParser",
     "GraniteToolParser", "Hermes2ProToolParser", "MistralToolParser",
     "Internlm2ToolParser", "Llama3JsonToolParser", "JambaToolParser",
-    "PythonicToolParser"
+    "PythonicToolParser", "Phi4MiniJsonToolParser"
 ]
