86263dc5c5bd966a0251dcf436e04e5b6c6b0a9f
Andrew Feldman <afeldman@neuralmagic.com>
2025-04-04 16:11:00 +0000

    test load weights failure
    Signed-off-by: Andrew Feldman <afeldman@neuralmagic.com>

--- tests/v1/shutdown/test_startup_error.py
+++ tests/v1/shutdown/test_startup_error.py
@@ -12,8 +12,8 @@ from vllm.utils import cuda_device_count_stateless
 from vllm.v1.engine.async_llm import AsyncLLM
 
 
-def evil_forward(self, *args, **kwargs):
-    """Evil forward method that raise an exception."""
+def evil_method(self, *args, **kwargs):
+    """Evil method that raises an exception."""
 
     if get_tensor_model_parallel_rank() == 0:
         raise Exception("Simulated Error in startup!")
@@ -28,17 +28,19 @@ MODELS = [
 
 @pytest.mark.parametrize("model", MODELS)
 @pytest.mark.parametrize("tensor_parallel_size", [2, 1])
+@pytest.mark.parametrize("failing_method", ["forward", "load_weights"])
 def test_async_llm_startup_error(monkeypatch, model: str,
-                                 tensor_parallel_size: int) -> None:
+                                 tensor_parallel_size: int,
+                                 failing_method: str) -> None:
     """Test that AsyncLLM propagates an __init__ error & frees memory.
-    
+    Test profiling (forward()) and load weights failures.
     AsyncLLM always uses an MP client.
     """
     if cuda_device_count_stateless() < tensor_parallel_size:
         pytest.skip(reason="Not enough CUDA devices")
 
     # Monkeypatch an error in the model.
-    monkeypatch.setattr(LlamaForCausalLM, "forward", evil_forward)
+    monkeypatch.setattr(LlamaForCausalLM, failing_method, evil_method)
 
     engine_args = AsyncEngineArgs(model=model,
                                   enforce_eager=True,
@@ -59,9 +61,12 @@ def test_async_llm_startup_error(monkeypatch, model: str,
 @pytest.mark.parametrize("model", MODELS)
 @pytest.mark.parametrize("tensor_parallel_size", [2, 1])
 @pytest.mark.parametrize("enable_multiprocessing", [True])
+@pytest.mark.parametrize("failing_method", ["forward", "load_weights"])
 def test_llm_startup_error(monkeypatch, model: str, tensor_parallel_size: int,
-                           enable_multiprocessing: bool) -> None:
+                           enable_multiprocessing: bool,
+                           failing_method: str) -> None:
     """Test that LLM propagates an __init__ error and frees memory.
+    Test profiling (forward()) and load weights failures.
     TODO(andy) - LLM without multiprocessing.
     """
     if model != "meta-llama/Llama-3.2-1B":
@@ -75,7 +80,7 @@ def test_llm_startup_error(monkeypatch, model: str, tensor_parallel_size: int,
         m.setenv("VLLM_ENABLE_V1_MULTIPROCESSING", MP_VALUE)
 
         # Monkeypatch an error in the model.
-        monkeypatch.setattr(LlamaForCausalLM, "forward", evil_forward)
+        monkeypatch.setattr(LlamaForCausalLM, failing_method, evil_method)
 
         with pytest.raises(
                 Exception,
