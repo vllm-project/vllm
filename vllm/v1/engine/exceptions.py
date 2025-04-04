f824c154df143f7b88c2aed4e314cd7cb0345cd9
afeldman-nm <156691304+afeldman-nm@users.noreply.github.com>
2025-04-04 12:14:22 -0400

    Update vllm/v1/engine/exceptions.py
    Co-authored-by: Cyrus Leung <cyrus.tl.leung@gmail.com>

--- vllm/v1/engine/exceptions.py
+++ vllm/v1/engine/exceptions.py
@@ -8,7 +8,7 @@ class EngineDeadError(Exception):
     """Raised when the EngineCore dies. Unrecoverable."""
 
     def __init__(self, *args, suppress_context: bool = False, **kwargs):
-        ENGINE_DEAD_MESSAGE = "EngineCore encountered an issue. See stack trace for the root cause."  # noqa: E501
+        ENGINE_DEAD_MESSAGE = "EngineCore encountered an issue. See stack trace (above) for the root cause."  # noqa: E501
 
         super().__init__(ENGINE_DEAD_MESSAGE, *args, **kwargs)
         # Make stack trace clearer when using with LLMEngine by
