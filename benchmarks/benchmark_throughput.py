e0dc790fd8473d98c4676bca14ff6590630a543c
StevenShi-23 <shi.ziji.sm@gmail.com>
2025-04-04 14:24:12 +0800

    [Benchmark] Explicit set hf dataset subset for AIMO
    Signed-off-by: StevenShi-23 <shi.ziji.sm@gmail.com>

--- benchmarks/benchmark_throughput.py
+++ benchmarks/benchmark_throughput.py
@@ -334,7 +334,7 @@ def get_requests(args, tokenizer):
             sample_kwargs["enable_multimodal_chat"] = True
         elif args.dataset_path in AIMODataset.SUPPORTED_DATASET_PATHS:
             dataset_cls = AIMODataset
-            common_kwargs['dataset_subset'] = args.hf_subset
+            common_kwargs['dataset_subset'] = None
             common_kwargs['dataset_split'] = "train"
     else:
         raise ValueError(f"Unknown dataset name: {args.dataset_name}")
