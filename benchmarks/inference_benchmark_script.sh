0704a6b9a8725592386c6f346ac81490edac7530
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2025-04-04 21:31:28 -0700

    Update inference_benchmark_script.sh

--- benchmarks/inference_benchmark_script.sh
+++ benchmarks/inference_benchmark_script.sh
@@ -16,9 +16,9 @@ else
 fi
 ls
 
-gsutil cp gs://ml-auto-solutions/output/pytorch_xla/vllm_benchmark_nightly/vllm-nightly-v6e-4-2025-03-26-21-42-04/test-mar26.json .
-mv test-mar26.json benchmark-vllm-1.0qps-20250404-195413-meta-llama-Meta-Llama-3-8B.json
-ls
+# gsutil cp gs://ml-auto-solutions/output/pytorch_xla/vllm_benchmark_nightly/vllm-nightly-v6e-4-2025-03-26-21-42-04/test-mar26.json .
+# mv test-mar26.json benchmark-vllm-1.0qps-20250404-195413-meta-llama-Meta-Llama-3-8B.json
+# ls
 cat *meta-llama-Meta-Llama-3-8B.json
 cat *meta-llama-Meta-Llama-3-8B.json >> metric_result.jsonl
 echo '' >> metric_report.jsonl && rm *meta-llama-Meta-Llama-3-8B.json
