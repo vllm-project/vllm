5f13e6b703675f9863361ca44dd8838f68a2aba5
Lucia Fang <116399278+luccafong@users.noreply.github.com>
2025-04-05 21:23:40 -0700

    [Model] fix model testing for TeleChat2ForCausalLM and V0 llama4 (#16112)
    Signed-off-by: Lu Fang <fanglu@fb.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/attention/backends/flash_attn.py
+++ vllm/attention/backends/flash_attn.py
@@ -617,10 +617,15 @@ class FlashAttentionImpl(AttentionImpl):
         blocksparse_params: Optional[Dict[str, Any]] = None,
         logits_soft_cap: Optional[float] = None,
         attn_type: str = AttentionType.DECODER,
+        use_irope: bool = False,
     ) -> None:
         if blocksparse_params is not None:
             raise ValueError(
                 "FlashAttention does not support block-sparse attention.")
+        if use_irope:
+            logger.warning(
+                "Using irope in V0 is not supported yet, it will fall back "
+                "to global attention for long context.")
         self.num_heads = num_heads
         self.head_size = head_size
         self.scale = float(scale)
