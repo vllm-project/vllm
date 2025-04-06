2aec24503985f58bfbeba1f0b8809a4a851220d7
DefTruth <qiustudent_r@163.com>
2025-04-06 20:46:34 +0800

    [Kernel] Remove redundant Exp calculations
    Signed-off-by: DefTruth <qiustudent_r@163.com>

--- vllm/attention/ops/triton_merge_attn_states.py
+++ vllm/attention/ops/triton_merge_attn_states.py
@@ -66,10 +66,10 @@ def merge_attn_states_kernel(
     max_lse = tl.maximum(p_lse, s_lse)
     p_lse = p_lse - max_lse
     s_lse = s_lse - max_lse
-    # Reuse precompute exp values
-    p_lse_exp = tl.exp(p_lse)
-    s_lse_exp = tl.exp(s_lse)
-    out_se = (p_lse_exp + s_lse_exp)
+    # Will reuse precomputed Exp values for scale factor computation.
+    p_se = tl.exp(p_lse)
+    s_se = tl.exp(s_lse)
+    out_se = (p_se + s_se)
 
     if OUTPUT_LSE:
         out_lse = tl.log(out_se) + max_lse
@@ -87,8 +87,8 @@ def merge_attn_states_kernel(
     # NOTE(woosuk): Be careful with the numerical stability.
     # We should compute the scale first, and then multiply it with the output.
     # Do not multiply the output with tl.exp(p_lse) or tl.exp(s_lse) directly.
-    p_scale = p_lse_exp / out_se
-    s_scale = s_lse_exp / out_se
+    p_scale = p_se / out_se
+    s_scale = s_se / out_se
     out = p_out * p_scale + s_out * s_scale
     tl.store(output + token_idx * num_heads * HEAD_SIZE +
              head_idx * HEAD_SIZE + head_arange,
