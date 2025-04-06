6c700857559b0cb6ef8ebd95f1253f4d1751a1ee
Roger Wang <ywang@roblox.com>
2025-04-06 00:17:09 -0700

    clean up test registry
    Signed-off-by: Roger Wang <ywang@roblox.com>

--- tests/models/test_registry.py
+++ tests/models/test_registry.py
@@ -23,7 +23,6 @@ from .registry import HF_EXAMPLE_MODELS
 
 @pytest.mark.parametrize("model_arch", ModelRegistry.get_supported_archs())
 def test_registry_imports(model_arch):
-
     model_info = HF_EXAMPLE_MODELS.get_hf_info(model_arch)
     model_info.check_transformers_version(on_fail="skip")
 
@@ -92,8 +91,8 @@ def test_registry_is_pp(model_arch, is_pp, init_cuda):
 
 
 def test_hf_registry_coverage():
-    untested_archs = set(ModelRegistry.get_supported_archs() -
-                         HF_EXAMPLE_MODELS.get_supported_archs())
+    untested_archs = (ModelRegistry.get_supported_archs() -
+                      HF_EXAMPLE_MODELS.get_supported_archs())
 
     assert not untested_archs, (
         "Please add the following architectures to "
