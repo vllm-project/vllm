800b4073fa5211ed904877deadf15c0654b1ed39
Jinzhen Lin <linjinzhen@hotmail.com>
2025-04-06 14:05:31 +0800

    fix typo
    Signed-off-by: Jinzhen Lin <linjinzhen@hotmail.com>

--- vllm/model_executor/layers/quantization/utils/marlin_utils.py
+++ vllm/model_executor/layers/quantization/utils/marlin_utils.py
@@ -158,7 +158,7 @@ def check_moe_marlin_supports_layer(layer: LinearBase, group_size: int) \
 
     # gate-up: (n, k) = (intermediate_size_per_partition * 2, hidden_size)
     # down: (n, k) = (hidden_size, intermediate_size_per_partition)
-    # moe marlin requres n % 128 == 0 and k % 64 == 0
+    # moe marlin requires n % 128 == 0 and k % 64 == 0
     return hidden_size % 128 == 0 and \
         intermediate_size_per_partition % max(64, group_size) == 0 and \
         group_size in [-1, 32, 64, 128]
