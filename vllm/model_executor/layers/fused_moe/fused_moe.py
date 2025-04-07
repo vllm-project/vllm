2c2966c313cd16de7e2c207eb907eabc8c19f4c2
tjtanaa <tunjian.tan@embeddedllm.com>
2025-04-07 16:28:22 +0000

    Fix ROCm V1 Engine Fused MoE Bug
    Signed-off-by: tjtanaa <tunjian.tan@embeddedllm.com>

--- vllm/model_executor/layers/fused_moe/fused_moe.py
+++ vllm/model_executor/layers/fused_moe/fused_moe.py
@@ -461,6 +461,20 @@ def invoke_fused_moe_kernel(A: torch.Tensor,
                             use_int4_w4a16: bool,
                             block_shape: Optional[List[int]] = None) -> None:
     assert topk_weights is not None or not mul_routed_weight
+    if current_platform.is_rocm() and topk_weights is not None:
+        # This is to handle the bug https://github.com/ROCm/pytorch/issues/2020
+        # where the In the HIPGraph, it could occur that the `topk_weights`
+        # tensor has the following properties:
+        # .shape: ([1024, 1])
+        # .is_contiguous(): True
+        # .stride() : [1,1024]
+        # .is_contiguous(memory_format=torch.channels_last) is False
+        # .is_contiguous(memory_format=torch.contiguous_format) is True
+        # This only happens when using V1 Engine on ROCm with HIPGraph
+        # with torch.compile Dynamo.
+        # V1 Engine on ROCm with eager mode is fine.
+        # V0 Engine on ROCm with HIPGraph is fine.
+        topk_weights = topk_weights.view(-1).reshape(topk_weights.shape)
     assert topk_weights is None or topk_weights.stride(1) == 1
     assert sorted_token_ids.stride(0) == 1
 
