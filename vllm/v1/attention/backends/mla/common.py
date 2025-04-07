127a11e6c33ddbc41baa2fb8885caf4fb3caedb8
DefTruth <qiustudent_r@163.com>
2025-04-07 17:39:14 +0800

    [V1] support cuda merge_attn_states for V1
    Signed-off-by: DefTruth <qiustudent_r@163.com>

--- vllm/v1/attention/backends/mla/common.py
+++ vllm/v1/attention/backends/mla/common.py
@@ -195,7 +195,7 @@ from vllm import _custom_ops as ops
 from vllm.attention.backends.abstract import (AttentionBackend, AttentionLayer,
                                               AttentionMetadata,
                                               MLAAttentionImpl)
-from vllm.attention.ops.triton_merge_attn_states import merge_attn_states
+from vllm.attention.ops.merge_attn_states import merge_attn_states
 from vllm.logger import init_logger
 from vllm.model_executor.layers.linear import (ColumnParallelLinear,
                                                LinearBase, RowParallelLinear,
