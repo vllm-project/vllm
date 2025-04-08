06efa465adba883633c6e09fdc7c554f8adc72c2
Staszek Pasko <staszek@gmail.com>
2025-04-08 20:06:54 +0200

    Implement custom serializaton for MultiModalKwargs
    WIP, just handles the basic case of simple str->Tensor dict.

--- vllm/v1/serial_utils.py
+++ vllm/v1/serial_utils.py
@@ -8,10 +8,12 @@ import cloudpickle
 import torch
 from msgspec import msgpack
 
+from vllm.multimodal.inputs import MultiModalKwargs
+
 CUSTOM_TYPE_TENSOR = 1
 CUSTOM_TYPE_PICKLE = 2
 CUSTOM_TYPE_CLOUDPICKLE = 3
-
+CUSTOM_TYPE_TENSORDICT = 4 
 
 class MsgpackEncoder:
     """Encoder with custom torch tensor serialization."""
@@ -44,6 +46,22 @@ def custom_enc_hook(obj: Any) -> Any:
         # https://gist.github.com/tlrmchlsmth/8067f1b24a82b6e2f90450e7764fa103 # noqa: E501
         return msgpack.Ext(CUSTOM_TYPE_TENSOR, pickle.dumps(obj.numpy()))
 
+    # clunky workaround for https://github.com/vllm-project/vllm/issues/16185
+    # if the object is multimodal kwargs, and can be represented as a simple dict
+    # then convert to numpy format before transmitting.
+    # TODO: support other NestedTensors forms
+    if isinstance(obj, MultiModalKwargs):
+        numpydict = {}
+        total_size = 0
+        for key in obj:
+            if isinstance(obj[key], torch.Tensor):
+                numpydict[key] = obj[key].numpy()
+                total_size += obj[key].shape.numel() * obj[key].dtype.itemsize
+        # check if all keys were converted
+        if len(obj) == len(numpydict):
+            msg = msgpack.Ext(CUSTOM_TYPE_TENSORDICT, pickle.dumps(numpydict))
+            return msg
+
     if isinstance(obj, FunctionType):
         return msgpack.Ext(CUSTOM_TYPE_CLOUDPICKLE, cloudpickle.dumps(obj))
 
@@ -57,5 +75,10 @@ def custom_ext_hook(code: int, data: memoryview) -> Any:
         return pickle.loads(data)
     if code == CUSTOM_TYPE_CLOUDPICKLE:
         return cloudpickle.loads(data)
+    if code == CUSTOM_TYPE_TENSORDICT:
+        numpydict = pickle.loads(data)
+        for key in numpydict:
+            numpydict[key] = torch.from_numpy(numpydict[key])
+        return MultiModalKwargs(numpydict)
 
     raise NotImplementedError(f"Extension type code {code} is not supported")
