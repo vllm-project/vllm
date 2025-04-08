c338c99bab846fec12d71c16477f16416d22be7a
Chanh Nguyen <cnguyen@linkedin.com>
2025-04-08 06:26:59 +0000

    Add unit test
    Signed-off-by: Chanh Nguyen <cnguyen@linkedin.com>

---
+++ tests/compile/piecewise/test_full_cudagraph.py
@@ -0,0 +1,33 @@
+# SPDX-License-Identifier: Apache-2.0
+from vllm import LLM, SamplingParams
+from vllm.config import CompilationConfig, CompilationLevel
+
+
+def run_model(compilation_config: CompilationConfig):
+    prompts = ["Hello, my name is"]
+    sampling_params = SamplingParams(temperature=0.0, max_tokens=20)
+
+    llm = LLM(model="Qwen/Qwen2-1.5B-Instruct",
+              compilation_config=compilation_config)
+
+    return llm.generate(prompts, sampling_params)
+
+
+def test_full_cudagraph(monkeypatch):
+    with monkeypatch.context() as m:
+        m.setenv("VLLM_USE_V1", "1")
+        m.setenv("VLLM_FLASH_ATTN_VERSION", "3")
+
+        full_cudagraph_responses = run_model(
+            compilation_config=CompilationConfig(
+                level=CompilationLevel.FULL_GRAPH,
+                use_cudagraph=True,
+            ))
+
+        piecewise_responses = run_model(compilation_config=CompilationConfig(
+            level=CompilationLevel.PIECEWISE,
+            use_cudagraph=False,
+        ))
+
+        assert full_cudagraph_responses[0].outputs[
+            0].text == piecewise_responses[0].outputs[0].text
