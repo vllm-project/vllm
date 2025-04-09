1b752a943baffb2bbbd97e388091362b753a7488
Jinzhen Lin <linjinzhen@hotmail.com>
2025-04-09 15:17:41 +0800

    generate kernels when building
    Signed-off-by: Jinzhen Lin <linjinzhen@hotmail.com>

--- csrc/moe/marlin_moe_wna16/generate_kernels.py
+++ csrc/moe/marlin_moe_wna16/generate_kernels.py
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
+import glob
 import itertools
 import os
+import subprocess
 
 import jinja2
 
@@ -42,50 +44,60 @@ THREAD_M_BLOCKS = [0.5, 1, 2, 3, 4]
 GROUP_BLOCKS = [0, -1, 2, 4, 8]
 DTYPES = ["fp16", "bf16"]
 
-for scalar_type, dtype in itertools.product(SCALAR_TYPES, DTYPES):
-    settings = []
-    bit = int(scalar_type[8])
-    has_zp = "B" not in scalar_type
-    all_template_str_list = []
 
-    for group_blocks, m_blocks, thread_configs in itertools.product(
-            GROUP_BLOCKS, THREAD_M_BLOCKS, THREAD_CONFIGS):
+def remove_old_kernels():
+    for filename in glob.glob(os.path.dirname(__file__) + "/kernel_*.cu"):
+        subprocess.call(["rm", "-f", filename])
 
-        has_act_order = group_blocks == 0
-        if has_zp and has_act_order:
-            continue
-        if thread_configs[2] == 256:
-            if m_blocks <= 1 and thread_configs[0] != 128:
+
+def generate_new_kernels():
+    for scalar_type, dtype in itertools.product(SCALAR_TYPES, DTYPES):
+        has_zp = "B" not in scalar_type
+        all_template_str_list = []
+
+        for group_blocks, m_blocks, thread_configs in itertools.product(
+                GROUP_BLOCKS, THREAD_M_BLOCKS, THREAD_CONFIGS):
+
+            has_act_order = group_blocks == 0
+            if has_zp and has_act_order:
                 continue
-            if m_blocks > 1 and thread_configs[0] != 64:
-                continue
+            if thread_configs[2] == 256:
+                if m_blocks <= 1 and thread_configs[0] != 128:
+                    continue
+                if m_blocks > 1 and thread_configs[0] != 64:
+                    continue
 
-        k_blocks = thread_configs[0] // 16
-        n_blocks = thread_configs[1] // 16
-        threads = thread_configs[2]
+            k_blocks = thread_configs[0] // 16
+            n_blocks = thread_configs[1] // 16
+            threads = thread_configs[2]
 
-        c_dtype = "half" if dtype == "fp16" else "nv_bfloat16"
+            c_dtype = "half" if dtype == "fp16" else "nv_bfloat16"
 
-        template_str = jinja2.Template(TEMPLATE).render(
-            scalar_t=c_dtype,
-            w_type_id=scalar_type + ".id()",
-            threads=threads,
-            thread_m_blocks=max(m_blocks, 1),
-            thread_n_blocks=n_blocks,
-            thread_k_blocks=k_blocks,
-            m_block_size_8=m_blocks == 0.5,
-            stages="pipe_stages",
-            has_act_order=has_act_order,
-            has_zp=has_zp,
-            group_blocks=group_blocks,
-            is_zp_float=False,
-        )
+            template_str = jinja2.Template(TEMPLATE).render(
+                scalar_t=c_dtype,
+                w_type_id=scalar_type + ".id()",
+                threads=threads,
+                thread_m_blocks=max(m_blocks, 1),
+                thread_n_blocks=n_blocks,
+                thread_k_blocks=k_blocks,
+                m_block_size_8=m_blocks == 0.5,
+                stages="pipe_stages",
+                has_act_order=has_act_order,
+                has_zp=has_zp,
+                group_blocks=group_blocks,
+                is_zp_float=False,
+            )
 
-        all_template_str_list.append(template_str)
+            all_template_str_list.append(template_str)
 
-    file_content = FILE_HEAD + "\n\n"
-    file_content += "\n\n".join(all_template_str_list) + "\n\n}\n"
-    filename = f"kernel_{dtype}_{scalar_type[6:].lower()}.cu"
+        file_content = FILE_HEAD + "\n\n"
+        file_content += "\n\n".join(all_template_str_list) + "\n\n}\n"
+        filename = f"kernel_{dtype}_{scalar_type[6:].lower()}.cu"
 
-    with open(os.path.join(os.path.dirname(__file__), filename), "w") as f:
-        f.write(file_content)
+        with open(os.path.join(os.path.dirname(__file__), filename), "w") as f:
+            f.write(file_content)
+
+
+if __name__ == "__main__":
+    remove_old_kernels()
+    generate_new_kernels()
