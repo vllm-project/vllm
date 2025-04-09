e72e5e446061c6f15e2fd409bf3447f64a0d3656
rshaw@neuralmagic.com <robertgshaw2@gmail.com>
2025-04-09 15:08:34 +0000

    updated
    Signed-off-by: rshaw@neuralmagic.com <robertgshaw2@gmail.com>

--- vllm/distributed/kv_transfer/kv_connector/v1/__init__.py
+++ vllm/distributed/kv_transfer/kv_connector/v1/__init__.py
@@ -1,9 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
-# yapf: disable
 from vllm.distributed.kv_transfer.kv_connector.v1.base import (
     KVConnectorBase_V1, KVConnectorRole)
-
-# yapf: enable
 
 __all__ = [
     "KVConnectorRole",
