cd882eba465c68ea709c418a85303ee41a05151d
qingjun <qingjun@minimaxi.com>
2025-04-09 20:31:11 +0800

    fix code
    Signed-off-by: qingjun <qingjun@minimaxi.com>

--- vllm/model_executor/models/minimax_text_01.py
+++ vllm/model_executor/models/minimax_text_01.py
@@ -80,8 +80,7 @@ class MiniMaxText01RMSNormTP(CustomOp):
         super().__init__()
         self.tp_world = get_tensor_model_parallel_world_size()
         self.tp_rank = get_tensor_model_parallel_rank()
-        self.weight = nn.Parameter(torch.ones(int(hidden_size /
-                                                  self.tp_world)))
+        self.weight = nn.Parameter(torch.ones(hidden_size))
 
         self.weight.weight_loader = self.weight_loader
         self.variance_epsilon = eps
@@ -111,7 +110,8 @@ class MiniMaxText01RMSNormTP(CustomOp):
             variance = tensor_model_parallel_all_reduce(
                 variance) / self.tp_world
         x = x * torch.rsqrt(variance + self.variance_epsilon)
-        x = x.to(orig_dtype) * self.weight
+        weight = self.weight.view(-1)
+        x = x.to(orig_dtype) * weight
         return x
 
     def forward(
