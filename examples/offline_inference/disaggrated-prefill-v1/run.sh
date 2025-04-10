1d8415d095461651f2efc2debf7c1c762e098c45
rshaw@neuralmagic.com <robertgshaw2@gmail.com>
2025-04-10 21:59:54 +0000

    rename
    Signed-off-by: rshaw@neuralmagic.com <robertgshaw2@gmail.com>

--- examples/offline_inference/disaggrated-prefill-v1/run.sh
+++
@@ -1,5 +0,0 @@
-rm -rf local_storage/
-rm output.txt
-
-VLLM_ENABLE_V1_MULTIPROCESSING=0 CUDA_VISIBLE_DEVICES=5 python3 prefill_example.py
-VLLM_ENABLE_V1_MULTIPROCESSING=0 CUDA_VISIBLE_DEVICES=5 python3 decode_example.py
