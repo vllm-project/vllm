f42f7e3c1bbeb310f603e9d1a35449fff41f6171
LiuXiaoxuanPKU <lilyliupku@gmail.com>
2025-04-09 17:43:45 -0700

    slots for eagle
    Signed-off-by: LiuXiaoxuanPKU <lilyliupku@gmail.com>

--- vllm/v1/engine/core.py
+++ vllm/v1/engine/core.py
@@ -98,6 +98,7 @@ class EngineCore:
             cache_config=vllm_config.cache_config,
             lora_config=vllm_config.lora_config,
             kv_cache_config=kv_cache_config,
+            speculative_config=vllm_config.speculative_config,
             structured_output_manager=self.structured_output_manager,
             include_finished_set=vllm_config.parallel_config.data_parallel_size
             > 1,
