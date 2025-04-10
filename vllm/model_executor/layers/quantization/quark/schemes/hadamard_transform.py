b5a43092a02c8170f979e4c6ff51a7d54300a2ae
Riley Corzine <Riley.Corzine@xilinx.com>
2025-04-10 01:07:22 -0700

    Update hadamard_transform.py

--- vllm/model_executor/layers/quantization/quark/schemes/hadamard_transform.py
+++ vllm/model_executor/layers/quantization/quark/schemes/hadamard_transform.py
@@ -7,8 +7,8 @@ from vllm.distributed import (divide, get_tensor_model_parallel_rank,
                                 tensor_model_parallel_all_reduce)
 from torch import nn
 import torch
-FHT_1024 = torch.ops._rocm_C.fast_hadamard_transform_1024
-FHT_512 = torch.ops._rocm_C.fast_hadamard_transform_512
+
+register_FHT = {1024: torch.ops._rocm_C.fast_hadamard_transform, 512: torch.ops._rocm_C.fast_hadamard_transform}
 
 hadamard_sizes={28:torch.FloatTensor([
     [
@@ -132,6 +132,10 @@ class HadamardTransform(nn.Module):
                 self._device=getattr(weight,"device")
             else:
                 raise ValueError("cannot initialize hadamard transform: corresponding layer has no device")
+            if hasattr(weight,"dtype"):
+                self._dtype=getattr(weight,"dtype")
+            else:
+                raise ValueError("cannot initialize hadamamrd trnasform: corresponding layer has no dtype")
         else:
             raise ValueError("cannot initialize hadamard transform: corresponding layer has not yet been initialized")
         self.partition_input_size=layer.input_size_per_partition
@@ -161,8 +165,7 @@ def matrix_multiply_via_linear(A,X,m,n):
 
     #import torch.nn.functional as F
     #X=F.linear(X,self.hadamard_k.weight)
-    #print("the shapes: ",self.hadamard_k.weight.shape,X.shape,og_shape)
-    
+
     X=A(X)
 
     X=X.view(-1,n,m*get_tensor_model_parallel_world_size())
@@ -178,31 +181,47 @@ def matrix_multiply_via_linear(A,X,m,n):
     return X
 
 class QuaRotR4(HadamardTransform):
-    def __init__(self, layer: RowParallelLinear):
+    def __init__(self, layer: RowParallelLinear, size_FHT = None, size_k = None):
         super().__init__(layer)
 
         """
         select the appropriate hadamard matrix, based on the target input size
         """
-        for k in hadamard_sizes.keys():
-            if self.actual_input_size%k==0:
-                hadamard_k=hadamard_sizes[k].to(self._device)
-                #initialize the RowParallelLinear and load the weights
-                self.hadamard_k=RowParallelLinear(input_size=k,
-                                                    output_size=k,
-                                                    bias=False,
-                                                    return_bias=False)
-                self.hadamard_k.weight_loader(self.hadamard_k.weight,hadamard_k)
-                self.k=k
-                self.scale=1.0/torch.tensor(self.actual_input_size).sqrt()
-                self.chunk_size=self.actual_input_size//k
-                if self.chunk_size==512:
-                    self.FHT=FHT_512
-                elif self.chunk_size==1024:
-                    self.FHT=FHT_1024
-                break
+        if size_FHT is None and not(size_k is None):
+            if self.actual_input_size//size_k in hadamard_sizes:
+                size_FHT=self.actual_input_size//size_k
+            else:
+                raise ValueError(f"No matching size_FHT for size_k={size_k}")
+        elif size_k is None and not(size_FHT is None):
+            if self.actual_input_size//size_FHT in hadamard_sizes:
+                size_k=self.actual_input_size//size_FHT
+            else:
+                raise ValueError(f"No matching size_k for size_FHT={size_FHT}")
+        elif size_k is None and size_FHT is None:
+            import itertools
+            for s_FHT,s_k in itertools.product(register_FHT.keys(),hadamard_sizes.keys()):
+                if s_FHT*s_k==self.actual_input_size:
+                    size_FHT,size_k=s_FHT,s_k
+                    break
+            else:
+                raise ValueError(f"No matching size_k and size_FHT found for input size {self.actual_input_size}")
         else:
-            raise ValueError(f"there is no matching k for {self.actual_input_size}")
+            if size_k*size_FHT != self.actual_input_size:
+                raise ValueError(f"size_k={size_k} and size_FHT={size_FHT} do not match {self.actual_input_size}")
+
+
+        hadamard_k=hadamard_sizes[size_k].to(self._device).to(self._dtype)
+        #initialize the RowParallelLinear and load the weights
+        self.hadamard_k=RowParallelLinear(input_size=size_k,
+                                            output_size=size_k,
+                                            bias=False,
+                                            return_bias=False)
+        self.hadamard_k.to(self._device).to(self._dtype)
+        self.hadamard_k.weight_loader(self.hadamard_k.weight,hadamard_k)
+        self.k=size_k
+        self.scale=1.0/torch.tensor(self.actual_input_size).sqrt()
+        self.chunk_size=size_FHT
+        self.FHT=register_FHT[size_FHT]
         
     def forward(self, X):
         og_shape=X.shape
@@ -224,7 +243,11 @@ class QuaRotR4(HadamardTransform):
             X[:,1,:]=Y[:,0,:]-Y[:,1,:]
         """
 
-        X=matrix_multiply_via_linear(A=self.hadamard_k,X=X,m=self.hadamard_k.input_size_per_partition,n=self.chunk_size)
+        if get_tensor_model_parallel_world_size()==1:
+            X=X.view(-1,self.hadamard_k.input_size_per_partition,self.chunk_size)
+            X=self.hadamard_k.weight@X
+        else:
+            X=matrix_multiply_via_linear(A=self.hadamard_k,X=X,m=self.hadamard_k.input_size_per_partition,n=self.chunk_size)
 
         X=X.view(og_shape)
 
