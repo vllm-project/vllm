1a4b6a097e2bca986a5a48ade70f80a2723b55cd
Andrew Feldman <afeldman@neuralmagic.com>
2025-04-10 15:18:01 +0000

    added llm 2-rank forward error test back
    Signed-off-by: Andrew Feldman <afeldman@neuralmagic.com>

--- tests/v1/shutdown/test_forward_error.py
+++ tests/v1/shutdown/test_forward_error.py
@@ -32,7 +32,7 @@ def evil_forward(self, *args, **kwargs):
 
 @pytest.mark.timeout(SHUTDOWN_TEST_TIMEOUT_SEC)
 @pytest.mark.parametrize("enable_multiprocessing", [True])
-@pytest.mark.parametrize("tensor_parallel_size", [1])
+@pytest.mark.parametrize("tensor_parallel_size", [2, 1])
 @pytest.mark.parametrize("model", MODELS)
 def test_llm_model_error(monkeypatch, tensor_parallel_size: int,
                          enable_multiprocessing: bool, model: str) -> None:
