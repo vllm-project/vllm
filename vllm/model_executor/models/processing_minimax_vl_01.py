13568a9be4bb10b433d36608b2567dd3e16415e8
qingjun <qingjun@minimaxi.com>
2025-04-11 11:58:32 +0800

    fix ce

--- vllm/model_executor/models/processing_minimax_vl_01.py
+++ vllm/model_executor/models/processing_minimax_vl_01.py
@@ -10,7 +10,7 @@ from transformers.processing_utils import ProcessingKwargs, ProcessorMixin#, _va
 from transformers.tokenization_utils_base import PreTokenizedInput, TextInput
 from transformers.utils import logging
 
-from .image_processor import CustomBatchFeature
+from .image_processer import CustomBatchFeature
 logger = logging.get_logger(__name__)
 
 import os
