4afa50e82c6f09f1c4476e2d5bb401f53033813c
rshaw@neuralmagic.com <robertgshaw2@gmail.com>
2025-04-13 20:28:43 +0000

    stash
    Signed-off-by: rshaw@neuralmagic.com <robertgshaw2@gmail.com>

--- vllm/v1/engine/core.py
+++ vllm/v1/engine/core.py
@@ -95,7 +95,6 @@ class EngineCore:
         self.scheduler: SchedulerInterface = Scheduler(
             vllm_config=vllm_config,
             kv_cache_config=kv_cache_config,
-            speculative_config=vllm_config.speculative_config,
             structured_output_manager=self.structured_output_manager,
             include_finished_set=vllm_config.parallel_config.data_parallel_size
             > 1,
