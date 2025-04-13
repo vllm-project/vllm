0e202ff04667accb1387fb78fa90b4c9a27b36cc
Jennifer Zhao <ai.jenniferzhao@gmail.com>
2025-04-13 22:24:03 +0000

    address comments
    Signed-off-by: Jennifer Zhao <ai.jenniferzhao@gmail.com>

--- benchmarks/benchmark_throughput.py
+++ benchmarks/benchmark_throughput.py
@@ -598,19 +598,23 @@ if __name__ == "__main__":
         "--prefix-len",
         type=int,
         default=None,
-        help="Number of prefix tokens to be used in RandomDataset "
+        help=f"Number of prefix tokens to be used in RandomDataset "
         "and SonnetDataset. For RandomDataset, the total input "
-        "length is the sum of `prefix-len` and a random context length "
+        "length is the sum of prefix-len (default: "
+        f"{RandomDataset.DEFAULT_PREFIX_LEN}) and a random context length "
         "sampled from [input_len * (1 - range_ratio), "
         "input_len * (1 + range_ratio)]. For SonnetDataset, "
-        "prefix_len controls how much of the input is fixed versus random, "
-        "but the total input length remains approximately input_len tokens.")
+        f"prefix_len (default: {SonnetDataset.DEFAULT_PREFIX_LEN}) "
+        "controls how much of the input is fixed lines versus "
+        "random lines, but the total input length remains approximately "
+        "input_len tokens.")
     # random dataset
     parser.add_argument(
         "--random-range-ratio",
         type=float,
         default=None,
-        help="Range ratio for sampling input/output length, "
+        help=f"Range ratio (default : {RandomDataset.DEFAULT_RANGE_RATIO}) "
+        "for sampling input/output length, "
         "used only for RandomDataset. Must be in the range [0, 1) to "
         "define a symmetric sampling range "
         "[length * (1 - range_ratio), length * (1 + range_ratio)].",
