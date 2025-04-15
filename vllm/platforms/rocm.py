8917199382f333c6e599315bed7e86336c54632b
Mengqing Cao <cmq0113@163.com>
2025-04-15 09:27:15 +0800

    [SpecDecode] Make spec decoding extensible to different backends
    Signed-off-by: Mengqing Cao <cmq0113@163.com>

--- vllm/platforms/rocm.py
+++ vllm/platforms/rocm.py
@@ -309,3 +309,7 @@ class RocmPlatform(Platform):
         gcn_arch = torch.cuda.get_device_properties(0).gcnArchName
         supported_archs = ['gfx94']
         return any(gfx in gcn_arch for gfx in supported_archs)
+
+    @classmethod
+    def supports_spec_decoding(cls) -> bool:
+        return True
