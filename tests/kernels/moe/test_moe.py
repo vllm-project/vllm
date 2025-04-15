b1c40b724d5b304b4e0d0ef0622cb0636723bca9
Bill Nell <bnell@redhat.com>
2025-04-15 15:02:05 +0000

    test pplx w/naive implementation
    Signed-off-by: Bill Nell <bnell@redhat.com>

--- tests/kernels/moe/test_moe.py
+++ tests/kernels/moe/test_moe.py
@@ -14,8 +14,9 @@ import vllm.model_executor.layers.fused_moe  # noqa
 from tests.kernels.utils import (opcheck, stack_and_dev, torch_moe,
                                  torch_moe_single)
 from vllm.config import VllmConfig, set_current_vllm_config
-from vllm.model_executor.layers.fused_moe import fused_moe, fused_experts
-from vllm.model_executor.layers.fused_moe.fused_moe import fused_topk
+from vllm.model_executor.layers.fused_moe import fused_moe
+from vllm.model_executor.layers.fused_moe.fused_moe import (
+    fused_topk, moe_align_block_size)
 from vllm.model_executor.layers.fused_moe.moe_torch_iterative import (
     fused_moe as iterative_moe)
 from vllm.model_executor.layers.quantization.utils.marlin_utils_fp4 import (
