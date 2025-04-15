17b597b5695e61fb4b60e7350473641c5fd1dfef
唯勤 <xsank.mz@alibaba-inc.com>
2025-04-15 18:01:53 +0800

    add model loading test
    Signed-off-by: 唯勤 <xsank.mz@alibaba-inc.com>

--- tests/models/registry.py
+++ tests/models/registry.py
@@ -273,6 +273,8 @@ _CROSS_ENCODER_EXAMPLE_MODELS = {
     "BertForSequenceClassification": _HfExamplesInfo("cross-encoder/ms-marco-MiniLM-L-6-v2"),  # noqa: E501
     "RobertaForSequenceClassification": _HfExamplesInfo("cross-encoder/quora-roberta-base"),  # noqa: E501
     "XLMRobertaForSequenceClassification": _HfExamplesInfo("BAAI/bge-reranker-v2-m3"),  # noqa: E501
+    "ModernBertForSequenceClassification":
+        _HfExamplesInfo("Alibaba-NLP/gte-reranker-modernbert-base"),  # noqa: E501
 }
 
 _MULTIMODAL_EXAMPLE_MODELS = {
