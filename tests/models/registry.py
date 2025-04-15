f53eeb6c64413bf062aff4384b6ea78a88e50753
唯勤 <xsank.mz@alibaba-inc.com>
2025-04-15 19:10:57 +0800

    add min tf version of modernbert
    Signed-off-by: 唯勤 <xsank.mz@alibaba-inc.com>

--- tests/models/registry.py
+++ tests/models/registry.py
@@ -274,7 +274,8 @@ _CROSS_ENCODER_EXAMPLE_MODELS = {
     "RobertaForSequenceClassification": _HfExamplesInfo("cross-encoder/quora-roberta-base"),  # noqa: E501
     "XLMRobertaForSequenceClassification": _HfExamplesInfo("BAAI/bge-reranker-v2-m3"),  # noqa: E501
     "ModernBertForSequenceClassification":
-        _HfExamplesInfo("Alibaba-NLP/gte-reranker-modernbert-base"),
+        _HfExamplesInfo("Alibaba-NLP/gte-reranker-modernbert-base",
+                        min_transformers_version="4.49"),
 }
 
 _MULTIMODAL_EXAMPLE_MODELS = {
