b7e89a0279d4fe791eadc92b1f982b406c18d132
Stanislaw Wozniak <stw@zurich.ibm.com>
2025-04-16 14:03:51 +0000

    GraniteMoeHybrid model
    Signed-off-by: Stanislaw Wozniak <stw@zurich.ibm.com>

--- vllm/model_executor/models/registry.py
+++ vllm/model_executor/models/registry.py
@@ -65,6 +65,7 @@ _TEXT_GENERATION_MODELS = {
     "GPTNeoXForCausalLM": ("gpt_neox", "GPTNeoXForCausalLM"),
     "GraniteForCausalLM": ("granite", "GraniteForCausalLM"),
     "GraniteMoeForCausalLM": ("granitemoe", "GraniteMoeForCausalLM"),
+    "GraniteMoeHybridForCausalLM": ("granitemoehybrid", "GraniteMoeHybridForCausalLM"),
     "GraniteMoeSharedForCausalLM": ("granitemoeshared", "GraniteMoeSharedForCausalLM"),   # noqa: E501
     "GritLM": ("gritlm", "GritLM"),
     "Grok1ModelForCausalLM": ("grok1", "Grok1ForCausalLM"),
