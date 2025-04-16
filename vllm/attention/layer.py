2e228b2a30aec3a6e9921b3181ca51029f289ea5
Luka Govedič <lgovedic@redhat.com>
2025-04-16 20:57:23 +0000

    Add output_scale to all attention backends
    Signed-off-by: Luka Govedič <lgovedic@redhat.com>

--- vllm/attention/layer.py
+++ vllm/attention/layer.py
@@ -414,6 +414,7 @@ def unified_attention_with_output(
     value: torch.Tensor,
     output: torch.Tensor,
     layer_name: str,
+    output_scale: torch.Tensor = None,
 ) -> None:
     wait_for_kv_layer_from_connector(layer_name)
     forward_context: ForwardContext = get_forward_context()
@@ -428,7 +429,8 @@ def unified_attention_with_output(
                       value,
                       kv_cache,
                       attn_metadata,
-                      output=output)
+                      output=output,
+                      output_scale=output_scale)
 
     maybe_save_kv_layer_to_connector(layer_name, kv_cache)
 
@@ -439,6 +441,7 @@ def unified_attention_with_output_fake(
     value: torch.Tensor,
     output: torch.Tensor,
     layer_name: str,
+    output_scale: Optional[torch.Tensor] = None,
 ) -> None:
     return
 
