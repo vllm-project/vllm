b5f20e12794f17c404e727725fff0172c6be297f
qingjun <qingjun@minimaxi.com>
2025-04-17 17:13:24 +0800

    Removed unused files, including minimax_image_processer.py and minimax_vl_01_processing.py, and cleaned up redundant methods in minimax_text_01.py. Refactored the code structure to improve maintainability.
    Signed-off-by: qingjun <qingjun@minimaxi.com>

--- vllm/model_executor/models/minimax_text_01.py
+++ vllm/model_executor/models/minimax_text_01.py
@@ -1056,24 +1056,6 @@ class MiniMaxText01ForCausalLM(nn.Module, HasInnerState, IsHybrid,
     ) -> torch.Tensor:
         return self.model.embed_tokens(input_ids)
 
-    # def get_input_embeddings(self):
-    #     return self.model.embed_tokens
-
-    def set_input_embeddings(self, value):
-        self.model.embed_tokens = value
-
-    def get_output_embeddings(self):
-        return self.lm_head
-
-    def set_output_embeddings(self, new_embeddings):
-        self.lm_head = new_embeddings
-
-    def set_decoder(self, decoder):
-        self.model = decoder
-
-    def get_decoder(self):
-        return self.model
-
     def forward(self,
                 input_ids: torch.Tensor,
                 positions: torch.Tensor,
