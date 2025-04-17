3725309d32a8e752ba3add67c1da2fcfcd8d48b8
Yong Hoon Shin <yhshin@meta.com>
2025-04-17 15:29:28 -0700

    Fix max token index formula

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -243,11 +243,11 @@ class GPUModelRunner(LoRAModelRunnerMixin):
         # OPTIMIZATION: Cache the tensors rather than creating them every step.
 
         # For long context, we may need to store using int64 so max token idx doesn't overflow 
-        # token_indices calculated as such:
-        # E.g., [0, 1, 0, 1, 2, 3, 4, 0, 1, 2]
+        # token_indices is calculated by adding (req_idx * max_model_len) to per-request token indices 
+        # e.g. [0, 1, 0, 1, 2, 3, 4, 0, 1, 2] 
         # -> [0, 1, M, M + 1, M + 2, M + 3, M + 4, 2 * M, 2 * M + 1, 2 * M + 2]
         # where M is the max_model_len.
-        max_token_idx = (self.max_num_tokens + (self.max_num_reqs + 1) * self.max_model_len) 
+        max_token_idx = self.max_num_tokens + self.max_num_reqs * self.max_model_len
         self.arange_np = np.arange(max(self.max_num_reqs + 1,
                                        self.max_model_len,
                                        self.max_num_tokens),
@@ -535,7 +535,6 @@ class GPUModelRunner(LoRAModelRunnerMixin):
         # NOTE(woosuk): We use torch.index_select instead of np.take here
         # because torch.index_select is much faster than np.take for large
         # tensors.
-
         torch.index_select(self.input_batch.token_ids_cpu_tensor.flatten(),
                            0,
                            torch.from_numpy(token_indices),
