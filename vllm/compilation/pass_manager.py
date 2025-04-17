4c116aea5984da6a76f03e4bc9f794ebc63820dc
Luka Govedič <lgovedic@redhat.com>
2025-04-17 15:49:56 +0000

    Add flag for attention fusion pass
    Signed-off-by: Luka Govedič <lgovedic@redhat.com>

--- vllm/compilation/pass_manager.py
+++ vllm/compilation/pass_manager.py
@@ -60,8 +60,9 @@ class PostGradPassManager(CustomGraphPass):
             if self.pass_config.enable_async_tp:
                 self.passes += [AsyncTPPass(config)]
 
-        # TODO enable flag
-        self.passes += [AttnFusionPass(config)]
+        if self.pass_config.enable_attn_fusion:
+            self.passes += [AttnFusionPass(config)]
+
         self.fix_functionalization = FixFunctionalizationPass(config)
 
     def add(self, pass_: InductorPass):
