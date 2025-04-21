54b149662a28ef1113f21d01b1574bce82010d39
jiang.li <jiang1.li@intel.com>
2025-04-21 11:24:35 +0000

    fix contextmanager
    Signed-off-by: jiang.li <jiang1.li@intel.com>

--- vllm/worker/cpu_model_runner.py
+++ vllm/worker/cpu_model_runner.py
@@ -619,16 +619,18 @@ class CPUModelRunnerBase(ModelRunnerBase[TModelInputForCPU]):
 def _set_global_compilation_settings():
     import torch._inductor.config
 
-    # Note: The CPPGEMM backend requires freezing parameters.
-    freezing_value = torch._inductor.config.freezing
-    torch._inductor.config.freezing = True
-    # Note: workaround for "ValueError: fast mode: can't pickle cyclic objects
-    # including object type dict"
-    force_disable_caches = torch._inductor.config.force_disable_caches
-    torch._inductor.config.force_disable_caches = True
-    yield
-    torch._inductor.config.freezing = freezing_value
-    torch._inductor.config.force_disable_caches = force_disable_caches
+    try:
+        # Note: The CPPGEMM backend requires freezing parameters.
+        freezing_value = torch._inductor.config.freezing
+        torch._inductor.config.freezing = True
+        # Note: workaround for "ValueError: fast mode: can't pickle cyclic
+        # objects including object type dict"
+        force_disable_caches = torch._inductor.config.force_disable_caches
+        torch._inductor.config.force_disable_caches = True
+        yield
+    finally:
+        torch._inductor.config.freezing = freezing_value
+        torch._inductor.config.force_disable_caches = force_disable_caches
 
 
 class CPUModelRunner(CPUModelRunnerBase[ModelInputForCPUWithSamplingMetadata]):
