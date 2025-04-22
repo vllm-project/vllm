f8f47980a3eaf1f829878932393b5a885c4ab16c
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-04-22 13:55:36 +0100

    Improve configs - `SpeculativeConfig` (#16971)
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>
    Signed-off-by: Mu Huai <tianbowen.tbw@antgroup.com>

--- vllm/engine/arg_utils.py
+++ vllm/engine/arg_utils.py
@@ -768,11 +768,18 @@ class EngineArgs:
                             help=('Maximum number of forward steps per '
                                   'scheduler call.'))
 
-        parser.add_argument('--speculative-config',
-                            type=json.loads,
-                            default=None,
-                            help='The configurations for speculative decoding.'
-                            ' Should be a JSON string.')
+        # Speculative arguments
+        speculative_group = parser.add_argument_group(
+            title="SpeculativeConfig",
+            description=SpeculativeConfig.__doc__,
+        )
+        speculative_group.add_argument(
+            '--speculative-config',
+            type=json.loads,
+            default=None,
+            help='The configurations for speculative decoding.'
+            ' Should be a JSON string.')
+
         parser.add_argument(
             '--ignore-patterns',
             action="append",
