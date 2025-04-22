5b794cae8d0df288e83845e95a2874101e98357a
kliuae <17350011+kliuae@users.noreply.github.com>
2025-04-22 11:42:34 +0800

    [ROCm] Add aiter tkw1 kernel for Llama4 fp8 (#16727)
    Signed-off-by: kliuae <kuanfu.liu@embeddedllm.com>
    Signed-off-by: tjtanaa <tunjian.tan@embeddedllm.com>
    Co-authored-by: tjtanaa <tunjian.tan@embeddedllm.com>
    Co-authored-by: vllmellm <vllm.ellm@embeddedllm.com>

--- vllm/envs.py
+++ vllm/envs.py
@@ -77,7 +77,6 @@ if TYPE_CHECKING:
     VLLM_ROCM_USE_AITER: bool = False
     VLLM_ROCM_USE_AITER_LINEAR: bool = True
     VLLM_ROCM_USE_AITER_MOE: bool = True
-    VLLM_ROCM_USE_AITER_FP8_BLOCK_SCALED_MOE: bool = False
     VLLM_ROCM_USE_AITER_RMSNORM: bool = True
     VLLM_ROCM_FP8_PADDING: bool = True
     VLLM_ROCM_MOE_PADDING: bool = True
@@ -545,13 +544,6 @@ environment_variables: dict[str, Callable[[], Any]] = {
     "VLLM_ROCM_USE_AITER_MOE":
     lambda: (os.getenv("VLLM_ROCM_USE_AITER_MOE", "True").lower() in
              ("true", "1")),
-
-    # Whether to use aiter block scaled moe kernel.
-    # By default this is disabled.
-    "VLLM_ROCM_USE_AITER_FP8_BLOCK_SCALED_MOE":
-    lambda:
-    (os.getenv("VLLM_ROCM_USE_AITER_FP8_BLOCK_SCALED_MOE", "false").lower() in
-     ("true", "1")),
 
     # use aiter rms norm op if aiter ops are enabled.
     "VLLM_ROCM_USE_AITER_RMSNORM":
