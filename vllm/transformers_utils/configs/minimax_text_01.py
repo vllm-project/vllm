fdc8e32764d85e4b886cb449e486cc7704201b40
qingjun <qingjun@minimaxi.com>
2025-04-22 16:13:56 +0800

    fix code
    Signed-off-by: qingjun <qingjun@minimaxi.com>

--- vllm/transformers_utils/configs/minimax_text_01.py
+++ vllm/transformers_utils/configs/minimax_text_01.py
@@ -4,9 +4,6 @@
 from transformers.configuration_utils import PretrainedConfig
 from transformers.utils import logging
 
-logger = logging.get_logger(__name__)
-
-
 class MiniMaxText01Config(PretrainedConfig):
     model_type = "MiniMaxText01"
     keys_to_ignore_at_inference = ["past_key_values"]
