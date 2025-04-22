c4f49106c486c5dfd55921807e45a85edbbc509b
Reid <61492567+reidliu41@users.noreply.github.com>
2025-04-22 23:40:24 +0800

    [frontend] enhance tool_calls type check (#16882)
    Signed-off-by: reidliu41 <reid201711@gmail.com>
    Co-authored-by: reidliu41 <reid201711@gmail.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/entrypoints/chat_utils.py
+++ vllm/entrypoints/chat_utils.py
@@ -1095,7 +1095,11 @@ def _parse_chat_message_content(
         if role == 'assistant':
             parsed_msg = _AssistantParser(message)
 
-            if "tool_calls" in parsed_msg:
+            # The 'tool_calls' is not None check ensures compatibility.
+            # It's needed only if downstream code doesn't strictly
+            # follow the OpenAI spec.
+            if ("tool_calls" in parsed_msg
+                and parsed_msg["tool_calls"] is not None):
                 result_msg["tool_calls"] = list(parsed_msg["tool_calls"])
         elif role == "tool":
             parsed_msg = _ToolParser(message)
