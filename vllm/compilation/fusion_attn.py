0e7189e1318eb7178ea1ea5a026df154f6645485
Luka Govedič <lgovedic@redhat.com>
2025-04-22 05:24:50 +0000

    with pattern match
    Signed-off-by: Luka Govedič <lgovedic@redhat.com>

--- vllm/compilation/fusion_attn.py
+++ vllm/compilation/fusion_attn.py
@@ -1,17 +1,118 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import torch
+import torch._inductor.pattern_matcher as pm
 from torch import fx
+from torch._higher_order_ops.auto_functionalize import auto_functionalized
+from torch._inductor.pattern_matcher import PatternMatcherPass
+from torch._subclasses.fake_tensor import (FakeTensorMode,
+                                           unset_fake_temporarily)
 
 from vllm.attention import Attention
 from vllm.config import VllmConfig
 from vllm.logger import init_logger
 from vllm.platforms import current_platform
 
+# TODO(luka) move this around
+from .fusion import QUANT_OPS, QuantKey, empty_bf16, empty_fp32
 from .fx_utils import find_getitem, get_only_user, is_auto_func, is_func
 from .vllm_inductor_pass import VllmInductorPass
 
 logger = init_logger(__name__)
+
+ATTN_OP = torch.ops.vllm.unified_attention_with_output.default
+RESHAPE_OP = torch.ops.aten.reshape.default
+
+
+class AttentionStaticQuantPattern:
+
+    def __init__(
+        self,
+        num_heads: int,
+        head_size: int,
+        quant_dtype: torch.dtype,
+        symmetric=True,
+    ):
+        self.num_heads = num_heads
+        self.head_size = head_size
+        self.quant_dtype = quant_dtype
+        self.quant_key = QuantKey(dtype=quant_dtype,
+                                  static=True,
+                                  per_tensor=True,
+                                  symmetric=symmetric)
+        assert self.quant_key in QUANT_OPS, \
+            f"unsupported quantization scheme {self.quant_key}"
+        self.QUANT_OP = QUANT_OPS[self.quant_key]
+
+    def empty_quant(self, *args, **kwargs):
+        kwargs = {'dtype': self.quant_dtype, 'device': "cuda", **kwargs}
+        return torch.empty(*args, **kwargs)
+
+    def register(self, pm_pass: PatternMatcherPass):
+
+        def pattern(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor,
+                    output_attn: torch.Tensor, output_quant: torch.Tensor,
+                    scale: torch.Tensor):
+            view_7 = RESHAPE_OP(output_attn,
+                                [-1, self.num_heads, self.head_size])
+
+            at1 = auto_functionalized(
+                ATTN_OP,
+                query=q,
+                key=k,
+                value=v,
+                output=view_7,
+                layer_name='model.layers.0.self_attn.attn',
+                output_scale=None)
+            attn_out_view = RESHAPE_OP(at1[1],
+                                       [-1, self.num_heads * self.head_size])
+
+            at2 = auto_functionalized(self.QUANT_OP,
+                                      result=output_quant,
+                                      input=attn_out_view,
+                                      scale=scale)
+            return at2[1]
+
+        def replacement(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor,
+                        output_attn: torch.Tensor, output_quant: torch.Tensor,
+                        scale: torch.Tensor):
+            view_7 = RESHAPE_OP(output_quant,
+                                [-1, self.num_heads, self.head_size])
+
+            at1 = auto_functionalized(
+                ATTN_OP,
+                query=q,
+                key=k,
+                value=v,
+                output=view_7,
+                layer_name='model.layers.0.self_attn.attn',
+                output_scale=scale)
+
+            return RESHAPE_OP(at1[1], [-1, self.num_heads * self.head_size])
+
+        # custom fake mode, not tracing yet?
+        with unset_fake_temporarily(), FakeTensorMode():
+            inputs = [
+                empty_bf16(5, self.num_heads, self.head_size),  # q
+                empty_bf16(5, self.num_heads, self.head_size),  # k
+                empty_bf16(5, self.num_heads, self.head_size),  # v
+                empty_bf16(5, self.num_heads * self.head_size),  # attn_output
+                self.empty_quant(5, self.num_heads *
+                                 self.head_size),  # quant_output
+                empty_fp32(1, 1)  # scale
+            ]
+
+            def extra_check(match):
+                print(match)
+                return False
+
+            pm.register_replacement(pattern,
+                                    replacement,
+                                    inputs,
+                                    pm.fwd_only,
+                                    pm_pass,
+                                    extra_check=extra_check)
+            # extra_check=lambda m: record_match())
 
 
 class AttnFusionPass(VllmInductorPass):
@@ -31,6 +132,13 @@ class AttnFusionPass(VllmInductorPass):
         super().__init__(config)
         self.static_fwd_ctx = config.compilation_config.static_forward_context
 
+        self.patterns = PatternMatcherPass(pass_name="attn_fusion_pass")
+
+        key, layer = list(self.static_fwd_ctx.items())[0]
+        AttentionStaticQuantPattern(layer.num_heads, layer.head_size,
+                                    current_platform.fp8_dtype()).register(
+                                        self.patterns)
+
     def __call__(self, graph: torch.fx.graph.Graph) -> None:
         RESHAPE_OP = torch.ops.aten.reshape.default
         ATTN_OP = torch.ops.vllm.unified_attention_with_output.default
@@ -44,6 +152,11 @@ class AttnFusionPass(VllmInductorPass):
 
         self.begin()
         self.dump_graph(graph, "before_attn_fusion")
+
+        count2 = self.patterns.apply(graph)
+        logger.debug("Matched %s patterns", count2)
+        self.dump_graph(graph, "after_pattern_match")
+
         count = 0
         for node in graph.nodes:
             # Find the unified attention nodes
