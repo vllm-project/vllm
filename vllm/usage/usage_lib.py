3bd0c9ba75ad0ef2374f7ce14c5f7e8df5edcd6e
Daniel <dyli@google.com>
2025-04-23 07:40:26 +0000

    Use import torch_xla

--- vllm/usage/usage_lib.py
+++ vllm/usage/usage_lib.py
@@ -176,13 +176,12 @@ class UsageMessage:
             self.cuda_runtime = torch.version.cuda
         if current_platform.is_tpu():
             # try:
-            import torch_xla.runtime as xr
-            from torch_xla.core import xla_model as xm
-            self.gpu_count = xr.world_size()
+            import torch_xla
+            self.gpu_count = torch_xla.runtime.world_size()
             self.gpu_type = torch_xla.tpu.tpu_type()
-            self.gpu_memory_per_device = xm.get_memory_info().bytes_limit
+            self.gpu_memory_per_device = torch_xla.core.xla_model.get_memory_info().bytes_limit
             # except ImportError:
-                # pass
+            # pass
         self.provider = _detect_cloud_provider()
         self.architecture = platform.machine()
         self.platform = platform.platform()
