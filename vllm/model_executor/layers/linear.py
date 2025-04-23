1ee255ebc570eacab5e90153b6f3a95c09b9ec9b
Sage Moore <sage@neuralmagic.com>
2025-04-23 14:28:00 +0000

    init
    Signed-off-by: Sage Moore <sage@neuralmagic.com>

--- vllm/model_executor/layers/linear.py
+++ vllm/model_executor/layers/linear.py
@@ -1257,6 +1257,7 @@ class RowParallelLinear(LinearBase):
         # bias will not get added more than once in TP>1 case)
         bias_ = None if (self.tp_rank > 0 or self.skip_bias_add) else self.bias
         event = torch.cuda.Event()
+        event2 = torch.cuda.Event()
         m_over_two = input_parallel.shape[0] // 2
         if m_over_two > 16:
             first_output_parallel = self.quant_method.apply(
@@ -1265,6 +1266,7 @@ class RowParallelLinear(LinearBase):
             if self.reduce_results and self.tp_size > 1:
                 first_output = tensor_model_parallel_all_reduce(
                     first_output_parallel)
+                event2.wait()
             else:
                 first_output = first_output_parallel
 
@@ -1275,9 +1277,10 @@ class RowParallelLinear(LinearBase):
                 if self.reduce_results and self.tp_size > 1:
                     second_output = tensor_model_parallel_all_reduce(
                         second_output_parallel)
+                    event2.record()
                 else:
                     second_output = second_output_parallel
-            torch.cuda.synchronize()
+            # torch.cuda.synchronize()
             output = torch.cat((first_output, second_output), dim=0)
         else:
             output_parallel = self.quant_method.apply(self,
