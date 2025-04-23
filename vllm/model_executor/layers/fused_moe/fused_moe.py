ba394302065d9fbaee79693bf992333fa29c7bee
Pavani Majety <pmajety@nvidia.com>
2025-04-23 14:17:59 -0700

    remove changes to fused_moe.py
    Signed-off-by: Pavani Majety <pmajety@nvidia.com>

--- vllm/model_executor/layers/fused_moe/fused_moe.py
+++ vllm/model_executor/layers/fused_moe/fused_moe.py
@@ -21,7 +21,6 @@ from vllm.model_executor.layers.quantization.utils.int8_utils import (
 from vllm.platforms import current_platform
 from vllm.triton_utils import tl, triton
 from vllm.utils import direct_register_custom_op
-from vllm.scalar_type import scalar_types
 
 from .rocm_aiter_fused_moe import is_rocm_aiter_moe_enabled
 
@@ -1538,4 +1537,3 @@ def fused_moe(
                          a1_scale=a1_scale,
                          a2_scale=a2_scale,
                          block_shape=block_shape)
-
