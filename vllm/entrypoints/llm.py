e71623220fbae146610728d50fe41939cc431527
Alex-Brooks <Alex.Brooks@ibm.com>
2025-04-25 20:58:21 +0000

    Enable lora in beam search
    Signed-off-by: Alex-Brooks <Alex.Brooks@ibm.com>

--- vllm/entrypoints/llm.py
+++ vllm/entrypoints/llm.py
@@ -523,6 +523,7 @@ class LLM:
         self,
         prompts: list[Union[TokensPrompt, TextPrompt]],
         params: BeamSearchParams,
+        lora_request: Optional[Union[list[LoRARequest], LoRARequest]] = None,
     ) -> list[BeamSearchOutput]:
         """
         Generate sequences using beam search.
@@ -531,6 +532,7 @@ class LLM:
             prompts: A list of prompts. Each prompt can be a string or a list
                 of token IDs.
             params: The beam search parameters.
+            lora_request: LoRA request to use for generation, if any.
         """
         # TODO: how does beam search work together with length penalty,
         # frequency, penalty, and stopping criteria, etc.?
@@ -605,7 +607,8 @@ class LLM:
             # we don't need to use tqdm here
             output = self.generate(prompts_batch,
                                    sampling_params=beam_search_params,
-                                   use_tqdm=False)
+                                   use_tqdm=False,
+                                   lora_request=lora_request)
 
             for (start, end), instance in zip(instance_start_and_end,
                                               instances):
