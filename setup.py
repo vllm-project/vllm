af2af4dc30ef3b19da44a64336911f08a8a3af42
Aaron Pham <contact@aarnphm.xyz>
2025-04-26 10:45:26 -0400

    [Chore] added stubs for `vllm_flash_attn` during development mode (#17228)
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>
    Signed-off-by: Yuqi Zhang <yuqizhang@google.com>

--- setup.py
+++ setup.py
@@ -378,7 +378,6 @@ class repackage_wheel(build_ext):
                 "vllm/vllm_flash_attn/_vllm_fa2_C.abi3.so",
                 "vllm/vllm_flash_attn/_vllm_fa3_C.abi3.so",
                 "vllm/vllm_flash_attn/flash_attn_interface.py",
-                "vllm/vllm_flash_attn/__init__.py",
                 "vllm/cumem_allocator.abi3.so",
                 # "vllm/_version.py", # not available in nightly wheels yet
             ]
