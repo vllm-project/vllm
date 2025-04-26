6995a06ce0fc4a75ffaa97b11059628b4c780ac4
Aaron Pham <contact@aarnphm.xyz>
2025-04-26 12:37:48 +0000

    fix: add version check for bw
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- vllm/vllm_flash_attn/__init__.py
+++ vllm/vllm_flash_attn/__init__.py
@@ -1,5 +1,13 @@
 # SPDX-License-Identifier: Apache-2.0
 
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version("vllm-flash-attn")
+except importlib.metadata.PackageNotFoundError:
+    # in this case, vllm-flash-attn is built from installing vllm editable
+    __version__ = "0.0.0.dev0"
+
 from .flash_attn_interface import (fa_version_unsupported_reason,
                                    flash_attn_varlen_func,
                                    flash_attn_with_kvcache,
