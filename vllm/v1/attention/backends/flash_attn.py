0324d04841ce4dc867a56c2714d213b9dffef034
Lucas Wilkinson <LucasWilkinson@users.noreply.github.com>
2025-04-28 16:55:50 -0400

    [BugFix] Fix cascade attention - RuntimeError: scheduler_metadata must have shape (metadata_size) (#17283)
    Signed-off-by: Lucas Wilkinson <lwilkinson@neuralmagic.com>
    Signed-off-by: Mu Huai <tianbowen.tbw@antgroup.com>

--- vllm/v1/attention/backends/flash_attn.py
+++ vllm/v1/attention/backends/flash_attn.py
@@ -372,7 +372,7 @@ class FlashAttentionMetadataBuilder:
             suffix_kv_lens = torch.from_numpy(suffix_kv_lens).to(
                 self.runner.device)
             prefix_scheduler_metadata = schedule(
-                batch_size=num_reqs,
+                batch_size=1,
                 cu_query_lens=cu_prefix_query_lens,
                 max_query_len=num_actual_tokens,
                 seqlens=prefix_kv_lens,
