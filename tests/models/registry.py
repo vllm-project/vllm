273278efd356bb0c9c55fb13ed8fc3b619c94553
qingjun <qingjun@minimaxi.com>
2025-04-28 13:55:42 +0800

    add for test
    Signed-off-by: qingjun <qingjun@minimaxi.com>

--- tests/models/registry.py
+++ tests/models/registry.py
@@ -185,6 +185,8 @@ _TEXT_GENERATION_EXAMPLE_MODELS = {
                                          trust_remote_code=True),
     "MiniMaxText01ForCausalLM": _HfExamplesInfo("MiniMaxAI/MiniMax-Text-01",
                                                 trust_remote_code=True),
+    "MiniMaxVL01ForCausalLM": _HfExamplesInfo("MiniMaxAI/MiniMax-VL-01", # noqa: E501
+                                              trust_remote_code=True),
     "MistralForCausalLM": _HfExamplesInfo("mistralai/Mistral-7B-Instruct-v0.1"),
     "MixtralForCausalLM": _HfExamplesInfo("mistralai/Mixtral-8x7B-Instruct-v0.1"),  # noqa: E501
     "QuantMixtralForCausalLM": _HfExamplesInfo("mistral-community/Mixtral-8x22B-v0.1-AWQ"),  # noqa: E501
