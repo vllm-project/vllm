536f05c8a83a7778d788d01b3428cf208ffc8724
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-04-29 21:25:23 +0200

    Remove now unneeded test case
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- tests/engine/test_arg_utils.py
+++ tests/engine/test_arg_utils.py
@@ -67,7 +67,6 @@ def test_contains_type(type_hints, type, expected):
     ({int, float}, int, int),
     ({int, float}, str, None),
     ({str, Literal["x", "y"]}, Literal, Literal["x", "y"]),
-    ({Literal["x", "y"], Literal["z"]}, Literal, Literal["x", "y"]),
 ])
 def test_get_type(type_hints, type, expected):
     assert get_type(type_hints, type) == expected
