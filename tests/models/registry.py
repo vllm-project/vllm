6867fa3c937e1b52f95c136db2d65c22b5c31821
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-04-29 20:52:25 +0100

    Remove Falcon3 2x7B from CI (#17404)
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>
    Signed-off-by: wenhuach21 <wenhua.cheng@intel.com>

--- tests/models/registry.py
+++ tests/models/registry.py
@@ -191,7 +191,7 @@ _TEXT_GENERATION_EXAMPLE_MODELS = {
                                                 trust_remote_code=True),
     "MistralForCausalLM": _HfExamplesInfo("mistralai/Mistral-7B-Instruct-v0.1"),
     "MixtralForCausalLM": _HfExamplesInfo("mistralai/Mixtral-8x7B-Instruct-v0.1",  # noqa: E501
-                                          {"falcon3": "ehristoforu/Falcon3-MoE-2x7B-Insruct"}),  # noqa: E501
+                                          {"tiny": "TitanML/tiny-mixtral"}),  # noqa: E501
     "QuantMixtralForCausalLM": _HfExamplesInfo("mistral-community/Mixtral-8x22B-v0.1-AWQ"),  # noqa: E501
     "MptForCausalLM": _HfExamplesInfo("mpt", is_available_online=False),
     "MPTForCausalLM": _HfExamplesInfo("mosaicml/mpt-7b"),
