Fix overflow issues
Signed-off-by: Pavani Majety <pmajety@nvidia.com>

vllm/model_executor/layers/fused_moe/cutlass_moe.py
@@ -274,7 +274,6 @@ def cutlass_moe_fp4(
     ops.get_cutlass_moe_mm_data(topk_ids, expert_offsets, problem_sizes1,
                                 problem_sizes2, a_map, c_map, e,n,k)
 
-
     rep_a_fp4, rep_a_blockscale, rep_a_gs, sf_offsets = ops.scaled_expert_fp4_quant(
                                     a, a1_gscale, expert_offsets, use_expert_map=True, 
                                     expert_map=a_map, use_dynamic_scaling=True)
@@ -292,7 +291,7 @@ def cutlass_moe_fp4(
                                  device=device, dtype=out_dtype)
     
     torch.ops._C.silu_and_mul(intermediate, c1)
-    
+     
     int_fp4, int_blockscale, int_gs, sf_offsets = ops.scaled_expert_fp4_quant(  
                    intermediate, a2_gscale, expert_offsets, use_expert_map=False,
                                                     use_dynamic_scaling=True)
@@ -300,7 +299,6 @@ def cutlass_moe_fp4(
 
     alphas_2 = (w2_tensorscale / int_gs).clamp(max=FLT_MAX,
                                                min=FLT_MIN).to(torch.float32)
-    
     c2 = ops.cutlass_fp4_moe_mm(int_fp4, w2_fp4, int_blockscale,
                         w2_blockscale, alphas_2, problem_sizes2, 
                         expert_offsets[:-1], sf_offsets[:-1],