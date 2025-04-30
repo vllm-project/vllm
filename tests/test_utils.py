0db123e7591e7c750aa03b5757b6fc594d62f632
Russell Bryant <rbryant@redhat.com>
2025-04-30 09:44:41 -0400

    don't alias the socket module
    Signed-off-by: Russell Bryant <rbryant@redhat.com>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -706,11 +706,11 @@ def test_make_zmq_socket_ipv6():
     socket_type = zmq.REP  # Example socket type
 
     # Create the socket
-    socket = make_zmq_socket(ctx, ipv6_path, socket_type)
+    sock = make_zmq_socket(ctx, ipv6_path, socket_type)
 
     # Verify that the IPV6 option is set
-    assert socket.getsockopt(zmq.IPV6) == 1, "IPV6 option should be enabled for IPv6 addresses"
+    assert sock.getsockopt(zmq.IPV6) == 1, "IPV6 option should be enabled for IPv6 addresses"
 
     # Clean up
-    socket.close()
+    sock.close()
     ctx.term()
