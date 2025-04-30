70d6034bf2c2b39aef8afd47c5d125029ebce40b
Benjamin Chislett <benjamin.chislett@centml.ai>
2025-04-29 17:02:10 -0700

    [V1][Feature] Enable Speculative Decoding with Structured Outputs (#14702)
    Signed-off-by: Benjamin Chislett <benjamin.chislett@centml.ai>
    Signed-off-by: Benjamin Chislett <chislett.ben@gmail.com>
    Signed-off-by: Mu Huai <tianbowen.tbw@antgroup.com>

--- vllm/v1/structured_output/backend_guidance.py
+++ vllm/v1/structured_output/backend_guidance.py
@@ -144,6 +144,27 @@ class GuidanceGrammar(StructuredOutputGrammar):
 
         return r
 
+    def validate_tokens(self, tokens: list[int]) -> list[int]:
+        """Checks if the list of tokens are accepted by the parser in sequence.
+        Will not advance the parser.
+
+        Returns the prefix list of tokens that are accepted by the parser.
+        """
+        if len(tokens) == 0:
+            return []
+        if self.ll_matcher.is_stopped():
+            return []
+
+        num_tokens = self.ll_matcher.validate_tokens(tokens)
+
+        self.check_error()
+
+        return tokens[:num_tokens]
+
+    def rollback(self, num_tokens: int) -> None:
+        self.ll_matcher.rollback(num_tokens)
+        self.check_error()
+
     def fill_bitmask(self, bitmask: torch.Tensor, idx: int) -> None:
         # this will automatically return [EOS] mask if the matcher is stopped
         # or otherwise in an error state
