fa4babdf9487f3d249f005bf526e0b61373c92c7
Marko Rosenmueller <5467316+dr75@users.noreply.github.com>
2025-04-30 14:27:21 +0200

    [Core] Prevent side-channel attacks via cache salting (#17045)
    Signed-off-by: Marko Rosenmueller <5467316+dr75@users.noreply.github.com>
    Signed-off-by: Yuqi Zhang <yuqizhang@google.com>

--- tests/v1/core/test_kv_cache_utils.py
+++ tests/v1/core/test_kv_cache_utils.py
@@ -29,7 +29,8 @@ from vllm.v1.request import Request
 def make_request(request_id,
                  prompt_token_ids,
                  mm_positions=None,
-                 mm_hashes=None):
+                 mm_hashes=None,
+                 cache_salt=None):
     if mm_positions is None:
         multi_modal_inputs = None
     else:
@@ -45,6 +46,7 @@ def make_request(request_id,
         eos_token_id=100,
         arrival_time=0,
         lora_request=None,
+        cache_salt=cache_salt,
     )
 
 
@@ -211,6 +213,45 @@ def test_generate_block_hash_extra_keys_no_mm_inputs():
     extra_keys, next_mm_idx = generate_block_hash_extra_keys(request, 0, 5, 0)
     assert extra_keys is None
     assert next_mm_idx == 0
+
+
+def test_generate_block_hash_extra_keys_cache_salt():
+    request = make_request(
+        request_id=0,
+        prompt_token_ids=[_ for _ in range(6)],
+        mm_positions=None,
+        mm_hashes=None,
+        cache_salt="salt",
+    )
+
+    # salt is added for the first token
+    extra_keys, _ = generate_block_hash_extra_keys(request, 0, 1, 0)
+    assert extra_keys == ('salt', )
+    extra_keys, _ = generate_block_hash_extra_keys(request, 0, 10, 0)
+    assert extra_keys == ('salt', )
+
+    # no salt added for other tokens
+    extra_keys, _ = generate_block_hash_extra_keys(request, 1, 2, 0)
+    assert extra_keys is None
+    extra_keys, _ = generate_block_hash_extra_keys(request, 6, 10, 0)
+    assert extra_keys is None
+
+    # works together with other extra keys
+    request_mm = make_request(
+        request_id=0,
+        prompt_token_ids=[_ for _ in range(20)],
+        mm_positions=[
+            PlaceholderRange(offset=0, length=5),
+        ],
+        mm_hashes=["hash1"],
+        cache_salt="salt",
+    )
+
+    # Test with no extra keys
+    extra_keys, next_mm_idx = generate_block_hash_extra_keys(
+        request_mm, 0, 5, 0)
+    assert extra_keys == ("hash1", "salt")
+    assert next_mm_idx == 1
 
 
 @pytest.mark.parametrize("hash_fn", [sha256, hash])
