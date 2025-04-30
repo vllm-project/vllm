7cb6a81c9e1d5c1741a3cf5d9b9ec41bdccf3dad
Stanislaw Wozniak <stw@zurich.ibm.com>
2025-04-30 08:26:59 +0000

    Cleanup
    Co-authored-by: Thomas Ortner <boh@zurich.ibm.com>
    Signed-off-by: Stanislaw Wozniak <stw@zurich.ibm.com>

--- examples/offline_inference/basic/basic.py
+++ examples/offline_inference/basic/basic.py
@@ -1,6 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
-import torch
 from vllm import LLM, SamplingParams
 
 # Sample prompts.
@@ -12,18 +11,15 @@ prompts = [
 ]
 # Create a sampling params object.
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
-# sampling_params = SamplingParams(temperature=0.)
 
-DIR = '/block/granite/granite-4.0-tiny-base-pipecleaner-hf'
-# DIR = '/block/granite/granite-hybridmoe-7b-a1b-base-pipecleaner-hf'
 
 def main():
     # Create an LLM.
-    llm = LLM(model=DIR, dtype=torch.float16, gpu_memory_utilization=0.5)#, enforce_eager=True)
+    llm = LLM(model="facebook/opt-125m")
     # Generate texts from the prompts.
     # The output is a list of RequestOutput objects
     # that contain the prompt, generated text, and other information.
-    outputs = llm.generate(prompts[1], sampling_params)
+    outputs = llm.generate(prompts, sampling_params)
     # Print the outputs.
     print("\nGenerated Outputs:\n" + "-" * 60)
     for output in outputs:
