237fb2545886af0c1556904ca2da5fa842ba617e
Sage Moore <sage@neuralmagic.com>
2025-05-01 18:54:22 +0000

    init
    Signed-off-by: Sage Moore <sage@neuralmagic.com>

--- vllm/model_executor/layers/linear.py
+++ vllm/model_executor/layers/linear.py
@@ -1256,37 +1256,47 @@ class RowParallelLinear(LinearBase):
         # Only fuse bias add into GEMM for rank 0 (this ensures that
         # bias will not get added more than once in TP>1 case)
         bias_ = None if (self.tp_rank > 0 or self.skip_bias_add) else self.bias
-        event = torch.cuda.Event()
-        event2 = torch.cuda.Event()
+
+        is_ar_required = self.reduce_results and self.tp_size > 1
+        microbatch_threshold = 16
         m_over_two = input_parallel.shape[0] // 2
-        if m_over_two > 16:
+        should_use_microbatching = m_over_two > microbatch_threshold and \
+            is_ar_required
+
+        # If it makes sense to try and overlap the gemm and all
+        # reduce using microbatching
+        if should_use_microbatching:
+            event = torch.cuda.Event()
+            event2 = torch.cuda.Event()
+
+            # Run the first microbatch in the current stream
             first_output_parallel = self.quant_method.apply(
                 self, input_parallel[:m_over_two], bias=bias_)
-            event.record()
-            if self.reduce_results and self.tp_size > 1:
-                first_output = tensor_model_parallel_all_reduce(
-                    first_output_parallel)
-                event2.wait()
-            else:
-                first_output = first_output_parallel
 
+            # Prevent the second microbatch's gemm from starting until the
+            # first microbatch's gemm has finished
+            event.record()
+            first_output = tensor_model_parallel_all_reduce(
+                first_output_parallel)
+            # Block here until the second microbatch's all reduce finishes
+            event2.wait()
+
+            # Run the second microbatch in the backup stream
             with torch.cuda.stream(self.backup_stream):
                 event.wait()
                 second_output_parallel = self.quant_method.apply(
                     self, input_parallel[m_over_two:], bias=bias_)
-                if self.reduce_results and self.tp_size > 1:
-                    second_output = tensor_model_parallel_all_reduce(
-                        second_output_parallel)
-                    event2.record()
-                else:
-                    second_output = second_output_parallel
-            # torch.cuda.synchronize()
+                second_output = tensor_model_parallel_all_reduce(
+                    second_output_parallel)
+                # Notify the main stream that this microbatch has completed
+                event2.wait()
+            # torch.cuda.synchronize(self.backup_stream)
             output = torch.cat((first_output, second_output), dim=0)
         else:
             output_parallel = self.quant_method.apply(self,
                                                       input_parallel,
                                                       bias=bias_)
-            if self.reduce_results and self.tp_size > 1:
+            if is_ar_required:
                 output = tensor_model_parallel_all_reduce(output_parallel)
             else:
                 output = output_parallel
