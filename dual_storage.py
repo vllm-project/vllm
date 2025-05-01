89a57883f5ff65cafb06f50fa981df8b24e2cdd6
mgoin <mgoin64@gmail.com>
2025-05-01 23:29:03 +0000

    Example script
    Signed-off-by: mgoin <mgoin64@gmail.com>

---
+++ dual_storage.py
@@ -0,0 +1,66 @@
+# SPDX-License-Identifier: Apache-2.0
+
+from vllm import LLM, SamplingParams
+from vllm.config import KVTransferConfig
+
+context = "Hi " * 1000
+context2 = "Hey " * 500
+prompts = [
+    context + "Hello, my name is",
+    context + "The capital of France is",
+    context2 + "Your name is",
+    context2 + "The capital of China is",
+]
+
+sampling_params = SamplingParams(temperature=0, top_p=0.95, max_tokens=1)
+
+kv_transfer_config = KVTransferConfig(kv_connector="MultiConnector",
+                                      kv_role="kv_both",
+                                      kv_connector_extra_config={
+                                          "connectors": [
+                                              {
+                                                  "kv_connector":
+                                                  "SharedStorageConnector",
+                                                  "kv_role": "kv_both",
+                                                  "kv_connector_extra_config":
+                                                  {
+                                                      "shared_storage_path":
+                                                      "local_storage"
+                                                  }
+                                              },
+                                              {
+                                                  "kv_connector":
+                                                  "SharedStorageConnector",
+                                                  "kv_role": "kv_both",
+                                                  "kv_connector_extra_config":
+                                                  {
+                                                      "shared_storage_path":
+                                                      "external_storage"
+                                                  }
+                                              },
+                                          ]
+                                      })
+
+llm = LLM(model="meta-llama/Llama-3.2-1B-Instruct",
+          enforce_eager=True,
+          gpu_memory_utilization=0.8,
+          kv_transfer_config=kv_transfer_config)
+
+# 1ST generation (prefill instance)
+outputs = llm.generate(
+    prompts,
+    sampling_params,
+)
+
+new_prompts = []
+for output in outputs:
+    prompt = output.prompt
+    generated_text = output.outputs[0].text
+    new_prompts.append(prompt + generated_text)
+    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+
+# Write new_prompts to output.txt
+with open("output.txt", "w") as f:
+    for prompt in new_prompts:
+        f.write(prompt + "\n")
+print(f"Saved {len(new_prompts)} prompts to output.txt")
