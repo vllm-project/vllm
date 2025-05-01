75133942a2e75ad97ab0101c53c89aebc2bd6aa2
Isotr0py <2037008807@qq.com>
2025-05-01 23:59:19 +0800

    remove placeholder from sys.modules
    Signed-off-by: Isotr0py <2037008807@qq.com>

--- vllm/triton_utils/importing.py
+++ vllm/triton_utils/importing.py
@@ -1,9 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
-import importlib
-import sys
+
 import types
 from importlib.util import find_spec
-from typing import Optional
 
 from vllm.logger import init_logger
 
@@ -18,37 +16,17 @@ if not HAS_TRITON:
     logger.info("Triton not installed or not compatible; certain GPU-related"
                 " functions will not be available.")
 
-    class TritonModulePlaceholder(types.ModuleType):
-
-        def __init__(
-            self,
-            name: str,
-            dummy_objects: Optional[list[str]] = None,
-        ):
-            super().__init__(name)
-
-            if dummy_objects is not None:
-                for obj_name in dummy_objects:
-                    setattr(self, obj_name, object)
-
-    class TritonLanguagePlaceholder(TritonModulePlaceholder):
+    class TritonPlaceholder(types.ModuleType):
 
         def __init__(self):
-            super().__init__("triton.language")
-            self.constexpr = None
-            self.dtype = None
-
-    class TritonPlaceholder(TritonModulePlaceholder):
-
-        def __init__(self):
-            super().__init__("triton", dummy_objects=["Config"])
+            super().__init__("triton")
             self.jit = self._dummy_decorator("jit")
             self.autotune = self._dummy_decorator("autotune")
             self.heuristics = self._dummy_decorator("heuristics")
             self.language = TritonLanguagePlaceholder()
             logger.warning_once(
                 "Triton is not installed. Using dummy decorators. "
-                "Install it via `pip install triton` to enable kernel "
+                "Install it via `pip install triton` to enable kernel"
                 "compilation.")
 
         def _dummy_decorator(self, name):
@@ -60,27 +38,10 @@ if not HAS_TRITON:
 
             return decorator
 
-    # Hack `_is_triton_available` in torch to return False
-    torch_hints = importlib.import_module("torch._inductor.runtime.hints")
-    torch_hints._is_triton_available = lambda: False  # type: ignore[attr-defined]
+    class TritonLanguagePlaceholder(types.ModuleType):
 
-    # Replace the triton module and torch triton helpers in sys.modules
-    # with the placeholder
-    sys.modules['triton'] = TritonPlaceholder()
-    sys.modules['triton.language'] = TritonLanguagePlaceholder()
-    sys.modules['torch._inductor.runtime.triton_helpers'] = types.ModuleType(
-        "triton_helpers")
-
-    # Replace triton submodules with dummy objects to keep compatibility with
-    # torch triton check
-    triton_modules_with_objects = {
-        "triton.compiler": ["CompiledKernel"],
-        "triton.runtime.autotuner": ["OutOfResources"],
-        "triton.runtime.jit": ["KernelInterface"],
-    }
-    for module_name, dummy_objects in triton_modules_with_objects.items():
-        sys.modules[module_name] = TritonModulePlaceholder(
-            module_name, dummy_objects=dummy_objects)
-
-if 'triton' in sys.modules:
-    logger.info("Triton module has been replaced with a placeholder.")
+        def __init__(self):
+            super().__init__("triton.language")
+            self.constexpr = None
+            self.dtype = None
+            self.int64 = None
