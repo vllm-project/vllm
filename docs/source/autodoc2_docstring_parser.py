f622e00eea5e9973fae1eb90f27c9829f3cdd37a
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-05-01 23:07:45 +0100

    pre-commit
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- docs/source/autodoc2_docstring_parser.py
+++ docs/source/autodoc2_docstring_parser.py
@@ -1,9 +1,11 @@
+# SPDX-License-Identifier: Apache-2.0
 from docutils import nodes
 from myst_parser.parsers.sphinx_ import MystParser
 from sphinx.ext.napoleon import docstring
 
 
 class NapoleonParser(MystParser):
+
     def parse(self, input_string: str, document: nodes.document) -> None:
         # Get the Sphinx configuration
         config = document.settings.env.config
@@ -12,8 +14,7 @@ class NapoleonParser(MystParser):
             docstring.GoogleDocstring(
                 str(docstring.NumpyDocstring(input_string, config)),
                 config,
-            )
-        )
+            ))
         return super().parse(parsed_content, document)
 
 
