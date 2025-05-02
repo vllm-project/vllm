5bbc79bfe63162f6201876a11e1486887beb2f08
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-05-02 22:08:05 +0100

    pre-commit
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- vllm/distributed/kv_transfer/__init__.py
+++ vllm/distributed/kv_transfer/__init__.py
@@ -1,9 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
+from vllm.distributed.kv_transfer.kv_connector.base import KVConnectorBaseType
 from vllm.distributed.kv_transfer.kv_transfer_state import (
     ensure_kv_transfer_initialized, get_kv_transfer_group,
     has_kv_transfer_group, is_v1_kv_transfer_group)
-from vllm.distributed.kv_transfer.kv_connector.base import KVConnectorBaseType
 
 __all__ = [
     "get_kv_transfer_group", "has_kv_transfer_group",
