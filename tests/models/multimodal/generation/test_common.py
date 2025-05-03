eb205ea3651c5a4f38890044b3f2b9989068f790
Isotr0py <2037008807@qq.com>
2025-05-03 19:47:50 +0800

    fix test
    Signed-off-by: Isotr0py <2037008807@qq.com>

--- tests/models/multimodal/generation/test_common.py
+++ tests/models/multimodal/generation/test_common.py
@@ -154,6 +154,7 @@ VLM_TEST_SETTINGS = {
         max_num_seqs=2,
         auto_cls=AutoModelForTextToWaveform,
         vllm_output_post_proc=model_utils.qwen2_vllm_to_hf_output,
+        patch_hf_runner=model_utils.qwen2_5_omni_patch_hf_runner,
         image_size_factors=[(), (0.25,), (0.25, 0.25, 0.25), (0.25, 0.2, 0.15)],
         marks=[pytest.mark.core_model, pytest.mark.cpu_model],
     ),
