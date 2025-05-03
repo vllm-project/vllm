0e859331f608f4841643a7b222d450435baa9fd0
DarkLight1337 <tlleungac@connect.ust.hk>
2025-05-03 15:43:58 +0000

    Update script
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- benchmark_processing.py
+++ benchmark_processing.py
@@ -5,7 +5,7 @@ import csv
 import time
 from datetime import datetime
 from pathlib import Path
-from typing import Literal, Union
+from typing import Literal, Union, get_args
 
 import numpy as np
 import pandas as pd
@@ -14,281 +14,20 @@ from tqdm import tqdm
 import vllm.envs as envs
 from tests.models.registry import HF_EXAMPLE_MODELS
 from tests.multimodal.utils import random_audio, random_image, random_video
-from vllm import AsyncEngineArgs, SamplingParams
+from vllm import AsyncEngineArgs, EngineArgs, SamplingParams
 from vllm.config import ModelConfig, ParallelProcessorBackend
 from vllm.engine.async_llm_engine import AsyncLLMEngine
+from vllm.engine.llm_engine import LLMEngine
+from vllm.engine.protocol import EngineClient
 from vllm.inputs import InputProcessingContext
 from vllm.multimodal import MULTIMODAL_REGISTRY
 from vllm.transformers_utils.tokenizer import cached_tokenizer_from_config
 from vllm.usage.usage_lib import UsageContext
 
-
-def get_engine(
-    engine_args: AsyncEngineArgs,
-    usage_context: UsageContext = UsageContext.ENGINE_CONTEXT,
-):
-    # Create the engine configs.
-    vllm_config = engine_args.create_engine_config(usage_context)
-
-    engine_cls = AsyncLLMEngine
-    if envs.VLLM_USE_V1:
-        from vllm.v1.engine.async_llm import AsyncLLM
-        engine_cls = AsyncLLM
-
-    return engine_cls.from_vllm_config(
-        vllm_config=vllm_config,
-        usage_context=usage_context,
-        disable_log_stats=engine_args.disable_log_stats,
-    )
+ModalityStr = Literal["audio", "image", "video"]
 
 
-async def benchmark_audio_processing(
-    model_id: str,
-    parallel_backend: ParallelProcessorBackend,
-    *,
-    num_audios: int = 200,
-) -> float:
-    rng = np.random.RandomState(0)
-    audios = [random_audio(rng, 1024, 4096, 16000) for _ in range(num_audios)]
-
-    model_info = HF_EXAMPLE_MODELS.find_hf_info(model_id)
-    args = AsyncEngineArgs(
-        model=model_id,
-        tokenizer=model_info.tokenizer or model_id,
-        tokenizer_mode=model_info.tokenizer_mode,
-        trust_remote_code=model_info.trust_remote_code,
-        parallel_processor_backend=parallel_backend,
-        max_model_len=4096,
-        limit_mm_per_prompt={
-            "audio": 1,
-            "image": 0,
-            "video": 0
-        },
-        enforce_eager=True,
-        disable_log_requests=True,
-    )
-    engine = get_engine(args)
-
-    model_config = await engine.get_model_config()
-    tokenizer = cached_tokenizer_from_config(model_config)
-    prompt = tokenizer.apply_chat_template(
-        [
-            {
-                "role":
-                "user",
-                "content": [
-                    {
-                        "type": "audio"
-                    },
-                    {
-                        "type": "text",
-                        "text": "Describe this audio"
-                    },
-                ]
-            },
-        ],
-        tokenize=False,
-    )
-
-    start_s = time.monotonic()
-    progbar = tqdm(
-        [None] * len(audios),
-        desc=f"Audio processing for {model_id=}, {parallel_backend=}",
-    )
-
-    async def _chat(idx: int):
-        generator = engine.generate(
-            {
-                "prompt": prompt,
-                "multi_modal_data": {
-                    "audio": [audios[idx]]
-                }
-            },
-            sampling_params=SamplingParams(max_tokens=1),
-            request_id=str(idx),
-        )
-
-        async for output in generator:
-            progbar.update()
-
-    await asyncio.gather(*(_chat(idx) for idx in range(len(audios))))
-
-    total_s = (time.monotonic() - start_s)
-
-    return total_s / num_audios
-
-
-async def benchmark_image_processing(
-    model_id: str,
-    parallel_backend: ParallelProcessorBackend,
-    *,
-    num_images: int = 400,
-) -> float:
-    rng = np.random.RandomState(0)
-    images = [random_image(rng, 256, 1024) for _ in range(num_images)]
-
-    model_info = HF_EXAMPLE_MODELS.find_hf_info(model_id)
-    args = AsyncEngineArgs(
-        model=model_id,
-        tokenizer=model_info.tokenizer or model_id,
-        tokenizer_mode=model_info.tokenizer_mode,
-        trust_remote_code=model_info.trust_remote_code,
-        parallel_processor_backend=parallel_backend,
-        max_model_len=4096,
-        limit_mm_per_prompt={
-            "audio": 0,
-            "image": 1,
-            "video": 0
-        },
-        enforce_eager=True,
-        disable_log_requests=True,
-    )
-    engine = get_engine(args)
-
-    model_config = await engine.get_model_config()
-    tokenizer = cached_tokenizer_from_config(model_config)
-    prompt = tokenizer.apply_chat_template(
-        [
-            {
-                "role":
-                "user",
-                "content": [
-                    {
-                        "type": "image"
-                    },
-                    {
-                        "type": "text",
-                        "text": "Describe this image"
-                    },
-                ]
-            },
-        ],
-        tokenize=False,
-    )
-
-    start_s = time.monotonic()
-    progbar = tqdm(
-        [None] * len(images),
-        desc=f"Image processing for {model_id=}, {parallel_backend=}",
-    )
-
-    async def _chat(idx: int):
-        generator = engine.generate(
-            {
-                "prompt": prompt,
-                "multi_modal_data": {
-                    "image": [images[idx]]
-                }
-            },
-            sampling_params=SamplingParams(max_tokens=1),
-            request_id=str(idx),
-        )
-
-        async for output in generator:
-            progbar.update()
-
-    await asyncio.gather(*(_chat(idx) for idx in range(len(images))))
-
-    total_s = (time.monotonic() - start_s)
-
-    return total_s / num_images
-
-
-async def benchmark_video_processing(
-    model_id: str,
-    parallel_backend: ParallelProcessorBackend,
-    *,
-    num_videos: int = 100,
-) -> float:
-    rng = np.random.RandomState(0)
-    videos = [random_video(rng, 4, 16, 256, 1024) for _ in range(num_videos)]
-
-    model_info = HF_EXAMPLE_MODELS.find_hf_info(model_id)
-    args = AsyncEngineArgs(
-        model=model_id,
-        tokenizer=model_info.tokenizer or model_id,
-        tokenizer_mode=model_info.tokenizer_mode,
-        trust_remote_code=model_info.trust_remote_code,
-        parallel_processor_backend=parallel_backend,
-        max_model_len=4096,
-        limit_mm_per_prompt={
-            "audio": 0,
-            "image": 0,
-            "video": 1
-        },
-        enforce_eager=True,
-        disable_log_requests=True,
-    )
-    engine = get_engine(args)
-
-    model_config = await engine.get_model_config()
-    tokenizer = cached_tokenizer_from_config(model_config)
-    prompt = tokenizer.apply_chat_template(
-        [
-            {
-                "role":
-                "user",
-                "content": [
-                    {
-                        "type": "video"
-                    },
-                    {
-                        "type": "text",
-                        "text": "Describe this video"
-                    },
-                ]
-            },
-        ],
-        tokenize=False,
-    )
-
-    start_s = time.monotonic()
-    progbar = tqdm(
-        [None] * len(videos),
-        desc=f"Video processing for {model_id=}, {parallel_backend=}",
-    )
-
-    async def _chat(idx: int):
-        generator = engine.generate(
-            {
-                "prompt": prompt,
-                "multi_modal_data": {
-                    "video": [videos[idx]]
-                }
-            },
-            sampling_params=SamplingParams(max_tokens=1),
-            request_id=str(idx),
-        )
-
-        async for output in generator:
-            progbar.update()
-
-    await asyncio.gather(*(_chat(idx) for idx in range(len(videos))))
-
-    total_s = (time.monotonic() - start_s)
-
-    return total_s / num_videos
-
-
-BENCHMARK_FNS = {
-    "audio": benchmark_audio_processing,
-    "image": benchmark_image_processing,
-    "video": benchmark_video_processing,
-}
-
-
-async def benchmark_processing(
-    modality: str,
-    model_id: str,
-    parallel_backend: ParallelProcessorBackend,
-) -> float:
-    return await BENCHMARK_FNS[modality](model_id, parallel_backend)
-
-
-async def benchmark_one(
-    model_id: str,
-    parallel_backend: ParallelProcessorBackend,
-) -> dict[str, float]:
+def get_supported_modalities(model_id: str) -> list[ModalityStr]:
     model_info = HF_EXAMPLE_MODELS.find_hf_info(model_id)
     model_config = ModelConfig(
         model_id,
@@ -312,11 +51,241 @@ async def benchmark_one(
     processing_info = factories.info(ctx)
     supported_mm_limits = processing_info.get_supported_mm_limits()
 
+    return list(supported_mm_limits.keys())
+
+
+def engine_from_args(engine_args: EngineArgs) -> LLMEngine:
+    return LLMEngine.from_engine_args(
+        engine_args,
+        usage_context=UsageContext.ENGINE_CONTEXT,
+    )
+
+
+def async_engine_from_args(engine_args: AsyncEngineArgs) -> EngineClient:
+    vllm_config = engine_args.create_engine_config(
+        usage_context=UsageContext.ENGINE_CONTEXT)
+
+    engine_cls = AsyncLLMEngine
+    if envs.VLLM_USE_V1:
+        from vllm.v1.engine.async_llm import AsyncLLM
+        engine_cls = AsyncLLM
+
+    return engine_cls.from_vllm_config(
+        vllm_config=vllm_config,
+        disable_log_requests=engine_args.disable_log_requests,
+        disable_log_stats=engine_args.disable_log_stats,
+    )
+
+
+def get_engine(
+    model_id: str,
+    parallel_backend: ParallelProcessorBackend,
+    modalities: list[ModalityStr],
+) -> LLMEngine:
+    model_info = HF_EXAMPLE_MODELS.find_hf_info(model_id)
+    args = EngineArgs(
+        model=model_id,
+        tokenizer=model_info.tokenizer or model_id,
+        tokenizer_mode=model_info.tokenizer_mode,
+        trust_remote_code=model_info.trust_remote_code,
+        parallel_processor_backend=parallel_backend,
+        max_model_len=4096,
+        limit_mm_per_prompt={
+            m: m in modalities
+            for m in get_args(ModalityStr)
+        },
+        enforce_eager=True,
+    )
+
+    return engine_from_args(args)
+
+
+def get_async_engine(
+    model_id: str,
+    parallel_backend: ParallelProcessorBackend,
+    modalities: list[ModalityStr],
+) -> EngineClient:
+    model_info = HF_EXAMPLE_MODELS.find_hf_info(model_id)
+    args = AsyncEngineArgs(
+        model=model_id,
+        tokenizer=model_info.tokenizer or model_id,
+        tokenizer_mode=model_info.tokenizer_mode,
+        trust_remote_code=model_info.trust_remote_code,
+        parallel_processor_backend=parallel_backend,
+        max_model_len=4096,
+        limit_mm_per_prompt={
+            m: m in modalities
+            for m in get_args(ModalityStr)
+        },
+        enforce_eager=True,
+        disable_log_requests=True,
+    )
+
+    return async_engine_from_args(args)
+
+
+def get_prompt(model_config: ModelConfig, modality: ModalityStr) -> str:
+    tokenizer = cached_tokenizer_from_config(model_config)
+
+    return tokenizer.apply_chat_template(
+        [
+            {
+                "role":
+                "user",
+                "content": [
+                    {
+                        "type": modality
+                    },
+                    {
+                        "type": "text",
+                        "text": f"Describe this {modality}"
+                    },
+                ]
+            },
+        ],
+        tokenize=False,
+    )
+
+
+def get_benchmark_data(modality: ModalityStr):
+    rng = np.random.RandomState(0)
+
+    if modality == "audio":
+        return [random_audio(rng, 1024, 4096, 16000) for _ in range(100)]
+    if modality == "image":
+        return [random_image(rng, 256, 1024) for _ in range(200)]
+    if modality == "video":
+        return [random_video(rng, 4, 16, 256, 1024) for _ in range(50)]
+
+
+def get_benchmark_mm_data(
+        modalities: list[ModalityStr]) -> dict[ModalityStr, list]:
+    return {m: get_benchmark_data(m) for m in modalities}
+
+
+def benchmark(
+    model_id: str,
+    parallel_backend: ParallelProcessorBackend,
+    engine: LLMEngine,
+    modality: ModalityStr,
+    data: list,
+) -> float:
+    model_config = engine.get_model_config()
+    prompt = get_prompt(model_config, modality)
+
+    start_s = time.monotonic()
+    progbar = tqdm(
+        [None] * len(data),
+        desc=f"Benchmarking {model_id=}, {parallel_backend=}, {modality=}",
+    )
+
+    def _chat(idx: int, item):
+        engine.add_request(
+            prompt={
+                "prompt": prompt,
+                "multi_modal_data": {
+                    modality: item
+                }
+            },
+            params=SamplingParams(max_tokens=1),
+            request_id=str(idx),
+        )
+
+        engine.step()
+        progbar.update()
+
+    for args in enumerate(data):
+        _chat(*args)
+
+    total_s = (time.monotonic() - start_s)
+
+    return total_s / len(data)
+
+
+async def benchmark_async(
+    model_id: str,
+    parallel_backend: ParallelProcessorBackend,
+    engine: EngineClient,
+    modality: ModalityStr,
+    data: list,
+) -> float:
+    model_config = await engine.get_model_config()
+    prompt = get_prompt(model_config, modality)
+
+    start_s = time.monotonic()
+    progbar = tqdm(
+        [None] * len(data),
+        desc=f"Benchmarking {model_id=}, {parallel_backend=}, {modality=}",
+    )
+
+    async def _chat(idx: int, item):
+        generator = engine.generate(
+            prompt={
+                "prompt": prompt,
+                "multi_modal_data": {
+                    modality: item
+                }
+            },
+            sampling_params=SamplingParams(max_tokens=1),
+            request_id=str(idx),
+        )
+
+        async for output in generator:
+            progbar.update()
+
+    await asyncio.gather(*(_chat(*args) for args in enumerate(data)))
+
+    total_s = (time.monotonic() - start_s)
+
+    return total_s / len(data)
+
+
+def benchmark_mm(
+    model_id: str,
+    parallel_backend: ParallelProcessorBackend,
+) -> dict[str, float]:
+    modalities = get_supported_modalities(model_id)
+    engine = get_engine(model_id, parallel_backend, modalities)
+    mm_data = get_benchmark_mm_data(modalities)
+
     return {
-        modality: await benchmark_processing(modality, model_id,
-                                             parallel_backend)
-        for modality in supported_mm_limits
+        m: benchmark(model_id, parallel_backend, engine, m, data)
+        for m, data in mm_data.items()
     }
+
+
+async def benchmark_mm_async(
+    model_id: str,
+    parallel_backend: ParallelProcessorBackend,
+) -> dict[str, float]:
+    modalities = get_supported_modalities(model_id)
+    engine = get_async_engine(model_id, parallel_backend, modalities)
+    mm_data = get_benchmark_mm_data(modalities)
+
+    return {
+        m: await benchmark_async(model_id, parallel_backend, engine, m, data)
+        for m, data in mm_data.items()
+    }
+
+
+def benchmark_one(
+    model_id: str,
+    parallel_backend: ParallelProcessorBackend,
+) -> dict[str, float]:
+    return benchmark_mm(
+        model_id,
+        parallel_backend,
+    )
+
+
+async def benchmark_one_async(
+    model_id: str,
+    parallel_backend: ParallelProcessorBackend,
+) -> dict[str, float]:
+    return await benchmark_mm_async(
+        model_id,
+        parallel_backend,
+    )
 
 
 # Some models OOM or don't have chat template
@@ -368,11 +337,11 @@ MODELS = [
 ]
 
 
-async def main(
+def _resolve_args(
     model_id: Union[str, Literal["all"]],
     parallel_backend: Literal[ParallelProcessorBackend, "all"],
     output_dir: str,
-) -> None:
+):
     models = MODELS if model_id == "all" else [model_id]
     parallel_backends = (("uni", "mp", "mt") if parallel_backend == "all" else
                          (parallel_backend, ))
@@ -383,6 +352,26 @@ async def main(
             raise ValueError("Output path must be a directory")
     else:
         output_path.mkdir()
+
+    print("[Models]")
+    print("\n".join("- " + model for model in models))
+
+    print("[Parallel Backends]")
+    print("\n".join("- " + backend for backend in parallel_backends))
+
+    return models, parallel_backends, output_path
+
+
+def main(
+    model_id: Union[str, Literal["all"]],
+    parallel_backend: Literal[ParallelProcessorBackend, "all"],
+    output_dir: str,
+) -> None:
+    models, parallel_backends, output_path = _resolve_args(
+        model_id,
+        parallel_backend,
+        output_dir,
+    )
 
     timestamp = int(datetime.now().timestamp())
     csv_filepath = output_path / f"processing_{timestamp}.csv"
@@ -402,7 +391,56 @@ async def main(
 
         for model in models:
             for backend in parallel_backends:
-                res_one = await benchmark_one(model_id, backend)
+                res_one = benchmark_one(model, backend)
+
+                csv_writer.writerow({
+                    "model_id": model,
+                    "parallel_backend": backend,
+                    **{
+                        f"ms_{k}": v * 1000
+                        for k, v in res_one.items()
+                    },
+                })
+
+    print(f"Saved results to: {csv_filepath}")
+
+    json_filepath = output_path / f"processing_{timestamp}.json"
+
+    pd.read_csv(csv_filepath).to_json(json_filepath, indent=4)
+
+    print(f"Saved results to: {json_filepath}")
+
+
+async def main_async(
+    model_id: Union[str, Literal["all"]],
+    parallel_backend: Literal[ParallelProcessorBackend, "all"],
+    output_dir: str,
+) -> None:
+    models, parallel_backends, output_path = _resolve_args(
+        model_id,
+        parallel_backend,
+        output_dir,
+    )
+
+    timestamp = int(datetime.now().timestamp())
+    csv_filepath = output_path / f"processing_{timestamp}.csv"
+
+    with csv_filepath.open("w") as f:
+        csv_writer = csv.DictWriter(
+            f,
+            fieldnames=[
+                "model_id",
+                "parallel_backend",
+                "ms_audio",
+                "ms_image",
+                "ms_video",
+            ],
+        )
+        csv_writer.writeheader()
+
+        for model in models:
+            for backend in parallel_backends:
+                res_one = await benchmark_one_async(model, backend)
 
                 csv_writer.writerow({
                     "model_id": model,
@@ -442,8 +480,14 @@ if __name__ == "__main__":
                         type=str,
                         required=True,
                         help="Directory to save the results")
+    parser.add_argument("--sync",
+                        action="store_true",
+                        help="Test the sync engine instead of async engine")
 
     args = parser.parse_args()
-    coro = main(args.model, args.parallel_backend, args.output_dir)
 
-    asyncio.run(coro)
+    if args.sync:
+        main(args.model, args.parallel_backend, args.output_dir)
+    else:
+        coro = main_async(args.model, args.parallel_backend, args.output_dir)
+        asyncio.run(coro)
