493cda1136ffcbb83bd2e3786e8dd6c9a1935e83
rshaw@neuralmagic.com <robertgshaw2@gmail.com>
2025-05-05 22:23:56 +0000

    update to use separate scheduler
    Signed-off-by: rshaw@neuralmagic.com <robertgshaw2@gmail.com>

--- vllm/v1/core/sched/scheduler_disagg.py
+++ vllm/v1/core/sched/scheduler_disagg.py
@@ -343,7 +343,7 @@ class DisaggregatedScheduler(Scheduler):
                 request.num_computed_tokens = num_computed_tokens
 
                 # Encoder-related.
-                if not request.do_remote_prefill and encoder_inputs_to_schedule:
+                if encoder_inputs_to_schedule:
                     scheduled_encoder_inputs[request.request_id] = (
                         encoder_inputs_to_schedule)
                     # Allocate the encoder cache.
