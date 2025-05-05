0c5b5e471c3715c14d4f1b3776ef8b6db0badd6a
David Xia <david@davidxia.com>
2025-05-05 15:22:08 +0000

    [Bugfix]: make `test_openai_schema.py` pass
    This change makes `pytest tests/entrypoints/openai/test_openai_schema.py` pass.
    We handle user input errors, enrich HTTP error messages with more information,
    and update the error response definitions for several API endpoints.
    
    FIX #17037
    FIX #17038
    
    Signed-off-by: David Xia <david@davidxia.com>

--- tests/entrypoints/openai/test_openai_schema.py
+++ tests/entrypoints/openai/test_openai_schema.py
@@ -1,7 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
+from typing import Final
+
 import pytest
 import schemathesis
-from schemathesis import GenerationConfig
+from schemathesis.generation import GenerationConfig
 
 from ...utils import RemoteOpenAIServer
 
@@ -9,6 +11,7 @@ schemathesis.experimental.OPEN_API_3_1.enable()
 
 MODEL_NAME = "HuggingFaceTB/SmolVLM-256M-Instruct"
 MAXIMUM_IMAGES = 2
+DEFAULT_TIMEOUT: Final[int] = 10  # in seconds
 
 
 @pytest.fixture(scope="module")
@@ -33,7 +36,7 @@ def server():
 @pytest.fixture(scope="module")
 def get_schema(server):
     # avoid generating null (\x00) bytes in strings during test case generation
-    return schemathesis.openapi.from_uri(
+    return schemathesis.specs.openapi.loaders.from_uri(
         f"{server.url_root}/openapi.json",
         generation_config=GenerationConfig(allow_x00=False),
     )
@@ -45,5 +48,13 @@ schema = schemathesis.from_pytest_fixture("get_schema")
 @schema.parametrize()
 @schema.override(headers={"Content-Type": "application/json"})
 async def test_openapi_stateless(case):
-    #No need to verify SSL certificate for localhost
-    await case.call_and_validate(verify=False)
+    key = (
+        case.operation.method.upper(),
+        case.operation.path,
+    )
+    timeout = {
+        ("POST", "/v1/chat/completions"): 30,
+    }.get(key, DEFAULT_TIMEOUT)
+
+    # No need to verify SSL certificate for localhost
+    case.call_and_validate(verify=False, timeout=timeout)
