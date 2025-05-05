0be957f2e86b08da446af264a20ceb65ed3ee353
Luka Govedič <lgovedic@redhat.com>
2025-05-05 19:39:45 +0000

    temp debugging of unittest
    Signed-off-by: Luka Govedič <lgovedic@redhat.com>

--- tests/compile/test_fusion_attn.py
+++ tests/compile/test_fusion_attn.py
@@ -1,4 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
+from copy import deepcopy
+
 import torch.nn
 
 # yapf: disable
@@ -6,6 +8,7 @@ from tests.compile.backend import TestBackend
 from vllm.attention import Attention, AttentionMetadata, get_attn_backend
 from vllm.compilation.fusion_attn import AttnFusionPass
 from vllm.compilation.fx_utils import find_auto_fn
+from vllm.compilation.noop_elimination import NoOpEliminationPass
 from vllm.config import CompilationConfig, VllmConfig, set_current_vllm_config
 from vllm.forward_context import set_forward_context
 from vllm.model_executor.layers.linear import (QKVParallelLinear,
@@ -39,11 +42,21 @@ class TestLayer(torch.nn.Module):
                                         bias=False,
                                         return_bias=False)
 
+        def load_randn_weight(l):
+            l.weight.data = torch.randn_like(l.weight)
+
+        load_randn_weight(self.qkv_proj)
+        # load_randn_weight(self.attn)
+        load_randn_weight(self.o_proj)
+
     def forward(self, input_: torch.Tensor):
-        qkv = self.qkv_proj(input_)
-        q, k, v = qkv.split([self.qkv_size] * 3, dim=-1)
-        out = self.attn(q, k, v)
-        return self.o_proj(out)
+        # qkv = self.qkv_proj(input_)
+        # q, k, v = qkv.split([self.qkv_size] * 3, dim=-1)
+        # out = self.attn(q, k, v)
+        # return self.o_proj(out)
+
+        q, k, v = input_.split([self.qkv_size] * 3, dim=-1)
+        return self.attn(q, k, v)
 
 
 class TestModel(torch.nn.Module):
@@ -68,30 +81,33 @@ PassConfig = CompilationConfig.PassConfig
 
 
 def test_attention_fusion(dist_init):
+    torch.manual_seed(0)
     torch.set_default_device("cuda")
 
-    input = torch.rand(9, 4096)
+    input = torch.rand(9, 4096*3)
 
-    enable_attn_fusion = True
+    enable_attn_fusion = False
     pass_config = PassConfig(enable_attn_fusion=enable_attn_fusion,
                              enable_noop=True)
     compile_config = CompilationConfig(pass_config=pass_config)
     vllm_config = VllmConfig(compilation_config=compile_config)
 
-    head_size = 32
     dtype = torch.bfloat16
-    block_size = 16
+    quant_dtype = current_platform.fp8_dtype()
 
-    attn_backend = get_attn_backend(
-        head_size,
-        dtype,
-        None,
-        block_size,
-        False,
-    )
-    impl = attn_backend.get_impl_cls()(0, 0, 0.0)
-    assert impl.fused_output_quant_supported(current_platform.fp8_dtype(),
-                                             True, False)
+    # head_size = 32
+    # block_size = 16
+    # kvcache_dtype = torch.bfloat16
+    # attn_backend = get_attn_backend(
+    #     head_size,
+    #     dtype,
+    #     None,
+    #     block_size,
+    #     False,
+    # )
+    # impl = attn_backend.get_impl_cls()(0, 0, 0.0, kvcache_dtype)
+    # assert impl.fused_output_quant_supported(quant_dtype,
+    #                                          True, False)
     # TODO use layerwise impls from layers instead
 
     # attn_meta_builder = attn_backend.get_builder_cls()()
@@ -104,21 +120,29 @@ def test_attention_fusion(dist_init):
         multi_modal_placeholder_index_maps=None,
         enable_kv_scales_calculation=False,
     )
-    with set_current_vllm_config(vllm_config), set_forward_context(
-            attn_metadata, vllm_config, num_tokens=9):
+
+    vllm_config0 = deepcopy(vllm_config)
+    with set_current_vllm_config(vllm_config0), set_forward_context(
+            attn_metadata, vllm_config0, num_tokens=9):
         # attention layers set the forward context on config
         model = TestModel(1)
-        out = model(input)
+        out = model(input.clone())
 
-        cc = vllm_config.compilation_config
-        passes = []
-        if enable_attn_fusion:
-            # passes += [NoOpEliminationPass(cc.pass_config)] # TODO
-            passes += [
-                AttnFusionPass(cc.pass_config, cc.static_forward_context)
-            ]
-        inductor_backend = TestBackend(*passes)
-        out2 = torch.compile(model, backend=inductor_backend)(input)
+    # set new config so new attn backends (and kv-caches) are created
+    with set_current_vllm_config(vllm_config), set_forward_context(
+            attn_metadata, vllm_config, num_tokens=9):
+        model2 = TestModel(1)
+
+        attn_dict: dict[str, Attention] = vllm_config.compilation_config.static_forward_context
+        assert all(attn_dict[k].impl.fused_output_quant_supported(quant_dtype, True, False) for k in attn_dict)
+        #
+        # passes = []
+        # if enable_attn_fusion:
+        #     passes += [NoOpEliminationPass(vllm_config)] # TODO
+        #     passes += [AttnFusionPass(vllm_config)]
+        # inductor_backend = TestBackend(*passes)
+        # out2 = torch.compile(model2, backend=inductor_backend)(input)
+        out2 = model2(input)
 
     torch.testing.assert_close(out, out2)
 
