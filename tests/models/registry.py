ca1d9990ea99d2b379ac61eab29c67ec8bdb4167
Stanislaw Wozniak <stw@zurich.ibm.com>
2025-05-05 09:53:09 +0000

    Temporarily marking model as is_available_online=False until it appears in HF Transformers
    Signed-off-by: Stanislaw Wozniak <stw@zurich.ibm.com>

--- tests/models/registry.py
+++ tests/models/registry.py
@@ -163,8 +163,7 @@ _TEXT_GENERATION_EXAMPLE_MODELS = {
                                           {"1b": "EleutherAI/pythia-1.4b"}),
     "GraniteForCausalLM": _HfExamplesInfo("ibm/PowerLM-3b"),
     "GraniteMoeForCausalLM": _HfExamplesInfo("ibm/PowerMoE-3b"),
-    # NOTE: GraniteMoeHybridForCausalLM not yet in HF main, so test_registry_imports would fail
-    #"GraniteMoeHybridForCausalLM": _HfExamplesInfo("ibm-granite/granite-4.0-tiny-preview"),  # noqa: E501
+    "GraniteMoeHybridForCausalLM": _HfExamplesInfo("ibm-granite/granite-4.0-tiny-preview", is_available_online=False),  # noqa: E501
     "GraniteMoeSharedForCausalLM": _HfExamplesInfo("ibm-research/moe-7b-1b-active-shared-experts"),  # noqa: E501
     "Grok1ModelForCausalLM": _HfExamplesInfo("hpcai-tech/grok-1",
                                              trust_remote_code=True),
