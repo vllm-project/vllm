d168cfa77f85ad80648baa5c8a366469a0eeca86
Xiongfei Wei <isaacwxf23@gmail.com>
2025-05-05 17:20:36 +0000

    overwrite simple_compile_backend
    Signed-off-by: Xiongfei Wei <isaacwxf23@gmail.com>

--- vllm/model_executor/models/commandr.py
+++ vllm/model_executor/models/commandr.py
@@ -54,7 +54,7 @@ from .utils import (extract_layer_index, is_pp_missing_parameter,
                     maybe_prefix)
 
 
-@torch.compile(backend=current_platform.get_simple_compile_backend())
+@torch.compile(backend=current_platform.simple_compile_backend)
 def layer_norm_func(hidden_states, weight, variance_epsilon):
     input_dtype = hidden_states.dtype
     hidden_states = hidden_states.to(torch.float32)
