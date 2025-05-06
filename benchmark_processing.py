78c833ee08d4eba0e85dfb2f5933321ef4ec4936
DarkLight1337 <tlleungac@connect.ust.hk>
2025-05-06 07:36:19 +0000

    Separate different models completely
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- benchmark_processing.py
+++ benchmark_processing.py
@@ -3,7 +3,6 @@ import argparse
 import asyncio
 import csv
 import time
-import traceback
 from datetime import datetime
 from pathlib import Path
 from typing import Literal, Union, get_args
@@ -16,6 +15,7 @@ import vllm.envs as envs
 from tests.multimodal.utils import random_audio, random_image, random_video
 from vllm import AsyncEngineArgs, EngineArgs, SamplingParams
 from vllm.config import ModelConfig, ParallelProcessorBackend
+from vllm.distributed import cleanup_dist_env_and_memory
 from vllm.engine.async_llm_engine import AsyncLLMEngine
 from vllm.engine.llm_engine import LLMEngine
 from vllm.engine.protocol import EngineClient
@@ -79,45 +79,86 @@ def async_engine_from_args(engine_args: AsyncEngineArgs) -> EngineClient:
     )
 
 
+_engine = None
+
+
+def _reset_engine():
+    global _engine
+
+    _engine = None
+
+    cleanup_dist_env_and_memory()
+
+    time.sleep(10)
+
+
 def get_engine(
     model_id: str,
-    parallel_backend: ParallelProcessorBackend,
     modalities: list[ModalityStr],
+    parallel_backend: ParallelProcessorBackend,
 ) -> LLMEngine:
-    args = EngineArgs(
-        model=model_id,
-        trust_remote_code=True,
-        parallel_processor_backend=parallel_backend,
-        limit_mm_per_prompt={
-            m: m in modalities
-            for m in get_args(ModalityStr)
-        },
-        disable_mm_preprocessor_cache=True,
-        enforce_eager=True,
-    )
+    global _engine
 
-    return engine_from_args(args)
+    if _engine is None or parallel_backend == "mt":
+        args = EngineArgs(
+            model=model_id,
+            trust_remote_code=True,
+            parallel_processor_backend=parallel_backend,
+            limit_mm_per_prompt={
+                m: m in modalities
+                for m in get_args(ModalityStr)
+            },
+            disable_mm_preprocessor_cache=True,
+            enforce_eager=True,
+        )
+
+        if _engine is not None:
+            _reset_engine()
+
+        engine = engine_from_args(args)
+        _engine = engine
+    else:
+        model_config = _engine.get_model_config()
+        model_config.parallel_processor_backend = parallel_backend
+        model_config.multimodal_config.parallel_processor_backend = \
+            parallel_backend
+
+    return _engine
 
 
-def get_async_engine(
+async def get_async_engine(
     model_id: str,
-    parallel_backend: ParallelProcessorBackend,
     modalities: list[ModalityStr],
+    parallel_backend: ParallelProcessorBackend,
 ) -> EngineClient:
-    args = AsyncEngineArgs(
-        model=model_id,
-        trust_remote_code=True,
-        parallel_processor_backend=parallel_backend,
-        limit_mm_per_prompt={
-            m: m in modalities
-            for m in get_args(ModalityStr)
-        },
-        disable_mm_preprocessor_cache=True,
-        enforce_eager=True,
-        disable_log_requests=True,
-    )
+    global _engine
 
-    return async_engine_from_args(args)
+    if _engine is None or parallel_backend == "mt":
+        args = AsyncEngineArgs(
+            model=model_id,
+            trust_remote_code=True,
+            parallel_processor_backend=parallel_backend,
+            limit_mm_per_prompt={
+                m: m in modalities
+                for m in get_args(ModalityStr)
+            },
+            disable_mm_preprocessor_cache=True,
+            enforce_eager=True,
+            disable_log_requests=True,
+        )
+
+        if _engine is not None:
+            _reset_engine()
+
+        engine = async_engine_from_args(args)
+        _engine = engine
+    else:
+        model_config = await _engine.get_model_config()
+        model_config.parallel_processor_backend = parallel_backend
+        model_config.multimodal_config.parallel_processor_backend = \
+            parallel_backend
+
+    return _engine
 
 
 def get_prompt(model_config: ModelConfig, modality: ModalityStr) -> str:
@@ -219,20 +260,21 @@ def get_benchmark_mm_data(
     return {m: get_benchmark_data(m) for m in modalities}
 
 
-def benchmark(
+def benchmark_run(
+    engine: LLMEngine,
+    model_config: ModelConfig,
     model_id: str,
     parallel_backend: ParallelProcessorBackend,
-    engine: LLMEngine,
     modality: ModalityStr,
     data: list,
-) -> float:
-    model_config = engine.get_model_config()
+):
     prompt = get_prompt(model_config, modality)
 
     start_s = time.monotonic()
     progbar = tqdm(
         [None] * len(data),
-        desc=f"Benchmarking {model_id=}, {parallel_backend=}, {modality=}",
+        desc=f"Benchmarking {model_id=}, {parallel_backend=}, "
+        f"{modality=}",
     )
 
     def _chat(idx: int, item):
@@ -254,24 +296,24 @@ def benchmark(
         _chat(*args)
 
     total_s = (time.monotonic() - start_s)
-
     return total_s / len(data)
 
 
-async def benchmark_async(
+async def benchmark_run_async(
+    engine: EngineClient,
+    model_config: ModelConfig,
     model_id: str,
     parallel_backend: ParallelProcessorBackend,
-    engine: EngineClient,
     modality: ModalityStr,
     data: list,
-) -> float:
-    model_config = await engine.get_model_config()
+):
     prompt = get_prompt(model_config, modality)
 
     start_s = time.monotonic()
     progbar = tqdm(
         [None] * len(data),
-        desc=f"Benchmarking {model_id=}, {parallel_backend=}, {modality=}",
+        desc=f"Benchmarking {model_id=}, {parallel_backend=}, "
+        f"{modality=}",
     )
 
     async def _chat(idx: int, item):
@@ -292,116 +334,69 @@ async def benchmark_async(
     await asyncio.gather(*(_chat(*args) for args in enumerate(data)))
 
     total_s = (time.monotonic() - start_s)
-
     return total_s / len(data)
 
 
-def benchmark_mm(
+def benchmark(
     model_id: str,
-    parallel_backend: ParallelProcessorBackend,
-) -> dict[str, float]:
-    modalities = get_supported_modalities(model_id)
-    engine = get_engine(model_id, parallel_backend, modalities)
-    mm_data = get_benchmark_mm_data(modalities)
+    parallel_backends: list[ParallelProcessorBackend],
+) -> dict[ParallelProcessorBackend, dict[ModalityStr, float]]:
+    supported_modalities = get_supported_modalities(model_id)
+    mm_data = get_benchmark_mm_data(supported_modalities)
 
-    return {
-        m: benchmark(model_id, parallel_backend, engine, m, data)
-        for m, data in mm_data.items()
-    }
+    all_results = dict[ParallelProcessorBackend, dict[ModalityStr, float]]()
+    for parallel_backend in parallel_backends:
+        engine = get_engine(model_id, supported_modalities, parallel_backend)
+        model_config = engine.get_model_config()
+
+        parallel_results = dict[ModalityStr, float]()
+        for modality, data in mm_data.items():
+            parallel_results[modality] = benchmark_run(
+                engine,
+                model_config,
+                model_id,
+                parallel_backend,
+                modality,
+                data,
+            )
+
+        all_results[parallel_backend] = parallel_results
+        del engine
+
+    return all_results
 
 
-async def benchmark_mm_async(
+async def benchmark_async(
     model_id: str,
-    parallel_backend: ParallelProcessorBackend,
-) -> dict[str, float]:
-    modalities = get_supported_modalities(model_id)
-    engine = get_async_engine(model_id, parallel_backend, modalities)
-    mm_data = get_benchmark_mm_data(modalities)
+    parallel_backends: list[ParallelProcessorBackend],
+) -> dict[ParallelProcessorBackend, dict[ModalityStr, float]]:
+    supported_modalities = get_supported_modalities(model_id)
+    mm_data = get_benchmark_mm_data(supported_modalities)
 
-    return {
-        m: await benchmark_async(model_id, parallel_backend, engine, m, data)
-        for m, data in mm_data.items()
-    }
+    all_results = dict[ParallelProcessorBackend, dict[ModalityStr, float]]()
+    for parallel_backend in parallel_backends:
+        engine = await get_async_engine(model_id, supported_modalities,
+                                        parallel_backend)
+        model_config = await engine.get_model_config()
 
+        parallel_results = dict[ModalityStr, float]()
+        for modality, data in mm_data.items():
+            parallel_results[modality] = await benchmark_run_async(
+                engine,
+                model_config,
+                model_id,
+                parallel_backend,
+                modality,
+                data,
+            )
 
-def benchmark_one(
-    model_id: str,
-    parallel_backend: ParallelProcessorBackend,
-) -> dict[str, float]:
-    return benchmark_mm(
-        model_id,
-        parallel_backend,
-    )
+        all_results[parallel_backend] = parallel_results
+        del engine
 
-
-async def benchmark_one_async(
-    model_id: str,
-    parallel_backend: ParallelProcessorBackend,
-) -> dict[str, float]:
-    return await benchmark_mm_async(
-        model_id,
-        parallel_backend,
-    )
+    return all_results
 
 
-# Some models OOM or don't have chat template
-MODELS = [
-    # "rhymes-ai/Aria",
-    "CohereForAI/aya-vision-8b",
-    # "Salesforce/blip2-opt-6.7b",
-    # "facebook/chameleon-7b",
-    # "deepseek-ai/deepseek-vl2-tiny",
-    # "microsoft/Florence-2-base",
-    # "adept/fuyu-8b",
-    "google/gemma-3-4b-it",
-    # "THUDM/glm-4v-9b",
-    # "ibm-granite/granite-speech-3.3-8b",
-    "h2oai/h2ovl-mississippi-800m",
-    "OpenGVLab/InternVL2-1B",
-    "HuggingFaceTB/SmolVLM-256M-Instruct",
-    "HuggingFaceTB/SmolVLM2-2.2B-Instruct",
-    # "moonshotai/Kimi-VL-A3B-Instruct",
-    # "meta-llama/Llama-4-Scout-17B-16E-Instruct",
-    "llava-hf/llava-1.5-7b-hf",
-    "llava-hf/llava-v1.6-mistral-7b-hf",
-    "llava-hf/LLaVA-NeXT-Video-7B-hf",
-    "llava-hf/llava-onevision-qwen2-0.5b-ov-hf",
-    # "meta-llama/Llama-3.2-11B-Vision-Instruct",
-    # "TIGER-Lab/Mantis-8B-siglip-llama3",
-    "openbmb/MiniCPM-Llama3-V-2_5",
-    "openbmb/MiniCPM-o-2_6",
-    "openbmb/MiniCPM-V-2_6",
-    # "MiniMaxAI/MiniMax-VL-01",
-    # "allenai/Molmo-7B-D-0924",
-    # "allenai/Molmo-7B-O-0924",
-    # "nvidia/NVLM-D-72B",
-    "AIDC-AI/Ovis2-1B",
-    # "google/paligemma-3b-mix-224",
-    # "google/paligemma2-3b-ft-docci-448",
-    # "microsoft/Phi-3.5-vision-instruct",
-    "microsoft/Phi-4-multimodal-instruct",
-    # "mistralai/Pixtral-12B-2409",
-    # "mistral-community/pixtral-12b",
-    "Qwen/Qwen-VL-Chat",
-    "Qwen/Qwen2-VL-2B-Instruct",
-    "Qwen/Qwen2.5-VL-3B-Instruct",
-    "Qwen/Qwen2-Audio-7B-Instruct",
-    # "Qwen/Qwen2.5-Omni-7B",
-    # "Skywork/Skywork-R1V-38B",
-    "fixie-ai/ultravox-v0_5-llama-3_2-1b",
-    "openai/whisper-large-v3",
-]
-
-
-def _resolve_args(
-    model_id: Union[str, Literal["all"]],
-    parallel_backend: Literal[ParallelProcessorBackend, "all"],
-    output_dir: str,
-):
-    models = MODELS if model_id == "all" else [model_id]
-    parallel_backends = (("uni", "mp", "mt") if parallel_backend == "all" else
-                         (parallel_backend, ))
-
+def resolve_output_path(output_dir: str):
     output_path = Path(output_dir)
     if output_path.exists():
         if not output_path.is_dir():
@@ -409,144 +404,116 @@ def _resolve_args(
     else:
         output_path.mkdir()
 
-    print("[Models]")
-    print("\n".join("- " + model for model in models))
+    return output_path
 
-    print("[Parallel Backends]")
-    print("\n".join("- " + backend for backend in parallel_backends))
 
-    return models, parallel_backends, output_path
+def save_results(
+    all_results: dict[ParallelProcessorBackend, dict[ModalityStr, float]],
+    model_id: str,
+    output_path: Path,
+    append: bool,
+):
+    timestamp = int(datetime.now().timestamp())
+
+    if append:
+        csv_filepaths = sorted(output_path.glob("processing_*.csv"))
+        if csv_filepaths:
+            prev_filepath = csv_filepaths[-1]
+            with prev_filepath.open("r") as f:
+                csv_reader = csv.DictReader(f)
+                prev_rows = list(csv_reader)
+        else:
+            prev_rows = []
+    else:
+        prev_rows = []
+
+    csv_filepath = output_path / f"processing_{timestamp}.csv"
+
+    with csv_filepath.open("w") as f:
+        csv_writer = csv.DictWriter(
+            f,
+            fieldnames=[
+                "model_id",
+                "parallel_backend",
+                "ms_audio",
+                "ms_image",
+                "ms_video",
+            ],
+        )
+        csv_writer.writeheader()
+
+        for row in prev_rows:
+            csv_writer.writerow(row)
+
+        for backend, parallel_results in all_results.items():
+            csv_writer.writerow({
+                "model_id": model_id,
+                "parallel_backend": backend,
+                **{
+                    f"ms_{modality}": avg_s * 1000
+                    for modality, avg_s in parallel_results.items()
+                },
+            })
+
+    print(f"Saved results to: {csv_filepath}")
+
+    if append:
+        json_filepaths = sorted(output_path.glob("processing_*.json"))
+        if json_filepaths:
+            json_filepath = json_filepaths[-1]
+        else:
+            json_filepath = output_path / f"processing_{timestamp}.json"
+    else:
+        json_filepath = output_path / f"processing_{timestamp}.json"
+
+    pd.read_csv(csv_filepath).to_json(json_filepath,
+                                      orient="records",
+                                      indent=4)
+
+    print(f"Saved results to: {json_filepath}")
 
 
 def main(
     model_id: Union[str, Literal["all"]],
-    parallel_backend: Literal[ParallelProcessorBackend, "all"],
+    parallel_backends: list[ParallelProcessorBackend],
     output_dir: str,
+    append: bool,
 ) -> None:
-    models, parallel_backends, output_path = _resolve_args(
-        model_id,
-        parallel_backend,
-        output_dir,
-    )
+    output_path = resolve_output_path(output_dir)
 
-    timestamp = int(datetime.now().timestamp())
-    csv_filepath = output_path / f"processing_{timestamp}.csv"
+    all_results = benchmark(model_id, parallel_backends)
 
-    try:
-        with csv_filepath.open("w") as f:
-            csv_writer = csv.DictWriter(
-                f,
-                fieldnames=[
-                    "model_id",
-                    "parallel_backend",
-                    "ms_audio",
-                    "ms_image",
-                    "ms_video",
-                ],
-            )
-            csv_writer.writeheader()
-
-            for model in models:
-                for backend in parallel_backends:
-                    try:
-                        res_one = benchmark_one(model, backend)
-                    except Exception as e:
-                        print(f"Failed to benchmark {model=}, {backend=}")
-                        traceback.print_exception(e)
-                        continue
-
-                    csv_writer.writerow({
-                        "model_id": model,
-                        "parallel_backend": backend,
-                        **{
-                            f"ms_{k}": v * 1000
-                            for k, v in res_one.items()
-                        },
-                    })
-    finally:
-        print(f"Saved results to: {csv_filepath}")
-
-        json_filepath = output_path / f"processing_{timestamp}.json"
-
-        pd.read_csv(csv_filepath).to_json(json_filepath,
-                                          orient="records",
-                                          indent=4)
-
-        print(f"Saved results to: {json_filepath}")
+    save_results(all_results, model_id, output_path, append=append)
 
 
 async def main_async(
     model_id: Union[str, Literal["all"]],
-    parallel_backend: Literal[ParallelProcessorBackend, "all"],
+    parallel_backends: list[ParallelProcessorBackend],
     output_dir: str,
+    append: bool,
 ) -> None:
-    models, parallel_backends, output_path = _resolve_args(
-        model_id,
-        parallel_backend,
-        output_dir,
-    )
+    output_path = resolve_output_path(output_dir)
 
-    timestamp = int(datetime.now().timestamp())
-    csv_filepath = output_path / f"processing_{timestamp}.csv"
+    all_results = await benchmark_async(model_id, parallel_backends)
 
-    try:
-        with csv_filepath.open("w") as f:
-            csv_writer = csv.DictWriter(
-                f,
-                fieldnames=[
-                    "model_id",
-                    "parallel_backend",
-                    "ms_audio",
-                    "ms_image",
-                    "ms_video",
-                ],
-            )
-            csv_writer.writeheader()
-
-            for model in models:
-                for backend in parallel_backends:
-                    try:
-                        res_one = await benchmark_one_async(model, backend)
-                    except Exception as e:
-                        print(f"Failed to benchmark {model=}, {backend=}")
-                        traceback.print_exception(e)
-                        continue
-
-                    csv_writer.writerow({
-                        "model_id": model,
-                        "parallel_backend": backend,
-                        **{
-                            f"ms_{k}": v * 1000
-                            for k, v in res_one.items()
-                        },
-                    })
-    finally:
-        print(f"Saved results to: {csv_filepath}")
-
-        json_filepath = output_path / f"processing_{timestamp}.json"
-
-        pd.read_csv(csv_filepath).to_json(json_filepath,
-                                          orient="records",
-                                          indent=4)
-
-        print(f"Saved results to: {json_filepath}")
+    save_results(all_results, model_id, output_path, append=append)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Benchmark parallel multi-modal processing")
-    parser.add_argument(
-        "-m",
-        "--model",
-        type=str,
-        default="HuggingFaceTB/SmolVLM-256M-Instruct",
-        help="Name of the model, or 'all' to select all MODELS")
-    parser.add_argument("-p",
-                        "--parallel-backend",
+    parser.add_argument("-m",
+                        "--model",
                         type=str,
-                        choices=["uni", "mp", "mt", "all"],
-                        default="all",
-                        help="Parallel backend to use, or 'all' to select all")
+                        default="HuggingFaceTB/SmolVLM-256M-Instruct",
+                        help="Name of the model")
+    parser.add_argument("-p",
+                        "--parallel-backends",
+                        type=str,
+                        nargs="+",
+                        choices=["uni", "mp", "mt"],
+                        default=["uni", "mp", "mt"],
+                        help="Parallel backends to test")
     parser.add_argument("-o",
                         "--output-dir",
                         type=str,
@@ -555,11 +522,20 @@ if __name__ == "__main__":
     parser.add_argument("--sync",
                         action="store_true",
                         help="Test the sync engine instead of async engine")
+    parser.add_argument("--append",
+                        action="store_true",
+                        help="Append to results file if it exists")
 
     args = parser.parse_args()
 
     if args.sync:
-        main(args.model, args.parallel_backend, args.output_dir)
+        main(args.model,
+             args.parallel_backends,
+             args.output_dir,
+             append=args.append)
     else:
-        coro = main_async(args.model, args.parallel_backend, args.output_dir)
+        coro = main_async(args.model,
+                          args.parallel_backends,
+                          args.output_dir,
+                          append=args.append)
         asyncio.run(coro)
