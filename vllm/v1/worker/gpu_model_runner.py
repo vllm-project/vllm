60737f54413cfd97d3513139a6f824509f1d6065
RobMcH <robert@poolside.ai>
2025-05-06 17:39:13 +0200

    add flag for post processing logprobs
    Signed-off-by: RobMcH <robert@poolside.ai>

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -155,7 +155,8 @@ class GPUModelRunner(LoRAModelRunnerMixin):
         self.encoder_cache_size = encoder_cache_size
 
         # Sampler
-        self.sampler = Sampler()
+        self.sampler = Sampler(
+            post_process_logprobs=self.model_config.post_process_logprobs)
 
         # Lazy initialization
         # self.model: nn.Module  # Set after load_model
