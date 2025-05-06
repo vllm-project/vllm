423c0befc60cbddf74f933444e9f7957ef912439
vllmellm <vllm.ellm@embeddedllm.com>
2025-05-06 16:47:33 +0800

    Update vllm/platforms/rocm.py
    Co-authored-by: Hongxia Yang <62075498+hongxiayang@users.noreply.github.com>

--- vllm/platforms/rocm.py
+++ vllm/platforms/rocm.py
@@ -173,11 +173,11 @@ class RocmPlatform(Platform):
                     if use_v1:
                         logger.info("Using AITER MLA backend on V1 engine.")
                         logger.warning(
-                            "Increasing the model execution timeout"
-                            "using the VLLM_ROCM_EXECUTE_MODEL_TIMEOUT"
-                            "environment variable is recommended"
-                            "if timeout error is encountered"
-                            "when running %s"
+                            "Increasing the model execution timeout "
+                            "using the VLLM_ROCM_EXECUTE_MODEL_TIMEOUT "
+                            "environment variable is recommended "
+                            "if timeout error is encountered "
+                            "when running %s "
                             "backend on V1 engine.", selected_backend)
                         return "vllm.v1.attention.backends.mla.rocm_aiter_mla.AiterMLABackend"  # noqa: E501
                     else:
