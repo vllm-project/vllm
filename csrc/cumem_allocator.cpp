768ae9e1721e6180a8757aff6ad656fb8215237f
Yang Wang <wyatuestc@gmail.com>
2025-05-06 14:17:34 +0800

    reduce memory chunk size
    reduce memory chunk size to reduce rocm hang and save more memory.
    
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- csrc/cumem_allocator.cpp
+++ csrc/cumem_allocator.cpp
@@ -8,7 +8,7 @@
 #ifndef USE_ROCM
 static const char* PYARGS_PARSE = "KKKK";
 #else
-  #define MEMCREATE_CHUNK_SIZE (512 * 1024 * 1024)
+  #define MEMCREATE_CHUNK_SIZE (2 * 1024 * 1024)
   #define MIN(a, b) (a < b ? a : b)
 
 static const char* PYARGS_PARSE = "KKKO";
