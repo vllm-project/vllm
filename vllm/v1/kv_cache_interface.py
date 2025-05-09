e999b50dac9f4dd7e89baa26460c04e301ab0264
Chen Zhang <zhangch99@outlook.com>
2025-05-09 08:15:38 -0700

    revert
    Signed-off-by: Chen Zhang <zhangch99@outlook.com>

--- vllm/v1/kv_cache_interface.py
+++ vllm/v1/kv_cache_interface.py
@@ -74,7 +74,7 @@ class FullAttentionSpec(AttentionSpec):
 
     @property
     def type_id(self) -> str:
-        return f"full_attention_{self.num_kv_heads}_{self.block_size}_{self.page_size_bytes}"  # noqa
+        return f"full_attention_{self.block_size}_{self.page_size_bytes}"
 
     def max_memory_usage_bytes(self, vllm_config: VllmConfig) -> int:
         max_model_len = vllm_config.model_config.max_model_len
@@ -90,7 +90,7 @@ class SlidingWindowSpec(AttentionSpec):
 
     @property
     def type_id(self) -> str:
-        return f"sliding_window_{self.sliding_window}_{self.num_kv_heads}_{self.block_size}_{self.page_size_bytes}"  # noqa
+        return f"sliding_window_{self.sliding_window}_{self.block_size}_{self.page_size_bytes}"  # noqa
 
     def max_memory_usage_bytes(self, vllm_config: VllmConfig) -> int:
         max_model_len = vllm_config.model_config.max_model_len
