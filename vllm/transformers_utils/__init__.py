01e7e7b600e43b2b942de2cc485930ba2208e6d9
Jee Jee Li <pandaleefree@gmail.com>
2025-05-12 18:46:45 +0800

    [Misc] Improve modelscope  import error  (#17983)
    Signed-off-by: Jee Jee Li <pandaleefree@gmail.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/transformers_utils/__init__.py
+++ vllm/transformers_utils/__init__.py
@@ -3,17 +3,21 @@
 from vllm.envs import VLLM_USE_MODELSCOPE
 
 if VLLM_USE_MODELSCOPE:
-    # Patch here, before each import happens
-    import modelscope
-    from packaging import version
+    try:
+        # Patch here, before each import happens
+        import modelscope
+        from packaging import version
 
-    # patch_hub begins from modelscope>=1.18.1
-    if version.parse(modelscope.__version__) <= version.parse('1.18.0'):
+        # patch_hub begins from modelscope>=1.18.1
+        if version.parse(modelscope.__version__) <= version.parse('1.18.0'):
+            raise ImportError(
+                'Using vLLM with ModelScope needs modelscope>=1.18.1, please '
+                'install by `pip install modelscope -U`')
+        from modelscope.utils.hf_util import patch_hub
+
+        # Patch hub to download models from modelscope to speed up.
+        patch_hub()
+    except ImportError as err:
         raise ImportError(
-            'Using vLLM with ModelScope needs modelscope>=1.18.1, please '
-            'install by `pip install modelscope -U`')
-
-    from modelscope.utils.hf_util import patch_hub
-
-    # Patch hub to download models from modelscope to speed up.
-    patch_hub()
+            "Please install modelscope>=1.18.1 via "
+            "`pip install modelscope>=1.18.1` to use ModelScope.") from err
