42b1ad012bf244f228b6303cf2902607e4cfac85
rshaw@neuralmagic.com <robertgshaw2@gmail.com>
2025-05-12 14:24:26 +0000

    updated
    Signed-off-by: rshaw@neuralmagic.com <robertgshaw2@gmail.com>

--- tests/v1/kv_connector/unit/test_nixl_connector.py
+++ tests/v1/kv_connector/unit/test_nixl_connector.py
@@ -6,7 +6,8 @@ from vllm.distributed.kv_transfer.kv_connector.v1.nixl_connector import (
 from .utils import create_request, create_scheduler, create_vllm_config
 
 
-def test_scheduler_worker_inferface():
+def test_basic_inferface():
+    """Unit test for basic NixlConnector interface functionality."""
 
     vllm_config = create_vllm_config()
     scheduler = create_scheduler(vllm_config)
@@ -37,3 +38,36 @@ def test_scheduler_worker_inferface():
             req_meta.local_block_ids, scheduler.kv_cache_manager.
             single_type_manager.req_to_blocks[request_id]):
         assert block_id == block.block_id
+
+
+def test_prompt_less_than_block_size():
+    """
+    Test that we can handle case where prompt is < block.
+
+    In this case, the P worker will send empty remote_block_ids.
+    The D worker should not schedule an async read in this case,
+    since there is nothing to pull.
+    """
+    vllm_config = create_vllm_config()
+    scheduler = create_scheduler(vllm_config)
+
+    # Half of a block.
+    BLOCK_SIZE = vllm_config.cache_config.block_size
+    NUM_TOKENS = int(BLOCK_SIZE * 0.5)
+
+    # Request will have 0 remote blocks.
+    request = create_request(request_id=1,
+                             num_tokens=NUM_TOKENS,
+                             do_remote_prefill=True,
+                             num_remote_blocks=0)
+    scheduler.add_request(request)
+    scheduler_output = scheduler.schedule()
+
+    # This request should not have to read async.
+    kv_connector_metadata = scheduler_output.kv_connector_metadata
+    assert kv_connector_metadata is not None
+    assert isinstance(kv_connector_metadata, NixlConnectorMetadata)
+    assert len(kv_connector_metadata.requests) == 0
+
+    # This request should be scheduled regularly.
+    assert len(scheduler_output.scheduled_new_reqs) == 1
