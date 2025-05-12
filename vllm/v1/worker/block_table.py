caa7b43aa9b1b21f4df605c89ed1ce91aed2b99d
Pooya Moradi <pooyam@google.com>
2025-05-12 22:48:17 +0000

    Hack vllm to use jax block table

--- vllm/v1/worker/block_table.py
+++ vllm/v1/worker/block_table.py
@@ -7,92 +7,92 @@ from vllm.logger import init_logger
 
 logger = init_logger(__name__)
 
+from vllm.v1.worker.block_table_jax import BlockTableJax as BlockTable
+# class BlockTable:
 
-class BlockTable:
+#     def __init__(
+#         self,
+#         max_num_reqs: int,
+#         max_num_blocks_per_req: int,
+#         max_num_batched_tokens: int,
+#         pin_memory: bool,
+#         device: torch.device,
+#     ):
+#         self.max_num_reqs = max_num_reqs
+#         self.max_num_blocks_per_req = max_num_blocks_per_req
+#         self.max_num_batched_tokens = max_num_batched_tokens
+#         self.pin_memory = pin_memory
+#         self.device = device
 
-    def __init__(
-        self,
-        max_num_reqs: int,
-        max_num_blocks_per_req: int,
-        max_num_batched_tokens: int,
-        pin_memory: bool,
-        device: torch.device,
-    ):
-        self.max_num_reqs = max_num_reqs
-        self.max_num_blocks_per_req = max_num_blocks_per_req
-        self.max_num_batched_tokens = max_num_batched_tokens
-        self.pin_memory = pin_memory
-        self.device = device
+#         self.block_table = torch.zeros(
+#             (max_num_reqs, max_num_blocks_per_req),
+#             device=self.device,
+#             dtype=torch.int32,
+#         )
+#         self.block_table_cpu = torch.zeros(
+#             (max_num_reqs, max_num_blocks_per_req),
+#             device="cpu",
+#             dtype=torch.int32,
+#             pin_memory=pin_memory,
+#         )
+#         self.block_table_np = self.block_table_cpu.numpy()
+#         self.num_blocks_per_row = np.zeros(max_num_reqs, dtype=np.int32)
 
-        self.block_table = torch.zeros(
-            (max_num_reqs, max_num_blocks_per_req),
-            device=self.device,
-            dtype=torch.int32,
-        )
-        self.block_table_cpu = torch.zeros(
-            (max_num_reqs, max_num_blocks_per_req),
-            device="cpu",
-            dtype=torch.int32,
-            pin_memory=pin_memory,
-        )
-        self.block_table_np = self.block_table_cpu.numpy()
-        self.num_blocks_per_row = np.zeros(max_num_reqs, dtype=np.int32)
+#         self.slot_mapping_cpu = torch.zeros(self.max_num_batched_tokens,
+#                                             dtype=torch.int64,
+#                                             device="cpu",
+#                                             pin_memory=self.pin_memory)
+#         self.slot_mapping_np = self.slot_mapping_cpu.numpy()
+#         self.slot_mapping = torch.zeros(self.max_num_batched_tokens,
+#                                         dtype=torch.int64,
+#                                         device=self.device)
 
-        self.slot_mapping_cpu = torch.zeros(self.max_num_batched_tokens,
-                                            dtype=torch.int64,
-                                            device="cpu",
-                                            pin_memory=self.pin_memory)
-        self.slot_mapping_np = self.slot_mapping_cpu.numpy()
-        self.slot_mapping = torch.zeros(self.max_num_batched_tokens,
-                                        dtype=torch.int64,
-                                        device=self.device)
+#     def append_row(
+#         self,
+#         block_ids: list[int],
+#         row_idx: int,
+#     ) -> None:
+#         if not block_ids:
+#             return
+#         num_blocks = len(block_ids)
+#         start = self.num_blocks_per_row[row_idx]
+#         self.num_blocks_per_row[row_idx] += num_blocks
+#         self.block_table_np[row_idx, start:start + num_blocks] = block_ids
 
-    def append_row(
-        self,
-        block_ids: list[int],
-        row_idx: int,
-    ) -> None:
-        if not block_ids:
-            return
-        num_blocks = len(block_ids)
-        start = self.num_blocks_per_row[row_idx]
-        self.num_blocks_per_row[row_idx] += num_blocks
-        self.block_table_np[row_idx, start:start + num_blocks] = block_ids
+#     def add_row(self, block_ids: list[int], row_idx: int) -> None:
+#         self.num_blocks_per_row[row_idx] = 0
+#         self.append_row(block_ids, row_idx)
 
-    def add_row(self, block_ids: list[int], row_idx: int) -> None:
-        self.num_blocks_per_row[row_idx] = 0
-        self.append_row(block_ids, row_idx)
+#     def move_row(self, src: int, tgt: int) -> None:
+#         num_blocks = self.num_blocks_per_row[src]
+#         self.block_table_np[tgt, :num_blocks] = self.block_table_np[
+#             src, :num_blocks]
+#         self.num_blocks_per_row[tgt] = num_blocks
 
-    def move_row(self, src: int, tgt: int) -> None:
-        num_blocks = self.num_blocks_per_row[src]
-        self.block_table_np[tgt, :num_blocks] = self.block_table_np[
-            src, :num_blocks]
-        self.num_blocks_per_row[tgt] = num_blocks
+#     def swap_row(self, src: int, tgt: int) -> None:
+#         num_blocks_src = self.num_blocks_per_row[src]
+#         num_blocks_tgt = self.num_blocks_per_row[tgt]
+#         self.num_blocks_per_row[src] = num_blocks_tgt
+#         self.num_blocks_per_row[tgt] = num_blocks_src
 
-    def swap_row(self, src: int, tgt: int) -> None:
-        num_blocks_src = self.num_blocks_per_row[src]
-        num_blocks_tgt = self.num_blocks_per_row[tgt]
-        self.num_blocks_per_row[src] = num_blocks_tgt
-        self.num_blocks_per_row[tgt] = num_blocks_src
+#         self.block_table_np[[src, tgt]] = self.block_table_np[[tgt, src]]
 
-        self.block_table_np[[src, tgt]] = self.block_table_np[[tgt, src]]
+#     def commit(self, num_reqs: int) -> None:
+#         self.block_table[:num_reqs].copy_(self.block_table_cpu[:num_reqs],
+#                                           non_blocking=True)
 
-    def commit(self, num_reqs: int) -> None:
-        self.block_table[:num_reqs].copy_(self.block_table_cpu[:num_reqs],
-                                          non_blocking=True)
+#     def clear(self) -> None:
+#         self.block_table.fill_(0)
+#         self.block_table_cpu.fill_(0)
 
-    def clear(self) -> None:
-        self.block_table.fill_(0)
-        self.block_table_cpu.fill_(0)
+#     def get_device_tensor(self) -> torch.Tensor:
+#         """Ruturns the device tensor of the block table."""
+#         return self.block_table
 
-    def get_device_tensor(self) -> torch.Tensor:
-        """Ruturns the device tensor of the block table."""
-        return self.block_table
+#     def get_cpu_tensor(self) -> torch.Tensor:
+#         """Returns the CPU tensor of the block table."""
+#         return self.block_table_cpu
 
-    def get_cpu_tensor(self) -> torch.Tensor:
-        """Returns the CPU tensor of the block table."""
-        return self.block_table_cpu
-
-    def get_numpy_array(self) -> np.ndarray:
-        """Returns the numpy array of the block table."""
-        return self.block_table_np
+#     def get_numpy_array(self) -> np.ndarray:
+#         """Returns the numpy array of the block table."""
+#         return self.block_table_np
