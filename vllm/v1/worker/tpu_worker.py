01c26b248e6b240f09cae3c7e0329abab1421fae
Siyuan Liu <lsiyuan@google.com>
2025-05-13 22:28:26 +0000

    import workers from tpu commons
    Signed-off-by: Siyuan Liu <lsiyuan@google.com>

--- vllm/v1/worker/tpu_worker.py
+++ vllm/v1/worker/tpu_worker.py
@@ -1,6 +1,4 @@
 # SPDX-License-Identifier: Apache-2.0
 """A TPU worker class."""
 
-import tpu_commons.worker.tpu_torch_xla_worker as tpu_torch_xla_worker
-
-TPUWorker = tpu_torch_xla_worker.TPUWorker
+from tpu_commons.worker import TPUWorker  # noqa: F401
