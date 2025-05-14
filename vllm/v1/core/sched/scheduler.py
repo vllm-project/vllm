d9a95d69a7a00dcebb214d5c2d6517789f1ed165
rshaw@neuralmagic.com <robertgshaw2@gmail.com>
2025-05-14 19:54:34 +0000

    updated
    Signed-off-by: rshaw@neuralmagic.com <robertgshaw2@gmail.com>

--- vllm/v1/core/sched/scheduler.py
+++ vllm/v1/core/sched/scheduler.py
@@ -842,11 +842,9 @@ class Scheduler(SchedulerInterface):
             # Request is already finished.
             if request is None:
                 # If unfreed kv req_ids free the blocks.
-                should_free_on_abort = (
-                    self.connector is None
-                    or self.connector.should_free_pending_on_abort())
-                if (req_id in self.pending_kv_free_req_ids
-                        and should_free_on_abort):
+                should_free = (self.connector is None or
+                               self.connector.should_free_pending_on_abort())
+                if (req_id in self.pending_kv_free_req_ids and should_free):
                     self.pending_kv_free_req_ids.remove(req_id)
                     self._free_blocks(req_id)
                 continue
