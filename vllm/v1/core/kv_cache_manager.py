c5c3f67c30551b44c71cc83363c79498742ac926
Chen Zhang <zhangch99@outlook.com>
2025-05-14 10:09:39 +0800

    [v1][KVCacheManager] pass num_new_computed_tokens to kv cache manager (#18001)
    Signed-off-by: Chen Zhang <zhangch99@outlook.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/v1/core/kv_cache_manager.py
+++ vllm/v1/core/kv_cache_manager.py
@@ -121,13 +121,6 @@ class KVCacheManager:
                 - A list of blocks that are computed for the request.
                 - The number of computed tokens.
         """
-
-        # Request already has blocks from async load via KVConnector.
-        num_existing_blocks = len(
-            self.single_type_manager.req_to_blocks[request.request_id])
-        if num_existing_blocks > 0:
-            return KVCacheBlocks.create_empty(), request.num_computed_tokens
-
         # Prefix caching is disabled or
         # When the request requires prompt logprobs, we skip prefix caching.
         if (not self.enable_caching
@@ -172,6 +165,7 @@ class KVCacheManager:
         self,
         request: Request,
         num_new_tokens: int,
+        num_new_computed_tokens: int = 0,
         new_computed_blocks: Optional[KVCacheBlocks] = None,
         num_lookahead_tokens: int = 0,
         delay_cache_blocks: bool = False,
@@ -183,8 +177,10 @@ class KVCacheManager:
             num_new_tokens: The number of tokens to allocate, including external
                 tokens. Note that this does not include tokens that have
                 already been computed locally (i.e. new_computed_blocks).
-            new_computed_blocks: The new computed blocks just hitting the
-                prefix caching.
+            num_new_computed_tokens: The number of new computed tokens just
+                hitting the prefix caching, excluding external tokens.
+            new_computed_blocks: The cached blocks for the above new computed 
+                tokens.
             num_lookahead_tokens: The number of speculative tokens to allocate.
                 This is used by spec decode proposers with kv-cache such 
                 as eagle.
@@ -229,7 +225,7 @@ class KVCacheManager:
         # The number of computed tokens is the number of computed tokens plus
         # the new prefix caching hits
         num_computed_tokens = (request.num_computed_tokens +
-                               len(new_computed_block_list) * self.block_size)
+                               num_new_computed_tokens)
         num_tokens_need_slot = min(
             num_computed_tokens + num_new_tokens + num_lookahead_tokens,
             self.max_model_len)
