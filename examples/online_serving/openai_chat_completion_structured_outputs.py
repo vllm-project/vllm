17b0ae24050375ef86cdf751d04b39db9e2ac534
Aaron Pham <contact@aarnphm.xyz>
2025-05-15 03:14:23 -0400

    chore: unify examples for structured outputs
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- examples/online_serving/openai_chat_completion_structured_outputs.py
+++ examples/online_serving/openai_chat_completion_structured_outputs.py
@@ -1,52 +1,62 @@
+# ruff: noqa: E501
 # SPDX-License-Identifier: Apache-2.0
-"""
-To run this example, you need to start the vLLM server:
+
+from __future__ import annotations
+
+__doc__ = f"""
+This script demonstrates various structured output capabilities of vLLM's OpenAI-compatible server.
+It can run individual constraint types or all of them.
+It supports both streaming responses and concurrent non-streaming requests.
+
+To use this example, you must start an vLLM server with any model of your choice.
 
 ```bash
 vllm serve Qwen/Qwen2.5-3B-Instruct
 ```
-"""
 
+To serve a reasoning model, you can use the following command:
+```bash
+vllm serve deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B \
+    --reasoning-parser deepseek_r1
+```
+
+Examples:
+
+Run all constraints, non-streaming:
+python {__file__}
+
+Run only regex constraint, streaming:
+python {__file__} --constraint regex --stream
+
+Run json and choice constraints, non-streaming:
+python {__file__} --constraint json choice
+"""  # noqa: E501
+
+import argparse
+import asyncio
+import os
 from enum import Enum
+from typing import Any, Literal, get_args
 
-from openai import BadRequestError, OpenAI
+from openai import AsyncOpenAI, AsyncStream
+from openai.types.chat import ChatCompletion, ChatCompletionChunk
 from pydantic import BaseModel
 
 
-# Guided decoding by Choice (list of possible options)
-def guided_choice_completion(client: OpenAI, model: str):
-    completion = client.chat.completions.create(
-        model=model,
-        messages=[{
-            "role": "user",
-            "content": "Classify this sentiment: vLLM is wonderful!"
-        }],
-        extra_body={"guided_choice": ["positive", "negative"]},
-    )
-    return completion.choices[0].message.content
+async def print_stream_response(
+    stream_response: AsyncStream[ChatCompletionChunk],
+    title: str,
+):
+    print(f"\n{title} (Streaming):")
+    full_response = ""
+    async for chunk in stream_response:
+        content = chunk.choices[0].delta.content or ""
+        print(content, end="", flush=True)
+        full_response += content
+    print()
+    return full_response
 
 
-# Guided decoding by Regex
-def guided_regex_completion(client: OpenAI, model: str):
-    prompt = ("Generate an email address for Alan Turing, who works in Enigma."
-              "End in .com and new line. Example result:"
-              "alan.turing@enigma.com\n")
-
-    completion = client.chat.completions.create(
-        model=model,
-        messages=[{
-            "role": "user",
-            "content": prompt,
-        }],
-        extra_body={
-            "guided_regex": r"\w+@\w+\.com\n",
-            "stop": ["\n"]
-        },
-    )
-    return completion.choices[0].message.content
-
-
-# Guided decoding by JSON using Pydantic schema
 class CarType(str, Enum):
     sedan = "sedan"
     suv = "SUV"
@@ -60,101 +70,195 @@ class CarDescription(BaseModel):
     car_type: CarType
 
 
-def guided_json_completion(client: OpenAI, model: str):
-    json_schema = CarDescription.model_json_schema()
+ConstraintsFormat = Literal["choice", "regex", "json", "grammar",
+                            "structural_tag"]
 
-    prompt = ("Generate a JSON with the brand, model and car_type of"
-              "the most iconic car from the 90's")
-    completion = client.chat.completions.create(
-        model=model,
-        messages=[{
+PARAMS: dict[ConstraintsFormat, Any] = {
+    "choice": {
+        "messages": [{
             "role": "user",
-            "content": prompt,
+            "content": "Classify this sentiment: vLLM is wonderful!"
         }],
-        extra_body={"guided_json": json_schema},
-    )
-    return completion.choices[0].message.content
-
-
-# Guided decoding by Grammar
-def guided_grammar_completion(client: OpenAI, model: str):
-    simplified_sql_grammar = """
-        root ::= select_statement
-
-        select_statement ::= "SELECT " column " from " table " where " condition
-
-        column ::= "col_1 " | "col_2 "
-
-        table ::= "table_1 " | "table_2 "
-
-        condition ::= column "= " number
-
-        number ::= "1 " | "2 "
-    """
-
-    prompt = ("Generate an SQL query to show the 'username' and 'email'"
-              "from the 'users' table.")
-    completion = client.chat.completions.create(
-        model=model,
-        messages=[{
-            "role": "user",
-            "content": prompt,
+        "extra_body": {
+            "guided_choice": ["positive", "negative"]
+        },
+    },
+    "regex": {
+        "messages": [{
+            "role":
+            "user",
+            "content":
+            "Generate an email address for Alan Turing, who works in Enigma. End in .com and new line. Example result: 'alan.turing@enigma.com\\n'"
         }],
-        extra_body={"guided_grammar": simplified_sql_grammar},
-    )
-    return completion.choices[0].message.content
+        "extra_body": {
+            "guided_regex": r"\\w+@\\w+\\.com\\n",
+            "stop": ["\\n"],
+        },
+    },
+    "json": {
+        "messages": [{
+            "role":
+            "user",
+            "content":
+            "Generate a JSON with the brand, model and car_type of the most iconic car from the 90's"
+        }],
+        "response_format": {
+            "type": "json_schema",
+            "json_schema": CarDescription.model_json_schema(),
+        },
+    },
+    "grammar": {
+        "messages": [{
+            "role":
+            "user",
+            "content":
+            "Generate an SQL query to show the 'username' and 'email'from the 'users' table."
+        }],
+        "extra_body": {
+            "guided_grammar": """
+root ::= select_statement
 
+select_statement ::= "SELECT " column " from " table " where " condition
 
-# Extra backend options
-def extra_backend_options_completion(client: OpenAI, model: str):
-    prompt = ("Generate an email address for Alan Turing, who works in Enigma."
-              "End in .com and new line. Example result:"
-              "alan.turing@enigma.com\n")
+column ::= "col_1 " | "col_2 "
 
-    try:
-        # The guided_decoding_disable_fallback option forces vLLM to use
-        # xgrammar, so when it fails you get a 400 with the reason why
-        completion = client.chat.completions.create(
-            model=model,
-            messages=[{
-                "role": "user",
-                "content": prompt,
-            }],
-            extra_body={
-                "guided_regex": r"\w+@\w+\.com\n",
-                "stop": ["\n"],
-                "guided_decoding_backend": "xgrammar",
-                "guided_decoding_disable_fallback": True,
+table ::= "table_1 " | "table_2 "
+
+condition ::= column "= " number
+
+number ::= "1 " | "2 "
+""",
+            "guided_decoding_disable_any_whitespace": True,
+        }
+    },
+    "structural_tag": {
+        "messages": [
+            {
+                "role":
+                "user",
+                "content":
+                """
+You have access to the following function to retrieve the weather in a city:
+
+{
+    "name": "get_weather",
+    "parameters": {
+        "city": {
+            "param_type": "string",
+            "description": "The city to get the weather for",
+            "required": True
+        }
+    }
+}
+
+If a you choose to call a function ONLY reply in the following format:
+<{start_tag}={function_name}>{parameters}{end_tag}
+where
+
+start_tag => `<function`
+parameters => a JSON dict with the function argument name as key and function
+              argument value as value.
+end_tag => `</function>`
+
+Here is an example,
+<function=example_function_name>{"example_name": "example_value"}</function>
+
+Reminder:
+- Function calls MUST follow the specified format
+- Required parameters MUST be specified
+- Only call one function at a time
+- Put the entire function call reply on one line
+- Always add your sources when using search results to answer the user query
+
+You are a helpful assistant.
+
+Given the previous instructions, what is the weather in New York City, Boston,
+and San Francisco?""",
             },
-        )
-        return completion.choices[0].message.content
-    except BadRequestError as e:
-        print("This error is expected:", e)
+        ],
+        "response_format": {
+            "type":
+            "structural_tag",
+            "structures": [{
+                "begin": "<function=get_weather>",
+                "schema": {
+                    "type": "object",
+                    "properties": {
+                        "city": {
+                            "type": "string"
+                        }
+                    },
+                    "required": ["city"],
+                },
+                "end": "</function>"
+            }],
+            "triggers": ["<function="]
+        },
+    },
+}
 
 
-def main():
-    client: OpenAI = OpenAI(
-        base_url="http://localhost:8000/v1",
-        api_key="-",
+async def main():
+    parser = argparse.ArgumentParser(
+        description=
+        "Run OpenAI Chat Completion with various structured outputs capabilities",
+    )
+    _ = parser.add_argument(
+        "--constraint",
+        type=str,
+        nargs="+",
+        choices=[*get_args(ConstraintsFormat), "all"],
+        default=["all"],
+        help="Specify which constraint(s) to run.",
+    )
+    _ = parser.add_argument(
+        "--stream",
+        action=argparse.BooleanOptionalAction,
+        default=False,
+        help="Enable streaming output",
+    )
+    args = parser.parse_args()
+
+    base_url = os.getenv("OPENAI_BASE_URL", "http://localhost:8000/v1")
+    client = AsyncOpenAI(base_url=base_url, api_key="EMPTY")
+    model = (await client.models.list()).data[0].id
+
+    constraints: list[ConstraintsFormat] = list(get_args(ConstraintsFormat)) \
+        if "all" in args.constraint \
+        else list(set(args.constraint))
+
+    results = await asyncio.gather(
+        *[
+            client.chat.completions.create(
+                model=model,
+                stream=args.stream,
+                **PARAMS[name],
+            ) for name in constraints
+        ],
+        return_exceptions=True,
     )
 
-    model = client.models.list().data[0].id
-
-    print("Guided Choice Completion:")
-    print(guided_choice_completion(client, model))
-
-    print("\nGuided Regex Completion:")
-    print(guided_regex_completion(client, model))
-
-    print("\nGuided JSON Completion:")
-    print(guided_json_completion(client, model))
-
-    print("\nGuided Grammar Completion:")
-    print(guided_grammar_completion(client, model))
-
-    print("\nExtra Backend Options Completion:")
-    print(extra_backend_options_completion(client, model))
+    if args.stream:
+        for constraint_name, stream_or_exc in zip(constraints, results):
+            if isinstance(stream_or_exc, Exception):
+                print(f"Error for {constraint_name}: {stream_or_exc}\n")
+            else:
+                print_stream_response(
+                    stream_or_exc,
+                    title=constraint_name,
+                )
+    else:
+        for constraint_name, response_or_exc in zip(constraints, results):
+            print(f"Constraint: {constraint_name}\n")
+            if isinstance(response_or_exc, Exception):
+                print(f"Output:\n  Error: {response_or_exc}\n")
+            else:
+                assert isinstance(response_or_exc, ChatCompletion)
+                # response_or_exc is a ChatCompletion object
+                print(
+                    f"Output:\n  {response_or_exc.choices[0].message.content!r}\n"
+                )
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
