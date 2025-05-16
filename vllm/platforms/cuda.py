15d673b286a91034e6cbf788cd27eab824a8882a
youkaichao <youkaichao@gmail.com>
2025-05-16 10:38:58 +0800

    disable inductor
    Signed-off-by: youkaichao <youkaichao@gmail.com>

--- vllm/platforms/cuda.py
+++ vllm/platforms/cuda.py
@@ -158,6 +158,8 @@ class CudaPlatformBase(Platform):
                 "currently not supported with CUDA Graphs.")
             vllm_config.model_config.enforce_eager = True
             compilation_config.use_cudagraph = False
+            # FIXME: inductor breaks cudagraph (from @bnell)
+            compilation_config.use_inductor = False
 
     @classmethod
     def get_current_memory_usage(cls,
