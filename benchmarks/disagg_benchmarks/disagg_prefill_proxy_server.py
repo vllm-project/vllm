a63aac712b10fc5f5416defacdea3220bc5391ac
gc-fu <guancheng.fu@intel.com>
2025-05-20 00:53:05 +0800

    fix
    Signed-off-by: gc-fu <guancheng.fu@intel.com>

--- benchmarks/disagg_benchmarks/disagg_prefill_proxy_server.py
+++ benchmarks/disagg_benchmarks/disagg_prefill_proxy_server.py
@@ -32,7 +32,8 @@ async def handle_request():
         prefill_request = original_request_data.copy()
         # change max_tokens = 1 to let it only do prefill
         prefill_request["max_tokens"] = 1
-        # Set min_tokens = 1 to prevent min_tokens > max_tokens errors when max_tokens is 1
+        # Prevent min_tokens > max_tokens error
+        # by setting min_tokens = 1 when max_tokens = 1
         prefill_request["min_tokens"] = 1
 
         # finish prefill
