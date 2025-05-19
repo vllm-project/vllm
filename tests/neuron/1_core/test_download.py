0d0ce8834c9b582405d28bc325c1a1d8f9979b75
Liangfu Chen <liangfc@amazon.com>
2025-05-19 20:09:55 +0000

    remove test_download, due to existence of 2_core/test_mistral
    Signed-off-by: Liangfu Chen <liangfc@amazon.com>

--- tests/neuron/1_core/test_download.py
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-
-# Load model directly
-from transformers import AutoModelForCausalLM, AutoTokenizer
-
-def test_download():
-    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-1B")
-    model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.2-1B")
