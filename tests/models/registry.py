79e94f1eeaa4e291506c250dfc39ef55f398794e
dhia.rhaiem <dhia.rhaiem@tii.ae>
2025-05-20 13:47:49 +0000

    small fix
    Signed-off-by: dhia.rhaiem <dhia.rhaiem@tii.ae>

--- tests/models/registry.py
+++ tests/models/registry.py
@@ -147,7 +147,7 @@ _TEXT_GENERATION_EXAMPLE_MODELS = {
     "ExaoneForCausalLM": _HfExamplesInfo("LGAI-EXAONE/EXAONE-3.0-7.8B-Instruct"),  # noqa: E501
     "Fairseq2LlamaForCausalLM": _HfExamplesInfo("mgleize/fairseq2-dummy-Llama-3.2-1B"),  # noqa: E501
     "FalconForCausalLM": _HfExamplesInfo("tiiuae/falcon-7b"),
-    "FalconH1ForCausalLM":_HfExamplesInfo("tiiuae/falcon-7b",
+    "FalconH1ForCausalLM":_HfExamplesInfo("tiiuae/Falcon-H1-1.5B-Instruct",
                                           is_available_online=False),
     "GemmaForCausalLM": _HfExamplesInfo("google/gemma-1.1-2b-it"),
     "Gemma2ForCausalLM": _HfExamplesInfo("google/gemma-2-9b"),
