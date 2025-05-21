25ed6738d411a5da4f0f8daee7365443baa97ef5
Bill Nell <bnell@redhat.com>
2025-05-21 21:17:52 +0000

    wip
    Signed-off-by: Bill Nell <bnell@redhat.com>

--- vllm/model_executor/layers/fused_moe/fused_batched_moe.py
+++ vllm/model_executor/layers/fused_moe/fused_batched_moe.py
@@ -316,8 +316,8 @@ def invoke_moe_batched_triton_kernel(
         expert_num_tokens: torch.Tensor,  # [E]
         compute_type: tl.dtype,
         # Quantization data
-        A_scale: torch.Tensor,
-        B_scale: torch.Tensor,
+        A_scale: Optional[torch.Tensor],
+        B_scale: Optional[torch.Tensor],
         B_zp: torch.Tensor,
         # Quantization schemes
         use_fp8_w8a8: bool,
@@ -500,15 +500,16 @@ class BatchedExperts(mk.FusedMoEPermuteExpertsUnpermute):
         block_m: Optional[int] = None,
     ):
         super().__init__()
-        assert block_shape is None
+        #assert block_shape is None
         assert block_m is None
-        assert not use_fp8_w8a8, "NYI"
         assert not use_int8_w8a8, "NYI"
         assert not use_int8_w8a16, "NYI"
         assert not use_int4_w4a16, "NYI"
         self.max_num_tokens = max_num_tokens
         self.world_size = world_size
         self.dp_size = dp_size
+        self.use_fp8_w8a8 = use_fp8_w8a8
+        self.block_shape = block_shape
 
     def workspace_shapes(
         self,
@@ -527,6 +528,66 @@ class BatchedExperts(mk.FusedMoEPermuteExpertsUnpermute):
         workspace13 = num_experts * max_num_tokens * num_dp * K
         workspace2 = max_num_tokens * num_dp * N
         return (workspace13, workspace2, a.dtype)
+
+    def native_w8a8_block_matmul(A: torch.Tensor,
+                                 B: torch.Tensor,
+                                 As: torch.Tensor,
+                                 Bs: torch.Tensor):
+        """This function performs matrix multiplication with block-wise
+        quantization using native torch.
+        It is agnostic to the input data type and can be used for both int8 and
+        fp8 data types.
+
+        It takes two input tensors `A` and `B` (int8) with scales `As` and
+        `Bs` (float32).
+        The output is returned in the specified `output_dtype`.
+        """
+        A = A.to(torch.float32)
+        B = B.to(torch.float32)
+        assert A.shape[-1] == B.shape[-1]
+        assert B.ndim == 2 and B.is_contiguous() and Bs.ndim == 2
+        assert self.block_shape is not None and len(self.block_shape) == 2
+        block_n, block_k = self.block_shape[0], self.block_shape[1]
+        assert (A.shape[-1] + block_k - 1) // block_k == As.shape[-1]
+        assert A.shape[:-1] == As.shape[:-1]
+
+        M = A.numel() // A.shape[-1]
+        N, K = B.shape
+        origin_C_shape = A.shape[:-1] + (N, )
+        A = A.reshape(M, A.shape[-1])
+        As = As.reshape(M, As.shape[-1])
+        n_tiles = (N + block_n - 1) // block_n
+        k_tiles = (K + block_k - 1) // block_k
+        assert n_tiles == Bs.shape[0]
+        assert k_tiles == Bs.shape[1]
+
+        C_shape = (M, N)
+        C = torch.zeros(C_shape, dtype=torch.float32, device=A.device)
+
+        A_tiles = [
+            A[:, i * block_k:min((i + 1) * block_k, K)] for i in range(k_tiles)
+        ]
+        B_tiles = [[
+            B[
+                j * block_n:min((j + 1) * block_n, N),
+                i * block_k:min((i + 1) * block_k, K),
+            ] for i in range(k_tiles)
+        ] for j in range(n_tiles)]
+        C_tiles = [
+            C[:, j * block_n:min((j + 1) * block_n, N)] for j in range(n_tiles)
+        ]
+        As_tiles = [As[:, i:i + 1] for i in range(k_tiles)]
+
+        for i in range(k_tiles):
+            for j in range(n_tiles):
+                a = A_tiles[i]
+                b = B_tiles[j][i]
+                c = C_tiles[j]
+                s = As_tiles[i] * Bs[j][i]
+                c[:, :] += torch.matmul(a, b.t()) * s
+
+        C = C.reshape(origin_C_shape).to(output_dtype)
+        return C
 
     def apply(
         self,
@@ -580,9 +641,15 @@ class BatchedExperts(mk.FusedMoEPermuteExpertsUnpermute):
             else:
                 num = int(expert_num_tokens[expert].item())
             tmp = _resize_cache(workspace2, (num, N))
-            input = hidden_states[expert, :num, :] @ w1[expert].transpose(0, 1)
-            self.activation(activation, tmp, input)
-            out[expert, :num, :] = tmp @ w2[expert].transpose(0, 1)
+            if self.use_fp8_w8a8:
+                assert False # TBD
+                input = hidden_states[expert, :num, :] @ w1[expert].transpose(0, 1)
+                self.activation(activation, tmp, input)
+                out[expert, :num, :] = tmp @ w2[expert].transpose(0, 1)
+            else:
+                input = hidden_states[expert, :num, :] @ w1[expert].transpose(0, 1)
+                self.activation(activation, tmp, input)
+                out[expert, :num, :] = tmp @ w2[expert].transpose(0, 1)
 
         return out
 
@@ -732,7 +799,7 @@ class BatchedTritonExperts(mk.FusedMoEPermuteExpertsUnpermute):
                         intermediate_cache1.view(-1, N))
 
         #qintermediate_cache2 = intermediate_cache2
-        a2q_scale = a2_scale
+
         # TODO (varun) : support w8a8
         #assert not self.use_fp8_w8a8
         if self.use_fp8_w8a8:
@@ -753,6 +820,7 @@ class BatchedTritonExperts(mk.FusedMoEPermuteExpertsUnpermute):
                     per_act_token, self.block_shape)
         else:
             qintermediate_cache2 = intermediate_cache2
+            a2q_scale = a2_scale
 
         invoke_moe_batched_triton_kernel(A=qintermediate_cache2,
                                          B=w2,
