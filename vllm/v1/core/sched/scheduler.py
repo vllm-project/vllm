f5ffb3b9179725fa4efa56f336ae4f8c22b2cdc6
rshaw@neuralmagic.com <robertgshaw2@gmail.com>
2025-05-23 19:17:41 +0000

    updated
    Signed-off-by: rshaw@neuralmagic.com <robertgshaw2@gmail.com>

--- vllm/v1/core/sched/scheduler.py
+++ vllm/v1/core/sched/scheduler.py
@@ -318,7 +318,7 @@ class Scheduler(SchedulerInterface):
                         request.status = RequestStatus.WAITING
                     else:
                         logger.debug(
-                            "% is still in WAITING_FOR_REMOTE_KVS state.",
+                            "%s is still in WAITING_FOR_REMOTE_KVS state.",
                             request.request_id)
                         self.waiting.popleft()
                         skipped_waiting_requests.appendleft(request)
