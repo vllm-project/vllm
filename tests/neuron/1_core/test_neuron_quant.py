407f421f3b31a6edfee2be1f517a97c4c22ae07c
Satyajith Chilappagari <satchill@amazon.com>
2025-05-27 15:10:33 -0700

    [Neuron] Support quantization on neuron (#18283)
    Signed-off-by: Satyajith Chilappagari <satchill@amazon.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

---
+++ tests/neuron/1_core/test_neuron_quant.py
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: Apache-2.0
+from vllm.model_executor.layers.quantization.neuron_quant import (
+    NeuronQuantConfig)
+
+
+def test_get_supported_act_dtypes():
+    neuron_quant_config = NeuronQuantConfig()
+    supported_act_dtypes = neuron_quant_config.get_supported_act_dtypes()
+    target_list = ["any_dtype1", "any_dtype2"]
+    for dtype in target_list:
+        assert dtype in supported_act_dtypes
