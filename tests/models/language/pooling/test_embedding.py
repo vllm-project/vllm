ab3abe0b966f1649681672e983b460bd0b7c0953
Isotr0py <2037008807@qq.com>
2025-05-28 01:13:15 +0800

    revert and try qwen2.5 variant
    Signed-off-by: Isotr0py <2037008807@qq.com>

--- tests/models/language/pooling/test_embedding.py
+++ tests/models/language/pooling/test_embedding.py
@@ -21,7 +21,7 @@ from ...utils import check_embeddings_close
                      marks=[pytest.mark.core_model]),
         pytest.param("intfloat/e5-mistral-7b-instruct",
                      marks=[pytest.mark.core_model, pytest.mark.cpu_model]),
-        pytest.param("ssmits/Qwen2-7B-Instruct-embed-base"),
+        pytest.param("ssmits/Qwen2.5-7B-embed-base"),
         # [Cross-Encoder]
         pytest.param("sentence-transformers/stsb-roberta-base-v2"),
     ],
@@ -42,7 +42,7 @@ def test_models(
         monkeypatch.setenv("VLLM_USE_TRITON_FLASH_ATTN", "False")
 
     vllm_extra_kwargs = {}
-    if model == "ssmits/Qwen2-7B-Instruct-embed-base":
+    if model == "ssmits/Qwen2.5-7B-embed-base":
         monkeypatch.setenv("VLLM_ATTENTION_BACKEND", "XFORMERS")
         vllm_extra_kwargs["override_pooler_config"] = \
             PoolerConfig(pooling_type="MEAN", normalize=False)
@@ -53,11 +53,7 @@ def test_models(
     # https://github.com/UKPLab/sentence-transformers/blob/v3.1.1/sentence_transformers/models/Transformer.py#L159
     # This makes the input_ids different between hf_model and vllm_model.
     # So we need to strip the input texts to avoid test failing.
-
-    # Some embedding models may lowercase prompts in sentence_transformers, see:
-    # https://github.com/UKPLab/sentence-transformers/blob/v3.2.1/sentence_transformers/models/Transformer.py#L393-L395
-    #  For alignment and simplicity, we lowercase all prompts in the test.
-    example_prompts = [str(s).strip().lower() for s in example_prompts]
+    example_prompts = [str(s).strip() for s in example_prompts]
 
     with hf_runner(model, dtype=dtype,
                    is_sentence_transformer=True) as hf_model:
