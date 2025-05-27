80074717dd3d44444bab2c49a71d79a11e1de19a
mgoin <mgoin64@gmail.com>
2025-05-27 18:11:27 +0000

    Update with cudagraphs and remove uncommon schemes
    Signed-off-by: mgoin <mgoin64@gmail.com>

--- benchmarks/kernels/bench_fp8_gemm.py
+++ benchmarks/kernels/bench_fp8_gemm.py
@@ -73,35 +73,36 @@ WEIGHT_SHAPES = {
 @triton.testing.perf_report(
     triton.testing.Benchmark(
         x_names=["batch_size"],
-        x_vals=[1, 16, 64, 128, 256, 512, 1024, 2048],
+        x_vals=[1, 16, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384],
         x_log=False,
         line_arg="provider",
         line_vals=[
             "torch-bf16",
-            "fp8-tensor-w-token-a",
+            # "fp8-tensor-w-token-a",
             "fp8-tensor-w-tensor-a",
             "fp8-channel-w-token-a",
-            "fp8-channel-w-tensor-a",
-            "fp8-tensor-w-token-a-noquant",
+            # "fp8-channel-w-tensor-a",
+            # "fp8-tensor-w-token-a-noquant",
             "fp8-tensor-w-tensor-a-noquant",
             "fp8-channel-w-token-a-noquant",
-            "fp8-channel-w-tensor-a-noquant",
+            # "fp8-channel-w-tensor-a-noquant",
         ],
         line_names=[
             "torch-bf16",
-            "fp8-tensor-w-token-a",
+            # "fp8-tensor-w-token-a",
             "fp8-tensor-w-tensor-a",
             "fp8-channel-w-token-a",
-            "fp8-channel-w-tensor-a",
-            "fp8-tensor-w-token-a-noquant",
+            # "fp8-channel-w-tensor-a",
+            # "fp8-tensor-w-token-a-noquant",
             "fp8-tensor-w-tensor-a-noquant",
             "fp8-channel-w-token-a-noquant",
-            "fp8-channel-w-tensor-a-noquant",
+            # "fp8-channel-w-tensor-a-noquant",
         ],
-        ylabel="GB/s (larger is better)",
+        ylabel="TB/s (larger is better)",
         plot_name="BF16 vs FP8 GEMMs",
         args={},
-    ))
+    )
+)
 def benchmark(batch_size, provider, N, K):
     M = batch_size
     device = "cuda"
@@ -112,9 +113,9 @@ def benchmark(batch_size, provider, N, K):
     b = torch.ones((N, K), device=device, dtype=dtype) * 5.0
 
     if "torch-bf16" in provider:
-        ms, min_ms, max_ms = triton.testing.do_bench(
-            lambda: torch.nn.functional.linear(a, b),
-            quantiles=[0.5, 0.2, 0.8])
+        ms, min_ms, max_ms = triton.testing.do_bench_cudagraph(
+            lambda: torch.nn.functional.linear(a, b), quantiles=[0.5, 0.2, 0.8]
+        )
 
     elif "fp8" in provider:
         # Weights are always quantized ahead of time
@@ -125,62 +126,50 @@ def benchmark(batch_size, provider, N, K):
                 b_fp8, scale_b_fp8 = vllm_scaled_fp8_quant(b)
                 assert scale_b_fp8.numel() == 1
                 a_fp8, scale_a_fp8 = vllm_scaled_fp8_quant(
-                    a, use_per_token_if_dynamic=True)
+                    a, use_per_token_if_dynamic=True
+                )
 
                 def run_quant():
-                    return vllm_scaled_mm(a_fp8, b_fp8, scale_a_fp8,
-                                          scale_b_fp8, dtype)
+                    return vllm_scaled_mm(a_fp8, b_fp8, scale_a_fp8, scale_b_fp8, dtype)
 
             elif "tensor-w-tensor-a" in provider:
                 # Static per-tensor quantization with fixed scales
                 # for both A and B
-                scale_a = torch.tensor([1.0],
-                                       device=device,
-                                       dtype=torch.float32)
-                scale_b = torch.tensor([1.0],
-                                       device=device,
-                                       dtype=torch.float32)
+                scale_a = torch.tensor([1.0], device=device, dtype=torch.float32)
+                scale_b = torch.tensor([1.0], device=device, dtype=torch.float32)
                 b_fp8, scale_b_fp8 = vllm_scaled_fp8_quant(b, scale_b)
                 assert scale_b_fp8.numel() == 1
                 a_fp8, scale_a_fp8 = vllm_scaled_fp8_quant(a, scale_a)
 
                 def run_quant():
-                    return vllm_scaled_mm(a_fp8, b_fp8, scale_a_fp8,
-                                          scale_b_fp8, dtype)
+                    return vllm_scaled_mm(a_fp8, b_fp8, scale_a_fp8, scale_b_fp8, dtype)
 
             elif "channel-w-token-a" in provider:
                 # Static per-channel quantization for weights, per-token
                 # quant for A
-                scale_b = torch.tensor((N, ),
-                                       device=device,
-                                       dtype=torch.float32)
+                scale_b = torch.tensor((N,), device=device, dtype=torch.float32)
                 b_fp8, scale_b_fp8 = vllm_scaled_fp8_quant(b, scale_b)
                 scale_b_fp8 = scale_b_fp8.expand(N).contiguous()
                 assert scale_b_fp8.numel() == N
                 a_fp8, scale_a_fp8 = vllm_scaled_fp8_quant(
-                    a, use_per_token_if_dynamic=True)
+                    a, use_per_token_if_dynamic=True
+                )
 
                 def run_quant():
-                    return vllm_scaled_mm(a_fp8, b_fp8, scale_a_fp8,
-                                          scale_b_fp8, dtype)
+                    return vllm_scaled_mm(a_fp8, b_fp8, scale_a_fp8, scale_b_fp8, dtype)
 
             elif "channel-w-tensor-a" in provider:
                 # Static per-channel quantization for weights, per-tensor
                 # quant for A
-                scale_a = torch.tensor([1.0],
-                                       device=device,
-                                       dtype=torch.float32)
-                scale_b = torch.tensor((N, ),
-                                       device=device,
-                                       dtype=torch.float32)
+                scale_a = torch.tensor([1.0], device=device, dtype=torch.float32)
+                scale_b = torch.tensor((N,), device=device, dtype=torch.float32)
                 b_fp8, scale_b_fp8 = vllm_scaled_fp8_quant(b, scale_b)
                 scale_b_fp8 = scale_b_fp8.expand(N).contiguous()
                 assert scale_b_fp8.numel() == N
                 a_fp8, scale_a_fp8 = vllm_scaled_fp8_quant(a, scale_a)
 
                 def run_quant():
-                    return vllm_scaled_mm(a_fp8, b_fp8, scale_a_fp8,
-                                          scale_b_fp8, dtype)
+                    return vllm_scaled_mm(a_fp8, b_fp8, scale_a_fp8, scale_b_fp8, dtype)
 
         else:
             # In these cases, we quantize the activations during the GEMM call
@@ -191,70 +180,58 @@ def benchmark(batch_size, provider, N, K):
 
                 def run_quant():
                     a_fp8, scale_a_fp8 = vllm_scaled_fp8_quant(
-                        a, use_per_token_if_dynamic=True)
-                    return vllm_scaled_mm(a_fp8, b_fp8, scale_a_fp8,
-                                          scale_b_fp8, dtype)
+                        a, use_per_token_if_dynamic=True
+                    )
+                    return vllm_scaled_mm(a_fp8, b_fp8, scale_a_fp8, scale_b_fp8, dtype)
 
             elif "tensor-w-tensor-a" in provider:
                 # Static per-tensor quantization with fixed scales
                 # for both A and B
-                scale_a = torch.tensor([1.0],
-                                       device=device,
-                                       dtype=torch.float32)
-                scale_b = torch.tensor([1.0],
-                                       device=device,
-                                       dtype=torch.float32)
+                scale_a = torch.tensor([1.0], device=device, dtype=torch.float32)
+                scale_b = torch.tensor([1.0], device=device, dtype=torch.float32)
                 b_fp8, scale_b_fp8 = vllm_scaled_fp8_quant(b, scale_b)
                 assert scale_b_fp8.numel() == 1
 
                 def run_quant():
                     a_fp8, scale_a_fp8 = vllm_scaled_fp8_quant(a, scale_a)
-                    return vllm_scaled_mm(a_fp8, b_fp8, scale_a_fp8,
-                                          scale_b_fp8, dtype)
+                    return vllm_scaled_mm(a_fp8, b_fp8, scale_a_fp8, scale_b_fp8, dtype)
 
             elif "channel-w-token-a" in provider:
                 # Static per-channel quantization for weights, per-token
                 # quant for A
-                scale_b = torch.tensor((N, ),
-                                       device=device,
-                                       dtype=torch.float32)
+                scale_b = torch.tensor((N,), device=device, dtype=torch.float32)
                 b_fp8, scale_b_fp8 = vllm_scaled_fp8_quant(b, scale_b)
                 scale_b_fp8 = scale_b_fp8.expand(N).contiguous()
                 assert scale_b_fp8.numel() == N
 
                 def run_quant():
                     a_fp8, scale_a_fp8 = vllm_scaled_fp8_quant(
-                        a, use_per_token_if_dynamic=True)
-                    return vllm_scaled_mm(a_fp8, b_fp8, scale_a_fp8,
-                                          scale_b_fp8, dtype)
+                        a, use_per_token_if_dynamic=True
+                    )
+                    return vllm_scaled_mm(a_fp8, b_fp8, scale_a_fp8, scale_b_fp8, dtype)
 
             elif "channel-w-tensor-a" in provider:
                 # Static per-channel quantization for weights, per-tensor
                 # quant for A
-                scale_a = torch.tensor([1.0],
-                                       device=device,
-                                       dtype=torch.float32)
-                scale_b = torch.tensor((N, ),
-                                       device=device,
-                                       dtype=torch.float32)
+                scale_a = torch.tensor([1.0], device=device, dtype=torch.float32)
+                scale_b = torch.tensor((N,), device=device, dtype=torch.float32)
                 b_fp8, scale_b_fp8 = vllm_scaled_fp8_quant(b, scale_b)
                 scale_b_fp8 = scale_b_fp8.expand(N).contiguous()
                 assert scale_b_fp8.numel() == N
 
                 def run_quant():
                     a_fp8, scale_a_fp8 = vllm_scaled_fp8_quant(a, scale_a)
-                    return vllm_scaled_mm(a_fp8, b_fp8, scale_a_fp8,
-                                          scale_b_fp8, dtype)
+                    return vllm_scaled_mm(a_fp8, b_fp8, scale_a_fp8, scale_b_fp8, dtype)
 
         b_fp8 = b_fp8.t()
 
-        ms, min_ms, max_ms = triton.testing.do_bench(lambda: run_quant(),
-                                                     quantiles=[0.5, 0.2, 0.8])
+        ms, min_ms, max_ms = triton.testing.do_bench_cudagraph(
+            lambda: run_quant(), quantiles=[0.5, 0.2, 0.8]
+        )
 
-    # Calculate GB/s
-    gbps = lambda ms: (2 * M * N * K + M * N) * a.element_size() * 1e-9 / (
-        ms * 1e-3)
-    return gbps(ms), gbps(max_ms), gbps(min_ms)
+    # Calculate TB/s
+    tbps = lambda ms: (2 * M * N * K + M * N) * a.element_size() * 1e-12 / (ms * 1e-3)
+    return tbps(ms), tbps(max_ms), tbps(min_ms)
 
 
 def prepare_shapes(args):
@@ -289,11 +266,9 @@ if __name__ == "__main__":
 
     KN_model_names = prepare_shapes(args)
     for K, N, model_name in KN_model_names:
-        print(f"{model_name} N={N} K={K}: ")
-        benchmark.run(print_data=True,
-                      show_plots=True,
-                      save_path="bench_fp8_res",
-                      N=N,
-                      K=K)
+        print(f"{model_name}, N={N} K={K}, BF16 vs FP8 GEMMs TB/s:")
+        benchmark.run(
+            print_data=True, show_plots=True, save_path="bench_fp8_res", N=N, K=K
+        )
 
     print("Benchmark finished!")
