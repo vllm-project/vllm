1116a1197cf591e596fbfac634b6c3929d6c4583
Reid <61492567+reidliu41@users.noreply.github.com>
2025-05-27 15:07:01 +0800

    [Misc] improve docs (#18734)
    Signed-off-by: reidliu41 <reid201711@gmail.com>
    Co-authored-by: reidliu41 <reid201711@gmail.com>
    Signed-off-by: amit <amit.man@gmail.com>

--- examples/offline_inference/neuron_eagle.py
+++ examples/offline_inference/neuron_eagle.py
@@ -15,40 +15,46 @@ prompts = [
     "What is annapurna labs?",
 ]
 
-# Create a sampling params object.
-sampling_params = SamplingParams(top_k=1, max_tokens=500, ignore_eos=True)
 
-# Create an LLM.
-llm = LLM(
-    model="/home/ubuntu/model_hf/Meta-Llama-3.1-70B-Instruct",
-    speculative_config={
-        "model": "/home/ubuntu/model_hf/Llama-3.1-70B-Instruct-EAGLE-Draft",
-        "num_speculative_tokens": 5,
-        "max_model_len": 2048,
-    },
-    max_num_seqs=4,
-    # The max_model_len and block_size arguments are required to be same as
-    # max sequence length when targeting neuron device.
-    # Currently, this is a known limitation in continuous batching support
-    # in neuronx-distributed-inference.
-    max_model_len=2048,
-    block_size=2048,
-    # The device can be automatically detected when AWS Neuron SDK is installed.
-    # The device argument can be either unspecified for automated detection,
-    # or explicitly assigned.
-    device="neuron",
-    tensor_parallel_size=32,
-    override_neuron_config={
-        "enable_eagle_speculation": True,
-        "enable_fused_speculation": True,
-    },
-)
+def main():
+    # Create a sampling params object.
+    sampling_params = SamplingParams(top_k=1, max_tokens=500, ignore_eos=True)
 
-# Generate texts from the prompts. The output is a list of RequestOutput objects
-# that contain the prompt, generated text, and other information.
-outputs = llm.generate(prompts, sampling_params)
-# Print the outputs.
-for output in outputs:
-    prompt = output.prompt
-    generated_text = output.outputs[0].text
-    print(f"Prompt: {prompt!r}, \n\n\n\ Generated text: {generated_text!r}")
+    # Create an LLM.
+    llm = LLM(
+        model="/home/ubuntu/model_hf/Meta-Llama-3.1-70B-Instruct",
+        speculative_config={
+            "model": "/home/ubuntu/model_hf/Llama-3.1-70B-Instruct-EAGLE-Draft",
+            "num_speculative_tokens": 5,
+            "max_model_len": 2048,
+        },
+        max_num_seqs=4,
+        # The max_model_len and block_size arguments are required to be same as
+        # max sequence length when targeting neuron device.
+        # Currently, this is a known limitation in continuous batching support
+        # in neuronx-distributed-inference.
+        max_model_len=2048,
+        block_size=2048,
+        # The device can be automatically detected when AWS Neuron SDK is installed.
+        # The device argument can be either unspecified for automated detection,
+        # or explicitly assigned.
+        device="neuron",
+        tensor_parallel_size=32,
+        override_neuron_config={
+            "enable_eagle_speculation": True,
+            "enable_fused_speculation": True,
+        },
+    )
+
+    # Generate texts from the prompts. The output is a list of RequestOutput objects
+    # that contain the prompt, generated text, and other information.
+    outputs = llm.generate(prompts, sampling_params)
+    # Print the outputs.
+    for output in outputs:
+        prompt = output.prompt
+        generated_text = output.outputs[0].text
+        print(f"Prompt: {prompt!r}, \n\n\n\ Generated text: {generated_text!r}")
+
+
+if __name__ == "__main__":
+    main()
