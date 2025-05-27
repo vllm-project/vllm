c594729939df517acada048f0bd6f8e9dd555e79
wang.yuqi <noooop@126.com>
2025-05-27 20:53:30 +0800

    fix

--- tests/models/language/pooling/test_nomic_max_model_len.py
+++ tests/models/language/pooling/test_nomic_max_model_len.py
@@ -20,8 +20,7 @@ max_model_len = int(original_max_position_embeddings * factor)
 
 @pytest.mark.parametrize("model_info", MODELS)
 def test_default(model_info, vllm_runner):
-    with vllm_runner(model_info.name,
-                     task="embed",
+    with vllm_runner(model_info.name, task="embed",
                      max_model_len=None) as vllm_model:
         model_config = vllm_model.model.llm_engine.model_config
         if model_info.name == "nomic-ai/nomic-embed-text-v2-moe":
@@ -29,30 +28,28 @@ def test_default(model_info, vllm_runner):
             # by sentence_bert_config.json.
             assert model_config.max_model_len == 512
         else:
-            assert model_config.max_model_len == 2048
+            assert (
+                model_config.max_model_len == original_max_position_embeddings)
 
 
 @pytest.mark.parametrize("model_info", MODELS)
 def test_set_max_model_len_legal(model_info, vllm_runner):
-    # set max_model_len < 512
-    with vllm_runner(model_info.name,
-                     task="embed",
+    # set max_model_len <= 512
+    with vllm_runner(model_info.name, task="embed",
                      max_model_len=256) as vllm_model:
         model_config = vllm_model.model.llm_engine.model_config
         assert model_config.max_model_len == 256
 
-    # set 512 < max_model_len < 2048
+    # set 512 < max_model_len <= 2048
     if model_info.name == "nomic-ai/nomic-embed-text-v2-moe":
         # For nomic-embed-text-v2-moe the length is set to 512
         # by sentence_bert_config.json.
         with pytest.raises(ValueError):
-            with vllm_runner(model_info.name,
-                             task="embed",
+            with vllm_runner(model_info.name, task="embed",
                              max_model_len=1024):
                 pass
     else:
-        with vllm_runner(model_info.name,
-                         task="embed",
+        with vllm_runner(model_info.name, task="embed",
                          max_model_len=1024) as vllm_model:
             model_config = vllm_model.model.llm_engine.model_config
             assert model_config.max_model_len == 1024
@@ -62,9 +59,7 @@ def test_set_max_model_len_legal(model_info, vllm_runner):
 def test_set_max_model_len_illegal(model_info, vllm_runner):
     # set max_model_len > 2048
     with pytest.raises(ValueError):
-        with vllm_runner(model_info.name,
-                         task="embed",
-                         max_model_len=4096):
+        with vllm_runner(model_info.name, task="embed", max_model_len=4096):
             pass
 
     # set max_model_len > 2048 by hf_overrides
