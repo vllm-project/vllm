3ad6473b9346088143d3326e7dbcc2286659ea50
Cyrus Leung <tlleungac@connect.ust.hk>
2025-05-27 17:19:18 +0800

    [CI/Build] Remove imports of built-in `re` (#18750)
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- docs/mkdocs/hooks/generate_examples.py
+++ docs/mkdocs/hooks/generate_examples.py
@@ -1,10 +1,10 @@
 # SPDX-License-Identifier: Apache-2.0
-
 import itertools
-import re
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Literal
+
+import regex as re
 
 ROOT_DIR = Path(__file__).parent.parent.parent.parent
 ROOT_DIR_RELATIVE = '../../../../..'
