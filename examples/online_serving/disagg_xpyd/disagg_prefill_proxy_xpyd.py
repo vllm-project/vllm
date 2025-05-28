4b15e270739f0e4928324c1180b4ee2039dda249
Abatom <abzhonghua@gmail.com>
2025-05-28 15:17:58 +0800

    format
    Signed-off-by: Abatom <abzhonghua@gmail.com>

--- examples/online_serving/disagg_xpyd/disagg_prefill_proxy_xpyd.py
+++ examples/online_serving/disagg_xpyd/disagg_prefill_proxy_xpyd.py
@@ -32,17 +32,17 @@ def _listen_for_register(poller, router_socket):
                 global prefill_instances
                 global prefill_cv
                 with prefill_cv:
-                    prefill_instances[
-                        data["http_address"]] = data["zmq_address"]
+                    prefill_instances[data["http_address"]] = data["zmq_address"]
             elif data["type"] == "D":
                 global decode_instances
                 global decode_cv
                 with decode_cv:
-                    decode_instances[
-                        data["http_address"]] = data["zmq_address"]
+                    decode_instances[data["http_address"]] = data["zmq_address"]
             else:
-                print("Unexpected, Received message from %s, data: %s",
-                      remote_address, data)
+                print(
+                    "Unexpected, Received message from %s, data: %s",
+                    remote_address,
+                    data)
 
 
 def start_service_discovery(hostname, port):
@@ -58,9 +58,9 @@ def start_service_discovery(hostname, port):
     poller = zmq.Poller()
     poller.register(router_socket, zmq.POLLIN)
 
-    _listener_thread = threading.Thread(target=_listen_for_register,
-                                        args=[poller, router_socket],
-                                        daemon=True)
+    _listener_thread = threading.Thread(
+        target=_listen_for_register, args=[poller, router_socket], daemon=True
+    )
     _listener_thread.start()
     return _listener_thread
 
@@ -78,29 +78,27 @@ async def forward_request(url, data, request_id):
     async with aiohttp.ClientSession(timeout=AIOHTTP_TIMEOUT) as session:
         headers = {
             "Authorization": f"Bearer {os.environ.get('OPENAI_API_KEY')}",
-            "X-Request-Id": request_id
+            "X-Request-Id": request_id,
         }
-        async with session.post(url=url, json=data,
-                                headers=headers) as response:
+        async with session.post(url=url, json=data, headers=headers) as response:
             if response.status == 200:
                 # if response.headers.get('Transfer-Encoding') == 'chunked':
                 if True:
-                    async for chunk_bytes in response.content.iter_chunked(
-                            1024):
+                    async for chunk_bytes in response.content.iter_chunked(1024):
                         yield chunk_bytes
                 else:
                     content = await response.read()
                     yield content
 
 
-@app.route('/v1/completions', methods=['POST'])
+@app.route("/v1/completions", methods=["POST"])
 async def handle_request():
     try:
         original_request_data = await request.get_json()
 
         prefill_request = original_request_data.copy()
         # change max_tokens = 1 to let it only do prefill
-        prefill_request['max_tokens'] = 1
+        prefill_request["max_tokens"] = 1
 
         global count
         global prefill_instances
@@ -109,8 +107,7 @@ async def handle_request():
             # prefill_addr, prefill_zmq_addr = random.choice(
             #     list(prefill_instances.items()))
             prefill_list = list(prefill_instances.items())
-            prefill_addr, prefill_zmq_addr = prefill_list[count %
-                                                          len(prefill_list)]
+            prefill_addr, prefill_zmq_addr = prefill_list[count % len(prefill_list)]
 
         global decode_instances
         global decode_cv
@@ -118,26 +115,27 @@ async def handle_request():
             # decode_addr, decode_zmq_addr = random.choice(
             #     list(decode_instances.items()))
             decode_list = list(decode_instances.items())
-            decode_addr, decode_zmq_addr = decode_list[count %
-                                                       len(decode_list)]
+            decode_addr, decode_zmq_addr = decode_list[count % len(decode_list)]
 
-        print(f"handle_request count: {count}, [HTTP:{prefill_addr}, "
-              f"ZMQ:{prefill_zmq_addr}] 👉 [HTTP:{decode_addr}, "
-              f"ZMQ:{decode_zmq_addr}]")
+        print(
+            f"handle_request count: {count}, [HTTP:{prefill_addr}, "
+            f"ZMQ:{prefill_zmq_addr}] 👉 [HTTP:{decode_addr}, "
+            f"ZMQ:{decode_zmq_addr}]"
+        )
         count += 1
 
-        request_id = (
-            f"___prefill_addr_{prefill_zmq_addr}___decode_addr_{decode_zmq_addr}_{random_uuid()}"
-        )
+        request_id = (f"___prefill_addr_{prefill_zmq_addr}___decode_addr_{decode_zmq_addr}_{random_uuid()}")
 
         # finish prefill
-        async for _ in forward_request(f'http://{prefill_addr}/v1/completions',
-                                       prefill_request, request_id):
+        async for _ in forward_request(
+            f'http://{prefill_addr}/v1/completions', prefill_request, request_id
+        ):
             continue
 
         # return decode
-        generator = forward_request(f'http://{decode_addr}/v1/completions',
-                                    original_request_data, request_id)
+        generator = forward_request(
+            f'http://{decode_addr}/v1/completions', original_request_data, request_id
+        )
         response = await make_response(generator)
         response.timeout = None
 
@@ -152,7 +150,7 @@ async def handle_request():
         print("".join(traceback.format_exception(*exc_info)))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     t = start_service_discovery("0.0.0.0", 30001)
-    app.run(host='0.0.0.0', port=10001)
+    app.run(host="0.0.0.0", port=10001)
     t.join()
