7cb947ed57a0802f4b04b17a53bba2a643751f93
Luis Vega <lvega@nvidia.com>
2025-05-28 16:26:11 -0700

    added nemotron-h support
    Signed-off-by: Luis Vega <2478335+vegaluisjose@users.noreply.github.com>

--- vllm/model_executor/models/registry.py
+++ vllm/model_executor/models/registry.py
@@ -91,6 +91,7 @@ _TEXT_GENERATION_MODELS = {
     "MPTForCausalLM": ("mpt", "MPTForCausalLM"),
     "MiMoForCausalLM": ("mimo", "MiMoForCausalLM"),
     "NemotronForCausalLM": ("nemotron", "NemotronForCausalLM"),
+    "NemotronHForCausalLM": ("nemotron_h", "NemotronHForCausalLM"),
     "OlmoForCausalLM": ("olmo", "OlmoForCausalLM"),
     "Olmo2ForCausalLM": ("olmo2", "Olmo2ForCausalLM"),
     "OlmoeForCausalLM": ("olmoe", "OlmoeForCausalLM"),
