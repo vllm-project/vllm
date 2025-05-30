0a6437863203a55d4659e0f86ea50ba327db7ec5
Wenhua Cheng <wenhua.cheng@intel.com>
2025-05-30 10:24:47 +0800

    improve the robustness of parsing vlms config in AutoRound (#18894)
    Signed-off-by: wenhuach21 <wenhua.cheng@intel.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/model_executor/layers/quantization/auto_round.py
+++ vllm/model_executor/layers/quantization/auto_round.py
@@ -116,8 +116,9 @@ class AutoRoundConfig(QuantizationConfig):
 
         quantized = True
         if self.block_name_to_quantize:
-            quantized = any(name in layer_name
-                            for name in self.block_name_to_quantize)
+            quantized = any(
+                layer_name.startswith(name)
+                for name in self.block_name_to_quantize)
         elif isinstance(layer, ParallelLMHead):
             quantized = False
 
