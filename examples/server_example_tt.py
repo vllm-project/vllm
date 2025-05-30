923f501aacb81373a4105ed0f358502e35c2d59c
Radoica Draskic <rdraskic@tenstorrent.com>
2025-05-30 09:50:46 +0000

    Add max_num_seqs as argument to server example

--- examples/server_example_tt.py
+++ examples/server_example_tt.py
@@ -10,6 +10,7 @@ register_tt_models()  # Import and register models from tt-metal
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, default="meta-llama/Llama-3.1-70B-Instruct", help="Model name")
+    parser.add_argument("--max_num_seqs", type=int, default=32, help="Maximum number of sequences to be processed in a single iteration")
     args, unknown_args = parser.parse_known_args()
     
     check_tt_model_supported(args.model)
@@ -17,7 +18,7 @@ def main():
     sys.argv.extend([
         "--model", args.model,
         "--block_size", "64",
-        "--max_num_seqs", "32",
+        "--max_num_seqs", str(args.max_num_seqs),
         "--num_scheduler_steps", "10",
     ])
     runpy.run_module('vllm.entrypoints.openai.api_server', run_name='__main__')
