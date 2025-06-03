794d9899591080ea5182d9bcc46e7169c7946d25
Jee Jee Li <pandaleefree@gmail.com>
2025-06-03 08:23:18 +0000

    Done
    Signed-off-by: Jee Jee Li <pandaleefree@gmail.com>

--- tests/lora/test_quant_model.py
+++ tests/lora/test_quant_model.py
@@ -37,14 +37,6 @@ else:
     ]
 
 
-@pytest.fixture(autouse=True)
-def v1(run_with_both_engines_lora):
-    # Simple autouse wrapper to run both engines for each test
-    # This can be promoted up to conftest.py to run for every
-    # test in a package
-    pass
-
-
 def do_sample(llm: vllm.LLM,
               lora_path: str,
               lora_id: int,
