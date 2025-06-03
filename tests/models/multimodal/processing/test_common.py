1282bd812ea4e1511378bad5b918d609280d2b89
汪志鹏 <wangzhipeng628@gmail.com>
2025-06-03 13:13:13 +0800

    Add tarsier model support (#18985)
    Signed-off-by: 汪志鹏 <wangzhipeng628@gmail.com>

--- tests/models/multimodal/processing/test_common.py
+++ tests/models/multimodal/processing/test_common.py
@@ -282,6 +282,7 @@ def _test_processing_correctness_one(
     "Skywork/Skywork-R1V-38B",
     "fixie-ai/ultravox-v0_5-llama-3_2-1b",
     "openai/whisper-large-v3",
+    "omni-research/Tarsier-7b",
 ])
 @pytest.mark.parametrize("hit_rate", [0.3, 0.5, 1.0])
 @pytest.mark.parametrize("num_batches", [32])
