086ec9d992a45480c4ffd64021b930a44848398b
22quinn <33176974+22quinn@users.noreply.github.com>
2025-06-03 19:24:03 -0700

    Remove noisy warnings
    Signed-off-by: 22quinn <33176974+22quinn@users.noreply.github.com>

--- vllm/env_override.py
+++ vllm/env_override.py
@@ -18,7 +18,6 @@ if os.environ.get('NCCL_CUMEM_ENABLE') == '1':
         "This may increase memory overhead with cudagraph+allreduce: "
         "https://github.com/NVIDIA/nccl/issues/1234")
 elif not os.path.exists('/dev/nvidia-caps-imex-channels'):
-    logger.warning("Overriding NCCL_CUMEM_ENABLE=0")
     # NCCL requires NCCL_CUMEM_ENABLE to work with
     # multi-node NVLink, typically on GB200-NVL72 systems.
     # The ultimate way to detect multi-node NVLink is to use
@@ -32,10 +31,8 @@ elif not os.path.exists('/dev/nvidia-caps-imex-channels'):
 
 # see https://github.com/vllm-project/vllm/pull/15951
 # it avoids unintentional cuda initialization from torch.cuda.is_available()
-logger.warning("Overriding PYTORCH_NVML_BASED_CUDA_CHECK=1")
 os.environ['PYTORCH_NVML_BASED_CUDA_CHECK'] = '1'
 
-logger.warning("Overriding TORCHINDUCTOR_COMPILE_THREADS=1")
 # see https://github.com/vllm-project/vllm/issues/10480
 os.environ['TORCHINDUCTOR_COMPILE_THREADS'] = '1'
 # see https://github.com/vllm-project/vllm/issues/10619
