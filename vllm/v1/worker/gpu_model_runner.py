bc3efcc477e97cbf5117b3496d8b944a590ba3a9
charlifu <charlifu@amd.com>
2025-06-04 16:53:51 +0000

    enable full graph capture for TritonAttn
    Signed-off-by: charlifu <charlifu@amd.com>

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -2045,11 +2045,12 @@ class GPUModelRunner(LoRAModelRunnerMixin):
             if self.vllm_config.compilation_config.full_cuda_graph:
                 attn_backend_name = attn_backend_i.__name__
                 flash_attn_version = get_flash_attn_version()
-                if attn_backend_name != "FlashAttentionBackend" or \
-                    flash_attn_version != 3:
+                if not (attn_backend_name == "FlashAttentionBackend" and \
+                    flash_attn_version == 3) and \
+                        attn_backend_name != "TritonAttentionBackend":
                     raise ValueError(
                         f"full_cuda_graph is only supported with "
-                        f"FA3. Current attention backend is "
+                        f"FA3 or triton. Current attention backend is "
                         f"{attn_backend_name}, FlashAttention version is "
                         f"{flash_attn_version}.")
 
