755416f38e8594f6a3620a6013324a3da812bbca
simon-mo <simon.mo@hey.com>
2025-06-04 21:38:06 -0700

    fix syntax
    Signed-off-by: simon-mo <simon.mo@hey.com>

--- .buildkite/scripts/annotate-release.sh
+++ .buildkite/scripts/annotate-release.sh
@@ -10,23 +10,22 @@ if [ -z "$RELEASE_VERSION" ]; then
   exit 1
 fi
 
-# Create the annotation using a here document
 buildkite-agent annotate --style 'info' --context 'release-workflow' << EOF
 To download the wheel:
-```
+\`\`\`
 aws s3 cp s3://vllm-wheels/${RELEASE_VERSION}/vllm-${RELEASE_VERSION}-cp38-abi3-manylinux1_x86_64.whl .
 aws s3 cp s3://vllm-wheels/${RELEASE_VERSION}+cu126/vllm-${RELEASE_VERSION}+cu126-cp38-abi3-manylinux1_x86_64.whl .
 aws s3 cp s3://vllm-wheels/${RELEASE_VERSION}+cu118/vllm-${RELEASE_VERSION}+cu118-cp38-abi3-manylinux1_x86_64.whl . 
-```
+\`\`\`
 
 To download and upload the image:
 
-```
+\`\`\`
 docker pull public.ecr.aws/q9t5s3a7/vllm-release-repo:${BUILDKITE_COMMIT}
 docker tag public.ecr.aws/q9t5s3a7/vllm-release-repo:${BUILDKITE_COMMIT} vllm/vllm-openai
 docker tag vllm/vllm-openai vllm/vllm-openai:latest
 docker tag vllm/vllm-openai vllm/vllm-openai:v${RELEASE_VERSION}
 docker push vllm/vllm-openai:latest
 docker push vllm/vllm-openai:v${RELEASE_VERSION}
-```
+\`\`\`
 EOF 
