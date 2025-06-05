879f999b557cdb118f1b1bbc7ad11d59d0e5d6b5
chaunceyjiang <chaunceyjiang@gmail.com>
2025-06-05 12:09:27 +0000

    [Bugfix]: Fix messy code when using logprobs
    Signed-off-by: chaunceyjiang <chaunceyjiang@gmail.com>

--- vllm/transformers_utils/detokenizer_utils.py
+++ vllm/transformers_utils/detokenizer_utils.py
@@ -6,12 +6,6 @@ from typing import Optional
 from .tokenizer import AnyTokenizer
 
 
-def _replace_none_with_empty(tokens: list[Optional[str]]):
-    for i, token in enumerate(tokens):
-        if token is None:
-            tokens[i] = ""
-
-
 def _convert_tokens_to_string_with_added_encoders(
     tokenizer: AnyTokenizer,
     output_tokens: list[str],
@@ -64,20 +58,25 @@ def convert_prompt_ids_to_tokens(
     """
     # We do not need to convert the whole prompt to tokens.
     # Offset a little more in case we have special tokens.
-    new_tokens = tokenizer.convert_ids_to_tokens(
-        prompt_ids[-INITIAL_INCREMENTAL_DETOKENIZATION_OFFSET - 2:],
-        skip_special_tokens=skip_special_tokens)
+    new_tokens = []
+    token_ids = prompt_ids[-INITIAL_INCREMENTAL_DETOKENIZATION_OFFSET - 2:]
+    for token_id in token_ids:
+        new_token = tokenizer.decode([token_id],
+                                     skip_special_tokens=skip_special_tokens)
+        if new_token is None:
+            # This is required to guard against out-of-vocab prompt token ids
+            new_token = ""
+        new_tokens.append(new_token)
     read_offset = len(new_tokens)
     prefix_offset = max(
         read_offset - INITIAL_INCREMENTAL_DETOKENIZATION_OFFSET, 0)
-    # This is required to guard against out-of-vocab prompt token ids
-    _replace_none_with_empty(new_tokens)  # type: ignore[arg-type]
     return new_tokens, prefix_offset, read_offset
 
 
 def convert_ids_list_to_tokens(
     tokenizer: AnyTokenizer,
     token_ids: list[int],
+    skip_special_tokens: bool = False,
 ) -> list[str]:
     """Detokenize the input ids individually.
 
@@ -89,8 +88,15 @@ def convert_ids_list_to_tokens(
       Python list of token string representations
     
     """
-    token_str_lst = tokenizer.decode(token_ids)
-    _replace_none_with_empty(token_str_lst)  # type: ignore
+    token_str_lst = []
+    for token_id in token_ids:
+        token_str = tokenizer.decode(
+            [token_id],
+            skip_special_tokens=skip_special_tokens,
+        )
+        if token_str is None:
+            token_str = ""
+        token_str_lst.append(token_str)
     return token_str_lst
 
 
