7736c2c4436b554b8e637a9b15c40b3270bdac94
Aaruni Aggarwal <47731267+AaruniAggarwal@users.noreply.github.com>
2025-06-07 09:22:52 +0530

    [CI][PowerPC] Use a more appropriate way to select testcase in tests/models/language/pooling/test_embedding.py (#19253)
    Signed-off-by: Aaruni Aggarwal <aaruniagg@gmail.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- .buildkite/scripts/hardware_ci/run-cpu-test-ppc64le.sh
+++ .buildkite/scripts/hardware_ci/run-cpu-test-ppc64le.sh
@@ -38,7 +38,7 @@ function cpu_tests() {
     pytest -v -s tests/models/language/generation/test_common.py::test_models[False-5-32-facebook/opt-125m]
     pytest -v -s tests/models/language/generation/test_common.py::test_models[False-5-32-google/gemma-1.1-2b-it]
     pytest -v -s tests/models/language/pooling/test_classification.py::test_models[float-jason9693/Qwen2.5-1.5B-apeach]
-    pytest -v -s tests/models/language/pooling/test_embedding.py::test_models[half-BAAI/bge-base-en-v1.5]"
+    pytest -v -s tests/models/language/pooling/test_embedding.py -m cpu_model"
 }
 
 # All of CPU tests are expected to be finished less than 40 mins.
