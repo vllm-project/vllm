021528c7d8a8387a54fa8a4be1b82b060be023e1
Varun Sundar Rabindranath <vsundarr@redhat.com>
2025-06-10 10:46:00 -0700

    add bf16 tma kernels
    Signed-off-by: Varun Sundar Rabindranath <vsundarr@redhat.com>

--- tests/kernels/moe/test_ibm_moe.py
+++ tests/kernels/moe/test_ibm_moe.py
@@ -7,7 +7,8 @@ from typing import Optional
 
 import torch
 
-from vllm.model_executor.layers.fused_moe.ibm_fused_moe.persistent_gg_bf16 import grouped_gemm_persistent
+from vllm.model_executor.layers.fused_moe.ibm_fused_moe.persistent_gg_bf16 import grouped_gemm_persistent as ibm_gg_bf16
+from vllm.model_executor.layers.fused_moe.ibm_fused_moe.persistent_gg_bf16_tma import grouped_gemm_persistent as ibm_gg_bf16_tma
 from vllm.model_executor.layers.fused_moe.moe_permute_unpermute import (_moe_permute)
 
 
@@ -102,7 +103,8 @@ TOPKs = [2, 6]
 @pytest.mark.parametrize("E", Es)
 @pytest.mark.parametrize("TOPK", TOPKs)
 @pytest.mark.parametrize("dtype", [torch.bfloat16])
-def test_ibm_gg_bf16(M, N, K, E, TOPK, dtype):
+@pytest.mark.parametrize("impl", [ibm_gg_bf16, ibm_gg_bf16_tma])
+def test_ibm_bf16(M, N, K, E, TOPK, dtype, impl):
 
     config: TestConfig = TestConfig(m = M, n = N, k = K, e = E, num_topk = TOPK, dtype = dtype, group_size = 128)
     tt: TestTensors = TestTensors.make(config)
@@ -111,8 +113,8 @@ def test_ibm_gg_bf16(M, N, K, E, TOPK, dtype):
     print (f"M {M} N {N} K {K} E {E} TOPK {TOPK} | A {gtt.a_grouped.shape}  ...", flush=True)
 
     ref_output = torch_gg(gtt) 
-    output = grouped_gemm_persistent(inputs = gtt.a_grouped,
-                                     expert_indices = gtt.expert_ids_grouped,
-                                     expert_weights = gtt.w)
+    impl_output = impl(inputs = gtt.a_grouped,
+                       expert_indices = gtt.expert_ids_grouped,
+                       expert_weights = gtt.w)
 
     torch.testing.assert_close(ref_output, output)
