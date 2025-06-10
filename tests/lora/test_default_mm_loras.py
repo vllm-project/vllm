65e82f2ec7c7abc62c2d36343c8fdb5ebe0a5fbd
Alex-Brooks <Alex.Brooks@ibm.com>
2025-06-10 11:38:05 +0000

    Swap default lora tests to phi4mm
    Signed-off-by: Alex-Brooks <Alex.Brooks@ibm.com>

--- tests/lora/test_default_mm_loras.py
+++ tests/lora/test_default_mm_loras.py
@@ -3,20 +3,26 @@
 Tests for applying default registered multimodal loras.
 """
 
+import os
+
 import pytest
+from huggingface_hub import snapshot_download
 
 from vllm.lora.request import LoRARequest
 
 from ..conftest import AudioTestAssets, VllmRunner
 
-MODEL = "ibm-granite/granite-speech-3.3-2b"
-AUDIO_PROMPT = "<|start_of_role|>system<|end_of_role|>Knowledge Cutoff Date: April 2024.\nToday's Date: December 19, 2024.\nYou are Granite, developed by IBM. You are a helpful AI assistant<|end_of_text|>\n<|start_of_role|>user<|end_of_role|><|audio|>can you transcribe the speech into a written format?<|end_of_text|>\n<|start_of_role|>assistant<|end_of_role|>"  # noqa: E501
+MODEL_PATH = snapshot_download("microsoft/Phi-4-multimodal-instruct")
+AUDIO_LORA_PATH = os.path.join(MODEL_PATH, "speech-lora")
+IMAGE_LORA_PATH = os.path.join(MODEL_PATH, "vision-lora")
+
+AUDIO_PROMPT = "<|user|><|audio_1|>Can you transcribe this audio?<|end|><|assistant|>"  # noqa: E501
 
 # Responses are greedy decoded; we just check the end of
 # the generated text. If the lora is inactive, this model
 # generates commentary on the transcription.
-RESPONSE_SUFFIX_WITH_LORA = "the first words i spoke in the original chorus a little piece of practical poetry mary had a little lamb its fleece was white as snow and everywhere that mary went the lamb would surely go"  # noqa: E501
-RESPONSE_SUFFIX_WITHOUT_LORA = "This is a simplified transcription of the given text, which appears to be a poetic description or narrative."  # noqa: E501
+RESPONSE_SUFFIX_WITH_LORA = "Spoken text: The first words I spoke in the original chronograph, a little piece of practical poetry. Mary had a little lamb, it slept with quite a snow, and everywhere that Mary went, the lamb was sure to go."  # noqa: E501
+RESPONSE_SUFFIX_WITHOUT_LORA = "Certainly! Here is the transcription of the audio you provided:\n\nThe first words I spoke in the original phonograph record: A little piece of practical poetry. Mary had a little lamb; its fleece was white as snow, and everywhere that Mary went, the lamb was sure to go."  # noqa: E501
 
 
 @pytest.fixture(autouse=True)
@@ -28,11 +34,13 @@ def v1(run_with_both_engines_lora):
 
 
 VLLM_RUNNER_BASE_KWARGS = {
-    "model_name": MODEL,
+    "model_name": MODEL_PATH,
     "dtype": "half",
     "enable_lora": "True",
-    "max_lora_rank": 64,
-    "max_model_len": 2048,
+    "max_num_seqs": 2,
+    "max_lora_rank": 320,
+    "max_model_len": 12800,
+    "gpu_memory_utilization": 0.8,
     "limit_mm_per_prompt": {
         "audio": 1
     },
@@ -56,6 +64,7 @@ def run_test(vllm_runner, audio_assets, lora_request, expected_suffix,
                 lora_request=lora_request,
             ) for prompts, audios in inputs
         ]
+
         assert vllm_outputs_with_default_lora[-1][-1][-1].endswith(
             expected_suffix)
 
@@ -69,7 +78,7 @@ def test_active_default_mm_lora(
         vllm_runner,
         audio_assets,
         lora_request=None,
-        default_mm_loras={"audio": MODEL},
+        default_mm_loras={"audio": AUDIO_LORA_PATH},
         expected_suffix=RESPONSE_SUFFIX_WITH_LORA,
     )
 
@@ -79,11 +88,12 @@ def test_inactive_default_mm_lora(
     audio_assets: AudioTestAssets,
 ):
     """Ensure that modalities are filtered properly."""
+    # Default image lora won't be active since we only pass audio
     run_test(
         vllm_runner,
         audio_assets,
         lora_request=None,
-        default_mm_loras={"image": MODEL},
+        default_mm_loras={"image": IMAGE_LORA_PATH},
         expected_suffix=RESPONSE_SUFFIX_WITHOUT_LORA,
     )
 
@@ -96,8 +106,8 @@ def test_default_mm_lora_succeeds_with_redundant_lora_request(
     run_test(
         vllm_runner,
         audio_assets,
-        lora_request=LoRARequest("audio", 1, MODEL),
-        default_mm_loras={"audio": MODEL},
+        lora_request=LoRARequest("audio", 1, AUDIO_LORA_PATH),
+        default_mm_loras={"audio": AUDIO_LORA_PATH},
         expected_suffix=RESPONSE_SUFFIX_WITH_LORA,
     )
 
@@ -111,7 +121,7 @@ def test_default_mm_lora_fails_with_overridden_lora_request(
     run_test(
         vllm_runner,
         audio_assets,
-        lora_request=LoRARequest("speech", 2, MODEL),
-        default_mm_loras={"audio": "this path is overridden"},
+        lora_request=LoRARequest("speech", 2, AUDIO_LORA_PATH),
+        default_mm_loras={"audio": IMAGE_LORA_PATH},
         expected_suffix=RESPONSE_SUFFIX_WITH_LORA,
     )
