3c2766be4d88bca4df2df24313ef15f94b67f659
Chendi.Xue <chendi.xue@intel.com>
2025-06-11 22:22:40 +0300

    Add doc_string for the usage and example
    Signed-off-by: Chendi.Xue <chendi.xue@intel.com>

--- vllm/model_executor/custom_op.py
+++ vllm/model_executor/custom_op.py
@@ -159,6 +159,13 @@ class CustomOp(nn.Module):
     op_registry_oot: dict[str, type['CustomOp']] = {}
 
     # Decorator to register custom ops.
+    # For OOT custom ops:
+    #   if in-tree layer class is registered with an oot_custom_op layer,
+    #   the oot_custom_op layer will be used instead.
+    #   use `name=<In-Tree Layer class Name>` and `is_oot_custom_op=True`
+    # Examples:
+    # - @CustomOp.register("UnquantizedFusedMoEMethod", is_oot_custom_op=True)
+    # - @CustomOp.register("RMSNorm", is_oot_custom_op=True)
     @classmethod
     def register(cls, name: str, is_oot_custom_op=False):
 
