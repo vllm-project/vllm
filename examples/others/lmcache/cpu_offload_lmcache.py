0d239946dc5432d8a9001e5983852f0f68d72b56
runzhen <wangrunzhen@gmail.com>
2025-06-11 05:42:20 -0700

    [Bugfix] Update the example code, make it work with the latest lmcache (#19453)
    Signed-off-by: Runzhen Wang <wangrunzhen@gmail.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- examples/others/lmcache/cpu_offload_lmcache.py
+++ examples/others/lmcache/cpu_offload_lmcache.py
@@ -28,8 +28,8 @@ import os
 import time
 from dataclasses import asdict
 
-from lmcache.experimental.cache_engine import LMCacheEngineBuilder
 from lmcache.integration.vllm.utils import ENGINE_NAME
+from lmcache.v1.cache_engine import LMCacheEngineBuilder
 
 from vllm import LLM, SamplingParams
 from vllm.config import KVTransferConfig
