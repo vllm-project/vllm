6228f644486f8e1067456a0fdb2c62b60c59d861
Max de Bayser <mbayser@br.ibm.com>
2025-06-12 17:29:51 -0300

    remove debug prints
    Signed-off-by: Max de Bayser <mbayser@br.ibm.com>

--- tests/models/language/generation/test_common.py
+++ tests/models/language/generation/test_common.py
@@ -146,8 +146,6 @@ def test_models(hf_runner, vllm_runner, example_prompts, model: str,
             vllm_outputs_from_embeds = vllm_model.generate_greedy_logprobs(
                 prompt_embeds, max_tokens, num_logprobs)
 
-    print(f"{hf_outputs=}")
-    print(f"{vllm_outputs=}")
     check_logprobs_close(
         outputs_0_lst=hf_outputs,
         outputs_1_lst=vllm_outputs,
