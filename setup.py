cf2b6f602e03c1c0df112e3d24d3fa0918ef1a22
Sanger Steel <sangersteel@gmail.com>
2025-06-12 16:32:25 -0400

    fix: Update Tensorizer versions
    Signed-off-by: Sanger Steel <sangersteel@gmail.com>

--- setup.py
+++ setup.py
@@ -690,7 +690,7 @@ setup(
     ext_modules=ext_modules,
     install_requires=get_requirements(),
     extras_require={
-        "tensorizer": ["tensorizer>=2.9.0"],
+        "tensorizer": ["tensorizer>=2.10.0"],
         "fastsafetensors": ["fastsafetensors >= 0.1.10"],
         "runai": ["runai-model-streamer", "runai-model-streamer-s3", "boto3"],
         "audio": ["librosa", "soundfile"],  # Required for audio processing
