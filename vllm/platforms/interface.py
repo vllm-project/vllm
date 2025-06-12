c5b00d1acaf4f184e58977b1b49c761474c78446
Dan Blanaru <48605845+DanBlanaru@users.noreply.github.com>
2025-06-12 16:34:18 +0000

    revert interface

--- vllm/platforms/interface.py
+++ vllm/platforms/interface.py
@@ -58,7 +58,10 @@ class _Backend(enum.Enum):
     PALLAS_VLLM_V1 = enum.auto()
     IPEX = enum.auto()
     BLOCK_SPARSE_FLASH_ATTN = enum.auto()
+    DUAL_CHUNK_FLASH_ATTN = enum.auto()
     BOK = enum.auto()
+    NO_ATTENTION = enum.auto()
+    FLEX_ATTENTION = enum.auto()
 
 
 class PlatformEnum(enum.Enum):
