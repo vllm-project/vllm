3c43940eaac7b5a8d414cd8653a3af5bced45519
Xuyu Fu <fuxuyu@gmail.com>
2025-06-14 16:51:04 -0700

    add trace header to beam search
    Signed-off-by: Xuyu Fu <fuxuyu@gmail.com>
    
    Signed-off-by:  <fuxuyu@gmail.com>

--- vllm/entrypoints/openai/serving_engine.py
+++ vllm/entrypoints/openai/serving_engine.py
@@ -326,6 +326,7 @@ class OpenAIServing:
         request_id: str,
         params: BeamSearchParams,
         lora_request: LoRARequest | None = None,
+        trace_headers: Optional[Mapping[str, str]] = None,
     ) -> AsyncGenerator[RequestOutput, None]:
         beam_width = params.beam_width
         max_tokens = params.max_tokens
@@ -427,6 +428,7 @@ class OpenAIServing:
                             beam_search_params,
                             request_id_item,
                             lora_request=lora_req,
+                            trace_headers=trace_headers,
                         )
                     )
                 )
