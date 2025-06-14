abcf846607548d0efdbdf29c3927203602cbfb14
Varun Sundar Rabindranath <vsundarr@redhat.com>
2025-06-13 21:07:13 -0700

    add batched per token quant
    Signed-off-by: Varun Sundar Rabindranath <vsundarr@redhat.com>

--- vllm/model_executor/layers/fused_moe/batched_utils.py
+++ vllm/model_executor/layers/fused_moe/batched_utils.py
@@ -4,11 +4,217 @@
 Batched utility kernels used in the fused_moe operation.
 """
 
+from typing import Optional
+
 import torch
 
+from vllm.model_executor.layers.quantization.utils.fp8_utils import (
+    _do_per_token_group_quant_fp8, _do_per_token_group_quant_fp8_colmajor)
+from vllm.platforms import current_platform
 from vllm.triton_utils import tl, triton
 
-## Batched Per Token Quant kernel ####
+## Batched Per Token Quant ####
+
+
+@triton.jit
+def _batched_per_token_group_quant_fp8(
+        expert_num_tokens,
+        stride_ye,
+        stride_yqe,
+        stride_yse,
+        # Pointers to inputs and output
+        y_ptr,
+        y_q_ptr,
+        y_s_ptr,
+        group_size,
+        # Num columns of y
+        y_num_columns,
+        y_row_stride,
+        # Avoid to divide zero
+        eps,
+        # Information for float8
+        fp8_min,
+        fp8_max,
+        # Meta-parameters
+        BLOCK: tl.constexpr):
+
+    expert_id = tl.program_id(axis=0)
+    e_num_tokens = tl.load(expert_num_tokens + expert_id)
+    if e_num_tokens == 0:
+        # early exit
+        return
+
+    groups_per_row = y_num_columns // group_size
+    valid_groups_in_experts = e_num_tokens * groups_per_row
+    group_id = tl.program_id(axis=1)
+    if group_id >= valid_groups_in_experts:
+        # early exit
+        return
+
+    y_ptr = y_ptr + expert_id * stride_ye
+    y_q_ptr = y_q_ptr + expert_id * stride_yqe
+    y_s_ptr = y_s_ptr + expert_id * stride_yse
+
+    _do_per_token_group_quant_fp8(
+        group_id,  # group id
+        # Pointers to inputs and output
+        y_ptr,
+        y_q_ptr,
+        y_s_ptr,
+        group_size,
+        # Num columns of y
+        y_num_columns,
+        y_row_stride,
+        # Avoid to divide zero
+        eps,
+        # Information for float8
+        fp8_min,
+        fp8_max,
+        # Meta-parameters
+        BLOCK)
+
+
+@triton.jit
+def _batched_per_token_group_quant_fp8_colmajor(
+    expert_num_tokens,
+    stride_ye,
+    stride_yqe,
+    stride_yse,
+    # Pointers to inputs and output
+    y_ptr,
+    y_q_ptr,
+    y_s_ptr,
+    group_size,
+    # Num columns of y
+    y_num_columns,
+    y_row_stride,
+    # Stride from one column to the next of y_s
+    y_s_col_stride,
+    # Avoid to divide zero
+    eps,
+    # Information for float8
+    fp8_min,
+    fp8_max,
+    # Meta-parameters
+    BLOCK: tl.constexpr,
+):
+    expert_id = tl.program_id(axis=0)
+    e_num_tokens = tl.load(expert_num_tokens + expert_id)
+    if e_num_tokens == 0:
+        # early exit
+        return
+
+    groups_per_row = y_num_columns // group_size
+    valid_groups_in_experts = e_num_tokens * groups_per_row
+    group_id = tl.program_id(axis=1)
+    if group_id >= valid_groups_in_experts:
+        # early exit
+        return
+
+    y_ptr = y_ptr + expert_id * stride_ye
+    y_q_ptr = y_q_ptr + expert_id * stride_yqe
+    y_s_ptr = y_s_ptr + expert_id * stride_yse
+
+    _do_per_token_group_quant_fp8_colmajor(
+        group_id,
+        # Pointers to inputs and output
+        y_ptr,
+        y_q_ptr,
+        y_s_ptr,
+        group_size,
+        # Num columns of y
+        y_num_columns,
+        y_row_stride,
+        # Stride from one column to the next of y_s
+        y_s_col_stride,
+        # Avoid to divide zero
+        eps,
+        # Information for float8
+        fp8_min,
+        fp8_max,
+        # Meta-parameters
+        BLOCK)
+
+
+def batched_per_token_group_quant_fp8(
+        x: torch.Tensor,
+        x_q: Optional[torch.Tensor],
+        expert_num_tokens: torch.Tensor,
+        group_size: int,
+        column_major_scales: bool,
+        eps: float = 1e-10) -> tuple[torch.Tensor, torch.Tensor]:
+
+    assert (x.size(-1) % group_size == 0), (
+        f"the last dimension of `x` {x.size(-1)} must be divisible "
+        f"by `group_size` {group_size}")
+    assert x.stride(-1) == 1, "`x` groups must be contiguous"
+
+    dtype = current_platform.fp8_dtype()
+    finfo = torch.finfo(dtype)
+    fp8_min = finfo.min
+    fp8_max = finfo.max
+
+    assert x_q is None or x_q.shape == x.shape
+    if x_q is None:
+        x_q = torch.empty_like(x, device=x.device, dtype=dtype)
+
+    E, MAX_TOKENS, HIDDEN_SIZE = x.shape
+    shape = (E, MAX_TOKENS, HIDDEN_SIZE // group_size)
+    x_s = torch.empty(shape, device=x.device, dtype=torch.float32)
+    if column_major_scales:
+        x_s = x_s.permute(-1, -2)
+
+    M = (MAX_TOKENS * HIDDEN_SIZE) // group_size
+    N = group_size
+    BLOCK = triton.next_power_of_2(N)
+    # heuristics for number of warps
+    num_warps = min(max(BLOCK // 256, 1), 8)
+    num_stages = 1
+
+    grid = (E, M)
+
+    if column_major_scales:
+        _batched_per_token_group_quant_fp8_colmajor[grid](
+            expert_num_tokens,
+            x.stride(0),
+            x_q.stride(0),
+            x_s.stride(0),
+            x,
+            x_q,
+            x_s,
+            group_size,
+            x.shape[1],
+            x.stride(0),
+            x_s.stride(1),
+            eps,
+            fp8_min=fp8_min,
+            fp8_max=fp8_max,
+            BLOCK=BLOCK,
+            num_warps=num_warps,
+            num_stages=num_stages,
+        )
+    else:
+        _batched_per_token_group_quant_fp8[grid](
+            expert_num_tokens,
+            x.stride(0),
+            x_q.stride(0),
+            x_s.stride(0),
+            x,
+            x_q,
+            x_s,
+            group_size,
+            x.shape[1],
+            x.stride(0),
+            eps,
+            fp8_min=fp8_min,
+            fp8_max=fp8_max,
+            BLOCK=BLOCK,
+            num_warps=num_warps,
+            num_stages=num_stages,
+        )
+
+    return x_q, x_s
+
 
 ## Batched Silu and Mul Kernel ####
 
