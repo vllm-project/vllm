463ccaa30dfea26838afcb1d2f90068c46fa7814
Bill Nell <bnell@redhat.com>
2025-06-17 02:31:08 +0000

    replace import that lint removed
    Signed-off-by: Bill Nell <bnell@redhat.com>

--- vllm/model_executor/layers/fused_moe/deep_gemm_moe.py
+++ vllm/model_executor/layers/fused_moe/deep_gemm_moe.py
@@ -6,6 +6,7 @@ from typing import Optional
 
 import torch
 
+import vllm.model_executor.layers.quantization.deepgemm
 import vllm.model_executor.layers.fused_moe.modular_kernel as mk
 from vllm.logger import init_logger
 from vllm.model_executor.layers.fused_moe.moe_permute_unpermute import (
