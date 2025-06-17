fbd9aa8e11347c6d3036c96fe22dcfcea77ea1d1
Lucas Wilkinson <LucasWilkinson@users.noreply.github.com>
2025-06-16 23:32:49 -0400

    [Wheel Size] Only build FA2 8.0+PTX (#19336)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- cmake/external_projects/vllm_flash_attn.cmake
+++ cmake/external_projects/vllm_flash_attn.cmake
@@ -38,7 +38,7 @@ else()
   FetchContent_Declare(
           vllm-flash-attn
           GIT_REPOSITORY https://github.com/vllm-project/flash-attention.git
-          GIT_TAG 8798f27777fb57f447070301bf33a9f9c607f491
+          GIT_TAG 763ad155a1c826f71ff318f41edb1e4e5e376ddb
           GIT_PROGRESS TRUE
           # Don't share the vllm-flash-attn build between build types
           BINARY_DIR ${CMAKE_BINARY_DIR}/vllm-flash-attn
