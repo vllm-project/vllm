d704b86ab76ce11bfd8636ce6c265635be2b9fc8
Agata Dobrzyniewicz <160237065+adobrzyn@users.noreply.github.com>
2025-06-18 17:40:03 +0200

    Fix linear for v1 (#1452)
    Fix after https://github.com/HabanaAI/vllm-hpu-extension/pull/211
    
    ---------
    
    Signed-off-by: Agata Dobrzyniewicz <adobrzyniewicz@habana.ai>

--- vllm/v1/worker/hpu_model_runner.py
+++ vllm/v1/worker/hpu_model_runner.py
@@ -2145,6 +2145,7 @@ class HPUModelRunner:
         if prompt_profile_cfg or decode_profile_cfg:
             self._generate_profiling(prompt_profile_cfg, decode_profile_cfg)
             raise AssertionError("Finished profiling")
+        self.bucketing_ctx.generate_prompt_buckets()
         kv_caches = self.kv_caches
         max_blocks = int(kv_caches[0][0].size(0) // self.block_size)
         self.bucketing_ctx.generate_decode_buckets(max_blocks)
