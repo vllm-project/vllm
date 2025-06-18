0383e73153efd9ed0ed6855bd7cd099c734c3c21
Andrew Feldman <afeldman@redhat.com>
2025-06-17 20:51:51 -0400

    InputBatch fix
    Signed-off-by: Andrew Feldman <afeldman@redhat.com>

--- vllm/v1/worker/lora_model_runner_mixin.py
+++ vllm/v1/worker/lora_model_runner_mixin.py
@@ -5,7 +5,7 @@ Define LoRA functionality mixin for model runners.
 """
 
 from contextlib import contextmanager
-from typing import TYPE_CHECKING, Union
+from typing import Union
 
 import numpy as np
 import torch.nn as nn
@@ -16,11 +16,10 @@ from vllm.lora.layers import LoRAMapping
 from vllm.lora.request import LoRARequest
 from vllm.lora.worker_manager import LRUCacheWorkerLoRAManager
 from vllm.model_executor.models import supports_lora, supports_multimodal
+from vllm.v1.worker.gpu_input_batch import InputBatch as GPUInputBatch
+from vllm.v1.worker.tpu_input_batch import InputBatch as TPUInputBatch
 
-if TYPE_CHECKING:
-    from vllm.v1.worker.gpu_input_batch import InputBatch as GPUInputBatch
-    from vllm.v1.worker.tpu_input_batch import InputBatch as TPUInputBatch
-    InputBatch = Union[TPUInputBatch, GPUInputBatch]
+InputBatch = Union[TPUInputBatch, GPUInputBatch]
 
 logger = init_logger(__name__)
 
