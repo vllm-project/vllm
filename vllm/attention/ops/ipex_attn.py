b6f1bd86b29a78cbc7fed30754f9518f1472e12e
Ning Xie <andy.xning@gmail.com>
2025-06-18 08:21:06 +0800

    [MISC] correct copy_blocks src_to_dists param type (#19696)
    Signed-off-by: Andy Xie <andy.xning@gmail.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/attention/ops/ipex_attn.py
+++ vllm/attention/ops/ipex_attn.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
-from typing import Dict, List, Optional, Tuple
+from typing import List, Optional, Tuple
 
 try:
     import intel_extension_for_pytorch.llm.modules as ipex_modules
@@ -120,7 +120,7 @@ class _PagedAttention:
     @staticmethod
     def copy_blocks(
         kv_caches: List[torch.Tensor],
-        src_to_dists: Dict[int, List[int]],
+        src_to_dists: torch.Tensor,
         *args,
     ) -> None:
         key_caches = [kv_cache[0] for kv_cache in kv_caches]
