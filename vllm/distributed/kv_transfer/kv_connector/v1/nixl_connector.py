aa9630cdfa2681292f822c61731e712c39586dd2
Linkun Chen <github@lkchen.net>
2025-06-19 10:08:31 -0700

    init
    Signed-off-by: Linkun Chen <github@lkchen.net>

--- vllm/distributed/kv_transfer/kv_connector/v1/nixl_connector.py
+++ vllm/distributed/kv_transfer/kv_connector/v1/nixl_connector.py
@@ -836,17 +836,20 @@ class NixlConnectorWorker:
         """
         done_req_ids: set[str] = set()
         for req_id, handles in list(transfers.items()):
-            for handle, xfer_stime in handles:
+            xfer_stats = []
+            for handle, _ in handles:
                 xfer_state = self.nixl_wrapper.check_xfer_state(handle)
+                xfer_stats.append(xfer_state)
                 if xfer_state == "DONE":
                     self.nixl_wrapper.release_xfer_handle(handle)
-                    done_req_ids.add(req_id)
-                    del transfers[req_id]
                 elif xfer_state == "PROC":
                     continue
                 else:
                     raise RuntimeError("Transfer failed with state %s",
                                        xfer_state)
+            if all(xfer_state == "DONE" for xfer_state in xfer_stats):
+                done_req_ids.add(req_id)
+                del transfers[req_id]
         return done_req_ids
 
     def start_load_kv(self, metadata: NixlConnectorMetadata):
