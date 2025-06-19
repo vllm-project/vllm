650b001dc034d76506f1bafa8a8c629e4e8ef681
Your Name <you@example.com>
2025-06-19 18:25:56 +0000

    defautl size

--- vllm/beam/filtering.py
+++ vllm/beam/filtering.py
@@ -1,10 +1,14 @@
 import asyncio
 from collections.abc import AsyncGenerator
-from typing import Callable, Optional
+from typing import Callable, Optional, Union
 from urllib.request import Request
 from vllm.beam.debug import BeamDebugInfo
 import torch
-from vllm.entrypoints.openai.protocol import CompletionRequest
+from vllm.entrypoints.openai.protocol import CompletionRequest, CompletionResponse, \
+    ErrorResponse
+from vllm.logger import init_logger
+
+logger = init_logger(__name__)
 
 
 def format_filter(filter_params_list):
@@ -13,65 +17,75 @@ def format_filter(filter_params_list):
         raise ValueError(f"Expect 2 keys, seeing {filter_params_list=}")
     return [dict(zip(keys, vals)) for vals in filter_params_list]
 
+
 DEFAULT_CHAR_SERVER_FILTER = format_filter(
-        [
-            ("annotations_porn", 0.1098),
-            ("annotations_racist", 0.2814),
-            ("annotations_disturbing", 0.1827),
-            ("annotations_harmful_promotes_selfharm", 0.0749),
-            ("annotations_harmful_promotes_terrorism", 0.1129),
-        ]
-        )
-    
+    [
+        ("annotations_porn", 0.1098),
+        ("annotations_racist", 0.2814),
+        ("annotations_disturbing", 0.1827),
+        ("annotations_harmful_promotes_selfharm", 0.0749),
+        ("annotations_harmful_promotes_terrorism", 0.1129),
+    ]
+)
+
 MAX_GENERATIONS = 10
 _CHUNK_SIZE = 16
+_DEFAULT_BEAM_SIZE = 3
 
 class BeamValidator:
     def __init__(self, classi_idx, classifier_names):
         self.classi_idx = classi_idx
         self.classifier_names = classifier_names
 
-    async def get_n_valid_beams(self, create_completion: Callable, request: CompletionRequest, raw_request: Optional[Request] = None):
+    async def get_n_valid_beams(self, create_completion: Callable,
+                                request: CompletionRequest,
+                                raw_request: Optional[Request] = None) -> list[
+        Union[AsyncGenerator[str, None], CompletionResponse, ErrorResponse]]:
         request.stream = False
-        n = request.n
+        n = request.n if request.n > 1 else _DEFAULT_BEAM_SIZE
         request.n = 1
+        # TODO(@tanuj): accept max tokens as a parameter
         request.max_tokens = _CHUNK_SIZE
         request.echo = True
         tasks = []
+        # TODO(@tanuj): deep copy request and raw_request?
         for _ in range(n):
             request = request
             tasks.append(create_completion(
                 request,
+                raw_request=raw_request,
             ))
         res = await asyncio.gather(*tasks)
         request.n = n
         beam_validator_res = self.validate(res)
         filtered_res = [r for r, valid in zip(res, beam_validator_res) if valid]
-        print('everything is filtered', len(filtered_res) == 0)
+        logger.debug("Filtered count: %d", len(filtered_res))
         if len(filtered_res) == 0:
             return res
-        
+
         return filtered_res
-    
-    def validate(self, responses: list[AsyncGenerator], debug_infos_G: list[BeamDebugInfo] = None):
-        #TODO(@tanuj) - share this with the beam scorer
+
+    def validate(self, responses: list[AsyncGenerator],
+                 debug_infos_G: list[BeamDebugInfo] = None):
+        # TODO(@tanuj) - share this with the beam scorer
         heads = [response.choices[0].additional_heads[0] for response in responses]
         heads_tensor = torch.tensor(heads, dtype=torch.float)
         prob_GC = torch.sigmoid(heads_tensor)
         valid_G = torch.ones(prob_GC.shape[0], dtype=torch.bool)
-        
+
         for g in range(heads_tensor.shape[0]):
-            filtered = self.get_filtered_classifiers(prob_GC[g], DEFAULT_CHAR_SERVER_FILTER)
+            filtered = self.get_filtered_classifiers(prob_GC[g],
+                                                     DEFAULT_CHAR_SERVER_FILTER)
             if debug_infos_G is not None:
                 debug_infos_G[g].filtered_classifiers = filtered
-            
+
             if filtered:
                 valid_G[g] = False
                 for choice in responses[g].choices:
                     choice.is_filtered = True
 
         return valid_G
-    
+
     def get_filtered_classifiers(self, prob_C, filter_params) -> list[str]:
         relevant_filters = [
             (p["name"], self.classi_idx[p["name"]], p["threshold"])
