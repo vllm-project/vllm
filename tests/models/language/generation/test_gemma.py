520eda988239a1c314a46614c147f92baf4fc845
Yu-Hang Tang <Tang.Maxin@gmail.com>
2025-06-19 00:58:20 +0000

    pre-commit formatter fix
    Signed-off-by: Yu-Hang Tang <Tang.Maxin@gmail.com>

--- tests/models/language/generation/test_gemma.py
+++ tests/models/language/generation/test_gemma.py
@@ -1,14 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
-import pytest
 import numpy as np
+import pytest
 
-
-MODELS = [
-    "google/gemma-2b",
-    "google/gemma-2-2b",
-    "google/gemma-3-4b-it"
-]
+MODELS = ["google/gemma-2b", "google/gemma-2-2b", "google/gemma-3-4b-it"]
 
 
 @pytest.mark.parametrize("model", MODELS)
@@ -17,11 +12,9 @@ def test_dummy_loader(vllm_runner, model: str) -> None:
             model,
             load_format="dummy",
     ) as llm:
-        normalizers = llm.collective_rpc(
-            lambda self: self.worker.model_runner.model.model.normalizer.cpu().item()
-        )
+        normalizers = llm.collective_rpc(lambda self: self.worker.model_runner.
+                                         model.model.normalizer.cpu().item())
         assert np.allclose(
             normalizers,
             llm.llm_engine.model_config.hf_config.hidden_size**0.5,
-            rtol=1e-3
-        )
+            rtol=1e-3)
