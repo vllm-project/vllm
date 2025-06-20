d2993bccb7f4fb8ab5e2ba8d5fd4e6bdb5918ef6
kourosh hakhamaneshi <31483498+kouroshHakha@users.noreply.github.com>
2025-06-19 22:01:16 -0700

    [Bugfix][Ray] Set the cuda context eagerly in the ray worker  (#19583)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/platforms/cuda.py
+++ vllm/platforms/cuda.py
@@ -72,6 +72,17 @@ class CudaPlatformBase(Platform):
         return [torch.float32]
 
     @classmethod
+    def set_device(cls, device: torch.device) -> None:
+        """
+        Set the device for the current platform.
+        """
+        super().set_device(device)
+        # With this trick we can force the device to be set eagerly
+        # see https://github.com/pytorch/pytorch/issues/155668
+        # for why and when it is needed
+        _ = torch.zeros(1, device=device)
+
+    @classmethod
     def get_device_capability(cls,
                               device_id: int = 0
                               ) -> Optional[DeviceCapability]:
