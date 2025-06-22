595cd56849aba2ff5b4a9a5630f7c6118d546ef2
Aaron Pham <contact@aarnphm.xyz>
2025-06-22 18:28:11 -0400

    chore: update theme and bind correct item
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- docs/mkdocs/javascript/shortcuts.js
+++ docs/mkdocs/javascript/shortcuts.js
@@ -1,7 +1,14 @@
+document$.subscribe(() => {
+  const isMac = window.navigator.userAgent.toLowerCase().includes("mac")
+  const shortcut = isMac ? "⌘+k" : "ctrl+k";
+  document.documentElement.style.setProperty('--md-search-extension-shortcut', shortcut);
+})
+
 keyboard$.subscribe(key => {
+  const query = document.querySelector('[data-md-component="search-query"]')
   if (key.mode === 'global' && key.type === 'k' && (key.ctrlKey || key.metaKey)) {
     key.claim();
-    query$.focus();
-    query$.select();
+    query.focus();
+    query.select();
   }
 })
