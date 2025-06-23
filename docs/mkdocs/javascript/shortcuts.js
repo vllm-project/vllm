5b2bbc50a4219888a9a1b54f35021e391bc4c595
Aaron Pham <contact@aarnphm.xyz>
2025-06-23 12:14:36 -0400

    chore: use raw addEventListener
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- docs/mkdocs/javascript/shortcuts.js
+++ docs/mkdocs/javascript/shortcuts.js
@@ -2,11 +2,13 @@ const isMac = window.navigator.userAgent.toLowerCase().includes("mac")
 const shortcut = isMac ? "⌘+k" : "ctrl+k";
 document.documentElement.style.setProperty('--md-search-extension-shortcut', shortcut);
 
-keyboard$.subscribe(key => {
-  const query = document.querySelector('[data-md-component="search-query"]')
-  if (key.mode === 'global' && key.type === 'k' && (key.ctrlKey || key.metaKey)) {
-    key.claim();
-    query.focus();
-    query.select();
+document.addEventListener('keydown', function (event) {
+  if ((event.ctrlKey || event.metaKey) && event.key.toLowerCase() === 'k') {
+    event.preventDefault();
+    const query = document.querySelector('[data-md-component="search-query"]');
+    if (query) {
+      query.focus();
+      query.select();
+    }
   }
-})
+});
