6d2f19cccb0315062f6f93d6a889ceea72645368
Aaron Pham <contact@aarnphm.xyz>
2025-06-24 17:39:14 -0400

    chore: update js
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- docs/mkdocs/javascript/shortcuts.js
+++ docs/mkdocs/javascript/shortcuts.js
@@ -1,13 +1,13 @@
 document.addEventListener('keydown', function (event) {
-  if (event.key === '/') {
+  if (event.key.lower() === '/') {
     // we actually don't want to bind this key on docs given that it uses builtin mkdocs search.
     event.preventDefault();
-  } elif ((event.altKey || event.metaKey) && event.key === 'k') {
+  } else if ((event.altKey || event.metaKey) && event.key.lower() === 'k') {
     event.preventDefault();
     const query = document.querySelector('[data-md-component="search-query"]');
     if (query) {
       query.focus();
       query.select();
     }
-  }
+  };
 });
