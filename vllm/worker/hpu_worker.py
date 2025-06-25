1f60b754a9cca4a085f490f097383270a3bb3120
Xinyu Chen <xichen@habana.ai>
2025-06-25 14:50:51 +0800

    DP: Fix init_device for DP (#1464)
    Co-authored-by: Kacper Pietkun <kacper.pietkun@intel.com>

--- vllm/worker/hpu_worker.py
+++ vllm/worker/hpu_worker.py
@@ -206,7 +206,10 @@ class HPUWorker(LocalOrDistributedWorkerBase):
     def init_device(self) -> None:
         if self.device_config.device.type == "hpu":
             self.device = torch.device("hpu")
-            torch.hpu.set_device(self.local_rank)
+            if envs.VLLM_DP_SIZE > 1:
+                torch.hpu.set_device(self.device)
+            else:
+                torch.hpu.set_device(self.local_rank)
         elif self.device_config.device_type == "cpu":
             self.device = torch.device("cpu")
         else:
