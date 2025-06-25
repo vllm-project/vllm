d3783291c8f88ad2f43cfebf0b2d844b7bb2722d
bnellnm <49004751+bnellnm@users.noreply.github.com>
2025-06-25 02:22:58 -0400

    [Kernels][Bugfix] Use torch op for all kernels in FusedMoE forward.  Add additional testing for cudagraphs. (#19717)
    Signed-off-by: Bill Nell <bnell@redhat.com>
    Signed-off-by: kyolebu <kyu@redhat.com>

--- vllm/model_executor/layers/fused_moe/deep_gemm_moe.py
+++ vllm/model_executor/layers/fused_moe/deep_gemm_moe.py
@@ -48,7 +48,7 @@ def _valid_deep_gemm(hidden_states: torch.Tensor, w1: torch.Tensor,
     M = hidden_states.size(0)
     _, K, N = w2.size()
     if not _valid_deep_gemm_shape(M, N, K):
-        logger.debug("DeepGemm disabled: unalinged problem size.")
+        logger.debug("DeepGemm disabled: unaligned problem size.")
         return False
 
     if (w1.dtype != torch.float8_e4m3fn or w2.dtype != torch.float8_e4m3fn):
