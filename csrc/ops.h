29b7146a862e09858aee31666e1bcafc5e703ee1
li haoyang <haoyanli@amd.com>
2025-06-27 11:54:24 +0800

    [Feature] add quick all reduce (#19744)
    Signed-off-by: ilmarkov <imarkov@redhat.com>
    Signed-off-by: Haoyang Li <Haoyang.Li@amd.com>
    Co-authored-by: ilmarkov <imarkov@redhat.com>
    Signed-off-by: Diego-Castan <diego.castan@ibm.com>

--- csrc/ops.h
+++ csrc/ops.h
@@ -360,3 +360,14 @@ std::tuple<int64_t, torch::Tensor> allocate_shared_buffer_and_handle(
     int64_t size);
 int64_t open_mem_handle(torch::Tensor& mem_handle);
 void free_shared_buffer(int64_t buffer);
+
+#ifdef USE_ROCM
+fptr_t init_custom_qr(int64_t rank, int64_t world_size,
+                      std::optional<int64_t> qr_max_size = std::nullopt);
+void qr_destroy(fptr_t _fa);
+torch::Tensor qr_get_handle(fptr_t _fa);
+void qr_open_handles(fptr_t _fa, const std::vector<torch::Tensor>& handles);
+void qr_all_reduce(fptr_t _fa, torch::Tensor& inp, torch::Tensor& out,
+                   int64_t quant_level, bool cast_bf2half = false);
+int64_t qr_max_size();
+#endif
