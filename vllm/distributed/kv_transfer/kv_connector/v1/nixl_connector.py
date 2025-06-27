f9617c75ad8a077ca9492aa60c5687e637a855c5
rshaw@neuralmagic.com <robertgshaw2@gmail.com>
2025-06-27 18:48:05 +0000

    updated
    Signed-off-by: rshaw@neuralmagic.com <robertgshaw2@gmail.com>

--- vllm/distributed/kv_transfer/kv_connector/v1/nixl_connector.py
+++ vllm/distributed/kv_transfer/kv_connector/v1/nixl_connector.py
@@ -968,7 +968,10 @@ class NixlConnectorWorker:
         )
 
         # Begin async xfer.
+        start = time.perf_counter()
         self.nixl_wrapper.transfer(handle)
+        end = time.perf_counter()
+        logger.info("======== LAUNCH TIME: %s ========", end - start)
 
         # Use handle to check completion in future step().
         # TODO (NickLucche) surface xfer elapsed time
