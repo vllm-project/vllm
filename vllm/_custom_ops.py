2c51269de103a9fe757001a0fbff8f3213ef4302
Michael Goin <mgoin64@gmail.com>
2025-06-28 14:48:13 +0900

    Fix num_token_padding support for static per-tensor scaled_fp8_quant (#20188)
    Signed-off-by: mgoin <mgoin64@gmail.com>
    Signed-off-by: Diego-Castan <diego.castan@ibm.com>

--- vllm/_custom_ops.py
+++ vllm/_custom_ops.py
@@ -1274,8 +1274,7 @@ def scaled_fp8_quant(
             scale = torch.zeros(1, device=input.device, dtype=torch.float32)
             torch.ops._C.dynamic_scaled_fp8_quant(output, input, scale)
     else:
-        # num_token_padding not implemented for this case
-        assert (scale.numel() == 1 and num_token_padding is None)
+        assert scale.numel() == 1
         torch.ops._C.static_scaled_fp8_quant(output, input, scale)
 
     return output, scale
