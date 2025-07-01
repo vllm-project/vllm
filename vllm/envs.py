2bc79a5c7884b05ce2ddbdacf8351c9b16ecdbf4
Alex-Brooks <Alex.Brooks@ibm.com>
2025-07-01 15:14:10 +0000

    Consolidate resolver, add entrypoint
    Signed-off-by: Alex-Brooks <Alex.Brooks@ibm.com>

--- vllm/envs.py
+++ vllm/envs.py
@@ -86,6 +86,7 @@ if TYPE_CHECKING:
     VLLM_HTTP_TIMEOUT_KEEP_ALIVE: int = 5  # seconds
     VLLM_PLUGINS: list[str] | None = None
     VLLM_LORA_RESOLVER_CACHE_DIR: str | None = None
+    VLLM_LORA_RESOLVER_HF_REPO: str | None = None
     VLLM_TORCH_PROFILER_DIR: str | None = None
     VLLM_TORCH_PROFILER_RECORD_SHAPES: bool = False
     VLLM_TORCH_PROFILER_WITH_PROFILE_MEMORY: bool = False
@@ -798,6 +799,12 @@ environment_variables: dict[str, Callable[[], Any]] = {
     # VLLM_ALLOW_RUNTIME_LORA_UPDATING is enabled.
     "VLLM_LORA_RESOLVER_CACHE_DIR": lambda: os.getenv(
         "VLLM_LORA_RESOLVER_CACHE_DIR", None
+    ),
+    # A remote HF repo containing one or more LoRA adapters, which
+    # may be downloaded and leveraged as needed. Only works if plugins
+    # are enabled and VLLM_ALLOW_RUNTIME_LORA_UPDATING is enabled.
+    "VLLM_LORA_RESOLVER_HF_REPO": lambda: os.getenv(
+        "VLLM_LORA_RESOLVER_HF_REPO", None
     ),
     # Enables torch profiler if set.
     # Both AsyncLLM's CPU traces as well as workers'
