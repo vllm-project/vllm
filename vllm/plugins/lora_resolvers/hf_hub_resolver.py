731493a7eef30c1c23beb56cc0d741503f97e6c2
Alex-Brooks <Alex.Brooks@ibm.com>
2025-07-01 16:53:21 +0000

    do hf calls/download in async threads
    Signed-off-by: Alex-Brooks <Alex.Brooks@ibm.com>

--- vllm/plugins/lora_resolvers/hf_hub_resolver.py
+++ vllm/plugins/lora_resolvers/hf_hub_resolver.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+import asyncio
 import os
 
 from huggingface_hub import HfApi, snapshot_download
@@ -13,29 +14,24 @@ from vllm.plugins.lora_resolvers.filesystem_resolver import FilesystemResolver
 class HfHubResolver(FilesystemResolver):
     def __init__(self, repo_name: str):
         self.repo_name = repo_name
-        # At initialization time, get all directories
-        # in the repo containing an adapter_config.json;
-        # these are the dirs we will allows downloads for
-        # for potential LoRA requests.
-        repo_files = HfApi().list_repo_files(repo_id=repo_name)
-        self.adapter_dirs = {
-            os.path.dirname(name)
-            for name in repo_files
-            if name.endswith("adapter_config.json")
-        }
+        self.adapter_dirs: None | set[str] = None
 
     async def resolve_lora(
         self, base_model_name: str, lora_name: str
     ) -> LoRARequest | None:
         """Resolves potential LoRA requests in a remote repo on HF Hub.
         This is effectively the same behavior as the filesystem resolver, but
-        with an extra guard + snapshot_download on dirs containing an adapter
-        config prior to inspecting the cached dir to build a potential LoRA
+        with a snapshot_download on dirs containing an adapter config prior
+        to inspecting the cached dir to build a potential LoRA
         request.
         """
+        if self.adapter_dirs is None:
+            self.adapter_dirs = await self._get_adapter_dirs()
         if lora_name in self.adapter_dirs:
-            repo_path = snapshot_download(
-                repo_id=self.repo_name, allow_patterns=f"{lora_name}/*"
+            repo_path = await asyncio.to_thread(
+                snapshot_download,
+                repo_id=self.repo_name,
+                allow_patterns=f"{lora_name}/*",
             )
             lora_path = os.path.join(repo_path, lora_name)
             maybe_lora_request = await self._get_lora_req_from_path(
@@ -43,6 +39,17 @@ class HfHubResolver(FilesystemResolver):
             )
             return maybe_lora_request
         return None
+
+    async def _get_adapter_dirs(self) -> set[str]:
+        """Gets the subpaths within a HF repo containing an adapter config."""
+        repo_files = await asyncio.to_thread(
+            HfApi().list_repo_files, repo_id=self.repo_name
+        )
+        return {
+            os.path.dirname(name)
+            for name in repo_files
+            if name.endswith("adapter_config.json")
+        }
 
 
 def register_hf_hub_resolver():
