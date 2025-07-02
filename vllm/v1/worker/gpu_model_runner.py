c1160e99603d3ec85c7f0db47309d63e57cd506c
Sanger Steel <sangersteel@gmail.com>
2025-07-02 11:51:51 -0400

    chore: Apply suggested change
    Signed-off-by: Sanger Steel <sangersteel@gmail.com>

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -1819,7 +1819,7 @@ class GPUModelRunner(LoRAModelRunnerMixin):
     ) -> None:
         TensorizerLoader.save_model(self.model,
                                     tensorizer_config=tensorizer_config,
-                                    model_config=self.model_config)
+                                    model_config=self.model_config,)
 
     def _get_prompt_logprobs_dict(
         self,
