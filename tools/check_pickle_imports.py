d4eb6fd5240fd4271bd622df6c0ddb4f5315f9a4
Jee Jee Li <pandaleefree@gmail.com>
2025-07-03 09:16:15 +0000

    Done
    Signed-off-by: Jee Jee Li <pandaleefree@gmail.com>

--- tools/check_pickle_imports.py
+++ tools/check_pickle_imports.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 # SPDX-License-Identifier: Apache-2.0
 import os
 import sys
