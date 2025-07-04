9496cbd6c37361d78ed1460e9c798e99cb91def2
Thomas Parnell <tpa@zurich.ibm.com>
2025-07-04 19:46:53 +0200

    Enable V1 for Hybrid SSM/Attention Models (#20016)
    Signed-off-by: Thomas Parnell <tpa@zurich.ibm.com>
    Co-authored-by: Stanislaw Wozniak <stw@zurich.ibm.com>
    Co-authored-by: Tyler Michael Smith <tysmith@redhat.com>
    Co-authored-by: Chen Zhang <zhangch99@outlook.com>
    Signed-off-by: Jinzhen Lin <linjinzhen@hotmail.com>

--- tests/v1/test_oracle.py
+++ tests/v1/test_oracle.py
@@ -13,7 +13,6 @@ UNSUPPORTED_MODELS_V1 = [
     "openai/whisper-large-v3",  # transcription
     "facebook/bart-large-cnn",  # encoder decoder
     "state-spaces/mamba-130m-hf",  # mamba1
-    "hmellor/tiny-random-BambaForCausalLM",  # hybrid
     "BAAI/bge-m3",  # embedding
 ]
 
