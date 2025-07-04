73fbef91f7e9714f5bc76a63ee3fa2220d034996
Jee Jee Li <pandaleefree@gmail.com>
2025-07-04 15:40:42 +0800

    [Misc] Add SPDX-FileCopyrightText (#20428)
    Signed-off-by: Jee Jee Li <pandaleefree@gmail.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- examples/online_serving/openai_chat_completion_client_with_tools_xlam_streaming.py
+++ examples/online_serving/openai_chat_completion_client_with_tools_xlam_streaming.py
@@ -1,4 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 # ruff: noqa: E501
 """
 Set up this example by starting a vLLM OpenAI-compatible server with tool call
