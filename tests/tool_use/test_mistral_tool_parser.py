170a04e47a3b7b3286e2e8ff36014a230b61b07e
avigny <47987522+avigny@users.noreply.github.com>
2025-07-07 01:02:46 +0200

    Update non streaming tests with v11 tokenizer and tool call format
    Signed-off-by: avigny <47987522+avigny@users.noreply.github.com>

--- tests/tool_use/test_mistral_tool_parser.py
+++ tests/tool_use/test_mistral_tool_parser.py
@@ -15,17 +15,24 @@ from vllm.entrypoints.openai.tool_parsers import MistralToolParser
 from vllm.transformers_utils.detokenizer import detokenize_incrementally
 from vllm.transformers_utils.tokenizer import AnyTokenizer, get_tokenizer
 
-MODEL = "mistralai/Mistral-7B-Instruct-v0.3"
-
 
 @pytest.fixture(scope="module")
 def mistral_tokenizer():
+    MODEL = "mistralai/Mistral-7B-Instruct-v0.3"
     return get_tokenizer(tokenizer_name=MODEL)
 
+@pytest.fixture(scope="module")
+def mistral_v11_tokenizer():
+    MODEL = "mistralai/Mistral-Small-3.2-24B-Instruct-2506"
+    return get_tokenizer(tokenizer_name=MODEL, tokenizer_mode="mistral")
 
 @pytest.fixture
 def mistral_tool_parser(mistral_tokenizer):
     return MistralToolParser(mistral_tokenizer)
+
+@pytest.fixture
+def mistral_v11_tool_parser(mistral_v11_tokenizer):
+    return MistralToolParser(mistral_v11_tokenizer)
 
 
 def assert_tool_calls(actual_tool_calls: list[ToolCall],
@@ -162,6 +169,108 @@ def test_extract_tool_calls(mistral_tool_parser, model_output,
 
     assert extracted_tool_calls.content == expected_content
 
+@pytest.mark.parametrize(
+    ids=[
+        "single_tool_add", "single_tool_weather",
+    ],
+    argnames=["model_output", "expected_tool_calls", "expected_content"],
+    argvalues=[
+        (
+            '''[TOOL_CALLS]add{"a": 3.5, "b": 4}''',  # noqa: E501
+            [
+                ToolCall(function=FunctionCall(name="add",
+                                               arguments=json.dumps({
+                                                   "a": 3.5,
+                                                   "b": 4
+                                               })))
+            ],
+            None),
+        (
+            '''[TOOL_CALLS]get_current_weather{"city": "San Francisco", "state": "CA", "unit": "celsius"}''',  # noqa: E501
+            [
+                ToolCall(function=FunctionCall(name="get_current_weather",
+                                               arguments=json.dumps(
+                                                   {
+                                                       "city": "San Francisco",
+                                                       "state": "CA",
+                                                       "unit": "celsius"
+                                                   })))
+            ],
+            None)
+    ]
+)
+def test_extract_tool_calls_v11_tokenizer(mistral_v11_tool_parser, model_output,
+                            expected_tool_calls, expected_content):
+    extracted_tool_calls = mistral_v11_tool_parser.extract_tool_calls(
+        model_output, request=None)  # type: ignore[arg-type]
+    assert extracted_tool_calls.tools_called
+
+    assert_tool_calls(extracted_tool_calls.tool_calls, expected_tool_calls)
+
+    assert extracted_tool_calls.content == expected_content
+
+
+def _test_extract_tool_calls_streaming(tool_parser, tokenizer,
+                                      model_output, expected_tool_calls,
+                                      expected_content):
+    other_content: str = ''
+    function_names: list[str] = []
+    function_args_strs: list[str] = []
+    tool_call_idx: int = -1
+    tool_call_ids: list[Optional[str]] = []
+
+    for delta_message in stream_delta_message_generator(
+            tool_parser, tokenizer, model_output):
+        # role should never be streamed from tool parser
+        assert not delta_message.role
+
+        if delta_message.content:
+            other_content += delta_message.content
+
+        streamed_tool_calls = delta_message.tool_calls
+
+        if streamed_tool_calls and len(streamed_tool_calls) > 0:
+            # make sure only one diff is present - correct even for parallel
+            assert len(streamed_tool_calls) == 1
+            tool_call = streamed_tool_calls[0]
+
+            # if a new tool is being called, set up empty arguments
+            if tool_call.index != tool_call_idx:
+                tool_call_idx = tool_call.index
+                function_args_strs.append("")
+                tool_call_ids.append(None)
+
+            # if a tool call ID is streamed, make sure one hasn't been already
+            if tool_call.id and not tool_call_ids[tool_call.index]:
+                tool_call_ids[tool_call.index] = tool_call.id
+
+            # if parts of the function start being streamed
+            if tool_call.function:
+                # if the function name is defined, set it. it should be streamed
+                # IN ENTIRETY, exactly one time.
+                if tool_call.function.name:
+                    assert isinstance(tool_call.function.name, str)
+                    function_names.append(tool_call.function.name)
+
+                if tool_call.function.arguments:
+                    # make sure they're a string and then add them to the list
+                    assert isinstance(tool_call.function.arguments, str)
+
+                    function_args_strs[
+                        tool_call.index] += tool_call.function.arguments
+
+    assert other_content == expected_content
+
+    actual_tool_calls = [
+        ToolCall(id=tool_call_id,
+                 function=FunctionCall(
+                     name=function_name,
+                     arguments=partial_json_parser.ensure_json(
+                         function_args_str, Allow.OBJ | Allow.STR)))
+        for tool_call_id, function_name, function_args_str in zip(
+            tool_call_ids, function_names, function_args_strs)
+    ]
+    assert_tool_calls(actual_tool_calls, expected_tool_calls)
 
 @pytest.mark.parametrize(
     ids=[
