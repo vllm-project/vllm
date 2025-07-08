4af5159fa1a5f2dc91069033e4cafaa76a5f1988
Andrew Feldman <afeldman@redhat.com>
2025-07-08 10:31:44 -0400

    entrypoint example
    Signed-off-by: Andrew Feldman <afeldman@redhat.com>

---
+++ examples/offline_inference/logits_processor/logits_processor_entrypoint.py
@@ -0,0 +1,43 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+
+from vllm import LLM, SamplingParams
+
+# Sample prompts.
+prompts = [
+    "Hello, my name is",
+    "The president of the United States is",
+    "The capital of France is",
+    "The future of AI is",
+]
+# Create a mixture of requests which do and don't utilize the dummy logitproc
+sampling_params_list = [
+    SamplingParams(temperature=0.8, top_p=0.95, extra_args={"target_token": 128}),
+    SamplingParams(temperature=0.8, top_p=0.95),
+    SamplingParams(temperature=0.8, top_p=0.95, extra_args={"target_token": 67}),
+    SamplingParams(temperature=0.8, top_p=0.95),
+]
+
+
+def main():
+    # Create an LLM.
+    llm = LLM(
+        model="facebook/opt-125m",
+        logits_processors_entrypoints=["dummy_logitproc"],
+    )
+    # Generate texts from the prompts.
+    # The output is a list of RequestOutput objects
+    # that contain the prompt, generated text, and other information.
+    outputs = llm.generate(prompts, sampling_params_list)
+    # Print the outputs.
+    print("\nGenerated Outputs:\n" + "-" * 60)
+    for output in outputs:
+        prompt = output.prompt
+        generated_text = output.outputs[0].text
+        print(f"Prompt:    {prompt!r}")
+        print(f"Output:    {generated_text!r}")
+        print("-" * 60)
+
+
+if __name__ == "__main__":
+    main()
