fb4fd8593720f22a6e49d575e13c063bb6cb5395
Sanger Steel <sangersteel@gmail.com>
2025-07-08 01:47:43 -0400

    [Frontend] [Core] Integrate Tensorizer in to S3 loading machinery, allow passing arbitrary arguments during save/load (#19619)
    Signed-off-by: Sanger Steel <sangersteel@gmail.com>
    Co-authored-by: Eta <esyra@coreweave.com>
    Signed-off-by: Diego-Castan <diego.castan@ibm.com>

--- setup.py
+++ setup.py
@@ -689,7 +689,7 @@ setup(
     install_requires=get_requirements(),
     extras_require={
         "bench": ["pandas", "datasets"],
-        "tensorizer": ["tensorizer>=2.9.0"],
+        "tensorizer": ["tensorizer==2.10.1"],
         "fastsafetensors": ["fastsafetensors >= 0.1.10"],
         "runai": ["runai-model-streamer", "runai-model-streamer-s3", "boto3"],
         "audio": ["librosa", "soundfile"],  # Required for audio processing
