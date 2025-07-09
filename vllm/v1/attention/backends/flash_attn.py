5a62e1c22de4485553a3c254e57fe2f5270289ed
Lucas Wilkinson <lwilkins@redhat.com>
2025-07-09 21:40:53 +0000

    review comments
    Signed-off-by: Lucas Wilkinson <lwilkins@redhat.com>

--- vllm/v1/attention/backends/flash_attn.py
+++ vllm/v1/attention/backends/flash_attn.py
@@ -212,6 +212,10 @@ class FlashAttentionMetadataBuilder(
               common_prefix_len: int,
               common_attn_metadata: CommonAttentionMetadata,
               fast_build: bool = False) -> FlashAttentionMetadata:
+        """
+        fast_build disables AOT scheduling, used when there will be few 
+        iterations i.e. spec-decode
+        """
         num_reqs = common_attn_metadata.num_reqs
         num_actual_tokens = common_attn_metadata.num_actual_tokens
         max_query_len = common_attn_metadata.max_query_len
