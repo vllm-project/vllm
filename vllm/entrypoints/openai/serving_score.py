a7de96791cce8eef5ebab191b40d61cb45371b6e
yewentao256 <zhyanwentao@126.com>
2025-07-10 15:34:49 -0400

    remove space
    Signed-off-by: yewentao256 <zhyanwentao@126.com>

--- vllm/entrypoints/openai/serving_score.py
+++ vllm/entrypoints/openai/serving_score.py
@@ -216,7 +216,7 @@ class ServingScores(OpenAIServing):
                 # cross_encoder models defaults to using pad_token.
                 tokenized_prompts = await asyncio.gather(*(
                     tokenize_async(
-                        text=t1,  #  type: ignore[arg-type]
+                        text=t1,  # type: ignore[arg-type]
                         text_pair=t2,  # type: ignore[arg-type]
                         **tokenization_kwargs) for t1, t2 in input_pairs))
             else:
