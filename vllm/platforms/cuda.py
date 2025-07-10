c280dff1aed3ac8448ec32381643f7a335e5e808
Kunshang Ji <kunshang.ji@intel.com>
2025-07-11 01:36:22 +0800

    add/fix interface for cpu/cuda/rocm/xpu
    Signed-off-by: Kunshang Ji <kunshang.ji@intel.com>

--- vllm/platforms/cuda.py
+++ vllm/platforms/cuda.py
@@ -470,7 +470,7 @@ class CudaPlatformBase(Platform):
         return supported
 
     @classmethod
-    def empty_cache(cls, ):
+    def empty_cache(cls):
         torch.cuda.empty_cache()
 
     @classmethod
@@ -491,7 +491,7 @@ class CudaPlatformBase(Platform):
 
     @classmethod
     def synchronize(cls):
-        return torch.cuda.synchronize()
+        torch.cuda.synchronize()
 
 
 # NVML utils
