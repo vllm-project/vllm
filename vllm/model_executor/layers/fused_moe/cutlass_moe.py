ec4eb49f55af6184ea828789ef3e1447de5af8c2
yewentao256 <zhyanwentao@126.com>
2025-07-10 16:36:05 +0000

    log-debug-with-more-info
    Signed-off-by: yewentao256 <zhyanwentao@126.com>

--- vllm/model_executor/layers/fused_moe/cutlass_moe.py
+++ vllm/model_executor/layers/fused_moe/cutlass_moe.py
@@ -556,12 +556,20 @@ def _valid_cutlass_block_scaled_grouped_gemm(w1: torch.Tensor,
     _, K, N = w2.size()
     if not _valid_cutlass_block_scaled_grouped_gemm_shape(N, K):
         logger.debug(
-            "CutlassBlockScaledGroupedGemm disabled: unalinged problem size.")
+            "CutlassBlockScaledGroupedGemm disabled: unalinged problem size. "
+            "N: %s, K: %s",
+            N,
+            K,
+        )
         return False
 
     if (w1.dtype != torch.float8_e4m3fn or w2.dtype != torch.float8_e4m3fn):
         logger.debug(
-            "CutlassBlockScaledGroupedGemm disabled: invalid weight dtype(s).")
+            "CutlassBlockScaledGroupedGemm disabled: invalid weight dtype(s). "
+            "w1.dtype: %s, w2.dtype: %s",
+            w1.dtype,
+            w2.dtype,
+        )
         return False
 
     return True
