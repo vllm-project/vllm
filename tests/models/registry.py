1f3978c8b70009da9c453e13540d0a3db24972dd
Michael Goin <mgoin64@gmail.com>
2025-07-11 11:55:21 +0900

    [Model] Support HF format of minimax (#20211)
    Signed-off-by: mgoin <mgoin64@gmail.com>

--- tests/models/registry.py
+++ tests/models/registry.py
@@ -218,6 +218,8 @@ _TEXT_GENERATION_EXAMPLE_MODELS = {
                                          trust_remote_code=True),
     "MiniCPM3ForCausalLM": _HfExamplesInfo("openbmb/MiniCPM3-4B",
                                          trust_remote_code=True),
+    "MiniMaxForCausalLM": _HfExamplesInfo("MiniMaxAI/MiniMax-Text-01-hf",
+                                          min_transformers_version="4.53"),
     "MiniMaxText01ForCausalLM": _HfExamplesInfo("MiniMaxAI/MiniMax-Text-01",
                                                 trust_remote_code=True,
                                                 revision="a59aa9cbc53b9fb8742ca4e9e1531b9802b6fdc3"),  # noqa: E501
