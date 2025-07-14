11b1b0b60056b65dc29472129a47490ba120c23e
Pavani Majety <pmajety@nvidia.com>
2025-07-14 10:10:15 -0700

    ruff again
    Signed-off-by: Pavani Majety <pmajety@nvidia.com>

--- vllm/engine/arg_utils.py
+++ vllm/engine/arg_utils.py
@@ -1418,8 +1418,9 @@ class EngineArgs:
                 and not envs.is_set("VLLM_ATTENTION_BACKEND")
             ) or envs.VLLM_ATTENTION_BACKEND == "FLASH_ATTN_VLLM_V1"
             supported = False
-            if current_platform.is_rocm() or (current_platform.is_cuda()
-                  and current_platform.has_device_capability(100)):
+            if current_platform.is_rocm() or (
+                    current_platform.is_cuda()
+                    and current_platform.has_device_capability(100)):
                 supported = True
             elif fp8_attention and will_use_fa:
                 from vllm.attention.utils.fa_utils import (
