c74084942b250da8319cfdd461d9d6e5949bc921
ant-yy <vito.yy@antgroup.com>
2025-07-14 22:10:32 +0800

    [Model] Add Ling implementation (#20680)
    Signed-off-by: vito.yy <vito.yy@antgroup.com>
    Signed-off-by: Paul Pak <paulpak58@gmail.com>

--- vllm/model_executor/models/registry.py
+++ vllm/model_executor/models/registry.py
@@ -41,6 +41,7 @@ _TEXT_GENERATION_MODELS = {
     "BaiChuanForCausalLM": ("baichuan", "BaiChuanForCausalLM"),
     # baichuan-13b, lower case 'c' in the class name
     "BaichuanForCausalLM": ("baichuan", "BaichuanForCausalLM"),
+    "BailingMoeForCausalLM": ("bailing_moe", "BailingMoeForCausalLM"),
     "BambaForCausalLM": ("bamba", "BambaForCausalLM"),
     "BloomForCausalLM": ("bloom", "BloomForCausalLM"),
     "ChatGLMModel": ("chatglm", "ChatGLMForCausalLM"),
