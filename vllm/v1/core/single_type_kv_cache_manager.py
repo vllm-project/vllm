5a0ccdf95f118ec1c6646338196b655bee96d6f1
Lu Fang <fanglu@fb.com>
2025-07-14 11:31:03 -0700

    adding example and comment
    Signed-off-by: Lu Fang <fanglu@fb.com>

--- vllm/v1/core/single_type_kv_cache_manager.py
+++ vllm/v1/core/single_type_kv_cache_manager.py
@@ -411,6 +411,43 @@ class ChunkedLocalAttentionManager(SingleTypeKVCacheManager):
         kv_cache_spec: KVCacheSpec,
         use_eagle: bool,
     ) -> tuple[list[KVCacheBlock], ...]:
+        """
+        For chunked local attention, we need to find the longest cache hit
+        prefix of the blocks that is not longer than `max_length`. The prefix
+        should be a common prefix hit for all the kv cache groups in
+        `kv_cache_group_ids`. If no cache hit is found, return an empty list.
+        note we mark as computed if the whole block is outside of the local 
+        window, and set the block as null. Examples:
+
+        1. Attention chunk size of 8, block size of 4, max length of 15
+        for next token at 15th (zero-indexed), 8th - 14th tokens are in 
+        the window(needs lookup), 0th - 7th are not int the window, 
+        so they are already marked as computed. We check the complete 
+        block3 (8th - 11th tokens), Assume block 3 is hit, we will return 
+        [null, null, block 3], otherwise, we return [null, null]
+
+        2. Attention chunk size of 8, block size of 4, max length of 16
+        for next token at 16th (zero-indexed), 0th - 15th tokens are not 
+        in the window, so they are already marked as computed. 
+        we return 4 blocks[null, null, null, null]
+
+        3. Attention chunk size of 8, block size of 4, max length of 16, 
+        use_eagle is true,for next token at 16th (zero-indexed), 
+        we only look for cache hit for 0th - 14th tokens, since last token
+        15th needs to be recomputed for eagle, so we return [null, null, block 3] 
+        if block3 is hit and [null, null] if block3 is not hit.
+
+        Args:
+            block_hashes: The block hashes of the request.
+            max_length: The maximum length of the cache hit prefix.
+            kv_cache_group_ids: The ids of the kv cache groups.
+            block_pool: The block pool.
+            kv_cache_spec: The kv cache spec.
+            use_eagle: Whether to use eagle.
+
+        Returns:
+            A list of cached blocks
+        """
         assert isinstance(kv_cache_spec, ChunkedLocalAttentionSpec), (
             "ChunkedLocalAttentionManager can only be used for " +
             "chunked local attention groups")
