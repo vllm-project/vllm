498da59df677b5911f9aca95d17eecc7bc1e3486
moontidef <53668275+relic-yuexi@users.noreply.github.com>
2025-07-15 15:15:20 +0800

    Update tool_calls to fit with openai

--- vllm/entrypoints/openai/protocol.py
+++ vllm/entrypoints/openai/protocol.py
@@ -1566,7 +1566,7 @@ class ChatMessage(OpenAIBaseModel):
     annotations: Optional[OpenAIAnnotation] = None
     audio: Optional[OpenAIChatCompletionAudio] = None
     function_call: Optional[FunctionCall] = None
-    tool_calls: list[ToolCall] = Field(default_factory=list)
+    tool_calls: Optional[list[ToolCall]] = None
 
     # vLLM-specific fields that are not in OpenAI spec
     reasoning_content: Optional[str] = None
@@ -1620,7 +1620,7 @@ class DeltaMessage(OpenAIBaseModel):
     role: Optional[str] = None
     content: Optional[str] = None
     reasoning_content: Optional[str] = None
-    tool_calls: list[DeltaToolCall] = Field(default_factory=list)
+    tool_calls: Optional[list[DeltaToolCall]] = None
 
 
 class ChatCompletionResponseStreamChoice(OpenAIBaseModel):
