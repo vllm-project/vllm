1e864138b4e5a5a8b017d9d90dbc899a43a5c955
Rahul Tuli <rtuli@redhat.com>
2025-07-17 15:44:53 -0400

    Add: README.nd for eagle1 examples, remove --enforce-eager flag

--- examples/speculators_examples/eagle1/serve_llama.sh
+++ examples/speculators_examples/eagle1/serve_llama.sh
@@ -1,1 +1,1 @@
-VLLM_USE_V1=1 vllm serve nm-testing/eagle-llama3.1-8b-instruct-converted-0717 --enforce-eager --port 5000 >output.txt
+VLLM_USE_V1=1 VLLM_DISABLE_COMPILE_CACHE=1 vllm serve nm-testing/eagle-llama3.1-8b-instruct-converted-0717 --port 5000 >output.txt
