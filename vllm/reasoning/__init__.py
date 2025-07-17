953e08a3bf08f4a8e3cdea3988d1b8d4b286be6a
Sebastian Schönnenbeck <sebastian.schoennenbeck@comma-soft.com>
2025-07-17 10:45:42 +0200

    More general setup
    Signed-off-by: Sebastian Schönnenbeck <sebastian.schoennenbeck@comma-soft.com>

--- vllm/reasoning/__init__.py
+++ vllm/reasoning/__init__.py
@@ -5,14 +5,16 @@ from .abs_reasoning_parsers import ReasoningParser, ReasoningParserManager
 from .deepseek_r1_reasoning_parser import DeepSeekR1ReasoningParser
 from .granite_reasoning_parser import GraniteReasoningParser
 from .hunyuan_a13b_reasoning_parser import HunyuanA13BReasoningParser
-from .nemotron_reasoning_parser import NemotronReasoningParser
 from .qwen3_reasoning_parser import Qwen3ReasoningParser
+from .string_based_reasoning_parser import (NemotronReasoningParser,
+                                            get_string_based_reasoning_parser)
 
 __all__ = [
     "ReasoningParser",
     "ReasoningParserManager",
     "DeepSeekR1ReasoningParser",
     "GraniteReasoningParser",
+    "get_string_based_reasoning_parser",
     "HunyuanA13BReasoningParser",
     "NemotronReasoningParser",
     "Qwen3ReasoningParser",
