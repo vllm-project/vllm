65ee8f4331bf33032785faf070e77ef8b12d5ab3
Rahul Tuli <rtuli@redhat.com>
2025-07-18 14:10:33 -0400

    Fix bug with Qwen
    Signed-off-by: Rahul Tuli <rtuli@redhat.com>

--- vllm/model_executor/models/qwen2.py
+++ vllm/model_executor/models/qwen2.py
@@ -352,8 +352,8 @@ class Qwen2Model(nn.Module):
             hidden_states = intermediate_tensors["hidden_states"]
             residual = intermediate_tensors["residual"]
 
+        aux_hidden_states = []
         if len(self.aux_hidden_state_layers) > 0:
-            aux_hidden_states = []
             for idx, layer in enumerate(
                     self.layers[self.start_layer:self.end_layer]):
                 if idx in self.aux_hidden_state_layers:
