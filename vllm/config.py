464b73fb4384f30dfc1fc7b425004b91651b020f
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2025-07-18 06:05:28 -0700

    fix test
    Signed-off-by: Woosuk Kwon <woosuk.kwon@berkeley.edu>

--- vllm/config.py
+++ vllm/config.py
@@ -2771,6 +2771,11 @@ class SpeculativeConfig:
                             )
                 else:
                     self.method = "draft_model"
+                    raise NotImplementedError(
+                        "Speculative decoding with draft model is not "
+                        "supported yet. Please consider using other "
+                        "speculative decoding methods such as ngram, medusa, "
+                        "eagle, or deepseek_mtp.")
 
                 # Replace hf_config for EAGLE draft_model
                 if self.method in ("eagle", "eagle3"):
