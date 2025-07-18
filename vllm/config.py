db4b27b3f1d296b92fdb6563e92d04463f402dbf
Thomas Parnell <tpa@zurich.ibm.com>
2025-07-18 19:38:49 +0000

    Piecewise CUDA Graphs for Hybrid models
    Signed-off-by: Thomas Parnell <tpa@zurich.ibm.com>

--- vllm/config.py
+++ vllm/config.py
@@ -4357,6 +4357,7 @@ class CompilationConfig:
             self.splitting_ops = [] if self.full_cuda_graph else [
                 "vllm.unified_attention",
                 "vllm.unified_attention_with_output",
+                "vllm.mamba_mixer2",
             ]
 
 
