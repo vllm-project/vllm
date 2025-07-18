6f27dd42892117213d52ecc2c71320f3bc4fbed1
Tomasz Pawlowski <tpawlowski@habana.ai>
2025-07-18 13:19:33 +0200

    Fix AttributeError during shutdown of RayDistributedExecutor (#1599)
    This PR fixes two issues that occurs during shutdown() of
    `RayDistributedExecutor`:
    - It stores logger reference, so it's not destroyed before
    `shutdown(self)` is called
    ```
    Exception ignored in: <function LLMEngine.__del__ at 0x7f1fb8e39f30>
    Traceback (most recent call last):
      File "/workspace/vllm/vllm/engine/llm_engine.py", line 520, in __del__
      File "/workspace/vllm/vllm/executor/ray_distributed_executor.py", line 131, in shutdown
    AttributeError: 'NoneType' object has no attribute 'info'
    Exception ignored in: <function RayDistributedExecutor.__del__ at 0x7f1fa30aaa70>
    Traceback (most recent call last):
      File "/workspace/vllm/vllm/executor/ray_distributed_executor.py", line 659, in __del__
      File "/workspace/vllm/vllm/executor/ray_distributed_executor.py", line 131, in shutdown
    AttributeError: 'NoneType' object has no attribute 'info'
    ```
    - safely executes `self._run_workers` in `shutdown(self)`
    ```
    --- Logging error ---
    Exception ignored in: <function RayDistributedExecutor.__del__ at 0x7f3c5c966a70>
    Traceback (most recent call last):  
      File "/workspace/vllm/vllm/executor/ray_distributed_executor.py", line 662, in __del__  
      File "/workspace/vllm/vllm/executor/ray_distributed_executor.py", line 139, in shutdown  
      File "/workspace/vllm/vllm/executor/ray_distributed_executor.py", line 543, in _run_workers  
      File "/workspace/vllm/vllm/worker/worker_base.py", line 649, in execute_method  
      File "/usr/lib/python3.10/logging/__init__.py", line 1512, in exception  
      File "/usr/lib/python3.10/logging/__init__.py", line 1506, in error  
      File "/usr/lib/python3.10/logging/__init__.py", line 1624, in _log  
      File "/usr/lib/python3.10/logging/__init__.py", line 1634, in handle  
      File "/usr/lib/python3.10/logging/__init__.py", line 1696, in callHandlers  
      File "/usr/lib/python3.10/logging/__init__.py", line 968, in handle  
      File "/usr/lib/python3.10/logging/__init__.py", line 1108, in emit  
      File "/usr/lib/python3.10/logging/__init__.py", line 1022, in handleError  
      File "/usr/lib/python3.10/traceback.py", line 120, in print_exception  
      File "/usr/local/lib/python3.10/dist-packages/exceptiongroup/_formatting.py", line 246, in format  
      File "/usr/local/lib/python3.10/dist-packages/exceptiongroup/_formatting.py", line 62, in emit
    TypeError: 'NoneType' object is not callable 
    ```
    
    ---------
    
    Co-authored-by: Michał Kuligowski <mkuligowski@habana.ai>

--- vllm/executor/ray_distributed_executor.py
+++ vllm/executor/ray_distributed_executor.py
@@ -127,18 +127,19 @@ class RayDistributedExecutor(DistributedExecutorBase):
         self.shutdown_workers = True
         self.terminate_ray = True
 
-    def shutdown(self) -> None:
-        logger.info(
-            "Shutting down Ray distributed executor. If you see error log "
-            "from logging.cc regarding SIGTERM received, please ignore because "
-            "this is the expected termination process in Ray.")
+    def shutdown(self, logger=logger) -> None:
+        if logger is not None:
+            logger.info(
+                "Shutting down Ray distributed executor. If you see error log "
+                "from logging.cc regarding SIGTERM received, please ignore "
+                "because this is the expected termination process in Ray.")
         if getattr(self, 'shutdown_workers', False):
-            self._run_workers("shutdown")
             self.shutdown_workers = False
+            getattr(self._run_workers, 'shutdown', lambda: None)()
         if getattr(self, 'terminate_ray', False):
+            self.terminate_ray = False
             for worker in self.workers:
                 worker.__ray_terminate__.remote()
-            self.terminate_ray = False
         if hasattr(self, "forward_dag") and self.forward_dag is not None:
             self.forward_dag.teardown()
             import ray
