34f718981fa34df47cfd45693481dab61d5a50dc
CLFutureX <775523362@qq.com>
2025-07-18 17:06:57 +0800

    code optimizer Signed-off-by: CLFutureX <775523362@qq.com>

--- vllm/v1/core/sched/scheduler.py
+++ vllm/v1/core/sched/scheduler.py
@@ -256,8 +256,12 @@ class Scheduler(SchedulerInterface):
                         if req_index == len(scheduled_running_reqs):
                             no_schedule_running_reqs = self.running[req_index:]
                         else:
-                            scheduled_running_reqs_set = set(scheduled_running_reqs)
-                            no_schedule_running_reqs = [req for req in self.running if req not in scheduled_running_reqs_set]
+                            scheduled_running_reqs_set = set(
+                                scheduled_running_reqs)
+                            no_schedule_running_reqs = [
+                                req for req in self.running
+                                if req not in scheduled_running_reqs_set
+                            ] 
                         preempted_req = max(
                             no_schedule_running_reqs,
                             key=lambda r: (r.priority, r.arrival_time),
