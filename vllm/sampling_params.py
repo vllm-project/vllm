479a1c492d54310aed08e15aae3a017fbdb759fa
Sungjae Lee <33976427+llsj14@users.noreply.github.com>
2025-07-18 10:38:23 +0000

    rename parameter and logit processor
    Signed-off-by: Sungjae Lee <33976427+llsj14@users.noreply.github.com>
    Signed-off-by: Sungjae Lee <sung-jae.lee@navercorp.com>

--- vllm/sampling_params.py
+++ vllm/sampling_params.py
@@ -178,7 +178,7 @@ class SamplingParams(
         extra_args: Arbitrary additional args, that can be used by custom
             sampling implementations, plugins, etc. Not used by any in-tree
             sampling implementations.
-        max_think_tokens: Maximum number of tokens allowed for thinking
+        thinking_token_budget: Maximum number of tokens allowed for thinking
     """
 
     n: int = 1
@@ -228,7 +228,7 @@ class SamplingParams(
     _bad_words_token_ids: Optional[list[list[int]]] = None
 
     # Maximum number of tokens allowed for thinking operations.
-    max_think_tokens: Optional[int] = None
+    thinking_token_budget: Optional[int] = None
 
     @staticmethod
     def from_optional(
@@ -245,7 +245,7 @@ class SamplingParams(
         stop: Optional[Union[str, list[str]]] = None,
         stop_token_ids: Optional[list[int]] = None,
         bad_words: Optional[list[str]] = None,
-        max_think_tokens: Optional[int] = None,
+        thinking_token_budget: Optional[int] = None,
         include_stop_str_in_output: bool = False,
         ignore_eos: bool = False,
         max_tokens: Optional[int] = 16,
@@ -289,7 +289,7 @@ class SamplingParams(
             stop=stop,
             stop_token_ids=stop_token_ids,
             bad_words=bad_words,
-            max_think_tokens=max_think_tokens,
+            thinking_token_budget=thinking_token_budget,
             include_stop_str_in_output=include_stop_str_in_output,
             ignore_eos=ignore_eos,
             max_tokens=max_tokens,
@@ -558,7 +558,7 @@ class SamplingParams(
             f"stop={self.stop}, "
             f"stop_token_ids={self.stop_token_ids}, "
             f"bad_words={self.bad_words}, "
-            f"max_think_tokens={self.max_think_tokens}, "
+            f"thinking_token_budget={self.thinking_token_budget}, "
             f"include_stop_str_in_output={self.include_stop_str_in_output}, "
             f"ignore_eos={self.ignore_eos}, "
             f"max_tokens={self.max_tokens}, "
