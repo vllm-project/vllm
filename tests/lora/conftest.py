0c86898a67094d4d0a621b9424f060954f8e40a7
Jee Jee Li <pandaleefree@gmail.com>
2025-07-19 17:15:41 +0800

    [V0 deprecation] Remove long context LoRA (#21169)
    Signed-off-by: Jee Jee Li <pandaleefree@gmail.com>
    Signed-off-by: Diego-Castan <diego.castan@ibm.com>

--- tests/lora/conftest.py
+++ tests/lora/conftest.py
@@ -221,11 +221,6 @@ def phi2_lora_files():
     return snapshot_download(repo_id="isotr0py/phi-2-test-sql-lora")
 
 
-@pytest.fixture(scope="session")
-def long_context_lora_files_16k_1():
-    return snapshot_download(repo_id="SangBinCho/long_context_16k_testing_1")
-
-
 @pytest.fixture
 def llama_2_7b_engine_extra_embeddings():
     cleanup_dist_env_and_memory(shutdown_ray=True)
