fe68027a08ad873de57ebc1b103615ca0ee68fce
Robert Shaw <robshaw@redhat.com>
2025-07-20 22:45:45 +0000

    updated
    Signed-off-by: Robert Shaw <robshaw@redhat.com>

--- vllm/v1/engine/utils.py
+++ vllm/v1/engine/utils.py
@@ -617,7 +617,6 @@ def launch_core_engines(
             CoreEngine(index=i, local=True)
             for i in range(dp_rank, dp_rank + local_engine_count)
         ]
-    logger.info(f"{engines_to_handshake=}")
 
     # Whether the started engines will handshake only with co-located
     # front-end processes. In external_dp_lb mode, ranks > 0 handshake with
