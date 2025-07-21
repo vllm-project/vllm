d6196f7bf873e66c7ca9cce58574a32fbf093a49
Vensenmu <vensenmu@gmail.com>
2025-07-21 18:29:36 +0800

    Add SiglipSo400mVisionModel details
    Signed-off-by: Vensenmu <vensenmu@gmail.com>

--- tests/models/multimodal/processing/test_common.py
+++ tests/models/multimodal/processing/test_common.py
@@ -317,6 +317,7 @@ def _test_processing_correctness_one(
     "openai/whisper-large-v3",
     "omni-research/Tarsier-7b",
     "omni-research/Tarsier2-Recap-7b",
+    "HuggingFaceM4/siglip-so400m-14-980-flash-attn2-navit"
 ])
 @pytest.mark.parametrize("hit_rate", [0.3, 0.5, 1.0])
 @pytest.mark.parametrize("num_batches", [32])
