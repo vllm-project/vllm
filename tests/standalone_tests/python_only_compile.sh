dffd84b6b369337af16fee405345e99b08fd59c2
atalman <atalman@fb.com>
2025-07-21 11:12:01 -0700

    test_cudnn

--- tests/standalone_tests/python_only_compile.sh
+++ tests/standalone_tests/python_only_compile.sh
@@ -22,6 +22,8 @@ echo 'import os; os.system("touch /tmp/changed.file")' >> vllm/__init__.py
 VLLM_TEST_USE_PRECOMPILED_NIGHTLY_WHEEL=1 VLLM_USE_PRECOMPILED=1 pip3 install -vvv -e . \
   --extra-index-url https://download.pytorch.org/whl/test/cu128
 
+pip3 install nvidia-cudnn-cu12==9.5.1.17 --force-reinstall
+
 # Run the script
 python3 -c 'import vllm'
 
