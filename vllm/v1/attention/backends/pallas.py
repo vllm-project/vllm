ed45838880929b961157ab1c69e9ce6db3072fa4
Lucas Wilkinson <LucasWilkinson@users.noreply.github.com>
2025-07-21 12:10:30 -0400

    [Attention] Clean up iRoPE in V1 (#21188)
    Signed-off-by: Lucas Wilkinson <lwilkins@redhat.com>
    Co-authored-by: Michael Goin <mgoin64@gmail.com>
    Signed-off-by: Jinzhen Lin <linjinzhen@hotmail.com>

--- vllm/v1/attention/backends/pallas.py
+++ vllm/v1/attention/backends/pallas.py
@@ -148,12 +148,7 @@ class PallasAttentionBackendImpl(AttentionImpl):
         logits_soft_cap: Optional[float] = None,
         attn_type: str = AttentionType.DECODER,
         kv_sharing_target_layer_name: Optional[int] = None,
-        use_irope: bool = False,
     ) -> None:
-        if use_irope:
-            logger.warning_once(
-                "Using irope in Pallas is not supported yet, it will fall back "
-                "to global attention for long context.")
         self.num_heads = num_heads
         self.head_size = head_size
         self.scale = float(scale)
