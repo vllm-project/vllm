a80bea91483625515cfd14cb51960cb90e26fdf4
Konrad Zawora <kzawora@habana.ai>
2025-07-21 17:16:25 +0300

    remove unneeded if
    Signed-off-by: Konrad Zawora <kzawora@habana.ai>

--- vllm/platforms/cuda.py
+++ vllm/platforms/cuda.py
@@ -593,10 +593,8 @@ class NonNvmlCudaPlatform(CudaPlatformBase):
         fp8_attention = kv_cache_dtype.startswith("fp8")
         will_use_fa = (not envs.is_set("VLLM_ATTENTION_BACKEND")
                        ) or envs.VLLM_ATTENTION_BACKEND == "FLASH_ATTN_VLLM_V1"
-        supported = False
-        if cls.is_device_capability(100):
-            supported = True
-        elif fp8_attention and will_use_fa:
+        supported = cls.is_device_capability(100)
+        if fp8_attention and will_use_fa:
             from vllm.attention.utils.fa_utils import flash_attn_supports_fp8
             supported = flash_attn_supports_fp8()
         return supported
