365511993b7bc12935029bbee6a9635c21a47e1e
Jialin Ouyang <Jialin.Ouyang@gmail.com>
2025-07-21 12:21:23 -0700

    Added a TODO to clean up incr_ref and decr_ref
    Signed-off-by: Jialin Ouyang <Jialin.Ouyang@gmail.com>

--- vllm/v1/core/kv_cache_utils.py
+++ vllm/v1/core/kv_cache_utils.py
@@ -154,6 +154,8 @@ class KVCacheBlock:
     # Whether the block is a null block that should never be cached.
     is_null: bool = False
 
+    # TODO(Jialin): For performance, let callers to handle ref_cnt bumps to
+    # avoid function calls.
     def incr_ref(self):
         self.ref_cnt += 1
 
