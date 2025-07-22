18df6482ddde26c9a27bd166707c71c0900fc6fe
mgoin <mgoin64@gmail.com>
2025-07-22 16:41:08 -0400

    Add back missed fp8 block case
    Signed-off-by: mgoin <mgoin64@gmail.com>

--- vllm/model_executor/layers/quantization/utils/fp8_utils.py
+++ vllm/model_executor/layers/quantization/utils/fp8_utils.py
@@ -833,14 +833,21 @@ def validate_fp8_block_shape(input_size: int, output_size: int,
             f"is not divisible by weight quantization block_k = {block_k}.")
 
     # Required by column parallel or enabling merged weights
-    if ((tp_size > 1 and output_size // sum(output_partition_sizes) == tp_size)
-            or len(output_partition_sizes) > 1):
-        for output_partition_size in output_partition_sizes:
+    is_tp_split = (tp_size > 1
+                   and output_size // sum(output_partition_sizes) == tp_size)
+    is_merged_gemm = len(output_partition_sizes) > 1
+    if is_tp_split or is_merged_gemm:
+        sizes_to_check = output_partition_sizes
+        if not is_tp_split and is_merged_gemm:
+            # In case of merged matrices, we allow the last
+            # matrix to not be a multiple of block size
+            sizes_to_check = output_partition_sizes[:-1]
+        for output_partition_size in sizes_to_check:
             if output_partition_size % block_n != 0:
                 raise ValueError(
-                    f"Weight output_partition_size = {output_partition_size} "
-                    "is not divisible by weight quantization "
-                    f"block_n = {block_n}.")
+                    f"Weight output_partition_size = "
+                    f"{output_partition_size} is not divisible by "
+                    f"weight quantization block_n = {block_n}.")
 
 
 def create_fp8_weight_parameter(
