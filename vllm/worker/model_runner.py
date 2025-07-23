8e49131849afc4c5dc45991ec9a024cd4763dcd9
KazusatoOko <kazusto.oko@sakana.ai>
2025-07-23 16:07:28 +0000

    Fix the synchronization error for tensor_parallel_size>=2
    Signed-off-by: KazusatoOko <kazusto.oko@sakana.ai>

--- vllm/worker/model_runner.py
+++ vllm/worker/model_runner.py
@@ -1941,14 +1941,14 @@ class ModelRunner(GPUModelRunnerBase[ModelInputForGPUWithSamplingMetadata]):
                     valid_outputs.append(sequence_group_output)
                     sampled_token_ids.append(
                         sequence_group_output.samples[0].output_token)
-                sampled_token_ids = torch.tensor(sampled_token_ids
-                    ).to(self.device)
+                sampled_token_ids = torch.tensor(sampled_token_ids).to(
+                    self.device)
                 sampled_token_ids = broadcast_tensor_dict(
                     {"sampled_token_ids": 
-                        sampled_token_ids})["sampled_token_ids"]
+                    sampled_token_ids})["sampled_token_ids"]
             else:
                 sampled_token_ids = broadcast_tensor_dict(
-                    )["sampled_token_ids"]
+                )["sampled_token_ids"]
             if len(sampled_token_ids) > 0:
                 sampled_token_embeds = \
                     self.model.get_input_embeddings(sampled_token_ids)
