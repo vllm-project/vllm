afbde12701d360192d4eeee2abf732c62784b06d
Shintarou Okada <kokuzen@gmail.com>
2025-07-24 18:56:36 +0900

    Replace `--expand-tools-even-if-tool-choice-none` with `--exclude-tools-when-tool-choice-none` for v0.10.0 (#20544)
    Signed-off-by: okada <kokuzen@gmail.com>
    Signed-off-by: okada shintarou <okada@preferred.jp>
    Co-authored-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- vllm/entrypoints/openai/api_server.py
+++ vllm/entrypoints/openai/api_server.py
@@ -1646,6 +1646,8 @@ async def init_app_state(
         chat_template_content_format=args.chat_template_content_format,
         return_tokens_as_token_ids=args.return_tokens_as_token_ids,
         enable_auto_tools=args.enable_auto_tool_choice,
+        exclude_tools_when_tool_choice_none=args.
+        exclude_tools_when_tool_choice_none,
         tool_parser=args.tool_call_parser,
         reasoning_parser=args.reasoning_parser,
         enable_prompt_tokens_details=args.enable_prompt_tokens_details,
