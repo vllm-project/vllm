5835afde6dada75bc775d49461577512a093f083
Boyuan Feng <boyuan@meta.com>
2025-07-23 19:44:04 -0700

    nit
    Signed-off-by: Boyuan Feng <boyuan@meta.com>

--- vllm/utils/__init__.py
+++ vllm/utils/__init__.py
@@ -87,6 +87,9 @@ DEFAULT_MAX_NUM_BATCHED_TOKENS = 2048
 POOLING_MODEL_MAX_NUM_BATCHED_TOKENS = 32768
 MULTIMODAL_MODEL_MAX_NUM_BATCHED_TOKENS = 5120
 
+# an arbitrary factor to estimate the cudagraph memory usage
+_CUDAGRAPH_MEMORY_ESTIMATION_FACTOR = 4
+
 # Exception strings for non-implemented encoder/decoder scenarios
 
 # Reminder: Please update docs/features/compatibility_matrix.md
@@ -2758,16 +2761,17 @@ def memory_profiling(
     peak_activation_memory = result.torch_peak_increase
 
     # cudagraph memory
-    # an arbitrary factor to estimate the cudagraph memory usage
-    cudagraph_memory_factor = 4
     # peak_activation_memory is measured with max_num_batched_tokens
     # but max_capture_size is the maximum batch size for cudagraph
     # capture. So we scale peak_activation_memory and give some
     # buffers via cudagraph_memory_factor.
+    assert vllm_config.scheduler_config.max_num_batched_tokens > 0, (
+        "max_num_batched_tokens is expected to be positive "
+        "for estimating CUDAGraph memory usage. Please file an issue.")
     estimated_cudagraph_memory = peak_activation_memory * (
         vllm_config.compilation_config.max_capture_size /
         vllm_config.scheduler_config.max_num_batched_tokens
-    ) * cudagraph_memory_factor
+    ) * _CUDAGRAPH_MEMORY_ESTIMATION_FACTOR
     cudagraph_memory = (estimated_cudagraph_memory
                         if vllm_config.compilation_config.use_cudagraph else 0)
 
