4c5795c55f3cc8f33e014246cc0b80e63f3ac7ca
vllmellm <vllm.ellm@embeddedllm.com>
2025-07-25 11:13:34 +0000

    fix merge conflict
    Signed-off-by: vllmellm <vllm.ellm@embeddedllm.com>

--- vllm/v1/attention/backends/rocm_aiter_fa.py
+++ vllm/v1/attention/backends/rocm_aiter_fa.py
@@ -8,7 +8,8 @@ import torch
 
 from vllm import _custom_ops as ops
 from vllm.attention.backends.abstract import (AttentionBackend, AttentionImpl,
-                                              AttentionMetadata, AttentionType)
+                                              AttentionMetadata, AttentionType,
+                                              is_quantized_kv_cache)
 from vllm.config import VllmConfig
 from vllm.logger import init_logger
 from vllm.platforms import current_platform
