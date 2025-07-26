7733bb95d7f796a75545427fd86695fa9ca35fdb
Wentao Ye <44945378+yewentao256@users.noreply.github.com>
2025-07-25 20:07:07 -0400

    [Perf] Cuda Kernel for Int8 Per Token Group Quant (#21476)
    Signed-off-by: yewentao256 <zhyanwentao@126.com>
    Signed-off-by: Paul Pak <paulpak58@gmail.com>

--- csrc/quantization/compressed_tensors/int8_quant_kernels.cu
+++ csrc/quantization/compressed_tensors/int8_quant_kernels.cu
@@ -1,6 +1,8 @@
 #include <ATen/cuda/CUDAContext.h>
 #include <torch/all.h>
 
+#include "../per_token_group_quant_8bit.h"
+
 #include <cmath>
 
 #include "../../dispatch_utils.h"
@@ -335,4 +337,12 @@ void dynamic_scaled_int8_quant(
                   hidden_size);
         }
       });
+}
+
+void per_token_group_quant_int8(const torch::Tensor& input,
+                                torch::Tensor& output_q,
+                                torch::Tensor& output_s, int64_t group_size,
+                                double eps, double int8_min, double int8_max) {
+  per_token_group_quant_8bit(input, output_q, output_s, group_size, eps,
+                             int8_min, int8_max);
 }
