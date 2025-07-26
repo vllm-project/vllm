0d423eda509857af0d89563f9c47509ad3d230e8
tovam <tovam@pliops.com>
2025-07-27 02:50:50 +0300

    Add stat logger plugin unit tests
    Signed-off-by: tovam <tovam@pliops.com>

---
+++ tests/plugins/vllm_add_dummy_stat_logger/setup.py
@@ -0,0 +1,14 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+
+from setuptools import setup
+
+setup(
+    name='vllm_add_dummy_stat_logger',
+    version='0.1',
+    packages=['vllm_add_dummy_stat_logger'],
+    entry_points={
+        'vllm.stat_logger_plugins': [
+            'dummy_stat_logger = vllm_add_dummy_stat_logger.dummy_stat_logger:DummyStatLogger'  # noqa
+        ]
+    })
