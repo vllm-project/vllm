1ecd6bdf2ae176351263bb0df33633edd01f6ae3
DarkLight1337 <tlleungac@connect.ust.hk>
2025-07-27 09:39:24 +0000

    Skip roberta seq cls for V1
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- tests/models/language/pooling/test_jina.py
+++ tests/models/language/pooling/test_jina.py
@@ -4,6 +4,7 @@ from functools import partial
 
 import pytest
 
+import vllm.envs as envs
 from vllm import PoolingParams
 
 from ...utils import EmbedModelInfo, RerankModelInfo
@@ -62,6 +63,10 @@ def test_embed_models_correctness(hf_runner, vllm_runner,
 @pytest.mark.parametrize("model_info", RERANK_MODELS)
 def test_rerank_models_mteb(hf_runner, vllm_runner,
                             model_info: RerankModelInfo) -> None:
+    if (model_info.architecture == "XLMRobertaForSequenceClassification"
+            and envs.VLLM_USE_V1):
+        pytest.skip("Not supported yet")
+
     mteb_test_rerank_models(hf_runner, vllm_runner, model_info)
 
 
