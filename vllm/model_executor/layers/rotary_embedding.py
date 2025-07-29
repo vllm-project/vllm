034c756c697dffcc527ddb175a9fab919feb220f
Chendi.Xue <chendi.xue@intel.com>
2025-07-29 02:17:44 -0500

    [SW-234344] Fix 'RotaryEmbedding' object has no attribute 'sin' (#1659)
    ## Essential Elements of an Effective PR Description Checklist
    - [x] The purpose of the PR, such as "Fix some issue (link existing
    issues this PR will resolve)".
    - [ ] The test plan, such as providing test command.
    - [ ] The test results, such as pasting the results comparison before
    and after, or e2e results
    
    
    ## Purpose
    
    port commit from #1658 for fixing SW-234344 for habana_main
    
    ## Test Plan
    
    ## Test Result
    
    <!--- pyml disable-next-line no-emphasis-as-heading -->
    
    Signed-off-by: Chendi.Xue <chendi.xue@intel.com>

--- vllm/model_executor/layers/rotary_embedding.py
+++ vllm/model_executor/layers/rotary_embedding.py
@@ -268,7 +268,8 @@ class RotaryEmbedding(CustomOp):
         # Prepare cos-sin caches for long-context + LoRA with offsets for every
         # forward, since the offset information wasn't available previously
         if hasattr(self, "scaling_factors") or hasattr(
-                self, "scaling_factor") or self.sin is None:
+                self, "scaling_factor") or not hasattr(
+                    self, "sin") or self.sin is None:
             self.prepare_cos_sin(positions, offsets)
 
         num_tokens = positions.numel()
@@ -812,7 +813,8 @@ class Phi3LongRoPEScaledRotaryEmbedding(CustomOp):
         rope_mode: RotaryPosEmbeddingMode
         rope_mode = RotaryPosEmbeddingMode.BLOCKWISE
 
-        if hasattr(self, "scaling_factors") or self.sin is None:
+        if hasattr(self, "scaling_factors") or not hasattr(
+                self, "sin") or self.sin is None:
             self.prepare_cos_sin(positions, offsets)
         if self.recompute_cos_sin:
             self.prepare_cos_sin(positions, offsets, recompute_cos_sin=True)
