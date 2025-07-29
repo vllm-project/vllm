d062ba86a58330f80d06e5a25ea1cb0cd16fdfcd
HelloWorldU <asd001liu@gamil.com>
2025-07-29 23:47:50 +0800

    Add Kimi-Audio integration for vLLM

--- vllm/transformers_utils/processors/__init__.py
+++ vllm/transformers_utils/processors/__init__.py
@@ -4,5 +4,6 @@
 from vllm.transformers_utils.processors.deepseek_vl2 import (
     DeepseekVLV2Processor)
 from vllm.transformers_utils.processors.ovis import OvisProcessor
+from vllm.transformers_utils.processors.kimi_audio import KimiAudioProcessor
 
-__all__ = ["DeepseekVLV2Processor", "OvisProcessor"]
+__all__ = ["DeepseekVLV2Processor", "OvisProcessor", "KimiAudioProcessor"]
