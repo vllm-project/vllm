9f5d4ab209ceb9329e54b9e28d661a1ab50262e9
yewentao256 <zhyanwentao@126.com>
2025-07-30 13:50:29 -0400

    use strided only
    Signed-off-by: yewentao256 <zhyanwentao@126.com>

--- csrc/quantization/fp8/common.cu
+++ csrc/quantization/fp8/common.cu
@@ -12,74 +12,6 @@
 namespace vllm {
 
 template <typename scalar_t, typename fp8_type>
-__global__ void scaled_fp8_quant_kernel(fp8_type* __restrict__ out,
-                                        const scalar_t* __restrict__ input,
-                                        const float* __restrict__ scale,
-                                        int64_t num_elems) {
-  int tid = blockDim.x * blockIdx.x + threadIdx.x;
-
-  // Invert the scale so that we can use multiplications to avoid expensive
-  // division.
-  const float inverted_scale = 1.0f / (*scale);
-  vectorize_with_alignment<16>(
-      input, out, num_elems, tid, blockDim.x * gridDim.x,
-      [=] __device__(fp8_type & dst, const scalar_t& src) {
-        dst = scaled_fp8_conversion<true, fp8_type>(static_cast<float>(src),
-                                                    inverted_scale);
-      });
-}
-
-template <typename scalar_t, typename fp8_type>
-__global__ void dynamic_per_token_scaled_fp8_quant_kernel(
-    fp8_type* __restrict__ out, float* __restrict__ scale,
-    scalar_t const* __restrict__ input, float const* __restrict__ scale_ub,
-    const int hidden_size) {
-  int const tid = threadIdx.x;
-  int const token_idx = blockIdx.x;
-
-  // Use int64 to avoid overflowing an int32 when calculating this offset
-  int64_t offset = static_cast<int64_t>(token_idx) * hidden_size;
-  scalar_t const* __restrict__ token_input = &input[offset];
-  fp8_type* __restrict__ token_output = &out[offset];
-
-  // 1) compute per-token absmax
-  float absmax_val = 0.0f;
-  vectorize_read_with_alignment<16>(token_input, hidden_size, tid, blockDim.x,
-                                    [&] __device__(const scalar_t& src) {
-                                      const float v =
-                                          fabsf(static_cast<float>(src));
-                                      absmax_val = fmaxf(absmax_val, v);
-                                    });
-
-  using BlockReduce = cub::BlockReduce<float, 256>;
-  __shared__ typename BlockReduce::TempStorage reduceStorage;
-  float const block_absmax_val_maybe =
-      BlockReduce(reduceStorage).Reduce(absmax_val, cub::Max{}, blockDim.x);
-  __shared__ float token_scale;
-  if (tid == 0) {
-    if (scale_ub) {
-      token_scale = fminf(block_absmax_val_maybe, *scale_ub);
-    } else {
-      token_scale = block_absmax_val_maybe;
-    }
-    // token scale computation
-    token_scale = fmaxf(token_scale / quant_type_max_v<fp8_type>,
-                        min_scaling_factor<fp8_type>::val());
-    scale[token_idx] = token_scale;
-  }
-  __syncthreads();
-
-  // 2) quantize
-  // Note that we don't use inverted scales so we can match FBGemm impl.
-  vectorize_with_alignment<16>(
-      token_input, token_output, hidden_size, tid, blockDim.x,
-      [=] __device__(fp8_type & dst, const scalar_t& src) {
-        dst = scaled_fp8_conversion<false, fp8_type>(static_cast<float>(src),
-                                                     token_scale);
-      });
-}
-
-template <typename scalar_t, typename fp8_type>
 __global__ void scaled_fp8_quant_kernel_strided(
     fp8_type* __restrict__ out, const scalar_t* __restrict__ input,
     const float* __restrict__ scale, int hidden_size, int64_t in_row_stride,
@@ -215,8 +147,6 @@ void static_scaled_fp8_quant(torch::Tensor& out,          // [..., d]
 
   const int64_t in_row_stride = input.stride(-2);
   const int64_t out_row_stride = out.stride(-2);
-  const bool is_contig_rows =
-      (in_row_stride == hidden_size) && (out_row_stride == hidden_size);
 
   const at::cuda::OptionalCUDAGuard device_guard(device_of(input));
   const cudaStream_t stream = at::cuda::getCurrentCUDAStream();
@@ -224,19 +154,11 @@ void static_scaled_fp8_quant(torch::Tensor& out,          // [..., d]
       input.scalar_type(), "scaled_fp8_quant_kernel_scalar_type", [&] {
         VLLM_DISPATCH_FP8_TYPES(
             out.scalar_type(), "scaled_fp8_quant_kernel_fp8_type", [&] {
-              if (is_contig_rows) {
-                const int num_elems = input.numel();
-                vllm::scaled_fp8_quant_kernel<scalar_t, fp8_t>
-                    <<<grid, block, 0, stream>>>(
-                        out.data_ptr<fp8_t>(), input.data_ptr<scalar_t>(),
-                        scale.data_ptr<float>(), num_elems);
-              } else {
-                vllm::scaled_fp8_quant_kernel_strided<scalar_t, fp8_t>
-                    <<<grid, block, 0, stream>>>(
-                        out.data_ptr<fp8_t>(), input.data_ptr<scalar_t>(),
-                        scale.data_ptr<float>(), hidden_size, in_row_stride,
-                        out_row_stride);
-              }
+              vllm::scaled_fp8_quant_kernel_strided<scalar_t, fp8_t>
+                  <<<grid, block, 0, stream>>>(
+                      out.data_ptr<fp8_t>(), input.data_ptr<scalar_t>(),
+                      scale.data_ptr<float>(), hidden_size, in_row_stride,
+                      out_row_stride);
             });
       });
 }
@@ -258,43 +180,27 @@ void dynamic_scaled_fp8_quant(torch::Tensor& out,          // [..., d]
 
   const int64_t in_row_stride = input.stride(-2);
   const int64_t out_row_stride = out.stride(-2);
-  const bool is_contig_rows =
-      (in_row_stride == hidden_size) && (out_row_stride == hidden_size);
 
   const at::cuda::OptionalCUDAGuard device_guard(device_of(input));
   const cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 
   // scale tensor should be initialised to <=0 before reduction
-  if (!is_contig_rows) {
-    scale.fill_(0.0f);
-  }
+  scale.fill_(0.0f);
 
   VLLM_DISPATCH_FLOATING_TYPES(
       input.scalar_type(), "scaled_fp8_quant_kernel_scalar_type", [&] {
         VLLM_DISPATCH_FP8_TYPES(
             out.scalar_type(), "scaled_fp8_quant_kernel_fp8_type", [&] {
-              if (is_contig_rows) {
-                const int num_elems = input.numel();
-                vllm::segmented_max_reduction<scalar_t, fp8_t>
-                    <<<grid, block, 0, stream>>>(scale.data_ptr<float>(),
-                                                 input.data_ptr<scalar_t>(),
-                                                 num_elems);
-                vllm::scaled_fp8_quant_kernel<scalar_t, fp8_t>
-                    <<<grid, block, 0, stream>>>(
-                        out.data_ptr<fp8_t>(), input.data_ptr<scalar_t>(),
-                        scale.data_ptr<float>(), num_elems);
-              } else {
-                vllm::segmented_max_reduction_strided<scalar_t, fp8_t>
-                    <<<grid, block, 0, stream>>>(
-                        scale.data_ptr<float>(), input.data_ptr<scalar_t>(),
-                        hidden_size, in_row_stride, (int64_t)num_tokens);
+              vllm::segmented_max_reduction_strided<scalar_t, fp8_t>
+                  <<<grid, block, 0, stream>>>(
+                      scale.data_ptr<float>(), input.data_ptr<scalar_t>(),
+                      hidden_size, in_row_stride, (int64_t)num_tokens);
 
-                vllm::scaled_fp8_quant_kernel_strided_dynamic<scalar_t, fp8_t>
-                    <<<grid, block, 0, stream>>>(
-                        out.data_ptr<fp8_t>(), input.data_ptr<scalar_t>(),
-                        scale.data_ptr<float>(), hidden_size, in_row_stride,
-                        out_row_stride);
-              }
+              vllm::scaled_fp8_quant_kernel_strided_dynamic<scalar_t, fp8_t>
+                  <<<grid, block, 0, stream>>>(
+                      out.data_ptr<fp8_t>(), input.data_ptr<scalar_t>(),
+                      scale.data_ptr<float>(), hidden_size, in_row_stride,
+                      out_row_stride);
             });
       });
 }
@@ -316,8 +222,6 @@ void dynamic_per_token_scaled_fp8_quant(
 
   const int64_t in_row_stride = input.stride(-2);
   const int64_t out_row_stride = out.stride(-2);
-  const bool is_contig_rows =
-      (in_row_stride == hidden_size) && (out_row_stride == hidden_size);
 
   const at::cuda::OptionalCUDAGuard device_guard(device_of(input));
   const cudaStream_t stream = at::cuda::getCurrentCUDAStream();
@@ -327,23 +231,12 @@ void dynamic_per_token_scaled_fp8_quant(
         VLLM_DISPATCH_FP8_TYPES(
             out.scalar_type(),
             "dynamic_per_token_scaled_fp8_quant_kernel_fp8_type", [&] {
-              if (is_contig_rows) {
-                vllm::dynamic_per_token_scaled_fp8_quant_kernel<scalar_t, fp8_t>
-                    <<<grid, block, 0, stream>>>(
-                        out.data_ptr<fp8_t>(), scales.data_ptr<float>(),
-                        input.data_ptr<scalar_t>(),
-                        scale_ub.has_value() ? scale_ub->data_ptr<float>()
-                                             : nullptr,
-                        hidden_size);
-              } else {
-                vllm::dynamic_per_token_scaled_fp8_quant_kernel_strided<
-                    scalar_t, fp8_t><<<grid, block, 0, stream>>>(
-                    out.data_ptr<fp8_t>(), scales.data_ptr<float>(),
-                    input.data_ptr<scalar_t>(),
-                    scale_ub.has_value() ? scale_ub->data_ptr<float>()
-                                         : nullptr,
-                    hidden_size, in_row_stride, out_row_stride);
-              }
+              vllm::dynamic_per_token_scaled_fp8_quant_kernel_strided<
+                  scalar_t, fp8_t><<<grid, block, 0, stream>>>(
+                  out.data_ptr<fp8_t>(), scales.data_ptr<float>(),
+                  input.data_ptr<scalar_t>(),
+                  scale_ub.has_value() ? scale_ub->data_ptr<float>() : nullptr,
+                  hidden_size, in_row_stride, out_row_stride);
             });
       });
 }
--- csrc/quantization/fp8/common.cuh
+++ csrc/quantization/fp8/common.cuh
@@ -55,45 +55,4 @@ __device__ __forceinline__ fp8_type scaled_fp8_conversion(float const val,
 #endif
 }
 
-// Compute the absolute maximum m of the input tensor and store
-// m / float8_e4m3::max() in *scale. Each thread block performs a
-// reduction tree and the memory in scale is atomically updated.
-// So to get the right answer, *scale needs to be initialized to
-// a value <= 0.0 and we need to wait for all thread blocks to
-// finish before consuming *scale.
-template <typename scalar_t, typename fp8_type>
-__global__ void segmented_max_reduction(float* __restrict__ scale,
-                                        const scalar_t* __restrict__ input,
-                                        int64_t num_elems) {
-  __shared__ float cache[256];
-  int64_t i = blockDim.x * blockIdx.x + threadIdx.x;
-
-  // First store maximum for all values processes by
-  // the current thread in cache[threadIdx.x]
-  scalar_t tmp = 0.0;
-  while (i < num_elems) {
-    float x = static_cast<float>(input[i]);
-    tmp = fmaxf(tmp, fabsf(x));
-    i += blockDim.x * gridDim.x;
-  }
-  cache[threadIdx.x] = tmp;
-
-  __syncthreads();
-
-  // Now perform parallel reduction within the thread block
-  int ib = blockDim.x / 2;
-  while (ib != 0) {
-    if (threadIdx.x < ib && cache[threadIdx.x + ib] > cache[threadIdx.x]) {
-      cache[threadIdx.x] = cache[threadIdx.x + ib];
-    }
-    __syncthreads();
-    ib /= 2;
-  }
-  // Finally, since cache[0] contains the maximum for this thread block,
-  // atomically write the max to the target location
-  if (threadIdx.x == 0) {
-    atomicMaxFloat(scale, cache[0] / quant_type_max_v<fp8_type>);
-  }
-}
-
 }  // namespace vllm
