166e85ea0ef236484273f5c1460aa6c6aa4f7e7e
DarkLight1337 <tlleungac@connect.ust.hk>
2025-07-30 03:22:53 +0000

    Doc
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- vllm/transformers_utils/processors/__init__.py
+++ vllm/transformers_utils/processors/__init__.py
@@ -5,6 +5,7 @@ Multi-modal processors may be defined in this directory for the following
 reasons:
 
 - There is no processing file defined by HF Hub or Transformers library.
+- There is a need to override the existing processor to support vLLM.
 """
 
 from vllm.transformers_utils.processors.deepseek_vl2 import (
