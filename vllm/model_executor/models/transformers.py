0af4810b825ddb52f6bc5fdae48637f144450555
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-07-30 19:04:59 +0200

    Revert public method as it's too brittle to use for our purposes right now
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- vllm/model_executor/models/transformers.py
+++ vllm/model_executor/models/transformers.py
@@ -449,13 +449,15 @@ class TransformersBase(nn.Module, SupportsQuant, SupportsLoRA, SupportsPP):
                 sliding_window=self.config.interleaved_sliding_window)
 
         # Set correct attn and init on "meta" to delay allocating GPU tensors
+        # TODO: @raushan, use the public `model.set_attn_implementation()`
+        # method once its checks are fixed in Transformers.
+        self.text_config._attn_implementation = "vllm"
         with init_on_device_without_buffers("meta"), config_override:
             self.model: PreTrainedModel = AutoModel.from_config(
                 self.config,
                 torch_dtype=self.model_config.dtype,
                 trust_remote_code=self.model_config.trust_remote_code,
             )
-            self.model.set_attn_implementation("vllm")
 
         self.pipeline_parallel()
         self.tensor_parallel()
