5f8dbb0763c5fe52832124c2b264f0385f66e813
DarkLight1337 <tlleungac@connect.ust.hk>
2025-07-30 03:17:32 +0000

    [Misc] Remove redunand config definitions
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- vllm/transformers_utils/processors/__init__.py
+++ vllm/transformers_utils/processors/__init__.py
@@ -1,5 +1,11 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+"""
+Multi-modal processors may be defined in this directory for the following
+reasons:
+
+- There is no processing file defined by HF Hub or Transformers library.
+"""
 
 from vllm.transformers_utils.processors.deepseek_vl2 import (
     DeepseekVLV2Processor)
