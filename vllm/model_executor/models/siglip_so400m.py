d336fa333a1835f5b0b5a77af6a63c13e8f938f9
Vensenmu <vensenmu@gmail.com>
2025-08-02 23:10:29 +0800

    Add SiglipSo400mVisionModel details-5
    Signed-off-by: Vensenmu <vensenmu@gmail.com>

--- vllm/model_executor/models/siglip_so400m.py
+++ vllm/model_executor/models/siglip_so400m.py
@@ -7,25 +7,23 @@ import torch
 from torch import nn
 from transformers import SiglipVisionConfig
 
-from vllm.attention.layer import MultiHeadAttention
+from vllm.attention import Attention
 from vllm.distributed import divide, get_tensor_model_parallel_world_size
-from vllm.model_executor.layers.activation import get_act_fn
 from vllm.model_executor.layers.linear import (ColumnParallelLinear,
-                                               QKVParallelLinear,
                                                RowParallelLinear)
 from vllm.model_executor.layers.quantization import QuantizationConfig
-from vllm.model_executor.layers.vocab_parallel_embedding import (
-    VocabParallelEmbedding)
 from vllm.model_executor.model_loader.weight_utils import default_weight_loader
+from vllm.model_executor.models.siglip import SiglipMLP
 from vllm.model_executor.models.vision import VisionEncoderInfo
 
 
 class SiglipSo400mEncoderInfo(VisionEncoderInfo[SiglipVisionConfig]):
+    """Helper class to provide model-specific info for NaViT-based SigLIP."""
 
     def get_num_image_tokens(self, *, image_width: int,
                              image_height: int) -> int:
-        return (image_width // self.get_patch_size()) * (image_height //
-                                                         self.get_patch_size())
+        patch_size = self.get_patch_size()
+        return (image_height // patch_size) * (image_width // patch_size)
 
     def get_image_size(self) -> int:
         return self.vision_config.image_size
@@ -33,203 +31,243 @@ class SiglipSo400mEncoderInfo(VisionEncoderInfo[SiglipVisionConfig]):
     def get_patch_size(self) -> int:
         return self.vision_config.patch_size
 
-    def get_patch_grid_length(self) -> int:
-        image_size, patch_size = self.get_image_size(), self.get_patch_size()
-        return image_size // patch_size
 
-
-class SiglipSo400mVisionEmbeddings(nn.Module):
+class SiglipNavitVisionEmbeddings(nn.Module):
+    """NaViT-style Vision Embeddings layer."""
 
     def __init__(self, config: SiglipVisionConfig):
         super().__init__()
-        self.config = config
         self.embed_dim = config.hidden_size
+        self.image_size = config.image_size
+        self.patch_size = config.patch_size
         self.patch_embedding = nn.Conv2d(
             in_channels=config.num_channels,
             out_channels=self.embed_dim,
-            kernel_size=config.patch_size,
-            stride=config.patch_size,
-            padding=0,
+            kernel_size=self.patch_size,
+            stride=self.patch_size,
+            padding="valid",
         )
-        self.num_patches = (config.image_size // config.patch_size)**2
-        self.position_embedding = VocabParallelEmbedding(
-            num_embeddings=self.num_patches,
-            embedding_dim=self.embed_dim,
-        )
-        self.register_buffer(
-            "position_ids",
-            torch.arange(self.position_embedding.num_embeddings).unsqueeze(0),
-            persistent=False)
+        self.num_patches_per_side = self.image_size // self.patch_size
+        self.num_positions = self.num_patches_per_side**2
+        self.position_embedding = nn.Embedding(self.num_positions,
+                                               self.embed_dim)
 
-    def forward(self, pixel_values: torch.Tensor) -> torch.Tensor:
-        patch_embeds = self.patch_embedding(pixel_values)
+    def forward(self,
+                pixel_values: torch.FloatTensor,
+                patch_attention_mask: torch.BoolTensor,
+                tgt_sizes: Optional[torch.IntTensor] = None) -> torch.Tensor:
+        batch_size, _, max_im_h, max_im_w = pixel_values.shape
+        target_dtype = self.patch_embedding.weight.dtype
+        patch_embeds = self.patch_embedding(pixel_values.to(target_dtype))
         embeddings = patch_embeds.flatten(2).transpose(1, 2)
-        embeddings += self.position_embedding(
-            self.position_ids[:, :embeddings.size(1)])
+
+        device = embeddings.device
+        max_nb_patches_h = max_im_h // self.patch_size
+        max_nb_patches_w = max_im_w // self.patch_size
+
+        boundaries = torch.arange(1 / self.num_patches_per_side,
+                                  1.0,
+                                  1 / self.num_patches_per_side,
+                                  device=device)
+
+        position_ids = torch.full(size=(batch_size,
+                                        max_nb_patches_h * max_nb_patches_w),
+                                  fill_value=0,
+                                  device=device,
+                                  dtype=torch.long)
+
+        for batch_idx, p_attn_mask in enumerate(patch_attention_mask):
+            p_attn_mask = p_attn_mask.to(device)
+
+            nb_patches_h = p_attn_mask[:, 0].sum()
+            nb_patches_w = p_attn_mask[0].sum()
+
+            fractional_coords_h = torch.arange(0,
+                                               1 - 1e-6,
+                                               1 / nb_patches_h,
+                                               device=device)
+            fractional_coords_w = torch.arange(0,
+                                               1 - 1e-6,
+                                               1 / nb_patches_w,
+                                               device=device)
+
+            bucket_coords_h = torch.bucketize(fractional_coords_h,
+                                              boundaries,
+                                              right=True)
+            bucket_coords_w = torch.bucketize(fractional_coords_w,
+                                              boundaries,
+                                              right=True)
+
+            pos_ids = (bucket_coords_h[:, None] * self.num_patches_per_side +
+                       bucket_coords_w).flatten()
+
+            position_ids[batch_idx, p_attn_mask.view(-1)] = pos_ids
+
+        embeddings += self.position_embedding(position_ids)
         return embeddings
 
 
 class SiglipSo400mAttention(nn.Module):
 
-    def __init__(self,
-                 config: SiglipVisionConfig,
-                 quant_config: Optional[QuantizationConfig] = None):
+    def __init__(
+        self,
+        config: SiglipVisionConfig,
+        quant_config: Optional[QuantizationConfig] = None,
+    ) -> None:
         super().__init__()
         self.embed_dim = config.hidden_size
         self.num_heads = config.num_attention_heads
         self.head_dim = self.embed_dim // self.num_heads
-        self.scale = self.head_dim**-0.5
-
-        self.qkv_proj = QKVParallelLinear(
-            hidden_size=self.embed_dim,
-            head_size=self.head_dim,
-            total_num_heads=self.num_heads,
-            quant_config=quant_config,
-        )
-        self.out_proj = RowParallelLinear(
-            input_size=self.embed_dim,
-            output_size=self.embed_dim,
-            quant_config=quant_config,
-        )
 
         tp_size = get_tensor_model_parallel_world_size()
         self.num_heads_per_partition = divide(self.num_heads, tp_size)
-        self.attn = MultiHeadAttention(self.num_heads_per_partition,
-                                       self.head_dim, self.scale)
 
-    def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
-        qkv, _ = self.qkv_proj(hidden_states)
-        q, k, v = qkv.chunk(3, dim=-1)
+        self.q_proj = ColumnParallelLinear(self.embed_dim,
+                                           self.embed_dim,
+                                           bias=True,
+                                           quant_config=quant_config)
+        self.k_proj = ColumnParallelLinear(self.embed_dim,
+                                           self.embed_dim,
+                                           bias=True,
+                                           quant_config=quant_config)
+        self.v_proj = ColumnParallelLinear(self.embed_dim,
+                                           self.embed_dim,
+                                           bias=True,
+                                           quant_config=quant_config)
+        self.out_proj = RowParallelLinear(self.embed_dim,
+                                          self.embed_dim,
+                                          bias=True,
+                                          quant_config=quant_config)
+
+        scaling = self.head_dim**-0.5
+        self.attn = Attention(self.num_heads_per_partition, self.head_dim,
+                              scaling)
+
+    def forward(self,
+                hidden_states: torch.Tensor) -> tuple[torch.Tensor, None]:
+        q, _ = self.q_proj(hidden_states)
+        k, _ = self.k_proj(hidden_states)
+        v, _ = self.v_proj(hidden_states)
+
         out = self.attn(q, k, v)
-        out, _ = self.out_proj(out)
-        return out
 
-
-class SiglipSo400mMLP(nn.Module):
-
-    def __init__(self,
-                 config: SiglipVisionConfig,
-                 quant_config: Optional[QuantizationConfig] = None):
-        super().__init__()
-        self.fc1 = ColumnParallelLinear(
-            input_size=config.hidden_size,
-            output_size=config.intermediate_size,
-            quant_config=quant_config,
-        )
-        self.fc2 = RowParallelLinear(
-            input_size=config.intermediate_size,
-            output_size=config.hidden_size,
-            quant_config=quant_config,
-        )
-        self.act = get_act_fn(config.hidden_act)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x, _ = self.fc1(x)
-        x = self.act(x)
-        x, _ = self.fc2(x)
-        return x
+        attn_output, _ = self.out_proj(out)
+        return attn_output, None
 
 
 class SiglipSo400mEncoderLayer(nn.Module):
 
-    def __init__(self,
-                 config: SiglipVisionConfig,
-                 quant_config: Optional[QuantizationConfig] = None):
+    def __init__(
+        self,
+        config: SiglipVisionConfig,
+        quant_config: Optional[QuantizationConfig] = None,
+    ) -> None:
         super().__init__()
-        self.norm1 = nn.LayerNorm(config.hidden_size,
-                                  eps=config.layer_norm_eps)
-        self.attn = SiglipSo400mAttention(config, quant_config)
-        self.norm2 = nn.LayerNorm(config.hidden_size,
-                                  eps=config.layer_norm_eps)
-        self.mlp = SiglipSo400mMLP(config, quant_config)
+        self.self_attn = SiglipSo400mAttention(config,
+                                               quant_config=quant_config)
+        self.layer_norm1 = nn.LayerNorm(config.hidden_size,
+                                        eps=config.layer_norm_eps)
+        self.mlp = SiglipMLP(config, quant_config=quant_config)
+        self.layer_norm2 = nn.LayerNorm(config.hidden_size,
+                                        eps=config.layer_norm_eps)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        residual = x
-        x = self.norm1(x)
-        x = self.attn(x)
-        x = residual + x
+    def forward(self,
+                hidden_states: torch.Tensor) -> tuple[torch.Tensor, None]:
+        residual = hidden_states
+
+        x = self.layer_norm1(hidden_states)
+        x, _ = self.self_attn(x)
+        x = x + residual
 
         residual = x
-        x = self.norm2(x)
+        x = self.layer_norm2(x)
         x = self.mlp(x)
-        x = residual + x
-        return x
+        x = x + residual
+
+        return x, None
 
 
-class SiglipSo400mVisionTransformer(nn.Module):
+class SiglipSo400mMultiheadAttentionPoolingHead(nn.Module):
+
+    def __init__(
+        self,
+        config: SiglipVisionConfig,
+        quant_config: Optional[QuantizationConfig] = None,
+    ) -> None:
+        super().__init__()
+        self.probe = nn.Parameter(torch.randn(1, 1, config.hidden_size))
+        self.attention = torch.nn.MultiheadAttention(
+            config.hidden_size, config.num_attention_heads, batch_first=True)
+        self.layernorm = nn.LayerNorm(config.hidden_size,
+                                      eps=config.layer_norm_eps)
+        self.mlp = SiglipMLP(config=config, quant_config=quant_config)
+
+    def forward(self, hidden_state: torch.Tensor) -> torch.Tensor:
+        batch_size = hidden_state.shape[0]
+        probe = self.probe.repeat(batch_size, 1, 1)
+
+        hidden_state, _ = self.attention(probe, hidden_state, hidden_state)
+
+        residual = hidden_state
+        x = self.layernorm(hidden_state)
+        x = self.mlp(x)
+        x = x + residual
+
+        return x[:, 0]
+
+
+class SiglipSo400mVisionModel(nn.Module):
+    _supports_multimodal = True
+    config_class = SiglipVisionConfig
 
     def __init__(self,
                  config: SiglipVisionConfig,
                  quant_config: Optional[QuantizationConfig] = None):
         super().__init__()
         self.config = config
-        self.embeddings = SiglipSo400mVisionEmbeddings(config)
+        self.embeddings = SiglipNavitVisionEmbeddings(config)
+
         self.encoder_layers = nn.ModuleList([
             SiglipSo400mEncoderLayer(config, quant_config)
             for _ in range(config.num_hidden_layers)
         ])
-        self.norm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
 
-    def forward(self, pixel_values: torch.Tensor) -> torch.Tensor:
-        x = self.embeddings(pixel_values)
+        self.post_layernorm = nn.LayerNorm(config.hidden_size,
+                                           eps=config.layer_norm_eps)
+        self.head = SiglipSo400mMultiheadAttentionPoolingHead(
+            config, quant_config)
+
+    def forward(
+        self,
+        pixel_values: torch.Tensor,
+        patch_attention_mask: torch.BoolTensor,
+    ) -> torch.Tensor:
+        x = self.embeddings(pixel_values, patch_attention_mask)
+
         for layer in self.encoder_layers:
-            x = layer(x)
-        pooling_scheme = getattr(self.config, "pooling_scheme", "mean")
-        if pooling_scheme == "mean":
-            pooled_output = x.mean(dim=1)
-        elif pooling_scheme == "cls":
-            pooled_output = x[:, 0]
-        else:
-            raise ValueError(f"Unsupported pooling scheme: {pooling_scheme}")
+            x, _ = layer(x)
+
+        x = self.post_layernorm(x)
+        pooled_output = self.head(x)
         return pooled_output
 
-
-class SiglipSo400mVisionModel(nn.Module):
-
-    def __init__(self,
-                 config: SiglipVisionConfig,
-                 quant_config: Optional[QuantizationConfig] = None):
-        super().__init__()
-        self.encoder = SiglipSo400mVisionTransformer(config, quant_config)
-
-    def forward(self, pixel_values: torch.Tensor) -> torch.Tensor:
-        return self.encoder(pixel_values)
-
-    def load_weights(self, weights: Iterable[tuple[str,
-                                                   torch.Tensor]]) -> set[str]:
-        stacked_params_mapping = [
-            ("qkv_proj", "q_proj", "q"),
-            ("qkv_proj", "k_proj", "k"),
-            ("qkv_proj", "v_proj", "v"),
-        ]
+    def load_weights(self, weights: Iterable[tuple[str, torch.Tensor]]):
         params_dict = dict(self.named_parameters())
-        loaded_params: set[str] = set()
+        for hf_name, loaded_weight in weights:
+            if not hf_name.startswith("vision_model."):
+                continue
+            vllm_name = hf_name[len("vision_model."):]
+            vllm_name = vllm_name.replace("encoder.layers", "encoder_layers")
 
-        for name, loaded_weight in weights:
+            if "self_attn." in vllm_name:
+                vllm_name = vllm_name.replace("self_attn.", "self_attn.")
 
-            is_qkv = False
-            for (param_name, weight_name, shard_id) in stacked_params_mapping:
-                if weight_name not in name:
-                    continue
-
-                vllm_param_name = name.replace(weight_name, param_name)
-
-                if vllm_param_name in params_dict:
-                    param = params_dict[vllm_param_name]
-                    weight_loader = param.weight_loader
-                    weight_loader(param, loaded_weight, shard_id)
-                    is_qkv = True
-                    loaded_params.add(name)
-                    break
-
-            if is_qkv:
+            if "head.attention.in_proj" in vllm_name:
+                param = params_dict.get(vllm_name)
+                if param is not None:
+                    default_weight_loader(param, loaded_weight)
                 continue
 
-            if name in params_dict:
-                param = params_dict[name]
-                weight_loader = getattr(param, "weight_loader",
-                                        default_weight_loader)
-                weight_loader(param, loaded_weight)
-                loaded_params.add(name)
-
-        return loaded_params
+            param = params_dict.get(vllm_name)
+            if param is not None:
+                default_weight_loader(param, loaded_weight)
