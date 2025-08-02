5d32be35bcaea6509723e36986b7cc6d54c51ae6
Stefan Berger <stefanb@linux.ibm.com>
2025-08-02 12:17:41 +0000

    Improve reporting of error message if cause is known
    Extend the reporting of an error over RPC by the cause of the error if it
    is known. This then for example not only reports that the signature
    verification failed but also the reason, such as when an unsigned file was
    found.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- vllm/v1/engine/core.py
+++ vllm/v1/engine/core.py
@@ -787,8 +787,11 @@ class EngineCoreProc(EngineCore):
                 output.result = UtilityResult(result)
             except BaseException as e:
                 logger.exception("Invocation of %s method failed", method_name)
+                message = str(e)
+                if e.__cause__:
+                    message += f" caused by {e.__cause__}"
                 output.failure_message = (f"Call to {method_name} method"
-                                          f" failed: {str(e)}")
+                                          f" failed: {message}")
             self.output_queue.put_nowait(
                 (client_idx, EngineCoreOutputs(utility_output=output)))
         elif request_type == EngineCoreRequestType.EXECUTOR_FAILED:
