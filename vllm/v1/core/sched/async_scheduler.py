ba8ee40018e4ef3dc8204bb83e5e26f1b6ab8cd9
qizixi <qizixi@meta.com>
2025-08-04 14:47:58 -0700

    refactor async_scheduler to keep num_output_placeholders constant
    Signed-off-by: qizixi <qizixi@meta.com>

--- vllm/v1/core/sched/async_scheduler.py
+++ vllm/v1/core/sched/async_scheduler.py
@@ -20,11 +20,10 @@ class AsyncScheduler(Scheduler):
         super()._update_after_schedule(scheduler_output)
         for req_id in scheduler_output.num_scheduled_tokens:
             request = self.requests[req_id]
-            if (request.num_computed_tokens == request.num_tokens +
+            if (request.num_computed_tokens == request.num_tokens_with_spec +
                     request.num_output_placeholders):
                 # The request will generate a new token in this scheduling step.
-                # TODO(woosuk): Support speculative decoding.
-                request.num_output_placeholders += 1
+                request.num_output_placeholders = 1 + len(request.spec_token_ids)
 
     def _update_request_with_output(
         self,
@@ -34,10 +33,6 @@ class AsyncScheduler(Scheduler):
         status_before_update = request.status
         new_token_ids, stopped = super()._update_request_with_output(
             request, new_token_ids)
-
-        # Update the number of output placeholders.
-        request.num_output_placeholders -= len(new_token_ids)
-        assert request.num_output_placeholders >= 0
 
         # Cache the new tokens. Preempted requests should be skipped.
         if status_before_update == RequestStatus.RUNNING:
