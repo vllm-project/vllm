402039de7d98cd215bd070556a8bebc2f5eae0e3
zhouchong <zhouchong03@baidu.com>
2025-08-04 11:17:38 +0800

    [Model][V1] Support Ernie MTP
    Signed-off-by: zhouchong <zhouchong03@baidu.com>

--- vllm/model_executor/models/registry.py
+++ vllm/model_executor/models/registry.py
@@ -257,6 +257,7 @@ _SPECULATIVE_DECODING_MODELS = {
     "EagleLlama4ForCausalLM": ("llama4_eagle", "EagleLlama4ForCausalLM"),
     "EagleMiniCPMForCausalLM": ("minicpm_eagle", "EagleMiniCPMForCausalLM"),
     "Eagle3LlamaForCausalLM": ("llama_eagle3", "Eagle3LlamaForCausalLM"),
+    "ErnieMTPModel": ("ernie_mtp", "ErnieMTP"),
     "DeepSeekMTPModel": ("deepseek_mtp", "DeepSeekMTP"),
     "Glm4MoeMTPModel": ("glm4_moe_mtp", "Glm4MoeMTP"),
     "MedusaModel": ("medusa", "Medusa"),
