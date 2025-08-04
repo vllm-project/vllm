497f22bfad3d9a133984561c6e38e09c150a69ad
Thomas Parnell <tpa@zurich.ibm.com>
2025-08-04 17:29:12 -0400

    Initial enablement
    Signed-off-by: Thomas Parnell <tpa@zurich.ibm.com>

--- vllm/v1/attention/backends/mamba_selectors.py
+++ vllm/v1/attention/backends/mamba_selectors.py
@@ -1,12 +1,15 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 from vllm.attention.backends.abstract import AttentionBackend
+from vllm.v1.attention.backends.linear_attn import LinearAttentionBackend
 from vllm.v1.attention.backends.mamba_attn import Mamba2AttentionBackend
 
 
 def get_mamba_attn_backend(mamba_type: str) -> type[AttentionBackend]:
     if mamba_type == "mamba2":
         return Mamba2AttentionBackend
+    elif mamba_type == "MiniMaxText01LinearAttention":
+        return LinearAttentionBackend
 
     raise NotImplementedError(f"Mamba Attention type {mamba_type} is not "
                               "supported yet.")
