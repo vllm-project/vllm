c6adc294e122b84e124db3a536038ffe228a9441
Siyuan Liu <lsiyuan@google.com>
2025-08-05 22:40:08 +0000

    use env var
    Signed-off-by: Siyuan Liu <lsiyuan@google.com>

--- .buildkite/scripts/tpu/docker_run_bm.sh
+++ .buildkite/scripts/tpu/docker_run_bm.sh
@@ -12,7 +12,7 @@ source /etc/environment
 source $ENV_FILE
 
 remove_docker_container() { 
-    docker rm -f tpu-test || true;
+    docker rm -f $CONTAINER_NAME || true;
 }
 
 trap remove_docker_container EXIT
