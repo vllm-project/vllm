8bd045e9a3415825515d31055c4f07d675052b0f
Chen Zhang <zhangch99@outlook.com>
2025-08-04 19:24:03 -0700

    Support external tool call in response API + harmony (#16)

--- responses_api.py
+++ responses_api.py
@@ -1,13 +1,19 @@
 """
 vllm serve /data/woosuk/os-mini-weights/pytorch-rc-20b --tokenizer /data/xmo/os-mini/models/hf-converted --enforce-eager
 """
+import json
 import time
-from openai import OpenAI
+from openai import BadRequestError, NotFoundError, OpenAI
 import argparse
 
+import ast
+import requests
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--model", type=str, required=False, choices=["gpt-4.1", "o4-mini"])
+parser.add_argument("--model",
+                    type=str,
+                    required=False,
+                    choices=["gpt-4.1", "o4-mini"])
 parser.add_argument("--port", type=int, required=False, default=8000)
 args = parser.parse_args()
 
@@ -53,10 +59,22 @@ def test_chat():
     response = client.responses.create(
         model=MODEL,
         input=[
-            {"role": "system", "content": "Respond in Korean."},
-            {"role": "user", "content": "Hello!"},
-            {"role": "assistant", "content": "Hello! How can I help you today?"},
-            {"role": "user", "content": "What is 13 * 24? Explain your answer."},
+            {
+                "role": "system",
+                "content": "Respond in Korean."
+            },
+            {
+                "role": "user",
+                "content": "Hello!"
+            },
+            {
+                "role": "assistant",
+                "content": "Hello! How can I help you today?"
+            },
+            {
+                "role": "user",
+                "content": "What is 13 * 24? Explain your answer."
+            },
         ],
     )
     print(response)
@@ -68,7 +86,10 @@ def test_chat_with_input_type():
         input=[
             {
                 "role": "user",
-                "content": [{"type": "input_text", "text": "What is 13*24?"}],
+                "content": [{
+                    "type": "input_text",
+                    "text": "What is 13*24?"
+                }],
             },
         ],
     )
@@ -79,10 +100,14 @@ def test_structured_output():
     response = client.responses.create(
         model=MODEL,
         input=[
-            {"role": "system", "content": "Extract the event information."},
+            {
+                "role": "system",
+                "content": "Extract the event information."
+            },
             {
                 "role": "user",
-                "content": "Alice and Bob are going to a science fair on Friday.",
+                "content":
+                "Alice and Bob are going to a science fair on Friday.",
             },
         ],
         text={
@@ -92,9 +117,18 @@ def test_structured_output():
                 "schema": {
                     "type": "object",
                     "properties": {
-                        "name": {"type": "string"},
-                        "date": {"type": "string"},
-                        "participants": {"type": "array", "items": {"type": "string"}},
+                        "name": {
+                            "type": "string"
+                        },
+                        "date": {
+                            "type": "string"
+                        },
+                        "participants": {
+                            "type": "array",
+                            "items": {
+                                "type": "string"
+                            }
+                        },
                     },
                     "required": ["name", "date", "participants"],
                     "additionalProperties": False,
@@ -110,6 +144,7 @@ def test_structured_output():
 def test_structured_output_with_parse():
 
     from pydantic import BaseModel
+
     class CalendarEvent(BaseModel):
         name: str
         date: str
@@ -133,8 +168,14 @@ def test_store():
         )
         print(response)
 
-        response = client.responses.retrieve(response.id)
-        print(response)
+        try:
+            response = client.responses.retrieve(response.id)
+            print(response)
+        except NotFoundError:
+            is_not_found = True
+        else:
+            is_not_found = False
+        assert is_not_found == (not store)
 
 
 def test_background():
@@ -204,7 +245,9 @@ def test_web_search():
     response = client.responses.create(
         model=MODEL,
         input="Who is the president of South Korea as of now?",
-        tools=[{"type": "web_search_preview"}],
+        tools=[{
+            "type": "web_search_preview"
+        }],
     )
     print(response)
 
@@ -213,61 +256,362 @@ def test_code_interpreter():
     response = client.responses.create(
         model=MODEL,
         input="Multiply 643258029438.6132 * 23516705917230.84279 using Python.",
-        tools=[{"type": "code_interpreter", "container": {"type": "auto"}}],
+        tools=[{
+            "type": "code_interpreter",
+            "container": {
+                "type": "auto"
+            }
+        }],
     )
     print(response)
+
+
+def get_weather(latitude, longitude):
+    response = requests.get(
+        f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
+    )
+    data = response.json()
+    return data['current']['temperature_2m']
+
+
+def get_place_to_travel():
+    return "Paris"
+
+
+def call_function(name, args):
+    if name == "get_weather":
+        return get_weather(**args)
+    elif name == "get_place_to_travel":
+        return get_place_to_travel()
+    else:
+        raise ValueError(f"Unknown function: {name}")
 
 
 def test_function_calling():
-    tools = [
-        {
-            "type": "function",
-            "name": "send_email",
-            "description": "Send an email to a given recipient with a subject and message.",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "to": {"type": "string", "description": "The recipient email address."},
-                    "subject": {"type": "string", "description": "Email subject line."},
-                    "body": {"type": "string", "description": "Body of the email message."},
+    tools = [{
+        "type": "function",
+        "name": "get_weather",
+        "description":
+        "Get current temperature for provided coordinates in celsius.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "latitude": {
+                    "type": "number"
                 },
-                "required": ["to", "subject", "body"],
-                "additionalProperties": False,
+                "longitude": {
+                    "type": "number"
+                }
             },
+            "required": ["latitude", "longitude"],
+            "additionalProperties": False
         },
-    ]
+        "strict": True
+    }]
 
     response = client.responses.create(
         model=MODEL,
-        input="Can you send an email to alice@example.com and bob@example.com saying hi?",
+        input="What's the weather like in Paris today?",
         tools=tools,
     )
+    print("The first response:")
     print(response)
-    print("Output:")
+    print("output:")
     for out in response.output:
         print(out)
+    print("--------------------------------")
+
+    assert len(response.output) == 2
+    assert response.output[0].type == "reasoning"
+    assert response.output[1].type == "function_call"
+
+    tool_call = response.output[1]
+
+    name = tool_call.name
+    args = json.loads(tool_call.arguments)
+
+    result = call_function(name, args)
+    print("tool call result: ", result, type(result))
+
+    response_2 = client.responses.create(
+        model=MODEL,
+        input=[{
+            "type": "function_call_output",
+            "call_id": tool_call.call_id,
+            "output": str(result)
+        }],
+        tools=tools,
+        previous_response_id=response.id,
+    )
+    print("The second response:")
+    print(response_2)
+    print("output:")
+    for out in response_2.output:
+        print(out)
+    print("--------------------------------")
+    print(response_2.output_text)
+
+    # NOTE: chain-of-thought should be removed.
+    response_3 = client.responses.create(
+        model=MODEL,
+        input="What's the weather like in Paris today?",
+        tools=tools,
+        previous_response_id=response_2.id,
+    )
+    print("The third response:")
+    print(response_3)
+    print("output:")
+    for out in response_3.output:
+        print(out)
+    print("--------------------------------")
+    print(response_3.output_text)
+
+
+def test_function_calling_multi_turn():
+    tools = [{
+        "type": "function",
+        "name": "get_place_to_travel",
+        "description": "Get a random place to travel",
+        "parameters": {
+            "type": "object",
+            "properties": {},
+            "required": [],
+            "additionalProperties": False
+        },
+        "strict": True
+    }, {
+        "type": "function",
+        "name": "get_weather",
+        "description":
+        "Get current temperature for provided coordinates in celsius.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "latitude": {
+                    "type": "number"
+                },
+                "longitude": {
+                    "type": "number"
+                }
+            },
+            "required": ["latitude", "longitude"],
+            "additionalProperties": False
+        },
+        "strict": True
+    }]
+
+    response = client.responses.create(
+        model=MODEL,
+        input=
+        "Help me plan a trip to a random place. And tell me the weather there.",
+        tools=tools,
+    )
+    print("The first response:")
+    print(response)
+    print("output:")
+    for out in response.output:
+        print(out)
+    print("--------------------------------")
+
+    assert len(response.output) == 2
+    assert response.output[0].type == "reasoning"
+    assert response.output[1].type == "function_call"
+
+    tool_call = response.output[1]
+
+    name = tool_call.name
+    args = json.loads(tool_call.arguments)
+
+    result = call_function(name, args)
+    print("tool call result: ", result, type(result))
+
+    response_2 = client.responses.create(
+        model=MODEL,
+        input=[{
+            "type": "function_call_output",
+            "call_id": tool_call.call_id,
+            "output": str(result)
+        }],
+        tools=tools,
+        previous_response_id=response.id,
+    )
+    print("The second response:")
+    print(response_2)
+    print("output:")
+    for out in response_2.output:
+        print(out)
+    print("--------------------------------")
+    assert len(response_2.output) == 2
+    assert response_2.output[0].type == "reasoning"
+    assert response_2.output[1].type == "function_call"
+
+    tool_call = response_2.output[1]
+
+    name = tool_call.name
+    args = json.loads(tool_call.arguments)
+
+    result = call_function(name, args)
+    print("tool call result: ", result, type(result))
+
+    response_3 = client.responses.create(
+        model=MODEL,
+        input=[{
+            "type": "function_call_output",
+            "call_id": tool_call.call_id,
+            "output": str(result)
+        }],
+        tools=tools,
+        previous_response_id=response_2.id,
+    )
+    print("The third response:")
+    print(response_3)
+    print("output:")
+    for out in response_3.output:
+        print(out)
+    print("--------------------------------")
+    print(response_3.output_text)
+
+
+def test_function_calling_required():
+    tools = [{
+        "type": "function",
+        "name": "get_weather",
+        "description":
+        "Get current temperature for provided coordinates in celsius.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "latitude": {
+                    "type": "number"
+                },
+                "longitude": {
+                    "type": "number"
+                }
+            },
+            "required": ["latitude", "longitude"],
+            "additionalProperties": False
+        },
+        "strict": True
+    }]
+    try:
+        response = client.responses.create(
+            model=MODEL,
+            input="What's the weather like in Paris today?",
+            tools=tools,
+            tool_choice="required",
+        )
+    except BadRequestError as e:
+        print(e)
+        return
+    else:
+        raise ValueError("Should raise BadRequestError")
+
+
+def test_function_calling_full_history():
+    tools = [{
+        "type": "function",
+        "name": "get_weather",
+        "description":
+        "Get current temperature for provided coordinates in celsius.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "latitude": {
+                    "type": "number"
+                },
+                "longitude": {
+                    "type": "number"
+                }
+            },
+            "required": ["latitude", "longitude"],
+            "additionalProperties": False
+        },
+        "strict": True
+    }]
+
+    input_messages = [{
+        "role": "user",
+        "content": "What's the weather like in Paris today?"
+    }]
+
+    response = client.responses.create(
+        model=MODEL,
+        input=input_messages,
+        tools=tools,
+    )
+
+    print(response)
+    print("output:")
+    for out in response.output:
+        print(out)
+    print("--------------------------------")
+
+    tool_call = response.output[-1]
+    name = tool_call.name
+    args = json.loads(tool_call.arguments)
+
+    result = call_function(name, args)
+    print("tool call result: ", result, type(result))
+
+    input_messages.extend(
+        response.output)  # append model's function call message
+    input_messages.append({  # append result message
+        "type": "function_call_output",
+        "call_id": tool_call.call_id,
+        "output": str(result)
+    })
+
+    print("input_messages: ", input_messages)
+
+    response_2 = client.responses.create(
+        model=MODEL,
+        input=input_messages,
+        tools=tools,
+    )
+    print(response_2.output_text)
 
 
 if __name__ == "__main__":
     # 1. Stateless & Non-streaming tests:
-    # test_basic()
-    # test_basic_with_instructions()
-    # test_basic_with_reasoning_effort()
-    # test_chat()
-    # test_chat_with_input_type()
-    # test_structured_output()
-    # test_structured_output_with_parse()
+    print("===test_basic:")
+    test_basic()
+    print("===test_basic_with_instructions:")
+    test_basic_with_instructions()
+    print("===test_basic_with_reasoning_effort:")
+    test_basic_with_reasoning_effort()
+    print("===test_chat:")
+    test_chat()  # should we overwrite system message?
+    print("===test_chat_with_input_type:")
+    test_chat_with_input_type()
+    print("===test_structured_output:")
+    test_structured_output()
+    print("===test_structured_output_with_parse:")
+    test_structured_output_with_parse()
 
     # 2. Stateful & Non-streaming tests:
-    # test_store()
-    # test_background()
-    # test_background_cancel()
-    # test_stateful_multi_turn()
+    print("===test_store:")
+    test_store()
+    print("===test_background:")
+    test_background()
+    print("===test_background_cancel:")
+    test_background_cancel()
+    print("===test_stateful_multi_turn:")
+    test_stateful_multi_turn()
 
     # 3. Streaming tests:
+    # print("===test_streaming:")
     # test_streaming()
 
-    # # 4. Tool tests:
-    # test_web_search()
+    # 4. Tool tests:
+    print("===test_web_search:")
+    test_web_search()  # can crash occasionally
+    print("===test_code_interpreter:")
     test_code_interpreter()
-    # test_function_calling()
+    print("===test_function_calling:")
+    test_function_calling()
+    print("===test_function_calling_multi_turn:")
+    test_function_calling_multi_turn()  # can crash occasionally
+    print("===test_function_calling_required:")
+    test_function_calling_required()
+    print("===test_function_calling_full_history:")
+    test_function_calling_full_history()
--- vllm/entrypoints/harmony_utils.py
+++ vllm/entrypoints/harmony_utils.py
@@ -3,19 +3,24 @@
 import datetime
 import json
 from collections.abc import Iterable
-from typing import Any, Literal, Optional
+from typing import Any, Literal, Optional, Union
 
-from openai.types.responses import (ResponseInputParam, ResponseOutputMessage,
-                                    ResponseOutputText)
+from openai.types.responses import (ResponseOutputMessage, ResponseOutputText,
+                                    ResponseOutputItem)
+from openai.types.responses.response_function_tool_call import (
+    ResponseFunctionToolCall)
 from openai.types.responses.response_function_web_search import (
     ActionFind, ActionOpenPage, ActionSearch, ResponseFunctionWebSearch)
-from openai_harmony import (Conversation, DeveloperContent,
+from openai_harmony import (Author, Conversation, DeveloperContent,
                             HarmonyEncodingName, Message, ReasoningEffort,
                             Role, StreamableParser, SystemContent, TextContent,
-                            load_harmony_encoding)
+                            ToolDescription, load_harmony_encoding)
 
 from vllm.entrypoints.openai.protocol import (ResponseReasoningItem,
+                                              OpenAIBaseModel,
+                                              ResponseInputOutputItem,
                                               ResponseReasoningTextContent)
+from vllm.entrypoints.tool import Tool
 from vllm.utils import random_uuid
 
 REASONING_EFFORT = {
@@ -63,10 +68,31 @@ def get_system_message(
     return sys_msg
 
 
-def get_developer_message(instructions: Optional[str] = None) -> Message:
+def get_developer_message(instructions: Optional[str] = None,
+                          tools: Optional[list[Tool]] = None) -> Message:
     dev_msg_content = DeveloperContent.new()
     if instructions is not None:
         dev_msg_content = dev_msg_content.with_instructions(instructions)
+    if tools is not None:
+        function_tools = []
+        for tool in tools:
+            if tool.type in ("web_search_preview", "code_interpreter"):
+                # These are built-in tools that are added to the system message.
+                pass
+            elif tool.type == "function":
+                function_tools.append(tool)
+            else:
+                raise ValueError(f"tool type {tool.type} not supported")
+        if function_tools:
+            function_tool_descriptions = [
+                ToolDescription.new(
+                    name=tool.name,
+                    description=tool.description,
+                    parameters=tool.parameters,
+                ) for tool in function_tools
+            ]
+            dev_msg_content = dev_msg_content.with_function_tools(
+                function_tool_descriptions)
     dev_msg = Message.from_role_and_content(Role.DEVELOPER, dev_msg_content)
     return dev_msg
 
@@ -75,22 +101,72 @@ def get_user_message(content: str) -> Message:
     return Message.from_role_and_content(Role.USER, content)
 
 
-def parse_response_input(response_msg: ResponseInputParam) -> Message:
-    role = response_msg["role"]
-    content = response_msg["content"]
-    if isinstance(content, str):
-        msg = Message.from_role_and_content(role, content)
+def parse_response_input(
+    response_msg: ResponseInputOutputItem,
+    prev_responses: list[Union[ResponseOutputItem, ResponseReasoningItem]]
+) -> Message:
+    if not isinstance(response_msg, dict):
+        response_msg = response_msg.model_dump()
+    if "type" not in response_msg or response_msg["type"] == "message":
+        role = response_msg["role"]
+        content = response_msg["content"]
+        if role == "system":
+            # User is trying to set a system message. Change it to:
+            # <|start|>developer<|message|># Instructions
+            # {instructions}<|end|>
+            role = "developer"
+            text_prefix = "Instructions:\n"
+        else:
+            text_prefix = ""
+        if isinstance(content, str):
+            msg = Message.from_role_and_content(role, text_prefix + content)
+        else:
+            contents = [
+                TextContent(text=text_prefix + c["text"]) for c in content
+            ]
+            msg = Message.from_role_and_contents(role, contents)
+    elif response_msg["type"] == "function_call_output":
+        call_id = response_msg["call_id"]
+        call_response: Optional[ResponseFunctionToolCall] = None
+        for prev_response in reversed(prev_responses):
+            if isinstance(prev_response, ResponseFunctionToolCall
+                          ) and prev_response.call_id == call_id:
+                call_response = prev_response
+                break
+        if call_response is None:
+            raise ValueError(f"No call message found for {call_id}")
+        msg = Message.from_author_and_content(
+            Author.new(Role.TOOL, f"functions.{call_response.name}"),
+            response_msg["output"])
+    elif response_msg["type"] == "reasoning":
+        content = response_msg["content"]
+        assert len(content) == 1
+        msg = Message.from_role_and_content(Role.ASSISTANT, content[0]["text"])
+    elif response_msg["type"] == "function_call":
+        msg = Message.from_role_and_content(Role.ASSISTANT,
+                                            response_msg["arguments"])
+        msg = msg.with_channel("commentary")
+        msg = msg.with_recipient(f"functions.{response_msg['name']}")
+        msg = msg.with_content_type("json")
     else:
-        contents = [TextContent(text=c["text"]) for c in content]
+        raise ValueError(f"Unknown input type: {response_msg['type']}")
+    return msg
+
+
+def parse_response_output(output: ResponseOutputItem) -> Message:
+    if isinstance(output, ResponseOutputMessage):
+        role = output.role
+        contents = [TextContent(text=c.text) for c in output.content]
         msg = Message.from_role_and_contents(role, contents)
-    return msg
-
-
-def parse_response_output(output: ResponseOutputMessage) -> Message:
-    role = output.role
-    contents = [TextContent(text=c.text) for c in output.content]
-    msg = Message.from_role_and_contents(role, contents)
-    return msg
+        return msg
+    elif isinstance(output, ResponseFunctionToolCall):
+        msg = Message.from_role_and_content(Role.ASSISTANT, output.arguments)
+        msg = msg.with_channel("commentary")
+        msg = msg.with_recipient(output.name)
+        msg = msg.with_content_type("json")
+        return msg
+    else:
+        raise ValueError(f"Unknown output type: {type(output)}")
 
 
 def render_for_completion(messages: list[Message]) -> list[int]:
@@ -111,7 +187,8 @@ def get_streamable_parser_for_assistant() -> StreamableParser:
 def parse_output_message(message: Message):
     if message.author.role != "assistant":
         # This is a message from a tool to the assistant (e.g., search result).
-        # Don't include it in the final output for now. TODO: Handle this.
+        # Don't include it in the final output for now. This aligns with
+        # OpenAI's behavior on models like o4-mini.
         return []
 
     output_items = []
@@ -140,13 +217,36 @@ def parse_output_message(message: Message):
             type="web_search_call",
         )
         output_items.append(web_search_item)
-    elif message.channel in ("analysis", "commentary"):
+    elif message.channel == "analysis":
         for content in message.content:
             reasoning_item = ResponseReasoningItem(
                 content=[ResponseReasoningTextContent(text=content.text)],
                 status=None,
             )
             output_items.append(reasoning_item)
+    elif message.channel == "commentary":
+        if message.recipient.startswith("functions."):
+            function_name = message.recipient.split(".")[-1]
+            for content in message.content:
+                random_id = random_uuid()
+                response_item = ResponseFunctionToolCall(
+                    arguments=content.text,
+                    call_id=f"call_{random_id}",
+                    type="function_call",
+                    name=function_name,
+                    id=f"ft_{random_id}",
+                )
+                output_items.append(response_item)
+        elif message.recipient.startswith(
+                "python") or message.recipient.startswith("browser"):
+            for content in message.content:
+                reasoning_item = ResponseReasoningItem(
+                    text=content.text,
+                    status=None,
+                )
+                output_items.append(reasoning_item)
+        else:
+            raise ValueError(f"Unknown recipient: {message.recipient}")
     elif message.channel == "final":
         contents = []
         for content in message.content:
