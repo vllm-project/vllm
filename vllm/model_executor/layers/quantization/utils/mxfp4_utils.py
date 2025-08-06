9fcc402b4f84255564cd7472cd7f866ab6fcbc62
Yongye Zhu <zyy1102000@gmail.com>
2025-08-06 12:37:27 -0700

    [gpt-oss] flashinfer mxfp4 (#22339)
    Signed-off-by: simon-mo <xmo@berkeley.edu>
    Signed-off-by: Yongye Zhu <zyy1102000@gmail.com>
    Co-authored-by: simon-mo <xmo@berkeley.edu>
    Signed-off-by: Diego-Castan <diego.castan@ibm.com>

--- vllm/model_executor/layers/quantization/utils/mxfp4_utils.py
+++ vllm/model_executor/layers/quantization/utils/mxfp4_utils.py
@@ -1,10 +1,32 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+from typing import Callable, Optional
+
 import torch
 
 from vllm.utils import direct_register_custom_op
 
 OCP_MX_BLOCK_SIZE = 32
+
+
+def _can_support_mxfp4(use_grouped_topk: bool = False,
+                       topk_group: Optional[int] = None,
+                       num_expert_group: Optional[int] = None,
+                       expert_map: Optional[torch.Tensor] = None,
+                       custom_routing_function: Optional[Callable] = None,
+                       e_score_correction_bias: Optional[torch.Tensor] = None,
+                       apply_router_weight_on_input: bool = False,
+                       scoring_func: str = "softmax",
+                       activation: str = "silu",
+                       expert_load_view: Optional[torch.Tensor] = None,
+                       logical_to_physical_map: Optional[torch.Tensor] = None,
+                       logical_replica_count: Optional[torch.Tensor] = None):
+    return not (use_grouped_topk or topk_group or num_expert_group
+                or expert_map or custom_routing_function
+                or e_score_correction_bias or apply_router_weight_on_input
+                or scoring_func != "softmax" or activation != "silu"
+                or expert_load_view or logical_to_physical_map
+                or logical_replica_count)
 
 
 def _dequant_mxfp4(x: torch.Tensor, scale: torch.Tensor,
