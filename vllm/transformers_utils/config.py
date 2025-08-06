dd600d6e81689430d3ab22ee0030dc8b90a2bb7a
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-08-06 06:56:14 +0100

    Update transformers to `v4.55` (#21931)
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>
    Signed-off-by: Isotr0py <2037008807@qq.com>
    Signed-off-by: isotr0py <2037008807@qq.com>
    Signed-off-by: Isotr0py <mozf@mail2.sysu.edu.cn>
    Co-authored-by: DarkLight1337 <tlleungac@connect.ust.hk>
    Co-authored-by: Isotr0py <2037008807@qq.com>
    Co-authored-by: Isotr0py <mozf@mail2.sysu.edu.cn>
    Co-authored-by: Woosuk Kwon <woosuk.kwon@berkeley.edu>

--- vllm/transformers_utils/config.py
+++ vllm/transformers_utils/config.py
@@ -35,8 +35,8 @@ from vllm.transformers_utils.configs import (ChatGLMConfig, DeepseekVLV2Config,
                                              MllamaConfig, MLPSpeculatorConfig,
                                              Nemotron_Nano_VL_Config,
                                              NemotronConfig, NVLM_D_Config,
-                                             Ovis2_5Config, RWConfig,
-                                             SpeculatorsConfig,
+                                             Ovis2_5Config, OvisConfig,
+                                             RWConfig, SpeculatorsConfig,
                                              Step3TextConfig, Step3VLConfig,
                                              UltravoxConfig)
 # yapf: enable
@@ -86,6 +86,7 @@ _CONFIG_REGISTRY: dict[str, type[PretrainedConfig]] = {
     "speculators": SpeculatorsConfig,
     "nemotron": NemotronConfig,
     "NVLM_D": NVLM_D_Config,
+    "ovis": OvisConfig,
     "ovis2_5": Ovis2_5Config,
     "ultravox": UltravoxConfig,
     "step3_vl": Step3VLConfig,
--- vllm/transformers_utils/configs/__init__.py
+++ vllm/transformers_utils/configs/__init__.py
@@ -24,6 +24,7 @@ from vllm.transformers_utils.configs.nemotron import NemotronConfig
 from vllm.transformers_utils.configs.nemotron_h import NemotronHConfig
 from vllm.transformers_utils.configs.nemotron_vl import Nemotron_Nano_VL_Config
 from vllm.transformers_utils.configs.nvlm_d import NVLM_D_Config
+from vllm.transformers_utils.configs.ovis import OvisConfig
 from vllm.transformers_utils.configs.ovis2_5 import Ovis2_5Config
 from vllm.transformers_utils.configs.speculators.base import SpeculatorsConfig
 from vllm.transformers_utils.configs.step3_vl import (Step3TextConfig,
@@ -46,6 +47,7 @@ __all__ = [
     "NemotronHConfig",
     "Nemotron_Nano_VL_Config",
     "NVLM_D_Config",
+    "OvisConfig",
     "Ovis2_5Config",
     "SpeculatorsConfig",
     "UltravoxConfig",
