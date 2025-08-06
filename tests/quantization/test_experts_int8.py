d5d517fc660cfcfb8ef1f40b16ad1a6d2fa0b91f
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-08-06 06:56:14 +0100

    Update transformers to `v4.55` (#21931)
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>
    Signed-off-by: Isotr0py <2037008807@qq.com>
    Signed-off-by: isotr0py <2037008807@qq.com>
    Signed-off-by: Isotr0py <mozf@mail2.sysu.edu.cn>
    Co-authored-by: DarkLight1337 <tlleungac@connect.ust.hk>
    Co-authored-by: Isotr0py <2037008807@qq.com>
    Co-authored-by: Isotr0py <mozf@mail2.sysu.edu.cn>
    Co-authored-by: Woosuk Kwon <woosuk.kwon@berkeley.edu>
    Signed-off-by: Joachim Studnia <joachim@mistral.ai>

--- tests/quantization/test_experts_int8.py
+++ tests/quantization/test_experts_int8.py
@@ -9,6 +9,8 @@ import pytest
 
 from tests.quantization.utils import is_quant_method_supported
 
+from ..models.registry import HF_EXAMPLE_MODELS
+
 MODELS = ["ai21labs/Jamba-tiny-random", "pfnet/plamo-2-1b"]
 
 
@@ -25,6 +27,8 @@ def test_model_experts_int8_startup(
     dtype: str,
     max_tokens: int,
 ) -> None:
+    model_info = HF_EXAMPLE_MODELS.find_hf_info(model)
+    model_info.check_transformers_version(on_fail="skip")
 
     with vllm_runner(model, dtype=dtype,
                      quantization="experts_int8") as vllm_model:
