deb0b57f47e90ffe896e6723252a75c55bd7940d
sstamenk <sstamenk@amd.com>
2025-08-06 17:55:42 +0200

    Make comment shorter
    Signed-off-by: sstamenk <sstamenk@amd.com>

--- vllm/model_executor/layers/quantization/utils/w8a8_utils.py
+++ vllm/model_executor/layers/quantization/utils/w8a8_utils.py
@@ -120,7 +120,7 @@ def requantize_with_max_scale(
     if unfused_module_in_checkpoint:
         start = 0
         for idx, logical_width in enumerate(logical_widths):
-            # For decoupled Q/KV in the case of QKVCrossParallelLinear, skip Q component.
+            # For decoupled Q/KV in QKVCrossParallelLinear, skip Q component.
             if idx == 0 and logical_width == 0:
                 continue
             end = start + logical_width
