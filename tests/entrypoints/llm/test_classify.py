bf14fc43a7ffe8538599f63f2c33abf356b5879b
wang.yuqi <noooop@126.com>
2025-08-07 14:52:05 +0800

    + tests
    Signed-off-by: wang.yuqi <noooop@126.com>

--- tests/entrypoints/llm/test_classify.py
+++ tests/entrypoints/llm/test_classify.py
@@ -65,3 +65,11 @@ def test_pooling_params(llm: LLM):
     assert torch.allclose(
         softmax(wo_activation), w_activation, atol=1e-2
     ), "w_activation should be close to activation(wo_activation)."
+
+
+@pytest.mark.skip_global_cleanup
+def test_encode_api(llm: LLM):
+    err_msg = ("LLM.encode\(\) uses ALL pooling, which does "
+               "not support chunked prefill.+")
+    with pytest.raises(ValueError, match=err_msg):
+        llm.encode(prompts, use_tqdm=False)
