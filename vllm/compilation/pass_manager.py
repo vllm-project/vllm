8fbd0ddb8327290faabe27307d0d1eb42e0753ae
jindih <jindih@nvidia.com>
2025-08-07 10:16:59 +0000

    support silu+fp4 quant fusion
    Signed-off-by: jindih <jindih@nvidia.com>

--- vllm/compilation/pass_manager.py
+++ vllm/compilation/pass_manager.py
@@ -14,7 +14,8 @@ if current_platform.is_cuda_alike():
 if current_platform.is_cuda():
     from .collective_fusion import AllReduceFusionPass, AsyncTPPass
 
-from .activation_quant_fusion import ActivationQuantFusionPass
+from .activation_quant_fusion import (ActivationQuantFusionPass, 
+                                      ActivationFP4QuantFusionPass)
 from .fix_functionalization import FixFunctionalizationPass
 from .inductor_pass import CustomGraphPass, InductorPass, get_pass_context
 from .noop_elimination import NoOpEliminationPass
@@ -64,6 +65,7 @@ class PostGradPassManager(CustomGraphPass):
         if self.pass_config.enable_fusion:
             self.passes += [FusionPass.instance(config)]
             self.passes += [ActivationQuantFusionPass(config)]
+            self.passes += [ActivationFP4QuantFusionPass(config)]
 
         if self.pass_config.enable_attn_fusion:
             self.passes += [AttnFusionPass(config)]
