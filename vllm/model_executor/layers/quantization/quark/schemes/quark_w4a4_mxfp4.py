2a9a2afdc9332a520cd0c544d8270e69641bc840
Gregory Shtrasberg <Gregory.Shtrasberg@amd.com>
2025-08-07 14:44:25 -0500

    Missing import

--- vllm/model_executor/layers/quantization/quark/schemes/quark_w4a4_mxfp4.py
+++ vllm/model_executor/layers/quantization/quark/schemes/quark_w4a4_mxfp4.py
@@ -6,6 +6,7 @@ from typing import Any, Callable, Optional
 import torch
 import torch.nn.functional as F
 
+from vllm import envs
 from vllm.logger import init_logger
 from vllm.model_executor.layers.quantization.quark.schemes import QuarkScheme
 from vllm.model_executor.layers.quantization.utils.mxfp4_utils import (
