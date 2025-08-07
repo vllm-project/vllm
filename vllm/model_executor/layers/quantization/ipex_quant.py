a41cd7a97507faf98351816d6c1c9af7783f6b30
Syed Muhammad Bin Asif <92625830+syedmba@users.noreply.github.com>
2025-08-07 11:31:03 +0800

    [Bugfix] Add proper comparison for package versions (#22314)
    Signed-off-by: Syed Muhammad Bin Asif <syedmba7@connect.hku.hk>
    Signed-off-by: Joachim Studnia <joachim@mistral.ai>

--- vllm/model_executor/layers/quantization/ipex_quant.py
+++ vllm/model_executor/layers/quantization/ipex_quant.py
@@ -4,6 +4,7 @@
 from typing import Any, Optional
 
 import torch
+from packaging import version
 
 from vllm.model_executor.layers.linear import (LinearBase, LinearMethodBase,
                                                UnquantizedLinearMethod)
@@ -135,7 +136,8 @@ class IPEXGPTQLinearMethod(GPTQLinearMethod):
 
         try:
             import intel_extension_for_pytorch as ipex
-            if ipex.__version__ < MIN_IPEX_VERSION:
+            if version.parse(
+                    ipex.__version__) < version.parse(MIN_IPEX_VERSION):
                 raise ImportError(
                     "intel_extension_for_pytorch version is "
                     "wrong. Please install "
@@ -199,7 +201,8 @@ class IPEXAWQLinearMethod(AWQLinearMethod):
 
         try:
             import intel_extension_for_pytorch as ipex
-            if ipex.__version__ < MIN_IPEX_VERSION:
+            if version.parse(
+                    ipex.__version__) < version.parse(MIN_IPEX_VERSION):
                 raise ImportError(
                     "intel_extension_for_pytorch version is "
                     "wrong. Please install "
