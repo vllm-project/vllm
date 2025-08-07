a318923ba1b31a8863eee00daaf471f1d2b5e828
DarkLight1337 <tlleungac@connect.ust.hk>
2025-08-07 16:53:26 +0000

    Clean
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- tests/entrypoints/openai/test_audio.py
+++ tests/entrypoints/openai/test_audio.py
@@ -248,7 +248,7 @@ async def test_single_chat_session_input_audio(
 @pytest.mark.parametrize("audio_url", TEST_AUDIO_URLS)
 async def test_chat_streaming_audio(client: openai.AsyncOpenAI,
                                     model_name: str, audio_url: str):
-    messages = messages = dummy_messages_from_audio_url(audio_url)
+    messages = dummy_messages_from_audio_url(audio_url)
 
     # test single completion
     chat_completion = await client.chat.completions.create(
