ca92e4a7e710a4e166394f978c580fd296808b9a
Syed Muhammad Bin Asif <92625830+syedmba@users.noreply.github.com>
2025-08-07 11:31:03 +0800

    [Bugfix] Add proper comparison for package versions (#22314)
    Signed-off-by: Syed Muhammad Bin Asif <syedmba7@connect.hku.hk>
    Signed-off-by: Paul Pak <paulpak58@gmail.com>

--- vllm/model_executor/layers/quantization/gptq_bitblas.py
+++ vllm/model_executor/layers/quantization/gptq_bitblas.py
@@ -3,6 +3,7 @@
 from typing import Any, Optional
 
 import torch
+from packaging import version
 from torch.nn.parameter import Parameter
 
 from vllm.logger import init_logger
@@ -63,7 +64,8 @@ class GPTQBitBLASConfig(QuantizationConfig):
 
         try:
             import bitblas
-            if bitblas.__version__ < MINIMUM_BITBLAS_VERSION:
+            if version.parse(bitblas.__version__) < version.parse(
+                    MINIMUM_BITBLAS_VERSION):
                 raise ImportError(
                     "bitblas version is wrong. Please "
                     f"install bitblas>={MINIMUM_BITBLAS_VERSION}")
