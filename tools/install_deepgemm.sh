f5e646f07640462c90b8a20111011950020c0009
zhewenli <zhewenli@meta.com>
2025-08-07 10:01:53 -0700

    update

--- tools/install_deepgemm.sh
+++ tools/install_deepgemm.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
 
+ln -sf "$(which python3)" /usr/local/bin/python3
+
 WORKSPACE=$1
 if [ -z "$WORKSPACE" ]; then
     WORKSPACE=$(pwd)/deepgemm_workspace
@@ -13,7 +15,7 @@ if [ ! -d DeepGEMM ]; then
     git clone --recursive https://github.com/deepseek-ai/DeepGEMM
 fi
 cd DeepGEMM
-./develop.sh 
+./develop.sh
 PIP_NO_BUILD_ISOLATION=0 pip install -vvv -e .
 
 popd
