8db0205b55a223278f0d73fc9f50127af93707c0
wang.yuqi <noooop@126.com>
2025-08-08 18:51:28 +0800

    fix
    Signed-off-by: wang.yuqi <noooop@126.com>

--- vllm/model_executor/models/interfaces.py
+++ vllm/model_executor/models/interfaces.py
@@ -641,9 +641,9 @@ def supports_cross_encoding(
     return is_pooling_model(model) and _supports_cross_encoding(model)
 
 
-def default_pooling_type(pooling_type):
+def default_pooling_type(pooling_type: str) -> object:
     """Set default_pooling_type decorator. """
-    def func(model):
+    def func(model: object):
         model.default_pooling_type = pooling_type
         return model
 
