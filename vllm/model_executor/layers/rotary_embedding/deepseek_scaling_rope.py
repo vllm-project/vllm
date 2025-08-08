9708eb2c0f25769dd8b7cd87e48f114587f538e8
vllmellm <vllm.ellm@embeddedllm.com>
2025-08-08 12:05:03 +0000

    remove unnecessary command
    Signed-off-by: vllmellm <vllm.ellm@embeddedllm.com>

--- vllm/model_executor/layers/rotary_embedding/deepseek_scaling_rope.py
+++ vllm/model_executor/layers/rotary_embedding/deepseek_scaling_rope.py
@@ -90,11 +90,7 @@ class DeepseekScalingRotaryEmbedding(RotaryEmbedding):
     ) -> tuple[torch.Tensor, Optional[torch.Tensor]]:
         """PyTorch-native implementation equivalent to forward()."""
         if self.is_rocm_aiter_enabled:
-            query, key = self.forward_hip_rocm_aiter(positions, query, key,
-                                                     offsets)
-            if positions.numel() == 1:
-                key = key.clone()
-            return query, key
+            return self.forward_hip_rocm_aiter(positions, query, key, offsets)
 
         assert key is not None
         query_rot = query[..., :self.rotary_dim]
