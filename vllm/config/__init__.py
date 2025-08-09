2e2dbcf16a349410b6b00401927f5b20cf95a715
Noam Gat <noamgat@gmail.com>
2025-08-09 11:25:22 +0300

    Linting
    Signed-off-by: Noam Gat <noamgat@gmail.com>

--- vllm/config/__init__.py
+++ vllm/config/__init__.py
@@ -3838,7 +3838,6 @@ GuidedDecodingBackend = Literal["auto", "xgrammar", "guidance", "outlines",
                                 "lm-format-enforcer"]
 
 
-
 @config
 @dataclass
 class DecodingConfig:
