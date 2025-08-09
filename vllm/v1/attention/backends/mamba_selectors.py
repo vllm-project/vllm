e5f3d50492e61b6730495d8dd5d651be97e28563
Thomas Parnell <tpa@zurich.ibm.com>
2025-08-09 08:08:48 +0200

    [V1] [Hybrid] Support Minimax-Text-01 in V1  (#22151)
    Signed-off-by: Thomas Parnell <tpa@zurich.ibm.com>
    Signed-off-by: Paul Pak <paulpak58@gmail.com>

--- vllm/v1/attention/backends/mamba_selectors.py
+++ vllm/v1/attention/backends/mamba_selectors.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 from vllm.attention.backends.abstract import AttentionBackend
+from vllm.v1.attention.backends.linear_attn import LinearAttentionBackend
 from vllm.v1.attention.backends.mamba1_attn import Mamba1AttentionBackend
 from vllm.v1.attention.backends.mamba_attn import Mamba2AttentionBackend
 from vllm.v1.attention.backends.short_conv_attn import (
@@ -10,9 +11,10 @@ from vllm.v1.attention.backends.short_conv_attn import (
 def get_mamba_attn_backend(mamba_type: str) -> type[AttentionBackend]:
     if mamba_type == "mamba1":
         return Mamba1AttentionBackend
-
     if mamba_type == "mamba2":
         return Mamba2AttentionBackend
+    if mamba_type == "linear_attention":
+        return LinearAttentionBackend
 
     if mamba_type == "short_conv":
         return ShortConvAttentionBackend
