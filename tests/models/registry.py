f9b8b8c637c05f2868cbe22c6a24c44c0d9142eb
Isotr0py <mozf@mail2.sysu.edu.cn>
2025-08-09 15:03:26 +0800

    [Bugfix] Fix failing GPT-OSS initialization test (#22557)
    Signed-off-by: Isotr0py <mozf@mail2.sysu.edu.cn>
    Signed-off-by: Joachim Studnia <joachim@mistral.ai>

--- tests/models/registry.py
+++ tests/models/registry.py
@@ -200,7 +200,7 @@ _TEXT_GENERATION_EXAMPLE_MODELS = {
                                        {"6b": "EleutherAI/gpt-j-6b"}),
     "GPTNeoXForCausalLM": _HfExamplesInfo("EleutherAI/pythia-70m",
                                           {"1b": "EleutherAI/pythia-1.4b"}),
-    "GptOssForCausalLM": _HfExamplesInfo("openai/gpt-oss-20b"),
+    "GptOssForCausalLM": _HfExamplesInfo("lmsys/gpt-oss-20b-bf16"),
     "GraniteForCausalLM": _HfExamplesInfo("ibm/PowerLM-3b"),
     "GraniteMoeForCausalLM": _HfExamplesInfo("ibm/PowerMoE-3b"),
     "GraniteMoeHybridForCausalLM": _HfExamplesInfo("ibm-granite/granite-4.0-tiny-preview"),  # noqa: E501
