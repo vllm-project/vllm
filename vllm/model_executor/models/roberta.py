63b03cc2ff7c27b90153050ebf1ae323ed16fe96
wang.yuqi <noooop@126.com>
2025-08-11 15:06:37 +0800

    conflicts
    Signed-off-by: wang.yuqi <noooop@126.com>

--- vllm/model_executor/models/roberta.py
+++ vllm/model_executor/models/roberta.py
@@ -20,8 +20,7 @@ from vllm.model_executor.models.utils import (AutoWeightsLoader, WeightsMapper,
 from vllm.sequence import IntermediateTensors
 
 from .bert_with_rope import BertWithRope, JinaRobertaModel
-from .interfaces import (SupportsCrossEncoding, SupportsV0Only,
-                         default_pooling_type)
+from .interfaces import SupportsCrossEncoding, SupportsV0Only
 
 
 class RobertaEmbedding(nn.Module):
