c7834b352376af1d66862be7a3236b8c1749a424
Cyrus Leung <tlleungac@connect.ust.hk>
2025-08-11 15:15:27 +0800

    [Misc] Move tensor schema tests (#22612)
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>
    Signed-off-by: Joachim Studnia <joachim@mistral.ai>

---
+++ tests/utils_/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+"""
+This module is named `utils_` instead of `utils` to avoid obscuring
+`tests/utils.py`.
+"""
