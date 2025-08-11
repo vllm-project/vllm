742e9f91c6b4064910b7852fba6dbb75f6d57d59
22quinn <33176974+22quinn@users.noreply.github.com>
2025-08-11 10:31:36 -0700

    [BugFix] [Spec Decode] Remove LlamaForCausalLMEagle3 to fix CI (#22611)
    Signed-off-by: 22quinn <33176974+22quinn@users.noreply.github.com>
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- tests/models/registry.py
+++ tests/models/registry.py
@@ -525,10 +525,11 @@ _SPECULATIVE_DECODING_EXAMPLE_MODELS = {
                                             trust_remote_code=True,
                                             speculative_model="yuhuili/EAGLE3-LLaMA3.1-Instruct-8B",
                                             tokenizer="meta-llama/Llama-3.1-8B-Instruct"),
-    "LlamaForCausalLMEagle3": _HfExamplesInfo("AngelSlim/Qwen3-8B_eagle3",  # noqa: E501
-                                              trust_remote_code=True,
-                                              speculative_model="AngelSlim/Qwen3-8B_eagle3",
-                                              tokenizer="Qwen/Qwen3-8B"),
+    # TODO: Re-enable this once tests/models/test_initialization.py is fixed, see PR #22333 #22611   # noqa: E501
+    # "LlamaForCausalLMEagle3": _HfExamplesInfo("AngelSlim/Qwen3-8B_eagle3",  # noqa: E501
+    #                                         trust_remote_code=True,
+    #                                         speculative_model="AngelSlim/Qwen3-8B_eagle3",   # noqa: E501
+    #                                         tokenizer="Qwen/Qwen3-8B"),
     "EagleLlama4ForCausalLM": _HfExamplesInfo(
         "morgendave/EAGLE-Llama-4-Scout-17B-16E-Instruct",
         trust_remote_code=True,
