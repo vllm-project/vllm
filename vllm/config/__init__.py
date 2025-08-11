d13c58903a1259ebdd434fd6419f564e5240fa03
22quinn <33176974+22quinn@users.noreply.github.com>
2025-08-11 10:24:07 -0700

    comment out
    Signed-off-by: 22quinn <33176974+22quinn@users.noreply.github.com>

--- vllm/config/__init__.py
+++ vllm/config/__init__.py
@@ -2864,24 +2864,24 @@ class PoolerConfig:
     ## for embeddings models
     normalize: Optional[bool] = None
     """
-    Whether to normalize the embeddings outputs.
+    Whether to normalize the embeddings outputs. 
     """
     dimensions: Optional[int] = None
     """
-    Reduce the dimensions of embeddings if model
+    Reduce the dimensions of embeddings if model 
     support matryoshka representation.
     """
 
     ## for classification models
     activation: Optional[bool] = None
     """
-    Whether to apply activation function to the classification outputs.
+    Whether to apply activation function to the classification outputs. 
     """
 
     ## for reward models
     softmax: Optional[bool] = None
     """
-    Whether to apply softmax to the reward outputs.
+    Whether to apply softmax to the reward outputs. 
     """
     step_tag_id: Optional[int] = None
     """
