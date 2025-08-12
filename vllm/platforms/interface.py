007dd90859cc0337510536677418a43d8f66e286
Yongye Zhu <zyy1102000@gmail.com>
2025-08-12 06:21:44 -0400

    [gpt-oss] Enable gpt-oss on ampere (#22714)
    Signed-off-by: Yongye Zhu <zyy1102000@gmail.com>

--- vllm/platforms/interface.py
+++ vllm/platforms/interface.py
@@ -196,8 +196,8 @@ class Platform:
     @classmethod
     def get_attn_backend_cls(cls, selected_backend: _Backend, head_size: int,
                              dtype: torch.dtype, kv_cache_dtype: Optional[str],
-                             block_size: int, use_v1: bool,
-                             use_mla: bool) -> str:
+                             block_size: int, use_v1: bool, use_mla: bool,
+                             has_sink: bool) -> str:
         """Get the attention backend class of a device."""
         return ""
 
