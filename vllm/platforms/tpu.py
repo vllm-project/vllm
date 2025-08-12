63a5b8259bbea3cbc2a717e3e4a62e66a967cc9b
Yongye Zhu <zyy1102000@gmail.com>
2025-08-12 06:21:44 -0400

    [gpt-oss] Enable gpt-oss on ampere (#22714)
    Signed-off-by: Yongye Zhu <zyy1102000@gmail.com>

--- vllm/platforms/tpu.py
+++ vllm/platforms/tpu.py
@@ -46,8 +46,8 @@ class TpuPlatform(Platform):
     @classmethod
     def get_attn_backend_cls(cls, selected_backend: _Backend, head_size: int,
                              dtype: torch.dtype, kv_cache_dtype: Optional[str],
-                             block_size: int, use_v1: bool,
-                             use_mla: bool) -> str:
+                             block_size: int, use_v1: bool, use_mla: bool,
+                             has_sink) -> str:
         if (selected_backend != _Backend.PALLAS
                 and selected_backend != _Backend.PALLAS_VLLM_V1):
             logger.info("Cannot use %s backend on TPU.", selected_backend)
