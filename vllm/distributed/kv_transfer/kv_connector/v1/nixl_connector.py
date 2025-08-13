f11d6dca8b0684a071f60cc2e033db6a13bf1d1b
zhenwei <zhenweiliu@habana.ai>
2025-08-13 11:11:19 +0300

    fix and remove scripts
    Signed-off-by: zhenwei <zhenweiliu@habana.ai>

--- vllm/distributed/kv_transfer/kv_connector/v1/nixl_connector.py
+++ vllm/distributed/kv_transfer/kv_connector/v1/nixl_connector.py
@@ -265,8 +265,7 @@ class NixlConnector(KVConnectorBase_V1):
     def wait_for_save(self):
         assert self.connector_worker is not None
         assert isinstance(self._connector_metadata, NixlConnectorMetadata)
-        if self.connector_worker.use_host_buffer and \
-           self.connector_worker.copy_blocks:
+        if self.connector_worker.use_host_buffer:
             self.connector_worker.save_kv_to_host(self._connector_metadata)
 
 
