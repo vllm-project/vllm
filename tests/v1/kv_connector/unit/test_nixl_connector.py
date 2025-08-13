3675ca02614bd5d1f373d4febef9eeb7632ff100
Nicolò Lucchesi <nlucches@redhat.com>
2025-08-13 15:03:53 +0200

    [Nixl][CI] Fix tests (#22806)
    Signed-off-by: NickLucche <nlucches@redhat.com>

--- tests/v1/kv_connector/unit/test_nixl_connector.py
+++ tests/v1/kv_connector/unit/test_nixl_connector.py
@@ -229,6 +229,9 @@ class FakeNixlConnectorWorker(NixlConnectorWorker):
                 num_blocks=1,
                 block_len=self.block_len,
                 attn_backend_name=self.backend_name,
+                # `self.kv_cache_layout` is only forced to HND when vllm engine
+                # is started. We mock HND here.
+                kv_cache_layout="HND",
             ),
             remote_tp_size=remote_tp_size)
         return {0: remote_agent_name}
