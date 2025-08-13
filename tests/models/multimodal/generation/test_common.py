d5e7e96103a774cad4bb39d9eb74c294c9534e48
DarkLight1337 <tlleungac@connect.ust.hk>
2025-08-13 17:00:05 +0000

    Revert one
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- tests/models/multimodal/generation/test_common.py
+++ tests/models/multimodal/generation/test_common.py
@@ -561,6 +561,8 @@ VLM_TEST_SETTINGS = {
         get_stop_token_ids=lambda tok: tok.convert_tokens_to_ids(['<|im_end|>', '<|endoftext|>']),  # noqa: E501
         hf_output_post_proc=model_utils.minicpmv_trunc_hf_output,
         patch_hf_runner=model_utils.minicpmo_26_patch_hf_runner,
+        # FIXME: https://huggingface.co/openbmb/MiniCPM-o-2_6/discussions/49
+        marks=[pytest.mark.skip("HF import fails")],
     ),
     "minicpmv_26": VLMTestInfo(
         models=["openbmb/MiniCPM-V-2_6"],
