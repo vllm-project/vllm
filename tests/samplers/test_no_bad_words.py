91d7dedc06765e1431b7d782dca659b0e7419d17
Robert Shaw <robshaw@redhat.com>
2025-08-14 03:13:47 +0000

    convert to enforce eager
    Signed-off-by: Robert Shaw <robshaw@redhat.com>

--- tests/samplers/test_no_bad_words.py
+++ tests/samplers/test_no_bad_words.py
@@ -50,7 +50,7 @@ class TestOneTokenBadWord:
                                             add_special_tokens=False)[0]
 
     def test_one_token_bad_word(self, vllm_runner):
-        with vllm_runner(self.MODEL) as llm:
+        with vllm_runner(self.MODEL, enforce_eager=True) as llm:
             output_token_ids = self._generate(llm)
             assert output_token_ids[0] == self.target_token_id
 
@@ -97,7 +97,7 @@ class TestTwoTokenBadWord:
                                                 add_special_tokens=False)[0]
 
     def test_two_token_bad_word(self, vllm_runner):
-        with vllm_runner(self.MODEL, dtype="half") as llm:
+        with vllm_runner(self.MODEL, enforce_eager=True) as llm:
             output_token_ids = self._generate(llm)
             assert output_token_ids[:2] == [
                 self.target_token_id1, self.target_token_id2
