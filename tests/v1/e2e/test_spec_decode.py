c3c06de224bcaf088e1baf344821291c2b571f8d
qizixi <qizixi@meta.com>
2025-08-14 00:25:49 -0700

    fix unit test
    Signed-off-by: qizixi <qizixi@meta.com>

--- tests/v1/e2e/test_spec_decode.py
+++ tests/v1/e2e/test_spec_decode.py
@@ -2,6 +2,7 @@
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 from __future__ import annotations
 
+import math
 import random
 from typing import Any, Union
 
@@ -14,6 +15,7 @@ from vllm.assets.base import VLLM_S3_BUCKET_URL
 from vllm.assets.image import VLM_IMAGES_DIR
 from vllm.distributed import cleanup_dist_env_and_memory
 from vllm.platforms import current_platform
+from vllm.v1.metrics.reader import Counter, Metric, Vector
 
 
 def get_test_prompts(mm_enabled: bool):
@@ -65,6 +67,23 @@ def get_test_prompts(mm_enabled: bool):
         prompts.append([{"role": "user", "content": prompt}])
 
     return prompts
+
+
+def get_acceptance_rate(metrics: list[Metric]):
+    num_drafts = num_accepted = 0
+    acceptance_counts = [0] * 3
+    for metric in metrics:
+        if metric.name == "vllm:spec_decode_num_drafts":
+            assert isinstance(metric, Counter)
+            num_drafts += metric.value
+        elif metric.name == "vllm:spec_decode_num_accepted_tokens":
+            assert isinstance(metric, Counter)
+            num_accepted += metric.value
+        elif metric.name == "vllm:spec_decode_num_accepted_tokens_per_pos":
+            assert isinstance(metric, Vector)
+            for pos in range(len(metric.values)):
+                acceptance_counts[pos] += metric.values[pos]
+    return 1.0 * num_accepted / num_drafts + 1
 
 
 @pytest.fixture
@@ -155,14 +174,12 @@ def test_ngram_correctness(
     ])
 @pytest.mark.parametrize("attn_backend",
                          get_attn_backend_list_based_on_platform())
-@pytest.mark.parametrize("full_cuda_graph", [True, False])
 def test_eagle_correctness(
     monkeypatch: pytest.MonkeyPatch,
     sampling_config: SamplingParams,
     model_setup: tuple[str, str, str, int],
     mm_enabled: bool,
     attn_backend: str,
-    full_cuda_graph: bool,
 ):
     # Generate test prompts inside the function instead of using fixture
     test_prompts = get_test_prompts(mm_enabled)
@@ -204,7 +221,6 @@ def test_eagle_correctness(
                 "max_model_len": 2048,
             },
             max_model_len=2048,
-            compilation_config={"full_cuda_graph": full_cuda_graph},
         )
         spec_outputs = spec_llm.chat(test_prompts, sampling_config)
         matches = 0
@@ -221,5 +237,95 @@ def test_eagle_correctness(
         # Upon failure, inspect the outputs to check for inaccuracy.
         assert matches > int(0.66 * len(ref_outputs))
         del spec_llm
+        torch.cuda.empty_cache()
+        cleanup_dist_env_and_memory()
+
+
+@pytest.mark.parametrize(
+    "model_setup",
+    [
+        ("eagle", "meta-llama/Llama-3.1-8B-Instruct",
+         "yuhuili/EAGLE-LLaMA3.1-Instruct-8B", 1),
+    ],
+)
+def test_full_vs_piecewise_cudagraph(
+    monkeypatch: pytest.MonkeyPatch,
+    sampling_config: SamplingParams,
+    model_setup: tuple[str, str, str, int],
+):
+    test_prompts = get_test_prompts(mm_enabled=False)
+    '''
+    Compare the eagle speculative decoding outputs and acceptance
+    rate should match between piecewise and full cudagraph mode
+    model_setup: (method, model_name, eagle_model_name, tp_size)
+    '''
+    with monkeypatch.context() as m:
+        m.setenv("VLLM_USE_V1", "1")
+        m.setenv("VLLM_GPU_MEMORY_UTILIZATION", "0.8")
+        if current_platform.is_rocm():
+            m.setenv("VLLM_ROCM_USE_AITER", "1")
+
+        method, model_name, spec_model_name, tp_size = model_setup
+
+        spec_llm_piecewise_cudagraph = LLM(
+            model=model_name,
+            trust_remote_code=True,
+            tensor_parallel_size=tp_size,
+            speculative_config={
+                "method": method,
+                "model": spec_model_name,
+                "num_speculative_tokens": 3,
+                "max_model_len": 2048,
+            },
+            max_model_len=2048,
+            compilation_config={"full_cuda_graph": False},
+            disable_log_stats=False,
+        )
+        piecewise_cudagraph_outputs = spec_llm_piecewise_cudagraph.chat(
+            test_prompts, sampling_config)
+        piecewise_cudagraph_metrics = spec_llm_piecewise_cudagraph.get_metrics(
+        )
+        del spec_llm_piecewise_cudagraph
+        torch.cuda.empty_cache()
+        cleanup_dist_env_and_memory()
+
+        spec_llm_full_cudagraph = LLM(
+            model=model_name,
+            trust_remote_code=True,
+            tensor_parallel_size=tp_size,
+            speculative_config={
+                "method": method,
+                "model": spec_model_name,
+                "num_speculative_tokens": 3,
+                "max_model_len": 2048,
+            },
+            max_model_len=2048,
+            compilation_config={"full_cuda_graph": True},
+            disable_log_stats=False,
+        )
+        full_cudagraph_outputs = spec_llm_full_cudagraph.chat(
+            test_prompts, sampling_config)
+        full_cudagraph_metrics = spec_llm_full_cudagraph.get_metrics()
+        matches = 0
+        misses = 0
+        for piecewise, full in zip(piecewise_cudagraph_outputs,
+                                   full_cudagraph_outputs):
+            if piecewise.outputs[0].text == full.outputs[0].text:
+                matches += 1
+            else:
+                misses += 1
+                print(
+                    f"piecewise_cudagraph_output: {piecewise.outputs[0].text}")
+                print(f"full_cudagraph_output: {full.outputs[0].text}")
+
+        # Heuristic: expect at least 66% of the prompts to match exactly between
+        # piecewise and full cudagraph mode, and acceptance rate to be within
+        # 0.1 atol
+        # Upon failure, inspect the outputs to check for inaccuracy.
+        assert matches > int(0.66 * len(piecewise_cudagraph_outputs))
+        piecewise_acceptance = get_acceptance_rate(piecewise_cudagraph_metrics)
+        full_acceptance = get_acceptance_rate(full_cudagraph_metrics)
+        assert math.isclose(piecewise_acceptance, full_acceptance, abs_tol=0.1)
+        del spec_llm_full_cudagraph
         torch.cuda.empty_cache()
         cleanup_dist_env_and_memory()
