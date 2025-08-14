a587f5a18027f9ebda1865ee29739cf06374fc97
Zhiyu Cheng <zhiyuc@nvidia.com>
2025-08-14 00:42:53 +0000

    minor
    Signed-off-by: Zhiyu Cheng <zhiyuc@nvidia.com>

--- vllm/env_override.py
+++ vllm/env_override.py
@@ -20,6 +20,4 @@ os.environ['PYTORCH_NVML_BASED_CUDA_CHECK'] = '1'
 # see https://github.com/vllm-project/vllm/issues/10480
 os.environ['TORCHINDUCTOR_COMPILE_THREADS'] = '1'
 # see https://github.com/vllm-project/vllm/issues/10619
-# Guard for PyTorch _inductor API availability
-if hasattr(torch, "_inductor") and hasattr(torch._inductor, "config"):
-    torch._inductor.config.compile_threads = 1
+torch._inductor.config.compile_threads = 1
