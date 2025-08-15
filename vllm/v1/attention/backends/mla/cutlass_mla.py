74f441f4b517a895ad12afd314a6f40caf657c4e
fhl2000 <63384265+fhl2000@users.noreply.github.com>
2025-08-15 22:01:39 +0800

    [Core] Allow full cudagraph with separate attention routines and orthogonal to compilation, add support for FA2 and FlashInfer (#20059)
    Signed-off-by: fhl <2410591650@qq.com>
    Signed-off-by: fhl2000 <63384265+fhl2000@users.noreply.github.com>
    Signed-off-by: Lucas Wilkinson <lwilkins@redhat.com>
    Signed-off-by: Lucas Wilkinson <LucasWilkinson@users.noreply.github.com>
    Co-authored-by: Luka Govedič <ProExpertProg@users.noreply.github.com>
    Co-authored-by: Lucas Wilkinson <lwilkins@redhat.com>
    Co-authored-by: Lucas Wilkinson <LucasWilkinson@users.noreply.github.com>

--- vllm/v1/attention/backends/mla/cutlass_mla.py
+++ vllm/v1/attention/backends/mla/cutlass_mla.py
@@ -22,7 +22,7 @@ logger = init_logger(__name__)
 class CutlassMLAMetadataBuilder(MLACommonMetadataBuilder[MLACommonMetadata]):
     # enable full CUDA Graph support for decode-only capture
     attn_cudagraph_support: ClassVar[
-        AttentionCGSupport] = AttentionCGSupport.PURE_DECODE_ONLY
+        AttentionCGSupport] = AttentionCGSupport.UNIFORM_SINGLE_TOKEN_DECODE
 
 
 class CutlassMLABackend(MLACommonBackend):
