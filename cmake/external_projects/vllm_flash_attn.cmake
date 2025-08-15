b556b4028ef24a7a88dba93c67e176ab3849a836
Lucas Wilkinson <LucasWilkinson@users.noreply.github.com>
2025-08-15 17:41:07 -0400

    [Attention] FA3 Attention Sinks Perf Boost (#22478)
    Signed-off-by: Lucas Wilkinson <lwilkins@redhat.com>
    Signed-off-by: Joachim Studnia <joachim@mistral.ai>

--- cmake/external_projects/vllm_flash_attn.cmake
+++ cmake/external_projects/vllm_flash_attn.cmake
@@ -38,7 +38,7 @@ else()
   FetchContent_Declare(
           vllm-flash-attn
           GIT_REPOSITORY https://github.com/vllm-project/flash-attention.git
-          GIT_TAG 93cf5a08f421a3efd0c4a7e005ef8f742b578ce0
+          GIT_TAG 2d3b7508f67ad976f781e2042ace676419dd78dd
           GIT_PROGRESS TRUE
           # Don't share the vllm-flash-attn build between build types
           BINARY_DIR ${CMAKE_BINARY_DIR}/vllm-flash-attn
