113b686e6ec5cbae9388858272d2a395ddf9dbc6
Lucas Wilkinson <LucasWilkinson@users.noreply.github.com>
2025-08-17 11:52:04 -0400

    [BugFix] Fix for IMA in FA3 varlen combine (#22967)
    Signed-off-by: Lucas Wilkinson <lwilkins@redhat.com>

--- cmake/external_projects/vllm_flash_attn.cmake
+++ cmake/external_projects/vllm_flash_attn.cmake
@@ -38,7 +38,7 @@ else()
   FetchContent_Declare(
           vllm-flash-attn
           GIT_REPOSITORY https://github.com/vllm-project/flash-attention.git
-          GIT_TAG 2d3b7508f67ad976f781e2042ace676419dd78dd
+          GIT_TAG 57b4e68b9f9d94750b46de8f8dbd2bfcc86edd4f
           GIT_PROGRESS TRUE
           # Don't share the vllm-flash-attn build between build types
           BINARY_DIR ${CMAKE_BINARY_DIR}/vllm-flash-attn
