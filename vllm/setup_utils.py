eb08389f942882d00f8343ea4c6a7c0a5bf91953
Lucas Wilkinson <lwilkins@redhat.com>
2025-08-17 19:06:14 +0000

    merge conflict
    Signed-off-by: Lucas Wilkinson <lwilkins@redhat.com>

--- vllm/setup_utils.py
+++ vllm/setup_utils.py
@@ -13,7 +13,8 @@ from torch.utils.cpp_extension import CUDA_HOME
 
 
 def is_sccache_available() -> bool:
-    return which("sccache") is not None
+    return which("sccache") is not None and \
+        not bool(int(os.getenv("VLLM_DISABLE_SCCACHE", "0")))
 
 
 def is_ccache_available() -> bool:
