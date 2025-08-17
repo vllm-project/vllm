0f7fea145ba4b1d13fe3776745b0a159fbae0a2a
Roger Wang <hey@rogerw.me>
2025-08-17 00:48:50 -0700

    add
    Signed-off-by: Roger Wang <hey@rogerw.me>

--- vllm/model_executor/models/registry.py
+++ vllm/model_executor/models/registry.py
@@ -262,6 +262,7 @@ _SPECULATIVE_DECODING_MODELS = {
     "EagleMiniCPMForCausalLM": ("minicpm_eagle", "EagleMiniCPMForCausalLM"),
     "Eagle3LlamaForCausalLM": ("llama_eagle3", "Eagle3LlamaForCausalLM"),
     "EagleQwen2ForCausalLM": ("qwen2_eagle", "EagleQwen2ForCausalLM"),
+    "Qwen2ForCausalLMEagle": ("qwen2_eagle", "EagleQwen2ForCausalLM"),
     # TODO: Re-enable this once tests/models/test_initialization.py is fixed, see PR #22333 #22611  # noqa: E501
     # "LlamaForCausalLMEagle3": ("llama_eagle3", "Eagle3LlamaForCausalLM"),
     "DeepSeekMTPModel": ("deepseek_mtp", "DeepSeekMTP"),
