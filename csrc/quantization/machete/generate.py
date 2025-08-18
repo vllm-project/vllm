f3c48f0df05edf7017c6005a8319d5510e0cbde4
Andy Lo <andy@mistral.ai>
2025-08-18 07:10:26 +0200

    [Bugfix][CI] Machete kernels: deterministic ordering for more cache hits (#23055)
    Signed-off-by: Andy Lo <andy@mistral.ai>

--- csrc/quantization/machete/generate.py
+++ csrc/quantization/machete/generate.py
@@ -349,9 +349,12 @@ def to_cute_constant(value: list[int]):
 
 
 def unique_schedules(impl_configs: list[ImplConfig]):
-    return list(
-        set(sch for impl_config in impl_configs
-            for sch in impl_config.schedules))
+    # Use dict over set for deterministic ordering
+    return list({
+        sch: None
+        for impl_config in impl_configs
+        for sch in impl_config.schedules
+    }.keys())
 
 
 def unsigned_type_with_bitwidth(num_bits):
