80f002ceb9c16be9d086fda6d76772a6000f0849
Anil Tolwani <aniltolwani@gmail.com>
2025-08-17 20:52:13 -0700

    exists okay overrride for now

--- vllm/transformers_utils/configs/ovis.py
+++ vllm/transformers_utils/configs/ovis.py
@@ -72,7 +72,7 @@ IMAGE_TOKEN = "<image>"
 IMAGE_ATOM_ID = -300
 IMAGE_INDICATOR_IDS = [-301, -302, -303, -304, -305]
 
-AutoConfig.register("aimv2", AIMv2Config)
+AutoConfig.register("aimv2", AIMv2Config, exist_ok=True)
 
 
 # ----------------------------------------------------------------------
