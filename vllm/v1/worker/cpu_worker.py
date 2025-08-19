8e2c206e43528117d8a6716e3923619362e86711
Nikhil Suryawanshi <77109245+nikheal2@users.noreply.github.com>
2025-08-19 10:10:37 +0530

    [Hardware][IBM Z]Enable v1 for s390x and s390x dockerfile fixes (#22725)
    Signed-off-by: Nikhil Suryawanshi <suryawanshin74@gmail.com>

--- vllm/v1/worker/cpu_worker.py
+++ vllm/v1/worker/cpu_worker.py
@@ -43,8 +43,9 @@ class CPUWorker(Worker):
         # Setup OpenMP threads affinity.
         omp_cpuids = envs.VLLM_CPU_OMP_THREADS_BIND
         if omp_cpuids == "auto" and platform.system() == "Linux":
-            if current_platform.get_cpu_architecture() == CpuArchEnum.POWERPC:
-                # For POWERPC SMT-8/4/2
+            cpu_arch = current_platform.get_cpu_architecture()
+            if cpu_arch in (CpuArchEnum.POWERPC, CpuArchEnum.S390X):
+                # For S390X/POWERPC SMT-8/4/2
                 self.local_omp_cpuid = self._get_autobind_cpu_ids(
                     lambda cpus: [cpu for cpu in cpus if cpu.id % 8 < 4])
             elif current_platform.get_cpu_architecture() == CpuArchEnum.X86:
