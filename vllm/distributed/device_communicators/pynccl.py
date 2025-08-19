a76171059113a091f4c1bb18955dabef576dea44
Amir Samani <asamani@nvidia.com>
2025-08-19 14:25:05 -0700

    wip

--- vllm/distributed/device_communicators/pynccl.py
+++ vllm/distributed/device_communicators/pynccl.py
@@ -13,6 +13,7 @@ from vllm.distributed.device_communicators.pynccl_wrapper import (
     ncclRedOpTypeEnum, ncclUniqueId)
 from vllm.distributed.utils import StatelessProcessGroup
 from vllm.logger import init_logger
+from vllm.utils import current_stream
 
 
 logger = init_logger(__name__)
