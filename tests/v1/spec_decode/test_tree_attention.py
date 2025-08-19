7a2ebfe40d182eaedc7d8de485fc777ecb600c77
Andrew O'Neill <a.oneill@samsung.com>
2025-08-19 16:17:07 +0000

    use benchmark from fa2 module

--- tests/v1/spec_decode/test_tree_attention.py
+++ tests/v1/spec_decode/test_tree_attention.py
@@ -12,30 +12,7 @@ from tests.v1.attention.utils import (_Backend, create_standard_kv_cache_spec,
 from vllm.config import ParallelConfig, SpeculativeConfig
 from vllm.v1.attention.backends.utils import CommonAttentionMetadata
 from vllm.v1.attention.backends.flash_attn import (FlashAttentionMetadataBuilder, TreeMetadata)
-
-import torch.utils.benchmark as benchmark
-
-
-def benchmark_forward(
-    fn, *inputs, repeats=10, desc="", verbose=True, amp=False, amp_dtype=torch.float16, **kwinputs
-):
-    """Use Pytorch Benchmark on the forward pass of an arbitrary function."""
-    if verbose:
-        print(desc, "- Forward pass")
-
-    def amp_wrapper(*inputs, **kwinputs):
-        with torch.autocast(device_type="cuda", dtype=amp_dtype, enabled=amp):
-            fn(*inputs, **kwinputs)
-
-    t = benchmark.Timer(
-        stmt="fn_amp(*inputs, **kwinputs)",
-        globals={"fn_amp": amp_wrapper, "inputs": inputs, "kwinputs": kwinputs},
-        num_threads=torch.get_num_threads(),
-    )
-    m = t.timeit(repeats)
-    if verbose:
-        print(m)
-    return t, m
+from vllm.vllm_flash_attn.utils.benchmark import benchmark_forward
 
 
 class MockAttentionLayer(torch.nn.Module):
