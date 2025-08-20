c9f3c8d601b534c3441873e17de29b0879ed01d6
Bill Nell <bnell@redhat.com>
2025-08-20 23:38:55 +0000

    fixes + add prepare_no_receive to deepep ll
    Signed-off-by: Bill Nell <bnell@redhat.com>

--- vllm/model_executor/layers/shared_fused_moe/shared_fused_moe.py
+++ vllm/model_executor/layers/shared_fused_moe/shared_fused_moe.py
@@ -1,24 +1,35 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+from typing import Optional
+
 import torch
 
 from vllm.model_executor.layers.fused_moe.layer import FusedMoE
 
 #@CustomOp.register("shared_fused_moe") ???
 
+
 # TODO: add shared + fused combo function?
 class SharedFusedMoE(FusedMoE):
+
     def __init__(self, shared_experts: torch.nn.Module, **kwargs):
-        super().__init__(**kwargs, shared_experts=shared_experts)
-        #self.shared_experts_fn = shared_experts # for now
+        super().__init__(**kwargs)
+        self._shared_experts = shared_experts
+
+    @property
+    def shared_experts(self) -> Optional[torch.nn.Module]:
+        return self._shared_experts
 
     def forward(
-        self,
-        hidden_states: torch.Tensor,
-        router_logits: torch.Tensor
-    ) -> tuple[torch.Tensor, torch.Tensor]:
-        #shared_out = self.shared_experts_fn(hidden_states)
+            self, hidden_states: torch.Tensor,
+            router_logits: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
         shared_out, fused_out = super().forward(
             hidden_states=hidden_states,
             router_logits=router_logits,
         )
+
+        # If shared_out is None, it means the shared experts have not
+        #if shared_out is None:
+        #    shared_out = self.shared_experts_fn(hidden_states)
 
         return shared_out, fused_out
