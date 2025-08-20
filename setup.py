f79103ead8f0635ddfcef12f94339a9e9f9ba176
Michael Goin <mgoin64@gmail.com>
2025-08-20 08:05:54 -0400

    Update to flashinfer-python==0.2.12 and disable AOT compile for non-release image (#23129)
    Signed-off-by: mgoin <mgoin64@gmail.com>

--- setup.py
+++ setup.py
@@ -685,7 +685,7 @@ setup(
                   "mistral_common[audio]"],  # Required for audio processing
         "video": [],  # Kept for backwards compatibility
         # FlashInfer should be updated together with the Dockerfile
-        "flashinfer": ["flashinfer-python==0.2.11"],
+        "flashinfer": ["flashinfer-python==0.2.12"],
     },
     cmdclass=cmdclass,
     package_data=package_data,
