3f8a7238a47d352d90c1e6c037ee2374bd3c3d7f
Kyle Sayers <kylesayrs@gmail.com>
2025-08-20 12:02:26 -0400

    fix typo
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- vllm/model_executor/layers/linear.py
+++ vllm/model_executor/layers/linear.py
@@ -229,8 +229,7 @@ class UnquantizedLinearMethod(LinearMethodBase):
               x: torch.Tensor,
               bias: Optional[torch.Tensor] = None) -> torch.Tensor:
 
-        return dispatch_unquantized_gemm()(layer, x, self.layer_weight_data,
-                                           bias)
+        return dispatch_unquantized_gemm()(layer, x, self.layer, bias)
 
 
 class LinearBase(CustomOp):
