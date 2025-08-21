b7823007835b2f71a126c9e25c46c3dc0c25145b
mengxingkongzhouhan <117415539+mengxingkongzhouhan@users.noreply.github.com>
2025-08-21 15:25:51 +0800

    Update policy_factory.py
    Signed-off-by: mengxingkongzhouhan <117415539+mengxingkongzhouhan@users.noreply.github.com>

--- vllm/distributed/eplb/policy/policy_factory.py
+++ vllm/distributed/eplb/policy/policy_factory.py
@@ -1,7 +1,6 @@
 # Copyright Huawei Technologies Co., Ltd. 2023-2024. All rights reserved.
 from .policy_abstract import DynamicConfig, EplbPolicy
 from .policy_dynamic_ep import DynamicEplb
-from .policy_dynamic_ep_v2 import DynamicEplbV2
 from .policy_default_eplb import DefaultEplb
 
 
@@ -18,8 +17,7 @@ class PolicyFactory:
             0:
             DefaultEplb,  # RandomLoadBalance: shuffle last physical expert on NPU 1 and 3
             1:
-            DynamicEplb,  # Dynamic EPLB policy: overall expert replacement based on current moe load
-            2:
-            DynamicEplbV2,  # Dynamic EPLB policy V2:  expert replacement with constrained number of expert shuffle
+            DynamicEplbV2,  # Dynamic EPLB policy:  expert replacement with constrained number of expert shuffle
         }
+
         return policy.get(policy_type, DefaultEplb)(config)
