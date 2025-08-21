139ea1614efc90052d314d9fabf8a53a29a93c50
Pavani Majety <pmajety@nvidia.com>
2025-08-21 13:02:11 -0700

    [Core] Always use tensor cores for Flashinfer Decode Wrapper (#23214)
    Signed-off-by: Pavani Majety <pmajety@nvidia.com>
    Signed-off-by: Joachim Studnia <joachim@mistral.ai>

--- tests/kernels/attention/test_flashinfer_trtllm_attention.py
+++ tests/kernels/attention/test_flashinfer_trtllm_attention.py
@@ -136,9 +136,7 @@ def test_flashinfer_trtllm_decode_with_baseline(
 
     # Baseline Decode
     wrapper = flashinfer.BatchDecodeWithPagedKVCacheWrapper(
-        workspace_buffer,
-        kv_layout,
-        use_tensor_cores=((num_qo_heads // num_kv_heads) > 4))
+        workspace_buffer, kv_layout, use_tensor_cores=True)
     wrapper.plan(kv_indptr,
                  kv_indices,
                  kv_last_page_lens,
