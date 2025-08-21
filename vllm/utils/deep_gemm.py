e41192f21d6640261daa5fdc153d7ecabf135e70
yewentao256 <zhyanwentao@126.com>
2025-08-21 09:03:11 -0700

    fix assert
    Signed-off-by: yewentao256 <zhyanwentao@126.com>

--- vllm/utils/deep_gemm.py
+++ vllm/utils/deep_gemm.py
@@ -204,7 +204,6 @@ def calc_diff(x: torch.Tensor, y: torch.Tensor):
 
 def should_use_deepgemm_for_fp8_linear(output_dtype: torch.dtype,
                                        weight: torch.Tensor):
-    assert output_dtype == torch.bfloat16
     return (is_deep_gemm_supported() and output_dtype == torch.bfloat16
             and weight.shape[0] % 128 == 0 and weight.shape[1] % 128 == 0)
 
