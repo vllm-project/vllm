b81e5cf590502e08345f65ebe569b7e09480f334
Sahithi Chigurupati <chigurupati.sahithi@gmail.com>
2025-08-22 18:35:35 -0700

    Remove V0 only core tests
    Signed-off-by: Sahithi Chigurupati <chigurupati.sahithi@gmail.com>

--- tests/core/block/e2e/test_correctness.py
+++ tests/core/block/e2e/test_correctness.py
@@ -16,88 +16,13 @@ from .conftest import get_token_ids_from_llm_generator
         # Use a small model for a fast test.
         "model": "facebook/opt-125m",
 
-        # skip cuda graph creation for fast test.
-        "enforce_eager": True,
-
-        # Allow only 5 sequences of ~1024 tokens in worst case.
-        "block_size": 16,
-        "num_gpu_blocks_override": 5 * (64 + 1),
-    }])
-@pytest.mark.parametrize("per_test_common_llm_kwargs", [{}])
-@pytest.mark.parametrize("baseline_llm_kwargs", [{}])
-@pytest.mark.parametrize("test_llm_kwargs", [{
-    "preemption_mode": "swap"
-}, {
-    "preemption_mode": "recompute"
-}])
-@pytest.mark.parametrize("batch_size", [10])
-@pytest.mark.parametrize("seed", [1])
-def test_block_manager_with_preemption(baseline_llm_generator,
-                                       test_llm_generator, batch_size):
-    """Verify block manager produces same outputs even when there is preemption.
-
-    This constructs two LLM, each with limited number of GPU blocks. The limit
-    is decided such that as the sequences in the batch grow, sequences must be
-    preempted and removed from cache.
-
-    If the output token ids are equivalent, then we have confidence that the KV
-    cache is not corrupted.
-
-    NOTE: We want a significant number of generated tokens so that any incorrect
-    KV mapping has time to build up error.
-
-    NOTE(Kuntai): Though we have removed block manager v1, this test is still
-    useful as it asserts the behavior of block manager v2 (now it is called 
-    SelfAttnBlockSpaceManager) is the same when swapping / preemption, so we  
-    keep this test.
-    """
-    output_len = 1024
-    temperature = 0.0
-
-    # We want to ensure equality even with preemption.
-    # We force the total block size to be 1 + cdiv(output_len, block_size)
-    # so that only one sequence can fit at a time (once the sequences grow).
-
-    prompts = [
-        "Hello, my name is",
-        "The president of the United States is",
-        "The capital of France is",
-        "The future of AI is",
-    ]
-
-    prompts = [prompt for prompt, _ in zip(cycle(prompts), range(batch_size))]
-
-    sampling_params = SamplingParams(
-        max_tokens=output_len,
-        ignore_eos=True,
-        temperature=temperature,
-    )
-
-    baseline_token_ids = get_token_ids_from_llm_generator(
-        baseline_llm_generator, prompts, sampling_params)
-
-    test_token_ids = get_token_ids_from_llm_generator(test_llm_generator,
-                                                      prompts, sampling_params)
-
-    for expected_token_ids, actual_token_ids in zip(baseline_token_ids,
-                                                    test_token_ids):
-        assert expected_token_ids == actual_token_ids
-
-    assert baseline_token_ids == test_token_ids
-
-
-@pytest.mark.parametrize(
-    "common_llm_kwargs",
-    [{
-        # Use a small model for a fast test.
-        "model": "facebook/opt-125m",
-
         # Our prompts will generate 128 tokens; since the prompts themselves are
         # small, we don't need much KV space beyond 128.
         "max_model_len": 160,
 
         # skip cuda graph creation for fast test.
         "enforce_eager": True,
+        "block_size": 16,
     }])
 @pytest.mark.parametrize(
     "per_test_common_llm_kwargs",
@@ -110,7 +35,7 @@ def test_block_manager_with_preemption(baseline_llm_generator,
             "num_gpu_blocks_override": 2 * (8 + 1),
         },
         {
-            "block_size": 8,
+            "block_size": 16,
 
             # Allow only 2 sequences of ~128 tokens in worst case.
             # Note 16 = 128/block_size
@@ -122,31 +47,22 @@ def test_block_manager_with_preemption(baseline_llm_generator,
 }])
 @pytest.mark.parametrize(
     "test_llm_kwargs",
-    [
-        {
-            # We run one test with block_size < lookahead_slots, one test with
-            # block_size > lookahead_slots
-            "num_lookahead_slots": 10,
-            "preemption_mode": "swap",
-        },
-        {
-            "num_lookahead_slots": 10,
-            "preemption_mode": "recompute",
-        }
-    ])
+    [{
+        # We run one test with block_size < lookahead_slots, one test with
+        # block_size > lookahead_slots
+        "num_lookahead_slots": 10,
+    }])
 @pytest.mark.parametrize("batch_size", [4])
 @pytest.mark.parametrize("seed", [1])
-def test_lookahead_greedy_equality_with_preemption(baseline_llm_generator,
-                                                   test_llm_generator,
-                                                   batch_size):
+def test_lookahead_greedy_equality(baseline_llm_generator, test_llm_generator,
+                                   batch_size):
     """Verify vLLM produces the same output with greedy sampling, when lookahead
     scheduling is used vs. not.
 
     Lookahead scheduling is not expected to modify the output, as it simply
     allocates empty slots ahead of the known token ids in a sliding fashion.
 
-    This test constrains the total number of blocks to force preemption. It also
-    varies the block size so that the lookahead size is less than and greater
+    This test varies the block size so that the lookahead size is less than and greater
     than the block size.
     """
     output_len = 128
@@ -191,7 +107,6 @@ def test_lookahead_greedy_equality_with_preemption(baseline_llm_generator,
 
             # skip cuda graph creation for fast test.
             "enforce_eager": True,
-            "enable_chunked_prefill": True,
         },
     ])
 @pytest.mark.parametrize("per_test_common_llm_kwargs",
@@ -278,16 +193,12 @@ def test_chunked_prefill_block_manager(baseline_llm_generator,
     }])
 @pytest.mark.parametrize("per_test_common_llm_kwargs", [{}])
 @pytest.mark.parametrize("baseline_llm_kwargs", [{}])
-@pytest.mark.parametrize("test_llm_kwargs", [{
-    "preemption_mode": "swap"
-}, {
-    "preemption_mode": "recompute"
-}])
+@pytest.mark.parametrize("test_llm_kwargs", [{}])
 @pytest.mark.parametrize("batch_size", [10])
 @pytest.mark.parametrize("seed", [1])
-def test_block_manager_prefix_caching_enabled_with_preemption(
-        baseline_llm_generator, test_llm_generator, batch_size):
-    """Verify block manager produces same outputs even when there is preemption.
+def test_block_manager_prefix_caching_enabled(baseline_llm_generator,
+                                              test_llm_generator, batch_size):
+    """Verify block manager produces same outputs with prefix caching enabled.
 
     This constructs two LLM, each with limited number of GPU blocks. The limit
     is decided such that as the sequences in the batch grow, sequences must be
@@ -360,17 +271,13 @@ def test_block_manager_prefix_caching_enabled_with_preemption(
 }])
 @pytest.mark.parametrize("test_llm_kwargs", [{
     "enable_prefix_caching": True,
-    "preemption_mode": "swap"
-}, {
-    "enable_prefix_caching": True,
-    "preemption_mode": "recompute"
 }])
 @pytest.mark.parametrize("batch_size", [10])
 @pytest.mark.parametrize("seed", [1])
-def test_auto_prefix_caching_with_preemption(baseline_llm_generator,
-                                             test_llm_generator, batch_size):
+def test_auto_prefix_caching(baseline_llm_generator, test_llm_generator,
+                             batch_size):
     """Verify block manager v2 with auto prefix caching enabled produces same
-    outputs as auto prefix caching disabled, even when there is preemption.
+    outputs as auto prefix caching disabled.
 
     This constructs two LLM, each with limited number of GPU blocks. The limit
     is decided such that as the sequences in the batch grow, sequences must be
@@ -425,9 +332,9 @@ def test_auto_prefix_caching_with_preemption(baseline_llm_generator,
         "enforce_eager": True,
 
         # we keep the blocks small, so that hit eviction quickly
-        "max_model_len": 48,
+        "max_model_len": 128,
         "block_size": 16,
-        "num_gpu_blocks_override": 3,
+        "num_gpu_blocks_override": 8,
     }])
 @pytest.mark.parametrize("per_test_common_llm_kwargs", [{}])
 @pytest.mark.parametrize("baseline_llm_kwargs", [{
