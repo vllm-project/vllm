57c1e0b30e7a4887764f0d5c744e8b13f391130d
汪志鹏 <wangzhipeng628@gmail.com>
2025-08-24 20:52:24 +0800

    [New Model]Donut model (#23229)
    Signed-off-by: 汪志鹏 <wangzhipeng628@gmail.com>
    Signed-off-by: Joachim Studnia <joachim@mistral.ai>

--- vllm/engine/llm_engine.py
+++ vllm/engine/llm_engine.py
@@ -1822,7 +1822,7 @@ class LLMEngine:
                 assert isinstance(mm_processor, EncDecMultiModalProcessor)
 
                 if mm_processor.pad_dummy_encoder_prompt:
-                    return  # Skip encoder length check for Whisper
+                    return  # Skip encoder length check for Whisper and Donut
 
             if model_config.is_multimodal_model:
                 suggestion = (
