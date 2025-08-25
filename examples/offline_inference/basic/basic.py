597d6ab1db71745c3c4b682d93538fee8d5b54b8
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2025-08-24 19:10:05 -0700

    revert
    Signed-off-by: Woosuk Kwon <woosuk.kwon@berkeley.edu>

--- examples/offline_inference/basic/basic.py
+++ examples/offline_inference/basic/basic.py
@@ -11,7 +11,7 @@ prompts = [
     "The future of AI is",
 ]
 # Create a sampling params object.
-sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=10)
+sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
 
 def main():
