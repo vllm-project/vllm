a05ce1b53671587e7b68ae54f69b1d3e8869fe1a
Zebing Lin <linzebing1995@gmail.com>
2025-08-25 11:45:43 -0400

    [Core] Use sha256 bytes instead of BlockHash to reduce GC overhead
    Signed-off-by: linzebing <linzebing1995@gmail.com>

--- vllm/v1/core/block_pool.py
+++ vllm/v1/core/block_pool.py
@@ -8,7 +8,9 @@ from vllm.distributed.kv_events import (AllBlocksCleared, BlockRemoved,
                                         BlockStored, KVCacheEvent)
 from vllm.logger import init_logger
 from vllm.v1.core.kv_cache_utils import (BlockHash, BlockHashWithGroupId,
-                                         FreeKVCacheBlockQueue, KVCacheBlock)
+                                         FreeKVCacheBlockQueue, KVCacheBlock,
+                                         make_block_hash_with_group_id,
+                                         split_block_hash_with_group_id)
 from vllm.v1.request import Request
 
 logger = init_logger(__name__)
@@ -83,8 +85,10 @@ class BlockPool:
         """
         cached_blocks = []
         for group_id in kv_cache_group_ids:
+            block_hash_with_group_id = make_block_hash_with_group_id(
+                block_hash, group_id)
             cached_blocks_one_group = self.cached_block_hash_to_block.get(
-                BlockHashWithGroupId(block_hash, group_id))
+                block_hash_with_group_id)
             if not cached_blocks_one_group:
                 return None
             first_block = next(iter(cached_blocks_one_group.values()))
@@ -123,20 +127,20 @@ class BlockPool:
         assert len(request.block_hashes) >= num_full_blocks
         new_block_hashes = request.block_hashes[num_cached_blocks:]
 
-        new_hashes: Optional[list[int]] = ([] if self.enable_kv_cache_events
-                                           else None)
+        new_hashes: Optional[list[BlockHash]] = (
+            [] if self.enable_kv_cache_events else None)
         for i, blk in enumerate(new_full_blocks):
             assert blk.block_hash is None
             block_hash = new_block_hashes[i]
 
             # Update and added the full block to the cache.
-            block_hash_with_group_id = BlockHashWithGroupId(
+            block_hash_with_group_id = make_block_hash_with_group_id(
                 block_hash, kv_cache_group_id)
             blk.block_hash = block_hash_with_group_id
             self.cached_block_hash_to_block[block_hash_with_group_id][
                 blk.block_id] = blk
             if new_hashes is not None:
-                new_hashes.append(block_hash.hash_value)
+                new_hashes.append(block_hash)
 
         if self.enable_kv_cache_events:
             if num_cached_blocks == 0:
@@ -144,7 +148,8 @@ class BlockPool:
             else:
                 parent_block = blocks[num_cached_blocks - 1]
                 assert parent_block.block_hash is not None
-                parent_block_hash = parent_block.block_hash.get_hash_value()
+                parent_block_hash, _ = split_block_hash_with_group_id(
+                    parent_block.block_hash)
 
             self.kv_event_queue.append(
                 BlockStored(
@@ -218,7 +223,9 @@ class BlockPool:
             # we disable hybrid kv cache manager when kv cache event is
             # enabled, so there is only one group.
             self.kv_event_queue.append(
-                BlockRemoved(block_hashes=[block_hash.get_hash_value()]))
+                BlockRemoved(block_hashes=[
+                    split_block_hash_with_group_id(block_hash)[0]
+                ]))
         return True
 
     def touch(self, blocks: tuple[list[KVCacheBlock], ...]) -> None:
