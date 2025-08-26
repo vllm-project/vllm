2663d0eeb5dd5d8a961371d443388c4bfa02df6e
Roger Wang <hey@rogerw.io>
2025-08-26 08:52:50 +0000

    typing
    Signed-off-by: Roger Wang <hey@rogerw.io>

--- vllm/multimodal/processing.py
+++ vllm/multimodal/processing.py
@@ -960,7 +960,7 @@ class BaseProcessingInfo:
 
 _I = TypeVar("_I", bound=BaseProcessingInfo)
 
-MultiModalHashes = dict[str, list[Optional[str]]]
+MultiModalHashes = dict[str, list[str]]
 """
 A collection of hashes with a similar structure as
 [`MultiModalKwargsItems`][vllm.multimodal.inputs.MultiModalKwargsItems].
