e7f0247bf624b8308a45732694efdade385ceadb
Huy Do <huydhn@gmail.com>
2025-08-26 04:16:09 -0700

    Fix writing benchmark results with tuple keys (#23633)
    Signed-off-by: Huy Do <huydhn@gmail.com>
    Signed-off-by: Joachim Studnia <joachim@mistral.ai>

--- vllm/benchmarks/lib/utils.py
+++ vllm/benchmarks/lib/utils.py
@@ -54,7 +54,12 @@ class InfEncoder(json.JSONEncoder):
 
     def clear_inf(self, o: Any):
         if isinstance(o, dict):
-            return {k: self.clear_inf(v) for k, v in o.items()}
+            return {
+                str(k)
+                if not isinstance(k, (str, int, float, bool, type(None)))
+                else k: self.clear_inf(v)
+                for k, v in o.items()
+            }
         elif isinstance(o, list):
             return [self.clear_inf(v) for v in o]
         elif isinstance(o, float) and math.isinf(o):
