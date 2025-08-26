58a3d6544297e853bdde904b87dcc150948f17f5
Isotr0py <mozf@mail2.sysu.edu.cn>
2025-08-27 03:46:52 +0800

    [Model] Enable video support for InternVL3.5 models (#23658)
    Signed-off-by: Isotr0py <mozf@mail2.sysu.edu.cn>

--- tests/models/multimodal/processing/test_tensor_schema.py
+++ tests/models/multimodal/processing/test_tensor_schema.py
@@ -38,7 +38,12 @@ ARCH_NEEDS_EXTRAS = [
     "MiniCPMV",
     "PaliGemmaForConditionalGeneration",
 ]
-REPO_ID_TO_SKIP = {"nm-testing/pixtral-12b-FP8-dynamic": "duplicated test"}
+REPO_ID_TO_SKIP = {
+    "nm-testing/pixtral-12b-FP8-dynamic": "duplicated test",
+    # FIXME(Isotr0py): enable GPT-OSS based InternVL3.5 model
+    # after support PP for GPT-OSS
+    "OpenGVLab/InternVL3_5-GPT-OSS-20B-A4B-Preview": "Broken model",
+}
 
 ImageInput = list[Image.Image]
 VideoInput = Union[list[Image.Image], list[np.ndarray],
