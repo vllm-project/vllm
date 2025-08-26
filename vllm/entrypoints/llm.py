581a89db5993ded36dbf7f9fc3546c8512ab258e
Zerohertz <ohg3417@gmail.com>
2025-08-26 21:18:48 +0900

    docs: type hint in entrypoints llm
    WARNING -  griffe: vllm/entrypoints/llm.py:150: No type or annotation for parameter '**kwargs'
    
    Signed-off-by: Zerohertz <ohg3417@gmail.com>

--- vllm/entrypoints/llm.py
+++ vllm/entrypoints/llm.py
@@ -186,7 +186,7 @@ class LLM:
                                            CompilationConfig]] = None,
         logits_processors: Optional[list[Union[str,
                                                type[LogitsProcessor]]]] = None,
-        **kwargs,
+        **kwargs: object,
     ) -> None:
         """LLM constructor."""
 
