1d8af764b01084627026e6736f330b75a9dc738d
Michael Goin <mgoin64@gmail.com>
2025-08-25 21:29:00 -0400

    [CI Fix] Pin deepep and pplx tags in tools/ep_kernels/, gate multigpu tests (#23568)
    Signed-off-by: mgoin <mgoin64@gmail.com>
    Signed-off-by: Joachim Studnia <joachim@mistral.ai>

--- tests/utils.py
+++ tests/utils.py
@@ -696,9 +696,12 @@ def multi_process_parallel(
     os.environ["RAY_RUNTIME_ENV_IGNORE_GITIGNORE"] = "1"
     ray.init(
         runtime_env={
-            "working_dir": VLLM_PATH,
-            "excludes":
-            ["build", ".git", "cmake-build-*", "shellcheck", "dist"]
+            "working_dir":
+            VLLM_PATH,
+            "excludes": [
+                "build", ".git", "cmake-build-*", "shellcheck", "dist",
+                "ep_kernels_workspace"
+            ]
         })
 
     distributed_init_port = get_open_port()
