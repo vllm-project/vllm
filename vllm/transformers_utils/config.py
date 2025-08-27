6975536494547022c75372278deefa98a7ca3dd7
youkaichao <youkaichao@gmail.com>
2025-08-27 19:31:26 +0800

    Update vllm/transformers_utils/config.py
    Signed-off-by: youkaichao <youkaichao@gmail.com>

--- vllm/transformers_utils/config.py
+++ vllm/transformers_utils/config.py
@@ -507,7 +507,7 @@ def get_config(
         config.quantization_config = quantization_config
         # auto-enable DeepGEMM UE8M0 on Hopper if model config requests it
         scale_fmt = quantization_config.get("scale_fmt", None)
-        if scale_fmt in ("ue8m0"):
+        if scale_fmt in ("ue8m0",):
             if not envs.is_set("VLLM_USE_DEEP_GEMM_E8M0_HOPPER"):
                 os.environ["VLLM_USE_DEEP_GEMM_E8M0_HOPPER"] = "1"
                 logger.info_once(
