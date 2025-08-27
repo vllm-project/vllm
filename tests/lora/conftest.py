4779a0a7dfd0af9c7c8dfcfe31c65bcd87168e71
Jee Jee Li <pandaleefree@gmail.com>
2025-08-27 13:51:50 +0800

    [CI/Build] Remove redundant LoRA model tests (#23706)
    Signed-off-by: Jee Jee Li <pandaleefree@gmail.com>

--- tests/lora/conftest.py
+++ tests/lora/conftest.py
@@ -216,11 +216,6 @@ def tinyllama_lora_files():
     return snapshot_download(repo_id="jashing/tinyllama-colorist-lora")
 
 
-@pytest.fixture(scope="session")
-def phi2_lora_files():
-    return snapshot_download(repo_id="isotr0py/phi-2-test-sql-lora")
-
-
 @pytest.fixture
 def reset_default_device():
     """
