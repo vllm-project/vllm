221008915705829fbb0e3d3c60b1ed801fa01515
Michele Gazzetti <michele.gazzetti1@ibm.com>
2025-08-27 08:54:27 +0000

    Add tests to ensure that the TerraTorch backend can successfully initialise SemanticSegmentation and PixelwiseRegression models
    Signed-off-by: Michele Gazzetti <michele.gazzetti1@ibm.com>

---
+++ tests/models/test_terratorch.py
@@ -0,0 +1,41 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+
+import pytest
+import torch
+
+from tests.conftest import VllmRunner
+
+MODEL_NAME = "mgazz/Prithvi_v2_eo_300_tl_unet_agb"
+DTYPE = "float16"
+
+
+@pytest.mark.parametrize(
+    "model",
+    [
+        "mgazz/Prithvi_v2_eo_300_tl_unet_agb",
+        "mgazz/Prithvi-EO-2.0-300M-TL-Sen1Floods11"
+    ],
+)
+@pytest.mark.parametrize("dtype", ["float16"])
+def test_inference(
+    vllm_runner: type[VllmRunner],
+    model: str,
+    dtype: str,
+) -> None:
+
+    with vllm_runner(model,
+                     skip_tokenizer_init=True,
+                     enforce_eager=True,
+                     dtype=dtype,
+                     runner="pooling",
+                     model_impl="terratorch") as vllm_model:
+
+        pixel_values = torch.full((6, 512, 512), 1.0, dtype=torch.float16)
+        location_coords = torch.full((1, 2), 1.0, dtype=torch.float16)
+        prompt = dict(prompt_token_ids=[1],
+                      multi_modal_data=dict(pixel_values=pixel_values,
+                                            location_coords=location_coords))
+        vllm_output = vllm_model.llm.encode(prompt)
+    assert torch.equal(
+        torch.isnan(vllm_output[0].outputs.data).any(), torch.tensor(False))
