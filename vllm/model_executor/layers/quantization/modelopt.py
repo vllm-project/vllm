b3c17f32114ce50ce009c8672dd65da795086829
Yongye Zhu <zyy1102000@gmail.com>
2025-08-27 17:33:21 -0400

    DP/EP Support for gpt-oss with deepep-ht comm kernel on SM100 (#23608)
    Signed-off-by: He-Jingkai <he-jingkai@outlook.com>

--- vllm/model_executor/layers/quantization/modelopt.py
+++ vllm/model_executor/layers/quantization/modelopt.py
@@ -311,6 +311,7 @@ class ModelOptFp8MoEMethod(FusedMoEMethodBase):
         self,
         prepare_finalize: mk.FusedMoEPrepareAndFinalize,
         moe: FusedMoEConfig,
+        layer: torch.nn.Module,
     ) -> mk.FusedMoEPermuteExpertsUnpermute:
         experts = select_cutlass_fp8_gemm_impl(
             moe,
@@ -1032,6 +1033,7 @@ class ModelOptNvFp4FusedMoE(FusedMoEMethodBase):
         self,
         prepare_finalize: mk.FusedMoEPrepareAndFinalize,
         moe: FusedMoEConfig,
+        layer: torch.nn.Module,
     ) -> mk.FusedMoEPermuteExpertsUnpermute:
         experts = select_nvfp4_gemm_impl(
             moe,
