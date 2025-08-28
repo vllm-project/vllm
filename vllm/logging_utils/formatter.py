657c3ee5e4149ff9017c800dd72d206816d8c212
zjy0516 <riverclouds.zhu@qq.com>
2025-08-29 00:16:36 +0800

    use relpath in debug level log
    Signed-off-by: zjy0516 <riverclouds.zhu@qq.com>

--- vllm/logging_utils/formatter.py
+++ vllm/logging_utils/formatter.py
@@ -2,6 +2,7 @@
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
 import logging
+import os
 
 
 class NewLineFormatter(logging.Formatter):
@@ -9,9 +10,25 @@ class NewLineFormatter(logging.Formatter):
 
     def __init__(self, fmt, datefmt=None, style="%"):
         logging.Formatter.__init__(self, fmt, datefmt, style)
+        self.root_dir = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "../.."))
 
     def format(self, record):
-        msg = logging.Formatter.format(self, record)
+        logger = logging.getLogger(record.name)
+        if logger.getEffectiveLevel() == logging.DEBUG:
+            abs_path = getattr(record, "pathname", None)
+            if abs_path:
+                try:
+                    relpath = os.path.relpath(abs_path, self.root_dir)
+                except Exception:
+                    relpath = record.filename
+            else:
+                relpath = record.filename
+            record.fileinfo = relpath
+        else:
+            record.fileinfo = record.filename
+
+        msg = super().format(record)
         if record.message != "":
             parts = msg.split(record.message)
             msg = msg.replace("\n", "\r\n" + parts[0])
