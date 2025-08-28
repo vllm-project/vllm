18e51bc649449aae9e128a2f96c31ed69a2051d3
raghavan <oneraghavan@gmail.com>
2025-08-28 08:51:22 +0000

    Small cleanup in llm.py

--- vllm/entrypoints/llm.py
+++ vllm/entrypoints/llm.py
@@ -365,9 +365,7 @@ class LLM:
             sampling_params = self.get_default_sampling_params()
 
         tokenization_kwargs: dict[str, Any] = {}
-        truncate_prompt_tokens = None
-        if isinstance(sampling_params, SamplingParams):
-            truncate_prompt_tokens = sampling_params.truncate_prompt_tokens
+        truncate_prompt_tokens = sampling_params.truncate_prompt_tokens
 
         _validate_truncation_size(model_config.max_model_len,
                                   truncate_prompt_tokens, tokenization_kwargs)
