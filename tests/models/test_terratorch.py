c38617d9c632d36f8ce82c599bf54e3284291680
Michele Gazzetti <michele.gazzetti1@ibm.com>
2025-08-28 16:23:48 +0000

    update terratorch test
    Signed-off-by: Michele Gazzetti <michele.gazzetti1@ibm.com>

--- tests/models/test_terratorch.py
+++ tests/models/test_terratorch.py
@@ -5,8 +5,7 @@ import pytest
 import torch
 
 from tests.conftest import VllmRunner
-
-DTYPE = "float16"
+from vllm.utils import set_default_torch_num_threads
 
 
 @pytest.mark.parametrize(
@@ -21,18 +20,26 @@ def test_inference(
     model: str,
 ) -> None:
 
-    with vllm_runner(model,
-                     skip_tokenizer_init=True,
-                     enforce_eager=True,
-                     dtype=DTYPE,
-                     runner="pooling",
-                     model_impl="terratorch") as vllm_model:
+    pixel_values = torch.full((6, 512, 512), 1.0, dtype=torch.float16)
+    location_coords = torch.full((1, 2), 1.0, dtype=torch.float16)
+    prompt = dict(prompt_token_ids=[1],
+                  multi_modal_data=dict(pixel_values=pixel_values,
+                                        location_coords=location_coords))
+    with (
+            set_default_torch_num_threads(1),
+            vllm_runner(
+                model,
+                runner="pooling",
+                dtype=torch.float16,
+                enforce_eager=True,
+                skip_tokenizer_init=True,
+                # Limit the maximum number of sequences to avoid the
+                # test going OOM during the warmup run
+                max_num_seqs=32,
+            ) as vllm_model,
+    ):
 
-        pixel_values = torch.full((6, 512, 512), 1.0, dtype=torch.float16)
-        location_coords = torch.full((1, 2), 1.0, dtype=torch.float16)
-        prompt = dict(prompt_token_ids=[1],
-                      multi_modal_data=dict(pixel_values=pixel_values,
-                                            location_coords=location_coords))
         vllm_output = vllm_model.llm.encode(prompt)
-    assert torch.equal(
-        torch.isnan(vllm_output[0].outputs.data).any(), torch.tensor(False))
+        assert torch.equal(
+            torch.isnan(vllm_output[0].outputs.data).any(),
+            torch.tensor(False))
