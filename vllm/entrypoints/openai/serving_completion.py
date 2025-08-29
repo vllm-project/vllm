ab064b29a24a3b7111a5ac0042a4a43362bf6879
22quinn <33176974+22quinn@users.noreply.github.com>
2025-08-28 20:55:34 -0700

    no more error
    Signed-off-by: 22quinn <33176974+22quinn@users.noreply.github.com>

--- vllm/entrypoints/openai/serving_completion.py
+++ vllm/entrypoints/openai/serving_completion.py
@@ -127,8 +127,12 @@ class OpenAIServingCompletion(OpenAIServing):
         try:
             lora_request = self._maybe_get_adapters(request)
 
-            # tokenizer = await self.engine_client.get_tokenizer(lora_request)
-            tokenizer = None
+            if (await
+                    self.engine_client.get_model_config()).skip_tokenizer_init:
+                tokenizer = None
+            else:
+                tokenizer = await self.engine_client.get_tokenizer(lora_request
+                                                                   )
 
             request_prompts, engine_prompts = await self._preprocess_completion(
                 request,
