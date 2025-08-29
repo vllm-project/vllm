7d951cf8977d3b38b840b569b735c8c95f88bf8e
n1ck-guo <heng.guo@intel.com>
2025-08-28 20:27:28 -0400

    fix by comment
    Signed-off-by: n1ck-guo <heng.guo@intel.com>

--- vllm/model_executor/layers/quantization/auto_round.py
+++ vllm/model_executor/layers/quantization/auto_round.py
@@ -379,9 +379,11 @@ class AutoRoundConfig(QuantizationConfig):
 
     def get_quant_method(self, layer: torch.nn.Module, prefix: str):
         if prefix and self.extra_config:
+            if prefix == "lm_head" and prefix not in self.extra_config:
+                return UnquantizedLinearMethod()
             for layer_name in self.extra_config:
-                if layer_name == prefix or layer_name.endswith(prefix):
-                    if self.extra_config[layer_name]['bits'] >= 16:
+                if layer_name == prefix or layer_name == f"model.{prefix}":
+                    if self.extra_config[layer_name].get('bits', 16) >= 16:
                         return UnquantizedLinearMethod()
         if (current_platform.is_cpu() or current_platform.is_xpu()
                 or self.backend == "ipex"):
