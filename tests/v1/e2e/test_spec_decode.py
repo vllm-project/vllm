9c640b2fed8253c3a2fe5a5de3395649888eea7e
Asher Zhang <asherszhang@tencent.com>
2025-08-29 14:28:20 +0800

    tests: disable unit test for hunyuan until transfomers support merged.
    Signed-off-by: Asher Zhang <asherszhang@tencent.com>

--- tests/v1/e2e/test_spec_decode.py
+++ tests/v1/e2e/test_spec_decode.py
@@ -130,8 +130,8 @@ def test_ngram_correctness(
     [
         # TODO: Re-enable this once tests/models/test_initialization.py is fixed, see PR #22333 #22611  # noqa: E501
         # (("eagle3", "Qwen/Qwen3-8B", "AngelSlim/Qwen3-8B_eagle3", 1), False),
-        (("eagle3", "tencent/Hunyuan-1.8B-Instruct",
-          "AngelSlim/Hunyuan-1.8B-Instruct_eagle3", 1), False),
+        # TODO: Re-enable this once transfomers release with cf487cd(transfomers git) # noqa: E501
+        # (("eagle3", "tencent/Hunyuan-1.8B-Instruct", "AngelSlim/Hunyuan-1.8B-Instruct_eagle3", 1), False), #noqa: E501
         (("eagle", "meta-llama/Llama-3.1-8B-Instruct",
           "yuhuili/EAGLE-LLaMA3.1-Instruct-8B", 1), False),
         (("eagle3", "meta-llama/Llama-3.1-8B-Instruct",
@@ -152,7 +152,8 @@ def test_ngram_correctness(
     ids=[
         # TODO: Re-enable this once tests/models/test_initialization.py is fixed, see PR #22333 #22611  # noqa: E501
         # "qwen3_eagle3",
-        "hunyuan_eagle3",
+        # TODO: Re-enable this once transfomers release with cf487cd(transfomers git) # noqa: E501
+        # "hunyuan_eagle3",
         "llama3_eagle",
         "llama3_eagle3",
         "llama4_eagle",
