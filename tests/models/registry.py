4aa77481577d646aefd011210ffea58dd7ad7286
EduardDurech <39579228+EduardDurech@users.noreply.github.com>
2025-08-29 14:29:18 +0200

    [MODEL] `Apertus` and `XIELU` (#23068)
    Signed-off-by: EduardDurech <39579228+EduardDurech@users.noreply.github.com>
    Co-authored-by: AllenHaoHuang <allenhuangdd@gmail.com>
    Signed-off-by: Joachim Studnia <joachim@mistral.ai>

--- tests/models/registry.py
+++ tests/models/registry.py
@@ -137,6 +137,9 @@ class _HfExamplesInfo:
 # yapf: disable
 _TEXT_GENERATION_EXAMPLE_MODELS = {
     # [Decoder-only]
+    "ApertusForCausalLM": _HfExamplesInfo("swiss-ai/Apertus-8B",
+                                          min_transformers_version="4.56.0",
+                                          trust_remote_code=True),
     "AquilaModel": _HfExamplesInfo("BAAI/AquilaChat-7B",
                                    trust_remote_code=True),
     "AquilaForCausalLM": _HfExamplesInfo("BAAI/AquilaChat2-7B",
--- vllm/model_executor/models/registry.py
+++ vllm/model_executor/models/registry.py
@@ -39,6 +39,7 @@ logger = init_logger(__name__)
 # yapf: disable
 _TEXT_GENERATION_MODELS = {
     # [Decoder-only]
+    "ApertusForCausalLM": ("apertus", "ApertusForCausalLM"),
     "AquilaModel": ("llama", "LlamaForCausalLM"),
     "AquilaForCausalLM": ("llama", "LlamaForCausalLM"),  # AquilaChat2
     "ArceeForCausalLM": ("arcee", "ArceeForCausalLM"),
