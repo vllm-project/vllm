f2cd9e09fea9ebe728c56de1d9d4dedd36d75dea
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-09-01 17:08:34 +0200

    `gd_params` -> `so_params`
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- vllm/v1/structured_output/backend_outlines.py
+++ vllm/v1/structured_output/backend_outlines.py
@@ -161,32 +161,32 @@ def validate_structured_output_request_outlines(params: SamplingParams):
     if params.structured_outputs is None:
         return
 
-    gd_params = params.structured_outputs
+    so_params = params.structured_outputs
 
-    if gd_params.regex:
-        validate_regex_is_buildable(gd_params.regex)
-    elif gd_params.json:
-        if isinstance(gd_params.json, str):
+    if so_params.regex:
+        validate_regex_is_buildable(so_params.regex)
+    elif so_params.json:
+        if isinstance(so_params.json, str):
             try:
                 # make sure schema is valid json
-                json.loads(gd_params.json)
-                schema = gd_params.json
+                json.loads(so_params.json)
+                schema = so_params.json
             except json.JSONDecodeError as e:
                 raise ValueError("Invalid JSON grammar specification.") from e
         else:
             try:
-                schema = json.dumps(gd_params.json)
+                schema = json.dumps(so_params.json)
             except Exception as e:
                 raise ValueError(
                     f"Error serializing structured outputs jsonschema: {e}"
                 ) from e
         pattern = json_schema.build_regex_from_schema(schema)
         validate_regex_is_buildable(pattern)
-    elif gd_params.choice:
-        choices = [regex_escape(str(choice)) for choice in gd_params.choice]
+    elif so_params.choice:
+        choices = [regex_escape(str(choice)) for choice in so_params.choice]
         regex = "(" + "|".join(choices) + ")"
         validate_regex_is_buildable(regex)
-    elif gd_params.grammar:
+    elif so_params.grammar:
         raise ValueError("Outlines structured outputs backend "
                          "does not support grammar specifications")
 
