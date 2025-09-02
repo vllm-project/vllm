16ccbdedbd0681f52ff1423e70b4fa6478bc9bc9
Micah Williamson <micah.williamson@amd.com>
2025-09-02 16:19:07 +0000

    Disable VLLM_ROCM_USE_AITER_RMSNORM by default for now to work around crash

--- vllm/envs.py
+++ vllm/envs.py
@@ -96,7 +96,7 @@ if TYPE_CHECKING:
     VLLM_ROCM_USE_AITER_PAGED_ATTN: bool = False
     VLLM_ROCM_USE_AITER_LINEAR: bool = True
     VLLM_ROCM_USE_AITER_MOE: bool = True
-    VLLM_ROCM_USE_AITER_RMSNORM: bool = True
+    VLLM_ROCM_USE_AITER_RMSNORM: bool = False
     VLLM_ROCM_USE_AITER_MLA: bool = True
     VLLM_ROCM_USE_AITER_MHA: bool = True
     VLLM_ROCM_USE_SKINNY_GEMM: bool = True
@@ -759,7 +759,7 @@ environment_variables: dict[str, Callable[[], Any]] = {
 
     # use aiter rms norm op if aiter ops are enabled.
     "VLLM_ROCM_USE_AITER_RMSNORM":
-    lambda: (os.getenv("VLLM_ROCM_USE_AITER_RMSNORM", "True").lower() in
+    lambda: (os.getenv("VLLM_ROCM_USE_AITER_RMSNORM", "False").lower() in
              ("true", "1")),
 
     # Whether to use aiter mla ops.
