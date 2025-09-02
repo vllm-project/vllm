4b63c8eba445bcece83390acc68f03140c9f1a25
n1ck-guo <heng.guo@intel.com>
2025-09-01 22:01:16 -0400

    format
    Signed-off-by: n1ck-guo <heng.guo@intel.com>

--- vllm/model_executor/layers/quantization/auto_round.py
+++ vllm/model_executor/layers/quantization/auto_round.py
@@ -127,7 +127,7 @@ class AutoRoundConfig(QuantizationConfig):
                     self.group_size if quantized else -1,
                     self.sym if quantized else True,
                 )
-            
+
             # exact match first
             if name in self.extra_config:
                 cfg = self.extra_config[name]
@@ -137,7 +137,7 @@ class AutoRoundConfig(QuantizationConfig):
                             self.group_size if quantized else -1),
                     cfg.get("sym", self.sym if quantized else True),
                 )
-            
+
             REGEX_SPECIAL_CHARS = set(r"*+?^$()[]{}|\\")
             for pattern, cfg in self.extra_config.items():
                 if not isinstance(pattern, str) or not any(
@@ -156,7 +156,7 @@ class AutoRoundConfig(QuantizationConfig):
                 except re.error:
                     # Invalid regex, ignore.
                     continue
-            
+
             return (
                 self.weight_bits if quantized else 16,
                 self.group_size if quantized else -1,
