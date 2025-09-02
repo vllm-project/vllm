cecc59a2f63166db0c8ca8551221674fc22cd58d
Xiao Li <ilx@meta.com>
2025-09-01 20:14:04 -0700

    fix type checking in test
    Signed-off-by: Xiao Li <ilx@meta.com>

--- tests/test_kernels_alias_modules.py
+++ tests/test_kernels_alias_modules.py
@@ -3,14 +3,19 @@
 
 import logging
 import sys
+import typing
 from logging import config as logging_config
 
 from vllm.kernels import _alias_modules
 
+if typing.TYPE_CHECKING:
+
+    def config() -> None:
+        pass
+
 
 def test_alias_modules():
     _alias_modules(logging, __name__)
-    # type: ignore
     from tests.test_kernels_alias_modules import config
     assert config == logging_config
     current_module = sys.modules[__name__]
@@ -20,7 +25,6 @@ def test_alias_modules():
 def test_no_error_when_duplicating_alias_modules():
     _alias_modules(logging, __name__)
     _alias_modules(logging, __name__)
-    # type: ignore
     from tests.test_kernels_alias_modules import config
     assert config == logging_config
     current_module = sys.modules[__name__]
