7799f2764d86da905095dd81834ca300466a53b4
yewentao256 <zhyanwentao@126.com>
2025-09-02 12:15:52 -0700

    fix routed_scaling_factor double mul issue
    Signed-off-by: yewentao256 <zhyanwentao@126.com>

--- vllm/model_executor/models/deepseek_v2.py
+++ vllm/model_executor/models/deepseek_v2.py
@@ -186,15 +186,8 @@ class DeepseekV2MoE(nn.Module):
         # router_logits: (num_tokens, n_experts)
         router_logits, _ = self.gate(hidden_states)
 
-        if hidden_states.dtype != torch.float16:
-            final_hidden_states = self.experts(
-                hidden_states=hidden_states,
-                router_logits=router_logits) * self.routed_scaling_factor
-        else:
-            # Fix FP16 overflow
-            # See DeepseekV2DecoderLayer for more details.
-            final_hidden_states = self.experts(hidden_states=hidden_states,
-                                               router_logits=router_logits)
+        final_hidden_states = self.experts(hidden_states=hidden_states,
+                                           router_logits=router_logits)
         if shared_output is not None:
             if hidden_states.dtype != torch.float16:
                 final_hidden_states = final_hidden_states + shared_output
