53264d4e96c8744b7073a0a6f484d2e5404c7fd5
Kyle Sayers <kylesayrs@gmail.com>
2025-09-02 15:02:38 -0400

    integration
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- vllm/model_executor/layers/quantization/compressed_tensors/transform/schemes/linear_qutlass_nvfp4.py
+++ vllm/model_executor/layers/quantization/compressed_tensors/transform/schemes/linear_qutlass_nvfp4.py
@@ -4,18 +4,57 @@ from typing import Optional
 
 import torch
 
+from vllm.model_executor.layers.quantization.compressed_tensors.compressed_tensors import (  # noqa: E501
+    CompressedTensorsScheme, CompressedTensorsW4A4Fp4)
 from vllm.model_executor.layers.quantization.compressed_tensors.transform.linear import (  # noqa: E501
-    CompressedTensorsLinearTransformMethod)
+    CompressedTensorsLinearTransformMethod, TransformTuple)
+
+__all__ = ["is_qutlass_fp4_scheme", "QutlassFP4LinearMethod"]
 
 
-# Because qutlass fuses hadamard with quantization, it cannot automatically be
-# composed with kernels in the way CompressedTensorsLinearTransformMethod does.
-# Therefore, a separate scheme must be created for each quantized dtype
-class QutlassLinearMethodNvFP4(CompressedTensorsLinearTransformMethod):
+def is_qutlass_fp4_scheme(quant_scheme: CompressedTensorsScheme,
+                          input_tfms: dict[int, TransformTuple]) -> bool:
+    return isinstance(
+        quant_scheme,
+        (CompressedTensorsW4A4Fp4, )) and len(input_tfms) == 1 and input_tfms[
+            0].scheme.head_dim == quant_scheme.group_size
+
+
+class QutlassFP4LinearMethod(CompressedTensorsLinearTransformMethod):
+
+    def create_weights(self, layer, input_size_per_partition,
+                       output_partition_sizes, input_size, output_size,
+                       params_dtype, **extra_weight_attrs):
+        # initializes fp4 qparams
+        assert isinstance(layer.scheme, (CompressedTensorsW4A4Fp4, ))
+        ret = super().create_weights(layer, input_size_per_partition,
+                                     output_partition_sizes, input_size,
+                                     output_size, params_dtype,
+                                     **extra_weight_attrs)
+
+        assert self.input_transform is not None
+        assert len(self.input_transform.weight) == 1
+        assert self.input_transform.weight[0].size(
+            0) == layer.scheme.group_size
+
+        return ret
 
     def apply(self,
               layer: torch.nn.Module,
               x: torch.Tensor,
               bias: Optional[torch.Tensor] = None) -> torch.Tensor:
-        # fused hadamard quant linear method
-        raise NotImplementedError()
+        assert bias is None
+
+        # x_e2m1, x_e8m0, clip_mask = fusedQuantizeNv(
+        #     x, self.input_transform.weight[0], method='quest')
+        # x_scale_block = to_blocked(x_e8m0)
+        # out = matmul_nvf4_bf16_tn(x_e2m1, layer.weight_scale, x_scale_block,
+        #                           layer.weight)
+
+        # # TODO (@ksayers): Confirm that this is done in parallel
+        # if self.output_transform is not None:
+        #     for part_id, (start, length) in enumerate(self.partition_ranges):
+        #         x[:, start:start + length] = self.output_transform(
+        #             x[:, start:start + length].contiguous(), part_id=part_id)
+
+        return x
