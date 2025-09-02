fe91edfaba30055eb45337bb73c3bafb8325be4a
Wentao Ye <44945378+yewentao256@users.noreply.github.com>
2025-09-02 18:22:30 -0400

    [Bug] R1 Accuracy: Fix `routed_scaling_factor` Double Mul Issue (#24119)
    Signed-off-by: yewentao256 <zhyanwentao@126.com>
    Signed-off-by: Joachim Studnia <joachim@mistral.ai>

--- vllm/model_executor/models/deepseek_v2.py
+++ vllm/model_executor/models/deepseek_v2.py
@@ -160,7 +160,8 @@ class DeepseekV2MoE(nn.Module):
             topk_group=config.topk_group,
             prefix=f"{prefix}.experts",
             scoring_func=config.scoring_func,
-            routed_scaling_factor=self.routed_scaling_factor,
+            # we do scaling outside, set factor to 1.0 to avoid double mul
+            routed_scaling_factor=1.0,
             e_score_correction_bias=self.gate.e_score_correction_bias,
             enable_eplb=self.enable_eplb,
             num_redundant_experts=self.n_redundant_experts)
