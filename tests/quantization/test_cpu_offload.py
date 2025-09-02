ec47161eaf04a24d5eb9b8b644a7fae18c38c8d7
Alex Yun <alexyun04@gmail.com>
2025-09-02 18:23:03 -0500

    cut 25% cpu offload test time with enforce eager
    Signed-off-by: Alex Yun <alexyun04@gmail.com>

--- tests/quantization/test_cpu_offload.py
+++ tests/quantization/test_cpu_offload.py
@@ -15,13 +15,15 @@ from ..utils import compare_two_settings
                     reason="fp8 is not supported on this GPU type.")
 def test_cpu_offload_fp8():
     # Test quantization of an unquantized checkpoint
-    compare_two_settings("meta-llama/Llama-3.2-1B-Instruct",
-                         ["--quantization", "fp8"],
-                         ["--quantization", "fp8", "--cpu-offload-gb", "1"],
-                         max_wait_seconds=480)
+    compare_two_settings(
+        "meta-llama/Llama-3.2-1B-Instruct",
+        ["--quantization", "fp8", "--enforce-eager"],
+        ["--quantization", "fp8", "--enforce-eager", "--cpu-offload-gb", "1"],
+        max_wait_seconds=480)
     # Test loading a quantized checkpoint
-    compare_two_settings("neuralmagic/Qwen2-0.5B-Instruct-FP8", [],
-                         ["--cpu-offload-gb", "1"],
+    compare_two_settings("neuralmagic/Qwen2-0.5B-Instruct-FP8",
+                         ["--enforce-eager"],
+                         ["--enforce-eager", "--cpu-offload-gb", "1"],
                          max_wait_seconds=480)
 
 
@@ -31,14 +33,16 @@ def test_cpu_offload_gptq(monkeypatch):
     # This quant method is sensitive to dummy weights, so we force real weights
     monkeypatch.setenv('VLLM_TEST_FORCE_LOAD_FORMAT', 'auto')
     # Test GPTQ Marlin
-    compare_two_settings("Qwen/Qwen2-0.5B-Instruct-GPTQ-Int4", [],
-                         ["--cpu-offload-gb", "1"],
+    compare_two_settings("Qwen/Qwen2-0.5B-Instruct-GPTQ-Int4",
+                         ["--enforce-eager"],
+                         ["--enforce-eager", "--cpu-offload-gb", "1"],
                          max_wait_seconds=480)
     # Test GPTQ
-    compare_two_settings("Qwen/Qwen2-0.5B-Instruct-GPTQ-Int4",
-                         ["--quantization", "gptq"],
-                         ["--quantization", "gptq", "--cpu-offload-gb", "1"],
-                         max_wait_seconds=480)
+    compare_two_settings(
+        "Qwen/Qwen2-0.5B-Instruct-GPTQ-Int4",
+        ["--quantization", "gptq", "--enforce-eager"],
+        ["--quantization", "gptq", "--enforce-eager", "--cpu-offload-gb", "1"],
+        max_wait_seconds=480)
 
 
 @pytest.mark.skipif(not is_quant_method_supported("awq_marlin"),
@@ -47,14 +51,15 @@ def test_cpu_offload_awq(monkeypatch):
     # This quant method is sensitive to dummy weights, so we force real weights
     monkeypatch.setenv('VLLM_TEST_FORCE_LOAD_FORMAT', 'auto')
     # Test AWQ Marlin
-    compare_two_settings("Qwen/Qwen2-0.5B-Instruct-AWQ", [],
-                         ["--cpu-offload-gb", "1"],
+    compare_two_settings("Qwen/Qwen2-0.5B-Instruct-AWQ", ["--enforce-eager"],
+                         ["--enforce-eager", "--cpu-offload-gb", "1"],
                          max_wait_seconds=480)
     # Test AWQ
-    compare_two_settings("Qwen/Qwen2-0.5B-Instruct-AWQ",
-                         ["--quantization", "awq"],
-                         ["--quantization", "awq", "--cpu-offload-gb", "1"],
-                         max_wait_seconds=480)
+    compare_two_settings(
+        "Qwen/Qwen2-0.5B-Instruct-AWQ",
+        ["--enforce-eager", "--quantization", "awq"],
+        ["--enforce-eager", "--quantization", "awq", "--cpu-offload-gb", "1"],
+        max_wait_seconds=480)
 
 
 @pytest.mark.skipif(not is_quant_method_supported("gptq_marlin"),
@@ -63,15 +68,17 @@ def test_cpu_offload_compressed_tensors(monkeypatch):
     # This quant method is sensitive to dummy weights, so we force real weights
     monkeypatch.setenv('VLLM_TEST_FORCE_LOAD_FORMAT', 'auto')
     # Test wNa16
-    compare_two_settings("nm-testing/tinyllama-oneshot-w4a16-channel-v2", [],
-                         ["--cpu-offload-gb", "1"],
+    compare_two_settings("nm-testing/tinyllama-oneshot-w4a16-channel-v2",
+                         ["--enforce-eager"],
+                         ["--enforce-eager", "--cpu-offload-gb", "1"],
                          max_wait_seconds=480)
     # Test w4a16_marlin24
     compare_two_settings("nm-testing/llama7b-one-shot-2_4-w4a16-marlin24-t",
-                         [], ["--cpu-offload-gb", "1"],
+                         ["--enforce-eager"],
+                         ["--enforce-eager", "--cpu-offload-gb", "1"],
                          max_wait_seconds=480)
     # Test w8a8
     compare_two_settings(
-        "nm-testing/tinyllama-oneshot-w8w8-test-static-shape-change", [],
-        ["--cpu-offload-gb", "1"],
+        "nm-testing/tinyllama-oneshot-w8w8-test-static-shape-change",
+        ["--enforce-eager"], ["--enforce-eager", "--cpu-offload-gb", "1"],
         max_wait_seconds=480)
