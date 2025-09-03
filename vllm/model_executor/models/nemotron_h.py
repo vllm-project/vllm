0685bbdf898ba87267467f7ff1b7ea33b8283397
Tomer Asida <57313761+tomeras91@users.noreply.github.com>
2025-09-03 14:24:07 +0300

    use the more vLLM standard way of loading q k v weights, with stacked_params_mapping
    Signed-off-by: Tomer Asida <57313761+tomeras91@users.noreply.github.com>

--- vllm/model_executor/models/nemotron_h.py
+++ vllm/model_executor/models/nemotron_h.py
@@ -426,11 +426,12 @@ class NemotronHModel(nn.Module):
 
     def load_weights(self, weights: Iterable[tuple[str,
                                                    torch.Tensor]]) -> set[str]:
-        attb_params_mapping = {
-            "q_proj": "q",
-            "k_proj": "k",
-            "v_proj": "v",
-        }
+        stacked_params_mapping = [
+            # (param_name, shard_name, shard_id)
+            ("qkv_proj", "q_proj", "q"),
+            ("qkv_proj", "k_proj", "k"),
+            ("qkv_proj", "v_proj", "v"),
+        ]
 
         params_dict = dict(self.named_parameters())
         loaded_params: set[str] = set()
@@ -444,16 +445,20 @@ class NemotronHModel(nn.Module):
             if "dt_bias" in name:
                 loaded_weight = loaded_weight.to(torch.float32)
 
-            # load attn params
-            if any(proj in name for proj in ["q_proj", "k_proj", "v_proj"]):
-                weight_name = next(proj
-                                   for proj in ["q_proj", "k_proj", "v_proj"]
-                                   if proj in name)
-                name = name.replace(weight_name, "qkv_proj")
+            # load stacked params
+            for param_name, weight_name, shard_id in stacked_params_mapping:
+                if weight_name not in name:
+                    continue
+                name = name.replace(weight_name, param_name)
+                # Skip loading extra bias for GPTQ models.
+                if name.endswith(".bias") and name not in params_dict:
+                    continue
+
                 param = params_dict[name]
                 weight_loader = param.weight_loader
-                weight_loader(param, loaded_weight,
-                              attb_params_mapping[weight_name])
+                weight_loader(param, loaded_weight, shard_id)
+                break
+
             # load other params
             else:
                 param = params_dict[name]
