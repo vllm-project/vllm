76f3c966ab2b99e0e7731eef233eb95ec31716b4
Sage Moore <sage@neuralmagic.com>
2025-09-03 03:07:59 +0000

    remove pplx changes
    Signed-off-by: Sage Moore <sage@neuralmagic.com>

--- vllm/model_executor/layers/fused_moe/pplx_prepare_finalize.py
+++ vllm/model_executor/layers/fused_moe/pplx_prepare_finalize.py
@@ -13,9 +13,6 @@ from vllm.model_executor.layers.fused_moe.topk_weight_and_reduce import (
 from vllm.model_executor.layers.fused_moe.utils import (
     _validate_scale_shape, moe_kernel_quantize_input)
 from vllm.utils import cdiv, round_up
-from vllm.v1.worker.ubatching import (dbo_current_ubatch_id,
-                                      dbo_yield_and_switch_from_comm_to_compute,
-                                      dbo_yield_and_switch_from_compute_to_comm)
 
 logger = init_logger(__name__)
 
@@ -66,7 +63,7 @@ class PplxPrepareAndFinalize(mk.FusedMoEPrepareAndFinalize):
 
     def __init__(
         self,
-        a2as: list[pplx.AllToAll],
+        a2a: pplx.AllToAll,
         max_num_tokens: int,
         num_local_experts: int,
         num_dispatchers: int,
@@ -74,7 +71,7 @@ class PplxPrepareAndFinalize(mk.FusedMoEPrepareAndFinalize):
         super().__init__()
         assert max_num_tokens > 0
         assert num_local_experts > 0
-        self.a2as = a2as
+        self.a2a = a2a
         self.max_num_tokens = max_num_tokens
         self.num_local_experts = num_local_experts
         self.num_dispatchers_ = num_dispatchers
@@ -108,7 +105,6 @@ class PplxPrepareAndFinalize(mk.FusedMoEPrepareAndFinalize):
                Optional[torch.Tensor]]:
         num_tokens = a1.size(0)  # M
         hidden_dim = a1.size(-1)  # K
-        a2a_idx = dbo_current_ubatch_id()
 
         assert topk_ids.size(0) == num_tokens
         # expert_map should be None because with expert map, -1 id is used for
@@ -203,8 +199,7 @@ class PplxPrepareAndFinalize(mk.FusedMoEPrepareAndFinalize):
         # There's not much point setting this unless it is != indices.size(0)
         bound_m: Optional[torch.Tensor] = None
 
-        dbo_yield_and_switch_from_compute_to_comm()
-        self.a2as[a2a_idx].dispatch(
+        self.a2a.dispatch(
             out_expert_num_tokens=expert_num_tokens,
             out_expert_x=expert_x,
             out_expert_x_scale=expert_x_scale,
@@ -213,7 +208,6 @@ class PplxPrepareAndFinalize(mk.FusedMoEPrepareAndFinalize):
             indices=topk_ids.view(dtype=torch.uint32),
             bound_m=bound_m,
         )
-        dbo_yield_and_switch_from_comm_to_compute()
 
         if expert_x_scale is not None:
             expert_x_scale = expert_x_scale[:, :, :orig_a_scale_block_shape]
@@ -240,7 +234,6 @@ class PplxPrepareAndFinalize(mk.FusedMoEPrepareAndFinalize):
         # This argument is optional
         # There's not much point setting this unless it is != topk_ids.size(0)
         bound_m: Optional[torch.Tensor] = None
-        a2a_idx = dbo_current_ubatch_id()
 
         # TODO (bnell): fails in test_pplx_moe.py, figure out what's going on
         #num_tokens = output.size(0)  # M
@@ -256,12 +249,10 @@ class PplxPrepareAndFinalize(mk.FusedMoEPrepareAndFinalize):
         if apply_router_weight_on_input:
             topk_weights = torch.ones_like(topk_weights)
 
-        dbo_yield_and_switch_from_compute_to_comm()
-        self.a2as[a2a_idx].combine(
+        self.a2a.combine(
             out_tokens=output,
             indices=topk_ids.view(dtype=torch.uint32),
             weights=topk_weights,
             expert_y=fused_expert_output,
             bound_m=bound_m,
         )
-        dbo_yield_and_switch_from_comm_to_compute()
