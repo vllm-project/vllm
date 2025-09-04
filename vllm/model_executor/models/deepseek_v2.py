fix aiter routed scaling factor
Signed-off-by: kliuae <kuanfu.liu@embeddedllm.com>

vllm/model_executor/models/deepseek_v2.py
@@ -163,7 +163,8 @@ class DeepseekV2MoE(nn.Module):
             prefix=f"{prefix}.experts",
             scoring_func=config.scoring_func,
             # we do scaling outside, set factor to 1.0 to avoid double mul
-            routed_scaling_factor=1.0,
+            routed_scaling_factor=1.0
+            if is_rocm_aiter_moe_enabled() else self.routed_scaling_factor,
             e_score_correction_bias=self.gate.e_score_correction_bias,
             enable_eplb=self.enable_eplb,
             num_redundant_experts=self.n_redundant_experts,