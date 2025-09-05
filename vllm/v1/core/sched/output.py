418cbcf42867cddeffa0d926a85184f299fe539a
Andrew Sansom <qthequartermasterman@gmail.com>
2025-09-05 09:40:33 -0500

    Update vllm/v1/core/sched/output.py
    Co-authored-by: Cyrus Leung <cyrus.tl.leung@gmail.com>
    Signed-off-by: Andrew Sansom <qthequartermasterman@gmail.com>

--- vllm/v1/core/sched/output.py
+++ vllm/v1/core/sched/output.py
@@ -81,7 +81,7 @@ class NewRequestData:
         return (f"NewRequestData("
                 f"req_id={self.req_id},"
                 f"prompt_token_ids_len={prompt_token_ids_len},"
-                f"prompt_embeds={prompt_embeds_shape},"
+                f"prompt_embeds_shape={prompt_embeds_shape},"
                 f"mm_kwargs={self.mm_kwargs},"
                 f"mm_hashes={self.mm_hashes},"
                 f"mm_positions={self.mm_positions},"
