a1b7a4feaec54502f6c155ed66cfa9e8a9683b50
Bangsheng Tang <5318912+bangshengtang@users.noreply.github.com>
2025-09-06 14:02:47 -0700

    break execute_model in gpu_model_runner into sub-functions for custom scopes (#24265)
    Co-authored-by: Bangsheng Tang <bangsheng@meta.com>
    Signed-off-by: xuebwang-amd <xuebwang@amd.com>

--- vllm/envs.py
+++ vllm/envs.py
@@ -168,6 +168,7 @@ if TYPE_CHECKING:
     VLLM_ALLREDUCE_USE_SYMM_MEM: bool = False
     VLLM_TUNED_CONFIG_FOLDER: Optional[str] = None
     VLLM_DISABLE_PAD_FOR_CUDAGRAPH: bool = False
+    VLLM_CUSTOM_SCOPES_FOR_PROFILING: bool = False
 
 
 def get_default_cache_root():
@@ -1200,6 +1201,9 @@ environment_variables: dict[str, Callable[[], Any]] = {
     "VLLM_TUNED_CONFIG_FOLDER":
     lambda: os.getenv("VLLM_TUNED_CONFIG_FOLDER", None),
 
+    # Add optional custom scopes for profiling, disable to avoid overheads
+    "VLLM_CUSTOM_SCOPES_FOR_PROFILING":
+    lambda: bool(int(os.getenv("VLLM_CUSTOM_SCOPES_FOR_PROFILING", "0"))),
 }
 
 # --8<-- [end:env-vars-definition]
