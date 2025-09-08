805017e456c173a6223c930ac71c848e1bbc871f
Ming Yang <minos.future@gmail.com>
2025-09-07 18:59:03 -0700

    Update dispatch config
    Signed-off-by: Ming Yang <minos.future@gmail.com>

--- vllm/model_executor/layers/fused_moe/deepep_ht_prepare_finalize.py
+++ vllm/model_executor/layers/fused_moe/deepep_ht_prepare_finalize.py
@@ -49,9 +49,9 @@ class DeepEPHTPrepareAndFinalize(mk.FusedMoEPrepareAndFinalize):
         return torch.int64
 
     def _get_dispatch_config(self) -> Optional[deep_ep.Config]:
-        if self.dp_size not in self.available_rank_configs:
+        if self.num_dispatchers_ not in self.available_rank_configs:
             return None
-        return deep_ep.Buffer.get_dispatch_config(self.dp_size)
+        return deep_ep.Buffer.get_dispatch_config(self.num_dispatchers_)
 
     def _get_combine_config(self) -> Optional[deep_ep.Config]:
         if self.num_dispatchers_ not in self.available_rank_configs:
