22fb0a6f1f57b8f5959ca94c1be0a839115ad448
Xingyu Liu <charlotteliu12x@gmail.com>
2025-09-07 20:30:06 -0700

    update metavar
    Signed-off-by: Xingyu Liu <charlotteliu12x@gmail.com>

--- vllm/engine/arg_utils.py
+++ vllm/engine/arg_utils.py
@@ -546,9 +546,9 @@ class EngineArgs:
             default=EngineArgs.disable_async_output_proc,
             help="Disable async output processing. This may result in "
             "lower performance.")
-        model_group.add_argument("--config-format",
-                                 metavar=",".join(get_args(ConfigFormat)),
-                                 **model_kwargs["config_format"])
+        config_format_kwargs = model_kwargs["config_format"].copy()
+        config_format_kwargs["metavar"] = ",".join(get_args(ConfigFormat))
+        model_group.add_argument("--config-format", **config_format_kwargs)
         # This one is a special case because it can bool
         # or str. TODO: Handle this in get_kwargs
         model_group.add_argument("--hf-token",
