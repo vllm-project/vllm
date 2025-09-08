90dd13d969c8ebdd0e1c6373c5f2dfaf55edf181
chenmenglong <chenmenglong1@huawei.com>
2025-09-08 20:11:53 +0800

    fix pre-commit
    Signed-off-by: chenmenglong <chenmenglong1@huawei.com>

--- vllm/model_executor/layers/fused_moe/layer.py
+++ vllm/model_executor/layers/fused_moe/layer.py
@@ -1539,7 +1539,7 @@ class FusedMoE(CustomOp):
             # ExpertTokensMetadata.expert_num_tokens for better performance.
             # For other implementations or when metadata is not available,
             # we fall back to here.
-            
+
             # There is no expert_num_tokens in
             # expert_tokens_meta of DeepEPHTPrepareAndFinalize
             # so it is not supported DeepEPHTPrepareAndFinalize for now.
