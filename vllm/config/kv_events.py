205603c15edb950745fad6a1625cd6fd9df5b5b7
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-09-08 10:33:24 +0100

    Update vllm/config/kv_events.py
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- vllm/config/kv_events.py
+++ vllm/config/kv_events.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+
 from typing import Optional
 
 from pydantic.dataclasses import dataclass
