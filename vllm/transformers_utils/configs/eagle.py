2243d7ffca536f006484e0412c91ac89989880b7
Wenlong Wang <wangwenlong2755@gmail.com>
2025-09-09 21:24:23 -0700

    [BugFix][Spec Decode] Fix out-of-range index triggered by eagle3; re-enable test for LlamaForCausalLMEagle3 (#24392)
    Signed-off-by: wwl2755 <wangwenlong2755@gmail.com>
    Signed-off-by: Joachim Studnia <joachim@mistral.ai>

--- vllm/transformers_utils/configs/eagle.py
+++ vllm/transformers_utils/configs/eagle.py
@@ -46,6 +46,7 @@ class EAGLEConfig(PretrainedConfig):
         # Eagle model name should follow naming convention of
         # LlamaForCausalLM -> EagleLlamaForCausalLM
         # LlamaForCausalLM -> Eagle3LlamaForCausalLM
+        # LlamaForCausalLMEagle3 -> LlamaForCausalLMEagle3
         if method == "eagle":
             assert self.model is not None, \
                 "model should not be None when method is eagle"
@@ -53,6 +54,7 @@ class EAGLEConfig(PretrainedConfig):
                 f"Eagle{arch}" if not arch.startswith("Eagle") \
                     else arch for arch in self.model.architectures
             ]
+
         elif method == "eagle3":
             assert self.model is not None, \
                 "model should not be None when method is eagle3"
