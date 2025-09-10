beccd65cd29c302df54f280e54634b07abae1049
zhxchen17 <zhxchen17@fb.com>
2025-09-10 08:44:16 -0700

    AOT compilation workflow [2/n]
    Signed-off-by: zhxchen17 <zhxchen17@fb.com>

--- tests/compile/test_aot_compile.py
+++ tests/compile/test_aot_compile.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
+import tempfile
 from contextlib import contextmanager
 
 import pytest
@@ -59,3 +60,38 @@ def test_no_eval_frame(monkeypatch: pytest.MonkeyPatch):
                 "fail_on_recompile"):
             ret = CompiledMod(vllm_config=vllm_config)(*args)
             assert torch.allclose(ret, expected)
+
+
+def test_force_aot_load(monkeypatch: pytest.MonkeyPatch):
+    with tempfile.TemporaryDirectory() as tmpdirname, monkeypatch.context(
+    ) as m:
+        args = (torch.randn(10, 10), )
+        m.setenv("VLLM_USE_AOT_COMPILE", "1")
+        m.setenv("VLLM_FORCE_AOT_LOAD", "1")
+        m.setenv("VLLM_CACHE_ROOT", tmpdirname)
+        vllm_config = make_vllm_config()
+        with use_vllm_config(vllm_config):
+            CompiledMod = support_torch_compile(MyMod)
+            try:
+                CompiledMod(vllm_config=vllm_config)(*args)
+            except Exception as e:
+                assert isinstance(e, FileNotFoundError)
+            else:
+                raise AssertionError(
+                    "Expected failed aot compilation with clean state.")
+
+
+def test_basic(monkeypatch: pytest.MonkeyPatch):
+    with monkeypatch.context() as m:
+        args = (torch.randn(10, 10), )
+        CompiledMod = support_torch_compile(MyMod)
+
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            m.setenv("VLLM_CACHE_ROOT", tmpdirname)
+            m.setenv("VLLM_USE_AOT_COMPILE", "1")
+            vllm_config = make_vllm_config()
+            with use_vllm_config(vllm_config):
+                expected = CompiledMod(vllm_config=vllm_config)(*args)
+                m.setenv("VLLM_FORCE_AOT_LOAD", "1")
+                ret = CompiledMod(vllm_config=vllm_config)(*args)
+                assert torch.allclose(ret, expected)
