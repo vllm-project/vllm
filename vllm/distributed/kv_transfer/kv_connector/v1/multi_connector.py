80c2c9e4fff995fd8b8f72ef88f416000417f2da
Chauncey <chaunceyjiang@gmail.com>
2025-09-10 12:24:11 +0800

    [P/D] MultiConnector supports shutdown (#24425)
    Signed-off-by: chaunceyjiang <chaunceyjiang@gmail.com>
    Signed-off-by: xuebwang-amd <xuebwang@amd.com>

--- vllm/distributed/kv_transfer/kv_connector/v1/multi_connector.py
+++ vllm/distributed/kv_transfer/kv_connector/v1/multi_connector.py
@@ -88,6 +88,18 @@ class MultiConnector(KVConnectorBase_V1):
         for c in self._connectors:
             c.clear_connector_metadata()
 
+    def shutdown(self):
+        exception: Optional[Exception] = None
+        for c in self._connectors:
+            try:
+                c.shutdown()
+            except Exception as e:
+                logger.exception("Exception during connector %s shutdown.",
+                                 c.__class__.__name__)
+                exception = e
+        if exception:
+            raise exception
+
     # ==============================
     # Worker-side methods
     # ==============================
