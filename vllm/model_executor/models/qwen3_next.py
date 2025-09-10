c0243db7c4db936d090d0214e308e771f8485168
Tao He <linzhu.ht@alibaba-inc.com>
2025-09-10 11:36:57 +0800

    Fixes lint issues.
    Signed-off-by: Tao He <linzhu.ht@alibaba-inc.com>

--- vllm/model_executor/models/qwen3_next.py
+++ vllm/model_executor/models/qwen3_next.py
@@ -23,8 +23,8 @@ from vllm.logger import init_logger
 from vllm.model_executor.layers.fla.ops import (
     RMSNormGated, chunk_gated_delta_rule, fused_recurrent_gated_delta_rule)
 from vllm.model_executor.layers.fused_moe import FusedMoE
-from vllm.model_executor.layers.layernorm import (GemmaRMSNorm as
-                                                  Qwen3NextRMSNorm)
+from vllm.model_executor.layers.layernorm import (
+    GemmaRMSNorm as Qwen3NextRMSNorm)
 from vllm.model_executor.layers.linear import (ColumnParallelLinear,
                                                MergedColumnParallelLinear,
                                                QKVParallelLinear,
