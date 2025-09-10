0ca86441fa943d6850ce04958c177fb4627842dc
WorldExplored <srreyansh.sethi@gmail.com>
2025-09-10 02:42:31 -0400

    Fixed pre-commit issue
    Signed-off-by: <Srreyansh Sethi srreyansh.sethi@gmail.com>

--- tests/config/test_config_utils.py
+++ tests/config/test_config_utils.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
-import enum
+
 from dataclasses import dataclass
 from typing import Optional
 
