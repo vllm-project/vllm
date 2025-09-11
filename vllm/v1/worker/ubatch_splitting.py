fa30304de6b61c0969f30300494b5c9e891d9e10
Sage Moore <sage@neuralmagic.com>
2025-09-11 17:06:26 +0000

    lint
    Signed-off-by: Sage Moore <sage@neuralmagic.com>

--- vllm/v1/worker/ubatch_splitting.py
+++ vllm/v1/worker/ubatch_splitting.py
@@ -9,10 +9,11 @@ from vllm.config import VllmConfig
 from vllm.forward_context import DPMetadata
 from vllm.logger import init_logger
 from vllm.utils import round_up
-
-from vllm.v1.worker.ubatch_utils import UBatchSlices, UbatchSlice, is_second_ubatch_empty
+from vllm.v1.worker.ubatch_utils import (UbatchSlice, UBatchSlices,
+                                         is_second_ubatch_empty)
 
 logger = init_logger(__name__)
+
 
 def should_ubatch_with_num_tokens(
     should_ubatch: bool,
@@ -22,17 +23,16 @@ def should_ubatch_with_num_tokens(
 ) -> tuple[bool, Optional[torch.Tensor]]:
     dp_size = vllm_config.parallel_config.data_parallel_size
     dp_rank = vllm_config.parallel_config.data_parallel_rank
-    return DPMetadata.should_ubatch_across_dp(
-        should_ubatch, orig_num_tokens_per_ubatch,
-        padded_num_tokens_per_ubatch, dp_size, dp_rank)
+    return DPMetadata.should_ubatch_across_dp(should_ubatch,
+                                              orig_num_tokens_per_ubatch,
+                                              padded_num_tokens_per_ubatch,
+                                              dp_size, dp_rank)
 
 
 def get_dp_padding_ubatch(
-    num_tokens_unpadded: int, 
-    num_tokens_padded: int,
-    should_attempt_ubatching: bool,
-    vllm_config: VllmConfig
-) -> tuple[bool, Optional[torch.Tensor]]:
+        num_tokens_unpadded: int, num_tokens_padded: int,
+        should_attempt_ubatching: bool,
+        vllm_config: VllmConfig) -> tuple[bool, Optional[torch.Tensor]]:
     """
     1. Decides if each DP rank is going to microbatch. Either all ranks
     run with microbatching or none of them do. If this function decides
@@ -59,9 +59,8 @@ def get_dp_padding_ubatch(
 
     # If this DP rank doesn't want to attempt microbatching
     if not should_attempt_ubatching:
-        (should_ubatch,
-            num_tokens_across_dp) = should_ubatch_with_num_tokens(
-                False, 0, 0, vllm_config)
+        (should_ubatch, num_tokens_across_dp) = should_ubatch_with_num_tokens(
+            False, 0, 0, vllm_config)
         assert should_ubatch is False
         assert num_tokens_across_dp is None
         return should_ubatch, num_tokens_across_dp
@@ -74,13 +73,14 @@ def get_dp_padding_ubatch(
     # Sanity Check that the existing padding isn't giving us an empty second
     # ubatch. Abort if so
     if is_second_ubatch_empty(num_tokens_unpadded, num_tokens_padded):
-        logger.debug(f"Aborting ubatching {num_tokens_unpadded} {num_tokens_padded}")
+        logger.debug("Aborting ubatching %s %s", num_tokens_unpadded,
+                     num_tokens_padded)
         should_ubatch = False
 
     # Note that we compute the number of padded tokens per ubatch
-    (should_ubatch,
-        num_tokens_across_dp) = should_ubatch_with_num_tokens(
-            should_ubatch, num_tokens_unpadded // 2, num_tokens_per_ubatch, vllm_config)
+    (should_ubatch, num_tokens_across_dp) = should_ubatch_with_num_tokens(
+        should_ubatch, num_tokens_unpadded // 2, num_tokens_per_ubatch,
+        vllm_config)
     if not should_ubatch:
         assert num_tokens_across_dp is None
         return should_ubatch, num_tokens_across_dp
@@ -94,17 +94,20 @@ def get_dp_padding_ubatch(
                                             dtype=torch.int32)
     return should_ubatch, num_tokens_after_padding
 
-def ubatch_split(max_num_scheduled_tokens: int,
-        num_tokens_unpadded: int,
-        num_tokens_padded: int,
-        vllm_config: VllmConfig,
-    ) -> tuple[Optional[UBatchSlices], Optional[torch.Tensor]]:
+
+def ubatch_split(
+    max_num_scheduled_tokens: int,
+    num_tokens_unpadded: int,
+    num_tokens_padded: int,
+    vllm_config: VllmConfig,
+) -> tuple[Optional[UBatchSlices], Optional[torch.Tensor]]:
     """
-    Coordinates amongst all DP ranks to determine if and how the full batch should
-    be split into microbatches.
+    Coordinates amongst all DP ranks to determine if and how the full batch
+    should be split into microbatches.
 
     Returns: tuple[
-        ubatch_slices: if this is set then all DP ranks have agreed to microbatch
+        ubatch_slices: if this is set then all DP ranks have agreed to 
+        microbatch
         num_tokens_after_padding: A tensor containing the total number of
         tokens per-microbatch for each DP rank including padding. Will be
         None if ubatch_slices is None
@@ -126,11 +129,9 @@ def ubatch_split(max_num_scheduled_tokens: int,
 
     # Don't microbatch unless every other DP worker is also microbatching
     num_tokens_after_padding = None
-    (should_ubatch,
-     num_tokens_after_padding) = get_dp_padding_ubatch(num_tokens_unpadded, 
-                                                          num_tokens_padded, 
-                                                          should_attempt_ubatching, 
-                                                          vllm_config)
+    (should_ubatch, num_tokens_after_padding) = get_dp_padding_ubatch(
+        num_tokens_unpadded, num_tokens_padded, should_attempt_ubatching,
+        vllm_config)
     if not should_ubatch:
         return (None, None)
 
@@ -142,7 +143,7 @@ def ubatch_split(max_num_scheduled_tokens: int,
     total_num_tokens_per_ubatch = int(num_tokens_after_padding[0].item())
     padded_first_ubatch_slice = slice(0, total_num_tokens_per_ubatch)
     padded_second_ubatch_slice = slice(total_num_tokens_per_ubatch,
-                                        num_tokens_unpadded)
+                                       num_tokens_unpadded)
 
     # Note there's an assumption here that there's 1 token per request
     ubatch_slices = [
