7a9b032f5a1bc991e78ea71ff71231693014f08d
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-09-11 14:37:35 +0200

    Fix cases of `model_config.{multimodal_field}`
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- vllm/entrypoints/chat_utils.py
+++ vllm/entrypoints/chat_utils.py
@@ -797,9 +797,9 @@ class MultiModalContentParser(BaseMultiModalContentParser):
         super().__init__()
 
         self._tracker = tracker
-
+        multimodal_config = self._tracker.model_config.multimodal_config
         self._connector = MediaConnector(
-            media_io_kwargs=self._tracker._model_config.media_io_kwargs,
+            media_io_kwargs=multimodal_config.media_io_kwargs,
             allowed_local_media_path=tracker.allowed_local_media_path,
         )
 
@@ -860,8 +860,9 @@ class AsyncMultiModalContentParser(BaseMultiModalContentParser):
         super().__init__()
 
         self._tracker = tracker
+        multimodal_config = self._tracker.model_config.multimodal_config
         self._connector = MediaConnector(
-            media_io_kwargs=self._tracker._model_config.media_io_kwargs,
+            media_io_kwargs=multimodal_config.media_io_kwargs,
             allowed_local_media_path=tracker.allowed_local_media_path,
         )
 
