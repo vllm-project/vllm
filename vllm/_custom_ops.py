b3eb768ae4fd2cd75b72389cbdd32e96b5c47b3c
TaehyunKim <73943231+ca1207@users.noreply.github.com>
2025-09-11 15:29:40 +0900

    [Model] New model support for Motif-1-Tiny (#23414)
    Signed-off-by: ca1207 <ca1207zzz@gmail.com>
    Signed-off-by: TaehyunKim <73943231+ca1207@users.noreply.github.com>
    Co-authored-by: WyldeCat <skan1543@gmail.com>
    Co-authored-by: Jee Jee Li <pandaleefree@gmail.com>

--- vllm/_custom_ops.py
+++ vllm/_custom_ops.py
@@ -280,6 +280,13 @@ def fused_add_rms_norm(input: torch.Tensor, residual: torch.Tensor,
     torch.ops._C.fused_add_rms_norm(input, residual, weight, epsilon)
 
 
+def poly_norm(out: torch.Tensor, input: torch.Tensor, weight: torch.Tensor,
+              bias: torch.Tensor, epsilon: float) -> None:
+    # TODO: Remove this contiguous call when the kernel is updated to support non-contiguous input
+    input_contiguous = input.contiguous()
+    torch.ops._C.poly_norm(out, input_contiguous, weight, bias, epsilon)
+
+
 def apply_repetition_penalties_torch(
         logits: torch.Tensor, prompt_mask: torch.Tensor,
         output_mask: torch.Tensor, repetition_penalties: torch.Tensor) -> None:
@@ -709,6 +716,7 @@ def cutlass_sparse_scaled_mm_supported(cuda_device_capability: int) -> bool:
 
 def cutlass_group_gemm_supported(cuda_device_capability: int) -> bool:
     return torch.ops._C.cutlass_group_gemm_supported(cuda_device_capability)
+
 
 def cutlass_sparse_compress(a: torch.Tensor) \
     -> tuple[torch.Tensor, torch.Tensor]:
