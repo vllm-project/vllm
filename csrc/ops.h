b1d76184cc94df2696205a91ad263f52b99eff40
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2025-09-12 16:58:09 -0700

    [Chore] Remove unused batched RoPE op & kernel
    Signed-off-by: Woosuk Kwon <woosuk.kwon@berkeley.edu>

--- csrc/ops.h
+++ csrc/ops.h
@@ -122,12 +122,6 @@ void rotary_embedding(torch::Tensor& positions, torch::Tensor& query,
                       std::optional<torch::Tensor> key, int64_t head_size,
                       torch::Tensor& cos_sin_cache, bool is_neox);
 
-void batched_rotary_embedding(torch::Tensor& positions, torch::Tensor& query,
-                              std::optional<torch::Tensor> key,
-                              int64_t head_size, torch::Tensor& cos_sin_cache,
-                              bool is_neox, int64_t rot_dim,
-                              torch::Tensor& cos_sin_cache_offsets);
-
 void silu_and_mul(torch::Tensor& out, torch::Tensor& input);
 
 void silu_and_mul_quant(torch::Tensor& out, torch::Tensor& input,
