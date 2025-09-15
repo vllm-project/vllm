f0b6a5c3852d8c7fd40872d9a82c35fa82870e51
simondanielsson <simon.danielsson99@hotmail.com>
2025-09-15 16:39:49 +0000

    Implement dummy browser backend
    Signed-off-by: simondanielsson <simon.danielsson99@hotmail.com>

--- vllm/entrypoints/tool.py
+++ vllm/entrypoints/tool.py
@@ -6,8 +6,8 @@ from typing import TYPE_CHECKING, Any
 
 from openai_harmony import Author, Message, Role, TextContent
 
+import vllm.envs as envs
 from vllm.logger import init_logger
-import vllm.envs
 
 if TYPE_CHECKING:
     # Avoid circular import.
@@ -31,7 +31,8 @@ def validate_gpt_oss_install():
     except PackageNotFoundError:
         raise ImportError("Package 'gpt_oss' is not installed.") from None
     except InvalidVersion as e:
-        raise ImportError(f"Invalid version string for 'gpt_oss': {e}") from None
+        raise ImportError(
+            f"Invalid version string for 'gpt_oss': {e}") from None
 
     if pkg_version < Version("0.0.3"):
         raise ImportError(
@@ -64,16 +65,93 @@ class HarmonyBrowserTool(Tool):
         except ImportError as e:
             self.enabled = False
             logger.warning_once(
-                "gpt_oss is not installed properly (%s), browsing is disabled", e
-            )
+                "gpt_oss is not installed properly (%s), browsing is disabled",
+                e)
             return
 
         if envs.VLLM_GPT_OSS_USE_DUMMY_BROWSER_TOOL:
-            browser_backend = ...
+            from gpt_oss.tools.simple_browser.backend import (Backend,
+                                                              ClientSession,
+                                                              PageContents,
+                                                              chz,
+                                                              process_html)
+
+            @chz.chz(typecheck=True)
+            class DummyBrowserBackend(Backend):
+
+                @abstractmethod
+                async def search(
+                    self,
+                    query: str,
+                    topn: int,
+                    session: ClientSession,
+                ) -> PageContents:
+                    titles_and_urls = [
+                        (
+                            "Nvidia",
+                            "https://en.wikipedia.org/wiki/Nvidia",
+                            "Nvidia was founded in 1993 by Jensen Huang, "
+                            "Chris Malachowsky, and Curtis Priem.",
+                        ),
+                        (
+                            "President of South Korea",
+                            "https://en.wikipedia.org/wiki/"
+                            "President_of_South_Korea",
+                            "On 3 June 2025, Lee Jae Myung of the Democratic "
+                            "Party was elected as president of South Korea.",
+                        ),
+                    ]
+                    search_results = (
+                        f"<li><a href='{url}'>{title}</a> {summary}</li>"
+                        for title, url, summary in titles_and_urls)
+                    html_page = f"""
+                    <html><body>
+                    <h1>Search Results</h1>
+                    <ul>
+                    {"".join(search_results)}
+                    </ul>
+                    </body></html>
+                    """
+                    return process_html(
+                        html=html_page,
+                        url="",
+                        title=query,
+                        display_urls=True,
+                        session=session,
+                    )
+
+                @abstractmethod
+                async def fetch(self, url: str,
+                                session: ClientSession) -> PageContents:
+                    if "nvidia" in url.lower():
+                        html = (
+                            "[0†Nvidia†en.wikipedia.org】 Nvidia was founded "
+                            "in 1993 by Jensen Huang, Chris Malachowsky, and "
+                            "Curtis Priem. (https://en.wikipedia.org/wiki"
+                            "/Nvidia)")
+                    else:
+                        html = (
+                            "[0†President of South Korea†en.wikipedia.org】 "
+                            "On 3 June 2025, Lee Jae Myung of the Democratic "
+                            "Party was elected as president of South Korea. "
+                            "(https://en.wikipedia.org/"
+                            "wiki/President_of_South_Korea)")
+                    return process_html(
+                        html=html,
+                        url=url,
+                        title="",
+                        session=session,
+                    )
+
+            browser_backend = DummyBrowserBackend(source="web")
+            backend_name = "dummy"
         else:
             browser_backend = ExaBackend(source="web", api_key=exa_api_key)
+            backend_name = "Exa"
+
         self.browser_tool = SimpleBrowserTool(backend=browser_backend)
-        logger.info_once("Browser tool initialized")
+        logger.info_once("Browser tool initialized with %s backend",
+                         backend_name)
 
     async def get_result(self, context: "ConversationContext") -> Any:
         from vllm.entrypoints.context import HarmonyContext
