456eb6b0376b4b255d1e1d9bace5d48c621f3a80
Eugene Khvedchenia <ekhvedchenia@nvidia.com>
2025-09-15 12:16:29 +0300

    Added NVidia copyright
    Signed-off-by: Eugene Khvedchenia <ekhvedchenia@nvidia.com>

--- vllm/multimodal/evs.py
+++ vllm/multimodal/evs.py
@@ -1,5 +1,12 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+# Copyright (c) 2023-2024, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
 
 import typing
 
