0d486362e0011150e93699155f99e729e881fabf
chengda-wu <wuchengda@huawei.com>
2025-09-17 17:19:35 +0800

    add add_token_and_check_stop
    Signed-off-by: chengda_wu <wuchengda@huawei.com>

--- vllm/v1/core/sched/utils.py
+++ vllm/v1/core/sched/utils.py
@@ -67,3 +67,35 @@ def check_stop(request: Request,
         request.stop_reason = last_token_id
         return True
     return False
+
+def add_token_and_check_stop(token_ids: list[int], request: Request, max_model_len: int):
+    remain_len = min(max_model_len - request.num_tokens, request.max_tokens - request.num_output_tokens)
+    if remain_len <= 0:
+        request.status = RequestStatus.FINISHED_LENGTH_CAPPED
+        return True, 0
+
+    possible_len = min(len(token_ids), remain_len)
+
+    true_idx = 0
+    sampling_params = request.sampling_params
+
+    for true_idx, last_token_id in enumerate(token_ids[:possible_len], 1):
+        if (not sampling_params.ignore_eos
+                and last_token_id == request.eos_token_id):
+            request.append_output_token_ids(token_ids[:true_idx])
+            request.status = RequestStatus.FINISHED_STOPPED
+            return True, true_idx
+
+        if last_token_id in (sampling_params.stop_token_ids or ()):
+            request.append_output_token_ids(token_ids[:true_idx])
+            request.status = RequestStatus.FINISHED_STOPPED
+            request.stop_reason = last_token_id
+            return True, true_idx
+
+    request.append_output_token_ids(token_ids[:true_idx])
+
+    if remain_len - true_idx <= 0:
+        request.status = RequestStatus.FINISHED_LENGTH_CAPPED
+        return True, true_idx
+
+    return False, true_idx
