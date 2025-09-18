1b215ee1d6eb4b9e2ea5fba4f5b9cb9e9c178243
baonudesifeizhai <baonudesifeizhai@gmail.com>
2025-09-18 17:29:39 -0400

    yapf
    Signed-off-by: baonudesifeizhai <baonudesifeizhai@gmail.com>

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -888,9 +888,9 @@ class GPUModelRunner(LoRAModelRunnerMixin, KVConnectorModelRunnerMixin):
         # encoder lengths for inputs scheduled in this batch
         num_reqs_int = int(num_reqs)
         if num_reqs_int == 0:
-            return np.zeros((0,), dtype=np.int32)
+            return np.zeros((0, ), dtype=np.int32)
 
-        encoder_seq_lens = np.zeros((num_reqs_int,), dtype=np.int32)
+        encoder_seq_lens = np.zeros((num_reqs_int, ), dtype=np.int32)
         for req_id in scheduler_output.scheduled_encoder_inputs:
             req_index = self.input_batch.req_id_to_index[req_id]
             encoder_seq_lens[req_index] = self.max_encoder_len
