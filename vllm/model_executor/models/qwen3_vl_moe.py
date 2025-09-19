e19c80c99cf2532265000adf6be095a5dc18b3cd
Roger Wang <hey@rogerw.io>
2025-09-19 03:46:16 -0700

    [Bugfix][Perf] Misc fixes for Qwen3 VL (#25238)
    Signed-off-by: Roger Wang <hey@rogerw.io>

--- vllm/model_executor/models/qwen3_vl_moe.py
+++ vllm/model_executor/models/qwen3_vl_moe.py
@@ -344,3 +344,5 @@ class Qwen3VLMoeForConditionalGeneration(Qwen3VLForConditionalGeneration):
                         config.text_config.hidden_size)
             for _ in range(self.deepstack_num_level)
         ] if self.use_deepstack else None
+        self.visual_dim = config.vision_config.out_hidden_size
+        self.multiscale_dim = self.visual_dim * self.deepstack_num_level
