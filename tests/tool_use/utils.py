84873055cd048938d24bab182650667c13066e69
David Chen <530634352@qq.com>
2025-09-19 15:06:55 +0800

    fix test
    Signed-off-by: David Chen <530634352@qq.com>

--- tests/tool_use/utils.py
+++ tests/tool_use/utils.py
@@ -252,29 +252,6 @@ SEARCH_TOOL: ChatCompletionToolParam = {
     }
 }
 
-PRODUCT_TOOL: ChatCompletionToolParam = {
-    "type": "function",
-    "function": {
-        "name": "get_product_info",
-        "description":
-        "Get detailed information of a product based on its product ID.",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "inserted": {
-                    "type": "boolean",
-                    "description": "inserted.",
-                },
-                "product_id": {
-                    "type": "integer",
-                    "description": "The product ID of the product.",
-                },
-            },
-            "required": ["product_id", "inserted"],
-        },
-    },
-}
-
 MESSAGES_WITHOUT_TOOLS: list[ChatCompletionMessageParam] = [{
     "role":
     "user",
@@ -383,13 +360,3 @@ MESSAGES_WITH_PARALLEL_TOOL_RESPONSE: list[ChatCompletionMessageParam] = [{
     "The weather in Orlando FL is 78 degrees fahrenheit with clear"
     "skies."
 }]
-
-MESSAGES_ASKING_FOR_PRODUCT_INFO: list[ChatCompletionMessageParam] = [
-    {
-        "role":
-        "user",
-        "content":
-        "Hi! Do you have any detailed information about the product id "
-        "7355608 and inserted true?",
-    },
-]
