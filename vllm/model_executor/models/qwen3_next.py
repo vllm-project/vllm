3809b66ca185c57c19bf2028236f37697d3f78a1
JartX <sagformas@epdcenter.es>
2025-09-20 05:18:13 +0200

    [BUGFIX] GPTQ quantization compatibility for Qwen3 Next MOE models (AutoGPTQ and AutoRound-GPTQ) (#25268)
    Signed-off-by: JartX <sagformas@epdcenter.es>
    Signed-off-by: xuebwang-amd <xuebwang@amd.com>

--- vllm/model_executor/models/qwen3_next.py
+++ vllm/model_executor/models/qwen3_next.py
@@ -148,9 +148,11 @@ class Qwen3NextSparseMoeBlock(nn.Module):
 
     def _maybe_ignore_quant_config(self, quant_config: QuantizationConfig):
         # GPTQ configs do not have a list of ignored modules, however AutoGPTQ
-        # seems to avoid gate quantization.
-        # See: https://huggingface.co/Qwen/Qwen3-30B-A3B-GPTQ-Int4
-        if isinstance(quant_config, (GPTQConfig, GPTQMarlinConfig)):
+        # seems to avoid gate quantization while AutoRound does.
+        if isinstance(
+                quant_config,
+            (GPTQConfig,
+             GPTQMarlinConfig)) and not quant_config.autoround_version:
             return None
         return quant_config
 
