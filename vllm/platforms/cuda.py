4f0eb52fcf28308708bd1c0f63d98a306e14303a
Hank_ <37239608+ILikeIneine@users.noreply.github.com>
2025-09-20 18:20:28 +0800

    raise error when no valid mla backend
    Co-authored-by: Matthew Bonanni <mbonanni@redhat.com>
    Signed-off-by: Hank_ <37239608+ILikeIneine@users.noreply.github.com>

--- vllm/platforms/cuda.py
+++ vllm/platforms/cuda.py
@@ -303,13 +303,13 @@ class CudaPlatformBase(Platform):
             if use_triton:
                 return _get_version("Triton MLA",
                                     "triton_mla.TritonMLABackend")
-            logger.warning(
-                "Selected MLA backend is not valid, falling back to Triton MLA."
-            )
-            return _get_version("Triton MLA", "triton_mla.TritonMLABackend")
+            # If this point is reached, no valid backend has been selected or found
+            if selected_backend is None:
+                raise ValueError("No valid MLA backend could be found for this configuration.")
+            else:
+                raise ValueError(f"{selected_backend.name} is not a valid backend for this configuration.")
 
         if use_v1:
-            assert not use_mla  # will not trigger
             FLASHINFER_V1 = "vllm.v1.attention.backends.flashinfer.FlashInferBackend"  # noqa: E501
             FLEX_ATTENTION_V1 = "vllm.v1.attention.backends.flex_attention.FlexAttentionBackend"  # noqa: E501
             TRITON_ATTN_VLLM_V1 = "vllm.v1.attention.backends.triton_attn.TritonAttentionBackend"  # noqa: E501
