04aabb954a2ef67b8ef763ad75c982e15ef0fe15
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2025-09-21 16:03:28 -0700

    Remove V0 attention backends (#25351)
    Signed-off-by: Woosuk Kwon <woosuk.kwon@berkeley.edu>
    Signed-off-by: charlifu <charlifu@amd.com>

--- vllm/attention/backends/utils.py
+++ vllm/attention/backends/utils.py
@@ -338,10 +338,9 @@ class CommonAttentionState(AttentionState):
             # The encoder decoder model works only with XFormers and
             # Flash Attention backend. Assert the same.
             assert self.runner.attn_backend.get_name() in \
-                   ["XFORMERS", "FLASH_ATTN", "ROCM_FLASH"], \
-                f"Expected attn_backend name to be either 'XFORMERS'," \
-                f"'ROCM_FLASH', or 'FLASH_ATTN', but " \
-                f"got '{self.runner.attn_backend.get_name()}'"
+                   ["XFORMERS", "FLASH_ATTN"], \
+                f"Expected attn_backend name to be either 'XFORMERS' or " \
+                f"'FLASH_ATTN', but got '{self.runner.attn_backend.get_name()}'"
             self._update_captured_metadata_for_enc_dec_model(
                 batch_size=batch_size, attn_metadata=attn_metadata)
 
@@ -360,10 +359,9 @@ class CommonAttentionState(AttentionState):
             # The encoder decoder model works only with XFormers and
             # Flash Attention backend. Assert the same.
             assert self.runner.attn_backend.get_name() in \
-                   ["XFORMERS", "FLASH_ATTN", "ROCM_FLASH"], \
-                f"Expected attn_backend name to be either 'XFORMERS'," \
-                f"'ROCM_FLASH', or 'FLASH_ATTN', but " \
-                f"got '{self.runner.attn_backend.get_name()}'"
+                   ["XFORMERS", "FLASH_ATTN"], \
+                f"Expected attn_backend name to be either 'XFORMERS' or " \
+                f"'FLASH_ATTN', but got '{self.runner.attn_backend.get_name()}'"
             self._add_additional_input_buffers_for_enc_dec_model(
                 attn_metadata=attn_metadata, input_buffers=input_buffers)
         return input_buffers
