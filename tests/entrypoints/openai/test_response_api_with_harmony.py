9af54e5adcabdb6adfb008a9734ee646fa3c8c86
chaunceyjiang <chaunceyjiang@gmail.com>
2025-09-22 09:11:54 +0000

    [Feature][Responses API] Stream Function Call - harmony
    Signed-off-by: chaunceyjiang <chaunceyjiang@gmail.com>

--- tests/entrypoints/openai/test_response_api_with_harmony.py
+++ tests/entrypoints/openai/test_response_api_with_harmony.py
@@ -360,53 +360,56 @@ async def test_streaming_types(client: OpenAI, model_name: str):
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("model_name", [MODEL_NAME])
-async def test_streaming_types(client: OpenAI, model_name: str):
-    prompts = [
-        "tell me a story about a cat in 20 words",
-    ]
+async def test_function_calling_with_streaming_types(client: OpenAI,
+                                                     model_name: str):
 
     # this links the "done" type with the "start" type
     # so every "done" type should have a corresponding "start" type
     # and every open block should be closed by the end of the stream
     pairs_of_event_types = {
-        "response.completed": "response.created",
-        "response.output_item.done": "response.output_item.added",
-        "response.content_part.done": "response.content_part.added",
-        "response.output_text.done": "response.output_text.delta",
-        "response.web_search_call.done": "response.web_search_call.added",
-        "response.reasoning_text.done": "response.reasoning_text.delta",
-        "response.reasoning_part.done": "response.reasoning_part.added",
+        "response.completed":
+        "response.created",
+        "response.output_item.done":
+        "response.output_item.added",
+        "response.output_text.done":
+        "response.output_text.delta",
+        "response.reasoning_text.done":
+        "response.reasoning_text.delta",
+        "response.reasoning_part.done":
+        "response.reasoning_part.added",
+        "response.function_call_arguments.done":
+        "response.function_call_arguments.delta",  # noqa
     }
 
-    for prompt in prompts:
-        response = await client.responses.create(
-            model=model_name,
-            input=prompt,
-            reasoning={"effort": "low"},
-            tools=[],
-            stream=True,
-            background=False,
-        )
+    tools = [GET_WEATHER_SCHEMA]
+    input_list = [{
+        "role": "user",
+        "content": "What's the weather like in Paris today?",
+    }]
+    stream_response = await client.responses.create(
+        model=model_name,
+        input=input_list,
+        tools=tools,
+        stream=True,
+    )
 
-        stack_of_event_types = []
-        async for event in response:
-            if event.type == 'response.created':
-                stack_of_event_types.append(event.type)
-            elif event.type == 'response.completed':
-                assert stack_of_event_types[-1] == pairs_of_event_types[
-                    event.type]
-                stack_of_event_types.pop()
-            if event.type.endswith("added"):
-                stack_of_event_types.append(event.type)
-            elif event.type.endswith("delta"):
-                if stack_of_event_types[-1] == event.type:
-                    continue
-                stack_of_event_types.append(event.type)
-            elif event.type.endswith("done"):
-                assert stack_of_event_types[-1] == pairs_of_event_types[
-                    event.type]
-                stack_of_event_types.pop()
-        assert len(stack_of_event_types) == 0
+    stack_of_event_types = []
+    async for event in stream_response:
+        if event.type == 'response.created':
+            stack_of_event_types.append(event.type)
+        elif event.type == 'response.completed':
+            assert stack_of_event_types[-1] == pairs_of_event_types[event.type]
+            stack_of_event_types.pop()
+        if event.type.endswith("added"):
+            stack_of_event_types.append(event.type)
+        elif event.type.endswith("delta"):
+            if stack_of_event_types[-1] == event.type:
+                continue
+            stack_of_event_types.append(event.type)
+        elif event.type.endswith("done"):
+            assert stack_of_event_types[-1] == pairs_of_event_types[event.type]
+            stack_of_event_types.pop()
+    assert len(stack_of_event_types) == 0
 
 
 @pytest.mark.asyncio
