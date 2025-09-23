fc52f9da8c82edf8beb4ad2abcbae5c0a25fb892
wang.yuqi <noooop@126.com>
2025-09-23 19:15:51 +0800

    fix StepPooler
    Signed-off-by: wang.yuqi <noooop@126.com>

--- vllm/model_executor/layers/pooler.py
+++ vllm/model_executor/layers/pooler.py
@@ -759,7 +759,13 @@ class StepPooler(Pooler):
         pooling_metadata: PoolingMetadata,
     ) -> PoolerOutput:
         pooled_data = self.extract_states(hidden_states, pooling_metadata)
-        pooled_data = self.head(pooled_data, pooling_metadata)
+
+        pooling_params = get_pooling_params(pooling_metadata)
+        assert len(pooling_params) == len(pooling_params)
+
+        pooled_data = [
+            self.head(d, p) for d, p in zip(pooled_data, pooling_params)
+        ]
         return build_output(pooled_data)
 
 
