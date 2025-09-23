f0bc56967bc3c717ddace564e89f07f843cf3102
Lucas Wilkinson <lwilkins@redhat.com>
2025-09-23 13:49:26 +0000

    precommit fix
    Signed-off-by: Lucas Wilkinson <lwilkins@redhat.com>

--- vllm/model_executor/layers/fused_moe/deepep_ht_prepare_finalize.py
+++ vllm/model_executor/layers/fused_moe/deepep_ht_prepare_finalize.py
@@ -11,17 +11,12 @@ from vllm.model_executor.layers.fused_moe.topk_weight_and_reduce import (
     TopKWeightAndReduceContiguous, TopKWeightAndReduceDelegate)
 from vllm.model_executor.layers.fused_moe.utils import (
     moe_kernel_quantize_input)
-
-# yapf conflicts with isort for this block
-# yapf: disable
+from vllm.utils import round_up
 from vllm.v1.worker.ubatching import (
     dbo_current_ubatch_id, dbo_enabled, dbo_switch_to_comm,
     dbo_switch_to_compute, dbo_switch_to_compute_sync,
     dbo_yield_and_switch_from_comm_to_compute,
     dbo_yield_and_switch_from_compute_to_comm)
-
-# yapf: enable
-from vllm.utils import round_up
 
 
 class DeepEPHTPrepareAndFinalize(mk.FusedMoEPrepareAndFinalize):
