84c2362844343b724a271ee82a3210bfb2952098
Matthew Bonanni <mbonanni@redhat.com>
2025-09-22 20:26:17 -0400

    [BugFix] [DP/EP] Fix slow execution when BS <= DP (#25407)
    Signed-off-by: Matthew Bonanni <mbonanni@redhat.com>
    Co-authored-by: Robert Shaw <114415538+robertgshaw2-redhat@users.noreply.github.com>
    Co-authored-by: Chris Bamford <chrisbam4d@gmail.com>
    Signed-off-by: Joachim Studnia <joachim@mistral.ai>

--- vllm/v1/worker/gpu_worker.py
+++ vllm/v1/worker/gpu_worker.py
@@ -487,7 +487,7 @@ class Worker(WorkerBase):
                     sort_by="self_cuda_time_total"))
 
     def execute_dummy_batch(self) -> None:
-        self.model_runner._dummy_run(1)
+        self.model_runner._dummy_run(1, uniform_decode=True)
 
     def add_lora(self, lora_request: LoRARequest) -> bool:
         return self.model_runner.add_lora(lora_request)
