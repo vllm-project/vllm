15f24d2cc3c05c06935ccfab8229018a114bc35b
Junhong <liujunhong11@huawei.com>
2025-09-23 11:13:48 +0800

    [llama_eagle3] delete get_input_embeddings
    Signed-off-by: Junhong <liujunhong11@huawei.com>

--- vllm/model_executor/models/llama_eagle3.py
+++ vllm/model_executor/models/llama_eagle3.py
@@ -291,15 +291,3 @@ class Eagle3LlamaForCausalLM(LlamaForCausalLM):
         )
         loader.load_weights(model_weights.items())
 
-    def get_input_embeddings(
-        self,
-        input_ids: torch.Tensor,
-        multimodal_embeddings: Optional[MultiModalEmbeddings] = None,
-    ) -> torch.Tensor:
-        inputs_embeds = self.model.get_input_embeddings(input_ids)
-        if multimodal_embeddings is not None \
-            and len(multimodal_embeddings) != 0:
-            inputs_embeds = merge_multimodal_embeddings(
-                input_ids, inputs_embeds, multimodal_embeddings,
-                self.config.image_token_index)
-        return inputs_embeds
