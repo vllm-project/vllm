9f8a4f061c4893141e4c589644235f5d60c224f3
chaunceyjiang <chaunceyjiang@gmail.com>
2025-09-23 08:12:40 +0000

    [Frontend] Skip stop in reasoning content
    Signed-off-by: chaunceyjiang <chaunceyjiang@gmail.com>

--- tests/v1/engine/test_output_processor.py
+++ tests/v1/engine/test_output_processor.py
@@ -15,6 +15,7 @@ from tests.v1.engine.utils import (
     MockEngineCore,
 )
 from vllm import PoolingParams
+from vllm.config import VllmConfig, StructuredOutputsConfig
 from vllm.config import DecodingConfig, VllmConfig
 from vllm.logprobs import PromptLogprobs, SampleLogprobs
 from vllm.outputs import CompletionOutput, RequestOutput
@@ -46,7 +47,8 @@ def _ref_convert_id_to_token(
     [RequestOutputKind.DELTA, RequestOutputKind.FINAL_ONLY])
 def test_incremental_detokenization(request_output_kind: RequestOutputKind,
                                     dummy_test_vectors):
-    vllm_config = VllmConfig(decoding_config=DecodingConfig())
+    vllm_config = VllmConfig(
+        structured_outputs_config=StructuredOutputsConfig())
     output_processor = OutputProcessor(vllm_config=vllm_config,
                                        tokenizer=dummy_test_vectors.tokenizer,
                                        log_stats=False)
@@ -421,7 +423,8 @@ def test_logprobs_processor(request_output_kind: RequestOutputKind,
                             num_sample_logprobs: Optional[int],
                             num_prompt_logprobs: Optional[int],
                             dummy_test_vectors):
-    vllm_config = VllmConfig(decoding_config=DecodingConfig())
+    vllm_config = VllmConfig(
+        structured_outputs_config=StructuredOutputsConfig())
     output_processor = OutputProcessor(vllm_config=vllm_config,
                                        tokenizer=dummy_test_vectors.tokenizer,
                                        log_stats=False)
@@ -595,7 +598,8 @@ def test_stop_token(
         dummy_test_vectors.tokenizer.eos_token_id if is_eos_test else None
     )  # '<|end_of_text|>'
     stop_token_ids = [128009] if not is_eos_test else None  # '<|eot_id|>'
-    vllm_config = VllmConfig(decoding_config=DecodingConfig())
+    vllm_config = VllmConfig(
+        structured_outputs_config=StructuredOutputsConfig())
     output_processor = OutputProcessor(vllm_config=vllm_config,
                                        tokenizer=dummy_test_vectors.tokenizer,
                                        log_stats=False)
@@ -706,7 +710,8 @@ def test_stop_token(
                          [None, NUM_SAMPLE_LOGPROBS_UNDER_TEST])
 def test_stop_string(include_stop_str_in_output: bool,
                      num_sample_logprobs: Optional[int], dummy_test_vectors):
-    vllm_config = VllmConfig(decoding_config=DecodingConfig())
+    vllm_config = VllmConfig(
+        structured_outputs_config=StructuredOutputsConfig())
     output_processor = OutputProcessor(vllm_config=vllm_config,
                                        tokenizer=dummy_test_vectors.tokenizer,
                                        log_stats=False)
@@ -837,7 +842,8 @@ def test_stop_string(include_stop_str_in_output: bool,
 
 
 def test_iteration_stats(dummy_test_vectors):
-    vllm_config = VllmConfig(decoding_config=DecodingConfig())
+    vllm_config = VllmConfig(
+        structured_outputs_config=StructuredOutputsConfig())
     output_processor = OutputProcessor(vllm_config=vllm_config,
                                        tokenizer=dummy_test_vectors.tokenizer,
                                        log_stats=True)
