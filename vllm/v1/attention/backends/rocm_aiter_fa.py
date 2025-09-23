5cc7ae5ab7e0d56989e72be3f83cf4c5bfd5cc46
zejunchen-zejun <zejun.chen@amd.com>
2025-09-23 21:24:09 +0800

    [ROCm][FP8] determine the kv cache dtype according to the platform in aiter attention backend
    Signed-off-by: zejunchen-zejun <zejun.chen@amd.com>

--- vllm/v1/attention/backends/rocm_aiter_fa.py
+++ vllm/v1/attention/backends/rocm_aiter_fa.py
@@ -479,8 +479,13 @@ class AiterFlashAttentionImpl(AttentionImpl):
             )
 
         if self.kv_cache_dtype.startswith("fp8"):
-            key_cache = key_cache.view(current_platform.fp8_dtype())
-            value_cache = value_cache.view(current_platform.fp8_dtype())
+            if current_platform.is_fp8_fnuz():
+                # MI300/308 only support fp8_e4m3fnuz
+                key_cache = key_cache.view(torch.float8_e4m3fnuz)
+                value_cache = value_cache.view(torch.float8_e4m3fnuz)
+            else:
+                key_cache = key_cache.view(torch.float8_e4m3fn)
+                value_cache = value_cache.view(torch.float8_e4m3fn)
 
         if not attn_metadata.use_cascade:
             cu_seqlens_q = attn_metadata.query_start_loc
