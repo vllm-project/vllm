b198fb1872fd97fd60a07b2dc07baf912750884d
bzantium <ryumin93@gmail.com>
2025-09-24 23:36:04 +0900

    apply pre-commit
    Signed-off-by: bzantium <ryumin93@gmail.com>

--- vllm/model_executor/models/minicpm3.py
+++ vllm/model_executor/models/minicpm3.py
@@ -85,14 +85,14 @@ class MiniCPM3Attention(nn.Module):
 
         if self.q_lora_rank is not None:
             self.q_a_proj = ReplicatedLinear(self.hidden_size,
-                                            self.q_lora_rank,
-                                            bias=False,
-                                            quant_config=quant_config)
+                                             self.q_lora_rank,
+                                             bias=False,
+                                             quant_config=quant_config)
             self.q_a_layernorm = RMSNorm(self.q_lora_rank, eps=config.rms_norm_eps)
-            self.q_b_proj = ColumnParallelLinear(q_lora_rank,
-                                                self.num_heads * self.qk_head_dim,
-                                                bias=False,
-                                                quant_config=quant_config)
+            self.q_b_proj = ColumnParallelLinear(self.q_lora_rank,
+                                                 self.num_heads * self.qk_head_dim,
+                                                 bias=False,
+                                                 quant_config=quant_config)
         else:
             self.q_proj = ColumnParallelLinear(self.hidden_size,
                                                self.num_heads *
