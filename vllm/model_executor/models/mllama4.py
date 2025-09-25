c4bb22b5b49900b2162d951473ed33ad9bf20f7e
Lucas Kabela <lucaskabela@meta.com>
2025-09-25 15:59:34 -0700

    Remove errenously added import
    Signed-off-by: Lucas Kabela <lucaskabela@meta.com>

--- vllm/model_executor/models/mllama4.py
+++ vllm/model_executor/models/mllama4.py
@@ -32,7 +32,6 @@ from transformers.models.llama4.image_processing_llama4_fast import (
 from vllm.attention.layer import MultiHeadAttention
 from vllm.config import VllmConfig
 from vllm.distributed import get_tensor_model_parallel_world_size
-from vllm.inputs import InputProcessingContext
 from vllm.model_executor.layers.linear import (ColumnParallelLinear,
                                                QKVParallelLinear,
                                                ReplicatedLinear,
