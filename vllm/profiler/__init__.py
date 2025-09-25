a80e912f4d87794195fc8edcd00425354664b079
Duncan Moss <djm.moss@gmail.com>
2025-09-25 10:07:03 -0700

    pre-commit again
    Signed-off-by: Duncan Moss <djm.moss@gmail.com>

--- vllm/profiler/__init__.py
+++ vllm/profiler/__init__.py
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 from .nsys_profiler import NsysIterationProfiler
 
 __all__ = ["NsysIterationProfiler"]
