8effd771a48fba85a8f8ad615007de4c31d677cd
Varun Sundar Rabindranath <vsundarr@redhat.com>
2025-09-25 14:41:17 +0000

    GPTOSS Support Batched All2All via Marlin
    Signed-off-by: Varun Sundar Rabindranath <vsundarr@redhat.com>

--- vllm/model_executor/layers/quantization/mxfp4.py
+++ vllm/model_executor/layers/quantization/mxfp4.py
@@ -22,8 +22,9 @@ from vllm.model_executor.layers.fused_moe.config import (
     ocp_mx_moe_quant_config,
 )
 from vllm.model_executor.layers.fused_moe.fused_marlin_moe import (
-    MarlinExperts,
     fused_marlin_moe,
+    BatchedMarlinExperts,
+    MarlinExperts,
 )
 from vllm.model_executor.layers.fused_moe.gpt_oss_triton_kernels_moe import (
     OAITritonExperts,
@@ -797,9 +798,19 @@ class Mxfp4MoEMethod(FusedMoEMethodBase):
             prepare_finalize.activation_format
             == mk.FusedMoEActivationFormat.BatchedExperts
         ):
-            raise NotImplementedError(
-                "Mxfp4 does not support batched experts format for EP"
-            )
+            if self.mxfp4_backend == Mxfp4Backend.MARLIN:
+                max_num_tokens_per_rank = prepare_finalize.max_num_tokens_per_rank()
+                assert max_num_tokens_per_rank is not None
+                assert self.moe_quant_config is not None
+                return BatchedMarlinExperts(
+                    max_num_tokens=max_num_tokens_per_rank,
+                    num_dispatchers=prepare_finalize.num_dispatchers(),
+                    quant_config=self.moe_quant_config,
+                )
+            else:
+                raise NotImplementedError(
+                    "Incompatible Mxfp4 backend for EP batched experts format"
+                )
         else:
             assert self.moe_quant_config is not None
             if (
