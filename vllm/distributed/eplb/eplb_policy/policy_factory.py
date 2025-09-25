f1c6d1b587beb5056885e7598bc9015a42618e42
Mercykid-bash <ruanche0218@gmail.com>
2025-09-25 11:33:39 +0800

    Update policy_factory.py
    Signed-off-by: Mercykid-bash <ruanche0218@gmail.com>

--- vllm/distributed/eplb/eplb_policy/policy_factory.py
+++ vllm/distributed/eplb/eplb_policy/policy_factory.py
@@ -21,13 +21,11 @@ class PolicyFactory:
         Dynamic EPLB:  expert replacement with
         constrained number of expert shuffle
         """
-        policy = {
-            0:
-            DefaultEplb,
-            1:
-            SwiftBalancer,
-            2:
-            FlashLB,
+        policy: dict[int, Type[EplbPolicy]] = {
+            0: DefaultEplb,
+            1: SwiftBalancer,
+            2: FlashLB,
         }
-
-        return policy.get(policy_type, DefaultEplb)()
+        
+        selected_policy = policy.get(policy_type, DefaultEplb)
+        return selected_policy()
