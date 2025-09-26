5bc620488d8c63257e79270a97d6c8afb5deb6e0
vllmellm <vllm.ellm@embeddedllm.com>
2025-09-26 13:33:32 +0000

    fix bug when +rotary_embedding is requested as custom_ops for Llama4 and deepseek models.
    Signed-off-by: vllmellm <vllm.ellm@embeddedllm.com>

--- vllm/model_executor/layers/rotary_embedding/deepseek_scaling_rope.py
+++ vllm/model_executor/layers/rotary_embedding/deepseek_scaling_rope.py
@@ -128,6 +128,15 @@ class DeepseekScalingRotaryEmbedding(RotaryEmbedding):
             key = key_rot
         return query, key
 
+    def forward_hip(
+        self,
+        positions: torch.Tensor,
+        query: torch.Tensor,
+        key: Optional[torch.Tensor] = None,
+        offsets: Optional[torch.Tensor] = None,
+    ) -> tuple[torch.Tensor, Optional[torch.Tensor]]:
+        return self.forward_native(positions, query, key, offsets)
+
     def forward_cuda(
         self,
         positions: torch.Tensor,
--- vllm/model_executor/layers/rotary_embedding/llama4_vision_rope.py
+++ vllm/model_executor/layers/rotary_embedding/llama4_vision_rope.py
@@ -73,6 +73,13 @@ class Llama4VisionRotaryEmbedding(RotaryEmbedding):
         key_out = torch.view_as_real(key_ * freqs_ci).flatten(3)
         return query_out.type_as(query), key_out.type_as(key)
 
+    def forward_hip(
+        self,
+        query: torch.Tensor,
+        key: Optional[torch.Tensor] = None,
+    ) -> tuple[torch.Tensor, Optional[torch.Tensor]]:
+        return self.forward_native(query, key)
+
     def forward_cuda(  # type: ignore[override]
         self,
         query: torch.Tensor,
