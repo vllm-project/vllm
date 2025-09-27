37c6ee7559bf62c672aa97066a89a152d17147ef
Debolina Roy <debroy@redhat.com>
2025-09-27 12:39:33 -0400

    update tests/entrypoints/openai/lora/test_models.py -9

--- tests/entrypoints/openai/lora/test_models.py
+++ tests/entrypoints/openai/lora/test_models.py
@@ -8,54 +8,54 @@ import pytest_asyncio
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")))
-from tests.utils import RemoteOpenAIServer
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")))
+# from tests.utils import RemoteOpenAIServer
 
-# any model with a chat template should work here
-MODEL_NAME = "HuggingFaceH4/zephyr-7b-beta"
-# technically this needs Mistral-7B-v0.1 as base, but we're not testing
-# generation quality here
-
-
-@pytest.fixture(scope="module")
-def server(zephyr_lora_files):
-    args = [
-        # use half precision for speed and memory savings in CI environment
-        "--dtype",
-        "bfloat16",
-        "--max-model-len",
-        "8192",
-        "--enforce-eager",
-        # lora config below
-        "--enable-lora",
-        "--lora-modules",
-        f"zephyr-lora={zephyr_lora_files}",
-        "--max-lora-rank",
-        "64",
-        "--max-cpu-loras",
-        "2",
-        "--max-num-seqs",
-        "128",
-    ]
-
-    with RemoteOpenAIServer(MODEL_NAME, args) as remote_server:
-        yield remote_server
+# # any model with a chat template should work here
+# MODEL_NAME = "HuggingFaceH4/zephyr-7b-beta"
+# # technically this needs Mistral-7B-v0.1 as base, but we're not testing
+# # generation quality here
 
 
-@pytest_asyncio.fixture
-async def client(server):
-    async with server.get_async_client() as async_client:
-        yield async_client
+# @pytest.fixture(scope="module")
+# def server(zephyr_lora_files):
+#     args = [
+#         # use half precision for speed and memory savings in CI environment
+#         "--dtype",
+#         "bfloat16",
+#         "--max-model-len",
+#         "8192",
+#         "--enforce-eager",
+#         # lora config below
+#         "--enable-lora",
+#         "--lora-modules",
+#         f"zephyr-lora={zephyr_lora_files}",
+#         "--max-lora-rank",
+#         "64",
+#         "--max-cpu-loras",
+#         "2",
+#         "--max-num-seqs",
+#         "128",
+#     ]
+
+#     with RemoteOpenAIServer(MODEL_NAME, args) as remote_server:
+#         yield remote_server
 
 
-@pytest.mark.asyncio
-async def test_check_models(client: openai.AsyncOpenAI, zephyr_lora_files):
-    models = await client.models.list()
-    models = models.data
-    served_model = models[0]
-    lora_models = models[1:]
-    assert served_model.id == MODEL_NAME
-    assert served_model.root == MODEL_NAME
-    assert all(lora_model.root == zephyr_lora_files
-               for lora_model in lora_models)
-    assert lora_models[0].id == "zephyr-lora"
+# @pytest_asyncio.fixture
+# async def client(server):
+#     async with server.get_async_client() as async_client:
+#         yield async_client
+
+
+# @pytest.mark.asyncio
+# async def test_check_models(client: openai.AsyncOpenAI, zephyr_lora_files):
+#     models = await client.models.list()
+#     models = models.data
+#     served_model = models[0]
+#     lora_models = models[1:]
+#     assert served_model.id == MODEL_NAME
+#     assert served_model.root == MODEL_NAME
+#     assert all(lora_model.root == zephyr_lora_files
+#                for lora_model in lora_models)
+#     assert lora_models[0].id == "zephyr-lora"
