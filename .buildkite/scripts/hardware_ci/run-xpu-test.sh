ff9bf0576e6a64b44f470b29dee4d2ae240d7b1e
Cyrus Leung <tlleungac@connect.ust.hk>
2025-09-27 13:51:15 +0800

    [CI/Build] Reorganize root-level V1 tests (#25767)
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- .buildkite/scripts/hardware_ci/run-xpu-test.sh
+++ .buildkite/scripts/hardware_ci/run-xpu-test.sh
@@ -44,7 +44,6 @@ docker run \
     pytest -v -s v1/structured_output
     pytest -v -s v1/spec_decode --ignore=v1/spec_decode/test_max_len.py --ignore=v1/spec_decode/test_eagle.py --ignore=v1/spec_decode/test_tree_attention.py
     pytest -v -s v1/kv_connector/unit --ignore=v1/kv_connector/unit/test_multi_connector.py --ignore=v1/kv_connector/unit/test_nixl_connector.py --ignore=v1/kv_connector/unit/test_shared_storage_connector.py
+    pytest -v -s v1/test_metrics
     pytest -v -s v1/test_serial_utils.py
-    pytest -v -s v1/test_utils.py
-    pytest -v -s v1/test_metrics_reader.py
 '
