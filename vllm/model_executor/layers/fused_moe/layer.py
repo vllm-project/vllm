8905cc58e9e8e3d1403e638941aa5fe5891c5972
Danielle Robinson <dmmaddix@amazon.com>
2025-09-27 04:30:15 +0000

    Revert back expert_params to take 3 proj weights

--- vllm/model_executor/layers/fused_moe/layer.py
+++ vllm/model_executor/layers/fused_moe/layer.py
@@ -1718,7 +1718,7 @@ class FusedMoE(CustomOp):
             cls,
             ckpt_gate_proj_name: str,
             ckpt_down_proj_name: str,
-           # ckpt_up_proj_name: str,
+            ckpt_up_proj_name: str,
             num_experts: int,
             num_redundant_experts: int = 0) -> list[tuple[str, str, int, str]]:
 
@@ -1735,13 +1735,13 @@ class FusedMoE(CustomOp):
         return [
             # (param_name, weight_name, expert_id, shard_id)
             ("experts.w13_" if weight_name
-             in [ckpt_gate_proj_name] else "experts.w2_", # [ckpt_gate_proj_name, ckpt_up_proj_name]
+             in [ckpt_gate_proj_name, ckpt_up_proj_name] else "experts.w2_",
              f"experts.{physical_to_logical_map[expert_id]}.{weight_name}.",
              expert_id, shard_id) for expert_id in range(num_physical_experts)
             for shard_id, weight_name in [
                 ("w1", ckpt_gate_proj_name),
                 ("w2", ckpt_down_proj_name),
-                #("w3", ckpt_up_proj_name),
+                ("w3", ckpt_up_proj_name),
             ]
         ]
 
