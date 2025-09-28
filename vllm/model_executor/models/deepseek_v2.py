a95229bbf9664f45a552cb07075bd451004c246f
Yongye Zhu <zyy1102000@gmail.com>
2025-09-28 00:34:42 -0400

    indexer ref code cleanup (#47)

--- vllm/model_executor/models/deepseek_v2.py
+++ vllm/model_executor/models/deepseek_v2.py
@@ -586,34 +586,6 @@ direct_register_custom_op(
     dispatch_key=current_platform.dispatch_key,
 )
 
-
-def ref_fp8_mqa_logits(
-    q: torch.Tensor,
-    kv: torch.Tensor,
-    weights: torch.Tensor,
-    cu_seqlen_ks: torch.Tensor,
-    cu_seqlen_ke: torch.Tensor,
-):
-    # print(f"q_shape: {q.shape}, v_shape: {kv.shape}, weights.shape: {weights.shape}")
-    k = kv
-    q = q.float()
-    k = k.float()
-
-    seq_len_kv = kv.shape[0]
-    mask_lo = (torch.arange(0, seq_len_kv, device="cuda")[None, :]
-               >= cu_seqlen_ks[:, None])
-    mask_hi = (torch.arange(0, seq_len_kv, device="cuda")[None, :]
-               < cu_seqlen_ke[:, None])
-    mask = mask_lo & mask_hi
-
-    score = torch.einsum("mhd,nd->hmn", q, k)
-    logits = (score.relu() * weights.transpose(0, 1)).sum(dim=0)
-    logits = logits.masked_fill(~mask, float("-inf"))
-
-    cost = mask.sum()
-    return logits, cost
-
-
 @torch.inference_mode()
 def indexer_k_quant_and_cache(
     k,
