c3f71b88fac0edc883904cbfd9f978a21a3e9460
Roger Wang <hey@rogerw.io>
2025-09-27 17:19:23 -0700

    update
    Signed-off-by: Roger Wang <hey@rogerw.io>

--- vllm/model_executor/models/qwen3_vl.py
+++ vllm/model_executor/models/qwen3_vl.py
@@ -679,7 +679,7 @@ class Qwen3VLProcessingInfo(Qwen2VLProcessingInfo):
         )
 
         # NOTE: By default in Qwen3-VL, one video token is converted to
-        # "<timestamp seconds>" (on average 9 tokens) + vision_start_token + video_token + vision_end_token # noqa: E501
+        # "<{timestamp} seconds>" (at most 9 tokens) + vision_start_token + video_token + vision_end_token # noqa: E501
         formatted_video_soft_tokens = video_soft_tokens * 12
         return formatted_video_soft_tokens
 
