acbcb579b30ba12a2e59f3cdac481167ea748503
root <root@smci355-ccs-aus-m01-25.cs-aus.dcgpu>
2025-09-29 07:44:57 +0000

    add rmsnorm_quant4 fusion

--- vllm/compilation/rocm_fusion.py
+++ vllm/compilation/rocm_fusion.py
@@ -86,6 +86,66 @@ class SiluMulMXFP4GemmPattern:
         )
 
 
+ADD_RMS_OP = torch.ops._C.fused_add_rms_norm.default
+
+
+class AddRMSNormMXFP4GemmPattern:
+    def __init__(self, epsilon: float):
+        self.epsilon = epsilon
+        self.FUSED_OP = torch.ops.vllm.add_rmsnorm_mxfp4_gemm.default
+        self.QUANT_F4GEMM_OP = torch.ops.vllm.gemm_with_dynamic_quant.default
+        
+    def register(self, pm_pass: PatternMatcherPass):
+
+        def pattern(
+            result: torch.Tensor, input: torch.Tensor, 
+            residual: torch.Tensor, weight_rms: torch.Tensor, 
+            weight_gemm: torch.Tensor, scale: torch.Tensor):
+            at1 = auto_functionalized(ADD_RMS_OP,
+                                    input=input,
+                                    residual=residual,
+                                    weight=weight_rms,
+                                    epsilon=self.epsilon)
+            at2 = auto_functionalized(self.QUANT_F4GEMM_OP,
+                                    result=result,
+                                    x=at1[1],
+                                    weight=weight_gemm,
+                                    weight_scale=scale,
+                                    x_scales=None)
+            return at2[1], at1[2]
+        
+        def replacement(
+            result: torch.Tensor, input: torch.Tensor, 
+            residual: torch.Tensor, weight_rms: torch.Tensor, 
+            weight_gemm: torch.Tensor, scale: torch.Tensor):
+            at = auto_functionalized(self.FUSED_OP,
+                                    result=result,
+                                    input=input,
+                                    residual=residual,
+                                    residual_out=residual,
+                                    weight_rms=weight_rms,
+                                    weight_gemm=weight_gemm,
+                                    scale=scale,
+                                    epsilon=self.epsilon)
+            return at[1], at[2]
+
+        inputs = [
+            empty_bf16(4, 4),  # result
+            empty_bf16(4, 4),  # input
+            empty_bf16(4, 4),  # residual
+            empty_bf16(1, 4),   # weight_rms
+            empty_fp4(4, 4),   # weight_gemm
+            empty_fp4(1, 1),    # scale
+        ]
+
+        pm.register_replacement(
+            pattern,
+            replacement,
+            inputs,
+            pm.fwd_only,
+            pm_pass)
+
+
 class ROCmFusionPass(VllmPatternMatcherPass):
     """
     This pass fuses a pre-defined set of custom ops into fused ops.
@@ -101,13 +161,15 @@ class ROCmFusionPass(VllmPatternMatcherPass):
 
         SiluMulMXFP4GemmPattern().register(self.patterns)
 
+        for epsilon in [1e-5, 1e-6]:
+            AddRMSNormMXFP4GemmPattern(epsilon).register(self.patterns)
+
         self.dump_patterns(config, self.patterns)
 
     @VllmInductorPass.time_and_log
     def __call__(self, graph: fx.Graph):
-        logger.info(graph)
         self.matched_count = self.patterns.apply(graph)
-        logger.info("Replaced %s patterns", self.matched_count)
+        logger.debug("Replaced %s patterns", self.matched_count)
 
     def uuid(self) -> Any:
         return self.hash_source(self, SiluMulMXFP4GemmPattern)
