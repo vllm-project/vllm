c5c5606d0b0433ca2ca0c267cb2a835dba60e548
Benjamin Chislett <bchislett@nvidia.com>
2025-09-30 20:08:48 +0000

    enable spec compat for flashinfer-mla
    Signed-off-by: Benjamin Chislett <bchislett@nvidia.com>

--- vllm/v1/attention/backends/utils.py
+++ vllm/v1/attention/backends/utils.py
@@ -260,7 +260,8 @@ class AttentionMetadataBuilder(abc.ABC, Generic[M]):
             if (speculative_config is not None
                     and speculative_config.num_speculative_tokens is not None):
                 self.reorder_batch_threshold = \
-                    1 + speculative_config.num_speculative_tokens
+                    max(self.reorder_batch_threshold,
+                        1 + speculative_config.num_speculative_tokens)
 
     @abstractmethod
     def build(self,
