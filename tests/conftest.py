c7b495c53c8f8dac22bdde9ef0b193b2e40a696a
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-09-30 14:32:25 +0200

    Gemini comment
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -731,7 +731,7 @@ class VllmRunner:
         init_ctx = (nullcontext() if default_torch_num_threads is None else
                     set_default_torch_num_threads(default_torch_num_threads))
 
-        if "compilation_config" not in kwargs:
+        if not kwargs.get("compilation_config", None):
             kwargs["compilation_config"] = {"cudagraph_capture_sizes": [8]}
 
         with init_ctx:
