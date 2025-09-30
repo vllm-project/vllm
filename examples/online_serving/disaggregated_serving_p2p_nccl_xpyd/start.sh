d45d8feb228f276523661a12b86d7e6364e8894e
Rui Qiao <ruisearch42@gmail.com>
2025-09-29 17:29:54 -0700

    use local dir
    Signed-off-by: Rui Qiao <ruisearch42@gmail.com>

--- examples/online_serving/disaggregated_serving_p2p_nccl_xpyd/start.sh
+++ examples/online_serving/disaggregated_serving_p2p_nccl_xpyd/start.sh
@@ -1,11 +1,17 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+NCCL_XPYD_SCRIPT=$SCRIPT_DIR/disagg_example_p2p_nccl_xpyd.sh
+
 PREFILL_GPUS=0 DECODE_GPUS=1 PREFILL_PORTS=20003 DECODE_PORTS=20005 \
 MODEL=Qwen/Qwen3-1.7B \
-ASYNC_TRANSFER=false \
-NCCL_DEBUG=INFO \
+ASYNC_TRANSFER=true \
+NCCL_DEBUG=WARN \
 NCCL_DEBUG_SUBSYS=ALL \
 NCCL_P2P_DISABLE=1 \
 NCCL_SHM_DISABLE=1 \
 NCCL_IB_DISABLE=1 \
 NCCL_NET_GDR_LEVEL=0 \
 VLLM_LOGGING_LEVEL=DEBUG \
-bash /home/ubuntu/vllm/examples/online_serving/disaggregated_serving_p2p_nccl_xpyd/disagg_example_p2p_nccl_xpyd.sh
+PATH=/usr/local/nvidia/bin:$PATH \
+bash $NCCL_XPYD_SCRIPT
