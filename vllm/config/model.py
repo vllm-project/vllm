886bd1239b5d3db8d004e6ecff2d4f94a5e2d549
Yongye Zhu <zyy1102000@gmail.com>
2025-09-30 05:14:41 -0400

    [New Model] DeepSeek-V3.2 (Rebased to Main) (#25896)
    Signed-off-by: Chen Zhang <zhangch99@outlook.com>
    Signed-off-by: youkaichao <youkaichao@gmail.com>
    Signed-off-by: Lucas Wilkinson <lwilkins@redhat.com>
    Signed-off-by: mgoin <mgoin64@gmail.com>
    Signed-off-by: NickLucche <nlucches@redhat.com>
    Signed-off-by: Yongye Zhu <zyy1102000@gmail.com>
    Signed-off-by: Barry Kang <43644113+Barry-Delaney@users.noreply.github.com>
    Signed-off-by: Lucia Fang <fanglu@meta.com>
    Co-authored-by: Chen Zhang <zhangch99@outlook.com>
    Co-authored-by: youkaichao <youkaichao@gmail.com>
    Co-authored-by: Lucas Wilkinson <lwilkins@redhat.com>
    Co-authored-by: Robert Shaw <114415538+robertgshaw2-redhat@users.noreply.github.com>
    Co-authored-by: Lucas Wilkinson <LucasWilkinson@users.noreply.github.com>
    Co-authored-by: yewentao256 <zhyanwentao@126.com>
    Co-authored-by: Wentao Ye <44945378+yewentao256@users.noreply.github.com>
    Co-authored-by: mgoin <mgoin64@gmail.com>
    Co-authored-by: Lucia Fang <116399278+luccafong@users.noreply.github.com>
    Co-authored-by: Lucia Fang <fanglu@meta.com>
    Co-authored-by: NickLucche <nlucches@redhat.com>
    Co-authored-by: Siyuan Fu <siyuanf@nvidia.com>
    Co-authored-by: Matthew Bonanni <mbonanni@redhat.com>
    Co-authored-by: Xiaozhu Meng <mxz297@gmail.com>
    Co-authored-by: Barry Kang <43644113+Barry-Delaney@users.noreply.github.com>

--- vllm/config/model.py
+++ vllm/config/model.py
@@ -1082,14 +1082,14 @@ class ModelConfig:
         if not hasattr(self.hf_text_config, "model_type"):
             return False
         elif self.hf_text_config.model_type in \
-            ('deepseek_v2', 'deepseek_v3', 'deepseek_mtp',
+            ('deepseek_v2', 'deepseek_v3', 'deepseek_v32', 'deepseek_mtp',
               'kimi_k2', 'longcat_flash'):
             return self.hf_text_config.kv_lora_rank is not None
         elif self.hf_text_config.model_type == 'eagle':
             # if the model is an EAGLE module, check for the
             # underlying architecture
             return self.hf_text_config.model.model_type in \
-                    ('deepseek_v2', 'deepseek_v3') \
+                    ('deepseek_v2', 'deepseek_v3', 'deepseek_v32') \
                 and self.hf_text_config.kv_lora_rank is not None
         return False
 
