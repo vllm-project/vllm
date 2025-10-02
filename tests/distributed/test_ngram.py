bc1a1188511303e5b1725fa1abe7b6d772cbd24f
Ekagra Ranjan <3116519+ekagra-ranjan@users.noreply.github.com>
2025-10-02 16:54:46 +0000

    lint
    Signed-off-by: Ekagra Ranjan <3116519+ekagra-ranjan@users.noreply.github.com>

--- tests/distributed/test_ngram.py
+++ tests/distributed/test_ngram.py
@@ -1,11 +1,12 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
-import torch
 import os
+
+import torch
+
 from tests.utils import init_test_distributed_environment
 from tests.v1.spec_decode.test_ngram import test_ngram_proposer
 from vllm.distributed.parallel_state import cleanup_dist_env_and_memory
-
 
 if __name__ == "__main__":
     pp_size = 1
@@ -13,9 +14,12 @@ if __name__ == "__main__":
     tp_size = int(os.environ['WORLD_SIZE'])
     device = torch.device(f"cuda:{local_rank}")
     torch.cuda.set_device(device)
-    init_test_distributed_environment(tp_size=tp_size, pp_size=pp_size, rank=local_rank,
-                                        distributed_init_port=None, local_rank=local_rank)
+    init_test_distributed_environment(tp_size=tp_size,
+                                      pp_size=pp_size,
+                                      rank=local_rank,
+                                      distributed_init_port=None,
+                                      local_rank=local_rank)
 
     test_ngram_proposer()
     cleanup_dist_env_and_memory()
-    print(f"test_ngram_distributed() successfully passed!")
+    print("test_ngram_distributed() successfully passed!")
