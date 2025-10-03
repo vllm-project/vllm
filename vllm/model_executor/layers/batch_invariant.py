5d68c8943b9faef9aee96dd8f641c6442f6ce76b
Bram Wasti <bwasti@fb.com>
2025-10-03 16:10:20 -0500

    Update vllm/model_executor/layers/batch_invariant.py
    Co-authored-by: Wentao Ye <44945378+yewentao256@users.noreply.github.com>
    Signed-off-by: Bram Wasti <bwasti@fb.com>

--- vllm/model_executor/layers/batch_invariant.py
+++ vllm/model_executor/layers/batch_invariant.py
@@ -567,6 +567,6 @@ def init_batch_invariance():
             warning = "Forcibly updating attention backend to" \
                 f" {supported_backends[0]} for batch_invariant. " \
                 f" Supported backends: {supported_backends}."
-            logger.warning(warning)
+            logger.warning_once(warning)
             os.environ["VLLM_ATTENTION_BACKEND"] = supported_backends[0]
         enable_batch_invariant_mode()
