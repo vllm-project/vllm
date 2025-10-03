ea2c72a262e9644c4aefd430dbb3ca357e7e4967
Paul Pak <52512091+paulpak58@users.noreply.github.com>
2025-10-03 21:59:48 +0900

    [backends][short_conv] CUDA graph piecewise edits (#24215)
    Signed-off-by: Paul Pak <paulpak58@gmail.com>

--- vllm/model_executor/layers/mamba/short_conv.py
+++ vllm/model_executor/layers/mamba/short_conv.py
@@ -115,7 +115,7 @@ class ShortConv(MambaBase, CustomOp):
             self_kv_cache = self.kv_cache[forward_context.virtual_engine]
             conv_state = self_kv_cache[0].transpose(-1, -2)
             state_indices_tensor = attn_metadata.state_indices_tensor
-            has_initial_states_p = attn_metadata.has_initial_states
+            has_initial_states_p = attn_metadata.has_initial_states_p
 
         BCx, _ = self.in_proj(hidden_states)
 
