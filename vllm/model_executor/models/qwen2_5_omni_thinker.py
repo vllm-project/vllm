f70f1d0c3a14d807b557634b25a2698da55cc18b
Divyansh Singhvi <divyanshsinghvi@gmail.com>
2025-10-07 23:13:17 +0530

    Fixes mrope adde static func
    Signed-off-by: Divyansh Singhvi <divyanshsinghvi@gmail.com>

--- vllm/model_executor/models/qwen2_5_omni_thinker.py
+++ vllm/model_executor/models/qwen2_5_omni_thinker.py
@@ -992,6 +992,14 @@ class Qwen2_5OmniThinkerForConditionalGeneration(
     def get_language_model(self) -> torch.nn.Module:
         return self.language_model
 
+    @staticmethod
+    def _split_list_into_ranges(lst: torch.Tensor, interval: int) -> list[list[int]]:
+        ranges: list[list[int]] = [[] for _ in range((max(lst) // interval) + 1)]
+        for num in lst:
+            index = num // interval
+            ranges[index].append(num)
+        return ranges
+
     @classmethod
     def get_mrope_input_positions(
         cls,
