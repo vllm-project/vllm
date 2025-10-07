e5e6e689c825cf2075ce6f5a2047c1f5e771428e
PatrykSaffer <patryk.saffer@mistral.ai>
2025-10-07 10:56:23 +0200

    Update _custom_ops.py
    Signed-off-by: PatrykSaffer <patryk.saffer@mistral.ai>

--- vllm/_custom_ops.py
+++ vllm/_custom_ops.py
@@ -2061,6 +2061,7 @@ def copy_blocks(
 ) -> None:
     torch.ops._C_cache_ops.copy_blocks(key_caches, value_caches, block_mapping)
 
+
 def copy_blocks_mla(kv_caches: list[torch.Tensor], block_mapping: torch.Tensor) -> None:
     torch.ops._C_cache_ops.copy_blocks_mla(kv_caches, block_mapping)
 
