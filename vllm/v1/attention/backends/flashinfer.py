a7bd5621fca9f87999fb55a28ab8f1dae945d873
elvischenv <219235043+elvischenv@users.noreply.github.com>
2025-10-07 01:06:02 +0000

    bump flashinfer to v0.4.0rc4
    Signed-off-by: elvischenv <219235043+elvischenv@users.noreply.github.com>

--- vllm/v1/attention/backends/flashinfer.py
+++ vllm/v1/attention/backends/flashinfer.py
@@ -1191,7 +1191,7 @@ def fast_plan_decode(
     qo_indptr_host = _get_range_buf(batch_size + 1, "cpu")
 
     try:
-        # Make sure we pass exactly 15 arguments for tensor core version
+        # Make sure we pass exactly 18 arguments for tensor core version
         self._plan_info = self._cached_module.plan(
             self._float_workspace_buffer,
             self._int_workspace_buffer,
@@ -1208,6 +1208,9 @@ def fast_plan_decode(
             head_dim,
             head_dim,
             False,  # causal
+            window_left,
+            -1,
+            False,
         )
     except Exception as e:
         raise RuntimeError(f"Error in tensor core plan: {e}") from e
