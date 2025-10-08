9056207593495733c2f6e0a9956355b208ca7982
Elaine Zhao <elaineyz@amazon.com>
2025-10-08 14:08:24 -0700

    [Bugfix] Respect min_tokens in scheduler stop check (#26317)
    Signed-off-by: Elaine Zhao <elaineyz@amazon.com>
    Signed-off-by: Joachim Studnia <joachim@mistral.ai>

--- vllm/v1/core/sched/utils.py
+++ vllm/v1/core/sched/utils.py
@@ -58,6 +58,11 @@ def check_stop(
 
     sampling_params = request.sampling_params
     assert sampling_params is not None
+
+    min_tokens = sampling_params.min_tokens
+    if request.num_output_tokens < min_tokens:
+        return False
+
     last_token_id = request.output_token_ids[-1]
     if not sampling_params.ignore_eos and last_token_id == request.eos_token_id:
         request.status = RequestStatus.FINISHED_STOPPED
