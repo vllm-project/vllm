d729795bde40053a0b511c532cc01430b3314462
Randall Smith <ransmith@amd.com>
2025-10-08 12:32:11 -0500

    Use view inside of host code instead of reshape in model code
    Signed-off-by: Randall Smith <ransmith@amd.com>

--- vllm/model_executor/models/qwen3_moe.py
+++ vllm/model_executor/models/qwen3_moe.py
@@ -302,11 +302,11 @@ class Qwen3MoeAttention(nn.Module):
         qkv, _ = self.qkv_proj(hidden_states)
         q, k, v = qkv.split([self.q_size, self.kv_size, self.kv_size], dim=-1)
         # Add qk-norm
-        q_by_head = q.reshape(-1, self.head_dim)
+        q_by_head = q.view(*q.shape[:-1], q.shape[-1] // self.head_dim, self.head_dim)
         q_by_head = self.q_norm(q_by_head)
         q = q_by_head.view(q.shape)
 
-        k_by_head = k.reshape(-1, self.head_dim)
+        k_by_head = k.view(*k.shape[:-1], k.shape[-1] // self.head_dim, self.head_dim)
         k_by_head = self.k_norm(k_by_head)
         k = k_by_head.view(k.shape)
         q, k = self.rotary_emb(positions, q, k)
