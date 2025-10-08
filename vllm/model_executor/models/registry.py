377603fd92f01235194cbebb1f903a14975262e7
Daniele <36171005+dtrifiro@users.noreply.github.com>
2025-10-08 12:18:32 +0200

    [Misc] add `usedforsecurity=False` in md5 hash call (#26357)
    Signed-off-by: Daniele Trifirò <dtrifiro@redhat.com>
    Signed-off-by: xuebwang-amd <xuebwang@amd.com>

--- vllm/model_executor/models/registry.py
+++ vllm/model_executor/models/registry.py
@@ -609,7 +609,7 @@ class _LazyRegisteredModel(_BaseRegisteredModel):
 
         if model_path.exists():
             with open(model_path, "rb") as f:
-                module_hash = hashlib.md5(f.read()).hexdigest()
+                module_hash = hashlib.md5(f.read(), usedforsecurity=False).hexdigest()
 
             mi = self._load_modelinfo_from_cache(module_hash)
             if mi is not None:
