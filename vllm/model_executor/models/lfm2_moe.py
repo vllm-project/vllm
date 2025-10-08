4ef54724192febd0b4bf9409c31229613d1bc8bf
Lukas Geiger <lukas.geiger94@gmail.com>
2025-10-08 21:48:33 +0100

    [Models] Improve iteration over layers (#26425)
    Signed-off-by: Lukas Geiger <lukas.geiger94@gmail.com>
    Signed-off-by: xuebwang-amd <xuebwang@amd.com>

--- vllm/model_executor/models/lfm2_moe.py
+++ vllm/model_executor/models/lfm2_moe.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 from collections.abc import Iterable
+from itertools import islice
 from typing import Any, Optional
 
 import torch
@@ -492,7 +493,7 @@ class Lfm2MoeModel(nn.Module):
             hidden_states = intermediate_tensors["hidden_states"]
             residual = intermediate_tensors["residual"]
 
-        for layer in self.layers[self.start_layer : self.end_layer]:
+        for layer in islice(self.layers, self.start_layer, self.end_layer):
             hidden_states, residual = layer(
                 positions=positions,
                 hidden_states=hidden_states,
--- vllm/model_executor/models/qwen3_vl.py
+++ vllm/model_executor/models/qwen3_vl.py
@@ -26,6 +26,7 @@
 
 from collections.abc import Iterable, Mapping, Sequence
 from functools import partial
+from itertools import islice
 from typing import Any, Callable, Optional, Union
 
 import numpy as np
@@ -1106,11 +1107,9 @@ class Qwen3LLMModel(Qwen3Model):
             assert intermediate_tensors is not None
             hidden_states = intermediate_tensors["hidden_states"]
             residual = intermediate_tensors["residual"]
-        for layer_idx, layer in enumerate(
-            self.layers[self.start_layer : self.end_layer]
+        for layer_idx, layer in islice(
+            enumerate(self.layers), self.start_layer, self.end_layer
         ):
-            layer_idx = layer_idx + self.start_layer
-
             hidden_states, residual = layer(
                 positions,
                 hidden_states,
