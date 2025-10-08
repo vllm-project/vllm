bc0f7c702ae6df34e07e7c9f2c4c92e76b24230b
Haipeng Li <li2haipeng@gmail.com>
2025-10-08 23:14:09 +0000

    using ENV to pass in config folder path

--- vllm/lora/ops/triton_ops/utils.py
+++ vllm/lora/ops/triton_ops/utils.py
@@ -7,6 +7,7 @@ import json
 import os
 from pathlib import Path
 from typing import Any, Dict, Optional
+from vllm import envs
 from vllm.logger import init_logger
 logger = init_logger(__name__)
 
@@ -141,34 +142,40 @@ def _get_lora_b_ptr(
     )
     return _LORA_B_PTR_DICT.get(key)
 
+
 @functools.lru_cache
 def load_v1_op_config(op_type: str,
                       add_inputs: Optional[bool]) -> Optional[Dict]:
-    gpu_name = torch.cuda.get_device_name()
-    gpu_name = gpu_name.replace(' ', '_')
-    gpu_name = gpu_name.replace('-', '_')
+    
+    user_defined_config_folder = envs.VLLM_TUNED_CONFIG_FOLDER
+    if user_defined_config_folder is not None:
+        gpu_name = torch.cuda.get_device_name()
+        gpu_name = gpu_name.replace(' ', '_')
+        gpu_name = gpu_name.replace('-', '_')
 
-    config_fname = None
-    if op_type == "shrink":
-        config_fname = f"{gpu_name}_{op_type.upper()}.json"
+        config_fname = None
+        if op_type == "shrink":
+            config_fname = f"{gpu_name}_{op_type.upper()}.json"
+        else:
+            assert op_type == "expand"
+            config_fname = (f"{gpu_name}_"
+                            f"{op_type.upper()}_"
+                            f"{str(add_inputs).upper()}.json")
+
+        config_path = Path(
+            f'{user_defined_config_folder}/{config_fname}'
+        )
+        if not config_path.exists():
+            logger.warning_once(f"No LoRA kernel config files found in the provided folder")
+            return None
+
+        # Load json
+        logger.info_once(f"Using tuned LoRA kernel configs from {config_path}.")
+        with open(str(config_path)) as f:
+            config_data = json.load(f)
     else:
-        assert op_type == "expand"
-        config_fname = (f"{gpu_name}_"
-                        f"{op_type.upper()}_"
-                        f"{str(add_inputs).upper()}.json")
-
-    config_path = Path(
-        f'{os.path.dirname(os.path.realpath(__file__))}/configs/{config_fname}'
-    )
-    if not config_path.exists():
-        logger.warning_once(f"Didn't find config path {config_path}, using default lora kernel configs")
-        return None
-
-    # Load json
-    logger.warning_once(f"Using LoRA configs from {config_path}.")
-    config_data = None
-    with open(str(config_path)) as f:
-        config_data = json.load(f)
+        config_data = None
+    
     return config_data
 
 
@@ -180,7 +187,6 @@ def get_v1_op_configs(
         hidden_size: int,
         rank: int,
         num_slices: int,
-        out_dim: Optional[int] = None,
         add_inputs: Optional[bool] = None) -> dict[str, Optional[int]]:
 
     assert op_type in ["shrink", "expand"]
@@ -208,17 +214,16 @@ def get_v1_op_configs(
             'num_stages': 2,
             'max_nreg': None
         }
-
     m = batch
-    if op_type == "shrink":
-        k, n = (hidden_size, rank)
-    else:
-        k, n = (rank, hidden_size)
+
+    k, n = (hidden_size, rank) if op_type == "shrink" else (rank, hidden_size)
 
     config_data: Any
     config_data = load_v1_op_config(op_type, add_inputs)
     if not config_data:
+        logger.warning_once(f"Using default LoRA kernel configs")
         return default
+    
     # config is structured as config_data[max_loras][num_slices][m][k][n] = {}
     # slice by max_loras
     config_data = config_data.get(str(max_loras)) or config_data[min(
