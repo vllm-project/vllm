955e2dc679e526d7a75ad513fae39c49c6066d0b
Siyuan Fu <siyuanf@nvidia.com>
2025-10-08 21:15:59 -0700

    update sampler.cu
    Signed-off-by: Siyuan Fu <siyuanf@nvidia.com>

--- vllm/model_executor/models/deepseek_v2.py
+++ vllm/model_executor/models/deepseek_v2.py
@@ -645,9 +645,12 @@ def sparse_attn_indexer(
             )
             num_rows = logits.shape[0]
             assert topk_tokens == 2048, "top_k_per_row assumes size 2048"
-            topk_indices = torch.empty(
-                num_rows, topk_tokens, dtype=torch.int32, device=logits.device
-            )
+            # topk_indices = torch.empty(
+            #     num_rows, topk_tokens, dtype=torch.int32, device=logits.device
+            # )
+            topk_indices = topk_indices_buffer[
+                chunk.token_start : chunk.token_end, :topk_tokens
+            ]
             topk_values = torch.empty(
                 num_rows, topk_tokens, dtype=logits.dtype, device=logits.device
             )
@@ -661,9 +664,6 @@ def sparse_attn_indexer(
                 logits.stride(0),
                 logits.stride(1),
             )
-            topk_indices_buffer[
-                chunk.token_start : chunk.token_end, : topk_indices.shape[-1]
-            ] = topk_indices.to(dtype=torch.int32)
 
     if has_decode:
         decode_metadata = attn_metadata.decode
@@ -697,29 +697,21 @@ def sparse_attn_indexer(
             decode_metadata.schedule_metadata,
             max_model_len=max_model_len,
         )
-        # padded query len
-        current_device = padded_q_fp8_decode_tokens.device
-        padded_num_tokens = batch_size * next_n
-        row_indices = torch.arange(padded_num_tokens, device=current_device) // next_n
-        next_n_offset = (
-            torch.arange(padded_num_tokens, device=padded_q_fp8_decode_tokens.device)
-            % next_n
-        )
-        index_end_pos = (
-            decode_metadata.seq_lens[row_indices] - next_n + next_n_offset + 1
-        ).unsqueeze(1)
         num_rows = logits.shape[0]
         assert topk_tokens == 2048, "top_k_per_row assumes size 2048"
-        topk_indices = torch.empty(
-            num_rows, topk_tokens, dtype=torch.int32, device=logits.device
-        )
+        # topk_indices = torch.empty(
+        #     num_rows, topk_tokens, dtype=torch.int32, device=logits.device
+        # )
+        topk_indices = topk_indices_buffer[:num_decode_tokens, :topk_tokens]
         topk_values = torch.empty(
             num_rows, topk_tokens, dtype=logits.dtype, device=logits.device
         )
-        torch.ops._C.top_k_per_row(
+
+        torch.ops._C.top_k_per_row_decode(
             logits,
-            torch.zeros(num_rows, dtype=torch.int32, device=logits.device),
-            index_end_pos.to(dtype=torch.int32, device=logits.device),
+            batch_size,
+            next_n,
+            decode_metadata.seq_lens,
             topk_indices,
             topk_values,
             num_rows,
@@ -733,9 +725,6 @@ def sparse_attn_indexer(
                 topk_indices.reshape(batch_size, -1, topk_indices.shape[-1]),
                 decode_lens,
             )
-        topk_indices_buffer[:num_decode_tokens, : topk_indices.shape[-1]] = (
-            topk_indices.to(dtype=torch.int32)
-        )
 
     return topk_indices_buffer
 
