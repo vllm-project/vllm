ca0f24686fbf5c4f58bcef9386c87c8049f9eb6b
Mercykid-bash <ruanche0218@gmail.com>
2025-10-09 16:46:38 +0800

    Update policy_factory.py
    Signed-off-by: Mercykid-bash <ruanche0218@gmail.com>

--- vllm/distributed/eplb/eplb_policy/policy_factory.py
+++ vllm/distributed/eplb/eplb_policy/policy_factory.py
@@ -3,22 +3,21 @@
 
 from .abstract_policy import EplbPolicy
 from .default_eplb_policy import DefaultEplb
+from vllm.config.parallel import EPLBPolicyType
 
 
 class PolicyFactory:
-
     @staticmethod
-    def generate_policy(policy_type: int) -> EplbPolicy:
+    def generate_policy(policy_type: EPLBPolicyType) -> EplbPolicy:
         """
         DefaultEplb: The rearrangement algorithm
         is adapted from [DeepSeek EPLB]
-
         Dynamic EPLB:  expert replacement with
         constrained number of expert shuffle
         """
-        policy: dict[int, type[EplbPolicy]] = {
-            0: DefaultEplb,
+        
+        policy: dict[EPLBPolicyType, type[EplbPolicy]] = {
+            EPLBPolicyType.DEFAULT_EPLB: DefaultEplb,
         }
-
         selected_policy = policy.get(policy_type, DefaultEplb)
         return selected_policy()
