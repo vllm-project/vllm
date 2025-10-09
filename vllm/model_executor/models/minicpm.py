67250ad5757cf14cf01b63208803006f98a65a35
bzantium <ryumin93@gmail.com>
2025-10-10 01:10:45 +0900

    apply precommit
    Signed-off-by: bzantium <ryumin93@gmail.com>

--- vllm/model_executor/models/minicpm.py
+++ vllm/model_executor/models/minicpm.py
@@ -389,13 +389,13 @@ class MiniCPMDecoderLayer(nn.Module):
 @support_torch_compile
 class MiniCPMModel(nn.Module):
     stacked_params_mapping = [
-            # (param_name, shard_name, shard_id)
-            ("qkv_proj", "q_proj", "q"),
-            ("qkv_proj", "k_proj", "k"),
-            ("qkv_proj", "v_proj", "v"),
-            ("gate_up_proj", "gate_proj", 0),
-            ("gate_up_proj", "up_proj", 1),
-        ]
+        # (param_name, shard_name, shard_id)
+        ("qkv_proj", "q_proj", "q"),
+        ("qkv_proj", "k_proj", "k"),
+        ("qkv_proj", "v_proj", "v"),
+        ("gate_up_proj", "gate_proj", 0),
+        ("gate_up_proj", "up_proj", 1),
+    ]
 
     def __init__(self, *, vllm_config: VllmConfig, prefix: str = ""):
         super().__init__()
