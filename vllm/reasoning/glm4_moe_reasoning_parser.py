0417bb433a434a1b95a5f1b1df11acfb59cb3e40
chaunceyjiang <chaunceyjiang@gmail.com>
2025-10-09 02:34:30 +0000

    [Frontend] Improve the performance of is_reasoning_end
    Signed-off-by: chaunceyjiang <chaunceyjiang@gmail.com>

--- vllm/reasoning/glm4_moe_reasoning_parser.py
+++ vllm/reasoning/glm4_moe_reasoning_parser.py
@@ -8,13 +8,13 @@ from transformers import PreTrainedTokenizerBase
 
 from vllm.entrypoints.openai.protocol import ChatCompletionRequest, DeltaMessage
 from vllm.logger import init_logger
-from vllm.reasoning import BaseThinkingReasoningParser, ReasoningParserManager
+from vllm.reasoning import ReasoningParser, ReasoningParserManager
 
 logger = init_logger(__name__)
 
 
 @ReasoningParserManager.register_module("glm45")
-class Glm4MoeModelReasoningParser(BaseThinkingReasoningParser):
+class Glm4MoeModelReasoningParser(ReasoningParser):
     """
     Reasoning parser for the Glm4MoeModel model.
 
@@ -25,15 +25,6 @@ class Glm4MoeModelReasoningParser(BaseThinkingReasoningParser):
     from the model's output.
     """
 
-    @property
-    def start_token(self) -> str:
-        """The token that starts reasoning content."""
-        return "<think>"
-
-    @property
-    def end_token(self) -> str:
-        """The token that ends reasoning content."""
-        return "</think>"
 
     def __init__(self, tokenizer: PreTrainedTokenizerBase, *args, **kwargs):
         super().__init__(tokenizer, *args, **kwargs)
