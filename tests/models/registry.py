8d2b8c0ff2ca042cbcb6a839e1bf5e50ea6bdca5
Shane A <shanea@allenai.org>
2025-10-10 09:43:15 -0700

    [Model] Add FlexOlmo model implementation (#24923)
    Signed-off-by: Shane A <shanea@allenai.org>

--- tests/models/registry.py
+++ tests/models/registry.py
@@ -250,6 +250,7 @@ _TEXT_GENERATION_EXAMPLE_MODELS = {
     "Fairseq2LlamaForCausalLM": _HfExamplesInfo("mgleize/fairseq2-dummy-Llama-3.2-1B"),
     "FalconForCausalLM": _HfExamplesInfo("tiiuae/falcon-7b"),
     "FalconH1ForCausalLM": _HfExamplesInfo("tiiuae/Falcon-H1-0.5B-Base"),
+    "FlexOlmoForCausalLM": _HfExamplesInfo("allenai/Flex-reddit-2x7B-1T"),
     "GemmaForCausalLM": _HfExamplesInfo("google/gemma-1.1-2b-it"),
     "Gemma2ForCausalLM": _HfExamplesInfo("google/gemma-2-9b"),
     "Gemma3ForCausalLM": _HfExamplesInfo("google/gemma-3-1b-it"),
