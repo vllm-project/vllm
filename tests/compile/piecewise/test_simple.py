473fe67e3727619229da0bbfec2c3a326ee0f1d0
Boyuan Feng <boyuan@meta.com>
2025-10-09 23:39:19 -0700

    fix test_simple_inductor_graph_partition (#26522)
    Signed-off-by: Boyuan Feng <boyuan@meta.com>
    Signed-off-by: xuebwang-amd <xuebwang@amd.com>

--- tests/compile/piecewise/test_simple.py
+++ tests/compile/piecewise/test_simple.py
@@ -143,9 +143,13 @@ def test_simple_piecewise_compile(use_inductor):
 
 @torch.inference_mode()
 @pytest.mark.parametrize("splitting_ops", [["silly.attention"], []])
-def test_simple_inductor_graph_partition(splitting_ops):
+def test_simple_inductor_graph_partition(splitting_ops, monkeypatch):
     if not is_torch_equal_or_newer("2.9.0.dev"):
         pytest.skip("inductor graph partition is only available in PyTorch 2.9+")
+
+    # disable compile cache so that we run separately for different splitting_ops
+    # and get the expected number of cudagraphs captured.
+    monkeypatch.setenv("VLLM_DISABLE_COMPILE_CACHE", "1")
 
     _run_simple_model(
         # Inductor graph partition automatically resets splitting_ops to an empty list
