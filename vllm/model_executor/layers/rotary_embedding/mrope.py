b6c23c18a517590a73d559463f1c48b0cce969b4
Roger Wang <hey@rogerw.io>
2025-10-11 03:23:43 +0000

    revert
    Signed-off-by: Roger Wang <hey@rogerw.io>

--- vllm/model_executor/layers/rotary_embedding/mrope.py
+++ vllm/model_executor/layers/rotary_embedding/mrope.py
@@ -1330,6 +1330,43 @@ class MRotaryEmbedding(RotaryEmbedding):
         return llm_positions, mrope_position_delta
 
     @staticmethod
+    def _get_llm_pos_ids_for_vision(
+        start_idx: int,
+        vision_idx: int,
+        spatial_merge_size: int,
+        t_index: list[int],
+        grid_hs: torch.Tensor,
+        grid_ws: torch.Tensor,
+    ) -> torch.Tensor:
+        llm_pos_ids_list = []
+        llm_grid_h = grid_hs[vision_idx] // spatial_merge_size
+        llm_grid_w = grid_ws[vision_idx] // spatial_merge_size
+        h_index = (
+            torch.arange(llm_grid_h)
+            .view(1, -1, 1)
+            .expand(len(t_index), -1, llm_grid_w)
+            .flatten()
+        )
+        w_index = (
+            torch.arange(llm_grid_w)
+            .view(1, 1, -1)
+            .expand(len(t_index), llm_grid_h, -1)
+            .flatten()
+        )
+        t_index_tensor = (
+            torch.Tensor(t_index)
+            .to(llm_grid_h.device)
+            .view(-1, 1)
+            .expand(-1, llm_grid_h * llm_grid_w)
+            .long()
+            .flatten()
+        )
+        _llm_pos_ids = torch.stack([t_index_tensor, h_index, w_index])
+        llm_pos_ids_list.append(_llm_pos_ids + start_idx)
+        llm_pos_ids = torch.cat(llm_pos_ids_list, dim=1)
+        return llm_pos_ids
+
+    @staticmethod
     def _split_list_into_ranges(lst: torch.Tensor, interval: int) -> list[list[int]]:
         ranges: list[list[int]] = [[] for _ in range((max(lst) // interval) + 1)]
         for num in lst:
