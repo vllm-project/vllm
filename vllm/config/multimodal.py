f6872f5b5e7cbc12178afe3be10ef96c13ab0df5
Anand Roy <86306690+andycandy@users.noreply.github.com>
2025-10-11 16:26:37 +0000

    ran pre-commit
    Signed-off-by: Anand Roy <86306690+andycandy@users.noreply.github.com>

--- vllm/config/multimodal.py
+++ vllm/config/multimodal.py
@@ -147,17 +147,17 @@ class MultiModalConfig:
             else:
                 value[k] = BaseDummyOptions(**v)
         return value
-    
+
     @model_validator(mode="after")
-    def _validate_shm_cache(self):  
-        if self.mm_processor_cache_type != "shm":  
-            default_size = self.model_fields["mm_shm_cache_max_object_size_mb"].default  
-            if self.mm_shm_cache_max_object_size_mb != default_size:  
-                raise ValueError(  
-                    "`mm_shm_cache_max_object_size_mb` can only be set when "  
-                    "`mm_processor_cache_type` is 'shm'."  
-                )  
-        return self  
+    def _validate_multimodal_config(self):
+        if self.mm_processor_cache_type != "shm":
+            default_size = self.model_fields["mm_shm_cache_max_object_size_mb"].default
+            if self.mm_shm_cache_max_object_size_mb != default_size:
+                raise ValueError(
+                    "`mm_shm_cache_max_object_size_mb` can only be set when "
+                    "`mm_processor_cache_type` is 'shm'."
+                )
+        return self
 
     def compute_hash(self) -> str:
         """
