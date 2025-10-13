a71ca8a68ad45cc6722a3c754c464a1398c1023f
Bill Nell <bnell@redhat.com>
2025-10-13 17:07:34 -0400

    remove some prints
    Signed-off-by: Bill Nell <bnell@redhat.com>

--- vllm/model_executor/layers/fused_moe/layer.py
+++ vllm/model_executor/layers/fused_moe/layer.py
@@ -248,7 +248,7 @@ class FusedMoEMethodBase(QuantizeMethodBase):
                 use_fp8=use_fp8,
             )
 
-            print(f"MAX NUM TOKENS = {moe.max_num_tokens}")
+            #print(f"MAX NUM TOKENS = {moe.max_num_tokens}")
 
             handle = all2all_manager.get_handle(all_to_all_args)
             prepare_finalize = DeepEPHybridPrepareAndFinalize(
