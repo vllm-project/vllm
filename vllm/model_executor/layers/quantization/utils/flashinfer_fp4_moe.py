34475aeb045db6460bc6e69a5aa4faa2a4dcf1e4
XiaobingSuper <xiaobingzhangupc@gmail.com>
2025-10-13 10:39:45 +0800

    support flashinfer_fp4 moe for 5090 gpu
    Signed-off-by: XiaobingSuper <xiaobingzhangupc@gmail.com>

--- vllm/model_executor/layers/quantization/utils/flashinfer_fp4_moe.py
+++ vllm/model_executor/layers/quantization/utils/flashinfer_fp4_moe.py
@@ -32,7 +32,7 @@ def is_flashinfer_fp4_cutlass_moe_available() -> bool:
         envs.VLLM_USE_FLASHINFER_MOE_FP4
         and has_flashinfer_cutlass_fused_moe()
         and current_platform.is_cuda()
-        and current_platform.is_device_capability(100)
+        and any(current_platform.is_device_capability(cap) for cap in [100, 120])
     )
 
 
