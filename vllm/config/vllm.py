0f8e420cda125c63041ade139953a654b24a930b
Morrison Turnansky <mturnans@redhat.com>
2025-10-13 16:09:38 -0400

    Update vllm/config/vllm.py
    Co-authored-by: Luka Govedič <ProExpertProg@users.noreply.github.com>
    Signed-off-by: Morrison Turnansky <mturnans@redhat.com>

--- vllm/config/vllm.py
+++ vllm/config/vllm.py
@@ -328,11 +328,7 @@ class VllmConfig:
 
         # If user does not set custom ops via none or all set it here based on
         # compilation level and backend.
-        if (
-            self.compilation_config.custom_ops.count("none")
-            + self.compilation_config.custom_ops.count("all")
-            == 0
-        ):
+        if none(s in self.compilation_config.custom_ops for s in ("all", "none")):
             if (
                 self.compilation_config.backend == "inductor"
                 and self.compilation_config.level > CompilationLevel.NO_COMPILATION
