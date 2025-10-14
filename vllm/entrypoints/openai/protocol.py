68c046afb26b0b0bb0529ef18450e77a1db1980a
zhaotyer <89376832+zhaotyer@users.noreply.github.com>
2025-10-14 09:04:21 +0000

    merge with main branch

--- vllm/entrypoints/openai/protocol.py
+++ vllm/entrypoints/openai/protocol.py
@@ -1503,7 +1503,7 @@ class EmbeddingCompletionRequest(OpenAIBaseModel):
     encoding_format: Literal["float", "base64"] = "float"
     dimensions: int | None = None
     user: str | None = None
-    truncate_prompt_tokens: Annotated[int, Field(ge=-1)] | None = None
+    truncate_prompt_tokens: Annotated[int, Field(ge=-1)] | None = -1
 
     # --8<-- [start:embedding-extra-params]
     add_special_tokens: bool = Field(
@@ -1557,7 +1557,7 @@ class EmbeddingChatRequest(OpenAIBaseModel):
     encoding_format: Literal["float", "base64"] = "float"
     dimensions: int | None = None
     user: str | None = None
-    truncate_prompt_tokens: Annotated[int, Field(ge=-1)] | None = None
+    truncate_prompt_tokens: Annotated[int, Field(ge=-1)] | None = -1
 
     # --8<-- [start:chat-embedding-extra-params]
     add_generation_prompt: bool = Field(
@@ -1705,7 +1705,7 @@ class ScoreRequest(OpenAIBaseModel):
     model: str | None = None
     text_1: list[str] | str | ScoreMultiModalParam
     text_2: list[str] | str | ScoreMultiModalParam
-    truncate_prompt_tokens: Annotated[int, Field(ge=-1)] | None = None
+    truncate_prompt_tokens: Annotated[int, Field(ge=-1)] | None = -1
 
     # --8<-- [start:score-extra-params]
 
@@ -1739,7 +1739,7 @@ class RerankRequest(OpenAIBaseModel):
     query: str | ScoreMultiModalParam
     documents: list[str] | ScoreMultiModalParam
     top_n: int = Field(default_factory=lambda: 0)
-    truncate_prompt_tokens: Annotated[int, Field(ge=-1)] | None = None
+    truncate_prompt_tokens: Annotated[int, Field(ge=-1)] | None = -1
 
     # --8<-- [start:rerank-extra-params]
 
