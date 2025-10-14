e7b961308bcf90e39c3e1721044bbedcede77bc6
Mercykid-bash <ruanche0218@gmail.com>
2025-10-14 10:58:30 +0800

    Update abstract_policy.py
    Signed-off-by: Mercykid-bash <ruanche0218@gmail.com>

--- vllm/distributed/eplb/eplb_policy/abstract_policy.py
+++ vllm/distributed/eplb/eplb_policy/abstract_policy.py
@@ -22,9 +22,9 @@ class EplbPolicy(ABC):
 
         Parameters:
             old_global_expert_indices: [num_moe_layers, num_physical_experts],
-            mapping from physical experts to logical experts.
-            weight: [layers, num_logical_experts],the load statistics
-            for all logical experts
+                mapping from physical experts to logical experts.
+            weight: [layers, num_logical_experts], the load statistics
+                for all logical experts
             num_replicas: number of physical experts, must be a multiple of
                 `num_ranks`
             num_groups: number of expert groups
@@ -33,10 +33,10 @@ class EplbPolicy(ABC):
 
         Returns:
             physical_to_logical_map: [layers, num_replicas], the expert
-             index of each replica
+                index of each replica
             logical_to_physical_map: [layers, num_logical_experts, X],
-             the replica indices for each expert
+                the replica indices for each expert
             expert_count: [layers, num_logical_experts], number of
-            physical replicas for each logical expert
+                physical replicas for each logical expert
         """
         pass
