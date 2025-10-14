77dc3324cece723f28aef1a52f79fd2606e83342
Luka Govedič <ProExpertProg@users.noreply.github.com>
2025-10-14 19:55:02 -0400

    [torch.compile] Fix tests for torch==2.9 inductor partition (#26116)
    Signed-off-by: ProExpertProg <lgovedic@redhat.com>
    Signed-off-by: Luka Govedič <lgovedic@redhat.com>
    Signed-off-by: 0xrushi <6279035+0xrushi@users.noreply.github.com>

--- vllm/attention/layer.py
+++ vllm/attention/layer.py
@@ -38,10 +38,6 @@ from vllm.utils import GiB_bytes, direct_register_custom_op
 
 logger = init_logger(__name__)
 USE_XFORMERS_OPS = None
-try:
-    tag_cudagraph_unsafe = (torch._C.Tag.cudagraph_unsafe,)
-except AttributeError:
-    tag_cudagraph_unsafe = ()  # type: ignore[assignment]
 
 
 def check_xformers_availability():
@@ -879,7 +875,6 @@ direct_register_custom_op(
     op_name="unified_attention",
     op_func=unified_attention,
     fake_impl=unified_attention_fake,
-    tags=tag_cudagraph_unsafe,
 )
 
 
@@ -931,7 +926,6 @@ direct_register_custom_op(
     op_func=unified_attention_with_output,
     mutates_args=["output", "output_block_scale"],
     fake_impl=unified_attention_with_output_fake,
-    tags=tag_cudagraph_unsafe,
 )
 
 
