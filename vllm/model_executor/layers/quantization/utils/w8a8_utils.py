056dc66568b2b230707b5803f185998f76ee882a
vllmellm <vllm.ellm@embeddedllm.com>
2025-10-15 12:20:41 +0000

    fix per token and per tensor gemm selection condition
    Signed-off-by: vllmellm <vllm.ellm@embeddedllm.com>

--- vllm/model_executor/layers/quantization/utils/w8a8_utils.py
+++ vllm/model_executor/layers/quantization/utils/w8a8_utils.py
@@ -464,8 +464,16 @@ class Fp8LinearOp:
         else:
             qinput, x_scale = input_2d, input_scale
 
-        per_tensor_weights = weight_scale.numel() == 1
-        per_tensor_activations = x_scale.numel() == 1
+        # Must have dim() conditions
+        # In per-token quant scenario, when the number of token is 1,
+        # the scale will only have 1 elements.
+        # Without checking the dim(),
+        # we cannot distingushes between per-tensor and per-token quant.
+        # Example:
+        # When the number of token is 1, per-token scale is [[1]]
+        # When per-tensor scale is [1] or ().
+        per_tensor_weights = (weight_scale.numel() == 1) and weight_scale.dim() < 2
+        per_tensor_activations = (x_scale.numel() == 1) and x_scale.dim() < 2
 
         # TODO(luka) do this dispatch during init (after ScaledMM refactor)
         w8a8_scaled_mm_func = dispatch_w8a8_scaled_mm(
