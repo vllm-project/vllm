f573c84f8324d5a32bf1ec116aa17013cb1bc35c
DarkLight1337 <tlleungac@connect.ust.hk>
2025-10-15 07:21:13 +0000

    [Misc] Remove `isort` and `yapf` ignores
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- vllm/model_executor/layers/quantization/compressed_tensors/schemes/__init__.py
+++ vllm/model_executor/layers/quantization/compressed_tensors/schemes/__init__.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
+from .compressed_tensors_24 import CompressedTensors24
 from .compressed_tensors_scheme import CompressedTensorsScheme
 from .compressed_tensors_w4a4_nvfp4 import CompressedTensorsW4A4Fp4
 from .compressed_tensors_w4a8_fp8 import CompressedTensorsW4A8Fp8
@@ -14,8 +15,6 @@ from .compressed_tensors_w8a8_fp8 import CompressedTensorsW8A8Fp8
 from .compressed_tensors_w8a8_int8 import CompressedTensorsW8A8Int8
 from .compressed_tensors_w8a16_fp8 import CompressedTensorsW8A16Fp8
 from .compressed_tensors_wNa16 import WNA16_SUPPORTED_BITS, CompressedTensorsWNA16
-
-from .compressed_tensors_24 import CompressedTensors24  # isort: skip
 
 __all__ = [
     "CompressedTensorsScheme",
