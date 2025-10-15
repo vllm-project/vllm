0c67e8cbb72a293205b4a9b146aa1fdee18dae3e
Bill Nell <bnell@redhat.com>
2025-10-15 15:01:58 -0400

    almost all working
    Signed-off-by: Bill Nell <bnell@redhat.com>

--- vllm/distributed/device_communicators/all2all.py
+++ vllm/distributed/device_communicators/all2all.py
@@ -332,10 +332,9 @@ class DeepEPHybridAll2AllManager(DeepEPAll2AllManagerBase):
     def _make_all2all_kwargs(self, **kwargs) -> dict[Any, Any]:
         extra_kwargs = dict(
             group=self.cpu_group,
-            num_sms_dispatch_api=32,
-            num_sms_combine_api=32,
-            num_sms_preprocessing_api=128,
-            nvlink_domain_size=2,  # hack for now. dp world_size
+            #num_sms_dispatch_api=32,
+            #num_sms_combine_api=32,
+            #num_sms_preprocessing_api=128,
         )
         return {**kwargs, **extra_kwargs}
 
@@ -345,7 +344,7 @@ class DeepEPHybridAll2AllManager(DeepEPAll2AllManagerBase):
         buffer_kwargs = self._make_all2all_kwargs(**kwargs)
         logger.debug("DeepEP Hybrid all2all args %s", buffer_kwargs)
         handle: deep_ep.Buffer = self.handle_cache.get_or_create(
-            buffer_kwargs, deep_ep.HybridEpBuffer
+            buffer_kwargs, deep_ep.HybridEPBuffer
         )
         return handle
 
