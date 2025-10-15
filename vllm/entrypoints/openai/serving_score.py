136a17fe6edacba292d899af4cfd424e0ca98d9f
Cyrus Leung <tlleungac@connect.ust.hk>
2025-10-15 21:03:58 +0800

    [Chore] Separate out `vllm.utils.func` (#26904)
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- vllm/entrypoints/openai/serving_score.py
+++ vllm/entrypoints/openai/serving_score.py
@@ -37,7 +37,8 @@ from vllm.logger import init_logger
 from vllm.lora.request import LoRARequest
 from vllm.outputs import PoolingRequestOutput, ScoringRequestOutput
 from vllm.transformers_utils.tokenizer import AnyTokenizer, MistralTokenizer
-from vllm.utils import make_async, merge_async_iterators
+from vllm.utils import merge_async_iterators
+from vllm.utils.func import make_async
 
 logger = init_logger(__name__)
 
