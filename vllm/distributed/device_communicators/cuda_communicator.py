25f8d59d344ba472fad316fc53bead99fd985d4d
HakJu Kim <hakju.kim@moreh.io>
2025-10-15 10:39:50 +0900

    applied pre-commit result this happened because of code conflict(semantically)
    Signed-off-by: HakJu Kim <hakju.kim@moreh.io>

--- vllm/distributed/device_communicators/cuda_communicator.py
+++ vllm/distributed/device_communicators/cuda_communicator.py
@@ -116,7 +116,7 @@ class CudaCommunicator(DeviceCommunicatorBase):
 
                 self.all2all_manager = FlashInferAllToAllManager(self.cpu_group)
                 logger.info("Using Flashinfer all2allv manager.")
-            elif all2all_backend == "mori":
+            elif self.all2all_backend == "mori":
                 from .all2all import MoriAll2AllManager
 
                 self.all2all_manager = MoriAll2AllManager(self.cpu_group)
