54e6544d58a99029020fec663eca22dd914670d9
Shixian Cui <shixian@amazon.com>
2025-10-15 18:24:23 +0000

    slight better L2 cache reuse by place split-k at first grid axis
    Signed-off-by: Shixian Cui <shixian@amazon.com>

--- vllm/model_executor/layers/fused_moe/fused_moe.py
+++ vllm/model_executor/layers/fused_moe/fused_moe.py
@@ -348,7 +348,7 @@ def fused_moe_kernel(
     # -----------------------------------------------------------
     # Map program ids `pid` to the block of C it should compute.
     # This is done in a grouped ordering to promote L2 data reuse.
-    pid = tl.program_id(axis=0)
+    pid = tl.program_id(axis=1)
     num_pid_m = tl.cdiv(EM, BLOCK_SIZE_M)
     num_pid_n = tl.cdiv(N, BLOCK_SIZE_N)
     num_pid_in_group = GROUP_SIZE_M * num_pid_n
@@ -357,7 +357,7 @@ def fused_moe_kernel(
     group_size_m = min(num_pid_m - first_pid_m, GROUP_SIZE_M)
     pid_m = first_pid_m + ((pid % num_pid_in_group) % group_size_m)
     pid_n = (pid % num_pid_in_group) // group_size_m
-    pid_k = tl.program_id(axis=1)
+    pid_k = tl.program_id(axis=0)
 
     # ----------------------------------------------------------
     # Create pointers for the first blocks of A and B.
@@ -631,8 +631,8 @@ def invoke_fused_moe_kernel(A: torch.Tensor,
             **config,
         )
     else:
-        grid = lambda META: (triton.cdiv(EM, META['BLOCK_SIZE_M']) * triton.cdiv(
-            B.size(1), META['BLOCK_SIZE_N']), META['SPLIT_K'])
+        grid = lambda META: (META['SPLIT_K'], triton.cdiv(EM, META['BLOCK_SIZE_M']) * triton.cdiv(
+            B.size(1), META['BLOCK_SIZE_N']))
         config = config.copy()
         BLOCK_SIZE_K = config.pop("BLOCK_SIZE_K")
         if block_shape is not None:
