ba94aa72a5eb86bb253d0e824e395bb841aa1689
DarkLight1337 <tlleungac@connect.ust.hk>
2025-10-15 08:30:44 +0000

    Fix circular import
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- vllm/model_executor/layers/quantization/compressed_tensors/schemes/__init__.py
+++ vllm/model_executor/layers/quantization/compressed_tensors/schemes/__init__.py
@@ -1,7 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
-from .compressed_tensors_24 import CompressedTensors24
 from .compressed_tensors_scheme import CompressedTensorsScheme
 from .compressed_tensors_w4a4_nvfp4 import CompressedTensorsW4A4Fp4
 from .compressed_tensors_w4a8_fp8 import CompressedTensorsW4A8Fp8
@@ -15,6 +14,9 @@ from .compressed_tensors_w8a8_fp8 import CompressedTensorsW8A8Fp8
 from .compressed_tensors_w8a8_int8 import CompressedTensorsW8A8Int8
 from .compressed_tensors_w8a16_fp8 import CompressedTensorsW8A16Fp8
 from .compressed_tensors_wNa16 import WNA16_SUPPORTED_BITS, CompressedTensorsWNA16
+
+# This avoids circular import error
+from .compressed_tensors_24 import CompressedTensors24  # isort: skip
 
 __all__ = [
     "CompressedTensorsScheme",
