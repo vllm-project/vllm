bd359cc96b7d36ae4bdfde6fcb7c717282bc5203
Sophie du Couédic <sop@zurich.ibm.com>
2025-10-15 19:59:30 +0200

    support infinite burstiness
    Signed-off-by: Sophie du Couédic <sop@zurich.ibm.com>

--- vllm/benchmarks/serve.py
+++ vllm/benchmarks/serve.py
@@ -189,6 +189,10 @@ async def get_request(
         request_rates.append(current_request_rate)
         if current_request_rate == float("inf"):
             delay_ts.append(0)
+        elif burstiness == float("inf"):
+            # when burstiness tends to infinity, the delay time becomes constant
+            # and tends to the inverse of the request rate
+            delay_ts.append(1.0 / current_request_rate)
         else:
             theta = 1.0 / (current_request_rate * burstiness)
 
