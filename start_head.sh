ca9f59b9140e3b5c516ec315aed24ecebe0a9847
Patrick von Platen <patrick.v.platen@gmail.com>
2025-10-16 10:40:03 +0000

    up
    Signed-off-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- start_head.sh
+++
@@ -1,3 +0,0 @@
-IP=$(hostname -I)
-echo $IP
-python3 start_ray.py --is_head --address ${IP} --ray_port 6379 --num_gpus 8 --num_cpus 20 --nnodes 4
--- start_worker.sh
+++
@@ -1,1 +0,0 @@
-python3 start_ray.py --address ${1} --ray_port 6379 --num_gpus 8 --num_cpus 20
