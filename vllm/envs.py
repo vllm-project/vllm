1317034379522f75b5876ff5b5136ef312c32b37
kliuae <17350011+kliuae@users.noreply.github.com>
2025-10-16 10:41:34 +0800

    [ROCm][FEAT] Fuse DeepSeek shared experts into AITER fused_moe ops (#24097)
    Signed-off-by: chenjun <junchen2@amd.com>
    Signed-off-by: kliuae <kuanfu.liu@embeddedllm.com>
    Co-authored-by: valarLip <103567126+valarLip@users.noreply.github.com>
    Co-authored-by: TJian <tunjian.tan@embeddedllm.com>

--- vllm/envs.py
+++ vllm/envs.py
@@ -113,6 +113,7 @@ if TYPE_CHECKING:
     VLLM_ROCM_USE_TRITON_ROPE: bool = False
     VLLM_ROCM_USE_AITER_FP8BMM: bool = True
     VLLM_ROCM_USE_AITER_UNIFIED_ATTENTION: bool = False
+    VLLM_ROCM_USE_AITER_FUSION_SHARED_EXPERTS: bool = True
     VLLM_ROCM_USE_SKINNY_GEMM: bool = True
     VLLM_ROCM_FP8_PADDING: bool = True
     VLLM_ROCM_MOE_PADDING: bool = True
@@ -912,6 +913,12 @@ environment_variables: dict[str, Callable[[], Any]] = {
     # Use AITER triton unified attention for V1 attention
     "VLLM_ROCM_USE_AITER_UNIFIED_ATTENTION": lambda: (
         os.getenv("VLLM_ROCM_USE_AITER_UNIFIED_ATTENTION", "False").lower()
+        in ("true", "1")
+    ),
+    # Whether to use aiter fusion shared experts ops.
+    # By default is enabled.
+    "VLLM_ROCM_USE_AITER_FUSION_SHARED_EXPERTS": lambda: (
+        os.getenv("VLLM_ROCM_USE_AITER_FUSION_SHARED_EXPERTS", "True").lower()
         in ("true", "1")
     ),
     # use rocm skinny gemms
