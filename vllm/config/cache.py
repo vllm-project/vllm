c95c171abe1e54452de7d662cc43cf773cc1fd3e
Mandy Li <mandy.j.li@intel.com>
2025-10-16 12:10:57 -0700

    Support block size of 256 used by Intel HPU (#26883)
    Signed-off-by: mandy-li <mandy.j.li@intel.com>
    Signed-off-by: Joachim Studnia <joachim@mistral.ai>

--- vllm/config/cache.py
+++ vllm/config/cache.py
@@ -19,7 +19,7 @@ else:
 
 logger = init_logger(__name__)
 
-BlockSize = Literal[1, 8, 16, 32, 64, 128]
+BlockSize = Literal[1, 8, 16, 32, 64, 128, 256]
 CacheDType = Literal["auto", "bfloat16", "fp8", "fp8_e4m3", "fp8_e5m2", "fp8_inc"]
 MambaDType = Literal["auto", "float32"]
 PrefixCachingHashAlgo = Literal["sha256", "sha256_cbor"]
