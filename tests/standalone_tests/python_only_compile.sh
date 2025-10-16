22f1d3ec70378b7faebdd28c810daf24704acf38
Huy Do <huydhn@gmail.com>
2025-10-15 20:59:09 -0700

    Fix ao issue and use official 2.9.0 wheels
    Signed-off-by: Huy Do <huydhn@gmail.com>

--- tests/standalone_tests/python_only_compile.sh
+++ tests/standalone_tests/python_only_compile.sh
@@ -18,9 +18,7 @@ apt autoremove -y
 
 echo 'import os; os.system("touch /tmp/changed.file")' >> vllm/__init__.py
 
-# TESTING, TO BE REMOVED
-VLLM_TEST_USE_PRECOMPILED_NIGHTLY_WHEEL=1 VLLM_USE_PRECOMPILED=1 pip3 install -vvv -e . \
-    --extra-index-url https://download.pytorch.org/whl/test/cu128
+VLLM_TEST_USE_PRECOMPILED_NIGHTLY_WHEEL=1 VLLM_USE_PRECOMPILED=1 pip3 install -vvv -e .
 
 # Run the script
 python3 -c 'import vllm'
