c549415ff5a96e14f04485fa113a9c9a7470c7ae
Akash Kaothalkar <akash.kaothalkar@ibm.com>
2025-10-15 23:37:40 -0500

    feat: remove forward_cpu
    Signed-off-by: Akash Kaothalkar <akash.kaothalkar@ibm.com>

--- vllm/v1/sample/ops/topk_topp_sampler.py
+++ vllm/v1/sample/ops/topk_topp_sampler.py
@@ -65,7 +65,8 @@ class TopKTopPSampler(nn.Module):
                 )
                 self.forward = self.forward_native
         elif current_platform.is_cpu():
-            if current_platform.get_cpu_architecture() == CpuArchEnum.RISCV:
+            arch = current_platform.get_cpu_architecture()
+            if arch in (CpuArchEnum.RISCV, CpuArchEnum.POWERPC):
                 self.forward = self.forward_native
             else:
                 self.forward = self.forward_cpu
