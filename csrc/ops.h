b50e6e7d2628d78d40a988d34aad84d02cecc115
Isotr0py <mozf@mail2.sysu.edu.cn>
2025-10-18 03:03:43 +0800

    [Chore] Remove unused `PolyNorm` layer (#27110)
    Signed-off-by: Isotr0py <mozf@mail2.sysu.edu.cn>
    Signed-off-by: Joachim Studnia <joachim@mistral.ai>

--- csrc/ops.h
+++ csrc/ops.h
@@ -92,9 +92,6 @@ void rms_norm(torch::Tensor& out, torch::Tensor& input, torch::Tensor& weight,
 void fused_add_rms_norm(torch::Tensor& input, torch::Tensor& residual,
                         torch::Tensor& weight, double epsilon);
 
-void poly_norm(torch::Tensor& out, torch::Tensor& input, torch::Tensor& weight,
-               torch::Tensor& bias, double epsilon);
-
 void apply_repetition_penalties_(torch::Tensor& logits,
                                  const torch::Tensor& prompt_mask,
                                  const torch::Tensor& output_mask,
