f20944e2d0b6e365efe93ccccc15ce8290e5a3ba
Lucas Wilkinson <lwilkins@redhat.com>
2025-10-17 16:30:23 +0000

    update
    Signed-off-by: Lucas Wilkinson <lwilkins@redhat.com>

--- cmake/external_projects/vllm_flash_attn.cmake
+++ cmake/external_projects/vllm_flash_attn.cmake
@@ -38,7 +38,7 @@ else()
   FetchContent_Declare(
           vllm-flash-attn
           GIT_REPOSITORY https://github.com/vllm-project/flash-attention.git
-          GIT_TAG 60fe2a75c5bcd79294649ccc1bd80b62eb87fabf
+          GIT_TAG b72d12210e49f9b8d9b70d495f5f203b678d114c
           GIT_PROGRESS TRUE
           # Don't share the vllm-flash-attn build between build types
           BINARY_DIR ${CMAKE_BINARY_DIR}/vllm-flash-attn
