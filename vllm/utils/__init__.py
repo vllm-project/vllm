eb1451bae9d96dd7548d391560189f7b0467b876
a798347923 <2645302020@qq.com>
2025-10-18 10:19:30 +0800

    add import torch.cuda

--- vllm/utils/__init__.py
+++ vllm/utils/__init__.py
@@ -1349,6 +1349,7 @@ def _cuda_device_count_stateless(cuda_visible_devices: str | None = None) -> int
     # https://github.com/pytorch/pytorch/blob/
     # c1cd946818442aca8c7f812b16d187ce1586c3bc/
     # torch/cuda/__init__.py#L831C1-L831C17
+    import torch.cuda
     import torch.version
 
     from vllm.platforms import current_platform
