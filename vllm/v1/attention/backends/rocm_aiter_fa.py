c37e4dce94712f108760a4ad75616004acafaa1c
Doug Lehr <douglehr@amd.com>
2025-10-21 14:36:54 -0500

    [ROCM] Add sinks argument to AiterFlashAttentionImpl
    AiterFlashAttentionImpl cannot handle sinks yet. However, we need to be able
    to be able to maintain the same attn signature, even if sinks = None

--- vllm/v1/attention/backends/rocm_aiter_fa.py
+++ vllm/v1/attention/backends/rocm_aiter_fa.py
@@ -417,6 +417,7 @@ class AiterFlashAttentionImpl(AttentionImpl):
         logits_soft_cap: float | None = None,
         attn_type: AttentionType = AttentionType.DECODER,
         kv_sharing_target_layer_name: int | None = None,
+        sinks: Optional[torch.Tensor] = None,
     ) -> None:
         self.num_heads = num_heads
         self.head_size = head_size
@@ -448,6 +449,8 @@ class AiterFlashAttentionImpl(AttentionImpl):
                 "are not implemented for "
                 "FlashAttentionImpl"
             )
+        if self.sinks is not None:
+            raise NotImplementedError("Sinks are not supported for AiterFlashAttentionImpl")
 
     def forward(
         self,
