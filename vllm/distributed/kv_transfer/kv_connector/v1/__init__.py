ac2e403d56321ed64be5471042a84bba438e99a7
Tyler Michael Smith <tyler@neuralmagic.com>
2025-10-21 19:30:47 -0400

    [P/D] KVConnector for decode benchmarking (#25986)
    Signed-off-by: Tyler Michael Smith <tlrmchlsmth@gmail.com>
    Signed-off-by: Tyler Michael Smith <tyler@neuralmagic.com>
    Signed-off-by: Joachim Studnia <joachim@mistral.ai>

--- vllm/distributed/kv_transfer/kv_connector/v1/__init__.py
+++ vllm/distributed/kv_transfer/kv_connector/v1/__init__.py
@@ -4,5 +4,8 @@ from vllm.distributed.kv_transfer.kv_connector.v1.base import (
     KVConnectorBase_V1,
     KVConnectorRole,
 )
+from vllm.distributed.kv_transfer.kv_connector.v1.decode_bench_connector import (  # noqa E:501
+    DecodeBenchConnector,
+)
 
-__all__ = ["KVConnectorRole", "KVConnectorBase_V1"]
+__all__ = ["KVConnectorRole", "KVConnectorBase_V1", "DecodeBenchConnector"]
