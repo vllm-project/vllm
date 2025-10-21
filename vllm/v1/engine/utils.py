af3dbb8d6f5a2ed8850001248108276d29d34698
w00689259 <wangzhuo66@huawei.com>
2025-10-21 20:25:23 +0800

    fault reporter bug fix
    Signed-off-by: w00689259 <wangzhuo66@huawei.com>

--- vllm/v1/engine/utils.py
+++ vllm/v1/engine/utils.py
@@ -1233,7 +1233,9 @@ class FaultHandler:
         for engine_index in engine_indexes:
             identity = self.client_cmd_registry.get(engine_index)
             serialized_instruction = serialize_method_call(instruction, **kwargs)
-            self.cmd_socket.send_multipart([identity, b"", serialized_instruction])
+            self.cmd_socket.send_multipart(
+                [identity, b"", serialized_instruction.encode("utf-8")]
+            )
 
         poller = zmq.Poller()
         poller.register(self.cmd_socket, zmq.POLLIN)
@@ -1253,10 +1255,10 @@ class FaultHandler:
                     return False
                 identity, _, response = parts
                 response_dict = json.loads(response.decode("utf-8"))
-                engine_id = response_dict.get("engine_id")
+                engine_id = response_dict.get("engine_index")
                 success = response_dict.get("success", False)
                 if engine_id in engine_indexes:
-                    engine_indexes.remove(engine_id)
+                    engine_indexes.remove(int(engine_id))
                 if not success:
                     logger.error(
                         "Engine %s reported failure: %s",
