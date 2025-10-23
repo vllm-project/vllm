adc7f738ecdc9d29c83753ef114ed08713c7df00
a798347923 <2645302020@qq.com>
2025-10-23 16:47:34 +0800

    change fault_report_port to internal_fault_report_port add external_fault_notify_port
    Signed-off-by: a798347923 <2645302020@qq.com>

--- vllm/v1/engine/utils.py
+++ vllm/v1/engine/utils.py
@@ -128,7 +128,7 @@ class CoreEngineProcManager:
             zmq_addr = get_engine_client_zmq_addr(
                 local_only=False,
                 host=vllm_config.parallel_config.data_parallel_master_ip,
-                port=vllm_config.fault_tolerance_config.fault_report_port,
+                port=vllm_config.fault_tolerance_config.internal_fault_report_port,
             )
             self.engine_down_socket = make_zmq_socket(
                 ctx=zmq_ctx,
@@ -873,7 +873,7 @@ def launch_core_engines(
         addresses.fault_report_addr = get_engine_client_zmq_addr(
             local_only=False,
             host=vllm_config.parallel_config.data_parallel_master_ip,
-            port=vllm_config.fault_tolerance_config.fault_report_port,
+            port=vllm_config.fault_tolerance_config.internal_fault_report_port,
         )
         addresses.client_cmd_addr = get_engine_client_zmq_addr(
             local_only=client_local_only, host=host
