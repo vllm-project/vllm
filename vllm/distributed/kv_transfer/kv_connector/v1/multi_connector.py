8e6013868b0c90c739a5f0934424c54aa221dc67
tovam <tovam@pliops.com>
2025-10-23 11:59:19 +0300

    Make reset_cache optionally return None for unimplemented connectors
    Signed-off-by: tovam <tovam@pliops.com>

--- vllm/distributed/kv_transfer/kv_connector/v1/multi_connector.py
+++ vllm/distributed/kv_transfer/kv_connector/v1/multi_connector.py
@@ -344,4 +344,4 @@ class MultiConnector(KVConnectorBase_V1):
         return stats_by_connector
 
     def reset_cache(self) -> bool:
-        return all(connector.reset_cache() for connector in self._connectors)
+        return all(c.reset_cache() is not False for c in self._connectors)
