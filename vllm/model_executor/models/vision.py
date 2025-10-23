fb5f3ff927c311ddd0dd31684ee05a161140f61c
Isotr0py <mozf@mail2.sysu.edu.cn>
2025-10-23 22:44:42 +0800

    add comment about the workaround
    Signed-off-by: Isotr0py <mozf@mail2.sysu.edu.cn>

--- vllm/model_executor/models/vision.py
+++ vllm/model_executor/models/vision.py
@@ -546,6 +546,13 @@ def get_llm_pos_ids_for_vision(
     return llm_pos_ids
 
 
+# Due to a performance regression with Conv3D in PyTorch2.9, we reshape
+# Conv3D weights to Linear weights for better performance.
+# See: https://github.com/vllm-project/vllm/issues/27406
+# and https://github.com/pytorch/pytorch/issues/166122
+# FIXME(Isotr0py): Revert the PR introduces this workaround
+# (https://github.com/vllm-project/vllm/pull/27418),
+# once the performance issue is resolved in PyTorch.
 def conv3d_to_linear_weight(conv3d_weight: torch.Tensor) -> torch.Tensor:
     """
     Reshape Conv3D weight to Linear weight. Only work when kernel_size==stride.
