f13275f31db6fbbf840a09719a07faeed80e8743
Varun Sundar Rabindranath <vsundarr@redhat.com>
2025-10-24 17:50:42 +0000

    better error message
    Signed-off-by: Varun Sundar Rabindranath <vsundarr@redhat.com>

--- vllm/model_executor/layers/quantization/mxfp4.py
+++ vllm/model_executor/layers/quantization/mxfp4.py
@@ -794,7 +794,8 @@ class Mxfp4MoEMethod(FusedMoEMethodBase):
                 )
             else:
                 raise NotImplementedError(
-                    "Incompatible Mxfp4 backend for EP batched experts format"
+                    f"Incompatible Mxfp4 backend ({self.mxfp4_backend}) for "
+                    "EP batched experts format"
                 )
         else:
             assert self.moe_quant_config is not None
@@ -817,7 +818,7 @@ class Mxfp4MoEMethod(FusedMoEMethodBase):
                 return OAITritonExperts(self.moe_quant_config)
             else:
                 raise NotImplementedError(
-                    "Incompatible Mxfp4 backend for EP batched experts format"
+                    f"Incompatible Mxfp4 backend ({self.mxfp4_backend}) for EP"
                 )
 
     def _route_and_experts(
