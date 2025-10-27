d65eca05d17121be95e4e04b7fc44bb03da9ddfd
KuntaiDu <kuntai@uchicago.edu>
2025-10-27 09:26:43 -0700

    turn off HMA connector when connector is set
    Signed-off-by: KuntaiDu <kuntai@uchicago.edu>

--- vllm/config/vllm.py
+++ vllm/config/vllm.py
@@ -569,6 +569,11 @@ class VllmConfig:
                 # Hybrid KV cache manager is not supported on non-GPU platforms.
                 self.scheduler_config.disable_hybrid_kv_cache_manager = True
             if self.kv_transfer_config is not None:
+                # NOTE(Kuntai): turn HMA off for connector for now.
+                # TODO(Kuntai): have a more elegent solution to selectively
+                # turn off HMA for connector.
+                self.scheduler_config.disable_hybrid_kv_cache_manager = True
+            if self.kv_transfer_config is not None:
                 # Hybrid KV cache manager is not compatible with KV transfer.
                 self.scheduler_config.disable_hybrid_kv_cache_manager = True
             if self.kv_events_config is not None:
