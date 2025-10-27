79584625c9c753c8acd8b69dee8d7e56ed6bda8d
Cyrus Leung <tlleungac@connect.ust.hk>
2025-10-27 23:30:38 +0800

    [Misc] Clean up more utils (#27567)
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- vllm/config/model.py
+++ vllm/config/model.py
@@ -1619,6 +1619,29 @@ class ModelConfig:
         return is_encoder_decoder(self.hf_config)
 
     @property
+    def uses_alibi(self) -> bool:
+        cfg = self.hf_text_config
+
+        return (
+            getattr(cfg, "alibi", False)  # Falcon
+            or "BloomForCausalLM" in self.architectures  # Bloom
+            or getattr(cfg, "position_encoding_type", "") == "alibi"  # codellm_1b_alibi
+            or (
+                hasattr(cfg, "attn_config")  # MPT
+                and (
+                    (
+                        isinstance(cfg.attn_config, dict)
+                        and cfg.attn_config.get("alibi", False)
+                    )
+                    or (
+                        not isinstance(cfg.attn_config, dict)
+                        and getattr(cfg.attn_config, "alibi", False)
+                    )
+                )
+            )
+        )
+
+    @property
     def uses_mrope(self) -> bool:
         return uses_mrope(self.hf_config)
 
