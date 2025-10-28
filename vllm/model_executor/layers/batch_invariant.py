b61985ed7195c00050b8b6d8a025bfe16d37cdda
Wentao Ye <44945378+yewentao256@users.noreply.github.com>
2025-10-28 16:51:35 -0400

    [Test] Batch Invariant: Unit test using parameterized backend (#27478)
    Signed-off-by: yewentao256 <zhyanwentao@126.com>

--- vllm/model_executor/layers/batch_invariant.py
+++ vllm/model_executor/layers/batch_invariant.py
@@ -753,13 +753,13 @@ def override_envs_for_invariance():
     curr_attn_backend = envs.VLLM_ATTENTION_BACKEND
     supported_backends = [
         "FLASH_ATTN",  # best supported backend
-        "FLEX_ATTENTION",
         "FLASHINFER",
         "FLASH_ATTN_MLA",
         "FLASHINFER_MLA",
         "TRITON_MLA",
         # Not yet supported MLA backends
         # "FLASHMLA",
+        # "FLEX_ATTENTION", # IMA issue even if we disable batch invariance
     ]
     if curr_attn_backend not in supported_backends:
         warning = (
