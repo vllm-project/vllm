fe133e5ba2e2b34410038808e6fcdd5a9decdd33
Alex-Brooks <Alex.Brooks@ibm.com>
2025-10-29 23:32:44 +0000

    fix adapter path
    Signed-off-by: Alex-Brooks <Alex.Brooks@ibm.com>

--- vllm/plugins/lora_resolvers/filesystem_resolver.py
+++ vllm/plugins/lora_resolvers/filesystem_resolver.py
@@ -17,19 +17,19 @@ class FilesystemResolver(LoRAResolver):
     ) -> LoRARequest | None:
         lora_path = os.path.join(self.lora_cache_dir, lora_name)
         maybe_lora_request = await self._get_lora_req_from_path(
-            lora_name, lora_path, base_model_name)
+            lora_name, lora_path, base_model_name
+        )
         return maybe_lora_request
 
     async def _get_lora_req_from_path(
-            self, lora_name: str, lora_path: str,
-            base_model_name: str) -> Optional[LoRARequest]:
+        self, lora_name: str, lora_path: str, base_model_name: str
+    ) -> None | LoRARequest:
         """Builds a LoraRequest pointing to the lora path if it's a valid
         LoRA adapter and has a matching base_model_name.
         """
         if os.path.exists(lora_path):
-            adapter_config_path = os.path.join(
-                self.lora_cache_dir, lora_name, "adapter_config.json"
-            )
+            adapter_config_path = os.path.join(lora_path, "adapter_config.json")
+
             if os.path.exists(adapter_config_path):
                 with open(adapter_config_path) as file:
                     adapter_config = json.load(file)
