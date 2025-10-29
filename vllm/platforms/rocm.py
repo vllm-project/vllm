0a51484c9428cb76aad23ad944a94183d0ac9c49
Xiake Sun <xisun@amd.com>
2025-10-29 22:44:03 +0800

    [ROCm][Platform] Add MI308X device id in _ROCM_DEVICE_ID_NAME_MAP (#27623)
    Signed-off-by: Xiake Sun <xiake.sun@amd.com>
    Signed-off-by: slava.sidik <slava.sidik@huawei.com>

--- vllm/platforms/rocm.py
+++ vllm/platforms/rocm.py
@@ -72,6 +72,7 @@ _ROCM_DEVICE_ID_NAME_MAP: dict[str, str] = {
     "0x74a0": "AMD_Instinct_MI300A",
     "0x74a1": "AMD_Instinct_MI300X",
     "0x74b5": "AMD_Instinct_MI300X",  # MI300X VF
+    "0x74a2": "AMD_Instinct_MI308X",
     "0x74a5": "AMD_Instinct_MI325X",
     "0x74b9": "AMD_Instinct_MI325X",  # MI325X VF
     "0x74a9": "AMD_Instinct_MI300X_HF",
