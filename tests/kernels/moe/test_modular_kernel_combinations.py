2d7abf97be6820c7d0de2840431f89ef22b23160
Bill Nell <bnell@redhat.com>
2025-10-29 14:47:22 -0400

    rebase + cleanups
    Signed-off-by: Bill Nell <bnell@redhat.com>

--- tests/kernels/moe/test_modular_kernel_combinations.py
+++ tests/kernels/moe/test_modular_kernel_combinations.py
@@ -91,7 +91,7 @@ def rank_worker(
     exceptions = []
     count = 0
 
-    torch.set_printoptions(profile="full")
+    torch.set_printoptions(profile="full")  # debugging
 
     for m, topk in product(Ms, TOPKs):
         # override m and topk
@@ -147,7 +147,13 @@ def run(config: Config):
 
     vllm_config, env_dict = config.make_env_data()
     parallel_launch_with_config(
-        config.world_size, rank_worker, vllm_config, env_dict, config, weights, config.verbose,
+        config.world_size,
+        rank_worker,
+        vllm_config,
+        env_dict,
+        config,
+        weights,
+        config.verbose,
         config.exit_first,
     )
 
--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -1185,8 +1185,10 @@ class GPUModelRunner(LoRAModelRunnerMixin, KVConnectorModelRunnerMixin):
         # running prefills. This lets us set enforce_eager on the prefiller in
         # a P/D setup and still use CUDA graphs (enabled by this padding) on the
         # decoder.
-        allow_dp_padding = (self.compilation_config.cudagraph_mode != CUDAGraphMode.NONE
-                            or envs.VLLM_ALL2ALL_BACKEND == "deepep_hybrid")
+        allow_dp_padding = (
+            self.compilation_config.cudagraph_mode != CUDAGraphMode.NONE
+            or self.parallel_config.all2all_backend == "deepep_hybrid"
+        )
 
         ubatch_slices, num_tokens_across_dp = coordinate_batch_across_dp(
             num_tokens_unpadded=num_tokens_unpadded,
@@ -3473,8 +3475,10 @@ class GPUModelRunner(LoRAModelRunnerMixin, KVConnectorModelRunnerMixin):
         num_sampled_tokens = np.ones(num_reqs, dtype=np.int32)
 
         # Disable DP padding when running eager
-        allow_dp_padding = (self.compilation_config.cudagraph_mode != CUDAGraphMode.NONE
-                            or envs.VLLM_ALL2ALL_BACKEND == "deepep_hybrid")
+        allow_dp_padding = (
+            self.compilation_config.cudagraph_mode != CUDAGraphMode.NONE
+            or self.parallel_config.all2all_backend == "deepep_hybrid"
+        )
 
         # We currently only microbatch if the number of tokens is
         # over a certain threshold.
