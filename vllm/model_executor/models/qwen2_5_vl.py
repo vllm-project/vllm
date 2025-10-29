1f221dd07417cf7207cfac5e3c10a90163048a39
Chenheli Hua <huachenheli@outlook.com>
2025-10-29 16:17:48 -0700

    [Temp fix] Disable torch.compile for Qwen2.5 VL's VisionBlock temporarily.  (#27760)
    Signed-off-by: Chenheli Hua <huachenheli@outlook.com>
    Signed-off-by: Roger Wang <hey@rogerw.io>
    Co-authored-by: Roger Wang <hey@rogerw.io>
    Signed-off-by: slava.sidik <slava.sidik@huawei.com>

--- vllm/model_executor/models/qwen2_5_vl.py
+++ vllm/model_executor/models/qwen2_5_vl.py
@@ -460,15 +460,17 @@ class Qwen2_5_VisionAttention(nn.Module):
         return output
 
 
-@support_torch_compile(
-    dynamic_arg_dims={
-        "x": 0,
-        "cu_seqlens": 0,
-        "rotary_pos_emb": 0,
-        "seqlens": 0,
-    },
-    mark_unbacked_dims={"seqlens": 0},
-)
+# (FIXME): Enable this after dynamic slicing is fixed
+# See https://github.com/vllm-project/vllm/pull/27760
+# @support_torch_compile(
+#     dynamic_arg_dims={
+#         "x": 0,
+#         "cu_seqlens": 0,
+#         "rotary_pos_emb": 0,
+#         "seqlens": 0,
+#     },
+#     mark_unbacked_dims={"seqlens": 0},
+# )
 class Qwen2_5_VisionBlock(nn.Module):
     def __init__(
         self,
