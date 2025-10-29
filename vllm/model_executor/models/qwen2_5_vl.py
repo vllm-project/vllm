023cc01982b253062ab8f34cc5442a4e41e00fb1
JartX <sagformas@epdcenter.es>
2025-10-29 17:55:35 +0100

    [FIXBUG] Qwen3VL hallucinations without Contiguous on Torch.SDPA (#27744)
    Signed-off-by: JartX <sagformas@epdcenter.es>
    Co-authored-by: Lukas Geiger <lukas.geiger94@gmail.com>

--- vllm/model_executor/models/qwen2_5_vl.py
+++ vllm/model_executor/models/qwen2_5_vl.py
@@ -428,6 +428,14 @@ class Qwen2_5_VisionAttention(nn.Module):
             )
         elif self.attn_backend == _Backend.TORCH_SDPA:
             # Execute attention entry by entry for speed & less VRAM.
+            from vllm.platforms import current_platform
+
+            # Never remove the next contiguous logic
+            # Without it, hallucinations occur with the backend
+            if current_platform.is_rocm():
+                q = q.contiguous()
+                k = k.contiguous()
+                v = v.contiguous()
             outputs = []
             for i in range(1, len(cu_seqlens)):
                 start_idx = cu_seqlens[i - 1]
