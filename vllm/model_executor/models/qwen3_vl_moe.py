7576d4fc9f080ae304eb0ade371c6dbb108f7eca
Lukas Geiger <lukas.geiger94@gmail.com>
2025-10-29 11:48:06 +0000

    [Model][Qwen3VL] Add `torch.compile` support for Qwen3VL
    Signed-off-by: Lukas Geiger <lukas.geiger94@gmail.com>

--- vllm/model_executor/models/qwen3_vl_moe.py
+++ vllm/model_executor/models/qwen3_vl_moe.py
@@ -365,6 +365,7 @@ class Qwen3VLMoeForConditionalGeneration(Qwen3VLForConditionalGeneration):
         multimodal_config = vllm_config.model_config.multimodal_config
 
         self.config = config
+        self.vllm_config = vllm_config
         self.multimodal_config = multimodal_config
         self.use_data_parallel = multimodal_config.mm_encoder_tp_mode == "data"
 
