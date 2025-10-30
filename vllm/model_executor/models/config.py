696dd7b236ad141b64859b7f30cb14054c450166
Guan Luo <41310872+GuanLuo@users.noreply.github.com>
2025-10-30 21:21:46 +0800

    fix: fix up
    Signed-off-by: Guan Luo <41310872+GuanLuo@users.noreply.github.com>

--- vllm/model_executor/models/config.py
+++ vllm/model_executor/models/config.py
@@ -334,7 +334,8 @@ class HybridAttentionMambaModelConfig(VerifyAndUpdateConfig):
             return
 
         # Save the user input before it gets modified by MambaModelConfig
-        mamba_block_size = vllm_config.cache_config.mamba_block_size
+        # NOTE(gluo): F841 is suppressed as mamba_block_size is used but ruff flags it
+        mamba_block_size = vllm_config.cache_config.mamba_block_size  # noqa: F841
         # Enable FULL_AND_PIECEWISE by default
         MambaModelConfig.verify_and_update_config(vllm_config)
 
