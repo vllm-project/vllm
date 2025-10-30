31a2ee3c38a1fae8c3e3d4455171f6a87ce262ac
NickLucche <nlucches@redhat.com>
2025-10-30 15:15:31 +0000

    refactor kv transfer methods into decorator
    Signed-off-by: NickLucche <nlucches@redhat.com>

---
+++ vllm/attention/utils/kv_transfer_utils.py
@@ -0,0 +1,55 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+from collections.abc import Callable
+from functools import wraps
+from typing import TYPE_CHECKING
+
+from vllm.distributed.kv_transfer import (
+    get_kv_transfer_group,
+    has_kv_transfer_group,
+    is_v1_kv_transfer_group,
+)
+from vllm.forward_context import ForwardContext, get_forward_context
+
+if TYPE_CHECKING:
+    from vllm.attention import Attention
+    from vllm.attention.layer import MLAAttention
+
+
+def maybe_transfer_kv_layer(func: Callable) -> Callable:
+    """Decorator that handles KV layer transfer prior and after execution of
+    an attention layer, if enabled. Otherwise, the wrapper is a no-op.
+
+    On entry: waits for the KV layer from the connector.
+    On exit: saves the KV layer to the connector.
+
+    """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if not has_kv_transfer_group() or not is_v1_kv_transfer_group():
+            return func(*args, **kwargs)
+
+        layer_name: str = kwargs["layer_name"]
+        forward_context: ForwardContext = get_forward_context()
+        attn_metadata = forward_context.attn_metadata
+        if attn_metadata is None:
+            return func(*args, **kwargs)
+        assert isinstance(attn_metadata, dict)
+        # Wait for KV layer on entry
+        connector = get_kv_transfer_group()
+        connector.wait_for_layer_load(layer_name)
+
+        # Execute the function
+        result = func(*args, **kwargs)
+
+        # Save KV cache layer on exit
+        attn_layer: Attention | MLAAttention = forward_context.no_compile_layers[
+            layer_name
+        ]
+        kv_cache = attn_layer.kv_cache[forward_context.virtual_engine]
+        connector.save_kv_layer(layer_name, kv_cache, attn_metadata[layer_name])
+
+        return result
+
+    return wrapper
