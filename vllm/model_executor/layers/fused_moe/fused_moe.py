17040999c42d66e398783dbbb65e546e98681f16
Roger Meier <r.meier@siemens.com>
2025-10-31 01:36:56 +0800

    [Model] Use the same fused_moe configs for all H200 devices (#23642)
    Signed-off-by: Roger Meier <r.meier@siemens.com>
    Signed-off-by: Joachim Studnia <joachim@mistral.ai>

--- vllm/model_executor/layers/fused_moe/fused_moe.py
+++ vllm/model_executor/layers/fused_moe/fused_moe.py
@@ -818,6 +818,9 @@ def get_config_file_name(
     E: int, N: int, dtype: str | None, block_shape: list[int] | None = None
 ) -> str:
     device_name = current_platform.get_device_name().replace(" ", "_")
+    # Set device_name to H200 if a device from the H200 family is detected
+    if "H200" in device_name:
+        device_name = "H200"
     dtype_selector = "" if not dtype else f",dtype={dtype}"
     block_shape_selector = (
         "" if not block_shape or not all(block_shape) else f",block_shape={block_shape}"
