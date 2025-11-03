cc8eb43b9f5a18d142e2ec0360521d52bcd1c267
CalebZ9909 <calebzhang540@gmail.com>
2025-11-03 05:41:33 +0000

    add ucclep integration

--- vllm/envs.py
+++ vllm/envs.py
@@ -167,6 +167,7 @@ if TYPE_CHECKING:
         "deepep_low_latency",
         "allgather_reducescatter",
         "flashinfer_all2allv",
+        "uccl_ep_low_latency",
     ] = "allgather_reducescatter"
     VLLM_MAX_TOKENS_PER_EXPERT_FP4_MOE: int = 163840
     VLLM_TOOL_PARSE_REGEX_TIMEOUT_SECONDS: int = 1
@@ -1197,6 +1198,7 @@ environment_variables: dict[str, Callable[[], Any]] = {
             "deepep_low_latency",
             "allgather_reducescatter",
             "flashinfer_all2allv",
+             "uccl_ep_low_latency",
         ],
     ),
     # Flashinfer MoE backend for vLLM's fused Mixture-of-Experts support.
