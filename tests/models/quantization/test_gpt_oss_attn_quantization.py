55b125922f2e5ab74d2e4b3636471b3297057ddd
xuebwang-amd <xuebwang@amd.com>
2025-11-03 08:56:04 +0000

    fix pre-commit issues and rename test function name
    Signed-off-by: xuebwang-amd <xuebwang@amd.com>

--- tests/models/quantization/test_gpt_oss_attn_quantization.py
+++ tests/models/quantization/test_gpt_oss_attn_quantization.py
@@ -9,8 +9,8 @@ Run `pytest tests/models/quantization/test_gpt_oss_attn_quantization.py`.
 import importlib
 import importlib.metadata
 from dataclasses import dataclass
-import huggingface_hub
 
+import huggingface_hub
 import lm_eval
 import pytest
 from packaging import version
@@ -21,6 +21,7 @@ QUARK_MXFP4_AVAILABLE = importlib.util.find_spec("quark") is not None and versio
     importlib.metadata.version("amd-quark")
 ) >= version.parse("0.8.99")
 
+
 def has_huggingface_access(repo):
     try:
         huggingface_hub.list_repo_refs(repo)
@@ -28,12 +29,17 @@ def has_huggingface_access(repo):
     except huggingface_hub.errors.RepositoryNotFoundError:
         return False
 
-HF_HUB_AMD_ORG_ACCESS = all([has_huggingface_access(model_name) for model_name in MODEL_NAMES])
+
+HF_HUB_AMD_ORG_ACCESS = all(
+    [has_huggingface_access(model_name) for model_name in MODEL_NAMES]
+)
+
 
 @dataclass
 class ModelCase:
     model_id: str
     tp: int
+
 
 @dataclass
 class EvaluationConfig:
@@ -45,15 +51,20 @@ class EvaluationConfig:
             "tensor_parallel_size=4,dtype=auto,gpu_memory_utilization=0.9,trust_remote_code=False"
         )
 
+
 EXPECTED_ACCURACIES = {"arc_challenge": 0.20}
+
 
 @pytest.mark.skipif(not QUARK_MXFP4_AVAILABLE, reason="amd-quark>=0.9 is not available")
 @pytest.mark.skipif(
     not HF_HUB_AMD_ORG_ACCESS,
-    reason="Read access to huggingface.co/amd is required for this test.")
+    reason="Read access to huggingface.co/amd is required for this test.",
+)
 @pytest.mark.parametrize("model_name", MODEL_NAMES)
 @pytest.mark.parametrize("task_name, expected_accuracy", EXPECTED_ACCURACIES.items())
-def test_mixed_precision_model_accuracies(model_name: str, task_name: str, expected_accuracy: float):
+def test_gpt_oss_attention_quantization(
+    model_name: str, task_name: str, expected_accuracy: float
+):
     measured_accuracy = lm_eval.simple_evaluate(
         model="vllm",
         model_args=EvaluationConfig(model_name).get_model_args(),
@@ -63,6 +74,6 @@ def test_mixed_precision_model_accuracies(model_name: str, task_name: str, expec
 
     rtol = 0.05
     assert (
-            measured_accuracy - rtol < expected_accuracy
-            and measured_accuracy + rtol > expected_accuracy
-        ), f"Expected: {expected_accuracy} |  Measured: {measured_accuracy}"
+        measured_accuracy - rtol < expected_accuracy
+        and measured_accuracy + rtol > expected_accuracy
+    ), f"Expected: {expected_accuracy} |  Measured: {measured_accuracy}"
