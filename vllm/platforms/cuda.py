6f3f128fb173227af5c4737750d164cc220c2981
chiangzhang <chiangzhang@tencent.com>
2025-11-04 19:34:23 +0800

    Make better use of the Decoupled Kernel Block Size
    PR 24486 implemented this mechanism but did not fully utilize it.
    The block_size restriction during startup (when using operators such as FlashMla) has now been lifted.
    
    Signed-off-by: chiangzhang <chiangzhang@tencent.com>

--- vllm/platforms/cuda.py
+++ vllm/platforms/cuda.py
@@ -201,18 +201,23 @@ class CudaPlatformBase(Platform):
 
             from vllm.attention.ops.flashmla import is_flashmla_dense_supported
 
+            def round_up_block_size(size):
+                cache_config.block_size = (
+                    (cache_config.block_size + size - 1) // size * size
+                )
+
             if (
                 use_flashmla
                 and is_flashmla_dense_supported()[0]
                 and cache_config.block_size % 64 != 0
             ):
-                cache_config.block_size = 64
-                logger.info("Forcing kv cache block size to 64 for FlashMLA backend.")
+                round_up_block_size(64)
+                logger.info("Forcing kv cache block size to 64*N for FlashMLA backend.")
 
             if use_cutlass_mla and cache_config.block_size % 128 != 0:
-                cache_config.block_size = 128
+                round_up_block_size(128)
                 logger.info(
-                    "Forcing kv cache block size to 128 for CUTLASS_MLA backend."
+                    "Forcing kv cache block size to 128*N for CUTLASS_MLA backend."
                 )
 
             if (
@@ -220,16 +225,16 @@ class CudaPlatformBase(Platform):
                 and cache_config.block_size != 32
                 and cache_config.block_size % 64 != 0
             ):
-                cache_config.block_size = 64
+                round_up_block_size(64)
                 logger.info(
-                    "Forcing kv cache block size to 64 for FlashInferMLA backend."
+                    "Forcing kv cache block size to 64*N for FlashInferMLA backend."
                 )
 
             # TODO(Chen): remove this hacky code
-            if use_sparse and cache_config.block_size != 64:
-                cache_config.block_size = 64
+            if use_sparse and cache_config.block_size % 64 != 0:
+                round_up_block_size(64)
                 logger.info(
-                    "Forcing kv cache block size to 64 for FlashMLASparse backend."
+                    "Forcing kv cache block size to 64*N for FlashMLASparse backend."
                 )
         # lazy import to avoid circular import
         from vllm.config import CUDAGraphMode
@@ -359,7 +364,7 @@ class CudaPlatformBase(Platform):
                     head_size,
                     dtype,
                     kv_cache_dtype,
-                    None,
+                    block_size,
                     use_mla,
                     has_sink,
                     use_sparse,
@@ -383,7 +388,7 @@ class CudaPlatformBase(Platform):
             head_size,
             dtype,
             kv_cache_dtype,
-            None,
+            block_size,
             use_mla,
             has_sink,
             use_sparse,
