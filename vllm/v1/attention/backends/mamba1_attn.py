cbca9f4971e6d181f1917aacfb6a7c9a6904ada1
asafg <39553475+Josephasafg@users.noreply.github.com>
2025-11-05 15:36:33 +0200

    Removed redudnant assertion
    Signed-off-by: asafg <39553475+Josephasafg@users.noreply.github.com>

--- vllm/v1/attention/backends/mamba1_attn.py
+++ vllm/v1/attention/backends/mamba1_attn.py
@@ -3,16 +3,12 @@
 
 from dataclasses import dataclass
 
-import torch
-
 from vllm.attention.backends.abstract import AttentionBackend
-from vllm.config import VllmConfig
 from vllm.v1.attention.backends.mamba_attn import (
     BaseMambaAttentionMetadata,
     BaseMambaAttentionMetadataBuilder,
 )
 from vllm.v1.attention.backends.utils import CommonAttentionMetadata
-from vllm.v1.kv_cache_interface import AttentionSpec, MambaSpec
 
 
 class Mamba1AttentionBackend(AttentionBackend):
@@ -30,16 +26,6 @@ class Mamba1AttentionMetadataBuilder(
     BaseMambaAttentionMetadataBuilder[Mamba1AttentionMetadata]
 ):
     metadata_cls = Mamba1AttentionMetadata
-
-    def __init__(
-        self,
-        kv_cache_spec: AttentionSpec,
-        layer_names: list[str],
-        vllm_config: VllmConfig,
-        device: torch.device,
-    ):
-        super().__init__(kv_cache_spec, layer_names, vllm_config, device)
-        assert isinstance(kv_cache_spec, MambaSpec)
 
     def build(
         self,
