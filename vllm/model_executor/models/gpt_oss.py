83e733475eb8808cf1d8dbda2249e4f5a55b7529
xuebwang-amd <xuebwang@amd.com>
2025-11-05 13:09:42 +0000

    make kv cache name loading more robust
    Signed-off-by: xuebwang-amd <xuebwang@amd.com>

--- vllm/model_executor/models/gpt_oss.py
+++ vllm/model_executor/models/gpt_oss.py
@@ -30,7 +30,10 @@ from vllm.model_executor.layers.vocab_parallel_embedding import (
     ParallelLMHead,
     VocabParallelEmbedding,
 )
-from vllm.model_executor.model_loader.weight_utils import default_weight_loader
+from vllm.model_executor.model_loader.weight_utils import (
+    default_weight_loader,
+    maybe_remap_kv_scale_name,
+)
 from vllm.model_executor.models.utils import sequence_parallel_chunk
 from vllm.platforms import current_platform
 from vllm.sequence import IntermediateTensors
@@ -539,6 +542,12 @@ class GptOssModel(nn.Module):
                 weight_loader(param, weight.flatten()[0])
                 loaded_params.add(scale_name)
                 continue
+
+            if "scale" in name:
+                # Remapping the name of FP8 kv-scale.
+                name = maybe_remap_kv_scale_name(name, params_dict)
+                if name is None:
+                    continue
 
             if ".w13_weight" in name:
                 # Handle MLP gate and up projection weights
