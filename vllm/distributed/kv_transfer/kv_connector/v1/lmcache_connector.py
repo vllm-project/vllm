17631944d436517da196fd8e19fcb7b782a551c6
Martin Hickey <martin.hickey@ie.ibm.com>
2025-11-07 17:46:16 +0000

    Remove change to the LMCache connector
    The changes to the connector is for a separate PR and this
    PR is independent of it for now.
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>

--- vllm/distributed/kv_transfer/kv_connector/v1/lmcache_connector.py
+++ vllm/distributed/kv_transfer/kv_connector/v1/lmcache_connector.py
@@ -1,8 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
-import time
-from collections.abc import Iterable
-from typing import TYPE_CHECKING, Any, Optional
+from typing import TYPE_CHECKING, Any
 
 import torch
 from lmcache.integration.vllm.vllm_v1_adapter import (
@@ -10,7 +8,6 @@ from lmcache.integration.vllm.vllm_v1_adapter import (
 )
 
 from vllm.config import VllmConfig
-from vllm.distributed.kv_events import BlockStored, KVCacheEvent, KVEventBatch
 from vllm.distributed.kv_transfer.kv_connector.v1.base import (
     KVConnectorBase_V1,
     KVConnectorMetadata,
@@ -18,7 +15,6 @@ from vllm.distributed.kv_transfer.kv_connector.v1.base import (
 )
 from vllm.logger import init_logger
 from vllm.v1.core.sched.output import SchedulerOutput
-from vllm.v1.outputs import KVConnectorOutput
 
 if TYPE_CHECKING:
     from vllm.attention.backends.abstract import AttentionMetadata
@@ -57,8 +53,6 @@ class LMCacheConnectorV1(KVConnectorBase_V1):
             cls = LMCacheConnectorLatestImpl
 
         self._lmcache_engine = cls(vllm_config, role, self)
-
-        self._kv_events: list[KVCacheEvent] = []
 
     # ==============================
     # Worker-side methods
@@ -142,30 +136,6 @@ class LMCacheConnectorV1(KVConnectorBase_V1):
         """
         return self._lmcache_engine.get_finished(finished_req_ids)
 
-    def get_kv_connector_kv_cache_events(self) -> Optional["KVEventBatch"]:
-        """
-        Get the KV connector kv cache events collected during the last interval.
-        """
-        events = self._lmcache_engine.get_kv_events()
-        if not events:
-            return None
-
-        lmcache_kv_events: KVEventBatch | None = None
-        for event in events:
-            if lmcache_kv_events is None:
-                lmcache_kv_events = KVEventBatch(ts=time.time(), events=[])
-            block = BlockStored(
-                block_hashes=event.block_hashes,
-                parent_block_hash=event.parent_block_hash,
-                token_ids=event.token_ids,
-                lora_id=event.lora_id,
-                block_size=event.block_size,
-                medium=event.medium,
-            )
-            lmcache_kv_events.events.append(block)
-
-        return lmcache_kv_events
-
     # ==============================
     # Scheduler-side methods
     # ==============================
@@ -213,25 +183,6 @@ class LMCacheConnectorV1(KVConnectorBase_V1):
         """
         return self._lmcache_engine.build_connector_meta(scheduler_output)
 
-    def update_connector_output(self, connector_output: KVConnectorOutput):
-        """
-        Update KVConnector state from worker-side connectors output.
-
-        Args:
-            connector_output (KVConnectorOutput): the worker-side
-                connectors output.
-        """
-        # Get the KV events
-        kv_events = connector_output.kv_cache_events
-        if (
-            not kv_events
-            or not isinstance(kv_events, KVEventBatch)
-            or not kv_events.events
-        ):
-            return
-        self._kv_events.extend(kv_events.events)
-        return
-
     def request_finished(
         self,
         request: "Request",
@@ -248,14 +199,3 @@ class LMCacheConnectorV1(KVConnectorBase_V1):
             returned by the engine.
         """
         return self._lmcache_engine.request_finished(request, block_ids)
-
-    def take_events(self) -> Iterable["KVCacheEvent"]:
-        """
-        Take the KV cache events from the connector.
-
-        Yields:
-            New KV cache events since the last call.
-        """
-        if self._kv_events is not None:
-            yield from self._kv_events
-            self._kv_events.clear()
