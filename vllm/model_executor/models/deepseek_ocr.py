5c29831d7df15a4a4752f639a0002a5b4b432536
Isotr0py <mozf@mail2.sysu.edu.cn>
2025-11-08 13:01:27 +0800

    [Model] Consolidate Deepseek-MoE implementation with DeepSeek-v2 (#28101)
    Signed-off-by: Kunshang Ji <kunshang.ji@intel.com>
    Signed-off-by: Isotr0py <mozf@mail2.sysu.edu.cn>
    Co-authored-by: Kunshang Ji <kunshang.ji@intel.com>
    Signed-off-by: slava.sidik <slava.sidik@huawei.com>

--- vllm/model_executor/models/deepseek_ocr.py
+++ vllm/model_executor/models/deepseek_ocr.py
@@ -417,18 +417,10 @@ class DeepseekOCRForCausalLM(nn.Module, SupportsMultiModal, SupportsPP):
                 f"Only 2D tile_tag is supported currently, got: {self.tile_tag}"
             )
 
-        if self.text_config.topk_method == "noaux_tc":
-            architectures = ["DeepseekV3ForCausalLM"]
-        elif not self.text_config.use_mla:
-            architectures = ["DeepseekForCausalLM"]
-        else:
-            architectures = ["DeepseekV2ForCausalLM"]
-
         self.language_model = init_vllm_registered_model(
             vllm_config=vllm_config,
             hf_config=self.text_config,
             prefix=maybe_prefix(prefix, "language_model"),
-            architectures=architectures,
         )
 
         self.make_empty_intermediate_tensors = (
