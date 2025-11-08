e0360c1c92c001b6f3d9f0d4de5d90d89ac6d842
Michael Goin <mgoin64@gmail.com>
2025-11-08 10:20:55 +0800

    [Perf][DeepSeek] Add sigmoid+bias fusion to fused_grouped_topk from TRTLLM (#28124)
    Signed-off-by: mgoin <mgoin64@gmail.com>
    Co-authored-by: Robert Shaw <114415538+robertgshaw2-redhat@users.noreply.github.com>
    Co-authored-by: Wentao Ye <44945378+yewentao256@users.noreply.github.com>
    Signed-off-by: Joachim Studnia <joachim@mistral.ai>

--- csrc/moe/torch_bindings.cpp
+++ csrc/moe/torch_bindings.cpp
@@ -107,9 +107,10 @@ TORCH_LIBRARY_EXPAND(TORCH_EXTENSION_NAME, m) {
 
   // Apply grouped topk routing to select experts.
   m.def(
-      "grouped_topk(Tensor scores, Tensor scores_with_bias, int n_group, int "
+      "grouped_topk(Tensor scores, int n_group, int "
       "topk_group, int topk, bool renormalize, float "
-      "routed_scaling_factor) -> (Tensor, Tensor)");
+      "routed_scaling_factor, Tensor bias, int scoring_func) -> (Tensor, "
+      "Tensor)");
   m.impl("grouped_topk", torch::kCUDA, &grouped_topk);
 #endif
 }
