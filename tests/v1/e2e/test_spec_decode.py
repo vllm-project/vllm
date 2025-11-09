59b8f33dc57a6242b769241b604f37d47113810f
Nicolò Lucchesi <nlucches@redhat.com>
2025-11-09 17:04:59 +0100

    [CI] Fix flaky `test_eagle_correctness` test (#28364)
    Signed-off-by: NickLucche <nlucches@redhat.com>
    Signed-off-by: Joachim Studnia <joachim@mistral.ai>

--- tests/v1/e2e/test_spec_decode.py
+++ tests/v1/e2e/test_spec_decode.py
@@ -433,9 +433,9 @@ def test_eagle_correctness(
                 print(f"ref_output: {ref_output.outputs[0].text}")
                 print(f"spec_output: {spec_output.outputs[0].text}")
 
-        # Heuristic: expect at least 66% of the prompts to match exactly
+        # Heuristic: expect at least 60% of the prompts to match exactly
         # Upon failure, inspect the outputs to check for inaccuracy.
-        assert matches > int(0.66 * len(ref_outputs))
+        assert matches > int(0.6 * len(ref_outputs))
         del spec_llm
         torch.cuda.empty_cache()
         cleanup_dist_env_and_memory()
