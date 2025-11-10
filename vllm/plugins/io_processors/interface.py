e01032fee78c9b6cc7c557ece9413c4c68780206
Alex-Brooks <Alex.Brooks@ibm.com>
2025-11-10 05:49:44 +0000

    allow input / output only io plugins
    Signed-off-by: Alex-Brooks <Alex.Brooks@ibm.com>

--- vllm/plugins/io_processors/interface.py
+++ vllm/plugins/io_processors/interface.py
@@ -1,8 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
-from abc import ABC, abstractmethod
+from abc import ABC
 from collections.abc import AsyncGenerator, Sequence
+from enum import Enum
 from typing import Any, Generic, TypeVar
 
 from vllm.config import VllmConfig
@@ -16,18 +17,31 @@ IOProcessorInput = TypeVar("IOProcessorInput")
 IOProcessorOutput = TypeVar("IOProcessorOutput")
 
 
+class IOProcessorPluginType(Enum):
+    INPUT_OUTPUT = 1
+    INPUT_ONLY = 2
+    OUTPUT_ONLY = 3
+
+
 class IOProcessor(ABC, Generic[IOProcessorInput, IOProcessorOutput]):
+    plugin_type = IOProcessorPluginType.INPUT_OUTPUT
+
     def __init__(self, vllm_config: VllmConfig):
         self.vllm_config = vllm_config
 
-    @abstractmethod
     def pre_process(
         self,
         prompt: IOProcessorInput,
         request_id: str | None = None,
         **kwargs,
     ) -> PromptType | Sequence[PromptType]:
-        raise NotImplementedError
+        if self.plugin_type != IOProcessorPluginType.OUTPUT_ONLY:
+            raise NotImplementedError(
+                ".pre_process() is not implemented for this IO plugin"
+            )
+        raise AssertionError(
+            ".pre_process should not be invoked for output only plugins"
+        )
 
     async def pre_process_async(
         self,
@@ -37,14 +51,19 @@ class IOProcessor(ABC, Generic[IOProcessorInput, IOProcessorOutput]):
     ) -> PromptType | Sequence[PromptType]:
         return self.pre_process(prompt, request_id, **kwargs)
 
-    @abstractmethod
     def post_process(
         self,
         model_output: Sequence[PoolingRequestOutput],
         request_id: str | None = None,
         **kwargs,
     ) -> IOProcessorOutput:
-        raise NotImplementedError
+        if self.plugin_type != IOProcessorPluginType.INPUT_ONLY:
+            raise NotImplementedError(
+                ".post_process() is not implemented for this IO plugin"
+            )
+        raise AssertionError(
+            ".post_process() should not be invoked for input only plugins"
+        )
 
     async def post_process_async(
         self,
@@ -61,24 +80,34 @@ class IOProcessor(ABC, Generic[IOProcessorInput, IOProcessorOutput]):
         collected_output = [output[1] for output in sorted_output]
         return self.post_process(collected_output, request_id, **kwargs)
 
-    @abstractmethod
     def parse_request(
         self,
         request: Any,
         has_preprocess_partial: bool = False,
     ) -> IOProcessorInput:
-        raise NotImplementedError
+        if self.plugin_type != IOProcessorPluginType.OUTPUT_ONLY:
+            raise NotImplementedError(
+                ".parse_request() is not implemented for this IO plugin"
+            )
+        raise AssertionError(
+            ".parse_request() should not be invoked for output only plugins"
+        )
 
     def validate_or_generate_params(
         self, params: SamplingParams | PoolingParams | None = None
     ) -> SamplingParams | PoolingParams:
         return params or PoolingParams()
 
-    @abstractmethod
     def output_to_response(
         self, plugin_output: IOProcessorOutput
     ) -> IOProcessorResponse:
-        raise NotImplementedError
+        if self.plugin_type != IOProcessorPluginType.INPUT_ONLY:
+            raise NotImplementedError(
+                ".output_to_response() is not implemented for this IO plugin"
+            )
+        raise AssertionError(
+            ".output_to_response() should not be invoked for input only plugins"
+        )
 
     def get_modified_lora_request(self, engine_prompts, lora_request):
         # FIXME - clean this up, unify sync/async
