ba5af5b99ce5ffef817487f6d10a7816d94fe0f0
mgoin <mgoin64@gmail.com>
2025-11-10 22:32:30 +0000

    Lazy import aiter rocm in fp8_utils.py
    Signed-off-by: mgoin <mgoin64@gmail.com>

--- vllm/model_executor/layers/quantization/utils/fp8_utils.py
+++ vllm/model_executor/layers/quantization/utils/fp8_utils.py
@@ -12,7 +12,6 @@ import torch
 
 import vllm.envs as envs
 from vllm import _custom_ops as ops
-from vllm._aiter_ops import rocm_aiter_ops
 from vllm.logger import init_logger
 from vllm.model_executor.layers.quantization.input_quant_fp8 import QuantFP8
 from vllm.model_executor.layers.quantization.utils.quant_utils import (
@@ -313,6 +312,8 @@ class W8A8BlockFp8LinearOp:
         weight_scale: torch.Tensor,
         input_scale: torch.Tensor | None = None,
     ) -> torch.Tensor:
+        from vllm._aiter_ops import rocm_aiter_ops
+
         assert self.act_quant_group_shape == GroupShape(1, 128)
 
         n, k = weight.shape
