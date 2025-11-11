2d9799c086dcb24bf00a10430863715f1ab165ec
ilmarkov <markovilya197@gmail.com>
2025-11-11 13:02:49 -0800

    Remove comment
    Signed-off-by: ilmarkov <markovilya197@gmail.com>

--- vllm/distributed/device_communicators/symm_mem.py
+++ vllm/distributed/device_communicators/symm_mem.py
@@ -88,8 +88,6 @@ class SymmMemCommunicator:
             self.max_size = SYMM_MEM_ALL_REDUCE_MAX_SIZES[self.device_capability][
                 self.world_size
             ]
-        # TODO: Find the root cause of
-        # https://github.com/vllm-project/vllm/issues/26922 and fix throughly
         try:
             self.buffer = torch_symm_mem.empty(
                 self.max_size // self.dtype.itemsize,
