2cbd5974b2644f1b8bc410e5ad0597cc5fe9713f
Bill Nell <bnell@redhat.com>
2025-11-11 19:50:36 +0000

    update some names
    Signed-off-by: Bill Nell <bnell@redhat.com>

--- tests/kernels/moe/modular_kernel_tools/mk_objects.py
+++ tests/kernels/moe/modular_kernel_tools/mk_objects.py
@@ -40,7 +40,12 @@ from vllm.model_executor.layers.quantization.utils.w8a8_utils import (
 from vllm.platforms import current_platform
 from vllm.utils.deep_gemm import is_deep_gemm_supported
 from vllm.utils.flashinfer import has_flashinfer_cutlass_fused_moe
-from vllm.utils.import_utils import has_deep_ep, has_deep_gemm, has_pplx, has_rose
+from vllm.utils.import_utils import (
+    has_deep_ep,
+    has_deep_gemm,
+    has_pplx,
+    has_pplx_garden,
+)
 
 
 @dataclass
@@ -234,17 +239,17 @@ if has_pplx():
         backend="pplx",
     )
 
-if has_rose():
-    from vllm.model_executor.layers.fused_moe.rose_prepare_finalize import (
-        RosePrepareAndFinalize,
+if has_pplx_garden():
+    from vllm.model_executor.layers.fused_moe.pplx_garden_prepare_finalize import (
+        PplxGardenPrepareAndFinalize,
     )
 
     register_prepare_and_finalize(
-        RosePrepareAndFinalize,
+        PplxGardenPrepareAndFinalize,
         batched_format,
         common_float_and_int_types,
         blocked_quantization_support=True,
-        backend="rose",
+        backend="pplx_garden",
     )
 
 if has_flashinfer_cutlass_fused_moe() and current_platform.has_device_capability(100):
