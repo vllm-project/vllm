285e70b45b00089b9a93eec09092a5fec2255788
Yong Hoon Shin <yhshin@meta.com>
2025-11-11 13:15:37 -0800

    Add SPDX header
    Signed-off-by: Yong Hoon Shin <yhshin@meta.com>

--- tests/transformers_utils/test_config.py
+++ tests/transformers_utils/test_config.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+
 from transformers import PretrainedConfig
 
 from vllm.transformers_utils.config import is_interleaved
