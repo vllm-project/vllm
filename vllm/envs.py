dad36b095c582210e084a47aa8ce95c709c623e1
NargiT <3153333+NargiT@users.noreply.github.com>
2025-11-12 21:09:54 +0100

    Update envs.py
    Signed-off-by: NargiT <3153333+NargiT@users.noreply.github.com>

--- vllm/envs.py
+++ vllm/envs.py
@@ -1484,6 +1484,11 @@ environment_variables: dict[str, Callable[[], Any]] = {
     # After enabled, PromptLogprobs and SampleLogprobs would populated as
     # FlatLogprobs.
     "VLLM_FLAT_LOGPROBS": lambda: bool(int(os.getenv("VLLM_FLAT_LOGPROBS", "0"))),
+
+    # Flag to prefix each log line with a human friendly and coloured process name.
+    # The feature goes against standard log formatter, if you want to keep this information simply set VLLM_ENABLE_PREFIX_PROC_LOGS=0 and
+    # add %(processName)s to the log format.
+    "VLLM_ENABLE_PREFIX_PROC_LOGS": lambda(bool(int(os.getenv("VLLM_ENABLE_PREFIX_PROC_LOGS", "1"))),
 }
 
 # --8<-- [end:env-vars-definition]
