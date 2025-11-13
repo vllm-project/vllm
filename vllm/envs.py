6d626eada00af5a3427bc7738dcc1675d5fa863d
nargit <NargiT@users.noreply.github.com>
2025-11-13 11:18:21 +0100

    fix linting
    Signed-off-by: nargit <NargiT@users.noreply.github.com>

--- vllm/envs.py
+++ vllm/envs.py
@@ -1499,10 +1499,9 @@ environment_variables: dict[str, Callable[[], Any]] = {
     # After enabled, PromptLogprobs and SampleLogprobs would populated as
     # FlatLogprobs.
     "VLLM_FLAT_LOGPROBS": lambda: bool(int(os.getenv("VLLM_FLAT_LOGPROBS", "0"))),
-
-    # Flag to prefix each log line with a human friendly and coloured process name.
-    # The feature goes against standard log formatter, if you want to keep this
-    # information simply set VLLM_ENABLE_PREFIX_PROC_LOGS=0 and add %(processName)s
+    # Flag to prefix each log line with a human-friendly and coloured process name.
+    # The feature goes against the standard log formatter, if you want to keep this
+    # information, set VLLM_ENABLE_PREFIX_PROC_LOGS=0 and add %(processName)s
     # to the log format.
     "VLLM_ENABLE_PREFIX_PROC_LOGS": lambda: bool(
         int(os.getenv("VLLM_ENABLE_PREFIX_PROC_LOGS", "1"))
