65a585a4193b2892f8e86544f19550b3e1e50e07
Nick Hill <nhill@redhat.com>
2025-11-13 16:32:29 -0800

    try VLLM_BATCH_INVARIANT instead of float32
    Signed-off-by: Nick Hill <nhill@redhat.com>

--- vllm/config/vllm.py
+++ vllm/config/vllm.py
@@ -580,15 +580,6 @@ class VllmConfig:
                         "to 'spawn'."
                     )
 
-            if (
-                self.model_config.logits_processors
-                and self.speculative_config is not None
-            ):
-                raise ValueError(
-                    "Custom LogitsProcessors are currently "
-                    "not supported with speculative decoding."
-                )
-
         # Final off-switch for CP/APC:
         # Disable for (a) collected blockers, (b) encoder–decoder, or
         # (c) explicit CP=False when APC wasn't requested.
