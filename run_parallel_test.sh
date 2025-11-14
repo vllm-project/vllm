bf4f8e5e33868d4fad4a9af3fea52d2aa172aac8
Bram Wasti <bwasti@meta.com>
2025-11-14 10:57:10 -0800

    Untrack personal scratch files
    Remove tracking for development/test files that are personal working
    directory scratch files and not relevant to the repo.
    
    Signed-off-by: Bram Wasti <bwasti@meta.com>

--- run_parallel_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Run with 2 GPUs (or 2 CPU processes if no GPUs)
-LD_PRELOAD="/usr/local/fbcode/platform010/lib/libcublasLt.so:/usr/local/fbcode/platform010/lib/libcublas.so" \
-torchrun --nproc_per_node=2 test_real_parallelism.py
