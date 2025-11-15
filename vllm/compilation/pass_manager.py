ca9d4651c27d818db91ab7a91d432c226a1be546
charlifu <charlifu@amd.com>
2025-11-15 15:51:03 +0000

    fix unit test
    Signed-off-by: charlifu <charlifu@amd.com>

--- vllm/compilation/pass_manager.py
+++ vllm/compilation/pass_manager.py
@@ -6,10 +6,6 @@ from torch import fx as fx
 
 from vllm import envs
 from vllm._aiter_ops import rocm_aiter_ops
-from vllm.compilation.rocm_aiter_fusion import (
-    RocmAiterRMSNormFp8GroupQuantFusionPass,
-    RocmAiterSiluMulFp8GroupQuantFusionPass,
-)
 from vllm.config import VllmConfig, set_current_vllm_config
 from vllm.logger import init_logger
 from vllm.platforms import current_platform
@@ -17,6 +13,12 @@ from vllm.utils.system_utils import set_env_var
 
 from .post_cleanup import PostCleanupPass
 from .vllm_inductor_pass import VllmInductorPass
+
+if rocm_aiter_ops.is_enabled():
+    from vllm.compilation.rocm_aiter_fusion import (
+        RocmAiterRMSNormFp8GroupQuantFusionPass,
+        RocmAiterSiluMulFp8GroupQuantFusionPass,
+    )
 
 if current_platform.is_cuda_alike():
     from .activation_quant_fusion import ActivationQuantFusionPass
