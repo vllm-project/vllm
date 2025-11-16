ed8419063ea2aef419b492dbe67879db42087a17
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2025-11-16 11:49:00 -0800

    support mla
    Signed-off-by: Woosuk Kwon <woosuk.kwon@berkeley.edu>

--- vllm/v1/worker/gpu/attn_utils.py
+++ vllm/v1/worker/gpu/attn_utils.py
@@ -6,7 +6,6 @@ from typing import Any
 import torch
 
 from vllm.attention.backends.abstract import AttentionBackend
-from vllm.attention.layer import Attention
 from vllm.config import VllmConfig, get_layers_from_vllm_config
 from vllm.model_executor.layers.attention_layer_base import AttentionLayerBase
 from vllm.v1.attention.backends.utils import (
@@ -38,13 +37,14 @@ def init_attn_backend(
 ):
     attn_backends: dict[str, AttentionBackend] = {}
     attn_metadata_builders: list[AttentionMetadataBuilder] = []
-
     flashinfer_workspace: torch.Tensor | None = None
-    attn_layers = get_layers_from_vllm_config(vllm_config, Attention)
     for kv_cache_group_spec in kv_cache_config.kv_cache_groups:
         layer_names = kv_cache_group_spec.layer_names
         any_layer_name = next(iter(layer_names))
 
+        attn_layers = get_layers_from_vllm_config(
+            vllm_config, AttentionLayerBase, layer_names
+        )
         attn_backend = attn_layers[any_layer_name].get_attn_backend()
         for layer_name in layer_names:
             attn_backends[layer_name] = attn_backend
@@ -106,15 +106,20 @@ def _reshape_kv_cache(
                 kv_cache_spec.head_size,
             )
 
-            dtype = kv_cache_spec.dtype
-            kv_cache_stride_order = attn_backend.get_kv_cache_stride_order()
-            kv_cache_shape = tuple(kv_cache_shape[i] for i in kv_cache_stride_order)
+            # FIXME(woosuk): Add kv_cache_stride_order to all attention backends.
+            try:
+                kv_cache_stride_order = attn_backend.get_kv_cache_stride_order()
+                assert len(kv_cache_stride_order) == len(kv_cache_shape)
+            except (AttributeError, NotImplementedError):
+                kv_cache_stride_order = tuple(range(len(kv_cache_shape)))
 
+            kv_cache_shape = tuple(kv_cache_shape[i] for i in kv_cache_stride_order)
             inv_order = [
                 kv_cache_stride_order.index(i)
                 for i in range(len(kv_cache_stride_order))
             ]
 
+            dtype = kv_cache_spec.dtype
             raw_tensor = raw_tensor.view(dtype)
             raw_tensor = raw_tensor.view(kv_cache_shape)
             kv_caches[layer_name] = raw_tensor.permute(*inv_order)
