7b874bfc91e8582ca980b5ed2833597f8fb8a43d
Bram Wasti <bwasti@meta.com>
2025-11-16 19:41:06 -0800

    Remove unsupported TRITON_MLA backend from batch invariance
    TRITON_MLA is not actually supported but was incorrectly listed in
    the batch invariance supported backends and test parametrizations.
    This removes it from:
    - override_envs_for_invariance() supported_backends list
    - test_v1_generation_is_deterministic_across_batch_sizes_with_needle
    - test_logprobs_bitwise_batch_invariance_bs1_vs_bsN
    - test_simple_generation
    - test_logprobs_without_batch_invariance_should_fail
    
    Signed-off-by: Bram Wasti <bwasti@meta.com>

--- vllm/model_executor/layers/batch_invariant.py
+++ vllm/model_executor/layers/batch_invariant.py
@@ -805,11 +805,10 @@ def override_envs_for_invariance():
         "FLASH_ATTN",  # best supported backend
         "FLASHINFER",
         "FLASH_ATTN_MLA",
-        "TRITON_MLA",
+        "FLASHINFER_MLA",
         # Not yet supported MLA backends
         # "FLASHMLA",
         # "FLEX_ATTENTION", # IMA issue even if we disable batch invariance
-        # "FLASHINFER_MLA", https://github.com/vllm-project/vllm/pull/28967
     ]
     if curr_attn_backend not in supported_backends:
         warning = (
