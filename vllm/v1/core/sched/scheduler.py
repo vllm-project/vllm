ad439f68fd9687eaf3382f3b83f9e60b29673620
wang.yuqi <yuqi.wang@daocloud.io>
2025-11-18 14:43:45 +0800

    Ruff
    Signed-off-by: wang.yuqi <yuqi.wang@daocloud.io>

--- vllm/v1/core/sched/scheduler.py
+++ vllm/v1/core/sched/scheduler.py
@@ -503,7 +503,8 @@ class Scheduler(SchedulerInterface):
                         not self.scheduler_config.enable_chunked_prefill
                         and num_new_tokens > token_budget
                     ):
-                        # If chunked_prefill is disabled, we can stop the scheduling here.
+                        # If chunked_prefill is disabled,
+                        # we can stop the scheduling here.
                         break
 
                     num_new_tokens = min(num_new_tokens, token_budget)
