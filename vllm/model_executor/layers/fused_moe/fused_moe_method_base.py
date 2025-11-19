35f9c5351b021d25c1863fdb7f27404ce3d4135c
Chen Bruce <bruceszchen@tencent.com>
2025-11-19 20:46:24 +0800

    [Feat][Perf] Enable deepep-low-latency with round-robin expert placement. (#28449)
    Signed-off-by: bruceszchen <bruceszchen@tencent.com>
    Co-authored-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>
    Signed-off-by: slava.sidik <slava.sidik@huawei.com>

--- vllm/model_executor/layers/fused_moe/fused_moe_method_base.py
+++ vllm/model_executor/layers/fused_moe/fused_moe_method_base.py
@@ -50,10 +50,15 @@ class FusedMoEMethodBase(QuantizeMethodBase):
         """
         return False
 
-    def maybe_make_prepare_finalize(self) -> FusedMoEPrepareAndFinalize | None:
+    def maybe_make_prepare_finalize(
+        self,
+        routing_tables: tuple[torch.Tensor, torch.Tensor, torch.Tensor] | None = None,
+    ) -> FusedMoEPrepareAndFinalize | None:
         from .all2all_utils import maybe_make_prepare_finalize
 
-        return maybe_make_prepare_finalize(self.moe, self.moe_quant_config)
+        return maybe_make_prepare_finalize(
+            self.moe, self.moe_quant_config, routing_tables
+        )
 
     def select_gemm_impl(
         self,
