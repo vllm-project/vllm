c5bb5652233eaf7d1a72c44e4d13d56931c56234
Randall Smith <ransmith@amd.com>
2025-11-19 12:19:30 -0600

    Remvoe skip global cleanup in test_struct_output_generate.py
    Signed-off-by: Randall Smith <ransmith@amd.com>

--- tests/v1/entrypoints/llm/test_struct_output_generate.py
+++ tests/v1/entrypoints/llm/test_struct_output_generate.py
@@ -97,7 +97,6 @@ def test_guided_decoding_deprecated():
     assert sp1.structured_outputs == guided_decoding
 
 
-@pytest.mark.skip_global_cleanup
 @pytest.mark.parametrize(
     "model_name, backend, tokenizer_mode, speculative_config",
     PARAMS_MODELS_BACKENDS_TOKENIZER_MODE,
@@ -602,7 +601,6 @@ Make the response as short as possible.
                 )
 
 
-@pytest.mark.skip_global_cleanup
 @pytest.mark.parametrize(
     "model_name, backend, tokenizer_mode, reasoning_parser, speculative_config",  # noqa: E501
     [
@@ -687,7 +685,6 @@ def test_structured_output_with_reasoning_matrices(
         jsonschema.validate(instance=output_json, schema=reasoning_schema)
 
 
-@pytest.mark.skip_global_cleanup
 @pytest.mark.parametrize("model_name, tokenizer_mode", PARAMS_MODELS_TOKENIZER_MODE)
 def test_structured_output_auto_mode(
     unsupported_json_schema: dict[str, Any],
@@ -734,7 +731,6 @@ def test_structured_output_auto_mode(
         assert isinstance(parsed_json, dict)
 
 
-@pytest.mark.skip_global_cleanup
 def test_guidance_no_additional_properties():
     llm = LLM(
         model="Qwen/Qwen2.5-1.5B-Instruct",
