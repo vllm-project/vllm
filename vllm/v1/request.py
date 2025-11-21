fafaa096862141f096eca0533652dbe9da7d72ec
Mark McLoughlin <markmc@redhat.com>
2025-11-21 22:45:00 +0000

    [KV Connector] Fix async connector prefix cache metrics (#28585)
    Signed-off-by: Mark McLoughlin <markmc@redhat.com>
    Co-authored-by: Nicolò Lucchesi <nlucches@redhat.com>
    Signed-off-by: Joachim Studnia <joachim@mistral.ai>

--- vllm/v1/request.py
+++ vllm/v1/request.py
@@ -121,6 +121,9 @@ class Request:
         # The number of requests being preempted by the scheduler
         self.num_preemptions = 0
 
+        # The number of tokens that have been computed remotely.
+        self.num_external_computed_tokens = 0
+
         self.block_hashes: list[BlockHash] = []
         self.get_hash_new_full_blocks: Callable[[], list[BlockHash]] | None = None
         if block_hasher is not None:
