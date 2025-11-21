4c53d2e691e8bdaaa7c57c5a2aac3d4a310066bd
Boyuan Feng <boyuan@meta.com>
2025-11-20 19:50:59 -0800

    [BugFix] skip combo kernel on cpu (#29129)
    Signed-off-by: Boyuan Feng <boyuan@meta.com>
    Signed-off-by: slava.sidik <slava.sidik@huawei.com>

--- vllm/config/compilation.py
+++ vllm/config/compilation.py
@@ -664,6 +664,8 @@ class CompilationConfig:
             is_torch_equal_or_newer("2.9.0.dev")
             and "combo_kernels" not in self.inductor_compile_config
             and "benchmark_combo_kernel" not in self.inductor_compile_config
+            # (fixme @boyuan) combo kernel does not support cpu yet.
+            and not current_platform.is_cpu()
         ):
             # use horizontal fusion, which is useful for fusing qk-norm and
             # qk-rope when query and key have different shapes.
