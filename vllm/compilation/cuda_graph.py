28579b55faf47e267ca7ff8b49e2f2272b88ec4f
Amir Samani <asamani@nvidia.com>
2025-11-21 14:20:08 -0800

    add stream to cuda graph catpure
    Signed-off-by: Amir Samani <asamani@nvidia.com>

--- vllm/compilation/cuda_graph.py
+++ vllm/compilation/cuda_graph.py
@@ -169,7 +169,11 @@ class CUDAGraphWrapper:
                 else:
                     set_graph_pool_id(current_platform.graph_pool_handle())
                 # mind-exploding: carefully manage the reference and memory.
-                with torch.cuda.graph(cudagraph, pool=self.graph_pool):
+                with torch.cuda.graph(
+                    cudagraph,
+                    pool=self.graph_pool,
+                    stream=torch.cuda.current_stream(),
+                ):
                     # `output` is managed by pytorch's cudagraph pool
                     output = self.runnable(*args, **kwargs)
                     if self.cudagraph_options.weak_ref_output:
