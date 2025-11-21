da3ba7cb4893acdfed18bc87b47191efd9f97cc6
Varun Sundar Rabindranath <vsundarr@redhat.com>
2025-11-21 18:54:16 +0000

    lint
    Signed-off-by: Varun Sundar Rabindranath <vsundarr@redhat.com>

--- .buildkite/scripts/scheduled_integration_test/qwen30b_a3b_fp8_block_ep_eplb.sh
+++ .buildkite/scripts/scheduled_integration_test/qwen30b_a3b_fp8_block_ep_eplb.sh
@@ -37,10 +37,7 @@ for BACK in "${BACKENDS[@]}"; do
   vllm serve "$MODEL" \
     --enforce-eager \
     --enable-eplb \
-    --eplb-config '{"window_size":10, \
-                    "step_interval":100, \
-                    "num_redundant_experts":0, \
-                    "log_balancedness":true}' \
+    --eplb-config '{"window_size":10, "step_interval":100, "num_redundant_experts":0, "log_balancedness":true}' \
     --tensor-parallel-size 2 \
     --data-parallel-size 2 \
     --enable-expert-parallel \
