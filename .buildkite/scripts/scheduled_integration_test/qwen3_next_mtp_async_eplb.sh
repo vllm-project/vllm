62d95a90e47c0e9111751b55e9fdadc904c4be67
David Chen <530634352@qq.com>
2025-11-25 15:52:38 +0800

    add async eplb nightly ci
    Signed-off-by: David Chen <530634352@qq.com>

--- .buildkite/scripts/scheduled_integration_test/qwen3_next_mtp_async_eplb.sh
+++ .buildkite/scripts/scheduled_integration_test/qwen3_next_mtp_async_eplb.sh
@@ -46,14 +46,14 @@ for BACK in "${BACKENDS[@]}"; do
   VLLM_ALL2ALL_BACKEND=$BACK \
   vllm serve "$MODEL" \
     --enforce-eager \
-    --tensor-parallel-size 2 \
-    --data-parallel-size 2 \
+    --tensor-parallel-size 4 \
     --enable-expert-parallel \
     --enable-eplb \
     --eplb-config '{"window_size":200,"step_interval":600,"use_async":"true"}' \
     --speculative-config '{"method":"qwen3_next_mtp","num_speculative_tokens":1}' \
     --trust-remote-code \
     --max-model-len 2048 \
+    --gpu-memory-utilization 0.9 \
     --port $PORT &
   SERVER_PID=$!
   wait_for_server $PORT
