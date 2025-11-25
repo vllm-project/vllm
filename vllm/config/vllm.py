e5768f36012b0e19421710ea8406d98884c85c46
yewentao256 <zhyanwentao@126.com>
2025-11-25 13:30:58 -0800

    fix log issue
    Signed-off-by: yewentao256 <zhyanwentao@126.com>

--- vllm/config/vllm.py
+++ vllm/config/vllm.py
@@ -1152,7 +1152,7 @@ def get_current_vllm_config() -> VllmConfig:
         # in ci, usually when we test custom ops/modules directly,
         # we don't set the vllm config. In that case, we set a default
         # config.
-        logger.warning("Current vLLM config is not set.")
+        logger.warning_once("Current vLLM config is not set.", scope="local")
         return VllmConfig()
     return _current_vllm_config
 
