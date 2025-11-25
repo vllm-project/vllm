3fa135cdccf21cff37718900bbb5a80da0dd7188
vllmellm <vllm.ellm@embeddedllm.com>
2025-11-25 14:56:06 +0800

    [Bugfix] [ROCm] [UX]: revert Flex attention backend (#29371)
    Signed-off-by: vllmellm <vllm.ellm@embeddedllm.com>
    Signed-off-by: slava.sidik <slava.sidik@huawei.com>

--- vllm/platforms/rocm.py
+++ vllm/platforms/rocm.py
@@ -262,6 +262,10 @@ class RocmPlatform(Platform):
                 f"is not MLA type while requested for MLA backend."
             )
 
+        if selected_backend == AttentionBackendEnum.FLEX_ATTENTION:
+            logger.info("Using FlexAttention backend.")
+            return AttentionBackendEnum.FLEX_ATTENTION.get_path()
+
         if selected_backend == AttentionBackendEnum.TRITON_ATTN:
             logger.info("Using Triton Attention backend on V1 engine.")
             return AttentionBackendEnum.TRITON_ATTN.get_path()
