39bafb95e543bc57b7c0b23d5d2400a90d511abe
Xieyang Xu <ashlippers@gmail.com>
2025-11-25 16:20:35 -0800

    dummy run corner case (#29433)
    Signed-off-by: Joachim Studnia <joachim@mistral.ai>

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -2789,7 +2789,7 @@ class GPUModelRunner(
                         # returns True. before returning early here we call
                         # dummy run to ensure coordinate_batch_across_dp
                         # is called into to avoid out of sync issues.
-                        self._dummy_run(1)
+                        self._dummy_run(self._get_num_input_tokens(1))
                     if not has_kv_transfer_group():
                         # Return empty ModelRunnerOutput if no work to do.
                         return EMPTY_MODEL_RUNNER_OUTPUT
