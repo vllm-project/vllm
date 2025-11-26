edeb8971b78aff5f6a9dd2b650d3089dcee97540
Lucia Fang <116399278+luccafong@users.noreply.github.com>
2025-11-25 16:14:23 -0800

    fix assertion for single world use case (uni) (#29429)
    Signed-off-by: Lu Fang <fanglu@fb.com>
    Co-authored-by: Lucia (Lu) Fang <fanglu@meta.com>
    Signed-off-by: dsuhinin <suhinin.dmitriy@gmail.com>

--- vllm/config/parallel.py
+++ vllm/config/parallel.py
@@ -593,9 +593,10 @@ class ParallelConfig:
                 "max_parallel_loading_workers is currently "
                 "not supported and will be ignored."
             )
-        if self.distributed_executor_backend != "mp" and self.nnodes > 1:
+        if self.distributed_executor_backend not in ("mp", "uni") and self.nnodes > 1:
             raise ValueError(
-                "nnodes > 1 can only be set when distributed exectuor backend is mp."
+                "nnodes > 1 can only be set when distributed executor "
+                "backend is mp or uni."
             )
 
     @property
