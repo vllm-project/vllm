e934681e7077b37bdf89a9f620c3a4e6ef30a7ee
Jee Jee Li <pandaleefree@gmail.com>
2025-11-27 21:56:28 +0800

    [LoRA] Continue optimizing MoE LoRA weight loading (#29322)
    Signed-off-by: Jee Jee Li <pandaleefree@gmail.com>
    Signed-off-by: dsuhinin <suhinin.dmitriy@gmail.com>

--- vllm/lora/layers/base.py
+++ vllm/lora/layers/base.py
@@ -60,7 +60,7 @@ class BaseLayerWithLoRA(nn.Module):
         source_layer: nn.Module,
         lora_config: LoRAConfig,
         packed_modules_list: list,
-        model_config: PretrainedConfig | None,
+        model_config: PretrainedConfig | None = None,
     ) -> bool:
         """Returns True if the layer can be replaced by this LoRA layer."""
         raise NotImplementedError
