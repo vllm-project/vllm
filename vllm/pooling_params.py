3407340c0527ca47009d895baff77790e3587205
果冻虾仁 <guodong@apache.org>
2025-11-29 00:41:05 +0800

    add add_truncate_prompt_tokens in repr for PoolingParams (#29683)
    Signed-off-by: dsuhinin <suhinin.dmitriy@gmail.com>

--- vllm/pooling_params.py
+++ vllm/pooling_params.py
@@ -220,6 +220,7 @@ class PoolingParams(
             f"returned_token_ids={self.returned_token_ids}, "
             f"requires_token_ids={self.requires_token_ids}, "
             f"skip_reading_prefix_cache={self.skip_reading_prefix_cache}, "
+            f"truncate_prompt_tokens={self.truncate_prompt_tokens}, "
             f"extra_kwargs={self.extra_kwargs})"
         )
 
