22944055d8ec52a2b9231fb60fd2e9f86295bc71
Isotr0py <mozf@mail2.sysu.edu.cn>
2025-11-30 00:16:00 +0800

    enable compressed-tensors AWQ for turing GPU
    Signed-off-by: Isotr0py <mozf@mail2.sysu.edu.cn>

--- vllm/model_executor/layers/quantization/compressed_tensors/schemes/compressed_tensors_wNa16.py
+++ vllm/model_executor/layers/quantization/compressed_tensors/schemes/compressed_tensors_wNa16.py
@@ -74,7 +74,7 @@ class CompressedTensorsWNA16(CompressedTensorsScheme):
     @classmethod
     def get_min_capability(cls) -> int:
         # ampere and up
-        return 80
+        return 75
 
     def create_weights(
         self,
