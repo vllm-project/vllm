Wrong dim
Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

vllm/model_executor/models/siglip.py
@@ -990,7 +990,8 @@ class SiglipTextEmbeddings(nn.Module):
                 embeddings.new_empty(
                     len(embeddings), config.projection_size - embeddings.shape[1]
                 ),
-            ]
+            ],
+            dim=1,
         )
 
 