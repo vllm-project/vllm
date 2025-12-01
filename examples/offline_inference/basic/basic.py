b31a975f33ac9f991697eef90b6f819fe4dca657
jiahanc <173873397+jiahanc@users.noreply.github.com>
2025-12-01 10:28:47 -0800

    add missing codes
    Signed-off-by: jiahanc <173873397+jiahanc@users.noreply.github.com>

--- examples/offline_inference/basic/basic.py
+++ examples/offline_inference/basic/basic.py
@@ -16,7 +16,7 @@ sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
 def main():
     # Create an LLM.
-    llm = LLM(model="Qwen/Qwen3-Next-80B-A3B-Instruct")
+    llm = LLM(model="facebook/opt-125m")
     # Generate texts from the prompts.
     # The output is a list of RequestOutput objects
     # that contain the prompt, generated text, and other information.
