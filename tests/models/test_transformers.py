dcf2d2ddee31650fddb7208e90b8c8d2f57c4466
badaoui <abdennacerbadaoui0@gmail.com>
2025-12-01 15:29:40 +0000

    restore ROCm skip condition for bitsandbytes
    Signed-off-by: badaoui <abdennacerbadaoui0@gmail.com>

--- tests/models/test_transformers.py
+++ tests/models/test_transformers.py
@@ -6,6 +6,8 @@ from typing import Any
 
 import pytest
 
+from vllm.platforms import current_platform
+
 from ..conftest import HfRunner, VllmRunner
 from ..utils import multi_gpu_test, prep_prompts
 from .registry import HF_EXAMPLE_MODELS
@@ -141,6 +143,12 @@ def test_quantization(
     max_tokens: int,
     num_logprobs: int,
 ) -> None:
+    if (
+        current_platform.is_rocm()
+        and quantization_kwargs.get("quantization", "") == "bitsandbytes"
+    ):
+        pytest.skip("bitsandbytes quantization is currently not supported in rocm.")
+
     with vllm_runner(
         model,
         model_impl="auto",
