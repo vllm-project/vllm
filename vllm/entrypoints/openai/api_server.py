b2171639c7d58c1bff280850bb812c246238ab4a
wang.yuqi <yuqi.wang@daocloud.io>
2025-12-01 11:24:31 +0800

    + register_pooling_api_routers
    Signed-off-by: wang.yuqi <yuqi.wang@daocloud.io>

--- vllm/entrypoints/openai/api_server.py
+++ vllm/entrypoints/openai/api_server.py
@@ -1366,25 +1366,13 @@ def build_app(args: Namespace) -> FastAPI:
     register_sagemaker_routes(router)
     app.include_router(router)
 
-    from vllm.entrypoints.pooling.classify.api_router import router as classify_router
-
-    app.include_router(classify_router)
-
-    from vllm.entrypoints.pooling.embed.api_router import router as embed_router
-
-    app.include_router(embed_router)
-
-    from vllm.entrypoints.pooling.pooling.api_router import router as pooling_router
-
-    app.include_router(pooling_router)
-
-    from vllm.entrypoints.pooling.score.api_router import router as score_router
-
-    app.include_router(score_router)
-
     app.root_path = args.root_path
 
     mount_metrics(app)
+
+    from vllm.entrypoints.pooling import register_pooling_api_routers
+
+    register_pooling_api_routers(app)
 
     app.add_middleware(
         CORSMiddleware,
