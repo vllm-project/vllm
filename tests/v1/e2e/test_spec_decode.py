4776f66bdbda25e18b20556670df0fe5d213747b
charlifu <charlifu@amd.com>
2025-12-03 17:25:40 +0000

    update test_spec_decode
    Signed-off-by: charlifu <charlifu@amd.com>

--- tests/v1/e2e/test_spec_decode.py
+++ tests/v1/e2e/test_spec_decode.py
@@ -413,7 +413,7 @@ def test_eagle_correctness(
                 "multi-token eagle spec decode on current platform"
             )
 
-        if attn_backend == "FLASH_ATTN" and current_platform.is_rocm():
+        if attn_backend == "ROCM_AITER_FA" and current_platform.is_rocm():
             if "deepseek" in model_setup[1].lower():
                 pytest.skip("FLASH_ATTN for deepseek not supported on ROCm platform")
             else:
