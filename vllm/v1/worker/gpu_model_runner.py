6cb9aa388d73d342a5fbd20d025a8485cdb67300
Ming Yang <minos.future@gmail.com>
2025-12-04 01:06:40 -0800

    set vllm config for model execution
    Signed-off-by: Ming Yang <minos.future@gmail.com>

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -34,6 +34,7 @@ from vllm.config import (
     CUDAGraphMode,
     VllmConfig,
     get_layers_from_vllm_config,
+    set_current_vllm_config,
     update_config,
 )
 from vllm.distributed.ec_transfer import get_ec_transfer, has_ec_transfer
@@ -4067,6 +4068,7 @@ class GPUModelRunner(
                     batch_descriptor=batch_desc,
                     ubatch_slices=ubatch_slices,
                 ),
+                set_current_vllm_config(self.vllm_config),
             ):
                 outputs = self.model(
                     input_ids=input_ids,
