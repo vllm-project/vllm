3298cc8bea24f6f7b7108391bc36241a37beb954
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-12-04 11:17:38 +0100

    Delete HF version of Phi 4 MM
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- vllm/model_executor/models/registry.py
+++ vllm/model_executor/models/registry.py
@@ -374,7 +374,6 @@ _MULTIMODAL_MODELS = {
     ),
     "Phi3VForCausalLM": ("phi3v", "Phi3VForCausalLM"),
     "Phi4MMForCausalLM": ("phi4mm", "Phi4MMForCausalLM"),
-    "Phi4MultimodalForCausalLM": ("phi4_multimodal", "Phi4MultimodalForCausalLM"),  # noqa: E501
     "PixtralForConditionalGeneration": ("pixtral", "PixtralForConditionalGeneration"),  # noqa: E501
     "QwenVLForConditionalGeneration": ("qwen_vl", "QwenVLForConditionalGeneration"),  # noqa: E501
     "Qwen2VLForConditionalGeneration": ("qwen2_vl", "Qwen2VLForConditionalGeneration"),  # noqa: E501
