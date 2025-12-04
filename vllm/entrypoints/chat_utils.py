2ddc616bc4f946019324487fb7b2fcde4901e16a
pridejcyang <pridejcyang@tencent.com>
2025-12-04 18:16:20 +0800

    fix tools type
    Signed-off-by: pridejcyang <pridejcyang@tencent.com>

--- vllm/entrypoints/chat_utils.py
+++ vllm/entrypoints/chat_utils.py
@@ -24,6 +24,7 @@ from openai.types.chat import (
     ChatCompletionContentPartInputAudioParam,
     ChatCompletionContentPartRefusalParam,
     ChatCompletionContentPartTextParam,
+    ChatCompletionFunctionToolParam,
     ChatCompletionMessageToolCallParam,
     ChatCompletionToolMessageParam,
 )
@@ -260,7 +261,7 @@ class CustomChatCompletionMessageParam(TypedDict, total=False):
     reasoning: str | None
     """The reasoning content for interleaved thinking."""
 
-    tools: str | None
+    tools: list[ChatCompletionFunctionToolParam] | None
     """The tools for developer role."""
 
 
@@ -294,7 +295,7 @@ class ConversationMessage(TypedDict, total=False):
     reasoning_content: str | None
     """Deprecated: The reasoning content for interleaved thinking."""
 
-    tools: str | None
+    tools: list[ChatCompletionFunctionToolParam] | None
     """The tools for developer role."""
 
 
@@ -1590,8 +1591,8 @@ def _parse_chat_message_content(
         if "name" in message and isinstance(message["name"], str):
             result_msg["name"] = message["name"]
 
-        if "tools" in message:
-            result_msg["tools"] = message["tools"]
+        if role == "developer":
+            result_msg["tools"] = message.get("tools", None)
     return result
 
 
