88f682b86062c7959698ac0b181bd541b77c4f69
sarathc-cerebras <sarath.chandran@cerebras.net>
2025-12-07 18:30:06 +0400

    fixes rope params handling
    Signed-off-by: sarathc-cerebras <sarath.chandran@cerebras.net>

--- vllm/model_executor/models/jais2.py
+++ vllm/model_executor/models/jais2.py
@@ -235,14 +235,6 @@ class Jais2DecoderLayer(nn.Module):
         quant_config = self.get_quant_config(vllm_config)
 
         self.hidden_size = config.hidden_size
-        rope_theta = getattr(config, "rope_theta", 500000)
-        rope_scaling = getattr(config, "rope_scaling", None)
-        if rope_scaling is not None and getattr(
-            config, "original_max_position_embeddings", None
-        ):
-            rope_scaling["original_max_position_embeddings"] = (
-                config.original_max_position_embeddings
-            )
         max_position_embeddings = getattr(config, "max_position_embeddings", 8192)
         # Support abacusai/Smaug-72B-v0.1 with attention_bias
         # Support internlm/internlm-7b with bias
@@ -256,8 +248,6 @@ class Jais2DecoderLayer(nn.Module):
             num_kv_heads=getattr(
                 config, "num_key_value_heads", config.num_attention_heads
             ),
-            rope_theta=rope_theta,
-            rope_scaling=rope_scaling,
             max_position_embeddings=max_position_embeddings,
             quant_config=quant_config,
             bias=attention_bias,
