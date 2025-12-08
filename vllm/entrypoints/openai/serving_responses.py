76cad233965016d555fae3fe8e6460e8d92b2290
Andrew Xia <axia@fb.com>
2025-12-07 21:24:22 -0800

    unit test
    Signed-off-by: Andrew Xia <axia@fb.com>

--- vllm/entrypoints/openai/serving_responses.py
+++ vllm/entrypoints/openai/serving_responses.py
@@ -318,8 +318,6 @@ class OpenAIServingResponses(OpenAIServing):
         if maybe_validation_error is not None:
             return maybe_validation_error
 
-        fbvscode.set_trace()
-
         # If the engine is dead, raise the engine's DEAD_ERROR.
         # This is required for the streaming case, where we return a
         # success status before we actually start generating text :).
