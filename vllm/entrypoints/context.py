17fd28cd2af46f405972a7a374a2fbc4ebce72d2
Andrew Xia <axia@fb.com>
2025-12-07 21:39:05 -0800

    fix
    Signed-off-by: Andrew Xia <axia@fb.com>

--- vllm/entrypoints/context.py
+++ vllm/entrypoints/context.py
@@ -182,23 +182,21 @@ class SimpleContext(ConversationContext):
         self.num_cached_tokens = output.num_cached_tokens or 0
         self.num_output_tokens += len(output.outputs[0].token_ids or [])
 
-        # only store if enable_response_messages is True, save memory
-        if self.request.enable_response_messages:
-            if len(self.input_messages) == 0:
-                output_prompt = output.prompt or ""
-                output_prompt_token_ids = output.prompt_token_ids or []
-                self.input_messages.append(
-                    ResponseRawMessageAndToken(
-                        message=output_prompt,
-                        tokens=output_prompt_token_ids,
-                    )
-                )
-            self.output_messages.append(
+        if len(self.input_messages) == 0:
+            output_prompt = output.prompt or ""
+            output_prompt_token_ids = output.prompt_token_ids or []
+            self.input_messages.append(
                 ResponseRawMessageAndToken(
-                    message=output.outputs[0].text,
-                    tokens=output.outputs[0].token_ids,
+                    message=output_prompt,
+                    tokens=output_prompt_token_ids,
                 )
             )
+        self.output_messages.append(
+            ResponseRawMessageAndToken(
+                message=output.outputs[0].text,
+                tokens=output.outputs[0].token_ids,
+            )
+        )
 
     def append_tool_output(self, output) -> None:
         raise NotImplementedError("Should not be called.")
