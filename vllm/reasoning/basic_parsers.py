2b1b913b1bb4dc6df66c1343aaa42de3903fe925
hdlj-h <hubert@hcompany.ai>
2025-12-09 09:28:35 +0100

    Remove comment Signed-off-by: hdlj-h <hubert@hcompany.ai>

--- vllm/reasoning/basic_parsers.py
+++ vllm/reasoning/basic_parsers.py
@@ -77,8 +77,6 @@ class BaseThinkingReasoningParser(ReasoningParser):
     def is_reasoning_end_streaming(
         self, input_ids: list[int], delta_ids: list[int]
     ) -> bool:
-        # We only check the end token beause interleaved reasoning and content is not
-        # compatible with the current structured output manager.
         end_token_id = self.end_token_id
         return end_token_id in delta_ids
 
