daf3d756b57b1293b83b63c5b0481625d2e62831
gnovack <gnovack@amazon.com>
2025-12-08 18:35:16 -0800

    Lora MoE Align Improvements (#29257)
    Signed-off-by: gnovack <gnovack@amazon.com>
    Signed-off-by: slava.sidik <slava.sidik@huawei.com>

--- csrc/moe/moe_ops.h
+++ csrc/moe/moe_ops.h
@@ -27,7 +27,7 @@ void moe_lora_align_block_size(
     int64_t max_num_tokens_padded, int64_t max_num_m_blocks,
     torch::Tensor sorted_token_ids, torch::Tensor expert_ids,
     torch::Tensor num_tokens_post_pad, torch::Tensor adapter_enabled,
-    torch::Tensor lora_ids);
+    torch::Tensor lora_ids, std::optional<torch::Tensor> maybe_expert_map);
 #ifndef USE_ROCM
 torch::Tensor moe_wna16_gemm(torch::Tensor input, torch::Tensor output,
                              torch::Tensor b_qweight, torch::Tensor b_scales,
