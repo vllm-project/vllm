cfac6b3f648fec000d0786a7bc2dad02b436ab82
ilmarkov <markovilya197@gmail.com>
2025-12-09 21:28:32 +0000

    Remove eplb config fix
    Signed-off-by: ilmarkov <markovilya197@gmail.com>

--- vllm/engine/arg_utils.py
+++ vllm/engine/arg_utils.py
@@ -423,10 +423,6 @@ class EngineArgs:
     )
     _api_process_count: int = ParallelConfig._api_process_count
     _api_process_rank: int = ParallelConfig._api_process_rank
-    num_redundant_experts: int | None = None
-    eplb_window_size: int | None = None
-    eplb_step_interval: int | None = None
-    eplb_log_balancedness: bool | None = None
     max_parallel_loading_workers: int | None = (
         ParallelConfig.max_parallel_loading_workers
     )
