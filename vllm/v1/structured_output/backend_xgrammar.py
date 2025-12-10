a86b695dad88092c0abb731935c651db5b6376b8
chaunceyjiang <chaunceyjiang@gmail.com>
2025-12-10 04:30:34 +0000

    [Bugfix] Fix the issue where DeepSeek v3.2 cannot use structured_output
    Signed-off-by: chaunceyjiang <chaunceyjiang@gmail.com>

--- vllm/v1/structured_output/backend_xgrammar.py
+++ vllm/v1/structured_output/backend_xgrammar.py
@@ -63,7 +63,7 @@ class XgrammarBackend(StructuredOutputBackend):
             max_id = max(vocab_dict.values())
             tokenizer_vocab_size = max(len(vocab_dict), max_id + 1)
 
-            vocab_size = self.vocab_size or tokenizer_vocab_size
+            vocab_size = self.tokenizer.vocab_size or tokenizer_vocab_size
             # maintain tokenizer's indexing
             encoded_vocab = [""] * vocab_size
             for token, idx in vocab_dict.items():
