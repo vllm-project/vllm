30c6a5a28badd5eea28b3d9ff5b476eaf3cd09d3
huanghaoyan.hhy <huanghaoyan.hhy@alibaba-inc.com>
2025-12-10 17:20:23 +0000

    set block size to max_model_len when prefix caching is disable
    Signed-off-by: huanghaoyan.hhy <huanghaoyan.hhy@alibaba-inc.com>

--- vllm/model_executor/models/config.py
+++ vllm/model_executor/models/config.py
@@ -387,11 +387,16 @@ class HybridAttentionMambaModelConfig(VerifyAndUpdateConfig):
             model_config=model_config,
         )
 
+        if (envs.VLLM_USE_LIGHTER_MAMBA_CACHE
+            and cache_config.enable_prefix_caching):
+            block_size = cache_config.block_size
+        else:
+            block_size = model_config.max_model_len
         # get mamba page size
         mamba_page_size = MambaSpec(
             shapes=model_cls.get_mamba_state_shape_from_config(vllm_config),
             dtypes=model_cls.get_mamba_state_dtype_from_config(vllm_config),
-            block_size=model_config.max_model_len if not envs.VLLM_USE_LIGHTER_MAMBA_CACHE else cache_config.block_size,
+            block_size=block_size,
             enable_caching=cache_config.enable_prefix_caching,
         ).page_size_bytes
 
