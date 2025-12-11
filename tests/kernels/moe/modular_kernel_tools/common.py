60906cddc06cb5f7fb5ac5983160332bb2302314
yewentao256 <zhyanwentao@126.com>
2025-12-11 08:43:05 -0800

    fix tests
    Signed-off-by: yewentao256 <zhyanwentao@126.com>

--- tests/kernels/moe/modular_kernel_tools/common.py
+++ tests/kernels/moe/modular_kernel_tools/common.py
@@ -594,7 +594,9 @@ def make_modular_kernel(
     )
 
     modular_kernel = mk.FusedMoEModularKernel(
-        prepare_finalize=prepare_finalize, fused_experts=fused_experts
+        prepare_finalize=prepare_finalize,
+        fused_experts=fused_experts,
+        moe_parallel_config=moe_parallel_config,
     )
 
     return modular_kernel
