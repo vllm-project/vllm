49902a156b269b686d24aed06643ff983fd2f4ef
WyldeCat <skan1543@gmail.com>
2025-12-11 04:46:05 +0000

    Remove aot scheduling to prevent bug

--- vllm/v1/attention/backends/grouped_diff_attn.py
+++ vllm/v1/attention/backends/grouped_diff_attn.py
@@ -26,7 +26,6 @@ from vllm.attention.utils.fa_utils import (
 if is_flash_attn_varlen_func_available():
     from vllm.attention.utils.fa_utils import (
         flash_attn_varlen_func,
-        get_scheduler_metadata,
         reshape_and_cache_flash,
     )
 
@@ -73,10 +72,6 @@ class GroupedDifferentialAttentionMetadata:
     cu_prefix_query_lens: torch.Tensor | None
     prefix_kv_lens: torch.Tensor | None
     suffix_kv_lens: torch.Tensor | None
-
-    # Optional aot scheduling
-    scheduler_metadata: torch.Tensor | None = None
-    prefix_scheduler_metadata: torch.Tensor | None = None
     max_num_splits: int = 0
 
     causal: bool = True
@@ -89,7 +84,6 @@ class GroupedDifferentialAttentionMetadata:
         local_block_table: torch.Tensor
         local_max_query_len: int
         local_max_seq_len: int
-        local_scheduler_metadata: torch.Tensor | None
 
     local_attn_metadata: LocalAttentionMetadata | None = None
 
@@ -227,38 +221,9 @@ class GroupedDifferentialAttentionMetadataBuilder(
 
         self.max_num_splits = 0  # No upper bound on the number of splits.
 
-        # Some kernels for aot schedule require num_heads_q % num_kv_heads == 0
-        self.aot_schedule = (get_flash_attn_version() == 3) and (
-            self.num_heads_q % self.num_heads_kv == 0
-        )
-
         self.use_full_cuda_graph = (
             self.compilation_config.cudagraph_mode.has_full_cudagraphs()
         )
-
-        if self.use_full_cuda_graph and self.aot_schedule:
-            self.max_cudagraph_size = self.compilation_config.max_capture_size
-
-            if self.max_cudagraph_size > 992:
-                # This condition derives from FA3's internal heuristic.
-                # TODO(woosuk): Support larger cudagraph sizes.
-                raise ValueError(
-                    "Capture size larger than 992 is not supported for full cuda graph."
-                )
-
-            self.scheduler_metadata = torch.zeros(
-                vllm_config.scheduler_config.max_num_seqs + 1,
-                dtype=torch.int32,
-                device=self.device,
-            )
-            # When using cuda graph, we need to set the upper bound of the
-            # number of splits so that large enough intermediate buffers are
-            # pre-allocated during capture.
-            self.max_num_splits = _DEFAULT_MAX_NUM_SPLITS_FOR_CUDA_GRAPH
-
-        # Sliding window size to be used with the AOT scheduler will be
-        # populated on first build() call.
-        self.aot_sliding_window: tuple[int, int] | None = None
 
     def build(
         self,
@@ -267,8 +232,7 @@ class GroupedDifferentialAttentionMetadataBuilder(
         fast_build: bool = False,
     ) -> GroupedDifferentialAttentionMetadata:
         """
-        fast_build disables AOT scheduling, used when there will be few
-        iterations i.e. spec-decode
+        Build GroupedDifferentialAttentionMetadata.
         """
         num_reqs = common_attn_metadata.num_reqs
         num_actual_tokens = common_attn_metadata.num_actual_tokens
@@ -280,83 +244,10 @@ class GroupedDifferentialAttentionMetadataBuilder(
         slot_mapping = common_attn_metadata.slot_mapping
         causal = common_attn_metadata.causal
 
-        # the overhead of the aot schedule is not worth it for spec-decode
-        aot_schedule = self.aot_schedule and not fast_build
-
-        if self.aot_sliding_window is None:
-            self.aot_sliding_window = (-1, -1)
-            # For the AOT scheduler we need the sliding window value to be
-            # constant for all layers to. We have to populate this on the first
-            # build() call so the layers are constructed (cannot populate)
-            # in __init__.
-            if aot_schedule:
-                sliding_window_configs = _get_sliding_window_configs(self.vllm_config)
-                if len(sliding_window_configs) == 1:
-                    sliding_window_config = sliding_window_configs.pop()
-                    if sliding_window_config is not None:
-                        self.aot_sliding_window = sliding_window_config
-                elif len(sliding_window_configs) > 1:
-                    self.aot_schedule = False
-                    aot_schedule = False
-
-        def schedule(
-            batch_size, cu_query_lens, max_query_len, seqlens, max_seq_len, causal
-        ):
-            cache_dtype = self.cache_config.cache_dtype
-            if cache_dtype.startswith("fp8"):
-                # Shorten line length by aliasing the long class name
-                diff_attn_backend = GroupedDifferentialAttentionBackend
-                qkv_dtype = diff_attn_backend.get_fp8_dtype_for_flashattn(cache_dtype)
-            else:
-                qkv_dtype = self.kv_cache_dtype
-            if aot_schedule:
-                return get_scheduler_metadata(
-                    batch_size=batch_size,
-                    max_seqlen_q=max_query_len,
-                    max_seqlen_k=max_seq_len,
-                    num_heads_q=self.num_heads_q,
-                    num_heads_kv=self.num_heads_kv,
-                    headdim=self.headdim,
-                    cache_seqlens=seqlens,
-                    qkv_dtype=qkv_dtype,
-                    cu_seqlens_q=cu_query_lens,
-                    page_size=self.block_size,
-                    causal=causal,
-                    window_size=self.aot_sliding_window,
-                    num_splits=self.max_num_splits,
-                )
-            return None
-
         cu_prefix_query_lens = None
         prefix_kv_lens = None
         suffix_kv_lens = None
-        prefix_scheduler_metadata = None
-        scheduler_metadata = schedule(
-            batch_size=num_reqs,
-            cu_query_lens=query_start_loc,
-            max_query_len=max_query_len,
-            seqlens=seq_lens,
-            max_seq_len=max_seq_len,
-            causal=causal,
-        )
         # For FA3 + full cudagraph
-        max_num_splits = 0
-        if self.use_full_cuda_graph and scheduler_metadata is not None:
-            n = scheduler_metadata.shape[0]
-            self.scheduler_metadata[:n] = scheduler_metadata
-            # NOTE(woosuk): We should zero out the rest of the scheduler
-            # metadata to guarantee the correctness. Otherwise, some thread
-            # blocks may use the invalid scheduler metadata and overwrite the
-            # output buffer.
-            self.scheduler_metadata[n:] = 0
-            scheduler_metadata = self.scheduler_metadata[:n]
-
-            if num_actual_tokens <= self.max_cudagraph_size:
-                # NOTE(woosuk): Setting num_splits > 1 may increase the memory
-                # usage, because the intermediate buffers of size [num_splits,
-                # num_heads, num_tokens, head_size] are allocated. Therefore,
-                # we only set num_splits when using cuda graphs.
-                max_num_splits = self.max_num_splits
 
         attn_metadata = GroupedDifferentialAttentionMetadata(
             num_actual_tokens=num_actual_tokens,
@@ -368,12 +259,9 @@ class GroupedDifferentialAttentionMetadataBuilder(
             slot_mapping=slot_mapping,
             use_cascade=False,
             common_prefix_len=common_prefix_len,
-            scheduler_metadata=scheduler_metadata,
             cu_prefix_query_lens=cu_prefix_query_lens,
             prefix_kv_lens=prefix_kv_lens,
             suffix_kv_lens=suffix_kv_lens,
-            prefix_scheduler_metadata=prefix_scheduler_metadata,
-            max_num_splits=max_num_splits,
             causal=causal,
         )
         return attn_metadata
@@ -717,7 +605,6 @@ class GroupedDifferentialAttentionImpl(AttentionImpl):
         max_seqlen_q = attn_metadata.max_query_len
         max_seqlen_k = attn_metadata.max_seq_len
         block_table = attn_metadata.block_table
-        scheduler_metadata = attn_metadata.scheduler_metadata
 
         descale_shape = (cu_seqlens_q.shape[0] - 1, key.shape[1])
 
@@ -735,7 +622,7 @@ class GroupedDifferentialAttentionImpl(AttentionImpl):
             window_size=self.sliding_window,
             block_table=block_table,
             softcap=self.logits_soft_cap,
-            scheduler_metadata=scheduler_metadata,
+            scheduler_metadata=None,
             fa_version=self.vllm_flash_attn_version,
             q_descale=layer._q_scale.expand(descale_shape),
             k_descale=layer._k_scale.expand(descale_shape),
