905b322ca41d99fd736de3be9c30e6744b07c574
tjtanaa <tunjian.tan@embeddedllm.com>
2025-12-13 17:52:08 +0000

    add unit tests test_vit_backend_functionality.py; fix siglip and qwen3 moe bug
    Signed-off-by: tjtanaa <tunjian.tan@embeddedllm.com>

--- vllm/model_executor/models/siglip2navit.py
+++ vllm/model_executor/models/siglip2navit.py
@@ -189,7 +189,7 @@ def apply_rotary_pos_emb(
 ) -> tuple[torch.Tensor, torch.Tensor]:
     cos = cos.chunk(2, dim=-1)[0].contiguous()
     sin = sin.chunk(2, dim=-1)[0].contiguous()
-    if is_flash_attn_backend and not current_platform.is_xpu():
+    if is_flash_attn_backend and current_platform.is_cuda():
         from vllm.vllm_flash_attn.layers.rotary import apply_rotary_emb
 
         apply_rotary_emb_func = apply_rotary_emb
