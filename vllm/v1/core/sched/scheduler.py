ab3957891201117aa6d1f3d35918a57b9679794c
huanghaoyan.hhy <huanghaoyan.hhy@alibaba-inc.com>
2025-12-13 16:09:07 +0000

    fix prefill chunk incorrectly including draft tokens
    Signed-off-by: huanghaoyan.hhy <huanghaoyan.hhy@alibaba-inc.com>

--- vllm/v1/core/sched/scheduler.py
+++ vllm/v1/core/sched/scheduler.py
@@ -239,16 +239,6 @@ class Scheduler(SchedulerInterface):
             "External KV connector is not verified yet"
         )
         if request.num_output_tokens == 0:  # prefill
-            # Ensure new tokens for a request in the prefill phase do not contain
-            # draft tokens, especially in the last prefill chunk. For a hybrid-model,
-            # extra draft tokens would corrupt the generated Mamba state.
-            # TODO: This logic does not yet handle resumed requests.
-            if request.num_computed_tokens < request.num_prompt_tokens:
-                num_new_tokens = min(
-                    request.num_prompt_tokens - request.num_computed_tokens,
-                    num_new_tokens,
-                )
-
             # To enable block-aligned caching of the Mamba state, `num_new_tokens`
             # must be a multiple of `block_size`.
             # As an exception, if `num_new_tokens` is less than `block_size`, the
@@ -334,11 +324,24 @@ class Scheduler(SchedulerInterface):
                 req_index += 1
                 continue
 
-            num_new_tokens = (
-                request.num_tokens_with_spec
-                + request.num_output_placeholders
-                - request.num_computed_tokens
-            )
+            # Ensure new tokens for a request in the prefill phase do not contain
+            # draft tokens, especially in the last prefill chunk. For a hybrid-model,
+            # extra draft tokens would corrupt the generated Mamba state.
+            # TODO: This logic does not yet handle resumed requests.
+            if request.num_computed_tokens < request.num_prompt_tokens:
+                num_new_tokens = (
+                    min(
+                        request.num_tokens_with_spec + request.num_output_placeholders,
+                        request.num_prompt_tokens,
+                    )
+                    - request.num_computed_tokens
+                )
+            else:
+                num_new_tokens = (
+                    request.num_tokens_with_spec
+                    + request.num_output_placeholders
+                    - request.num_computed_tokens
+                )
             if 0 < self.scheduler_config.long_prefill_token_threshold < num_new_tokens:
                 num_new_tokens = self.scheduler_config.long_prefill_token_threshold
             num_new_tokens = min(num_new_tokens, token_budget)
