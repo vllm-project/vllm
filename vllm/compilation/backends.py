61d91c99de77cff6b6427081333efec38fbf7b2c
Ilya Markov <markovilya197@gmail.com>
2025-12-14 11:15:11 +0100

    [torch.compile] Add encoder tag for compilation (#30489)
    Signed-off-by: ilmarkov <markovilya197@gmail.com>
    Signed-off-by: dsuhinin <suhinin.dmitriy@gmail.com>

--- vllm/compilation/backends.py
+++ vllm/compilation/backends.py
@@ -463,21 +463,27 @@ class PiecewiseCompileInterpreter(torch.fx.Interpreter):
 # the tag for the part of model being compiled,
 # e.g. backbone/eagle_head
 model_tag: str = "backbone"
+model_is_encoder: bool = False
 
 
 @contextmanager
-def set_model_tag(tag: str):
+def set_model_tag(tag: str, is_encoder: bool = False):
     """Context manager to set the model tag."""
     global model_tag
+    global model_is_encoder
     assert tag != model_tag, (
         f"Model tag {tag} is the same as the current tag {model_tag}."
     )
     old_tag = model_tag
+    old_is_encoder = model_is_encoder
+
     model_tag = tag
+    model_is_encoder = is_encoder
     try:
         yield
     finally:
         model_tag = old_tag
+        model_is_encoder = old_is_encoder
 
 
 class VllmBackend:
@@ -522,6 +528,9 @@ class VllmBackend:
         # models, we need to use the model_tag to distinguish
         # them, e.g. backbone (default), eagle_head, etc.
         self.prefix = prefix or model_tag
+
+        # Mark compilation for encoder.
+        self.is_encoder = model_is_encoder
 
         # Passes to run on the graph post-grad.
         self.pass_manager = resolve_obj_by_qualname(
