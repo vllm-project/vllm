f93ada554bea9b8e7c17482c13e3be888fd7eacb
汪志鹏 <wangzhipeng628@gmail.com>
2025-12-15 14:58:23 +0800

    [New Model] BAGEL support (AR only) (#28439)
    Signed-off-by: princepride <wangzhipeng628@gmail.com>
    Signed-off-by: 汪志鹏 <wangzhipeng628@gmail.com>
    Co-authored-by: Cyrus Leung <tlleungac@connect.ust.hk>
    Signed-off-by: slava.sidik <slava.sidik@huawei.com>

--- vllm/transformers_utils/processors/__init__.py
+++ vllm/transformers_utils/processors/__init__.py
@@ -8,6 +8,7 @@ reasons:
 - There is a need to override the existing processor to support vLLM.
 """
 
+from vllm.transformers_utils.processors.bagel import BagelProcessor
 from vllm.transformers_utils.processors.deepseek_vl2 import DeepseekVLV2Processor
 from vllm.transformers_utils.processors.hunyuan_vl import HunYuanVLProcessor
 from vllm.transformers_utils.processors.hunyuan_vl_image import HunYuanVLImageProcessor
@@ -15,6 +16,7 @@ from vllm.transformers_utils.processors.ovis import OvisProcessor
 from vllm.transformers_utils.processors.ovis2_5 import Ovis2_5Processor
 
 __all__ = [
+    "BagelProcessor",
     "DeepseekVLV2Processor",
     "HunYuanVLProcessor",
     "HunYuanVLImageProcessor",
