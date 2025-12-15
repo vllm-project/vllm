443641cd5f219a76c103cc9d923a0b9df0d6e01c
Chauncey <chaunceyjiang@gmail.com>
2025-12-15 20:54:52 +0800

    [Refactor] [2/N] Move tool parsers into the vLLM main directory (#30675)
    Signed-off-by: chaunceyjiang <chaunceyjiang@gmail.com>

--- vllm/entrypoints/openai/serving_engine.py
+++ vllm/entrypoints/openai/serving_engine.py
@@ -59,7 +59,6 @@ from vllm.entrypoints.openai.protocol import (
     TranslationRequest,
 )
 from vllm.entrypoints.openai.serving_models import OpenAIServingModels
-from vllm.entrypoints.openai.tool_parsers import ToolParser, ToolParserManager
 from vllm.entrypoints.pooling.classify.protocol import (
     ClassificationChatRequest,
     ClassificationCompletionRequest,
@@ -104,6 +103,7 @@ from vllm.sampling_params import BeamSearchParams, SamplingParams
 from vllm.tokenizers import TokenizerLike
 from vllm.tokenizers.deepseek_v32 import DeepseekV32Tokenizer
 from vllm.tokenizers.mistral import MistralTokenizer
+from vllm.tool_parsers import ToolParser, ToolParserManager
 from vllm.tracing import (
     contains_trace_headers,
     extract_trace_headers,
