d23136a22f589b7e3eb1cc5ba74907a4185bc2fb
Mark McLoughlin <markmc@redhat.com>
2025-12-16 16:35:46 +0000

    [Docs][API] Remove warning about LoRARequest being internal-only (#30774)
    Signed-off-by: Mark McLoughlin <markmc@redhat.com>
    Signed-off-by: slava.sidik <slava.sidik@huawei.com>

--- vllm/lora/request.py
+++ vllm/lora/request.py
@@ -14,11 +14,6 @@ class LoRARequest(
     """
     Request for a LoRA adapter.
 
-    Note that this class should be used internally. For online
-    serving, it is recommended to not allow users to use this class but
-    instead provide another layer of abstraction to prevent users from
-    accessing unauthorized LoRA adapters.
-
     lora_int_id must be globally unique for a given adapter.
     This is currently not enforced in vLLM.
     """
