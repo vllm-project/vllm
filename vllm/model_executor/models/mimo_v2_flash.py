ed42a1ab85889176bd90154d4ceae23fc54ccb55
Abatom <abzhonghua@gmail.com>
2025-12-18 16:21:18 +0800

    Supports PP
    Signed-off-by: Abatom <abzhonghua@gmail.com>

--- vllm/model_executor/models/mimo_v2_flash.py
+++ vllm/model_executor/models/mimo_v2_flash.py
@@ -46,6 +46,7 @@ from vllm.model_executor.model_loader.weight_utils import (
 from vllm.model_executor.models.utils import sequence_parallel_chunk
 from vllm.sequence import IntermediateTensors
 
+from .interfaces import SupportsPP
 from .utils import (
     AutoWeightsLoader,
     PPMissingLayer,
@@ -660,7 +661,7 @@ class MiMoV2Model(nn.Module):
         return loaded_params
 
 
-class MiMoV2FlashForCausalLM(nn.Module):
+class MiMoV2FlashForCausalLM(nn.Module, SupportsPP):
     def __init__(self, *, vllm_config: VllmConfig, prefix: str = ""):
         super().__init__()
         config = vllm_config.model_config.hf_config
