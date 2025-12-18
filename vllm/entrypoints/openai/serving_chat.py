8d66c78fb7a1c7739b9424111b20f2dd4b0408ea
inkcherry <mingzhi.liu@amd.com>
2025-12-19 01:50:42 +0800

    [Bugfix] fix DP-aware routing in OpenAI API requests (#29002)
    Signed-off-by: inkcherry <mingzhi.liu@amd.com>
    Signed-off-by: dsuhinin <suhinin.dmitriy@gmail.com>

--- vllm/entrypoints/openai/serving_chat.py
+++ vllm/entrypoints/openai/serving_chat.py
@@ -381,6 +381,7 @@ class OpenAIServingChat(OpenAIServing):
                         lora_request=lora_request,
                         trace_headers=trace_headers,
                         priority=request.priority,
+                        data_parallel_rank=data_parallel_rank,
                     )
 
                     generator = self.engine_client.generate(
