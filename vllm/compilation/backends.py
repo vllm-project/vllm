c62e0031c3a4b4cd0e72b7f6adabdebe7d103417
Lucas Kabela <lucaskabela@meta.com>
2025-12-18 08:23:31 -0800

    [Bugfix][torch2.10] Fix test_qwen2_5_vl_compilation with 2.10 RC (#30822)
    Signed-off-by: Lucas Kabela <lucaskabela@meta.com>
    Co-authored-by: Cyrus Leung <tlleungac@connect.ust.hk>
    Signed-off-by: dsuhinin <suhinin.dmitriy@gmail.com>

--- vllm/compilation/backends.py
+++ vllm/compilation/backends.py
@@ -520,6 +520,7 @@ class VllmBackend:
         self,
         vllm_config: VllmConfig,
         prefix: str = "",
+        is_encoder: bool = False,
     ):
         # if the model is initialized with a non-empty prefix,
         # then usually it's enough to use that prefix,
@@ -530,7 +531,7 @@ class VllmBackend:
         self.prefix = prefix or model_tag
 
         # Mark compilation for encoder.
-        self.is_encoder = model_is_encoder
+        self.is_encoder = is_encoder or model_is_encoder
 
         # Passes to run on the graph post-grad.
         self.pass_manager = resolve_obj_by_qualname(
@@ -797,7 +798,7 @@ class VllmBackend:
             or not self.compilation_config.cudagraph_copy_inputs
         ):
             return VllmSerializableFunction(
-                graph, example_inputs, self.prefix, self.split_gm
+                graph, example_inputs, self.prefix, self.split_gm, self.is_encoder
             )
 
         # index of tensors that have symbolic shapes (batch size)
@@ -835,5 +836,5 @@ class VllmBackend:
             return self.split_gm(*list_args)
 
         return VllmSerializableFunction(
-            graph, example_inputs, self.prefix, copy_and_call
+            graph, example_inputs, self.prefix, copy_and_call, self.is_encoder
         )
