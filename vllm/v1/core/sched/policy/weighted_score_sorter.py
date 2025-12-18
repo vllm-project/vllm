ab2cfa94b453c4fa197b9dfd9405337c8f5d7536
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-12-18 17:25:03 +0100

    Remove tuple stuff
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>
    Signed-off-by: weichen <calvin_zhu0210@outlook.com>

--- vllm/v1/core/sched/policy/weighted_score_sorter.py
+++ vllm/v1/core/sched/policy/weighted_score_sorter.py
@@ -2,9 +2,9 @@
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 import time
 from functools import total_ordering
-from typing import Any
 
 from vllm.v1.core.sched.policy.normalized_scorer import TimeAndLengthScorer
+from vllm.v1.request import Request
 
 TimeAndLengthScorer_Instance = None
 
@@ -20,15 +20,11 @@ if TimeAndLengthScorer_Instance is None:
 
 @total_ordering
 class WeightedScoreSorter:
-    def __init__(
-        self,
-        request_length: int,
-        request_arrival_time: float,
-        request_slo_requirement: list[Any] | None = None,
-    ):
-        self.request_length = request_length
-        self.request_arrival_time = request_arrival_time
-        self.request_slo_requirement = request_slo_requirement
+    def __init__(self, request: Request):
+        self.request = request
+        assert request.prompt_token_ids is not None
+        self.request_length = len(request.prompt_token_ids)
+        self.request_arrival_time = request.arrival_time
         self.__update_stats()
 
     def __lt__(self, other_request_weighted_score: "WeightedScoreSorter") -> bool:
