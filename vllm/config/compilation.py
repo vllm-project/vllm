c9ad7928292093c2b6bdce08348a7c7867c247fb
Zhengxu Chen <zhxchen17@fb.com>
2025-12-17 23:22:23 -0500

    [compile] Fix CI for test_gpt2_cache_hit (#30902)
    Signed-off-by: zhxchen17 <zhxchen17@fb.com>
    Signed-off-by: slava.sidik <slava.sidik@huawei.com>

--- vllm/config/compilation.py
+++ vllm/config/compilation.py
@@ -437,14 +437,14 @@ class CompilationConfig:
 
     compile_ranges_split_points: list[int] | None = None
     """Split points that represent compile ranges for inductor.
-    The compile ranges are 
-    [1, split_points[0]], 
-    [split_points[0] + 1, split_points[1]], ..., 
+    The compile ranges are
+    [1, split_points[0]],
+    [split_points[0] + 1, split_points[1]], ...,
     [split_points[-1] + 1, max_num_batched_tokens].
     Compile sizes are also used single element ranges,
     the range is represented as [compile_sizes[i], compile_sizes[i]].
-    
-    If a range overlaps with the compile size, graph for compile size 
+
+    If a range overlaps with the compile size, graph for compile size
     will be prioritized, i.e. if we have a range [1, 8] and a compile size 4,
     graph for compile size 4 will be compiled and used instead of the graph
     for range [1, 8].
