b26aaf7dc4bcd9ea0a3cb193a818a7135baa2dda
Jee Jee Li <pandaleefree@gmail.com>
2025-12-19 02:10:57 +0000

    Revert
    Signed-off-by: Jee Jee Li <pandaleefree@gmail.com>

--- vllm/model_executor/models/glm4_moe.py
+++ vllm/model_executor/models/glm4_moe.py
@@ -310,16 +310,12 @@ class Glm4MoeAttention(nn.Module):
         qkv, _ = self.qkv_proj(hidden_states)
         q, k, v = qkv.split([self.q_size, self.kv_size, self.kv_size], dim=-1)
         if self.use_qk_norm:
-            q_by_head = q.view(
-                *q.shape[:-1], q.shape[-1] // self.head_dim, self.head_dim
+            q = self.q_norm(q.reshape(-1, self.num_heads, self.head_dim)).reshape(
+                q.shape
             )
-            q_by_head = self.q_norm(q_by_head)
-            q = q_by_head.view(q.shape)
-            k_by_head = k.view(
-                *k.shape[:-1], k.shape[-1] // self.head_dim, self.head_dim
+            k = self.k_norm(k.reshape(-1, self.num_kv_heads, self.head_dim)).reshape(
+                k.shape
             )
-            k_by_head = self.k_norm(k_by_head)
-            k = k_by_head.view(k.shape)
 
         q, k = self.rotary_emb(positions, q, k)
         attn_output = self.attn(q, k, v)
