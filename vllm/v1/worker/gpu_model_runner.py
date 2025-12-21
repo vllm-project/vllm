c4886538c5beb5e2d284ee4d5bec0867a048ea5a
Shahar Mor <smor@nvidia.com>
2025-12-21 08:37:41 +0000

    fully working mixed requests version
    Signed-off-by: Shahar Mor <smor@nvidia.com>

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -100,6 +100,7 @@ from vllm.utils.torch_utils import (
     supports_dynamo,
 )
 from vllm.v1.attention.backends.gdn_attn import GDNAttentionMetadataBuilder
+from vllm.v1.attention.backends.mamba2_attn import Mamba2AttentionMetadataBuilder
 from vllm.v1.attention.backends.utils import (
     AttentionCGSupport,
     AttentionMetadataBuilder,
@@ -1653,7 +1654,9 @@ class GPUModelRunner(
             )
 
             extra_attn_metadata_args = {}
-            if use_spec_decode and isinstance(builder, GDNAttentionMetadataBuilder):
+            if use_spec_decode and isinstance(
+                builder, (GDNAttentionMetadataBuilder, Mamba2AttentionMetadataBuilder)
+            ):
                 assert ubid is None, "UBatching not supported with GDN yet"
                 extra_attn_metadata_args = dict(
                     num_accepted_tokens=self.num_accepted_tokens.gpu[:num_reqs_padded],
@@ -3600,6 +3603,14 @@ class GPUModelRunner(
                 common_attn_metadata=common_attn_metadata,
                 mm_embed_inputs=mm_embed_inputs,
             )
+            
+            # TODO smor - a patch, need  to solve in a more robust way
+            draft_token_ids = draft_token_ids.tolist()
+            for i in range(common_attn_metadata.num_reqs):
+                req_id = self.input_batch.req_ids[i]
+                if req_id in self.input_batch.spec_decode_unsupported_reqs:
+                    draft_token_ids[i] = []
+            
 
         return draft_token_ids
 
