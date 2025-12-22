7c3d40c65f2233313cc1305d8dc4f746ab0c1eac
penfree <qiupengfei@baidu.com>
2025-12-22 09:33:25 +0800

    [Bugfix] add token usage counting for ParsableContext
    Signed-off-by: penfree <qiupengfei@baidu.com>

--- vllm/entrypoints/openai/parser/responses_parser.py
+++ vllm/entrypoints/openai/parser/responses_parser.py
@@ -300,7 +300,12 @@ class StreamableResponsesParser:
                     previous_token_ids=self._previous_content_token_ids,
                     current_token_ids=current_content_token_ids,
                     delta_token_ids=delta_token_ids,
-                    request=self.request,
+                    # TODO： currently, only some tool parser need to access
+                    # request.tools in extract_tool_calls_streaming, and
+                    # ResponsesRequest have this attribute too. Maybe we should
+                    # update ToolParser params to allow ResponsesRequest along
+                    # with ChatCompletionRequest.
+                    request=self.request,  # type: ignore
                 )
 
                 if tool_delta is not None:
