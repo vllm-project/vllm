e78a4cc0e8e7cd9781ae4187575a0a98ab1f9760
rongfu.leng <rongfu.leng@daocloud.io>
2025-12-24 10:22:35 +0800

    [Misc] Remove unused custom ops `copy_blocks` and `copy_blocks_mla` (#30967)
    Signed-off-by: rongfu.leng <rongfu.leng@daocloud.io>
    Signed-off-by: slava.sidik <slava.sidik@huawei.com>

--- csrc/cache.h
+++ csrc/cache.h
@@ -9,16 +9,6 @@
 void swap_blocks(torch::Tensor& src, torch::Tensor& dst,
                  const torch::Tensor& block_mapping);
 
-// Note: the key_caches and value_caches vectors are constant but
-// not the Tensors they contain. The vectors need to be const refs
-// in order to satisfy pytorch's C++ operator registration code.
-void copy_blocks(std::vector<torch::Tensor> const& key_caches,
-                 std::vector<torch::Tensor> const& value_caches,
-                 const torch::Tensor& block_mapping);
-
-void copy_blocks_mla(std::vector<torch::Tensor> const& kv_caches,
-                     const torch::Tensor& block_mapping);
-
 void reshape_and_cache(torch::Tensor& key, torch::Tensor& value,
                        torch::Tensor& key_cache, torch::Tensor& value_cache,
                        torch::Tensor& slot_mapping,
