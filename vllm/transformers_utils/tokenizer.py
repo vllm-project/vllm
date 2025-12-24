d70acbf7ff2404e282589cf8e8eefbefcabe48e7
DarkLight1337 <tlleungac@connect.ust.hk>
2025-12-24 15:42:44 +0000

    Compat
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

---
+++ vllm/transformers_utils/tokenizer.py
@@ -0,0 +1,19 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+import warnings
+
+
+def __getattr__(name: str):
+    # Keep until lm-eval is updated
+    if name == "get_tokenizer":
+        from vllm.tokenizers import get_tokenizer
+
+        warnings.warn(
+            "`vllm.transformers_utils.tokenizer.get_tokenizer` "
+            "has been moved to `vllm.tokenizers.get_tokenizer`. "
+            "The old name will be removed in v0.14.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+
+        return get_tokenizer
