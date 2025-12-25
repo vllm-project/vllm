77bb403cedc6189a76a55e8409309e3629381b63
fangyuchu <fangyuchu@qq.com>
2025-12-25 09:49:42 +0800

    ClientSentinel: add extra check to prevent repeated pause commands on error
    Signed-off-by: fangyuchu <fangyuchu@qq.com>

--- vllm/v1/engine/core_client.py
+++ vllm/v1/engine/core_client.py
@@ -391,6 +391,8 @@ class ClientSentinel(BaseSentinel):
             fault_tolerance_config=fault_tolerance_config,
         )
         self.is_faulted = threading.Event()
+        self.engine_running = threading.Event()
+        self.engine_running.set()
         self.engine_registry = engine_registry
         self.engine_exception_q: queue.Queue[FaultInfo] = engine_exception_q
         self.engine_status_dict: ThreadSafeDict[int, str] = engine_status_dict
@@ -436,9 +438,10 @@ class ClientSentinel(BaseSentinel):
                 # Pause can be invoked again during fault-tolerance handling,
                 # so it's unnecessary to track whether all engines are currently
                 # paused.
-                self._submit_task(
-                    "pause", self.ft_config.gloo_comm_timeout, soft_pause=False
-                )
+                if self.engine_running.is_set():
+                    self._submit_task(
+                        "pause", self.ft_config.gloo_comm_timeout, soft_pause=False
+                    )
             except zmq.ZMQError:
                 # Socket is closed.
                 break
@@ -500,6 +503,7 @@ class ClientSentinel(BaseSentinel):
 
         if success:
             self.is_faulted.clear()
+            self.engine_running.set()
         return success
 
     def pause(self, timeout: int = 1, soft_pause: bool = True):
@@ -512,7 +516,7 @@ class ClientSentinel(BaseSentinel):
             "the instance.",
             level="warning",
         )
-
+        self.engine_running.clear()
         alive_engines = {
             identity
             for identity, index in self.engine_identity_to_index.items()
