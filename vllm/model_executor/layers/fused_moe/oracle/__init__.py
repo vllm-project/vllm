a8c5927eab80a9b83129ac0871c3b51648e6a1e6
Robert Shaw <robshaw@redhat.com>
2025-12-27 18:45:33 +0000

    updated with proper impots
    Signed-off-by: Robert Shaw <robshaw@redhat.com>

--- vllm/model_executor/layers/fused_moe/oracle/__init__.py
+++ vllm/model_executor/layers/fused_moe/oracle/__init__.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
-from fp8 import Fp8MoeBackend, get_fp8_moe_backend
+from .fp8 import Fp8MoeBackend, get_fp8_moe_backend
 
 __all__ = ["Fp8MoeBackend", "get_fp8_moe_backend"]
