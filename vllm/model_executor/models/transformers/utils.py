c40f84743b1aba206de79f5022b2c0d99fd2fe20
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-12-29 15:10:36 +0100

    Edtend to 3D
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- vllm/model_executor/models/transformers/utils.py
+++ vllm/model_executor/models/transformers/utils.py
@@ -25,7 +25,7 @@ from torch import nn
 
 from vllm.config.utils import getattr_iter
 from vllm.logger import init_logger
-from vllm.model_executor.layers.conv import Conv2dLayer
+from vllm.model_executor.layers.conv import Conv2dLayer, Conv3dLayer
 from vllm.model_executor.layers.layernorm import GemmaRMSNorm, RMSNorm
 from vllm.model_executor.layers.linear import (
     ColumnParallelLinear,
@@ -137,26 +137,42 @@ def replace_linear_class(
     )
 
 
-def replace_conv2d_class(conv2d: nn.Conv2d) -> Conv2dLayer:
-    """
-    Replace nn.Conv2d with vLLM's Conv2dLayer.
+TorchConv = nn.Conv2d | nn.Conv3d
+VllmConv = Conv2dLayer | Conv3dLayer
+
+
+def replace_conv_class(conv: TorchConv) -> VllmConv | TorchConv:
+    """Replace a Transformers Conv2d/Conv3d with vLLM's Conv2d/Conv3d.
 
     Args:
-        conv2d: `nn.Conv2d` to be replaced.
+        conv: `nn.Conv2d` or `nn.Conv3d` to be replaced.
     Returns:
-        The new Conv2dLayer.
+        The new `Conv2dLayer` or `Conv3dLayer`. If the conv module is not supported,
+        returns the original conv module.
     """
-    return Conv2dLayer(
-        in_channels=conv2d.in_channels,
-        out_channels=conv2d.out_channels,
-        kernel_size=conv2d.kernel_size,
-        stride=conv2d.stride,
-        padding=conv2d.padding,
-        dilation=conv2d.dilation,
-        groups=conv2d.groups,
-        bias=conv2d.bias is not None,
-        padding_mode=conv2d.padding_mode,
-        params_dtype=conv2d.weight.dtype,
+    # vLLM does not handle non-zero padding modes
+    if conv.padding_mode != "zeros":
+        return conv
+
+    vllm_conv_cls = {
+        nn.Conv2d: Conv2dLayer,
+        nn.Conv3d: Conv3dLayer,
+    }.get(type(conv))
+
+    if vllm_conv_cls is None:
+        return conv
+
+    return vllm_conv_cls(
+        in_channels=conv.in_channels,
+        out_channels=conv.out_channels,
+        kernel_size=conv.kernel_size,
+        stride=conv.stride,
+        padding=conv.padding,
+        dilation=conv.dilation,
+        groups=conv.groups,
+        bias=conv.bias is not None,
+        padding_mode=conv.padding_mode,
+        params_dtype=conv.weight.dtype,
     )
 
 
