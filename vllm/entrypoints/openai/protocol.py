d0a4fcc4cf3f65263108a191e214a914d8d896e1
Jackmin801 <ongjackm@gmail.com>
2025-12-29 22:30:45 +0000

    add option to entrypoint
    Signed-off-by: Jackmin801 <ongjackm@gmail.com>

--- vllm/entrypoints/openai/protocol.py
+++ vllm/entrypoints/openai/protocol.py
@@ -2006,6 +2006,7 @@ class TokenizerInfoResponse(OpenAIBaseModel):
 class LoadLoRAAdapterRequest(BaseModel):
     lora_name: str
     lora_path: str
+    load_inplace: bool = True
 
 
 class UnloadLoRAAdapterRequest(BaseModel):
