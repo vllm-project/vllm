6cefc2347a8b6c7bfaeb1f3a3f2d085404c1391f
Robert Shaw <robshaw@redhat.com>
2025-12-30 15:25:52 +0000

    split into separate situation
    Signed-off-by: Robert Shaw <robshaw@redhat.com>

--- vllm/model_executor/layers/fused_moe/triton_deep_gemm_moe.py
+++ vllm/model_executor/layers/fused_moe/triton_deep_gemm_moe.py
@@ -1,7 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
-from abc import ABC, abstractmethod
 
 import torch
 
@@ -12,120 +11,11 @@ from vllm.model_executor.layers.fused_moe.deep_gemm_moe import (
     _valid_deep_gemm,
     _valid_deep_gemm_shape,
 )
+from vllm.model_executor.layers.fused_moe.fallback import FallbackExperts
 from vllm.model_executor.layers.fused_moe.fused_moe import TritonExperts
 from vllm.utils.deep_gemm import (
     is_deep_gemm_e8m0_used,
 )
-
-
-class FallbackExperts(ABC, mk.FusedMoEPermuteExpertsUnpermute):
-    """Base class for runtime dispatching of expert implementations."""
-
-    def __init__(
-        self,
-        experts: mk.FusedMoEPermuteExpertsUnpermute,
-        fallback_experts: mk.FusedMoEPermuteExpertsUnpermute,
-    ):
-        super().__init__(experts.quant_config)
-        self.fallback_experts = fallback_experts
-        self.experts = experts
-
-    @property
-    def activation_formats(
-        self,
-    ) -> tuple[mk.FusedMoEActivationFormat, mk.FusedMoEActivationFormat]:
-        assert (
-            self.fallback_experts.activation_formats == self.experts.activation_formats
-        )
-        return self.fallback_experts.activation_formats
-
-    def supports_chunking(self) -> bool:
-        return (
-            self.experts.supports_chunking()
-            and self.fallback_experts.supports_chunking()
-        )
-
-    def supports_expert_map(self) -> bool:
-        return (
-            self.experts.supports_expert_map()
-            and self.fallback_experts.supports_expert_map()
-        )
-
-    def finalize_weight_and_reduce_impl(self) -> mk.TopKWeightAndReduce:
-        e_war = self.experts.finalize_weight_and_reduce_impl()
-        fbe_war = self.fallback_experts.finalize_weight_and_reduce_impl()
-        is_dge_war = e_war is not None
-        is_fbe_war = fbe_war is not None
-
-        if is_dge_war and is_fbe_war:
-            assert e_war == fbe_war, (
-                "Both implementations should agree on WeightAndReduce impls. "
-                f"Got e_war: {e_war}, and fbe_war: {fbe_war}"
-            )
-
-        if e_war is not None:
-            return e_war
-        assert fbe_war is not None
-        return fbe_war
-
-    @abstractmethod
-    def workspace_shapes(
-        self,
-        M: int,
-        N: int,
-        K: int,
-        topk: int,
-        global_num_experts: int,
-        local_num_experts: int,
-        expert_tokens_meta: mk.ExpertTokensMetadata | None,
-    ) -> tuple[tuple[int, ...], tuple[int, ...], tuple[int, ...]]:
-        raise NotImplementedError
-
-    @abstractmethod
-    def _select_experts_impl(
-        self,
-        hidden_states: torch.Tensor,
-        w1: torch.Tensor,
-        w2: torch.Tensor,
-    ) -> mk.FusedMoEPermuteExpertsUnpermute:
-        raise NotImplementedError
-
-    def apply(
-        self,
-        output: torch.Tensor,
-        hidden_states: torch.Tensor,
-        w1: torch.Tensor,
-        w2: torch.Tensor,
-        topk_weights: torch.Tensor,
-        topk_ids: torch.Tensor,
-        activation: str,
-        global_num_experts: int,
-        expert_map: torch.Tensor | None,
-        a1q_scale: torch.Tensor | None,
-        a2_scale: torch.Tensor | None,
-        workspace13: torch.Tensor,
-        workspace2: torch.Tensor,
-        expert_tokens_meta: mk.ExpertTokensMetadata | None,
-        apply_router_weight_on_input: bool,
-    ):
-        experts = self._select_experts_impl(hidden_states, w1, w2)
-        experts.apply(
-            output,
-            hidden_states,
-            w1,
-            w2,
-            topk_weights,
-            topk_ids,
-            activation,
-            global_num_experts,
-            expert_map,
-            a1q_scale,
-            a2_scale,
-            workspace13,
-            workspace2,
-            expert_tokens_meta,
-            apply_router_weight_on_input,
-        )
 
 
 class TritonOrDeepGemmExperts(FallbackExperts):
