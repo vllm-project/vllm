0f9b934a4d482e6929783c6d6d6d0eaba1e85ff2
X <x@simulatte.world>
2026-01-01 15:02:51 -0500

    rename SonicMoEExperts to SonicMoeExperts for title case consistency
    Signed-off-by: X <x@simulatte.world>

--- vllm/model_executor/layers/fused_moe/__init__.py
+++ vllm/model_executor/layers/fused_moe/__init__.py
@@ -22,7 +22,7 @@ from vllm.model_executor.layers.fused_moe.modular_kernel import (
 )
 from vllm.model_executor.layers.fused_moe.shared_fused_moe import SharedFusedMoE
 from vllm.model_executor.layers.fused_moe.sonic_moe import (
-    SonicMoEExperts,
+    SonicMoeExperts,
     is_sonic_moe_supported,
     is_valid_sonic_moe,
     permute_weights_for_sonic,
@@ -72,7 +72,7 @@ __all__ = [
     "is_valid_sonic_moe",
     "sonic_moe_forward",
     "permute_weights_for_sonic",
-    "SonicMoEExperts",
+    "SonicMoeExperts",
 ]
 
 if HAS_TRITON:
