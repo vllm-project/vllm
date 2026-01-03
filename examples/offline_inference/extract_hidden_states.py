6ff11acaa3cf755dd34b83dd10a93f8deb718d33
Fynn Schmitt-Ulms <fschmitt@redhat.com>
2026-01-03 03:07:55 +0000

    Add safetensors output to example script
    Signed-off-by: Fynn Schmitt-Ulms <fschmitt@redhat.com>

--- examples/offline_inference/extract_hidden_states.py
+++ examples/offline_inference/extract_hidden_states.py
@@ -1,3 +1,5 @@
+from safetensors import safe_open
+
 from vllm import LLM, SamplingParams
 
 # Example: Using the custom "extract_hidden_states" speculator method
@@ -36,7 +38,17 @@ outputs = llm.generate(prompts, sampling_params)
 for output in outputs:
     print("\nPrompt:", output.prompt)
     print("Prompt token ids:", output.prompt_token_ids)
-    print(
-        "Prompt hidden states path:",
-        output.kv_transfer_params.get("hidden_states_path"),
-    )
+
+    hidden_states_path = output.kv_transfer_params.get("hidden_states_path")
+    assert hidden_states_path is not None
+    print("Prompt hidden states path:", hidden_states_path)
+
+    with safe_open(hidden_states_path, "pt") as f:
+        token_ids = f.get_tensor("token_ids")
+        hidden_states = f.get_tensor("hidden_states")
+
+        print("Extracted token ids:", token_ids)  # Matches prompt token ids
+        print(
+            "Extracted hidden states shape:", hidden_states.shape
+        )  # [num_hidden_layers, prompt len, hidden size]
+        print("Extracted hidden states:", hidden_states)
