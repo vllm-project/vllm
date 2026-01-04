c10151a63f286c7702660d1a36a137fa2d533dcb
wang.yuqi <yuqi.wang@daocloud.io>
2026-01-04 15:00:41 +0800

    update
    Signed-off-by: wang.yuqi <yuqi.wang@daocloud.io>

--- vllm/model_executor/models/config.py
+++ vllm/model_executor/models/config.py
@@ -137,6 +137,8 @@ class NomicBertModelConfig(VerifyAndUpdateConfig):
         config.intermediate_size = config.n_inner
         config.hidden_size = config.n_embd
         config.num_hidden_layers = config.n_layer
+        model_config.model_arch_config.hidden_size = config.hidden_size
+        model_config.model_arch_config.num_hidden_layers = config.num_hidden_layers
 
         head_dim = config.hidden_size // config.num_attention_heads
         max_trained_positions = getattr(config, "max_trained_positions", 2048)
