7f85a8f0c6511dd16a390714f6df8723af6324d9
Andy Liu <andyliu@roblox.com>
2026-01-05 10:27:25 -0800

    use torch.where to avoid d2h operation
    Signed-off-by: Andy Liu <andyliu@roblox.com>

--- vllm/model_executor/models/glm4_moe_mtp.py
+++ vllm/model_executor/models/glm4_moe_mtp.py
@@ -106,7 +106,7 @@ class Glm4MoeMultiTokenPredictorLayer(nn.Module):
     ) -> torch.Tensor:
         assert inputs_embeds is not None
         # masking inputs at position 0, as not needed by MTP
-        inputs_embeds[positions == 0] = 0
+        inputs_embeds = torch.where(positions.unsqueeze(-1) == 0, 0, inputs_embeds)
         inputs_embeds = self.enorm(inputs_embeds)
         previous_hidden_states = self.hnorm(previous_hidden_states)
 
