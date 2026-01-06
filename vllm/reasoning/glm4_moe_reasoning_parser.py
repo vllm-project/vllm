683c642a88b4c020448fb192243eafac633cdf1e
chaunceyjiang <chaunceyjiang@gmail.com>
2026-01-06 15:50:18 +0800

    [Frontend] Support GLM-4.5 / GLM-4.7 with enable_thinking: false
    Signed-off-by: chaunceyjiang <chaunceyjiang@gmail.com>

--- vllm/reasoning/glm4_moe_reasoning_parser.py
+++ vllm/reasoning/glm4_moe_reasoning_parser.py
@@ -1,10 +1,10 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
-from vllm.reasoning.deepseek_r1_reasoning_parser import DeepSeekR1ReasoningParser
+from vllm.reasoning.holo2_reasoning_parser import Holo2ReasoningParser
 
 
-class Glm4MoeModelReasoningParser(DeepSeekR1ReasoningParser):
+class Glm4MoeModelReasoningParser(Holo2ReasoningParser):
     """
     Reasoning parser for the Glm4MoeModel model is same as DeepSeekR1ReasoningParser.
     """
