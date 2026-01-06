3ceb254e47250a74c955a794705caca457dd4d3a
Robert Shaw <robshaw@redhat.com>
2026-01-06 16:34:36 +0000

    nit
    Signed-off-by: Robert Shaw <robshaw@redhat.com>

--- vllm/model_executor/layers/fused_moe/triton_deep_gemm_moe.py
+++ vllm/model_executor/layers/fused_moe/triton_deep_gemm_moe.py
@@ -1,7 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
-
 import torch
 
 import vllm.model_executor.layers.fused_moe.modular_kernel as mk
