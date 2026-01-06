0a6335db01c9a31b81189c785ea9dc75136ac5cf
Ryan Rock <ryan.rock@amd.com>
2026-01-06 21:06:09 +0000

    [CI/Build] Enable test_kv_cache_events_dp for AMD
    Signed-off-by: Ryan Rock <ryan.rock@amd.com>

--- tests/v1/engine/test_engine_core_client.py
+++ tests/v1/engine/test_engine_core_client.py
@@ -37,7 +37,7 @@ from vllm.v1.executor.abstract import Executor
 from ...distributed.conftest import MockSubscriber
 from ...utils import create_new_process_for_each_test
 
-if not current_platform.is_cuda():
+if not current_platform.is_cuda_alike():
     pytest.skip(reason="V1 currently only supported on CUDA.", allow_module_level=True)
 
 MODEL_NAME = "meta-llama/Llama-3.2-1B-Instruct"
