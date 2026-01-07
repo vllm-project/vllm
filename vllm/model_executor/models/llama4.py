fefa376e1735f97fcd16e18be57818853d0492ce
Robert Shaw <robshaw@redhat.com>
2026-01-07 03:29:11 +0000

    delete llama 4 load time optim
    Signed-off-by: Robert Shaw <robshaw@redhat.com>

--- vllm/model_executor/models/llama4.py
+++ vllm/model_executor/models/llama4.py
@@ -432,7 +432,6 @@ class Llama4Model(LlamaModel):
 
         # Whether the MoE expert weights are loaded successfully.
         expert_param_loaded = False
-        # DELETE ME!
         loaded_weight = loaded_weight.to("cuda")
 
         # If fused is True, the loaded weight is in the layout of:
