ced69649969baf916cb659568935f1ad7c4d6c74
zxy <zhou0493@e.ntu.edu.sg>
2026-01-07 18:11:28 +0800

    minor
    Signed-off-by: zxy <zhou0493@e.ntu.edu.sg>

--- vllm/model_executor/models/interns1_pro.py
+++ vllm/model_executor/models/interns1_pro.py
@@ -22,7 +22,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Inference-only InternS1_1 model compatible with HuggingFace weights."""
+"""Inference-only InternS1Pro model compatible with HuggingFace weights."""
 
 import typing
 from collections.abc import Callable, Iterable
