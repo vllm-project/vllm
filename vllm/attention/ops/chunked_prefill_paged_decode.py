f7e8dc3cc3c969fee5634ab707aea9d96e90dfc9
BlankR <hjyblanche@gmail.com>
2026-01-07 01:00:16 -0800

    [Misc] Improve error messages for unsupported types and parameters (#30593)
    Signed-off-by: BlankR <hjyblanche@gmail.com>
    Co-authored-by: Wentao Ye <44945378+yewentao256@users.noreply.github.com>

--- vllm/attention/ops/chunked_prefill_paged_decode.py
+++ vllm/attention/ops/chunked_prefill_paged_decode.py
@@ -292,7 +292,10 @@ def chunked_prefill_paged_decode(
         elif kv_cache_dtype == "fp8_e5m2":
             target_dtype = torch.float8_e5m2
         else:
-            raise ValueError("Unsupported FP8 dtype:", kv_cache_dtype)
+            raise ValueError(
+                f"Unsupported FP8 kv_cache_dtype {kv_cache_dtype}: "
+                f"should be one of 'fp8', 'fp8_e4m3', 'fp8_e5m2'."
+            )
 
         key_cache = key_cache.view(target_dtype)
         value_cache = value_cache.view(target_dtype)
