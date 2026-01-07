c39ccb0ae1bb223ac1adfe91a236b4505a33a35e
elvischenv <219235043+elvischenv@users.noreply.github.com>
2026-01-07 00:57:17 -0800

    add VLLM_USE_FLASHINFER_ROPE to envs
    Signed-off-by: elvischenv <219235043+elvischenv@users.noreply.github.com>

--- vllm/model_executor/layers/rotary_embedding/base.py
+++ vllm/model_executor/layers/rotary_embedding/base.py
@@ -4,8 +4,11 @@
 
 import torch
 
+from vllm import envs
 from vllm._aiter_ops import rocm_aiter_ops
 from vllm.model_executor.custom_op import CustomOp
+from vllm.platforms import current_platform
+from vllm.utils.flashinfer import has_flashinfer
 
 from .common import ApplyRotaryEmb
 
@@ -34,18 +37,20 @@ class RotaryEmbeddingBase(CustomOp):
         self.base = base
         self.is_neox_style = is_neox_style
         self.dtype = dtype
-        # TODO(mgoin): disabled for now due to failures
-        # Flashinfer only supports head_size=64, 128, 256, 512.
-        # https://github.com/flashinfer-ai/flashinfer/blob/ebfd655efe830048dba5d582aaa61d61d1cf9a87/include/flashinfer/utils.cuh#L174-L202
-        # self.use_flashinfer = (self.enabled()
-        #                        and dtype in (torch.float16, torch.bfloat16)
-        #                        and current_platform.is_cuda()
-        #                        and has_flashinfer()
-        #                        and self.head_size in [64, 128, 256, 512])
 
         # Check if use_flashinfer is already set
         if not hasattr(self, "use_flashinfer"):
-            self.use_flashinfer = False
+            # TODO(mgoin): VLLM_USE_FLASHINFER_ROPE is disabled for now due to failures
+            # Flashinfer only supports head_size=64, 128, 256, 512.
+            # https://github.com/flashinfer-ai/flashinfer/blob/ebfd655efe830048dba5d582aaa61d61d1cf9a87/include/flashinfer/utils.cuh#L174-L202
+            self.use_flashinfer = (
+                self.enabled()
+                and dtype in (torch.float16, torch.bfloat16)
+                and current_platform.is_cuda()
+                and has_flashinfer()
+                and self.head_size in [64, 128, 256, 512]
+                and envs.VLLM_USE_FLASHINFER_ROPE
+            )
 
         if init_cache:
             cache = self._compute_cos_sin_cache()
