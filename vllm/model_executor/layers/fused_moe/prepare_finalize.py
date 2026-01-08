f8851e0a53477021590fb69666e1606fc4fd7c09
Robert Shaw <robshaw@redhat.com>
2026-01-07 19:01:47 -0500

    stash
    Signed-off-by: Robert Shaw <robshaw@redhat.com>

--- vllm/model_executor/layers/fused_moe/prepare_finalize.py
+++ vllm/model_executor/layers/fused_moe/prepare_finalize.py
@@ -11,12 +11,96 @@ from vllm.model_executor.layers.fused_moe.topk_weight_and_reduce import (
 )
 from vllm.model_executor.layers.fused_moe.utils import moe_kernel_quantize_input
 
+class MoEPrepareAndFinalizeNaiveEP(mk.FusedMoEPrepareAndFinalize):
+    def __init__(self) -> None:
+        super().__init__()
+        self.dummy_tensor = torch.empty(1, device='cuda')
+
+    @property
+    def activation_format(self) -> mk.FusedMoEActivationFormat:
+        return mk.FusedMoEActivationFormat.Standard
+
+    def max_num_tokens_per_rank(self) -> int | None:
+        return None
+
+    def topk_indices_dtype(self) -> torch.dtype | None:
+        return None
+
+    def num_dispatchers(self) -> int:
+        return 1
+
+    def output_is_reduced(self) -> bool:
+        return False
+    
+    def prepare(
+        self,
+        a1: torch.Tensor,
+        topk_weights: torch.Tensor,
+        topk_ids: torch.Tensor,
+        num_experts: int,
+        expert_map: torch.Tensor | None,
+        apply_router_weight_on_input: bool,
+        quant_config: FusedMoEQuantConfig,
+    ) -> mk.PrepareResultType:
+        from vllm.distributed import get_ep_group
+
+        if not hasattr(self, "dummy_tensor"):
+            self.dummy_tensor = torch.zeros(1, device='cuda')
+
+        extra_tensors = [topk_weights,topk_ids]
+        a1, _, extra_tensors = get_ep_group().dispatch(
+            a1,
+            self.dummy_tensor,
+            is_sequence_parallel=False, # TODO?
+            extra_tensors=extra_tensors,
+        )
+        topk_weights, topk_ids = extra_tensors
+
+        a1q, a1q_scale = moe_kernel_quantize_input(
+            a1,
+            quant_config.a1_scale,
+            quant_config.quant_dtype,
+            quant_config.per_act_token_quant,
+            quant_config.block_shape,
+        )
+        expert_tokens_meta = None # TODO?
+
+        return a1q, a1q_scale, None, topk_weights, topk_ids
+
+    def finalize(
+        self,
+        output: torch.Tensor,
+        fused_expert_output: torch.Tensor,
+        topk_weights: torch.Tensor,
+        topk_ids: torch.Tensor,
+        apply_router_weight_on_input: bool,
+        weight_and_reduce_impl: mk.TopKWeightAndReduce,
+    ) -> None:
+        if isinstance(weight_and_reduce_impl, TopKWeightAndReduceDelegate):
+            weight_and_reduce_impl = TopKWeightAndReduceContiguous()
+        weight_and_reduce_impl.apply(
+            output=output,
+            fused_expert_output=fused_expert_output,
+            topk_weights=topk_weights,
+            topk_ids=topk_ids,
+            apply_router_weight_on_input=apply_router_weight_on_input,
+        )
+
+        from vllm.distributed import get_ep_group
+        combined_output = get_ep_group().combine(
+            output,
+            is_sequence_parallel=False
+        )
+
+        combined_output.copy_(output)
+
+
 
 class MoEPrepareAndFinalizeNoEP(mk.FusedMoEPrepareAndFinalize):
     def __init__(self, defer_input_quant: bool = False) -> None:
         super().__init__()
         self.defer_input_quant = defer_input_quant
-
+    
     @property
     def activation_format(self) -> mk.FusedMoEActivationFormat:
         return mk.FusedMoEActivationFormat.Standard
