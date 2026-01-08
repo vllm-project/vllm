70ed83a732169119eb02c15f78bce0741f355d97
Isotr0py <mozf@mail2.sysu.edu.cn>
2026-01-08 18:33:48 +0800

    [MM Encoder]: Make MMEncoderAttention's `scale` takes effect properly  (#31950)
    Signed-off-by: Isotr0py <mozf@mail2.sysu.edu.cn>
    Signed-off-by: dsuhinin <suhinin.dmitriy@gmail.com>

--- vllm/model_executor/models/isaac.py
+++ vllm/model_executor/models/isaac.py
@@ -984,6 +984,7 @@ class Siglip2VisionAttention(nn.Module):
         self.attn = MMEncoderAttention(
             num_heads=self.num_attention_heads_per_partition,
             head_size=self.hidden_size_per_attention_head,
+            scale=self.hidden_size_per_attention_head**-0.5,
             prefix=f"{prefix}.attn",
             multimodal_config=multimodal_config,
         )
