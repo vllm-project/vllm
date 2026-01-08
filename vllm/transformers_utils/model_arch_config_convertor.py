f0bc73868205001efd353b494de6ab67b8e1c161
Xingyu Liu <charlotteliu12x@gmail.com>
2026-01-08 13:11:26 -0800

    add deepseek_mtp to is_deepseek_mla for eagle
    Signed-off-by: Xingyu Liu <charlotteliu12x@gmail.com>

--- vllm/transformers_utils/model_arch_config_convertor.py
+++ vllm/transformers_utils/model_arch_config_convertor.py
@@ -201,7 +201,7 @@ class ModelArchConfigConvertorBase:
             # underlying architecture
             return (
                 self.hf_text_config.model.model_type
-                in ("deepseek_v2", "deepseek_v3", "deepseek_v32")
+                in ("deepseek_v2", "deepseek_v3", "deepseek_v32", "deepseek_mtp")
                 and self.hf_text_config.kv_lora_rank is not None
             )
         return False
