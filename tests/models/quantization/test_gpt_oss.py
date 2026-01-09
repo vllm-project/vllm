e23834d826c8443f15161f162db7d53a206a39a3
xuebwang-amd <xuebwang@amd.com>
2026-01-09 15:22:40 +0000

    update tests/models/quantization/test_gpt_oss.py
    Signed-off-by: xuebwang-amd <xuebwang@amd.com>

--- tests/models/quantization/test_gpt_oss.py
+++ tests/models/quantization/test_gpt_oss.py
@@ -1,10 +1,15 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
-"""Test attention quantization of gpt-oss model.
-The qkv_proj and o_proj in self_attention can be either quantized or excluded.
+"""
+End-to-end accuracy test for GPT-OSS model quantization.
 
-Run `pytest tests/models/quantization/test_gpt_oss.py`.
+Config:
+    Task:   gsm8k_platinum
+    Filter: flexible-extract
+    n-shot: 5
+    Metric: exact_match
 
+Run: pytest tests/models/quantization/test_gpt_oss.py
 """
 
 import importlib
@@ -16,13 +21,18 @@ import lm_eval
 import pytest
 from packaging import version
 
-MODEL_NAMES = [
-    "amd/gpt-oss-20b-MoE-WMXFP4-AMXFP4-Attention-WFP8-AFP8-KV-FP8",
-]
+MODEL_ACCURACIES = {
+    # Full quantization: attention linears and MoE linears
+    "amd/gpt-oss-20b-WFP8-AFP8-KVFP8": 0.89,
+    # MoE linears only quantization
+    # "amd/gpt-oss-20b-MoE-Quant-W-MXFP4-A-FP8-KV-FP8": 0.89,
+    # MoE linears only quantization
+    # "amd/gpt-oss-20b-MoE-Quant-W-MXFP4-A-MXFP4-KV-FP8": 0.90,
+}
 
 QUARK_MXFP4_AVAILABLE = importlib.util.find_spec("quark") is not None and version.parse(
     importlib.metadata.version("amd-quark")
-) >= version.parse("0.8.99")
+) >= version.parse("0.9.0")
 
 
 def has_huggingface_access(repo):
@@ -34,7 +44,7 @@ def has_huggingface_access(repo):
 
 
 HF_HUB_AMD_ORG_ACCESS = all(
-    [has_huggingface_access(model_name) for model_name in MODEL_NAMES]
+    [has_huggingface_access(model_name) for model_name in MODEL_ACCURACIES]
 )
 
 
@@ -48,13 +58,13 @@ class ModelCase:
 class EvaluationConfig:
     model_name: str
 
-    def get_model_args(self):
+    def get_model_args(self, tp_size: int):
         return {
             "pretrained": self.model_name,
             "chat_template_args": {"reasoning_effort": "low"},
             "enable_thinking": True,
             "think_end_token": "200008",
-            "tensor_parallel_size": 1,
+            "tensor_parallel_size": tp_size,
             "dtype": "auto",
             "gpu_memory_utilization": 0.95,
             "trust_remote_code": False,
@@ -63,20 +73,17 @@ class EvaluationConfig:
         }
 
 
-EXPECTED_ACCURACIES = {"gsm8k_platinum": 0.89}
-
-
 @pytest.mark.skipif(not QUARK_MXFP4_AVAILABLE, reason="amd-quark>=0.9 is not available")
 @pytest.mark.skipif(
     not HF_HUB_AMD_ORG_ACCESS,
     reason="Read access to huggingface.co/amd is required for this test.",
 )
-@pytest.mark.parametrize("model_name", MODEL_NAMES)
-@pytest.mark.parametrize("task_name, expected_accuracy", EXPECTED_ACCURACIES.items())
+@pytest.mark.parametrize("tp_size", [1, 2, 4, 8])
+@pytest.mark.parametrize("model_name, expected_accuracy", MODEL_ACCURACIES.items())
 def test_gpt_oss_attention_quantization(
-    model_name: str, task_name: str, expected_accuracy: float
+    model_name: str, tp_size: int, expected_accuracy: float
 ):
-    model_args = EvaluationConfig(model_name).get_model_args()
+    model_args = EvaluationConfig(model_name).get_model_args(tp_size)
 
     extra_run_kwargs = {
         "gen_kwargs": {"max_gen_toks": 8000},
@@ -85,13 +92,16 @@ def test_gpt_oss_attention_quantization(
         "num_fewshot": 5,
     }
 
-    measured_accuracy = lm_eval.simple_evaluate(
+    lm_eval_out = lm_eval.simple_evaluate(
         model="vllm",
         model_args=model_args,
-        tasks=task_name,
+        tasks="gsm8k_platinum",
         batch_size="auto",
         **extra_run_kwargs,
-    )["results"][task_name]["acc,none"]
+    )
+    measured_accuracy = float(
+        lm_eval_out["results"]["gsm8k_platinum"]["exact_match,flexible-extract"]
+    )
 
     rtol = 0.05
     assert (
