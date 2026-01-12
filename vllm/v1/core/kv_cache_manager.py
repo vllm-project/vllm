8cfd981a7bde7140d5c1604b125ab4c491498c91
NickLucche <nlucches@redhat.com>
2026-01-12 06:46:55 -0500

    partial prefix cache hit + block_size_ratio + signatures
    Signed-off-by: NickLucche <nlucches@redhat.com>

--- vllm/v1/core/kv_cache_manager.py
+++ vllm/v1/core/kv_cache_manager.py
@@ -84,7 +84,7 @@ class KVCacheBlocks:
         assert len(self.blocks) == 1, "Only one group is supported"
         return [block.block_id for block in self.blocks[0] if block.block_hash is None]
 
-    def get_unhashed_block_ids_all_groups(self) -> list[int]:
+    def get_unhashed_block_ids_all_groups(self) -> list[list[int]]:
         """Get block_ids of unhashed blocks from KVCacheBlocks instance."""
         # Skip padding blocks.
         return [[block.block_id for block in group if block.block_hash is None and not block.is_null] for group in self.blocks]
