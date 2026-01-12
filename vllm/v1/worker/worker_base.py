10ed4e328e1417859902716f65acf7e2ee695426
zhaoning <zhaoning@bjuci.com.cn>
2026-01-12 14:32:09 +0800

    feat：update the eq logic.
    Signed-off-by: zhaoning <zhaoning@bjuci.com.cn>

--- vllm/v1/worker/worker_base.py
+++ vllm/v1/worker/worker_base.py
@@ -216,12 +216,13 @@ class WorkerWrapperBase:
         to adjust the rpc_rank of workers after we create all workers.
         """
         if self.rpc_rank in rank_mapping:
-            self.rpc_rank = rank_mapping[self.rpc_rank]
+            new_rank = rank_mapping[self.rpc_rank]
             if self.global_rank == self.rpc_rank:
                 # Currently, only the Ray executor initiates adjust_rank;
                 # in this case, global_rank is identical to rpc_rank,
                 # so global_rank is updated synchronously as well.
-                self.global_rank = self.rpc_rank
+                self.global_rank = new_rank
+            self.rpc_rank = new_rank
 
     def update_environment_variables(
         self,
