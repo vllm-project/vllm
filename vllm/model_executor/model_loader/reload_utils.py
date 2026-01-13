6beac38d38cf2c07e8aea1480a3cc4c4de863464
Kyle Sayers <kylesayrs@gmail.com>
2026-01-13 21:01:07 +0000

    WIP: delay materialization to better support out-of-order loading
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- vllm/model_executor/model_loader/reload_utils.py
+++ vllm/model_executor/model_loader/reload_utils.py
@@ -7,7 +7,7 @@ from itertools import chain
 import torch
 
 from vllm.logger import init_logger
-from vllm.model_executor.model_loader.utils import process_weights_after_loading
+#from vllm.model_executor.model_loader.utils import process_weights_after_loading
 from vllm.model_executor.model_loader.weight_utils import default_weight_loader
 
 logger = init_logger(__name__)
@@ -19,8 +19,11 @@ RESTORE_ATTRS = ["weight_loader"]
 TODO:
 * decide on reloading interface, back-compat with reload_weights
 * pass arguments to process_weights_after_loading
+* only onload once all weights are present
 * check composability with MLA processing
 * check composability with EPLB
+* do attention/MLA processing after module weights are processed
+    * probably means call process_after_weight_loading after weight loading
 
 Limitations:
 * Does not compose with CPU offloading. This is because `device_loading_context`
@@ -38,15 +41,12 @@ def record_metadata_for_reloading(model: torch.nn.Module):
         module.restore_metadata = {}
 
         for name, param in get_module_tensors(module).items():
-            meta = param.to(device=torch.device("meta"))
-
-            for attr_name in RESTORE_ATTRS:
-                if hasattr(param, attr_name):
-                    setattr(meta, attr_name, getattr(param, attr_name))
+            meta = to_meta_tensor(param)
 
             module.restore_metadata[name] = meta
 
 
+@torch.no_grad()
 def layerwise_restore_and_process(layer: torch.nn.Module):
     """
     Called after `record_metadata_for_reloading`.
@@ -65,34 +65,46 @@ def layerwise_restore_and_process(layer: torch.nn.Module):
     # track loading progress to determine when to process/copy
     load_numel_remaining = get_layer_size(layer)
 
+    import inspect
+
     for name, param in get_module_tensors(layer).items():
-        original_weight_loader = getattr(param, "weight_loader", default_weight_loader)
+        weight_loader = getattr(param, "weight_loader", default_weight_loader)
+        loaded_weight_kwargs: list[inspect.BoundArguments] = list()
+        weight_loader_signature = inspect.signature(weight_loader)
 
-        @wraps(original_weight_loader)
-        def restore_and_process_loader(
-            param: torch.Tensor, loaded_weight: torch.Tensor
-        ):
+        @wraps(weight_loader)
+        def restore_and_process_loader(*args, **kwargs):
             nonlocal load_numel_remaining
+            nonlocal loaded_weight_kwargs
+            nonlocal name
 
-            # materialize meta tensor if necessary
-            if param.device == torch.device("meta"):
-                param = materialize_meta_tensor(param)
-                setattr(layer, name, param)
+            args = weight_loader_signature.bind(*args, **kwargs)
+            args.apply_defaults()
 
-            # load weight into target
-            original_weight_loader(param, loaded_weight)
+            # cache loaded weights on load device until all are ready
+            loaded_weight_kwargs.append(args)
 
-            # process and copy when weights are loaded
-            load_numel_remaining -= loaded_weight.numel()
+            # process and copy when all weights are loaded
+            load_numel_remaining -= get_numel_loaded(weight_loader, args)
             if load_numel_remaining <= 0:
-                # process weights (no-op for online quant)
-                process_weights_after_loading(layer)
+                # materialize layer onto device
+                materialize_layer(layer)
+
+                # load weights
+                for args in loaded_weight_kwargs:
+                    param = getattr(layer, name)
+                    args.arguments["param"] = param
+                    weight_loader(*args.args, **args.kwargs)
+
+                # TODO: process weights
+                print("PROCESS WEIGHTS")
+                #process_weights_after_loading(layer)
 
                 # copy newly processed values into
                 # original tensor data (and cudagraph)
                 assert get_module_tensors(layer).keys() == original_parameters.keys()
                 for name in original_parameters.keys():
-                    original_parameters[name].copy_(layer[name])
+                    original_parameters[name].data.copy_(getattr(layer, name))
                     setattr(layer, name, original_parameters[name])
 
         param.weight_loader = restore_and_process_loader
@@ -113,17 +125,83 @@ def get_layer_size(layer: torch.nn.Module) -> int:
     return sum(tensor.numel() for tensor in get_module_tensors(layer).values())
 
 
-def materialize_meta_tensor(tensor: torch.Tensor) -> torch.Tensor:
+from torch.utils._python_dispatch import TorchDispatchMode
+
+class CopyNumelCounter(TorchDispatchMode):
+    """
+    Tracks total number of elements modified with `copy_`. Useful for keeping
+    track of weight loading where underlying weights can be arbitrarily
+    transformed (such as with `narrow`) before calling copy.
+
+    Assumes that copy kwargs are not used
+    """
+
+    def __init__(self, param: torch.Tensor):
+        super().__init__()
+        self.copied_numel = 0
+        self.param = param
+
+    def __torch_dispatch__(self, func, types, args=(), kwargs=None):
+        if kwargs is None:
+            kwargs = {}
+        if func is torch.ops.aten.copy_.default:
+            if args[0].device.type == "meta":
+                assert args[0].numel() == args[1].numel()
+                self.copied_numel += args[0].numel()
+        out = func(*args, **kwargs)
+        return out
+
+
+import inspect
+def get_numel_loaded(weight_loader: callable, args: inspect.BoundArguments):
+    args.arguments["param"] = to_meta_tensor(args.arguments["param"])
+
+    # TODO: use context exit
+    context = CopyNumelCounter(args.arguments["param"])
+    with context:
+        breakpoint()
+        weight_loader(*args.args, **args.kwargs)
+
+    print(f"found {context.copied_numel} loaded")
+    return context.copied_numel
+
+
+def materialize_layer(layer: torch.nn.Module):
+    for name, tensor in get_module_tensors(layer).items():
+        setattr(layer, name, materialize_meta_tensor(tensor))
+
+
+def materialize_meta_tensor(meta_tensor: torch.Tensor) -> torch.Tensor:
+    # TODO: need a way of (reconstructing?) vLLMBaseParameters on the meta device
+
     # should be called within a torch device context
-    return torch.empty_strided(
-        size=tuple(tensor.size()),
-        stride=tuple(tensor.stride()),
-        dtype=tensor.dtype,
+    tensor = meta_tensor.__class__(data=torch.empty_strided(
+        size=tuple(meta_tensor.size()),
+        stride=tuple(meta_tensor.stride()),
+        dtype=meta_tensor.dtype,
         requires_grad=False,
-    )
+    ))
+
+    for attr_name in RESTORE_ATTRS:
+        if hasattr(meta_tensor, attr_name):
+            setattr(tensor, attr_name, getattr(meta_tensor, attr_name))
+
+    return tensor
+
+
+def to_meta_tensor(tensor: torch.Tensor) -> torch.Tensor:
+    meta_tensor = tensor.__class__(data=tensor.data.to("meta"))
+
+    for attr_name in RESTORE_ATTRS:
+        if hasattr(tensor, attr_name):
+            setattr(meta_tensor, attr_name, getattr(tensor, attr_name))
+
+    return meta_tensor
 
 
 def get_module_tensors(module: torch.nn.Module) -> dict[str, torch.Tensor]:
-    tensors = module._parameters.copy()
-    tensors.update(module._buffers)
-    return tensors
+    return {
+        name: value
+        for name, value in chain(module._parameters.items(), module._buffers.items())
+        if value is not None
+    }
