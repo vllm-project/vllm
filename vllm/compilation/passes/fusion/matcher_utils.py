bf0e3db4fc923a257a95dc63ef870cc1cfa51bd5
Tres Popp <tres.popp@amd.com>
2026-01-13 14:20:48 +0000

    Support ROCm aiter specific fusion of per_tensor RMSNorm+QuantFP8
    This adds patterns similar to fusion.py for static scale, per-tensor
    RMSNorm+Quantization. This requires custom_ops=["+rms_norm"] and aiter
    enabled.
    
    Speedups measured on MI325 are 4-5% for Llama3.
    
    Signed-off-by: Tres Popp <tres.popp@amd.com>

--- vllm/compilation/passes/fusion/matcher_utils.py
+++ vllm/compilation/passes/fusion/matcher_utils.py
@@ -310,10 +310,9 @@ class MatcherQuantFP8(MatcherCustomOp):
         self.is_tma_aligned = is_tma_aligned
 
         if match_rocm_aiter:
-            assert not quant_key.scale.group_shape.is_per_tensor(), (
-                "ROCm aiter fusion pass does not support per tensor quantization"
-            )
-            if quant_key.scale.group_shape.is_per_token():
+            if quant_key.scale.group_shape.is_per_tensor():
+                self.QUANT_OP = torch.ops.vllm.rocm_aiter_per_tensor_quant.default
+            elif quant_key.scale.group_shape.is_per_token():
                 self.QUANT_OP = rocm_aiter_ops.get_per_token_quant_op()
             else:
                 assert quant_key.scale.group_shape.col == 128, (
@@ -353,6 +352,12 @@ class MatcherQuantFP8(MatcherCustomOp):
         scale: torch.Tensor | None = None,
     ) -> tuple[torch.Tensor, torch.Tensor]:
         quant_key_group_shape = self.quant_key.scale.group_shape
+        if quant_key_group_shape == GroupShape.PER_TENSOR:
+            return self.QUANT_OP(
+                x=input,
+                quant_dtype=self.quant_key.dtype,
+                scale=scale,
+            )
         if quant_key_group_shape == GroupShape.PER_TOKEN:
             return self.QUANT_OP(  # type: ignore[no-any-return]
                 x=input,
