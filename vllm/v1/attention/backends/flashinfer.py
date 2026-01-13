ae40ceecdac41c6f23407a97c11be37addd20e11
Andrii <askliar@nvidia.com>
2026-01-13 16:36:32 +0100

    fix typo

--- vllm/v1/attention/backends/flashinfer.py
+++ vllm/v1/attention/backends/flashinfer.py
@@ -1598,8 +1598,7 @@ def fast_plan_decode(
     """
     # Warm up with the original plan if it is first call, and always run the
     # original plan if we run for dynamic shape. For fixed shape (cudagraph),
-    # this warm up is to generate the _cachedcccccbvgvjlrvjgejhbkkiklfkbkvtkdvhhvbtbnhgnk
-    # _module for the decode wrapper.
+    # this warm up is to generate the _cached_module for the decode wrapper.
     if not self.is_cuda_graph_enabled or getattr(self, "vllm_first_call", True):
         self.plan(
             indptr=indptr_cpu,
