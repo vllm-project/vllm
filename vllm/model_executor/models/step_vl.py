82137283829e44e93224e3736d6c36604da9626b
ltd0924 <32387785+ltd0924@users.noreply.github.com>
2026-01-14 21:50:51 +0800

    Update vllm/model_executor/models/step_vl.py
    Co-authored-by: Cyrus Leung <cyrus.tl.leung@gmail.com>
    Signed-off-by: ltd0924 <32387785+ltd0924@users.noreply.github.com>
    Signed-off-by: luotingdan <luotingdan@stepfun.com>

--- vllm/model_executor/models/step_vl.py
+++ vllm/model_executor/models/step_vl.py
@@ -621,7 +621,7 @@ class StepVLForConditionalGeneration(Step3VLForConditionalGeneration):
     )
 
     def __init__(self, *, vllm_config: VllmConfig, prefix: str = "") -> None:
-        super(Step3VLForConditionalGeneration, self).__init__()
+        super().__init__()
 
         config = vllm_config.model_config.hf_config
         multimodal_config = vllm_config.model_config.multimodal_config
