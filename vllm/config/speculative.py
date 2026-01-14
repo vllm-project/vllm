003bdd82ac3dea4963a6bb7665d8d9ee7fff31fd
Xingyu Liu <charlotteliu12x@gmail.com>
2026-01-14 11:29:28 -0800

    fix speculative
    Signed-off-by: Xingyu Liu <charlotteliu12x@gmail.com>

--- vllm/config/speculative.py
+++ vllm/config/speculative.py
@@ -421,7 +421,7 @@ class SpeculativeConfig:
                         )
                         model_info, arch = (
                             self.draft_model_config.registry.inspect_model_cls(
-                                self.draft_model_config.architectures,
+                                self.draft_model_config.architecture,
                                 self.draft_model_config,
                             )
                         )
