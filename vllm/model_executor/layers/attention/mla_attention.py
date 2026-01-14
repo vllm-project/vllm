15bd1b7c17e0860f538ce90b852a21f50ca5d12a
Matthew Bonanni <mbonanni@redhat.com>
2026-01-14 11:44:11 -0500

    Change log from debug to info
    Signed-off-by: Matthew Bonanni <mbonanni@redhat.com>

--- vllm/model_executor/layers/attention/mla_attention.py
+++ vllm/model_executor/layers/attention/mla_attention.py
@@ -1294,24 +1294,24 @@ class MLACommonImpl(MLACommonBaseImpl[M], Generic[M]):
         super().__init__(*args, **kwargs)
 
         if use_trtllm_ragged_deepseek_prefill():
-            logger.debug_once("Using TRT-LLM ragged DeepSeek prefill for MLA")
+            logger.info_once("Using TRT-LLM ragged DeepSeek prefill for MLA")
             self._run_prefill_context_chunk = (
                 self._run_prefill_context_chunk_trtllm_ragged
             )
             self._run_prefill_new_tokens = self._run_prefill_new_tokens_trtllm_ragged
             self._pad_v = False
         elif use_flashinfer_prefill():
-            logger.debug_once("Using FlashInfer prefill for MLA")
+            logger.info_once("Using FlashInfer prefill for MLA")
             self._run_prefill_context_chunk = self._run_prefill_context_chunk_fi
             self._run_prefill_new_tokens = self._run_prefill_new_tokens_fi
             self._pad_v = False
         elif use_cudnn_prefill():
-            logger.debug_once("Using CUDNN prefill for MLA")
+            logger.info_once("Using CUDNN prefill for MLA")
             self._run_prefill_context_chunk = self._run_prefill_context_chunk_cudnn
             self._run_prefill_new_tokens = self._run_prefill_new_tokens_cudnn
             self._pad_v = False
         else:  # Use FlashAttention
-            logger.debug_once("Using FlashAttention prefill for MLA")
+            logger.info_once("Using FlashAttention prefill for MLA")
             self._run_prefill_context_chunk = self._run_prefill_context_chunk_fa
             self._run_prefill_new_tokens = self._run_prefill_new_tokens_fa
 
