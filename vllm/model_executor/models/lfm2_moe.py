d3317bbba44ec11bbef435308f98284487fce45a
Paul Pak <52512091+paulpak58@users.noreply.github.com>
2026-01-16 23:12:55 -0700

    [Models] Lfm2Moe: minor name changes for resolving lora conflicts (#29063)
    Signed-off-by: Paul Pak <paulpak58@gmail.com>

--- vllm/model_executor/models/lfm2_moe.py
+++ vllm/model_executor/models/lfm2_moe.py
@@ -349,7 +349,7 @@ class Lfm2MoeShortConvDecoderLayer(nn.Module):
     ) -> None:
         super().__init__()
         self.layer_idx = layer_idx
-        self.conv = ShortConv(
+        self.short_conv = ShortConv(
             config=config,
             dim=config.hidden_size,
             layer_idx=layer_idx,
@@ -388,7 +388,7 @@ class Lfm2MoeShortConvDecoderLayer(nn.Module):
         else:
             hidden_states, residual = self.operator_norm(hidden_states, residual)
         output = torch.empty_like(hidden_states)
-        self.conv(
+        self.short_conv(
             hidden_states,
             output,
         )
@@ -509,6 +509,9 @@ class Lfm2MoeModel(nn.Module):
             if "expert_bias" in name:
                 name = name.replace("expert_bias", "gate.e_score_correction_bias")
 
+            if ".conv." in name:
+                name = name.replace(".conv.", ".short_conv.", 1)
+
             for param_name, weight_name, shard_id in stacked_params_mapping:
                 # Skip non-stacked layers and experts (experts handled below).
                 if weight_name not in name:
@@ -595,6 +598,7 @@ class Lfm2MoeForCausalLM(
             "w1",
             "w3",
         ],
+        "in_proj": ["in_proj"],
     }
 
     # LoRA specific attributes
