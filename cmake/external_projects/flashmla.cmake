8ba5489d81d47d6ff02c2b2051273ad56646d63a
Lucas Wilkinson <lwilkins@redhat.com>
2026-01-19 16:27:56 +0000

    fix
    Signed-off-by: Lucas Wilkinson <lwilkins@redhat.com>

--- cmake/external_projects/flashmla.cmake
+++ cmake/external_projects/flashmla.cmake
@@ -19,7 +19,7 @@ else()
   FetchContent_Declare(
         flashmla
         GIT_REPOSITORY https://github.com/vllm-project/FlashMLA
-        GIT_TAG 8c73e5c7b4e8bc3a719506544aeb14ccb3529ff7
+        GIT_TAG 0db4f3f800e0f1df59fd0ce6b5a92596b4e45dc4
         GIT_PROGRESS TRUE
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ""
