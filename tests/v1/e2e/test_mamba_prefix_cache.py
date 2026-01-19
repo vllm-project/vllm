c200506002246d3beab1d8f4b2e8b81782d4fdec
Chen Zhang <zhangch99@outlook.com>
2026-01-19 00:23:20 -0800

    skip test
    Signed-off-by: Chen Zhang <zhangch99@outlook.com>

--- tests/v1/e2e/test_mamba_prefix_cache.py
+++ tests/v1/e2e/test_mamba_prefix_cache.py
@@ -473,6 +473,10 @@ def apply_patch(monkeypatch: pytest.MonkeyPatch):
     monkeypatch.setattr(mamba_utils, "do_mamba_copy_block", fake_copy_fn)
 
 
+@pytest.skip(
+    reason="Skipping test_mamba_prefix_cache because it is based on spec "
+    "decode which is not allowed now."
+)
 def test_mamba_prefix_cache(monkeypatch: pytest.MonkeyPatch):
     run_ref_mamba_state_in_subprocess()
     apply_patch(monkeypatch)
