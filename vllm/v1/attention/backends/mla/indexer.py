803117e3c8f58471f0b0204d741d0e5f8e4ad2f3
ganyi <ygan@amd.com>
2026-01-20 08:39:06 +0000

    make gluon impl as default
    Signed-off-by: ganyi <ygan@amd.com>

--- vllm/v1/attention/backends/mla/indexer.py
+++ vllm/v1/attention/backends/mla/indexer.py
@@ -7,7 +7,6 @@ import torch
 
 from vllm.config import VllmConfig
 from vllm.logger import init_logger
-from vllm.platforms import current_platform
 from vllm.utils.deep_gemm import get_paged_mqa_logits_metadata, is_deep_gemm_supported
 from vllm.v1.attention.backend import (
     AttentionBackend,
@@ -27,7 +26,7 @@ logger = init_logger(__name__)
 class DeepseekV32IndexerBackend(AttentionBackend):
     @staticmethod
     def get_supported_kernel_block_sizes() -> list[int | MultipleOf]:
-        return [1 if current_platform.is_rocm() else 64]
+        return [64]
 
     @classmethod
     def get_supported_head_sizes(cls) -> list[int]:
