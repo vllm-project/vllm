a12d20a899c6a690d2598407e484c8e837b41480
DCCS-4560 <alivanov@chi-mi325x-pod1-112.ord.vultr.cpe.ice.amd.com>
2026-01-20 03:02:39 +0000

    .
    Signed-off-by: DCCS-4560 <alivanov@chi-mi325x-pod1-112.ord.vultr.cpe.ice.amd.com>

--- .buildkite/scripts/hardware_ci/run-amd-test.sh
+++ .buildkite/scripts/hardware_ci/run-amd-test.sh
@@ -231,6 +231,7 @@ elif [[ $commands == *"VLLM_TEST_GROUP_NAME=mi325_4-2-node-tests-4-gpus-in-total
 
   if [[ "$commands" =~ ^(.*)"["(.*)"] && ["(.*)"]"$ ]]; then
       prefix=$( echo "${BASH_REMATCH[1]}" | sed 's/;//g')
+      echo "PREFIX: ${prefix}"
       myIFS=$IFS
       IFS=','
       read -ra node0 <<< ${BASH_REMATCH[2]}
@@ -240,6 +241,7 @@ elif [[ $commands == *"VLLM_TEST_GROUP_NAME=mi325_4-2-node-tests-4-gpus-in-total
         command_node_0=${node0[i]}
         command_node_1=${node1[i]}
         export commands="curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh --version ${DCKR_VER} && rm get-docker.sh && nohup bash -c 'dockerd &' && sleep 5 && ${prefix} && ../.buildkite/scripts/run-multi-node-test.sh ./tests 2 2 ${image_name} ${command_node_0} ${command_node_1}"
+        echo "COMMANDS: ${commands}"
         docker run \
           --privileged \
           --device /dev/kfd $BUILDKITE_AGENT_META_DATA_RENDER_DEVICES \
