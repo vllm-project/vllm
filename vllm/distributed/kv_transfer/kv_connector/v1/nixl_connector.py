ced9ad455d98374f9a2ce83729fd8e208f510c78
Liran Schour <lirans@il.ibm.com>
2026-01-21 13:21:51 +0000

    Code review fix
    Signed-off-by: Liran Schour <lirans@il.ibm.com>

--- vllm/distributed/kv_transfer/kv_connector/v1/nixl_connector.py
+++ vllm/distributed/kv_transfer/kv_connector/v1/nixl_connector.py
@@ -302,10 +302,10 @@ class NixlConnector(KVConnectorBase_V1):
     @property
     def prefer_cross_layer_blocks(self) -> bool:
         backend = get_current_attn_backend(self._vllm_config)
-        if backend().get_name() not in [
+        if backend().get_name() not in (
             "FLASH_ATTN",
             "FLASHINFER",
-        ]:
+        ):
             # For now there is no benefit to run cross layers when backend
             # does not support on HND
             return False
@@ -1421,31 +1421,7 @@ class NixlConnectorWorker:
         self.device_kv_caches = kv_caches
         self.dst_num_blocks[self.engine_id] = self.num_blocks
 
-        if self.kv_topo.cross_layers_blocks:
-            assert len(kv_caches) == 1
-            tensor_shape = list(kv_caches.values())[0].shape
-
-            # NOTE (liranschour) When FlashInfer is used, memory is registered
-            # with joint KV for each block and in Triton joint KV and num_layers
-            # for each block.
-            # In order to be able to split on kv_heads dim as required by
-            # heterogeneous TP, one must be able to index (K/V, layer_idx) separately.
-            # Hence we multiply the number of 'virtual' regions here and divide
-            # `block_len` below.
-            multiply = 1
-            for dim_idx in range(1, self.kv_topo.physical_kv_heads_position):
-                multiply *= tensor_shape[dim_idx]
-            logger.debug(
-                "Multiply is %d shape %s kv_heads_pos %d",
-                multiply,
-                tensor_shape,
-                self.kv_topo.physical_kv_heads_position,
-            )
-            for i in range(len(self.slot_size_per_layer)):
-                assert self.slot_size_per_layer[i] % multiply == 0
-                self.slot_size_per_layer[i] //= multiply
-            self.num_regions *= multiply
-        elif self.kv_topo.is_kv_layout_blocks_first:
+        if self.kv_topo.is_kv_layout_blocks_first:
             for i in range(len(self.slot_size_per_layer)):
                 assert self.slot_size_per_layer[i] % 2 == 0
                 self.slot_size_per_layer[i] //= 2
