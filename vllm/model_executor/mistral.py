99131559a25c149f0ea4ece622ab059ae96ff2bb
Patrick von Platen <patrick.v.platen@gmail.com>
2026-01-21 14:43:37 +0100

    up
    Signed-off-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- vllm/model_executor/mistral.py
+++ vllm/model_executor/mistral.py
@@ -56,7 +56,9 @@ class MistralAttention(LlamaAttention):
             attn_type=attn_type,
         )
 
-        llama_4_scaling_config: dict[str, int | float | str] | None = getattr(config, "llama_4_scaling", None)
+        llama_4_scaling_config: dict[str, int | float | str] | None = getattr(
+            config, "llama_4_scaling", None
+        )
         self.do_llama_4_scaling = llama_4_scaling_config is not None
         if self.do_llama_4_scaling:
             assert llama_4_scaling_config is not None
