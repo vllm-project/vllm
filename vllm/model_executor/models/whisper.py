5eb56756fd92f524ead8c658889b6e511a65fe3d
Patrick von Platen <patrick.v.platen@gmail.com>
2026-01-22 17:26:26 +0000

    up
    Signed-off-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- vllm/model_executor/models/whisper.py
+++ vllm/model_executor/models/whisper.py
@@ -501,9 +501,9 @@ class WhisperEncoder(nn.Module):
             embeds = nn.functional.gelu(self.conv2(embeds))
 
             embeds = embeds.transpose(-1, -2)
-            embeds = (
-                embeds + self.embed_positions.weight[: embeds.size(-2), :]
-            ).to(embeds.dtype)
+            embeds = (embeds + self.embed_positions.weight[: embeds.size(-2), :]).to(
+                embeds.dtype
+            )
 
             hidden_states.append(embeds)
             input_is_batched = embeds.ndim > 2
