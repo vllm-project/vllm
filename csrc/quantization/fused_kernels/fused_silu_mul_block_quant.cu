0dd8492fda1d0378f4123333b5705f6300216f63
Monishver Chandrasekaran <monishverchandrasekaran@gmail.com>
2026-01-22 01:36:35 -0500

    Revert "bug fix from pytest"
    This reverts commit 8946ae0022fffec7f57156587d9348b92e8c34b8.

--- csrc/quantization/fused_kernels/fused_silu_mul_block_quant.cu
+++ csrc/quantization/fused_kernels/fused_silu_mul_block_quant.cu
@@ -114,7 +114,7 @@ __global__ void silu_and_mul_per_block_quant_kernel(
             
             // Store scale with correct stride
             if constexpr (is_scale_transposed) {
-                token_scales[group_idx * num_tokens] = group_scale;
+                token_scales[group_idx * blockDim.x] = group_scale;
             } else {
                 token_scales[group_idx] = group_scale;
             }
@@ -123,7 +123,7 @@ __global__ void silu_and_mul_per_block_quant_kernel(
         
         // Broadcast scale to all threads
         if constexpr (is_scale_transposed) {
-            group_scale = token_scales[group_idx * num_tokens];
+            group_scale = token_scales[group_idx * blockDim.x];
         } else {
             group_scale = token_scales[group_idx];
         }
