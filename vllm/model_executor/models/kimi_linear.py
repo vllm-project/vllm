0e672df1e31afac9c9bde33e2ad22794347879a5
Harry Huang <vastrockhuang162@gmail.com>
2026-01-24 01:56:48 +0800

    [V1][Hybrid] Mamba Prefix Caching with align mode (#30877)
    Signed-off-by: huanghaoyan.hhy <huanghaoyan.hhy@alibaba-inc.com>
    Signed-off-by: Chen Zhang <zhangch99@outlook.com>
    Co-authored-by: Chen Zhang <zhangch99@outlook.com>
    Signed-off-by: slava.sidik <slava.sidik@huawei.com>

--- vllm/model_executor/models/kimi_linear.py
+++ vllm/model_executor/models/kimi_linear.py
@@ -26,6 +26,8 @@ from vllm.model_executor.layers.linear import (
 )
 from vllm.model_executor.layers.logits_processor import LogitsProcessor
 from vllm.model_executor.layers.mamba.mamba_utils import (
+    MambaStateCopyFunc,
+    MambaStateCopyFuncCalculator,
     MambaStateDtypeCalculator,
     MambaStateShapeCalculator,
 )
@@ -543,6 +545,14 @@ class KimiLinearForCausalLM(
             conv_kernel_size=hf_config.linear_attn_config["short_conv_kernel_size"],
             num_spec=num_spec,
         )
+
+    @classmethod
+    def get_mamba_state_copy_func(
+        cls,
+    ) -> tuple[
+        MambaStateCopyFunc, MambaStateCopyFunc, MambaStateCopyFunc, MambaStateCopyFunc
+    ]:
+        return MambaStateCopyFuncCalculator.kda_state_copy_func()
 
     def compute_logits(
         self,
--- vllm/model_executor/models/qwen3_next_mtp.py
+++ vllm/model_executor/models/qwen3_next_mtp.py
@@ -234,9 +234,11 @@ class Qwen3NextMTP(nn.Module, QwenNextMixtureOfExperts):
         config = vllm_config.model_config.hf_config
         self.vllm_config = vllm_config
         cache_config = vllm_config.cache_config
-        assert not cache_config.enable_prefix_caching, (
-            "Qwen3NextMTP currently does not support prefix caching"
-        )
+        if cache_config.mamba_cache_mode == "all":
+            raise NotImplementedError(
+                "Qwen3NextMTP currently does not support 'all' prefix caching, "
+                "please use '--mamba-cache-mode=align' instead"
+            )
 
         self.quant_config = vllm_config.quant_config
 
