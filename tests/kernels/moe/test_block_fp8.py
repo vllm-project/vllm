e2b99690e578f370f827b43fad159f0142f9c20f
Robert Shaw <robshaw@redhat.com>
2026-01-25 20:20:29 -0500

    reduce LOC changed
    Signed-off-by: Robert Shaw <robshaw@redhat.com>

--- tests/kernels/moe/test_block_fp8.py
+++ tests/kernels/moe/test_block_fp8.py
@@ -254,12 +254,11 @@ def test_w8a8_block_fp8_deep_gemm_fused_moe(M, N, K, E, topk, seed, monkeypatch)
         w2_scale=w2_s,
         block_shape=block_size,
     )
-    moe_config = make_dummy_moe_config()
 
     deep_gemm_experts = mk.FusedMoEModularKernel(
         prepare_finalize=MoEPrepareAndFinalizeNoEP(),
         fused_experts=TritonOrDeepGemmExperts(
-            moe_config=moe_config,
+            moe_config=make_dummy_moe_config(),
             quant_config=quant_config,
         ),
     )
