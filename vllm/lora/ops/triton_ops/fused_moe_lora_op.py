353e0bc740780987cfff616135dc4c90f92ea086
陈建华 <1647430658@qq.com>
2026-01-26 09:47:56 +0800

    Remove double_stream argument
    Signed-off-by: 陈建华 <1647430658@qq.com>

--- vllm/lora/ops/triton_ops/fused_moe_lora_op.py
+++ vllm/lora/ops/triton_ops/fused_moe_lora_op.py
@@ -460,7 +460,6 @@ def _fused_moe_lora(
     mul_routed_weight: bool = False,
     fully_sharded: bool = False,
     offset: int = 0,
-    double_stream: bool = False,
 ) -> None:
     assert len(lora_a_stacked) == len(lora_b_stacked) > 0
     assert (
@@ -494,10 +493,7 @@ def _fused_moe_lora(
         device=device,
     )
 
-    # PDL(GDC) is intended for single-stream dependency chains.
-    # Double stream should disable PDL and rely on the upper-level stream/event scheduling.
-    use_gdc = supports_pdl(device) and not fully_sharded and not double_stream
-
+    use_gdc = supports_pdl(device) and not fully_sharded
     _fused_moe_lora_shrink(
         a_intermediate_cache1,
         qcurr_hidden_states,
