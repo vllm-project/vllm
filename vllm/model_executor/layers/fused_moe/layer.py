2c063b8bf6f705e10d20b3e8c5557bd9ee2da656
Bill Nell <bnell@redhat.com>
2026-01-26 18:59:26 -0500

    fix
    Signed-off-by: Bill Nell <bnell@redhat.com>

--- vllm/model_executor/layers/fused_moe/layer.py
+++ vllm/model_executor/layers/fused_moe/layer.py
@@ -661,6 +661,8 @@ class FusedMoE(CustomOp):
             )
             # force reconstruction of runner
             self.init_runner(reconstruct=True)
+        else:
+            self.init_runner()
 
     @property
     def shared_experts(self) -> torch.nn.Module | None:
