0ffe4e287bf0497c114a0ee5b676db735e0828fd
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2026-01-27 20:35:12 +0100

    Add todo comments
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- vllm/model_executor/models/transformers/multimodal.py
+++ vllm/model_executor/models/transformers/multimodal.py
@@ -278,6 +278,7 @@ class MultiModalMixin(SupportsMultiModal, SupportsMRoPE):
         super(SupportsMRoPE, self).__init__(vllm_config=vllm_config, prefix=prefix)
 
         # In Transformers v5 this is handled by the conversion mapping
+        # TODO(hmellor): Remove this when we drop support for Transformers v4
         if Version(transformers.__version__) < Version("5.0.0"):
             # Backwards compatibility for prev released models. State dicts back then
             # had different formats and cannot be loaded with `AutoModel` mapping as is
