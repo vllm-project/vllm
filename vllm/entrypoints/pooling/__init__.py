3630512024371c1f671d99bcd540c8957d5e540c
wang.yuqi <yuqi.wang@daocloud.io>
2026-01-27 14:13:06 +0800

    fix
    Signed-off-by: wang.yuqi <yuqi.wang@daocloud.io>

--- vllm/entrypoints/pooling/__init__.py
+++ vllm/entrypoints/pooling/__init__.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING
 
 from fastapi import FastAPI
 
