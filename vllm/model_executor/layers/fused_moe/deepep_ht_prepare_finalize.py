d737e4b10af3e46a5ff803099a26cdc935f0fe91
Robert Shaw <robshaw@redhat.com>
2026-01-27 17:17:52 -0500

    fix deepep high throughput with nvfp4
    Signed-off-by: Robert Shaw <robshaw@redhat.com>

--- vllm/model_executor/layers/fused_moe/deepep_ht_prepare_finalize.py
+++ vllm/model_executor/layers/fused_moe/deepep_ht_prepare_finalize.py
@@ -239,6 +239,7 @@ class DeepEPHTPrepareAndFinalize(mk.FusedMoEPrepareAndFinalize):
                     quant_dtype=quant_config.quant_dtype,
                     per_act_token_quant=False,
                     block_shape=quant_config.block_shape,
+                    is_fp4_scale_swizzled=quant_config.is_nvfp4_scale_swizzled,
                 )
 
         return (
