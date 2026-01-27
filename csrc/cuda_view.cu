ae3e2216ad62c171eea3fde030f61005f9686118
wzhao18 <wzhao18.sz@gmail.com>
2026-01-27 06:17:08 +0000

    Use cudahostalloc for page-locked memory
    Signed-off-by: wzhao18 <wzhao18.sz@gmail.com>

--- csrc/cuda_view.cu
+++ csrc/cuda_view.cu
@@ -1,7 +1,6 @@
 #include <torch/all.h>
 #include <torch/cuda.h>
 #include <cuda_runtime.h>
-#include <sys/mman.h>
 
 // This function assumes that `cpu_tensor` is a CPU tensor,
 // and that UVA (Unified Virtual Addressing) is enabled.
@@ -25,32 +24,28 @@ torch::Tensor get_cuda_view_from_cpu_tensor(torch::Tensor& cpu_tensor) {
   // If CPU tensor is not pinned, allocate a new pinned memory buffer.
   torch::Tensor contiguous_cpu = cpu_tensor.contiguous();
   size_t nbytes = contiguous_cpu.nbytes();
-  long page_size = sysconf(_SC_PAGESIZE);
-  size_t aligned_size = (nbytes + page_size - 1) & ~(page_size - 1);
 
-  void* host_ptr = mmap(nullptr, aligned_size, PROT_READ | PROT_WRITE,
-                        MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-
-  if (host_ptr == MAP_FAILED) {
-    AT_ERROR("mmap failed to allocate ", aligned_size, " bytes");
+  void* host_ptr = nullptr;
+  cudaError_t err = cudaHostAlloc(&host_ptr, nbytes, cudaHostAllocMapped);
+  if (err != cudaSuccess) {
+    AT_ERROR("cudaHostAlloc failed: ", cudaGetErrorString(err));
   }
 
-  std::memcpy(host_ptr, contiguous_cpu.data_ptr(), nbytes);
-
-  cudaError_t err =
-      cudaHostRegister(host_ptr, aligned_size, cudaHostRegisterDefault);
+  err = cudaMemcpy(host_ptr, contiguous_cpu.data_ptr(), nbytes,
+                   cudaMemcpyDefault);
   if (err != cudaSuccess) {
-    munmap(host_ptr, aligned_size);
-    AT_ERROR("cudaHostRegister failed: ", cudaGetErrorString(err));
+    cudaFreeHost(host_ptr);
+    AT_ERROR("cudaMemcpy failed: ", cudaGetErrorString(err));
   }
 
   void* device_ptr = nullptr;
-  cudaHostGetDevicePointer(&device_ptr, host_ptr, 0);
+  err = cudaHostGetDevicePointer(&device_ptr, host_ptr, 0);
+  if (err != cudaSuccess) {
+    cudaFreeHost(host_ptr);
+    AT_ERROR("cudaHostGetDevicePointer failed: ", cudaGetErrorString(err));
+  }
 
-  auto deleter = [host_ptr, aligned_size](void*) {
-    cudaHostUnregister(host_ptr);
-    munmap(host_ptr, aligned_size);
-  };
+  auto deleter = [host_ptr](void*) { cudaFreeHost(host_ptr); };
 
   return torch::from_blob(device_ptr, contiguous_cpu.sizes(),
                           contiguous_cpu.strides(), deleter,
