9696241d8ee7fd3d1b9ee4e9c16bbeee6a8f45d9
Angela Yi <yiangela7@gmail.com>
2026-01-28 13:03:56 -0800

    [ez] Remove checks for torch version <= 2.8 (#33209)
    Signed-off-by: angelayi <yiangela7@gmail.com>
    Signed-off-by: rayleeku <rui008@e.ntu.edu.sg>

--- vllm/compilation/inductor_pass.py
+++ vllm/compilation/inductor_pass.py
@@ -19,13 +19,7 @@ from torch._subclasses.fake_tensor import FakeTensorMode, unset_fake_temporarily
 if TYPE_CHECKING:
     from vllm.config.utils import Range
 
-if is_torch_equal_or_newer("2.6"):
-    from torch._inductor.custom_graph_pass import CustomGraphPass
-else:
-    # CustomGraphPass is not present in 2.5 or lower, import our version
-    from .torch25_custom_graph_pass import (
-        Torch25CustomGraphPass as CustomGraphPass,
-    )
+from torch._inductor.custom_graph_pass import CustomGraphPass
 
 _pass_context = None
 P = ParamSpec("P")
