2f09c9498014c7f65433c2683f1810743f088619
Pai <416932041@qq.com>
2026-01-28 23:37:12 +0800

    add 1e1pd example
    Signed-off-by: PiratePai <416932041@qq.com>
    Signed-off-by: Pai <416932041@qq.com>

---
+++ examples/online_serving/disaggregated_encoder_shm/1e1pd_proxy.sh
@@ -0,0 +1,6 @@
+python ./disaggregated_encoder/disagg_epd_proxy.py \
+      --encode-servers-urls "http://127.0.0.1:23001" \
+      --prefill-servers-urls "disable" \
+      --decode-servers-urls "http://127.0.0.1:33001" \
+      --host 127.0.0.1 \
+      --port 8001
---
+++ examples/online_serving/disaggregated_encoder_shm/run_pd.sh
@@ -0,0 +1,25 @@
+unset ftp_proxy
+unset https_proxy
+unset http_proxy
+EC_SHARED_STORAGE_PATH="${EC_SHARED_STORAGE_PATH:-/user/ec_cache}"
+
+CUDA_VISIBLE_DEVICES=0 vllm serve "/model_path" \
+    --gpu-memory-utilization 0.7 \
+    --port "33001" \
+    --enforce-eager \
+    --enable-request-id-headers \
+    --served-model-name model_name \
+    --max-model-len 32768  \
+    --max-num-seqs 128 \
+    --ec-transfer-config '{
+      "ec_connector": "ECExampleConnectorSHM",
+      "ec_role": "ec_consumer",
+      "ec_ip": "127.0.0.1",
+      "ec_connector_module_path":"vllm.distributed.ec_transfer.ec_connector.example_connector_shm",
+      "ec_connector_extra_config": {
+            "shared_storage_path": "'"$EC_SHARED_STORAGE_PATH"'",
+            "listen_ports": [30161]
+        }
+    }' 
+
+
