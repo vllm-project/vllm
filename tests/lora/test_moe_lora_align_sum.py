49f100211ca84b5e8d46b35e9808af046b90c19b
gnovack <gnovack@amazon.com>
2026-01-30 00:33:59 +0000

    use 'max_active_experts' for moe lora input size
    Signed-off-by: gnovack <gnovack@amazon.com>

--- tests/lora/test_moe_lora_align_sum.py
+++ tests/lora/test_moe_lora_align_sum.py
@@ -47,6 +47,10 @@ def test_moe_lora_align_block_size(
     # compute paddings
     max_num_tokens_padded = topk_ids.numel() + num_experts * (block_size - 1)
     max_num_tokens_padded = round_up(max_num_tokens_padded, block_size)
+    if topk_ids.numel() < num_experts:
+        max_num_tokens_padded = min(
+            topk_ids.numel() * block_size, max_num_tokens_padded
+        )
     max_num_m_blocks = CEILDIV(max_num_tokens_padded, block_size)
 
     # init output tensors
