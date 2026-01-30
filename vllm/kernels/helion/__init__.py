e8546cf3dfcedc462d98b8b79b8999ebf00afca4
Yanan Cao <gmagogsfm@users.noreply.github.com>
2026-01-30 09:19:19 -0800

    [Kernel] [Helion] [1/N] Add Helion ConfigManager (#32740)
    Signed-off-by: Yanan Cao <gmagogsfm@gmail.com>
    Signed-off-by: rayleeku <rui008@e.ntu.edu.sg>

--- vllm/kernels/helion/__init__.py
+++ vllm/kernels/helion/__init__.py
@@ -6,28 +6,8 @@ from vllm.kernels.helion.config_manager import (
     ConfigManager,
     ConfigSet,
 )
-from vllm.kernels.helion.register import (
-    ConfiguredHelionKernel,
-    HelionKernelWrapper,
-    get_kernel_by_name,
-    get_registered_kernels,
-    register_kernel,
-    vllm_helion_lib,
-)
-from vllm.kernels.helion.utils import canonicalize_gpu_name, get_canonical_gpu_name
 
 __all__ = [
-    # Config management
     "ConfigManager",
     "ConfigSet",
-    # Kernel registration
-    "ConfiguredHelionKernel",
-    "HelionKernelWrapper",
-    "get_kernel_by_name",
-    "get_registered_kernels",
-    "register_kernel",
-    "vllm_helion_lib",
-    # Utilities
-    "canonicalize_gpu_name",
-    "get_canonical_gpu_name",
 ]
