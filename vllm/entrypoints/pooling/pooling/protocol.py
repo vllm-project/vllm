50cc50bfe13832932ff07acf7fdf54396339298c
Cyrus Leung <tlleungac@connect.ust.hk>
2026-01-31 20:51:15 +0800

    [Frontend] Use new Renderer for Completions and Tokenize API (#32863)
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>
    Signed-off-by: Pai <416932041@qq.com>

--- vllm/entrypoints/pooling/pooling/protocol.py
+++ vllm/entrypoints/pooling/pooling/protocol.py
@@ -3,11 +3,10 @@
 import time
 from typing import Any, Generic, TypeAlias, TypeVar
 
-from pydantic import (
-    Field,
-)
+from pydantic import Field
 
 from vllm import PoolingParams
+from vllm.config import ModelConfig
 from vllm.config.pooler import get_use_activation
 from vllm.entrypoints.openai.engine.protocol import OpenAIBaseModel, UsageInfo
 from vllm.entrypoints.pooling.base.protocol import (
@@ -18,6 +17,7 @@ from vllm.entrypoints.pooling.base.protocol import (
     EncodingRequestMixin,
     PoolingBasicRequestMixin,
 )
+from vllm.renderers import TokenizeParams
 from vllm.tasks import PoolingTask
 from vllm.utils import random_uuid
 
@@ -29,6 +29,18 @@ class PoolingCompletionRequest(
     ClassifyRequestMixin,
 ):
     task: PoolingTask | None = None
+
+    def build_tok_params(self, model_config: ModelConfig) -> TokenizeParams:
+        encoder_config = model_config.encoder_config or {}
+
+        return TokenizeParams(
+            max_total_tokens=model_config.max_model_len,
+            max_output_tokens=0,
+            truncate_prompt_tokens=self.truncate_prompt_tokens,
+            do_lower_case=encoder_config.get("do_lower_case", False),
+            add_special_tokens=self.add_special_tokens,
+            max_total_tokens_param="max_model_len",
+        )
 
     def to_pooling_params(self):
         return PoolingParams(
@@ -47,6 +59,18 @@ class PoolingChatRequest(
         default=None,
         description=("Additional kwargs to pass to the HF processor."),
     )
+
+    def build_tok_params(self, model_config: ModelConfig) -> TokenizeParams:
+        encoder_config = model_config.encoder_config or {}
+
+        return TokenizeParams(
+            max_total_tokens=model_config.max_model_len,
+            max_output_tokens=0,
+            truncate_prompt_tokens=self.truncate_prompt_tokens,
+            do_lower_case=encoder_config.get("do_lower_case", False),
+            add_special_tokens=self.add_special_tokens,
+            max_total_tokens_param="max_model_len",
+        )
 
     def to_pooling_params(self):
         return PoolingParams(
