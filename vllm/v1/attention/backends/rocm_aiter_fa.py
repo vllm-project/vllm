292bab8c668b9b8e0ad62bb8f59c3359f1461565
jennyyyyzhen <47012288+jennyyyyzhen@users.noreply.github.com>
2026-01-31 01:03:57 -0800

    [ROCM] Enable aiter attn backend for qwen3-next model (#32492)
    Signed-off-by: jennyyyyzhen <yzhen@hmc.edu>

--- vllm/v1/attention/backends/rocm_aiter_fa.py
+++ vllm/v1/attention/backends/rocm_aiter_fa.py
@@ -683,7 +683,7 @@ class AiterFlashAttentionBackend(AttentionBackend):
 
     @staticmethod
     def get_supported_kernel_block_sizes() -> list[int | MultipleOf]:
-        return [MultipleOf(16)]
+        return [16, 32]
 
     @classmethod
     def get_supported_head_sizes(cls) -> list[int]:
