53c95d70f17c5d9c91a8fbb3debd9b5c243a06d8
jma99_2333 <165961931+jma99fb@users.noreply.github.com>
2026-01-31 07:22:25 -0800

    Support clear mm and encoder cache (#33452)
    Signed-off-by: Roger Wang <hey@rogerw.io>
    Co-authored-by: Roger Wang <hey@rogerw.io>
    Signed-off-by: rayleeku <rui008@e.ntu.edu.sg>

--- vllm/v1/engine/async_llm.py
+++ vllm/v1/engine/async_llm.py
@@ -882,6 +882,9 @@ class AsyncLLM(EngineClient):
             reset_running_requests, reset_connector
         )
 
+    async def reset_encoder_cache(self) -> None:
+        await self.engine_core.reset_encoder_cache_async()
+
     async def sleep(self, level: int = 1) -> None:
         await self.reset_prefix_cache()
         await self.engine_core.sleep_async(level)
