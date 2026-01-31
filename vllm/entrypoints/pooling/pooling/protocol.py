4630bb2490802f3a118d8e1224479f1ed90cf3cb
DarkLight1337 <tlleungac@connect.ust.hk>
2026-01-31 14:00:07 +0000

    Address comment
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- vllm/entrypoints/pooling/pooling/protocol.py
+++ vllm/entrypoints/pooling/pooling/protocol.py
@@ -16,9 +16,12 @@ from vllm.entrypoints.pooling.base.protocol import (
     EncodingRequestMixin,
     PoolingBasicRequestMixin,
 )
+from vllm.logger import init_logger
 from vllm.renderers import TokenizeParams
 from vllm.tasks import PoolingTask
 from vllm.utils import random_uuid
+
+logger = init_logger(__name__)
 
 
 class PoolingCompletionRequest(
@@ -42,6 +45,13 @@ class PoolingCompletionRequest(
         )
 
     def to_pooling_params(self):
+        if self.normalize is not None:
+            logger.warning_once(
+                "`normalize` is deprecated and will be removed in v0.17. "
+                "Please pass `use_activation` instead."
+            )
+            self.use_activation = self.normalize
+
         return PoolingParams(
             truncate_prompt_tokens=self.truncate_prompt_tokens,
             use_activation=self.use_activation,
@@ -72,6 +82,13 @@ class PoolingChatRequest(
         )
 
     def to_pooling_params(self):
+        if self.normalize is not None:
+            logger.warning_once(
+                "`normalize` is deprecated and will be removed in v0.17. "
+                "Please pass `use_activation` instead."
+            )
+            self.use_activation = self.normalize
+
         return PoolingParams(
             truncate_prompt_tokens=self.truncate_prompt_tokens,
             use_activation=self.use_activation,
