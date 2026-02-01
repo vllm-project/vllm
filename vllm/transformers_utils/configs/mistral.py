f0c0ededdb61bfa92b150d9d1548278e444310e2
Andy Lo <andy@mistral.ai>
2026-02-01 20:38:03 +0000

    Fix mistral sliding window parsing
    Signed-off-by: Andy Lo <andy@mistral.ai>

--- vllm/transformers_utils/configs/mistral.py
+++ vllm/transformers_utils/configs/mistral.py
@@ -14,6 +14,7 @@ def adapt_config_dict(
     defaults: dict[str, Any],
 ) -> PretrainedConfig:
     config_dict = _remap_general_mistral_args(config_dict)
+    config_dict = _remap_mistral_sliding_window(config_dict)
 
     if bool(config_dict.get("quantization")):
         config_dict = _remap_mistral_quantization_args(config_dict)
@@ -157,6 +158,34 @@ def _remap_general_mistral_args(config: dict) -> dict:
 
     for new_key, (key, default_value) in top_level_mapping_with_default.items():
         config[new_key] = config.pop(key, default_value)
+
+    return config
+
+
+def _remap_mistral_sliding_window(config: dict) -> dict:
+    # Remap ragged_attention -> sliding_window
+    if ragged_attention_str := config.pop("ragged_attention", None):
+        # E.g. "None,32,32,32" -> [None, 32, 32, 32]
+        assert isinstance(ragged_attention_str, str)
+        ragged_pattern = [
+            None if s == "None" else int(s) for s in ragged_attention_str.split(",")
+        ]
+        assert len(set(ragged_pattern) - {None}) <= 1, ragged_pattern
+        config["sliding_window"] = ragged_pattern
+
+    # Remap sliding_window -> layer_types + sliding window for HF compatibility
+    # Mistral configs may define sliding_window as list[int]. Convert it
+    # to int and add the layer_types list[str] to make it HF compatible
+    if sliding_window := config.get("sliding_window"):
+        assert isinstance(sliding_window, list)
+        pattern_repeats = config["num_hidden_layers"] // len(sliding_window)
+        layer_types = sliding_window * pattern_repeats
+        config["layer_types"] = [
+            "full_attention" if layer_type is None else "sliding_attention"
+            for layer_type in layer_types
+        ]
+        assert len(set(sliding_window) - {None}) <= 1, sliding_window
+        config["sliding_window"] = next(filter(None, sliding_window), None)
 
     return config
 
