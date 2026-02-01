05f24b8a07a87175747081e8e970311d690ec97b
Andy Lo <andy@mistral.ai>
2026-02-01 23:15:04 +0000

    Fix
    Signed-off-by: Andy Lo <andy@mistral.ai>

--- vllm/transformers_utils/configs/mistral.py
+++ vllm/transformers_utils/configs/mistral.py
@@ -163,29 +163,24 @@ def _remap_general_mistral_args(config: dict) -> dict:
 
 
 def _remap_mistral_sliding_window(config: dict) -> dict:
-    # Remap ragged_attention -> sliding_window
-    if ragged_attention_str := config.pop("ragged_attention", None):
-        # E.g. "None,32,32,32" -> [None, 32, 32, 32]
-        assert isinstance(ragged_attention_str, str)
-        ragged_pattern = [
-            None if s == "None" else int(s) for s in ragged_attention_str.split(",")
-        ]
-        assert len(set(ragged_pattern) - {None}) <= 1, ragged_pattern
-        config["sliding_window"] = ragged_pattern
-
-    # Remap sliding_window -> layer_types + sliding window for HF compatibility
+    # Remap sliding_window (list) -> layer_types (list) + sliding window (int)
+    # for HF compatibility
     # Mistral configs may define sliding_window as list[int]. Convert it
     # to int and add the layer_types list[str] to make it HF compatible
     if sliding_window := config.get("sliding_window"):
-        assert isinstance(sliding_window, list)
-        pattern_repeats = config["num_hidden_layers"] // len(sliding_window)
-        layer_types = sliding_window * pattern_repeats
-        config["layer_types"] = [
-            "full_attention" if layer_type is None else "sliding_attention"
-            for layer_type in layer_types
-        ]
-        assert len(set(sliding_window) - {None}) <= 1, sliding_window
-        config["sliding_window"] = next(filter(None, sliding_window), None)
+        if isinstance(sliding_window, list):
+            pattern_repeats = config["num_hidden_layers"] // len(sliding_window)
+            layer_types = sliding_window * pattern_repeats
+            config["layer_types"] = [
+                "full_attention" if layer_type is None else "sliding_attention"
+                for layer_type in layer_types
+            ]
+            assert len(set(sliding_window) - {None}) <= 1, sliding_window
+            config["sliding_window"] = next(filter(None, sliding_window), None)
+        elif isinstance(sliding_window, int) and config.get("layer_types") is None:
+            config["layer_types"] = ["sliding_attention"] * config["num_hidden_layers"]
+        else:
+            raise ValueError(f"Unsupported sliding_window type: {sliding_window}")
 
     return config
 
