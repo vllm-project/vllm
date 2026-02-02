6c4e238597b62173df36b1363a1179410cb82860
Matthew Bonanni <mbonanni@redhat.com>
2026-02-02 11:53:51 -0500

    Change format
    Signed-off-by: Matthew Bonanni <mbonanni@redhat.com>

--- vllm/platforms/cuda.py
+++ vllm/platforms/cuda.py
@@ -364,7 +364,7 @@ class CudaPlatformBase(Platform):
         logger.info_once(
             "Using %s attention backend out of potential backends: %s",
             selected_backend.name,
-            tuple(b[0].name for b in valid_backends_priorities),
+            [b[0].name for b in valid_backends_priorities],
             scope="local",
         )
 
