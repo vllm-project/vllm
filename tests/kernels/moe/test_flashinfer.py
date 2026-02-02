408258eb5afd59eb648151c6e5665662b5397d44
Robert Shaw <robshaw@redhat.com>
2026-02-01 20:02:52 -0500

    fix tests
    Signed-off-by: Robert Shaw <robshaw@redhat.com>

--- tests/kernels/moe/test_flashinfer.py
+++ tests/kernels/moe/test_flashinfer.py
@@ -153,7 +153,7 @@ class TestData:
             is_act_and_mul=is_gated,
             routing_method=layer.routing_method_type,
             activation=activation,
-            device=layer.device,
+            device=w13_quantized.device,
         )
 
         return TestData(
