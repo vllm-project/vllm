09d6d5a88acc2d7e95d5c9df3c731800a6613b03
yewentao256 <zhyanwentao@126.com>
2026-02-03 21:13:25 +0000

    update
    Signed-off-by: yewentao256 <zhyanwentao@126.com>

--- vllm/compilation/fx_utils.py
+++ vllm/compilation/fx_utils.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
+import operator
 from collections.abc import Iterable, Iterator
 
 from torch import fx
@@ -11,6 +12,10 @@ from torch.fx.node import Target
 
 def is_func(node: fx.Node, target: Target) -> bool:
     return bool(node.op == "call_function" and node.target == target)
+
+
+def is_auto_func(node: fx.Node, op: OpOverload) -> bool:
+    return is_func(node, auto_functionalized) and node.args[0] == op
 
 
 # Returns the first auto_functionalized node with the given op (if it exists)
@@ -26,6 +31,22 @@ def find_auto_fn(nodes: Iterable[fx.Node], op: OpOverload) -> fx.Node:
     node = find_auto_fn_maybe(nodes, op)
     assert node is not None, f"Could not find {op} in nodes {nodes}"
     return node
+
+
+# Returns the getitem node that extracts the idx-th element from node
+# (if it exists)
+def find_getitem_maybe(node: fx.Node, idx: int) -> fx.Node | None:
+    for user in node.users:
+        if is_func(user, operator.getitem) and user.args[1] == idx:
+            return user
+    return None
+
+
+# Returns the getitem node that extracts the idx-th element from node
+def find_getitem(node: fx.Node, idx: int) -> fx.Node:
+    ret = find_getitem_maybe(node, idx)
+    assert ret is not None, f"Could not find getitem {idx} in node {node}"
+    return ret
 
 
 # An auto-functionalization-aware utility for finding nodes with a specific op
@@ -46,3 +67,11 @@ def find_op_nodes(
     for n in graph.find_nodes(op="call_function", target=auto_functionalized):
         if n.args[0] == op:
             yield n
+
+
+# Asserts that the node only has one user and returns it
+# Even if a node has only 1 user, it might share storage with another node,
+# which might need to be taken into account.
+def get_only_user(node: fx.Node) -> fx.Node:
+    assert len(node.users) == 1
+    return next(iter(node.users))
