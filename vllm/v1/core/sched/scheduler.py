3ebb8ebd4e6baddc03afdd17503568851aa2b4a7
Wentao Ye <44945378+yewentao256@users.noreply.github.com>
2026-02-03 15:18:07 -0500

    Update vllm/v1/core/sched/scheduler.py
    Co-authored-by: Nick Hill <nhill@redhat.com>
    Signed-off-by: Wentao Ye <44945378+yewentao256@users.noreply.github.com>

--- vllm/v1/core/sched/scheduler.py
+++ vllm/v1/core/sched/scheduler.py
@@ -216,9 +216,7 @@ class Scheduler(SchedulerInterface):
                 self.num_lookahead_tokens = self.num_spec_tokens
 
         # reusable read only placeholder list for speculative decoding.
-        self._spec_token_placeholders: list[int] = (
-            [-1] * self.num_spec_tokens if self.num_spec_tokens else []
-        )
+        self._spec_token_placeholders: list[int] = [-1] * self.num_spec_tokens
 
         # Create the KV cache manager.
         self.kv_cache_manager = KVCacheManager(
