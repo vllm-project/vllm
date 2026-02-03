30066de532f25cfdc678bb3f2f08b10807bc5c25
Lucas Wilkinson <LucasWilkinson@users.noreply.github.com>
2026-02-03 09:08:47 -0700

    [Attention][FA3] Update FA3 to include new swizzle optimization (#23465)
    Signed-off-by: Lucas Wilkinson <lwilkins@redhat.com>

--- cmake/external_projects/vllm_flash_attn.cmake
+++ cmake/external_projects/vllm_flash_attn.cmake
@@ -38,7 +38,7 @@ else()
   FetchContent_Declare(
           vllm-flash-attn
           GIT_REPOSITORY https://github.com/vllm-project/flash-attention.git
-          GIT_TAG 188be16520ceefdc625fdf71365585d2ee348fe2
+          GIT_TAG 2adfc8c2177c5b0e8ddeedfd5a8990d80eb496ff
           GIT_PROGRESS TRUE
           # Don't share the vllm-flash-attn build between build types
           BINARY_DIR ${CMAKE_BINARY_DIR}/vllm-flash-attn
