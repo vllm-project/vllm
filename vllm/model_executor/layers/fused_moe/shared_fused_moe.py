83c1863d91038cc85d7e5b72483cb63601244677
Bill Nell <bnell@redhat.com>
2026-02-04 10:36:16 -0500

    wip
    Signed-off-by: Bill Nell <bnell@redhat.com>

--- vllm/model_executor/layers/fused_moe/shared_fused_moe.py
+++ vllm/model_executor/layers/fused_moe/shared_fused_moe.py
@@ -3,10 +3,6 @@
 
 import torch
 
-from vllm.distributed import (
-    get_tensor_model_parallel_world_size,
-    tensor_model_parallel_all_reduce,
-)
 from vllm.model_executor.layers.fused_moe.layer import FusedMoE
 
 
@@ -23,36 +19,7 @@ class SharedFusedMoE(FusedMoE):
         hidden_states: torch.Tensor,
         router_logits: torch.Tensor,
     ) -> tuple[torch.Tensor, torch.Tensor]:
-        if not self.use_overlapped:
-            if self._shared_experts is not None:
-                shared_out = self._shared_experts(hidden_states)
-
-                # Reduce shared expert outputs if necessary, since the MLP
-                # should have been created with reduce_results=False.
-                if (
-                    self.reduce_results
-                    and get_tensor_model_parallel_world_size() > 1
-                    and self.must_reduce_shared_expert_outputs()
-                ):
-                    shared_out = tensor_model_parallel_all_reduce(shared_out)
-            else:
-                shared_out = None
-
-            fused_out = super().forward(
-                hidden_states=hidden_states,
-                router_logits=router_logits,
-            )
-        else:
-            shared_out, fused_out = super().forward(
-                hidden_states=hidden_states,
-                router_logits=router_logits,
-            )
-            # ensure early TP reduction of shared expert outputs when required
-            if (
-                shared_out is not None
-                and self.reduce_results
-                and get_tensor_model_parallel_world_size() > 1
-                and self.must_reduce_shared_expert_outputs()
-            ):
-                shared_out = tensor_model_parallel_all_reduce(shared_out)
-        return shared_out, fused_out
+        return super().forward(
+            hidden_states=hidden_states,
+            router_logits=router_logits,
+        )
