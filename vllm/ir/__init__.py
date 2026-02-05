12b7c2a674292f86b99fc41568f495af59fddf63
Luka Govedič <lgovedic@redhat.com>
2026-02-04 21:05:14 -0500

    Add layernorm op and vllm_c & aiter implementation
    Signed-off-by: Luka Govedič <lgovedic@redhat.com>

--- vllm/ir/__init__.py
+++ vllm/ir/__init__.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+from . import ops
 from .op import register_op
 
-__all__ = ["register_op"]
+__all__ = ["register_op", "ops"]
