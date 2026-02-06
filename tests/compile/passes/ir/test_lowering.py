Fix aiter (remove variance)
Signed-off-by: Luka Govedič <lgovedic@redhat.com>

tests/compile/passes/ir/test_lowering.py
@@ -4,12 +4,12 @@ import pytest
 import torch
 from torch import nn
 
-import vllm.ir.ops
 import vllm.kernels  # noqa: F401 to register kernels
 from vllm.compilation.passes.ir.lowering_pass import (
     VllmIRLoweringPass,
 )
 from vllm.config import get_current_vllm_config
+from vllm.ir import ops
 
 from ...backend import TestBackend
 
@@ -17,31 +17,41 @@ from ...backend import TestBackend
 class Model(nn.Module):
     def __init__(self, hidden_size=16, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.weight = torch.ones(hidden_size)
+        self.hidden_size = hidden_size
+        self.weight = torch.ones(hidden_size, dtype=torch.bfloat16)
 
     def forward(self, x):
         y = x + 2.0
-        z = vllm.ir.ops.rms_norm(y, self.weight, 1e-5, variance_size=4)
-        return z + 3.0
+        z = ops.rms_norm(y, self.weight, 1e-5)
+        w = ops.rms_norm(z, self.weight, 1e-5)
+        return w + 3.0
 
 
-@pytest.mark.parametrize("rms_provider", vllm.ir.ops.rms_norm.supported_providers())
+@pytest.mark.parametrize("rms_provider", ops.rms_norm.supported_providers())
 def test_lowering_rms_norm(rms_provider, default_vllm_config):
     torch.set_default_device("cuda")
 
     lowering_pass = VllmIRLoweringPass(get_current_vllm_config())
     backend = TestBackend(lowering_pass)
+    backend_unlowered = TestBackend()
 
     model = Model()
-    x = torch.randn(8, 16)
-    with vllm.ir.ops.rms_norm.set_priority([rms_provider, "native"]):
-        output_eager = model(x)
+    x = torch.randn(8, 16, dtype=torch.bfloat16)
+    with ops.rms_norm.set_priority([rms_provider, "native"]):
         compiled_model = torch.compile(model, backend=backend, fullgraph=True)
-        output_compiled = compiled_model(x)
+        compiled_unlowered_model = torch.compile(
+            model, backend=backend_unlowered, fullgraph=True
+        )
+        output = compiled_model(x)
+        output_unlowered = compiled_unlowered_model(x)
+
+    selected = lowering_pass.selected_impls["rms_norm"]
+    assert len(selected) == 2
+    assert all(p == rms_provider for p in selected.values()), selected
 
     # TODO remove print
     backend.print_graphs()
 
-    output_compiled2 = compiled_model(x)
-    torch.testing.assert_close(output_eager, output_compiled)
-    torch.testing.assert_close(output_eager, output_compiled2)
+    output2 = compiled_model(x)
+    torch.testing.assert_close(output_unlowered, output)
+    torch.testing.assert_close(output_unlowered, output2)