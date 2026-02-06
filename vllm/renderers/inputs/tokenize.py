3078096d513ae37ed19205c6a01fbb56655663bb
DarkLight1337 <tlleungac@connect.ust.hk>
2026-02-06 09:59:19 +0000

    Doc
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- vllm/renderers/inputs/tokenize.py
+++ vllm/renderers/inputs/tokenize.py
@@ -10,21 +10,21 @@ from vllm.inputs import EmbedsPrompt, TokensPrompt
 
 DecoderOnlyTokPrompt: TypeAlias = TokensPrompt | EmbedsPrompt
 """
-A [`DecoderOnlyDictPrompt`][vllm.renderers.inputs.preprocessDecoderOnlyDictPrompt]
+A [`DecoderOnlyDictPrompt`][vllm.renderers.inputs.preprocess.DecoderOnlyDictPrompt]
 that has been tokenized.
 """
 
 
 EncoderTokPrompt: TypeAlias = TokensPrompt
 """
-A [`EncoderDictPrompt`][vllm.renderers.inputs.preprocessEncoderDictPrompt]
+A [`EncoderDictPrompt`][vllm.renderers.inputs.preprocess.EncoderDictPrompt]
 that has been tokenized.
 """
 
 
 DecoderTokPrompt: TypeAlias = TokensPrompt
 """
-A [`DecoderDictPrompt`][vllm.renderers.inputs.preprocessDecoderDictPrompt]
+A [`DecoderDictPrompt`][vllm.renderers.inputs.preprocess.DecoderDictPrompt]
 that has been tokenized.
 """
 
@@ -32,7 +32,7 @@ that has been tokenized.
 class EncoderDecoderTokPrompt(TypedDict):
     """
     A
-    [`EncoderDecoderDictPrompt`][vllm.renderers.inputs.preprocessEncoderDecoderDictPrompt]
+    [`EncoderDecoderDictPrompt`][vllm.renderers.inputs.preprocess.EncoderDecoderDictPrompt]
     that has been tokenized.
     """
 
@@ -45,13 +45,13 @@ SingletonTokPrompt: TypeAlias = (
     DecoderOnlyTokPrompt | EncoderTokPrompt | DecoderTokPrompt
 )
 """
-A [`SingletonDictPrompt`][vllm.renderers.inputs.preprocessSingletonDictPrompt]
+A [`SingletonDictPrompt`][vllm.renderers.inputs.preprocess.SingletonDictPrompt]
 that has been tokenized.
 """
 
 
 TokPrompt: TypeAlias = DecoderOnlyTokPrompt | EncoderDecoderTokPrompt
 """
-A [`DictPrompt`][vllm.renderers.inputs.preprocessDictPrompt]
+A [`DictPrompt`][vllm.renderers.inputs.preprocess.DictPrompt]
 that has been tokenized.
 """
