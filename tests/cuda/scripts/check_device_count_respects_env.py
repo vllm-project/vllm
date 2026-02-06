e8d71b1ef1970c7f7d8b8c1029bd78ec1648284c
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2026-02-06 01:29:41 +0000

    wip
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- tests/cuda/scripts/check_device_count_respects_env.py
+++ tests/cuda/scripts/check_device_count_respects_env.py
@@ -10,6 +10,7 @@ for key in ["CUDA_VISIBLE_DEVICES", "HIP_VISIBLE_DEVICES", "ROCR_VISIBLE_DEVICES
     os.environ.pop(key, None)
 
 import torch  # noqa: E402
+
 from vllm.platforms import current_platform  # noqa: F401, E402
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
