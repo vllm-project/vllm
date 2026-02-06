6218b1dfd58627f67caacfb4bb875c8b1bf1d82c
Chengcheng Pei <chengchengpei@outlook.com>
2026-02-05 16:47:56 -0800

    fixed Attention import
    Signed-off-by: Chengcheng Pei <chengchengpei@outlook.com>

--- vllm/model_executor/models/qwen3.py
+++ vllm/model_executor/models/qwen3.py
@@ -30,12 +30,12 @@ import torch
 from torch import nn
 from transformers import Qwen3Config
 
-from vllm.attention.layer import Attention
 from vllm.compilation.decorators import support_torch_compile
 from vllm.config import CacheConfig, VllmConfig
 from vllm.distributed import get_pp_group, get_tensor_model_parallel_world_size
 from vllm.logger import init_logger
 from vllm.model_executor.layers.attention.encoder_only_attention import (
+    Attention,
     EncoderOnlyAttention,
 )
 from vllm.model_executor.layers.layernorm import RMSNorm
