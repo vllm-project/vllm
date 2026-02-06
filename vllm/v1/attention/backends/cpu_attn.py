b146e3f77ff3df60d05ef84b41ccd3cee6c9b478
Gassan Salama <gassan.salama@arm.com>
2026-02-06 07:01:48 +0000

    [cpu][performance] CPU Paged Attention NEON BFMMLA BF16 Implementation (#32263)
    Signed-off-by: Gassan <gassan.salama@arm.com>

--- vllm/v1/attention/backends/cpu_attn.py
+++ vllm/v1/attention/backends/cpu_attn.py
@@ -487,10 +487,12 @@ def _get_attn_isa(
     if head_size is not None and head_size % 32 != 0 and head_size % 16 == 0:
         return "vec16"
     supports_amx = torch._C._cpu._is_amx_tile_supported()
+    supports_arm = current_platform.get_cpu_architecture() == CpuArchEnum.ARM
     if supports_amx and dtype in (torch.bfloat16,) and block_size % 32 == 0:
         return "amx"
     elif block_size % 32 == 0:
-        if current_platform.get_cpu_architecture() == CpuArchEnum.ARM:
+        if supports_arm:
+            # support ARM NEON FMLA and BFMMLA (bf16) for block size 32
             return "neon"
         else:
             return "vec"
