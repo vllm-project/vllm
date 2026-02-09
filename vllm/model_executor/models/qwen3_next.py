4d301ca61bd294107cb9fe946f16fd47e256ef50
zjy0516 <riverclouds.zhu@qq.com>
2026-02-09 11:37:08 +0800

    update
    Signed-off-by: zjy0516 <riverclouds.zhu@qq.com>

--- vllm/model_executor/models/qwen3_next.py
+++ vllm/model_executor/models/qwen3_next.py
@@ -28,6 +28,7 @@ from vllm.distributed import (
 )
 from vllm.forward_context import ForwardContext, get_forward_context
 from vllm.logger import init_logger
+from vllm.model_executor.custom_op import CustomOp
 from vllm.model_executor.layers.attention import Attention
 from vllm.model_executor.layers.fla.ops import (
     chunk_gated_delta_rule as fla_chunk_gated_delta_rule,
@@ -145,18 +146,19 @@ def fi_chunk_gated_delta_rule(
     )
 
 
-class ChunkGatedDeltaRule(nn.Module):
+@CustomOp.register("chunk_gated_delta_rule")
+class ChunkGatedDeltaRule(CustomOp):
     def __init__(self) -> None:
         super().__init__()
         if current_platform.is_cuda() and current_platform.is_device_capability(90):
             logger.info_once(
                 "Using FlashInfer GDN prefill kernel on CUDA compute capability 9.x"
             )
-            self.func = fi_chunk_gated_delta_rule
+            self._forward_method = self.forward_cuda
         else:
-            self.func = fla_chunk_gated_delta_rule
+            self._forward_method = self.forward_native
 
-    def forward(
+    def forward_cuda(
         self,
         q: torch.Tensor,
         k: torch.Tensor,
@@ -169,7 +171,33 @@ class ChunkGatedDeltaRule(nn.Module):
         head_first: bool = False,
         use_qk_l2norm_in_kernel: bool = True,
     ):
-        return self.func(
+        return fi_chunk_gated_delta_rule(
+            q=q,
+            k=k,
+            v=v,
+            g=g,
+            beta=beta,
+            initial_state=initial_state,
+            output_final_state=output_final_state,
+            cu_seqlens=cu_seqlens,
+            head_first=head_first,
+            use_qk_l2norm_in_kernel=use_qk_l2norm_in_kernel,
+        )
+
+    def forward_native(
+        self,
+        q: torch.Tensor,
+        k: torch.Tensor,
+        v: torch.Tensor,
+        g: torch.Tensor,
+        beta: torch.Tensor,
+        initial_state: torch.Tensor,
+        output_final_state: bool,
+        cu_seqlens: torch.LongTensor | None = None,
+        head_first: bool = False,
+        use_qk_l2norm_in_kernel: bool = True,
+    ):
+        return fla_chunk_gated_delta_rule(
             q=q,
             k=k,
             v=v,
