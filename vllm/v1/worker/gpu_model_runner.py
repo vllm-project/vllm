46aaf0f5038d025f7a36be3507bb88285b0a8cfa
tianyu.jiang <tianyu.jiang@enflame-tech.com>
2026-02-09 14:32:32 +0800

    [BugFix] Mistakenly passing num_reqs_padded as num_reqs in _dummy_run
    Signed-off-by: tianyu.jiang <tianyu.jiang@enflame-tech.com>

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -4787,7 +4787,7 @@ class GPUModelRunner(
             pad_attn = cudagraph_runtime_mode == CUDAGraphMode.FULL
             attn_metadata, _ = self._build_attention_metadata(
                 num_tokens=num_tokens_unpadded,
-                num_reqs=num_reqs_padded,
+                num_reqs=num_reqs,
                 max_query_len=max_query_len,
                 ubatch_slices=ubatch_slices_padded if pad_attn else ubatch_slices,
                 for_cudagraph_capture=is_graph_capturing,
