8f0ff64acbb4e0b122269b90a3bc1fff27457d5e
DarkLight1337 <tlleungac@connect.ust.hk>
2026-02-11 18:16:04 +0000

    Reorder
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- vllm/sampling_params.py
+++ vllm/sampling_params.py
@@ -603,37 +603,11 @@ class SamplingParams(
         structured_outputs_config: StructuredOutputsConfig | None,
         tokenizer: TokenizerLike | None,
     ) -> None:
-        self._validate_allowed_token_ids(tokenizer)
         self._validate_logprobs(model_config)
         self._validate_logit_bias(model_config)
+        self._validate_allowed_token_ids(tokenizer)
         self._validate_spec_decode(speculative_config)
         self._validate_structured_outputs(structured_outputs_config, tokenizer)
-
-    def _validate_allowed_token_ids(self, tokenizer: TokenizerLike | None) -> None:
-        allowed_token_ids = self.allowed_token_ids
-        if allowed_token_ids is None:
-            return
-
-        if len(allowed_token_ids) == 0:
-            raise VLLMValidationError(
-                "allowed_token_ids is not None and empty!",
-                parameter="allowed_token_ids",
-                value=allowed_token_ids,
-            )
-
-        if tokenizer is not None:
-            vocab_size = len(tokenizer)
-            invalid_token_ids = [
-                token_id
-                for token_id in allowed_token_ids
-                if token_id < 0 or token_id >= vocab_size
-            ]
-            if invalid_token_ids:
-                raise VLLMValidationError(
-                    "allowed_token_ids contains out-of-vocab token id!",
-                    parameter="allowed_token_ids",
-                    value=invalid_token_ids,
-                )
 
     def _validate_logprobs(self, model_config: ModelConfig) -> None:
         max_logprobs = model_config.max_logprobs
@@ -683,6 +657,32 @@ class SamplingParams(
                 parameter="logit_bias",
                 value=invalid_token_ids,
             )
+
+    def _validate_allowed_token_ids(self, tokenizer: TokenizerLike | None) -> None:
+        allowed_token_ids = self.allowed_token_ids
+        if allowed_token_ids is None:
+            return
+
+        if len(allowed_token_ids) == 0:
+            raise VLLMValidationError(
+                "allowed_token_ids is not None and empty!",
+                parameter="allowed_token_ids",
+                value=allowed_token_ids,
+            )
+
+        if tokenizer is not None:
+            vocab_size = len(tokenizer)
+            invalid_token_ids = [
+                token_id
+                for token_id in allowed_token_ids
+                if token_id < 0 or token_id >= vocab_size
+            ]
+            if invalid_token_ids:
+                raise VLLMValidationError(
+                    "allowed_token_ids contains out-of-vocab token id!",
+                    parameter="allowed_token_ids",
+                    value=invalid_token_ids,
+                )
 
     def _validate_spec_decode(
         self,
