82b94b72f988f4804674560b2730d90ab26efa3c
Kunshang Ji <kunshang.ji@intel.com>
2026-02-10 21:55:39 -0800

    replace more
    Signed-off-by: Kunshang Ji <kunshang.ji@intel.com>

--- vllm/model_executor/layers/quantization/quark/quark_moe.py
+++ vllm/model_executor/layers/quantization/quark/quark_moe.py
@@ -907,7 +907,7 @@ class QuarkOCP_MX_MoEMethod(QuarkMoEMethod):
 
         # secondly, process mxfp weights
         if self.emulate:
-            torch.cuda.empty_cache()
+            torch.accelerator.empty_cache()
             return
 
         from aiter.utility.fp4_utils import e8m0_shuffle
