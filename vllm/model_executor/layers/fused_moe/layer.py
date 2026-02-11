bbf1b759a13f598c91e939148b7a0abaa96a6672
bnellnm <49004751+bnellnm@users.noreply.github.com>
2026-02-11 17:33:14 -0500

    [Bugfix] Fix some issues with MoERunner PR #32344 (#34371)
    Signed-off-by: Bill Nell <bnell@redhat.com>

--- vllm/model_executor/layers/fused_moe/layer.py
+++ vllm/model_executor/layers/fused_moe/layer.py
@@ -700,7 +700,7 @@ class FusedMoE(CustomOp):
 
     @property
     def gate(self) -> torch.nn.Module | None:
-        return self._gate
+        return self._gate if self.use_overlapped else None
 
     @property
     def tp_size(self):
@@ -725,7 +725,7 @@ class FusedMoE(CustomOp):
     @property
     def is_internal_router(self) -> bool:
         # By default, router/gate is called before FusedMoE forward pass
-        return self._gate is not None
+        return self.gate is not None
 
     def _maybe_init_expert_routing_tables(
         self,
@@ -1457,7 +1457,6 @@ class FusedMoE(CustomOp):
         hidden_states: torch.Tensor,
         router_logits: torch.Tensor,
     ) -> torch.Tensor | tuple[torch.Tensor, torch.Tensor]:
-        self.ensure_moe_quant_config_init()
         return self.runner.forward(
             hidden_states,
             router_logits,
