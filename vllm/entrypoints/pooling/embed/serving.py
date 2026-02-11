16a84485a69ca4ead57f432f92c3020d1d20bb90
DarkLight1337 <tlleungac@connect.ust.hk>
2026-02-11 10:01:42 +0000

    Fix
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- vllm/entrypoints/pooling/embed/serving.py
+++ vllm/entrypoints/pooling/embed/serving.py
@@ -232,7 +232,7 @@ class OpenAIServingEmbedding(OpenAIServing):
         """Check if chunked processing should be used for this request."""
         return (
             isinstance(request, (EmbeddingCompletionRequest, EmbeddingChatRequest))
-            and self.pooler_config.supports_chunked_processing
+            and self.pooler_config.enable_chunked_processing
         )
 
     async def _process_chunked_request(
