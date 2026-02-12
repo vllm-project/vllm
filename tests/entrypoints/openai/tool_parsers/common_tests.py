814eacead6693cd20ef1b26f7bacd2c372e706f1
Ben Browning <bbrownin@redhat.com>
2026-02-12 15:49:42 -0500

    Adjust import order for pre-commit
    Signed-off-by: Ben Browning <bbrownin@redhat.com>

--- tests/entrypoints/openai/tool_parsers/common_tests.py
+++ tests/entrypoints/openai/tool_parsers/common_tests.py
@@ -9,8 +9,8 @@ from typing import Any
 import pytest
 
 from tests.entrypoints.openai.tool_parsers.utils import run_tool_extraction
-from vllm.tool_parsers import ToolParserManager
 from vllm.tokenizers import TokenizerLike
+from vllm.tool_parsers import ToolParserManager
 
 
 @dataclass
