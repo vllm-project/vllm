2fc5eecec8e9d6d889c03236b53467eae8481d70
Isotr0py <mozf@mail2.sysu.edu.cn>
2026-02-13 11:50:09 +0800

    fix
    Signed-off-by: Isotr0py <mozf@mail2.sysu.edu.cn>

--- vllm/model_executor/models/qwen3_next.py
+++ vllm/model_executor/models/qwen3_next.py
@@ -410,7 +410,7 @@ class Qwen3NextGatedDeltaNet(nn.Module, MambaBase):
         self.in_proj_qkv = MergedColumnParallelLinear(
             input_size=self.hidden_size,
             output_sizes=[
-                sum(self.key_dim, self.key_dim, self.value_dim),
+                sum((self.key_dim, self.key_dim, self.value_dim)),
                 self.value_dim,
             ],
             bias=False,
