38d6fbb97224ffc4e8eca4fe77dd7d727219b548
Athrael Soju <athrael.soju@gmail.com>
2026-02-14 15:50:20 +0000

    [Feature] Add ColModernVBERT model and configuration for multimodal retrieval
    Signed-off-by: Athrael Soju <athrael.soju@gmail.com>

--- vllm/transformers_utils/config.py
+++ vllm/transformers_utils/config.py
@@ -74,6 +74,7 @@ _CONFIG_REGISTRY: dict[str, type[PretrainedConfig]] = LazyConfigDict(
     afmoe="AfmoeConfig",
     bagel="BagelConfig",
     chatglm="ChatGLMConfig",
+    colmodernvbert="ColModernVBertConfig",
     deepseek_vl_v2="DeepseekVLV2Config",
     deepseek_v32="DeepseekV3Config",
     flex_olmo="FlexOlmoConfig",
