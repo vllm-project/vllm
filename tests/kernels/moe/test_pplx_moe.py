58ac35f5324021672d19fa842e482b8cc5329360
Robert Shaw <robshaw@redhat.com>
2026-02-16 22:10:39 -0500

    trying to fix pplx texts
    Signed-off-by: Robert Shaw <robshaw@redhat.com>

--- tests/kernels/moe/test_pplx_moe.py
+++ tests/kernels/moe/test_pplx_moe.py
@@ -38,6 +38,7 @@ from tests.kernels.quant_utils import dequant
 from tests.kernels.utils import torch_experts
 from vllm.config import VllmConfig, set_current_vllm_config
 from vllm.model_executor.layers.fused_moe import fused_topk, override_config
+from vllm.model_executor.layers.fused_moe.activation import MoEActivation
 from vllm.model_executor.layers.fused_moe.config import FusedMoEQuantConfig
 from vllm.model_executor.layers.fused_moe.fused_batched_moe import BatchedTritonExperts
 from vllm.model_executor.layers.fused_moe.fused_moe import get_default_config
@@ -608,13 +609,15 @@ def pplx_moe(
     else:
         _fused_experts = fused_experts
 
-    out = _fused_experts(
+    out = _fused_experts.apply(
         a_chunk,
         w1_chunk,
         w2_chunk,
         chunk_topk_weight,
         chunk_topk_ids,
         global_num_experts=num_experts,
+        activation=MoEActivation.SILU,
+        expert_map=None,
     )
 
     if use_cudagraphs:
