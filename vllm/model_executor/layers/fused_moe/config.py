73b3fe49298dd1ece33df8c905b5ee4b6a6fae50
Robert Shaw <robshaw@redhat.com>
2026-02-18 12:13:26 -0500

    clean up
    Signed-off-by: Robert Shaw <robshaw@redhat.com>

--- vllm/model_executor/layers/fused_moe/config.py
+++ vllm/model_executor/layers/fused_moe/config.py
@@ -127,19 +127,28 @@ def get_routing_method_type(
     scoring_func: str,
     top_k: int,
     renormalize: bool,
+    num_expert_group: int | None,
+    has_e_score_bias: bool,
 ) -> RoutingMethodType:
+    if has_e_score_bias:
+        if (num_expert_group or 0) > 0 and scoring_func == "sigmoid":
+            return RoutingMethodType.DeepSeekV3
+        else:
+            return RoutingMethodType.Unspecified
+
     if scoring_func == "sigmoid":
         if top_k == 1:
             return RoutingMethodType.Llama4
         else:
             return RoutingMethodType.Unspecified
-    elif scoring_func == "softmax":
+
+    if scoring_func == "softmax":
         if renormalize:
             return RoutingMethodType.Renormalize
         else:
             return RoutingMethodType.Default
-    else:
-        return RoutingMethodType.Unspecified
+
+    return RoutingMethodType.Unspecified
 
 
 @dataclass
