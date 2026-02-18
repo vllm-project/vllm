c77fe3a10f034f79e4811b6510acedf2f14103a2
timon0305 <timon0305@outlook.com>
2026-02-18 16:29:33 +0100

    feat(api): add optional admin control plane API and CLI
    Signed-off-by: timon0305 <timon0305@outlook.com>

--- vllm/entrypoints/serve/__init__.py
+++ vllm/entrypoints/serve/__init__.py
@@ -52,6 +52,12 @@ def register_vllm_serve_api_routers(app: FastAPI):
 
     attach_tokenize_router(app)
 
+    from vllm.entrypoints.serve.admin.api_router import (
+        attach_router as attach_admin_router,
+    )
+
+    attach_admin_router(app)
+
     from .instrumentator import register_instrumentator_api_routers
 
     register_instrumentator_api_routers(app)
