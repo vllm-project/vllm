6b563d477889926cfcac4703e57e304e3e254fc4
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2026-02-20 16:05:59 +0100

    Leave tensor schema skip alone and add another for hf reasons
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- tests/models/multimodal/processing/test_tensor_schema.py
+++ tests/models/multimodal/processing/test_tensor_schema.py
@@ -166,7 +166,12 @@ def test_model_tensor_schema(model_id: str):
 
     model_info = HF_EXAMPLE_MODELS.find_hf_info(model_id)
     model_info.check_available_online(on_fail="skip")
-    model_info.check_transformers_version(on_fail="skip")
+    model_info.check_transformers_version(
+        on_fail="skip",
+        check_max_version=False,
+        check_version_reason="vllm",
+    )
+    model_info.check_requirements(on_fail="skip", check_version_reason="hf")
 
     model_arch = next(
         arch for arch, info in HF_EXAMPLE_MODELS.hf_models.items() if info == model_info
