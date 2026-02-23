cf9a8b07d211f7c8b14bdd227083199af6acf8f2
atalman <atalman@fb.com>
2026-02-23 12:20:29 -0800

    update_torch_211

--- vllm/v1/attention/backends/cpu_attn.py
+++ vllm/v1/attention/backends/cpu_attn.py
@@ -486,7 +486,7 @@ def _get_attn_isa(
 ) -> str:
     if head_size is not None and head_size % 32 != 0 and head_size % 16 == 0:
         return "vec16"
-    supports_amx = torch._C._cpu._is_amx_tile_supported()
+    supports_amx = torch.cpu._is_amx_tile_supported()
     supports_arm = current_platform.get_cpu_architecture() == CpuArchEnum.ARM
     supports_vxe = current_platform.get_cpu_architecture() == CpuArchEnum.S390X
     if supports_amx and dtype in (torch.bfloat16,) and block_size % 32 == 0:
