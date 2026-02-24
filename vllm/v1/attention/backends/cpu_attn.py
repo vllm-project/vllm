0a2e27dba7cfb4c131cd9f6f7d700ee2be83a697
R3hankhan <Rehan.Khan7@ibm.com>
2026-02-24 20:55:39 +0530

    [CPU][Perf] Accelerate Attention head for s390x using vector intrinsics (#34434)
    Signed-off-by: Rehan Khan <Rehan.Khan7@ibm.com>
    Co-authored-by: Li, Jiang <jiang1.li@intel.com>

--- vllm/v1/attention/backends/cpu_attn.py
+++ vllm/v1/attention/backends/cpu_attn.py
@@ -25,7 +25,7 @@ from vllm.v1.kv_cache_interface import AttentionSpec, CrossAttentionSpec
 
 logger = init_logger(__name__)
 
-_CPU_ARCH_PREFER_MIXED_BATCH = (CpuArchEnum.X86, CpuArchEnum.ARM)
+_CPU_ARCH_PREFER_MIXED_BATCH = (CpuArchEnum.X86, CpuArchEnum.ARM, CpuArchEnum.S390X)
 
 
 class CPUAttentionBackend(AttentionBackend):
@@ -488,12 +488,15 @@ def _get_attn_isa(
         return "vec16"
     supports_amx = torch._C._cpu._is_amx_tile_supported()
     supports_arm = current_platform.get_cpu_architecture() == CpuArchEnum.ARM
+    supports_vxe = current_platform.get_cpu_architecture() == CpuArchEnum.S390X
     if supports_amx and dtype in (torch.bfloat16,) and block_size % 32 == 0:
         return "amx"
     elif block_size % 32 == 0:
         if supports_arm:
             # support ARM NEON FMLA and BFMMLA (bf16) for block size 32
             return "neon"
+        elif supports_vxe:
+            return "vxe"
         else:
             return "vec"
     else:
