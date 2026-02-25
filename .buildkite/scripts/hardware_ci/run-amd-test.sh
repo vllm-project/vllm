668c4505acdd564a408a3b52c13bf9c1b3bef94e
tjtanaa <tunjian.tan@embeddedllm.com>
2026-02-25 06:54:06 +0000

    remove add aws cli installation step
    Signed-off-by: tjtanaa <tunjian.tan@embeddedllm.com>

--- .buildkite/scripts/hardware_ci/run-amd-test.sh
+++ .buildkite/scripts/hardware_ci/run-amd-test.sh
@@ -73,6 +73,8 @@ done
 echo "--- Pulling container"
 image_name="public.ecr.aws/q9t5s3a7/vllm-ci-test-repo:${BUILDKITE_COMMIT}-rocm"
 container_name="rocm_${BUILDKITE_COMMIT}_$(tr -dc A-Za-z0-9 < /dev/urandom | head -c 10; echo)"
+# Install AWS CLI to authenticate to ECR Public Gallery to get higher rate limit for pulling images
+sudo apt-get update && sudo apt-get install -y awscli
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/q9t5s3a7
 docker pull "${image_name}"
 
