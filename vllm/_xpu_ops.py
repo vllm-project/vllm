a9ff4f80c0ae20156384a6d5390882dec1fc5653
Chendi.Xue <chendi.xue@intel.com>
2026-02-25 09:31:07 -0600

    [XPU]Fix for Qwen-OMNI crash (#35249)
    Signed-off-by: Chendi Xue <chendi.xue@intel.com>

--- vllm/_xpu_ops.py
+++ vllm/_xpu_ops.py
@@ -105,9 +105,10 @@ class xpu_ops:
             assert len(window_size) == 2
             real_window_size = (window_size[0], window_size[1])  # noqa: F841
 
-        # In encode attention, v maybe not contiguous and current
+        # In encode attention, k and v maybe not contiguous and current
         # kernel can't handle it
         if block_table is None:
+            k = k.contiguous()
             v = v.contiguous()
         return flash_attn_varlen_func(
             out=out,
