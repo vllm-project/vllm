e23e78cc8faf12ed20a0bf88ba9c59dd796b21e0
zjy0516 <riverclouds.zhu@qq.com>
2026-02-27 01:00:16 +0800

    init
    Signed-off-by: zjy0516 <riverclouds.zhu@qq.com>

--- vllm/config/vllm.py
+++ vllm/config/vllm.py
@@ -126,6 +126,7 @@ def enable_allreduce_rms_fusion(cfg: "VllmConfig") -> bool:
         # tp-dp combination broken:
         # https://github.com/vllm-project/vllm/issues/34458
         and cfg.parallel_config.data_parallel_size == 1
+        and cfg.parallel_config.pipeline_parallel_size == 1
     )
 
 
