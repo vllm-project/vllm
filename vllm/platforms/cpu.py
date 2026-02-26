3340b60ab0e4ae573785e97346fb848500f229ac
jiang1.li <jiang1.li@intel.com>
2026-02-26 16:04:25 +0000

    init
    Signed-off-by: jiang1.li <jiang1.li@intel.com>

--- vllm/platforms/cpu.py
+++ vllm/platforms/cpu.py
@@ -483,3 +483,30 @@ class CpuPlatform(Platform):
     @classmethod
     def support_hybrid_kv_cache(cls) -> bool:
         return True
+
+    @classmethod
+    def import_kernels(cls) -> None:
+        if Platform.get_cpu_architecture() in (CpuArchEnum.X86,):
+            # Note: The lib name are _C_AVX512 and _C_AVX2, but the module names
+            # are defined as _C. This will cause a exception "dynamic module does
+            # not define module export function". But the library is imported
+            # successfully. So ignore the exception for now, until we find
+            # a solution.
+            ignored_warning = "dynamic module does not define module export function"
+            if torch._C._cpu._is_avx512_supported():
+                try:
+                    import vllm._C_AVX512  # noqa: F401
+                except ImportError as e:
+                    if ignored_warning not in e.msg:
+                        logger.warning("Failed to import from vllm._C_AVX512: %r", e)
+            else:
+                try:
+                    import vllm._C_AVX2  # noqa: F401
+                except ImportError as e:
+                    if ignored_warning not in e.msg:
+                        logger.warning("Failed to import from vllm._C_AVX2: %r", e)
+        else:
+            try:
+                import vllm._C  # noqa: F401
+            except ImportError as e:
+                logger.warning("Failed to import from vllm._C: %r", e)
