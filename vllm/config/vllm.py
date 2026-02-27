634550593a5ab8681fab61d4fd405fd17a64129c
Jiangyun Zhu <riverclouds.zhu@qq.com>
2026-02-27 12:18:52 +0800

    [Bugfix] disable allreduce_rms_fusion by default when pp size > 1 (#35424)
    Signed-off-by: zjy0516 <riverclouds.zhu@qq.com>

--- vllm/config/vllm.py
+++ vllm/config/vllm.py
@@ -126,6 +126,9 @@ def enable_allreduce_rms_fusion(cfg: "VllmConfig") -> bool:
         # tp-dp combination broken:
         # https://github.com/vllm-project/vllm/issues/34458
         and cfg.parallel_config.data_parallel_size == 1
+        # tp-pp combination broken:
+        # https://github.com/vllm-project/vllm/issues/35426
+        and cfg.parallel_config.pipeline_parallel_size == 1
     )
 
 
