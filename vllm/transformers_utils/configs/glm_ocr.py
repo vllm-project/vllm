d9312629e9e316089904dc270ee4bd088026c199
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2026-02-27 14:22:17 +0100

    Fix `TypeError` in GLM OCR config
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- vllm/transformers_utils/configs/glm_ocr.py
+++ vllm/transformers_utils/configs/glm_ocr.py
@@ -74,8 +74,8 @@ class GlmOcrConfig(PretrainedConfig):
         if isinstance(text_config, dict):
             from transformers import AutoConfig
 
-            model_type = text_config.get("model_type", "chatglm")
-            self.text_config = AutoConfig.for_model(model_type, **text_config)
+            text_config.setdefault("model_type", "chatglm")
+            self.text_config = AutoConfig.for_model(**text_config)
         elif text_config is None:
             from transformers import AutoConfig
 
