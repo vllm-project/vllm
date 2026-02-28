ff5fcd29aee139db9b9ac229c9e4620d6be943fd
zhenwei-intel <zhenwei.liu@intel.com>
2026-02-27 19:57:33 -0800

    update of install nixl and ucx
    Signed-off-by: zhenwei-intel <zhenwei.liu@intel.com>

--- tools/install_nixl_from_source_ubuntu.py
+++ tools/install_nixl_from_source_ubuntu.py
@@ -32,8 +32,6 @@ def get_latest_nixl_version():
 
 
 NIXL_VERSION = os.environ.get("NIXL_VERSION", get_latest_nixl_version())
-UCX_VERSION = os.environ.get("UCX_VERSION", "v1.19.x")
-WITH_ZE = os.environ.get("WITH_ZE", "no").lower()
 
 
 def run_command(command, cwd=".", env=None):
@@ -141,7 +139,7 @@ def build_and_install_prerequisites(args):
     if not os.path.exists(UCX_DIR):
         run_command(["git", "clone", UCX_REPO_URL, UCX_DIR])
     ucx_source_path = os.path.abspath(UCX_DIR)
-    run_command(["git", "checkout", UCX_VERSION], cwd=ucx_source_path)
+    run_command(["git", "checkout", "v1.19.x"], cwd=ucx_source_path)
     run_command(["./autogen.sh"], cwd=ucx_source_path)
     configure_command = [
         "./configure",
@@ -154,7 +152,7 @@ def build_and_install_prerequisites(args):
         "--enable-devel-headers",
         "--with-verbs",
         "--enable-mt",
-        f"--with-ze={WITH_ZE}",
+        "--with-ze=no",
     ]
     run_command(configure_command, cwd=ucx_source_path)
     run_command(["make", "-j", str(os.cpu_count() or 1)], cwd=ucx_source_path)
