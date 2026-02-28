8a3a3f17e897dd9297ee89d08ffef16dd6e19cb7
DarkLight1337 <tlleungac@connect.ust.hk>
2026-02-28 17:00:38 +0000

    Rename
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- vllm/benchmarks/datasets.py
+++ vllm/benchmarks/datasets.py
@@ -2695,7 +2695,9 @@ class MMVUDataset(HuggingFaceDataset):
         if parser_fn is None:
             raise ValueError(f"Unsupported dataset path: {self.hf_name}")
 
-        remote_path = f"https://huggingface.co/datasets/{self.hf_name}/resolve/main"
+        remote_path_root = (
+            f"https://huggingface.co/datasets/{self.hf_name}/resolve/main"
+        )
         local_path_root = snapshot_download(self.hf_name, repo_type="dataset")
 
         output_len = output_len if output_len is not None else self.DEFAULT_OUTPUT_LEN
@@ -2707,7 +2709,7 @@ class MMVUDataset(HuggingFaceDataset):
 
             prompt = parser_fn(item)
             mm_content = process_video(
-                item["video"].replace(remote_path, local_path_root)
+                item["video"].replace(remote_path_root, local_path_root)
             )
             prompt_len = len(tokenizer.encode(prompt))
             if enable_multimodal_chat:
