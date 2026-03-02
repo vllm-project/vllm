3def34d3c08cbcf60151ee8cff604658dd9e1bb9
wang.yuqi <yuqi.wang@daocloud.io>
2026-03-02 16:22:45 +0800

    fix
    Signed-off-by: wang.yuqi <yuqi.wang@daocloud.io>

--- vllm/entrypoints/pooling/base/typing.py
+++ vllm/entrypoints/pooling/base/typing.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+
 from typing import TypeAlias
 
 from vllm.entrypoints.pooling.classify.protocol import (
