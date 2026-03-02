90e60307f1e0c4394f45b0cf68dec1130f1c6c70
Andrii Skliar <askliar@nvidia.com>
2026-03-02 13:45:38 +0100

    run pre-commit formatting
    Signed-off-by: Andrii Skliar <askliar@nvidia.com>

--- vllm/multimodal/utils.py
+++ vllm/multimodal/utils.py
@@ -2,7 +2,6 @@
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
 import mimetypes
-import warnings
 from collections import defaultdict
 from collections.abc import Generator, Sequence
 from itertools import groupby
