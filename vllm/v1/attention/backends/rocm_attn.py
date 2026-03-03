6212617ea77c42d2f7fade9a4ba118d4fb12e1b0
Jartx <sagformas@epdcenter.es>
2026-03-03 23:49:05 +0100

    Clarify comment on non-standard model block sizes
    Signed-off-by: JartX <sagformas@epdcenter.es>

--- vllm/v1/attention/backends/rocm_attn.py
+++ vllm/v1/attention/backends/rocm_attn.py
@@ -174,7 +174,7 @@ class RocmAttentionBackend(AttentionBackend):
         # but vLLM should allow support for non-standard sizes via the Triton path,
         # as addressed in this PR: https://github.com/vllm-project/vllm/pull/31380,
         # where the Triton kernel under rocm_atten does not support inference
-        # for a non-standard qwen3-next model with a block_size or qwen3_5
+        # for a non-standard qwen3-next model with a block_size of 544 or qwen3_5
         # with a block_size of 1056
         # We have fixed the Triton kernel so that the standard model uses the original
         # bit-addressing logic, while the non-standard model
