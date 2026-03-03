ce65108c9a6da3d9d9238b6a028e2a48201e6e7f
wang.yuqi <yuqi.wang@daocloud.io>
2026-03-03 16:54:29 +0800

    init
    Signed-off-by: wang.yuqi <yuqi.wang@daocloud.io>

--- tests/models/language/generation_ppl_test/test_qwen.py
+++ tests/models/language/generation_ppl_test/test_qwen.py
@@ -8,11 +8,10 @@ from tests.models.utils import GenerateModelInfo
 from .ppl_utils import wikitext_ppl_test
 
 MODELS = [
-    GenerateModelInfo("Qwen/Qwen3-0.6B"),
-    GenerateModelInfo("Qwen/Qwen3-0.6B-FP8"),
-    # transformers:
-    # Loading a GPTQ quantized model requires optimum, gptqmodel
-    # GenerateModelInfo("Qwen/Qwen3-0.6B-GPTQ-Int8"),
+    GenerateModelInfo("Qwen/Qwen3-0.6B", hf_ppl=23.864173889160156),
+    GenerateModelInfo("Qwen/Qwen3-0.6B-FP8", hf_ppl=24.313045501708984),
+
+    GenerateModelInfo("Qwen/Qwen3.5-0.8B", hf_ppl=19.38858413696289)
 ]
 
 
