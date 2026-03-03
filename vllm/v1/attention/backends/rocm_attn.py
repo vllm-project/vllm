2dbcd4b4aea5cb5ef0b3d462e4aac5cb7413339d
JartX <sagformas@epdcenter.es>
2026-03-03 23:30:12 +0100

    precommit
    Signed-off-by: JartX <sagformas@epdcenter.es>

--- vllm/v1/attention/backends/rocm_attn.py
+++ vllm/v1/attention/backends/rocm_attn.py
@@ -175,7 +175,7 @@ class RocmAttentionBackend(AttentionBackend):
         # as addressed in this PR: https://github.com/vllm-project/vllm/pull/31380,
         # where the Triton kernel under rocm_atten does not support inference
         # for a non-standard qwen3-next model with a block_size or qwen3_5
-	# with a block_size of 1056
+        # with a block_size of 1056
         # We have fixed the Triton kernel so that the standard model uses the original
         # bit-addressing logic, while the non-standard model
         # uses our optimized kernel logic.
