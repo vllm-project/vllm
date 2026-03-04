d440f752bc53f958e4bed6c9f789f33f550d13bc
Felix Marty <Felix.Marty@amd.com>
2026-03-04 18:52:28 +0100

    fix wrong inversion
    Signed-off-by: Felix Marty <Felix.Marty@amd.com>

--- vllm/model_executor/layers/fused_moe/oracle/nvfp4.py
+++ vllm/model_executor/layers/fused_moe/oracle/nvfp4.py
@@ -366,9 +366,6 @@ def convert_to_nvfp4_moe_kernel_format(
                 f" a13_scale={a13_scale}, a2_scale={a2_scale}"
             )
 
-        w13_scale_2 = 1.0 / w13_scale_2
-        w2_scale_2 = 1.0 / w2_scale_2
-
         a13_scale = a13_scale[0][0]
         a13_scale = 1.0 / a13_scale
 
