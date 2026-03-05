2602b3f8af29b89ec7e90a49039d5264619f38d5
Raphael Rialland <raphael.rialland@mistral.ai>
2026-03-05 16:30:20 +0100

    implement dispatch fix for FULL uniform decode speculative decoding
    Signed-off-by: Raphael Rialland <raphael.rialland@mistral.ai>

--- vllm/v1/cudagraph_dispatcher.py
+++ vllm/v1/cudagraph_dispatcher.py
@@ -297,12 +297,15 @@ class CudagraphDispatcher:
             num_tokens, uniform_decode, has_lora, effective_num_active_loras
         )
 
+        normalized_uniform = uniform_decode and self.cudagraph_mode.separate_routine()
+        batch_desc = self._create_padded_batch_descriptor(
+                    num_tokens, normalized_uniform, has_lora, effective_num_active_loras
+        )
+
         if CUDAGraphMode.FULL in allowed_modes:
             # check if key exists for full cudagraph
             # For pure FULL mode, keys are registered with uniform=False.
             batch_desc_to_check = batch_desc
-            if self.cudagraph_mode == CUDAGraphMode.FULL:
-                batch_desc_to_check = replace(batch_desc, uniform=False)
             if batch_desc_to_check in self.cudagraph_keys[CUDAGraphMode.FULL]:
                 return CUDAGraphMode.FULL, batch_desc_to_check
 
