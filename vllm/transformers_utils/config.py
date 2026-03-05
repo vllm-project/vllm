cca97abfe2f78b42cf2cf1763f0359c913b4a4ab
Yanhong Li <90665285+yanhong-lbh@users.noreply.github.com>
2026-03-05 11:51:06 -0800

    [Model] Add support for OLMo Hybrid (#32550)
    Signed-off-by: wendyliu235 <wenjun.liu@intel.com>

--- vllm/transformers_utils/config.py
+++ vllm/transformers_utils/config.py
@@ -97,6 +97,7 @@ _CONFIG_REGISTRY: dict[str, type[PretrainedConfig]] = LazyConfigDict(
     speculators="SpeculatorsConfig",
     nemotron="NemotronConfig",
     olmo3="Olmo3Config",
+    olmo_hybrid="OlmoHybridConfig",
     ovis="OvisConfig",
     ultravox="UltravoxConfig",
     step3_vl="Step3VLConfig",
