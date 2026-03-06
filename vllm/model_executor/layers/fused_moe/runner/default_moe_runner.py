98f7db09edf0a296f1ef4e067f95472b0a316596
Icey <1790571317@qq.com>
2026-03-06 03:08:20 +0000

    fix typos
    Signed-off-by: Icey <1790571317@qq.com>

--- vllm/model_executor/layers/fused_moe/runner/default_moe_runner.py
+++ vllm/model_executor/layers/fused_moe/runner/default_moe_runner.py
@@ -156,6 +156,7 @@ class DefaultMoERunner(MoERunner):
     Eventually, this class will be split up and specialized for different
     configurations, e.g. the presence or absence of shared experts, a gate, etc.
     """
+
     # --8<-- [end:default_moe_runner]
 
     def __init__(
--- vllm/model_executor/layers/fused_moe/runner/moe_runner.py
+++ vllm/model_executor/layers/fused_moe/runner/moe_runner.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
-from abc import ABC, abstractmethod, ABCMeta
+from abc import ABC, ABCMeta, abstractmethod
 
 import torch
 
