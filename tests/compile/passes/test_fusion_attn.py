a1340c3e21a8f1153f651ebd81bd284069d320f9
Carl You <4531192+carlyou@users.noreply.github.com>
2026-03-06 10:23:46 -0800

    chore: change per comment
    Signed-off-by: Carl You <4531192+carlyou@users.noreply.github.com>

--- tests/compile/passes/test_fusion_attn.py
+++ tests/compile/passes/test_fusion_attn.py
@@ -11,9 +11,9 @@ from tests.v1.attention.utils import BatchSpec, create_common_attn_metadata
 from vllm._custom_ops import cutlass_scaled_fp4_mm, scaled_fp4_quant
 from vllm.compilation.passes.fusion.attn_quant_fusion import (
     ATTN_OP,
-    MLA_ATTN_OP,
     AttnFusionPass,
 )
+from vllm.compilation.passes.fusion.mla_attn_quant_fusion import MLA_ATTN_OP
 from vllm.compilation.passes.fusion.matcher_utils import QUANT_OPS
 from vllm.compilation.passes.fx_utils import find_op_nodes
 from vllm.compilation.passes.utility.noop_elimination import NoOpEliminationPass
