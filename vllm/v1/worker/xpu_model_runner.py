f2e70447ba3bd6f5ebdc6d0b32780f3f16302ec1
Xinyu Chen <xinyu1.chen@intel.com>
2026-03-07 06:40:41 +0000

    Revert "remove torch.cuda.stream wrapper in xpu runner"
    This reverts commit b84022b908c305237cd937f6359a02e68a3d16f5.
    
    Signed-off-by: Xinyu Chen <xinyu1.chen@intel.com>

--- vllm/v1/worker/xpu_model_runner.py
+++ vllm/v1/worker/xpu_model_runner.py
@@ -52,6 +52,7 @@ def _torch_cuda_wrapper():
         torch.cuda.Stream = torch.xpu.Stream
         torch.cuda.default_stream = torch.xpu.current_stream
         torch.cuda.current_stream = torch.xpu.current_stream
+        torch.cuda.stream = torch.xpu.stream
         torch.cuda.mem_get_info = torch.xpu.mem_get_info
         torch.cuda.Event = torch.Event
         torch.cuda.set_stream = torch.xpu.set_stream
