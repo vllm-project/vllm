eac04b8571826f312f296bc8ed647b99f5ebb045
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2026-03-10 16:18:28 +0000

    [CI] Bump `mypy` version to 1.19.1 (#36104)
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- vllm/sampling_params.py
+++ vllm/sampling_params.py
@@ -533,6 +533,7 @@ class SamplingParams(
             if eos_ids:
                 self._all_stop_token_ids.update(eos_ids)
                 if not self.ignore_eos:
+                    assert self.stop_token_ids is not None
                     eos_ids.update(self.stop_token_ids)
                     self.stop_token_ids = list(eos_ids)
 
