4e00f19cd73fd12c75189e5ba52ed20d44ac12a4
wineandchord <guoqizhou19@gmail.com>
2026-03-11 11:54:38 +0800

    [Bugfix] Respect hf-config-path during GGUF speculator detection
    Signed-off-by: wineandchord <guoqizhou19@gmail.com>

--- vllm/engine/arg_utils.py
+++ vllm/engine/arg_utils.py
@@ -1479,6 +1479,7 @@ class EngineArgs:
                     revision=self.revision,
                     trust_remote_code=self.trust_remote_code,
                     vllm_speculative_config=self.speculative_config,
+                    hf_config_path=self.hf_config_path,
                 )
             )
 
