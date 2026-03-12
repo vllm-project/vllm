3c316b5676dc191c700a0473c855668a8c1bdd99
Benjamin Chislett <bchislett@nvidia.com>
2026-03-12 15:59:58 +0000

    update RMSNorm benchmark script
    Signed-off-by: Benjamin Chislett <bchislett@nvidia.com>

--- benchmarks/kernels/benchmark_rmsnorm.py
+++ benchmarks/kernels/benchmark_rmsnorm.py
@@ -1,7 +1,36 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+"""
+Utility script to benchmark vLLM's RMSNorm implementation against FlashInfer's 
+and HuggingFace's. Can be used to generate performance data for a range of batch sizes
+and hidden sizes, with or without residual connection.
+
+Optionally, users can enable CUDA graphs and multiple calls
+per benchmark run to help stabilize measurements.
+This is particularly important for avoiding the launch overhead
+which dominates the latency for small batch sizes.
+
+Note that batch_size does not account for num_heads,
+so in practice the effective batch size for the kernel may be num_requests * num_heads.
+Users can set the batch_size argument accordingly, to account for those cases.
+
+Example usage:
+
+# Measure GPT-OSS-shape RMSNorm without residuals, including the launch overhead
+python3 benchmarks/kernels/benchmark_rmsnorm.py --hidden-sizes 2880 --use-residual False
+
+# Measure+Profile DeepSeek-shape RMSNorm (with residuals, on by default) for low-latency
+
+nsys profile --cuda-graph-trace=node \
+    python3 benchmarks/kernels/benchmark_rmsnorm.py \
+    --hidden-sizes 7168 \
+    --calls-per-run 20 \
+    --use-cuda-graph
+
+"""
 
 import itertools
+import os
 
 import torch
 from flashinfer.norm import fused_add_rmsnorm, rmsnorm
@@ -111,9 +140,9 @@ def rmsnorm_vllm(
     return output
 
 
-def calculate_diff(batch_size, seq_len, hidden_size, use_residual=True):
+def calculate_diff(batch_size, hidden_size, use_residual):
     dtype = torch.bfloat16
-    x = torch.randn(batch_size, seq_len, hidden_size, dtype=dtype, device="cuda")
+    x = torch.randn(batch_size, hidden_size, dtype=dtype, device="cuda")
     weight = torch.ones(hidden_size, dtype=dtype, device="cuda")
     residual = torch.randn_like(x) if use_residual else None
 
@@ -144,65 +173,92 @@ def calculate_diff(batch_size, seq_len, hidden_size, use_residual=True):
         print("❌ Implementations differ")
 
 
-batch_size_range = [2**i for i in range(0, 7, 2)]
-seq_length_range = [2**i for i in range(6, 11, 1)]
-head_num_range = [32, 48]
-configs = list(itertools.product(head_num_range, batch_size_range, seq_length_range))
+def get_benchmark(args):
+    provider_titles = (
+        ["HuggingFace", "FlashInfer", "vLLM"]
+        if not args.omit_huggingface
+        else ["FlashInfer", "vLLM"]
+    )
+    providers = [x.lower() for x in provider_titles]
+    use_residual = args.use_residual
 
-
-def get_benchmark(use_residual):
     @triton.testing.perf_report(
         triton.testing.Benchmark(
-            x_names=["head_num", "batch_size", "seq_len"],
-            x_vals=[list(_) for _ in configs],
+            x_names=["batch_size", "hidden_size"],
+            x_vals=[
+                list(_) for _ in itertools.product(args.batch_sizes, args.hidden_sizes)
+            ],
             line_arg="provider",
-            line_vals=["huggingface", "flashinfer", "vllm"],
-            line_names=["HuggingFace", "FlashInfer", "vLLM"],
-            styles=[("blue", "-"), ("green", "-"), ("red", "-")],
+            line_vals=providers,
+            line_names=provider_titles,
+            styles=[("blue", "-"), ("green", "-"), ("red", "-")][: len(providers)],
             ylabel="us",
             plot_name=f"rmsnorm-perf-{'with' if use_residual else 'without'}-residual",
             args={},
         )
     )
-    def benchmark(head_num, batch_size, seq_len, provider):
+    def benchmark(batch_size, hidden_size, provider):
         dtype = torch.bfloat16
-        hidden_size = head_num * 128  # assuming head_dim = 128
 
-        x = torch.randn(batch_size, seq_len, hidden_size, dtype=dtype, device="cuda")
-        weight = torch.ones(hidden_size, dtype=dtype, device="cuda")
-        residual = torch.randn_like(x) if use_residual else None
+        input_buffers = [
+            dict(
+                x=torch.randn(batch_size, hidden_size, dtype=dtype, device="cuda"),
+                weight=torch.ones(hidden_size, dtype=dtype, device="cuda"),
+                residual=torch.randn(
+                    batch_size, hidden_size, dtype=dtype, device="cuda"
+                )
+                if use_residual
+                else None,
+            )
+            for _ in range(args.calls_per_run)
+        ]
 
         quantiles = [0.5, 0.2, 0.8]
 
-        if provider == "huggingface":
-            ms, min_ms, max_ms = triton.testing.do_bench(
-                lambda: rmsnorm_naive(
-                    x.clone(),
-                    weight,
-                    residual.clone() if residual is not None else None,
-                ),
-                quantiles=quantiles,
-            )
-        elif provider == "flashinfer":
-            ms, min_ms, max_ms = triton.testing.do_bench(
-                lambda: rmsnorm_flashinfer(
-                    x.clone(),
-                    weight,
-                    residual.clone() if residual is not None else None,
-                ),
-                quantiles=quantiles,
-            )
-        else:
-            ms, min_ms, max_ms = triton.testing.do_bench(
-                lambda: rmsnorm_vllm(
-                    x.clone(),
-                    weight,
-                    residual.clone() if residual is not None else None,
-                ),
-                quantiles=quantiles,
-            )
+        provider_fn = {
+            "huggingface": rmsnorm_naive,
+            "flashinfer": rmsnorm_flashinfer,
+            "vllm": rmsnorm_vllm,
+        }[provider]
+        run_fn = lambda: [provider_fn(**inputs) for inputs in input_buffers]
 
-        return 1000 * ms, 1000 * max_ms, 1000 * min_ms
+        if args.use_cuda_graph:
+            if args.calls_per_run <= 1:
+                print(
+                    "Warning: Using CUDA graph with calls_per_run <= 1"
+                    " may not provide meaningful results."
+                )
+
+            # Warmup for graph capture
+            torch.cuda.synchronize()
+            s = torch.cuda.Stream()
+            s.wait_stream(torch.cuda.current_stream())
+            with torch.cuda.stream(s):
+                for _ in range(3):
+                    run_fn()
+            torch.cuda.current_stream().wait_stream(s)
+
+            # Capture the graph
+            g = torch.cuda.CUDAGraph()
+            with torch.cuda.graph(g):
+                run_fn()
+
+            # Redefine run_fn to replay the graph
+            run_fn = lambda: g.replay()
+
+        torch.cuda.nvtx.range_push(
+            f"RMSNorm benchmark: {provider=}, {hidden_size=},"
+            f" {batch_size=} {use_residual=} {dtype=}"
+        )
+
+        ms, min_ms, max_ms = triton.testing.do_bench(run_fn, quantiles=quantiles)
+
+        torch.cuda.nvtx.range_pop()
+
+        scale = (
+            1000 / args.calls_per_run
+        )  # Convert to microseconds and account for calls per run
+        return scale * ms, scale * max_ms, scale * min_ms
 
     return benchmark
 
@@ -212,25 +268,19 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--batch-size",
+        "--batch-sizes", nargs="+", type=int, default=[2**i for i in range(0, 17, 1)]
+    )  # 1 to 65536
+    parser.add_argument(
+        "--hidden-sizes",
+        nargs="+",
         type=int,
-        default=4,
-        help="Batch size",
+        default=[2880, 4096, 7168],  # GPT-OSS, Qwen3.5, DeepSeek V3
     )
     parser.add_argument(
-        "--seq-len",
-        type=int,
-        default=128,
-        help="Sequence length",
-    )
-    parser.add_argument(
-        "--hidden-size",
-        type=int,
-        default=4096,
-        help="Hidden size (2nd dimension) of the sequence",
-    )
-    parser.add_argument(
-        "--use-residual", action="store_true", help="Whether to use residual connection"
+        "--use-residual",
+        type=bool,
+        default=True,
+        help="Whether to use residual connection. Defaults to True",
     )
     parser.add_argument(
         "--save-path",
@@ -238,18 +288,41 @@ if __name__ == "__main__":
         default="./configs/rmsnorm/",
         help="Path to save rmsnorm benchmark results",
     )
+    parser.add_argument(
+        "--omit-huggingface",
+        action="store_true",
+        help="Whether to omit HuggingFace implementation in the benchmark.",
+    )
+    parser.add_argument(
+        "--calls-per-run",
+        type=int,
+        default=1,
+        help="""Number of calls to the RMSNorm function per benchmark run.
+        Each call will use its own input buffers.
+        Used to help stabilize low-latency kernel measurements and account for PDL.
+        Can be used in combination with use-cuda-graph.
+        """,
+    )
+    parser.add_argument(
+        "--use-cuda-graph",
+        action="store_true",
+        help="""Use a cuda graph for each kernel invocation. Should be used alongside
+        calls-per-run, as it will otherwise not have anything to record in each graph.
+        """,
+    )
 
     args = parser.parse_args()
 
+    os.makedirs(args.save_path, exist_ok=True)
+
     # Run correctness test
     calculate_diff(
-        batch_size=args.batch_size,
-        seq_len=args.seq_len,
-        hidden_size=args.hidden_size,
+        batch_size=args.batch_sizes[-1],
+        hidden_size=args.hidden_sizes[-1],
         use_residual=args.use_residual,
     )
 
     # Get the benchmark function with proper use_residual setting
-    benchmark = get_benchmark(args.use_residual)
+    benchmark = get_benchmark(args)
     # Run performance benchmark
     benchmark.run(print_data=True, save_path=args.save_path)
