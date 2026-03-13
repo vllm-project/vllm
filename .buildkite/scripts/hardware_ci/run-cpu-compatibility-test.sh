5b0e4ba0632205b4d54a7ad0f3557d5f6dc158bd
jiang1.li <jiang1.li@intel.com>
2026-03-13 08:43:16 +0000

    add test
    Signed-off-by: jiang1.li <jiang1.li@intel.com>

--- .buildkite/scripts/hardware_ci/run-cpu-compatibility-test.sh
+++ .buildkite/scripts/hardware_ci/run-cpu-compatibility-test.sh
@@ -9,6 +9,13 @@ export VLLM_ENABLE_V1_MULTIPROCESSING=0
 
 wget https://downloadmirror.intel.com/913594/sde-external-10.7.0-2026-02-18-lin.tar.xz
 mkdir -p sde
-tar -xvf sde-external-10.7.0-2026-02-18-lin.tar.xz --strip-components=1  -C ./sde
+tar -xvf ./sde-external-10.7.0-2026-02-18-lin.tar.xz --strip-components=1 -C ./sde/
 
-./sde/sde64 -clx  -- python3 examples/basic/offline_inference/basic.py 
+# Test Sky Lake (AVX512F)
+./sde/sde64 -skl -- python3 examples/basic/offline_inference/generate.py --model facebook/opt-125m --dtype bfloat16
+
+# Test Cascade Lake (AVX512F + VNNI)
+./sde/sde64 -clx -- python3 examples/basic/offline_inference/generate.py --model facebook/opt-125m --dtype bfloat16
+
+# Test Cooper Lake (AVX512F + VNNI + BF16)
+./sde/sde64 -clx -- python3 examples/basic/offline_inference/generate.py --model facebook/opt-125m --dtype bfloat16
