cf54348d2ab4f0815f3f05d056fb3ff78ce5256c
whycoming <120623296@qq.com>
2026-03-13 23:57:49 +0800

    $style: apply ruff-format to SRTF preemption key expression\n\nSigned-off-by: whycoming <120623296@qq.com>
    Signed-off-by: whycoming <120623296@qq.com>

--- vllm/v1/core/sched/scheduler.py
+++ vllm/v1/core/sched/scheduler.py
@@ -470,7 +470,9 @@ class Scheduler(SchedulerInterface):
                             self.running,
                             key=lambda r: (
                                 r.priority,
-                                r.num_prompt_tokens + r.max_tokens - r.num_computed_tokens,
+                                r.num_prompt_tokens
+                                + r.max_tokens
+                                - r.num_computed_tokens,
                                 r.arrival_time,
                             ),
                         )
@@ -480,7 +482,9 @@ class Scheduler(SchedulerInterface):
                         preempted_req = max(
                             self.running,
                             key=lambda r: (
-                                r.num_prompt_tokens + r.max_tokens - r.num_computed_tokens,
+                                r.num_prompt_tokens
+                                + r.max_tokens
+                                - r.num_computed_tokens,
                                 r.arrival_time,
                             ),
                         )
