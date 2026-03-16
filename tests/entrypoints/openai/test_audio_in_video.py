0c4f66fd1c61f70d55fc8c52e7f8e67488a8c67e
Isotr0py <mozf@mail2.sysu.edu.cn>
2026-03-16 14:55:01 +0800

    add test
    Signed-off-by: Isotr0py <mozf@mail2.sysu.edu.cn>

--- tests/entrypoints/openai/test_audio_in_video.py
+++ tests/entrypoints/openai/test_audio_in_video.py
@@ -21,7 +21,7 @@ def server():
         "18432",
         "--enforce-eager",
         "--limit-mm-per-prompt",
-        json.dumps({"audio": 2, "video": 2}),
+        json.dumps({"audio": 3, "video": 3}),
     ]
 
     with RemoteOpenAIServer(
@@ -85,7 +85,7 @@ async def test_online_audio_in_video(
 async def test_online_audio_in_video_multi_videos(
     client: openai.AsyncOpenAI, video_assets: VideoTestAssets
 ):
-    """Test video input with `audio_in_video=True`"""
+    """Test multi-video input with `audio_in_video=True`"""
 
     # we don't use video_urls above because they missed audio stream.
     video_path = video_assets[0].video_path
@@ -125,3 +125,51 @@ async def test_online_audio_in_video_multi_videos(
         assert len(chat_completion.choices) == 1
         choice = chat_completion.choices[0]
         assert choice.finish_reason == "length"
+
+
+@pytest.mark.core_model
+@pytest.mark.asyncio
+async def test_online_audio_in_video_interleaved(
+    client: openai.AsyncOpenAI, video_assets: VideoTestAssets
+):
+    """Test interleaved video/audio input with `audio_in_video=True`"""
+
+    # we don't use video_urls above because they missed audio stream.
+    video_path = video_assets[0].video_path
+    with open(video_path, "rb") as f:
+        video_base64 = base64.b64encode(f.read()).decode("utf-8")
+
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": "What's in these two videos?"},
+                {
+                    "type": "video_url",
+                    "video_url": {"url": f"data:video/mp4;base64,{video_base64}"},
+                },
+                {
+                    "type": "audio_url",
+                    "audio_url": {"url": f"data:audio/mp4;base64,{video_base64}"},
+                },
+                {
+                    "type": "video_url",
+                    "video_url": {"url": f"data:video/mp4;base64,{video_base64}"},
+                },
+            ],
+        }
+    ]
+    with pytest.raises(
+        openai.BadRequestError,
+        match="use_audio_in_video requires equal number of audio and video items",
+    ):
+        await client.chat.completions.create(
+            model=MODEL_NAME,
+            messages=messages,
+            max_tokens=16,
+            extra_body={
+                "mm_processor_kwargs": {
+                    "use_audio_in_video": True,
+                }
+            },
+        )
