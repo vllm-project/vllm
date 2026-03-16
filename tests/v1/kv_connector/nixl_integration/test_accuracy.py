c5d69db6cee20facd274732802d87d1db10cd465
Nicolò Lucchesi <nlucches@redhat.com>
2026-03-16 19:58:06 +0100

    [PD][Nixl] Add support for hybrid SSM-FA models (#36687)

--- tests/v1/kv_connector/nixl_integration/test_accuracy.py
+++ tests/v1/kv_connector/nixl_integration/test_accuracy.py
@@ -18,6 +18,7 @@ EXPECTED_VALUES = {
     "deepseek-ai/deepseek-vl2-tiny": 0.19,
     "deepseek-ai/DeepSeek-V2-Lite-Chat": 0.65,
     "google/gemma-3-4b-it": 0.74,
+    "nvidia/NVIDIA-Nemotron-3-Nano-30B-A3B-FP8": 0.84,
 }
 
 SIMPLE_PROMPT = (
