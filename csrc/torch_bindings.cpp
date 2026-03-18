761e767d33c3a29fbc934b52b87412a317deb1f2
Kyle Sayers <kylesayrs@gmail.com>
2026-03-18 17:25:49 -0400

    add back accidentally deleted def
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- csrc/torch_bindings.cpp
+++ csrc/torch_bindings.cpp
@@ -526,6 +526,15 @@ TORCH_LIBRARY_EXPAND(TORCH_EXTENSION_NAME, ops) {
   ops.impl("cutlass_scaled_mm_supports_block_fp8",
            &cutlass_scaled_mm_supports_block_fp8);
 
+  // SM100 CUTLASS MLA decode
+  ops.def(
+      "sm100_cutlass_mla_decode(Tensor! out, Tensor! lse, Tensor q_nope,"
+      "                         Tensor q_pe, Tensor kv_c_and_k_pe_cache,"
+      "                         Tensor seq_lens, Tensor page_table,"
+      "                         Tensor workspace, float scale,"
+      "                         int num_kv_splits) -> ()");
+  // conditionally compiled so impl in source file
+
   // SM100 CUTLASS MLA workspace
   ops.def(
       "sm100_cutlass_mla_get_workspace_size(int max_seq_len, int num_batches,"
