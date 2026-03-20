13d19601f5449c215709207b311592255ce6561d
Guyue Huang <guyueh@oci-hsg-cs-001-vscode-02.cm.cluster>
2026-03-20 16:51:34 -0700

    fix log
    Signed-off-by: Guyue Huang <guyueh@oci-hsg-cs-001-vscode-02.cm.cluster>

--- vllm/model_executor/layers/quantization/base_config.py
+++ vllm/model_executor/layers/quantization/base_config.py
@@ -3,12 +3,12 @@
 
 import functools
 import inspect
-import logging
 from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING, Any
 
 import torch
 from torch import nn
+from vllm.logger import init_logger
 
 if TYPE_CHECKING:
     from vllm.model_executor.layers.quantization import QuantizationMethods
@@ -16,7 +16,7 @@ if TYPE_CHECKING:
 else:
     QuantizationMethods = str
 
-logger = logging.getLogger(__name__)
+logger = init_logger(__name__)
 
 
 class QuantizeMethodBase(ABC):
