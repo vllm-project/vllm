f0c3d36a75e3a55dd5fb5b67158c535c6a8b2ae9
sfeng33 <4florafeng@gmail.com>
2026-03-24 20:10:44 +0000

    Fix mypy
    Signed-off-by: sfeng33 <4florafeng@gmail.com>

--- vllm/entrypoints/serve/render/serving.py
+++ vllm/entrypoints/serve/render/serving.py
@@ -545,7 +545,7 @@ class OpenAIServingRender:
                     raise NotImplementedError(msg)
                 tokenizer = renderer.get_tokenizer()
                 request = tool_parser(tokenizer, request.tools).adjust_request(
-                    request=request
-                )  # type: ignore[arg-type]
+                    request=request  # type: ignore[arg-type]
+                )
 
         return conversation, [engine_prompt]
