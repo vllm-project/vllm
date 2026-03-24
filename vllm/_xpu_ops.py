7aa30174994eecc4e5f9d6d348420fba9310caf8
Wu, Xiaochang <xiaochang.wu@intel.com>
2026-03-24 10:24:00 +0800

    fix
    Signed-off-by: Wu, Xiaochang <xiaochang.wu@intel.com>

--- vllm/_xpu_ops.py
+++ vllm/_xpu_ops.py
@@ -339,7 +339,7 @@ class xpu_ops:
         raw_topk_indices: torch.Tensor,
         num_rows: int,
         stride0: int,
-        strdide1: int,
+        stride1: int,
         topk_tokens: int,
     ) -> None:
         torch.ops._C.top_k_per_row_prefill(
@@ -349,7 +349,7 @@ class xpu_ops:
             raw_topk_indices,
             num_rows,
             stride0,
-            strdide1,
+            stride1,
             topk_tokens,
         )
 
