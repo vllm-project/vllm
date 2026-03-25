702ec728e162c4add68383067ac45451a899c7e7
FrankMinions <frank747300711@126.com>
2026-03-25 23:11:10 +0800

    fix build bug
    Signed-off-by: FrankMinions <frank747300711@126.com>

--- vllm/model_executor/models/interfaces_base.py
+++ vllm/model_executor/models/interfaces_base.py
@@ -168,7 +168,7 @@ class VllmModelForPooling(VllmModel[T_co], Protocol[T_co]):
     default_pooling_type: ClassVar[str] = "LAST"
     """
     Indicates the
-    [vllm.model_executor.layers.pooler.PoolerConfig.pooling_type][]
+    [vllm.config.pooler.PoolerConfig.pooling_type][]
     to use by default.
 
     You can use the
