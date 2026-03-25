991996d716f25c006b25aea9af483138994608bf
Ekagra Ranjan <3116519+ekagra-ranjan@users.noreply.github.com>
2026-03-25 19:13:51 -0400

    [Cohere] Enable Cohere-Transcribe (#38120)
    Signed-off-by: Ekagra Ranjan <3116519+ekagra-ranjan@users.noreply.github.com>
    Signed-off-by: Rishi Puri <riship@nvidia.com>

--- examples/offline_inference/audio_language.py
+++ examples/offline_inference/audio_language.py
@@ -72,8 +72,7 @@ def run_audioflamingo3(question: str, audio_count: int) -> ModelRequestData:
 # CohereASR
 def run_cohere_asr(question: str, audio_count: int) -> ModelRequestData:
     assert audio_count == 1, "CohereASR only support single audio input per prompt"
-    # TODO (ekagra): add HF ckpt after asr release
-    model_name = "/host/engines/vllm/audio/2b-release"
+    model_name = "CohereLabs/cohere-transcribe-03-2026"
 
     prompt = (
         "<|startofcontext|><|startoftranscript|>"
