894c2e59d1e171fc9c319196cda352b9f49edcb5
Yifan <yzong@redhat.com>
2026-03-26 19:52:55 +0000

    Add streaming tests
    Signed-off-by: Yifan <yzong@redhat.com>

--- tests/tool_parsers/test_deepseekv31_tool_parser.py
+++ tests/tool_parsers/test_deepseekv31_tool_parser.py
@@ -3,7 +3,11 @@
 
 import pytest
 
-from vllm.tokenizers import get_tokenizer
+from tests.tool_parsers.common_tests import (
+    ToolParserTestConfig,
+    ToolParserTests,
+)
+from vllm.tokenizers import TokenizerLike, get_tokenizer
 from vllm.tool_parsers.deepseekv31_tool_parser import (
     DeepSeekV31ToolParser,
 )
@@ -59,3 +63,81 @@ def test_extract_tool_calls_with_multiple_tools(parser):
 
     # prefix is content
     assert result.content == "some prefix text"
+
+
+class TestDeepSeekV31ToolParserStreaming(ToolParserTests):
+    @pytest.fixture(scope="class")
+    def tokenizer(self) -> TokenizerLike:
+        return get_tokenizer(MODEL)
+
+    @pytest.fixture
+    def streaming(self) -> bool:
+        """Only test streaming mode."""
+        return True
+
+    @pytest.fixture
+    def test_config(self) -> ToolParserTestConfig:
+        return ToolParserTestConfig(
+            parser_name="deepseek_v31",
+            no_tool_calls_output="normal text",
+            single_tool_call_output=(
+                "normal text"
+                "<｜tool▁calls▁begin｜>"
+                '<｜tool▁call▁begin｜>foo<｜tool▁sep｜>{"x":1}<｜tool▁call▁end｜>'
+                "<｜tool▁calls▁end｜>"
+            ),
+            parallel_tool_calls_output=(
+                "some prefix text"
+                "<｜tool▁calls▁begin｜>"
+                '<｜tool▁call▁begin｜>foo<｜tool▁sep｜>{"x":1}<｜tool▁call▁end｜>'
+                '<｜tool▁call▁begin｜>bar<｜tool▁sep｜>{"y":2}<｜tool▁call▁end｜>'
+                "<｜tool▁calls▁end｜>"
+                " some suffix text"
+            ),
+            various_data_types_output=(
+                "<｜tool▁calls▁begin｜>"
+                "<｜tool▁call▁begin｜>foo<｜tool▁sep｜>"
+                '{"string_field": "hello", "int_field": 42, '
+                '"float_field": 3.14, "bool_field": true, '
+                '"null_field": null, '
+                '"array_field": [1, 2], '
+                '"object_field": {"k": "v"}}'
+                "<｜tool▁call▁end｜>"
+                "<｜tool▁calls▁end｜>"
+            ),
+            empty_arguments_output=(
+                "<｜tool▁calls▁begin｜>"
+                "<｜tool▁call▁begin｜>foo<｜tool▁sep｜>{}"
+                "<｜tool▁call▁end｜>"
+                "<｜tool▁calls▁end｜>"
+            ),
+            surrounding_text_output=(
+                "some prefix text"
+                "<｜tool▁calls▁begin｜>"
+                '<｜tool▁call▁begin｜>bar<｜tool▁sep｜>{"y":2}'
+                "<｜tool▁call▁end｜>"
+                "<｜tool▁calls▁end｜>"
+                " some suffix text"
+            ),
+            escaped_strings_output=(
+                "<｜tool▁calls▁begin｜>"
+                '<｜tool▁call▁begin｜>foo<｜tool▁sep｜>{"text": "a\\"b"}'
+                "<｜tool▁call▁end｜>"
+                "<｜tool▁calls▁end｜>"
+            ),
+            malformed_input_outputs=[
+                (
+                    "<｜tool▁calls▁begin｜>"
+                    '<｜tool▁call▁begin｜>foo<｜tool▁sep｜>{"x":1'
+                    "<｜tool▁call▁end｜>"
+                    "<｜tool▁calls▁end｜>"
+                ),
+            ],
+            single_tool_call_expected_name="foo",
+            single_tool_call_expected_args={"x": 1},
+            single_tool_call_expected_content="normal text",
+            parallel_tool_calls_count=2,
+            parallel_tool_calls_names=["foo", "bar"],
+            xfail_streaming={},
+            xfail_nonstreaming={},
+        )
