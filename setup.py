42ff218be931bbe30d7d7835e7495b3c0c614a6f
Chinmay Kulkarni <Chinmay.Kulkarni@amd.com>
2026-03-26 06:18:05 -0600

    [ZenCPU] AMD Zen CPU Backend with supported dtypes via zentorch weekly
    Signed-off-by: Chinmay Kulkarni <Chinmay.Kulkarni@amd.com>

--- setup.py
+++ setup.py
@@ -1045,7 +1045,9 @@ setup(
     install_requires=get_requirements(),
     extras_require={
         # AMD Zen CPU optimizations via zentorch
-        "zen": ["zentorch"],
+        "zen": [
+            "zentorch-weekly"
+        ],  # Zentorch has weekly releases. This pulls the latest one
         "bench": ["pandas", "matplotlib", "seaborn", "datasets", "scipy", "plotly"],
         "tensorizer": ["tensorizer==2.10.1"],
         "fastsafetensors": ["fastsafetensors >= 0.2.2"],
