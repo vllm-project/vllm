ffe9be748067da68d8e72b23eb773febdf3315db
Yifan <yzong@redhat.com>
2026-03-26 17:08:37 +0000

    Rename HYBRID_ATTN to SW_ATTN
    Signed-off-by: Yifan <yzong@redhat.com>

--- tests/v1/kv_connector/nixl_integration/config_sweep_accuracy_test.sh
+++ tests/v1/kv_connector/nixl_integration/config_sweep_accuracy_test.sh
@@ -23,7 +23,7 @@ hybrid_ssm_configs=(
   # TODO: (NickLucche) Address async scheduling issue with TP>1 separately as this may impact other models.
   "ENABLE_HMA_FLAG=1 PREFILLER_TP_SIZE=2 DECODER_TP_SIZE=2 GPU_MEMORY_UTILIZATION=0.8 MODEL_NAMES=ibm-granite/granite-4.0-h-tiny VLLM_SERVE_EXTRA_ARGS=--max-model-len,8192,--trust-remote-code,--no-async-scheduling"
 )
-hybrid_attn_configs=(
+sw_attn_configs=(
   "ENABLE_HMA_FLAG=1 GPU_MEMORY_UTILIZATION=0.8 MODEL_NAMES=google/gemma-3-4b-it PREFILLER_TP_SIZE=1 DECODER_TP_SIZE=2 VLLM_SERVE_EXTRA_ARGS=--max-model-len,8192"
   "ENABLE_HMA_FLAG=1 GPU_MEMORY_UTILIZATION=0.8 MODEL_NAMES=google/gemma-3-4b-it PREFILLER_TP_SIZE=2 DECODER_TP_SIZE=1 VLLM_SERVE_EXTRA_ARGS=--max-model-len,8192"
 )
@@ -35,9 +35,9 @@ if [[ -n "${DP_EP:-}" ]]; then
 elif [[ -n "${HYBRID_SSM:-}" ]]; then
   configs=("${hybrid_ssm_configs[@]}")
   echo "HYBRID_SSM is set, using hybrid_ssm_configs."
-elif [[ -n "${HYBRID_ATTN:-}" ]]; then
-  configs=("${hybrid_attn_configs[@]}")
-  echo "HYBRID_ATTN is set, using hybrid_attn_configs."
+elif [[ -n "${SW_ATTN:-}" ]]; then
+  configs=("${sw_attn_configs[@]}")
+  echo "SW_ATTN is set, using sw_attn_configs."
 else
   configs=("${tp_configs[@]}")
 fi
