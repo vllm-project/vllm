067d73ca1c16aee58765b191ff384fbefa038d99
WangErXiao <863579016@qq.com>
2026-03-27 22:12:12 +0800

    [Bugfix]Fix incorrect KV cache log output
    Signed-off-by: WangErXiao <863579016@qq.com>

--- vllm/v1/core/kv_cache_utils.py
+++ vllm/v1/core/kv_cache_utils.py
@@ -1295,12 +1295,23 @@ def _report_kv_cache_config(
         kv_cache_config: The resolved KV cache configuration
     """
     # Log the KV cache size and maximum concurrency.
-    max_model_len_str = f"{vllm_config.model_config.max_model_len:,}"
     max_concurrency = get_max_concurrency_for_kv_cache_config(
         vllm_config, kv_cache_config
     )
     num_tokens = int(vllm_config.model_config.max_model_len * max_concurrency)
+    dcp_size = vllm_config.parallel_config.decode_context_parallel_size
+    pcp_size = vllm_config.parallel_config.prefill_context_parallel_size
+    if pcp_size * dcp_size > 1:
+        num_tokens *= pcp_size * dcp_size
+        logger.info(
+            "Multiplying the GPU KV cache size by the cp_world_size %d "
+            "(pcp_world_size %d * dcp_world_size %d).",
+            pcp_size * dcp_size,
+            pcp_size,
+            dcp_size,
+        )
     num_tokens_str = f"{num_tokens:,}"
+    max_model_len_str = f"{vllm_config.model_config.max_model_len:,}"
     logger.info_once("GPU KV cache size: %s tokens", num_tokens_str, scope="local")
     logger.info_once(
         "Maximum concurrency for %s tokens per request: %.2fx",
