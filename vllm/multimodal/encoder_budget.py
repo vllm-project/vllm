d08b826737220117ac643c74b444f319f1b91b8d
NJX-njx <3771829673@qq.com>
2026-03-29 19:49:30 +0800

    style: fix ruff format
    Signed-off-by: NJX-njx <njx2413@gmail.com>
    Signed-off-by: NJX-njx <3771829673@qq.com>

--- vllm/multimodal/encoder_budget.py
+++ vllm/multimodal/encoder_budget.py
@@ -88,8 +88,7 @@ class MultiModalBudget:
                 model_config,
                 mm_registry,
                 processor,
-                mm_counts={m: mm_limits.get(m, 1)
-                           for m in active_modalities},
+                mm_counts={m: mm_limits.get(m, 1) for m in active_modalities},
             )
 
         if embed_only_modalities:
