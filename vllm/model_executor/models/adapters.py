7a39103521f6e9ffa2a6e302b62cb424d4a9d4a6
nikin <jingkuja@163.com>
2026-03-30 17:10:37 +0800

    fix/TokenizerRepeatedInitialization

--- vllm/model_executor/models/adapters.py
+++ vllm/model_executor/models/adapters.py
@@ -513,9 +513,9 @@ def load_weights_using_from_2_way_softmax(
         )
         loaded_weights = pooling_model_cls.load_weights(model, weights)
 
-    from vllm.tokenizers import get_tokenizer
+    from vllm.tokenizers import cached_get_tokenizer
 
-    tokenizer = get_tokenizer(
+    tokenizer = cached_get_tokenizer(
         model_config.tokenizer,
         revision=model_config.tokenizer_revision,
         tokenizer_mode=model_config.tokenizer_mode,
@@ -586,9 +586,9 @@ def load_weights_no_post_processing(model, weights: Iterable[tuple[str, torch.Te
         # Skip ModelForSequenceClassification in MRO to avoid infinite recursion
         loaded_weights = pooling_model_cls.load_weights(model, weights)
 
-    from vllm.tokenizers import get_tokenizer
+    from vllm.tokenizers import cached_get_tokenizer
 
-    tokenizer = get_tokenizer(
+    tokenizer = cached_get_tokenizer(
         model_config.tokenizer,
         revision=model_config.tokenizer_revision,
         tokenizer_mode=model_config.tokenizer_mode,
