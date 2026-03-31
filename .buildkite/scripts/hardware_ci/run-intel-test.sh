4bccad635bf4ee542b4654b832b1d58a40f36428
wendyliu235 <wenjun.liu@intel.com>
2026-03-31 06:25:53 +0000

    login
    Signed-off-by: wendyliu235 <wenjun.liu@intel.com>

--- .buildkite/scripts/hardware_ci/run-intel-test.sh
+++ .buildkite/scripts/hardware_ci/run-intel-test.sh
@@ -239,8 +239,8 @@ fi
 # --- Docker housekeeping ---
 cleanup_docker
 
-aws ecr-public get-login-password \
-  --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/q9t5s3a7
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin "$REGISTRY"
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 936637512419.dkr.ecr.us-east-1.amazonaws.com
 
 # --- Build or pull test image ---
 IMAGE="${IMAGE_TAG_XPU:-${image_name}}"
