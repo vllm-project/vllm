845cfc624d7ccab9d622b5ba9a8a191831796624
tjtanaa <tunjian.tan@embeddedllm.com>
2026-03-31 10:24:51 +0000

    update the mha backend selection logic
    Signed-off-by: tjtanaa <tunjian.tan@embeddedllm.com>

--- vllm/platforms/rocm.py
+++ vllm/platforms/rocm.py
@@ -326,13 +326,12 @@ def _get_backend_priorities(
                 AttentionBackendEnum.TRITON_MLA,
             ]
 
-    backends = [
-        AttentionBackendEnum.ROCM_ATTN,
-    ]
+    backends = []
     if rocm_aiter_ops.is_mha_enabled():
         backends.append(AttentionBackendEnum.ROCM_AITER_FA)
     if is_aiter_found_and_supported():
         backends.append(AttentionBackendEnum.ROCM_AITER_UNIFIED_ATTN)
+    backends.append(AttentionBackendEnum.ROCM_ATTN)
     backends.append(AttentionBackendEnum.TRITON_ATTN)
 
     return backends
