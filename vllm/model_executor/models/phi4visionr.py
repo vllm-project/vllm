0f8a87ad097ff12929b820f7956bc9ac82db83e2
Varun Sundar Rabindranath <vsundarr@redhat.com>
2026-03-31 22:15:23 +0000

    fix import error
    Signed-off-by: Varun Sundar Rabindranath <vsundarr@redhat.com>

--- vllm/model_executor/models/phi4visionr.py
+++ vllm/model_executor/models/phi4visionr.py
@@ -15,10 +15,10 @@ from transformers import BatchFeature, PretrainedConfig, Siglip2VisionConfig
 
 from vllm.config import VllmConfig
 from vllm.config.multimodal import BaseDummyOptions
+from vllm.inputs import MultiModalDataDict
 from vllm.logger import init_logger
 from vllm.multimodal import MULTIMODAL_REGISTRY
 from vllm.multimodal.inputs import (
-    MultiModalDataDict,
     MultiModalFieldConfig,
     MultiModalKwargsItems,
 )
