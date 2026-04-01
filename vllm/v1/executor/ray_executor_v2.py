6241521dd2751b036d43d4bfc965f68319c32068
Nick Hill <nickhill123@gmail.com>
2026-04-01 15:35:06 -0700

    [BugFix] Fix precommit breakage due to conflicting in-flight merges (#38759)
    Signed-off-by: Nick Hill <nickhill123@gmail.com>

--- vllm/v1/executor/ray_executor_v2.py
+++ vllm/v1/executor/ray_executor_v2.py
@@ -420,7 +420,7 @@ class RayExecutorV2(MultiprocExecutor):
         for response_mq in self.response_mqs:
             response_mq.wait_until_ready()
 
-        self.futures_queue = deque[tuple[FutureWrapper, Any]]()
+        self.futures_queue = deque[FutureWrapper]()
         self._post_init_executor()
 
         self.start_worker_monitor()
