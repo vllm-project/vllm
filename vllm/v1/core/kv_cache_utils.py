8c26b9446486180490a262746ef7037e468a374c
JartX <sagformas@epdcenter.es>
2026-04-01 15:46:59 +0200

    add assert
    Signed-off-by: JartX <sagformas@epdcenter.es>

--- vllm/v1/core/kv_cache_utils.py
+++ vllm/v1/core/kv_cache_utils.py
@@ -955,7 +955,9 @@ def _unify_page_size_per_token(
         target = max(adjusted_sizes)
         for layer_name, spec in new_kv_cache_spec.items():
             if spec.page_size_bytes < target:
-                new_kv_cache_spec[layer_name] = replace(spec, page_size_padded=target)
+                assert isinstance(spec, AttentionSpec)
+                new_kv_cache_spec[layer_name] = replace(
+                    spec, page_size_padded=target)
     return new_kv_cache_spec
 
 
