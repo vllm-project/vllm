2cf9a3f0f1c1f5c62f22fbda4ad607ccac7b1a2a
Martin Hickey <martin.hickey@ie.ibm.com>
2026-04-01 14:07:49 +0100

    Update vllm/distributed/kv_events.py
    Co-authored-by: Or Ozeri <or@ozery.com>
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>

--- vllm/distributed/kv_events.py
+++ vllm/distributed/kv_events.py
@@ -79,7 +79,7 @@ class BlockStored(KVCacheEvent):
                 self.lora_id,
                 self.medium,
                 tuple(self.extra_keys) if self.extra_keys else None,
-                self.group_idx if self.group_idx is not None else None,
+                self.group_idx,
             )
         )
 
