9dd60da8716844806ef0cc7b10a6b40d22b65eac
zhutaoyu <zhutaoyu97@gmail.com>
2026-04-01 00:35:47 +0000

    moe opt
    Signed-off-by: zhutaoyu <zhutaoyu97@gmail.com>

--- vllm/model_executor/layers/fused_moe/modular_kernel.py
+++ vllm/model_executor/layers/fused_moe/modular_kernel.py
@@ -1393,8 +1393,11 @@ class FusedMoEKernelModularImpl:
             apply_router_weight_on_input=apply_router_weight_on_input,
             expert_tokens_meta=expert_tokens_meta,
         )
+
+        # Aiter don't use temp buffer, so can skip copy if output is contiguous
         if (
-            not self.inplace
+            current_platform.is_rocm()
+            and not self.inplace
             and fused_out.shape == output.shape
             and fused_out.is_contiguous()
         ):
