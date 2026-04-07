48f6a7105532dd3036b85fe380ddd062de6cfda3
tjtanaa <tunjian.tan@embeddedllm.com>
2026-04-07 22:01:47 +0800

    enable triton kernel on xpu
    Signed-off-by: tjtanaa <tunjian.tan@embeddedllm.com>

--- vllm/model_executor/models/gemma4.py
+++ vllm/model_executor/models/gemma4.py
@@ -325,7 +325,7 @@ class Gemma4MoE(nn.Module):
             topk: int,
             renormalize: bool,
         ) -> tuple[torch.Tensor, torch.Tensor]:
-            if current_platform.is_cuda_alike():
+            if current_platform.is_cuda_alike() or current_platform.is_xpu():
                 return gemma4_fused_routing_kernel_triton(
                     gating_output, topk, per_expert_scale
                 )
