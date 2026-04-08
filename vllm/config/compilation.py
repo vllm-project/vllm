1eaca8a0d1679e0f5e315f5a306f275a4554e263
Jee Jee Li <pandaleefree@gmail.com>
2026-04-08 05:07:44 +0000

    Fix
    Signed-off-by: Jee Jee Li <pandaleefree@gmail.com>

--- vllm/config/compilation.py
+++ vllm/config/compilation.py
@@ -132,7 +132,7 @@ class PassConfig:
     """Enable async TP."""
     fuse_allreduce_rms: bool = None  # type: ignore[assignment]
     """Enable flashinfer allreduce fusion."""
-    fuse_minimax_qk_norm: bool = False
+    fuse_minimax_qk_norm: bool = None
     """Enable fused allreduce+RMSNorm for MiniMax QK norm."""
     enable_qk_norm_rope_fusion: bool = False
     """Enable fused Q/K RMSNorm + RoPE pass."""
