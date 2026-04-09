db37c0a68cd4fabd9bbc6739cfab8d0877c75d6e
Andy Lo <andy@mistral.ai>
2026-04-09 10:09:03 +0000

    Fix Dense + multinode + DP > 1
    Signed-off-by: Andy Lo <andy@mistral.ai>

--- vllm/v1/engine/core.py
+++ vllm/v1/engine/core.py
@@ -1203,9 +1203,13 @@ class EngineCoreProc(EngineCore):
                 # Non-MoE DP ranks are completely independent, so treat like DP=1.
                 # Note that parallel_config.data_parallel_index will still reflect
                 # the original DP rank.
+                nnodes = parallel_config.nnodes_within_dp
+                node_rank = parallel_config.node_rank_within_dp
                 parallel_config.data_parallel_size = 1
                 parallel_config.data_parallel_size_local = 1
                 parallel_config.data_parallel_rank = 0
+                parallel_config.nnodes = nnodes
+                parallel_config.node_rank = node_rank
                 engine_core = EngineCoreProc(*args, engine_index=dp_rank, **kwargs)
 
             assert engine_core is not None
@@ -2294,9 +2298,13 @@ class EngineCoreActor(EngineCoreActorMixin, EngineCoreProc):
         dp_rank: int = 0,
         local_dp_rank: int = 0,
     ):
+        nnodes = vllm_config.parallel_config.nnodes_within_dp
+        node_rank = vllm_config.parallel_config.node_rank_within_dp
         vllm_config.parallel_config.data_parallel_size = 1
         vllm_config.parallel_config.data_parallel_size_local = 1
         vllm_config.parallel_config.data_parallel_rank = 0
+        vllm_config.parallel_config.nnodes = nnodes
+        vllm_config.parallel_config.node_rank = node_rank
 
         EngineCoreActorMixin.__init__(
             self, vllm_config, addresses, dp_rank, local_dp_rank
