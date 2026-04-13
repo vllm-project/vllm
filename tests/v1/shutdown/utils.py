2393c1ed1f36ab080ab43ff2658b7dd5b572f6da
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2026-04-13 14:34:44 +0000

    revert timeout change as it didn't fix the issue
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- tests/v1/shutdown/utils.py
+++ tests/v1/shutdown/utils.py
@@ -2,5 +2,5 @@
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 """Shutdown test utils"""
 
-SHUTDOWN_TEST_TIMEOUT_SEC = 150
+SHUTDOWN_TEST_TIMEOUT_SEC = 120
 SHUTDOWN_TEST_THRESHOLD_BYTES = 2 * 2**30
