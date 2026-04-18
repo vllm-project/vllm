a6b8bcb9ca169035fcdad8125a04090ab2520548
weiguihua2 <weiguihua2@huawei.com>
2026-04-18 14:47:18 +0800

    fix(memory): fix memory compute
    Signed-off-by: chenxiao <Jaychou1620@Gmail.com>

--- vllm/v1/kv_cache_interface.py
+++ vllm/v1/kv_cache_interface.py
@@ -114,10 +114,11 @@ class FullAttentionSpec(AttentionSpec):
         max_model_len = vllm_config.model_config.max_model_len
         dcp_world_size = vllm_config.parallel_config.decode_context_parallel_size
         pcp_world_size = vllm_config.parallel_config.prefill_context_parallel_size
+        dycp_word_size = vllm_config.parallel_config.dp_per_domain
         # Note(hc): each dcp rank only need save
         # (max_model_len//dcp_world_size) tokens locally.
-        if dcp_world_size * pcp_world_size > 1:
-            max_model_len = cdiv(max_model_len, dcp_world_size * pcp_world_size)
+        if dcp_world_size * pcp_world_size * dycp_word_size > 1:
+            max_model_len = cdiv(max_model_len, dcp_world_size * pcp_world_size * dycp_word_size)
         return cdiv(max_model_len, self.block_size) * self.page_size_bytes
 
     @classmethod
