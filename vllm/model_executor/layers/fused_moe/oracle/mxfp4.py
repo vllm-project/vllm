2f0e20a1bc672320cf019dfa2057883ef1b44ce0
Itay Alroy <ialroy@nvidia.com>
2026-04-21 02:45:40 +0300

    Use use_batched_activation_format
    Signed-off-by: Itay Alroy <ialroy@nvidia.com>

--- vllm/model_executor/layers/fused_moe/oracle/mxfp4.py
+++ vllm/model_executor/layers/fused_moe/oracle/mxfp4.py
@@ -884,8 +884,7 @@ def make_mxfp4_moe_kernel(
         experts,
         shared_experts=(
             shared_experts
-            if moe_config.moe_parallel_config.use_deepep_ll_kernels
-            or moe_config.moe_parallel_config.use_nixl_ep_kernels
+            if moe_config.moe_parallel_config.use_batched_activation_format
             else None
         ),
         inplace=(
