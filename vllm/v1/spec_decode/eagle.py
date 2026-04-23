e4b5e978cc293ff9be622085ed8e440c3db12191
Matthew Bonanni <mbonanni@redhat.com>
2026-04-23 16:10:40 -0400

    Fix imports
    Signed-off-by: Matthew Bonanni <mbonanni@redhat.com>

--- vllm/v1/spec_decode/eagle.py
+++ vllm/v1/spec_decode/eagle.py
@@ -1,6 +1,12 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
+import torch
+
+from vllm.config import VllmConfig
+from vllm.v1.spec_decode.llm_base_proposer import SpecDecodeBaseProposer
+
+
 class EagleProposer(SpecDecodeBaseProposer):
     def __init__(
         self,
