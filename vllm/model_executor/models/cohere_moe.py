fa765cf96dce703c248d4b370ed0d82568dabb13
Terrencezzj <terrence@cohere.ai>
2026-04-24 15:44:42 +0000

    typo
    Signed-off-by: Terrencezzj <terrence@cohere.ai>

--- vllm/model_executor/models/cohere_moe.py
+++ vllm/model_executor/models/cohere_moe.py
@@ -1,25 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
-# Copyright 2024 Cohere and the HuggingFace Inc. team. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""PyTorch CohereMoe model.
-
-Self-contained implementation for the Cohere2 MoE architecture. Does not
-modify ``commandr.py``.
-"""
-
 from collections.abc import Iterable
 from itertools import islice
 
@@ -124,7 +105,7 @@ class CohereMoeMLP(nn.Module):
 
 
 class CohereMoeAttention(nn.Module):
-    """Cohere2 MoE attention with sliding-window interleave."""
+    """Cohere MoE attention with sliding-window interleave."""
 
     def __init__(
         self,
