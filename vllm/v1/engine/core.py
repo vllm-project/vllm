8a52f569497ccc4687f5dc01b9cb1057745730d2
arlenxu <arlenxu@tencent.com>
2026-04-25 22:07:12 +0800

    update
    Signed-off-by: arlenxu <arlenxu@tencent.com>
    Signed-off-by: xhx1022 <1737006628@qq.com>

--- vllm/v1/engine/core.py
+++ vllm/v1/engine/core.py
@@ -1612,17 +1612,10 @@ class EngineCoreProc(EngineCore):
         return not self.has_work()
 
     def _send_finish_outputs_to_client(
-        self,
-        req_ids: list[str],
-        client_index: int,
-        finish_reason: FinishReason,
+        self, req_ids: list[str], client_index: int, finish_reason: FinishReason
     ) -> None:
         outputs = [
-            EngineCoreOutput(
-                req_id,
-                [],
-                finish_reason=finish_reason,
-            )
+            EngineCoreOutput(req_id, [], finish_reason=finish_reason)
             for req_id in req_ids
         ]
         eco = EngineCoreOutputs(finished_requests=req_ids, outputs=outputs)
@@ -1631,11 +1624,7 @@ class EngineCoreProc(EngineCore):
     def _send_abort_outputs_to_client(
         self, req_ids: list[str], client_index: int
     ) -> None:
-        self._send_finish_outputs_to_client(
-            req_ids,
-            client_index,
-            FinishReason.ABORT,
-        )
+        self._send_finish_outputs_to_client(req_ids, client_index, FinishReason.ABORT)
 
     def _send_error_outputs_to_client(
         self, req_ids: list[str], client_index: int
