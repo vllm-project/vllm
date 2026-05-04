d258f25f359311faef16eed1fce27b3c4f2323d8
Linzhang Li <Blemiade_Qinchuan@sjtu.edu.cn>
2026-05-05 03:45:24 +0800

    feat: update xgrammar==0.2.0 to use structural tags for strict tool calling + reasoning for more models (#40894)
    Signed-off-by: Yuchuan <yuchuan.7streams@gmail.com>
    Signed-off-by: Michael Goin <mgoin64@gmail.com>
    Signed-off-by: mgoin <mgoin64@gmail.com>
    Signed-off-by: Ubospica <ubospica@gmail.com>
    Signed-off-by: sfeng33 <4florafeng@gmail.com>
    Co-authored-by: Michael Goin <mgoin64@gmail.com>
    Co-authored-by: Ubospica <ubospica@gmail.com>
    Co-authored-by: sfeng33 <4florafeng@gmail.com>

--- vllm/envs.py
+++ vllm/envs.py
@@ -226,6 +226,7 @@ if TYPE_CHECKING:
     VLLM_GPT_OSS_HARMONY_SYSTEM_INSTRUCTIONS: bool = False
     VLLM_SYSTEM_START_DATE: str | None = None
     VLLM_TOOL_JSON_ERROR_AUTOMATIC_RETRY: bool = False
+    VLLM_ENFORCE_STRICT_TOOL_CALLING: bool = False
     VLLM_CUSTOM_SCOPES_FOR_PROFILING: bool = False
     VLLM_NVTX_SCOPES_FOR_PROFILING: bool = False
     VLLM_KV_EVENTS_USE_INT_BLOCK_HASHES: bool = True
@@ -1592,6 +1593,12 @@ environment_variables: dict[str, Callable[[], Any]] = {
     # If disabled (default), raises an exception and fails the request
     "VLLM_TOOL_JSON_ERROR_AUTOMATIC_RETRY": lambda: bool(
         int(os.getenv("VLLM_TOOL_JSON_ERROR_AUTOMATIC_RETRY", "0"))
+    ),
+    # When 1,the model structural tags will be used to enforce the model
+    # output conforming to the model's tool-calling format and schema.
+    # Default 0 (off).
+    "VLLM_ENFORCE_STRICT_TOOL_CALLING": lambda: bool(
+        int(os.getenv("VLLM_ENFORCE_STRICT_TOOL_CALLING", "0"))
     ),
     # Add optional custom scopes for profiling, disable to avoid overheads
     "VLLM_CUSTOM_SCOPES_FOR_PROFILING": lambda: bool(
