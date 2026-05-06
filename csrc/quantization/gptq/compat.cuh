f6bcb0b82c960a8a5d4ef2096b83be955a857686
Dan Eble <105948349+eble-amd@users.noreply.github.com>
2026-05-06 02:51:25 -0400

    Revise HIP_VERSION guard on atomicAdd (#921)
    * Build with nightly ROCm libraries
    
    Explicitly upgrade "rocm[devel,libraries]" before building.
    
    Signed-off-by: Dan Eble <Dan.Eble@amd.com>
    
    * Revise HIP_VERSION guard on atomicAdd
    
    Following feedback on commit cd41cac516a6c52078f8a8088fb97557a069786c
    from rocm-systems developers, check for HIP version 7.13 without
    checking a specific patch level.
    
    Signed-off-by: Dan Eble <Dan.Eble@amd.com>
    
    ---------
    
    Signed-off-by: Dan Eble <Dan.Eble@amd.com>

--- csrc/quantization/gptq/compat.cuh
+++ csrc/quantization/gptq/compat.cuh
@@ -43,22 +43,18 @@ __device__ __forceinline__ void atomicAdd_half2(half2* address, half2 val) {
 
 //
 
-#if defined(__CUDA_ARCH__) || defined(USE_ROCM)
-  #if (defined(__CUDA_ARCH__) && __CUDA_ARCH__ < 700) || \
-      (defined(USE_ROCM) && HIP_VERSION < 71326173)
-
+#if (defined(__CUDA_ARCH__) && __CUDA_ARCH__ < 700) || \
+    (defined(USE_ROCM) && HIP_VERSION < 71300000)
 __device__ __forceinline__ void atomicAdd(half* address, half val) {
   atomicAdd_half(address, val);
 }
+#endif
 
-    #if (defined(__CUDA_ARCH__) && __CUDA_ARCH__ < 600) || \
-        (defined(USE_ROCM) && HIP_VERSION < 71326173)
+#if (defined(__CUDA_ARCH__) && __CUDA_ARCH__ < 600) || \
+    (defined(USE_ROCM) && HIP_VERSION < 71300000)
 __device__ __forceinline__ void atomicAdd(half2* address, half2 val) {
   atomicAdd_half2(address, val);
 }
-    #endif
-
-  #endif
 #endif
 
 }  // namespace gptq
