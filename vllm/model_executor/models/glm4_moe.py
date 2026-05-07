36fcfb4c81938a5301ee5d59cf6b8f8fdcde86a4
Stoyan Yanev <stoyan.yanev@cleverpine.com>
2026-05-07 11:37:54 +0300

    Use == instead of is for weight_loader comparison
    Consistent with other model implementations.
    
    Signed-off-by: Stoyan Yanev <stoyan.yanev@cleverpine.com>

--- vllm/model_executor/models/glm4_moe.py
+++ vllm/model_executor/models/glm4_moe.py
@@ -520,7 +520,7 @@ class Glm4MoeModel(nn.Module):
 
                 param = params_dict[name]
                 weight_loader = getattr(param, "weight_loader", default_weight_loader)
-                if weight_loader is default_weight_loader:
+                if weight_loader == default_weight_loader:
                     weight_loader(param, loaded_weight)
                 else:
                     weight_loader(param, loaded_weight, shard_id)
