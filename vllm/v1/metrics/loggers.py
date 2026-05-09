231529ceb54e9371909ce45bb40a252ceb11473a
russellfeng <russellfeng@tencent.com>
2026-05-09 14:02:12 +0800

    add last_engine_iteration_timestamp_seconds in metric.

--- vllm/v1/metrics/loggers.py
+++ vllm/v1/metrics/loggers.py
@@ -435,6 +435,16 @@ class PrometheusStatLogger(AggregateStatLoggerBase):
         }
         per_engine_labelvalues = self.per_engine_labelvalues
 
+        gauge_last_iteration_ts = Gauge(
+            "vllm:last_engine_iteration_timestamp_seconds",
+            "Unix timestamp of the last engine iteration",
+            labelnames=["model_name", "engine"],
+            multiprocess_mode="mostrecent",
+        )
+        self.gauge_last_iteration_ts = create_metric_per_engine(
+            gauge_last_iteration_ts, per_engine_labelvalues
+        )
+
         self.spec_decoding_prom = self._spec_decoding_cls(
             vllm_config.speculative_config, labelnames, per_engine_labelvalues
         )
@@ -1142,6 +1152,10 @@ class PrometheusStatLogger(AggregateStatLoggerBase):
 
         if iteration_stats is None:
             return
+        self.gauge_last_iteration_ts[engine_idx].set(
+            iteration_stats.iteration_timestamp
+        )
+
         if envs.VLLM_COMPUTE_NANS_IN_LOGITS:
             self.counter_corrupted_requests[engine_idx].inc(
                 iteration_stats.num_corrupted_reqs
