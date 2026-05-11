7d9f4f67b135f60313ac3e9615cc305e9734a093
wang.yuqi <yuqi.wang@daocloud.io>
2026-05-11 11:27:12 +0800

    fix
    Signed-off-by: wang.yuqi <yuqi.wang@daocloud.io>

--- vllm/entrypoints/pooling/offline.py
+++ vllm/entrypoints/pooling/offline.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+
 from abc import ABC, abstractmethod
 from collections.abc import Callable, Iterable, Sequence
 from typing import Any
