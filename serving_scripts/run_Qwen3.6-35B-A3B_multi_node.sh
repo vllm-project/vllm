b927ea0b3ba7b15bf90400a864b3210a4d7528f9
Jason Paul Miller <jasonversamiller@gmail.com>
2026-05-11 13:37:59 +0100

    added more time

--- serving_scripts/run_Qwen3.6-35B-A3B_multi_node.sh
+++ serving_scripts/run_Qwen3.6-35B-A3B_multi_node.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=512G
-#SBATCH --time=00:45:00
+#SBATCH --time=01:30:00
 #SBATCH --output=results/%x-%j.out
 #SBATCH --error=results/%x-%j.err
 #SBATCH --mail-user=jason.miller@eng.ox.ac.uk
