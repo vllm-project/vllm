c1d131dcc01d9cf70eff5c8641d854f4e162bccc
weimingc <17592131+meenchen@users.noreply.github.com>
2026-05-12 15:27:50 -0700

    Set params_dtype on vocab parallel embedding
    Signed-off-by: weimingc <17592131+meenchen@users.noreply.github.com>

--- vllm/model_executor/layers/vocab_parallel_embedding.py
+++ vllm/model_executor/layers/vocab_parallel_embedding.py
@@ -290,6 +290,7 @@ class VocabParallelEmbedding(PluggableLayer):
 
         if params_dtype is None:
             params_dtype = torch.get_default_dtype()
+        self.params_dtype = params_dtype
         # Divide the weight matrix along the vocabulary dimension.
         self.num_added_embeddings = self.num_embeddings - self.org_vocab_size
         self.num_embeddings_per_partition = divide(
