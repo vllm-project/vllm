01d7162da88fb7cc637150f4e8a9a9e57442211d
liranschour <liranschour@users.noreply.github.com>
2026-05-14 13:54:33 +0300

    Update vllm/distributed/kv_transfer/kv_connector/v1/offloading/scheduler.py
    Co-authored-by: Or Ozeri <or@ozery.com>
    Signed-off-by: liranschour <liranschour@users.noreply.github.com>

--- vllm/distributed/kv_transfer/kv_connector/v1/offloading/scheduler.py
+++ vllm/distributed/kv_transfer/kv_connector/v1/offloading/scheduler.py
@@ -785,9 +785,7 @@ class OffloadingConnectorScheduler:
         if self._req_status and all(
             rs.req.is_finished() for rs in self._req_status.values()
         ):
-            self._current_batch_jobs_to_flush.update(
-                jid for jid, js in self._jobs.items() if js.is_store
-            )
+            self._current_batch_jobs_to_flush.update(self._jobs.keys())
 
         meta = OffloadingConnectorMetadata(
             load_jobs=self._current_batch_load_jobs,
