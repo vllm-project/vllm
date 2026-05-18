b3f9981ab5f320e6ae107a865ce2da266c9eef20
Jason Paul Miller <jasonversamiller@gmail.com>
2026-05-18 03:20:27 +0100

    testing small

--- serving_scripts/run_Qwen1.5-MoE-A2.7B-Chat_multi_node.sh
+++ serving_scripts/run_Qwen1.5-MoE-A2.7B-Chat_multi_node.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=512G
-#SBATCH --time=00:25:00
+#SBATCH --time=00:40:00
 #SBATCH --output=results/%x-%j.out
 #SBATCH --error=results/%x-%j.err
 #SBATCH --mail-user=jason.miller@eng.ox.ac.uk
