9578d9882718c3c1ea3e7b000ac8af8825c683f2
wang.yuqi <yuqi.wang@daocloud.io>
2026-05-19 10:21:27 +0800

    fix
    Signed-off-by: wang.yuqi <yuqi.wang@daocloud.io>

--- vllm/entrypoints/generate/beam_search/online.py
+++ vllm/entrypoints/generate/beam_search/online.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+
 import asyncio
 from abc import ABC
 from collections.abc import AsyncGenerator, Mapping
