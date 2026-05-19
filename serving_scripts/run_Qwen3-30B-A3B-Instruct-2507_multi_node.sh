3b8bdb03ae093ef81b40da9cc0447e678b267594
Jason Paul Miller <jasonversamiller@gmail.com>
2026-05-19 21:08:31 +0100

    trying lower mem

--- serving_scripts/run_Qwen3-30B-A3B-Instruct-2507_multi_node.sh
+++ serving_scripts/run_Qwen3-30B-A3B-Instruct-2507_multi_node.sh
@@ -6,7 +6,7 @@
 #SBATCH --gres=gpu:h100:1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=512G
+#SBATCH --mem=128G
 #SBATCH --time=01:00:00
 #SBATCH --output=results/%x-%j.out
 #SBATCH --error=results/%x-%j.err
