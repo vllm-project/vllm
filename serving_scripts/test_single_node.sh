6c44121be661c74a1db063e2ef8c03c5cdd160ab
Jason Paul Miller <jasonversamiller@gmail.com>
2026-05-19 21:22:36 +0100

    trying lower mem

--- serving_scripts/test_single_node.sh
+++ serving_scripts/test_single_node.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:h100:2
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=512G
+#SBATCH --mem=128G
 #SBATCH --time=00:45:00
 #SBATCH --output=results/%x-%j.out
 #SBATCH --error=results/%x-%j.err
