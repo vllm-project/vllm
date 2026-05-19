64e79fb091441aea581d42e941788fa5ce48f1a9
Jason Paul Miller <jasonversamiller@gmail.com>
2026-05-19 21:09:59 +0100

    trying lower mem

--- serving_scripts/test_l40s.sh
+++ serving_scripts/test_l40s.sh
@@ -7,7 +7,7 @@
 #SBATCH --gres=gpu:l40s:1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=30G
+#SBATCH --mem=128G
 #SBATCH --time=00:45:00
 #SBATCH --output=results/%x-%j.out
 #SBATCH --error=results/%x-%j.err
