24dd1a576f6783500be52fc51311b705a0eb57c6
Wentao Ye <44945378+yewentao256@users.noreply.github.com>
2026-05-23 19:37:24 -0400

    [Model Runner v2] Force v1 runner for tests (#43233)
    Signed-off-by: yewentao256 <zhyanwentao@126.com>
    
    Signed-off-by: Pharos Peng <q1293132751@163.com>

--- tests/distributed/test_pipeline_parallel.py
+++ tests/distributed/test_pipeline_parallel.py
@@ -348,7 +348,14 @@ def _compare_tp(
         "mp",
     ]
 
-    compare_two_settings(model_id, pp_args, tp_args, pp_env, tp_env, method=method)
+    compare_two_settings(
+        model_id,
+        pp_args,
+        tp_args,
+        pp_env,
+        tp_env,
+        method=method,
+    )
 
 
 @pytest.mark.parametrize(
