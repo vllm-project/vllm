17f779cc3a9e2421ec654bc34eddbcd01290d473
jiang1.li <jiang1.li@intel.com>
2026-05-27 09:55:06 +0000

    remove
    Signed-off-by: jiang1.li <jiang1.li@intel.com>

--- vllm/model_executor/layers/quantization/__init__.py
+++ vllm/model_executor/layers/quantization/__init__.py
@@ -122,7 +122,6 @@ def get_quantization_config(quantization: str) -> type[QuantizationConfig]:
     from .compressed_tensors.compressed_tensors import (
         CompressedTensorsConfig,
     )
-    from .cpu_wna16 import CPUAWQConfig
     from .experts_int8 import ExpertsInt8Config
     from .fbgemm_fp8 import FBGEMMFp8Config
     from .fp8 import Fp8Config
@@ -166,7 +165,6 @@ def get_quantization_config(quantization: str) -> type[QuantizationConfig]:
         "mxfp4": Mxfp4Config,
         "gpt_oss_mxfp4": GptOssMxfp4Config,
         "deepseek_v4_fp8": DeepseekV4FP8Config,
-        "cpu_awq": CPUAWQConfig,
         "humming": HummingConfig,
         "online": OnlineQuantizationConfig,
     }
