20d69d100a967d51788bd852169afb516ba18434
Li, Jiang <jiang1.li@intel.com>
2026-05-28 22:36:31 +0800

    [CPU] Migrate cpu_awq into awq_marlin (#43841)
    Signed-off-by: jiang1.li <jiang1.li@intel.com>

--- vllm/config/model.py
+++ vllm/config/model.py
@@ -1028,7 +1028,6 @@ class ModelConfig:
                 "mxfp4",
                 "gpt_oss_mxfp4",
                 "deepseek_v4_fp8",
-                "cpu_awq",
                 "humming",
                 "gguf",
             ]
