b3fd8e69ffba354690ee9be2d8eb068e1cad6c98
wang.yuqi <yuqi.wang@daocloud.io>
2026-05-29 11:25:51 +0800

    + ms
    Signed-off-by: wang.yuqi <yuqi.wang@daocloud.io>

--- vllm/tracing/utils.py
+++ vllm/tracing/utils.py
@@ -47,9 +47,11 @@ class SpanAttributes:
     # Pooling request attributes
     GEN_AI_POOLING_IO_PROCESSOR = "gen_ai.pooling.io_processor"
     GEN_AI_POOLING_REQUEST_CLASS = "gen_ai.pooling.request.class"
-    GEN_AI_LATENCY_PREPROCESSING = "gen_ai.latency.preprocessing"
-    GEN_AI_LATENCY_ENGINE_CALL = "gen_ai.latency.engine_call"
-    GEN_AI_LATENCY_POSTPROCESSING = "gen_ai.latency.postprocessing"
+
+    GEN_AI_LATENCY_PREPROCESSING = "gen_ai.latency.preprocessing.ms"
+    GEN_AI_LATENCY_ENGINE_CALL = "gen_ai.latency.engine_call.ms"
+    GEN_AI_LATENCY_POSTPROCESSING = "gen_ai.latency.postprocessing.ms"
+
     GEN_AI_POOLING_EMBED_ENCODING_FORMAT = "gen_ai.pooling.embed.encoding.format"
     GEN_AI_POOLING_EMBED_DTYPE = "gen_ai.pooling.embed.dtype"
     GEN_AI_POOLING_EMBED_ENDIANNESS = "gen_ai.pooling.embed.endianness"
