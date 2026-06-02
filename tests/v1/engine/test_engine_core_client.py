a0e5a52243e0002f462640b60090eb2958bd0123
Nick Hill <nickhill123@gmail.com>
2026-06-01 19:12:32 -0700

    [Core] Move max_concurrent_batches to VllmConfig
    Signed-off-by: Nick Hill <nickhill123@gmail.com>

--- tests/v1/engine/test_engine_core_client.py
+++ tests/v1/engine/test_engine_core_client.py
@@ -1187,7 +1187,6 @@ def test_engine_core_proc_instantiation_cuda_empty(monkeypatch: pytest.MonkeyPat
         mock_executor.get_kv_cache_specs.return_value = [{"default": mock_spec}]
         mock_executor.determine_available_memory.return_value = [1024 * 1024 * 1024]
         mock_executor.initialize_from_config.return_value = None
-        mock_executor.max_concurrent_batches = 1
 
         return mock_executor
 
