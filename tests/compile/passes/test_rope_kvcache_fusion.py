50d68752ccc11e4ccda7c3d092e2e5a8156c2355
charlifu <charlifu@amd.com>
2026-06-08 17:34:11 +0000

    remove @
    Signed-off-by: charlifu <charlifu@amd.com>

--- tests/compile/passes/test_rope_kvcache_fusion.py
+++ tests/compile/passes/test_rope_kvcache_fusion.py
@@ -382,7 +382,7 @@ def test_rope_kvcache_fusion(
         torch.testing.assert_close(k_unfused, k_fused, atol=ATOL, rtol=RTOL)
         torch.testing.assert_close(v_unfused, v_fused, atol=ATOL, rtol=RTOL)
         # Cannot compare fp8_* directly here, cast to model dtype instead
-        # @TODO(charlifu): switch back to ATOL, RTOL after aiter fix is merged.
+        # TODO(charlifu): switch back to ATOL, RTOL after aiter fix is merged.
         torch.testing.assert_close(
             kv_cache_unfused.to(dtype),
             kv_cache_fused.to(dtype),
