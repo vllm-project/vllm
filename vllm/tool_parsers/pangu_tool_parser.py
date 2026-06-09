47a2984e1709d311ca4cdfa59f7fb4fba6549c72
ChanghuiYu <yusichaoran@163.com>
2026-06-09 10:41:38 +0800

    fix tool-parser bug
    Signed-off-by: ChanghuiYu <yusichaoran@163.com>

--- vllm/tool_parsers/pangu_tool_parser.py
+++ vllm/tool_parsers/pangu_tool_parser.py
@@ -22,7 +22,7 @@ from vllm.entrypoints.openai.engine.protocol import (
 )
 from vllm.logger import init_logger
 from vllm.tokenizers import TokenizerLike
-from vllm.tool_parsers.abstract_tool_parser import ToolParser
+from vllm.tool_parsers.abstract_tool_parser import Tool, ToolParser
 from vllm.tool_parsers.utils import (
     find_common_prefix,
     is_complete_json,
@@ -33,8 +33,8 @@ logger = init_logger(__name__)
 
 
 class PanguToolParser(ToolParser):
-    def __init__(self, tokenizer: TokenizerLike):
-        super().__init__(tokenizer)
+    def __init__(self, tokenizer: TokenizerLike, tools: list[Tool] | None = None):
+        super().__init__(tokenizer, tools)
 
         # initialize properties used for state when parsing tool calls in
         # streaming mode
