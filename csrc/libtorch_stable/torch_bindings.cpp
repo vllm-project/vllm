ac57f238f8ffdd331c9633461b5a2a06f6f8c2f1
Chris Leonard <chleonar@redhat.com>
2026-06-10 14:37:44 +0000

    migrated marlin.cu to stable abi and the marlin generated kernels
    Signed-off-by: Chris Leonard <chleonar@redhat.com>

--- csrc/libtorch_stable/torch_bindings.cpp
+++ csrc/libtorch_stable/torch_bindings.cpp
@@ -47,6 +47,16 @@ STABLE_TORCH_LIBRARY_FRAGMENT(_C, ops) {
   ops.def(
       "awq_marlin_repack(Tensor b_q_weight, SymInt size_k, "
       "SymInt size_n, int num_bits, bool is_a_8bit) -> Tensor");
+
+  // Marlin GEMM
+  ops.def(
+      "marlin_gemm(Tensor a, Tensor? c_or_none, Tensor b_q_weight, "
+      "Tensor? b_bias_or_none,Tensor b_scales, "
+      "Tensor? a_scales, Tensor? global_scale, Tensor? b_zeros_or_none, "
+      "Tensor? "
+      "g_idx_or_none, Tensor? perm_or_none, Tensor workspace, int b_type_id, "
+      "SymInt size_m, SymInt size_n, SymInt size_k, bool is_k_full, "
+      "bool use_atomic_add, bool use_fp32_reduce, bool is_zp_float) -> Tensor");
 #endif
 
 #ifndef USE_ROCM
