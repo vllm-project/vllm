15011128bceb165d08aa160cf0133cfda085486f
Dao007forever <dao007forever@gmail.com>
2026-06-11 11:20:08 -0700

    Update vllm/distributed/kv_transfer/kv_connector/v1/nixl/worker.py
    Co-authored-by: Nicolò Lucchesi <nicolo.lucchesi@gmail.com>
    Signed-off-by: Dao007forever <dao007forever@gmail.com>

--- vllm/distributed/kv_transfer/kv_connector/v1/nixl/worker.py
+++ vllm/distributed/kv_transfer/kv_connector/v1/nixl/worker.py
@@ -1007,8 +1007,7 @@ class NixlConnectorWorker:
         logger.debug(
             "Different block lengths collected: %s", set(self.block_len_per_layer)
         )
-        assert len(self.block_len_per_layer) == len(seen_base_addresses)
-        assert len(self._region_is_mla) == len(self.block_len_per_layer)
+        assert len(self.block_len_per_layer) == len(seen_base_addresses) == len(self._region_is_mla)
 
         self.kv_caches_base_addr[self.engine_id][self.tp_rank] = seen_base_addresses
         self.num_regions = len(caches_data)
