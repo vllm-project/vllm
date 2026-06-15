b0db9bf81f7f59ff06a5bfeb24ca4f999fef1c1f
wcy <233313160abc@gmail.com>
2026-06-15 20:06:42 +0800

    fix
    Co-authored-by: wcy2003 <233313160abc@gmail.com>
    
    Co-authored-by: lyd1992 <liuyudong@iscas.ac.cn>
    Signed-off-by: wcy <233313160abc@gmail.com>

--- vllm/model_executor/kernels/linear/mixed_precision/cpu.py
+++ vllm/model_executor/kernels/linear/mixed_precision/cpu.py
@@ -9,7 +9,7 @@ from vllm.model_executor.layers.quantization.utils.quant_utils import (
     pack_quantized_values_into_int32,
     unpack_quantized_values_into_int32,
 )
-from vllm.platforms import current_platform
+from vllm.platforms import CpuArchEnum, current_platform
 from vllm.scalar_type import scalar_types
 
 from .MPLinearKernel import MPLinearKernel, MPLinearLayerConfig
@@ -168,11 +168,13 @@ class CPUWNA16LinearKernel(MPLinearKernel):
             if zp.output_dim == 0:
                 zp.data = zp.t().contiguous()
 
+        supports_amx = torch.cpu._is_amx_tile_supported()
+        supports_riscv = current_platform.get_cpu_architecture() == CpuArchEnum.RISCV
         layer.use_w4a8 = (
             envs.VLLM_CPU_INT4_W4A8
             and not self.config.has_g_idx
             and self.config.act_type == torch.bfloat16
-            and torch.cpu._is_amx_tile_supported()
+            and (supports_amx or supports_riscv)
         )
         # layer.use_w4a8 = False
         # AWQ format will be converted to GPTQ format in `AWQMarlinLinearMethod`
