95858e3ab54798272f65019826009ccd962b028c
Thomas Ortner <boh@zurich.ibm.com>
2026-06-15 21:40:41 +0000

    Disable VLLM_USE_BREAKABLE_CUDAGRAPH
    Signed-off-by: Thomas Ortner <boh@zurich.ibm.com>

--- tests/plugins/vllm_dsv4_oot_platform/vllm_dsv4_oot_platform/__init__.py
+++ tests/plugins/vllm_dsv4_oot_platform/vllm_dsv4_oot_platform/__init__.py
@@ -15,4 +15,10 @@ def dsv4_oot_platform_plugin() -> str | None:
     # the environment is the opt-in. ``pip uninstall vllm_dsv4_oot_platform``
     # disables it. Out-of-tree plugins take precedence over built-in
     # platforms (see ``vllm.platforms.resolve_current_platform_cls_qualname``).
+
+    # Breakable is a CUDA-only feature; Force the env var to ``"0"``.
+    import os
+
+    os.environ["VLLM_USE_BREAKABLE_CUDAGRAPH"] = "0"
+
     return "vllm_dsv4_oot_platform.platform.DSv4OOTPlatform"
