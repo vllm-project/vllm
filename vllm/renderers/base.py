d26b92844dd5c28d61440ce0e66161972051b63b
wang.yuqi <yuqi.wang@daocloud.io>
2026-06-16 13:21:40 +0800

    fix
    Signed-off-by: wang.yuqi <yuqi.wang@daocloud.io>

--- vllm/renderers/base.py
+++ vllm/renderers/base.py
@@ -89,14 +89,6 @@ class BaseRenderer(ABC, Generic[_T]):
         # to keep the asyncio event loop responsive under concurrent load.
         self._mm_executor: Executor = self._executor
 
-        # Offloading tokenizer encode & decode to thread pool.
-        self._async_tokenizer_encode = make_async(
-            self.get_tokenizer().encode, executor=self._executor
-        )
-        self._async_tokenizer_decode = make_async(
-            self.get_tokenizer().decode, executor=self._executor
-        )
-
         self.mm_processor: BaseMultiModalProcessor | None = None
         self._readonly_mm_processor: BaseMultiModalProcessor | None = None
         self._mm_cache_stats: MultiModalCacheStats | None = None
@@ -430,7 +422,10 @@ class BaseRenderer(ABC, Generic[_T]):
         prompt: TextPrompt,
         params: TokenizeParams,
     ) -> TokensPrompt:
-        prompt_token_ids = await self._async_tokenizer_encode(
+        async_tokenizer_encode = make_async(
+            self.get_tokenizer().encode, executor=self._executor
+        )
+        prompt_token_ids = await async_tokenizer_encode(
             prompt["prompt"],
             **params.get_encode_kwargs(),
         )
@@ -444,7 +439,10 @@ class BaseRenderer(ABC, Generic[_T]):
         return prompt
 
     async def _detokenize_prompt_async(self, prompt: TokensPrompt) -> TokensPrompt:
-        prompt["prompt"] = await self._async_tokenizer_decode(
+        async_tokenizer_decode = make_async(
+            self.get_tokenizer().decode, executor=self._executor
+        )
+        prompt["prompt"] = await async_tokenizer_decode(
             prompt["prompt_token_ids"]
         )
 
