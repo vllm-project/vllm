f1cf4818521ddfcf67da956fe20de78a649c2c49
Bugen Zhao <i@bugenzhao.com>
2026-06-24 08:34:50 +0000

    migrate gemma4 to unified parser
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- rust/src/parser/src/reasoning/mod.rs
+++ rust/src/parser/src/reasoning/mod.rs
@@ -17,7 +17,6 @@
 mod cohere_cmd;
 mod deepseek_r1;
 mod delimited;
-mod gemma4;
 mod kimi;
 mod minimax_m3;
 mod qwen3;
@@ -29,8 +28,7 @@ use vllm_tokenizer::DynTokenizer;
 
 pub use self::cohere_cmd::CohereCmdReasoningParser;
 pub use self::deepseek_r1::DeepSeekR1ReasoningParser;
-pub(crate) use self::delimited::DelimitedReasoningParser;
-pub use self::gemma4::Gemma4ReasoningParser;
+pub(crate) use self::delimited::{DelimitedReasoningParser, last_reasoning_boundary};
 pub use self::kimi::KimiReasoningParser;
 pub use self::minimax_m3::MiniMaxM3ReasoningParser;
 pub use self::qwen3::Qwen3ReasoningParser;
