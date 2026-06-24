cc12546410c7ab2eb1d5a6b1ea6b5304421e8b6a
fangyuchu <fangyuchu@qq.com>
2026-06-24 18:50:33 +0800

    adapt to nixl-1.13.0 (#238)
    Signed-off-by: fangyuchu <fangyuchu@qq.com>

--- vllm/distributed/device_communicators/all2all.py
+++ vllm/distributed/device_communicators/all2all.py
@@ -563,7 +563,9 @@ class NixlEPAll2AllManager(All2AllManagerBase):
         if NixlEPAll2AllManager._buffer is None:
             return
         state = NixlEPAll2AllManager._buffer
-        state.buffer.get_local_buffer_tensor(dtype=torch.int8).zero_()
+        state.buffer.get_local_buffer_tensor(
+            dtype=torch.int8, use_rdma_buffer=True
+        ).zero_()
         torch.accelerator.synchronize()
         state.buffer.clean_mask_buffer()
         torch.accelerator.synchronize()
