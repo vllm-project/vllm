7db1d7f6d99ef45578b27e20e7b45ffe05196edd
Jane Xu <janeyx@meta.com>
2026-06-24 09:36:05 -0700

    Test FA2 remove dropout PR
    Signed-off-by: Jane Xu <janeyx@meta.com>

--- cmake/external_projects/vllm_flash_attn.cmake
+++ cmake/external_projects/vllm_flash_attn.cmake
@@ -38,8 +38,8 @@ if(VLLM_FLASH_ATTN_SRC_DIR)
 else()
   FetchContent_Declare(
           vllm-flash-attn
-          GIT_REPOSITORY https://github.com/vllm-project/flash-attention.git
-          GIT_TAG 803020a8fa15407871341d41eba4919ade2ee1ee
+          GIT_REPOSITORY https://github.com/janeyx99/flash-attention.git
+          GIT_TAG 1ed425aa312034ec3da2a75a3c779a0b4856f934
           GIT_PROGRESS TRUE
           # Don't share the vllm-flash-attn build between build types
           BINARY_DIR ${CMAKE_BINARY_DIR}/vllm-flash-attn
