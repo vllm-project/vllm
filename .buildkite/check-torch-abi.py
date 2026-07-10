d5e849bc2ecff35146c35cffb7d79a7fea75446e
Chris Leonard <chleonar@redhat.com>
2026-07-10 18:16:38 +0000

    added qutlass to the ALLOWED_UNSTABLE_LIBRARIES list
    Signed-off-by: Chris Leonard <chleonar@redhat.com>

--- .buildkite/check-torch-abi.py
+++ .buildkite/check-torch-abi.py
@@ -16,6 +16,7 @@ ALLOWED_UNSTABLE_LIBRARIES: tuple[str, ...] = (
     "vllm_flash_attn/_vllm_fa2_C.abi3.so",
     "_flashmla_C.abi3.so",
     "_flashmla_extension_C.abi3.so",
+    "_qutlass_C.abi3.so",
     "third_party/deep_gemm/_C*.so",
 )
 
