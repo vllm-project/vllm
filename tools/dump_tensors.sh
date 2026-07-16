bba7904c81a420ae24cfbd632a476e57a17fe000
loopyt <zhsong@baai.ac.cn>
2026-07-16 10:07:16 +0800

    add env

--- tools/dump_tensors.sh
+++ tools/dump_tensors.sh
@@ -1,3 +1,5 @@
+export GEMS_VENDOR=metax   # 指定厂商，FlagGems 加载 MetaX 后端
+export VLLM_PLUGINS=fl     # 加载 vllm-plugin-FL 插件
 export VLLM_ENABLE_V1_MULTIPROCESSING=0
 python dump_layer_outputs.py \
     --model  /models/MiniCPM5-2.6B \
