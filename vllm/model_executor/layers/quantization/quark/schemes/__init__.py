d7e50e04db47be22195e359ade2c709851084e4e
Jimmy Adams <Adam_ji@outlook.com>
2026-07-17 02:48:15 +0000

    Support DeepSeek-V4 Quark mixed quantization
    Signed-off-by: Jimmy Adams <Adam_ji@outlook.com>

--- vllm/model_executor/layers/quantization/quark/schemes/__init__.py
+++ vllm/model_executor/layers/quantization/quark/schemes/__init__.py
@@ -6,11 +6,13 @@ from .quark_ocp_mx import QuarkOCP_MX
 from .quark_scheme import QuarkScheme
 from .quark_w4a8_mxfp4_fp8 import QuarkW4A8_MXFP4_FP8
 from .quark_w8a8_fp8 import QuarkW8A8Fp8
+from .quark_w8a8_fp8_block import QuarkW8A8Fp8Block
 from .quark_w8a8_int8 import QuarkW8A8Int8
 
 __all__ = [
     "QuarkScheme",
     "QuarkW8A8Fp8",
+    "QuarkW8A8Fp8Block",
     "QuarkW8A8Int8",
     "QuarkOCP_MX",
     "QuarkW4A8_MXFP4_FP8",
