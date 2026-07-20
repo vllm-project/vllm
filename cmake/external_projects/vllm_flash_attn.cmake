79ece101a75e245aa629a6bb0f991cee95313bcd
Matthew Bonanni <mbonanni@redhat.com>
2026-07-20 09:52:12 -0400

    Update git tag after landing
    Signed-off-by: Matthew Bonanni <mbonanni@redhat.com>

--- cmake/external_projects/vllm_flash_attn.cmake
+++ cmake/external_projects/vllm_flash_attn.cmake
@@ -38,8 +38,8 @@ if(VLLM_FLASH_ATTN_SRC_DIR)
 else()
   FetchContent_Declare(
           vllm-flash-attn
-          GIT_REPOSITORY https://github.com/MatthewBonanni/flash-attention.git
-          GIT_TAG d94f1999fba20ed7aaabdaa23696eeb03d5b94c2
+          GIT_REPOSITORY https://github.com/vllm-project/flash-attention.git
+          GIT_TAG 168920233059c48de6199e2cda74003b2ce3d199
           GIT_PROGRESS TRUE
           # Don't share the vllm-flash-attn build between build types
           BINARY_DIR ${CMAKE_BINARY_DIR}/vllm-flash-attn
