9ffe12af9c2b0b06c1f09ac43307aabebb025d2b
Runyan <blackbird7@hotmail.com>
2026-07-23 03:47:30 +0800

    improve numerical stability and prevent overflow in p-less kernel
    Signed-off-by: Runyan <blackbird7@hotmail.com>

--- vllm/v1/worker/gpu/sample/p_less.py
+++ vllm/v1/worker/gpu/sample/p_less.py
@@ -20,6 +20,18 @@ def _p_less_kernel(
     if not p_less:
         return
 
+    max_logit = float("-inf")
+    for i in range(0, vocab_size, BLOCK_SIZE):
+        block = i + tl.arange(0, BLOCK_SIZE)
+        mask = block < vocab_size
+        logits = tl.load(
+            logits_ptr + token_idx * logits_stride + block,
+            mask=mask,
+            other=float("-inf"),
+        )
+        max_logit = tl.max(tl.maximum(logits, max_logit))
+    max_logit = max_logit.to(tl.float32)  # type: ignore
+
     sum_exps = 0.0
     sum_squared_exps = 0.0
     for i in range(0, vocab_size, BLOCK_SIZE):
@@ -30,10 +42,10 @@ def _p_less_kernel(
             mask=mask,
             other=float("-inf"),
         )
-        exps = tl.exp(logits)
+        exps = tl.exp(logits - max_logit)
         sum_exps += tl.sum(exps)
         sum_squared_exps += tl.sum(exps**2.0)
-    threshold = tl.log(sum_squared_exps) - tl.log(sum_exps)
+    threshold = tl.log(sum_squared_exps) - tl.log(sum_exps) + max_logit
 
     for i in range(0, vocab_size, BLOCK_SIZE):
         block = i + tl.arange(0, BLOCK_SIZE)
