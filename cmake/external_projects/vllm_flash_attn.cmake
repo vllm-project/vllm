10918728b6a0281181b8bc6fae27dad3df11d481
Chris Leonard <chleonar@redhat.com>
2026-07-23 14:50:25 +0000

    change git_tag for flash-attention to test FA3 migration to stable ABI
    Signed-off-by: Chris Leonard <chleonar@redhat.com>

--- cmake/external_projects/vllm_flash_attn.cmake
+++ cmake/external_projects/vllm_flash_attn.cmake
@@ -38,8 +38,8 @@ if(VLLM_FLASH_ATTN_SRC_DIR)
 else()
   FetchContent_Declare(
           vllm-flash-attn
-          GIT_REPOSITORY https://github.com/vllm-project/flash-attention.git
-          GIT_TAG 28e862d21806bc3580207aa0ad4e2759151e9827
+          GIT_REPOSITORY https://github.com/cleonard530/flash-attention.git
+          GIT_TAG 63fb93c6092e25885422d242c88fc3fef0529dc1
           GIT_PROGRESS TRUE
           # Don't share the vllm-flash-attn build between build types
           BINARY_DIR ${CMAKE_BINARY_DIR}/vllm-flash-attn
