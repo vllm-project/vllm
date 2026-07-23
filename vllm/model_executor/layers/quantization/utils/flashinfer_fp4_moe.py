Mob-max30/VLLM_Test

enable EPLB for multi-modal MoE models (MoE model is nested) and for more MoE backends and quantizations
Signed-off-by: jdebache <jdebache@nvidia.com>

vllm/model_executor/layers/quantization/utils/flashinfer_fp4_moe.py
@@ -14,6 +14,9 @@ from vllm.model_executor.layers.quantization.utils.flashinfer_utils import (
 from vllm.model_executor.layers.quantization.utils.nvfp4_utils import (
     swizzle_blockscale,
 )
+from vllm.model_executor.layers.quantization.utils.quant_utils import (
+    amax_for_moe_activation_quant,
+)
 
 if TYPE_CHECKING:
     from vllm.model_executor.layers.fused_moe import RoutedExperts
@@ -101,16 +104,15 @@ def prepare_nvfp4_moe_layer_for_flashinfer_cutedsl(
     torch.Tensor,
 ]:
     """Prepare weights for the CuteDSL wrapper-based NvFP4 MoE backend.
-
-    Converts weight scale factors to MMA layout expected by CuteDslMoEWrapper,
-    and interleaves w13 gate/linear rows.
+    Interleaves w13 gate/linear rows and swizzles the weight block scales.
     """
-    from flashinfer.cute_dsl.utils import convert_sf_to_mma_layout
-
     # Global scaling factors (same as other FlashInfer backends).
     num_experts = w13.shape[0]
-    a13_scale = a13_scale.max().to(torch.float32).expand(num_experts)
-    a2_scale = a2_scale.max().to(torch.float32).expand(num_experts)
+    enable_eplb = layer.moe_config.moe_parallel_config.enable_eplb
+    a13_scale = amax_for_moe_activation_quant(a13_scale, enable_eplb).expand(
+        num_experts
+    )
+    a2_scale = amax_for_moe_activation_quant(a2_scale, enable_eplb).expand(num_experts)
 
     half = w13.shape[1] // 2
     w13 = torch.cat([w13[:, half:], w13[:, :half]], dim=1)
@@ -120,29 +122,8 @@ def prepare_nvfp4_moe_layer_for_flashinfer_cutedsl(
     w13 = interleave_linear_and_gate(w13, group_size=64, dim=1)
     w13_scale = interleave_linear_and_gate(w13_scale, group_size=64, dim=1)
 
-    # Convert w13 scale factors: linear → swizzled → MMA layout.
     w13_scale = swizzle_blockscale(w13_scale)
-    E, M_padded, K_sf_padded = w13_scale.shape
-    w13_scale_flat = w13_scale.reshape(E * M_padded, K_sf_padded)
-    w13_scale = convert_sf_to_mma_layout(
-        w13_scale_flat,
-        m=M_padded,
-        k=K_sf_padded * 16,
-        num_groups=E,
-        sf_vec_size=16,
-    )
-
-    # Convert w2 scale factors: linear → swizzled → MMA layout.
     w2_scale = swizzle_blockscale(w2_scale)
-    E, M_padded, K_sf_padded = w2_scale.shape
-    w2_scale_flat = w2_scale.reshape(E * M_padded, K_sf_padded)
-    w2_scale = convert_sf_to_mma_layout(
-        w2_scale_flat,
-        m=M_padded,
-        k=K_sf_padded * 16,
-        num_groups=E,
-        sf_vec_size=16,
-    )
 
     return (
         w13,
@@ -154,6 +135,31 @@ def prepare_nvfp4_moe_layer_for_flashinfer_cutedsl(
         w2_scale_2,
         a2_scale,
     )
+
+
+def nvfp4_swizzled_scale_to_cutedsl_mma_view(scale: torch.Tensor) -> torch.Tensor:
+    """View a swizzled (E, M_padded, K_sf_padded) block-scale tensor in the
+    MMA layout expected by the CuteDSL MoE kernel.
+
+    The returned tensor aliases `scale`'s storage, so in-place updates of the
+    registered Parameter (weight reloads, EPLB rearrangement) are visible to
+    the kernel with no extra bookkeeping.
+    """
+    from flashinfer.cute_dsl.utils import convert_sf_to_mma_layout
+
+    num_experts, m_padded, k_sf_padded = scale.shape
+    mma_view = convert_sf_to_mma_layout(
+        scale.reshape(num_experts * m_padded, k_sf_padded),
+        m=m_padded,
+        k=k_sf_padded * 16,
+        num_groups=num_experts,
+        sf_vec_size=16,
+    )
+    assert mma_view.data_ptr() == scale.data_ptr(), (
+        "convert_sf_to_mma_layout no longer returns a view of its input; "
+        "the quant config would go stale after weight updates."
+    )
+    return mma_view
 
 
 def prepare_static_weights_for_trtllm_fp4_moe(
@@ -338,8 +344,13 @@ def prepare_nvfp4_moe_layer_for_fi_or_cutlass(
     # For some FI kernels, the input scales are shared by all experts.
     if is_global_sf_supported_for_nvfp4_backend(backend):
         num_experts = w13.shape[0]
-        a13_scale = a13_scale.max().to(torch.float32).expand(num_experts)
-        a2_scale = a2_scale.max().to(torch.float32).expand(num_experts)
+        enable_eplb = layer.moe_config.moe_parallel_config.enable_eplb
+        a13_scale = amax_for_moe_activation_quant(a13_scale, enable_eplb).expand(
+            num_experts
+        )
+        a2_scale = amax_for_moe_activation_quant(a2_scale, enable_eplb).expand(
+            num_experts
+        )
     else:
         a13_scale = a13_scale.max(dim=1).values.to(torch.float32)
 

vllm-project/vllm

vllm/model_executor/layers/quantization/utils/flashinfer_fp4_moe.py
@@ -14,6 +14,9 @@ from vllm.model_executor.layers.quantization.utils.flashinfer_utils import (
 from vllm.model_executor.layers.quantization.utils.nvfp4_utils import (
     swizzle_blockscale,
 )
+from vllm.model_executor.layers.quantization.utils.quant_utils import (
+    amax_for_moe_activation_quant,
+)
 
 if TYPE_CHECKING:
     from vllm.model_executor.layers.fused_moe import RoutedExperts
@@ -101,16 +104,15 @@ def prepare_nvfp4_moe_layer_for_flashinfer_cutedsl(
     torch.Tensor,
 ]:
     """Prepare weights for the CuteDSL wrapper-based NvFP4 MoE backend.
-
-    Converts weight scale factors to MMA layout expected by CuteDslMoEWrapper,
-    and interleaves w13 gate/linear rows.
+    Interleaves w13 gate/linear rows and swizzles the weight block scales.
     """
-    from flashinfer.cute_dsl.utils import convert_sf_to_mma_layout
-
     # Global scaling factors (same as other FlashInfer backends).
     num_experts = w13.shape[0]
-    a13_scale = a13_scale.max().to(torch.float32).repeat(num_experts)
-    a2_scale = a2_scale.max().to(torch.float32).repeat(num_experts)
+    enable_eplb = layer.moe_config.moe_parallel_config.enable_eplb
+    a13_scale = amax_for_moe_activation_quant(a13_scale, enable_eplb).repeat(
+        num_experts
+    )
+    a2_scale = amax_for_moe_activation_quant(a2_scale, enable_eplb).repeat(num_experts)
 
     half = w13.shape[1] // 2
     w13 = torch.cat([w13[:, half:], w13[:, :half]], dim=1)
@@ -120,29 +122,8 @@ def prepare_nvfp4_moe_layer_for_flashinfer_cutedsl(
     w13 = interleave_linear_and_gate(w13, group_size=64, dim=1)
     w13_scale = interleave_linear_and_gate(w13_scale, group_size=64, dim=1)
 
-    # Convert w13 scale factors: linear → swizzled → MMA layout.
     w13_scale = swizzle_blockscale(w13_scale)
-    E, M_padded, K_sf_padded = w13_scale.shape
-    w13_scale_flat = w13_scale.reshape(E * M_padded, K_sf_padded)
-    w13_scale = convert_sf_to_mma_layout(
-        w13_scale_flat,
-        m=M_padded,
-        k=K_sf_padded * 16,
-        num_groups=E,
-        sf_vec_size=16,
-    )
-
-    # Convert w2 scale factors: linear → swizzled → MMA layout.
     w2_scale = swizzle_blockscale(w2_scale)
-    E, M_padded, K_sf_padded = w2_scale.shape
-    w2_scale_flat = w2_scale.reshape(E * M_padded, K_sf_padded)
-    w2_scale = convert_sf_to_mma_layout(
-        w2_scale_flat,
-        m=M_padded,
-        k=K_sf_padded * 16,
-        num_groups=E,
-        sf_vec_size=16,
-    )
 
     return (
         w13,
@@ -154,6 +135,31 @@ def prepare_nvfp4_moe_layer_for_flashinfer_cutedsl(
         w2_scale_2,
         a2_scale,
     )
+
+
+def nvfp4_swizzled_scale_to_cutedsl_mma_view(scale: torch.Tensor) -> torch.Tensor:
+    """View a swizzled (E, M_padded, K_sf_padded) block-scale tensor in the
+    MMA layout expected by the CuteDSL MoE kernel.
+
+    The returned tensor aliases `scale`'s storage, so in-place updates of the
+    registered Parameter (weight reloads, EPLB rearrangement) are visible to
+    the kernel with no extra bookkeeping.
+    """
+    from flashinfer.cute_dsl.utils import convert_sf_to_mma_layout
+
+    num_experts, m_padded, k_sf_padded = scale.shape
+    mma_view = convert_sf_to_mma_layout(
+        scale.reshape(num_experts * m_padded, k_sf_padded),
+        m=m_padded,
+        k=k_sf_padded * 16,
+        num_groups=num_experts,
+        sf_vec_size=16,
+    )
+    assert mma_view.data_ptr() == scale.data_ptr(), (
+        "convert_sf_to_mma_layout no longer returns a view of its input; "
+        "the quant config would go stale after weight updates."
+    )
+    return mma_view
 
 
 def prepare_static_weights_for_trtllm_fp4_moe(
@@ -338,8 +344,13 @@ def prepare_nvfp4_moe_layer_for_fi_or_cutlass(
     # For some FI kernels, the input scales are shared by all experts.
     if is_global_sf_supported_for_nvfp4_backend(backend):
         num_experts = w13.shape[0]
-        a13_scale = a13_scale.max().to(torch.float32).repeat(num_experts)
-        a2_scale = a2_scale.max().to(torch.float32).repeat(num_experts)
+        enable_eplb = layer.moe_config.moe_parallel_config.enable_eplb
+        a13_scale = amax_for_moe_activation_quant(a13_scale, enable_eplb).repeat(
+            num_experts
+        )
+        a2_scale = amax_for_moe_activation_quant(a2_scale, enable_eplb).repeat(
+            num_experts
+        )
     else:
         a13_scale = a13_scale.max(dim=1).values.to(torch.float32)
 