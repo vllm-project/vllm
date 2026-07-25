9f4ac8ccec684e7c97edc9f4ae7fb92b7f52bfe0
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2026-07-25 11:06:28 +0100

    no need for tmpfile
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- tests/model_executor/model_loader/instanttensor_loader/test_weight_utils.py
+++ tests/model_executor/model_loader/instanttensor_loader/test_weight_utils.py
@@ -2,8 +2,6 @@
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
 import glob
-import shutil
-import tempfile
 
 import pytest
 import torch
@@ -24,27 +22,24 @@ def test_instanttensor_model_loader():
     model_dir = download_weights_from_hf(
         "openai-community/gpt2", cache_dir=None, allow_patterns=["*.safetensors"]
     )
-    with tempfile.TemporaryDirectory() as tmpdir:
-        for cached in glob.glob(f"{model_dir}/**/*.safetensors", recursive=True):
-            shutil.copy(cached, tmpdir)
-        safetensors = glob.glob(f"{tmpdir}/*.safetensors")
-        assert len(safetensors) > 0
+    safetensors = glob.glob(f"{model_dir}/*.safetensors")
+    assert len(safetensors) > 0
 
-        instanttensor_tensors = {}
-        hf_safetensors_tensors = {}
+    instanttensor_tensors = {}
+    hf_safetensors_tensors = {}
 
-        for name, tensor in instanttensor_weights_iterator(safetensors, True):
-            instanttensor_tensors[name] = tensor.to("cpu")
+    for name, tensor in instanttensor_weights_iterator(safetensors, True):
+        instanttensor_tensors[name] = tensor.to("cpu")
 
-        for name, tensor in safetensors_weights_iterator(safetensors, True):
-            hf_safetensors_tensors[name] = tensor
+    for name, tensor in safetensors_weights_iterator(safetensors, True):
+        hf_safetensors_tensors[name] = tensor
 
-        assert len(instanttensor_tensors) == len(hf_safetensors_tensors)
+    assert len(instanttensor_tensors) == len(hf_safetensors_tensors)
 
-        for name, instanttensor_tensor in instanttensor_tensors.items():
-            assert instanttensor_tensor.dtype == hf_safetensors_tensors[name].dtype
-            assert instanttensor_tensor.shape == hf_safetensors_tensors[name].shape
-            assert torch.all(instanttensor_tensor.eq(hf_safetensors_tensors[name]))
+    for name, instanttensor_tensor in instanttensor_tensors.items():
+        assert instanttensor_tensor.dtype == hf_safetensors_tensors[name].dtype
+        assert instanttensor_tensor.shape == hf_safetensors_tensors[name].shape
+        assert torch.all(instanttensor_tensor.eq(hf_safetensors_tensors[name]))
 
 
 if __name__ == "__main__":
