c1dea7e3b056a79d167c5a8a96676173b6d62c66
Zhenzhong1 <zhenzhong.xu@intel.com>
2026-07-28 11:39:35 +0800

    fixed pre-commit issues
    Signed-off-by: Zhenzhong1 <zhenzhong.xu@intel.com>

--- vllm/model_executor/layers/quantization/inc/schemes/__init__.py
+++ vllm/model_executor/layers/quantization/inc/schemes/__init__.py
@@ -2,8 +2,8 @@
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
 from .factory import resolve_scheme
-from .inc_mxfp8_scheme import INCMxfp8Scheme
 from .inc_fp8_scheme import INCFp8Scheme
+from .inc_mxfp8_scheme import INCMxfp8Scheme
 from .inc_scheme import INCLinearScheme, INCScheme
 from .inc_wna16_scheme import INCWna16Scheme
 
