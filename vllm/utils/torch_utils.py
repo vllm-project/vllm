65ab5620f41856fd07a50c3ee57f19159d7f129d
ranger112571 <1814757861@qq.com>
2026-07-29 18:31:46 +0800

    Update torch utils

--- vllm/utils/torch_utils.py
+++ vllm/utils/torch_utils.py
@@ -69,7 +69,7 @@ MODELOPT_TO_VLLM_KV_CACHE_DTYPE_MAP = {
 T = TypeVar("T")
 
 
-PIN_MEMORY = PIN_MEMORY()
+PIN_MEMORY = is_pin_memory_available()
 
 
 def is_quantized_kv_cache(kv_cache_dtype: str) -> bool:
