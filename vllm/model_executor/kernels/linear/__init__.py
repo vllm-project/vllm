6a169a5e7e10cf0d033e59cc3f9bdef7651c86b8
Zhu, Zufang <zufang.zhu@intel.com>
2026-08-03 00:05:49 -0700

    add torch backend for blockwise kernel on xpu
    Signed-off-by: Zhu, Zufang <zufang.zhu@intel.com>

--- vllm/model_executor/kernels/linear/__init__.py
+++ vllm/model_executor/kernels/linear/__init__.py
@@ -394,6 +394,7 @@ _POSSIBLE_FP8_BLOCK_KERNELS: dict[
     PlatformEnum.XPU: [
         XPUFp8BlockScaledMMKernel,
         TritonFp8BlockScaledMMKernel,
+        BlockWiseTorchFP8ScaledMMLinearKernel,
     ],
 }
 
