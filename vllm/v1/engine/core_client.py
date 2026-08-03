f1137f0db11ac12b00e6e177439a863cced69ad9
lqh-hh <liuqihang6@huawei.com>
2026-08-03 14:04:54 +0800

    Fix type checks for external Elastic EP preparation (#254)
    Signed-off-by: lqh-hh <liuqihang6@huawei.com>

--- vllm/v1/engine/core_client.py
+++ vllm/v1/engine/core_client.py
@@ -1904,7 +1904,7 @@ class DPLBAsyncMPClient(DPAsyncMPClient):
             new_engine_identities.discard(identity)
             self._apply_ready_response(payload)
 
-    def _setup_elastic_ep_reconfig_bootstrap(self) -> None:
+    def _setup_elastic_ep_reconfig_bootstrap(self) -> tuple[str, int]:
         from vllm.distributed.utils import create_tcp_store
         from vllm.utils.network_utils import get_open_ports_list
 
@@ -1924,6 +1924,7 @@ class DPLBAsyncMPClient(DPAsyncMPClient):
         )
         parallel_config._coord_store_port = store.port
         self._coord_store = store
+        return ip, store.port
 
     def _make_reconfig_request(
         self,
