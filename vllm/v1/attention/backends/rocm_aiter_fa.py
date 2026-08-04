46ed916b9e81a4030fc54e9143faaa4520299630
Jack Hu <Jack.Hu@amd.com>
2026-08-04 20:28:51 +0000

    Fixed precommit
    Signed-off-by: Jack Hu <Jack.Hu@amd.com>

--- vllm/v1/attention/backends/rocm_aiter_fa.py
+++ vllm/v1/attention/backends/rocm_aiter_fa.py
@@ -2,7 +2,6 @@
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 """Attention layer with AiterFlashAttention."""
 
-import math
 from dataclasses import dataclass
 from typing import ClassVar
 
