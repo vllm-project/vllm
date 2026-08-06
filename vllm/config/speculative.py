1a8f52b4481c2c256661acfa51b5ca786b9f0187
zhangyp26 <zhangyp26@chinatelecom.com.cn>
2026-08-06 14:43:38 +0800

    Add TeleChat4 model support
    Signed-off-by: zhangyp26 <zhangyp26@chinatelecom.com.cn>

--- vllm/config/speculative.py
+++ vllm/config/speculative.py
@@ -340,6 +340,7 @@ class SpeculativeConfig:
             "deepseek_v3",
             "deepseek_v32",
             "glm_moe_dsa",
+            "telechat4",
         ):
             hf_config.model_type = "deepseek_mtp"
         if hf_config.model_type == "deepseek_mtp":
--- vllm/reasoning/__init__.py
+++ vllm/reasoning/__init__.py
@@ -20,6 +20,10 @@ Example:
 
 
 _REASONING_PARSERS_TO_REGISTER = {
+    "telechat": (
+        "telechat_reasoning_parser",
+        "TeleChatReasoningParser",
+    ),
     "deepseek_r1": (  # name
         "deepseek_r1_reasoning_parser",  # filename
         "DeepSeekR1ReasoningParser",  # class_name
