517036687e42b593efe40cb4bce5670baa2d08d7
Robert Shaw <robshaw@redhat.com>
2026-08-10 14:41:05 +0000

    added deepepv2 change
    Signed-off-by: Robert Shaw <robshaw@redhat.com>

--- vllm/config/parallel.py
+++ vllm/config/parallel.py
@@ -677,6 +677,7 @@ class ParallelConfig:
                 "allgather_reducescatter",
                 "deepep_high_throughput",
                 "deepep_low_latency",
+                "deepep_v2"
                 "mori_high_throughput",
                 "mori_low_latency",
                 "nixl_ep",
