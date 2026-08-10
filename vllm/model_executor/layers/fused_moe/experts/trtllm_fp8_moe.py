5e1f2b4b4cd9a45ec5dc468d760507232dc1a542
jdebache <jdebache@nvidia.com>
2026-08-10 18:49:57 +0000

    leverage FP32 unpacked routing weights support for other calls to TRTLLM Fused MoE
    Signed-off-by: jdebache <jdebache@nvidia.com>

--- vllm/model_executor/layers/fused_moe/experts/trtllm_fp8_moe.py
+++ vllm/model_executor/layers/fused_moe/experts/trtllm_fp8_moe.py
@@ -17,7 +17,6 @@ from vllm.model_executor.layers.fused_moe.topk_weight_and_reduce import (
 )
 from vllm.model_executor.layers.fused_moe.utils import (
     fi_moe_largest_bucket,
-    trtllm_moe_pack_topk_ids_weights,
 )
 from vllm.model_executor.layers.quantization.utils.flashinfer_utils import (
     activation_to_flashinfer_int,
@@ -273,9 +272,6 @@ class TrtLlmFp8ExpertsModular(TrtLlmFp8ExpertsBase, mk.FusedMoEExpertsModular):
         import flashinfer
         from flashinfer.fused_moe import Fp8QuantizationType, WeightLayout
 
-        # Pack topk ids and weights into format expected by the kernel.
-        packed_topk_ids = trtllm_moe_pack_topk_ids_weights(topk_ids, topk_weights)
-
         if a1q_scale is None:
             raise RuntimeError(
                 "TRT-LLM FP8 experts require precomputed activation scales"
@@ -295,7 +291,7 @@ class TrtLlmFp8ExpertsModular(TrtLlmFp8ExpertsBase, mk.FusedMoEExpertsModular):
             hidden_states_scale = prepare_deepseek_fp8_x_sf(hidden_states, a1q_scale)
 
         flashinfer.fused_moe.trtllm_fp8_block_scale_routed_moe(
-            topk_ids=packed_topk_ids,
+            topk_ids=(topk_ids, topk_weights),
             routing_bias=None,
             hidden_states=hidden_states,
             hidden_states_scale=hidden_states_scale,
