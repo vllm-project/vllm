92366fe7fdbcbdf0cb5a4f58e3a696d5a76f5c9a
Zhu, Zufang <zufang.zhu@intel.com>
2026-08-12 08:50:35 +0000

    enable torch asn mxfp4 linear backend on xpu
    Signed-off-by: Zhu, Zufang <zufang.zhu@intel.com>

--- vllm/model_executor/kernels/linear/__init__.py
+++ vllm/model_executor/kernels/linear/__init__.py
@@ -89,6 +89,9 @@ from vllm.model_executor.kernels.linear.mxfp4.humming import (
 from vllm.model_executor.kernels.linear.mxfp4.marlin import (
     MarlinMxFp4LinearKernel,
 )
+from vllm.model_executor.kernels.linear.mxfp4.pytorch import (
+    TorchMxFp4LinearKernel,
+)
 from vllm.model_executor.kernels.linear.mxfp4.xpu import (
     XPUMxFp4LinearKernel,
 )
@@ -283,6 +286,7 @@ _LINEAR_BACKEND_KERNEL_MAP: dict[str, set[type]] = {
         ChannelWiseTorchFP8ScaledMMLinearKernel,
         RowWiseTorchFP8ScaledMMLinearKernel,
         BlockWiseTorchFP8ScaledMMLinearKernel,
+        TorchMxFp4LinearKernel,
     },
     "aiter": {
         AiterInt8ScaledMMLinearKernel,
@@ -537,6 +541,7 @@ _POSSIBLE_MXFP4_KERNELS: dict[PlatformEnum, list[type[MxFp4LinearKernel]]] = {
     ],
     PlatformEnum.XPU: [
         XPUMxFp4LinearKernel,
+        TorchMxFp4LinearKernel,
     ],
 }
 
