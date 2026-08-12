95c7f3b6488185dce957da0a30bfb98f59e10012
Lucas Wilkinson <lwilkins@redhat.com>
2026-08-12 16:24:29 +0000

    cleanup
    Signed-off-by: Lucas Wilkinson <lwilkins@redhat.com>

--- vllm/v1/kv_cache_interface.py
+++ vllm/v1/kv_cache_interface.py
@@ -261,7 +261,9 @@ class AttentionSpec(KVCacheSpec):
     @property
     def real_page_size_bytes(self) -> int:
         """Alias of ``unpadded_page_size_bytes``: with packing folded into
-        ``state_content_bytes`` there is no separate data-only page."""
+        ``state_content_bytes`` there is no separate data-only page.
+        TODO(lucas): follow up with TPU backend to see if we can remove this property.
+        """
         return self.unpadded_page_size_bytes
 
     def max_num_blocks_per_req(self, vllm_config: VllmConfig, max_len: int) -> int:
