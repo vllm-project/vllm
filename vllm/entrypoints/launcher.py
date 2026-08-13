edc6079d27eafc109af1a553d23c85eb20e73f6e
wang.yuqi <yuqi.wang@daocloud.io>
2026-08-13 23:24:05 +0800

    pre-commit
    Signed-off-by: wang.yuqi <yuqi.wang@daocloud.io>

--- vllm/entrypoints/launcher.py
+++ vllm/entrypoints/launcher.py
@@ -136,7 +136,8 @@ async def serve_http(
         await loop.run_in_executor(
             # Now engine_client.shutdown will be executed twice;
             # this is the first time.
-            None, partial(engine_client.shutdown, timeout=timeout)
+            None,
+            partial(engine_client.shutdown, timeout=timeout),
         )
         logger.info_once("[shutdown] API server: engine client stopped")
 
