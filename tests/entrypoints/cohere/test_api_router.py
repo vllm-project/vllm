fbfb4f60b07a6fa85a15caf1e2fd8cdca8fdf1ec
wang.yuqi <yuqi.wang@daocloud.io>
2026-08-14 15:34:48 +0800

    pre-commit
    Signed-off-by: wang.yuqi <yuqi.wang@daocloud.io>

--- tests/entrypoints/cohere/test_api_router.py
+++ tests/entrypoints/cohere/test_api_router.py
@@ -30,7 +30,9 @@ from vllm.entrypoints.cohere.protocol import (
     CohereChatV2Response,
 )
 from vllm.entrypoints.openai.engine.protocol import ErrorInfo, ErrorResponse
-from vllm.entrypoints.serve.exception_handling.handlers.http import http_exception_handler
+from vllm.entrypoints.serve.exception_handling.handlers.http import (
+    http_exception_handler
+)
 from vllm.entrypoints.serve.exception_handling.handlers.validation import (
     validation_exception_handler,
 )
