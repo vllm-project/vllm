6826e7e4f289fecdf903751b85872f2927c48c6f
andrewor14 <andrewor14@gmail.com>
2026-08-19 07:48:25 -0700

    [Tests] Delete deprecate torchao tests for v1 configs
    **Summary:** torchao's v1 configs were deprecated as of v0.17.0
    and officially removed in v0.18.0. These tests are testing
    functionality that no longer exist upstream and are causing
    breakages in https://github.com/vllm-project/vllm/pull/52183.
    
    Fixes https://github.com/vllm-project/vllm/issues/52947.
    
    **Tese Plan:**
    ```
    pytest -v -s tests/quantization/test_torchao.py
    ```

--- tests/quantization/test_torchao.py
+++ tests/quantization/test_torchao.py
@@ -44,45 +44,6 @@ def test_pre_quantized_model(vllm_runner):
 
 
 @pytest.mark.skipif(not TORCHAO_AVAILABLE, reason="torchao is not available")
-@pytest.mark.parametrize(
-    "pt_load_map_location",
-    [
-        f"{DEVICE_TYPE}:0",
-        # {"": "cuda"},
-    ],
-)
-def test_opt_125m_int8wo_model_loading_with_params(vllm_runner, pt_load_map_location):
-    torch._dynamo.reset()
-    model_name = "jerryzh168/opt-125m-int8wo-partial-quant"
-    with vllm_runner(
-        model_name=model_name,
-        quantization="torchao",
-        dtype="bfloat16",
-        pt_load_map_location=pt_load_map_location,
-        enforce_eager=True,
-    ) as llm:
-        output = llm.generate_greedy(["The capital of France is"], max_tokens=4)
-
-        assert output
-
-
-@pytest.mark.skipif(not TORCHAO_AVAILABLE, reason="torchao is not available")
-def test_qwenvl_int8wo_model_loading_with_params(vllm_runner):
-    torch._dynamo.reset()
-    model_name = "mobicham/Qwen2.5-VL-3B-Instruct_int8wo_ao"
-    with vllm_runner(
-        model_name=model_name,
-        quantization="torchao",
-        dtype="bfloat16",
-        pt_load_map_location=f"{DEVICE_TYPE}:0",
-        enforce_eager=True,
-    ) as llm:
-        output = llm.generate_greedy(["The capital of France is"], max_tokens=4)
-
-        assert output
-
-
-@pytest.mark.skipif(not TORCHAO_AVAILABLE, reason="torchao is not available")
 @pytest.mark.skip(
     reason="since torchao nightly is only compatible with torch nightly"
     "currently https://github.com/pytorch/ao/issues/2919, we'll have to skip "
