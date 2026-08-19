1d65e2d98987dd450797d6e212b99997cd0ac3b8
Jincheng Miao <jincheng.miao@intel.com>
2026-08-19 09:58:27 +0800

    [xpu] replace getMemoryInfo by torch.xpu.mem_get_info
    Signed-off-by: Jincheng Miao <jincheng.miao@intel.com>
    Signed-off-by: Jin, Youzhi <youzhi.jin@intel.com>

--- vllm/platforms/xpu.py
+++ vllm/platforms/xpu.py
@@ -91,10 +91,7 @@ def get_mem_info_wrapper(
             f"device must be int, str, torch.device, or None, got {type(device)}"
         )
 
-    # Call the underlying C++ implementation
-    free, total = torch.ops._C_cache_ops.getMemoryInfo(device)
-
-    return free, total
+    return torch.xpu.mem_get_info(device)
 
 
 torch.accelerator.get_memory_info = get_mem_info_wrapper
