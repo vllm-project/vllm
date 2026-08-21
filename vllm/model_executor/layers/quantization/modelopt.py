52a7c41622753a896ff987f083d2bf1d617e34ef
Luke Alonso <lalonso@gmail.com>
2026-08-21 08:04:36 -0700

    [Kernel] Add b12x FP4 MoE backend (#52018)
    Signed-off-by: Luke Alonso <lalonso@gmail.com>
    Signed-off-by: mgoin <mgoin64@gmail.com>
    Signed-off-by: Misha Goin <mgoin64@gmail.com>
    Co-authored-by: mgoin <mgoin64@gmail.com>

--- vllm/model_executor/layers/quantization/modelopt.py
+++ vllm/model_executor/layers/quantization/modelopt.py
@@ -1574,6 +1574,7 @@ class ModelOptNvFp4FusedMoE(FusedMoEMethodBase):
             w2_scale_2=layer.w2_weight_scale_2,
             a2_scale=layer.w2_input_scale,
             is_act_and_mul=self.moe.is_act_and_mul,
+            use_a16=self.use_a16,
         )
 
         replace_parameter(layer, "w13_weight", w13)
@@ -1610,6 +1611,7 @@ class ModelOptNvFp4FusedMoE(FusedMoEMethodBase):
             swiglu_alpha=getattr(layer, "swiglu_alpha", None),
             swiglu_beta=getattr(layer, "swiglu_beta", None),
             layer=layer,
+            use_a16=self.use_a16,
         )
 
     @property
