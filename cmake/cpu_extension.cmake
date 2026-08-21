526b0fedd040ded9dea32cc7032834f23194284b
Rehan Khan <Rehan.Khan7@ibm.com>
2026-08-21 18:13:25 +0530

    [CPU][Perf] Add vectorized Sampler Kernel
    Signed-off-by: Rehan Khan <Rehan.Khan7@ibm.com>

--- cmake/cpu_extension.cmake
+++ cmake/cpu_extension.cmake
@@ -489,6 +489,7 @@ set(VLLM_EXT_SRC
     "csrc/cpu/cpu_fused_moe.cpp"
     "csrc/cpu/cpu_attn.cpp"
     "csrc/cpu/cpu_isa.cpp"
+    "csrc/cpu/sampling_kernels.cpp"
     "csrc/cpu/torch_bindings.cpp")
 
 if (CMAKE_SYSTEM_PROCESSOR MATCHES "riscv64" AND VLLM_RVV_VLEN AND
@@ -577,6 +578,7 @@ if (ENABLE_X86_ISA)
         "csrc/cpu/cpu_isa.cpp"
         "csrc/cpu/dnnl_kernels.cpp"
         "csrc/cpu/mamba_cpu.cpp"
+        "csrc/cpu/sampling_kernels.cpp"
         "csrc/cpu/torch_bindings.cpp"
         # TODO: Remove these files
         "csrc/cpu/activation.cpp"
@@ -594,6 +596,7 @@ if (ENABLE_X86_ISA)
         "csrc/cpu/cpu_isa.cpp"
         "csrc/cpu/mamba_cpu.cpp"
         "csrc/cpu/dnnl_kernels.cpp"
+        "csrc/cpu/sampling_kernels.cpp"
         "csrc/cpu/torch_bindings.cpp"
         # TODO: Remove these files
         "csrc/cpu/activation.cpp"
