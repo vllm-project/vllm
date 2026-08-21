e67191378457025c33b6d1f65a75801d04f4d510
Bread21234 <will.theboss@yahoo.com>
2026-08-21 01:32:01 -0500

    comment: reduced unnecessary verbage

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -660,8 +660,11 @@ function (define_extension_target MOD_NAME)
   install(TARGETS ${MOD_NAME} LIBRARY DESTINATION ${ARG_DESTINATION} COMPONENT ${MOD_NAME})
 endfunction()
 
-# Disable the CUDA 12.8+ static global template stub for heavily templated
-# kernel sources that don't need it.
+#
+# For source files that use CUDA global function templates, disable the
+# static global template stub that CUDA 12.8+ generates by default.
+# This avoids link-time bloat/errors from un-instantiated __global__
+# template stubs in heavily-templated kernels (e.g. Marlin).
 #
 # Usage:
 #   disable_static_global_template_stub(SRCS ${MY_KERNEL_SOURCES})
