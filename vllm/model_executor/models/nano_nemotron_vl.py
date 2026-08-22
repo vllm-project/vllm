7286bef27c1462c6aa72d8b49475206956c9a897
DarkLight1337 <tlleungac@connect.ust.hk>
2026-08-22 08:59:11 +0000

    [MM] Address comments on #53275
    Signed-off-by: DarkLight1337 <tlleungac@connect.ust.hk>

--- vllm/model_executor/models/nano_nemotron_vl.py
+++ vllm/model_executor/models/nano_nemotron_vl.py
@@ -734,7 +734,7 @@ class NanoNemotronVLMultiModalProcessor(
                 prompt_ids=prompt_ids,
                 mm_kwargs=mm_info.kwargs,
                 mm_prompt_updates=mm_info.prompt_updates,
-                is_update_applied=self.hf_processor_applies_updates,
+                is_update_applied=self.renderer_applies_updates,
             )
 
         mm_placeholder_ranges = {
