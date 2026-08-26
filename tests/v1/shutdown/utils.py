1fac21425749e1106394fc304da9b2c067d32519
zhenwei-intel <zhenwei.liu@intel.com>
2026-08-26 12:15:07 -0700

    [XPU][CI] increase timeout of shutdown
    Signed-off-by: zhenwei-intel <zhenwei.liu@intel.com>

--- tests/v1/shutdown/utils.py
+++ tests/v1/shutdown/utils.py
@@ -2,5 +2,9 @@
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 """Shutdown test utils"""
 
-SHUTDOWN_TEST_TIMEOUT_SEC = 120
+from vllm.platforms import current_platform
+
+# XPU tears down engine core processes and reclaims device memory more slowly,
+# so the default budget expires before wait_for_gpu_memory_to_clear finishes.
+SHUTDOWN_TEST_TIMEOUT_SEC = 240 if current_platform.is_xpu() else 120
 SHUTDOWN_TEST_THRESHOLD_BYTES = 2 * 2**30
