2102c6c9494de9b8c42bee17b1ccdd5a5a088817
LopezCastroRoberto <rocastro@redhat.com>
2026-08-31 20:34:42 +0000

    make NVFP4 packed stores type-correct
    Signed-off-by: LopezCastroRoberto <rocastro@redhat.com>

--- csrc/libtorch_stable/quantization/fp4/nvfp4_quant_kernels.cu
+++ csrc/libtorch_stable/quantization/fp4/nvfp4_quant_kernels.cu
@@ -113,7 +113,7 @@ __global__ void __launch_bounds__(512, VLLM_BLOCKS_PER_SM(512))
           } else {
             int64_t outOffset =
                 rowIdx * (outputCols / CVT_FP4_ELTS_PER_THREAD) + colIdx;
-            out[outOffset] = out_val;
+            reinterpret_cast<fp4_packed_t*>(out)[outOffset] = out_val;
           }
         }
       } else if (valid) {
@@ -125,7 +125,7 @@ __global__ void __launch_bounds__(512, VLLM_BLOCKS_PER_SM(512))
               (uint64_t(out_val.hi) << 32) | uint64_t(out_val.lo);
           reinterpret_cast<uint64_t*>(out)[outOffset >> 1] = packed64;
         } else {
-          out[inOffset] = out_val;
+          reinterpret_cast<fp4_packed_t*>(out)[inOffset] = out_val;
         }
       }
     }
