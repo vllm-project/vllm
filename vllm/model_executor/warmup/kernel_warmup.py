71de4c90c7530be7e8548aa49f1a78d7bce08dfb
Andrii Skliar <askliar@nvidia.com>
2026-09-01 12:00:51 +0200

    fix pre-commit
    Signed-off-by: Andrii Skliar <askliar@nvidia.com>

--- vllm/model_executor/warmup/kernel_warmup.py
+++ vllm/model_executor/warmup/kernel_warmup.py
@@ -41,9 +41,6 @@ from vllm.model_executor.warmup.qwen_triton_warmup import qwen_triton_warmup
 from vllm.model_executor.warmup.replayssm_warmup import (
     replayssm_autotune_warmup,
 )
-from vllm.model_executor.warmup.sparse_mla_triton_warmup import (
-    sparse_mla_triton_warmup,
-)
 from vllm.platforms import current_platform
 from vllm.utils.deep_gemm import is_deep_gemm_supported
 from vllm.utils.flashinfer import has_flashinfer
