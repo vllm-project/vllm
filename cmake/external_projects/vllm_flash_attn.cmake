3e93c806dc6f0ded4db2dbbe16ddbe5cdaabf008
Simon Veitner <sveitner@redhat.com>
2026-09-01 19:26:41 +0000

    [Build] Update bundled FlashAttention revision
    Signed-off-by: Simon Veitner <sveitner@redhat.com>

--- cmake/external_projects/vllm_flash_attn.cmake
+++ cmake/external_projects/vllm_flash_attn.cmake
@@ -39,7 +39,7 @@ else()
   FetchContent_Declare(
           vllm-flash-attn
           GIT_REPOSITORY https://github.com/vllm-project/flash-attention.git
-          GIT_TAG 06bdd47c0d0383daf6a2ff0c418faff9c6da16e5
+          GIT_TAG 88a8b4c9980b7060ac249742b4be8dd8a2769e9d
           GIT_PROGRESS TRUE
           # Don't share the vllm-flash-attn build between build types
           BINARY_DIR ${CMAKE_BINARY_DIR}/vllm-flash-attn
