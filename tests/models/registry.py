a4d7cd6cb59deef0734db0b80b8def6e7d1e0fc5
bowentan <btan2@andrew.cmu.edu>
2026-09-02 18:53:45 -0700

    Update tests/models/registry.py
    Signed-off-by: bowentan <btan2@andrew.cmu.edu>

--- tests/models/registry.py
+++ tests/models/registry.py
@@ -364,6 +364,9 @@ _TEXT_GENERATION_EXAMPLE_MODELS = {
             "random": "ai21labs/Jamba-tiny-random",
         },
     ),
+    "K2HorizonForCausalLM": _HfExamplesInfo(
+        "IFM/K2-Horizon-36B", trust_remote_code=True
+    ),
     "KimiLinearForCausalLM": _HfExamplesInfo(
         "moonshotai/Kimi-Linear-48B-A3B-Instruct", trust_remote_code=True
     ),
