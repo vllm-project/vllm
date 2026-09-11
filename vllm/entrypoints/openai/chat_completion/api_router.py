76a6c63c35590750fa7b23faa4e58143ff8ba749
wang.yuqi <yuqi.wang@daocloud.io>
2026-09-11 09:55:28 +0800

    init
    Signed-off-by: wang.yuqi <yuqi.wang@daocloud.io>

--- vllm/entrypoints/openai/chat_completion/api_router.py
+++ vllm/entrypoints/openai/chat_completion/api_router.py
@@ -14,7 +14,7 @@ from vllm.entrypoints.openai.chat_completion.protocol import (
     ChatCompletionResponse,
 )
 from vllm.entrypoints.openai.chat_completion.serving import OpenAIServingChat
-from vllm.entrypoints.openai.sse_keep_alive import with_sse_keep_alive
+from vllm.entrypoints.serve.utils.sse_keep_alive import with_sse_keep_alive
 from vllm.entrypoints.serve.engine.protocol import ErrorResponse
 from vllm.entrypoints.serve.utils.api_utils import (
     load_aware_call,
