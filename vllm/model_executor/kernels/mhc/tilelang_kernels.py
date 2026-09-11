13c242054dc717ebf2ec77c8866ef53aef761ea0
LopezCastroRoberto <rocastro@redhat.com>
2026-09-11 13:46:19 +0000

    update
    Signed-off-by: LopezCastroRoberto <rocastro@redhat.com>

--- vllm/model_executor/kernels/mhc/tilelang_kernels.py
+++ vllm/model_executor/kernels/mhc/tilelang_kernels.py
@@ -1211,9 +1211,9 @@ class MhcPreBigFuseTileLangKernel(
             middle_args = (residual_out, *output_args, norm_weight)
         elif compile_key.use_norm_weight:
             assert norm_weight is not None
-            middle_args = (*output_args, norm_weight)
+            middle_args = (*output_args, norm_weight, post_mix, post_mix)
         else:
-            middle_args = output_args
+            middle_args = (*output_args, post_mix, post_mix)
         norm_eps_args = (compile_key.norm_eps,) if compile_key.use_norm_weight else ()
         return (
             *common_args,
